08212cfc823fcede0c22a53aeb245da01877b06a
deraadt <deraadt@openbsd.org>
1995-10-23 06:10:29 +0000

    XXX is 4/300 really write-through?

--- sys/arch/sparc/sparc/pmap.c
+++ sys/arch/sparc/sparc/pmap.c
@@ -2862,8 +2862,7 @@ pmap_changeprot(pm, va, prot, wired)
 			 * the latter check deals with a writethrough cache
 			 * problem on the 4/300
 			 */
-			if ((vactype==VAC_WRITEBACK ||
-			    (vactype==VAC_WRITETHROUGH && cputyp==CPU_SUN4)) &&
+			if (vactype == VAC_WRITEBACK &&
 			    (tpte & (PG_U|PG_NC|PG_TYPE)) == (PG_U|PG_OBMEM))
 				cache_flush_page((int)va);
 		} else {
