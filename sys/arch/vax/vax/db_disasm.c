0a26516baf152543d2256c3ec1a6400867c9ecb3
deraadt <deraadt@openbsd.org>
1995-11-01 17:16:51 +0000

    Copyright stuff

--- sys/arch/vax/vax/db_disasm.c
+++ sys/arch/vax/vax/db_disasm.c
@@ -1,3 +1,36 @@
+/*	$NetBSD: db_disasm.c,v 1.1.2.1 1995/10/28 15:30:22 ragge Exp $ */
+/*
+ * Copyright (c) 1995 Ludd, University of Lule}, Sweden.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed at Ludd, University of 
+ *      Lule}, Sweden and its contributors.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include <sys/param.h>
 #include <sys/proc.h>
 #include <sys/reboot.h>
@@ -300,7 +333,7 @@ db_disasm(loc, altfmt)
 	char *i_pl;
 	int inr, i;
 
-	i_pl = loc;
+	i_pl = (char *)loc;
 	inr = *i_pl;
 
 	if (instr[*i_pl].nargs < 0) {
@@ -310,7 +343,7 @@ db_disasm(loc, altfmt)
 		printf("\t%s\t",instr[inr].insn);
 		i_pl++;
 		for (i=0;i<instr[inr].nargs;i++) {
-			i_pl = argprint(i_pl);
+			i_pl = (char *)argprint(i_pl);
 			if (i<instr[inr].nargs-1)
 				printf(",");
 		}
@@ -318,7 +351,7 @@ db_disasm(loc, altfmt)
 
 
 
-        return i_pl;
+        return (int)i_pl;
 }
 
 argprint(plats)
@@ -343,5 +376,5 @@ argprint(plats)
 	default:
 		printf("Oinpl. s{tt.");
 	}
-	return plats;
+	return (int)plats;
 }
