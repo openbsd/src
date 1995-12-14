68ef75eff995a0c4e73394c0bc9134ecfa185501
deraadt <deraadt@openbsd.org>
1995-12-14 06:19:25 +0000

    from netbsd; add copyright notice

--- sys/dev/ramdisk.h
+++ sys/dev/ramdisk.h
@@ -1,3 +1,34 @@
+/*	$NetBSD: ramdisk.h,v 1.2 1995/10/26 15:46:24 gwr Exp $	*/
+
+/*
+ * Copyright (c) 1995 Gordon W. Ross
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 4. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Gordon W. Ross
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * RAM-disk ioctl functions:
@@ -27,9 +58,9 @@ struct rd_conf {
  *     Disk image resident in kernel (patched in or loaded).
  *     Requires that the function: rd_set_kmem() is called to
  *     attach the (initialized) kernel memory to be used by the
- *     device.  This can be called by an "open hook" if this
+ *     device.  It can be initialized by an "open hook" if this
  *     driver is compiled with the RD_OPEN_HOOK option.
- *     No attempt will be made to free this memory.
+ *     No attempt will ever be made to free this memory.
  */
 #define RD_KMEM_ALLOCATED 2
 /*
@@ -43,9 +74,9 @@ struct rd_conf {
  *     (Like the MFS hack, but better! 8^)  Device operates
  *     only while the server has the control device open and
  *     continues to service I/O requests.  The process that
- *     does this setconf will become the I/O server. 
- *     Support for this configuration type is optional:
- *         option  RAMDISK_SERVER
+ *     does this setconf will become the I/O server.  This
+ *     configuration type can be disabled using:
+ *         option  RAMDISK_SERVER=0
  */
 
 #ifdef	_KERNEL
