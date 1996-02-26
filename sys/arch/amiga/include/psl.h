147272f33ead4dc1eec7d01f63e1709b00727a81
niklas <niklas@openbsd.org>
1996-02-26 20:17:47 +0000

    Add IPL_ and IST_ constants in preparation for the Amiga ISA-kit

--- sys/arch/amiga/include/psl.h
+++ sys/arch/amiga/include/psl.h
@@ -3,6 +3,20 @@
 #ifndef _MACHINE_PSL_H_
 #define _MACHINE_PSL_H_
 
+/* Interrupt priority `levels'; not mutually exclusive. */
+#define	IPL_NONE	-1
+#define	IPL_BIO		3	/* block I/O */
+#define	IPL_NET		3	/* network */
+#define	IPL_TTY		4	/* terminal */
+#define	IPL_CLOCK	4	/* clock */
+#define	IPL_IMP		4	/* memory allocation */
+
+/* Interrupt sharing types. */
+#define	IST_NONE	0	/* none */
+#define	IST_PULSE	1	/* pulsed */
+#define	IST_EDGE	2	/* edge-triggered */
+#define	IST_LEVEL	3	/* level-triggered */
+
 #include <m68k/psl.h>
 
 #endif
