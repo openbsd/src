edd3f88c3648e7897af2aaa2cce8dd86affec1bc
niklas <niklas@openbsd.org>
1996-02-29 13:12:22 +0000

    Fix typo in comment

--- sys/scsi/scsi_base.c
+++ sys/scsi/scsi_base.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD: scsi_base.c,v 1.6 1996/02/29 13:12:22 niklas Exp $	*/
 /*	$NetBSD: scsi_base.c,v 1.33 1996/02/14 21:47:14 christos Exp $	*/
 
 /*
@@ -422,7 +423,7 @@ retry:
 	 * TRY_AGAIN_LATER, (as for polling)
 	 * After the wakeup, we must still check if it succeeded
 	 * 
-	 * If we have a bp however, all the error proccessing
+	 * If we have a bp however, all the error processing
 	 * and the buffer code both expect us to return straight
 	 * to them, so as soon as the command is queued, return
 	 */
