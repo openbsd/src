edf7d9200352297f63dd89c7846a3cd0eb9b38fd
tholo <tholo@openbsd.org>
1996-03-02 00:35:17 +0000

    timespec members are now named tv_{,u}sec again

--- bin/rcp/rcp.c
+++ bin/rcp/rcp.c
@@ -1,4 +1,5 @@
 /*	$NetBSD: rcp.c,v 1.9 1995/03/21 08:19:06 cgd Exp $	*/
+/*	$OpenBSD: rcp.c,v 1.4 1996/03/02 00:35:17 tholo Exp $	*/
 
 /*
  * Copyright (c) 1983, 1990, 1992, 1993
@@ -421,7 +422,7 @@ syserr:			run_err("%s: %s", name, strerror(errno));
 			 * versions expecting microseconds.
 			 */
 			(void)snprintf(buf, sizeof(buf), "T%ld 0 %ld 0\n",
-			    stb.st_mtimespec.ts_sec, stb.st_atimespec.ts_sec);
+			    stb.st_mtimespec.tv_sec, stb.st_atimespec.tv_sec);
 			(void)write(rem, buf, strlen(buf));
 			if (response() < 0)
 				goto next;
@@ -485,7 +486,7 @@ rsource(name, statp)
 		last++;
 	if (pflag) {
 		(void)snprintf(path, sizeof(path), "T%ld 0 %ld 0\n",
-		    statp->st_mtimespec.ts_sec, statp->st_atimespec.ts_sec);
+		    statp->st_mtimespec.tv_sec, statp->st_atimespec.tv_sec);
 		(void)write(rem, path, strlen(path));
 		if (response() < 0) {
 			closedir(dirp);
