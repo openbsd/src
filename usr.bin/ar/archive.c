a907c6c63ed1e615d9c62c3218848b52c777b36b
tholo <tholo@openbsd.org>
1996-03-02 00:40:57 +0000

    timespec members are now named tv_{,u}sec again

--- usr.bin/ar/archive.c
+++ usr.bin/ar/archive.c
@@ -1,4 +1,5 @@
 /*	$NetBSD: archive.c,v 1.7 1995/03/26 03:27:46 glass Exp $	*/
+/*	$OpenBSD: archive.c,v 1.2 1996/03/02 00:40:57 tholo Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993, 1994
@@ -231,17 +232,17 @@ put_arobj(cfp, sb)
 				    name, OLDARMAXNAME, name);
 				(void)fflush(stderr);
 			}
-			(void)sprintf(hb, HDR3, name, sb->st_mtimespec.ts_sec,
+			(void)sprintf(hb, HDR3, name, sb->st_mtimespec.tv_sec,
 			    sb->st_uid, sb->st_gid, sb->st_mode, sb->st_size,
 			    ARFMAG);
 			lname = 0;
 		} else if (lname > sizeof(hdr->ar_name) || strchr(name, ' '))
 			(void)sprintf(hb, HDR1, AR_EFMT1, lname,
-			    sb->st_mtimespec.ts_sec, sb->st_uid, sb->st_gid,
+			    sb->st_mtimespec.tv_sec, sb->st_uid, sb->st_gid,
 			    sb->st_mode, sb->st_size + lname, ARFMAG);
 		else {
 			lname = 0;
-			(void)sprintf(hb, HDR2, name, sb->st_mtimespec.ts_sec,
+			(void)sprintf(hb, HDR2, name, sb->st_mtimespec.tv_sec,
 			    sb->st_uid, sb->st_gid, sb->st_mode, sb->st_size,
 			    ARFMAG);
 		}
