cae5aa72ceb7e31055d8e414e08a5fe28c34a697
tholo <tholo@openbsd.org>
1996-03-24 16:53:36 +0000

    Prefix usage of "long long" with a "/* LONGLONG */" comment to shut up lint(1) about it

--- sys/arch/pc532/include/types.h
+++ sys/arch/pc532/include/types.h
@@ -64,7 +64,9 @@ typedef	short			  int16_t;
 typedef	unsigned short		u_int16_t;
 typedef	int			  int32_t;
 typedef	unsigned int		u_int32_t;
+/* LONGLONG */
 typedef	long long		  int64_t;
+/* LONGLONG */
 typedef	unsigned long long	u_int64_t;
 
 typedef int32_t			register_t;
