2730d32fc804e5eef001ac2787b18fe761bcf835
mickey <mickey@openbsd.org>
1996-04-19 07:31:22 +0000

    Make kernel config again. at least isa,eisa,pci ocnfigs were affected.

--- sys/conf/param.c
+++ sys/conf/param.c
@@ -1,4 +1,5 @@
-/*	$NetBSD: param.c,v 1.15 1995/03/08 00:54:44 cgd Exp $	*/
+/*	$OpenBSD: param.c,v 1.2 1996/04/19 07:31:24 mickey Exp $	*/
+/*	$NetBSD: param.c,v 1.16 1996/03/12 03:08:40 mrg Exp $	*/
 
 /*
  * Copyright (c) 1980, 1986, 1989 Regents of the University of California.
@@ -75,6 +76,12 @@
  * Compiled with -DHZ=xx -DTIMEZONE=x -DDST=x -DMAXUSERS=xx
  */
 
+#ifndef TIMEZONE
+# define TIMEZONE 0
+#endif
+#ifndef DST
+# define DST 0
+#endif
 #ifndef HZ
 #define	HZ 100
 #endif
