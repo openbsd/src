a4b5ca493661d27adb039d9c2addfd13795ad56d
niklas <niklas@openbsd.org>
1996-04-25 10:20:16 +0000

    Add COMPAT_SVR4_NCR support for making the SVR4 emulation behave as NCR SVR4, currently this means that syscall#76 (rdebug) should return ENXIO instead of issueing a SIGSYS.  Also removed svr4_conf.h no longer used.

---
+++ sys/compat/svr4/svr4_ncr.c
@@ -0,0 +1,44 @@
+/*	$OpenBSD: svr4_ncr.c,v 1.1 1996/04/25 10:20:18 niklas Exp $	 */
+
+/*
+ * Copyright (c) 1995, 1996 Niklas Hallqvist
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Handle NCR SVR4 peculiarities
+ */
+
+#ifdef COMPAT_SVR4_NCR
+
+#include <sys/errno.h>
+
+int
+svr4_ncr_sys_rdebug()
+{
+	return ENXIO;
+}
+
+#endif /* COMPAT_SVR4_NCR */
