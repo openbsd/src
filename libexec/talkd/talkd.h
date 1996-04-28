eb1968a4de5a591b82263b1b9eb701c93ad95d1a
mickey <mickey@openbsd.org>
1996-04-28 23:57:46 +0000

    Add this stuff to keep compilers happy.

---
+++ libexec/talkd/talkd.h
@@ -0,0 +1,59 @@
+/*	$OpenBSD: talkd.h,v 1.1 1996/04/28 23:57:46 mickey Exp $	*/
+
+/*
+ * Copyright (c) 1983 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* talkd.c */
+extern int	debug;
+extern char	hostname[];
+
+/* table.c */
+CTL_MSG *find_request __P((CTL_MSG *));
+CTL_MSG *find_match __P((CTL_MSG *));
+void	insert_table __P((CTL_MSG *, CTL_RESPONSE *));
+int	new_id __P((void));
+int	delete_invite __P((int));
+
+/* process.c */
+void	process_request __P(( register CTL_MSG *, register CTL_RESPONSE *));
+void	do_announce __P((register CTL_MSG *, CTL_RESPONSE *));
+int	find_user __P((char *name, char *tty));
+
+/* announce.c */
+int	announce __P((CTL_MSG *,char *));
+int	announce_proc __P((CTL_MSG *,char *));
+
+/* print.c */
+void	print_request __P((char *,register CTL_MSG *));
+void	print_response __P((char *,register CTL_RESPONSE *));
+
