da704da8fb8162394e94892f5241e53d03bf5fa4
deraadt <deraadt@openbsd.org>
1996-05-01 12:51:12 +0000

    arm32 libc support

---
+++ lib/libc/arch/arm32/DEFS.h
@@ -0,0 +1,3 @@
+/* $NetBSD: DEFS.h,v 1.1 1996/02/16 20:47:54 mark Exp $ */
+
+#include <machine/asm.h>
---
+++ lib/libc/arch/arm32/gen/infinity.c
@@ -0,0 +1,9 @@
+/* $NetBSD: infinity.c,v 1.1 1996/02/16 20:48:18 mark Exp $ */
+
+/*
+ *	$Id: infinity.c,v 1.1 1996/05/01 12:51:18 deraadt Exp $
+ */
+
+#include <math.h>
+
+char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
