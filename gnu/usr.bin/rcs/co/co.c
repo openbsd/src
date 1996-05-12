101f73df295de1dd16d3e3dceca35dec01130165
mickey <mickey@openbsd.org>
1996-05-12 03:26:55 +0000

    fix usage string.

--- gnu/usr.bin/rcs/co/co.c
+++ gnu/usr.bin/rcs/co/co.c
@@ -28,11 +28,14 @@ Report problems and direct all questions to:
 
 /*
  * $Log: co.c,v $
+ * Revision 1.5  1996/05/12 03:26:55  mickey
+ * fix usage string.
+ *
  * Revision 1.4  1996/05/10 15:25:04  deraadt
  * Z not L
  *
  * Revision 1.3  1996/05/07 09:52:38  mickey
- * fix comments s/$Id: co.c,v 1.4 1996/05/10 15:25:04 deraadt Exp $/Id/, and usage string.
+ * fix comments s/$Id: co.c,v 1.5 1996/05/12 03:26:55 mickey Exp $/Id/, and usage string.
  *
  * Revision 1.2  1996/04/19 12:40:04  mickey
  * -L<string> option added to support LOCALID behaviour.
@@ -201,10 +204,10 @@ static struct hshentries *gendeltas;	/* deltas to be generated	*/
 static struct hshentry *targetdelta;	/* final delta to be generated	*/
 static struct stat workstat;
 
-mainProg(coId, "co", "$Id: co.c,v 1.4 1996/05/10 15:25:04 deraadt Exp $")
+mainProg(coId, "co", "$Id: co.c,v 1.5 1996/05/12 03:26:55 mickey Exp $")
 {
 	static char const cmdusage[] =
-		"\nco usage: co -{fIlMpqru}[rev] -ddate -jjoins -ksubst -sstate -T -w[who] -Vn -xsuff -zzone -LlocalId file ...";
+		"\nco usage: co -{fIlMpqru}[rev] -ddate -jjoins -ksubst -sstate -T -w[who] -Vn -xsuff -zzone -ZlocalId file ...";
 
 	char *a, **newargv;
 	char const *author, *date, *rev, *state;
