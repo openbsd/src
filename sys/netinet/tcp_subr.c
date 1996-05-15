5bb74303808a57fca3736f5eae2304983354082e
mickey <mickey@openbsd.org>
1996-05-15 09:32:01 +0000

    remove unnecessary "XXX it should be sysctl()'ed"

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_subr.c,v 1.4 1996/05/15 09:12:11 mickey Exp $	*/
+/*	$OpenBSD: tcp_subr.c,v 1.5 1996/05/15 09:32:01 mickey Exp $	*/
 /*	$NetBSD: tcp_subr.c,v 1.22 1996/02/13 23:44:00 christos Exp $	*/
 
 /*
@@ -72,7 +72,6 @@ int 	tcp_rttdflt = TCPTV_SRTTDFLT / PR_SLOWHZ;
  * pretty well destroys the VJ compression (any packet with a timestamp
  * different from the previous one can't be compressed), as well as adding
  * more overhead.
- * XXX "tcp_do_rfc1323" should be sysctl() changeable.
  * XXX And it should be a settable per route characteristic (with this just
  * used as the default).
  */
