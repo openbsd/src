a1206c649a661a88741b81dc988cec4e17f8eb7d
mickey <mickey@openbsd.org>
1996-05-15 09:12:11 +0000

    fix NetBSD PR#854. allow to overwrite rfc1323 option in config file.

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_subr.c,v 1.3 1996/03/03 22:30:47 niklas Exp $	*/
+/*	$OpenBSD: tcp_subr.c,v 1.4 1996/05/15 09:12:11 mickey Exp $	*/
 /*	$NetBSD: tcp_subr.c,v 1.22 1996/02/13 23:44:00 christos Exp $	*/
 
 /*
@@ -65,7 +65,21 @@
 /* patchable/settable parameters for tcp */
 int 	tcp_mssdflt = TCP_MSS;
 int 	tcp_rttdflt = TCPTV_SRTTDFLT / PR_SLOWHZ;
-int	tcp_do_rfc1323 = 1;
+
+/*
+ * Configure kernel with options "TCP_DO_RFC1323=0" to disable RFC1323 stuff.
+ * This is a good idea over slow SLIP/PPP links, because the timestamp
+ * pretty well destroys the VJ compression (any packet with a timestamp
+ * different from the previous one can't be compressed), as well as adding
+ * more overhead.
+ * XXX "tcp_do_rfc1323" should be sysctl() changeable.
+ * XXX And it should be a settable per route characteristic (with this just
+ * used as the default).
+ */
+#ifndef TCP_DO_RFC1323
+#define TCP_DO_RFC1323 1
+#endif
+int    tcp_do_rfc1323 = TCP_DO_RFC1323;
 
 #ifndef TCBHASHSIZE
 #define	TCBHASHSIZE	128
