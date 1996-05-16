779227548c8ec5f14faf4e4bb92dd9386eb2da72
chuck <chuck@openbsd.org>
1996-05-16 02:37:25 +0000

    import level 1 bootstrap from sun3/sparc port

---
+++ sys/arch/mvme68k/stand/bootxx/conf.c
@@ -0,0 +1,14 @@
+/*	$NetBSD: conf.c,v 1.2 1995/10/13 21:45:00 gwr Exp $	*/
+
+#include <sys/types.h>
+#include <machine/prom.h>
+
+#include "stand.h"
+#include "libsa.h"
+
+struct devsw devsw[] = {
+	{ "bugsc", bugscstrategy, bugscopen, bugscclose, bugscioctl },
+};
+int     ndevs = (sizeof(devsw)/sizeof(devsw[0]));
+
+int debug;
---
+++ sys/arch/mvme68k/stand/bootxx/version.c
@@ -0,0 +1,8 @@
+/*	$OpenBSD: version.c,v 1.1 1996/05/16 02:37:26 chuck Exp $ */
+
+/*
+ * make a random change to this file when you want the bootblock
+ * revision to increase. like change this x to a y, or something.
+ */
+
+char *version = "$Revision: 1.1 $";
