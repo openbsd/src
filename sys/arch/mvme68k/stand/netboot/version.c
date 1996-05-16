234e76210b661f7514e8f76ae3d29fe345234bd1
chuck <chuck@openbsd.org>
1996-05-16 02:57:04 +0000

    up version #

--- sys/arch/mvme68k/stand/netboot/version.c
+++ sys/arch/mvme68k/stand/netboot/version.c
@@ -1,9 +1,8 @@
-/*	$OpenBSD: version.c,v 1.3 1996/04/28 10:49:26 deraadt Exp $ */
+/*	$OpenBSD: version.c,v 1.4 1996/05/16 02:57:04 chuck Exp $ */
 
 /*
- *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
- *
- *	1.1
+ * make a random change to this file when you want the bootblock
+ * revision to increase. like change this x to a z, or something.
  */
 
-char *version = "$Revision: 1.3 $";
+char *version = "$Revision: 1.4 $";
