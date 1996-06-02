34b5e5fd8a0b082adc1256ea819556a16cf10412
niklas <niklas@openbsd.org>
1996-06-02 17:23:02 +0000

    removed an unused variable

--- sys/dev/isa/ast.c
+++ sys/dev/isa/ast.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ast.c,v 1.11 1996/05/26 00:27:11 deraadt Exp $	*/
+/*	$OpenBSD: ast.c,v 1.12 1996/06/02 17:23:02 niklas Exp $	*/
 /*	$NetBSD: ast.c,v 1.28 1996/05/12 23:51:45 mycroft Exp $	*/
 
 /*
@@ -146,7 +146,6 @@ astattach(parent, self, aux)
 	struct ast_softc *sc = (void *)self;
 	struct isa_attach_args *ia = aux;
 	struct commulti_attach_args ca;
-	bus_chipset_tag_t bc = ia->ia_bc;
 	int i;
 
 	sc->sc_bc = ia->ia_bc;
