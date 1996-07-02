e7e2e26dce4694ac51fefd78240d146e7dd3b7ce
deraadt <deraadt@openbsd.org>
1996-07-02 23:05:38 +0000

    pretty the comment

--- lib/libc/crypt/md5crypt.c
+++ lib/libc/crypt/md5crypt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: md5crypt.c,v 1.2 1996/06/03 22:20:37 niklas Exp $ */
+/*	$OpenBSD: md5crypt.c,v 1.3 1996/07/02 23:05:38 deraadt Exp $ */
 
 /*
  * ----------------------------------------------------------------------------
@@ -11,7 +11,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: md5crypt.c,v 1.2 1996/06/03 22:20:37 niklas Exp $";
+static char rcsid[] = "$OpenBSD: md5crypt.c,v 1.3 1996/07/02 23:05:38 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <unistd.h>
@@ -44,12 +44,12 @@ md5crypt(pw, salt)
 	register const char *pw;
 	register const char *salt;
 {
-	static char	*magic = "$1$";	/*
-						 * This string is magic for
-						 * this algorithm.  Having
-						 * it this way, we can get
-						 * get better later on
-						 */
+	/*
+	 * This string is magic for this algorithm.  Having
+	 * it this way, we can get get better later on
+	 */
+	static char	*magic = "$1$";	
+
 	static char     passwd[120], *p;
 	static const char *sp,*ep;
 	unsigned char	final[16];
