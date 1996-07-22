c6a1eb7c01c50dd35d89edc2b4f3221abc758551
deraadt <deraadt@openbsd.org>
1996-07-22 09:11:32 +0000

    make COPTS work in bsd.lib.mk too

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.lib.mk,v 1.6 1996/05/25 09:03:43 pefo Exp $
+#	$OpenBSD: bsd.lib.mk,v 1.7 1996/07/22 09:11:32 deraadt Exp $
 #	$NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 #	@(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
 
@@ -82,6 +82,8 @@ SHLIB_MINOR != . ${.CURDIR}/shlib_version ; echo $$minor
 	    ${AS} -k -o ${.TARGET}.o
 	@${LD} -x -r ${.TARGET}.o -o ${.TARGET}
 	@rm -f ${.TARGET}.o
+
+CFLAGS+=	${COPTS}
 
 .if !defined(PICFLAG) && (${MACHINE_ARCH} != "mips")
 PICFLAG=-fpic
