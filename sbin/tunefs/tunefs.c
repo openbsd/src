f53698bb5e3d7ae38e16871224287ca2580e095e
deraadt <deraadt@openbsd.org>
1996-08-02 11:23:34 +0000

    add -p option to show current values; this information is available from dumpfs but not readily obvious. from freebsd by peter@haywire.dialix.com

--- sbin/tunefs/tunefs.c
+++ sbin/tunefs/tunefs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tunefs.c,v 1.2 1996/06/23 14:32:50 deraadt Exp $	*/
+/*	$OpenBSD: tunefs.c,v 1.3 1996/08/02 11:23:35 deraadt Exp $	*/
 /*	$NetBSD: tunefs.c,v 1.10 1995/03/18 15:01:31 cgd Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)tunefs.c	8.2 (Berkeley) 4/19/94";
 #else
-static char rcsid[] = "$OpenBSD: tunefs.c,v 1.2 1996/06/23 14:32:50 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: tunefs.c,v 1.3 1996/08/02 11:23:35 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -81,6 +81,7 @@ void bwrite(daddr_t, char *, int);
 int bread(daddr_t, char *, int);
 void getsb(struct fs *, char *);
 void usage __P((void));
+void printfs __P((void));
 
 int
 main(argc, argv)
@@ -122,6 +123,10 @@ again:
 			case 'A':
 				Aflag++;
 				continue;
+
+			case 'p':
+				printfs();
+				exit(0);
 
 			case 'a':
 				name = "maximum contiguous block count";
@@ -239,6 +244,7 @@ usage()
 	fprintf(stderr, "\t-e maximum blocks per file in a cylinder group\n");
 	fprintf(stderr, "\t-m minimum percentage of free space\n");
 	fprintf(stderr, "\t-o optimization preference (`space' or `time')\n");
+	fprintf(stderr, "\t-p no change - just prints current tuneable settings\n");
 	exit(2);
 }
 
@@ -256,6 +262,27 @@ getsb(fs, file)
 	if (fs->fs_magic != FS_MAGIC)
 		err(5, "%s: bad magic number", file);
 	dev_bsize = fs->fs_fsize / fsbtodb(fs, 1);
+}
+
+void
+printfs()
+{
+	warnx("maximum contiguous block count: (-a)               %d",
+	      sblock.fs_maxcontig);
+	warnx("rotational delay between contiguous blocks: (-d)   %d ms",
+	      sblock.fs_rotdelay);
+	warnx("maximum blocks per file in a cylinder group: (-e)  %d",
+	      sblock.fs_maxbpg);
+	warnx("minimum percentage of free space: (-m)             %d%%",
+	      sblock.fs_minfree);
+	warnx("optimization preference: (-o)                      %s",
+	      sblock.fs_optim == FS_OPTSPACE ? "space" : "time");
+	if (sblock.fs_minfree >= MINFREE &&
+	    sblock.fs_optim == FS_OPTSPACE)
+		warnx(OPTWARN, "time", ">=", MINFREE);
+	if (sblock.fs_minfree < MINFREE &&
+	    sblock.fs_optim == FS_OPTTIME)
+		warnx(OPTWARN, "space", "<", MINFREE);
 }
 
 void
