d990a191975bf7a0a77f35c7d1d13155d1f83355
downsj <downsj@openbsd.org>
1996-08-12 02:10:35 +0000

    Revert part of that last change; mapdev()/mapiodev() should be done all at once.

--- sys/arch/sparc/dev/amd7930.c
+++ sys/arch/sparc/dev/amd7930.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: amd7930.c,v 1.5 1996/08/12 01:19:38 downsj Exp $	*/
+/*	$OpenBSD: amd7930.c,v 1.6 1996/08/12 02:10:35 downsj Exp $	*/
 /*	$NetBSD: amd7930.c,v 1.10 1996/03/31 22:38:29 pk Exp $	*/
 
 /*
@@ -306,8 +306,9 @@ amd7930attach(parent, self, args)
 	}
 	pri = ra->ra_intr[0].int_pri;
 	printf(" pri %d, softpri %d\n", pri, PIL_AUSOFT);
-	amd = (volatile struct amd7930 *)(ra->ra_vaddr ? ra->ra_vaddr :
-		mapiodev(ra->ra_reg, 0, sizeof (*amd), ca->ca_bustype));
+	amd = (volatile struct amd7930 *)(ra->ra_vaddr ?
+		ra->ra_vaddr : mapiodev(ra->ra_reg, 0, sizeof (*amd),
+					ca->ca_bustype));
 
 	sc->sc_map.mr_mmr1 = AMD_MMR1_GX | AMD_MMR1_GER |
 			     AMD_MMR1_GR | AMD_MMR1_STG;
