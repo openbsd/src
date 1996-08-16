febfb4a3fcfbedb0bd9e35619403e305f5bf2d07
mickey <mickey@openbsd.org>
1996-08-16 09:15:57 +0000

    initial commit of Free's netipx w/ some changes. ip<-->ipx encapsulations does not work still. spx suspected to do too.

---
+++ sys/netipx/ipx.c
@@ -0,0 +1,384 @@
+/*	$OpenBSD: ipx.c,v 1.1 1996/08/16 09:15:57 mickey Exp $	*/
+/*	$NOWHERE: ipx.c,v 1.2 1996/05/07 09:49:37 mickey Exp $	*/
+
+/*-
+ *
+ * Copyright (c) 1996 Michael Shalayeff
+ * Copyright (c) 1995, Mike Mitchell
+ * Copyright (c) 1984, 1985, 1986, 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ *	@(#)ipx.c
+ *
+ * from FreeBSD Id: ipx.c,v 1.4 1996/03/11 15:13:46 davidg Exp
+ */
+
+#include <sys/param.h>
+#include <sys/queue.h>
+#include <sys/systm.h>
+#include <sys/mbuf.h>
+#include <sys/ioctl.h>
+#include <sys/protosw.h>
+#include <sys/errno.h>
+#include <sys/socket.h>
+#include <sys/socketvar.h>
+
+#include <net/if.h>
+#include <net/route.h>
+
+#include <netipx/ipx.h>
+#include <netipx/ipx_if.h>
+
+int ipx_interfaces;
+
+/*
+ * Generic internet control operations (ioctl's).
+ */
+/* ARGSUSED */
+int
+ipx_control(so, cmd, data, ifp)
+	struct socket *so;
+	u_long	cmd;
+	caddr_t	data;
+	register struct ifnet *ifp;
+{
+	register struct ifreq *ifr = (struct ifreq *)data;
+	register struct ipx_aliasreq *ifra = (struct ipx_aliasreq *)data;
+	register struct ipx_ifaddr *ia;
+	struct ifaddr *ifa;
+	int dstIsNew, hostIsNew;
+	int error = 0;
+
+	/*
+	 * Find address for this interface, if it exists.
+	 */
+	if (ifp)
+		for (ia = ipx_ifaddr.tqh_first; ia; ia = ia->ia_list.tqe_next)
+			if (ia->ia_ifp == ifp)
+				break;
+
+	switch (cmd) {
+	case SIOCAIFADDR:
+	case SIOCDIFADDR:
+		if (ifra->ifra_addr.sipx_family == AF_IPX)
+			for (; ia; ia = ia->ia_list.tqe_next) {
+				if (ia->ia_ifp == ifp  &&
+				    ipx_neteq(ia->ia_addr.sipx_addr,
+					      ifra->ifra_addr.sipx_addr))
+					break;
+			}
+		if (cmd == SIOCDIFADDR && ia == 0)
+			return (EADDRNOTAVAIL);
+		/* FALLTHROUGH */
+
+	case SIOCSIFADDR:
+	case SIOCSIFDSTADDR:
+		if ((so->so_state & SS_PRIV) == 0)
+			return (EPERM);
+
+		if (ia == (struct ipx_ifaddr *)NULL) {
+			ia = (struct ipx_ifaddr *)
+				malloc(sizeof *ia, M_IFADDR, M_WAITOK);
+			if (ia == (struct ipx_ifaddr *)NULL)
+				return (ENOBUFS);
+			bzero((caddr_t)ia, sizeof(*ia));
+			TAILQ_INSERT_TAIL(&ifp->if_addrlist,
+				(struct ifaddr *)ia, ifa_list);
+			TAILQ_INSERT_TAIL(&ipx_ifaddr, ia, ia_list);
+			ia->ia_ifp = ifp;
+			ia->ia_ifa.ifa_addr = (struct sockaddr *)&ia->ia_addr;
+
+			ia->ia_ifa.ifa_netmask =
+				(struct sockaddr *)&ipx_netmask;
+
+			ia->ia_ifa.ifa_dstaddr =
+				(struct sockaddr *)&ia->ia_dstaddr;
+			if (ifp->if_flags & IFF_BROADCAST) {
+				ia->ia_broadaddr.sipx_family = AF_IPX;
+				ia->ia_broadaddr.sipx_len = sizeof(ia->ia_addr);
+				ia->ia_broadaddr.sipx_addr.ipx_host = ipx_broadhost;
+			}
+			ipx_interfaces++;
+		}
+		break;
+
+	case SIOCSIFBRDADDR:
+		if ((so->so_state & SS_PRIV) == 0)
+			return (EPERM);
+		/* FALLTHROUGH */
+
+	case SIOCGIFADDR:
+	case SIOCGIFDSTADDR:
+	case SIOCGIFBRDADDR:
+		if (ia == (struct ipx_ifaddr *)NULL)
+			return (EADDRNOTAVAIL);
+		break;
+	}
+
+	switch (cmd) {
+
+	case SIOCGIFADDR:
+		*(struct sockaddr_ipx *)&ifr->ifr_addr = ia->ia_addr;
+		return (0);
+
+	case SIOCGIFBRDADDR:
+		if ((ifp->if_flags & IFF_BROADCAST) == 0)
+			return (EINVAL);
+		*(struct sockaddr_ipx *)&ifr->ifr_dstaddr = ia->ia_broadaddr;
+		return (0);
+
+	case SIOCGIFDSTADDR:
+		if ((ifp->if_flags & IFF_POINTOPOINT) == 0)
+			return (EINVAL);
+		*(struct sockaddr_ipx *)&ifr->ifr_dstaddr = ia->ia_dstaddr;
+		return (0);
+
+	case SIOCSIFDSTADDR:
+		if ((ifp->if_flags & IFF_POINTOPOINT) == 0)
+			return (EINVAL);
+		if (ia->ia_flags & IFA_ROUTE) {
+			rtinit(&(ia->ia_ifa), (int)RTM_DELETE, RTF_HOST);
+			ia->ia_flags &= ~IFA_ROUTE;
+		}
+		if (ifp->if_ioctl) {
+			error = (*ifp->if_ioctl)(ifp, SIOCSIFDSTADDR, (void *)ia);
+			if (error)
+				return (error);
+		}
+		*(struct sockaddr *)&ia->ia_dstaddr = ifr->ifr_dstaddr;
+		return (0);
+
+	case SIOCSIFADDR:
+		return (ipx_ifinit(ifp, ia,
+				(struct sockaddr_ipx *)&ifr->ifr_addr, 1));
+
+	case SIOCDIFADDR:
+		ipx_ifscrub(ifp, ia);
+		TAILQ_REMOVE(&ifp->if_addrlist, (struct ifaddr *)ia, ifa_list);
+		TAILQ_REMOVE(&ipx_ifaddr, ia, ia_list);
+		IFAFREE((&ia->ia_ifa));
+		if (0 == --ipx_interfaces) {
+			/*
+			 * We reset to virginity and start all over again
+			 */
+			ipx_thishost = ipx_zerohost;
+		}
+		return (0);
+	
+	case SIOCAIFADDR:
+		dstIsNew = 0; hostIsNew = 1;
+		if (ia->ia_addr.sipx_family == AF_IPX) {
+			if (ifra->ifra_addr.sipx_len == 0) {
+				ifra->ifra_addr = ia->ia_addr;
+				hostIsNew = 0;
+			} else if (ipx_neteq(ifra->ifra_addr.sipx_addr,
+					 ia->ia_addr.sipx_addr))
+				hostIsNew = 0;
+		}
+		if ((ifp->if_flags & IFF_POINTOPOINT) &&
+		    (ifra->ifra_dstaddr.sipx_family == AF_IPX)) {
+			if (hostIsNew == 0)
+				ipx_ifscrub(ifp, ia);
+			ia->ia_dstaddr = ifra->ifra_dstaddr;
+			dstIsNew  = 1;
+		}
+		if (ifra->ifra_addr.sipx_family == AF_IPX &&
+					    (hostIsNew || dstIsNew))
+			error = ipx_ifinit(ifp, ia, &ifra->ifra_addr, 0);
+		return (error);
+
+	default:
+		if (ifp->if_ioctl == 0)
+			return (EOPNOTSUPP);
+		return ((*ifp->if_ioctl)(ifp, cmd, data));
+	}
+}
+
+/*
+ * Delete any previous route for an old address.
+ */
+void
+ipx_ifscrub(ifp, ia)
+	register struct ifnet *ifp;
+	register struct ipx_ifaddr *ia; 
+{
+	if (ia->ia_flags & IFA_ROUTE) {
+		if (ifp->if_flags & IFF_POINTOPOINT) {
+			rtinit(&(ia->ia_ifa), (int)RTM_DELETE, RTF_HOST);
+		} else
+			rtinit(&(ia->ia_ifa), (int)RTM_DELETE, 0);
+		ia->ia_flags &= ~IFA_ROUTE;
+	}
+}
+
+/*
+ * Initialize an interface's IPX address
+ * and routing table entry.
+ */
+int
+ipx_ifinit(ifp, ia, sipx, scrub)
+	register struct ifnet *ifp;
+	register struct ipx_ifaddr *ia;
+	register struct sockaddr_ipx *sipx;
+	int scrub;
+{
+	struct sockaddr_ipx oldaddr;
+	register union ipx_host *h = &ia->ia_addr.sipx_addr.ipx_host;
+	int s = splimp(), error;
+
+	/*
+	 * Set up new addresses.
+	 */
+	oldaddr = ia->ia_addr;
+	ia->ia_addr = *sipx;
+
+	/*
+	 * The convention we shall adopt for naming is that
+	 * a supplied address of zero means that "we don't care".
+	 * if there is a single interface, use the address of that
+	 * interface as our 6 byte host address.
+	 * if there are multiple interfaces, use any address already
+	 * used.
+	 *
+	 * Give the interface a chance to initialize
+	 * if this is its first address,
+	 * and to validate the address if necessary.
+	 */
+	if (ipx_hosteqnh(ipx_thishost, ipx_zerohost)) {
+		if (ifp->if_ioctl &&
+		     (error = (*ifp->if_ioctl)(ifp, SIOCSIFADDR, (void *)ia))) {
+			ia->ia_addr = oldaddr;
+			splx(s);
+			return (error);
+		}
+		ipx_thishost = *h;
+	} else if (ipx_hosteqnh(sipx->sipx_addr.ipx_host, ipx_zerohost)
+	    || ipx_hosteqnh(sipx->sipx_addr.ipx_host, ipx_thishost)) {
+		*h = ipx_thishost;
+		if (ifp->if_ioctl &&
+		     (error = (*ifp->if_ioctl)(ifp, SIOCSIFADDR, (void *)ia))) {
+			ia->ia_addr = oldaddr;
+			splx(s);
+			return (error);
+		}
+		if (!ipx_hosteqnh(ipx_thishost,*h)) {
+			ia->ia_addr = oldaddr;
+			splx(s);
+			return (EINVAL);
+		}
+	} else {
+		ia->ia_addr = oldaddr;
+		splx(s);
+		return (EINVAL);
+	}
+	ia->ia_ifa.ifa_metric = ifp->if_metric;
+	/*
+	 * Add route for the network.
+	 */
+	if (scrub) {
+		ia->ia_ifa.ifa_addr = (struct sockaddr *)&oldaddr;
+		ipx_ifscrub(ifp, ia);
+		ia->ia_ifa.ifa_addr = (struct sockaddr *)&ia->ia_addr;
+	}
+	if (ifp->if_flags & IFF_POINTOPOINT)
+		rtinit(&(ia->ia_ifa), (int)RTM_ADD, RTF_HOST|RTF_UP);
+	else {
+		ia->ia_broadaddr.sipx_addr.ipx_net = ia->ia_addr.sipx_addr.ipx_net;
+		rtinit(&(ia->ia_ifa), (int)RTM_ADD, RTF_UP);
+	}
+	ia->ia_flags |= IFA_ROUTE;
+	splx(s);
+	return (0);
+}
+
+/*
+ * Return address info for specified IPX network.
+ */
+struct ipx_ifaddr *
+ipx_iaonnetof(dst)
+	register struct ipx_addr *dst;
+{
+	register struct ipx_ifaddr *ia;
+	register struct ipx_addr *compare;
+	register struct ifnet *ifp;
+	struct ipx_ifaddr *ia_maybe = 0;
+	union ipx_net net = dst->ipx_net;
+
+	for (ia = ipx_ifaddr.tqh_first; ia; ia = ia->ia_list.tqe_next) {
+		if ((ifp = ia->ia_ifp)) {
+			if (ifp->if_flags & IFF_POINTOPOINT) {
+				compare = &satoipx_addr(ia->ia_dstaddr);
+				if (ipx_hosteq(*dst, *compare))
+					return (ia);
+				if (ipx_neteqnn(net, ia->ia_addr.sipx_addr.ipx_net))
+					ia_maybe = ia;
+			} else {
+				if (ipx_neteqnn(net, ia->ia_addr.sipx_addr.ipx_net))
+					return (ia);
+			}
+		}
+	}
+	return (ia_maybe);
+}
+
+#ifdef	IPXDEBUG
+struct ipx_addr
+ipx_addr(str)
+	const char *str;
+{
+	struct ipx_addr	ret;
+
+
+	return ret;
+}
+
+char *
+ipx_ntoa(ipx)
+	struct ipx_addr	ipx;
+{
+	static char	bufs[4][4+1+(3*6)+5], *cbuf = bufs[4];
+
+	if (cbuf == bufs[4])
+		cbuf = bufs[0];
+	else
+		cbuf++;
+
+	sprintf(cbuf, "%04x.%02x:%02x:%02x:%02x:%02x:%02x.%u",
+		ipx.ipx_net.l_net,
+		ipx.ipx_host.c_host[0], ipx.ipx_host.c_host[1],
+		ipx.ipx_host.c_host[2], ipx.ipx_host.c_host[3],
+		ipx.ipx_host.c_host[4], ipx.ipx_host.c_host[5],
+		ipx.ipx_port);
+
+	return cbuf;
+}
+
+#endif
---
+++ sys/netipx/ipx.h
@@ -0,0 +1,233 @@
+/*	$OpenBSD: ipx.h,v 1.1 1996/08/16 09:15:58 mickey Exp $	*/
+/*	$NOWHERE: ipx.h,v 1.3 1996/05/07 12:32:01 mickey Exp $	*/
+
+/*-
+ *
+ * Copyright (c) 1996 Michael Shalayeff
+ * Copyright (c) 1995, Mike Mitchell
+ * Copyright (c) 1984, 1985, 1986, 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)ipx.h
+ *
+ * from FreeBSD Id: ipx.h,v 1.7 1996/01/30 22:58:48 mpp Exp
+ */
+
+#ifndef _NETIPX_IPX_H_
+#define	_NETIPX_IPX_H_
+
+/*
+ * Constants and Structures
+ */
+
+/*
+ * Protocols
+ */
+#define IPXPROTO_UNKWN	0		/* Unknown */
+#define IPXPROTO_RI	1		/* RIP Routing Information */
+#define IPXPROTO_ECHO	2		/* Echo Protocol */
+#define IPXPROTO_ERROR	3		/* Error Protocol */
+#define IPXPROTO_PXP	4		/* PXP Packet Exchange */
+#define IPXPROTO_SPX	5		/* SPX Sequenced Packet */
+#define IPXPROTO_NCP	17		/* NCP NetWare Core */
+#define IPXPROTO_PPROP	20		/* complicated flood w/ bcast */
+#define IPXPROTO_RAW	255		/* Placemarker*/
+#define IPXPROTO_MAX	256		/* Placemarker*/
+
+/*
+ * Port/Socket numbers: network standard functions
+ */
+
+#define IPXPORT_RI	1		/* NS RIP Routing Information */
+#define IPXPORT_ECHO	2		/* NS Echo */
+#define IPXPORT_RE	3		/* NS Router Error */
+#define IPXPORT_FSP	0x0451		/* NW FSP File Service */
+#define IPXPORT_SAP	0x0452		/* NW SAP Service Advertising */
+#define IPXPORT_RIP	0x0453		/* NW RIP Routing Information */
+#define IPXPORT_NETBIOS	0x0455		/* NW NetBIOS */
+#define IPXPORT_DIAGS	0x0456		/* NW Diagnostics */
+#define IPXPORT_WDOG	0x4001		/* NW Watchdog Packets */
+#define IPXPORT_SHELL	0x4003		/* NW Shell Socket */
+#define IPXPORT_MAX	0x8000		/* Maximum User Addressable Port */
+
+/* flags passed to ipx_outputfl as last parameter */
+
+#define	IPX_FORWARDING		0x1	/* most of ipx header exists */
+#define	IPX_ROUTETOIF		0x10	/* same as SO_DONTROUTE */
+#define	IPX_ALLOWBROADCAST	SO_BROADCAST	/* can send broadcast packets */
+
+#define IPX_MAXHOPS		15
+
+/* flags passed to get/set socket option */
+#define	SO_HEADERS_ON_INPUT	1
+#define	SO_HEADERS_ON_OUTPUT	2
+#define	SO_DEFAULT_HEADERS	3
+#define	SO_LAST_HEADER		4
+#define	SO_IPXIP_ROUTE		5
+#define SO_SEQNO		6
+#define	SO_ALL_PACKETS		7
+#define SO_MTU			8
+#define SO_IPXTUN_ROUTE		9
+
+/*
+ * IPX addressing
+ */
+#define	IPX_HOSTADDRLEN	6
+#define	IPX_NETADDRLEN	4
+#define	XXX	__attribute__((packed))
+
+typedef
+union ipx_host {
+	u_int8_t	c_host[IPX_HOSTADDRLEN]		XXX;
+	u_int16_t	s_host[IPX_HOSTADDRLEN/2]	XXX;
+} ipx_host_t;
+
+typedef
+union ipx_net {
+	u_int8_t	c_net[IPX_NETADDRLEN]		XXX;
+	u_int16_t	s_net[IPX_NETADDRLEN/2]		XXX;
+	u_int32_t	l_net				XXX;
+} ipx_net_t;
+
+typedef	u_int16_t	ipx_port_t;
+
+typedef
+struct ipx_addr {
+	ipx_net_t	ipx_net				XXX;
+	ipx_host_t	ipx_host			XXX;
+	ipx_port_t	ipx_port			XXX;
+} ipx_addr_t;
+
+/*
+ * Socket address
+ */
+struct sockaddr_ipx {
+	u_int8_t	sipx_len;
+	u_int8_t	sipx_family;
+	struct ipx_addr	sipx_addr;
+	u_int16_t	sipx_type;
+};
+#define sipx_net  sipx_addr.ipx_net
+#define sipx_network  sipx_addr.ipx_net.l_net
+#define sipx_node sipx_addr.ipx_host.c_host
+#define sipx_port sipx_addr.ipx_port
+
+/*
+ * Definitions for IPX Internet Datagram Protocol
+ */
+struct ipx {
+	u_int16_t	ipx_sum XXX;	/* Checksum */
+	u_int16_t	ipx_len XXX;	/* Length, in bytes, including header */
+	u_int8_t	ipx_tc  XXX;	/* Transport Control (i.e. hop count) */
+	u_int8_t	ipx_pt  XXX;	/* Packet Type (i.e. lev 2 protocol) */
+	ipx_addr_t	ipx_dna XXX;	/* Destination Network Address */
+	ipx_addr_t	ipx_sna XXX;	/* Source Network Address */
+};
+#undef	XXX
+
+#define ipx_neteqnn(a,b) \
+	(((a).s_net[0]==(b).s_net[0]) && ((a).s_net[1]==(b).s_net[1]))
+#define ipx_neteq(a,b) ipx_neteqnn((a).ipx_net, (b).ipx_net)
+#define satoipx_addr(sa) (((struct sockaddr_ipx *)&(sa))->sipx_addr)
+#define ipx_hosteqnh(s,t) ((s).s_host[0] == (t).s_host[0] && \
+	(s).s_host[1] == (t).s_host[1] && (s).s_host[2] == (t).s_host[2])
+#define ipx_hosteq(s,t) (ipx_hosteqnh((s).ipx_host,(t).ipx_host))
+#define ipx_nullnet(x) \
+	(((x).ipx_net.s_net[0]==0) && ((x).ipx_net.s_net[1]==0))
+#define ipx_nullhost(x) (((x).ipx_host.s_host[0]==0) && \
+	((x).ipx_host.s_host[1]==0) && ((x).ipx_host.s_host[2]==0))
+#define ipx_wildnet(x) (((x).ipx_net.s_net[0]==0xffff) && \
+	((x).ipx_net.s_net[1]==0xffff))
+#define ipx_wildhost(x) (((x).ipx_host.s_host[0]==0xffff) && \
+	((x).ipx_host.s_host[1]==0xffff) && ((x).ipx_host.s_host[2]==0xffff))
+
+#ifdef _KERNEL
+
+#define	satosipx(a)	((struct sockaddr_ipx *)(a))
+#define	sipxtosa(a)	((struct sockaddr *)(a))
+
+extern int ipxcksum;
+extern struct domain ipxdomain;
+extern struct sockaddr_ipx ipx_netmask;
+extern struct sockaddr_ipx ipx_hostmask;
+
+extern union ipx_host	ipx_thishost;
+extern union ipx_net	ipx_zeronet;
+extern union ipx_host	ipx_zerohost;
+extern union ipx_net	ipx_broadnet;
+extern union ipx_host	ipx_broadhost;
+
+extern u_long ipx_pexseq;
+extern u_char ipxctlerrmap[];
+
+struct route;
+struct sockaddr;
+struct socket;
+struct ipxpcb;
+void	ipx_abort __P((struct ipxpcb *ipxp));
+u_short	ipx_cksum __P((struct mbuf *m, int len));
+int	ipx_control __P((struct socket *so, u_long cmd, caddr_t data,
+			 struct ifnet *ifp));
+void	*ipx_ctlinput __P((int cmd, struct sockaddr *arg_as_sa, void *dummy));
+int	ipx_ctloutput __P((int req, struct socket *so, int level, int name,
+			   struct mbuf **value));
+int	ipx_do_route __P((struct ipx_addr *src, struct route *ro));
+void	ipx_drop __P((struct ipxpcb *ipxp, int errno));
+void	ipx_forward __P((struct mbuf *m));
+void	ipx_init __P((void));
+void	ipx_input __P((struct mbuf *, ...));
+void	ipxintr __P((void));
+int	ipx_output __P((struct mbuf *m0, ...));
+int	ipx_outputfl __P((struct mbuf *m0, struct route *ro, int flags));
+int	ipx_raw_usrreq __P((struct socket *so, int req, struct mbuf *m,
+			    struct mbuf *nam, struct mbuf *control));
+void	ipx_undo_route __P((struct route *ro));
+int	ipx_usrreq __P((struct socket *so, int req, struct mbuf *m,
+			struct mbuf *nam, struct mbuf *control));
+void	ipx_watch_output __P((struct mbuf *m, struct ifnet *ifp));
+
+#ifdef	IPXDEBUG
+struct ipx_addr	ipx_addr __P((const char *));
+char		*ipx_ntoa __P((struct ipx_addr));
+#endif
+
+#else
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+struct ipx_addr	ipx_addr __P((const char *));
+char		*ipx_ntoa __P((struct ipx_addr));
+__END_DECLS
+
+#endif /* _KERNEL */
+
+#endif /* !_NETIPX_IPX_H_ */
