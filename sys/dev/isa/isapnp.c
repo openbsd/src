3910d045d7589d1946729270b2aa2145c8d46f41
deraadt <deraadt@openbsd.org>
1996-08-16 08:35:01 +0000

    : 0 devices

--- sys/dev/isa/isapnp.c
+++ sys/dev/isa/isapnp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: isapnp.c,v 1.4 1996/08/15 17:28:40 shawn Exp $	*/
+/*	$OpenBSD: isapnp.c,v 1.5 1996/08/16 08:35:01 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996, Shawn Hsiao <shawn@alpha.secc.fju.edu.tw>
@@ -169,6 +169,8 @@ isapnpattach(parent, self, aux)
 			break;
 		}
 	}
+	if (num_pnp_devs == 0)
+		printf(": 0 devices");
 	printf("\n");
 }
 
