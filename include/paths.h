f181dd278274f50e689ebd13237010a90b430164
millert <millert@openbsd.org>
1996-08-19 03:24:04 +0000

    Adds _PATH_RSH for rsh-using rcmd()

--- include/paths.h
+++ include/paths.h
@@ -56,6 +56,7 @@
 #define	_PATH_MAN	"/usr/share/man"
 #define	_PATH_MEM	"/dev/mem"
 #define	_PATH_NOLOGIN	"/etc/nologin"
+#define	_PATH_RSH	"/usr/bin/rsh"
 #define	_PATH_SENDMAIL	"/usr/sbin/sendmail"
 #define	_PATH_SHELLS	"/etc/shells"
 #define	_PATH_TTY	"/dev/tty"
