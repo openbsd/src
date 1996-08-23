6ceb2a691757d2c1505bea0442082005af255521
niklas <niklas@openbsd.org>
1996-08-23 11:48:48 +0000

    It is clear noone even tried to compile OSF/1 emulation lately. Made it compile again.

--- sys/compat/osf1/osf1_mount.c
+++ sys/compat/osf1/osf1_mount.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: osf1_mount.c,v 1.2 1996/04/18 21:21:41 niklas Exp $	*/
+/*	$OpenBSD: osf1_mount.c,v 1.3 1996/08/23 11:48:48 niklas Exp $	*/
 /*	$NetBSD: osf1_mount.c,v 1.6 1996/02/17 23:08:36 jtk Exp $	*/
 
 /*
@@ -44,6 +44,8 @@
 #include <net/if.h>
 #include <netinet/in.h>
 
+#include <nfs/nfsproto.h>
+
 #include <machine/vmparam.h>
 
 /* File system type numbers. */
@@ -61,7 +63,8 @@
 #define	OSF1_MOUNT_FFM		11
 #define	OSF1_MOUNT_FDFS		12
 #define	OSF1_MOUNT_ADDON	13
-#define	OSF1_MOUNT_MAXTYPE	OSF1_MOUNT_ADDON
+#define	OSF1_MOUNT_NFSV3	14
+#define	OSF1_MOUNT_MAXTYPE	15
 
 #define	OSF1_MNT_WAIT		0x1
 #define	OSF1_MNT_NOWAIT		0x2
@@ -115,7 +118,7 @@ struct osf1_mfs_args {
 
 struct osf1_nfs_args {
 	struct sockaddr_in	*addr;
-	nfsv2fh_t		*fh;
+	caddr_t			fh;
 	int32_t			flags;
 	int32_t			wsize;
 	int32_t			rsize;
@@ -234,7 +237,7 @@ osf1_sys_fstatfs(p, v, retval)
 	struct osf1_statfs osfs;
 	int error;
 
-	if (error = getvnode(p->p_fd, uap->fd, &fp))
+	if (error = getvnode(p->p_fd, SCARG(uap, fd), &fp))
 		return (error);
 	mp = ((struct vnode *)fp->f_data)->v_mount;
 	sp = &mp->mnt_stat;
@@ -433,11 +436,12 @@ osf1_mount_nfs(p, osf_argp, bsd_argp)
 		return error;
 
 	bzero(&bsd_na, sizeof bsd_na);
+	bsd_na.version = 2;
 	bsd_na.addr = (struct sockaddr *)osf_na.addr;
 	bsd_na.addrlen = sizeof (struct sockaddr_in);
 	bsd_na.sotype = SOCK_DGRAM; 
 	bsd_na.proto = 0; 
-	bsd_na.fh = osf_na.fh;
+	bsd_na.fh = (char *)osf_na.fh;
 
 	if (osf_na.flags & ~OSF1_NFSMNT_FLAGS)
 		return EINVAL;
