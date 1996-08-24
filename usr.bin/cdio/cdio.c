33d336edbdee5d0a99a2275214369cd0a18b3118
deraadt <deraadt@openbsd.org>
1996-08-24 08:20:21 +0000

    cd0 not /dev/cd0

--- usr.bin/cdio/cdio.c
+++ usr.bin/cdio/cdio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdio.c,v 1.2 1996/08/24 06:32:16 downsj Exp $	*/
+/*	$OpenBSD: cdio.c,v 1.3 1996/08/24 08:20:21 deraadt Exp $	*/
 /*
  * Compact Disc Control Utility by Serge V. Vakulenko <vak@cronyx.ru>.
  * Based on the non-X based CD player by Jean-Marc Zucconi and
@@ -42,7 +42,7 @@
 #define ASTS_VOID       0x15  /* No current audio status to return */
 
 #ifndef DEFAULT_CD_DRIVE
-#  define DEFAULT_CD_DRIVE  "/dev/cd0"
+#  define DEFAULT_CD_DRIVE  "cd0"
 #endif
 
 #define CMD_DEBUG       1
