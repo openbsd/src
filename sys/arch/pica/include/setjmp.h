0fa72cd998726f968903e1785f36fd1032df1cfe
pefo <pefo@openbsd.org>
1996-08-30 21:31:44 +0000

    The pica tree is now obsolete and removed. Use arch/arc.

--- sys/arch/pica/include/setjmp.h
+++
@@ -1,8 +0,0 @@
-/*	$OpenBSD: setjmp.h,v 1.2 1996/06/06 23:07:13 deraadt Exp $	*/
-/*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:37:05 cgd Exp $	*/
-
-/*
- * machine/setjmp.h: machine dependent setjmp-related information.
- */
-
-#define	_JBLEN	83		/* size, in longs, of a jmp_buf */
