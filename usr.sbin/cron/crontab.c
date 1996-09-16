69f0c87b6c650e3c7c32d158dae7d17eddf3e0a6
millert <millert@openbsd.org>
1996-09-16 17:19:54 +0000

    umask != file mode

--- usr.sbin/cron/crontab.c
+++ usr.sbin/cron/crontab.c
@@ -16,7 +16,7 @@
  */
 
 #if !defined(lint) && !defined(LINT)
-static char rcsid[] = "$Id: crontab.c,v 1.3 1996/09/15 10:02:34 deraadt Exp $";
+static char rcsid[] = "$Id: crontab.c,v 1.4 1996/09/16 17:19:54 millert Exp $";
 #endif
 
 /* crontab - install and manage per-user crontab files
@@ -316,7 +316,7 @@ edit_cmd() {
 		}
 	}
 
-	um = umask(0600);
+	um = umask(077);
 	(void) sprintf(Filename, "/tmp/crontab.XXXXXXXX");
 	if ((t = mkstemp(Filename)) == -1) {
 		perror(Filename);
