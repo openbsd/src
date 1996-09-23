1702f2ec5118f51c3139b86318c7b5338bf5aac6
mickey <mickey@openbsd.org>
1996-09-23 15:35:45 +0000

    adjust number of types, since we've got 16 partitions in disklabel now.

--- sys/arch/i386/isa/fdreg.h
+++ sys/arch/i386/isa/fdreg.h
@@ -65,5 +65,5 @@
 #define	FDC_NPORT	8
 #define	FDC_MAXIOSIZE	NBPG	/* XXX should be MAXBSIZE */
 
-#define FDUNIT(dev)	(minor(dev) / 8)                                     
-#define FDTYPE(dev)	(minor(dev) % 8)
+#define FDUNIT(dev)	(minor(dev) / 16)
+#define FDTYPE(dev)	(minor(dev) % 16)
