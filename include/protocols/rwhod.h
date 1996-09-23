000104d1d6f4ff50d03245f743a02095353584c3
deraadt <deraadt@openbsd.org>
1996-09-23 05:15:54 +0000

    int32_t where possible; from netbsd

--- include/protocols/rwhod.h
+++ include/protocols/rwhod.h
@@ -44,21 +44,21 @@
 struct	outmp {
 	char	out_line[8];		/* tty name */
 	char	out_name[8];		/* user id */
-	long	out_time;		/* time on */
+	int32_t	out_time;		/* time on */
 };
 
 struct	whod {
 	char	wd_vers;		/* protocol version # */
 	char	wd_type;		/* packet type, see below */
 	char	wd_pad[2];
-	int	wd_sendtime;		/* time stamp by sender */
-	int	wd_recvtime;		/* time stamp applied by receiver */
+	int32_t	wd_sendtime;		/* time stamp by sender */
+	int32_t	wd_recvtime;		/* time stamp applied by receiver */
 	char	wd_hostname[32];	/* hosts's name */
-	int	wd_loadav[3];		/* load average as in uptime */
-	int	wd_boottime;		/* time system booted */
+	int32_t	wd_loadav[3];		/* load average as in uptime */
+	int32_t	wd_boottime;		/* time system booted */
 	struct	whoent {
-		struct	outmp we_utmp;	/* active tty info */
-		int	we_idle;	/* tty idle time */
+		struct		outmp we_utmp;	/* active tty info */
+		int32_t		we_idle;	/* tty idle time */
 	} wd_we[1024 / sizeof (struct whoent)];
 };
 
