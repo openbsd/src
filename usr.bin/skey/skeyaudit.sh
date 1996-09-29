fe317dc78854ec6dd7ea04f7c7c3eb9f598f9221
millert <millert@openbsd.org>
1996-09-29 04:46:17 +0000

    Will now work with SHA when it happens.

--- usr.bin/skey/skeyaudit.sh
+++ usr.bin/skey/skeyaudit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $OpenBSD: skeyaudit.sh,v 1.3 1996/09/27 15:41:36 millert Exp $
+# $OpenBSD: skeyaudit.sh,v 1.4 1996/09/29 04:46:18 millert Exp $
 # This script will look thru the skeykeys file for
 # people with sequence numbers less then LOWLIMIT=12
 # and send them an e-mail reminder to use skeyinit soon
@@ -28,10 +28,10 @@ fi
 
 for i in `$AWK '{print $1}' $KEYDB`
 do
-SEQ=`$GREP "^$i[ 	]" $KEYDB | $AWK '{if ($2 ~ /^MD[0-9]+/) {print $3} else {print $2}}'`
+SEQ=`$GREP "^$i[ 	]" $KEYDB | $AWK '{if ($2 ~ /^[A-z]/) {print $3} else {print $2}}'`
 if [ $SEQ -lt $LOWLIMIT ]
 then
-  KEY=`$GREP "^$i[ 	]" $KEYDB | $AWK '{if ($2 ~ /^MD[0-9]+/) {print $4} else {print $3}}'`
+  KEY=`$GREP "^$i[ 	]" $KEYDB | $AWK '{if ($2 ~ /^[A-z]/) {print $4} else {print $3}}'`
   if [ $SEQ -lt  3 ]
   then
   SUBJECT="IMPORTANT action required"
--- usr.bin/skey/skeyinfo.sh
+++ usr.bin/skey/skeyinfo.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# $OpenBSD: skeyinfo.sh,v 1.3 1996/09/27 15:41:37 millert Exp $
+# $OpenBSD: skeyinfo.sh,v 1.4 1996/09/29 04:46:17 millert Exp $
 # search /etc/skeykeys for the skey string for
 # this user OR user specified in 1st parameter
 
+KEYDB=/etc/skeykeys
 if [ -z "$1" ]; then
 	WHO=`/usr/bin/whoami`
 else
 	WHO=$1
 fi
 
-if [ -f /etc/skeykeys ]; then
-	/usr/bin/awk "{ if (\$1 == \"$WHO\" && \$2 ~ /^MD[0-9]+/) {print \$3-1,\$4} else {print \$2-1,\$3} }" < /etc/skeykeys
+if [ -f $KEYDB ]; then
+	/usr/bin/awk '/^'$WHO'[ 	]/ { if ($2 ~ /^[A-z]/) { print $3-1, $4} else { print $2-1, $3 } }' < $KEYDB
 fi
