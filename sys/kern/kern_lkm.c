96d1208010dc3bc3c636729b1ee87204daee3d1d
deraadt <deraadt@openbsd.org>
1996-10-02 20:22:13 +0000

    copyinstr correctly; spotted by bitblt

--- sys/kern/kern_lkm.c
+++ sys/kern/kern_lkm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_lkm.c,v 1.11 1996/09/27 14:41:16 briggs Exp $	*/
+/*	$OpenBSD: kern_lkm.c,v 1.12 1996/10/02 20:22:13 deraadt Exp $	*/
 /*	$NetBSD: kern_lkm.c,v 1.31 1996/03/31 21:40:27 christos Exp $	*/
 
 /*
@@ -208,7 +208,7 @@ lkmlookup(i, name, error)
 		 * Copy name and lookup id from all loaded
 		 * modules.  May fail.
 		 */
-	 	*error = copyinstr(name, istr, MAXLKMNAME-1, NULL);
+	 	*error = copyinstr(name, istr, MAXLKMNAME, NULL);
 		if (*error)
 			return NULL;
 
--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_prot.c,v 1.4 1996/08/25 09:51:37 deraadt Exp $	*/
+/*	$OpenBSD: kern_prot.c,v 1.5 1996/10/02 20:22:14 deraadt Exp $	*/
 /*	$NetBSD: kern_prot.c,v 1.33 1996/02/09 18:59:42 christos Exp $	*/
 
 /*
@@ -578,7 +578,7 @@ sys_setlogin(p, v, retval)
 		return (error);
 	error = copyinstr((caddr_t) SCARG(uap, namebuf),
 	    (caddr_t) p->p_pgrp->pg_session->s_login,
-	    sizeof (p->p_pgrp->pg_session->s_login) - 1, (size_t *)0);
+	    sizeof (p->p_pgrp->pg_session->s_login), (size_t *)0);
 	if (error == ENAMETOOLONG)
 		error = EINVAL;
 	return (error);
