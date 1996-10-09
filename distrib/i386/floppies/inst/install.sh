fc4aaaf5c24ebe47fbed093b3d58ba57c91796b6
deraadt <deraadt@openbsd.org>
1996-10-09 02:27:34 +0000

    nicer less

--- distrib/i386/floppies/inst/install.sh
+++ distrib/i386/floppies/inst/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.14 1996/10/07 18:48:23 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.15 1996/10/09 02:27:34 deraadt Exp $
 #
 # Copyright (c) 1994 Christopher G. Demetriou
 # All rights reserved.
@@ -161,7 +161,7 @@ echo -n "View the boot messages again? [n] "
 getresp "n"
 case "$resp" in
 y*|Y*)
-	less /kern/msgbuf
+	less -rsS /kern/msgbuf
 	;;
 *)
 	echo	""
