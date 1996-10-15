907625cd32e402198c22b95a22a509e7d2bf02c8
downsj <downsj@openbsd.org>
1996-10-15 08:31:54 +0000

    Yeah, ok, so I never even bothered to test compile it before...

--- usr.bin/vim/pty_openbsd.c
+++ usr.bin/vim/pty_openbsd.c
@@ -1,10 +1,11 @@
-/*	$OpenBSD: pty_openbsd.c,v 1.1.1.1 1996/09/07 21:40:28 downsj Exp $	*/
+/*	$OpenBSD: pty_openbsd.c,v 1.2 1996/10/15 08:31:54 downsj Exp $	*/
 
 /*
  * A quick, OpenBSD specific pty.c replacement.  It's not even entirely
  * correct; but it's certainly not GPL'd.
  */
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <util.h>
 
