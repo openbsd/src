1f94d27d226ec91339b69851117843375129c751
deraadt <deraadt@openbsd.org>
1996-10-16 12:33:39 +0000

    missing */

--- sys/dev/isa/pas.c
+++ sys/dev/isa/pas.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pas.c,v 1.12 1996/08/24 05:03:18 deraadt Exp $	*/
+/*	$OpenBSD: pas.c,v 1.13 1996/10/16 12:33:39 deraadt Exp $	*/
 /*	$NetBSD: pas.c,v 1.17 1996/05/12 23:53:18 mycroft Exp $	*/
 
 /*
@@ -345,7 +345,7 @@ pasprobe(parent, match, aux)
         }
 
 	/* Now a SoundBlaster */
-/*	sc->sc_iobase = ia->ia_iobase;
+/*	sc->sc_iobase = ia->ia_iobase; */
 	/* and set the SB iobase into the DSP as well ... */
 	sc->sc_sbdsp.sc_iobase = ia->ia_iobase;
 	if (sbdsp_reset(&sc->sc_sbdsp) < 0) {
