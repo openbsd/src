2af45b0af928382e2273cbbdccd0ba30b450eb80
deraadt <deraadt@openbsd.org>
1996-10-17 13:36:45 +0000

    more SIGBUS garbage

--- sys/arch/i386/i386/trap.c
+++ sys/arch/i386/i386/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.11 1996/08/27 10:46:52 downsj Exp $	*/
+/*	$OpenBSD: trap.c,v 1.12 1996/10/17 13:36:45 deraadt Exp $	*/
 /*	$NetBSD: trap.c,v 1.95 1996/05/05 06:50:02 mycroft Exp $	*/
 
 #undef DEBUG
@@ -280,6 +280,9 @@ trap(frame)
 #endif
 	case T_SEGNPFLT|T_USER:
 	case T_STKFLT|T_USER:
+		trapsignal(p, SIGSEGV, type &~ T_USER);
+		goto out;
+
 	case T_ALIGNFLT|T_USER:
 		trapsignal(p, SIGBUS, type &~ T_USER);
 		goto out;
