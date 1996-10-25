3ed93efa035bd8b78617a9be063dc43a502b8c4b
downsj <downsj@openbsd.org>
1996-10-25 06:27:42 +0000

    sprintf -> snprintf

--- usr.bin/apropos/apropos.c
+++ usr.bin/apropos/apropos.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: apropos.c,v 1.2 1996/06/26 05:31:14 deraadt Exp $      */
+/*      $OpenBSD: apropos.c,v 1.3 1996/10/25 06:27:42 downsj Exp $      */
 /*      $NetBSD: apropos.c,v 1.5 1995/09/04 20:46:20 tls Exp $      */
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)apropos.c	8.8 (Berkeley) 5/4/95";
 #else
-static char rcsid[] = "$OpenBSD: apropos.c,v 1.2 1996/06/26 05:31:14 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: apropos.c,v 1.3 1996/10/25 06:27:42 downsj Exp $";
 #endif
 #endif /* not lint */
 
@@ -149,7 +149,8 @@ apropos(argv, path, buildpath)
 		if (buildpath) {
 			char hold[MAXPATHLEN + 1];
 
-			(void)sprintf(hold, "%s/%s", name, _PATH_WHATIS);
+			(void)snprintf(hold, sizeof(hold), "%s/%s", name,
+			    _PATH_WHATIS);
 			name = hold;
 		}
 
