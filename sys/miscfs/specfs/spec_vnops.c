05ba17dc082e0aa9d58b473d7772e85eb4315139
deraadt <deraadt@openbsd.org>
1996-11-04 03:31:15 +0000

    permit locking on devices

--- sys/miscfs/specfs/spec_vnops.c
+++ sys/miscfs/specfs/spec_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: spec_vnops.c,v 1.3 1996/05/02 13:20:19 deraadt Exp $	*/
+/*	$OpenBSD: spec_vnops.c,v 1.4 1996/11/04 03:31:15 deraadt Exp $	*/
 /*	$NetBSD: spec_vnops.c,v 1.29 1996/04/22 01:42:38 christos Exp $	*/
 
 /*
@@ -707,8 +707,9 @@ int
 spec_advlock(v)
 	void *v;
 {
+	extern int ufs_advlock __P((void *v));
 
-	return (EOPNOTSUPP);
+	return (ufs_advlock(v));
 }
 
 /*
