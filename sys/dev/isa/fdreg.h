f756ca216fc96b14693fd04c40788486a6e7627e
downsj <downsj@openbsd.org>
1996-11-10 21:21:30 +0000

    Kill major number hack.

--- sys/dev/isa/fdreg.h
+++ sys/dev/isa/fdreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fdreg.h,v 1.7 1996/11/06 01:33:53 deraadt Exp $	*/
+/*	$OpenBSD: fdreg.h,v 1.8 1996/11/10 21:21:30 downsj Exp $	*/
 /*	$NetBSD: fdreg.h,v 1.8 1995/06/28 04:30:57 cgd Exp $	*/
 
 /*-
@@ -66,6 +66,5 @@
 #define	FDC_NPORT	8
 #define	FDC_MAXIOSIZE	NBPG	/* XXX should be MAXBSIZE */
 
-#define FDUNIT(dev)	((dev & 0x180) >> 7)
-#define FDTYPE(dev)	((minor(dev) & 0x70) >> 4)
-#define FDPART(dev)	(minor(dev) & 0x0f)
+#define FDUNIT(dev)	(minor(dev) / 16)
+#define FDTYPE(dev)	(minor(dev) % 16)
