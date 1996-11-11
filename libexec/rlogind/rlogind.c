0aa5a042e3c2b9b52699a0f1070881e660db77ce
mickey <mickey@openbsd.org>
1996-11-11 05:34:41 +0000

    correct type cast

--- libexec/rlogind/rlogind.c
+++ libexec/rlogind/rlogind.c
@@ -39,7 +39,7 @@ static char copyright[] =
 
 #ifndef lint
 /* from: static char sccsid[] = "@(#)rlogind.c	8.1 (Berkeley) 6/4/93"; */
-static char *rcsid = "$Id: rlogind.c,v 1.9 1996/08/27 10:26:59 deraadt Exp $";
+static char *rcsid = "$Id: rlogind.c,v 1.10 1996/11/11 05:34:41 mickey Exp $";
 #endif /* not lint */
 
 /*
@@ -689,7 +689,7 @@ do_krb_login(dest)
 			authopts, 0,
 			ticket, "rcmd",
 			instance, dest, (struct sockaddr_in *) 0,
-			kdata, "", (bit_64 *) 0, version);
+			kdata, "", (struct des_ks_struct *) 0, version);
 
 	if (rc != KSUCCESS)
 		return (rc);
