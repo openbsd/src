fbff6f92be27dc2cbec51dd5df0ce5a511cf1904
deraadt <deraadt@openbsd.org>
1996-12-06 15:33:31 +0000

    memset

--- libexec/telnetd/utility.c
+++ libexec/telnetd/utility.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: utility.c,v 1.4 1996/12/06 15:26:28 deraadt Exp $	*/
+/*	$OpenBSD: utility.c,v 1.5 1996/12/06 15:33:31 deraadt Exp $	*/
 /*	$NetBSD: utility.c,v 1.9 1996/02/28 20:38:29 thorpej Exp $	*/
 
 /*
@@ -39,7 +39,7 @@
 static char sccsid[] = "@(#)utility.c	8.4 (Berkeley) 5/30/95";
 static char rcsid[] = "$NetBSD: utility.c,v 1.9 1996/02/28 20:38:29 thorpej Exp $";
 #else
-static char rcsid[] = "$OpenBSD: utility.c,v 1.4 1996/12/06 15:26:28 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: utility.c,v 1.5 1996/12/06 15:33:31 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -102,7 +102,7 @@ stilloob(s)
     do {
 	FD_ZERO(&excepts);
 	FD_SET(s, &excepts);
-	bzero(&timeout, sizeof timeout);
+	memset((char *)&timeout, 0, sizeof timeout);
 	value = select(s+1, (fd_set *)0, (fd_set *)0, &excepts, &timeout);
     } while ((value == -1) && (errno == EINTR));
 
