fc87972848c2d50ab17735697b99ee42dc992984
millert <millert@openbsd.org>
1996-12-10 22:21:59 +0000

    Flex 2.5.4

--- usr.bin/lex/libyywrap.c
+++ usr.bin/lex/libyywrap.c
@@ -1,14 +1,15 @@
-/*	$OpenBSD: libyywrap.c,v 1.3 1996/07/13 22:22:06 millert Exp $	*/
+/*	$OpenBSD: libyywrap.c,v 1.4 1996/12/10 22:22:03 millert Exp $	*/
 
 /* libyywrap - flex run-time support library "yywrap" function */
 
-/* $Header: /home/cvs/src/usr.bin/lex/libyywrap.c,v 1.3 1996/07/13 22:22:06 millert Exp $ */
+/* $Header: /home/cvs/src/usr.bin/lex/libyywrap.c,v 1.4 1996/12/10 22:22:03 millert Exp $ */
 
 #include <sys/cdefs.h>
 
 int yywrap __P((void));
 
-int yywrap()
+int
+yywrap()
 	{
 	return 1;
 	}
--- usr.bin/lex/version.h
+++ usr.bin/lex/version.h
@@ -1,3 +1,3 @@
-/*	$OpenBSD: version.h,v 1.3 1996/07/13 22:22:14 millert Exp $	*/
+/*	$OpenBSD: version.h,v 1.4 1996/12/10 22:22:03 millert Exp $	*/
 
-#define FLEX_VERSION "2.5.3"
+#define FLEX_VERSION "2.5.4"
