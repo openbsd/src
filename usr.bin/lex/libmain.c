4d9cb65585f5f152d1323f5a889750b5d0d7562b
tholo <tholo@openbsd.org>
1996-12-14 07:07:56 +0000

    Silence lint

--- usr.bin/lex/libmain.c
+++ usr.bin/lex/libmain.c
@@ -1,14 +1,15 @@
-/*	$OpenBSD: libmain.c,v 1.3 1996/07/13 22:22:05 millert Exp $	*/
+/*	$OpenBSD: libmain.c,v 1.4 1996/12/14 07:07:56 tholo Exp $	*/
 
 /* libmain - flex run-time support library "main" function */
 
-/* $Header: /home/cvs/src/usr.bin/lex/libmain.c,v 1.3 1996/07/13 22:22:05 millert Exp $ */
+/* $Header: /home/cvs/src/usr.bin/lex/libmain.c,v 1.4 1996/12/14 07:07:56 tholo Exp $ */
 
 #include <sys/cdefs.h>
 
 int yylex __P((void));
 int main __P((int, char **, char **));
 
+/* ARGSUSED */
 int
 main( argc, argv, envp )
 int argc;
