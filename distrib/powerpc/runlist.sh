1c70065efcf1d6eb7df05966e3375ba820baeca7
rahnds <rahnds@openbsd.org>
1996-12-21 20:46:17 +0000

    Check-in of powerpc distrib support. NOTE: This will not work until the other pieces are checked in. This is primarily the NetBSD powerpc port, with modifications to support ELF.
    The distrib pieces will not work until the ELF crunchgen support is
    checked in.

---
+++ distrib/powerpc/runlist.sh
@@ -0,0 +1,13 @@
+#	$NetBSD: runlist.sh,v 1.1 1995/07/18 04:13:01 briggs Exp $
+
+if [ "X$1" = "X-d" ]; then
+	SHELLCMD=cat
+	shift
+else
+	SHELLCMD="sh"
+fi
+
+( while [ "X$1" != "X" ]; do
+	cat $1
+	shift
+done ) | awk -f ${TOPDIR}/list2sh.awk | ${SHELLCMD}
