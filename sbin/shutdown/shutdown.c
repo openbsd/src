bc6365451bd6b279d160e1b623dd841d482f588d
deraadt <deraadt@openbsd.org>
1996-12-22 02:59:53 +0000

    MAXHOSTNAMELEN, not +1

--- sbin/shutdown/shutdown.c
+++ sbin/shutdown/shutdown.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: shutdown.c,v 1.5 1996/09/20 07:12:57 deraadt Exp $	*/
+/*	$OpenBSD: shutdown.c,v 1.6 1996/12/22 02:59:53 deraadt Exp $	*/
 /*	$NetBSD: shutdown.c,v 1.9 1995/03/18 15:01:09 cgd Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)shutdown.c	8.2 (Berkeley) 2/16/94";
 #else
-static char rcsid[] = "$OpenBSD: shutdown.c,v 1.5 1996/09/20 07:12:57 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: shutdown.c,v 1.6 1996/12/22 02:59:53 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -279,7 +279,7 @@ timewarn(timeleft)
 	int timeleft;
 {
 	static int first;
-	static char hostname[MAXHOSTNAMELEN + 1];
+	static char hostname[MAXHOSTNAMELEN];
 	FILE *pf;
 	char wcmd[MAXPATHLEN + 4];
 	extern char **environ;
