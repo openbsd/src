8806ed62f61707af7c17e3337f38c457031d3b91
kstailey <kstailey@openbsd.org>
1996-12-27 23:23:27 +0000

    new NetBSD file; #include from <machine/cpu.h> on m68k boxen

---
+++ sys/arch/m68k/include/sysctl.h
@@ -0,0 +1,22 @@
+/*	$NetBSD: sysctl.h,v 1.1 1996/12/17 19:26:51 gwr Exp $	*/
+
+/*
+ * CTL_MACHDEP definitions.  (Common to all m68k ports.)
+ * This should be included by each m68k port's cpu.h so
+ * /usr/sbin/sysctl can be shared on all of them.
+ */
+#ifndef CTL_MACHDEP_NAMES
+
+#define	CPU_CONSDEV		1	/* dev_t: console terminal device */
+#define	CPU_ROOT_DEVICE		2	/* string: root device name */
+#define	CPU_BOOTED_KERNEL	3	/* string: booted kernel name */
+#define	CPU_MAXID		4	/* number of valid machdep ids */
+
+#define	CTL_MACHDEP_NAMES { \
+	{ 0, 0 }, \
+	{ "console_device", CTLTYPE_STRUCT }, \
+	{ "root_device", CTLTYPE_STRING }, \
+	{ "booted_kernel", CTLTYPE_STRING }, \
+}
+
+#endif	/* CTL_MACHDEP_NAMES */
