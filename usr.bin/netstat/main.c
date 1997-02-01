ed53389c2b5c0f7a099f788bacd8c27897e97689
kstailey <kstailey@openbsd.org>
1997-02-01 17:31:19 +0000

    accept "-f local" for AF_LOCAL"

--- usr.bin/netstat/main.c
+++ usr.bin/netstat/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.6 1997/01/15 23:42:55 millert Exp $	*/
+/*	$OpenBSD: main.c,v 1.7 1997/02/01 17:31:19 kstailey Exp $	*/
 /*	$NetBSD: main.c,v 1.9 1996/05/07 02:55:02 thorpej Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ char copyright[] =
 #if 0
 static char sccsid[] = "from: @(#)main.c	8.4 (Berkeley) 3/1/94";
 #else
-static char *rcsid = "$OpenBSD: main.c,v 1.6 1997/01/15 23:42:55 millert Exp $";
+static char *rcsid = "$OpenBSD: main.c,v 1.7 1997/02/01 17:31:19 kstailey Exp $";
 #endif
 #endif /* not lint */
 
@@ -240,6 +240,8 @@ main(argc, argv)
 		case 'f':
 			if (strcmp(optarg, "inet") == 0)
 				af = AF_INET;
+			else if (strcmp(optarg, "local") == 0)
+				af = AF_LOCAL;
 			else if (strcmp(optarg, "unix") == 0)
 				af = AF_UNIX;
 			else if (strcmp(optarg, "ipx") == 0)
