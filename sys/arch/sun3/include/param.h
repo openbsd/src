ce99c2ce7f5b9442b00ad3b5ff5a1f106e73923d
kstailey <kstailey@openbsd.org>
1997-02-14 17:49:35 +0000

    move spl() stuff from psl.h to param.h

--- sys/arch/sun3/include/param.h
+++ sys/arch/sun3/include/param.h
@@ -148,10 +148,78 @@
 #define sun3_btop(x)		((unsigned)(x) >> PGSHIFT)
 #define sun3_ptob(x)		((unsigned)(x) << PGSHIFT)
 
-/* XXX - Does this really belong here? -gwr */
 #include <machine/psl.h>
 
 #if defined(_KERNEL) && !defined(_LOCORE)
+
+#ifndef __GNUC__
+/* No inline, use real function in locore.s */
+extern int _spl(int new);
+#else	/* GNUC */
+/*
+ * Define an inline function for PSL manipulation.
+ * This is as close to a macro as one can get.
+ * If not optimizing, the one in locore.s is used.
+ * (See the GCC extensions info document.)
+ */
+extern __inline__ int _spl(int new)
+{
+	register int old;
+
+	__asm __volatile (
+		"clrl %0; movew sr,%0; movew %1,sr" :
+			"&=d" (old) : "di" (new));
+	return (old);
+}
+#endif	/* GNUC */
+
+/*
+ * The rest of this is sun3 specific, because other ports may
+ * need to do special things in spl0() (i.e. simulate SIR).
+ * Suns have a REAL interrupt register, so spl0() and splx(s)
+ * have no need to check for any simulated interrupts, etc.
+ */
+
+#define spl0()  _spl(PSL_S|PSL_IPL0)
+#define spl1()  _spl(PSL_S|PSL_IPL1)
+#define spl2()  _spl(PSL_S|PSL_IPL2)
+#define spl3()  _spl(PSL_S|PSL_IPL3)
+#define spl4()  _spl(PSL_S|PSL_IPL4)
+#define spl5()  _spl(PSL_S|PSL_IPL5)
+#define spl6()  _spl(PSL_S|PSL_IPL6)
+#define spl7()  _spl(PSL_S|PSL_IPL7)
+#define splx(x)	_spl(x)
+
+/* IPL used by soft interrupts: netintr(), softclock() */
+#define splsoftclock()  spl1()
+#define splsoftnet()    spl1()
+
+/* Highest block device (strategy) IPL. */
+#define splbio()        spl2()
+
+/* Highest network interface IPL. */
+#define splnet()        spl3()
+
+/* Highest tty device IPL. */
+#define spltty()        spl4()
+
+/* Requirement: imp >= (highest network, tty, or disk IPL) */
+#define splimp()        spl4()
+
+/* Intersil clock hardware interrupts (hard-wired at 5) */
+#define splclock()      spl5()
+#define splstatclock()  splclock()
+
+/* Zilog Serial hardware interrupts (hard-wired at 6) */
+#define splzs()         spl6()
+
+/* Block out all interrupts (except NMI of course). */
+#define splhigh()       spl7()
+#define splsched()      spl7()
+
+/* Get current sr value (debug, etc.) */
+extern int getsr __P((void));
+
 extern void _delay __P((unsigned));
 #define delay(us)	_delay((us)<<8)
 #define	DELAY(n)	delay(n)
