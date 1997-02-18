f051ed198cc62ed9ef5669c68dff2b8b58b4ce42
gene <gene@openbsd.org>
1997-02-18 05:13:13 +0000

    Synch declaration of copypage w/ m68k-general definitions per Ken's changes. This might get moved out soon.

--- sys/arch/mac68k/include/pmap.h
+++ sys/arch/mac68k/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.3 1996/05/26 18:35:58 briggs Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.4 1997/02/18 05:13:13 gene Exp $	*/
 /*	$NetBSD: pmap.h,v 1.13 1996/05/18 18:52:41 briggs Exp $	*/
 
 /*
@@ -211,7 +211,7 @@ void	TBIA __P((void));
 void	TBIS __P((vm_offset_t));
 void	DCFP __P((vm_offset_t));
 void	ICPP __P((vm_offset_t));
-void	copypage __P((caddr_t, caddr_t));
+void	copypage __P((void *, void *));
 __END_DECLS
 
 #endif	/* _KERNEL */
