baa96d8a54ceadfc9771c27ad6ca42ac904bc283
tholo <tholo@openbsd.org>
1997-02-19 07:08:28 +0000

    Get database renames correct

--- kerberosIV/kdb/krb_dbm.c
+++ kerberosIV/kdb/krb_dbm.c
@@ -1,4 +1,4 @@
-/*	$Id: krb_dbm.c,v 1.2 1996/08/22 01:18:49 deraadt Exp $	*/
+/*	$Id: krb_dbm.c,v 1.3 1997/02/19 07:08:28 tholo Exp $	*/
 
 /*-
  * Copyright (C) 1989 by the Massachusetts Institute of Technology
@@ -453,7 +453,7 @@ kerb_db_rename(from, to)
 {
 #if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
     char *fromdb = gen_dbsuffix (from, ".db");
-    char *todb = gen_dbsuffix (from, ".db");
+    char *todb = gen_dbsuffix (to, ".db");
 #else
     char *fromdir = gen_dbsuffix (from, ".dir");
     char *todir = gen_dbsuffix (to, ".dir");
@@ -466,7 +466,7 @@ kerb_db_rename(from, to)
     
 #if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
     if (rename (fromdb, todb) == 0) {
-	(void) unlink (fromdb);
+	(void) unlink (fromok);
 	ok = 1;
     }
     free (fromdb);
