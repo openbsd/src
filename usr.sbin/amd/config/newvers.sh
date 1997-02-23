004cda8392569f5aabe4a0a4f5fb1cf27d8fe633
deraadt <deraadt@openbsd.org>
1997-02-23 03:41:56 +0000

    build host and user is not important

--- usr.sbin/amd/config/newvers.sh
+++ usr.sbin/amd/config/newvers.sh
@@ -37,16 +37,13 @@
 # SUCH DAMAGE.
 #
 #	from: @(#)newvers.sh	8.1 (Berkeley) 6/6/93
-#	$Id: newvers.sh,v 1.2 1996/10/26 20:10:22 millert Exp $
+#	$Id: newvers.sh,v 1.3 1997/02/23 03:41:56 deraadt Exp $
 #
 PATH=/usr/ucb:/bin:/usr/bin:$PATH
 if [ $# -ne 3 ]; then echo "Usage: newvers program arch os" >&2; exit 1; fi
 version="version.$1"
 if [ ! -r $version ]; then echo 0 > $version; chmod 444 $version; fi
 v=`cat $version`
-u=${LOGNAME-${USER-root}}
-h=`hostname`
-#h=`expr "$h" : '\([^.]*\)'`
 t=`date`
 if [ ! -s "$d../config/RELEASE"  -o ! -s "$d../text/COPYRIGHT" ]; then
 	echo ERROR: config file missing >&2
@@ -74,7 +71,6 @@ sed \
 	$d../config/RELEASE
 cat << %%
  #${v}: ${t}\\n\\
-Built by ${u}@${h} for \\
 %%
 case "$2" in
 [aeiou]*) echo "an \\" ;;
