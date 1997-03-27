b87714da688475f1f59039ef7ddbd34c1be06821
deraadt <deraadt@openbsd.org>
1997-03-27 17:15:27 +0000

    if lost+found generation code is enabled, create it with a nice mode

--- sbin/newfs/mkfs.c
+++ sbin/newfs/mkfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkfs.c,v 1.6 1997/02/23 03:51:23 millert Exp $	*/
+/*	$OpenBSD: mkfs.c,v 1.7 1997/03/27 17:15:27 deraadt Exp $	*/
 /*	$NetBSD: mkfs.c,v 1.25 1995/06/18 21:35:38 cgd Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)mkfs.c	8.3 (Berkeley) 2/3/94";
 #else
-static char rcsid[] = "$OpenBSD: mkfs.c,v 1.6 1997/02/23 03:51:23 millert Exp $";
+static char rcsid[] = "$OpenBSD: mkfs.c,v 1.7 1997/03/27 17:15:27 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -883,7 +883,7 @@ fsinit(utime)
 			memcpy(&buf[i], &lost_found_dir[2],
 			    DIRSIZ(0, &lost_found_dir[2]));
 	}
-	node.di_mode = IFDIR | UMASK;
+	node.di_mode = IFDIR | 1700;
 	node.di_nlink = 2;
 	node.di_size = sblock.fs_bsize;
 	node.di_db[0] = alloc(node.di_size, node.di_mode);
