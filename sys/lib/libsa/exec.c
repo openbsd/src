e7afde4a091f3a03ee4e43f214c4337956af4ebc
weingart <weingart@openbsd.org>
1997-04-11 19:17:03 +0000

    machdep_start has to do esym mucking...

--- sys/lib/libsa/exec.c
+++ sys/lib/libsa/exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.c,v 1.16 1997/03/31 07:43:25 deraadt Exp $	*/
+/*	$OpenBSD: exec.c,v 1.17 1997/04/11 19:17:03 weingart Exp $	*/
 /*	$NetBSD: exec.c,v 1.15 1996/10/13 02:29:01 christos Exp $	*/
 
 /*-
@@ -150,10 +150,7 @@ exec(path, loadaddr, howto)
 
 		/* and that many bytes of string table */
 		printf("+%d]", i);
-
-#define	round_to_size(x) (((int)(x) + sizeof(int) - 1) & ~(sizeof(int) - 1))
-		esym = (char *)round_to_size(addr - (char *)loadaddr);
-#undef round_to_size
+		esym = addr;
 	} else {
 		ssym = 0;
 		esym = 0;
