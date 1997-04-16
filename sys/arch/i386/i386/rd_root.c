c58c57ba7cdb6f106dce180ddde5592a505b774f
deraadt <deraadt@openbsd.org>
1997-04-16 00:53:07 +0000

    nice printf

--- sys/arch/i386/i386/rd_root.c
+++ sys/arch/i386/i386/rd_root.c
@@ -61,7 +61,7 @@ rd_attach_hook(unit, rd)
 		rd->rd_addr = (caddr_t) rd_root_image;
 		rd->rd_size = (size_t)  rd_root_size;
 		rd->rd_type = RD_KMEM_FIXED;
-		printf("rd%d: fixed, %d blocks", unit, MINIROOTSIZE);
+		printf("rd%d: fixed, %d blocks\n", unit, MINIROOTSIZE);
 	}
 }
 
