ba2efeb70bae4861c68b23de145927956bd4a053
deraadt <deraadt@openbsd.org>
1997-04-19 10:10:47 +0000

    constrain to MAXHOSTNAMELEN-1 not MAXDNAME, duh

--- lib/libc/net/res_comp.c
+++ lib/libc/net/res_comp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: res_comp.c,v 1.5 1997/03/27 20:45:33 deraadt Exp $	*/
+/*	$OpenBSD: res_comp.c,v 1.6 1997/04/19 10:10:47 deraadt Exp $	*/
 
 /*
  * ++Copyright++ 1985, 1993
@@ -60,7 +60,7 @@
 static char sccsid[] = "@(#)res_comp.c	8.1 (Berkeley) 6/4/93";
 static char rcsid[] = "$From: res_comp.c,v 8.11 1996/12/02 09:17:22 vixie Exp $";
 #else
-static char rcsid[] = "$OpenBSD: res_comp.c,v 1.5 1997/03/27 20:45:33 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: res_comp.c,v 1.6 1997/04/19 10:10:47 deraadt Exp $";
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -100,7 +100,9 @@ dn_expand(msg, eomorig, comp_dn, exp_dn, length)
 
 	dn = exp_dn;
 	cp = comp_dn;
-	eom = exp_dn + (length > MAXDNAME ? MAXDNAME : length);
+	if (length > MAXHOSTNAMELEN-1)
+		length = MAXHOSTNAMELEN-1;
+	eom = exp_dn + length;
 	/*
 	 * fetch next label in domain name
 	 */
