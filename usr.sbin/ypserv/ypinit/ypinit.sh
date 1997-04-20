65c2a3c5992571939c6afdb1c5a8f299c8af4e0a
maja <maja@openbsd.org>
1997-04-20 10:00:19 +0000

    Added man page, support for amd.home and ypservers. Integrated some code from ypinit in NetBSD (thorpej@netbsd.org).

--- usr.sbin/ypserv/ypinit/ypinit.sh
+++ usr.sbin/ypserv/ypinit/ypinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: ypinit.sh,v 1.3 1996/03/02 03:01:40 dm Exp $
+#	$Id: ypinit.sh,v 1.4 1997/04/20 10:00:27 maja Exp $
 #
 # ypinit.sh - setup an master or slave server.
 #
@@ -8,6 +8,8 @@ HOSTNAME=/bin/hostname
 YPWHICH=/usr/bin/ypwhich
 YPXFR=/usr/sbin/ypxfr
 YP_DIR=/var/yp
+MAKEDBM=/usr/sbin/makedbm
+ERROR_EXISTS="NO"
 
 #set -xv
 
@@ -21,6 +23,13 @@ then
 		SERVERTYPE=MASTER
 		ERROR=
 	fi
+
+	if [ $1 = "-u" ]		# ypinit -u
+	then
+		DOMAIN=`${DOMAINNAME}`
+		SERVERTYPE=UPDATE
+		ERROR=
+	fi
 fi
 
 if [ $# -eq 2 ]
@@ -31,11 +40,19 @@ then
 		SERVERTYPE=MASTER
 		ERROR=
 	fi
+
 	if [ $1 = "-s" ]		# ypinit -s master_server
 	then
 		DOMAIN=`${DOMAINNAME}`
 		SERVERTYPE=SLAVE
 		MASTER=${2}
+		ERROR=
+	fi
+
+	if [ $1 = "-u" ]		# ypinit -u domainname
+	then
+		DOMAIN=${2}
+		SERVERTYPE=UPDATE
 		ERROR=
 	fi
 fi
@@ -51,33 +68,57 @@ then
 	fi
 fi
 
-if [ "${ERROR}" = "USAGE" ]
-then
-	echo "usage: ypinit -m [domainname]" 1>&2
-	echo "       ypinit -s master_server [domainname]" 1>&2
-	echo "" 1>&2
-	echo "\
-where -m is used to build a master YP server data base, and -s is used for" 1>&2
-	echo "\
-a slave data base.  master_server must be an existing reachable YP server." 1>&2
+if [ "${ERROR}" = "USAGE" ]; then
+	cat << \__usage 1>&2
+usage: ypinit -m [domainname]
+       ypinit -s master_server [domainname]
+       ypinit -u [domainname]
+
+The `-m' flag builds a master YP server, and the `-s' flag builds
+a slave YP server.  When building a slave YP server, `master_server'
+must be an existing, reachable YP server.
+The `-u' is for updating the ypservers map on a master server.
+__usage
+
 	exit 1
 fi
 
 # Check if domainname is set, don't accept an empty domainname
+if [ -z "${DOMAIN}" ]; then
+	cat << \__no_domain 1>&2
+The local host's YP domain name has not been set.  Please set it with
+the domainname(8) command or pass the domain as an argument to ypinit(8).
+__no_domain
 
-if [ -z "${DOMAIN}" ]
-then
-	echo "The local host's domain name hasn't been set. Please set it." 1>&2
 	exit 1
 fi
 
 # Check if hostname is set, don't accept an empty hostname
-
 HOST=`${HOSTNAME}`
+if [ -z "${HOST}" ]; then
+	cat << \__no_hostname 1>&2
+The local host's hostname has not been set.  Please set it with the
+hostname(8) command.
+__no_hostname
 
-if [ -z "${HOST}" ]
+	exit 1
+fi
+
+# Check if we have contact with master.
+if [ "${SERVERTYPE}" = "SLAVE" ];
 then
-	echo "The local host's name hasn't been set. Please set it." 1>&2
+	COUNT=`${YPWHICH} -d ${DOMAIN} -m 2>/dev/null | grep -i "${MASTER} | wc -l | tr -d " "`
+	if [ "$COUNT" = "0" ]
+	then
+		echo "Can't enumerate maps from ${MASTER}. Please check that it is running." 1>&2
+		exit 1
+	fi
+fi
+
+# Check if user is root
+ID=`id -u`
+if [ "${ID}" != "0" ]; then
+	echo "You have to be the superuser to run this.  Please login as root." 1>&2
 	exit 1
 fi
 
@@ -85,55 +126,72 @@ fi
 
 if [ ! -d ${YP_DIR} -o -f ${YP_DIR} ]
 then
-	echo "The directory ${YP_DIR} doesn't exist. Restore it from the distribution." 1>&2
-	echo "(Or move ${YP_DIR}.no to ${YP_DIR} if YP has not been activated before." 1>&2
+	echo "The directory ${YP_DIR} doesn't exist.  Restore it from the distribution." 1>&2
 	exit 1
 
 fi
 
-#echo "Server Type: ${SERVERTYPE} Domain: ${DOMAIN} Master: ${MASTER}"
-
-echo "Installing the YP data base will require that you answer a few questions."
-echo "Questions will all be asked at the beginning of the procedure."
+echo -n "Server Type: ${SERVERTYPE} Domain: ${DOMAIN}"
+if [ "${SERVERTYPE}" = "SLAVE" ]; then
+	echo -n " Master: ${MASTER}"
+fi
 echo ""
 
-if [ -d ${YP_DIR}/${DOMAIN} ]; then
-	
-	echo -n "Can we destroy the existing ${YP_DIR}/${DOMAIN} and its contents? [y/n: n]  "
-	read KILL
+if [ "${SERVERTYPE}" != "UPDATE" ];
+then
+	cat << \__notice1
 
-	ERROR=
-	case ${KILL} in
-	y*)	ERROR=DELETE;;
-	Y*)	ERROR=DELETE;;
-	*)	ERROR=;;
+Creating an YP server will require that you answer a few questions.
+Questions will all be asked at the beginning of the procedure.
+
+__notice1
+
+	echo -n "Do you want this procedure to quit on non-fatal errors? [y/n: n]  "
+	read DOEXIT
+
+	case ${DOEXIT} in
+	y*|Y*)
+		ERROR_EXIT="YES"
+		;;
+
+	*)	ERROR_EXIT="NO"
+		echo ""
+		echo "Ok, please remember to go back and redo manually whatever fails."
+		echo "If you don't, something might not work. "
+		;;
 	esac
 
-	if [ -z "${ERROR}" ]
-	then
-		echo "OK, please clean it up by hand and start again.  Bye"
-		exit 0
-	fi
+	if [ -d "${YP_DIR}/${DOMAIN}" ]; then
+		echo ""	
+		echo -n "Can we destroy the existing ${YP_DIR}/${DOMAIN} and its contents? [y/n: n]  "
+		read KILL
 
-	if [ "${ERROR}" = "DELETE" ]
-	then
-		rm -r -f ${YP_DIR}/${DOMAIN}
-		
-		if [ $?  -ne 0 ]
-		then
-			echo "Can't clean up old directory ${YP_DIR}/${DOMAIN}.  Fatal error." 1>&2
-			exit 1
+		ERROR=
+		case ${KILL} in
+		y*|Y*)
+			ERROR="DELETE"
+			;;
+
+		*)	ERROR=
+			;;
+		esac
+
+		if [ "${ERROR}" = "DELETE" ]; then
+			if ! rm -rf ${YP_DIR}/${DOMAIN}; then
+				echo "Can't clean up old directory ${YP_DIR}/${DOMAIN}." 1>&2
+				exit 1
+			fi
+		else
+			echo "OK, please clean it up by hand and start again.  Bye"
+			exit 0
 		fi
+	
 	fi
 
-fi
-
-mkdir ${YP_DIR}/${DOMAIN}
-
-if [ $?  -ne 0 ]
-then
-	echo "Can't make new directory ${YP_DIR}/${DOMAIN}.  Fatal error." 1>&2
-	exit 1
+	if ! mkdir "${YP_DIR}/${DOMAIN}"; then
+		echo "Can't make new directory ${YP_DIR}/${DOMAIN}." 1>&2
+		exit 1
+	fi
 fi
 
 if [ "${SERVERTYPE}" = "MASTER" ];
@@ -143,7 +201,7 @@ then
 	then
 		if [ ! -f ${YP_DIR}/Makefile.main ]
 		then
-			echo "Can't find ${YP_DIR}/Makefile.main. " 1>&2
+			echo "Can't find ${YP_DIR}/Makefile.main. " 1>&2
 			exit 1
 		fi
 		cp ${YP_DIR}/Makefile.main ${YP_DIR}/Makefile
@@ -153,32 +211,29 @@ then
 	
 	if [ -z "${SUBDIR}" ]
 	then
-		echo "Can't find line starting with 'SUBDIR=' in ${YP_DIR}/Makefile. " 1>&2
+		echo "Can't find line starting with 'SUBDIR=' in ${YP_DIR}/Makefile. " 1>&2
 		exit 1
 	fi
 
 	NEWSUBDIR="SUBDIR="
-	for DIR in `echo ${SUBDIR} | cut -c8-255`
-	do
-		if [ ${DIR} != ${DOMAIN} ]
-		then
+	for DIR in `echo ${SUBDIR} | cut -c8-255`; do
+		if [ ${DIR} != ${DOMAIN} ]; then
 			NEWSUBDIR="${NEWSUBDIR} ${DIR}"
 		fi
 	done
 	NEWSUBDIR="${NEWSUBDIR} ${DOMAIN}"
 
-	if [ -f ${YP_DIR}/Makefile.tmp ]
-	then 
+	if [ -f ${YP_DIR}/Makefile.tmp ]; then 
 		rm ${YP_DIR}/Makefile.tmp
 	fi
 
 	mv ${YP_DIR}/Makefile ${YP_DIR}/Makefile.tmp
-	sed -e "s/^${SUBDIR}/${NEWSUBDIR}/" ${YP_DIR}/Makefile.tmp > ${YP_DIR}/Makefile
+	sed -e "s/^${SUBDIR}/${NEWSUBDIR}/" ${YP_DIR}/Makefile.tmp > \
+	    ${YP_DIR}/Makefile
 	rm ${YP_DIR}/Makefile.tmp
 
-	if [ ! -f ${YP_DIR}/Makefile.yp ]
-	then
-		echo "Can't find ${YP_DIR}/Makefile.yp. " 1>&2
+	if [ ! -f ${YP_DIR}/Makefile.yp ]; then
+		echo "Can't find ${YP_DIR}/Makefile.yp. " 1>&2
 		exit 1
 	fi
 
@@ -189,21 +244,157 @@ fi
 if [ "${SERVERTYPE}" = "SLAVE" ];
 then
 
-	echo ""
+	echo "There will be no further questions. The remainder of the procedure"
+	echo "should take a few minutes, to copy the databases from ${MASTER}."
 
 	for MAP in `${YPWHICH} -d ${DOMAIN} -m | cut -d\  -f1`
 	do
 		echo "Transfering ${MAP}..."
-		${YPXFR} -h ${MASTER} -c -d ${DOMAIN} ${MAP}
-
-		if [ $?  -ne 0 ]
-		then
-			echo "Can't transfer map ${MAP}." 1>&2
-			exit 1
+		if ! ${YPXFR} -h ${MASTER} -c -d ${DOMAIN} ${MAP}; then
+			echo "Can't transfer map ${MAP}." 1>&2
+			ERROR_EXISTS="YES"
+			if [ "${ERROR_EXIT}" = "YES" ]; then
+				exit 1
+			fi
 		fi
 	done
 
 	echo ""
+	if [ "${ERROR_EXISTS}" = "YES"  ]; then
+		echo "${HOST} has been setup as an YP slave server with errors. " 1>&2
+		echo "Please remember fix any problem that occurred." 1>&2
+	else
+		echo "${HOST} has been setup as an YP slave server without any errors. "
+	fi
+
 	echo "Don't forget to update map ypservers on ${MASTER}."
 	exit 0
+fi
+
+LIST_OK="NO"
+
+while [ "${LIST_OK}" = "NO" ];
+do
+	
+	if [ "${SERVERTYPE}" = "MASTER" ];
+	then
+		HOST_LIST="${HOST}"
+		echo ""
+		echo "At this point, we have to construct a list of this domains YP servers."
+		echo "${HOST} is already known as master server."
+		echo "Please continue to add any slave servers, one per line. When you are"
+		echo "done with the list, type a <control D>."
+		echo "	master server   :  ${HOST}"
+	fi
+
+	if [ "${SERVERTYPE}" = "UPDATE" ];
+	then
+		HOST_LIST="${HOST}"
+		NEW_LIST=""
+		MASTER_NAME=""
+		SHORT_HOST=`echo ${HOST} | cut -d. -f1`
+		if [ -f ${YP_DIR}/${DOMAIN}/ypservers.db ];
+		then
+			for srv in `${MAKEDBM} -u ${YP_DIR}/${DOMAIN}/ypservers | grep -v "^YP" | tr "\t" " " | cut -d\  -f1`;
+			do
+				short_srv=`echo ${srv} | cut -d. -f1`
+				if [ "${SHORT_HOST}" != "${short_srv}" ]
+				then
+					if [ "${NEW_LIST}" = "" ];
+					then
+						NEW_LIST="${srv}"
+					else
+						NEW_LIST="${NEW_LIST} ${srv}"
+					fi
+				fi
+			done;
+			MASTER_NAME=`${MAKEDBM} -u ${YP_DIR}/${DOMAIN}/ypservers | grep "^YP_MASTER_NAME" | tr "\t" " " | cut -d\  -f2`
+		fi
+		echo ""
+		echo "Update the list of hosts running YP servers in domain ${DOMAIN}."
+		echo "Master for this domain is ${MASTER_NAME}."
+		echo ""
+		echo "First verify old servers, type \\ to remove a server." 
+		echo "Then add new servers, one per line. When done type a <control D>." 
+		echo ""
+		echo "	master server   :  ${HOST}"
+		if [ "${NEW_LIST}" != "" ]; then
+			for node in $NEW_LIST; do
+				echo -n "	verify host     : [${node}] "
+				read verify
+				if [ "${verify}" != "\\" ]; then
+					HOST_LIST="${HOST_LIST} ${node}"
+				fi
+			done;
+		fi
+	fi
+
+	echo -n "	next host to add:  "
+
+	while read h
+	do
+		echo -n "	next host to add:  "
+		HOST_LIST="${HOST_LIST} ${h}"
+	done
+
+	echo ""
+	echo "The current list of NIS servers looks like this:"
+	echo ""
+
+	for h in `echo ${HOST_LIST}`;
+	do
+		echo ${h}
+	done
+
+	echo ""
+	echo -n "Is this correct?  [y/n: y]  "
+	read hlist_ok
+
+	case $hlist_ok in
+	n*)	echo "Let's try the whole thing again...";;
+	N*)	echo "Let's try the whole thing again...";;
+	*)	LIST_OK="YES";;
+	esac
+
+done
+
+echo "Building ${YP_DIR}/${DOMAIN}/ypservers..."
+for host in ${HOST_LIST};
+do
+	echo "${host} ${host}"
+done | ${MAKEDBM} - ${YP_DIR}/${DOMAIN}/ypservers
+
+if [ $? -ne 0 ]; then
+	echo "" 1>&2
+	echo "Couldn't build yp data base ${YP_DIR}/${DOMAIN}/ypservers." 1>&2
+	ERROR_EXISTS="YES"
+	if [ "${ERROR_EXIT}" = "YES" ]; then 
+		exit 1
+	fi
+fi
+
+if [ "${SERVERTYPE}" = "MASTER" ]; then
+	
+	CUR_PWD=`pwd`
+	cd ${YP_DIR}/${DOMAIN}
+	echo "Running ${YP_DIR}/${DOMAIN}/Makefile..."
+	if ! make NOPUSH=1; then
+		echo "" 1>&2
+		echo "Error running Makefile." 1>&2
+		ERROR_EXISTS="YES"
+		if [ "${ERROR_EXIT}" = "YES" ]; then 
+			exit 1
+		fi
+	fi
+
+	cd ${CUR_PWD}
+
+	echo ""
+	if [ "${ERROR_EXISTS}" = "YES" ]; then
+		echo "${HOST} has been setup as an YP master server with errors. " 1>&2
+		echo "Please remember fix any problem that occurred." 1>&2
+	else
+		echo "${HOST} has been setup as an YP master server without any errors. "
+	fi
+
 fi
