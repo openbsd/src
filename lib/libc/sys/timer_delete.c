fec2e44ed066f6dc78ac92ad54ea0c6dc2c8099d
tholo <tholo@openbsd.org>
1997-04-26 08:49:33 +0000

    Add $OpenBSD$ tags

--- lib/libc/sys/timer_delete.c
+++ lib/libc/sys/timer_delete.c
@@ -1,3 +1,7 @@
+#if defined(SYSLIBC_SCCS) && !defined(lint)
+static char rcsid[] = "$OpenBSD: timer_delete.c,v 1.2 1997/04/26 08:49:33 tholo Exp $";
+#endif /* SYSLIBC_SCCS and not lint */
+
 #include <signal.h>
 #include <time.h>
 #include <errno.h>
--- lib/libc/sys/timer_getoverrun.c
+++ lib/libc/sys/timer_getoverrun.c
@@ -1,3 +1,7 @@
+#if defined(SYSLIBC_SCCS) && !defined(lint)
+static char rcsid[] = "$OpenBSD: timer_getoverrun.c,v 1.2 1997/04/26 08:49:34 tholo Exp $";
+#endif /* SYSLIBC_SCCS and not lint */
+
 #include <signal.h>
 #include <time.h>
 #include <errno.h>
