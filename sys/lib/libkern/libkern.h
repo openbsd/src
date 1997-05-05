981dff9e975e1e3790a3c1ad0323e8ae919c3fce
millert <millert@openbsd.org>
1997-05-05 14:35:09 +0000

    Removed strchr() for libkern.

--- sys/lib/libkern/libkern.h
+++ sys/lib/libkern/libkern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: libkern.h,v 1.10 1997/05/04 23:33:38 millert Exp $	*/
+/*	$OpenBSD: libkern.h,v 1.11 1997/05/05 14:35:09 millert Exp $	*/
 /*	$NetBSD: libkern.h,v 1.7 1996/03/14 18:52:08 christos Exp $	*/
 
 /*-
@@ -127,7 +127,6 @@ size_t	 strlen __P((const char *));
 char	*strcat __P((char *, const char *));
 char	*strcpy __P((char *, const char *));
 char	*strncpy __P((char *, const char *, size_t));
-char	*strchr __P((const char *, int));
 int	 strcmp __P((const char *, const char *));
 int	 strncmp __P((const char *, const char *, size_t));
 int	 strncasecmp __P((const char *, const char *, size_t));
