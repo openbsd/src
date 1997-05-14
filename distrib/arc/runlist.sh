fd3953542033f51a81c52a1b265b481cb4b95be1
pefo <pefo@openbsd.org>
1997-05-14 21:13:30 +0000

    More ARC ramdisk stuff

---
+++ distrib/arc/runlist.sh
@@ -0,0 +1,13 @@
+#	$Id: runlist.sh,v 1.1 1997/05/14 21:15:45 pefo Exp $
+
+if [ "X$1" = "X-d" ]; then
+	SHELLCMD=cat
+	shift
+else
+	SHELLCMD="sh -e"
+fi
+
+( while [ "X$1" != "X" ]; do
+	cat $1
+	shift
+done ) | awk -f ${TOPDIR}/list2sh.awk | ${SHELLCMD}
