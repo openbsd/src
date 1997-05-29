efd699c0c99b06c81423486f23ab9658b198bbcf
mickey <mickey@openbsd.org>
1997-05-29 04:35:29 +0000

    do \b

--- sys/arch/i386/stand/libsa/dev_i386.c
+++ sys/arch/i386/stand/libsa/dev_i386.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dev_i386.c,v 1.10 1997/04/23 06:49:07 mickey Exp $	*/
+/*	$OpenBSD: dev_i386.c,v 1.11 1997/05/29 04:35:29 mickey Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -113,6 +113,11 @@ putchar(c)
 	static int pos = 0;
 
 	switch(c) {
+	case '\b':	/* BS erases */
+		putc('\b');
+		putc(' ');
+		putc('\b');
+		break;
 	case '\t':
 		do
 			putc(' ');
