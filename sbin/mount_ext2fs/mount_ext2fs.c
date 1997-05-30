fa459b2e99cc08c53f515cc52a7966639cff6632
downsj <downsj@openbsd.org>
1997-05-30 08:48:58 +0000

    No longer enforce read-only mounts.

--- sbin/mount_ext2fs/mount_ext2fs.c
+++ sbin/mount_ext2fs/mount_ext2fs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mount_ext2fs.c,v 1.3 1997/04/24 08:28:54 downsj Exp $	*/
+/*	$OpenBSD: mount_ext2fs.c,v 1.4 1997/05/30 08:48:58 downsj Exp $	*/
 /*	$NetBSD: mount_ffs.c,v 1.3 1996/04/13 01:31:19 jtc Exp $	*/
 
 /*-
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)mount_ufs.c	8.2 (Berkeley) 3/27/94";
 #else
-static char rcsid[] = "$OpenBSD: mount_ext2fs.c,v 1.3 1997/04/24 08:28:54 downsj Exp $";
+static char rcsid[] = "$OpenBSD: mount_ext2fs.c,v 1.4 1997/05/30 08:48:58 downsj Exp $";
 #endif
 #endif /* not lint */
 
@@ -97,14 +97,6 @@ main(argc, argv)
 
         args.fspec = argv[0];		/* The name of the device file. */
 	fs_name = argv[1];		/* The mount point. */
-
-#if 1
-	/*
-	 * This version of ext2fs is buggy.  Enforce read only mounts.
-	 * If the user wants a writeable mount, they remove this code.
-	 */
-	mntflags |= MNT_RDONLY;
-#endif
 
 #define DEFAULT_ROOTUID	-2
 	args.export.ex_root = DEFAULT_ROOTUID;
