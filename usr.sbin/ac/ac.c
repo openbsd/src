4ecde8365d797d0c6e6df4c9b5e122255fdf79bd
deraadt <deraadt@openbsd.org>
1997-06-02 09:44:51 +0000

    correct for our pty list

--- usr.sbin/ac/ac.c
+++ usr.sbin/ac/ac.c
@@ -14,7 +14,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: ac.c,v 1.3 1997/01/15 23:43:44 millert Exp $";
+static char rcsid[] = "$Id: ac.c,v 1.4 1997/06/02 09:44:51 deraadt Exp $";
 #endif
 
 #include <sys/types.h>
@@ -499,12 +499,12 @@ ac(fp)
 			break;
 		default:
 			/*
-			 * if they came in on tty[p-y]*, then it is only
+			 * if they came in on a pseudo-tty, then it is only
 			 * a login session if the ut_host field is non-empty
 			 */
 			if (*usr.ut_name) {
 				if (strncmp(usr.ut_line, "tty", 3) != 0 ||
-				    strchr("pqrstuvwxy", usr.ut_line[3]) == 0 ||
+				    strchr("pqrstuvwxyzPQRST", usr.ut_line[3]) == 0 ||
 				    *usr.ut_host != '\0')
 					head = log_in(head, &usr);
 			} else
