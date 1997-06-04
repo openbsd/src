879344f6b01408ba169fbf7f178d46f04b4d8c02
kstailey <kstailey@openbsd.org>
1997-06-04 22:51:02 +0000

    Raise memory resource limits, drive by the need to run netscape 4.0b5 which has a text segment of > 8MB.
    If you maintain a port you might want to do the same.

--- sys/arch/i386/include/vmparam.h
+++ sys/arch/i386/include/vmparam.h
@@ -60,18 +60,18 @@
 /*
  * Virtual memory related constants, all in bytes
  */
-#define	MAXTSIZ		(8*1024*1024)		/* max text size */
+#define	MAXTSIZ		(64*1024*1024)		/* max text size */
 #ifndef DFLDSIZ
-#define	DFLDSIZ		(16*1024*1024)		/* initial data size limit */
+#define	DFLDSIZ		(128*1024*1024)		/* initial data size limit */
 #endif
 #ifndef MAXDSIZ
-#define	MAXDSIZ		(256*1024*1024)		/* max data size */
+#define	MAXDSIZ		(1*1024*1024*1024)	/* max data size */
 #endif
 #ifndef	DFLSSIZ
-#define	DFLSSIZ		(512*1024)		/* initial stack size limit */
+#define	DFLSSIZ		(2*1024*1024)		/* initial stack size limit */
 #endif
 #ifndef	MAXSSIZ
-#define	MAXSSIZ		(8*1024*1024)		/* max stack size */
+#define	MAXSSIZ		(32*1024*1024)		/* max stack size */
 #endif
 
 /*
