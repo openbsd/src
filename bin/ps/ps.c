a30d4caff37c4987820c2f01800f9651b0855064
michaels <michaels@openbsd.org>
1997-06-13 00:37:39 +0000

    return 1 if the process does not exist.

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ps.c,v 1.7 1996/12/22 02:57:50 tholo Exp $	*/
+/*	$OpenBSD: ps.c,v 1.8 1997/06/13 00:37:39 michaels Exp $	*/
 /*	$NetBSD: ps.c,v 1.15 1995/05/18 20:33:25 mycroft Exp $	*/
 
 /*-
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)ps.c	8.4 (Berkeley) 4/2/94";
 #else
-static char rcsid[] = "$OpenBSD: ps.c,v 1.7 1996/12/22 02:57:50 tholo Exp $";
+static char rcsid[] = "$OpenBSD: ps.c,v 1.8 1997/06/13 00:37:39 michaels Exp $";
 #endif
 #endif /* not lint */
 
@@ -320,7 +320,7 @@ main(argc, argv)
 	 */
 	printheader();
 	if (nentries == 0)
-		exit(0);
+		exit(1);
 	/*
 	 * sort proc list
 	 */
