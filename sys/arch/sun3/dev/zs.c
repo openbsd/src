3303184587767eb226ee25bd984d23a2217c5761
kstailey <kstailey@openbsd.org>
1997-06-23 13:40:47 +0000

    In zs_abort, wrap a #ifdef DDB around a Debugger() invocation. Chris Jones via Gene Skonicki.

--- sys/arch/sun3/dev/zs.c
+++ sys/arch/sun3/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: zs.c,v 1.7 1997/01/16 04:04:00 kstailey Exp $	*/
+/*	$OpenBSD: zs.c,v 1.8 1997/06/23 13:40:47 kstailey Exp $	*/
 /*	$NetBSD: zs.c,v 1.42 1996/11/20 18:57:03 gwr Exp $	*/
 
 /*-
@@ -690,7 +690,7 @@ zs_abort()
 		rr0 = zc->zc_csr;
 		ZS_DELAY();
 	} while (rr0 & ZSRR0_BREAK);
-
-	/* XXX - Always available, but may be the PROM monitor. */
+#ifdef DDB
 	Debugger();
+#endif
 }
