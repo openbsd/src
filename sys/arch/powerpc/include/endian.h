bb7b6af9d57407813fd20bb83021c59d70d37072
grr <grr@openbsd.org>
1997-06-24 20:50:42 +0000

    change like sparc/include/endian.h

--- sys/arch/powerpc/include/endian.h
+++ sys/arch/powerpc/include/endian.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: endian.h,v 1.4 1997/04/04 03:05:34 millert Exp $	*/
+/*	$OpenBSD: endian.h,v 1.5 1997/06/24 20:50:42 grr Exp $	*/
 /*	$NetBSD: endian.h,v 1.2 1996/10/13 03:16:41 christos Exp $	*/
 
 /*-
@@ -71,10 +71,10 @@ __END_DECLS
 #define	htonl(x)	(x)
 #define	htons(x)	(x)
 
-#define	NTOHL(x)	(void) (x)
-#define	NTOHS(x)	(void) (x)
-#define	HTONL(x)	(void) (x)
-#define	HTONS(x)	(void) (x)
+#define	NTOHL(x)	(x)
+#define	NTOHS(x)	(x)
+#define	HTONL(x)	(x)
+#define	HTONS(x)	(x)
 
 #else
 
