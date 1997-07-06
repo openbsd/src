5478cfae97812454a1a92e40e879037760365599
niklas <niklas@openbsd.org>
1997-07-06 18:12:01 +0000

    wd and acd support

--- sys/arch/alpha/alpha/conf.c
+++ sys/arch/alpha/alpha/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.11 1997/05/06 19:47:12 niklas Exp $	*/
+/*	$OpenBSD: conf.c,v 1.12 1997/07/06 18:12:01 niklas Exp $	*/
 /*	$NetBSD: conf.c,v 1.16 1996/10/18 21:26:57 cgd Exp $	*/
 
 /*-
@@ -44,9 +44,13 @@
 #include <sys/conf.h>
 #include <sys/vnode.h>
 
+#include "wdc.h"
+bdev_decl(wd);
 bdev_decl(sw);
 #include "st.h"
 #include "cd.h"
+#include "acd.h"
+bdev_decl(acd);
 #include "sd.h"
 #include "ss.h"
 #include "uk.h"
@@ -57,11 +61,11 @@ bdev_decl(rd);
 
 struct bdevsw	bdevsw[] =
 {
-	bdev_notdef(),			/* 0 */
+	bdev_disk_init(NWDC,wd),	/* 0: ST506/ESDI/IDE disk */
 	bdev_swap_init(1,sw),		/* 1: swap pseudo-device */
 	bdev_tape_init(NST,st),		/* 2: SCSI tape */
 	bdev_disk_init(NCD,cd),		/* 3: SCSI CD-ROM */
-	bdev_notdef(),			/* 4 */
+	bdev_disk_init(NACD,acd),	/* 4: ATAPI CD-ROM */
 	bdev_notdef(),			/* 5 */
 	bdev_disk_init(NRD,rd),		/* 6: ram disk driver */
 	bdev_disk_init(NCCD,ccd),	/* 7: concatenated disk driver */
@@ -116,9 +120,9 @@ cdev_decl(ipl);
 #else
 #define NIPF 0
 #endif
-
 cdev_decl(prom);			/* XXX XXX XXX */
-
+cdev_decl(wd);
+cdev_decl(acd);
 
 struct cdevsw	cdevsw[] =
 {
@@ -158,6 +162,8 @@ struct cdevsw	cdevsw[] =
 	cdev_uk_init(NUK,uk),		/* 33: SCSI unknown */
 	cdev_random_init(1,random),	/* 34: random data source */
 	cdev_gen_ipf(NIPF,ipl),		/* 35: IP filter log */
+	cdev_disk_init(NWDC,wd),	/* 36: ST506/ESDI/IDE disk */
+	cdev_disk_init(NACD,acd),	/* 37: ATAPI CD-ROM */
 };
 int	nchrdev = sizeof (cdevsw) / sizeof (cdevsw[0]);
 
@@ -235,6 +241,8 @@ static int chrtoblktbl[] = {
 	/* 33 */	NODEV,
 	/* 34 */	NODEV,
 	/* 35 */	NODEV,
+	/* 36 */	0,
+	/* 37 */	4,
 };
 
 /*
