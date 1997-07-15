f3e644d36aebf566f04949d86228ea7717678cb0
millert <millert@openbsd.org>
1997-07-15 01:54:22 +0000

    Add a missing const

--- include/sha1.h
+++ include/sha1.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sha1.h,v 1.7 1997/07/12 20:06:01 millert Exp $	*/
+/*	$OpenBSD: sha1.h,v 1.8 1997/07/15 01:54:23 millert Exp $	*/
 
 /*
  * SHA-1 in C
@@ -15,9 +15,9 @@ typedef struct {
     u_char buffer[64];
 } SHA1_CTX;
   
-void SHA1Transform __P((u_int32_t state[5], u_char buffer[64]));
+void SHA1Transform __P((u_int32_t state[5], const u_char buffer[64]));
 void SHA1Init __P((SHA1_CTX *context));
-void SHA1Update __P((SHA1_CTX *context, u_char *data, u_int len));
+void SHA1Update __P((SHA1_CTX *context, const u_char *data, u_int len));
 void SHA1Final __P((u_char digest[20], SHA1_CTX *context));
 char *SHA1End __P((SHA1_CTX *, char *));
 char *SHA1File __P((char *, char *));
--- lib/libc/hash/sha1.c
+++ lib/libc/hash/sha1.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sha1.c,v 1.7 1997/07/12 00:51:18 millert Exp $	*/
+/*	$OpenBSD: sha1.c,v 1.8 1997/07/15 01:54:24 millert Exp $	*/
 
 /*
  * SHA-1 in C
@@ -50,7 +50,7 @@
  */
 void SHA1Transform(state, buffer)
     u_int32_t state[5];
-    u_char buffer[64];
+    const u_char buffer[64];
 {
     u_int32_t a, b, c, d, e;
     typedef union {
@@ -130,7 +130,7 @@ void SHA1Init(context)
  */
 void SHA1Update(context, data, len)
     SHA1_CTX *context;
-    u_char *data;
+    const u_char *data;
     u_int len;
 {
     u_int i, j;
