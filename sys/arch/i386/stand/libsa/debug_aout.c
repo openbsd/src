8c8104205c5be5cef14e91c9a2e183edf48adf74
mickey <mickey@openbsd.org>
1997-07-17 23:28:32 +0000

    includes

--- sys/arch/i386/stand/libsa/debug_aout.c
+++ sys/arch/i386/stand/libsa/debug_aout.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: debug_aout.c,v 1.1 1997/04/01 04:52:46 mickey Exp $	*/
+/*	$OpenBSD: debug_aout.c,v 1.2 1997/07/17 23:28:32 mickey Exp $	*/
 
 /*
  * Copyright (c) 1997 Michael Shalayeff
@@ -32,9 +32,9 @@
  *
  */
 
-#include <stand.h>
-#include <debug.h>
+#include <lib/libsa/stand.h>
 #include <sys/exec.h>
+#include "debug.h"
 
 int
 aout_init()
