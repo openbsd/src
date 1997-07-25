e98fead9ff5174553e04e8aa825670796fe9674b
weingart <weingart@openbsd.org>
1997-07-25 22:52:43 +0000

    Typo (from NetBSD #2622)

--- sys/arch/i386/isa/pcvt/pcvt_kbd.c
+++ sys/arch/i386/isa/pcvt/pcvt_kbd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcvt_kbd.c,v 1.10 1997/05/30 01:43:05 mickey Exp $	*/
+/*	$OpenBSD: pcvt_kbd.c,v 1.11 1997/07/25 22:52:43 weingart Exp $	*/
 
 /*
  * Copyright (c) 1992, 1995 Hellmuth Michaelis and Joerg Wunsch.
@@ -367,7 +367,7 @@ kbd_setmode(int mode)
 		cmd |= COMMAND_PCSCAN;	/*     yes, setup command */
 
 	kbc_8042cmd(CONTR_WRITE);
-	kbc_cmd(cmd);
+	kbd_cmd(cmd);
 	
 #endif /* PCVT_SCANSET > 1 */
 
