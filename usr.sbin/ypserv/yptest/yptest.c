8518f70f1ae9d187f82f2b89af6ee3783eb89d47
maja <maja@openbsd.org>
1997-07-26 11:59:03 +0000

    bug killed in NetBSD by thorpej.

--- usr.sbin/ypserv/yptest/yptest.c
+++ usr.sbin/ypserv/yptest/yptest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yptest.c,v 1.5 1996/06/26 21:26:40 maja Exp $ */
+/*	$OpenBSD: yptest.c,v 1.6 1997/07/26 11:59:03 maja Exp $ */
 
 /*
  * Copyright (c) 1994 Mats O Jansson <moj@stacken.kth.se>
@@ -32,7 +32,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: yptest.c,v 1.5 1996/06/26 21:26:40 maja Exp $";
+static char rcsid[] = "$OpenBSD: yptest.c,v 1.6 1997/07/26 11:59:03 maja Exp $";
 #endif
 
 #include <stdio.h>
@@ -45,8 +45,8 @@ static char rcsid[] = "$OpenBSD: yptest.c,v 1.5 1996/06/26 21:26:40 maja Exp $";
 
 static int
 yptest_foreach(status,key,keylen,val,vallen,data)
-int status,keylen,vallen,*data;
-char *key,*val;
+int status,keylen,vallen;
+char *key,*val,*data;
 {
   if (status == 2)
     return(0);
