6c64bcfe5c253c8e2f227e5c33634d9ce1ec6bb6
millert <millert@openbsd.org>
1997-08-18 05:29:40 +0000

    inttypes.h as required by XPG4.2.  Nothing uses this yet and all in-tree sources should use the BSD-style types anyway.

---
+++ include/inttypes.h
@@ -0,0 +1,53 @@
+/*	$OpenBSD: inttypes.h,v 1.1 1997/08/18 05:29:40 millert Exp $	*/
+
+/*
+ * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Todd C. Miller.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _INTTYPES_H_
+#define _INTTYPES_H_
+
+/* Machine type dependent parameters. */
+#include <machine/types.h>
+
+/*
+ * Fixed-size integer types as specified in XPG4.2.
+ * int8_t, int16_t, int32_t, int64_t are defined in <machine/types.h>
+ */
+
+typedef u_int8_t		uint8_t;
+typedef u_int16_t		uint16_t;
+typedef u_int32_t		uint32_t;
+typedef u_int64_t		uint64_t;
+
+/* XXX - these really belong in <machine/types.h> (maybe create u_register_t) */
+typedef long			intptr_t;
+typedef unsigned long		uintptr_t;
+
+#endif /* _INTTYPES_H_ */
