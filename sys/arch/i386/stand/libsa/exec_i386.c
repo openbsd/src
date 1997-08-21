2ae5bb8b924b2254a17f35a053fe20d6e45d6d32
mickey <mickey@openbsd.org>
1997-08-21 20:18:52 +0000

    missing )

--- sys/arch/i386/stand/libsa/exec_i386.c
+++ sys/arch/i386/stand/libsa/exec_i386.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_i386.c,v 1.11 1997/08/13 03:41:46 mickey Exp $	*/
+/*	$OpenBSD: exec_i386.c,v 1.12 1997/08/21 20:18:52 mickey Exp $	*/
 
 /*
  * Copyright (c) 1997 Michael Shalayeff
@@ -59,7 +59,7 @@ machdep_start(startaddr, howto, loadaddr, ssym, esym)
 	       x->a_midmag, x->a_text, x->a_data, x->a_bss, x->a_syms,
 	       x->a_entry, x->a_trsize, x->a_drsize);
 
-	printf("/bsd(%x,%x,%x,%x,%x,%x,%x,%x\n",
+	printf("/bsd(%x,%x,%x,%x,%x,%x,%x,%x)\n",
 		howto, bootdev, 0, round_to_size(esym),
 		extmem, cnvmem, (int)&apminfo, (int)&kentry);
 	getchar();
