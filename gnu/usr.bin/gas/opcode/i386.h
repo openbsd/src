b197fe6061e300121895227af3f0ffa8d8e23ca4
chuck <chuck@openbsd.org>
1997-09-04 22:27:28 +0000

    add %cr4 (from freebsd)

--- gnu/usr.bin/gas/opcode/i386.h
+++ gnu/usr.bin/gas/opcode/i386.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GAS; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
    
-/* $Id: i386.h,v 1.2 1996/06/17 00:22:45 deraadt Exp $ */
+/* $Id: i386.h,v 1.3 1997/09/04 22:27:28 chuck Exp $ */
 
 static const template i386_optab[] = {
 
@@ -757,6 +757,7 @@ static const reg_entry i386_regtab[] = {
   {"ds", SReg2, 3}, {"fs", SReg3, 4}, {"gs", SReg3, 5},
   /* control registers */
   {"cr0", Control, 0},   {"cr2", Control, 2},   {"cr3", Control, 3},
+  {"cr4", Control, 4},
   /* debug registers */
   {"db0", Debug, 0},   {"db1", Debug, 1},   {"db2", Debug, 2},
   {"db3", Debug, 3},   {"db6", Debug, 6},   {"db7", Debug, 7},
