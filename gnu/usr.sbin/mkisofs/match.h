6d18eb08eac0b30bcdc97c37d9818c16d4534311
downsj <downsj@openbsd.org>
1997-09-15 06:01:52 +0000

    mkisofs 1.11.  Not wrappered, unfortunately.  Stripped of most of the extra junk.

---
+++ gnu/usr.sbin/mkisofs/match.h
@@ -0,0 +1,15 @@
+/*	$OpenBSD: match.h,v 1.1.1.1 1997/09/15 06:01:53 downsj Exp $	*/
+/*
+ * 27th March 1996. Added by Jan-Piet Mens for matching regular expressions
+ * 		    in paths.
+ * 
+ */
+
+/*
+ * 	$From: match.h,v 1.1 1997/02/23 15:56:12 eric Rel $
+ */
+
+#include "fnmatch.h"
+
+void add_match();
+int matches();
