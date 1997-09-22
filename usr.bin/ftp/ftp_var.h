619b3d998be444c68f3bfa644455a79e058dbd98
millert <millert@openbsd.org>
1997-09-22 05:14:11 +0000

    socks5 support

--- usr.bin/ftp/ftp_var.h
+++ usr.bin/ftp/ftp_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ftp_var.h,v 1.14 1997/09/04 04:37:16 millert Exp $	*/
+/*	$OpenBSD: ftp_var.h,v 1.15 1997/09/22 05:14:11 millert Exp $	*/
 /*	$NetBSD: ftp_var.h,v 1.18 1997/08/18 10:20:25 lukem Exp $	*/
 
 /*
@@ -46,6 +46,10 @@
 #ifndef SMALL
 #include <histedit.h>
 #endif /* !SMALL */
+
+#ifdef SOCKS
+#include <socks.h>
+#endif
 
 #include "stringlist.h"
 #include "extern.h"
