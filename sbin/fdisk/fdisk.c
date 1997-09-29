23623e1d4e5cd35da6624019049d7823ff3653fc
weingart <weingart@openbsd.org>
1997-09-29 22:50:06 +0000

    Clean up junk before commit of new fdisk

--- sbin/fdisk/fdisk.c
+++ sbin/fdisk/fdisk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fdisk.c,v 1.19 1997/07/06 18:19:29 niklas Exp $	*/
+/*	$OpenBSD: fdisk.c,v 1.20 1997/09/29 22:50:06 weingart Exp $	*/
 /*	$NetBSD: fdisk.c,v 1.11 1995/10/04 23:11:19 ghudson Exp $	*/
 
 /*
@@ -28,7 +28,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: fdisk.c,v 1.19 1997/07/06 18:19:29 niklas Exp $";
+static char rcsid[] = "$OpenBSD: fdisk.c,v 1.20 1997/09/29 22:50:06 weingart Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -46,7 +46,7 @@ static char rcsid[] = "$OpenBSD: fdisk.c,v 1.19 1997/07/06 18:19:29 niklas Exp $
 #include <unistd.h>
 #include <util.h>
 
-#define _PATH_MBR	"/usr/mdec/sdboot"
+#define _PATH_MBR	"/usr/mdec/mbr"
 
 /*
  * 14-Dec-89  Robert Baron (rvb) at Carnegie-Mellon University
