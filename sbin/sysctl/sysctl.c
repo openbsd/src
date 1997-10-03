2ba7890d88034c3957f2f4a3483a0ffb874f33b0
deraadt <deraadt@openbsd.org>
1997-10-03 21:15:19 +0000

    put real geo field in ()

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysctl.c,v 1.27 1997/10/03 20:58:04 deraadt Exp $	*/
+/*	$OpenBSD: sysctl.c,v 1.28 1997/10/03 21:15:19 deraadt Exp $	*/
 /*	$NetBSD: sysctl.c,v 1.9 1995/09/30 07:12:50 thorpej Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)sysctl.c	8.1 (Berkeley) 6/6/93";
 #else
-static char *rcsid = "$OpenBSD: sysctl.c,v 1.27 1997/10/03 20:58:04 deraadt Exp $";
+static char *rcsid = "$OpenBSD: sysctl.c,v 1.28 1997/10/03 21:15:19 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -506,7 +506,7 @@ parse(string, flags)
 
 		if (!nflag)
 			(void)printf("%s = ", string);
-		printf("Cylinders=%d Tracks=%d Sectors=%d %08x\n",
+		printf("Cylinders=%d Tracks=%d Sectors=%d (%08x)\n",
 		    BIOSNTRACKS(geo), BIOSNHEADS(geo), BIOSNSECTS(geo), geo);
 		return;
 	}
