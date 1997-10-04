541d9c4c8fb73725d5a6d8a7e09734dc39d878ac
deraadt <deraadt@openbsd.org>
1997-10-04 00:03:08 +0000

    byte order of ether_type; khym@bga.com

--- usr.sbin/ipsend/common/ip.c
+++ usr.sbin/ipsend/common/ip.c
@@ -75,7 +75,7 @@ struct	in_addr	gwip;
 		perror("arp");
 		return -2;
 	    }
-	eh->ether_type = ETHERTYPE_IP;
+	eh->ether_type = htons(ETHERTYPE_IP);
 	last_gw.s_addr = gwip.s_addr;
 	err = sendip(nfd, s, sizeof(*eh) + len);
 	return err;
@@ -110,7 +110,7 @@ int	frag;
 		return -2;
 	    }
 	bcopy(eh->ether_dhost, last_arp, sizeof(last_arp));
-	eh->ether_type = ETHERTYPE_IP;
+	eh->ether_type = htons(ETHERTYPE_IP);
 
 	bcopy((char *)ip, (char *)&ipsv, sizeof(*ip));
 	last_gw.s_addr = gwip.s_addr;
