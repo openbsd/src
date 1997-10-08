94f6dc156c3f47b68abec01197706f81a2ea2e68
niklas <niklas@openbsd.org>
1997-10-08 07:07:50 +0000

    Proto regdump here until after 2.2, remove some statics

--- sys/arch/m68k/m68k/regdump.c
+++ sys/arch/m68k/m68k/regdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: regdump.c,v 1.1 1997/09/19 17:27:14 niklas Exp $	*/
+/*	$OpenBSD: regdump.c,v 1.2 1997/10/08 07:07:50 niklas Exp $	*/
 /*	$NetBSD: regdump.c,v 1.1 1997/04/09 19:21:47 thorpej Exp $	*/
 
 /*
@@ -51,8 +51,11 @@
 #include <machine/reg.h>
 #include <machine/psl.h>
 
-static void dumpmem __P((int *, int, int));
-static char *hexstr __P((int, int));
+/* XXX until m68k/cpu.h has it */
+void regdump __P((struct trapframe *, int));
+
+void dumpmem __P((int *, int, int));
+char *hexstr __P((int, int));
 
 /*
  * Print a register and stack dump.
