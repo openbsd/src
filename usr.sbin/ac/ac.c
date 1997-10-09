f470cc8ca608a2c689d9a472317630e457294137
deraadt <deraadt@openbsd.org>
1997-10-09 15:07:17 +0000

    if user specified wtmp file, use timestamp on final record instead of current system time for "end of run" time; tim@pool1.convey.ru

--- usr.sbin/ac/ac.c
+++ usr.sbin/ac/ac.c
@@ -14,7 +14,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: ac.c,v 1.6 1997/08/30 19:54:45 deraadt Exp $";
+static char rcsid[] = "$Id: ac.c,v 1.7 1997/10/09 15:07:17 deraadt Exp $";
 #endif
 
 #include <sys/types.h>
@@ -513,7 +513,8 @@ ac(fp)
 		}
 	}
 	(void)fclose(fp);
-	usr.ut_time = time((time_t *)0);
+	if (!(Flags & AC_W))
+		usr.ut_time = time((time_t *)0);
 	(void)strcpy(usr.ut_line, "~");
 	
 	if (Flags & AC_D) {
