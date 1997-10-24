261e186eb5f8ae58497159283b25ee0ca7caa55e
mickey <mickey@openbsd.org>
1997-10-24 00:53:30 +0000

    use real boot device

--- sys/arch/i386/stand/installboot/installboot.c
+++ sys/arch/i386/stand/installboot/installboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: installboot.c,v 1.21 1997/10/22 23:34:41 mickey Exp $	*/
+/*	$OpenBSD: installboot.c,v 1.22 1997/10/24 00:53:30 mickey Exp $	*/
 /*	$NetBSD: installboot.c,v 1.5 1995/11/17 23:23:50 gwr Exp $ */
 
 /*
@@ -156,7 +156,7 @@ main(argc, argv)
 			bios_diskinfo_t di;
 
 			mib[2] = BIOS_DISKINFO;
-			mib[3] = 0xa0000004;
+			mib[3] = biosdev;
 			size = sizeof(di);
 
 			if (sysctl(mib, 4, &di, &size, NULL, 0) == -1)
