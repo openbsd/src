68024c1777f1af564a456dc88b3454d4b44fd604
chuck <chuck@openbsd.org>
1997-11-04 20:45:15 +0000

    db_output.c: kill db_ksprintn, db_printf_guts, kdbprintf, db_printf.   db_printf moves to kern/subr_prf.c and uses the new kprintf located there. db_output.h: kill kdbprintf [use db_printf]

--- sys/ddb/db_output.h
+++ sys/ddb/db_output.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_output.h,v 1.8 1997/07/07 19:45:25 niklas Exp $ */
+/*	$OpenBSD: db_output.h,v 1.9 1997/11/04 20:45:17 chuck Exp $ */
 /*	$NetBSD: db_output.h,v 1.9 1996/04/04 05:13:50 cgd Exp $	*/
 
 /* 
@@ -37,7 +37,5 @@ void db_force_whitespace __P((void));
 void db_putchar __P((int));
 int db_print_position __P((void));
 int db_printf __P((const char *, ...))
-    __kprintf_attribute__((__format__(__kprintf__,1,2)));
-int kdbprintf __P((const char *, ...))
     __kprintf_attribute__((__format__(__kprintf__,1,2)));
 void db_end_line __P((int));
