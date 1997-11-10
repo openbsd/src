33083d14d417efd58c612d9d8c3820e3be1ee2b4
deraadt <deraadt@openbsd.org>
1997-11-10 00:09:40 +0000

    use clnt_destroy(); PR#345

--- usr.sbin/ypserv/yppush/yppush.c
+++ usr.sbin/ypserv/yppush/yppush.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yppush.c,v 1.8 1997/11/04 07:40:51 deraadt Exp $ */
+/*	$OpenBSD: yppush.c,v 1.9 1997/11/10 00:09:40 deraadt Exp $ */
 
 /*
  * Copyright (c) 1995 Mats O Jansson <moj@stacken.kth.se>
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: yppush.c,v 1.8 1997/11/04 07:40:51 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: yppush.c,v 1.9 1997/11/10 00:09:40 deraadt Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -173,7 +173,7 @@ char *indata;
 		return;
 	}
 	if (transp->xp_port >= IPPORT_RESERVED) {
-		svcudp_destroy(transp);
+		clnt_destroy(transp);
 		fprintf(stderr, "yppush: Cannot allocate reserved port.\n");
 		return;
 	}
