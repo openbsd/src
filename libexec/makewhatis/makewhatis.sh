9a70283e83484d94aff8ebe9059b4a5f209738ab
millert <millert@openbsd.org>
1997-11-13 04:40:14 +0000

    From NetBSD:     makewhatis.sh rewrite by mrg@netbsd.org that uses getNAME(8).     Much faster for unformatted man pages now that there is no need     to format them on the fly.  Removes duplicate inode entries, so     files with multiple hard links are only parsed once. OpenBSD changes:     set $PATH to a reasonable value     avoid /tmp races via mktemp(1)     obey $TMPDIR

--- libexec/makewhatis/makewhatis.sh
+++ libexec/makewhatis/makewhatis.sh
@@ -1,47 +1,49 @@
 #! /bin/sh
 #
-# Written by J.T. Conklin <jtc@netbsd.org>.
+# written by matthew green <mrg@eterna.com.au>, based on the
+# original by J.T. Conklin <jtc@netbsd.org> and Thorsten
+# Frueauf <frueauf@ira.uka.de>.
+#
 # Public domain.
 #
+# $OpenBSD: makewhatis.sh,v 1.5 1997/11/13 04:40:15 millert Exp $
+#
 
-TDIR=/tmp/whatis$$
-FILE=$TDIR/whatis
+PATH=/usr/bin:/bin; export PATH
 
-um=`umask`
-umask 022
-if ! mkdir $TDIR ; then
-	printf "tmp directory %s already exists, looks like:\n" $TDIR
-	ls -alF $TDIR
+# Create temp files safely
+LIST=`mktemp ${TMPDIR=/tmp}/makewhatislist.XXXXXXXXXX` || exit 1
+WHATIS=`mktemp ${TMPDIR=/tmp}/whatis.XXXXXXXXXX` || {
+	rm -f $LIST
 	exit 1
-fi
-umask $um
-
-trap "rm -rf $TDIR; exit 1" 1 2 15
+}
+trap "rm -f $LIST $WHATIS; exit 1" 1 2 15
 
 MANDIR=${1-/usr/share/man}
 if test ! -d "$MANDIR"; then 
 	echo "makewhatis: $MANDIR: not a directory"
-	rm -rf $TDIR
+	rm -f $LIST $WHATIS
 	exit 1
 fi
 
-find $MANDIR -type f -name '*.0' -print | while read file
+find $MANDIR \( -type f -o -type l \) -name '*.[0-9]*' -ls | \
+    sort -n | awk '{if (u[$1]) next; u[$1]++ ; print $11}' > $LIST
+ 
+egrep '\.[1-9]$' $LIST | xargs /usr/libexec/getNAME | \
+	sed -e 's/ [a-zA-Z0-9]* \\-/ -/' >> $WHATIS
+
+egrep '\.0$' $LIST | while read file
 do
 	sed -n -f /usr/share/man/makewhatis.sed $file;
-done > $FILE
+done > $WHATIS
 
-find $MANDIR -type f -name '*.0.Z' -print | while read file
+egrep '\.[0].(gz|Z)$' $LIST | while read file
 do
-	zcat $file | sed -n -f /usr/share/man/makewhatis.sed;
-done >> $FILE
+	gzip -fdc $file | sed -n -f /usr/share/man/makewhatis.sed;
+done >> $WHATIS
 
-find $MANDIR -type f -name '*.0.gz' -print | while read file
-do
-	gzip -dc $file | sed -n -f /usr/share/man/makewhatis.sed;
-done >> $FILE
+sort -u -o $WHATIS $WHATIS
 
-sort -u -o $FILE $FILE
-
-install -o bin -g bin -m 444 $FILE "$MANDIR/whatis.db"
-rm -rf $TDIR
+install -o root -g bin -m 444 $WHATIS "$MANDIR/whatis.db"
+rm -f $LIST $WHATIS
 exit 0
