14a72b62c862c8a5cd1332ff55e40ffb17a4ab1a
deraadt <deraadt@openbsd.org>
1997-11-17 19:21:48 +0000

    oh, a timespec

--- sys/sys/unpcb.h
+++ sys/sys/unpcb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unpcb.h,v 1.3 1997/11/17 18:09:17 deraadt Exp $	*/
+/*	$OpenBSD: unpcb.h,v 1.4 1997/11/17 19:21:48 deraadt Exp $	*/
 /*	$NetBSD: unpcb.h,v 1.6 1994/06/29 06:46:08 cgd Exp $	*/
 
 /*
@@ -71,7 +71,7 @@ struct	unpcb {
 	struct	mbuf *unp_addr;		/* bound address of socket */
 	int	unp_cc;			/* copy of rcv.sb_cc */
 	int	unp_mbcnt;		/* copy of rcv.sb_mbcnt */
-	struct	timeval	unp_ctime;	/* holds creation time */
+	struct	timespec unp_ctime;	/* holds creation time */
 };
 
 #define	sotounpcb(so)	((struct unpcb *)((so)->so_pcb))
