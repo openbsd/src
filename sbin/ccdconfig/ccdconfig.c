17c72ddc69b6dac59fd538186ce420289986acd9
niklas <niklas@openbsd.org>
1997-11-26 22:34:04 +0000

    Add a CCDF_OLD flag used to get the old I/O policy

--- sbin/ccdconfig/ccdconfig.c
+++ sbin/ccdconfig/ccdconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ccdconfig.c,v 1.9 1997/09/14 08:52:13 deraadt Exp $	*/
+/*	$OpenBSD: ccdconfig.c,v 1.10 1997/11/26 22:34:05 niklas Exp $	*/
 /*	$NetBSD: ccdconfig.c,v 1.6 1996/05/16 07:11:18 thorpej Exp $	*/
 
 /*-
@@ -77,6 +77,7 @@ struct	flagval {
 	{ "CCDF_SWAP",		CCDF_SWAP },
 	{ "CCDF_UNIFORM",	CCDF_UNIFORM },
 	{ "CCDF_MIRROR",	CCDF_MIRROR },
+	{ "CCDF_OLD",		CCDF_OLD },
 	{ NULL,			0 },
 };
 
