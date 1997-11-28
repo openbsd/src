07bdb1e7b92f43ba2a64af246fb82831ee546109
art <art@openbsd.org>
1997-11-28 12:58:44 +0000

    This file is necessary.

---
+++ kerberosIV/include/version.h
@@ -0,0 +1,11 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+char *krb4_long_version = "@(#)$Version: krb4-0.9.7";
+char *krb4_version = "krb4-0.9.7";
+
+#ifndef VERSION
+#define VERSION "0.9.7"
+#endif
+
+#endif /* __VERSION_H__ */
