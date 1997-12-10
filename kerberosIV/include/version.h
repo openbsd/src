6f9ba2f52db0291446b2e028615ae513399162f0
art <art@openbsd.org>
1997-12-10 22:25:02 +0000

    krb4_version was already defined in kerberosIV/krb.h

--- kerberosIV/include/version.h
+++ kerberosIV/include/version.h
@@ -1,11 +1,8 @@
+/*	$OpenBSD: version.h,v 1.3 1997/12/10 22:25:02 art Exp $	*/
 #ifndef __VERSION_H__
-#define __VERSION_H__
 
 extern char *krb4_long_version;
-extern char *krb4_version;
 
-#ifndef VERSION
 #define VERSION "0.9.7"
-#endif
 
 #endif /* __VERSION_H__ */
