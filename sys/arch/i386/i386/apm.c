041ad13227f78961d6d3e374a548b6177582d6d7
rees <rees@openbsd.org>
1997-12-11 01:03:51 +0000

    oops -- local change leaked out (but it doesn't hurt anything)

--- sys/arch/i386/i386/apm.c
+++ sys/arch/i386/i386/apm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: apm.c,v 1.17 1997/12/11 00:58:44 rees Exp $	*/
+/*	$OpenBSD: apm.c,v 1.18 1997/12/11 01:03:51 rees Exp $	*/
 
 /*-
  * Copyright (c) 1995 John T. Kohl.  All rights reserved.
@@ -514,11 +514,7 @@ apm_set_powstate(dev, state)
 	return 0;
 }
 
-#ifdef HANDBOOK
-int apmidleon = 0;
-#else
 int apmidleon = 1;
-#endif
 
 void
 apm_cpu_busy()
