edc1379212ad2203fcce7424690371e25394cd7c
brian <brian@openbsd.org>
1997-12-17 21:18:07 +0000

    char filename[200] => char filename[MAXPATHLEN]
    Pointed out by: Theo de Raadt <deraadt@cvs.openbsd.org>

--- usr.sbin/ppp/systems.c
+++ usr.sbin/ppp/systems.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: systems.c,v 1.3 1997/12/17 21:17:48 brian Exp $
+ * $Id: systems.c,v 1.4 1997/12/17 21:18:07 brian Exp $
  *
  *  TODO:
  */
@@ -232,7 +232,7 @@ ReadSystem(const char *name, const char *file, int doexec)
   int n, len;
   u_char olauth;
   char line[LINE_LEN];
-  char filename[200];
+  char filename[MAXPATHLEN];
   int linenum;
   int argc;
   char **argv;
