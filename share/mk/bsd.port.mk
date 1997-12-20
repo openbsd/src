08fe6ac4b2a4a71fc242c5aa0bd07bf4d5b47ee7
joey <joey@openbsd.org>
1997-12-20 01:26:57 +0000

    openbsd maintainer changed from imp to me

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
-#	$OpenBSD: bsd.port.mk,v 1.20 1997/12/20 01:24:08 todd Exp $
+#	$OpenBSD: bsd.port.mk,v 1.21 1997/12/20 01:26:57 joey Exp $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -24,7 +24,7 @@
 # mailing list, and any correspondece should be directed there.
 #
 FreeBSD_MAINTAINER=	asami@FreeBSD.ORG
-OpenBSD_MAINTAINER=	imp@OpenBSD.ORG
+OpenBSD_MAINTAINER=	joey@OpenBSD.ORG
 
 # Supported Variables and their behaviors:
 #
