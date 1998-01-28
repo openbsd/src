208d82a41c2590f06d1365690f76379e19b7fe2b
pefo <pefo@openbsd.org>
1998-01-28 11:14:35 +0000

    Includes for generic Mips

---
+++ sys/arch/mips/include/setjmp.h
@@ -0,0 +1,7 @@
+/*	$OpenBSD: setjmp.h,v 1.1 1998/01/28 11:14:56 pefo Exp $	*/
+
+/*
+ * machine/setjmp.h: machine dependent setjmp-related information.
+ */
+
+#define	_JBLEN	83		/* size, in longs, of a jmp_buf */
