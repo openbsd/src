fe4e3d5a59c607e1316fd38733ccfdd5be714fb8
brian <brian@openbsd.org>
1998-02-04 01:01:24 +0000

    Correct magic number format string Spotted by: Bruce M. Walter <walter@fortean.com>

--- usr.sbin/ppp/lcp.c
+++ usr.sbin/ppp/lcp.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: lcp.c,v 1.8 1998/01/21 02:13:34 brian Exp $
+ * $Id: lcp.c,v 1.9 1998/02/04 01:01:24 brian Exp $
  *
  * TODO:
  *      o Validate magic number received from peer.
@@ -714,7 +714,7 @@ LcpDecodeConfig(u_char *cp, int plen, int mode_type)
 	LcpInfo.want_magic = GenerateMagic();
 	break;
       case MODE_REJ:
-	LogPrintf(LogLCP, " Magic 0x%80x is REJected!\n", magic);
+	LogPrintf(LogLCP, " Magic 0x%08x is REJected!\n", magic);
 	LcpInfo.want_magic = 0;
 	LcpInfo.his_reject |= (1 << type);
 	break;
