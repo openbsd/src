b4d69f8c5eacfa3d90bbbaa60e97f3cee9248589
deraadt <deraadt@openbsd.org>
1998-02-22 13:02:26 +0000

    fix db_console chunk of code properly

--- sys/arch/i386/isa/pccons.c
+++ sys/arch/i386/isa/pccons.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pccons.c,v 1.37 1998/02/05 16:48:30 deraadt Exp $	*/
+/*	$OpenBSD: pccons.c,v 1.39 1998/02/22 13:03:00 deraadt Exp $	*/
 /*	$NetBSD: pccons.c,v 1.99.4.1 1996/06/04 20:03:53 cgd Exp $	*/
 
 /*-
@@ -1681,7 +1681,7 @@ top:
 	if (pc_xmode > 0) {
 #if defined(DDB) && defined(XSERVER_DDB)
 		/* F12 enters the debugger while in X mode */
-		if (dt == 88 && db_console)
+		if (dt == 88)
 			if (db_console)
 				Debugger();
 #endif
@@ -1744,7 +1744,7 @@ top:
 	/*
 	 * Check for cntl-alt-esc.
 	 */
-	if (db_console && dt == 1 &&
+	if (dt == 1 &&
 	    (shift_state & (KB_CTL | KB_ALT)) == (KB_CTL | KB_ALT)) {
 		screen_restore(1);
 		if (db_console)
