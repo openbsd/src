4ee44e3c09cf204b433352597a6e0454522f79bc
deraadt <deraadt@openbsd.org>
1998-02-25 23:44:57 +0000

    patch could not have been tested. panics machine on boot

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_usrreq.c,v 1.18 1998/02/25 11:17:43 deraadt Exp $	*/
+/*	$OpenBSD: tcp_usrreq.c,v 1.19 1998/02/25 23:44:58 deraadt Exp $	*/
 /*	$NetBSD: tcp_usrreq.c,v 1.20 1996/02/13 23:44:16 christos Exp $	*/
 
 /*
@@ -90,7 +90,6 @@ tcp_usrreq(so, req, m, nam, control)
 	int req;
 	struct mbuf *m, *nam, *control;
 {
-	struct sockaddr_in *sin = mtod(nam, struct sockaddr_in *);
 	register struct inpcb *inp;
 	register struct tcpcb *tp = NULL;
 	int s;
@@ -190,12 +189,6 @@ tcp_usrreq(so, req, m, nam, control)
 	 * Send initial segment on connection.
 	 */
 	case PRU_CONNECT:
-		/* Trying to connect to some broadcast address */
-		if (in_broadcast(sin->sin_addr, NULL)) {
-			error = EINVAL;
-			break;
-		}
-
 		if (inp->inp_lport == 0) {
 			error = in_pcbbind(inp, NULL);
 			if (error)
@@ -204,7 +197,6 @@ tcp_usrreq(so, req, m, nam, control)
 		error = in_pcbconnect(inp, nam);
 		if (error)
 			break;
-
 		tp->t_template = tcp_template(tp);
 		if (tp->t_template == 0) {
 			in_pcbdisconnect(inp);
