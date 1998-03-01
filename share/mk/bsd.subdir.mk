afb785405c10f8deed6d467b96ba0a794a605dc1
niklas <niklas@openbsd.org>
1998-03-01 09:18:06 +0000

    Add Benedikt Stockebrand <benedikt@devnull.ruhr.de>'s SKIPDIR handling It actually has proven useful for developer work when in-transit changes of tools breaks certain parts of the system.  We do not recommend it on a regular basis though, as having customized setups make bug handling more troublesome

--- share/mk/bsd.subdir.mk
+++ share/mk/bsd.subdir.mk
@@ -1,10 +1,13 @@
-#	$OpenBSD: bsd.subdir.mk,v 1.9 1997/06/02 23:58:34 mickey Exp $
+#	$OpenBSD: bsd.subdir.mk,v 1.10 1998/03/01 09:18:06 niklas Exp $
 #	$NetBSD: bsd.subdir.mk,v 1.11 1996/04/04 02:05:06 jtc Exp $
 #	@(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
 
 .if !target(.MAIN)
 .MAIN: all
 .endif
+
+# Make sure this is defined
+SKIPDIR?=
 
 _SUBDIRUSE: .USE
 .if defined(SUBDIR)
@@ -23,10 +26,24 @@ _SUBDIRUSE: .USE
 		if [ -e ${.CURDIR}/$${_newdir_}/Makefile.bsd-wrapper ]; then \
 			_makefile_spec_="-f Makefile.bsd-wrapper"; \
 		fi; \
-		echo "===> $${_nextdir_}"; \
-		cd ${.CURDIR}/$${_newdir_}; \
-		${MAKE} ${.MAKEFLAGS} $${_makefile_spec_} _THISDIR_="$${_nextdir_}" \
-		    ${.TARGET:S/realinstall/install/:S/.depend/depend/}); \
+		subskipdir=''; \
+		for skipdir in ${SKIPDIR}; do \
+			subentry=$${skipdir#$${entry}}; \
+			if [ X$${subentry} != X$${skipdir} ]; then \
+				if [ X$${subentry} = X ]; then \
+					echo "($${_nextdir_} skipped)"; \
+					break; \
+				fi; \
+				subskipdir="$${subskipdir} $${subentry#/}"; \
+			fi; \
+		done; \
+		if [ X$${skipdir} = X -o X$${subentry} != X ]; then \
+			echo "===> $${_nextdir_}"; \
+			cd ${.CURDIR}/$${_newdir_}; \
+			${MAKE} ${.MAKEFLAGS} SKIPDIR="$${subskipdir}" \
+			    $${_makefile_spec_} _THISDIR_="$${_nextdir_}" \
+			    ${.TARGET:S/realinstall/install/:S/.depend/depend/}; \
+		fi); \
 	done
 
 ${SUBDIR}::
