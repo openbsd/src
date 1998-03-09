e91858c84c95176bd49c51c2dd3d7bd2c31ab023
deraadt <deraadt@openbsd.org>
1998-03-09 08:14:49 +0000

    main() _always_ returns int!

--- libexec/smtpd/src/smtpd.c
+++ libexec/smtpd/src/smtpd.c
@@ -2,7 +2,7 @@
  * smtpd, Obtuse SMTP daemon, storing agent. does simple collection of
  * mail messages, for later forwarding by smtpfwdd.
  *
- * $Id: smtpd.c,v 1.4 1998/02/07 21:29:40 beck Exp $
+ * $Id: smtpd.c,v 1.5 1998/03/09 08:14:49 deraadt Exp $
  * 
  * Copyright (c) 1996, 1997 Obtuse Systems Corporation. All rights
  * reserved.
@@ -40,7 +40,7 @@
 
 char *obtuse_copyright =
 "Copyright 1996 - Obtuse Systems Corporation - All rights reserved.";
-char *obtuse_rcsid = "$Id: smtpd.c,v 1.4 1998/02/07 21:29:40 beck Exp $";
+char *obtuse_rcsid = "$Id: smtpd.c,v 1.5 1998/03/09 08:14:49 deraadt Exp $";
 
 #include <stdarg.h>
 #include <stdlib.h>
@@ -1965,7 +1965,7 @@ snarfdata(int in, int out, long *size, int bin)
  * The brains of this operation
  */
 
-void
+int
 main(int argc, char **argv)
 {
   int opt;
--- libexec/smtpd/src/smtpfwdd.c
+++ libexec/smtpd/src/smtpfwdd.c
@@ -4,7 +4,7 @@
  * to pick it up and invokes sendmail (or sendmail-like agent) to
  * deliver it.
  *
- * $Id: smtpfwdd.c,v 1.1.1.1 1997/12/12 05:50:27 beck Exp $
+ * $Id: smtpfwdd.c,v 1.2 1998/03/09 08:14:51 deraadt Exp $
  * 
  * Copyright (c) 1996, 1997 Obtuse Systems Corporation. All rights
  * reserved.
@@ -41,7 +41,7 @@
  */
 char *obtuse_copyright =
 "Copyright 1996 - Obtuse Systems Corporation - All rights reserved.";
-char *obtuse_rcsid = "$Id: smtpfwdd.c,v 1.1.1.1 1997/12/12 05:50:27 beck Exp $";
+char *obtuse_rcsid = "$Id: smtpfwdd.c,v 1.2 1998/03/09 08:14:51 deraadt Exp $";
 
 #include <stdio.h>
 #include <signal.h>
@@ -760,7 +760,7 @@ forward(char *fname)
  * The brains of this operation
  */
 
-void
+int
 main(int argc, char **argv)
 {
   int opt;
