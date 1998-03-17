b2f8cbb81fb3d647231964796d98b1967eddec4d
deraadt <deraadt@openbsd.org>
1998-03-17 10:55:24 +0000

    add a VIATECH card too

--- sys/dev/isa/if_ed.c
+++ sys/dev/isa/if_ed.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ed.c,v 1.36 1998/03/17 10:50:33 deraadt Exp $	*/
+/*	$OpenBSD: if_ed.c,v 1.37 1998/03/17 10:55:24 deraadt Exp $	*/
 /*	$NetBSD: if_ed.c,v 1.105 1996/10/21 22:40:45 thorpej Exp $	*/
 
 /*
@@ -397,13 +397,15 @@ ed_pci_match(parent, match, aux)
 	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_WINBOND2 &&
 	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_WINBOND2_W89C940) ||
 	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_NETVIN &&
-	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_NETVIN_VN5000) ||
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_NETVIN_NV5000) ||
 	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_COMPEX &&
 	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_COMPEX_COMPEXE) ||
 	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_KTI &&
 	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_KTI_KTIE) ||
 	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_SURECOM &&
-	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_SURECOM_NE34))
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_SURECOM_NE34) ||
+	    (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_VIATECH &&
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_VIATECH_VT86C926))
 		return (1);
 	return (0);
 }
