d9f10fb2846b04f19efad1c657357d53dff30470
matthieu <matthieu@openbsd.org>
1998-04-01 20:14:16 +0000

    add read/write mouse driver

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.23 1997/11/23 05:17:58 mickey Exp $	*/
+/*	$OpenBSD: conf.h,v 1.24 1998/04/01 20:14:16 matthieu Exp $	*/
 /*	$NetBSD: conf.h,v 1.33 1996/05/03 20:03:32 christos Exp $	*/
 
 /*-
@@ -204,6 +204,13 @@ extern struct cdevsw cdevsw[];
 #define	cdev_mouse_init(c,n) { \
 	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
 	(dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
+	(dev_type_stop((*))) enodev, 0, dev_init(c,n,select), \
+	(dev_type_mmap((*))) enodev }
+
+/* open, close, read, write, ioctl, select */
+#define	cdev_mousewr_init(c,n) { \
+	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
+	dev_init(c,n,write), dev_init(c,n,ioctl), \
 	(dev_type_stop((*))) enodev, 0, dev_init(c,n,select), \
 	(dev_type_mmap((*))) enodev }
 
