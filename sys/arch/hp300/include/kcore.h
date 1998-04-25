ff9b52a6e0e6e77e72efa3b1e389e653ee08696f
downsj <downsj@openbsd.org>
1998-04-25 08:28:16 +0000

    kcore.h stub, uses m68k include.

---
+++ sys/arch/hp300/include/kcore.h
@@ -0,0 +1,9 @@
+/*	$OpenBSD: kcore.h,v 1.1 1998/04/25 08:28:16 downsj Exp $	*/
+/*	$NetBSD: kcore.h,v 1.1 1996/05/12 02:47:46 mhitch Exp $	*/
+
+#ifndef _MACHINE_KCORE_H_
+#define _MACHINE_KCORE_H_
+
+#include <m68k/kcore.h>
+
+#endif
