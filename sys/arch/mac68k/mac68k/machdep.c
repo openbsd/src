3ce1dd7173351557e14e18cca870f270f90ed1e4
gene <gene@openbsd.org>
1998-05-03 07:19:54 +0000

    Don't use option MADHATTER.  It is rather silly, and there is no reason not to pull in the internal video mappings for these models. Includes support for internal video on the LC475, Centris and Quadras in the 600s, and Q800 and Q900 models. Poof.  We support far more than NetBSD.

--- sys/arch/mac68k/mac68k/machdep.c
+++ sys/arch/mac68k/mac68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.52 1998/05/03 07:15:15 gene Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.53 1998/05/03 07:19:54 gene Exp $	*/
 /*	$NetBSD: machdep.c,v 1.134 1997/02/14 06:15:30 scottr Exp $	*/
 
 /*
@@ -2078,7 +2078,6 @@ struct {
 	{ MACH_MACPB180C,	(caddr_t)0xfc040000,	512 * 1024 },
 	{ MACH_MACPB500,	(caddr_t)0x60000000,	512 * 1024 },
 	{ MACH_MACLC520,	(caddr_t)0x60000000,	1024 * 1024 },
-#ifdef MADHATTER
 	{ MACH_MACLC475,	(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACLC475_33,	(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACLC575,	(caddr_t)0xf9000000,	1024 * 1024 },
@@ -2089,13 +2088,10 @@ struct {
 	{ MACH_MACQ610,		(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACQ630,		(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACQ650,		(caddr_t)0xf9000000,	1024 * 1024 },
-#endif
 	{ MACH_MACQ700,		(caddr_t)0xf9000000,	1024 * 1024 },
-#ifdef MADHATTER
 	{ MACH_MACQ800,		(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACQ900,		(caddr_t)0xf9000000,	1024 * 1024 },
 	{ MACH_MACQ950,		(caddr_t)0xf9000000,	1024 * 1024 },
-#endif
 	{ 0,			(caddr_t)0x0,		0 },
 };				/* End of intvid_info[] initialization. */
 
