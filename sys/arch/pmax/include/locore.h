eedcd2a0b4d1554f4254e7b156266df1c8276475
millert <millert@openbsd.org>
1998-05-08 19:02:31 +0000

    remove conflicting decl of switch_exit

--- sys/arch/pmax/include/locore.h
+++ sys/arch/pmax/include/locore.h
@@ -48,7 +48,6 @@
 struct user;
 extern int  copykstack __P((struct user *up));
 extern void MachSaveCurFPState __P((struct proc *p));
-extern int switch_exit __P((void)); /* XXX never really returns? */
 extern void blkclr __P((caddr_t val, int size));   /* bulk aligned bzero */
 
 /* MIPS-generic locore functions used by trap.c */
