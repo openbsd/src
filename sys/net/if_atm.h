429305eec2d32f4d0b1b025636fc233d045aaf8d
deraadt <deraadt@openbsd.org>
1998-05-10 18:53:05 +0000

    Make ATM_LLC_SETTYPE do the rigth thing: swap byte on LE machines, don't swap on BE machines. The previous revision required a ntohs() in atm_output(), to work on LE machines. This was broken for BE machines; bouyer

--- sys/net/if_atm.h
+++ sys/net/if_atm.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: if_atm.h,v 1.8 1996/11/09 23:02:31 chuck Exp $       */
+/*      $OpenBSD: if_atm.h,v 1.9 1998/05/10 18:53:05 deraadt Exp $       */
 
 /*
  *
@@ -98,8 +98,8 @@ struct atmllc {
 /* ATM_LLC macros: note type code in host byte order */
 #define ATM_LLC_TYPE(X) (((X)->type[0] << 8) | ((X)->type[1]))
 #define ATM_LLC_SETTYPE(X,V) { \
-	(X)->type[1] = ((V) >> 8) & 0xff; \
-	(X)->type[0] = ((V) & 0xff); \
+	(X)->type[0] = ((V) >> 8) & 0xff; \
+	(X)->type[1] = ((V) & 0xff); \
 }
 
 #ifdef _KERNEL
