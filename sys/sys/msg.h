a7d0e5246044569f46a044bba28ed8f95d69c7c4
deraadt <deraadt@openbsd.org>
1998-05-11 06:22:19 +0000

    u_short -> unsigned short; const msgsnd msgp arg

--- sys/sys/msg.h
+++ sys/sys/msg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: msg.h,v 1.2 1996/03/03 12:12:02 niklas Exp $	*/
+/*	$OpenBSD: msg.h,v 1.3 1998/05/11 06:22:19 deraadt Exp $	*/
 /*	$NetBSD: msg.h,v 1.9 1996/02/09 18:25:18 christos Exp $	*/
 
 /*
@@ -33,30 +33,30 @@
 #define MSG_NOERROR	010000		/* don't complain about too long msgs */
 
 struct msqid_ds {
-	struct	ipc_perm msg_perm;	/* msg queue permission bits */
-	struct	msg *msg_first;	/* first message in the queue */
-	struct	msg *msg_last;	/* last message in the queue */
-	u_long	msg_cbytes;	/* number of bytes in use on the queue */
-	u_long	msg_qnum;	/* number of msgs in the queue */
-	u_long	msg_qbytes;	/* max # of bytes on the queue */
-	pid_t	msg_lspid;	/* pid of last msgsnd() */
-	pid_t	msg_lrpid;	/* pid of last msgrcv() */
-	time_t	msg_stime;	/* time of last msgsnd() */
-	long	msg_pad1;
-	time_t	msg_rtime;	/* time of last msgrcv() */
-	long	msg_pad2;
-	time_t	msg_ctime;	/* time of last msgctl() */
-	long	msg_pad3;
-	long	msg_pad4[4];
+	struct ipc_perm	msg_perm;	/* msg queue permission bits */
+	struct msg	*msg_first;	/* first message in the queue */
+	struct msg	*msg_last;	/* last message in the queue */
+	unsigned long	msg_cbytes;	/* number of bytes in use on the queue */
+	unsigned long	msg_qnum;	/* number of msgs in the queue */
+	unsigned long	msg_qbytes;	/* max # of bytes on the queue */
+	pid_t		msg_lspid;	/* pid of last msgsnd() */
+	pid_t		msg_lrpid;	/* pid of last msgrcv() */
+	time_t		msg_stime;	/* time of last msgsnd() */
+	long		msg_pad1;
+	time_t		msg_rtime;	/* time of last msgrcv() */
+	long		msg_pad2;
+	time_t		msg_ctime;	/* time of last msgctl() */
+	long		msg_pad3;
+	long		msg_pad4[4];
 };
 
 struct msg {
-	struct	msg *msg_next;	/* next msg in the chain */
-	long	msg_type;	/* type of this message */
-    				/* >0 -> type of this message */
-    				/* 0 -> free header */
-	u_short	msg_ts;		/* size of this message */
-	short	msg_spot;	/* location of start of msg in buffer */
+	struct msg	*msg_next;	/* next msg in the chain */
+	long		msg_type;	/* type of this message */
+    					/* >0 -> type of this message */
+	    				/* 0 -> free header */
+	unsigned short	msg_ts;		/* size of this message */
+	short		msg_spot;	/* location of start of msg in buffer */
 };
 
 /*
@@ -153,7 +153,7 @@ struct msqid_ds *msqids;	/* MSGMNI msqid_ds struct's */
 __BEGIN_DECLS
 int msgctl __P((int, int, struct msqid_ds *));
 int msgget __P((key_t, int));
-int msgsnd __P((int, void *, size_t, int));
+int msgsnd __P((int, const void *, size_t, int));
 int msgrcv __P((int, void *, size_t, long, int));
 __END_DECLS
 #else
