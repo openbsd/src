b7f14957f0395712fa759420b3ea269266a6d201
art <art@openbsd.org>
1998-05-18 02:18:52 +0000

    kedstroy from kth-krb-980514

--- kerberosIV/kdestroy/kdestroy.c
+++ kerberosIV/kdestroy/kdestroy.c
@@ -1,100 +1,121 @@
-/*	$OpenBSD: kdestroy.c,v 1.4 1998/02/25 15:50:54 art Exp $	*/
-/* $KTH: kdestroy.c,v 1.8 1997/03/30 16:15:03 joda Exp $");
-
+/*	$OpenBSD: kdestroy.c,v 1.5 1998/05/18 02:18:52 art Exp $	*/
+/*	$KTH: kdestroy.c,v 1.10 1998/05/13 22:44:24 assar Exp $		*/
 /*
- * This source code is no longer held under any constraint of USA
- * `cryptographic laws' since it was exported legally.  The cryptographic
- * functions were removed from the code and a "Bones" distribution was
- * made.  A Commodity Jurisdiction Request #012-94 was filed with the
- * USA State Department, who handed it to the Commerce department.  The
- * code was determined to fall under General License GTDA under ECCN 5D96G,
- * and hence exportable.  The cryptographic interfaces were re-added by Eric
- * Young, and then KTH proceeded to maintain the code in the free world.
+ * Copyright (c) 1998 Kungliga Tekniska Högskolan
+ * (Royal Institute of Technology, Stockholm, Sweden). 
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ *
+ * 3. All advertising materials mentioning features or use of this software 
+ *    must display the following acknowledgement: 
+ *      This product includes software developed by Kungliga Tekniska 
+ *      Högskolan and its contributors. 
+ *
+ * 4. Neither the name of the Institute nor the names of its contributors 
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE. 
  */
 
-/*-
- * Copyright (C) 1989 by the Massachusetts Institute of Technology
- *
- * Export of this software from the United States of America is assumed
- * to require a specific license from the United States Government.
- * It is the responsibility of any person or organization contemplating
- * export to obtain such a license before exporting.
- *
- * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
- * distribute this software and its documentation for any purpose and
- * without fee is hereby granted, provided that the above copyright
- * notice appear in all copies and that both that copyright notice and
- * this permission notice appear in supporting documentation, and that
- * the name of M.I.T. not be used in advertising or publicity pertaining
- * to distribution of the software without specific, written prior
- * permission.  M.I.T. makes no representations about the suitability of
- * this software for any purpose.  It is provided "as is" without express
- * or implied warranty.
- *
- */
-
-/*
- * This program causes Kerberos tickets to be destroyed.
- * Options are: 
- *
- *   -q[uiet]	- no bell even if tickets not destroyed
- *   -f[orce]	- no message printed at all 
- *   -t		- do not destroy tokens
- */
-
-#include "kuser_locl.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <kerberosIV/krb.h>
 #include <kerberosIV/kafs.h>
+#include <getarg.h>
+#include <err.h>
 
-char progname[] = "kdestroy";
+int quiet_flag;
+#ifdef LEGACY_KDESTROY
+int unlog_flag;
+#else
+int unlog_flag = 1;
+#endif
+int help_flag;
+int version_flag;
+
+struct getargs args[] = {
+    { "quiet", 		'q',	arg_flag, 	&quiet_flag, 
+      "don't print any messages" },
+    { NULL, 		'f',	arg_flag, 	&quiet_flag },
+#ifdef LEGACY_KDESTROY
+    { "unlog", 		't',	arg_flag,       &unlog_flag, 
+      "destroy tokens" },
+#else
+    { "unlog", 		't',	arg_negative_flag, &unlog_flag,
+    "don't destroy tokens" },
+#endif
+    { "version", 	0,	arg_flag,	&version_flag },
+    { "help",		'h',	arg_flag,	&help_flag }
+};
+
+int num_args = sizeof(args) / sizeof(args[0]);
 
 static void
-usage(void)
+usage(int code)
 {
-    fprintf(stderr, "Usage: %s [-f] [-q] [-t]\n", progname);
-    exit(1);
+    arg_printusage(args, num_args, "");
+    exit(code);
 }
 
 int
 main(int argc, char **argv)
 {
-    int fflag=0, tflag = 0, k_errno;
-    int c;
+    int optind = 0;
+    int ret;
 
-    while((c = getopt(argc, argv, "fqt")) >= 0){
-	switch(c){
-	case 'f':
-	case 'q':
-	    fflag++;
-	    break;
-	case 't':
-	    tflag++;
-	    break;
-	default:
-	    usage();
-	}
-    }
-    if(argc - optind > 0)
-	usage();
+    if(getarg(args, num_args, argc, argv, &optind))
+	usage(1);
 
-    k_errno = dest_tkt();
+    if(help_flag)
+	usage(0);
 
-    if(!tflag && k_hasafs())
+    if(version_flag)
+	errx(0, "%s", krb4_version);
+    
+    ret = dest_tkt();
+
+    if(unlog_flag && k_hasafs())
 	k_unlog();
 
-    if (fflag) {
-	if (k_errno != 0 && k_errno != RET_TKFIL)
+    if (quiet_flag) {
+	if (ret != 0 && ret != RET_TKFIL)
 	    exit(1);
 	else
 	    exit(0);
-    } else {
-	if (k_errno == 0)
-	    printf("Tickets destroyed.\n");
-	else if (k_errno == RET_TKFIL)
-	    printf("No tickets to destroy.\n");
-	else {
-	    printf("Tickets NOT destroyed.\n");
-	    exit(1);
-	}
+    }
+    if (ret == 0)
+	printf("Tickets destroyed.\n");
+    else if (ret == RET_TKFIL)
+	printf("No tickets to destroy.\n");
+    else {
+	printf("Tickets NOT destroyed.\n");
+	exit(1);
     }
     exit(0);
 }
