c12c56de5b4c497cd1925fd790e1724cb4c1ec29
millert <millert@openbsd.org>
1998-06-29 00:50:56 +0000

    remove files from old dir setup

--- gnu/usr.bin/bc/version.h
+++
@@ -1,5 +0,0 @@
-/* 	$NetBSD: version.h,v 1.3 1994/12/02 00:43:45 phil Exp $  */
-
-#define BC_VERSION  \
-"bc 1.03 (Nov 2, 1994)\nCopyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc."
-
