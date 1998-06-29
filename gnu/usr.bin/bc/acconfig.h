85702ea006b93f6c36e7c580b7e39df2496e6617
millert <millert@openbsd.org>
1998-06-29 00:18:54 +0000

    GNU bc 1.05a

--- gnu/usr.bin/bc/acconfig.h
+++ gnu/usr.bin/bc/acconfig.h
@@ -1,4 +1,15 @@
-/* 	$NetBSD: acconfig.h,v 1.2 1994/12/02 00:43:20 phil Exp $  */
+/* PACKAGE name */
+#undef PACKAGE
+
+/* Package VERSION number */
+#undef VERSION
 
 /* define if the math lib is to be loaded from a file. */
 #undef BC_MATH_FILE
+
+/* Define to use the readline library. */
+#undef READLINE
+
+/* Define to `size_t' if <sys/types.h> and <stddef.h> don't define.  */
+#undef ptrdiff_t
+
