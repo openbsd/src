3d32e0c261607d529a2426a72a80ebf722eb3536
art <art@openbsd.org>
1998-07-07 19:09:53 +0000

    oops, do not forget to change the version number

--- kerberosIV/include/version.h
+++ kerberosIV/include/version.h
@@ -1,8 +1,8 @@
-/*	$OpenBSD: version.h,v 1.4 1998/02/18 10:42:22 art Exp $	*/
+/*	$OpenBSD: version.h,v 1.5 1998/07/07 19:09:53 art Exp $	*/
 #ifndef __VERSION_H__
 
 extern char *krb4_long_version;
 
-#define VERSION "0.9.8"
+#define VERSION "0.9.9"
 
 #endif /* __VERSION_H__ */
