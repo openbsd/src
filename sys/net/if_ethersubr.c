5f73dc90f1419b3a3295489b4c37c334c171c34c
ryker <ryker@openbsd.org>
1998-07-07 19:26:18 +0000

    htons(ETHERTYPE_IPX)

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ethersubr.c,v 1.25 1998/02/03 19:06:28 deraadt Exp $	*/
+/*	$OpenBSD: if_ethersubr.c,v 1.26 1998/07/07 19:26:18 ryker Exp $	*/
 /*	$NetBSD: if_ethersubr.c,v 1.19 1996/05/07 02:40:30 thorpej Exp $	*/
 
 /*
@@ -241,7 +241,7 @@ ether_output(ifp, m0, dst, rt0)
 #endif
 #ifdef IPX
 	case AF_IPX:
-		etype = ETHERTYPE_IPX;
+		etype = htons(ETHERTYPE_IPX);
  		bcopy((caddr_t)&satosipx(dst)->sipx_addr.ipx_host,
 		    (caddr_t)edst, sizeof (edst));
 		if (!bcmp((caddr_t)edst, (caddr_t)&ipx_thishost, sizeof(edst)))
