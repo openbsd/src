9f10690ddec870ec23333353809cf00c10f801b1
millert <millert@openbsd.org>
1998-07-12 17:09:17 +0000

    sendmail 8.9.1

--- usr.sbin/sendmail/src/mailstats.h
+++ usr.sbin/sendmail/src/mailstats.h
@@ -1,38 +1,19 @@
 /*
- * Copyright (c) 1983 Eric P. Allman
+ * Copyright (c) 1998 Sendmail, Inc.  All rights reserved.
+ * Copyright (c) 1983 Eric P. Allman.  All rights reserved.
  * Copyright (c) 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the sendmail distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
  *
- *	@(#)mailstats.h	8.1 (Berkeley) 6/7/93
+ *	@(#)mailstats.h	8.8 (Berkeley) 5/19/98
  */
+
+#define STAT_VERSION	2
+#define STAT_MAGIC	0x1B1DE
 
 /*
 **  Statistics structure.
@@ -40,10 +21,14 @@
 
 struct statistics
 {
+	int	stat_magic;		/* magic number */
+	int	stat_version;		/* stat file version */
 	time_t	stat_itime;		/* file initialization time */
 	short	stat_size;		/* size of this structure */
 	long	stat_nf[MAXMAILERS];	/* # msgs from each mailer */
 	long	stat_bf[MAXMAILERS];	/* kbytes from each mailer */
 	long	stat_nt[MAXMAILERS];	/* # msgs to each mailer */
 	long	stat_bt[MAXMAILERS];	/* kbytes to each mailer */
+	long	stat_nr[MAXMAILERS];	/* # rejects by each mailer */
+	long	stat_nd[MAXMAILERS];	/* # discards by each mailer */
 };
