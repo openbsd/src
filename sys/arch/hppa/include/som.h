7104ef8b66903ae8b8cf8de882139e23dc0a2de3
mickey <mickey@openbsd.org>
1998-07-14 17:14:19 +0000

    add MD som defines, etc.

---
+++ sys/arch/hppa/include/som.h
@@ -0,0 +1,52 @@
+/*	$OpenBSD: som.h,v 1.1 1998/07/14 17:14:19 mickey Exp $	*/
+
+/*
+ * Copyright (c) 1998 Michael Shalayeff
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Michael Shalayeff.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_HPPA_SOM_H_
+#define	_HPPA_SOM_H_
+
+/*
+ * system_id values
+ */
+
+#define	SOM_BSD		800
+#define	SOM_PA10	0x20b
+#define	SOM_PA11	0x210
+#define	SOM_PA12	0x211
+#define	SOM_PA20	0x214
+
+
+#define	SOM_BADMAGIC(fh) \
+	((fh)->a_magic != SOM_EXEC_MAGIC && \
+	 (fh)->a_magic != SOM_SHARE_MAGIC && \
+	 (fh)->a_magic != SOM_DEMAND_MAGIC)
+
+#endif /* _HPPA_SOM_H_ */
