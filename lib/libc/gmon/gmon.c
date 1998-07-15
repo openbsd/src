bb7286032af9c29439177f7ad08171b5a463eec2
millert <millert@openbsd.org>
1998-07-15 18:46:25 +0000

    open gmon.out mode 0644, not 0666

--- lib/libc/gmon/gmon.c
+++ lib/libc/gmon/gmon.c
@@ -32,7 +32,7 @@
  */
 
 #if !defined(lint) && defined(LIBC_SCCS)
-static char rcsid[] = "$OpenBSD: gmon.c,v 1.9 1998/07/05 18:36:52 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: gmon.c,v 1.10 1998/07/15 18:46:25 millert Exp $";
 #endif
 
 #include <sys/param.h>
@@ -212,7 +212,7 @@ _mcleanup()
 		proffile = "gmon.out";
 	}
 
-	fd = open(proffile , O_CREAT|O_TRUNC|O_WRONLY, 0666);
+	fd = open(proffile , O_CREAT|O_TRUNC|O_WRONLY, 0644);
 	if (fd < 0) {
 		perror( proffile );
 		return;
