4eb1a0d51956a0732196aab07d2d3cca53ee3ca4
deraadt <deraadt@openbsd.org>
1998-08-11 02:53:04 +0000

    fix "find . !" coredump; thepish@freebsd

--- usr.bin/find/operator.c
+++ usr.bin/find/operator.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: operator.c,v 1.2 1996/06/26 05:33:12 deraadt Exp $	*/
+/*	$OpenBSD: operator.c,v 1.3 1998/08/11 02:53:04 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -38,7 +38,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)operator.c	8.1 (Berkeley) 6/6/93";*/
-static char rcsid[] = "$OpenBSD: operator.c,v 1.2 1996/06/26 05:33:12 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: operator.c,v 1.3 1998/08/11 02:53:04 deraadt Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -193,7 +193,7 @@ not_squish(plan)
 			int notlevel = 1;
 
 			node = yanknode(&plan);
-			while (node->type == N_NOT) {
+			while (node != NULL && node->type == N_NOT) {
 				++notlevel;
 				node = yanknode(&plan);
 			}
