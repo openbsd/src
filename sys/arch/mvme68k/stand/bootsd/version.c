caaa4bee5126d725f88bf881dbd3c4991079bedf
smurph <smurph@openbsd.org>
1998-08-18 04:17:07 +0000

    New bootblocks code to work with bootxx.

--- sys/arch/mvme68k/stand/bootsd/version.c
+++ sys/arch/mvme68k/stand/bootsd/version.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: version.c,v 1.7 1997/04/22 16:13:39 gvf Exp $ */
+/*	$OpenBSD: version.c,v 1.8 1998/08/18 04:17:08 smurph Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
  * revision to increase. like change this q to an x, or something.
  */
 
-char *version = "$Revision: 1.7 $";
+char *version = "$Revision: 1.8 $";
