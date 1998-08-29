2a60fca2ae4e8a216b14d808fbd379442018237a
mickey <mickey@openbsd.org>
1998-08-29 01:25:14 +0000

    tabs vs space; hptmask definition

--- sys/arch/hppa/include/reg.h
+++ sys/arch/hppa/include/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.1 1998/07/07 21:32:45 mickey Exp $	*/
+/*	$OpenBSD: reg.h,v 1.2 1998/08/29 01:25:14 mickey Exp $	*/
 
 /* 
  * Copyright (c) 1990,1994 The University of Utah and
@@ -33,31 +33,31 @@
  *     int mfsr(sreg)		- move from space register
  */
 
-#define CR_RCTR	 0
-#define CR_PIDR1 8
-#define CR_PIDR2 9
-#define CR_CCR   10
-#define CR_SAR	 11
-#define CR_PIDR3 12
-#define CR_PIDR4 13
-#define CR_IVA	 14
-#define CR_EIEM  15
-#define CR_ITMR  16
-#define CR_PCSQ  17
-#define CR_PCOQ  18
-#define CR_IIR   19
-#define CR_ISR   20
-#define CR_IOR   21
-#define CR_IPSW  22
-#define CR_EIRR  23
-#define CR_PTOV  24
-#define CR_VTOP  25
-#define CR_TR2   26
-#define CR_TR3   27
-#define CR_TR4   28
-#define CR_TR5   29
-#define CR_TR6   30
-#define CR_TR7   31
+#define	CR_RCTR		0
+#define	CR_PIDR1	8
+#define	CR_PIDR2	9
+#define	CR_CCR		10
+#define	CR_SAR		11
+#define	CR_PIDR3	12
+#define	CR_PIDR4	13
+#define	CR_IVA		14
+#define	CR_EIEM		15
+#define	CR_ITMR		16
+#define	CR_PCSQ		17
+#define	CR_PCOQ		18
+#define	CR_IIR		19
+#define	CR_ISR		20
+#define	CR_IOR		21
+#define	CR_IPSW		22
+#define	CR_EIRR		23
+#define	CR_HPTMASK	24
+#define	CR_VTOP		25
+#define	CR_TR2		26
+#define	CR_TR3		27
+#define	CR_TR4		28
+#define	CR_TR5		29
+#define	CR_TR6		30
+#define	CR_TR7		31
 
 #define CCR_MASK 0xff
 
