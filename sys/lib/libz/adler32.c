5dc7a4a5addcae914e7f9c60ffdf9167ae224167
millert <millert@openbsd.org>
1998-09-08 04:07:46 +0000

    Let's commit the correct files, shall we.  This is the real zlib-1.1.3...

--- sys/lib/libz/adler32.c
+++ sys/lib/libz/adler32.c
@@ -1,9 +1,9 @@
 /* adler32.c -- compute the Adler-32 checksum of a data stream
- * Copyright (C) 1995-1996 Mark Adler
+ * Copyright (C) 1995-1998 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
-/* $Id: adler32.c,v 1.3 1998/09/08 03:32:04 millert Exp $ */
+/* @(#) $Id: adler32.c,v 1.4 1998/09/08 04:07:47 millert Exp $ */
 
 #include "zlib.h"
 
@@ -18,7 +18,7 @@
 #define DO16(buf)   DO8(buf,0); DO8(buf,8);
 
 /* ========================================================================= */
-uLong adler32(adler, buf, len)
+uLong ZEXPORT adler32(adler, buf, len)
     uLong adler;
     const Bytef *buf;
     uInt len;
