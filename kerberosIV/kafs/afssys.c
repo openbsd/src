8ed6eeef72a8fde06fc20616ec5a1640787a7b09
art <art@openbsd.org>
1998-09-23 21:25:39 +0000

    detect xfs when it's there

--- kerberosIV/kafs/afssys.c
+++ kerberosIV/kafs/afssys.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: afssys.c,v 1.6 1998/09/18 00:58:26 art Exp $	*/
+/*	$OpenBSD: afssys.c,v 1.7 1998/09/23 21:25:39 art Exp $	*/
 /*	$KTH: afssys.c,v 1.57 1998/05/09 17:19:03 joda Exp $	*/
 
 /*
@@ -85,5 +85,5 @@ k_setpag(void)
 int
 k_hasafs(void)
 {
-    return xfspioctl(AFSCALL_PROBE, NULL, 0, NULL, 0) == 1;
+    return xfspioctl(AFSCALL_PROBE, NULL, 0, NULL, 0) == 0;
 }
