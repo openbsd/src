7802006c36d95f82c119dc97e058abc81cd88037
deraadt <deraadt@openbsd.org>
1998-09-24 21:52:10 +0000

    parse /etc/resolv.conf if it exists...

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.30 1998/09/23 06:51:20 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.31 1998/09/24 21:52:10 deraadt Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997,1998 Todd Miller, Theo de Raadt
@@ -369,9 +369,12 @@ case "$resp" in
 		fi
 
 		resp="none"
-		if [ -f /tmp/resolv.conf ]; then
+		if [ -f /etc/resolv.conf ]; then
+			resp=`grep '^nameserver ' /etc/resolv.conf | \
+			    sed -e 's/^nameserver //' | sed -e '1q'`
+		elif [ -f /tmp/resolv.conf ]; then
 			resp=`grep '^nameserver ' /tmp/resolv.conf | \
-			    sed -e 's/^nameserver //'`
+			    sed -e 's/^nameserver //' | sed -e '1q'`
 		fi
 		echo -n	"Enter IP address of primary nameserver: [$resp] "
 		getresp "$resp"
