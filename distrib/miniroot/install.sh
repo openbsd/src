c303d0f24d9f4318684df0be50a7fe56a6af528f
deraadt <deraadt@openbsd.org>
1998-09-28 13:37:54 +0000

    if no default route, set it to "none"

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.37 1998/09/28 13:28:35 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.38 1998/09/28 13:37:54 deraadt Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997,1998 Todd Miller, Theo de Raadt
@@ -352,6 +352,7 @@ case "$resp" in
 		    grep '^default' |
 		    sed -e 's/^default          //' -e 's/ .*//'`
 		if [ "X${resp}" = "X" ]; then
+			resp=none
 			if [ -f /tmp/mygate ]; then
 				resp=`cat /etc/mygate`
 				if [ "X${resp}" = "X" ]; then
