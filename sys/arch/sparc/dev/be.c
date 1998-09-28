72ea81cabc30ca1c81b0c69df80c5c419bbf6a31
jason <jason@openbsd.org>
1998-09-28 05:12:22 +0000

    don't use bcopy to get the tx_flags value, get it directly

--- sys/arch/sparc/dev/be.c
+++ sys/arch/sparc/dev/be.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: be.c,v 1.12 1998/09/15 22:36:20 jason Exp $	*/
+/*	$OpenBSD: be.c,v 1.13 1998/09/28 05:12:22 jason Exp $	*/
 
 /*
  * Copyright (c) 1998 Theo de Raadt and Jason L. Wright.
@@ -515,7 +515,7 @@ betint(sc)
 		if (sc->sc_no_td <= 0)
 			break;
 
-		bcopy(&sc->sc_desc->be_txd[bix], &txd, sizeof(txd));
+		txd.tx_flags = sc->sc_desc->be_txd[bix].tx_flags;
 
 		if (txd.tx_flags & BE_TXD_OWN)
 			break;
