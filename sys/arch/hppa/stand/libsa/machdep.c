ce764d601aa6b4e085db3f2ab85f31836da60aa7
mickey <mickey@openbsd.org>
1998-09-29 07:23:48 +0000

    lot more debug

--- sys/arch/hppa/stand/libsa/machdep.c
+++ sys/arch/hppa/stand/libsa/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.1.1.1 1998/06/23 18:46:42 mickey Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.2 1998/09/29 07:23:48 mickey Exp $	*/
 /*	$NOWHERE: machdep.c,v 2.0 1998/06/17 20:49:17 mickey Exp $	*/
 
 /*
@@ -31,8 +31,16 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <sys/param.h>
+#include <sys/reboot.h>
 #include "libsa.h"
+#include <machine/iomod.h>
+#include <machine/pdc.h>
+#include <machine/lifvar.h>
 
+#include "dev_hppa.h"
+
+extern struct	stable_storage sstor;	/* contents of Stable Storage */
 int howto, bootdev;
 
 void
@@ -43,5 +51,44 @@ machdep()
 	debug_init();
 #endif
 	cninit();
-	getbinfo();
+
+#ifdef PDCDEBUG
+	if (debug) {
+		printf("SSTOR:\n");
+		printf("pri_boot=");	DEVPATH_PRINT(&sstor.ss_pri_boot);
+		printf("alt_boot=");	DEVPATH_PRINT(&sstor.ss_alt_boot);
+		printf("console =");	DEVPATH_PRINT(&sstor.ss_console);
+		printf("keyboard=");	DEVPATH_PRINT(&sstor.ss_keyboard);
+		printf("mem=%d, fn=%s, osver=%d\n"
+		       "os={%x,%x,%x,%x,%x,%x,%x,%x,%x,%x,%x,\n"
+		           "%x,%x,%x,%x,%x,%x,%x,%x,%x,%x,%x}\n",
+		       sstor.ss_fast_size, sstor.ss_filenames,
+		       sstor.ss_os_version,
+		       sstor.ss_os[0], sstor.ss_os[1], sstor.ss_os[2],
+		       sstor.ss_os[3], sstor.ss_os[4], sstor.ss_os[5],
+		       sstor.ss_os[6], sstor.ss_os[7], sstor.ss_os[8],
+		       sstor.ss_os[9], sstor.ss_os[10], sstor.ss_os[11],
+		       sstor.ss_os[12], sstor.ss_os[13], sstor.ss_os[14],
+		       sstor.ss_os[15], sstor.ss_os[16], sstor.ss_os[17],
+		       sstor.ss_os[18], sstor.ss_os[19], sstor.ss_os[20],
+		       sstor.ss_os[21]);
+
+		printf("PAGE0:\n");
+		printf("ivec=%x, pf=%p[%u], toc=%p[%u], rendz=%p\n"
+		       "mem: cont=%u, phys=%u, pdc_spa=%u, resv=%u, free=%x\n"
+		       "cpu_hpa=%p, pdc=%p, imm_hpa=%p[%u,%u]\n"
+		       "soft=%u, tic/10ms=%u\n",
+		       PAGE0->ivec_special, PAGE0->ivec_mempf,
+		       PAGE0->ivec_mempflen, PAGE0->ivec_toc,
+		       PAGE0->ivec_toclen, PAGE0->ivec_rendz,
+		       PAGE0->memc_cont, PAGE0->memc_phsize, PAGE0->memc_adsize,
+		       PAGE0->memc_resv, PAGE0->mem_free, PAGE0->mem_hpa,
+		       PAGE0->mem_pdc, PAGE0->imm_hpa, PAGE0->imm_spa_size,
+		       PAGE0->imm_max_mem, PAGE0->imm_soft_boot,
+		       PAGE0->mem_10msec);
+		printf("console:  ");	PZDEV_PRINT(&PAGE0->mem_cons);
+		printf("boot:     ");	PZDEV_PRINT(&PAGE0->mem_boot);
+		printf("keyboard: ");	PZDEV_PRINT(&PAGE0->mem_kbd);
+	}
+#endif
 }
