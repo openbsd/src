7d86e489cc4f2b4e4e7c79087b53b62d2c808949
millert <millert@openbsd.org>
1998-10-05 17:30:58 +0000

    make DIOCGPDINFO equivalent to DIOCGDINFO for cd's

--- sys/dev/atapi/acd.c
+++ sys/dev/atapi/acd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acd.c,v 1.33 1998/10/03 21:18:59 millert Exp $	*/
+/*	$OpenBSD: acd.c,v 1.34 1998/10/05 17:30:58 millert Exp $	*/
 
 /*
  * Copyright (c) 1996 Manuel Bouyer.  All rights reserved.
@@ -722,6 +722,7 @@ acdioctl(dev, cmd, addr, flag, p)
 
 	switch (cmd) {
 	case DIOCGDINFO:
+	case DIOCGPDINFO:
 		*(struct disklabel *)addr = *acd->sc_dk.dk_label;
 		return 0;
 
