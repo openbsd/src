03fd712e8041c74aec88a2fbed008d7122f2b5ab
wvdputte <wvdputte@openbsd.org>
1998-11-06 04:33:16 +0000

    add missing const to match prototype in string.h

--- lib/libc/string/strlcpy.c
+++ lib/libc/string/strlcpy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: strlcpy.c,v 1.1 1998/07/01 01:29:45 millert Exp $	*/
+/*	$OpenBSD: strlcpy.c,v 1.2 1998/11/06 04:33:16 wvdputte Exp $	*/
 
 /*
  * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -28,7 +28,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: strlcpy.c,v 1.1 1998/07/01 01:29:45 millert Exp $";
+static char *rcsid = "$OpenBSD: strlcpy.c,v 1.2 1998/11/06 04:33:16 wvdputte Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -41,7 +41,7 @@ static char *rcsid = "$OpenBSD: strlcpy.c,v 1.1 1998/07/01 01:29:45 millert Exp 
  */
 size_t strlcpy(dst, src, siz)
 	char *dst;
-	char *src;
+	const char *src;
 	size_t siz;
 {
 	register char *d = dst;
