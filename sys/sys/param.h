921287b54e289ff481aa8f0030e257c5b29690f2
deraadt <deraadt@openbsd.org>
1998-11-11 17:57:34 +0000

    define OpenBSD2_4 and date properly

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.20 1998/07/15 23:12:55 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.21 1998/11/11 17:57:34 deraadt Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
 /*-
@@ -45,8 +45,8 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define OpenBSD	199806		/* OpenBSD version (year & month). */
-#define OpenBSD2_3 1		/* OpenBSD 2.3 */
+#define OpenBSD	199812		/* OpenBSD version (year & month). */
+#define OpenBSD2_4 1		/* OpenBSD 2.4 */
 
 #ifndef NULL
 #define	NULL	0
