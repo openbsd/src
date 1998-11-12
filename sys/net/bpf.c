2be38f979348aea402b27dcdf9102514e24dfecc
deraadt <deraadt@openbsd.org>
1998-11-12 16:35:02 +0000

    crank BPF_BUFSIZE so that fddi frames fit

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bpf.c,v 1.13 1998/06/26 09:13:10 deraadt Exp $	*/
+/*	$OpenBSD: bpf.c,v 1.14 1998/11/12 16:35:02 deraadt Exp $	*/
 /*	$NetBSD: bpf.c,v 1.33 1997/02/21 23:59:35 thorpej Exp $	*/
 
 /*
@@ -85,7 +85,7 @@ caddr_t bpf_alloc();
 #define BPF_BUFSIZE (MCLBYTES-8)
 #define UIOMOVE(cp, len, code, uio) uiomove(cp, len, code, uio)
 #else
-#define BPF_BUFSIZE 4096
+#define BPF_BUFSIZE 8192	/* 4096 too small for FDDI frames */
 #define UIOMOVE(cp, len, code, uio) uiomove(cp, len, uio)
 #endif
 
