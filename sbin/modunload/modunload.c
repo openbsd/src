1332101b48e3bb6fe9e4af5c0ae2567a5ae6b027
deraadt <deraadt@openbsd.org>
1998-11-18 16:06:43 +0000

    better strtol() checking

--- sbin/modunload/modunload.c
+++ sbin/modunload/modunload.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: modunload.c,v 1.6 1998/11/18 10:43:04 art Exp $	*/
+/*	$OpenBSD: modunload.c,v 1.7 1998/11/18 16:06:43 deraadt Exp $	*/
 /*	$NetBSD: modunload.c,v 1.9 1995/05/28 05:23:05 jtc Exp $	*/
 
 /*
@@ -81,7 +81,8 @@ main(argc, argv)
 		switch (c) {
 		case 'i':
 			modnum = strtol(optarg, &endptr, 0);
-			if (*endptr != '\0')
+			if (modnum == LONG_MIN || modnum == LONG_MAX ||
+			    *endptr != '\0')
                                 errx(1, "not a valid number");
 			break;	/* number */
 		case 'n':
