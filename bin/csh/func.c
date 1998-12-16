3a1192655d162fa501922f3e6783919884a8afd8
deraadt <deraadt@openbsd.org>
1998-12-16 17:46:06 +0000

    fix built-in printf argument expansion (ie. printf '%d\n' `echo 23`); gjm11@dpmms.cam.ac.uk

--- bin/csh/func.c
+++ bin/csh/func.c
@@ -1,4 +1,4 @@
-/*    $OpenBSD: func.c,v 1.7 1997/11/15 21:51:30 todd Exp $       */
+/*    $OpenBSD: func.c,v 1.8 1998/12/16 17:46:06 deraadt Exp $       */
 /*    $NetBSD: func.c,v 1.11 1996/02/09 02:28:29 christos Exp $       */
 
 /*-
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)func.c	8.1 (Berkeley) 5/31/93";
 #else
-static char rcsid[] = "$OpenBSD: func.c,v 1.7 1997/11/15 21:51:30 todd Exp $";
+static char rcsid[] = "$OpenBSD: func.c,v 1.8 1998/12/16 17:46:06 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -1512,9 +1512,22 @@ doprintf(v, t)
     Char **v;
     struct command *t;
 {
+    Char **newv;
     char **c;
     extern int progprintf __P((int, char **));
     int ret;
+
+    gflag = 0;
+    tglob(v);
+    if (gflag) {
+        newv = globall(v);
+        if (newv == 0) {
+            stderror(ERR_NAME | ERR_NOMATCH);
+            return;
+        }
+        v = newv;
+        gargv=0;
+    }
 
     ret = progprintf(blklen(v), c = short2blk(v));
     (void) fflush(cshout);
