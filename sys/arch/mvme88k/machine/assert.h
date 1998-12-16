128edab9379a19991af6b1cd833b8179edc04942
smurph <smurph@openbsd.org>
1998-12-16 03:37:08 +0000

    Part of remove to get rid of unwanted directories

--- sys/arch/mvme88k/machine/assert.h
+++
@@ -1,8 +0,0 @@
-#define assert(x) \
-({\
-	if (!(x)) {\
-		printf("assertion failure \"%s\" line %d file %s\n", \
-		#x, __LINE__, __FILE__); \
-		panic("assertion"); \
-	} \
-})
--- sys/arch/mvme88k/machine/setjmp.h
+++
@@ -1,7 +0,0 @@
-/*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:37:10 cgd Exp $	*/
-
-/*
- * machine/setjmp.h: machine dependent setjmp-related information.
- */
-
-#define	_JBLEN	22		/* size, in longs, of a jmp_buf */
