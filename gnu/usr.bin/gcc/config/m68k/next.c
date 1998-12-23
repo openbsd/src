78ae16f687e2cffb10ac68f2c2dedf8f04d3aa25
espie <espie@openbsd.org>
1998-12-23 02:56:56 +0000

    2.7.2 -> 2.8.1: old i386 & m68k, nextstep renaming.

--- gnu/usr.bin/gcc/config/m68k/next.c
+++
@@ -1,7 +0,0 @@
-/* next.c:  Functions for NeXT as target machine for GNU C compiler.  */
-
-/* Note that the include below means that we can't debug routines in
-   m68k.c when running on a COFF system.  */
-
-#include "m68k/m68k.c"
-#include "nextstep.c"
