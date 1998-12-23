9c8da33edb6b15c931770a31d117988ce8d402c5
espie <espie@openbsd.org>
1998-12-23 02:48:25 +0000

    2.7.2 -> 2.8.1: renamed i370 files, i860 files no longer needed. This completes config arches that are not related to OpenBSD.

--- gnu/usr.bin/gcc/config/i860/xm-paragon.h
+++
@@ -1,4 +0,0 @@
-/* Configuration for GCC for Intel i860 running OSF/1AD.  */
-
-#include "i860/xm-i860.h"
-#include "xm-svr3.h"
--- gnu/usr.bin/gcc/config/i860/xm-sysv4.h
+++
@@ -1,4 +0,0 @@
-/* Configuration for GCC for Intel i860 running System V Release 4.  */
-
-#include "i860/xm-i860.h"
-#include "xm-svr4.h"
