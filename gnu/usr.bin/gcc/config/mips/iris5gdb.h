b52ddd205160a75d92b12c2e8a4e636c64959369
espie <espie@openbsd.org>
1998-12-23 01:30:51 +0000

    2.7.2 -> 2.8.1 cruft: - a29k-udi.h was renamed - gdb frag files instead of whole new configs.

--- gnu/usr.bin/gcc/config/mips/iris5gdb.h
+++
@@ -1,7 +0,0 @@
-/* Definitions of target machine for GNU compiler.  Irix version 5 with gas
-   and gdb.  */
-
-/* Use stabs instead of ECOFF debug format.  */
-#define PREFERRED_DEBUGGING_TYPE DBX_DEBUG
-
-#include "mips/iris5gas.h"
