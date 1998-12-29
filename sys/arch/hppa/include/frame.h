8792d27d36b7fcbdca50d4a0e089bb853de488cb
mickey <mickey@openbsd.org>
1998-12-29 21:52:37 +0000

    fix special tr* registers names, spaces vs tabs

--- sys/arch/hppa/include/frame.h
+++ sys/arch/hppa/include/frame.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: frame.h,v 1.3 1998/10/30 19:22:43 mickey Exp $	*/
+/*	$OpenBSD: frame.h,v 1.4 1998/12/29 21:52:37 mickey Exp $	*/
 
 
 #ifndef _MACHINE_FRAME_H_
@@ -35,29 +35,29 @@ struct trapframe {
 	u_int	r16;
 	u_int	r17;
 	u_int	r18;
-	u_int	t4;          /* r19 */
-	u_int	t3;          /* r20 */
-	u_int	t2;          /* r21 */
-	u_int	t1;          /* r22 */
-	u_int	arg3;        /* r23 */
-	u_int	arg2;        /* r24 */
-	u_int	arg1;	      /* r25 */
-	u_int	arg0;	      /* r26 */
-	u_int	dp;	      /* r27 */
-	u_int	ret0;	      /* r28 */
-	u_int	ret1;	      /* r29 */
-	u_int	sp;	      /* r30 */
+	u_int	t4;	/* r19 */
+	u_int	t3;	/* r20 */
+	u_int	t2;	/* r21 */
+	u_int	t1;	/* r22 */
+	u_int	arg3;	/* r23 */
+	u_int	arg2;	/* r24 */
+	u_int	arg1;	/* r25 */
+	u_int	arg0;	/* r26 */
+	u_int	dp;	/* r27 */
+	u_int	ret0;	/* r28 */
+	u_int	ret1;	/* r29 */
+	u_int	sp;	/* r30 */
 	u_int	r31;
-	u_int	sar;	      /* cr11 */
+	u_int	sar;	/* cr11 */
 	u_int	iioq_head;
 	u_int	iisq_head;
 	u_int	iioq_tail;
 	u_int	iisq_tail;
-	u_int	eiem;        /* cr15 */
-	u_int	iir;         /* cr19 */
-	u_int	isr;         /* cr20 */
-	u_int	ior;         /* cr21 */
-	u_int	ipsw;	      /* cr22 */
+	u_int	eiem;	/* cr15 */
+	u_int	iir;	/* cr19 */
+	u_int	isr;	/* cr20 */
+	u_int	ior;	/* cr21 */
+	u_int	ipsw;	/* cr22 */
 	u_int	sr4;
 	u_int	sr0;
 	u_int	sr1;
@@ -66,15 +66,15 @@ struct trapframe {
 	u_int	sr5;
 	u_int	sr6;
 	u_int	sr7;
-	u_int	rctr;         /* cr0 */
-	u_int	pidr1;        /* cr8 */
-	u_int	pidr2;        /* cr9 */
-	u_int	ccr;	       /* cr10 */
-	u_int	pidr3;        /* cr12 */
-	u_int	pidr4;        /* cr13 */
-	u_int	ptov;         /* cr24 */
-	u_int	tr1;          /* cr25 */
-	u_int	tr2;          /* cr26 */
+	u_int	rctr;	/* cr0 */
+	u_int	pidr1;	/* cr8 */
+	u_int	pidr2;	/* cr9 */
+	u_int	ccr;	/* cr10 */
+	u_int	pidr3;	/* cr12 */
+	u_int	pidr4;	/* cr13 */
+	u_int	hptm;	/* cr24 */
+	u_int	vtop;	/* cr25 */
+	u_int	tr2;	/* cr26 */
 	u_int	fpu; 
 
 	int tf_regs[10];
