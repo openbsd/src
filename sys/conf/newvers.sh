072b1e98bed53973860afbe7bdf9ffc31d499d20
deraadt <deraadt@openbsd.org>
1999-01-06 19:29:51 +0000

    relabel current for now

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.10 1998/09/15 21:36:23 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.11 1999/01/06 19:29:51 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -46,7 +46,7 @@ v=`cat version` u=${USER-root} d=`pwd` h=`hostname` t=`date`
 id=`basename ${d}`
 
 ost="OpenBSD"
-osr="2.4"
+osr="2.4-current"
 
 echo "char ostype[] = \"${ost}\";" > vers.c
 echo "char osrelease[] = \"${osr}\";" >> vers.c
