ec86ded1c2b7bd69de7ee7b640266f05c40f02f0
deraadt <deraadt@openbsd.org>
1999-01-08 00:03:00 +0000

    global decl for *intr() routines, plus add INET6

--- sys/arch/wgrisc/wgrisc/trap.c
+++ sys/arch/wgrisc/wgrisc/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.2 1997/07/23 06:58:33 denny Exp $	*/
+/*	$OpenBSD: trap.c,v 1.3 1999/01/08 00:03:00 deraadt Exp $	*/
 /*
  * Copyright (c) 1988 University of Utah.
  * Copyright (c) 1992, 1993
@@ -39,7 +39,7 @@
  * from: Utah Hdr: trap.c 1.32 91/04/06
  *
  *	from: @(#)trap.c	8.5 (Berkeley) 1/11/94
- *      $Id: trap.c,v 1.2 1997/07/23 06:58:33 denny Exp $
+ *      $Id: trap.c,v 1.3 1999/01/08 00:03:00 deraadt Exp $
  */
 
 #include <sys/param.h>
@@ -1008,6 +1008,12 @@ interrupt(statusReg, causeReg, pc, what, args)
 		if (netisr & (1 << NETISR_IP)) {
 			netisr &= ~(1 << NETISR_IP);
 			ipintr();
+		}
+#endif
+#ifdef INET6
+		if (netisr & (1 << NETISR_IPV6)) {
+			netisr &= ~(1 << NETISR_IPV6);
+			ipv6intr();
 		}
 #endif
 #ifdef NETATALK
