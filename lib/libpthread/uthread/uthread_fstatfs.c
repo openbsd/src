a34f1cf0d466d806b81d89ea7719ec93f927ccf8
d <d@openbsd.org>
1999-01-10 23:10:25 +0000

    ident

--- lib/libpthread/uthread/uthread_fstatfs.c
+++ lib/libpthread/uthread/uthread_fstatfs.c
@@ -29,6 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * $OpenBSD: uthread_fstatfs.c,v 1.2 1999/01/10 23:10:25 d Exp $
  */
 #include <signal.h>
 #include <errno.h>
