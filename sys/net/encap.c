383cfbdc74f379f92d1abccec8b3e3211a701c0d
angelos <angelos@openbsd.org>
1999-01-11 22:52:49 +0000

    Remove unnecessary panic().

--- sys/net/encap.c
+++ sys/net/encap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: encap.c,v 1.25 1999/01/07 06:05:02 deraadt Exp $	*/
+/*	$OpenBSD: encap.c,v 1.26 1999/01/11 22:52:49 angelos Exp $	*/
 
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
@@ -630,7 +630,7 @@ va_dcl
       return ENOBUFS;
 
     if ((m->m_flags & M_PKTHDR) == 0)
-      panic("encap_output()");
+      SENDERR(EINVAL);
 
     len = m->m_pkthdr.len; 
 
