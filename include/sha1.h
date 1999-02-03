5a7ec76725a4f17bcba0349b4cd10a17ad13f252
angelos <angelos@openbsd.org>
1999-02-03 03:13:18 +0000

    Add some useful defines (used by ssyslog).

--- include/sha1.h
+++ include/sha1.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sha1.h,v 1.8 1997/07/15 01:54:23 millert Exp $	*/
+/*	$OpenBSD: sha1.h,v 1.9 1999/02/03 03:13:18 angelos Exp $	*/
 
 /*
  * SHA-1 in C
@@ -22,5 +22,21 @@ void SHA1Final __P((u_char digest[20], SHA1_CTX *context));
 char *SHA1End __P((SHA1_CTX *, char *));
 char *SHA1File __P((char *, char *));
 char *SHA1Data __P((const u_char *, size_t, char *));
+
+#define SHA1_DIGESTSIZE       20
+#define SHA1_BLOCKSIZE        64
+#define HTONDIGEST(x) { \
+      x[0] = htonl(x[0]); \
+      x[1] = htonl(x[1]); \
+      x[2] = htonl(x[2]); \
+      x[3] = htonl(x[3]); \
+      x[4] = htonl(x[4]); }
+
+#define NTOHDIGEST(x) { \
+      x[0] = ntohl(x[0]); \
+      x[1] = ntohl(x[1]); \
+      x[2] = ntohl(x[2]); \
+      x[3] = ntohl(x[3]); \
+      x[4] = ntohl(x[4]); }
 
 #endif /* _SHA1_H */
