9f76f4002b2202cbaec4bc749817a15bb2a96cc8
d <d@openbsd.org>
1999-02-07 23:50:59 +0000

    implement sparc atomic lock with LDSTUB

--- lib/libpthread/arch/sparc/_atomic_lock.c
+++ lib/libpthread/arch/sparc/_atomic_lock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.4 1998/12/21 13:03:45 d Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.5 1999/02/07 23:51:00 d Exp $	*/
 /*
  * Atomic lock for sparc
  */
@@ -8,12 +8,37 @@
 int
 _atomic_lock(volatile _spinlock_lock_t * lock)
 {
-	return _thread_slow_atomic_lock(lock);
+	_spinlock_lock_t old;
+
+	/*
+	 *  "  ldstub  [address], reg_rd
+	 * 
+	 *  The atomic load-store instructions copy a byte from memory
+	 *  into r[rd]m then rewrite the addressed byte in memory to all
+	 *  ones [_SPINLOCK_LOCKED]. The operation is performed
+	 *  atomically, that is, without allowing intervening interrupts
+	 *  or deferred traps. In a multiprocessor system, two or more
+	 *  processors executing atomic load-store unsigned byte [...]
+	 *  addressing the same byte [...] simultaneously are guaranteed
+	 *  to execute them in an undefined, but serial order."
+	 *    - p101, The SPARC Architecure Manual (version 8) Prentice-Hall
+	 *
+	 * "LDSTUB loads a byte value from memory to a register and writes
+	 *  the value FF_16 into the addressed byte atomically. LDSTUB
+	 *  is the classic test-and-set instruction. Like SWAP, it has
+	 *  a consensus number of two and so cannot resolve more than
+	 *  two contending processes in a wait-free fashion."
+	 *    - p129, The SPARC Architecture Manual (version 9) Prentice-Hall
+	 *  (See also section J.6 (spinlocks))
+	 */
+	__asm__("ldstub %1,%0" : "=r" (old), "+m" (*lock));
+
+	return (old == _SPINLOCK_LOCKED);
 }
 
 int
 _atomic_is_locked(volatile _spinlock_lock_t * lock)
 {
 	
-	return _thread_slow_atomic_is_locked(lock);
+	return (*lock == _SPINLOCK_LOCKED);
 }
--- sys/arch/sparc/include/spinlock.h
+++ sys/arch/sparc/include/spinlock.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: spinlock.h,v 1.1 1999/01/08 08:25:36 d Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.2 1999/02/07 23:50:59 d Exp $	*/
 
 #ifndef _MACHINE_SPINLOCK_H_
 #define _MACHINE_SPINLOCK_H_
 
-#define _SPINLOCK_UNLOCKED	(0)
-#define _SPINLOCK_LOCKED	(1)
-typedef int _spinlock_lock_t;
+#define _SPINLOCK_UNLOCKED	(0x00)
+#define _SPINLOCK_LOCKED	(0xFF)
+typedef unsigned char _spinlock_lock_t;
 
 #endif
