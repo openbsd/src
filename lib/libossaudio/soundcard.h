2411ffc7cc1f951211f75a3c8cce1e1617f0f224
millert <millert@openbsd.org>
1999-02-17 01:27:36 +0000

    #undef _POSIX_SOURCE after inclusion of machine/endian.h (if it was not already defined).

--- lib/libossaudio/soundcard.h
+++ lib/libossaudio/soundcard.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: soundcard.h,v 1.1.1.1 1998/05/01 09:23:00 provos Exp $	*/
+/*	$OpenBSD: soundcard.h,v 1.2 1999/02/17 01:27:36 millert Exp $	*/
 /*	$NetBSD: soundcard.h,v 1.4 1997/10/29 20:23:27 augustss Exp $	*/
 
 /*
@@ -106,8 +106,13 @@
  * include all of endian.h because it contains a lot
  * junk symbols.  [augustss]
  */
+#ifndef _POSIX_SOURCE
 #define _POSIX_SOURCE		/* avoid dragging in a lot of junk */
 #include <machine/endian.h>
+#undef _POSIX_SOURCE
+#else
+#include <machine/endian.h>
+#endif
 #if _QUAD_LOWWORD == 0
 #define  AFMT_S16_NE AFMT_S16_LE
 #else
