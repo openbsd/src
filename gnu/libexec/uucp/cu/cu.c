96ce071fd08258819de75a82525b2e7597b6c099
deraadt <deraadt@openbsd.org>
1999-02-24 10:21:20 +0000

    broken send file function fix, Frederick Bruckman, netbsd pr 6834

--- gnu/libexec/uucp/cu/cu.c
+++ gnu/libexec/uucp/cu/cu.c
@@ -26,7 +26,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char cu_rcsid[] = "$Id: cu.c,v 1.1.1.1 1995/10/18 08:38:27 deraadt Exp $";
+const char cu_rcsid[] = "$Id: cu.c,v 1.2 1999/02/24 10:21:20 deraadt Exp $";
 #endif
 
 #include "cu.h"
@@ -2101,7 +2101,7 @@ fcusend_buf (qconn, zbufarg, cbufarg)
 	 of normal characters; when we send a newline we look for
 	 *zCuvar_echonl.  */
       if ((fCuvar_echocheck && ! fCuvar_binary)
-	  || (fnl && *zCuvar_echonl != '\0'))
+	  && (fnl && *zCuvar_echonl != '\0'))
 	{
 	  long iend;
 
