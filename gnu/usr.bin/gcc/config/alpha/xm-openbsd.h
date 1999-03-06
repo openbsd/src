4eb6772e7918b1e68cd7d5bba40cb66094528369
espie <espie@openbsd.org>
1999-03-06 04:08:44 +0000

    Wrong file, fixed.

--- gnu/usr.bin/gcc/config/alpha/xm-openbsd.h
+++ gnu/usr.bin/gcc/config/alpha/xm-openbsd.h
@@ -1,4 +1,4 @@
-/* Configuration fragment for hosts running a version of OpenBSD.
+/* Configuration file for an host running alpha OpenBSD.
    Copyright (C) 1999 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
@@ -17,19 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
-/* This file gets included by all architectures. It holds stuff
-   that ought to be defined when hosting a compiler on an OpenBSD
-   machine, independently of the architecture. It's included by
-   ${cpu_type}/xm-openbsd.h, not included directly.  */
 
-/* OpenBSD is trying to be POSIX-compliant, to the point of fixing
-   problems that may occur with gcc's interpretation.  */
-#undef POSIX
-#define POSIX
-
-/* Ensure we get gnu C's defaults.  */
-#ifdef __GNUC__
-#define alloca __builtin_alloca
-#endif
-
+#include <xm-openbsd.h>
+#include <alpha/xm-alpha.h>
 
