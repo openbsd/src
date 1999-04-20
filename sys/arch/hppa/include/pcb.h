6cd5cc568b3cb4f99614ab6882d62384ab6b1cb3
mickey <mickey@openbsd.org>
1999-04-20 19:46:22 +0000

    add onfault handler and cache the space id into pcb; better coredump

--- sys/arch/hppa/include/pcb.h
+++ sys/arch/hppa/include/pcb.h
@@ -1,12 +1,20 @@
-/*	$OpenBSD: pcb.h,v 1.2 1999/02/25 17:36:53 mickey Exp $	*/
+/*	$OpenBSD: pcb.h,v 1.3 1999/04/20 19:46:22 mickey Exp $	*/
 
+#ifndef _MACHINE_PCB_H_
+#define _MACHINE_PCB_H_
+
+#include <machine/reg.h>
 
 struct pcb {
 	struct trapframe pcb_tf;
+	/* would be nice to align to cache line size here XXX */
+	int (*pcb_onfault) __P((void));	/* SW copy fault handler */
+	pa_space_t pcb_space;		/* copy pmap_space, for asm's sake */
 };
 
-
 struct md_coredump {
-	struct  trapframe md_tf;
+	struct reg md_reg;
 }; 
 
+
+#endif /* _MACHINE_PCB_H_ */
