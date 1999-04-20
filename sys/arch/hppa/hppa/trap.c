b9b95e0dc5741d332b083de04cd16eeabd8a3727
mickey <mickey@openbsd.org>
1999-04-20 20:58:01 +0000

    syscall(), fix interrupt handling

--- sys/arch/hppa/hppa/trap.c
+++ sys/arch/hppa/hppa/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.2 1999/01/11 05:11:22 millert Exp $	*/
+/*	$OpenBSD: trap.c,v 1.3 1999/04/20 20:58:01 mickey Exp $	*/
 
 /*
  * Copyright (c) 1998 Michael Shalayeff
@@ -34,8 +34,11 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/syscall.h>
+#include <sys/ktrace.h>
 
 #include <vm/vm.h>
+#include <uvm/uvm.h>
 
 #include <machine/iomod.h>
 #include <machine/cpufunc.h>
@@ -81,9 +84,11 @@ const char *trap_type[] = {
 int trap_types = sizeof(trap_type)/sizeof(trap_type[0]);
 
 u_int32_t sir;
+int want_resched;
 
 void pmap_hptdump __P((void));
-void cpu_intr __P((struct trapframe *));
+void cpu_intr __P((u_int32_t t, struct trapframe *frame));
+void syscall __P((struct trapframe *frame, int *args));
 
 void
 trap(type, frame)
@@ -97,10 +102,10 @@ trap(type, frame)
 	u_int opcode, t;
 	int ret;
 
-	va = frame->ior;
-	space = (pa_space_t) frame->isr;
+	va = frame->tf_ior;
+	space = (pa_space_t) frame->tf_isr;
 
-	if (USERMODE(frame->iioq_head)) {
+	if (USERMODE(frame->tf_iioq_head)) {
 		type |= T_USER;
 		p->p_md.md_regs = frame;
 	}
@@ -108,7 +113,7 @@ trap(type, frame)
 	if ((type & ~T_USER) != T_INTERRUPT)
 		printf("trap: %d, %s for %x:%x at %x:%x\n",
 		       type, trap_type[type & ~T_USER], space, va,
-		       frame->iisq_head, frame->iioq_head);
+		       frame->tf_iisq_head, frame->tf_iioq_head);
 
 	switch (type) {
 	case T_NONEXIST:
@@ -125,20 +130,18 @@ trap(type, frame)
 	case T_INTERRUPT:
 	case T_INTERRUPT|T_USER:
 		mfctl(CR_EIRR, t);
-		t &= frame->eiem;
+		t &= frame->tf_eiem;
+		/* ACK it now */
 		/* hardcode intvl timer intr, to save for proc switching */
 		if (t & INT_ITMR) {
-			/* ACK it now */
 			mtctl(INT_ITMR, CR_EIRR);
 			/* we've got an interval timer interrupt */
 			cpu_initclocks();
 			hardclock(frame);
-		} else {
-#ifdef INTRDEBUG
-			printf ("cpu_intr: 0x%08x\n", t);
-#endif
-			cpu_intr(frame);
+			t ^= INT_ITMR;
 		}
+		if (t)
+			cpu_intr(t, frame);
 		return;
 
 	case T_HPMC:
@@ -154,12 +157,12 @@ trap(type, frame)
 
 	case T_DTLBMISS:
 		va = trunc_page(va);
-		opcode = frame->iir;
+		opcode = frame->tf_iir;
 
 #ifdef DDB
 		Debugger();
 #endif
-		ret = vm_fault(map, va, FAULT_TYPE(opcode), FALSE);
+		ret = uvm_fault(map, va, FAULT_TYPE(opcode), FALSE);
 		if (ret == KERN_SUCCESS)
 			break;
 		panic("trap: vm_fault(%p, %x, %d, %d): %d",
@@ -173,6 +176,88 @@ trap(type, frame)
 	}
 }
 
+/*
+ * call actual syscall routine
+ * from the low-level syscall handler:
+ * - all HPPA_FRAME_NARGS syscall's arguments supposed to be copied onto
+ *   our stack, this wins compared to copyin just needed amount anyway
+ * - register args are copied onto stack too
+ */
+void
+syscall(frame, args)
+	struct trapframe *frame;
+	int *args;
+{
+	register struct proc *p;
+	register const struct sysent *callp;
+	int nsys, code, argsize, error;
+	int rval[2];
+
+	uvmexp.syscalls++;
+
+	if (!USERMODE(frame->tf_iioq_head))
+		panic("syscall");
+
+	p = curproc;
+	nsys = p->p_emul->e_nsysent;
+	callp = p->p_emul->e_sysent;
+	code = frame->tf_arg0;
+	switch (code) {
+	case SYS_syscall:
+		code = frame->tf_arg1;
+		args += 1;
+		break;
+	case SYS___syscall:
+		if (callp != sysent)
+			break;
+		code = frame->tf_arg1; /* XXX or arg2? */
+		args += 2;
+	}
+
+	if (code < 0 || code >= nsys)
+		callp += p->p_emul->e_nosys;	/* bad syscall # */
+	else
+		callp += code;
+	argsize = callp->sy_argsize;
+
+#ifdef SYSCALL_DEBUG
+	scdebug_call(p, code, args);
+#endif
+#ifdef KTRACE
+	if (KTRPOINT(p, KTR_SYSCALL))
+		ktrsyscall(p->p_tracep, code, argsize, args);
+#endif
+
+	rval[0] = 0;
+	rval[1] = 0;
+	switch (error = (*callp->sy_call)(p, args, rval)) {
+	case 0:
+		/* curproc may change iside the fork() */
+		p = curproc;
+		frame->tf_ret0 = rval[0];
+		frame->tf_ret1 = rval[1];
+		break;
+	case ERESTART:
+		frame->tf_iioq_head -= 4; /* right? XXX */
+		break;
+	case EJUSTRETURN:
+		break;
+	default:
+		if (p->p_emul->e_errno)
+			error = p->p_emul->e_errno[error];
+		frame->tf_ret0 = error;
+		break;
+	}
+
+#ifdef SYSCALL_DEBUG
+	scdebug_ret(p, code, error, rval);
+#endif
+#ifdef KTRACE
+	if (KTRPOINT(p, KTR_SYSRET))
+		ktrsysret(p->p_tracep, code, error, rval[0]);
+#endif
+}
+
 /* all the interrupts, minus cpu clock, which is the last */
 struct cpu_intr_vector {
 	const char *name;
@@ -180,60 +265,70 @@ struct cpu_intr_vector {
 	int (*handler) __P((void *));
 	void *arg;
 } cpu_intr_vectors[CPU_NINTS - 1];
-#define	ECPU_INTR_VECTORS	&cpu_intr_vectors[CPU_NINTS - 1]
 
-int
-cpu_intr_establish(pri, handler, arg, name)
-	int pri;
+void *
+cpu_intr_establish(pri, irq, handler, arg, name)
+	int pri, irq;
 	int (*handler) __P((void *));
 	void *arg;
 	const char *name;
 {
 	register struct cpu_intr_vector *p;
 
-	for (p = cpu_intr_vectors; p < ECPU_INTR_VECTORS; p++)
-		if (!p->handler)
-			break;
+	/* don't allow to override any established vectors,
+	   AND interval timer hard-bound one */
+	if (irq >= (CPU_NINTS - 1) || cpu_intr_vectors[irq].handler)
+		return NULL;
 
-	/* no more vectors, fail */
-	if (p >= ECPU_INTR_VECTORS)
-		return 0;
-
+	p = &cpu_intr_vectors[irq];
 	p->name = name;
 	p->pri = pri;
 	p->handler = handler;
 	p->arg = arg;
 
-	return p - cpu_intr_vectors;
+	return p;
 }
 
 void
-cpu_intr(frame)
+cpu_intr(t, frame)
+	u_int32_t t;
 	struct trapframe *frame;
 {
-	register u_int32_t t;
+	u_int32_t eirr;
 	register struct cpu_intr_vector *p;
 	register int bit;
 
 	do {
-		mfctl(CR_EIRR, t);
-		t &= frame->eiem;
-		bit = ffs(t) - 1;
+		mfctl(CR_EIRR, eirr);
+		eirr = (t | eirr) & frame->tf_eiem;
+		bit = ffs(eirr) - 1;
 		if (bit >= 0) {
 			mtctl(1 << bit, CR_EIRR);
+			eirr &= ~(1 << bit);
+			/* ((struct iomod *)cpu_gethpa(0))->io_eir = 0; */
 #ifdef INTRDEBUG
-			printf ("cpu_intr: 0x%x\n", (1 << bit));
+			printf ("cpu_intr: 0x%08x\n", (1 << bit));
 #endif
 			p = &cpu_intr_vectors[bit];
 			if (p->handler) {
 				register int s = splx(p->pri);
 				if (!(p->handler)(p->arg))
+#ifdef INTRDEBUG1
+					panic ("%s: can't handle interrupt",
+					       p->name);
+#else
 					printf ("%s: can't handle interrupt\n",
 						p->name);
+#endif
 				splx(s);
-			} else
+			} else {
+#ifdef INTRDEBUG
+				panic  ("cpu_intr: stray interrupt %d", bit);
+#else
 				printf ("cpu_intr: stray interrupt %d\n", bit);
+#endif
+			}
 		}
-	} while (bit >= 0);
+	} while (eirr);
 }
 
