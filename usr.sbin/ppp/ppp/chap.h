aa45260b8ea05e4864864b9585ecf73a9f4cfe26
brian <brian@openbsd.org>
1999-04-21 08:07:24 +0000

    Split the recorded chap challenge into two - one for the receiver and one for the sender.  This allows two simultaneous chap conversations - something that I *thought* I was already doing on a daily basis myself until the existence of the problem was Beaten into me by: sos@FreeBSD.org

--- usr.sbin/ppp/ppp/chap.h
+++ usr.sbin/ppp/ppp/chap.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: chap.h,v 1.5 1999/02/18 19:46:19 brian Exp $
+ * $Id: chap.h,v 1.6 1999/04/21 08:07:24 brian Exp $
  *
  *	TODO:
  */
@@ -39,7 +39,10 @@ struct chap {
     } buf;
   } child;
   struct authinfo auth;
-  u_char challenge[CHAPCHALLENGELEN + AUTHLEN];
+  struct {
+    u_char local[CHAPCHALLENGELEN + AUTHLEN];	/* I invented this one */
+    u_char peer[CHAPCHALLENGELEN + AUTHLEN];	/* Peer gave us this one */
+  } challenge;
 #ifdef HAVE_DES
   unsigned NTRespSent : 1;		/* Our last response */
   int peertries;
