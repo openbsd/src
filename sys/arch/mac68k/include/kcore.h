0a280ab071a428e03ad5deac9f172d9a7abafbaa
downsj <downsj@openbsd.org>
1999-04-23 05:12:35 +0000

    Add kcore.h

---
+++ sys/arch/mac68k/include/kcore.h
@@ -0,0 +1,9 @@
+/*	$OpenBSD: kcore.h,v 1.1 1999/04/23 05:12:35 downsj Exp $	*/
+/*	$NetBSD: kcore.h,v 1.1 1997/04/23 19:15:16 scottr Exp $	*/
+
+#ifndef _MACHINE_KCORE_H_
+#define _MACHINE_KCORE_H_
+
+#include <m68k/kcore.h>
+
+#endif
