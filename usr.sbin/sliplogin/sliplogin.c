6fbe710b6fabb44c83e298ed0245f862fd7ea9df
deraadt <deraadt@openbsd.org>
1999-04-28 17:52:44 +0000

    check correct arg; garath@ntplx.net

--- usr.sbin/sliplogin/sliplogin.c
+++ usr.sbin/sliplogin/sliplogin.c
@@ -39,7 +39,7 @@ char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)sliplogin.c	5.6 (Berkeley) 3/2/91";*/
-static char rcsid[] = "$Id: sliplogin.c,v 1.9 1998/07/10 08:06:55 deraadt Exp $";
+static char rcsid[] = "$Id: sliplogin.c,v 1.10 1999/04/28 17:52:44 deraadt Exp $";
 #endif /* not lint */
 
 /*
@@ -227,8 +227,8 @@ main(argc, argv)
 	s = getdtablesize();
 	for (fd = 3 ; fd < s ; fd++)
 		(void) close(fd);
-	if (strlen(argv[0]) > MAXLOGNAME)
-		errx(1, "login %s too long", argv[0]);
+	if (argc > 1 && strlen(argv[1]) > MAXLOGNAME)
+		errx(1, "login %s too long", argv[1]);
 	openlog(name, LOG_PID, LOG_DAEMON);
 	uid = getuid();
 	if (argc > 1) {
