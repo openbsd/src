e8c9655d47f1f0feaaac1356ec0b1b3430b01d01
d <d@openbsd.org>
1999-05-26 00:08:21 +0000

    bye bye gethostbyname_r and getservbyname_r tests

--- lib/libc_r/TEST/test_netdb.c
+++ lib/libc_r/TEST/test_netdb.c
@@ -20,33 +20,19 @@
 static void test_serv()
 {
 	struct servent *serv;
-	char answer[1024];
 
 	CHECKhn(serv = getservbyname("telnet", "tcp"));
 	printf("getservbyname -> port %d\n", ntohs(serv->s_port));
-
-	CHECKhn(serv = getservbyname_r("telnet", "tcp", serv, answer, 1024));
-	printf("getservbyname_r -> port %d\n", ntohs(serv->s_port));
 }
 
 static void test_host()
 {
 	struct hostent *host;
 	struct in_addr addr;
-	char answer[1024];
-	int error;
 
 	CHECKhn(host = gethostbyname("localhost"));
 	memcpy(&addr, host->h_addr, sizeof(addr));
 	printf("gethostbyname -> %s\n", inet_ntoa(addr));
-
-	if ((host = gethostbyname_r("localhost", host, answer, 1024, &error))
-		!= NULL) {
-		memcpy(&addr, host->h_addr, sizeof(addr));
-		printf("gethostbyname_r -> %s\n", inet_ntoa(addr));
-	} else {
-		DIE(error, "gethostbyname_r");
-	}
 }
 
 static void test_localhost()
