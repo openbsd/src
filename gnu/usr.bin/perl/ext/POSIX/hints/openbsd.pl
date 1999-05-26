71b9bec96562d9f1bf4ba95afcef2f7819ec5cb9
espie <espie@openbsd.org>
1999-05-26 17:11:34 +0000

    egcs Internal compiler errors on m68k.

--- gnu/usr.bin/perl/ext/POSIX/hints/openbsd.pl
+++ gnu/usr.bin/perl/ext/POSIX/hints/openbsd.pl
@@ -1,3 +1,4 @@
 # BSD platforms have extra fields in struct tm that need to be initialized.
 #  XXX A Configure test is needed.
-$self->{CCFLAGS} = $Config{ccflags} . ' -DSTRUCT_TM_HASZONE' ;
+
+$self->{CCFLAGS} = $Config{ccflags} . ' -O1 -DSTRUCT_TM_HASZONE' ;
---
+++ gnu/usr.bin/perl/ext/SDBM_File/hints/openbsd.pl
@@ -0,0 +1,1 @@
+$self->{CCFLAGS} = $Config{ccflags} . ' -O1' ;
