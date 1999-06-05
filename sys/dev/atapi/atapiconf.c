c26b576724ddf376f4054cae8b2a06d5de54aade
deraadt <deraadt@openbsd.org>
1999-06-05 21:58:14 +0000

    say media error instead of medium error

--- sys/dev/atapi/atapiconf.c
+++ sys/dev/atapi/atapiconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atapiconf.c,v 1.22 1998/06/09 13:29:58 provos Exp $	*/
+/*	$OpenBSD: atapiconf.c,v 1.23 1999/06/05 21:58:14 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996 Manuel Bouyer.  All rights reserved.
@@ -686,7 +686,7 @@ atapi_sense(acp, sense_key, flags)
 		break;
 
 	case ATAPI_SK_MEDIUM_ERROR:
-		SILENT_PRINTF(flags,("medium error"));
+		SILENT_PRINTF(flags,("media error"));
 		acp->status = ERROR;
 		break;
 
