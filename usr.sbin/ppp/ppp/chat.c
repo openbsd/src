0ddae45d209a17f57f405fd589f832b2ff497e3d
brian <brian@openbsd.org>
1999-06-26 02:57:01 +0000

    Don't bother read()ing if we ``expect'' nothing in our chat script.

--- usr.sbin/ppp/ppp/chat.c
+++ usr.sbin/ppp/ppp/chat.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: chat.c,v 1.8 1999/06/26 02:56:52 brian Exp $
+ *	$Id: chat.c,v 1.9 1999/06/26 02:57:01 brian Exp $
  */
 
 #include <sys/param.h>
@@ -318,7 +318,7 @@ static int
 chat_IsSet(struct descriptor *d, const fd_set *fdset)
 {
   struct chat *c = descriptor2chat(d);
-  return physical_IsSet(&c->physical->desc, fdset);
+  return c->argptr && physical_IsSet(&c->physical->desc, fdset);
 }
 
 static void
