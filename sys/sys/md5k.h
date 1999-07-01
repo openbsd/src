5b88ee6ef32a4630ef7b64e48c5a2d66cfcf3ce1
deraadt <deraadt@openbsd.org>
1999-07-01 20:21:18 +0000

    indent

--- sys/sys/md5k.h
+++ sys/sys/md5k.h
@@ -1,6 +1,26 @@
-/*	$OpenBSD: md5k.h,v 1.4 1998/11/24 10:19:25 niklas Exp $	*/
+/*	$OpenBSD: md5k.h,v 1.5 1999/07/01 20:21:18 deraadt Exp $	*/
 
-/* GLOBAL.H - RSAREF types and constants
+/*
+ * Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+ * rights reserved.
+ * 
+ * License to copy and use this software is granted provided that it
+ * is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+ * Algorithm" in all material mentioning or referencing this software
+ * or this function.
+ * 
+ * License is also granted to make and use derivative works provided
+ * that such works are identified as "derived from the RSA Data
+ * Security, Inc. MD5 Message-Digest Algorithm" in all material
+ * mentioning or referencing the derived work.
+ * 
+ * RSA Data Security, Inc. makes no representations concerning either
+ * the merchantability of this software or the suitability of this
+ * software for any particular purpose. It is provided "as is"
+ * without express or implied warranty of any kind.
+ * 
+ * These notices must be retained in any copies of any part of this
+ * documentation and/or software.
  */
 
 #ifndef _MD5K_H_
@@ -15,44 +35,18 @@ typedef u_int16_t UINT2;
 /* UINT4 defines a four byte word */
 typedef u_int32_t UINT4;
 
-/* MD5.H - header file for MD5C.C
- */
-
-/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
-
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
-
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
-
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
-
-These notices must be retained in any copies of any part of this
-documentation and/or software.
- */
-
 /* MD5 context. */
 typedef struct {
-  UINT4 state[4];                                   /* state (ABCD) */
-  UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-  u_int8_t buffer[64];                              /* input buffer */
+	UINT4 state[4];		/* state (ABCD) */
+	UINT4 count[2];		/* number of bits, modulo 2^64 (lsb first) */
+	u_int8_t buffer[64];	/* input buffer */
 } MD5_CTX;
 
 #include <sys/cdefs.h>
 
-void MD5Init __P ((MD5_CTX *));
-void MD5Update __P
-  ((MD5_CTX *, unsigned char *, unsigned int));
-void MD5Final __P ((unsigned char [16], MD5_CTX *));
+void MD5Init __P((MD5_CTX *));
+void MD5Update __P((MD5_CTX *, unsigned char *, unsigned int));
+void MD5Final __P((unsigned char [16], MD5_CTX *));
 
 #define _MD5_H_
 
