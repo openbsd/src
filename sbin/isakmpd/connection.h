500838637cef6a8dff815b3f0b6796af5f6a2ce2
niklas <niklas@openbsd.org>
1999-07-07 22:12:20 +0000

    Merge with EOM 1.6
    author: ho
    Add copyright notice.
    
    author: ho
    Add connection_exist

--- sbin/isakmpd/connection.h
+++ sbin/isakmpd/connection.h
@@ -1,8 +1,9 @@
-/*	$OpenBSD: connection.h,v 1.2 1999/06/02 06:32:23 niklas Exp $	*/
-/*	$EOM: connection.h,v 1.4 1999/05/28 13:53:22 ho Exp $	*/
+/*	$OpenBSD: connection.h,v 1.3 1999/07/07 22:12:20 niklas Exp $	*/
+/*	$EOM: connection.h,v 1.6 1999/06/07 00:10:48 ho Exp $	*/
 
 /*
  * Copyright (c) 1999 Niklas Hallqvist.  All rights reserved.
+ * Copyright (c) 1999 Hakan Olsson.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -43,6 +44,7 @@
 
 #include <sys/types.h>
 
+extern int connection_exist (char *);
 extern void connection_init (void);
 extern char *connection_passive_lookup_by_ids (u_int8_t *, u_int8_t *);
 extern void connection_reinit (void);
