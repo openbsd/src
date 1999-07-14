a354b5cf97d72f1b2f51bce8022e6fd188a9bc7c
d <d@openbsd.org>
1999-07-14 03:51:50 +0000

    KNF

--- sys/dev/ic/rl2.h
+++ sys/dev/ic/rl2.h
@@ -1,31 +1,39 @@
-/*	$OpenBSD: rl2.h,v 1.1 1999/06/21 23:21:46 d Exp $	*/
+/*	$OpenBSD: rl2.h,v 1.2 1999/07/14 03:51:50 d Exp $	*/
 /*
  * David Leonard <d@openbsd.org>, 1999. Public domain.
  *
  * Proxim RangeLAN2 parameters.
- *
- * Eventually, there should be a way of getting and setting these
- * from user space. Perhaps through ioctl().
  */
 
+/*
+ * Eventually, there should be a way of getting and setting these
+ * from user space. Ideally, via ioctl().
+ */
+
+/* User-configurable station parameters. */
 struct rl2_param {
-	u_int8_t  rp_roamconfig;	/* roam speed */
-#define RL2_ROAM_SLOW		0
-#define RL2_ROAM_NORMAL		1
-#define RL2_ROAM_FAST		2
-	u_int32_t rp_security;		/* security id */
+	u_int32_t	rp_security;		/* Security ID */
 #define RL2_SECURITY_DEFAULT	0x0010203
-	u_int8_t  rp_stationtype;
+	u_int8_t	rp_station_type;	/* Station type */
 #define RL2_STATIONTYPE_SLAVE		0
 #define RL2_STATIONTYPE_ALTMASTER	1
 #define RL2_STATIONTYPE_MASTER		2
-	u_int8_t  rp_domain;
-	u_int8_t  rp_channel;
-	u_int8_t  rp_subchannel;
-	char	  rp_master[11];	/* valid only when st.type is master */
+	u_int8_t	rp_domain;		/* Network domain */
+	u_int8_t	rp_channel;		/* Phys channel when master */
+	u_int8_t	rp_subchannel;		/* Logical master subchannel */
+	char		rp_master[11];		/* Name when master */
+	u_int8_t	rp_mac_optimize;
+#define RL2_MAC_OPTIM_LIGHT	0
+#define RL2_MAC_OPTIM_NORMAL	1
+	u_int8_t	rp_roam_config;		/* Roaming speed */
+#define RL2_ROAM_SLOW		0
+#define RL2_ROAM_NORMAL		1
+#define RL2_ROAM_FAST		2
+	u_int8_t	rp_peer_to_peer;	/* Ability to talk to peers */
 };
 
-/* XXX possible ioctls to use */
-#define RL2IOSPARAM	_IOW('2', 1, struct rl2_param)	/* set params */
-#define RL2IOGPARAM	_IOR('2', 2, struct rl2_param)	/* get params */
+#ifdef notyet
+#define RL2IOSPARAM    _IOW('2', 1, struct rl2_param)  /* set params */
+#define RL2IOGPARAM    _IOR('2', 2, struct rl2_param)  /* get params */
+#endif
 
