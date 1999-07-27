24193933af153069e0457eaeb923121a3b4c0e2b
brian <brian@openbsd.org>
1999-07-27 23:47:21 +0000

    o Overhaul filtering, adding facilities to jump over rules and to   negate the sense of rules. o Remove the redundant (and undocumented) ``host'' and ``port''   words (README.changes updated). o Don't permit (and ignore) garbage instead of the protocol.
    Mostly submitted by:  Peter Jeremy <jeremyp@gsmx07.alcatel.com.au>

--- usr.sbin/ppp/ppp/bundle.c
+++ usr.sbin/ppp/ppp/bundle.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: bundle.c,v 1.22 1999/07/15 02:10:31 brian Exp $
+ *	$Id: bundle.c,v 1.23 1999/07/27 23:47:21 brian Exp $
  */
 
 #include <sys/param.h>
@@ -854,6 +854,15 @@ bundle_Create(const char *prefix, int type, const char **argv)
   bundle.filter.dial.logok = 1;
   bundle.filter.alive.name = "ALIVE";
   bundle.filter.alive.logok = 1;
+  {
+    int	i;
+    for (i = 0; i < MAXFILTERS; i++) {
+        bundle.filter.in.rule[i].f_action = A_NONE;
+        bundle.filter.out.rule[i].f_action = A_NONE;
+        bundle.filter.dial.rule[i].f_action = A_NONE;
+        bundle.filter.alive.rule[i].f_action = A_NONE;
+    }
+  }
   memset(&bundle.idle.timer, '\0', sizeof bundle.idle.timer);
   bundle.idle.done = 0;
   bundle.notify.fd = -1;
