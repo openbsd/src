63a5c0ac0b8a1f5ae7282ec3525e0b5eaede747b
deraadt <deraadt@openbsd.org>
1999-07-28 01:17:56 +0000

    clarify

--- usr.bin/man/man.c
+++ usr.bin/man/man.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: man.c,v 1.13 1999/06/10 22:38:02 pjanzen Exp $	*/
+/*	$OpenBSD: man.c,v 1.14 1999/07/28 01:17:56 deraadt Exp $	*/
 /*	$NetBSD: man.c,v 1.7 1995/09/28 06:05:34 tls Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)man.c	8.17 (Berkeley) 1/31/95";
 #else
-static char rcsid[] = "$OpenBSD: man.c,v 1.13 1999/06/10 22:38:02 pjanzen Exp $";
+static char rcsid[] = "$OpenBSD: man.c,v 1.14 1999/07/28 01:17:56 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -691,7 +691,7 @@ check_pager(name)
 		save = name;
 		/* allocate space to add the "-s" */
 		if (!(name =
-		    malloc((u_int)(strlen(save) + sizeof("-s") + 1))))
+		    malloc((u_int)(strlen(save) + 1 + sizeof("-s")))))
 			err(1, NULL);
 		(void)sprintf(name, "%s %s", save, "-s");
 	}
