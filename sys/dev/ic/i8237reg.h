3e4a9499dfd10fb964f87f181c7484534933dc36
niklas <niklas@openbsd.org>
1999-08-04 23:07:49 +0000

    Add new manifest constant for demand mode DMA; form NetBSD

--- sys/dev/ic/i8237reg.h
+++ sys/dev/ic/i8237reg.h
@@ -1,10 +1,11 @@
-/*	$OpenBSD: i8237reg.h,v 1.2 1996/04/18 23:47:19 niklas Exp $	*/
+/*	$OpenBSD: i8237reg.h,v 1.3 1999/08/04 23:07:49 niklas Exp $	*/
 /*	$NetBSD: i8237reg.h,v 1.5 1996/03/01 22:27:09 mycroft Exp $	*/
 
 /*
  * Intel 8237 DMA Controller
  */
 
+#define	DMA37MD_DEMAND	0x00	/* demand mode */
 #define	DMA37MD_WRITE	0x04	/* read the device, write memory operation */
 #define	DMA37MD_READ	0x08	/* write the device, read memory operation */
 #define	DMA37MD_LOOP	0x10	/* auto-initialize mode */
