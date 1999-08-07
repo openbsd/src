43ab664ffb6ad72a438a6cc04f8836c2b7e38c72
millert <millert@openbsd.org>
1999-08-07 17:42:48 +0000

    Local changes that should not have gone in

--- lib/libc/stdio/fvwrite.c
+++ lib/libc/stdio/fvwrite.c
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: fvwrite.c,v 1.9 1999/08/07 17:35:58 millert Exp $";
+static char rcsid[] = "$OpenBSD: fvwrite.c,v 1.10 1999/08/07 17:42:48 millert Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
@@ -111,9 +111,7 @@ __sfvwrite(fp, uio)
 		do {
 			GETIOV(;);
 			if ((fp->_flags & (__SALC | __SSTR)) ==
-			    (__SALC | __SSTR) && fp->_w < len &&
-			    ((fp->_flags & __SAMX) == 0 || fp->_bf._size
-			    <= fp->_blksize)) {
+			    (__SALC | __SSTR) && fp->_w < len) {
 				size_t blen = fp->_p - fp->_bf._base;
 				unsigned char *_base;
 				int _size;
@@ -123,9 +121,6 @@ __sfvwrite(fp, uio)
 				do {
 					_size = (_size << 1) + 1;
 				} while (_size < blen + len);
-				/* Apply maximum if v?asnprintf */
-				if ((fp->_flags & __SAMX))
-					_size = MIN(_size, fp->_blksize);
 				_base = realloc(fp->_bf._base, _size + 1);
 				if (_base == NULL)
 					goto err;
