dad2ab0e38b3a40583ef09439ca2d529ddfa4b7e
niklas <niklas@openbsd.org>
1999-08-16 00:17:19 +0000

    We must tell we are little endian by default

--- gnu/egcs/gcc/config/mips/openbsd.h
+++ gnu/egcs/gcc/config/mips/openbsd.h
@@ -36,6 +36,10 @@ Boston, MA 02111-1307, USA.  */
    it as it is until we have some kind of profiling working.  */
 #define LIB_SPEC OBSD_LIB_SPEC
 
+/* We are little endian by default, unless an includer thinks otherwise.  */
+#ifndef TARGET_ENDIAN_DEFAULT
+#define TARGET_ENDIAN_DEFAULT 0
+#endif
 
 #include <mips/mips.h>
 
