4f85a33eacb1e50f9bb190488771e88c33b0fd24
niklas <niklas@openbsd.org>
1999-08-26 22:28:33 +0000

    Merge with EOM 1.4
    author: niklas
    typo
    
    author: niklas
    Support dynamic loading of libkeynote too.  Build isakmpd static by default.
    Stylistic cleanup of keynote policy code.  Correct some libcrypto calls.

--- sbin/isakmpd/policy.h
+++ sbin/isakmpd/policy.h
@@ -1,5 +1,5 @@
-/*	$OpenBSD: policy.h,v 1.1 1999/07/07 22:10:28 niklas Exp $	*/
-/*	$EOM: policy.h,v 1.2 1999/06/07 08:46:35 niklas Exp $ */
+/*	$OpenBSD: policy.h,v 1.2 1999/08/26 22:28:33 niklas Exp $	*/
+/*	$EOM: policy.h,v 1.4 1999/08/26 11:36:47 niklas Exp $ */
 
 /*
  * Copyright (c) 1999 Angelos D. Keromytis.  All rights reserved.
@@ -36,6 +36,38 @@
 
 #ifndef _POLICY_H_
 #define _POLICY_H_
+
+#if defined (USE_KEYNOTE)
+#define LK(sym, args) sym args
+#define LKV(sym) sym
+#elif defined (HAVE_DLOPEN)
+#define LK(sym, args) lk_ ## sym args
+#define LKV(sym) *lk_ ## sym
+#else
+#define LK(sym, args) !!libkeynote called but no USE_KEYNOTE nor HAVE_DLOPEN!!
+#define LKV(sym) !!libkeynote called but no USE_KEYNOTE nor HAVE_DLOPEN!!
+#endif
+
+#if defined(HAVE_DLOPEN) && !defined(USE_KEYNOTE)
+struct keynote_deckey;
+
+extern void *libkeynote;
+
+/*
+ * These prototypes matches OpenBSD keynote.h 1.6.  If you use
+ * a different version than that, you are on your own.
+ */
+extern int *lk_keynote_errno;
+extern int (*lk_kn_add_action) (int, char *, char *, int);
+extern int (*lk_kn_add_assertion) (int, char *, int, int);
+extern int (*lk_kn_add_authorizer) (int, char *);
+extern int (*lk_kn_close) (int);
+extern int (*lk_kn_do_query) (int, char **, int);
+extern char *(*lk_kn_encode_key) (struct keynote_deckey *, int, int, int);
+extern int (*lk_kn_init) (void);
+extern char **(*lk_kn_read_asserts) (char *, int, int *);
+extern int (*lk_kn_remove_authorizer) (int, char *);
+#endif /* HAVE_DLOPEN && !USE_KEYNOTE */
 
 extern void policy_init (void);
 
