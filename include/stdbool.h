9e5915d45653cc60150034d410b39048b5605c47
espie <espie@openbsd.org>
1999-09-24 23:09:09 +0000

    Copyright...

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,4 +1,10 @@
-/* $OpenBSD: stdbool.h,v 1.1 1999/09/24 22:33:10 espie Exp $ */
+/* $OpenBSD: stdbool.h,v 1.2 1999/09/24 23:09:09 espie Exp $ */
+
+/*
+ * Written by Marc Espie, September 25, 1999
+ * Public domain.
+ */
+
 #ifndef	_STDBOOL_H_
 #define	_STDBOOL_H_	
 
