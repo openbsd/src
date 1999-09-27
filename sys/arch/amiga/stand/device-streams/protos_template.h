eda7b12c542e7036acadb7140ff257f9bffcb1af
espie <espie@openbsd.org>
1999-09-27 08:33:37 +0000

    The device-streams tools, including binaries in uu.gz format.

---
+++ sys/arch/amiga/stand/device-streams/protos_template.h
@@ -0,0 +1,8 @@
+/* START: "system" */
+#include <clib/alib_protos.h>
+#include <clib/exec_protos.h>
+#include <clib/dos_protos.h>
+#include <pragmas/exec_pragmas.h>
+#include <pragmas/dos_pragmas.h>
+extern struct Library *DOSBase;
+/* END: "system" */
