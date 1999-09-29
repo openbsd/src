ce5e228925240c86e42f5cd48c96972ed2ac4ec2
niklas <niklas@openbsd.org>
1999-09-29 09:11:21 +0000

    Critical reliability fix for IPsec.  On i386 splsoftclock is not a perfect emulation of a "real" architecture's splsoftclock, as it assumes it is only invoked from higher spl levels.  Use splsoftnet instead.

--- sys/netinet/ip_ipsp.h
+++ sys/netinet/ip_ipsp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ipsp.h,v 1.41 1999/08/10 11:35:26 ho Exp $	*/
+/*	$OpenBSD: ip_ipsp.h,v 1.42 1999/09/29 09:11:21 niklas Exp $	*/
 
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
@@ -397,9 +397,11 @@ htonq(u_int64_t q)
 
 /*
  * Protects all tdb lists.
- * Must at least be splsoftclock.
+ * Must at least be splsoftnet (note: do not use splsoftclock as it is
+ * special on some architectures, assuming it is always an spl lowering
+ * operation).
  */
-#define spltdb	splsoftclock
+#define spltdb	splsoftnet
 
 extern int encdebug;
 extern int ipsec_in_use;
