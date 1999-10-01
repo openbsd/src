719ebef83b2e97fc6b6815f3ff37bf8879332ef2
angelos <angelos@openbsd.org>
1999-10-01 01:41:32 +0000

    Remove the totally broken Makefile.bsd-wrapper, go back to old scheme

---
+++ lib/libkeynote/config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.hin.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if you have the _close function.  */
+/* #undef HAVE__CLOSE */
+
+/* Define if you have the _open function.  */
+/* #undef HAVE__OPEN */
+
+/* Define if you have the _read function.  */
+/* #undef HAVE__READ */
+
+/* Define if you have the b64_ntop function.  */
+#define HAVE___B64_NTOP 1
+
+/* Define if you have the close function.  */
+#define HAVE_CLOSE 1
+
+/* Define if you have the getopt function.  */
+#define HAVE_GETOPT 1
+
+/* Define if you have the memcpy function.  */
+#define HAVE_MEMCPY 1
+
+/* Define if you have the open function.  */
+#define HAVE_OPEN 1
+
+/* Define if you have the read function.  */
+#define HAVE_READ 1
+
+/* Define if you have the regcomp function.  */
+#define HAVE_REGCOMP 1
+
+/* Define if you have the snprintf function.  */
+#define HAVE_SNPRINTF 1
+
+/* Define if you have the strcasecmp function.  */
+#define HAVE_STRCASECMP 1
+
+/* Define if you have the strchr function.  */
+#define HAVE_STRCHR 1
+
+/* Define if you have the stricmp function.  */
+/* #undef HAVE_STRICMP */
+
+/* Define if you have the strncasecmp function.  */
+#define HAVE_STRNCASECMP 1
+
+/* Define if you have the strnicmp function.  */
+/* #undef HAVE_STRNICMP */
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <io.h> header file.  */
+/* #undef HAVE_IO_H */
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <memory.h> header file.  */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the <openssl/crypto.h> header file.  */
+#define HAVE_OPENSSL_CRYPTO_H 1
+
+/* Define if you have the <regex.h> header file.  */
+#define HAVE_REGEX_H 1
+
+/* Define if you have the <ssl/crypto.h> header file.  */
+#define HAVE_SSL_CRYPTO_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the crypto library (-lcrypto).  */
+#define HAVE_LIBCRYPTO 1
+
+/* Define if you have /dev/urandom file.  */
+#define HAVE__DEV_URANDOM 1
