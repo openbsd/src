42c632a7ddc420f5adf2e16b2923682d7ab09342
downsj <downsj@openbsd.org>
1999-10-26 12:29:00 +0000

    Update my copyright.

--- sys/arch/i386/isa/pccom.c
+++ sys/arch/i386/isa/pccom.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: pccom.c,v 1.32 1999/08/08 01:34:15 niklas Exp $	*/
+/*	$OpenBSD: pccom.c,v 1.33 1999/10/26 12:29:00 downsj Exp $	*/
 /*	$NetBSD: com.c,v 1.82.4.1 1996/06/02 09:08:00 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997 - 1998, Jason Downs.  All rights reserved.
+ * Copyright (c) 1997 - 1999, Jason Downs.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Jason Downs for the
- *      OpenBSD system.
- * 4. Neither the name(s) of the author(s) nor the name OpenBSD
+ * 3. Neither the name(s) of the author(s) nor the name OpenBSD
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
