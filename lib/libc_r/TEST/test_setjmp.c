2cc170a2b53f31732bc793374583ab249a0bb381
d <d@openbsd.org>
1999-12-02 02:43:36 +0000

    add _setjmp/_longjmp test

--- lib/libc_r/TEST/test_setjmp.c
+++ lib/libc_r/TEST/test_setjmp.c
@@ -4,6 +4,22 @@
 int reached;
 
 void *
+_jump(arg)
+	void *arg;
+{
+	jmp_buf foo;
+
+	reached = 0;
+	if (_setjmp(foo)) {
+		ASSERT(reached);
+		return NULL;
+	}
+	reached = 1;
+	_longjmp(foo, 1);
+	PANIC("_longjmp");
+}
+
+void *
 jump(arg)
 	void *arg;
 {
@@ -27,9 +43,15 @@ main()
 
 	printf("jumping in main thread\n");
 	(void)jump(NULL);
+	printf("_jumping in main thread\n");
+	(void)_jump(NULL);
 
 	printf("jumping in child thread\n");
 	CHECKr(pthread_create(&child, NULL, jump, NULL));
+	CHECKr(pthread_join(child, &res));
+
+	printf("_jumping in child thread\n");
+	CHECKr(pthread_create(&child, NULL, _jump, NULL));
 	CHECKr(pthread_join(child, &res));
 
 	SUCCEED;
