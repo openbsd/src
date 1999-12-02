150e905746f437c0e741c624f473508525a4df47
d <d@openbsd.org>
1999-12-02 02:42:17 +0000

    better tests

--- lib/libc_r/TEST/test_setjmp.c
+++ lib/libc_r/TEST/test_setjmp.c
@@ -3,16 +3,34 @@
 
 int reached;
 
-main()
+void *
+jump(arg)
+	void *arg;
 {
 	jmp_buf foo;
 
 	reached = 0;
 	if (setjmp(foo)) {
 		ASSERT(reached);
-		SUCCEED;
+		return NULL;
 	}
 	reached = 1;
 	longjmp(foo, 1);
 	PANIC("longjmp");
+}
+
+int
+main()
+{
+	pthread_t child;
+	void *res;
+
+	printf("jumping in main thread\n");
+	(void)jump(NULL);
+
+	printf("jumping in child thread\n");
+	CHECKr(pthread_create(&child, NULL, jump, NULL));
+	CHECKr(pthread_join(child, &res));
+
+	SUCCEED;
 }
