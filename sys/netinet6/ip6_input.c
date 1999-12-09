7a4bdae10da9a86a6d9a06e96717dc7eca236506
angelos <angelos@openbsd.org>
1999-12-09 00:40:06 +0000

    Bogus include file crept in.

--- sys/netinet6/ip6_input.c
+++ sys/netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_input.c,v 1.2 1999/12/09 00:34:13 angelos Exp $	*/
+/*	$OpenBSD: ip6_input.c,v 1.3 1999/12/09 00:40:06 angelos Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -95,7 +95,6 @@
 #include <netinet/in_pcb.h>
 #include <netinet6/in6_var.h>
 #include <netinet6/ip6.h>
-#include <netinet6/in6_pcb.h>
 #include <netinet6/ip6_var.h>
 #include <netinet6/icmp6.h>
 #include <netinet6/in6_ifattach.h>
