2ded093fefdfcab135e59adb82f7a2cb807fc884
itojun <itojun@openbsd.org>
1999-12-10 07:26:55 +0000

    add ndp(8), arp(8) lookalike for IPv6.

--- usr.sbin/ndp/gnuc.h
+++ usr.sbin/ndp/gnuc.h
@@ -1,1 +1,3 @@
+/*	$OpenBSD: gnuc.h,v 1.2 1999/12/10 07:26:56 itojun Exp $	*/
+
 /* this is dummy to pacify gmt2local.c. */
--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -1,3 +1,5 @@
+/*	$OpenBSD: ndp.c,v 1.2 1999/12/10 07:26:56 itojun Exp $	*/
+
 /*
  * Copyright (C) 1995, 1996, 1997, 1998, and 1999 WIDE Project.
  * All rights reserved.
