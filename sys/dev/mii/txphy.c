5f832cef44d019fd9b6032806ac24651beededf6
jason <jason@openbsd.org>
1999-12-10 21:48:20 +0000

    simple driver for Texas Instruments TNETE2101 phy

---
+++ sys/dev/mii/txphy.c
@@ -0,0 +1,177 @@
+/*	$OpenBSD: txphy.c,v 1.1 1999/12/10 21:48:20 jason Exp $	*/
+
+/*
+ * Copyright (c) 1999 Jason L. Wright (jason@thought.net)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Jason L. Wright
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Driver for the Texas Instruments TNETE2101 phy.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/device.h>
+#include <sys/malloc.h>
+#include <sys/socket.h>
+#include <sys/errno.h>
+
+#include <net/if.h>
+#include <net/if_media.h>
+
+#include <dev/mii/mii.h>
+#include <dev/mii/miivar.h>
+#include <dev/mii/miidevs.h>
+
+int	txphymatch __P((struct device *, void *, void *));
+void	txphyattach __P((struct device *, struct device *, void *));
+
+struct cfattach txphy_ca = {
+	sizeof(struct mii_softc), txphymatch, txphyattach
+};
+
+struct cfdriver txphy_cd = {
+	NULL, "txphy", DV_DULL
+};
+
+int	txphy_service __P((struct mii_softc *, struct mii_data *, int));
+
+int
+txphymatch(parent, match, aux)
+	struct device *parent;
+	void *match;
+	void *aux;
+{
+	struct mii_attach_args *ma = aux;
+
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_xxTI &&
+	    MII_MODEL(ma->mii_id2) == MII_MODEL_xxTI_TNETE2101)
+		return (10);
+	return (0);
+}
+
+void
+txphyattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct mii_softc *sc = (struct mii_softc *)self;
+	struct mii_attach_args *ma = aux;
+	struct mii_data *mii = ma->mii_data;
+
+	printf(": %s, rev. %d\n", MII_STR_xxTI_TNETE2101,
+	    MII_REV(ma->mii_id2));
+
+	sc->mii_inst = mii->mii_instance;
+	sc->mii_phy = ma->mii_phyno;
+	sc->mii_service = txphy_service;
+	sc->mii_pdata = mii;
+
+	mii_phy_reset(sc);
+
+	sc->mii_capabilities =
+	    PHY_READ(sc, MII_BMSR) & ma->mii_capmask;
+	if (sc->mii_capabilities & BMSR_MEDIAMASK)
+		mii_add_media(sc);
+}
+
+int
+txphy_service(sc, mii, cmd)
+	struct mii_softc *sc;
+	struct mii_data *mii;
+	int cmd;
+{
+	struct ifmedia_entry *ife = mii->mii_media.ifm_cur;
+
+	/*
+	 * Can't isolate the RTL8139 phy, so it has to be the only one.
+	 */
+	if (IFM_INST(ife->ifm_media) != sc->mii_inst)
+		panic("txphy_service: attempt to isolate phy");
+
+	switch (cmd) {
+	case MII_POLLSTAT:
+		break;
+
+	case MII_MEDIACHG:
+		/*
+		 * If the interface is not up, don't do anything.
+		 */
+		if ((mii->mii_ifp->if_flags & IFF_UP) == 0)
+			break;
+
+		switch (IFM_SUBTYPE(ife->ifm_media)) {
+		case IFM_AUTO:
+			/*
+			 * If we're already in auto mode, just return.
+			 */
+			if (PHY_READ(sc, MII_BMCR) & BMCR_AUTOEN)
+				return (0);
+			(void) mii_phy_auto(sc, 1);
+			break;
+		default:
+			mii_phy_setmedia(sc);
+		}
+		break;
+
+	case MII_TICK:
+		/*
+		 * Only used for autonegotiation.
+		 */
+		if (IFM_SUBTYPE(ife->ifm_media) != IFM_AUTO)
+			return (0);
+
+		/*
+		 * Is the interface even up?
+		 */
+		if ((mii->mii_ifp->if_flags & IFF_UP) == 0)
+			return (0);
+
+		/*
+		 * The TNETE2101 autonegotiation doesn't need to be
+		 * kicked; it continues in the background.
+		 */
+		break;
+
+	case MII_DOWN:
+		mii_phy_down(sc);
+		return (0);
+	}
+
+	/* Update the media status. */
+	ukphy_status(sc);
+
+	/* Callback if something changed. */
+	if (sc->mii_active != mii->mii_media_active || cmd == MII_MEDIACHG) {
+		(*mii->mii_statchg)(sc->mii_dev.dv_parent);
+		sc->mii_active = mii->mii_media_active;
+	}
+	return (0);
+}
---
+++ sys/dev/mii/txphyreg.h
@@ -0,0 +1,68 @@
+/*	$OpenBSD: txphyreg.h,v 1.1 1999/12/10 21:48:20 jason Exp $	*/
+
+/*
+ * Copyright (c) 1999 Jason L. Wright (jason@thought.net)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Jason L. Wright
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _DEV_MII_TXPHYREG_H_
+#define	_DEV_MII_TXPHYREG_H_
+
+/*
+ * Texas Instruments TNETE2101 registers.
+ */
+#define	MII_TXPHY_ID		0x10	/* PHY identifier */
+
+#define	MII_TXPHY_CTL		0x11	/* PHY control */
+#define	TXCTL_IGLINK		0x8000	/* ignore link */
+#define	TXCTL_SWAPOL		0x4000	/* swap polarity */
+#define	TXCTL_MANCONF		0x2000	/* manual configuration */
+#define	TXCTL_SQEEN		0x1000	/* SQE enable */
+#define	TXCTL_MTEST		0x0800	/* manufacturing test */
+#define	TXCTL_FIBER		0x0400	/* 100BaseFX mode */
+#define	TXCTL_FEFEN		0x0200	/* far-end fault indication enable */
+#define	TXCTL_NOENDEC		0x0100	/* no encode/decode */
+#define	TXCTL_NOALIGN		0x0080	/* no symbol alignment */
+#define	TXCTL_DUPONLY		0x0040	/* duplex LED only */
+#define	TXCTL_REPEATER		0x0020	/* repeater mode enable */
+#define	TXCTL_RXRESET		0x0010	/* 100baseTX rx reset */
+#define	TXCTL_NOLINKP		0x0008	/* disable link pulse tx */
+#define	TXCTL_NFEW		0x0004	/* no far end wrap */
+#define	TXCTL_INTEN		0x0002	/* interrupt enable */
+#define	TXCTL_TINT		0x0001	/* test interrupt */
+
+#define	MII_TXPHY_STS		0x12	/* PHY status */
+#define	TXSTS_MINT		0x8000	/* mii interrupt */
+#define	TXSTS_PHOK		0x4000	/* power high ok */
+#define	TXSTS_PLOK		0x2000	/* polarity ok */
+#define	TXSTS_TPENERGY		0x1000	/* TP energy detect */
+#define	TXSTS_SYNCLOSS		0x0800	/* 100btx rx descrambler sync loss */
+#define	TXSTS_FEFI		0x0400	/* Far-end fault indictation */
+
+#endif /* _DEV_MII_MTDPHYREG_H_ */
