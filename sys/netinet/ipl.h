e4490cd843e89fcba792ee9b24341d6b381ee4c3
kjell <kjell@openbsd.org>
1999-12-14 04:17:17 +0000

    New ipfilter files. Preparing for merge of ipfilter 3.3.4.

---
+++ sys/netinet/ipl.h
@@ -0,0 +1,17 @@
+/* $OpenBSD: ipl.h,v 1.1 1999/12/14 04:17:17 kjell Exp $ */
+/*
+ * Copyright (C) 1993-1999 by Darren Reed.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that this notice is preserved and due credit is given
+ * to the original author and the contributors.
+ *
+ * @(#)ipl.h	1.21 6/5/96
+ */
+
+#ifndef	__IPL_H__
+#define	__IPL_H__
+
+#define	IPL_VERSION	"IP Filter: v3.3.4"
+
+#endif
