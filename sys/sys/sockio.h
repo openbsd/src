8e04b7fe842f63df548dfb86240a79388954bf27
angelos <angelos@openbsd.org>
1999-12-27 04:15:34 +0000

    Some more enc ioctls.

--- sys/sys/sockio.h
+++ sys/sys/sockio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sockio.h,v 1.12 1999/12/26 09:18:21 angelos Exp $	*/
+/*	$OpenBSD: sockio.h,v 1.13 1999/12/27 04:15:34 angelos Exp $	*/
 /*	$NetBSD: sockio.h,v 1.5 1995/08/23 00:40:47 thorpej Exp $	*/
 
 /*-
@@ -108,7 +108,9 @@
 #define	SIOCBRDGFLUSH	_IOWR('i', 72, struct ifbreq)	/* flush addr cache */
 
 #define SIOCGENCSA	_IOWR('i', 73, struct ifsa)	/* get enc sa */
-#define SIOCSENCSA	_IOW('i', 74, struct ifsa)	/* set enc sa */
+#define SIOCSENCDSTSA	_IOW('i', 74, struct ifsa)	/* set enc sa */
+#define SIOCSENCSRCSA	_IOW('i', 75, struct ifsa)	/* set enc sa */
+#define SIOCSENCCLEARSA	_IOW('i', 76, struct ifsa)	/* set enc sa */
 
 #define	SIOCSIFMTU	 _IOW('i', 127, struct ifreq)	/* set ifnet mtu */
 #define	SIOCGIFMTU	_IOWR('i', 126, struct ifreq)	/* get ifnet mtu */
