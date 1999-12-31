da5ebc5d06f7f8b94b4e38be3f669f8637a7ca29
deraadt <deraadt@openbsd.org>
1999-12-31 19:47:45 +0000

    indent

--- sys/net/if_enc.h
+++ sys/net/if_enc.h
@@ -38,14 +38,14 @@
 #define ENC_HDRLEN	12
 
 struct enc_softc {
-        struct ifnet		sc_if;  /* the interface */
-        union sockaddr_union	sc_dst;
-        u_int32_t		sc_spi;
-        u_int32_t		sc_sproto;
+	struct ifnet		sc_if;  /* the interface */
+	union sockaddr_union	sc_dst;
+	u_int32_t		sc_spi;
+	u_int32_t		sc_sproto;
 };
 
 struct ifsa {
-        char                    sa_ifname[IFNAMSIZ];    /* bridge ifs name */
+	char			sa_ifname[IFNAMSIZ];	/* bridge ifs name */
 	u_int32_t		sa_spi;
 	u_int8_t		sa_proto;
 	union sockaddr_union	sa_dst;
