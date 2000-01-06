9cea0838d7b5409413c2b78cb1eec189c7fd7329
d <d@openbsd.org>
2000-01-06 16:40:50 +0000

    don't use _C_LABEL. but do use __STRING

--- sys/arch/i386/include/cdefs.h
+++ sys/arch/i386/include/cdefs.h
@@ -10,9 +10,9 @@
 
 #ifndef	_C_LABEL
 #ifdef __STDC__
-#define _C_LABEL(x)	_STRING(_ ## x)
+#define _C_LABEL(x)	__STRING(_ ## x)
 #else
-#define _C_LABEL(x)	_STRING(_/**/x)
+#define _C_LABEL(x)	__STRING(_/**/x)
 #endif
 #endif /* _C_LABEL */
 
@@ -24,6 +24,8 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs \"" msg "\",30,0,0,0");		\
 	__asm__(".stabs \"_" #sym "\",1,0,0,0")
+#define __weak_alias(alias,sym)				\
+	__asm__(".weak _" #alias "; _" #alias "= _" __STRING(sym))
 #else
 #define __indr_reference(sym,alias)	\
 	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
@@ -31,13 +33,11 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs msg,30,0,0,0");			\
 	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+#define __weak_alias(alias,sym)				\
+	__asm__(".weak _/**/alias; _/**/alias = _/**/sym")
 #endif
 #else
 #define	__warn_references(sym,msg)	/* nothing */
 #endif
-
-#define __weak_alias(alias,sym)				\
-	__asm__(".weak " _C_LABEL(alias) ";"		\
-	_C_LABEL(alias) "=" _C_LABEL(sym))
 
 #endif /* !_MACHINE_CDEFS_H_ */
