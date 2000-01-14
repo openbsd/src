717aa507136f6e14211b4ba0ef2670efd8ae7eca
downsj <downsj@openbsd.org>
2000-01-14 09:11:01 +0000

    Might as well ding the version number.

--- sys/arch/hp300/stand/common/version.c
+++ sys/arch/hp300/stand/common/version.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: version.c,v 1.4 1999/08/16 09:43:08 downsj Exp $	*/
+/*	$OpenBSD: version.c,v 1.5 2000/01/14 09:11:01 downsj Exp $	*/
 
 /*
  * Record major changes in the boot code here, and increment the version
@@ -18,6 +18,8 @@
  *
  * 2.5			Added SYS_CDBOOT.
  *
+ * 2.6			Fixed RTC reading for 2001.
+ *
  */
 
-const char version[] = "2.5";
+const char version[] = "2.6";
