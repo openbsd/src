b6c0941ee286e4c4dadbf757ed62b7f920073de6
itojun <itojun@openbsd.org>
2000-01-17 06:29:27 +0000

    don't need IFF_RUNNING for gif, as no resource is allocated for interface operation.

--- sys/net/if_gif.c
+++ sys/net/if_gif.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_gif.c,v 1.6 2000/01/17 05:42:39 itojun Exp $	*/
+/*	$OpenBSD: if_gif.c,v 1.7 2000/01/17 06:29:27 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -309,7 +309,7 @@ gif_ioctl(ifp, cmd, data)
 		bcopy((caddr_t)dst, (caddr_t)sa, size);
 		sc->gif_pdst = sa;
 		
-		ifp->if_flags |= (IFF_UP|IFF_RUNNING);
+		ifp->if_flags |= IFF_UP;
 		if_up(ifp);		/* send up RTM_IFINFO */
 
 		break;
@@ -375,11 +375,6 @@ gif_ioctl(ifp, cmd, data)
 		break;
 
 	case SIOCSIFFLAGS:
-		/* sync "running" with "up" */
-		if (ifp->if_flags & IFF_UP)
-			ifp->if_flags |= IFF_RUNNING;
-		else
-			ifp->if_flags &= ~IFF_RUNNING;
 		break;
 
 	default:
