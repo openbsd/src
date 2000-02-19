ea94bb9b07127aa08c4da81eb2fd2e0ba0eb7111
millert <millert@openbsd.org>
2000-02-19 16:44:18 +0000

    From NetBSD:     Check for SCSI_ADW_WDTR_DISABLE flag in tidmask not tid

--- sys/dev/ic/adwlib.c
+++ sys/dev/ic/adwlib.c
@@ -2435,7 +2435,7 @@ ADW_SCSI_REQ_Q *scsiq;
 		 * microcode.
 		 */
 #ifdef SCSI_ADW_WDTR_DISABLE
-	if(!(tid & SCSI_ADW_WDTR_DISABLE))
+	if(!(tidmask & SCSI_ADW_WDTR_DISABLE))
 #endif /* SCSI_ADW_WDTR_DISABLE */
 		if ((sc->wdtr_able & tidmask) && inq->WBus16) {
 			ADW_READ_WORD_LRAM(iot, ioh, ASC_MC_WDTR_ABLE,
@@ -2473,7 +2473,7 @@ ADW_SCSI_REQ_Q *scsiq;
 		 * 'sdtr_able' bit. Write the new value to the microcode.
 		 */
 #ifdef SCSI_ADW_SDTR_DISABLE
-	if(!(tid & SCSI_ADW_SDTR_DISABLE))
+	if(!(tidmask & SCSI_ADW_SDTR_DISABLE))
 #endif /* SCSI_ADW_SDTR_DISABLE */
 		if ((sc->sdtr_able & tidmask) && inq->Sync) {
 			ADW_READ_WORD_LRAM(iot, ioh, ASC_MC_SDTR_ABLE, cfg_word);
@@ -2508,7 +2508,7 @@ ADW_SCSI_REQ_Q *scsiq;
 		 * bugs will at least work with the BIOS.
 		 */
 #ifdef SCSI_ADW_TAGQ_DISABLE
-	if(!(tid & SCSI_ADW_TAGQ_DISABLE))
+	if(!(tidmask & SCSI_ADW_TAGQ_DISABLE))
 #endif /* SCSI_ADW_TAGQ_DISABLE */
 		if ((sc->tagqng_able & tidmask) && inq->CmdQue) {
 			ADW_READ_WORD_LRAM(iot, ioh, ASC_MC_TAGQNG_ABLE,
