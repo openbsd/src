a1ae1d3f2c23e4e2549a6cf5df12cf39c07a055e
art <art@openbsd.org>
2000-03-04 16:42:15 +0000

    Remove unnecessary, unreached, confusing code.

--- sys/arch/sparc/sparc/mem.c
+++ sys/arch/sparc/sparc/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.11 1999/11/22 19:22:02 matthieu Exp $	*/
+/*	$OpenBSD: mem.c,v 1.12 2000/03/04 16:42:15 art Exp $	*/
 /*	$NetBSD: mem.c,v 1.13 1996/03/30 21:12:16 christos Exp $ */
 
 /*
@@ -219,12 +219,6 @@ mmrw(dev, uio, flags)
 		default:
 			return (ENXIO);
 		}
-		if (error)
-			break;
-		iov->iov_base += c;
-		iov->iov_len -= c;
-		uio->uio_offset += c;
-		uio->uio_resid -= c;
 	}
 	if (minor(dev) == 0) {
 unlock:
