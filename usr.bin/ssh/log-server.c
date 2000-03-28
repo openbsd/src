6d4b4ef7ec68ffe45c9a45568946cf157b00b8f4
markus <markus@openbsd.org>
2000-03-28 20:26:21 +0000

    do not log() if debuggin to stderr

--- usr.bin/ssh/log-server.c
+++ usr.bin/ssh/log-server.c
@@ -15,7 +15,7 @@
  */
 
 #include "includes.h"
-RCSID("$Id: log-server.c,v 1.12 2000/02/27 18:50:09 deraadt Exp $");
+RCSID("$Id: log-server.c,v 1.13 2000/03/28 20:26:21 markus Exp $");
 
 #include <syslog.h>
 #include "packet.h"
@@ -132,9 +132,11 @@ do_log(LogLevel level, const char *fmt, va_list args)
 	} else {
 		vsnprintf(msgbuf, sizeof(msgbuf), fmt, args);
 	}
-	if (log_on_stderr)
+	if (log_on_stderr) {
 		fprintf(stderr, "%s\n", msgbuf);
-	openlog(__progname, LOG_PID, log_facility);
-	syslog(pri, "%.500s", msgbuf);
-	closelog();
+	} else {
+		openlog(__progname, LOG_PID, log_facility);
+		syslog(pri, "%.500s", msgbuf);
+		closelog();
+	}
 }
