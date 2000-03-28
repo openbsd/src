133a7928464fee64fe0d69177d1837ade4d4e8d9
markus <markus@openbsd.org>
2000-03-28 21:15:45 +0000

    split sshd.c -> auth.c session.c sshd.c plus cleanup and goto-removal

---
+++ usr.bin/ssh/auth.h
@@ -0,0 +1,6 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+void	do_authentication(void);
+
+#endif
---
+++ usr.bin/ssh/session.h
@@ -0,0 +1,7 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+/* SSH1 */
+void	do_authenticated(struct passwd * pw);
+
+#endif
