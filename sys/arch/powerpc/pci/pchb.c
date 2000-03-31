1f4dc907b2cb23247ace4a1f82f92a909f00f51e
rahnds <rahnds@openbsd.org>
2000-03-31 04:41:53 +0000

    PCi Host Bridge, dummy driver to quiet "not configured" messages, hopefully will be used to configure special items on bridges. (such as hacks to enable devices?)

---
+++ sys/arch/powerpc/pci/pchb.c
@@ -0,0 +1,124 @@
+/*	$NetBSD: pchb.c,v 1.4 2000/01/25 07:19:11 tsubai Exp $	*/
+
+/*-
+ * Copyright (c) 1996 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <machine/bus.h>
+
+#include <dev/pci/pcivar.h>
+#include <dev/pci/pcireg.h>
+#include <dev/pci/pcidevs.h>
+
+int	pchbmatch __P((struct device *, void *, void *));
+void	pchbattach __P((struct device *, struct device *, void *));
+
+struct cfattach pchb_ca = {
+	sizeof(struct device), pchbmatch, pchbattach
+};
+
+struct cfdriver pchb_cd = {
+	NULL, "pchb", DV_DULL
+};
+
+int
+pchbmatch(parent, cf, aux)
+	struct device *parent;
+	void *cf;
+	void *aux;
+{
+	struct pci_attach_args *pa = aux;
+
+	/*
+	 * Match all known PCI host chipsets.
+	 */
+	switch (PCI_VENDOR(pa->pa_id)) {
+	case PCI_VENDOR_APPLE:
+		switch (PCI_PRODUCT(pa->pa_id)) {
+		case PCI_PRODUCT_APPLE_BANDIT:
+		case PCI_PRODUCT_APPLE_UNINORTH:
+		case PCI_PRODUCT_APPLE_UNINORTHETH:
+		case PCI_PRODUCT_APPLE_UNINORTHAGP:
+			return (1);
+		}
+		break;
+
+	case PCI_VENDOR_MOT:
+		switch (PCI_PRODUCT(pa->pa_id)) {
+		case PCI_PRODUCT_MOT_MPC106:
+			return (1);
+		}
+		break;
+	}
+
+	return (0);
+}
+
+void
+pchbattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct pci_attach_args *pa = aux;
+	char devinfo[256];
+
+	printf("\n");
+
+	/*
+	 * All we do is print out a description.  Eventually, we
+	 * might want to add code that does something that's
+	 * possibly chipset-specific.
+	 */
+
+	/*
+	pci_devinfo(pa->pa_id, pa->pa_class, 0, devinfo);
+	printf("%s: %s (rev. 0x%02x)\n", self->dv_xname, devinfo,
+	    PCI_REVISION(pa->pa_class));
+	*/
+#include "gm.h"
+#if NGM > 0
+	if ((PCI_VENDOR(pa->pa_id) == PCI_VENDOR_APPLE) && 
+		(PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_APPLE_UNINORTHETH ))
+	{
+		 gmac_enable_hack();
+		
+	}
+#endif
+}
