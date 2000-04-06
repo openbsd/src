414da0d88440f16b7b6bea71c2ee7d647ebd3d4c
todd <todd@openbsd.org>
2000-04-06 19:43:46 +0000

    add vm/vm.h for prototypes; from mickey@

--- sys/arch/hppa/include/db_machdep.h
+++ sys/arch/hppa/include/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_machdep.h,v 1.3 1999/08/14 03:31:50 mickey Exp $	*/
+/*	$OpenBSD: db_machdep.h,v 1.4 2000/04/06 19:43:46 todd Exp $	*/
 
 /*
  * Copyright (c) 1998 Michael Shalayeff
@@ -32,6 +32,8 @@
 
 #ifndef	_MACHINE_DB_MACHDEP_H_
 #define	_MACHINE_DB_MACHDEP_H_
+
+#include <vm/vm.h>
 
 /* types the generic ddb module needs */
 typedef	vaddr_t db_addr_t;
