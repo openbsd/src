f0f5fe561c79a3c1ce04396eaf660b077fdecd51
angelos <angelos@openbsd.org>
2000-04-10 07:33:44 +0000

    Oops on sanity logic.

--- sys/netinet/ip_ether.c
+++ sys/netinet/ip_ether.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ether.c,v 1.6 2000/01/07 21:59:34 angelos Exp $  */
+/*	$OpenBSD: ip_ether.c,v 1.7 2000/04/10 07:33:44 angelos Exp $  */
 
 /*
  * The author of this code is Angelos D. Keromytis (kermit@adk.gr)
@@ -207,8 +207,8 @@ etherip_output(struct mbuf *m, struct tdb *tdb, struct mbuf **mp, int skip,
 
     /* Some address family sanity checks */
     if ((tdb->tdb_src.sa.sa_family != 0) &&
-        ((tdb->tdb_src.sa.sa_family != AF_INET) ||
-	 (tdb->tdb_src.sa.sa_family != AF_INET6)))
+        (tdb->tdb_src.sa.sa_family != AF_INET) &&
+	(tdb->tdb_src.sa.sa_family != AF_INET6))
     {
         DPRINTF(("etherip_output(): IP in protocol-family <%d> attempted, aborting", tdb->tdb_src.sa.sa_family));
 	etheripstat.etherip_adrops++;
