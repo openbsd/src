fdd0b74ce27bb3bd0ce5aea02bbe4f66749d7707
hugh <hugh@openbsd.org>
2000-05-01 08:20:56 +0000

    Put -d into usage and reorder. markus ok.

--- usr.bin/ssh/ssh-keygen.c
+++ usr.bin/ssh/ssh-keygen.c
@@ -7,7 +7,7 @@
  */
 
 #include "includes.h"
-RCSID("$Id: ssh-keygen.c,v 1.21 2000/04/27 08:01:27 markus Exp $");
+RCSID("$Id: ssh-keygen.c,v 1.22 2000/05/01 08:20:56 hugh Exp $");
 
 #include <openssl/evp.h>
 #include <openssl/pem.h>
@@ -488,7 +488,7 @@ void
 usage(void)
 {
 	printf("ssh-keygen version %s\n", SSH_VERSION);
-	printf("Usage: %s [-b bits] [-p] [-c] [-l] [-x] [-X] [-y] [-f file] [-P pass] [-N new-pass] [-C comment]\n", __progname);
+	printf("Usage: %s [-b bits] [-c] [-d] [-f file] [-l] [-p] [-q] [-x] [-y] [-C comment] [-N new-pass] [-P pass] [-X]\n", __progname);
 	exit(1);
 }
 
