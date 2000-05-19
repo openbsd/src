9123f3b818df87482bf7ac848409fd708c48a5b1
maja <maja@openbsd.org>
2000-05-19 17:07:05 +0000

    Add enable to history when enable command i used. Reported by itojun@ and David Brownlee <abs@mono.org>. -moj

--- sys/kern/subr_userconf.c
+++ sys/kern/subr_userconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_userconf.c,v 1.19 2000/01/08 23:23:37 d Exp $	*/
+/*	$OpenBSD: subr_userconf.c,v 1.20 2000/05/19 17:07:05 maja Exp $	*/
 
 /*
  * Copyright (c) 1996 Mats O Jansson <moj@stacken.kth.se>
@@ -589,7 +589,7 @@ userconf_enable(devno)
 			printf(" already");
 		} else {
 			/* XXX add cmd 'e' <devno> eoc */
-			userconf_hist_cmd('d');
+			userconf_hist_cmd('e');
 			userconf_hist_int(devno);
 			userconf_hist_eoc();
 		}
