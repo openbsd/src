d8d0e822fc7cda4d1ce406af99862b2e3ffe8566
itojun <itojun@openbsd.org>
2000-06-04 13:38:14 +0000

    nuke non-standard include files (has been #error only for a long time so there should be no sideeffect)

--- sys/netinet6/icmp6.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: icmp6.h,v 1.7 2000/02/09 07:37:15 itojun Exp $	*/
-
-#error "do not include netinet6/ip6.h, include netinet/ip6.h"
--- sys/netinet6/ip6.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: ip6.h,v 1.4 2000/02/09 07:37:15 itojun Exp $	*/
-
-#error "do not include netinet6/ip6.h, include netinet/ip6.h"
