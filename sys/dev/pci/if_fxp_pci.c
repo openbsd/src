0750834ac2074beb7365554cf9f230be945a11e8
deraadt <deraadt@openbsd.org>
2000-06-18 15:27:54 +0000

    match 82559ER

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_fxp_pci.c,v 1.2 2000/04/26 19:12:08 chris Exp $	*/
+/*	$OpenBSD: if_fxp_pci.c,v 1.3 2000/06/18 15:27:54 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1995, David Greenman
@@ -118,6 +118,7 @@ fxp_pci_match(parent, match, aux)
 	switch (PCI_PRODUCT(pa->pa_id)) {
 	case PCI_PRODUCT_INTEL_82557:
 	case PCI_PRODUCT_INTEL_82559:
+	case PCI_PRODUCT_INTEL_82559ER:
 		return (1);
 	}
 
