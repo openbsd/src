f208e5e0a2ad659860449f5ac0159b0c50f49d1f
markus <markus@openbsd.org>
2000-06-18 17:13:41 +0000

    do not mess with tty's. makes EDITOR=ed cvs commit work over ssh2

--- usr.bin/ssh/aux.c
+++ usr.bin/ssh/aux.c
@@ -1,5 +1,5 @@
 #include "includes.h"
-RCSID("$OpenBSD: aux.c,v 1.2 2000/05/17 09:47:59 markus Exp $");
+RCSID("$OpenBSD: aux.c,v 1.3 2000/06/18 17:13:41 markus Exp $");
 
 #include "ssh.h"
 
@@ -22,6 +22,11 @@ void
 set_nonblock(int fd)
 {
 	int val;
+	if (isatty(fd)) {
+		/* do not mess with tty's */
+		debug("no set_nonblock for tty fd %d", fd);
+		return;
+	}
 	val = fcntl(fd, F_GETFL, 0);
 	if (val < 0) {
 		error("fcntl(%d, F_GETFL, 0): %s", fd, strerror(errno));
