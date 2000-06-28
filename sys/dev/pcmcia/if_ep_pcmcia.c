1d697a2dcd13895ff47bd5597ebc06f21a1c27ad
millert <millert@openbsd.org>
2000-06-28 18:03:37 +0000

    Remove pointless variable.  It is set to 0, then returned without modification.

--- sys/dev/pcmcia/if_ep_pcmcia.c
+++ sys/dev/pcmcia/if_ep_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ep_pcmcia.c,v 1.24 2000/05/29 18:04:07 aaron Exp $	*/
+/*	$OpenBSD: if_ep_pcmcia.c,v 1.25 2000/06/28 18:03:37 millert Exp $	*/
 /*	$NetBSD: if_ep_pcmcia.c,v 1.16 1998/08/17 23:20:40 thorpej Exp $  */
 
 /*-
@@ -394,7 +394,6 @@ ep_pcmcia_detach(dev, flags)
 	struct ep_pcmcia_softc *psc = (struct ep_pcmcia_softc *)dev;
 	struct ep_softc *sc = &psc->sc_ep;
 	struct ifnet *ifp = &sc->sc_arpcom.ac_if;
-	int rv = 0;
 
 	pcmcia_io_unmap(psc->sc_pf, psc->sc_io_window);
 	pcmcia_io_free(psc->sc_pf, &psc->sc_pcioh);
@@ -402,7 +401,7 @@ ep_pcmcia_detach(dev, flags)
 	ether_ifdetach(ifp);
 	if_detach(ifp);
 
-	return (rv);
+	return (0);
 }
 
 int
