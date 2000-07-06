c863cf8405b057b3ca584086021485cdf9d93dfb
mjacob <mjacob@openbsd.org>
2000-07-06 06:19:08 +0000

    Port latest in from FreeBSD- finally have some of the link issues sorted out so we should now be able to work with switches.

--- sys/dev/pci/if_wxvar.h
+++ sys/dev/pci/if_wxvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_wxvar.h,v 1.1 2000/02/11 14:51:50 jason Exp $	*/
+/*	$OpenBSD: if_wxvar.h,v 1.2 2000/07/06 06:19:08 mjacob Exp $	*/
 
 /*                  
  * Copyright (c) 1999, Traakan Software
@@ -318,11 +318,12 @@ typedef struct wx_softc {
 	/*
 	 * misc goodies
 	 */
-	u_int32_t		:	18,
+	u_int32_t		:	17,
 		wx_no_flow 	:	1,
 		wx_ilos		:	1,
 		wx_no_ilos	:	1,
 		wx_debug	:	1,
+		ane_failed	:	1,
 		linkup		:	1,
 		all_mcasts	:	1,
 		revision	:	8;	/* chip revision */
@@ -405,3 +406,9 @@ typedef struct wx_softc {
 #endif
 #define	R_NXT_IDX(x)	((x + RXINCR) & (WX_MAX_RDESC - 1))
 #define	R_PREV_IDX(x)	((x - RXINCR) & (WX_MAX_RDESC - 1))
+
+/*
+ * Link Up timeout, in milliseconds.
+ */
+
+#define	WX_LINK_UP_TIMEOUT	500
