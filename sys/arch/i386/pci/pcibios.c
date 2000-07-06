266184431a1708f25e46f101c9d5fa3bfa6ff1af
mickey <mickey@openbsd.org>
2000-07-06 18:32:15 +0000

    don't panic for nothing

--- sys/arch/i386/pci/pcibios.c
+++ sys/arch/i386/pci/pcibios.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcibios.c,v 1.3 2000/03/28 03:37:59 mickey Exp $	*/
+/*	$OpenBSD: pcibios.c,v 1.4 2000/07/06 18:32:15 mickey Exp $	*/
 /*	$NetBSD: pcibios.c,v 1.2 1999/11/17 07:33:41 thorpej Exp $	*/
 
 /*-
@@ -190,8 +190,8 @@ pcibios_init()
 
 		case 1:
 			/* Fatal error. */
-			panic("pcibios_init: interrupt fixup failed");
-			break;
+			printf("pcibios_init: interrupt fixup failed");
+			return;
 		}
 
 		/*
