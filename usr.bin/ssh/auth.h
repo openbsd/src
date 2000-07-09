6b6397843ec7bec1c40e49f59cfb59d57ea00a93
deraadt <deraadt@openbsd.org>
2000-07-09 01:21:15 +0000

    KNF

--- usr.bin/ssh/auth.h
+++ usr.bin/ssh/auth.h
@@ -7,7 +7,7 @@ void	do_authentication2(void);
 struct passwd *
 auth_get_user(void);
 
-int allowed_user(struct passwd * pw);;
+int allowed_user(struct passwd * pw);
 
 #define AUTH_FAIL_MAX 6
 #define AUTH_FAIL_LOG (AUTH_FAIL_MAX/2)
