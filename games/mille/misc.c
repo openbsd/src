e3073a5f0e5713fddf3381e8ad4309667d50c812
pjanzen <pjanzen@openbsd.org>
2000-07-23 21:50:20 +0000

    Die more cleanly if stdin can't be read from.

--- games/mille/misc.c
+++ games/mille/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.c,v 1.5 1999/09/30 03:24:32 pjanzen Exp $	*/
+/*	$OpenBSD: misc.c,v 1.6 2000/07/23 21:50:20 pjanzen Exp $	*/
 /*	$NetBSD: misc.c,v 1.4 1995/03/24 05:01:54 cgd Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)misc.c	8.1 (Berkeley) 5/31/93";
 #else
-static char rcsid[] = "$OpenBSD: misc.c,v 1.5 1999/09/30 03:24:32 pjanzen Exp $";
+static char rcsid[] = "$OpenBSD: misc.c,v 1.6 2000/07/23 21:50:20 pjanzen Exp $";
 #endif
 #endif /* not lint */
 
@@ -265,6 +265,6 @@ readch()
 
 	for (cnt = 0; read(STDIN_FILENO, &c, 1) <= 0; cnt++)
 		if (cnt > 100)
-			exit(1);
+			die(1);
 	return c;
 }
