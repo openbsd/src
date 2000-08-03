a7afdd230860191bd62e9704ad1fd3d1c05526b1
angelos <angelos@openbsd.org>
2000-08-03 07:55:48 +0000

    Correct handling of ip_off (askk@rsn.hk-r.se)

--- sys/netinet/ip_ipip.c
+++ sys/netinet/ip_ipip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ipip.c,v 1.3 2000/06/20 04:19:11 itojun Exp $ */
+/*	$OpenBSD: ip_ipip.c,v 1.4 2000/08/03 07:55:48 angelos Exp $ */
 
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
@@ -468,7 +468,8 @@ ipip_output(struct mbuf *m, struct tdb *tdb, struct mbuf **mp, int skip,
 		 */
 		m_copydata(m, sizeof(struct ip) + offsetof(struct ip, ip_off),
 			   sizeof(u_int16_t), (caddr_t) &ipo->ip_off);
-		ipo->ip_off &= ~(IP_DF | IP_MF | IP_OFFMASK);
+                ipo->ip_off = ntohs(ipo->ip_off) & ~(IP_DF | IP_MF |
+                                                     IP_OFFMASK);
 	    }
 #ifdef INET6
 	    else if (tp == (IPV6_VERSION >> 4))
