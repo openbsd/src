67df6c6f247e47ce317c4da32f0374f9304ef86f
kevlo <kevlo@openbsd.org>
2000-08-16 14:13:13 +0000

    Fix AK4531 data sheet link. Ok'd by deraadt@

--- sys/dev/pci/eap.c
+++ sys/dev/pci/eap.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: eap.c,v 1.9 2000/07/18 07:03:29 csapuntz Exp $ */
+/*      $OpenBSD: eap.c,v 1.10 2000/08/16 14:13:13 kevlo Exp $ */
 /*	$NetBSD: eap.c,v 1.25 1999/02/18 07:59:30 mycroft Exp $	*/
 
 /*
@@ -48,7 +48,7 @@
  * Data sheets can be found at 
  * http://www.ensoniq.com/multimedia/semi_html/html/es1370.zip
  * and
- * http://206.214.38.151/pdf/4531.pdf
+ * http://www.akm.com/pdf/4531.pdf
  *
  * Added Creative Ensoniq support: ES1371 + AC97 = hack city.
  * -- Ezra Story <ezy@panix.com>
