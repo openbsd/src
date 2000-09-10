6a117714d7f33f92c0431e0642f1404ef355d63b
miod <miod@openbsd.org>
2000-09-10 05:27:16 +0000

    2nd part of the ecoff execution fix, was forgotten.

--- sys/arch/mips/include/ecoff_machdep.h
+++ sys/arch/mips/include/ecoff_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ecoff.h,v 1.2 1999/01/27 04:46:05 imp Exp $	*/
+/*	$OpenBSD: ecoff_machdep.h,v 1.1 2000/09/10 05:27:17 miod Exp $	*/
 /*	$NetBSD: ecoff.h,v 1.4 1995/06/16 02:07:33 mellon Exp $	*/
 
 /*
