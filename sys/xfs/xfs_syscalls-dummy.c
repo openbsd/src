88c9b9a8d1a76af8f99381ada75d9ad88c3e52c3
deraadt <deraadt@openbsd.org>
2000-09-12 00:21:47 +0000

    Apparently Art did not even compile a kernel before commiting

--- sys/xfs/xfs_syscalls-dummy.c
+++ sys/xfs/xfs_syscalls-dummy.c
@@ -2,7 +2,6 @@
  * This code released into public domain by Artur Grabowski <art@openbsd.org>
  */
 #include <xfs/xfs_locl.h>
-#include <sys/syscallargs.h>
 
 int
 sys_xfspioctl(struct proc *proc, void *varg, register_t *retval)
