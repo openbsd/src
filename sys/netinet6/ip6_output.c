a855fa9bfe683b72e6794f7dd2e6ae47de7224d2
itojun <itojun@openbsd.org>
2000-09-16 13:58:23 +0000

    repair IPv6 multicasting.  INP_IPV6_MCAST is necessary. (i will try to remove this flag bit later)

--- sys/netinet6/ip6_output.c
+++ sys/netinet6/ip6_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_output.c,v 1.14 2000/08/19 09:17:36 itojun Exp $	*/
+/*	$OpenBSD: ip6_output.c,v 1.15 2000/09/16 13:58:23 itojun Exp $	*/
 /*	$KAME: ip6_output.c,v 1.122 2000/08/19 02:12:02 jinmei Exp $	*/
 
 /*
@@ -1404,6 +1404,17 @@ ip6_ctloutput(op, so, level, optname, mp)
 			case IPV6_LEAVE_GROUP:
 				error =	ip6_setmoptions(optname,
 					&inp->inp_moptions6, m);
+				/*
+				 * XXX: setting the flag would be redundant
+				 *      except at the first time. Also, we
+				 *      actually don't have to reset the flag,
+				 *      since ip6_freemoptions() would simply
+				 *      return when the inp_moptions6 is NULL.
+				 */
+				if (inp->inp_moptions6)
+					inp->inp_flags |= INP_IPV6_MCAST;
+				else
+					inp->inp_flags &= ~INP_IPV6_MCAST;
 				break;
 
 			case IPV6_PORTRANGE:
