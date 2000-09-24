977bfcd90182eb48609ff133a6170868f9e02743
d <d@openbsd.org>
2000-09-24 14:35:10 +0000

    unlock, instead of re-lock. fixes PR#1408

--- lib/libc/gen/getgrent.c
+++ lib/libc/gen/getgrent.c
@@ -33,7 +33,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: getgrent.c,v 1.12 2000/01/08 08:07:24 d Exp $";
+static char rcsid[] = "$OpenBSD: getgrent.c,v 1.13 2000/09/24 14:35:10 d Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -184,7 +184,7 @@ setgroupent(stayopen)
 		_gr_stayopen = stayopen;
 		retval = 1;
 	}
-	_THREAD_PRIVATE_MUTEX_LOCK(gr);
+	_THREAD_PRIVATE_MUTEX_UNLOCK(gr);
 	return (retval);
 }
 
