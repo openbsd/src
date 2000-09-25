a31e6b8327b1d1bdfcce54fb11a3af49c131a66f
aaron <aaron@openbsd.org>
2000-09-25 21:03:15 +0000

    Support the i82562; from FreeBSD. Apparently nothing more needed than to match its PCI device ID.

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_fxp_pci.c,v 1.3 2000/06/18 15:27:54 deraadt Exp $	*/
+/*	$OpenBSD: if_fxp_pci.c,v 1.4 2000/09/25 21:03:15 aaron Exp $	*/
 
 /*
  * Copyright (c) 1995, David Greenman
@@ -119,6 +119,7 @@ fxp_pci_match(parent, match, aux)
 	case PCI_PRODUCT_INTEL_82557:
 	case PCI_PRODUCT_INTEL_82559:
 	case PCI_PRODUCT_INTEL_82559ER:
+	case PCI_PRODUCT_INTEL_82562:
 		return (1);
 	}
 
