bd919f0a2785b7140913f35b12a1f804e67748e6
millert <millert@openbsd.org>
2000-10-04 21:19:38 +0000

    Fixes a format string error in the kill (k) command; vort@wiretapped.net

--- usr.bin/top/top.c
+++ usr.bin/top/top.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: top.c,v 1.3 1999/05/23 17:19:21 aaron Exp $	*/
+/*	$OpenBSD: top.c,v 1.4 2000/10/04 21:19:38 millert Exp $	*/
 
 const char copyright[] = "Copyright (c) 1984 through 1996, William LeFebvre";
 
@@ -761,7 +761,7 @@ Usage: %s [-ISbinqu] [-d x] [-s x] [-o field] [-U username] [number]\n",
 				{
 				    if ((errmsg = kill_procs(tempbuf2)) != NULL)
 				    {
-					new_message(MT_standout, errmsg);
+					new_message(MT_standout, "%s", errmsg);
 					putchar('\r');
 					no_command = Yes;
 				    }
@@ -778,7 +778,7 @@ Usage: %s [-ISbinqu] [-d x] [-s x] [-o field] [-U username] [number]\n",
 				{
 				    if ((errmsg = renice_procs(tempbuf2)) != NULL)
 				    {
-					new_message(MT_standout, errmsg);
+					new_message(MT_standout, "%s", errmsg);
 					putchar('\r');
 					no_command = Yes;
 				    }
