ebb42fdb8627dc1b439687762677679a9c4a3ee7
d <d@openbsd.org>
2000-10-05 13:27:55 +0000

    missed it by That Much

--- lib/libpthread/arch/alpha/uthread_machdep.h
+++ lib/libpthread/arch/alpha/uthread_machdep.h
@@ -1,6 +1,6 @@
-/*	$OpenBSD: uthread_machdep.h,v 1.6 2000/10/04 05:55:33 d Exp $	*/
+/*	$OpenBSD: uthread_machdep.h,v 1.7 2000/10/05 13:27:55 d Exp $	*/
 /* David Leonard, <d@csee.uq.edu.au>. Public domain. */
 
 struct _machdep_state {
-	int	fp;
+	int	sp;
 };
