380e4b1cf7ae2f3cc19b45b84844d7cafaad1992
aaron <aaron@openbsd.org>
2000-11-15 20:42:39 +0000

    This driver supports PC display adapter hardware within the wscons(4) console framework. It doesn't provide direct device driver entry points but makes its functions available via the internal wsdisplay(4) interface.
    The pcdisplay driver is indended as a minimal ``catch-all'' driver for
    the different kinds of MDA or CGA compatible adapters. It doesn't support
    multiple screens, nor colors or font loading.
    
    From NetBSD. Contains routines used by the vga(4) and ega(4) drivers, etc.
    This is more MI stuff in prepartion for wscons on i386 and alpha.

---
+++ sys/dev/ic/pcdisplay_subr.c
@@ -0,0 +1,260 @@
+/* $NetBSD: pcdisplay_subr.c,v 1.16 2000/06/08 07:01:19 cgd Exp $ */
+
+/*
+ * Copyright (c) 1995, 1996 Carnegie-Mellon University.
+ * All rights reserved.
+ *
+ * Author: Chris G. Demetriou
+ * 
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/device.h>
+#include <machine/bus.h>
+
+#include <dev/ic/mc6845reg.h>
+#include <dev/ic/pcdisplayvar.h>
+
+#include <dev/wscons/wsdisplayvar.h>
+
+void
+pcdisplay_cursor_init(scr, existing)
+	struct pcdisplayscreen *scr;
+	int existing;
+{
+#ifdef PCDISPLAY_SOFTCURSOR
+	bus_space_tag_t memt;
+	bus_space_handle_t memh;
+	int off;
+
+	pcdisplay_6845_write(scr->hdl, curstart, 0x10);
+	pcdisplay_6845_write(scr->hdl, curend, 0x10);
+	
+	if (existing) {
+		/*
+		 * This is the first screen. At this point, scr->active is
+		 * false and scr->mem is NULL (no backing store), so we
+		 * can't use pcdisplay_cursor() to do this.
+		 */
+		memt = scr->hdl->ph_memt;
+		memh = scr->hdl->ph_memh;
+		off = (scr->vc_crow * scr->type->ncols + scr->vc_ccol) * 2 +
+		    scr->dispoffset;
+
+		scr->cursortmp = bus_space_read_2(memt, memh, off);
+		bus_space_write_2(memt, memh, off, scr->cursortmp ^ 0x7700);
+	} else
+		scr->cursortmp = 0;
+#endif
+	scr->cursoron = 1;
+}
+
+void
+pcdisplay_cursor(id, on, row, col)
+	void *id;
+	int on, row, col;
+{
+#ifdef PCDISPLAY_SOFTCURSOR
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	int off;
+
+	/* Remove old cursor image */
+	if (scr->cursoron) {
+		off = scr->vc_crow * scr->type->ncols + scr->vc_ccol;
+		if (scr->active)
+			bus_space_write_2(memt, memh, scr->dispoffset + off * 2,
+			    scr->cursortmp);
+		else
+			scr->mem[off] = scr->cursortmp;
+	}
+		
+	scr->vc_crow = row;
+	scr->vc_ccol = col;
+
+	if ((scr->cursoron = on) == 0)
+		return;
+
+	off = (scr->vc_crow * scr->type->ncols + scr->vc_ccol);
+	if (scr->active) {
+		off = off * 2 + scr->dispoffset;
+		scr->cursortmp = bus_space_read_2(memt, memh, off);
+		bus_space_write_2(memt, memh, off, scr->cursortmp ^ 0x7700);
+	} else {
+		scr->cursortmp = scr->mem[off];
+		scr->mem[off] = scr->cursortmp ^ 0x7700;
+	}
+#else 	/* PCDISPLAY_SOFTCURSOR */
+	struct pcdisplayscreen *scr = id;
+	int pos;
+
+	scr->vc_crow = row;
+	scr->vc_ccol = col;
+	scr->cursoron = on;
+
+	if (scr->active) {
+		if (!on)
+			pos = 0x1010;
+		else
+			pos = scr->dispoffset / 2
+				+ row * scr->type->ncols + col;
+
+		pcdisplay_6845_write(scr->hdl, cursorh, pos >> 8);
+		pcdisplay_6845_write(scr->hdl, cursorl, pos);
+	}
+#endif	/* PCDISPLAY_SOFTCURSOR */
+}
+
+#if 0
+unsigned int
+pcdisplay_mapchar_simple(id, uni)
+	void *id;
+	int uni;
+{
+	if (uni < 128)
+		return (uni);
+
+	return (1); /* XXX ??? smiley */
+}
+#endif
+
+void
+pcdisplay_putchar(id, row, col, c, attr)
+	void *id;
+	int row, col;
+	u_int c;
+	long attr;
+{
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	int off;
+
+	off = row * scr->type->ncols + col;
+
+	if (scr->active)
+		bus_space_write_2(memt, memh, scr->dispoffset + off * 2,
+				  c | (attr << 8));
+	else
+		scr->mem[off] = c | (attr << 8);
+}
+
+void
+pcdisplay_copycols(id, row, srccol, dstcol, ncols)
+	void *id;
+	int row, srccol, dstcol, ncols;
+{
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	bus_size_t srcoff, dstoff;
+
+	srcoff = dstoff = row * scr->type->ncols;
+	srcoff += srccol;
+	dstoff += dstcol;
+
+	if (scr->active)
+		bus_space_copy_2(memt, memh,
+					scr->dispoffset + srcoff * 2,
+					memh, scr->dispoffset + dstoff * 2,
+					ncols);
+	else
+		bcopy(&scr->mem[srcoff], &scr->mem[dstoff], ncols * 2);
+}
+
+void
+pcdisplay_erasecols(id, row, startcol, ncols, fillattr)
+	void *id;
+	int row, startcol, ncols;
+	long fillattr;
+{
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	bus_size_t off;
+	u_int16_t val;
+	int i;
+
+	off = row * scr->type->ncols + startcol;
+
+	val = (fillattr << 8) | ' ';
+
+	if (scr->active)
+		bus_space_set_region_2(memt, memh, scr->dispoffset + off * 2,
+				       val, ncols);
+	else
+		for (i = 0; i < ncols; i++)
+			scr->mem[off + i] = val;
+}
+
+void
+pcdisplay_copyrows(id, srcrow, dstrow, nrows)
+	void *id;
+	int srcrow, dstrow, nrows;
+{
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	int ncols = scr->type->ncols;
+	bus_size_t srcoff, dstoff;
+
+	srcoff = srcrow * ncols + 0;
+	dstoff = dstrow * ncols + 0;
+
+	if (scr->active)
+		bus_space_copy_2(memt, memh,
+					scr->dispoffset + srcoff * 2,
+					memh, scr->dispoffset + dstoff * 2,
+					nrows * ncols);
+	else
+		bcopy(&scr->mem[srcoff], &scr->mem[dstoff],
+		      nrows * ncols * 2);
+}
+
+void
+pcdisplay_eraserows(id, startrow, nrows, fillattr)
+	void *id;
+	int startrow, nrows;
+	long fillattr;
+{
+	struct pcdisplayscreen *scr = id;
+	bus_space_tag_t memt = scr->hdl->ph_memt;
+	bus_space_handle_t memh = scr->hdl->ph_memh;
+	bus_size_t off, count;
+	u_int16_t val;
+	int i;
+
+	off = startrow * scr->type->ncols;
+	count = nrows * scr->type->ncols;
+
+	val = (fillattr << 8) | ' ';
+
+	if (scr->active)
+		bus_space_set_region_2(memt, memh, scr->dispoffset + off * 2,
+				       val, count);
+	else
+		for (i = 0; i < count; i++)
+			scr->mem[off + i] = val;
+}
---
+++ sys/dev/ic/pcdisplayvar.h
@@ -0,0 +1,101 @@
+/* $NetBSD: pcdisplayvar.h,v 1.8 2000/01/25 02:44:03 ad Exp $ */
+
+/*
+ * Copyright (c) 1998
+ *	Matthias Drochner.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed for the NetBSD Project
+ *	by Matthias Drochner.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#if 0
+#include "opt_pcdisplay.h"
+#endif
+
+#define offsetof(type, member) ((size_t)(&((type *)0)->member))
+
+struct pcdisplayscreen {
+	struct pcdisplay_handle *hdl;
+
+	const struct wsscreen_descr *type;
+
+	int active;		/* currently displayed */
+	u_int16_t *mem;		/* backing store for contents */
+
+	int cursoron;		/* cursor displayed? */
+#ifdef PCDISPLAY_SOFTCURSOR
+	int cursortmp;		/* glyph & attribute behind software cursor */
+#endif
+	int vc_ccol, vc_crow;	/* current cursor position */
+
+	int dispoffset;		/* offset of displayed area in video mem */
+	int visibleoffset;
+};
+
+struct pcdisplay_handle {
+	bus_space_tag_t	ph_iot, ph_memt;
+	bus_space_handle_t ph_ioh_6845, ph_memh;
+};
+
+static inline u_int8_t _pcdisplay_6845_read __P((struct pcdisplay_handle *,
+						 int));
+static inline void _pcdisplay_6845_write __P((struct pcdisplay_handle *,
+					      int, u_int8_t));
+
+static inline u_int8_t _pcdisplay_6845_read(ph, reg)
+	struct pcdisplay_handle *ph;
+	int reg;
+{
+	bus_space_write_1(ph->ph_iot, ph->ph_ioh_6845, MC6845_INDEX, reg);
+	return (bus_space_read_1(ph->ph_iot, ph->ph_ioh_6845, MC6845_DATA));
+}
+
+static inline void _pcdisplay_6845_write(ph, reg, val)
+	struct pcdisplay_handle *ph;
+	int reg;
+	u_int8_t val;
+{
+	bus_space_write_1(ph->ph_iot, ph->ph_ioh_6845, MC6845_INDEX, reg);
+	bus_space_write_1(ph->ph_iot, ph->ph_ioh_6845, MC6845_DATA, val);
+}
+
+#define pcdisplay_6845_read(ph, reg) \
+	_pcdisplay_6845_read(ph, offsetof(struct reg_mc6845, reg))
+#define pcdisplay_6845_write(ph, reg, val) \
+	_pcdisplay_6845_write(ph, offsetof(struct reg_mc6845, reg), val)
+
+void	pcdisplay_cursor_init __P((struct pcdisplayscreen *, int));
+void	pcdisplay_cursor __P((void *, int, int, int));
+#if 0
+unsigned int pcdisplay_mapchar_simple __P((void *, int));
+#endif
+int pcdisplay_mapchar __P((void *, int, unsigned int *));
+void	pcdisplay_putchar __P((void *, int, int, u_int, long));
+void	pcdisplay_copycols __P((void *, int, int, int,int));
+void	pcdisplay_erasecols __P((void *, int, int, int, long));
+void	pcdisplay_copyrows __P((void *, int, int, int));
+void	pcdisplay_eraserows __P((void *, int, int, long));
