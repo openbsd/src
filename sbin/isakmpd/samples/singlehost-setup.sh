e6fcd58c39c92de7e576330c05f771ff7705eb28
niklas <niklas@openbsd.org>
2000-11-23 12:56:25 +0000

    samples/singlehost-east.conf: Merge with EOM 1.10 samples/singlehost-west.conf: Merge with EOM 1.10 samples/singlehost-setup.sh: Merge with EOM 1.3
    author: niklas
    use networks that fits me better

--- sbin/isakmpd/samples/singlehost-setup.sh
+++ sbin/isakmpd/samples/singlehost-setup.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
-#	$OpenBSD: singlehost-setup.sh,v 1.1 1999/03/31 23:45:53 niklas Exp $
-#	$EOM: singlehost-setup.sh,v 1.2 1999/03/31 23:45:16 niklas Exp $
+#	$OpenBSD: singlehost-setup.sh,v 1.2 2000/11/23 12:56:25 niklas Exp $
+#	$EOM: singlehost-setup.sh,v 1.3 2000/11/23 12:24:43 niklas Exp $
 
 # A script to test single-host VPNs
 
 cleanup () {
   ipf -r -f - <<'  EOF'
-  pass out quick on lo2 proto 50
-  pass out quick on lo2 from 192.168.1.0/24 to any
-  pass out quick on lo3 proto 50
-  pass out quick on lo3 from 192.168.2.0/24 to any
+  pass out quick on lo2 proto 50 all
+  pass out quick on lo2 from 192.168.11.0/24 to any
+  pass out quick on lo3 proto 50 all
+  pass out quick on lo3 from 192.168.12.0/24 to any
   block out on lo2
   block out on lo3
   EOF
 }
 
-ifconfig lo2 192.168.1.1 netmask 0xffffff00
-ifconfig lo3 192.168.2.1 netmask 0xffffff00
-ifconfig lo4 10.1.0.1 netmask 0xffff0000
-ifconfig lo5 10.1.0.2 netmask 0xffff0000
+ifconfig lo2 192.168.11.1 netmask 0xffffff00
+ifconfig lo3 192.168.12.1 netmask 0xffffff00
+ifconfig lo4 10.1.0.11 netmask 0xffff0000
+ifconfig lo5 10.1.0.12 netmask 0xffff0000
 
 ipf -E -f - <<EOF
-pass out quick on lo2 proto 50
-pass out quick on lo2 from 192.168.1.0/24 to any
-pass out quick on lo3 proto 50
-pass out quick on lo3 from 192.168.2.0/24 to any
+pass out quick on lo2 proto 50 all
+pass out quick on lo2 from 192.168.11.0/24 to any
+pass out quick on lo3 proto 50 all
+pass out quick on lo3 from 192.168.12.0/24 to any
 block out on lo2
 block out on lo3
 EOF
@@ -37,6 +37,6 @@ isakmpd -c singlehost-west.conf -f west.fifo "$@"
 # Give them some slack...
 
 sleep 10
-ping -I 192.168.1.1 -c 30 192.168.2.1
+ping -I 192.168.11.1 -c 30 192.168.12.1
 
 cleanup
