e5d4b29d8dd73566e17c87615d627da3e721328e
provos <provos@openbsd.org>
2000-12-09 23:51:11 +0000

    remove unnecessary '\n'

--- usr.bin/ssh/compat.c
+++ usr.bin/ssh/compat.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: compat.c,v 1.31 2000/12/06 22:58:14 markus Exp $");
+RCSID("$OpenBSD: compat.c,v 1.32 2000/12/09 23:51:11 provos Exp $");
 
 #include "ssh.h"
 #include "packet.h"
@@ -88,7 +88,7 @@ compat_datafellows(const char *version)
 		ret = regexec(&reg, version, 0, NULL, 0);
 		regfree(&reg);
 		if (ret == 0) {
-			debug("match: %s pat %s\n", version, check[i].pat);
+			debug("match: %s pat %s", version, check[i].pat);
 			datafellows = check[i].bugs;
 			return;
 		}
