4d905ba1919fae6563d5c6068399969c76db69cf
itojun <itojun@openbsd.org>
2000-12-11 17:53:29 +0000

    typo in -s message

--- usr.bin/netstat/inet6.c
+++ usr.bin/netstat/inet6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: inet6.c,v 1.12 2000/12/11 17:40:52 itojun Exp $	*/
+/*	$OpenBSD: inet6.c,v 1.13 2000/12/11 17:53:29 itojun Exp $	*/
 /*	BSDI inet.c,v 2.3 1995/10/24 02:19:29 prb Exp	*/
 /*
  * Copyright (c) 1983, 1988, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)inet.c	8.4 (Berkeley) 4/20/94";
 #else
-/*__RCSID("$OpenBSD: inet6.c,v 1.12 2000/12/11 17:40:52 itojun Exp $");*/
+/*__RCSID("$OpenBSD: inet6.c,v 1.13 2000/12/11 17:53:29 itojun Exp $");*/
 /*__RCSID("KAME Id: inet6.c,v 1.10 2000/02/09 10:49:31 itojun Exp");*/
 #endif
 #endif /* not lint */
@@ -830,7 +830,7 @@ icmp6_stats(off, name)
 	p(icp6s_canterror,
 	    "\t%qu error%s not generated because old message was icmp6 or so\n");
 	p(icp6s_toofreq,
-	    "\t%qu error%s not generated because rate limitation\n");
+	    "\t%qu error%s not generated because of rate limitation\n");
 	for (first = 1, i = 0; i < 256; i++)
 		if (icmp6stat.icp6s_outhist[i] != 0) {
 			if (first) {
