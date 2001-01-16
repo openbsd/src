fc76c057342d9c6119d54258c342c80cc85e9c51
itojun <itojun@openbsd.org>
2001-01-16 06:16:34 +0000

    s/ND6DEBUG/ND6_DEBUG/ to synchronize with other places

--- sys/netinet6/icmp6.c
+++ sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: icmp6.c,v 1.28 2001/01/08 06:22:05 itojun Exp $	*/
+/*	$OpenBSD: icmp6.c,v 1.29 2001/01/16 06:16:34 itojun Exp $	*/
 /*	$KAME: icmp6.c,v 1.172 2000/12/11 19:27:06 itojun Exp $	*/
 
 /*
@@ -417,7 +417,7 @@ icmp6_input(mp, offp, proto)
 	code = icmp6->icmp6_code;
 
 	if ((sum = in6_cksum(m, IPPROTO_ICMPV6, off, icmp6len)) != 0) {
-#ifdef ND6DEBUG
+#ifdef ND6_DEBUG
 		log(LOG_ERR,
 		    "ICMP6 checksum error(%d|%x) %s\n",
 		    icmp6->icmp6_type, sum, ip6_sprintf(&ip6->ip6_src));
