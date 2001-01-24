2f80cd3ff81196ef00e6ecd9b0c5331f2e70d94c
mickey <mickey@openbsd.org>
2001-01-24 08:19:02 +0000

    alignment seems like a good idea; after art@alpha

--- lib/libc/arch/hppa/gen/infinity.c
+++ lib/libc/arch/hppa/gen/infinity.c
@@ -1,12 +1,13 @@
-/*	$OpenBSD: infinity.c,v 1.1 1999/09/14 00:21:15 mickey Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.2 2001/01/24 08:19:02 mickey Exp $	*/
 
 /* infinity.c */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: infinity.c,v 1.1 1999/09/14 00:21:15 mickey Exp $";
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.2 2001/01/24 08:19:02 mickey Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <math.h>
 
 /* bytes for +Infinity on a hppa */
-char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
+char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
+    { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
