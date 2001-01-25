bd04a432f893853f78ab2077ad84cd8c618a7b2f
deraadt <deraadt@openbsd.org>
2001-01-25 19:22:16 +0000

    protos

--- libexec/rexecd/rexecd.c
+++ libexec/rexecd/rexecd.c
@@ -39,7 +39,7 @@ char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)rexecd.c	5.12 (Berkeley) 2/25/91";*/
-static char rcsid[] = "$Id: rexecd.c,v 1.14 2000/09/15 07:13:46 deraadt Exp $";
+static char rcsid[] = "$Id: rexecd.c,v 1.15 2001/01/25 19:22:16 deraadt Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -57,6 +57,7 @@ static char rcsid[] = "$Id: rexecd.c,v 1.14 2000/09/15 07:13:46 deraadt Exp $";
 #include <string.h>
 #include <syslog.h>
 #include <paths.h>
+#include <err.h>
 #include <login_cap.h>
 
 #include <netinet/in.h>
