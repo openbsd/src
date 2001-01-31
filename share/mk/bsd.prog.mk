a6b15b4bd6bb2929d6a15ef225c7ab95a22f2ca5
art <art@openbsd.org>
2001-01-31 06:41:18 +0000

    ELF alpha has crtbegin and crtend.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.prog.mk,v 1.22 2000/12/12 00:46:49 provos Exp $
+#	$OpenBSD: bsd.prog.mk,v 1.23 2001/01/31 06:41:18 art Exp $
 #	$NetBSD: bsd.prog.mk,v 1.55 1996/04/08 21:19:26 jtc Exp $
 #	@(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
 
@@ -12,7 +12,7 @@
 
 CFLAGS+=	${COPTS}
 
-.if (${MACHINE_ARCH} == "powerpc")
+.if (${MACHINE_ARCH} == "powerpc") || (${MACHINE_ARCH} == "alpha")
 CRTBEGIN?=       ${DESTDIR}/usr/lib/crtbegin.o
 CRTEND?=         ${DESTDIR}/usr/lib/crtend.o
 .endif
