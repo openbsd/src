6f1c77899d8fe640ee099e309ccadd0f29f96674
deraadt <deraadt@openbsd.org>
2001-02-05 09:46:50 +0000

    please alpha

--- lib/libutil/login.c
+++ lib/libutil/login.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: login.c,v 1.5 1998/07/13 02:11:12 millert Exp $	*/
+/*	$OpenBSD: login.c,v 1.6 2001/02/05 09:46:50 deraadt Exp $	*/
 /*
  * Copyright (c) 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -34,7 +34,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /* from: static char sccsid[] = "@(#)login.c	8.1 (Berkeley) 6/4/93"; */
-static char *rcsid = "$Id: login.c,v 1.5 1998/07/13 02:11:12 millert Exp $";
+static char *rcsid = "$Id: login.c,v 1.6 2001/02/05 09:46:50 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -44,6 +44,7 @@ static char *rcsid = "$Id: login.c,v 1.5 1998/07/13 02:11:12 millert Exp $";
 #include <stdlib.h>
 #include <utmp.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "util.h"
 
