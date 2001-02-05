ee2641fd0c1fb7f0b04fcfe079459cea16ea4e63
deraadt <deraadt@openbsd.org>
2001-02-05 21:41:07 +0000

    default to 8 wide for user names

--- usr.bin/rusers/rusers.c
+++ usr.bin/rusers/rusers.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rusers.c,v 1.9 2001/01/31 19:48:06 deraadt Exp $	*/
+/*	$OpenBSD: rusers.c,v 1.10 2001/02/05 21:41:07 deraadt Exp $	*/
 
 /*-
  *  Copyright (c) 1993 John Brezak
@@ -29,7 +29,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: rusers.c,v 1.9 2001/01/31 19:48:06 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: rusers.c,v 1.10 2001/02/05 21:41:07 deraadt Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -165,7 +165,7 @@ rusers_reply(char *replyp, struct sockaddr_in *raddrp)
 				sizeof (local) - strlen (local) - 1);
 			local[sizeof (local) - 1] = 0;
 
-#define MAXNAME	16
+#define MAXNAME	8
 			printf("%-*.*s %-*.*s %-12.12s %8s %.18s\n",
 			    MAXNAME, UT_NAMESIZE,
 			    up->uia_arr[x]->ui_utmp.ut_name,
