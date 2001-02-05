0d566dc51eb1bf620d3125235429f991a185b407
matthieu <matthieu@openbsd.org>
2001-02-05 22:47:13 +0000

    when flags are FLG_WRONLY, say "write only" instead of "read only"

--- sbin/wsconsctl/wsconsctl.c
+++ sbin/wsconsctl/wsconsctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wsconsctl.c,v 1.2 2001/01/30 06:50:12 aaron Exp $	*/
+/*	$OpenBSD: wsconsctl.c,v 1.3 2001/02/05 22:47:13 matthieu Exp $	*/
 /*	$NetBSD: wsconsctl.c,v 1.2 1998/12/29 22:40:20 hannken Exp $ */
 
 /*-
@@ -212,7 +212,7 @@ main(argc, argv)
 			for (i = 0; i < argc; i++) {
 				f = field_by_name(argv[i]);
 				if ((f->flags & FLG_WRONLY) != 0)
-					errx(1, "%s: read only", argv[i]);
+					errx(1, "%s: write only", argv[i]);
 				f->flags |= FLG_GET;
 			}
 			(*getval)(fd);
