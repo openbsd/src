93dd0a1c60baafd848aeebee379671fb4bb82f59
drahn <drahn@openbsd.org>
2001-02-07 06:03:53 +0000

    Some devices use NPBG others want PAGE_SIZE, and the related macros. so provide both.

--- sys/arch/powerpc/include/param.h
+++ sys/arch/powerpc/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.12 2001/01/24 21:17:25 drahn Exp $	*/
+/*	$OpenBSD: param.h,v 1.13 2001/02/07 06:03:53 drahn Exp $	*/
 /*	$NetBSD: param.h,v 1.1 1996/09/30 16:34:28 ws Exp $	*/
 
 /*-
@@ -54,6 +54,9 @@
 #define	PGSHIFT		12
 #define	NBPG		4096
 #define	PGOFSET		(NBPG - 1)
+#define PAGE_SIZE	NBPG
+#define PAGE_MASK	PGOFSET
+#define PAGE_SHIFT	PGSHIFT
 
 #define	DEV_BSHIFT	9		/* log2(DEV_BSIZE) */
 #define	DEV_BSIZE	(1 << DEV_BSHIFT)
