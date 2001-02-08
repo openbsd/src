4d88166c38cf034c669a96646c225b334b90f3cb
markus <markus@openbsd.org>
2001-02-08 18:15:22 +0000

    update to 2.3.2

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.16 2001/01/08 22:29:05 markus Exp $ */
+/* $OpenBSD: version.h,v 1.17 2001/02/08 18:15:22 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_2.3.1"
+#define SSH_VERSION	"OpenSSH_2.3.2"
