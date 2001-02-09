8b9659ff36b9fb4bdfb83c805876723ea1195879
itojun <itojun@openbsd.org>
2001-02-09 09:04:59 +0000

    do not assume malloc() returns zero-filled region.  found by malloc.conf=AJ.

--- usr.bin/ssh/ssh-keyscan.c
+++ usr.bin/ssh/ssh-keyscan.c
@@ -8,7 +8,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-keyscan.c,v 1.14 2001/02/07 22:43:16 markus Exp $");
+RCSID("$OpenBSD: ssh-keyscan.c,v 1.15 2001/02/09 09:04:59 itojun Exp $");
 
 #include <sys/queue.h>
 #include <errno.h>
@@ -578,6 +578,7 @@ main(int argc, char **argv)
 	if (maxfd > fdlim_get(0))
 		fdlim_set(maxfd);
 	fdcon = xmalloc(maxfd * sizeof(con));
+	memset(fdcon, 0, maxfd * sizeof(con));
 
 	do {
 		while (ncon < maxcon) {
