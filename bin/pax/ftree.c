d0cc949de4a93416c2a9bad2d2e13db9bb2b3959
millert <millert@openbsd.org>
2001-02-09 17:14:43 +0000

    no need to revert a change that just fixed a typo

--- bin/pax/ftree.c
+++ bin/pax/ftree.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ftree.c,v 1.11 2001/02/09 14:04:33 espie Exp $	*/
+/*	$OpenBSD: ftree.c,v 1.12 2001/02/09 17:14:43 millert Exp $	*/
 /*	$NetBSD: ftree.c,v 1.4 1995/03/21 09:07:21 cgd Exp $	*/
 
 /*-
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)ftree.c	8.2 (Berkeley) 4/18/94";
 #else
-static char rcsid[] = "$OpenBSD: ftree.c,v 1.11 2001/02/09 14:04:33 espie Exp $";
+static char rcsid[] = "$OpenBSD: ftree.c,v 1.12 2001/02/09 17:14:43 millert Exp $";
 #endif
 #endif /* not lint */
 
@@ -171,7 +171,7 @@ ftree_add(str, chflg)
 	 * simple check for bad args
 	 */
 	if ((str == NULL) || (*str == '\0')) {
-		paxwarn(0, "Invalid file name arguement");
+		paxwarn(0, "Invalid file name argument");
 		return(-1);
 	}
 
