913a587d9dcc8192061ef387a553c335982ad25f
art <art@openbsd.org>
2001-02-19 18:26:27 +0000

    Zap some unnecessary defines.

--- sys/arch/amiga/dev/aucc.c
+++ sys/arch/amiga/dev/aucc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aucc.c,v 1.5 1999/01/02 00:02:49 niklas Exp $	*/
+/*	$OpenBSD: aucc.c,v 1.6 2001/02/19 18:26:27 art Exp $	*/
 /*	$NetBSD: aucc.c,v 1.22 1998/01/12 10:39:10 thorpej Exp $	*/
 
 /*
@@ -69,12 +69,6 @@ int     auccdebug = 1;
 #else
 #define DPRINTF(x)
 #endif
-
-#ifdef splaudio
-#undef splaudio
-#endif
-
-#define splaudio() spl4();
 
 /* clock frequency.. */
 extern int eclockfreq; 
