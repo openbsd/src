cec23f9a6ecddb94f82e90d34bfbcd371b605a93
art <art@openbsd.org>
2001-02-26 15:44:46 +0000

    Initialize the simple lock properly.

--- sys/dev/pci/cy82c693.c
+++ sys/dev/pci/cy82c693.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cy82c693.c,v 1.2 2001/01/16 15:37:53 art Exp $	*/
+/*	$OpenBSD: cy82c693.c,v 1.3 2001/02/26 15:44:46 art Exp $	*/
 /* $NetBSD: cy82c693.c,v 1.1 2000/06/06 03:07:39 thorpej Exp $ */
 
 /*-
@@ -60,9 +60,7 @@
 static struct cy82c693_handle cyhc_handle;
 static int cyhc_initialized;
 
-#if 0 /* SMP */
-static struct simplelock cyhc_slock = SIMPLELOCK_INITIALIZER;
-#endif
+static struct simplelock cyhc_slock = SLOCK_INITIALIZER;
 
 #define	CYHC_LOCK(s)							\
 do {									\
