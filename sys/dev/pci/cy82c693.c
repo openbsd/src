bfd292b4fbdcb0e7cf644c1de10c07a1c11ec1a4
art <art@openbsd.org>
2001-02-26 16:21:46 +0000

    Sigh. unstatic the simplelock to make this compile without LOCKDEBUG.

--- sys/dev/pci/cy82c693.c
+++ sys/dev/pci/cy82c693.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cy82c693.c,v 1.3 2001/02/26 15:44:46 art Exp $	*/
+/*	$OpenBSD: cy82c693.c,v 1.4 2001/02/26 16:21:46 art Exp $	*/
 /* $NetBSD: cy82c693.c,v 1.1 2000/06/06 03:07:39 thorpej Exp $ */
 
 /*-
@@ -60,7 +60,7 @@
 static struct cy82c693_handle cyhc_handle;
 static int cyhc_initialized;
 
-static struct simplelock cyhc_slock = SLOCK_INITIALIZER;
+struct simplelock cyhc_slock = SLOCK_INITIALIZER;
 
 #define	CYHC_LOCK(s)							\
 do {									\
