98a8a4fa526c16e453d5a4858cd80ce7cf93407e
deraadt <deraadt@openbsd.org>
2001-03-03 21:29:23 +0000

    use AC97_HOST_DONT_READANY since some machines are unreliable

--- sys/dev/pci/neo.c
+++ sys/dev/pci/neo.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: neo.c,v 1.4 2000/07/19 09:04:37 csapuntz Exp $       */
+/*      $OpenBSD: neo.c,v 1.5 2001/03/03 21:29:23 deraadt Exp $       */
 
 /*
  * Copyright (c) 1999 Cameron Grant <gandalf@vilnya.demon.co.uk>
@@ -724,7 +724,7 @@ enum ac97_host_flags
 neo_flags_codec(sc)
 	void *sc;
 {
-	return (AC97_HOST_DONT_READ);
+	return (AC97_HOST_DONT_READANY);
 }
 
 int
