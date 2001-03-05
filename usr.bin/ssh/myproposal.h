33375d9124938a85d7decd47d88cacb5512a9cbb
deraadt <deraadt@openbsd.org>
2001-03-05 15:56:16 +0000

    switch to aes128-cbc/hmac-md5 by default in SSH2 -- faster; provos & markus ok

--- usr.bin/ssh/myproposal.h
+++ usr.bin/ssh/myproposal.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: myproposal.h,v 1.11 2001/02/11 12:59:24 markus Exp $	*/
+/*	$OpenBSD: myproposal.h,v 1.12 2001/03/05 15:56:16 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2000 Markus Friedl.  All rights reserved.
@@ -26,12 +26,12 @@
 #define KEX_DEFAULT_KEX		"diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1"
 #define	KEX_DEFAULT_PK_ALG	"ssh-rsa,ssh-dss"
 #define	KEX_DEFAULT_ENCRYPT \
-	"3des-cbc,blowfish-cbc,cast128-cbc,arcfour," \
-	"aes128-cbc,aes192-cbc,aes256-cbc," \
+	"aes128-cbc,3des-cbc,blowfish-cbc,cast128-cbc,arcfour," \
+	"aes192-cbc,aes256-cbc," \
 	"rijndael128-cbc,rijndael192-cbc,rijndael256-cbc," \
 	"rijndael-cbc@lysator.liu.se"
 #define	KEX_DEFAULT_MAC \
-	"hmac-sha1,hmac-md5,hmac-ripemd160," \
+	"hmac-md5,hmac-sha1,hmac-ripemd160," \
 	"hmac-ripemd160@openssh.com," \
 	"hmac-sha1-96,hmac-md5-96"
 #define	KEX_DEFAULT_COMP	"none,zlib"
