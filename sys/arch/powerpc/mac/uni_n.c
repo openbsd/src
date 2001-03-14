2f11f28d9cc354b30c6f204aca3e363ab5e9ca28
deraadt <deraadt@openbsd.org>
2001-03-14 03:24:54 +0000

    KNF

--- sys/arch/powerpc/mac/uni_n.c
+++ sys/arch/powerpc/mac/uni_n.c
@@ -1,7 +1,34 @@
-/*	$OpenBSD: uni_n.c,v 1.1 2000/10/16 00:18:01 drahn Exp $	*/
+/*	$OpenBSD: uni_n.c,v 1.2 2001/03/14 03:24:54 deraadt Exp $	*/
 
-
-/* put BSD copyright here */
+/*
+ * Copyright (c) 1998 Dale Rahn. All rights reserved.
+ *
+ *   
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Dale Rahn.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */  
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -16,12 +43,12 @@ uni_n_config(int handle)
 	char *baseaddr;
 	int *ctladdr;
 	u_int32_t address;
+
 	if (OF_getprop(handle, "name", name, sizeof name) > 0) {
 		/* sanity test */
-		if (!strcmp (name, "uni-n")) { 
+		if (!strcmp (name, "uni-n")) {
 			if (OF_getprop(handle, "reg", &address,
-					sizeof address) > 0)
-			{
+			    sizeof address) > 0) {
 				printf("found uni-n at address %x\n", address);
 				baseaddr = mapiodev(address, NBPG);
 				ctladdr = (void*)(baseaddr + 0x20);
@@ -29,5 +56,4 @@ uni_n_config(int handle)
 			}
 		}
 	}
-	return;
 }
