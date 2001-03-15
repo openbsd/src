99e04d9376178773f5d7db5acb7a028ff7a050e1
mjacob <mjacob@openbsd.org>
2001-03-15 19:03:33 +0000

    Add in missing prototype for comspeedcomspeed.

--- sys/dev/ic/comvar.h
+++ sys/dev/ic/comvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: comvar.h,v 1.21 2001/03/15 18:47:53 art Exp $	*/
+/*	$OpenBSD: comvar.h,v 1.22 2001/03/15 19:03:33 mjacob Exp $	*/
 /*	$NetBSD: comvar.h,v 1.5 1996/05/05 19:50:47 christos Exp $	*/
 
 /*
@@ -135,6 +135,7 @@ struct com_softc {
 	int enabled;
 };
 
+int	comspeedcomspeed __P((long, long));
 int	comprobe1 __P((bus_space_tag_t, bus_space_handle_t));
 void	cominit __P((bus_space_tag_t, bus_space_handle_t, int));
 int	comstop __P((struct tty *, int));
