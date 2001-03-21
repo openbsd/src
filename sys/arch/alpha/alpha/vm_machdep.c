a2e925cffa1bea48c01442c4caf1ba7c90e36375
art <art@openbsd.org>
2001-03-21 10:15:05 +0000

    optimize a sanity check and put it behind ifdef DIAGNOSTIC.

--- sys/arch/alpha/alpha/vm_machdep.c
+++ sys/arch/alpha/alpha/vm_machdep.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: vm_machdep.c,v 1.18 2001/01/20 20:27:40 art Exp $ */
+/* $OpenBSD: vm_machdep.c,v 1.19 2001/03/21 10:15:05 art Exp $ */
 /* $NetBSD: vm_machdep.c,v 1.55 2000/03/29 03:49:48 simonb Exp $ */
 
 /*
@@ -325,8 +325,10 @@ pagemove(from, to, size)
 	long fidx, tidx;
 	ssize_t todo;
 
-	if (size % NBPG)
+#ifdef DIAGNOSTIC
+	if ((size & PAGE_MASK) != 0)
 		panic("pagemove");
+#endif
 
 	todo = size;			/* if testing > 0, need sign... */
 	while (todo > 0) {
