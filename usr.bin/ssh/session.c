8d75e1d5f120b715db9d60eab4353d329909bf4b
markus <markus@openbsd.org>
2001-03-21 21:06:30 +0000

    missing init; from mib@unimelb.edu.au

--- usr.bin/ssh/session.c
+++ usr.bin/ssh/session.c
@@ -33,7 +33,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: session.c,v 1.65 2001/03/21 11:43:44 markus Exp $");
+RCSID("$OpenBSD: session.c,v 1.66 2001/03/21 21:06:30 markus Exp $");
 
 #include "ssh.h"
 #include "ssh1.h"
@@ -1182,6 +1182,7 @@ session_new(void)
 			s->chanid = -1;
 			s->ptyfd = -1;
 			s->ttyfd = -1;
+			s->tty[0] = '\0';                                                       
 			s->term = NULL;
 			s->pw = NULL;
 			s->display = NULL;
