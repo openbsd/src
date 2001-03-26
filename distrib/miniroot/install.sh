70294e1d75a9cdcaf87380e2ec5db0ececb8881b
todd <todd@openbsd.org>
2001-03-26 14:46:55 +0000

    spelling

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.74 2000/06/16 03:19:42 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.75 2001/03/26 14:46:55 todd Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997,1998 Todd Miller, Theo de Raadt
@@ -70,7 +70,7 @@
 #	In a perfect world, this would be a nice C program, with a reasonable
 #	user interface.
 
-FILESYSTEMS="/tmp/filesystems"		# used thoughout
+FILESYSTEMS="/tmp/filesystems"		# used throughout
 FQDN=					# domain name
 
 trap "umount /tmp > /dev/null 2>&1" 0
@@ -329,7 +329,7 @@ if [ "`df /`" = "`df /mnt`" ]; then
 		done
 	) < ${FILESYSTEMS} > /tmp/fstab
 
-# XXX We no longer do the following. It is not neccessary. It can be done
+# XXX We no longer do the following. It is not necessary. It can be done
 # XXX after the install is complete.
 #
 #	echo	"The fstab is configured as follows:"
