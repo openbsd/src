60758426af5d3daa2451bb85a4bd31a1d517e3ce
deraadt <deraadt@openbsd.org>
2001-03-28 18:10:54 +0000

    new SIIG 8-port; apriori@world.std.com

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pucdata.c,v 1.9 2001/03/15 17:52:20 deraadt Exp $	*/
+/*	$OpenBSD: pucdata.c,v 1.10 2001/03/28 18:10:54 deraadt Exp $	*/
 /*	$NetBSD: pucdata.c,v 1.6 1999/07/03 05:55:23 cgd Exp $	*/
 
 /*
@@ -494,6 +494,21 @@ const struct puc_device_description puc_devices[] = {
 	    },
 	},
 
+	/* SIIG Cyber 8S PCI 16C850 (20x family): 8S */
+	{   /* "SIIG Cyber 8S PCI 16C850 (20x family)", */
+	    {	PCI_VENDOR_SIIG, PCI_PRODUCT_SIIG_2081,	0,	0	},
+	    {	0xffff, 0xffff,				0,	0	},
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x1c, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x20, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x20, 0x08, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x20, 0x10, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x20, 0x18, COM_FREQ },
+	    },
+	},
 
 	/*
 	 * VScom PCI-800, as sold on http://www.swann.com.au/isp/titan.html.
