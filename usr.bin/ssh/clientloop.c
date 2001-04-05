200bc02ad3dacc4bc615cb6bccf4e7e4f592f671
markus <markus@openbsd.org>
2001-04-05 20:01:10 +0000

    for ~R print message if server does not support rekeying. (and fix ~R).

--- usr.bin/ssh/clientloop.c
+++ usr.bin/ssh/clientloop.c
@@ -59,7 +59,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: clientloop.c,v 1.58 2001/04/05 11:09:15 markus Exp $");
+RCSID("$OpenBSD: clientloop.c,v 1.59 2001/04/05 20:01:10 markus Exp $");
 
 #include "ssh.h"
 #include "ssh1.h"
@@ -554,8 +554,12 @@ process_escapes(Buffer *bin, Buffer *bout, Buffer *berr, char *buf, int len)
 				continue;
 
 			case 'R':
-				if (compat20 && !(datafellows && SSH_BUG_NOREKEY))
-					need_rekeying = 1;
+				if (compat20) {
+					if (datafellows & SSH_BUG_NOREKEY)
+						log("Server does not support re-keying");
+					else
+						need_rekeying = 1;
+				}
 				continue;
 
 			case '&':
