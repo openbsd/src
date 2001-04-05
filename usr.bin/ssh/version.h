f74be0f1defef67734cf8e084155916ebbf886f4
markus <markus@openbsd.org>
2001-04-05 10:39:48 +0000

    temporary version 2.5.4 (supports rekeying). this is not an official release.

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.21 2001/03/27 17:46:50 provos Exp $ */
+/* $OpenBSD: version.h,v 1.22 2001/04/05 10:39:48 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_2.5.3"
+#define SSH_VERSION	"OpenSSH_2.5.4"
