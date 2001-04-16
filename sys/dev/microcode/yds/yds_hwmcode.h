67b902ea4fc884bd177deb223a0394b20c839553
deraadt <deraadt@openbsd.org>
2001-04-16 08:16:40 +0000

    fix header

--- sys/dev/microcode/yds/yds_hwmcode.h
+++ sys/dev/microcode/yds/yds_hwmcode.h
@@ -1,5 +1,4 @@
-/*	$OpenBSD */
-/*  $NetBSD$    */
+/*	$OpenBSD: yds_hwmcode.h,v 1.3 2001/04/16 08:16:40 deraadt Exp $	*/
 
 /*=============================================================================
  Copyright (c) 1997-1999  Yamaha Corporation. All Rights Reserved.
