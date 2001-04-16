5cc688c698e650f2a65d41c0d2ba1e90c74d411a
deraadt <deraadt@openbsd.org>
2001-04-16 08:05:34 +0000

    xrealloc dealing with ptr == nULL; mouring

--- usr.bin/ssh/xmalloc.c
+++ usr.bin/ssh/xmalloc.c
@@ -13,7 +13,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: xmalloc.c,v 1.14 2001/02/07 18:04:50 itojun Exp $");
+RCSID("$OpenBSD: xmalloc.c,v 1.15 2001/04/16 08:05:34 deraadt Exp $");
 
 #include "xmalloc.h"
 #include "log.h"
@@ -39,8 +39,9 @@ xrealloc(void *ptr, size_t new_size)
 	if (new_size == 0)
 		fatal("xrealloc: zero size");
 	if (ptr == NULL)
-		fatal("xrealloc: NULL pointer given as argument");
-	new_ptr = realloc(ptr, new_size);
+		new_ptr = malloc(new_size);
+	else
+		new_ptr = realloc(ptr, new_size);
 	if (new_ptr == NULL)
 		fatal("xrealloc: out of memory (new_size %lu bytes)", (u_long) new_size);
 	return new_ptr;
