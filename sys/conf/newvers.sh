806cb0bcbcdcbecdfe98af348b5e14027012d24a
deraadt <deraadt@openbsd.org>
2001-04-19 23:33:11 +0000

    move away from -beta

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.30 2001/04/15 02:35:09 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.31 2001/04/19 23:33:11 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -70,7 +70,7 @@ char osrelease[] = "${osr}";
 char osversion[] = "${id}#${v}";
 char sccs[8] = { ' ', ' ', ' ', ' ', '@', '(', '#', ')' };
 char version[] =
-    "${ost} ${osr}-beta (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
+    "${ost} ${osr} (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
 eof
 
 expr ${v} + 1 > version
