0db957ea4b50977e63ac73c531368d2666d606d4
markus <markus@openbsd.org>
2001-04-23 07:46:00 +0000

    import DSA changes from 0.9.6a (Bleichenbacher attack), ok provos@/deraadt@

--- lib/libcrypto/dsa/dsa_key.c
+++ lib/libcrypto/dsa/dsa_key.c
@@ -65,10 +65,11 @@
 #include <openssl/dsa.h>
 #include <openssl/rand.h>
 
+extern int	__BN_rand_range(BIGNUM *r, BIGNUM *range);
+
 int DSA_generate_key(DSA *dsa)
 	{
 	int ok=0;
-	unsigned int i;
 	BN_CTX *ctx=NULL;
 	BIGNUM *pub_key=NULL,*priv_key=NULL;
 
@@ -81,15 +82,9 @@ int DSA_generate_key(DSA *dsa)
 	else
 		priv_key=dsa->priv_key;
 
-	i=BN_num_bits(dsa->q);
-	for (;;)
-		{
-		if (!BN_rand(priv_key,i,0,0))
-			goto err;
-		if (BN_cmp(priv_key,dsa->q) >= 0)
-			BN_sub(priv_key,priv_key,dsa->q);
-		if (!BN_is_zero(priv_key)) break;
-		}
+	do
+		if (!__BN_rand_range(priv_key,dsa->q)) goto err;
+	while (BN_is_zero(priv_key));
 
 	if (dsa->pub_key == NULL)
 		{
