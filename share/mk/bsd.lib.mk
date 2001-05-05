b7d0ae7009438355d49d14ede0b9dd1ac4d0ffba
espie <espie@openbsd.org>
2001-05-05 17:44:49 +0000

    Synch with bsd.prog.mk. The only client won't see the difference.

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.lib.mk,v 1.28 2001/04/03 23:00:09 espie Exp $
+#	$OpenBSD: bsd.lib.mk,v 1.29 2001/05/05 17:44:49 espie Exp $
 #	$NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 #	@(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
 
@@ -268,9 +268,9 @@ realinstall:
 .if defined(LINKS) && !empty(LINKS)
 	@set ${LINKS}; \
 	while test $$# -ge 2; do \
-		l=${DESTDIR}${BINDIR}/$$1; \
+		l=${DESTDIR}$$1; \
 		shift; \
-		t=${DESTDIR}${BINDIR}/$$1; \
+		t=${DESTDIR}$$1; \
 		shift; \
 		echo $$t -\> $$l; \
 		rm -f $$t; \
