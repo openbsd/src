9be0f2b2936d9b771dbc8d2d6b5294b15fce6e97
millert <millert@openbsd.org>
2001-05-10 21:59:39 +0000

    Nuke the DCA check.  ACPI can only be console on a 425e; thorpej@netbsd.org

--- sys/arch/hp300/stand/common/apci.c
+++ sys/arch/hp300/stand/common/apci.c
@@ -1,8 +1,43 @@
-/*	$OpenBSD: apci.c,v 1.2 1998/05/10 11:31:17 downsj Exp $	*/
-/*	$NetBSD: apci.c,v 1.1 1997/05/12 07:41:55 thorpej Exp $	*/
+/*	$OpenBSD: apci.c,v 1.3 2001/05/10 21:59:39 millert Exp $	*/
+/*	$NetBSD: apci.c,v 1.2 1997/10/04 17:20:15 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1997, 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
- * Copyright (c) 1997 Jason R. Thorpe.  All rights reserved.
  * Copyright (c) 1988 University of Utah.
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -61,30 +96,15 @@ apciprobe(cp)
 {
 	struct apciregs *apci = apcicnaddr =
 	    (struct apciregs *)IIOV(FRODO_BASE + FRODO_APCI_OFFSET(1));
-	struct dcadevice *dca = (struct dcadevice *)sctoaddr(9);
 
 	cp->cn_pri = CN_DEAD;
 
-	/* Only 400-series machines can have this. */
-	switch (machineid) {
-	case HP_400:
-	case HP_425:
-	case HP_433:
-		break;
-	default:
+	/*
+	 * Only a 425e can have an APCI console.  On all other 4xx models,
+	 * the "first" serial port is mapped to the DCA at select code 9.
+	 */
+	if (machineid != HP_425 || mmuid != MMUID_425_E)
 		return;
-	}
-
-	/* Make sure there's not a DCA in the way. */
-	if (badaddr((caddr_t)dca) == 0) {
-		switch (dca->dca_id) {
-		case DCAID0:
-		case DCAID1:
-		case DCAREMID0:
-		case DCAREMID1:
-			return;
-		}
-	}
 
 #ifdef FORCEAPCICONSOLE
 	cp->cn_pri = CN_REMOTE;
