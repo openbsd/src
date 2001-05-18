d95c0f325a3b9b2a6f2ed7f4982ce6e1509e2aa3
mickey <mickey@openbsd.org>
2001-05-18 17:09:09 +0000

    stupidous debug was left enabled

--- sys/dev/ic/twe.c
+++ sys/dev/ic/twe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: twe.c,v 1.10 2001/05/10 20:07:35 mickey Exp $	*/
+/*	$OpenBSD: twe.c,v 1.11 2001/05/18 17:09:09 mickey Exp $	*/
 
 /*
  * Copyright (c) 2000, 2001 Michael Shalayeff.  All rights reserved.
@@ -33,7 +33,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define	TWE_DEBUG
+/* #define	TWE_DEBUG */
 
 #include <sys/param.h>
 #include <sys/systm.h>
