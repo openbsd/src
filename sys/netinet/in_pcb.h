646e5c6b1c202627468e3d09d93f4e708aaab72f
angelos <angelos@openbsd.org>
2001-05-27 03:53:09 +0000

    Keep local authentication material on the PCB.

--- sys/netinet/in_pcb.h
+++ sys/netinet/in_pcb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_pcb.h,v 1.34 2001/05/21 03:02:18 angelos Exp $	*/
+/*	$OpenBSD: in_pcb.h,v 1.35 2001/05/27 03:53:09 angelos Exp $	*/
 /*	$NetBSD: in_pcb.h,v 1.14 1996/02/13 23:42:00 christos Exp $	*/
 
 /*
@@ -137,6 +137,7 @@ struct inpcb {
 	struct ipsec_ref *inp_ipsec_remoteid;
 	struct ipsec_ref *inp_ipsec_localcred;
 	struct ipsec_ref *inp_ipsec_remotecred;
+	struct ipsec_ref *inp_ipsec_auth;
 #define	inp_flowinfo	inp_hu.hu_ipv6.ip6_flow
 
 	int	in6p_cksum;
