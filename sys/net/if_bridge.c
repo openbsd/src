590bd3ad71f3bc6b5cd153f375885e9fe718ff4f
jasoni <jasoni@openbsd.org>
2001-06-06 22:55:03 +0000

    in bridge_filter(), pullup hlen, not sizeof(struct ip); ok jason@

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_bridge.c,v 1.57 2001/06/01 00:28:25 angelos Exp $	*/
+/*	$OpenBSD: if_bridge.c,v 1.58 2001/06/06 22:55:03 jasoni Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Jason L. Wright (jason@thought.net)
@@ -1927,7 +1927,7 @@ bridge_filter(sc, ifp, eh, m)
 	if (hlen < sizeof(struct ip))
 		goto dropit;
 	if (hlen > m->m_len) {
-		if ((m = m_pullup(m, sizeof(struct ip))) == NULL)
+		if ((m = m_pullup(m, hlen)) == NULL)
 			return (NULL);
 		ip = mtod(m, struct ip *);
 	}
