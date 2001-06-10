f46779c73ac408d1aa8dccfd717f122db116e4e7
markus <markus@openbsd.org>
2001-06-10 11:29:20 +0000

    we support rekeying protocol errors are fatal.

--- usr.bin/ssh/dispatch.c
+++ usr.bin/ssh/dispatch.c
@@ -22,7 +22,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "includes.h"
-RCSID("$OpenBSD: dispatch.c,v 1.10 2001/02/18 18:33:53 markus Exp $");
+RCSID("$OpenBSD: dispatch.c,v 1.11 2001/06/10 11:29:20 markus Exp $");
 
 #include "ssh1.h"
 #include "ssh2.h"
@@ -39,9 +39,7 @@ dispatch_fn *dispatch[DISPATCH_MAX];
 void
 dispatch_protocol_error(int type, int plen, void *ctxt)
 {
-	error("Hm, dispatch protocol error: type %d plen %d", type, plen);
-	if (compat20 && type == SSH2_MSG_KEXINIT)
-		fatal("dispatch_protocol_error: rekeying is not supported");
+	fatal("dispatch_protocol_error: type %d plen %d", type, plen);
 }
 void
 dispatch_init(dispatch_fn *dflt)
