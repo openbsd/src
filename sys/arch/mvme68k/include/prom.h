fc3fb441da0bc7df2f27a2a15c7dd890bc0d75fd
miod <miod@openbsd.org>
2001-06-12 22:53:16 +0000

    Change RB_NOSYM, for it used to have the same value as RB_CONFIG ; makes boot -c happier.

--- sys/arch/mvme68k/include/prom.h
+++ sys/arch/mvme68k/include/prom.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: prom.h,v 1.8 2001/01/15 19:50:38 deraadt Exp $ */
+/*	$OpenBSD: prom.h,v 1.9 2001/06/12 22:53:16 miod Exp $ */
 
 /*
  * Copyright (c) 1995 Theo de Raadt
@@ -187,6 +187,6 @@ struct mvmeprom_args {
 #define MVMEPROM_REG_ARGEND	"a6"
 
 #ifndef RB_NOSYM
-#define RB_NOSYM 0x400
+#define RB_NOSYM 0x4000
 #endif
 
