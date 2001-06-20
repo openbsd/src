818b7e4800275c4c5324cda7ce8ff3e4ca64faa1
niklas <niklas@openbsd.org>
2001-06-20 23:12:48 +0000

    circumvent a 32-bit integer arithmetic overflow

--- sbin/savecore/savecore.c
+++ sbin/savecore/savecore.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: savecore.c,v 1.24 2001/06/04 14:59:49 mickey Exp $	*/
+/*	$OpenBSD: savecore.c,v 1.25 2001/06/20 23:12:48 niklas Exp $	*/
 /*	$NetBSD: savecore.c,v 1.26 1996/03/18 21:16:05 leo Exp $	*/
 
 /*-
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)savecore.c	8.3 (Berkeley) 1/2/94";
 #else
-static char rcsid[] = "$OpenBSD: savecore.c,v 1.24 2001/06/04 14:59:49 mickey Exp $";
+static char rcsid[] = "$OpenBSD: savecore.c,v 1.25 2001/06/20 23:12:48 niklas Exp $";
 #endif
 #endif /* not lint */
 
@@ -620,7 +620,7 @@ check_space()
 		syslog(LOG_ERR, "%s: %m", dirn);
 		exit(1);
 	}
- 	spacefree = (fsbuf.f_bavail * fsbuf.f_bsize) / 1024;
+ 	spacefree = ((off_t)fsbuf.f_bavail * fsbuf.f_bsize) / 1024;
 
 	(void)snprintf(path, sizeof(path), "%s/minfree", dirn);
 	if ((fp = fopen(path, "r")) == NULL)
