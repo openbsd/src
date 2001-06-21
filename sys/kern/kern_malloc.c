db98b60b7e9b5af8e399f74df0bfcd6528826ad0
niklas <niklas@openbsd.org>
2001-06-21 14:27:13 +0000

    Panic if we free stuff not in malloc region. (ifdef DIAGNOSTIC)

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_malloc.c,v 1.31 2001/05/14 08:03:13 angelos Exp $	*/
+/*	$OpenBSD: kern_malloc.c,v 1.32 2001/06/21 14:27:13 niklas Exp $	*/
 /*	$NetBSD: kern_malloc.c,v 1.15.4.2 1996/06/13 17:10:56 cgd Exp $	*/
 
 /*
@@ -339,6 +339,12 @@ free(addr, type)
 #ifdef MALLOC_DEBUG
 	if (debug_free(addr, type))
 		return;
+#endif
+
+#ifdef DIAGNOSTIC
+	if (addr < (void *)kmembase || addr >= (void *)kmemlimit)
+		panic("free of non-malloced addr %p type %s", addr,
+		    memname[type]);
 #endif
 
 	kup = btokup(addr);
