551b0ecb682f4739f1a9e55cb0864764325debc4
angelos <angelos@openbsd.org>
2001-06-23 02:23:01 +0000

    Drop the "v4" for TCP/UDP checksums.

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mbuf.h,v 1.39 2001/06/23 02:01:08 angelos Exp $	*/
+/*	$OpenBSD: mbuf.h,v 1.40 2001/06/23 02:23:01 angelos Exp $	*/
 /*	$NetBSD: mbuf.h,v 1.19 1996/02/09 18:25:14 christos Exp $	*/
 
 /*
@@ -149,10 +149,10 @@ struct mbuf {
 #define	M_UDPV4_CSUM_OUT	0x0004	/* UDP checksum needed */
 #define	M_IPV4_CSUM_IN_OK	0x0008	/* IPv4 checksum verified */
 #define	M_IPV4_CSUM_IN_BAD	0x0010	/* IPv4 checksum bad */
-#define	M_TCPV4_CSUM_IN_OK	0x0020	/* TCP/IPv4 checksum verified */
-#define	M_TCPV4_CSUM_IN_BAD	0x0040	/* TCP/IPv4 checksum bad */
-#define	M_UDPV4_CSUM_IN_OK	0x0080	/* UDP/IPv4 checksum verified */
-#define	M_UDPV4_CSUM_IN_BAD	0x0100	/* UDP/IPv4 checksum bad */
+#define	M_TCP_CSUM_IN_OK	0x0020	/* TCP/IPv4 checksum verified */
+#define	M_TCP_CSUM_IN_BAD	0x0040	/* TCP/IPv4 checksum bad */
+#define	M_UDP_CSUM_IN_OK	0x0080	/* UDP/IPv4 checksum verified */
+#define	M_UDP_CSUM_IN_BAD	0x0100	/* UDP/IPv4 checksum bad */
 
 /* KAME IPv6 */
 #define M_ANYCAST6	0x4000	/* received as IPv6 anycast */
