4f3a77a506ec2cecd71749280c834c75fdf34159
art <art@openbsd.org>
2001-06-23 05:40:57 +0000

    Only mips uses cpu_set_init_frame (and it shouldn't)

--- sys/arch/m68k/m68k/process_machdep.c
+++ sys/arch/m68k/m68k/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: process_machdep.c,v 1.3 1997/01/19 03:14:12 briggs Exp $	*/
+/*	$OpenBSD: process_machdep.c,v 1.4 2001/06/23 05:40:57 art Exp $	*/
 /*	$NetBSD: process_machdep.c,v 1.17 1996/05/06 20:05:24 gwr Exp $	*/
 
 /*
@@ -63,9 +63,6 @@
 #include <machine/psl.h>
 #include <machine/reg.h>
 
-#ifdef cpu_set_init_frame
-extern char kstack[];		/* XXX */
-#endif
 static __inline struct frame   *process_frame __P((struct proc *p));
 static __inline struct fpframe *process_fpframe __P((struct proc *p));
 
@@ -75,11 +72,8 @@ process_frame(p)
 {
 	void *ptr;
 
-#ifdef cpu_set_init_frame
-	ptr = (char *)p->p_addr + ((char *)p->p_md.md_regs - (char *)kstack);
-#else
 	ptr = p->p_md.md_regs;
-#endif
+
 	return (ptr);
 }
 
