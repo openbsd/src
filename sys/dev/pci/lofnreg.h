a17d19ba576c1ea1b5d17c2101b4f8cd3d47c2e8
jason <jason@openbsd.org>
2001-06-25 23:31:58 +0000

    some reg defs

--- sys/dev/pci/lofnreg.h
+++ sys/dev/pci/lofnreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lofnreg.h,v 1.1 2001/06/25 23:04:46 jason Exp $	*/
+/*	$OpenBSD: lofnreg.h,v 1.2 2001/06/25 23:31:58 jason Exp $	*/
 
 /*
  * Copyright (c) 2001 Jason L. Wright (jason@thought.net)
@@ -32,3 +32,27 @@
  */
 
 #define	LOFN_BAR0	0x10
+
+#define	LOFN_WIN_0	0x0000
+#define	LOFN_WIN_1	0x2000
+#define	LOFN_WIN_2	0x4000
+#define	LOFN_WIN_3	0x6000
+
+/* Control and status registers, relative to window number */
+#define	LOFN_REL_CR		0x1fd4		/* Command */
+#define	LOFN_REL_SR		0x1fd8		/* Status */
+#define	LOFN_REL_IER		0x1fdc		/* Interrupt enable */
+#define	LOFN_REL_RNC		0x1fe0		/* RNG config */
+#define	LOFN_REL_CFG1		0x1fe4		/* Config1 */
+#define	LOFN_REL_CFG2		0x1fe8		/* Config2 */
+#define	LOFN_REL_CHIPID		0x1fec		/* Chip ID */
+
+#define	LOFN_CR_ADDR_MASK	0x0000003f	/* Instruction addr offset */
+
+#define	LOFN_SR_CARRY		0x00000008	/* Carry from operation */
+#define	LOFN_SR_RNG_UF		0x00001000	/* RNG underflow */
+#define	LOFN_SR_RNG_RDY		0x00004000	/* RNG ready */
+#define	LOFN_SR_DONE		0x00008000	/* Operation done */
+
+#define	LOFN_IER_RDY		0x00004000	/* RNG ready */
+#define	LOFN_IER_DONE		0x00008000	/* Operation done */
