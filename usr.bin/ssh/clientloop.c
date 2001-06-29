efa05494d3cc72411fd3eab8c7d9eb0072857b23
stevesk <stevesk@openbsd.org>
2001-06-29 18:38:44 +0000

    sync function definition with declaration; ok markus@

--- usr.bin/ssh/clientloop.c
+++ usr.bin/ssh/clientloop.c
@@ -59,7 +59,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: clientloop.c,v 1.78 2001/06/25 08:25:36 markus Exp $");
+RCSID("$OpenBSD: clientloop.c,v 1.79 2001/06/29 18:38:44 stevesk Exp $");
 
 #include "ssh.h"
 #include "ssh1.h"
@@ -1271,7 +1271,7 @@ client_init_dispatch_15(void)
 	dispatch_set(SSH_MSG_CHANNEL_CLOSE, &channel_input_ieof);
 	dispatch_set(SSH_MSG_CHANNEL_CLOSE_CONFIRMATION, & channel_input_oclose);
 }
-void
+static void
 client_init_dispatch(void)
 {
 	if (compat20)
