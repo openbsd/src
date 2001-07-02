1cf0523aa65d62ba89b70c1a9299ad8bdcadce50
ho <ho@openbsd.org>
2001-07-02 09:18:14 +0000

    Put the ISES firmware under dev/microcode/.

--- sys/dev/pci/ises.c
+++ sys/dev/pci/ises.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ises.c,v 1.12 2001/06/27 00:20:16 ho Exp $	*/
+/*	$OpenBSD: ises.c,v 1.13 2001/07/02 09:18:15 ho Exp $	*/
 
 /*
  * Copyright (c) 2000, 2001 Håkan Olsson (ho@crt.se)
@@ -56,6 +56,7 @@
 
 #include <dev/pci/isesreg.h>
 #include <dev/pci/isesvar.h>
+#include <dev/microcode/ises/ises_fw.h>
 
 /*
  * Prototypes and count for the pci_device structure
