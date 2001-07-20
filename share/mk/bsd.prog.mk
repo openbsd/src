b26c92e255322997a629f1d5b83ea6a6dc92fa8c
mickey <mickey@openbsd.org>
2001-07-20 19:15:39 +0000

    every change you make, every step you take, we'll be whatching you

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.prog.mk,v 1.25 2001/07/19 23:16:17 espie Exp $
+#	$OpenBSD: bsd.prog.mk,v 1.26 2001/07/20 19:15:39 mickey Exp $
 #	$NetBSD: bsd.prog.mk,v 1.55 1996/04/08 21:19:26 jtc Exp $
 #	@(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
 
@@ -142,8 +142,8 @@ realinstall:
 install: maninstall _SUBDIRUSE
 .if defined(LINKS) && !empty(LINKS)
 .  for lnk file in ${LINKS}
-	@l=${DESTDIR}${lnk}; \
-	 t=${DESTDIR}${file}; \
+	@l=${DESTDIR}${BINDIR}${lnk}; \
+	 t=${DESTDIR}${BINDIR}${file}; \
 	 echo $$t -\> $$l; \
 	 rm -f $$t; ln $$l $$t
 .  endfor
