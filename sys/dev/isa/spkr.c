77647c89f19ecfd1be5185c09589fcde80413882
pvalchev <pvalchev@openbsd.org>
2001-07-31 22:08:14 +0000

    License clarification from Eric S. Raymond. In an email sent to us, he authorized us to change it.  It is now BSD-licensed.

--- sys/dev/isa/spkr.c
+++ sys/dev/isa/spkr.c
@@ -1,5 +1,38 @@
-/*	$OpenBSD: spkr.c,v 1.5 2001/04/17 04:30:50 aaron Exp $	*/
+/*	$OpenBSD: spkr.c,v 1.6 2001/07/31 22:08:14 pvalchev Exp $	*/
 /*	$NetBSD: spkr.c,v 1.1 1998/04/15 20:26:18 drochner Exp $	*/
+
+/*
+ * Copyright (c) 1990 Eric S. Raymond (esr@snark.thyrsus.com)
+ * Copyright (c) 1990 Andrew A. Chernov (ache@astral.msk.su)
+ * Copyright (c) 1990 Lennart Augustsson (lennart@augustsson.net)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Eric S. Raymond
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * spkr.c -- device driver for console speaker on 80386
