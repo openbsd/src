02ae7ef263e7cf46a72193aa054bd9761a01b994
art <art@openbsd.org>
2001-08-18 18:50:59 +0000

    I am sick and tired of bug reports LACKING INFORMATION.

--- sys/ddb/db_trap.c
+++ sys/ddb/db_trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_trap.c,v 1.6 2001/04/05 20:05:29 art Exp $	*/
+/*	$OpenBSD: db_trap.c,v 1.7 2001/08/18 18:50:59 art Exp $	*/
 /*	$NetBSD: db_trap.c,v 1.9 1996/02/05 01:57:18 christos Exp $	*/
 
 /* 
@@ -76,8 +76,13 @@ db_trap(type, code)
 		if (panicstr != NULL) {
 			if (db_print_position() != 0)
 				db_printf("\n");
-			db_printf("Run at least 'trace' and 'ps' and include "
-			    "the output when reporting this panic.\n");
+			db_printf("\n\n**********************************\n");
+			db_printf("RUN AT LEAST 'trace' AND 'ps' AND INCLUDE "
+			    "THE OUTPUT WHEN REPORTING THIS PANIC!\n");
+			db_printf("DO NOT EVEN BOTHER REPORTING THIS WITHOUT "
+			    "INCLUDING THAT INFORMATION!\n");
+			db_printf("DON'T WASTE OUR TIME!\n");
+			db_printf("**********************************\n\n");
 		}
 
 		db_command_loop();
