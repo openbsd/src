3a5d05b9c5b800a5313d8f149f71ae22cc1e7dc0
drahn <drahn@openbsd.org>
2001-08-18 05:39:03 +0000

    make this compile __with__ akdb in the kernel configuration. Without this, it would not configure on systems with akbd.

--- sys/arch/powerpc/powerpc/ofw_machdep.c
+++ sys/arch/powerpc/powerpc/ofw_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ofw_machdep.c,v 1.24 2001/08/11 16:18:42 matthieu Exp $	*/
+/*	$OpenBSD: ofw_machdep.c,v 1.25 2001/08/18 05:39:03 drahn Exp $	*/
 /*	$NetBSD: ofw_machdep.c,v 1.1 1996/09/30 16:34:50 ws Exp $	*/
 
 /*
@@ -52,6 +52,7 @@
 #include <dev/ofw/openfirm.h>
 
 #include <ukbd.h>
+#include <akbd.h>
 #include <dev/usb/ukbdvar.h>
 #include <powerpc/mac/akbdvar.h>
 
