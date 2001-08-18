d516ffeb95e8c89fb1374f22b4c51ca3574771d9
jason <jason@openbsd.org>
2001-08-18 16:26:37 +0000

    oops

--- sys/arch/sparc64/stand/ofwboot/vers.c
+++
@@ -1,6 +0,0 @@
-char ostype[] = "OpenBSD";
-char osrelease[] = "2.9";
-char osversion[] = "ofwboot#0";
-char sccs[8] = { ' ', ' ', ' ', ' ', '@', '(', '#', ')' };
-char version[] =
-    "OpenBSD 2.9-current (ofwboot) #0: Fri Aug 17 23:15:34 PDT 2001\n    root@u5:/usr/openbsd/src/sys/arch/sparc64/stand/ofwboot\n";
