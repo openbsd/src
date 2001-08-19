9d5067b7b8a2f7db9efec8a39929652ce1a12b62
art <art@openbsd.org>
2001-08-19 20:19:03 +0000

    _NLIST_DO_ELF and _KERN_DO_ELF are needed too. This really needs a cleanup.

--- sys/arch/sparc64/include/exec.h
+++ sys/arch/sparc64/include/exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.3 2001/08/19 20:13:52 art Exp $	*/
+/*	$OpenBSD: exec.h,v 1.4 2001/08/19 20:19:03 art Exp $	*/
 /*	$NetBSD: elf_machdep.h,v 1.7 2001/02/11 00:18:49 eeh Exp $	*/
 
 #define ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
@@ -14,6 +14,8 @@
 			break;
 
 #define NATIVE_EXEC_ELF
+#define _KERN_DO_ELF
+#define _NLIST_DO_ELF
 
 #ifdef __arch64__
 #define ARCH_ELFSIZE		64	/* MD native binary size */
