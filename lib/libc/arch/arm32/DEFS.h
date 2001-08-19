2a0f4b44e93c4f0d6fd2588b657f828bc1430284
brad <brad@openbsd.org>
2001-08-19 20:44:31 +0000

    remove unused bits and pieces. -- Ok'd by art@

--- lib/libc/arch/arm32/DEFS.h
+++
@@ -1,3 +0,0 @@
-/* $OpenBSD: DEFS.h,v 1.2 1996/08/19 08:11:23 tholo Exp $ */
-
-#include <machine/asm.h>
--- lib/libc/arch/ns32k/gen/__main.c
+++
@@ -1,5 +0,0 @@
-#if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: __main.c,v 1.2 1996/08/19 08:16:36 tholo Exp $";
-#endif /* LIBC_SCCS and not lint */
-
-__main(){}
