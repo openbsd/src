a22aaefd68fd79e64078a10e2a1b2a908c9b8881
itojun <itojun@openbsd.org>
2001-08-19 01:51:34 +0000

    fix buffer underrun on 1.51

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.51 2001/08/18 20:39:43 deraadt Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.52 2001/08/19 01:51:34 itojun Exp $	*/
 /*      $NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $      */
 
 /*
@@ -81,7 +81,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)ifconfig.c	8.2 (Berkeley) 2/16/94";
 #else
-static char rcsid[] = "$OpenBSD: ifconfig.c,v 1.51 2001/08/18 20:39:43 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: ifconfig.c,v 1.52 2001/08/19 01:51:34 itojun Exp $";
 #endif
 #endif /* not lint */
 
@@ -2385,21 +2385,21 @@ sec2str(total)
 			n = snprintf(p, end - p, "%dd", days);
 			if (n < 0 || n >= end - p)
 				return(result);
-			p -= n;
+			p += n;
 		}
 		if (!first || hours) {
 			first = 0;
 			n = snprintf(p, end - p, "%dh", hours);
 			if (n < 0 || n >= end - p)
 				return(result);
-			p -= n;
+			p += n;
 		}
 		if (!first || mins) {
 			first = 0;
 			n = snprintf(p, end - p, "%dm", mins);
 			if (n < 0 || n >= end - p)
 				return(result);
-			p -= n;
+			p += n;
 		}
 		snprintf(p, end - p, "%ds", secs);
 	} else
