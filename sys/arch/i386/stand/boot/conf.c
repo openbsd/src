6ec54ae991adc1c05248be8c559dfdf7abac3e3e
mickey <mickey@openbsd.org>
2001-08-20 20:44:49 +0000

    bump the version for new kbd probe; 10x for a reminder from fgsch@

--- sys/arch/i386/stand/boot/conf.c
+++ sys/arch/i386/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.17 2001/05/06 17:45:08 mickey Exp $	*/
+/*	$OpenBSD: conf.c,v 1.18 2001/08/20 20:44:49 mickey Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -48,7 +48,7 @@
 #include <dev/cons.h>
 #include <lib/libsa/exec.h>
 
-const char version[] = "1.27";
+const char version[] = "1.28";
 int	debug = 1;
 
 const struct x_sw execsw[] = {
