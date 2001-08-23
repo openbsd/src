b9b0c509fb2ec3b43460bd13c44a81216dfcaccd
art <art@openbsd.org>
2001-08-23 16:10:46 +0000

    weak_alias and warn_references

--- sys/arch/sparc64/include/cdefs.h
+++ sys/arch/sparc64/include/cdefs.h
@@ -1,9 +1,13 @@
-/*	$OpenBSD: cdefs.h,v 1.1.1.1 2001/08/18 04:16:40 jason Exp $	*/
+/*	$OpenBSD: cdefs.h,v 1.2 2001/08/23 16:10:46 art Exp $	*/
 /*	$NetBSD: cdefs.h,v 1.3 1999/03/20 01:40:26 thorpej Exp $	*/
 
 #ifndef	_MACHINE_CDEFS_H_
 #define	_MACHINE_CDEFS_H_
 
-/* No arch-specific cdefs. */
+#define __weak_alias(alias,sym)                                         \
+    __asm__(".weak " __STRING(alias) " ; " __STRING(alias) " = " __STRING(sym))
+#define __warn_references(sym,msg)                                      \
+    __asm__(".section .gnu.warning." __STRING(sym) " ; .ascii \"" msg "\" ; .text")
+
 
 #endif /* !_MACHINE_CDEFS_H_ */
