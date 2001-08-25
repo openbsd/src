1e36cdfc02ae07a090570a567e126c0dead89984
drahn <drahn@openbsd.org>
2001-08-25 15:20:15 +0000

    Fix the __infinity constant on powerpc. from a7r@andern.org

--- lib/libc/arch/powerpc/gen/infinity.c
+++ lib/libc/arch/powerpc/gen/infinity.c
@@ -1,12 +1,12 @@
-/*	$OpenBSD: infinity.c,v 1.2 2000/03/01 17:31:22 todd Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.3 2001/08/25 15:20:15 drahn Exp $	*/
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: infinity.c,v 1.2 2000/03/01 17:31:22 todd Exp $";
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.3 2001/08/25 15:20:15 drahn Exp $";
 #endif /* not lint */
 
 /* infinity.c */
 
 #include <math.h>
 
-/* bytes for +Infinity on a 387 */
-char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
+/* bytes for +Infinity on a PowerPC */
+char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
