189ec368508eba8ce1063f1e7c09adb91b720560
jakob <jakob@openbsd.org>
2001-09-02 12:06:57 +0000

    bad typecast

--- usr.sbin/tcpdump/print-icmp.c
+++ usr.sbin/tcpdump/print-icmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-icmp.c,v 1.11 2001/09/02 12:05:13 jakob Exp $	*/
+/*	$OpenBSD: print-icmp.c,v 1.12 2001/09/02 12:06:57 jakob Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1993, 1994, 1995, 1996
@@ -23,7 +23,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/print-icmp.c,v 1.11 2001/09/02 12:05:13 jakob Exp $ (LBL)";
+    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/print-icmp.c,v 1.12 2001/09/02 12:06:57 jakob Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -197,7 +197,7 @@ icmp_print(register const u_char *bp, register const u_char *bp2)
 				       "request": "reply",
 				       dp->icmp_id, dp->icmp_seq);
 		} else
-			buf = tok2str(icmp2str, "type-#%d", dp->icmp_type);
+			str = tok2str(icmp2str, "type-#%d", dp->icmp_type);
 		break;
 
 	case ICMP_UNREACH:
