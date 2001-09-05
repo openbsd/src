e77112fc123848e0b7d670ad35293d11b6b6e47d
art <art@openbsd.org>
2001-09-05 23:24:46 +0000

    explicit alignment, just like on alpha.

--- lib/libc/arch/sparc64/gen/infinity.c
+++ lib/libc/arch/sparc64/gen/infinity.c
@@ -1,8 +1,9 @@
-/*	$OpenBSD: infinity.c,v 1.1 2001/08/29 01:41:29 art Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.2 2001/09/05 23:24:46 art Exp $	*/
 
 /* infinity.c */
 
 #include <math.h>
 
 /* bytes for +Infinity on a sparc */
-char __infinity[] = { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
+char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
+	{ 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
