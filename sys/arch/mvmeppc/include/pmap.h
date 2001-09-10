71aa904aa8a1c3884e361e9a41a02402a9d13f04
drahn <drahn@openbsd.org>
2001-09-10 17:52:06 +0000

    Fix the other half.

--- sys/arch/mvmeppc/include/pmap.h
+++ sys/arch/mvmeppc/include/pmap.h
@@ -1,5 +1,7 @@
-/*	$OpenBSD: pmap.h,v 1.4 2001/09/10 16:44:52 mickey Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.5 2001/09/10 17:52:06 drahn Exp $	*/
 
 #include <powerpc/pmap.h>
 
+#ifndef	_LOCORE
 paddr_t vtophys __P((vaddr_t));
+#endif	/* _LOCORE */
