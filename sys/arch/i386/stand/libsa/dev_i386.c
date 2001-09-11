44987e8eb2e1e3d9cf518a0d7bdb09e2d7d67588
fgsch <fgsch@openbsd.org>
2001-09-11 06:06:15 +0000

    remove acd references; millert@ ok.

--- sys/arch/i386/stand/libsa/dev_i386.c
+++ sys/arch/i386/stand/libsa/dev_i386.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dev_i386.c,v 1.23 2000/06/08 01:51:32 mickey Exp $	*/
+/*	$OpenBSD: dev_i386.c,v 1.24 2001/09/11 06:06:15 fgsch Exp $	*/
 
 /*
  * Copyright (c) 1996-1999 Michael Shalayeff
@@ -41,7 +41,7 @@ extern int debug;
 /* XXX use slot for 'rd' for 'hd' pseudo-device */
 const char bdevs[][4] = {
 	"wd", "", "fd", "wt", "sd", "st", "cd", "mcd",
-	"", "", "", "", "", "", "", "scd", "", "hd", "acd"
+	"", "", "", "", "", "", "", "scd", "", "hd", ""
 };
 const int nbdevs = NENTS(bdevs);
 
