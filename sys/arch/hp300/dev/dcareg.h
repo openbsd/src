839498c2a2ee8e567acb8e2dd0d46e5616067fda
millert <millert@openbsd.org>
2001-09-23 22:01:38 +0000

    document some registers; copied from comreg.h

--- sys/arch/hp300/dev/dcareg.h
+++ sys/arch/hp300/dev/dcareg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dcareg.h,v 1.4 1997/01/12 15:12:21 downsj Exp $	*/
+/*	$OpenBSD: dcareg.h,v 1.5 2001/09/23 22:01:38 millert Exp $	*/
 /*	$NetBSD: dcareg.h,v 1.6 1996/02/24 00:55:02 thorpej Exp $	*/
 
 /*
@@ -108,30 +108,30 @@ struct dcadevice {
 #endif
 
 /* interrupt enable register */
-#define	IER_ERXRDY	0x1
-#define	IER_ETXRDY	0x2
-#define	IER_ERLS	0x4
-#define	IER_EMSC	0x8
+#define	IER_ERXRDY	0x1	/* Enable receiver interrupt */
+#define	IER_ETXRDY	0x2	/* Enable transmitter empty interrupt */
+#define	IER_ERLS	0x4	/* Enable line status interrupt */
+#define	IER_EMSC	0x8	/* Enable modem status interrupt */
 
 /* interrupt identification register */
 #define	IIR_IMASK	0xf
 #define	IIR_RXTOUT	0xc
-#define	IIR_RLS		0x6
-#define	IIR_RXRDY	0x4
-#define	IIR_TXRDY	0x2
-#define	IIR_NOPEND	0x1
-#define	IIR_MLSC	0x0
+#define	IIR_RLS		0x6	/* Line status change */
+#define	IIR_RXRDY	0x4	/* Receiver ready */
+#define	IIR_TXRDY	0x2	/* Transmitter ready */
+#define	IIR_NOPEND	0x1	/* No pending interrupts */
+#define	IIR_MLSC	0x0	/* Modem status */
 #define	IIR_FIFO_MASK	0xc0	/* set if FIFOs are enabled */
 
 /* fifo control register */
-#define	FIFO_ENABLE	0x01
-#define	FIFO_RCV_RST	0x02
-#define	FIFO_XMT_RST	0x04
+#define	FIFO_ENABLE	0x01	/* Turn the FIFO on */
+#define	FIFO_RCV_RST	0x02	/* Reset RX FIFO */
+#define	FIFO_XMT_RST	0x04	/* Reset TX FIFO */
 #define	FIFO_DMA_MODE	0x08
-#define	FIFO_TRIGGER_1	0x00
-#define	FIFO_TRIGGER_4	0x40
-#define	FIFO_TRIGGER_8	0x80
-#define	FIFO_TRIGGER_14	0xc0
+#define	FIFO_TRIGGER_1	0x00	/* Trigger RXRDY intr on 1 character */
+#define	FIFO_TRIGGER_4	0x40	/* ibid 4 */
+#define	FIFO_TRIGGER_8	0x80	/* ibid 8 */
+#define	FIFO_TRIGGER_14	0xc0	/* ibid 14 */
 
 /* character format control register */
 #define	CFCR_DLAB	0x80
@@ -148,29 +148,29 @@ struct dcadevice {
 #define	CFCR_5BITS	0x00
 
 /* modem control register */
-#define	MCR_LOOPBACK	0x10
-#define	MCR_IEN		0x08
-#define	MCR_DRS		0x04
-#define	MCR_RTS		0x02
-#define	MCR_DTR		0x01
+#define	MCR_LOOPBACK	0x10	/* Loop test: echos from TX to RX */
+#define	MCR_IEN		0x08	/* Out2: enables UART interrupts */
+#define	MCR_DRS		0x04	/* Out1: resets some internal modems */
+#define	MCR_RTS		0x02	/* Request To Send */
+#define	MCR_DTR		0x01	/* Data Terminal Ready */
 
 /* line status register */
 #define	LSR_RCV_FIFO	0x80
-#define	LSR_TSRE	0x40
-#define	LSR_TXRDY	0x20
-#define	LSR_BI		0x10
-#define	LSR_FE		0x08
-#define	LSR_PE		0x04
-#define	LSR_OE		0x02
-#define	LSR_RXRDY	0x01
-#define	LSR_RCV_MASK	0x1f
+#define	LSR_TSRE	0x40	/* Transmitter empty: byte sent */
+#define	LSR_TXRDY	0x20	/* Transmitter buffer empty */
+#define	LSR_BI		0x10	/* Break detected */
+#define	LSR_FE		0x08	/* Framing error: bad stop bit */
+#define	LSR_PE		0x04	/* Parity error */
+#define	LSR_OE		0x02	/* Overrun, lost incoming byte */
+#define	LSR_RXRDY	0x01	/* Byte ready in Receive Buffer */
+#define	LSR_RCV_MASK	0x1f	/* Mask for incoming data or error */
 
 /* modem status register */
-#define	MSR_DCD		0x80
-#define	MSR_RI		0x40
-#define	MSR_DSR		0x20
-#define	MSR_CTS		0x10
-#define	MSR_DDCD	0x08
-#define	MSR_TERI	0x04
-#define	MSR_DDSR	0x02
-#define	MSR_DCTS	0x01
+#define	MSR_DCD		0x80	/* Current Data Carrier Detect */
+#define	MSR_RI		0x40	/* Current Ring Indicator */
+#define	MSR_DSR		0x20	/* Current Data Set Ready */
+#define	MSR_CTS		0x10	/* Current Clear to Send */
+#define	MSR_DDCD	0x08	/* DCD has changed state */
+#define	MSR_TERI	0x04	/* RI has toggled low to high */
+#define	MSR_DDSR	0x02	/* DSR has changed state */
+#define	MSR_DCTS	0x01	/* CTS has changed state */
