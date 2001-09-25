2da2a24a0d1be6f05c2ae441b86184d56930cdc5
art <art@openbsd.org>
2001-09-25 07:15:05 +0000

    Add a bunch of relocation types.

--- sys/arch/sparc64/include/reloc.h
+++ sys/arch/sparc64/include/reloc.h
@@ -1,9 +1,19 @@
-/*	$OpenBSD: reloc.h,v 1.1 2001/09/24 23:36:47 art Exp $	*/
+/*	$OpenBSD: reloc.h,v 1.2 2001/09/25 07:15:05 art Exp $	*/
 
 #ifndef	_MACH_RELOC_H_
 #define	_MACH_RELOC_H_
 
+#define RELOC_NONE	0
+
+#define RELOC_COPY	19
+#define RELOC_GLOB_DAT	20
+#define RELOC_JMP_SLOT	21
 #define RELOC_RELATIVE	22
+
+#define RELOC_64	32
+
+#define RELOC_UA64	54
+
 
 #endif	/* _MACH_RELOC_H_ */
 
