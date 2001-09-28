04729580d6877d1b8687bed2e30f610a9c252eff
art <art@openbsd.org>
2001-09-28 09:14:07 +0000

    Redundant prototypes.

--- sys/kern/tty_subr.c
+++ sys/kern/tty_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tty_subr.c,v 1.13 2001/07/05 10:12:25 art Exp $	*/
+/*	$OpenBSD: tty_subr.c,v 1.14 2001/09/28 09:14:07 art Exp $	*/
 /*	$NetBSD: tty_subr.c,v 1.13 1996/02/09 19:00:43 christos Exp $	*/
 
 /*
@@ -63,13 +63,9 @@
 #endif
 
 void	cinit __P((void));
-int	ndqb __P((struct clist *, int));
-int	putc __P((int, struct clist *));
 #ifdef QBITS
 void	clrbits __P((u_char *, int, int));
 #endif
-int	b_to_q __P((u_char *, int, struct clist *));
-u_char *firstc __P((struct clist *, int *));
 
 /*
  * Initialize clists.
