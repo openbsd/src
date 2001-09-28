7eebc2d36d208fc1c1dc5c36b75f0f1762daf6e3
jason <jason@openbsd.org>
2001-09-28 15:18:15 +0000

    proper values for ieee rounding modes (from NetBSD)

--- sys/arch/sparc64/include/ieeefp.h
+++ sys/arch/sparc64/include/ieeefp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieeefp.h,v 1.1 2001/08/23 15:30:34 art Exp $	*/
+/*	$OpenBSD: ieeefp.h,v 1.2 2001/09/28 15:18:15 jason Exp $	*/
 /*
  * Written by J.T. Conklin, Apr 6, 1995
  * Public domain.
@@ -17,8 +17,8 @@ typedef int fp_except;
 typedef enum {
     FP_RN=0,			/* round to nearest representable number */
     FP_RZ=1,			/* round to zero (truncate) */
-    FP_RM=2,			/* round toward negative infinity */
-    FP_RP=3			/* round toward positive infinity */
+    FP_RP=2,			/* round toward positive infinity */
+    FP_RM=3			/* round toward negative infinity */
 } fp_rnd;
 
 #endif /* _SPARC_IEEEFP_H_ */
