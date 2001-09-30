145b857f6532dc65c3c238abb6643160bdba122c
jason <jason@openbsd.org>
2001-09-30 21:20:07 +0000

    pretty

--- sys/dev/sbus/cs4231.c
+++ sys/dev/sbus/cs4231.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cs4231.c,v 1.3 2001/09/30 21:03:26 jason Exp $	*/
+/*	$OpenBSD: cs4231.c,v 1.4 2001/09/30 21:20:07 jason Exp $	*/
 
 /*
  * Copyright (c) 1999 Jason L. Wright (jason@thought.net)
@@ -250,7 +250,7 @@ cs4231_attach(parent, self, aux)
 		burst = sbusburst;
 	sc->sc_burst = burst & sbusburst;
 
-	printf(": pri %d\n", sa->sa_pri);
+	printf("\n");
 
 	evcnt_attach(&sc->sc_dev, "intr", &sc->sc_intrcnt);
 
