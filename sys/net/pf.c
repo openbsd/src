1e0ccf29492292a752833536d69d69bca78cc4f8
dhartmei <dhartmei@openbsd.org>
2001-10-02 08:44:21 +0000

    Convert ip_off of the inner IP header to host order in pf_test_state_icmp(). Some of the IP header fields are already converted by ip_input.c (including ip_off), but of course not for inner headers of ICMP packets. The other fields which are left in network order are ok.
    This broke state search for any ICMP error message who referred to an IP
    header with the DF flag set, hence any ICMP_UNREACH_NEEDFRAG message.
    
    Found by Andreas Gunnarsson <andreas@crt.se>. Thank you.

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.160 2001/09/30 03:49:18 frantzen Exp $ */
+/*	$OpenBSD: pf.c,v 1.161 2001/10/02 08:44:21 dhartmei Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -3911,7 +3911,7 @@ pf_test_state_icmp(struct pf_state **state, int direction, struct ifnet *ifp,
 				return (PF_DROP);
 			}
 			/* ICMP error messages don't refer to non-first fragments */
-			if (h2.ip_off & IP_OFFMASK)
+			if (ntohs(h2.ip_off) & IP_OFFMASK)
 				return (PF_DROP);
 
 			/* offset of protocol header that follows h2 */
