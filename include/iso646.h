2013916786c1b2151fd5a8ede76946234d1e4c6b
espie <espie@openbsd.org>
2001-10-11 00:05:21 +0000

    C++ has all those as built-ins. Problem found with gcc 3.0.1 testsuite.
    okay theo.

--- include/iso646.h
+++ include/iso646.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iso646.h,v 1.2 1997/09/21 10:45:37 niklas Exp $	*/
+/*	$OpenBSD: iso646.h,v 1.3 2001/10/11 00:05:21 espie Exp $	*/
 /*	$NetBSD: iso646.h,v 1.1 1995/02/17 09:08:10 jtc Exp $	*/
 
 /* 
@@ -9,6 +9,7 @@
 #ifndef _ISO646_H_
 #define _ISO646_H_
 
+#ifndef __cplusplus
 #define and	&&
 #define and_eq	&=
 #define bitand	&
@@ -20,5 +21,6 @@
 #define or_eq	|=
 #define xor	^
 #define xor_eq	^=
+#endif
 
 #endif	/* !_ISO646_H_ */
