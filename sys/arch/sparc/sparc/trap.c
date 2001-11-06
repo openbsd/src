9c3600ce70c17fa0e018e2fe4cd9cdc683f9488e
mickey <mickey@openbsd.org>
2001-11-06 21:33:53 +0000

    child_return unscrewart

--- sys/arch/sparc/sparc/trap.c
+++ sys/arch/sparc/sparc/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.33 2001/11/06 19:53:16 miod Exp $	*/
+/*	$OpenBSD: trap.c,v 1.34 2001/11/06 21:33:53 mickey Exp $	*/
 /*	$NetBSD: trap.c,v 1.58 1997/09/12 08:55:01 pk Exp $ */
 
 /*
@@ -1149,9 +1149,10 @@ syscall(code, tf, pc)
  * Process the tail end of a fork() for the child.
  */
 void
-child_return(p)
-	struct proc *p;
+child_return(arg)
+	void *arg;
 {
+	struct proc *p = arg;
 
 	/*
 	 * Return values in the frame set by cpu_fork().
