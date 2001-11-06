6dc095b3d5df5d18ee0592aa537ec5b5141900e6
art <art@openbsd.org>
2001-11-06 00:20:22 +0000

    Minor sync to NetBSD.

--- sys/uvm/uvm_pager.h
+++ sys/uvm/uvm_pager.h
@@ -1,5 +1,5 @@
-/*	$OpenBSD: uvm_pager.h,v 1.11 2001/08/12 21:36:48 mickey Exp $	*/
-/*	$NetBSD: uvm_pager.h,v 1.15 2000/05/19 03:45:04 thorpej Exp $	*/
+/*	$OpenBSD: uvm_pager.h,v 1.12 2001/11/06 00:20:22 art Exp $	*/
+/*	$NetBSD: uvm_pager.h,v 1.16 2000/06/26 14:21:18 mrg Exp $	*/
 
 /*
  *
@@ -33,6 +33,46 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * from: Id: uvm_pager.h,v 1.1.2.14 1998/01/13 19:00:50 chuck Exp
+ */
+
+/*
+ * Copyright (c) 1990 University of Utah.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)vm_pager.h	8.5 (Berkeley) 7/7/94
  */
 
 #ifndef _UVM_UVM_PAGER_H_
@@ -117,27 +157,6 @@ struct uvm_pagerops {
 #ifdef _KERNEL
 
 /*
- * get/put return values
- * OK	   operation was successful
- * BAD	   specified data was out of the accepted range
- * FAIL	   specified data was in range, but doesn't exist
- * PEND	   operations was initiated but not completed
- * ERROR   error while accessing data that is in range and exists
- * AGAIN   temporary resource shortage prevented operation from happening
- * UNLOCK  unlock the map and try again
- * REFAULT [uvm_fault internal use only!] unable to relock data structures,
- *         thus the mapping needs to be reverified before we can procede
- */
-#define	VM_PAGER_OK		0
-#define	VM_PAGER_BAD		1
-#define	VM_PAGER_FAIL		2
-#define	VM_PAGER_PEND		3
-#define	VM_PAGER_ERROR		4
-#define VM_PAGER_AGAIN		5
-#define VM_PAGER_UNLOCK		6
-#define VM_PAGER_REFAULT	7
-
-/*
  * handle inline options
  */
 
@@ -172,6 +191,27 @@ struct vm_page **uvm_mk_pcluster  __P((struct uvm_object *, struct vm_page **,
 #define	UVMPAGER_MAPIN_WAITOK	0x01	/* it's okay to wait */
 #define	UVMPAGER_MAPIN_READ	0x02	/* host <- device */
 #define	UVMPAGER_MAPIN_WRITE	0x00	/* device -> host (pseudo flag) */
+
+/*
+ * get/put return values
+ * OK	   operation was successful
+ * BAD	   specified data was out of the accepted range
+ * FAIL	   specified data was in range, but doesn't exist
+ * PEND	   operations was initiated but not completed
+ * ERROR   error while accessing data that is in range and exists
+ * AGAIN   temporary resource shortage prevented operation from happening
+ * UNLOCK  unlock the map and try again
+ * REFAULT [uvm_fault internal use only!] unable to relock data structures,
+ *         thus the mapping needs to be reverified before we can procede
+ */
+#define	VM_PAGER_OK		0
+#define	VM_PAGER_BAD		1
+#define	VM_PAGER_FAIL		2
+#define	VM_PAGER_PEND		3
+#define	VM_PAGER_ERROR		4
+#define VM_PAGER_AGAIN		5
+#define VM_PAGER_UNLOCK		6
+#define VM_PAGER_REFAULT	7
 
 #endif /* _KERNEL */
 
