16755574cc8807bb0f4620a7a1af1689495ed50b
marc <marc@openbsd.org>
2001-11-10 02:44:48 +0000

    Add a pcap/pthread test as it was reported that using libpcap blocked all threads.   That no longer seems to be a problem as this test passes using the latest threads lib.

---
+++ regress/lib/libc_r/pcap/pcap.c
@@ -0,0 +1,70 @@
+/*	$OpenBSD: pcap.c,v 1.1 2001/11/10 02:44:48 marc Exp $ */
+/*
+ *	Copyright (c) 2001 Marco S. Hyman
+ *
+ *	Permission to copy all or part of this material with or without
+ *	modification for any purpose is granted provided that the above
+ *	copyright notice and this paragraph are duplicated in all copies.
+ *
+ *	THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ *	IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */ 
+
+#include <pcap.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "test.h"
+
+#define LOOPBACK_IF	"lo0"
+#define SNAPLEN		96
+#define NO_PROMISC	0
+#define	PKTCNT		3
+
+volatile int packet_count = 0;
+
+void
+packet_ignore(u_char *tag, const struct pcap_pkthdr *hdr, const u_char *data)
+{
+	packet_count += 1;
+}
+
+void *
+pcap_thread(void *arg)
+{
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *handle;
+
+	SET_NAME("pcap_thread");
+	handle = pcap_open_live(LOOPBACK_IF, SNAPLEN, NO_PROMISC, 0, errbuf);
+	if (!handle)
+		PANIC("You may need to run this test as UID 0 (root)");
+	ASSERT(pcap_loop(handle, PKTCNT, packet_ignore, 0) != -1);
+	return 0;
+}
+
+void *
+ping_thread(void *arg)
+{
+	SET_NAME("ping_thread");
+	ASSERT(system("ping -c 3 127.0.0.1") == 0);
+	sleep(2);
+	ASSERT(packet_count == 3);
+	SUCCEED;
+}
+
+int
+main(int argc, char **argv)
+{
+	pthread_t pcap;
+	pthread_t ping;
+
+	CHECKr(pthread_create(&pcap, NULL, pcap_thread, NULL));
+	sleep(1);
+	CHECKr(pthread_create(&ping, NULL, ping_thread, NULL));
+	while (1)
+		;
+	PANIC("while");
+}
