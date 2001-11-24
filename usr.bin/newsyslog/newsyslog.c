99b260f47a0f1a05072ced5767196e2358f04676
marc <marc@openbsd.org>
2001-11-24 19:53:22 +0000

    handle NL terminated pid files correctly, OK deraadt@

--- usr.bin/newsyslog/newsyslog.c
+++ usr.bin/newsyslog/newsyslog.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: newsyslog.c,v 1.40 2001/11/23 04:20:56 deraadt Exp $	*/
+/*	$OpenBSD: newsyslog.c,v 1.41 2001/11/24 19:53:22 marc Exp $	*/
 
 /*
  * Copyright (c) 1999 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -88,7 +88,7 @@ provided "as is" without express or implied warranty.
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: newsyslog.c,v 1.40 2001/11/23 04:20:56 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: newsyslog.c,v 1.41 2001/11/24 19:53:22 marc Exp $";
 #endif /* not lint */
 
 #ifndef CONF
@@ -332,7 +332,7 @@ send_signal(pidfile, signal)
 	if (fgets(line, sizeof(line), f)) {
 		errno = 0;
 		ulval = strtoul(line, &ep, 10);
-		if (line[0] == '\0' || *ep != '\0')
+		if (line[0] == '\0' || (*ep != '\0' && *ep != '\n'))
 			err = "invalid number in";
 		else if (errno == ERANGE && ulval == ULONG_MAX)
 			err = "out of range number in";
