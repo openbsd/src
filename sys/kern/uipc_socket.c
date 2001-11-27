662f489f0245c58a811fc0bdfccc927525df87bc
provos <provos@openbsd.org>
2001-11-27 17:55:39 +0000

    fix an error in sosend() that could make a transient error permant. verified with both netbsd and freebsd.
    from netbsd:
    Tue Jun  8 02:39:57 1999 UTC by thorpej
    
    In sosend(), if so_error is set, clear it before returning the error to
    the process (i.e. pre-Reno behavior).  The 4.4BSD behavior (introduced
    in Reno) caused transient errors to stick incorrectly.
    
    This is from PR #7640 (Havard Eidnes), cross-checked w/ FreeBSD, where
    Bill Fenner committed the same fix (as described in a comment in the
    Vat sources, by Van Jacobsen).

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.36 2001/11/27 15:51:36 provos Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.37 2001/11/27 17:55:39 provos Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
@@ -408,8 +408,12 @@ restart:
 		s = splsoftnet();
 		if (so->so_state & SS_CANTSENDMORE)
 			snderr(EPIPE);
-		if (so->so_error)
-			snderr(so->so_error);
+		if (so->so_error) {
+			error = so->so_error;
+			so->so_error = 0;
+			splx(s);
+			goto release;
+		}
 		if ((so->so_state & SS_ISCONNECTED) == 0) {
 			if (so->so_proto->pr_flags & PR_CONNREQUIRED) {
 				if ((so->so_state & SS_ISCONFIRMING) == 0 &&
