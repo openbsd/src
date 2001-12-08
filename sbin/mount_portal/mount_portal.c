6f3cf4edf087c71efea12f8355587de6b69c634d
miod <miod@openbsd.org>
2001-12-08 23:42:57 +0000

    Don't unlink() the same thing twice, ok millert@

--- sbin/mount_portal/mount_portal.c
+++ sbin/mount_portal/mount_portal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mount_portal.c,v 1.16 2001/11/17 20:01:53 deraadt Exp $	*/
+/*	$OpenBSD: mount_portal.c,v 1.17 2001/12/08 23:42:57 miod Exp $	*/
 /*	$NetBSD: mount_portal.c,v 1.8 1996/04/13 01:31:54 jtc Exp $	*/
 
 /*
@@ -47,7 +47,7 @@ char copyright[] =
 #if 0
 static char sccsid[] = "@(#)mount_portal.c	8.6 (Berkeley) 4/26/95";
 #else
-static char rcsid[] = "$OpenBSD: mount_portal.c,v 1.16 2001/11/17 20:01:53 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: mount_portal.c,v 1.17 2001/12/08 23:42:57 miod Exp $";
 #endif
 #endif /* not lint */
 
@@ -184,7 +184,6 @@ main(argc, argv)
 	/* XXX teeny race? */
 	if (bind(so, (struct sockaddr *) &un, sizeof(un)) < 0)
 		err(1, "bind(2)");
-	(void)unlink(un.sun_path);
 
 	(void)listen(so, 5);
 
