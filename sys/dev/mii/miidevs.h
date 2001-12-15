3236ff1153823f8c18aa4214e48df127ad0468c9
deraadt <deraadt@openbsd.org>
2001-12-15 02:41:38 +0000

    sync

--- sys/dev/mii/miidevs.h
+++ sys/dev/mii/miidevs.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: miidevs.h,v 1.28 2001/12/14 22:07:28 jason Exp $	*/
+/*	$OpenBSD: miidevs.h,v 1.29 2001/12/15 02:41:38 deraadt Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: miidevs,v 1.25 2001/12/14 22:06:46 jason Exp 
+ *	OpenBSD: miidevs,v 1.26 2001/12/15 02:41:33 deraadt Exp 
  */
 /* $NetBSD: miidevs,v 1.3 1998/11/05 03:43:43 thorpej Exp $ */
 
@@ -86,6 +86,7 @@
 /* Level 1 is completely different - from right to left.
 	(Two bits get lost in the third OUI byte.) */
 #define	MII_OUI_xxLEVEL1	0x1e0400	/* Level 1 */
+#define	MII_OUI_xxLEVEL1a	0x0004de	/* Level 1 */
 
 /* Don't know what's going on here. */
 #define	MII_OUI_xxDAVICOM	0x006040	/* Davicom Semiconductor */
@@ -168,6 +169,8 @@
 /* Level 1 PHYs */
 #define	MII_MODEL_xxLEVEL1_LXT970	0x0000
 #define	MII_STR_xxLEVEL1_LXT970	"LXT970 10/100 media interface"
+#define	MII_MODEL_xxLEVEL1a_LXT971	0x000e
+#define	MII_STR_xxLEVEL1a_LXT971	"LXT971 10/100 media interface"
 
 /* Myson Technology PHYs */
 #define	MII_MODEL_MYSON_MTD972	0x0000
