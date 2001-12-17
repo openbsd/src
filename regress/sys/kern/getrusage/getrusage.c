78b63d65c6927d1e0425a7944dd0cc063ab27adb
nordin <nordin@openbsd.org>
2001-12-17 01:22:05 +0000

    Check for monotonic user and system time.

---
+++ regress/sys/kern/getrusage/getrusage.c
@@ -0,0 +1,35 @@
+#include <err.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+
+int
+main()
+{
+	struct timeval utime;
+	struct timeval stime;
+        struct rusage r;
+        int i;
+
+	timerclear(&utime);
+	timerclear(&stime);
+	do {
+		if (getrusage(RUSAGE_SELF, &r) == -1)
+			err(1, "getrusage");
+
+		if (timercmp(&(r.ru_utime), &utime, <))
+			errx(1, "user time decreased");
+		utime = r.ru_utime;
+
+		if (timercmp(&(r.ru_stime), &stime, <))
+			errx(1, "system time decreased");
+		stime = r.ru_stime;
+        } while (utime.tv_sec < 1 && stime.tv_sec < 1);
+
+        return 0;
+}
