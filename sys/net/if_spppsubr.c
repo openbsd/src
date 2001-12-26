be8669b4d11e18038e2471d72e644550ce0874c6
mickey <mickey@openbsd.org>
2001-12-26 22:36:31 +0000

    misplaced (), causing tcp header be examined in non-tcp packets

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_spppsubr.c,v 1.11 2001/12/10 06:10:53 jason Exp $	*/
+/*	$OpenBSD: if_spppsubr.c,v 1.12 2001/12/26 22:36:31 mickey Exp $	*/
 /*
  * Synchronous PPP/Cisco link level subroutines.
  * Keepalive protocol implemented in both Cisco and PPP modes.
@@ -705,8 +705,8 @@ sppp_output(struct ifnet *ifp, struct mbuf *m,
 		    ((ip->ip_tos & IPTOS_LOWDELAY) ||
 	    	    ((ip->ip_p == IPPROTO_TCP &&
 	    	    m->m_len >= sizeof (struct ip) + sizeof (struct tcphdr) &&
-	    	    (INTERACTIVE (ntohs (tcp->th_sport)))) ||
-	    	    INTERACTIVE (ntohs (tcp->th_dport)))))
+	    	    (INTERACTIVE (ntohs (tcp->th_sport))) ||
+	    	     INTERACTIVE (ntohs (tcp->th_dport))))))
 			ifq = &sp->pp_fastq;
 	}
 #endif
