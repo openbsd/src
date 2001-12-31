24982a3439e0dcaf3a6a5984fe9c74e8d4001bef
mickey <mickey@openbsd.org>
2001-12-31 23:38:53 +0000

    man says bus_space_map() returns non-zero, not negative on failure

--- sys/arch/i386/pci/pchb.c
+++ sys/arch/i386/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pchb.c,v 1.27 2001/01/22 23:11:21 deraadt Exp $	*/
+/*	$OpenBSD: pchb.c,v 1.28 2001/12/31 23:38:53 mickey Exp $	*/
 /*	$NetBSD: pchb.c,v 1.6 1997/06/06 23:29:16 thorpej Exp $	*/
 
 /*
@@ -289,7 +289,7 @@ pchbattach(parent, self, aux)
 		case PCI_PRODUCT_INTEL_82860_HB:
 			sc->bt = pa->pa_memt;
 			if (bus_space_map(sc->bt, I82802_IOBASE, I82802_IOSIZE,
-			    0, &sc->bh) < 0)
+			    0, &sc->bh))
 				break;
 
 			/* probe and init rng */
