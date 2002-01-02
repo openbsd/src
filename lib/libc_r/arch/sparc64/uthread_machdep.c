e3f7b16082eb877eba4d717691238ffd6bfecb2c
art <art@openbsd.org>
2002-01-02 19:12:43 +0000

    fp and pc are 64 bit. And just to make things more fun -1, is not a misaligned stack on sparc64.

--- lib/libc_r/arch/sparc64/uthread_machdep.c
+++ lib/libc_r/arch/sparc64/uthread_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_machdep.c,v 1.1 2001/09/10 20:00:14 jason Exp $	*/
+/*	$OpenBSD: uthread_machdep.c,v 1.2 2002/01/02 19:12:43 art Exp $	*/
 
 /*
  * Machine-dependent thread state functions for OpenBSD/sparc.
@@ -24,12 +24,12 @@ _thread_machdep_init(statep, base, len, entry)
 	struct frame64 *f;
 
 	/* Locate the initial frame, aligned at the top of the stack */
-	f = (struct frame64 *)(((int)base + len - sizeof *f) & ~ALIGNBYTES);
+	f = (struct frame64 *)(((long)base + len - sizeof *f) & ~ALIGNBYTES);
 	
-	f->fr_fp = (struct frame64 *)-1;	/* purposefully misaligned */
+	f->fr_fp = 0;				/* purposefully misaligned */
 	f->fr_pc = -1;				/* for gdb */
-	statep->fp = (int)f;
-	statep->pc = -8 + (int)entry;
+	statep->fp = (u_long)f - BIAS;
+	statep->pc = -8 + (u_long)entry;
 }
 
 void
