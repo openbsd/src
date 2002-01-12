e9b4500b56d1aff642c840775cf3899bebe045af
markus <markus@openbsd.org>
2002-01-12 13:10:29 +0000

    undo local change

--- usr.bin/ssh/auth-skey.c
+++ usr.bin/ssh/auth-skey.c
@@ -22,7 +22,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "includes.h"
-RCSID("$OpenBSD: auth-skey.c,v 1.15 2002/01/11 23:26:30 markus Exp $");
+RCSID("$OpenBSD: auth-skey.c,v 1.16 2002/01/12 13:10:29 markus Exp $");
 
 #ifdef SKEY
 
@@ -60,7 +60,8 @@ skey_query(void *ctx, char **name, char **infotxt,
 
 	len = strlen(challenge) + strlen(PROMPT) + 1;
 	p = xmalloc(len);
-	snprintf(p, len, "%s%s", challenge, PROMPT);
+	strlcpy(p, challenge, len);
+	strlcat(p, PROMPT, len);
 	(*prompts)[0] = p;
 
 	return 0;
