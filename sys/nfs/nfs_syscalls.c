0030563eec50ba75854bae504b773bf855f048d7
hugh <hugh@openbsd.org>
2002-01-20 23:51:29 +0000

    Move nfs_norsvport out from behind #ifdef NFSSERVER so ramdisk kernels with only NFSCLIENT defined can build.

--- sys/nfs/nfs_syscalls.c
+++ sys/nfs/nfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nfs_syscalls.c,v 1.24 2002/01/16 21:51:16 ericj Exp $	*/
+/*	$OpenBSD: nfs_syscalls.c,v 1.25 2002/01/20 23:51:29 hugh Exp $	*/
 /*	$NetBSD: nfs_syscalls.c,v 1.19 1996/02/18 11:53:52 fvdl Exp $	*/
 
 /*
@@ -93,6 +93,7 @@ extern int nfsrvw_procrastinate;
 struct nfssvc_sock *nfs_udpsock, *nfs_cltpsock;
 int nuidhash_max = NFS_MAXUIDHASH;
 int nfsd_waiting = 0;
+int nfs_norsvport = 0;
 #ifdef NFSSERVER
 static int nfs_numnfsd = 0;
 static struct nfsdrt nfsdrt;
@@ -108,7 +109,6 @@ int nfs_niothreads = -1;
 
 #ifdef NFSSERVER
 static void nfsd_rt __P((int, struct nfsrv_descript *, int));
-int nfs_norsvport = 0;
 #endif
 
 int nfs_clientd(struct nfsmount *nmp, struct ucred *cred, 
