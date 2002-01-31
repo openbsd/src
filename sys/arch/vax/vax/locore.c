0951879d39e5b17987cf756dd5366620da0c347b
hugh <hugh@openbsd.org>
2002-01-31 11:44:53 +0000

    A little KNF.

--- sys/arch/vax/vax/locore.c
+++ sys/arch/vax/vax/locore.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.c,v 1.21 2002/01/31 11:42:52 hugh Exp $	*/
+/*	$OpenBSD: locore.c,v 1.22 2002/01/31 11:44:53 hugh Exp $	*/
 /*	$NetBSD: locore.c,v 1.43 2000/03/26 11:39:45 ragge Exp $	*/
 /*
  * Copyright (c) 1994, 1998 Ludd, University of Lule}, Sweden.
@@ -246,15 +246,14 @@ start()
 		dep_call = &ka680_calls;
 		switch((vax_siedata & 0xff00) >> 8) {
 		case VAX_STYP_675:
-			strcpy(cpu_model,"VAX 4000 400"); break;
+			strcpy(cpu_model,"VAX 4000 400");
+			break;
 		case VAX_STYP_680:
-			strcpy(cpu_model,"VAX 4000 500"); break;
-		case VAX_STYP_681:
-			strcpy(cpu_model,"VAX 4000 500A"); break;
+			strcpy(cpu_model,"VAX 4000 500");
+			break;
 		case VAX_STYP_690:
-			strcpy(cpu_model,"VAX 4000 600"); break;
-		case VAX_STYP_691:
-			strcpy(cpu_model,"VAX 4000 605A"); break;
+			strcpy(cpu_model,"VAX 4000 600");
+			break;
 		default:
 			strcpy(cpu_model,"VAX - Unknown Omega Class");
 		}
