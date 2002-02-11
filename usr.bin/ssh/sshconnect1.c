c037df5a211604f29270bff5077260b91b31ccb9
markus <markus@openbsd.org>
2002-02-11 16:15:46 +0000

    include md5.h, not evp.h

--- usr.bin/ssh/sshconnect1.c
+++ usr.bin/ssh/sshconnect1.c
@@ -13,10 +13,10 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshconnect1.c,v 1.47 2001/12/28 14:50:54 markus Exp $");
+RCSID("$OpenBSD: sshconnect1.c,v 1.48 2002/02/11 16:15:46 markus Exp $");
 
 #include <openssl/bn.h>
-#include <openssl/evp.h>
+#include <openssl/md5.h>
 
 #ifdef KRB4
 #include <krb.h>
