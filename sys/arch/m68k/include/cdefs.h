20872775d6347614d185dc5a5698f61e62416c13
fgsch <fgsch@openbsd.org>
2002-02-11 20:08:58 +0000

    o add __weak_alias definition. o _STRING -> __STRING o empty definitions if !__GNUC__
    miod@ testing and ok.

--- sys/arch/m68k/include/cdefs.h
+++ sys/arch/m68k/include/cdefs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdefs.h,v 1.5 1996/08/05 17:17:28 niklas Exp $	*/
+/*	$OpenBSD: cdefs.h,v 1.6 2002/02/11 20:08:58 fgsch Exp $	*/
 /*	$NetBSD: cdefs.h,v 1.2 1995/03/23 20:10:33 jtc Exp $	*/
 
 /*
@@ -10,9 +10,9 @@
 #define	_M68K_CDEFS_H_
 
 #ifdef __STDC__
-#define _C_LABEL(x)	_STRING(_ ## x)
+#define _C_LABEL(x)	__STRING(_ ## x)
 #else
-#define _C_LABEL(x)	_STRING(_/**/x)
+#define _C_LABEL(x)	__STRING(_/**/x)
 #endif
 
 #ifdef __GNUC__
@@ -23,6 +23,8 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs \"" msg "\",30,0,0,0");		\
 	__asm__(".stabs \"_" #sym "\",1,0,0,0")
+#define __weak_alias(alias,sym)		\
+	__asm__(".weak _" #alias "; _" #alias "= _" __STRING(sym))
 #else
 #define __indr_reference(sym,alias)	\
 	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
@@ -30,9 +32,13 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs msg,30,0,0,0");			\
 	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+#define __weak_alias(alias,sym)		\
+	__asm__(".weak _/**/alias; _/**/alias = _/**/sym")
 #endif
 #else
-#define	__warn_references(sym,msg)	/* nothing */
+#define __indr_reference(sym,alias)
+#define __warn_references(sym,msg)
+#define __weak_alias(alias,sym)
 #endif
 
 #endif /* !_M68K_CDEFS_H_ */
