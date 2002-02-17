71f51835acc9b4c84479df67de065fd7d4487da0
hugh <hugh@openbsd.org>
2002-02-17 07:48:03 +0000

    Get gdb (mostly) working on vax. Work by bjc@.

--- gnu/usr.bin/binutils/gdb/vax-tdep.c
+++ gnu/usr.bin/binutils/gdb/vax-tdep.c
@@ -1,5 +1,5 @@
 /* Print VAX instructions for GDB, the GNU debugger.
-   Copyright 1986, 1989, 1991, 1992 Free Software Foundation, Inc.
+   Copyright 1986, 1989, 1991, 1992, 1996 Free Software Foundation, Inc.
 
 This file is part of GDB.
 
@@ -30,19 +30,25 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 static unsigned char *print_insn_arg ();
 
 /* Print the vax instruction at address MEMADDR in debugged memory,
-   on STREAM.  Returns length of the instruction, in bytes.  */
+   from disassembler info INFO.
+   Returns length of the instruction, in bytes.  */
 
-int
-vax_print_insn (memaddr, stream)
+static int
+vax_print_insn (memaddr, info)
      CORE_ADDR memaddr;
-     GDB_FILE *stream;
+     disassemble_info *info;
 {
   unsigned char buffer[MAXLEN];
   register int i;
   register unsigned char *p;
   register char *d;
 
-  read_memory (memaddr, buffer, MAXLEN);
+  int status = (*info->read_memory_func) (memaddr, buffer, MAXLEN, info);
+  if (status != 0)
+    {
+      (*info->memory_error_func) (status, memaddr, info);
+      return -1;
+    }
 
   for (i = 0; i < NOPCODES; i++)
     if (votstrs[i].detail.code == buffer[0]
@@ -52,36 +58,36 @@ vax_print_insn (memaddr, stream)
   /* Handle undefined instructions.  */
   if (i == NOPCODES)
     {
-      fprintf_unfiltered (stream, "0%o", buffer[0]);
+      (*info->fprintf_func) (info->stream, "0%o", buffer[0]);
       return 1;
     }
 
-  fprintf_unfiltered (stream, "%s", votstrs[i].name);
+  (*info->fprintf_func) (info->stream, "%s", votstrs[i].name);
 
   /* Point at first byte of argument data,
      and at descriptor for first argument.  */
   p = buffer + 1 + (votstrs[i].detail.code >= 0x100);
-  d = votstrs[i].detail.args;
+  d = (char *)votstrs[i].detail.args;
 
   if (*d)
-    fputc_unfiltered (' ', stream);
+    (*info->fprintf_func) (info->stream, " ");
 
   while (*d)
     {
-      p = print_insn_arg (d, p, memaddr + (p - buffer), stream);
+      p = print_insn_arg (d, p, memaddr + (p - buffer), info);
       d += 2;
       if (*d)
-	fprintf_unfiltered (stream, ",");
+	(*info->fprintf_func) (info->stream, ",");
     }
   return p - buffer;
 }
 
 static unsigned char *
-print_insn_arg (d, p, addr, stream)
+print_insn_arg (d, p, addr, info)
      char *d;
      register char *p;
      CORE_ADDR addr;
-     GDB_FILE *stream;
+     disassemble_info *info;
 {
   register int regnum = *p & 0xf;
   float floatlitbuf;
@@ -89,10 +95,10 @@ print_insn_arg (d, p, addr, stream)
   if (*d == 'b')
     {
       if (d[1] == 'b')
-	fprintf_unfiltered (stream, "0x%x", addr + *p++ + 1);
+	(*info->fprintf_func) (info->stream, "0x%x", addr + *p++ + 1);
       else
 	{
-	  fprintf_unfiltered (stream, "0x%x", addr + *(short *)p + 2);
+	  (*info->fprintf_func) (info->stream, "0x%x", addr + *(short *)p + 2);
 	  p += 2;
 	}
     }
@@ -106,130 +112,152 @@ print_insn_arg (d, p, addr, stream)
 	if (d[1] == 'd' || d[1] == 'f' || d[1] == 'g' || d[1] == 'h')
 	  {
 	    *(int *)&floatlitbuf = 0x4000 + ((p[-1] & 0x3f) << 4);
-	    fprintf_unfiltered (stream, "$%f", floatlitbuf);
+	    (*info->fprintf_func) (info->stream, "$%f", floatlitbuf);
 	  }
 	else
-	  fprintf_unfiltered (stream, "$%d", p[-1] & 0x3f);
+	  (*info->fprintf_func) (info->stream, "$%d", p[-1] & 0x3f);
 	break;
 
       case 4:			/* Indexed */
-	p = (char *) print_insn_arg (d, p, addr + 1, stream);
-	fprintf_unfiltered (stream, "[%s]", reg_names[regnum]);
+	p = (char *) print_insn_arg (d, p, addr + 1, info);
+	(*info->fprintf_func) (info->stream, "[%s]", reg_names[regnum]);
 	break;
 
       case 5:			/* Register */
-	fprintf_unfiltered (stream, reg_names[regnum]);
+	(*info->fprintf_func) (info->stream, reg_names[regnum]);
 	break;
 
       case 7:			/* Autodecrement */
-	fputc_unfiltered ('-', stream);
+	(*info->fprintf_func) (info->stream, "-");
       case 6:			/* Register deferred */
-	fprintf_unfiltered (stream, "(%s)", reg_names[regnum]);
+	(*info->fprintf_func) (info->stream, "(%s)", reg_names[regnum]);
 	break;
 
       case 9:			/* Autoincrement deferred */
-	fputc_unfiltered ('@', stream);
+	(*info->fprintf_func) (info->stream, "@");
 	if (regnum == PC_REGNUM)
 	  {
-	    fputc_unfiltered ('#', stream);
-	    print_address (*(long *)p, stream);
+	    (*info->fprintf_func) (info->stream, "#");
+	    info->target = *(long *)p;
+	    (*info->print_address_func) (info->target, info);
 	    p += 4;
 	    break;
 	  }
       case 8:			/* Autoincrement */
 	if (regnum == PC_REGNUM)
 	  {
-	    fputc_unfiltered ('#', stream);
+	    (*info->fprintf_func) (info->stream, "#");
 	    switch (d[1])
 	      {
 	      case 'b':
-		fprintf_unfiltered (stream, "%d", *p++);
+		(*info->fprintf_func) (info->stream, "%d", *p++);
 		break;
 
 	      case 'w':
-		fprintf_unfiltered (stream, "%d", *(short *)p);
+		(*info->fprintf_func) (info->stream, "%d", *(short *)p);
 		p += 2;
 		break;
 
 	      case 'l':
-		fprintf_unfiltered (stream, "%d", *(long *)p);
+		(*info->fprintf_func) (info->stream, "%d", *(long *)p);
 		p += 4;
 		break;
 
 	      case 'q':
-		fprintf_unfiltered (stream, "0x%x%08x", ((long *)p)[1], ((long *)p)[0]);
+		(*info->fprintf_func) (info->stream, "0x%x%08x",
+				       ((long *)p)[1], ((long *)p)[0]);
 		p += 8;
 		break;
 
 	      case 'o':
-		fprintf_unfiltered (stream, "0x%x%08x%08x%08x",
-			 ((long *)p)[3], ((long *)p)[2],
-			 ((long *)p)[1], ((long *)p)[0]);
+		(*info->fprintf_func) (info->stream, "0x%x%08x%08x%08x",
+				       ((long *)p)[3], ((long *)p)[2],
+				       ((long *)p)[1], ((long *)p)[0]);
 		p += 16;
 		break;
 
 	      case 'f':
 		if (INVALID_FLOAT (p, 4))
-		  fprintf_unfiltered (stream, "<<invalid float 0x%x>>", *(int *) p);
+		  (*info->fprintf_func) (info->stream,
+					 "<<invalid float 0x%x>>",
+					 *(int *) p);
 		else
-		  fprintf_unfiltered (stream, "%f", *(float *) p);
+		  (*info->fprintf_func) (info->stream, "%f", *(float *) p);
 		p += 4;
 		break;
 
 	      case 'd':
 		if (INVALID_FLOAT (p, 8))
-		  fprintf_unfiltered (stream, "<<invalid float 0x%x%08x>>",
-			   ((long *)p)[1], ((long *)p)[0]);
+		  (*info->fprintf_func) (info->stream,
+					 "<<invalid float 0x%x%08x>>",
+					 ((long *)p)[1], ((long *)p)[0]);
 		else
-		  fprintf_unfiltered (stream, "%f", *(double *) p);
+		  (*info->fprintf_func) (info->stream, "%f", *(double *) p);
 		p += 8;
 		break;
 
 	      case 'g':
-		fprintf_unfiltered (stream, "g-float");
+		(*info->fprintf_func) (info->stream, "g-float");
 		p += 8;
 		break;
 
 	      case 'h':
-		fprintf_unfiltered (stream, "h-float");
+		(*info->fprintf_func) (info->stream, "h-float");
 		p += 16;
 		break;
 
 	      }
 	  }
 	else
-	  fprintf_unfiltered (stream, "(%s)+", reg_names[regnum]);
+	  (*info->fprintf_func) (info->stream, "(%s)+", reg_names[regnum]);
 	break;
 
       case 11:			/* Byte displacement deferred */
-	fputc_unfiltered ('@', stream);
+	(*info->fprintf_func) (info->stream, "@");
       case 10:			/* Byte displacement */
 	if (regnum == PC_REGNUM)
-	  print_address (addr + *p + 2, stream);
+	  {
+	    info->target = addr + *p + 2;
+	    (*info->print_address_func) (info->target, info);
+	  }
 	else
-	  fprintf_unfiltered (stream, "%d(%s)", *p, reg_names[regnum]);
+	  (*info->fprintf_func) (info->stream, "%d(%s)", *p, reg_names[regnum]);
 	p += 1;
 	break;
 
       case 13:			/* Word displacement deferred */
-	fputc_unfiltered ('@', stream);
+	(*info->fprintf_func) (info->stream, "@");
       case 12:			/* Word displacement */
 	if (regnum == PC_REGNUM)
-	  print_address (addr + *(short *)p + 3, stream);
+	  {
+	    info->target = addr + *(short *)p + 3;
+	    (*info->print_address_func) (info->target, info);
+	  }
 	else
-	  fprintf_unfiltered (stream, "%d(%s)", *(short *)p, reg_names[regnum]);
+	  (*info->fprintf_func) (info->stream, "%d(%s)",
+				 *(short *)p, reg_names[regnum]);
 	p += 2;
 	break;
 
       case 15:			/* Long displacement deferred */
-	fputc_unfiltered ('@', stream);
+	(*info->fprintf_func) (info->stream, "@");
       case 14:			/* Long displacement */
 	if (regnum == PC_REGNUM)
-	  print_address (addr + *(long *)p + 5, stream);
+	  {
+	    info->target = addr + *(long *)p + 5;
+	    (*info->print_address_func) (info->target, info);
+	  }
 	else
-	  fprintf_unfiltered (stream, "%d(%s)", *(long *)p, reg_names[regnum]);
+	  (*info->fprintf_func) (info->stream, "%d(%s)",
+				 *(long *)p, reg_names[regnum]);
 	p += 4;
       }
 
   return (unsigned char *) p;
+}
+
+void
+_initialize_vax_tdep ()
+{
+  tm_print_insn = vax_print_insn;
 }
