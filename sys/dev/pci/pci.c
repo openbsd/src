9d8cac032383ff32b0c01f93ed6ea6e1c79fc9a8
matthieu <matthieu@openbsd.org>
2002-02-18 23:23:46 +0000

    Correct fix for breakage in rev 1.23. Sorry for the mess.

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci.c,v 1.24 2002/02/18 23:09:30 deraadt Exp $	*/
+/*	$OpenBSD: pci.c,v 1.25 2002/02/18 23:23:46 matthieu Exp $	*/
 /*	$NetBSD: pci.c,v 1.31 1997/06/06 23:48:04 thorpej Exp $	*/
 
 /*
@@ -427,14 +427,9 @@ pciioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 		goto done;
 	}
 	/* Check bounds */
-#ifdef PCI_USERCONF
-	if (pci.sc_bus >= 256 ||
-	    io->pi_sel.pc_dev >= pci_bus_maxdevs(pc, pci.sc_bus) ||
-		error = EINVAL;
-		goto done;
-	}
-#endif
-	if (io->pi_sel.pc_func >= 8) {
+	if (pci->sc_bus >= 256 || 
+	    io->pi_sel.pc_dev >= pci_bus_maxdevs(pc, pci->sc_bus) ||
+	    io->pi_sel.pc_func >= 8) {
 		error = EINVAL;
 		goto done;
 	}
