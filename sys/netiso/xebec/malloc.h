9a40df432a9237b4c3a6539bf0c1a348df2e1c05
deraadt <deraadt@openbsd.org>
2002-02-18 21:46:03 +0000

    bye bye.  i delete this not because it is disgusting, but because it is disgusting code in the wrong place

--- sys/netiso/xebec/malloc.h
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: malloc.h,v 1.2 1996/03/04 10:37:13 mickey Exp $	*/
-/*	$NetBSD: malloc.h,v 1.4 1994/06/29 06:41:11 cgd Exp $	*/
-
-char *Malloc();
--- sys/netiso/xebec/procs.h
+++
@@ -1,5 +0,0 @@
-/*	$OpenBSD: procs.h,v 1.2 1996/03/04 10:37:18 mickey Exp $	*/
-/*	$NetBSD: procs.h,v 1.4 1994/06/29 06:41:13 cgd Exp $	*/
-
-extern char *stash();
-extern struct Object *SameState;
