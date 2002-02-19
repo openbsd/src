f48d33243a1d2f11e318dcfd9484a2c096f96846
itojun <itojun@openbsd.org>
2002-02-19 02:34:10 +0000

    sizeof pedant. From: Mike Pechkin <mpech@prosoft.org.lv>

--- usr.sbin/traceroute6/traceroute6.c
+++ usr.sbin/traceroute6/traceroute6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: traceroute6.c,v 1.17 2002/02/16 21:28:10 millert Exp $	*/
+/*	$OpenBSD: traceroute6.c,v 1.18 2002/02/19 02:34:10 itojun Exp $	*/
 /*	$KAME: traceroute6.c,v 1.39 2000/12/22 15:11:05 itojun Exp $	*/
 
 /*
@@ -1316,7 +1316,7 @@ inetname(sa)
 
 	if (first && !nflag) {
 		first = 0;
-		if (gethostname(domain, MAXHOSTNAMELEN) == 0 &&
+		if (gethostname(domain, sizeof(domain)) == 0 &&
 		    (cp = index(domain, '.')))
 			(void) strlcpy(domain, cp + 1, sizeof(domain));
 		else
