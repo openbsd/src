7f45b655e71914f1f8e21da58826f28f6df66af5
ericj <ericj@openbsd.org>
2002-02-19 00:06:34 +0000

    oops, left out ;

--- usr.bin/vi/common/exf.c
+++ usr.bin/vi/common/exf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exf.c,v 1.16 2002/02/18 23:56:10 ericj Exp $	*/
+/*	$OpenBSD: exf.c,v 1.17 2002/02/19 00:06:34 ericj Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993, 1994
@@ -989,7 +989,7 @@ file_write(sp, fm, tm, name, flags)
 			*--s = '.';
 		}
 	}
-	msgq(sp, M_INFO, "%s", s)
+	msgq(sp, M_INFO, "%s", s);
 	if (nf)
 		FREE_SPACE(sp, p, 0);
 	return (0);
