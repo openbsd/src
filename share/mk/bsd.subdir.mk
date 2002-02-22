d5b2db31d7eedf53ae0ec03a62813eee97cc02e7
art <art@openbsd.org>
2002-02-22 23:54:08 +0000

    If there is no regress target provide one with _SUBDIRUSE.

--- share/mk/bsd.subdir.mk
+++ share/mk/bsd.subdir.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.subdir.mk,v 1.10 1998/03/01 09:18:06 niklas Exp $
+#	$OpenBSD: bsd.subdir.mk,v 1.11 2002/02/22 23:54:08 art Exp $
 #	$NetBSD: bsd.subdir.mk,v 1.11 1996/04/04 02:05:06 jtc Exp $
 #	@(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
 
@@ -104,6 +104,10 @@ obj: _SUBDIRUSE
 
 .if !target(tags)
 tags: _SUBDIRUSE
+.endif
+
+.if !target(regress)
+regress: _SUBDIRUSE
 .endif
 
 .include <bsd.own.mk>
