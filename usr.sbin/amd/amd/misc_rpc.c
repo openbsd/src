a18b76084e2724eaacba545182c5ad2d96871d3f
deraadt <deraadt@openbsd.org>
2002-02-23 09:18:27 +0000

    remove things we do not need

--- usr.sbin/amd/amd/misc_rpc.c
+++ usr.sbin/amd/amd/misc_rpc.c
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)misc_rpc.c	8.1 (Berkeley) 6/6/93
- *	$Id: misc_rpc.c,v 1.3 2002/02/13 22:43:44 pvalchev Exp $
+ *	$Id: misc_rpc.c,v 1.4 2002/02/23 09:18:27 deraadt Exp $
  */
 
 /*
@@ -45,8 +45,13 @@
 
 #include "am.h"
 
-void rpc_msg_init P((struct rpc_msg *mp, u_long prog, u_long vers, u_long proc));
-void rpc_msg_init(mp, prog, vers, proc)
+void	rpc_msg_init P((struct rpc_msg *mp, u_long prog, u_long vers, u_long proc));
+int	pickup_rpc_reply P((voidp pkt, int len, voidp where, xdrproc_t where_xdr));
+int	make_rpc_packet P((char *buf, int buflen, unsigned long proc,
+	    struct rpc_msg *mp, voidp arg, xdrproc_t arg_xdr, AUTH *auth));
+
+void
+rpc_msg_init(mp, prog, vers, proc)
 struct rpc_msg *mp;
 unsigned long prog, vers, proc;
 {
@@ -65,8 +70,8 @@ unsigned long prog, vers, proc;
 /*
  * Field reply to call to mountd
  */
-int pickup_rpc_reply P((voidp pkt, int len, voidp where, xdrproc_t where_xdr));
-int pickup_rpc_reply(pkt, len, where, where_xdr)
+int
+pickup_rpc_reply(pkt, len, where, where_xdr)
 voidp pkt;
 int len;
 voidp where;
@@ -109,9 +114,8 @@ drop:
 	return error;
 }
 
-int make_rpc_packet P((char *buf, int buflen, unsigned long proc,
-			struct rpc_msg *mp, voidp arg, xdrproc_t arg_xdr, AUTH *auth));
-int make_rpc_packet(buf, buflen, proc, mp, arg, arg_xdr, auth)
+int
+make_rpc_packet(buf, buflen, proc, mp, arg, arg_xdr, auth)
 char *buf;
 int buflen;
 unsigned long proc;
@@ -154,178 +158,3 @@ AUTH *auth;
 	xdr_destroy(&msg_xdr);
 	return len;
 }
-
-
-/*
- * Early RPC seems to be missing these..
- * Extracted from the RPC 3.9 sources as indicated
- */
-
-#ifdef NEED_XDR_POINTER
-/* @(#)xdr_reference.c	1.1 87/11/04 3.9 RPCSRC */
-/*
- * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
- * unrestricted use provided that this legend is included on all tape
- * media and as a part of the software program in whole or part.  Users
- * may copy or modify Sun RPC without charge, but are not authorized
- * to license or distribute it to anyone else except as part of a product or
- * program developed by the user.
- * 
- * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE
- * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.
- * 
- * Sun RPC is provided with no support and without any obligation on the
- * part of Sun Microsystems, Inc. to assist in its use, correction,
- * modification or enhancement.
- * 
- * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE
- * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC
- * OR ANY PART THEREOF.
- * 
- * In no event will Sun Microsystems, Inc. be liable for any lost revenue
- * or profits or other special, indirect and consequential damages, even if
- * Sun has been advised of the possibility of such damages.
- * 
- * Sun Microsystems, Inc.
- * 2550 Garcia Avenue
- * Mountain View, California  94043
- */
-
-
-/*
- * xdr_pointer():
- *
- * XDR a pointer to a possibly recursive data structure. This
- * differs with xdr_reference in that it can serialize/deserialiaze
- * trees correctly.
- *
- *  What's sent is actually a union:
- *
- *  union object_pointer switch (boolean b) {
- *  case TRUE: object_data data;
- *  case FALSE: void nothing;
- *  }
- *
- * > objpp: Pointer to the pointer to the object.
- * > obj_size: size of the object.
- * > xdr_obj: routine to XDR an object.
- *
- */
-bool_t
-xdr_pointer(xdrs,objpp,obj_size,xdr_obj)
-	register XDR *xdrs;
-	char **objpp;
-	u_int obj_size;
-	xdrproc_t xdr_obj;
-{
-
-	bool_t more_data;
-
-	more_data = (*objpp != NULL);
-	if (! xdr_bool(xdrs,&more_data)) {
-		return (FALSE);
-	}
-	if (! more_data) {
-		*objpp = NULL;
-		return (TRUE);
-	}
-	return (xdr_reference(xdrs,objpp,obj_size,xdr_obj));
-}
-#endif /* NEED_XDR_POINTER */
-
-#ifdef NEED_CLNT_SPERRNO
-/* @(#)clnt_perror.c	1.1 87/11/04 3.9 RPCSRC */
-/*
- * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
- * unrestricted use provided that this legend is included on all tape
- * media and as a part of the software program in whole or part.  Users
- * may copy or modify Sun RPC without charge, but are not authorized
- * to license or distribute it to anyone else except as part of a product or
- * program developed by the user.
- * 
- * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE
- * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.
- * 
- * Sun RPC is provided with no support and without any obligation on the
- * part of Sun Microsystems, Inc. to assist in its use, correction,
- * modification or enhancement.
- * 
- * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE
- * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC
- * OR ANY PART THEREOF.
- * 
- * In no event will Sun Microsystems, Inc. be liable for any lost revenue
- * or profits or other special, indirect and consequential damages, even if
- * Sun has been advised of the possibility of such damages.
- * 
- * Sun Microsystems, Inc.
- * 2550 Garcia Avenue
- * Mountain View, California  94043
- */
-
-struct rpc_errtab {
-	enum clnt_stat status;
-	char *message;
-};
-
-static struct rpc_errtab  rpc_errlist[] = {
-	{ RPC_SUCCESS, 
-		"RPC: Success" }, 
-	{ RPC_CANTENCODEARGS, 
-		"RPC: Can't encode arguments" },
-	{ RPC_CANTDECODERES, 
-		"RPC: Can't decode result" },
-	{ RPC_CANTSEND, 
-		"RPC: Unable to send" },
-	{ RPC_CANTRECV, 
-		"RPC: Unable to receive" },
-	{ RPC_TIMEDOUT, 
-		"RPC: Timed out" },
-	{ RPC_VERSMISMATCH, 
-		"RPC: Incompatible versions of RPC" },
-	{ RPC_AUTHERROR, 
-		"RPC: Authentication error" },
-	{ RPC_PROGUNAVAIL, 
-		"RPC: Program unavailable" },
-	{ RPC_PROGVERSMISMATCH, 
-		"RPC: Program/version mismatch" },
-	{ RPC_PROCUNAVAIL, 
-		"RPC: Procedure unavailable" },
-	{ RPC_CANTDECODEARGS, 
-		"RPC: Server can't decode arguments" },
-	{ RPC_SYSTEMERROR, 
-		"RPC: Remote system error" },
-	{ RPC_UNKNOWNHOST, 
-		"RPC: Unknown host" },
-/*	{ RPC_UNKNOWNPROTO,
-		"RPC: Unknown protocol" },*/
-	{ RPC_PMAPFAILURE, 
-		"RPC: Port mapper failure" },
-	{ RPC_PROGNOTREGISTERED, 
-		"RPC: Program not registered"},
-	{ RPC_FAILED, 
-		"RPC: Failed (unspecified error)"}
-};
-
-
-/*
- * This interface for use by clntrpc
- */
-char *
-clnt_sperrno(stat)
-	enum clnt_stat stat;
-{
-	int i;
-
-	for (i = 0; i < sizeof(rpc_errlist)/sizeof(struct rpc_errtab); i++) {
-		if (rpc_errlist[i].status == stat) {
-			return (rpc_errlist[i].message);
-		}
-	}
-	return ("RPC: (unknown error code)");
-}
-
-#endif /* NEED_CLNT_SPERRNO */
-
