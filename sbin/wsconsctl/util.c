a1966d470b664e0c5ec96eba4f811ac5f55e129b
jason <jason@openbsd.org>
2002-02-23 05:44:20 +0000

    add sun keyboard type

--- sbin/wsconsctl/util.c
+++ sbin/wsconsctl/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.7 2002/02/16 21:27:38 millert Exp $ */
+/*	$OpenBSD: util.c,v 1.8 2002/02/23 05:44:20 jason Exp $ */
 /*	$NetBSD: util.c,v 1.8 2000/03/14 08:11:53 sato Exp $ */
 
 /*-
@@ -65,6 +65,7 @@ static const struct nameint kbtype_tab[] = {
 	{ WSKBD_TYPE_HPC_KBD,	"hpc-kbd" },
 	{ WSKBD_TYPE_HPC_BTN,	"hpc-btn" },
 	{ WSKBD_TYPE_ADB,	"adb" },
+	{ WSKBD_TYPE_SUN,	"sun" },
 };
 
 static const struct nameint mstype_tab[] = {
