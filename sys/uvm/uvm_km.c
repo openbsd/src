efc6a7aad8ca2748629f0bfefa96e2b8722e67bc
art <art@openbsd.org>
2002-03-06 22:05:31 +0000

    Some architectures don't allocate any memory out of the kernel_map space before uvm_km_init (alpha). Don't uvm_map 0 space, it gives very strange breakage on alpha.

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.27 2001/12/19 08:58:07 art Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.28 2002/03/06 22:05:31 art Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
@@ -234,8 +234,8 @@ uvm_km_init(start, end)
 
 	uvm_map_setup(&kernel_map_store, base, end, VM_MAP_PAGEABLE);
 	kernel_map_store.pmap = pmap_kernel();
-	if (uvm_map(&kernel_map_store, &base, start - base, NULL,
-	    UVM_UNKNOWN_OFFSET, 0, UVM_MAPFLAG(UVM_PROT_ALL, UVM_PROT_ALL,
+	if (base != start && uvm_map(&kernel_map_store, &base, start - base,
+	    NULL, UVM_UNKNOWN_OFFSET, 0, UVM_MAPFLAG(UVM_PROT_ALL, UVM_PROT_ALL,
 	    UVM_INH_NONE, UVM_ADV_RANDOM,UVM_FLAG_FIXED)) != KERN_SUCCESS)
 		panic("uvm_km_init: could not reserve space for kernel");
 	
