6237bbaf5e63a2b7d2e869c601e6c40dfa649448
markus <markus@openbsd.org>
2002-03-06 00:25:55 +0000

    OpenSSH_3.1

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.27 2001/12/05 15:04:48 markus Exp $ */
+/* $OpenBSD: version.h,v 1.28 2002/03/06 00:25:55 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_3.0.2"
+#define SSH_VERSION	"OpenSSH_3.1"
