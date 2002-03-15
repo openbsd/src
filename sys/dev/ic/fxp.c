87a60d476683d667e2c5b29164581bdc863ccdfa
art <art@openbsd.org>
2002-03-15 22:15:09 +0000

    Missing splx

--- sys/dev/ic/fxp.c
+++ sys/dev/ic/fxp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fxp.c,v 1.32 2002/03/14 01:26:54 millert Exp $	*/
+/*	$OpenBSD: fxp.c,v 1.33 2002/03/15 22:15:09 art Exp $	*/
 /*	$NetBSD: if_fxp.c,v 1.2 1997/06/05 02:01:55 thorpej Exp $	*/
 
 /*
@@ -928,6 +928,7 @@ fxp_stats_update(arg)
 	if (sc->rx_idle_secs > FXP_MAX_RX_IDLE) {
 		sc->rx_idle_secs = 0;
 		fxp_init(sc);
+		splx(s);
 		return;
 	}
 	/*
