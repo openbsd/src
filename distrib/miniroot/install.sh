6e0db516f28fe30c05d949e8493b2303c0b5d9ea
krw <krw@openbsd.org>
2002-03-17 03:04:47 +0000

    Examine correct fields in mount(8) output to determine if root is nfs mounted, i.e. install is running on a diskless machine.
    ok millert@

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.84 2002/03/04 13:59:52 krw Exp $
+#	$OpenBSD: install.sh,v 1.85 2002/03/17 03:04:47 krw Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2002 Todd Miller, Theo de Raadt, Ken Westerback
@@ -329,7 +329,7 @@ mount_fs "-o async"
 
 mount | while read line; do
 	set -- $line
-	if [ "$2" = "/" -a "$3" = "nfs" ]; then
+	if [ "$3" = "/" -a "$5" = "nfs" ]; then
 		echo "You appear to be running diskless."
 		echo -n	"Are the install sets on one of your currently mounted filesystems? [n] "
 		getresp "n"
