08690c222795a5bd9697f4f2c2b741b70eb1078d
art <art@openbsd.org>
2002-03-17 18:38:43 +0000

    In PT_{GET,SET}REGS there is no reason to not get/set %l* and %i*. This change will break debuggers, so if you have any, rebuild.

--- sys/arch/sparc64/include/reg.h
+++ sys/arch/sparc64/include/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.1.1.1 2001/08/18 04:16:40 jason Exp $	*/
+/*	$OpenBSD: reg.h,v 1.2 2002/03/17 18:38:43 art Exp $	*/
 /*	$NetBSD: reg.h,v 1.8 2001/06/19 12:59:16 wiz Exp $ */
 
 /*
@@ -129,8 +129,10 @@ struct reg64 {
 	int64_t	r_pc;		/* return pc */
 	int64_t	r_npc;		/* return npc */
 	int	r_y;		/* %y register -- 32-bits */
-	int64_t	r_global[8];	/* global registers in trap's caller */
-	int64_t	r_out[8];	/* output registers in trap's caller */
+	int64_t	r_global[8];	/* %g* registers in trap's caller */
+	int64_t	r_out[8];	/* %o* registers in trap's caller */
+	int64_t r_local[8];	/* %l* registers in trap's caller */
+	int64_t r_in[8];	/* %i* registers in trap's caller */
 };
 
 #include <machine/fsr.h>
--- sys/arch/sparc64/sparc64/process_machdep.c
+++ sys/arch/sparc64/sparc64/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: process_machdep.c,v 1.3 2002/03/14 00:42:24 miod Exp $	*/
+/*	$OpenBSD: process_machdep.c,v 1.4 2002/03/17 18:38:43 art Exp $	*/
 /*	$NetBSD: process_machdep.c,v 1.10 2000/09/26 22:05:50 eeh Exp $ */
 
 /*
@@ -97,6 +97,8 @@ process_read_regs(p, regs)
 		for (i = 0; i < 8; i++) {
 			regs->r_global[i] = tf->tf_global[i];
 			regs->r_out[i] = tf->tf_out[i];
+			regs->r_local[i] = tf->tf_local[i];
+			regs->r_in[i] = tf->tf_in[i];
 		}
 		return (0);
 	}
