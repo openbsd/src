0bd7eeae9e3f6234a47d2e7f274fb8fed4454f3b
millert <millert@openbsd.org>
2002-03-19 17:51:04 +0000

    Fix bus_space_copy_region_ macro that got messed up by unpeeing. miod@ OK

--- sys/arch/mac68k/include/bus.h
+++ sys/arch/mac68k/include/bus.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bus.h,v 1.7 2002/03/14 01:26:35 millert Exp $	*/
+/*	$OpenBSD: bus.h,v 1.8 2002/03/19 17:51:04 millert Exp $	*/
 /*	$NetBSD: bus.h,v 1.9 1998/01/13 18:32:15 scottr Exp $	*/
 
 /*-
@@ -543,7 +543,8 @@ int	mac68k_bus_space_probe(bus_space_tag_t t,
  */
 
 #define	__MAC68K_copy_region_N(BYTES)					\
-static __inline void __CONCAT(bus_space_copy_region_,BYTES)		\(bus_space_tag_t,						\
+static __inline void __CONCAT(bus_space_copy_region_,BYTES)		\
+	    (bus_space_tag_t,						\
 	    bus_space_handle_t bsh1, bus_size_t off1,			\
 	    bus_space_handle_t bsh2, bus_size_t off2,			\
 	    bus_size_t count);						\
