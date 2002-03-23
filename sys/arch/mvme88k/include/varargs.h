c6a43f76b8f2824a9d5249120e1f4c2c92a0fcee
miod <miod@openbsd.org>
2002-03-23 10:40:27 +0000

    Repair.

--- sys/arch/mvme88k/include/varargs.h
+++ sys/arch/mvme88k/include/varargs.h
@@ -1,12 +1,14 @@
-/*	$OpenBSD: varargs.h,v 1.5 2001/11/16 01:17:21 miod Exp $	*/
+/*	$OpenBSD: varargs.h,v 1.6 2002/03/23 10:40:27 miod Exp $	*/
 
 #ifndef _M88K_VARARGS_H_
 #define _M88K_VARARGS_H_
+
+#define	_VARARGS_H
 
 #include <machine/va-m88k.h>
 
 /* Define va_list from __gnuc_va_list.  */
 typedef	__gnuc_va_list	va_list;
-typedef	_BSD_VA_LIST_	__gnuc_va_list
+typedef	_BSD_VA_LIST_	__gnuc_va_list;
 
 #endif	/* _M88K_VARARGS_H_ */
