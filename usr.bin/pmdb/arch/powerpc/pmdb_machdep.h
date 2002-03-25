277c5ac26f8d44ecaa9ef0f8db0653509f054565
drahn <drahn@openbsd.org>
2002-03-25 22:41:51 +0000

    Make this closer to supporting breakpoints properly, still not working.

--- usr.bin/pmdb/arch/powerpc/pmdb_machdep.h
+++ usr.bin/pmdb/arch/powerpc/pmdb_machdep.h
@@ -1,3 +1,3 @@
-#define BREAKPOINT              { 0x00, 0x00, 0x00, 0x00 }
+#define BREAKPOINT              { 0x7C, 0x81, 0x08, 0x08 }
 #define BREAKPOINT_LEN          4
-#define BREAKPOINT_DECR_PC      0
+#define BREAKPOINT_DECR_PC      4
