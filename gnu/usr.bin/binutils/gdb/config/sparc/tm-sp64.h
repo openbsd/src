f9fd7347897f201c5529292cb987bbba1074fcf1
fgsch <fgsch@openbsd.org>
2002-03-31 20:54:57 +0000

    define GDB_TARGET_IS_SPARC64 to 1; from gdb-current. helps back porting sparc-tdep.c stuff needed by sparc64 gdb.

--- gnu/usr.bin/binutils/gdb/config/sparc/tm-sp64.h
+++ gnu/usr.bin/binutils/gdb/config/sparc/tm-sp64.h
@@ -20,7 +20,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#define GDB_TARGET_IS_SPARC64
+#ifndef GDB_TARGET_IS_SPARC64
+#define GDB_TARGET_IS_SPARC64 1
+#define
 
 #include "sparc/tm-sparc.h"
 
