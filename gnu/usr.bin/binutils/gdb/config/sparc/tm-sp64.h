350b326681b491f56254f1ad4b77940d8416bc8f
fgsch <fgsch@openbsd.org>
2002-03-31 21:53:55 +0000

    fixed my previous commit.

--- gnu/usr.bin/binutils/gdb/config/sparc/tm-sp64.h
+++ gnu/usr.bin/binutils/gdb/config/sparc/tm-sp64.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #ifndef GDB_TARGET_IS_SPARC64
 #define GDB_TARGET_IS_SPARC64 1
-#define
+#endif
 
 #include "sparc/tm-sparc.h"
 
