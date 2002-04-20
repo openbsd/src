ee75288f94671868d19e4181f524d87da8388cd6
tholo <tholo@openbsd.org>
2002-04-20 03:37:40 +0000

    Add missing copyright

--- usr.bin/gprof/i386.c
+++ usr.bin/gprof/i386.c
@@ -1,17 +1,41 @@
-/*	$OpenBSD: i386.c,v 1.5 2002/03/25 16:30:55 danh Exp $	*/
+/*	$OpenBSD: i386.c,v 1.6 2002/04/20 03:37:40 tholo Exp $	*/
 /*	$NetBSD: i386.c,v 1.5 1995/04/19 07:16:04 cgd Exp $	*/
 
+/*-
+ * Copyright (c) 1996 SigmaSoft, Th. Lockert
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef lint
-static char rcsid[] = "$OpenBSD: i386.c,v 1.5 2002/03/25 16:30:55 danh Exp $";
+static char rcsid[] = "$OpenBSD: i386.c,v 1.6 2002/04/20 03:37:40 tholo Exp $";
 #endif /* not lint */
 
 #include "gprof.h"
 
 #define	iscall(pc)	((*pc) == 0xE8)
 
-/*
- * gprof -c isn't currently supported...
- */
 void
 findcall( parentp , p_lowpc , p_highpc )
     nltype		*parentp;
