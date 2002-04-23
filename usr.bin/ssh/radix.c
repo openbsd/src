1d5426d384e14b151f3111ae5e1e7bb335373077
markus <markus@openbsd.org>
2002-04-23 12:58:26 +0000

    send complete ticket; semerad@ss1000.ms.mff.cuni.cz

--- usr.bin/ssh/radix.c
+++ usr.bin/ssh/radix.c
@@ -26,7 +26,7 @@
 #include "includes.h"
 #include "uuencode.h"
 
-RCSID("$OpenBSD: radix.c,v 1.19 2002/04/22 06:15:47 markus Exp $");
+RCSID("$OpenBSD: radix.c,v 1.20 2002/04/23 12:58:26 markus Exp $");
 
 #ifdef AFS
 #include <krb.h>
@@ -65,8 +65,7 @@ creds_to_radix(CREDENTIALS *creds, u_char *buf, size_t buflen)
 	buffer_put_short(&b, creds->kvno);
 
 	/* 32 bit size + data */
-	buffer_put_string(&b, creds->ticket_st.dat,
-	    sizeof(creds->ticket_st.length));
+	buffer_put_string(&b, creds->ticket_st.dat, creds->ticket_st.length);
 
 	ret = uuencode(buffer_ptr(&b), buffer_len(&b), (char *)buf, buflen);
 
