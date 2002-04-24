e46c50c0bfc26004b0ade4fde2891131b0abc386
dhartmei <dhartmei@openbsd.org>
2002-04-24 16:42:59 +0000

    Initialize if_addrhooks in if_attachhead() like in if_attach(), either one may be called.

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.52 2002/04/24 00:51:51 dhartmei Exp $	*/
+/*	$OpenBSD: if.c,v 1.53 2002/04/24 16:42:59 dhartmei Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -248,6 +248,10 @@ if_attachhead(ifp)
 	if (if_index == 0)
 		TAILQ_INIT(&ifnet);
 	TAILQ_INIT(&ifp->if_addrlist);
+	ifp->if_addrhooks = malloc(sizeof(*ifp->if_addrhooks), M_TEMP, M_NOWAIT);
+	if (ifp->if_addrhooks == NULL)
+		panic("if_attachhead: malloc");
+	TAILQ_INIT(ifp->if_addrhooks);
 	TAILQ_INSERT_HEAD(&ifnet, ifp, if_list);
 	if_attachsetup(ifp);
 }
