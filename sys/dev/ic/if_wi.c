2f3c57334951f4486913d4a4a31d207d065e5413
mickey <mickey@openbsd.org>
2002-04-30 22:56:33 +0000

    crc32_tab can be const

--- sys/dev/ic/if_wi.c
+++ sys/dev/ic/if_wi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_wi.c,v 1.59 2002/04/26 21:27:27 millert Exp $	*/
+/*	$OpenBSD: if_wi.c,v 1.60 2002/04/30 22:56:33 mickey Exp $	*/
 
 /*
  * Copyright (c) 1997, 1998, 1999
@@ -124,7 +124,7 @@ u_int32_t	widebug = WIDEBUG;
 
 #if !defined(lint) && !defined(__OpenBSD__)
 static const char rcsid[] =
-	"$OpenBSD: if_wi.c,v 1.59 2002/04/26 21:27:27 millert Exp $";
+	"$OpenBSD: if_wi.c,v 1.60 2002/04/30 22:56:33 mickey Exp $";
 #endif	/* lint */
 
 #ifdef foo
@@ -1674,7 +1674,7 @@ wi_init(sc)
 	return;
 }
 
-static u_int32_t crc32_tab[] = {
+static const u_int32_t crc32_tab[] = {
 	0x00000000L, 0x77073096L, 0xee0e612cL, 0x990951baL, 0x076dc419L,
 	0x706af48fL, 0xe963a535L, 0x9e6495a3L, 0x0edb8832L, 0x79dcb8a4L,
 	0xe0d5e91eL, 0x97d2d988L, 0x09b64c2bL, 0x7eb17cbdL, 0xe7b82d07L,
