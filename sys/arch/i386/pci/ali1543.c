27a89d582f16cbccb9b25b12ea336daa54d44e7f
mickey <mickey@openbsd.org>
2002-05-02 17:46:00 +0000

    update the nbsd rcsid to unconfuse some lamers

--- sys/arch/i386/pci/ali1543.c
+++ sys/arch/i386/pci/ali1543.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: ali1543.c,v 1.1 2001/10/25 19:03:49 mickey Exp $	*/
-/*	$NetBSD: ali1543.c,v 1.1 2001/08/27 08:21:21 haya Exp $	*/
+/*	$OpenBSD: ali1543.c,v 1.2 2002/05/02 17:46:00 mickey Exp $	*/
+/*	$NetBSD: ali1543.c,v 1.2 2001/09/13 14:00:52 tshiozak Exp $	*/
 
 /*
  * Copyright (c) 2001
