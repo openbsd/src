7204604f1a1d40584e752f3da664f0f2262ea5f0
matthieu <matthieu@openbsd.org>
2002-05-05 17:16:39 +0000

    fix panic triggered by using a 3.0 wsmoused on a 3.1 kernel, by checking that wsms_dev is not NULL before dereferencing it. ok jbm@

--- sys/dev/wscons/wsdisplay.c
+++ sys/dev/wscons/wsdisplay.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: wsdisplay.c,v 1.44 2002/04/30 23:07:48 mickey Exp $ */
+/* $OpenBSD: wsdisplay.c,v 1.45 2002/05/05 17:16:39 matthieu Exp $ */
 /* $NetBSD: wsdisplay.c,v 1.37.4.1 2000/06/30 16:27:53 simonb Exp $ */
 
 /*
@@ -3199,8 +3199,10 @@ wsmoused_release(struct wsdisplay_softc *sc)
 		}
 
 		/* inject event to notify wsmoused(8) to close mouse device */
-		wsmouse_input(wsms_dev, 0, 0, 0, 0,
-		    WSMOUSE_INPUT_WSMOUSED_CLOSE);
+		if (wsms_dev != NULL) 
+			wsmouse_input(wsms_dev, 0, 0, 0, 0,
+				      WSMOUSE_INPUT_WSMOUSED_CLOSE);
+		
 	}
 #endif /* NWSMOUSE > 0 */
 }
