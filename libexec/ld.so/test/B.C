8a5fdb2fd26a78ec343803ba8ecedf3c4bcadbc1
deraadt <deraadt@openbsd.org>
2002-05-23 23:16:28 +0000

    This stuff has no license.  Delete it.  If someone wants it back, get the licenses cleaned up, and put it in regress

--- libexec/ld.so/test/B.C
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: B.C,v 1.2 2001/01/28 19:34:29 niklas Exp $	*/
-
-#include "iostream.h"
-#include "a.h"
-AA e("e");;
-AA f("f");;
-/*
-AA f;
-*/
--- libexec/ld.so/test/tst.C
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: tst.C,v 1.3 2001/01/28 19:34:29 niklas Exp $	*/
-
-#include "iostream.h"
-#include "a.h"
-
-AA c("c");
-main()
-{
-}
