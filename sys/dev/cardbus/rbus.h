fd0e040ab6fd3ac3a5ee98cb9831fb10b198a2db
tdeval <tdeval@openbsd.org>
2002-05-27 23:39:55 +0000

    typo

--- sys/dev/cardbus/rbus.h
+++ sys/dev/cardbus/rbus.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rbus.h,v 1.2 2002/03/14 01:26:53 millert Exp $ */
+/*	$OpenBSD: rbus.h,v 1.3 2002/05/27 23:39:55 tdeval Exp $ */
 /*	$NetBSD: rbus.h,v 1.3 1999/12/15 12:28:55 kleink Exp $	*/
 /*
  * Copyright (c) 1999
@@ -42,7 +42,7 @@
  * 
  *  Ths rbus is a recursive bus-space administrator.  This means a
  *  parent bus-space administrator, which usually belongs to a bus
- *  bridge, makes some child bus-space administorators and gives
+ *  bridge, makes some child bus-space administrators and gives
  *  (restricted) bus-space for children.  There are a root bus-space
  *  administrator which maintains whole bus-space.
  *
