b94c7489467faa32b6206bdfebebae8cde4fc97d
art <art@openbsd.org>
2002-06-06 15:43:04 +0000

    Implement _atomic_lock in assembler to make it more readable. plus some various assembler improvements. Tested by various people a few months ago and in my tree for almost 6 months now.

--- lib/libc_r/arch/alpha/_atomic_lock.c
+++ lib/libc_r/arch/alpha/_atomic_lock.c
@@ -1,42 +1,11 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.5 1999/05/26 00:11:27 d Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.6 2002/06/06 15:43:04 art Exp $	*/
 /*
- * Atomic lock for alpha
+ * Atomi lock for alpha.
  */
 
 #include "spinlock.h"
 
-int
-_atomic_lock(volatile _spinlock_lock_t * lock)
-{
-	_spinlock_lock_t old;
-	_spinlock_lock_t new;
-	int success;
-
-	do {
-		/* load the value of the thread-lock (lock mem on load) */
-		__asm__( "ldq_l %0, %1" : "=r"(old) : "m"(*lock) );
-		if (old) 
-			new = old;	/* locked: no change */
-		else
-			new = _SPINLOCK_LOCKED;	/* unlocked: grab it */
-
-		success = 0;
-		/* store the new value of the thrd-lock (unlock mem on store) */
-		/*
-		 * XXX may need to add *large* branch forward for main line
-		 * branch prediction to be right :( [this note from linux]
-		 */
-		__asm__( "stq_c	%2, %0\n"
-			 "beq	%2, 1f\n"
-			 "mb\n"		
-			 "mov	1, %1\n"
-			 "1:"
-			: "=m"(*lock), "=r"(success)
-			: "r"(new) );
-	} while (!success);
-
-	return (old != _SPINLOCK_UNLOCKED);
-}
+/* _atomic lock is implemented in assembler. */
 
 int
 _atomic_is_locked(volatile _spinlock_lock_t * lock)
