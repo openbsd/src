17ccdb6de150df2373df08cc990ed73f2eb0071e
art <art@openbsd.org>
2002-06-08 04:44:01 +0000

    Let this build when the internal DIAGNOSTIC define is removed.

--- sys/ufs/ffs/ffs_softdep.c
+++ sys/ufs/ffs/ffs_softdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ffs_softdep.c,v 1.37 2002/03/14 01:27:14 millert Exp $	*/
+/*	$OpenBSD: ffs_softdep.c,v 1.38 2002/06/08 04:44:01 art Exp $	*/
 /*
  * Copyright 1998, 2000 Marshall Kirk McKusick. All Rights Reserved.
  *
@@ -3206,7 +3206,9 @@ initiate_write_inodeblock(inodedep, bp)
 	struct allocdirect *adp, *lastadp;
 	struct dinode *dp;
 	struct fs *fs;
+#ifdef DIAGNOSTIC
 	ufs_lbn_t prevlbn = 0;
+#endif
 	int i, deplist;
 
 	if (inodedep->id_state & IOSTARTED)
