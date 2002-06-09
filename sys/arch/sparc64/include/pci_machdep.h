b5f5d0608bd6db6e2436afb64980cf110f0ce932
jason <jason@openbsd.org>
2002-06-09 23:28:21 +0000

    2^5 not 5, duh, sorry matthieu

--- sys/arch/sparc64/include/pci_machdep.h
+++ sys/arch/sparc64/include/pci_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_machdep.h,v 1.8 2002/06/09 05:26:12 jason Exp $	*/
+/*	$OpenBSD: pci_machdep.h,v 1.9 2002/06/09 23:28:21 jason Exp $	*/
 /* $NetBSD: pci_machdep.h,v 1.7 2001/07/20 00:07:14 eeh Exp $ */
 
 /*
@@ -67,7 +67,7 @@ struct sparc_pci_chipset {
 
 #define	PCITAG_NODE(x)		(int)(((x)>>32)&0xffffffff)
 #define	PCITAG_BUS(t)		(((t) >> 16) & 0xff)
-#define	PCITAG_DEV(t)		(((t) >> 11) & 0x05)
+#define	PCITAG_DEV(t)		(((t) >> 11) & 0x1f)
 #define	PCITAG_FUNC(t)		(((t) >>  8) & 0x07)
 #define	PCITAG_OFFSET(x)	((x)&0xffffffff)
 #define	PCITAG_CREATE(n,b,d,f)	(((u_int64_t)(n)<<32)|((b)<<16)|((d)<<11)|((f)<<8))
