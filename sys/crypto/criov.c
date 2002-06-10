f4129a2116e04f112cd32cdbe9726a7149ef0ef5
espie <espie@openbsd.org>
2002-06-10 19:36:43 +0000

    __FUNCTION__ -> __func__

--- sys/crypto/criov.c
+++ sys/crypto/criov.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: criov.c,v 1.10 2002/03/01 02:46:57 provos Exp $	*/
+/*      $OpenBSD: criov.c,v 1.11 2002/06/10 19:36:43 espie Exp $	*/
 
 /*
  * Copyright (c) 1999 Theo de Raadt
@@ -144,9 +144,9 @@ cuio_apply(struct uio *uio, int off, int len,
 	unsigned int count;
 
 	if (len < 0)
-		panic("%s: len %d < 0", __FUNCTION__, len);
+		panic("%s: len %d < 0", __func__, len);
 	if (off < 0)
-		panic("%s: off %d < 0", __FUNCTION__, off);
+		panic("%s: off %d < 0", __func__, off);
 	
 	ind = 0;
 	while (off > 0) {
