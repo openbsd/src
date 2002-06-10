38904a5cc5f1be92ef00357c60b51d068b66b141
fgsch <fgsch@openbsd.org>
2002-06-10 16:56:27 +0000

    add __weak_alias define; miod@ ok

--- sys/arch/mvme88k/include/cdefs.h
+++ sys/arch/mvme88k/include/cdefs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdefs.h,v 1.9 2001/11/09 00:05:00 miod Exp $ */
+/*	$OpenBSD: cdefs.h,v 1.10 2002/06/10 16:56:27 fgsch Exp $ */
 /*	$NetBSD: cdefs.h,v 1.2 1995/03/23 20:10:48 jtc Exp $	*/
 
 /*
@@ -23,6 +23,8 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs \"" msg "\",30,0,0,0");		\
 	__asm__(".stabs \"_" #sym "\",1,0,0,0")
+#define __weak_alias(alias,sym)				\
+	__asm__(".weak _" #alias "; _" #alias "= _" __STRING(sym))
 #else
 #define __indr_reference(sym,alias)	\
 	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
@@ -30,6 +32,8 @@
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs msg,30,0,0,0");			\
 	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+#define __weak_alias(alias,sym)				\
+	__asm__(".weak _/**/alias; _/**/alias = _/**/sym")
 #endif
 #endif
 
