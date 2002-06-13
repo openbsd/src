76193f34833c31b68f87b2397d5ed700319176ee
deraadt <deraadt@openbsd.org>
2002-06-13 06:40:28 +0000

    i heard in the bar that this breaks things

--- libexec/ftpd/extern.h
+++ libexec/ftpd/extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extern.h,v 1.8 2002/06/10 09:57:35 mpech Exp $	*/
+/*	$OpenBSD: extern.h,v 1.9 2002/06/13 06:40:28 deraadt Exp $	*/
 /*	$NetBSD: extern.h,v 1.2 1995/04/11 02:44:49 cgd Exp $	*/
 
 /*
@@ -108,9 +108,9 @@ int	yyparse(void);
 
 union sockunion {
 	struct sockinet {
-		socklen_t si_len;
-		sa_family_t si_family;
-		in_port_t si_port;
+		u_char si_len;
+		u_char si_family;
+		u_short si_port;
 	} su_si;
 	struct sockaddr_in  su_sin;
 	struct sockaddr_in6 su_sin6;
