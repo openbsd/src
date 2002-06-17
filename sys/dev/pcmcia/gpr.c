25a77b56d511bb5b079af156dd2a190c4585d2ab
fgsch <fgsch@openbsd.org>
2002-06-17 20:41:51 +0000

    cosmetic changes in the copyright suggested by theo.

--- sys/dev/pcmcia/gpr.c
+++ sys/dev/pcmcia/gpr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gpr.c,v 1.3 2002/06/17 18:44:15 fgsch Exp $	*/
+/*	$OpenBSD: gpr.c,v 1.4 2002/06/17 20:41:51 fgsch Exp $	*/
 
 /*
  * Copyright (c) 2002, Federico G. Schwindt
@@ -7,15 +7,15 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of the Federico G. Schwindt nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Federico G. Schwindt nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
