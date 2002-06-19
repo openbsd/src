501cd988516cb0063df2d3884e56462cea4e8e83
millert <millert@openbsd.org>
2002-06-19 02:50:10 +0000

    Updates from lite2 via NetBSD; fixes license.

--- include/bitstring.h
+++ include/bitstring.h
@@ -1,30 +1,46 @@
-/*	$OpenBSD: bitstring.h,v 1.3 1997/09/21 10:45:26 niklas Exp $	*/
-/*	$NetBSD: bitstring.h,v 1.4 1994/10/26 00:55:45 cgd Exp $	*/
+/*	$OpenBSD: bitstring.h,v 1.4 2002/06/19 02:50:10 millert Exp $	*/
+/*	$NetBSD: bitstring.h,v 1.5 1997/05/14 15:49:55 pk Exp $	*/
 
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Paul Vixie.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)bitstring.h	5.2 (Berkeley) 4/4/90
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)bitstring.h	8.1 (Berkeley) 7/19/93
  */
 
 #ifndef _BITSTRING_H_
-#define _BITSTRING_H_
+#define	_BITSTRING_H_
 
 /* modified for SV/AT and bitstring bugfix by M.R.Murphy, 11oct91
  * bitstr_size changed gratuitously, but shorter
@@ -55,7 +71,7 @@ typedef	unsigned char bitstr_t;
 
 				/* allocate a bitstring on the stack */
 #define	bit_decl(name, nbits) \
-	(name)[bitstr_size(nbits)]
+	((name)[bitstr_size(nbits)])
 
 				/* is bit N of bitstring name set? */
 #define	bit_test(name, bit) \
@@ -63,11 +79,11 @@ typedef	unsigned char bitstr_t;
 
 				/* set bit N of bitstring name */
 #define	bit_set(name, bit) \
-	(name)[_bit_byte(bit)] |= _bit_mask(bit)
+	((name)[_bit_byte(bit)] |= _bit_mask(bit))
 
 				/* clear bit N of bitstring name */
 #define	bit_clear(name, bit) \
-	(name)[_bit_byte(bit)] &= ~_bit_mask(bit)
+	((name)[_bit_byte(bit)] &= ~_bit_mask(bit))
 
 				/* clear bits start ... stop in bitstring */
 #define	bit_nclear(name, start, stop) do { \
@@ -77,7 +93,7 @@ typedef	unsigned char bitstr_t;
 		bit_clear(_name, _start); \
 		_start++; \
 		} \
-} while (0)
+} while(0)
 
 				/* set bits start ... stop in bitstring */
 #define	bit_nset(name, start, stop) do { \
@@ -87,7 +103,7 @@ typedef	unsigned char bitstr_t;
 		bit_set(_name, _start); \
 		_start++; \
 		} \
-} while (0)
+} while(0)
 
 				/* find first bit clear in name */
 #define	bit_ffc(name, nbits, value) do { \
@@ -99,7 +115,7 @@ typedef	unsigned char bitstr_t;
 			break; \
 		} \
 	*(value) = _value; \
-} while (0)
+} while(0)
 
 				/* find first bit set in name */
 #define	bit_ffs(name, nbits, value) do { \
@@ -111,6 +127,6 @@ typedef	unsigned char bitstr_t;
 			break; \
 		} \
 	*(value) = _value; \
-} while (0)
+} while(0)
 
 #endif /* !_BITSTRING_H_ */
