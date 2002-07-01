717812e5d913e15943516b5ecb5710357f46e492
art <art@openbsd.org>
2002-07-01 19:37:57 +0000

    include string.h for memcpy

--- regress/sys/kern/nxstack/nxstack.c
+++ regress/sys/kern/nxstack/nxstack.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nxstack.c,v 1.1 2002/07/01 18:15:51 mickey Exp $	*/
+/*	$OpenBSD: nxstack.c,v 1.2 2002/07/01 19:37:57 art Exp $	*/
 
 /*
  * Copyright (c) 2002 Michael Shalayeff
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 
 void
 testfly()
