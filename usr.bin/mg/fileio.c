23aec73dd3a8c94fb3a6ac94a4ac0346d41ca986
vincent <vincent@openbsd.org>
2002-07-01 17:20:04 +0000

    KNF

--- usr.bin/mg/fileio.c
+++ usr.bin/mg/fileio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fileio.c,v 1.31 2002/06/21 05:37:20 vincent Exp $	*/
+/*	$OpenBSD: fileio.c,v 1.32 2002/07/01 17:20:04 vincent Exp $	*/
 
 /*
  *	POSIX fileio.c
@@ -371,32 +371,32 @@ startupfile(char *suffix)
 
 	if (suffix == NULL) {
 		if (snprintf(file, sizeof(file), "%s/.mg", home)
-			    >= sizeof(file))
-			return NULL;
+		    >= sizeof(file))
+			return (NULL);
 	} else {
 		if (snprintf(file, sizeof(file), "%s/.mg-%s", home, suffix)
-			    >= sizeof(file))
-			return NULL;
+		    >= sizeof(file))
+			return (NULL);
 	}
 
 	if (access(file, R_OK) == 0)
-		return file;
+		return (file);
 nohome:
 #ifdef STARTUPFILE
 	if (suffix == NULL) {
 		if (snprintf(file, sizeof(file), "%s", STARTUPFILE)
-			    >= sizeof(file))
-			return NULL;
+		    >= sizeof(file))
+			return (NULL);
 	} else {
 		if (snprintf(file, sizeof(file), "%s%s", STARTUPFILE, suffix)
-			    >= sizeof(file))
-			return NULL;
+		    >= sizeof(file))
+			return (NULL);
 	}
 
 	if (access(file, R_OK) == 0)
-		return file;
+		return (file);
 #endif
-	return NULL;
+	return (NULL);
 }
 #endif
 
@@ -440,7 +440,7 @@ copy(char *frname, char *toname)
 		return (FALSE);
 	}
 	/*
-	 * It is "normal" for this to fail since we can't garantee that
+	 * It is "normal" for this to fail since we can't guarantee that
 	 * we will be running as root
 	 */
 	if (fchown(ofd, orig.st_uid, orig.st_gid) && errno != EPERM)
