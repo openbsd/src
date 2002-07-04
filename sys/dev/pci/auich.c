eb23275432676f02da2adb1820531280313c5160
mickey <mickey@openbsd.org>
2002-07-04 23:52:20 +0000

    repair a dmesg cleanup

--- sys/dev/pci/auich.c
+++ sys/dev/pci/auich.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auich.c,v 1.26 2002/03/24 20:41:19 mickey Exp $	*/
+/*	$OpenBSD: auich.c,v 1.27 2002/07/04 23:52:20 mickey Exp $	*/
 
 /*
  * Copyright (c) 2000,2001 Michael Shalayeff
@@ -345,7 +345,7 @@ auich_attach(parent, self, aux)
 	sprintf(sc->sc_audev.version, "0x%02x", PCI_REVISION(pa->pa_class));
 	strcpy(sc->sc_audev.config, sc->sc_dev.dv_xname);
 
-	printf(": %s %s\n", intrstr, sc->sc_audev.name);
+	printf(": %s, %s\n", intrstr, sc->sc_audev.name);
 
 	/* allocate dma lists */
 #define	a(a)	(void *)(((u_long)(a) + sizeof(*(a)) - 1) & ~(sizeof(*(a))-1))
