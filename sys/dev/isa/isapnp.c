33c3a75fc7785ab016f8d587466188e3d76abf7a
miod <miod@openbsd.org>
2002-07-07 18:20:00 +0000

    Allow isapnp to compile without isadma configured. From NetBSD.

--- sys/dev/isa/isapnp.c
+++ sys/dev/isa/isapnp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: isapnp.c,v 1.33 2002/06/30 16:05:58 miod Exp $	*/
+/*	$OpenBSD: isapnp.c,v 1.34 2002/07/07 18:20:00 miod Exp $	*/
 /*	$NetBSD: isapnp.c,v 1.9.4.3 1997/10/29 00:40:43 thorpej Exp $	*/
 
 /*
@@ -47,6 +47,8 @@
 #include <dev/isa/isadmavar.h>
 
 #include <dev/isa/pnpdevs.h>
+
+#include "isadma.h"
 
 void isapnp_init(struct isapnp_softc *);
 static __inline u_char isapnp_shift_bit(struct isapnp_softc *);
@@ -259,6 +261,7 @@ isapnp_alloc_drq(isa, i)
 	struct device *isa;
 	struct isapnp_pin *i;
 {
+#if NISADMA > 0
 	int b;
 
 	if (i->bits == 0) {
@@ -271,6 +274,7 @@ isapnp_alloc_drq(isa, i)
 			i->num = b;
 			return 0;
 		}
+#endif
 
 	return EINVAL;
 }
