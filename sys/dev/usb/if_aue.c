008448255b97d5883b8d371e5e1d3ad39a3489b2
nate <nate@openbsd.org>
2002-07-09 14:45:43 +0000

    add new device from NetBSD

--- sys/dev/usb/if_aue.c
+++ sys/dev/usb/if_aue.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: if_aue.c,v 1.22 2002/07/04 05:51:13 nate Exp $ */
-/*	$NetBSD: if_aue.c,v 1.76 2002/06/25 01:07:38 nathanw Exp $	*/
+/*	$OpenBSD: if_aue.c,v 1.23 2002/07/09 14:45:43 nate Exp $ */
+/*	$NetBSD: if_aue.c,v 1.78 2002/07/08 17:46:23 augustss Exp $	*/
 /*
  * Copyright (c) 1997, 1998, 1999, 2000
  *	Bill Paul <wpaul@ee.columbia.edu>.  All rights reserved.
@@ -219,6 +219,7 @@ Static const struct aue_type aue_devs[] = {
  {{ USB_VENDOR_SIEMENS,		USB_PRODUCT_SIEMENS_SPEEDSTREAM}, PII },
  {{ USB_VENDOR_SMARTBRIDGES,	USB_PRODUCT_SMARTBRIDGES_SMARTNIC},PII },
  {{ USB_VENDOR_SMC,		USB_PRODUCT_SMC_2202USB},	  0 },
+ {{ USB_VENDOR_SMC,		USB_PRODUCT_SMC_2206USB},	  PII },
  {{ USB_VENDOR_SOHOWARE,	USB_PRODUCT_SOHOWARE_NUB100},	  0 },
 };
 #define aue_lookup(v, p) ((struct aue_type *)usb_lookup(aue_devs, v, p))
