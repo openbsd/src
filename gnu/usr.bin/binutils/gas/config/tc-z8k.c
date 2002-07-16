bb12193d77c46e86f3e7c5bb9afc4d8acdd49b4a
fgsch <fgsch@openbsd.org>
2002-07-16 13:48:36 +0000

    better. this is what has been applied to binutils.

--- gnu/usr.bin/binutils/gas/config/tc-z8k.c
+++ gnu/usr.bin/binutils/gas/config/tc-z8k.c
@@ -996,7 +996,7 @@ build_bytes (this_try, operand)
   frag_wane (frag_now);
   frag_new (0);
 
-  memset (buffer, 0, 20);
+  memset (buffer, 0, sizeof (buffer));
   class_ptr = this_try->byte_info;
 
   for (nibble = 0; (c = *class_ptr++); nibble++)
