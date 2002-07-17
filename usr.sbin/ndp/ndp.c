5d6447e8b99b84e40154da73a6d013d5f1b6ca8d
itojun <itojun@openbsd.org>
2002-07-17 13:49:03 +0000

    with -i, flag on command line and flag displayed are different, and it does nothing but confuse users.  sync them.
    for -nud and such, you need "-- -nud" due to posix arg parsing.
    
    sync w/kame

--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: ndp.c,v 1.24 2002/06/03 19:30:49 itojun Exp $	*/
-/*	$KAME: ndp.c,v 1.100 2002/06/03 19:25:27 itojun Exp $	*/
+/*	$OpenBSD: ndp.c,v 1.25 2002/07/17 13:49:03 itojun Exp $	*/
+/*	$KAME: ndp.c,v 1.101 2002/07/17 08:46:33 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, 1998, and 1999 WIDE Project.
@@ -1021,14 +1021,14 @@ ifinfo(ifname, argc, argv)
 	if (ND.flags) {
 		printf("\nFlags: ");
 		if ((ND.flags & ND6_IFF_PERFORMNUD))
-			printf("PERFORMNUD ");
+			printf("nud ");
 #ifdef ND6_IFF_ACCEPT_RTADV
 		if ((ND.flags & ND6_IFF_ACCEPT_RTADV))
-			printf("ACCEPT_RA ");
+			printf("accept_rtadv ");
 #endif
 #ifdef ND6_IFF_PREFER_SOURCE
 		if ((ND.flags & ND6_IFF_PREFER_SOURCE))
-			printf("PREFER_SRC ");
+			printf("prefer_source ");
 #endif
 	}
 	putc('\n', stdout);
