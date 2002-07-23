788634e13a59a12f6362191a0c64c0fddd994430
stevesk <stevesk@openbsd.org>
2002-07-23 16:03:10 +0000

    utmp_len is unsigned; display error consistent with other options. ok markus@

--- usr.bin/ssh/sshd.c
+++ usr.bin/ssh/sshd.c
@@ -42,7 +42,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshd.c,v 1.256 2002/07/19 15:43:33 markus Exp $");
+RCSID("$OpenBSD: sshd.c,v 1.257 2002/07/23 16:03:10 stevesk Exp $");
 
 #include <openssl/dh.h>
 #include <openssl/bn.h>
@@ -887,8 +887,10 @@ main(int ac, char **av)
 			break;
 		case 'u':
 			utmp_len = atoi(optarg);
-			if (utmp_len < 0 || utmp_len > MAXHOSTNAMELEN)
-				usage();
+			if (utmp_len > MAXHOSTNAMELEN) {
+				fprintf(stderr, "Invalid utmp length.\n");
+				exit(1);
+			}
 			break;
 		case 'o':
 			if (process_server_config_line(&options, optarg,
