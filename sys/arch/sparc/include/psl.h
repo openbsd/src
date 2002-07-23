cbf65e8b7ed00d44d34f3297ab876cb014489ed4
art <art@openbsd.org>
2002-07-23 14:00:38 +0000

    Define IPL_NONE

--- sys/arch/sparc/include/psl.h
+++ sys/arch/sparc/include/psl.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: psl.h,v 1.13 2002/05/15 23:17:54 art Exp $	*/
+/*	$OpenBSD: psl.h,v 1.14 2002/07/23 14:00:38 art Exp $	*/
 /*	$NetBSD: psl.h,v 1.12 1997/03/10 21:49:11 pk Exp $ */
 
 /*
@@ -79,6 +79,7 @@
 /*
  * Various interrupt levels.
  */
+#define IPL_NONE	0
 #define IPL_SOFTINT	1
 #define IPL_SOFTCLOCK	IPL_SOFTINT	/* softclock() interrupts */
 #define IPL_SOFTNET	IPL_SOFTINT	/* soft network interrupts */
