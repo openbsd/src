b945d35c7c2a379636e076c156df2b472611b1f0
drahn <drahn@openbsd.org>
2002-07-23 17:34:14 +0000

    Use the proper cardbus_conf_read() rather than pci (in a cardbus driver). ok fgsch@

--- sys/dev/cardbus/if_dc_cardbus.c
+++ sys/dev/cardbus/if_dc_cardbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_dc_cardbus.c,v 1.11 2002/06/09 03:14:17 todd Exp $	*/
+/*	$OpenBSD: if_dc_cardbus.c,v 1.12 2002/07/23 17:34:14 drahn Exp $	*/
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -126,7 +126,8 @@ dc_cardbus_attach(parent, self, aux)
 
 	csc->sc_intrline = ca->ca_intrline;
 
-	sc->dc_cachesize = pci_conf_read(cc, ca->ca_tag, DC_PCI_CFLT) & 0xFF;
+	sc->dc_cachesize = cardbus_conf_read(cc, cf, ca->ca_tag, DC_PCI_CFLT)
+	    & 0xFF;
 
 	dc_cardbus_setup(csc);
 
