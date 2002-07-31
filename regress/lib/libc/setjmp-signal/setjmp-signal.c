2c5f6984c78f203702dfb2509786ccd9a6a7d8ab
art <art@openbsd.org>
2002-07-31 05:25:55 +0000

    Oops. don't forget to reformat the code after cut 'n paste.

--- regress/lib/libc/setjmp-signal/setjmp-signal.c
+++ regress/lib/libc/setjmp-signal/setjmp-signal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setjmp-signal.c,v 1.1 2002/07/31 05:18:24 art Exp $	*/
+/*	$OpenBSD: setjmp-signal.c,v 1.2 2002/07/31 05:25:55 art Exp $	*/
 /*
  *	Written by Artur Grabowski <art@openbsd.org> 2002 Public Domain.
  */
@@ -11,16 +11,16 @@ jmp_buf jb;
 void
 segv_handler(int signum)
 {
-        longjmp(jb, 1);
+	longjmp(jb, 1);
 }
 
 int
 main()
 {
-        signal(SIGSEGV, segv_handler);
-        if (setjmp(jb) == 0) {
-                *((int *)0) = 0;
-                return (1);
-        }
-        return (0);
+	signal(SIGSEGV, segv_handler);
+	if (setjmp(jb) == 0) {
+		*((int *)addr) = 0;
+		return (1);
+	}
+	return (0);
 }
