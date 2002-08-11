2185ca0d4cedf99b2c8f9182353c4d24a58167db
hugh <hugh@openbsd.org>
2002-08-11 20:02:06 +0000

    Restore support for loading elf boot:   - compiles again   - already tested   - less to upgrade later

--- sys/arch/vax/boot/xxboot/bootxx.c
+++ sys/arch/vax/boot/xxboot/bootxx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bootxx.c,v 1.7 2002/07/31 18:36:12 miod Exp $ */
+/*	$OpenBSD: bootxx.c,v 1.8 2002/08/11 20:02:06 hugh Exp $ */
 /* $NetBSD: bootxx.c,v 1.16 2002/03/29 05:45:08 matt Exp $ */
 
 /*-
@@ -40,9 +40,7 @@
 #include "sys/reboot.h"
 #include "sys/disklabel.h"
 #include "sys/exec.h"
-#ifdef notyet
 #include "sys/exec_elf.h"
-#endif
 
 #include "lib/libsa/stand.h"
 #include "lib/libsa/ufs.h"
@@ -101,9 +99,7 @@ Xmain()
 {
 	union {
 		struct exec aout;
-#ifdef notyet
 		Elf32_Ehdr elf;
-#endif
 	} hdr;
 	int io;
 	u_long entry;
@@ -153,7 +149,6 @@ Xmain()
 		read(io, (void *) entry, hdr.aout.a_text + hdr.aout.a_data);
 		memset((void *) (entry + hdr.aout.a_text + hdr.aout.a_data),
 		       0, hdr.aout.a_bss);
-#ifdef notyet
 	} else if (memcmp(hdr.elf.e_ident, ELFMAG, SELFMAG) == 0) {
 		Elf32_Phdr ph;
 		size_t off = sizeof(hdr.elf);
@@ -181,7 +176,6 @@ Xmain()
 		read(io, (void *) ph.p_paddr, ph.p_filesz);
 		memset((void *) (ph.p_paddr + ph.p_filesz), 0,
 		       ph.p_memsz - ph.p_filesz);
-#endif
 	} else {
 		goto die;
 	}
