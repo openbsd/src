568079bc5da972a1145f847b602ed9fab2806ecb
mickey <mickey@openbsd.org>
2002-08-28 22:30:42 +0000

    do not forget to export the weak alias, proper line continuation

--- sys/arch/hppa/include/cdefs.h
+++ sys/arch/hppa/include/cdefs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdefs.h,v 1.5 2002/08/03 18:54:26 mickey Exp $	*/
+/*	$OpenBSD: cdefs.h,v 1.6 2002/08/28 22:30:42 mickey Exp $	*/
 /*	$NetBSD: cdefs.h,v 1.5 1996/10/12 18:08:12 cgd Exp $	*/
 
 /*
@@ -32,8 +32,8 @@
 #define	_MACHINE_CDEFS_H_
 
 #define	__weak_alias(alias,sym)						\
-    __asm__(".weak " __STRING(alias) " ; " __STRING(alias) " = " __STRING(sym))
+    __asm__(".export " __STRING(alias) ", entry ! .weak " __STRING(alias) " ! " __STRING(alias) " = " __STRING(sym))
 #define	__warn_references(sym,msg)					\
-    __asm__(".section .gnu.warning." __STRING(sym) " ; .ascii \"" msg "\" ; .text")
+    __asm__(".section .gnu.warning." __STRING(sym) " ! .ascii \"" msg "\" ; .text")
 
 #endif /* !_MACHINE_CDEFS_H_ */
