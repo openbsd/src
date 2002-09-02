e42197868362ab41f30355eeadc2f2ec2c4815af
avsm <avsm@openbsd.org>
2002-09-02 19:56:55 +0000

    - Precede internal bsd.regress.mk variables with a _ - Rename old variables to be easier to read, and deprecate old variables.  The old variables will still work, but emit warnings. - REGRESS_SKIP_SLOW only needs to be non-empty now, not explicitly set to 'yes' - REGRESS_LOG can contain more than one file to append results to - ERRORS variable can be set in Makefile to emit warnings or indicate fatal errors. - Add REGRESS_MAIL variable which can be set to an email address to send results to.  Currently this only works for a full regression run, since art doesnt want partial run results - sync bsd.regress.mk(5) with these changes
    ok art@ , also looked over by miod@

--- share/mk/bsd.regress.mk
+++ share/mk/bsd.regress.mk
@@ -1,12 +1,14 @@
-#	$OpenBSD: bsd.regress.mk,v 1.9 2002/02/17 01:10:15 marc Exp $
+# $OpenBSD: bsd.regress.mk,v 1.10 2002/09/02 19:56:55 avsm Exp $
+# Documented in bsd.regress.mk(5)
+
 # No man pages for regression tests.
 NOMAN=
 
 # No installation.
 install:
 
-# If REGRESSTARGETS is defined and PROG is not defined, set NOPROG
-.if defined(REGRESSTARGETS) && !defined(PROG)
+# If REGRESS_TARGETS is defined and PROG is not defined, set NOPROG
+.if defined(REGRESS_TARGETS) && !defined(PROG)
 NOPROG=
 .endif
 
@@ -15,63 +17,96 @@ NOPROG=
 .MAIN: all
 all: regress
 
-# XXX - Need full path to REGRESSLOG, otherwise there will be much pain.
+# Check for deprecated REGRESS* variables and assign them to the
+# new versions if the new version is not already defined. 
+_REGRESS_DEPRECATED=LOG:LOG SKIPTARGETS:SKIP_TARGETS SKIPSLOW:SKIP_SLOW \
+	SKIP:SKIP TARGETS:TARGETS MAXTIME:MAXTIME ROOTTARGETS:ROOT_TARGETS
 
-REGRESSLOG?=/dev/null
-REGRESSNAME=${.CURDIR:S/${BSDSRCDIR}\/regress\///}
+.for _I in ${_REGRESS_DEPRECATED}
+_REGRESS_OLD=REGRESS${_I:C/\:.*//}
+_REGRESS_NEW=REGRESS_${_I:C/.*\://}
+.  if defined(${_REGRESS_OLD})
+ERRORS:= ${ERRORS} "Warning: ${_REGRESS_OLD} is deprecated, use ${_REGRESS_NEW} instead."
+.    if !defined(${_REGRESS_NEW})
+${_REGRESS_NEW}:=${${_REGRESS_OLD}}
+.    endif
+.  endif
+.endfor
+
+# XXX - Need full path to REGRESS_LOG, otherwise there will be much pain.
+REGRESS_LOG?=/dev/null
+REGRESS_SKIP_TARGETS?=
+REGRESS_SKIP_SLOW?=no
+
+_REGRESS_NAME=${.CURDIR:S/${BSDSRCDIR}\/regress\///}
+_REGRESS_TMP?=/dev/null
+_REGRESS_OUT= | tee -a ${REGRESS_LOG} ${_REGRESS_TMP} 2>&1 > /dev/null
 
 .if defined(PROG) && !empty(PROG)
 run-regress-${PROG}: ${PROG}
 	./${PROG}
 .endif
 
-.if !defined(REGRESSTARGETS)
-REGRESSTARGETS=run-regress-${PROG}
-.  if defined(REGRESSSKIP)
-REGRESSSKIPTARGETS=run-regress-${PROG}
+.if !defined(REGRESS_TARGETS)
+REGRESS_TARGETS=run-regress-${PROG}
+.  if defined(REGRESS_SKIP)
+REGRESS_SKIP_TARGETS=run-regress-${PROG}
 .  endif
 .endif
 
-REGRESSSKIPSLOW?=no
-
-.if (${REGRESSSKIPSLOW:L} == "yes") && defined(REGRESSSLOWTARGETS)
-REGRESSSKIPTARGETS+=${REGRESSSLOWTARGETS}
+.if defined(REGRESS_SLOW_TARGETS) && !empty(REGRESS_SKIP_SLOW)
+REGRESS_SKIP_TARGETS+=${REGRESS_SLOW_TARGETS}
 .endif
 
-.if defined(REGRESSROOTTARGETS)
-ROOTUSER!=id -g
+.if defined(REGRESS_ROOT_TARGETS)
+_ROOTUSER!=id -g
 SUDO?=
-. if (${ROOTUSER} != 0) && empty(SUDO)
-REGRESSSKIPTARGETS+=${REGRESSROOTTARGETS}
-. endif
+.  if (${_ROOTUSER} != 0) && empty(SUDO)
+REGRESS_SKIP_TARGETS+=${REGRESS_ROOT_TARGETS}
+.  endif
 .endif
 
-REGRESSSKIPTARGETS?=
+.if defined(ERRORS)
+.BEGIN:
+.  for _m in ${ERRORS}
+	@echo 1>&2 ${_m}
+.  endfor
+.  if !empty(ERRORS:M"Fatal\:*") || !empty(ERRORS:M'Fatal\:*')
+	@exit 1
+.  endif
+.endif 
 
-regress:
-.for RT in ${REGRESSTARGETS} 
-.  if ${REGRESSSKIPTARGETS:M${RT}}
-	@echo -n "SKIP " >> ${REGRESSLOG}
+regress: .SILENT
+.if ! ${REGRESS_LOG:M/*}
+	echo =========================================================
+	echo REGRESS_LOG must contain an absolute path to the log-file.
+	echo It currently points to: ${REGRESS_LOG}
+	echo =========================================================
+	exit 1
+.endif
+.for RT in ${REGRESS_TARGETS} 
+.  if ${REGRESS_SKIP_TARGETS:M${RT}}
+	@echo -n "SKIP " ${_REGRESS_OUT}
 .  else
 # XXX - we need a better method to see if a test fails due to timeout or just
 #       normal failure.
-.   if !defined(REGRESSMAXTIME)
-	@if cd ${.CURDIR} && ${MAKE} ${RT}; then \
-	    echo -n "SUCCESS " >> ${REGRESSLOG} ; \
+.   if !defined(REGRESS_MAXTIME)
+	-if cd ${.CURDIR} && ${MAKE} ${RT}; then \
+	    echo -n "SUCCESS " ${_REGRESS_OUT} ; \
 	else \
-	    echo -n "FAIL " >> ${REGRESSLOG} ; \
+	    echo -n "FAIL " ${_REGRESS_OUT} ; \
 	    echo FAILED ; \
 	fi
 .   else
-	@if cd ${.CURDIR} && (ulimit -t ${REGRESSMAXTIME} ; ${MAKE} ${RT}); then \
-	    echo -n "SUCCESS " >> ${REGRESSLOG} ; \
+	-if cd ${.CURDIR} && (ulimit -t ${REGRESS_MAXTIME} ; ${MAKE} ${RT}); then \
+	    echo -n "SUCCESS " ${_REGRESS_OUT} ; \
 	else \
-	    echo -n "FAIL (possible timeout) " >> ${REGRESSLOG} ; \
+	    echo -n "FAIL (possible timeout) " ${_REGRESS_OUT} ; \
 	    echo FAILED ; \
 	fi
 .   endif
 .  endif
-	@echo ${REGRESSNAME}/${RT:S/^run-regress-//} >> ${REGRESSLOG}
+	@echo ${_REGRESS_NAME}/${RT:S/^run-regress-//} ${_REGRESS_OUT}
 .endfor
 
 .PHONY: regress
