02d6c9fe5ebbd29ba55eefa4e337828434edbf9a
mickey <mickey@openbsd.org>
2002-09-05 18:34:47 +0000

    sort out the nlist and do defines

--- sys/arch/hppa/include/exec.h
+++ sys/arch/hppa/include/exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.6 2001/01/22 14:51:03 art Exp $	*/
+/*	$OpenBSD: exec.h,v 1.7 2002/09/05 18:34:47 mickey Exp $	*/
 
 /* 
  * Copyright (c) 1994, The University of Utah and
@@ -37,13 +37,10 @@
 #define ELF_TARG_DATA           ELFDATA2MSB
 #define ELF_TARG_MACH           EM_PARISC   
 
-/* #define _NLIST_DO_AOUT */
-/* #define _NLIST_DO_ECOFF */
+#define _NLIST_DO_AOUT
 #define _NLIST_DO_ELF
 #define _NLIST_DO_SOM
 
-/* #define	_KERN_DO_AOUT */
-/* #define	_KERN_DO_ECOFF */
 #define	_KERN_DO_ELF
 #define	_KERN_DO_SOM
 
