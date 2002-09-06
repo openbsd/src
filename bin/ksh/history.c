f8811aacce312b8ed8bdae28411ecfb768513b9b
deraadt <deraadt@openbsd.org>
2002-09-06 19:45:14 +0000

    no , at end of enum

--- bin/ksh/history.c
+++ bin/ksh/history.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: history.c,v 1.13 2002/08/03 17:00:09 marc Exp $	*/
+/*	$OpenBSD: history.c,v 1.14 2002/09/06 19:45:14 deraadt Exp $	*/
 
 /*
  * command history
@@ -891,7 +891,7 @@ typedef enum state {
 	shdr,		/* expecting a header */
 	sline,		/* looking for a null byte to end the line */
 	sn1,		/* bytes 1 to 4 of a line no */
-	sn2, sn3, sn4,
+	sn2, sn3, sn4
 } State;
 
 static int
