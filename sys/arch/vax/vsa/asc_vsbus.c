400bbcc2425cdecd290b8ad960289fb5265702c3
hugh <hugh@openbsd.org>
2002-09-18 09:49:23 +0000

    From NetBSD and in line with recent local changes: :  date: 2002/09/13 03:00:18;  author: chuck;  state: Exp;  lines: +3 -3 :  increase sc_maxxfer from 63K to 64K.   otherwise fsck fails for :  file systems with larger block sizes.

--- sys/arch/vax/vsa/asc_vsbus.c
+++ sys/arch/vax/vsa/asc_vsbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: asc_vsbus.c,v 1.4 2001/08/25 13:33:37 hugh Exp $	*/
+/*	$OpenBSD: asc_vsbus.c,v 1.5 2002/09/18 09:49:23 hugh Exp $	*/
 /*	$NetBSD: asc_vsbus.c,v 1.22 2001/02/04 20:36:32 ragge Exp $	*/
 
 /*-
@@ -317,7 +317,7 @@ asc_vsbus_attach(struct device *parent, struct device *self, void *aux)
 	 * formula: 4 * period = (1000 / freq) * 4
 	 */
 	sc->sc_minsync = (1000 / sc->sc_freq);
-	sc->sc_maxxfer = 63 * 1024;
+	sc->sc_maxxfer = 64 * 1024;
 
 	printf("\n%s", self->dv_xname);	/* Pretty print */
 
