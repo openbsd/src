fadf94c8036553516308d1edd8ef310946fc76bf
krw <krw@openbsd.org>
2002-09-28 23:35:12 +0000

    Cleanup/fix/improve question asking.
    1) If the user escapes to a shell with '!' or executes a command with
    '!foo' at a prompt then repeat the prompt when the shell or command
    returns.
    
    2) If ask_which() is passed an empty list of devices , then set resp to
    'done' and return. Replaces a couple of checks before calling
    ask_which(). Also emit a message about the lack of devices.
    
    3) In ask_which(), delete any trailing blank which rmel() may have
    appended to the device list. Replaces a check before calling
    ask_which().
    
    4) Remove an unneeded blank line before the 'invalid choice' message
    from ask_which().
    
    5) Make the first disk device the default answer when asking for which
    disk contains install sets.
    
    A few code/verbiage nits:
    
    1) make _a a local in cutlast().
    
    2) use normal ': ${_defdev:=done}' idiom instead of an or condition in
    ask_which.
    
    3) Eliminate an unnecessary blank line/paragraph by putting 3(!)
    sentences into a single paragraph when describing process of selecting
    install sets.
    
    4) Eliminate another unnecessary blank line between the list of
    possible install sources and the question about which one to use.
    
    5) Eliminate another unnecessary blank line before the 'Extract
    more sets?' question
    
    6) Eliminate a double blank line after the sanity check error
    messages.
    
    ok deraadt@.

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$OpenBSD: install.sh,v 1.117 2002/09/25 12:19:12 krw Exp $
+#	$OpenBSD: install.sh,v 1.118 2002/09/28 23:35:12 krw Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2002 Todd Miller, Theo de Raadt, Ken Westerback
@@ -94,8 +94,6 @@ if [ ! -f /etc/fstab ]; then
 
 	while : ; do
 		_DKDEVS=`rmel "$DISK" $_DKDEVS`
-		_DKDEVS=${_DKDEVS% }
-		[ "$_DKDEVS" ] || break
 
 		# Always do ROOTDISK first, and repeat until
 		# it is configured acceptably.
