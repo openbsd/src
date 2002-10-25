9f1f17633d6b259511e13c825a55f2db056f8172
camield <camield@openbsd.org>
2002-10-25 06:34:18 +0000

    %ul -> %lu
    ok millert@

--- usr.sbin/named/named/ns_udp.c
+++ usr.sbin/named/named/ns_udp.c
@@ -1,10 +1,10 @@
-/*	$OpenBSD: ns_udp.c,v 1.1 1997/03/12 10:42:36 downsj Exp $	*/
+/*	$OpenBSD: ns_udp.c,v 1.2 2002/10/25 06:34:18 camield Exp $	*/
 
 #if !defined(lint) && !defined(SABER)
 #if 0
 static char rcsid[] = "$From: ns_udp.c,v 8.3 1996/08/27 08:33:23 vixie Exp $";
 #else
-static char rcsid[] = "$OpenBSD: ns_udp.c,v 1.1 1997/03/12 10:42:36 downsj Exp $";
+static char rcsid[] = "$OpenBSD: ns_udp.c,v 1.2 2002/10/25 06:34:18 camield Exp $";
 #endif
 #endif /* not lint */
 
@@ -81,7 +81,7 @@ ns_udp() {
 
 	res = lseek(fd, offset, SEEK_SET);
 	if (res != offset) {
-		dprintf(1, (ddt, "ns_udp: lseek %ul failed %lu %d\n",
+		dprintf(1, (ddt, "ns_udp: lseek %lu failed %lu %d\n",
 			    offset, res, errno));
 		goto cleanup;
 	}
@@ -97,7 +97,7 @@ ns_udp() {
 		sum = 1;
 		lseek(fd, offset, SEEK_SET);
 		if (res != offset) {
-			dprintf(1, (ddt, "ns_udp: lseek %ul failed %lu %d\n",
+			dprintf(1, (ddt, "ns_udp: lseek %lu failed %lu %d\n",
 				    offset, res, errno));
 			goto cleanup;
 		}
