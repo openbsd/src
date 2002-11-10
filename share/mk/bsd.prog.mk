98e0011e774b36244766d915eda5664e391ba93e
mickey <mickey@openbsd.org>
2002-11-10 23:03:55 +0000

    hppa is not a special case anymore

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.prog.mk,v 1.33 2002/07/25 18:19:06 art Exp $
+#	$OpenBSD: bsd.prog.mk,v 1.34 2002/11/10 23:03:55 mickey Exp $
 #	$NetBSD: bsd.prog.mk,v 1.55 1996/04/08 21:19:26 jtc Exp $
 #	@(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
 
@@ -17,7 +17,7 @@ CXXFLAGS+=     ${CXXDIAGFLAGS}
 CFLAGS+=	${COPTS}
 CXXFLAGS+=     ${CXXOPTS}
 
-.if (${ELF_TOOLCHAIN} == "yes") && (${MACHINE_ARCH} != "hppa")
+.if (${ELF_TOOLCHAIN} == "yes")
 CRTBEGIN?=       ${DESTDIR}/usr/lib/crtbegin.o
 CRTEND?=         ${DESTDIR}/usr/lib/crtend.o
 .endif
