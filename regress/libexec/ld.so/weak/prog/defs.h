0c5cc4d1e3266d2d1a4acfc693b00234084934d3
todd <todd@openbsd.org>
2002-11-13 22:08:39 +0000

    put common CFLAGS+= in Makefile.inc use single defs.h header test for a non-zero value
    ok fgsch@

--- regress/libexec/ld.so/weak/prog/defs.h
+++ regress/libexec/ld.so/weak/prog/defs.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: defs.h,v 1.1.1.1 2002/02/10 22:51:41 fgsch Exp $	*/
+/*	$OpenBSD: defs.h,v 1.2 2002/11/13 22:08:39 todd Exp $	*/
 
 /*
  * Public domain. 2002, Federico Schwindt <fgsch@openbsd.org>.
  */
 
-#define WEAK_REF	0
+#define WEAK_REF	3
 #define STRONG_REF	1
 
 int 	weak_func(void);
