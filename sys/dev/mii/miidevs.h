225afc10f604cc528531e5533a31a87966ce9d0d
nate <nate@openbsd.org>
2002-11-20 14:15:35 +0000

    sync

--- sys/dev/mii/miidevs.h
+++ sys/dev/mii/miidevs.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: miidevs.h,v 1.33 2002/04/08 20:54:21 nate Exp $	*/
+/*	$OpenBSD: miidevs.h,v 1.34 2002/11/20 14:15:35 nate Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: miidevs,v 1.30 2002/04/08 20:54:09 nate Exp 
+ *	OpenBSD: miidevs,v 1.31 2002/11/20 14:14:21 nate Exp 
  */
 /* $NetBSD: miidevs,v 1.3 1998/11/05 03:43:43 thorpej Exp $ */
 
@@ -121,6 +121,8 @@
 #define	MII_STR_xxBROADCOM_BCM5421S	"BCM5421S 10/100/1000baseTX PHY"
 #define	MII_MODEL_xxBROADCOM_BCM5701	0x0011
 #define	MII_STR_xxBROADCOM_BCM5701	"BCM5701 10/100/1000baseTX PHY"
+#define	MII_MODEL_xxBROADCOM_BCM5703	0x0016
+#define	MII_STR_xxBROADCOM_BCM5703	"BCM5703 10/100/1000baseTX PHY"
 #define	MII_MODEL_BROADCOM_BCM5400	0x0004
 #define	MII_STR_BROADCOM_BCM5400	"BCM5400 1000baseTX PHY"
 #define	MII_MODEL_BROADCOM_BCM5401	0x0005
