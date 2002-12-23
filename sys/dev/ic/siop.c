1c234f2fb85ff56fc8d632c9ea544ba3c072e7ca
krw <krw@openbsd.org>
2002-12-23 00:42:32 +0000

    Fix complete botch in handling of 'openings'.
    Assign a fixed value (SIOP_NTAGS) to the openings field in the
    adapter's template sc_link, rather than incrementing the value as
    cbd's are allocated. The template value is the one copied into each
    device's sc_link structure as it is created.
    
    Incrementing the value meant that each new device got a larger value
    for openings. The total number of openings claimed by devices on a
    bus soon exceeded the number of cbd's available. e.g. after 5
    devices there would be 132 allocated cbd's, but the total number of
    openings claimed by devices would be 300.
    
    A heavy i/o load on an adapter with multiple devices could have
    caused the upper scsi layer to try to queue more i/o's than the
    driver had cbd's to store them in. Such i/o's would fail with EIO if
    they were started with SCSI_NOSLEEP (e.g. sdstart()) or were not
    queued within the specified retry limit. I/o's for devices 'later'
    on the bus would be more likely to trigger this behaviour, due to
    their inflated openings values.
    
    This is good candidate for -stable.

--- sys/dev/ic/siop.c
+++ sys/dev/ic/siop.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siop.c,v 1.23 2002/11/16 04:37:29 krw Exp $ */
+/*	$OpenBSD: siop.c,v 1.24 2002/12/23 00:42:32 krw Exp $ */
 /*	$NetBSD: siop.c,v 1.65 2002/11/08 22:04:41 bouyer Exp $	*/
 
 /*
@@ -175,6 +175,7 @@ siop_attach(sc)
 	sc->sc_currschedslot = 0;
 	sc->sc_c.sc_link.adapter = &siop_adapter;
 	sc->sc_c.sc_link.device = &siop_dev;
+	sc->sc_c.sc_link.openings = SIOP_NTAG;
 
 	/* Start with one page worth of commands */
 	siop_morecbd(sc);
@@ -1860,7 +1861,6 @@ siop_morecbd(sc)
 	}
 	s = splbio();
 	TAILQ_INSERT_TAIL(&sc->cmds, newcbd, next);
-	sc->sc_c.sc_link.openings += SIOP_NCMDPB;
 	splx(s);
 	return;
 bad0:
