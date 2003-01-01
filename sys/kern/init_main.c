e4ffbdbdabbf9f7ed0079b17f159166a4e6bfcc7
pvalchev <pvalchev@openbsd.org>
2003-01-01 21:05:36 +0000

    2003 in copyright

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.98 2002/12/19 00:57:07 mickey Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.99 2003/01/01 21:05:36 pvalchev Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -101,7 +101,7 @@ extern void nfs_init(void);
 const char	copyright[] =
 "Copyright (c) 1982, 1986, 1989, 1991, 1993\n"
 "\tThe Regents of the University of California.  All rights reserved.\n"
-"Copyright (c) 1995-2002 OpenBSD. All rights reserved.  http://www.OpenBSD.org\n";
+"Copyright (c) 1995-2003 OpenBSD. All rights reserved.  http://www.OpenBSD.org\n";
 
 /* Components of the first process -- never freed. */
 struct	session session0;
