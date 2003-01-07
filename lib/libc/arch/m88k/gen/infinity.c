c7efede26a2eb270ec8c48ea6f733a8f80aeddba
miod <miod@openbsd.org>
2003-01-07 22:00:50 +0000

    Properly align __infinity - m88k is very strict wrt double alignment. Fixes - oh what a surprise - bus errors in awk.

--- lib/libc/arch/m88k/gen/infinity.c
+++ lib/libc/arch/m88k/gen/infinity.c
@@ -1,12 +1,13 @@
-/*	$OpenBSD: infinity.c,v 1.2 2000/03/01 17:31:20 todd Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.3 2003/01/07 22:00:50 miod Exp $	*/
 
-#ifndef lint
-static char rcsid[] = "$OpenBSD: infinity.c,v 1.2 2000/03/01 17:31:20 todd Exp $";
-#endif /* not lint */
+#if defined(LIBC_SCCS) && !defined(lint)
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.3 2003/01/07 22:00:50 miod Exp $";
+#endif /* LIBC_SCCS and not lint */
 
 /* infinity.c */
 
 #include <math.h>
 
-/* bytes for +Infinity on 88100 */
-char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
+/* bytes for +Infinity on m88k */
+char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
+    { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
