8513555c7d2acccba4d4b74d5d5015c61f81063a
miod <miod@openbsd.org>
2003-01-07 21:59:21 +0000

    One more pass to get thinks correct wrt weak symbols and syscalls.

--- lib/libc/arch/m88k/SYS.h
+++ lib/libc/arch/m88k/SYS.h
@@ -1,4 +1,4 @@
-/* *	$OpenBSD: SYS.h,v 1.7 2003/01/02 21:40:44 miod Exp $*/
+/*	$OpenBSD: SYS.h,v 1.8 2003/01/07 21:59:21 miod Exp $*/
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
@@ -43,58 +43,50 @@
 
 #ifdef __STDC__
 #define	__ENTRY(p,x)	ENTRY(p##x)
-#define	__DO_SYSCALL(x)	or r13,r0,SYS_##x
+#define	__SYSCALLNAME(p,x)	p##x
 #define	__ALIAS(prefix,name)	WEAK_ALIAS(name,prefix##name)
 #else
 #define	__ENTRY(p,x)	ENTRY(p/**/x)
-#define	__DO_SYSCALL(x)	or r13,r0,SYS_/**/x
+#define	__SYSCALLNAME(p,x)	p/**/x
 #define	__ALIAS(prefix,name)	WEAK_ALIAS(name,prefix/**/name)
 #endif
 
-#define	__SYSCALL(p,x,y)						\
-	align 8;							\
-	__ENTRY(p,x);							\
+#define	__DO_SYSCALL(x)							\
 	ld r10,r31,32;							\
 	ld r11,r31,36;							\
 	ld r12,r31,40;							\
-	__DO_SYSCALL(y);						\
-	tb0 0, r0, 128;							\
+	or r13,r0,__SYSCALLNAME(SYS_,x);				\
+	tb0 0, r0, 128
+
+#define	__SYSCALL__NOERROR(p,x,y)					\
+	align 8;							\
+	__ENTRY(p,x);							\
+	__ALIAS(p,x);							\
+	__DO_SYSCALL(y)
+
+#define	__SYSCALL(p,x,y)						\
+	__SYSCALL__NOERROR(p,x,y);					\
 	br __cerror
+
+#define	__PSEUDO_NOERROR(p,x,y)						\
+	__SYSCALL__NOERROR(p,x,y);					\
+	jmp r1
 
 #define	__PSEUDO(p,x,y)							\
 	__SYSCALL(p,x,y);						\
 	jmp r1
 
-#if 0
-#define	__PSEUDO_NOERROR(p,x,y)						\
-	__SYSCALL(p,x,y);						\
-	jmp r1
-#else
-/* XXX is this correct??? */
-#define	__PSEUDO_NOERROR(p,x,y)						\
-	align 8;							\
-	__ENTRY(p,x);							\
-	__DO_SYSCALL(y);						\
-	tb0 0, r0, 128;							\
-	or r0, r0, r0;							\
-	jmp r1
-#endif
-
 /*
- * For the thread_safe versions, we prepend _thread_sys_ to the function
- * name so that the 'C' wrapper can go around the real name.
+ * System calls entry points are really named _thread_sys_{syscall},
+ * and weakly aliased to the name {syscall}. This allows the thread
+ * library to replace system calls at link time.
  */
-
-#define	SYSCALL(x)		__ALIAS(_thread_sys_,x);		\
-				__SYSCALL(_thread_sys_,x,x)
-#define	RSYSCALL(x)		__ALIAS(_thread_sys_,x);		\
-				__PSEUDO(_thread_sys_,x,x)
-#define	PSEUDO(x,y)		__ALIAS(_thread_sys_,x);		\
-				__PSEUDO(_thread_sys_,x,y)
-#define	PSEUDO_NOERROR(x,y)	__ALIAS(_thread_sys_,x);		\
-				__PSEUDO_NOERROR(_thread_sys_,x,y)
-#define	SYSENTRY(x)		__ALIAS(_thread_sys_,x);		\
-				__ENTRY(_thread_sys_,x)
+#define	SYSCALL(x)		__SYSCALL(_thread_sys_,x,x)
+#define	RSYSCALL(x)		__PSEUDO(_thread_sys_,x,x)
+#define	PSEUDO(x,y)		__PSEUDO(_thread_sys_,x,y)
+#define	PSEUDO_NOERROR(x,y)	__PSEUDO_NOERROR(_thread_sys_,x,y)
+#define	SYSENTRY(x)		__ENTRY(_thread_sys_,x);		\
+				__ALIAS(_thread_sys_,x)
 
 #define	ASMSTR		.asciz
 
