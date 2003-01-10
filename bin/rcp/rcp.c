79dae9ad1bc10c15d42410b9c32a6328beda0710
djm <djm@openbsd.org>
2003-01-10 10:29:35 +0000

    Don't ftruncate after write error, creating sparse files of incorrect length mindrot bug #403, reported by rusr@cup.hp.com; ok markus@

--- bin/rcp/rcp.c
+++ bin/rcp/rcp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcp.c,v 1.31 2002/07/04 04:26:40 deraadt Exp $	*/
+/*	$OpenBSD: rcp.c,v 1.32 2003/01/10 10:29:35 djm Exp $	*/
 /*	$NetBSD: rcp.c,v 1.9 1995/03/21 08:19:06 cgd Exp $	*/
 
 /*
@@ -731,7 +731,7 @@ bad:			run_err("%s: %s", np, strerror(errno));
 			wrerr = YES;
 			wrerrno = j >= 0 ? EIO : errno;
 		}
-		if (ftruncate(ofd, size)) {
+		if (wrerr == NO && ftruncate(ofd, size) != 0) {
 			run_err("%s: truncate: %s", np, strerror(errno));
 			wrerr = DISPLAYED;
 		}
--- usr.bin/ssh/scp.c
+++ usr.bin/ssh/scp.c
@@ -75,7 +75,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: scp.c,v 1.97 2003/01/10 08:19:07 fgsch Exp $");
+RCSID("$OpenBSD: scp.c,v 1.98 2003/01/10 10:29:35 djm Exp $");
 
 #include "xmalloc.h"
 #include "atomicio.h"
@@ -848,7 +848,7 @@ bad:			run_err("%s: %s", np, strerror(errno));
 			wrerr = YES;
 			wrerrno = j >= 0 ? EIO : errno;
 		}
-		if (ftruncate(ofd, size)) {
+		if (wrerr == NO && ftruncate(ofd, size) != 0) {
 			run_err("%s: truncate: %s", np, strerror(errno));
 			wrerr = DISPLAYED;
 		}
