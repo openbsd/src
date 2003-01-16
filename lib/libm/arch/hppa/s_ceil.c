0c764565a81bbe3b64f3472e50a6ed4fe8ab5643
mickey <mickey@openbsd.org>
2003-01-16 19:17:33 +0000

    fpr0 is recommended to be operated in full 64bits, although seemed to work otherwise, fix it

--- lib/libm/arch/hppa/s_ceil.c
+++ lib/libm/arch/hppa/s_ceil.c
@@ -3,7 +3,7 @@
  */
 
 #if defined(LIBM_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: s_ceil.c,v 1.2 2002/09/11 15:16:52 mickey Exp $";
+static char rcsid[] = "$OpenBSD: s_ceil.c,v 1.3 2003/01/16 19:17:33 mickey Exp $";
 #endif
 
 #include <sys/types.h>
@@ -13,14 +13,15 @@ static char rcsid[] = "$OpenBSD: s_ceil.c,v 1.2 2002/09/11 15:16:52 mickey Exp $
 double
 ceil(double x)
 {
-	u_int32_t ofpsr, fpsr;
+	u_int64_t ofpsr, fpsr;
 
-	__asm__ __volatile__("fstw %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
-	fpsr = (ofpsr & ~0x600) | (FP_RP << 9);
-	__asm__ __volatile__("fldw 0(%0), %%fr0" :: "r" (&fpsr));
+	__asm__ __volatile__("fstds %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
+	fpsr = (ofpsr & ~((u_int64_t)FP_RM << (9 + 32))) |
+	    ((u_int64_t)FP_RP << (9 + 32));
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&fpsr));
 
 	__asm__ __volatile__("frnd,dbl %0,%0" : "+f" (x));
 
-	__asm__ __volatile__("fldw 0(%0), %%fr0" :: "r" (&ofpsr));
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&ofpsr));
 	return (x);
 }
