21b52ebd7c475b9c5ea0f121f0e96737f9597772
jason <jason@openbsd.org>
2003-01-27 00:06:14 +0000

    missing $OpenBSD$

--- usr.bin/pmdb/arch/powerpc/pmdb_machdep.h
+++ usr.bin/pmdb/arch/powerpc/pmdb_machdep.h
@@ -1,3 +1,5 @@
+/*	$OpenBSD: pmdb_machdep.h,v 1.3 2003/01/27 00:06:14 jason Exp $	*/
+
 #define BREAKPOINT              { 0x7C, 0x81, 0x08, 0x08 }
 #define BREAKPOINT_LEN          4
 #define BREAKPOINT_DECR_PC      4
