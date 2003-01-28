c45c3a53401f66f3b14b377b779adf017e85f5d7
mickey <mickey@openbsd.org>
2003-01-28 20:43:44 +0000

    add a license goo; pt out by ray@securityfoo.net

--- usr.bin/pmdb/arch/hppa/pmdb_machdep.h
+++ usr.bin/pmdb/arch/hppa/pmdb_machdep.h
@@ -1,4 +1,30 @@
-/*	$OpenBSD: pmdb_machdep.h,v 1.2 2002/03/15 18:33:03 mickey Exp $	*/
+/*	$OpenBSD: pmdb_machdep.h,v 1.3 2003/01/28 20:43:44 mickey Exp $	*/
+
+/*
+ * Copyright (c) 2002-2003 Michael Shalayeff
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR OR HIS RELATIVES BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF MIND, USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #define BREAKPOINT		{ 0x00, 0x01, 0x00, 0x00 }
 #define BREAKPOINT_LEN		4
