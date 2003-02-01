73116275216993081a2c6410894732dedf1a199d
miod <miod@openbsd.org>
2003-02-01 00:32:05 +0000

    Regen

--- sys/arch/hppa/dev/cpudevs.h
+++ sys/arch/hppa/dev/cpudevs.h
@@ -1,7 +1,7 @@
 /*
  * THIS FILE AUTOMATICALLY GENERATED. DO NOT EDIT.
  * generated from:
- *	OpenBSD: cpudevs,v 1.22 2002/12/23 21:54:53 mickey Exp 
+ *	OpenBSD: cpudevs,v 1.23 2003/02/01 00:31:59 miod Exp 
  */
 
 #define	HPPA_TYPE_NPROC	0x00
