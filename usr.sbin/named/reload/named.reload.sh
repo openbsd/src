a491b5a2920c86aaacf7e9067d5e6d9ffafe20b8
jakob <jakob@openbsd.org>
2003-02-21 08:59:42 +0000

    remove bind4; ok deraadt@

--- usr.sbin/named/reload/named.reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -
-#
-#	$OpenBSD: named.reload.sh,v 1.3 1997/03/12 14:51:59 downsj Exp $
-#	from named.reload	5.2 (Berkeley) 6/27/89
-#	$From: named.reload.sh,v 8.1 1994/12/15 06:24:14 vixie Exp $
-#
-
-exec %DESTSBIN%/ndc reload
--- usr.sbin/named/restart/named.restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -
-#
-#	$OpenBSD: named.restart.sh,v 1.3 1997/03/12 14:51:59 downsj Exp $
-#	from named.restart	5.4 (Berkeley) 6/27/89
-#	$Id: named.restart.sh,v 1.3 1997/03/12 14:51:59 downsj Exp $
-#
-
-exec %DESTSBIN%/ndc restart
