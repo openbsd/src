6bc4b5b0fce7cd13f435f1a35695f009c8135eb4
matthieu <matthieu@openbsd.org>
2003-02-24 22:30:12 +0000

    Bye, unused code.

--- lkm/ap/version.c
+++
@@ -1,8 +0,0 @@
-/*	$OpenBSD: version.c,v 1.3 2000/03/02 01:49:09 todd Exp $	*/
-/*
- * Loadable Kernel Module for XFree86 Aperture driver
- *
- * Copyright (c) 1994,1996 The XFree86 Project Inc.
- */
-char *ap_major_version = "1";
-char *ap_minor_version = "6";
--- lkm/ap/version.h
+++
@@ -1,8 +0,0 @@
-/*	$OpenBSD: version.h,v 1.1 1996/03/05 11:25:38 mickey Exp $	*/
-/*
- * Loadable Kernel Module for XFree86 Aperture driver
- *
- * Copyright (c) 1994 Matthieu Herrb
- */
-extern char *ap_major_version;
-extern char *ap_minor_version;
