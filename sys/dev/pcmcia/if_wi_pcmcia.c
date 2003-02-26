3820b30f1bce415e1fa9b84c73f72b297dced0fc
millert <millert@openbsd.org>
2003-02-26 18:58:32 +0000

    Add support for AirVast WN-100; nick@

--- sys/dev/pcmcia/if_wi_pcmcia.c
+++ sys/dev/pcmcia/if_wi_pcmcia.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: if_wi_pcmcia.c,v 1.39 2002/11/19 18:36:18 jason Exp $ */
+/* $OpenBSD: if_wi_pcmcia.c,v 1.40 2003/02/26 18:58:32 millert Exp $ */
 /* $NetBSD: if_wi_pcmcia.c,v 1.14 2001/11/26 04:34:56 ichiro Exp $ */
 
 /*
@@ -268,6 +268,11 @@ static const struct wi_pcmcia_product {
 	  PCMCIA_PRODUCT_NETGEAR2_MA401RA,
 	  PCMCIA_CIS_NETGEAR2_MA401RA,
 	  "Netgear MA401RA Wireless LAN PC CARD",
+	},
+	{ PCMCIA_VENDOR_AIRVAST,
+	  PCMCIA_PRODUCT_AIRVAST_WN_100,
+	  PCMCIA_CIS_AIRVAST_WN_100,
+	  "AirVast WN-100 Wireless LAN PC CARD",
 	},
 	{ 0,
 	  0,
