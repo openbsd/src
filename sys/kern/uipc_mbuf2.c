8bfd94d82a882e2860bc67b55d1158932aba2489
itojun <itojun@openbsd.org>
2003-03-03 12:08:30 +0000

    use M_READONLY instead of defining M_SHAREDCLUSTER().  the change should have been made when M_READONLY was introduced.  sync w/kame

--- sys/kern/uipc_mbuf2.c
+++ sys/kern/uipc_mbuf2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_mbuf2.c,v 1.18 2002/10/10 10:38:13 kjc Exp $	*/
+/*	$OpenBSD: uipc_mbuf2.c,v 1.19 2003/03/03 12:08:30 itojun Exp $	*/
 /*	$KAME: uipc_mbuf2.c,v 1.29 2001/02/14 13:42:10 itojun Exp $	*/
 /*	$NetBSD: uipc_mbuf.c,v 1.40 1999/04/01 00:23:25 thorpej Exp $	*/
 
@@ -72,10 +72,6 @@
 #include <sys/malloc.h>
 #include <sys/mbuf.h>
 
-#define M_SHAREDCLUSTER(m) \
-	(((m)->m_flags & M_EXT) != 0 && \
-	 ((m)->m_ext.ext_free || MCLISREFERENCED((m))))
-
 /* can't call it m_dup(), as freebsd[34] uses m_dup() with different arg */
 static struct mbuf *m_dup1(struct mbuf *, int, int, int);
 
@@ -122,7 +118,7 @@ m_pulldown(m, off, len, offp)
 		return (NULL);	/* mbuf chain too short */
 	}
 
-	sharedcluster = M_SHAREDCLUSTER(n);
+	sharedcluster = M_READONLY(n);
 
 	/*
 	 * the target data is on <n, off>.
