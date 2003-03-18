bcc2f5604ce6f1e8a83fbdc47285d0eff075851d
millert <millert@openbsd.org>
2003-03-18 03:46:12 +0000

    make rcsid const to pass gcc -Wall

--- lib/libcurses/tinfo/read_bsd_terminfo.c
+++ lib/libcurses/tinfo/read_bsd_terminfo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: read_bsd_terminfo.c,v 1.11 2003/03/17 21:57:57 millert Exp $	*/
+/*	$OpenBSD: read_bsd_terminfo.c,v 1.12 2003/03/18 03:46:12 millert Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999, 2000 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -28,7 +28,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: read_bsd_terminfo.c,v 1.11 2003/03/17 21:57:57 millert Exp $";
+static const char rcsid[] = "$OpenBSD: read_bsd_terminfo.c,v 1.12 2003/03/18 03:46:12 millert Exp $";
 #endif
 
 #include <curses.priv.h>
