8070a636aae0154211632313a4159eae1265ddfd
henning <henning@openbsd.org>
2003-04-02 22:31:06 +0000

    no \n w/ errx(3), reminded by andrushock

--- sbin/pfctl/pfctl.c
+++ sbin/pfctl/pfctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfctl.c,v 1.165 2003/04/02 14:09:20 henning Exp $ */
+/*	$OpenBSD: pfctl.c,v 1.166 2003/04/02 22:31:06 henning Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -946,10 +946,10 @@ pfctl_add_altq(struct pfctl *pf, struct pf_altq *a)
 		if ((pf->opts & PF_OPT_NOACTION) == 0) {
 			if (ioctl(pf->dev, DIOCADDALTQ, pf->paltq)) {
 				if (errno == ENXIO)
-					errx(1, "qtype not configured\n");
+					errx(1, "qtype not configured");
 				else if (errno == ENODEV)
 					errx(1, "%s: driver does not support "
-					    "altq\n", a->ifname);
+					    "altq", a->ifname);
 				else
 					err(1, "DIOCADDALTQ");
 			}
