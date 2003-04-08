5548cf89c92166a2afbe7c11920dfa56d6268be3
miod <miod@openbsd.org>
2003-04-08 20:34:01 +0000

    Ahem, fix printf format.

--- sys/arch/sparc/stand/boot/loadfile.c
+++ sys/arch/sparc/stand/boot/loadfile.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: loadfile.c,v 1.2 2003/03/18 05:11:57 miod Exp $	*/
+/*	$OpenBSD: loadfile.c,v 1.3 2003/04/08 20:34:01 miod Exp $	*/
 /*	$NetBSD: loadfile.c,v 1.3 1997/04/06 08:40:59 cgd Exp $	*/
 
 /*
@@ -190,7 +190,7 @@ elf_seek(int fd, off_t relpos)
 
 	if (relpos < 0) {
 #ifdef DEBUG
-		printf("elf_seek: attempting to seek backwards from %lx bytes, "
+		printf("elf_seek: attempting to seek backwards from %llx bytes, "
 		    "may fail!\n", -relpos);
 #endif
 		if (lseek(fd, relpos, SEEK_CUR) < 0)
