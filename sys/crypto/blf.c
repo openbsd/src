e230ed68dc254a0d7e9ae5fba6277717d3f5d528
markus <markus@openbsd.org>
2003-04-09 21:23:51 +0000

    makeing a local array static saves some bytes; idea from mickey@

--- sys/crypto/blf.c
+++ sys/crypto/blf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: blf.c,v 1.4 2002/03/15 18:19:52 millert Exp $	*/
+/*	$OpenBSD: blf.c,v 1.5 2003/04/09 21:23:51 markus Exp $	*/
 
 /*
  * Blowfish block cipher for OpenBSD
@@ -122,7 +122,7 @@ Blowfish_initstate(c)
 
 /* P-box and S-box tables initialized with digits of Pi */
 
-	const blf_ctx initstate =
+	static const blf_ctx initstate =
 
 	{ {
 		{
