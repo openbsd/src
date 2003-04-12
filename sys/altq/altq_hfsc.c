2e3f20a795a6a2031182dee15cb1a2f5e61f6738
henning <henning@openbsd.org>
2003-04-12 20:03:22 +0000

    life's easier if one initializes pointers...

--- sys/altq/altq_hfsc.c
+++ sys/altq/altq_hfsc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: altq_hfsc.c,v 1.17 2003/04/12 18:59:16 henning Exp $	*/
+/*	$OpenBSD: altq_hfsc.c,v 1.18 2003/04/12 20:03:22 henning Exp $	*/
 /*	$KAME: altq_hfsc.c,v 1.17 2002/11/29 07:48:33 kjc Exp $	*/
 
 /*
@@ -209,6 +209,7 @@ hfsc_add_queue(struct pf_altq *a)
 
 	opts = &a->pq_u.hfsc_opts;
 
+	parent = NULL;
 	if (a->qid != HFSC_ROOTCLASS_HANDLE)
 		if ((parent = clh_to_clp(hif, a->parent_qid)) == NULL)
 			return (EINVAL);
