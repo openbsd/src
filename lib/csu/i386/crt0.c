da7b23150369f5c54facf6fa8579821d15091976
drahn <drahn@openbsd.org>
2003-04-17 03:44:59 +0000

    i386 startup code for ELF. based on newer version of NetBSD code.

--- lib/csu/i386/crt0.c
+++ lib/csu/i386/crt0.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: crt0.c,v 1.9 2003/02/28 18:05:51 deraadt Exp $	*/
+/*	$OpenBSD: crt0.c,v 1.10 2003/04/17 03:44:59 drahn Exp $	*/
 /*	$NetBSD: crt0.c,v 1.20 1995/06/03 13:16:08 pk Exp $	*/
 
 /*
- * Copyright (c) 1993 Paul Kranenburg
+ * Copyright (c) 1995 Christopher G. Demetriou
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -15,7 +15,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Paul Kranenburg.
+ *      This product includes software developed by Christopher G. Demetriou
+ *	for the NetBSD Project.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
@@ -29,50 +30,66 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 
-
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: crt0.c,v 1.9 2003/02/28 18:05:51 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: crt0.c,v 1.10 2003/04/17 03:44:59 drahn Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
 #include <stdlib.h>
 
-#include "common.h"
+static char	*_strrchr(char *, char);
 
-extern void	start(void) asm("start");
+char	**environ;
+char	*__progname = "";
+
+char __progname_storage[NAME_MAX+1];
+
+#ifdef MCRT0
+extern void	monstartup(u_long, u_long);
+extern void	_mcleanup(void);
+extern unsigned char _etext, _eprol;
+#endif /* MCRT0 */
+
+
+__asm("
+	.text
+	.align  4
+	.globl  __start
+	.globl  _start
+_start:
+__start:
+	pushl	%ebx			#ps_strings
+	pushl   %ecx                    # obj
+	pushl   %edx                    # cleanup
+	movl    12(%esp),%eax
+	leal    20(%esp,%eax,4),%ecx
+	leal    16(%esp),%edx
+	pushl   %ecx
+	pushl   %edx
+	pushl   %eax
+	call    ___start
+");
 
 void
-start()
+___start(argc, argv, envp, cleanup, obj, ps_strings)
+	int argc;
+	char **argv;
+	char ** envp;
+	void (*cleanup)(void);			/* from shared loader */
+	const void *obj;			/* from shared loader */
+	struct ps_strings *ps_strings;
 {
-	struct kframe {
-		int	kargc;
-		char	*kargv[1];	/* size depends on kargc */
-		char	kargstr[1];	/* size varies */
-		char	kenvstr[1];	/* size varies */
-	};
-	/*
-	 *	ALL REGISTER VARIABLES!!!
-	 */
+	char *namep;
 	register struct kframe *kfp;
-	register char **argv, *ap;
 	char *s;
 
-#ifdef lint
-	kfp = 0;
-	initcode = initcode = 0;
-#else /* not lint */
-	/* just above the saved frame pointer */
-	asm ("lea 4(%%ebp), %0" : "=r" (kfp) );
-#endif /* not lint */
-	argv = &kfp->kargv[0];
-	environ = argv + kfp->kargc + 1;
+        environ = envp;
 
-	if ((ap = argv[0])) {	
-		if ((__progname = _strrchr(ap, '/')) == NULL)
-			__progname = ap;
+	if ((namep = argv[0]) != NULL) {	/* NULL ptr if argc = 0 */
+		if ((__progname = _strrchr(namep, '/')) == NULL)
+			__progname = namep;
 		else
 			++__progname;
 		for (s = __progname_storage; *__progname &&
@@ -82,45 +99,34 @@ start()
 		__progname = __progname_storage;
 	}
 
-#ifdef DYNAMIC
-	/* ld(1) convention: if DYNAMIC = 0 then statically linked */
-#ifdef stupid_gcc
-	if (&_DYNAMIC)
-#else
-	if ( ({volatile caddr_t x = (caddr_t)&_DYNAMIC; x; }) )
-#endif
-		__load_rtld(&_DYNAMIC);
-#endif /* DYNAMIC */
-
-asm("eprol:");
-
 #ifdef MCRT0
 	atexit(_mcleanup);
-	monstartup((u_long)&eprol, (u_long)&etext);
-#endif /* MCRT0 */
+	monstartup((u_long)&_eprol, (u_long)&_etext);
+#endif
 
-asm ("__callmain:");		/* Defined for the benefit of debuggers */
-	exit(main(kfp->kargc, argv, environ));
+	__init();
+
+	exit(main(argc, argv, environ));
 }
 
-#ifdef DYNAMIC
-	asm("	___syscall:");
-	asm("		popl %ecx");
-	asm("		popl %eax");
-	asm("		pushl %ecx");
-	asm("		int $0x80");
-	asm("		pushl %ecx");
-	asm("		jc 1f");
-	asm("		ret");
-	asm("	1:");
-	asm("		movl $-1,%eax");
-	asm("		ret");
 
-#endif /* DYNAMIC */
+static char *
+_strrchr(p, ch)
+register char *p, ch;
+{
+	register char *save;
 
-#include "common.c"
+	for (save = NULL;; ++p) {
+		if (*p == ch)
+			save = (char *)p;
+		if (!*p)
+			return(save);
+	}
+/* NOTREACHED */
+}
 
 #ifdef MCRT0
-asm ("	.text");
+asm ("  .text");
 asm ("_eprol:");
 #endif
+
