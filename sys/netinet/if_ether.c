6c7f164c6351b16aaf4f664f1e82ab1c8c11a715
dhartmei <dhartmei@openbsd.org>
2003-05-04 11:56:32 +0000

    Free mbuf when dropping invalid ARP with ar_hrd != ARPHRD_ETHER, instead of leaking. Reported by orjan.sjelvgren@ignite.com. ok jason@, krw@ and henning@.

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ether.c,v 1.45 2002/06/09 16:26:10 itojun Exp $	*/
+/*	$OpenBSD: if_ether.c,v 1.46 2003/05/04 11:56:32 dhartmei Exp $	*/
 /*	$NetBSD: if_ether.c,v 1.31 1996/05/11 12:59:58 mycroft Exp $	*/
 
 /*
@@ -445,8 +445,10 @@ arpintr()
 			continue;
 
 		ar = mtod(m, struct arphdr *);
-		if (ntohs(ar->ar_hrd) != ARPHRD_ETHER)
+		if (ntohs(ar->ar_hrd) != ARPHRD_ETHER) {
+			m_freem(m);
 			continue;
+		}
 
 		len += 2 * (ar->ar_hln + ar->ar_pln);
 		if (m->m_len < len && (m = m_pullup(m, len)) == NULL)
