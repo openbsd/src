37ab02fdcdf212ec915d9097bbd301896fc753a8
krw <krw@openbsd.org>
2003-05-06 11:40:34 +0000

    sprintf -> snprintf. strncpy -> bcopy since d_packname is not really a string.
    Ensure that all of d_packname is used by not null-terminating string
    that is put in it.
    
    Suggestions and motivation to remove strncpy() from deraadt@.
    
    ok deraadt@ tedu@.

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sd.c,v 1.57 2003/04/06 03:02:44 krw Exp $	*/
+/*	$OpenBSD: sd.c,v 1.58 2003/05/06 11:40:34 krw Exp $	*/
 /*	$NetBSD: sd.c,v 1.111 1997/04/02 02:29:41 mycroft Exp $	*/
 
 /*-
@@ -979,7 +979,8 @@ sdgetdisklabel(dev, sd, lp, clp, spoofonly)
 	struct cpu_disklabel *clp;
 	int spoofonly;
 {
-	char *errstring;
+	size_t len;
+	char *errstring, packname[sizeof(lp->d_packname) + 1];
 
 	bzero(lp, sizeof(struct disklabel));
 	bzero(clp, sizeof(struct cpu_disklabel));
@@ -1002,13 +1003,24 @@ sdgetdisklabel(dev, sd, lp, clp, spoofonly)
 		strncpy(lp->d_typename, "SCSI disk",
 		    sizeof(lp->d_typename));
 
-	if (strlen(sd->name.vendor) + strlen(sd->name.product) + 1 <
-	    sizeof(lp->d_packname))
-		sprintf(lp->d_packname, "%s %s", sd->name.vendor,
-		    sd->name.product);
-	else
-		strncpy(lp->d_packname, sd->name.product,
-		    sizeof(lp->d_packname));
+	/*
+	 * Try to fit '<vendor> <product>' into d_packname. If that doesn't fit
+	 * then leave out '<vendor> ' and use only as much of '<product>' as
+	 * does fit.
+	 */
+	len = snprintf(packname, sizeof(packname), "%s %s",
+	    sd->name.vendor, sd->name.product);
+	if (len > sizeof(lp->d_packname)) {
+		strlcpy(packname, sd->name.product, sizeof(packname));
+		len = strlen(packname);
+	}
+	/*
+	 * It is safe to use len as the count of characters to copy because
+	 * packname is sizeof(lp->d_packname)+1, the string in packname is
+	 * always null terminated and len does not count the terminating null.
+	 * d_packname is not a null terminated string.
+	 */
+	bcopy(packname, lp->d_packname, len);
 
 	lp->d_secperunit = sd->params.disksize;
 	lp->d_rpm = 3600;
