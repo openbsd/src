3236068803c05bb97524c93ab388f7ebd622ecb5
hin <hin@openbsd.org>
2003-05-11 03:49:07 +0000

    Stuff needed for heimdal-0.6rc1

--- kerberosV/include/krb5-types.h
+++ kerberosV/include/krb5-types.h
@@ -1,10 +1,16 @@
 /* krb5-types.h -- this file was generated for i386-unknown-openbsd2.7 by
-                   $Id: krb5-types.h,v 1.1 2001/05/25 08:17:54 hin Exp $ */
+                   $Id: krb5-types.h,v 1.2 2003/05/11 03:49:07 hin Exp $ */
 
 #ifndef __krb5_types_h__
 #define __krb5_types_h__
 
 #include <sys/types.h>
 #include <inttypes.h>
+#include <sys/socket.h>
+
+
+typedef socklen_t krb5_socklen_t;
+#include <unistd.h>
+typedef ssize_t krb5_ssize_t;
 
 #endif /* __krb5_types_h__ */
