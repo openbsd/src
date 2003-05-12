6814f08f9036bd5decd7eb1250a75053cefa0e25
deraadt <deraadt@openbsd.org>
2003-05-12 18:54:29 +0000

    strlcpy; tedu ok

--- sys/compat/common/kern_info_43.c
+++ sys/compat/common/kern_info_43.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_info_43.c,v 1.12 2002/03/14 20:31:31 mickey Exp $	*/
+/*	$OpenBSD: kern_info_43.c,v 1.13 2003/05/12 18:54:29 deraadt Exp $	*/
 /*	$NetBSD: kern_info_43.c,v 1.5 1996/02/04 02:02:22 christos Exp $	*/
 
 /*
@@ -278,17 +278,17 @@ compat_43_sys_getkerninfo(p, v, retval)
 
 		bsdi_si.bsdi_ostype = ((char *)(s - bsdi_strings)) +
 				       sizeof(bsdi_si);
-		strcpy(s, ostype);
+		strlcpy(s, ostype, bsdi_strings + sizeof bsdi_strings - s);
 		s += strlen(s) + 1;
 
 		bsdi_si.bsdi_osrelease = ((char *)(s - bsdi_strings)) +
 					  sizeof(bsdi_si);
-		strcpy(s, osrelease);
+		strlcpy(s, osrelease, bsdi_strings + sizeof bsdi_strings - s);
 		s += strlen(s) + 1;
 
 		bsdi_si.bsdi_machine = ((char *)(s - bsdi_strings)) +
 					sizeof(bsdi_si);
-		strcpy(s, machine);
+		strlcpy(s, machine, bsdi_strings + sizeof bsdi_strings - s);
 		s += strlen(s) + 1;
 
 		needed = sizeof(bsdi_si) + (s - bsdi_strings);
