06e4b6c1b379077ce5093020a68e3763f07aedab
itojun <itojun@openbsd.org>
2003-05-13 02:25:08 +0000

    corrected the pointer value for ifp nd6_rtmsg (from KIU Shueng Chuan) ok dhartmei

--- sys/netinet6/nd6_rtr.c
+++ sys/netinet6/nd6_rtr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6_rtr.c,v 1.23 2002/09/11 03:15:36 itojun Exp $	*/
+/*	$OpenBSD: nd6_rtr.c,v 1.24 2003/05/13 02:25:08 itojun Exp $	*/
 /*	$KAME: nd6_rtr.c,v 1.97 2001/02/07 11:09:13 itojun Exp $	*/
 
 /*
@@ -441,7 +441,7 @@ nd6_rtmsg(cmd, rt)
 	info.rti_info[RTAX_GATEWAY] = rt->rt_gateway;
 	info.rti_info[RTAX_NETMASK] = rt_mask(rt);
 	info.rti_info[RTAX_IFP] =
-	    (struct sockaddr *)TAILQ_FIRST(&rt->rt_ifp->if_addrlist);
+	    TAILQ_FIRST(&rt->rt_ifp->if_addrlist)->ifa_addr;
 	info.rti_info[RTAX_IFA] = rt->rt_ifa->ifa_addr;
 
 	rt_missmsg(cmd, &info, rt->rt_flags, 0);
