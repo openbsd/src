7eb4af50ca8506d439dc4aafbf606b616934ca32
miod <miod@openbsd.org>
2003-05-14 23:18:09 +0000

    Fix uninitialized variable.

--- sys/arch/hppa/dev/pdc.c
+++ sys/arch/hppa/dev/pdc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pdc.c,v 1.24 2002/03/14 03:15:53 millert Exp $	*/
+/*	$OpenBSD: pdc.c,v 1.25 2003/05/14 23:18:09 miod Exp $	*/
 
 /*
  * Copyright (c) 1998-2002 Michael Shalayeff
@@ -196,7 +196,7 @@ pdcopen(dev, flag, mode, p)
 	struct pdc_softc *sc;
 	struct tty *tp;
 	int s;
-	int error = 0, setuptimeout;
+	int error = 0, setuptimeout = 0;
 
 	if (unit >= pdc_cd.cd_ndevs || (sc = pdc_cd.cd_devs[unit]) == NULL)
 		return ENXIO;
