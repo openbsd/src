11078f76680d2666545f0ed200c64bceffc33d2f
fgsch <fgsch@openbsd.org>
2003-05-14 01:51:03 +0000

    remove unneeded FB_FORCELOW; miod@ agreed.

--- sys/arch/sparc/include/fbvar.h
+++ sys/arch/sparc/include/fbvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fbvar.h,v 1.8 2002/11/06 21:06:22 miod Exp $	*/
+/*	$OpenBSD: fbvar.h,v 1.9 2003/05/14 01:51:03 fgsch Exp $	*/
 /*	$NetBSD: fbvar.h,v 1.9 1997/07/07 23:31:30 pk Exp $ */
 
 /*
@@ -50,9 +50,7 @@
  */
 
 #define	FB_PFOUR	0x00010000	/* indicates fb is a pfour fb */
-#define FB_FORCELOW	0x00000001	/* run 24 bit fb in 8 bit mode */
-					/* or cg12 in 1 bit mode */
-#define FB_USERMASK	(0xffff)	/* flags that the user can set */
+#define FB_USERMASK	(0)		/* flags that the user can set */
 
 /*
  * Common frame buffer variables.
