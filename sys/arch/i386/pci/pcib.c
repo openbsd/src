2462b4f1192ecb2f2aaad185c329be9a4a739a85
henning <henning@openbsd.org>
2003-05-18 21:08:01 +0000

    match on Intel 82440MX PCI-ISA which misidentifies itself.

--- sys/arch/i386/pci/pcib.c
+++ sys/arch/i386/pci/pcib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcib.c,v 1.9 2002/03/14 01:26:33 millert Exp $	*/
+/*	$OpenBSD: pcib.c,v 1.10 2003/05/18 21:08:01 henning Exp $	*/
 /*	$NetBSD: pcib.c,v 1.6 1997/06/06 23:29:16 thorpej Exp $	*/
 
 /*-
@@ -86,6 +86,7 @@ pcibmatch(parent, match, aux)
 		case PCI_PRODUCT_INTEL_SIO:
 		case PCI_PRODUCT_INTEL_82371MX:
 		case PCI_PRODUCT_INTEL_82371AB_ISA:
+		case PCI_PRODUCT_INTEL_82440MX_ISA:
 			/* The above bridges mis-identify themselves */
 			return (1);
 		}
