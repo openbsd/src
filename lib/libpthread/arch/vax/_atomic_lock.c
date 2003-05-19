8987c395e7357b65282cb37b84d01decf7b97dc0
miod <miod@openbsd.org>
2003-05-19 18:31:49 +0000

    Missing licence

--- lib/libpthread/arch/vax/_atomic_lock.c
+++ lib/libpthread/arch/vax/_atomic_lock.c
@@ -1,7 +1,8 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.3 2002/11/01 20:14:59 miod Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.4 2003/05/19 18:31:49 miod Exp $	*/
 
 /*
  * Atomic lock for vax
+ * Written by Miodrag Vallat <miod@openbsd.org> - placed in the public domain.
  */
 
 #include "spinlock.h"
