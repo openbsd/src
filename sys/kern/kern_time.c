e23e6a793b4740f0c0b0f863d769f6292cb38174
mickey <mickey@openbsd.org>
2003-05-19 07:24:08 +0000

    no need for nfs headers since nqnfs had been removed way long time ago

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_time.c,v 1.32 2002/10/02 17:43:38 nordin Exp $	*/
+/*	$OpenBSD: kern_time.c,v 1.33 2003/05/19 07:24:08 mickey Exp $	*/
 /*	$NetBSD: kern_time.c,v 1.20 1996/02/18 11:57:06 fvdl Exp $	*/
 
 /*
@@ -46,12 +46,6 @@
 
 #include <sys/mount.h>
 #include <sys/syscallargs.h>
-
-#if defined(NFSCLIENT) || defined(NFSSERVER)
-#include <nfs/rpcv2.h>
-#include <nfs/nfsproto.h>
-#include <nfs/nfs_var.h>
-#endif
 
 #include <machine/cpu.h>
 
