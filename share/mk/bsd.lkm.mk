0f0367accacf6217e5016b7efe26a6f5ad076f51
millert <millert@openbsd.org>
2003-05-20 22:49:13 +0000

    Modules should build w/ propolice now; noticed by Jan Johansson deraadt@ OK

--- share/mk/bsd.lkm.mk
+++ share/mk/bsd.lkm.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.lkm.mk,v 1.18 2003/03/04 22:30:37 mickey Exp $
+#	$OpenBSD: bsd.lkm.mk,v 1.19 2003/05/20 22:49:13 millert Exp $
 
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
@@ -19,7 +19,6 @@ CFLAGS+=	${COPTS} -D_KERNEL -D_LKM -I/sys -I/sys/arch
 .if ${WARNINGS:L} == "yes"
 CFLAGS+=	${CDIAGFLAGS}
 .endif
-CFLAGS+=	-fno-stack-protector 
 
 LDFLAGS+= -r
 .if defined(LKM)
