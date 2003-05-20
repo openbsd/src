07005c65a7cf531f9e78a396ea0cf531bfdbabd6
mickey <mickey@openbsd.org>
2003-05-20 04:22:17 +0000

    an@isapnp w/ the isa card millert gave me shitloads of metric ages ago

---
+++ sys/dev/isa/if_an_isapnp.c
@@ -0,0 +1,89 @@
+/*	$OpenBSD: if_an_isapnp.c,v 1.1 2003/05/20 04:22:17 mickey Exp $	*/
+
+/*
+ * Copyright (c) 2003 Michael Shalayeff
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Michael Shalayeff.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR OR HIS RELATIVES BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF MIND, USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/timeout.h>
+#include <sys/socket.h>
+
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <net/if_media.h>
+
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+#include <machine/bus.h>
+#include <machine/intr.h>
+
+#include <dev/isa/isavar.h>
+
+#include <dev/ic/anvar.h>
+#include <dev/ic/anreg.h>
+
+int an_isapnp_match(struct device *, void *, void *);
+void an_isapnp_attach(struct device *, struct device *, void *);
+
+struct cfattach an_isapnp_ca = {
+	sizeof(struct an_softc), an_isapnp_match, an_isapnp_attach
+};
+
+int
+an_isapnp_match(parent, match, aux)
+	struct device *parent;
+	void *match, *aux;
+{
+	/* XXX This should be more intelligent */
+	return 1;
+}
+
+void
+an_isapnp_attach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct an_softc *sc = (void *)self;
+	struct isa_attach_args *ia = aux;
+
+	sc->an_btag = ia->ia_iot;
+	sc->an_bhandle = ia->ipa_io[0].h;
+
+	sc->sc_ih = isa_intr_establish(ia->ia_ic, ia->ia_irq, IST_EDGE,
+	    IPL_NET, an_intr, sc, sc->sc_dev.dv_xname);
+
+	printf(":");
+
+	/* Should look at ia->ia_devident... */
+	an_attach(sc);
+}
