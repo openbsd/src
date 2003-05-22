590b7217cf1c9818d3a74326f1233fe99ae2a8d9
jason <jason@openbsd.org>
2003-05-22 21:56:45 +0000

    Fix comment: IOTTE_PAMASK is 42..13 not 42..12

--- sys/arch/sparc64/dev/iommureg.h
+++ sys/arch/sparc64/dev/iommureg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iommureg.h,v 1.7 2003/05/16 17:18:14 jason Exp $	*/
+/*	$OpenBSD: iommureg.h,v 1.8 2003/05/22 21:56:45 jason Exp $	*/
 /*	$NetBSD: iommureg.h,v 1.6 2001/07/20 00:07:13 eeh Exp $	*/
 
 /*
@@ -97,7 +97,7 @@ struct iommu_strbuf {
 #define IOTTE_8K	0x0000000000000000LL
 #define IOTTE_STREAM	0x1000000000000000LL	/* Is page streamable? */
 #define	IOTTE_LOCAL	0x0800000000000000LL	/* Accesses to same bus segment? */
-#define IOTTE_PAMASK	0x000007ffffffe000LL	/* Let's assume this is correct (bits 42..12) */
+#define IOTTE_PAMASK	0x000007ffffffe000LL	/* Let's assume this is correct (bits 42..13) */
 #define IOTTE_C		0x0000000000000010LL	/* Accesses to cacheable space */
 #define IOTTE_W		0x0000000000000002LL	/* Writeable */
 #define IOTTE_SOFTWARE	0x0000000000001f80LL	/* For software use (bits 12..7) */
