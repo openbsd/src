9cf0b362605d8b8094b068d007166d141682403f
jason <jason@openbsd.org>
2003-06-02 18:53:18 +0000

    nuke clause 3 & 4

--- sys/dev/ic/bt458reg.h
+++ sys/dev/ic/bt458reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bt458reg.h,v 1.1 2003/05/31 21:01:59 jason Exp $	*/
+/*	$OpenBSD: bt458reg.h,v 1.2 2003/06/02 18:53:18 jason Exp $	*/
 
 /*
  * Copyright (c) 2003 Jason L. Wright (jason@thought.net)
@@ -12,8 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
