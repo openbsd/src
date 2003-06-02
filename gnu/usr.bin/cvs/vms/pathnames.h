f6e5dd4984b041e312202fd9fc01da7229964efb
millert <millert@openbsd.org>
2003-06-02 19:55:59 +0000

    Remove the advertising clause in the UCB license which Berkeley rescinded 22 July 1999.  Proofed by myself and Theo.

--- gnu/usr.bin/cvs/vms/pathnames.h
+++ gnu/usr.bin/cvs/vms/pathnames.h
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -31,7 +27,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)pathnames.h	5.2 (Berkeley) 4/9/90
- *	$Id: pathnames.h,v 1.1.1.1 1996/10/18 03:37:10 tholo Exp $
+ *	$Id: pathnames.h,v 1.2 2003/06/02 19:55:59 millert Exp $
  */
 
 #define	_PATH_RLOGIN	"/usr/bin/rlogin"
