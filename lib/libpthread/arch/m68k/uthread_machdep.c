ff6b2b9c1f5720588699f4160275f8128da6b1bc
miod <miod@openbsd.org>
2003-06-02 08:11:15 +0000

    Strict prototypes.

--- lib/libpthread/arch/m68k/uthread_machdep.c
+++ lib/libpthread/arch/m68k/uthread_machdep.c
@@ -1,11 +1,13 @@
-/*	$OpenBSD: uthread_machdep.c,v 1.1 2000/09/25 09:03:44 d Exp $	*/
+/*	$OpenBSD: uthread_machdep.c,v 1.2 2003/06/02 08:11:15 miod Exp $	*/
 /* David Leonard, <d@csee.uq.edu.au>. Public domain. */
 
 /*
  * Machine-dependent thread state functions for OpenBSD/m68k
  */
 
-#include "uthread_machdep.h"
+#include <pthread.h>
+#include "pthread_private.h"
+
 #define ALIGNBYTES	0x3
 
 struct frame {
