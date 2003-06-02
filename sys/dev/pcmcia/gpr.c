44de83a267bb8dde0075136a803241febbd53386
fgsch <fgsch@openbsd.org>
2003-06-02 19:21:49 +0000

    clean up license.

--- sys/dev/pcmcia/gpr.c
+++ sys/dev/pcmcia/gpr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gpr.c,v 1.9 2003/02/11 19:20:28 mickey Exp $	*/
+/*	$OpenBSD: gpr.c,v 1.10 2003/06/02 19:21:49 fgsch Exp $	*/
 
 /*
  * Copyright (c) 2002, Federico G. Schwindt
@@ -13,9 +13,6 @@
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 3. Neither the name of the author nor the names of its contributors
- *    may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
