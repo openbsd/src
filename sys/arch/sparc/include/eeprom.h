e3926dc7867fcafa1ad6be68b055c4f901ae353a
deraadt <deraadt@openbsd.org>
2003-06-02 18:17:36 +0000

    licence cleanup

--- sys/arch/sparc/include/eeprom.h
+++ sys/arch/sparc/include/eeprom.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eeprom.h,v 1.9 2002/03/14 01:26:43 millert Exp $	*/
+/*	$OpenBSD: eeprom.h,v 1.10 2003/06/02 18:17:36 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1995 Theo de Raadt
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Theo de Raadt.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
