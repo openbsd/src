40f5feb3224e85f6c0afdf83285f0745558f7eac
maja <maja@openbsd.org>
2003-06-02 21:58:26 +0000

    remove clause 3 and 4. -moj

--- usr.sbin/ypserv/ypserv/acl.h
+++ usr.sbin/ypserv/ypserv/acl.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acl.h,v 1.6 2002/07/19 02:38:40 deraadt Exp $ */
+/*	$OpenBSD: acl.h,v 1.7 2003/06/02 21:58:27 maja Exp $ */
 
 /*
  * Copyright (c) 1994 Mats O Jansson <moj@stacken.kth.se>
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Mats O Jansson
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
