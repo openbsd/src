4cde09c2e124795f464714427e6f71fea0918bd2
mickey <mickey@openbsd.org>
2003-06-02 20:28:04 +0000

    three four kill ...

--- lib/libc/arch/hppa/SYS.h
+++ lib/libc/arch/hppa/SYS.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: SYS.h,v 1.12 2002/10/31 23:07:37 mickey Exp $	*/
+/*	$OpenBSD: SYS.h,v 1.13 2003/06/02 20:28:04 mickey Exp $	*/
 
 /*
  * Copyright (c) 1998-2002 Michael Shalayeff
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Michael Shalayeff.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
