7ebf32b3d34aa1c1978835d6b9557d17f0bc62d2
jason <jason@openbsd.org>
2003-06-03 01:41:42 +0000

    nuke clause 3 & 4 (with henric's permission)

--- sys/arch/sparc64/dev/schizo.c
+++ sys/arch/sparc64/dev/schizo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: schizo.c,v 1.13 2003/05/11 19:20:36 jason Exp $	*/
+/*	$OpenBSD: schizo.c,v 1.14 2003/06/03 01:41:42 jason Exp $	*/
 
 /*
  * Copyright (c) 2002 Jason L. Wright (jason@thought.net)
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Jason L. Wright
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
