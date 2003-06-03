7bdea3879b63055b9d07f8f5bbfd0a72f52e91ed
weingart <weingart@openbsd.org>
2003-06-03 00:52:35 +0000

    Nuke terms 3 & 4 on my code.

--- usr.sbin/config/misc.c
+++ usr.sbin/config/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.c,v 1.2 2002/07/14 02:59:41 deraadt Exp $	*/
+/*	$OpenBSD: misc.c,v 1.3 2003/06/03 00:52:35 weingart Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by Tobias Weingartner.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -31,7 +26,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: misc.c,v 1.2 2002/07/14 02:59:41 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: misc.c,v 1.3 2003/06/03 00:52:35 weingart Exp $";
 #endif
 
 #include <sys/types.h>
