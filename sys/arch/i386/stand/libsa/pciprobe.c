6258f1768a3759271f102e760f235711c7e946e9
deraadt <deraadt@openbsd.org>
2003-06-04 17:04:05 +0000

    fix some 3/4 for toby

--- sys/arch/i386/stand/libsa/pciprobe.c
+++ sys/arch/i386/stand/libsa/pciprobe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pciprobe.c,v 1.5 2000/10/25 17:12:07 mickey Exp $	*/
+/*	$OpenBSD: pciprobe.c,v 1.6 2003/06/04 17:04:05 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Tobias Weingartner.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR 
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
