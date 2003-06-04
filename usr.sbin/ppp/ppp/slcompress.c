0404cd96830d615168bf13fdb767b9eba3c194b8
deraadt <deraadt@openbsd.org>
2003-06-04 16:08:07 +0000

    fix some more UCB term 3

--- usr.sbin/ppp/ppp/slcompress.c
+++ usr.sbin/ppp/ppp/slcompress.c
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -40,7 +36,7 @@
  * Van Jacobson (van@helios.ee.lbl.gov), Dec 31, 1989:
  *	- Initial distribution.
  *
- * $OpenBSD: slcompress.c,v 1.16 2003/06/02 04:29:23 millert Exp $
+ * $OpenBSD: slcompress.c,v 1.17 2003/06/04 16:08:08 deraadt Exp $
  */
 
 #include <sys/param.h>
--- usr.sbin/ppp/ppp/slcompress.h
+++ usr.sbin/ppp/ppp/slcompress.h
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -35,7 +31,7 @@
  *	Van Jacobson (van@helios.ee.lbl.gov), Dec 31, 1989:
  *	- Initial distribution.
  *
- * $OpenBSD: slcompress.h,v 1.6 2003/06/02 04:29:23 millert Exp $
+ * $OpenBSD: slcompress.h,v 1.7 2003/06/04 16:08:08 deraadt Exp $
  */
 
 #define MIN_VJ_STATES 3
