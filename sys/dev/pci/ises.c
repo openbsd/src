31efb3005403153d3f51cd9a3a5654148732a26b
ho <ho@openbsd.org>
2003-06-07 11:31:24 +0000

    Remove clause 3.

--- sys/dev/pci/ises.c
+++ sys/dev/pci/ises.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ises.c,v 1.23 2003/02/18 18:16:21 jason Exp $	*/
+/*	$OpenBSD: ises.c,v 1.24 2003/06/07 11:31:24 ho Exp $	*/
 
 /*
  * Copyright (c) 2000, 2001 Håkan Olsson (ho@crt.se)
@@ -12,8 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
--- sys/dev/pci/isesreg.h
+++ sys/dev/pci/isesreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: isesreg.h,v 1.8 2001/09/21 19:41:13 ho Exp $ $	*/
+/*	$OpenBSD: isesreg.h,v 1.9 2003/06/07 11:31:24 ho Exp $ $	*/
 
 /*
  * Copyright (c) 2000 Håkan Olsson (ho@crt.se)
@@ -13,8 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
