b2f9790acc3e1d58bace7637ecacec44763f62d6
pvalchev <pvalchev@openbsd.org>
2003-06-12 18:10:22 +0000

    spelling

--- usr.bin/mg/theo.c
+++ usr.bin/mg/theo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: theo.c,v 1.55 2003/05/26 12:59:16 henning Exp $	*/
+/*	$OpenBSD: theo.c,v 1.56 2003/06/12 18:10:22 pvalchev Exp $	*/
 /*
  * Copyright (c) 2002 Artur Grabowski <art@openbsd.org>
  * All rights reserved. 
@@ -138,7 +138,7 @@ static const char *talk[] = {
 	"You guys are about four days behind on theo.c",
 	"I'm just talking.",
 	"Vegetarian my ass",
-	"Wait a minute, that's a McNallys!",
+	"Wait a minute, that's a McNally's!",
 	"Your connection is breaking up.",
 	"germans are not allowed to get involved there"
 };
