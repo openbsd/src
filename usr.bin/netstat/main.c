2a29d244cfe1d0517792802707a803de1959f76b
jmc <jmc@openbsd.org>
2003-06-12 07:39:16 +0000

    - document -ltu options; from PR 3307 (Zoltan Glozik) - sync SYNOPSIS and usage();
    ok henning@ deraadt@

--- usr.bin/netstat/main.c
+++ usr.bin/netstat/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.36 2003/06/03 02:56:13 millert Exp $	*/
+/*	$OpenBSD: main.c,v 1.37 2003/06/12 07:39:16 jmc Exp $	*/
 /*	$NetBSD: main.c,v 1.9 1996/05/07 02:55:02 thorpej Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ char copyright[] =
 #if 0
 static char sccsid[] = "from: @(#)main.c	8.4 (Berkeley) 3/1/94";
 #else
-static char *rcsid = "$OpenBSD: main.c,v 1.36 2003/06/03 02:56:13 millert Exp $";
+static char *rcsid = "$OpenBSD: main.c,v 1.37 2003/06/12 07:39:16 jmc Exp $";
 #endif
 #endif /* not lint */
 
@@ -638,10 +638,12 @@ usage(void)
 	(void)fprintf(stderr,
 "usage: %s [-Aan] [-f address_family] [-M core] [-N system]\n", __progname);
 	(void)fprintf(stderr,
-"       %s [-gimnrs] [-f address_family] [-M core] [-N system]\n", __progname);
+"       %s [-bdgilmnqrstu] [-f address_family] [-M core] [-N system]\n", __progname);
 	(void)fprintf(stderr,
-"       %s [-n] [-I interface] [-M core] [-N system] [-w wait]\n", __progname);
+"       %s [-bdn] [-I interface] [-M core] [-N system] [-w wait]\n", __progname);
 	(void)fprintf(stderr,
 "       %s [-M core] [-N system] [-p protocol]\n", __progname);
+	(void)fprintf(stderr,
+"       %s [-s] [-f address_family] [-i] [-I interface]\n", __progname);
 	exit(1);
 }
