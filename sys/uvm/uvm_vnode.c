0c7004f2e2cdfebd36218d7c97b74ad8a0f34227
henric <henric@openbsd.org>
2003-06-12 05:55:05 +0000

    Fix compile with NFS but not FIFO.
    ok deraadt@

--- sys/uvm/uvm_vnode.c
+++ sys/uvm/uvm_vnode.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_vnode.c,v 1.36 2002/11/19 18:34:41 jason Exp $	*/
+/*	$OpenBSD: uvm_vnode.c,v 1.37 2003/06/12 05:55:05 henric Exp $	*/
 /*	$NetBSD: uvm_vnode.c,v 1.36 2000/11/24 20:34:01 chs Exp $	*/
 
 /*
@@ -1818,16 +1818,20 @@ uvm_vnp_uncache(vp)
 #if defined(NFSCLIENT)
 		extern int (**nfsv2_vnodeop_p)(void *);
 		extern int (**spec_nfsv2nodeop_p)(void *);
+#if defined(FIFO)
 		extern int (**fifo_nfsv2nodeop_p)(void *);
+#endif	/* defined(FIFO) */
 
 		/* vnode is NOT VOP_LOCKed: some vnode types _never_ lock */
 		if (vp->v_op == nfsv2_vnodeop_p ||
 		    vp->v_op == spec_nfsv2nodeop_p) {
 			is_ok_anyway = TRUE;
 		}
+#if defined(FIFO)
 		if (vp->v_op == fifo_nfsv2nodeop_p) {
 			is_ok_anyway = TRUE;
 		}
+#endif	/* defined(FIFO) */
 #endif	/* defined(NFSSERVER) || defined(NFSCLIENT) */
 		if (!is_ok_anyway)
 			panic("uvm_vnp_uncache: vnode not locked!");
