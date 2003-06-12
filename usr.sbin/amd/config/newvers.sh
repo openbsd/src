3e0810910d5b418f5a7d56724f68684d76cd5bdb
deraadt <deraadt@openbsd.org>
2003-06-12 01:15:08 +0000

    another UCB term 3 removal

--- usr.sbin/amd/config/newvers.sh
+++ usr.sbin/amd/config/newvers.sh
@@ -16,11 +16,7 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#	This product includes software developed by the University of
-#	California, Berkeley and its contributors.
-# 4. Neither the name of the University nor the names of its contributors
+# 3. Neither the name of the University nor the names of its contributors
 #    may be used to endorse or promote products derived from this software
 #    without specific prior written permission.
 #
@@ -37,7 +33,7 @@
 # SUCH DAMAGE.
 #
 #	from: @(#)newvers.sh	8.1 (Berkeley) 6/6/93
-#	$Id: newvers.sh,v 1.3 1997/02/23 03:41:56 deraadt Exp $
+#	$Id: newvers.sh,v 1.4 2003/06/12 01:15:08 deraadt Exp $
 #
 PATH=/usr/ucb:/bin:/usr/bin:$PATH
 if [ $# -ne 3 ]; then echo "Usage: newvers program arch os" >&2; exit 1; fi
