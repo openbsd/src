a727abd3984bc388b88ceccd89e0115187352100
jason <jason@openbsd.org>
2003-06-18 19:11:49 +0000

    configure the interface to properly support vlan frames

--- sys/arch/sparc/dev/hme.c
+++ sys/arch/sparc/dev/hme.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hme.c,v 1.41 2003/06/02 18:40:59 jason Exp $	*/
+/*	$OpenBSD: hme.c,v 1.42 2003/06/18 19:11:49 jason Exp $	*/
 
 /*
  * Copyright (c) 1998 Jason L. Wright (jason@thought.net)
@@ -36,6 +36,9 @@
  * this driver.
  */
 
+#include "bpfilter.h"
+#include "vlan.h"
+
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
@@ -61,10 +64,13 @@
 #include <netinet/if_ether.h>
 #endif
 
-#include "bpfilter.h"
 #if NBPFILTER > 0
 #include <net/bpf.h>
 #include <net/bpfdesc.h>
+#endif
+
+#if NVLAN > 0
+#include <net/if_vlan_var.h>
 #endif
 
 #include <machine/autoconf.h>
@@ -242,6 +248,7 @@ hmeattach(parent, self, aux)
 	ifp->if_watchdog = hmewatchdog;
 	ifp->if_flags =
 		IFF_BROADCAST | IFF_SIMPLEX | IFF_NOTRAILERS | IFF_MULTICAST;
+	ifp->if_capabilities |= IFCAP_VLAN_MTU;
 	IFQ_SET_MAXLEN(&ifp->if_snd, HME_TX_RING_SIZE);
 	IFQ_SET_READY(&ifp->if_snd);
 
@@ -542,6 +549,7 @@ hmeinit(sc)
 			   sc->sc_arpcom.ac_enaddr[3];
 	cr->mac_addr2 = (sc->sc_arpcom.ac_enaddr[4] << 8) |
 			   sc->sc_arpcom.ac_enaddr[5];
+	cr->tx_pkt_max = cr->rx_pkt_max = HME_MTU;
 
 	cr->jsize = HME_DEFAULT_JSIZE;
 	cr->ipkt_gap1 = HME_DEFAULT_IPKT_GAP1;
