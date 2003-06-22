1aa20553248626a6cef29c50b25fdef59099f586
deraadt <deraadt@openbsd.org>
2003-06-22 22:18:41 +0000

    bye bye gnu gzip.  The only parts we keep using are the scripts of questionable license.  These will be rewritten soon I hope.  Our compress, linked against libz, now does everything gzip does.

--- gnu/usr.bin/gzip/crypt.c
+++
@@ -1,6 +0,0 @@
-/* crypt.c (dummy version) -- do not perform encryption
- * Hardly worth copyrighting :-)
- */
-#ifdef RCSID
-static char rcsid[] = "$Id: crypt.c,v 1.1.1.1 1995/10/18 08:40:52 deraadt Exp $";
-#endif
