5decc9e4a6c078b357e0348c93d0c46c5ed69c95
jason <jason@openbsd.org>
2003-06-23 16:49:44 +0000

    Remove some debugging cruft.

--- sys/arch/sparc64/dev/creator.c
+++ sys/arch/sparc64/dev/creator.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: creator.c,v 1.27 2003/06/20 19:54:37 miod Exp $	*/
+/*	$OpenBSD: creator.c,v 1.28 2003/06/23 16:49:44 jason Exp $	*/
 
 /*
  * Copyright (c) 2002 Jason L. Wright (jason@thought.net)
@@ -504,10 +504,8 @@ creator_mmap(vsc, off, prot)
 			    off < (creator_map[i].uoff + creator_map[i].ulen))
 				break;
 		}
-		if (i == CREATOR_NMAPPINGS) {
-			printf("didn't find %llx\n", off);
+		if (i == CREATOR_NMAPPINGS)
 			break;
-		}
 
 		off -= creator_map[i].uoff;
 		off += creator_map[i].poff;
