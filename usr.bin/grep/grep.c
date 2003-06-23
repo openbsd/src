4d90a27a7bef2a33f3f003f4dd96ccb4c49d20e0
tedu <tedu@openbsd.org>
2003-06-23 22:32:48 +0000

    strncpy -> memcpy per deraadt suggestion. also add a note why we can't use strlcpy.

--- usr.bin/grep/grep.c
+++ usr.bin/grep/grep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: grep.c,v 1.14 2003/06/23 22:27:09 tedu Exp $	*/
+/*	$OpenBSD: grep.c,v 1.15 2003/06/23 22:32:48 tedu Exp $	*/
 
 /*-
  * Copyright (c) 1999 James Howard and Dag-Erling Coïdan Smørgrav
@@ -171,7 +171,8 @@ add_pattern(char *pat, size_t len)
 	if (pat[len - 1] == '\n')
 		--len;
 	pattern[patterns] = grep_malloc(len + 1);
-	strncpy(pattern[patterns], pat, len);
+	/* pat may not be NUL-terminated */
+	memcpy(pattern[patterns], pat, len);
 	pattern[patterns][len] = '\0';
 	++patterns;
 
