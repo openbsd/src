ccfe1020e75c93ecc32936162815ee614e2c5283
drahn <drahn@openbsd.org>
2003-06-24 14:43:12 +0000

    For now depend on the gccism __CHAR_UNSIGNED__ to define if we have (un)signed chars. Fixes other defines if -fsigned-chars is specified. ok deraadt.

--- sys/arch/powerpc/include/internal_types.h
+++ sys/arch/powerpc/include/internal_types.h
@@ -1,8 +1,10 @@
-/* $OpenBSD: internal_types.h,v 1.1 2002/04/24 21:53:12 espie Exp $ */
+/* $OpenBSD: internal_types.h,v 1.2 2003/06/24 14:43:12 drahn Exp $ */
 /* Public domain */
 #ifndef _MACHINE_INTERNAL_TYPES_H_
 #define _MACHINE_INTERNAL_TYPES_H_
 
+#ifdef __CHAR_UNSIGNED__
 #define __machine_has_unsigned_chars
+#endif
 
 #endif
