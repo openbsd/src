22ccc3b32707bf88258101d393b97abdc2136466
itojun <itojun@openbsd.org>
2003-06-28 14:42:16 +0000

    i don't think pfsync needs a link-local addr.

--- sys/netinet6/in6_ifattach.c
+++ sys/netinet6/in6_ifattach.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6_ifattach.c,v 1.33 2003/05/14 05:23:37 itojun Exp $	*/
+/*	$OpenBSD: in6_ifattach.c,v 1.34 2003/06/28 14:42:16 itojun Exp $	*/
 /*	$KAME: in6_ifattach.c,v 1.124 2001/07/18 08:32:51 jinmei Exp $	*/
 
 /*
@@ -574,6 +574,7 @@ in6_ifattach(ifp, altifp)
 	case IFT_BRIDGE:
 	case IFT_ENC:
 	case IFT_PFLOG:
+	case IFT_PFSYNC:
 		return;
 	case IFT_PROPVIRTUAL:
 		if (strncmp("bridge", ifp->if_xname, sizeof("bridge")) == 0 &&
