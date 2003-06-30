93fe51287ba64ea4b03291a8d90af39e98bc5f90
henning <henning@openbsd.org>
2003-06-30 10:51:09 +0000

    missing pf_tag_purge() cedric made me check

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_bridge.c,v 1.119 2003/06/25 09:41:18 henning Exp $	*/
+/*	$OpenBSD: if_bridge.c,v 1.120 2003/06/30 10:51:09 henning Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Jason L. Wright (jason@thought.net)
@@ -2080,6 +2080,9 @@ bridge_flushrule(struct bridge_iflist *bif)
 #endif
 		free(p, M_DEVBUF);
 	}
+#if NPF > 0
+	pf_tag_purge();
+#endif
 	return (0);
 }
 
