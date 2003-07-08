11b9f7e5b2d0f0625dd4d89acdfa292b563ea6c7
jfb <jfb@openbsd.org>
2003-07-08 00:11:50 +0000

    KNF + ansification
    ok deraadt@, vincent@

--- lib/libc/gen/time.c
+++ lib/libc/gen/time.c
@@ -28,21 +28,20 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: time.c,v 1.3 2003/06/02 20:18:35 millert Exp $";
+static char rcsid[] = "$OpenBSD: time.c,v 1.4 2003/07/08 00:11:50 jfb Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/time.h>
 
 time_t
-time(t)
-	time_t *t;
+time(time_t *t)
 {
 	struct timeval tt;
 
 	if (gettimeofday(&tt, (struct timezone *)0) < 0)
-		return(-1);
+		return (-1);
 	if (t)
-		*t = tt.tv_sec;
-	return(tt.tv_sec);
+		*t = (time_t)tt.tv_sec;
+	return (tt.tv_sec);
 }
