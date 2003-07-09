5650c8b4ca61f8ec0b23603645722d8aa137294b
jason <jason@openbsd.org>
2003-07-09 02:22:48 +0000

    add a bunch more traps from the v9 spec

--- sys/arch/sparc64/include/trap.h
+++ sys/arch/sparc64/include/trap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.h,v 1.2 2001/08/20 20:23:52 jason Exp $	*/
+/*	$OpenBSD: trap.h,v 1.3 2003/07/09 02:22:48 jason Exp $	*/
 /*	$NetBSD: trap.h,v 1.4 1999/06/07 05:28:04 eeh Exp $ */
 
 /*
@@ -44,28 +44,34 @@
 /*	through		0x00f	   unused */
 #define T_ILLINST	0x010	/* (7) illegal instruction */
 #define T_PRIVINST	0x011	/* (6) privileged opcode */
-/*			0x012	   unused */
+#define	T_UNIMP_LDD	0x012	/* (6) unimplemented LDD */
+#define	T_UNIMP_STD	0x013	/* (6) unimplemented STD */
+/*			0x014	   unused */
 /*	through		0x01f	   unused */
 #define T_FPDISABLED	0x020	/* (8) fpu disabled */
 #define T_FP_IEEE_754	0x021	/* (11) ieee 754 exception */
 #define T_FP_OTHER	0x022	/* (11) other fp exception */
 #define T_TAGOF		0x023	/* (14) tag overflow */
 #define T_CLEAN_WINDOW	0x024	/* (10) clean window exception */
+/*			0x025	   unused */
 /*	through		0x027	   unused */
 #define T_DIV0		0x028	/* (15) division routine was handed 0 */
+#define	T_PROCERR	0x029	/* (4) internal processor error */
 /*			0x02a	   unused */
 /*	through		0x02f	   unused */
 #define	T_DATAFAULT	0x030	/* (12) address fault during data fetch */
-/*			0x031	   unused */
-#define T_DATA_ERROR	0x032
-#define T_DATA_PROT	0x033	/* Data protection ??? */
+#define	T_DATA_MMU_MISS	0x031	/* (12) data access MMU miss */
+#define T_DATA_ERROR	0x032	/* (12) data access error */
+#define T_DATA_PROT	0x033	/* (12) Data protection ??? */
 #define	T_ALIGN		0x034	/* (10) address not properly aligned */
 #define	T_LDDF_ALIGN	0x035	/* (10) LDDF address not properly aligned */
 #define	T_STDF_ALIGN	0x036	/* (10) STDF address not properly aligned */
 #define T_PRIVACT	0x037	/* (11) privileged action */
-/*			0x038	   unused */
-/*	through		0x03F	   unused */
-#define T_ASYNC_ERROR	0x040	/* ???? */
+#define	T_LDQF_ALIGN	0x038	/* (10) LDQF address not properly aligned */
+#define	T_STQF_ALIGN	0x039	/* (10) STQF address not properly aligned */
+/*			0x03a	   unused */
+/*	through		0x03f	   unused */
+#define T_ASYNC_ERROR	0x040	/* (2) ???? */
 #define	T_L1INT		0x041	/* (31) level 1 interrupt */
 #define	T_L2INT		0x042	/* (30) level 2 interrupt */
 #define	T_L3INT		0x043	/* (29) level 3 interrupt */
@@ -82,7 +88,7 @@
 #define	T_L14INT	0x04e	/* (18) level 14 interrupt */
 #define	T_L15INT	0x04f	/* (17) level 15 interrupt */
 /*			0x050	   unused */
-/*	through		0x05F	   unused */
+/*	through		0x05f	   unused */
 #define T_INTVEC	0x060	/* (16) interrupt vector [Interrupt Global Regs]*/
 #define T_PA_WATCHPT	0x061	/* (12) Physical addr data watchpoint */
 #define T_VA_WATCHPT	0x062	/* (11) Virtual addr data watchpoint */
@@ -91,17 +97,17 @@
 /*	through		0x067	   unused */
 #define T_FDMMU_MISS	0x068	/* (2) fast data access MMU miss */
 /*	through		0x06b	   unused */
-#define T_FDMMU_PROT	0x06C	/* (2) fast data access protection */
-/*	through		0x06F	   unused */
+#define T_FDMMU_PROT	0x06c	/* (2) fast data access protection */
+/*	through		0x06f	   unused */
 /*  0x070...0x07f implementation dependent exceptions */
 #define T_SPILL_N_NORM	0x080	/* (9) spill (n=0..7) normal */
-/*	through		0x09F	   unused */
+/*	through		0x09f	   unused */
 #define T_SPILL_N_OTHER	0x0a0	/* (9) spill (n=0..7) other */
-/*	through		0x0bF	   unused */
+/*	through		0x0bf	   unused */
 #define T_FILL_N_NORM	0x0c0	/* (9) fill (n=0..7) normal */
-/*	through		0x0dF	   unused */
+/*	through		0x0df	   unused */
 #define T_FILL_N_OTHER	0x0e0	/* (9) fill (n=0..7) other */
-/*	through		0x0fF	   unused */
+/*	through		0x0ff	   unused */
 
 /* beginning of `user' vectors (from trap instructions) - all priority 16 */
 #define	T_SUN_SYSCALL	0x100	/* system call */
