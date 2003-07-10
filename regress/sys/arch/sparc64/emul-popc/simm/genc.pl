5cac43d7e831ad2dc6afa8cc70d6f15b41b9d8f8
jason <jason@openbsd.org>
2003-07-10 15:21:12 +0000

    unbreak

--- regress/sys/arch/sparc64/emul-popc/simm/genc.pl
+++ regress/sys/arch/sparc64/emul-popc/simm/genc.pl
@@ -1,5 +1,5 @@
-#!/bin/sh
-#	$OpenBSD: genc.pl,v 1.2 2003/07/10 14:42:36 jason Exp $
+#!/bin/perl
+#	$OpenBSD: genc.pl,v 1.3 2003/07/10 15:21:12 jason Exp $
 #
 # Copyright (c) 2003 Jason L. Wright (jason@thought.net)
 # All rights reserved.
