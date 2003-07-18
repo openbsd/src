d0cd8fb614711af7870fdec0e835f1d6c6c2ee9c
grange <grange@openbsd.org>
2003-07-18 05:02:08 +0000

    sync

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *		OpenBSD: pcidevs,v 1.650 2003/07/15 19:04:24 deraadt Exp 
+ *		OpenBSD: pcidevs,v 1.651 2003/07/18 04:59:37 grange Exp 
  */
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $ 	*/
 
@@ -3093,6 +3093,10 @@ static const struct pci_known_product pci_known_products[] = {
 	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801EB_LPC,
 	    "82801EB/ER LPC",
+	},
+	{
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801EB_SATA,
+	    "82801EB/ER SATA",
 	},
 	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801EB_USB_1,
