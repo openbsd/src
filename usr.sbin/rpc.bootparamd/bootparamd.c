f6073c030d7ca238977470ad8f0913cdbd7b0edb
mickey <mickey@openbsd.org>
2003-07-21 19:54:47 +0000

    use __progname

--- usr.sbin/rpc.bootparamd/bootparamd.c
+++ usr.sbin/rpc.bootparamd/bootparamd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bootparamd.c,v 1.15 2003/07/06 21:34:03 deraadt Exp $	*/
+/*	$OpenBSD: bootparamd.c,v 1.16 2003/07/21 19:54:47 mickey Exp $	*/
 
 /*
  * This code is not copyright, and is placed in the public domain.
@@ -57,8 +57,9 @@ extern int optind;
 static void
 usage(void)
 {
-	fprintf(stderr,
-	    "usage: rpc.bootparamd [-d] [-s] [-r router] [-f bootparmsfile]\n");
+	extern char *__progname;
+	fprintf(stderr, "usage: %s [-d] [-s] [-r router] [-f bootparmsfile]\n",
+	    __progname);
 	exit(1);
 }
 
