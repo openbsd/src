0a9b58de6c43585f0d8a086f3030ced6d789768f
deraadt <deraadt@openbsd.org>
2003-07-22 17:21:07 +0000

    nicer

--- usr.bin/patch/util.h
+++ usr.bin/patch/util.h
@@ -1,15 +1,15 @@
-/*	$OpenBSD: util.h,v 1.7 2003/07/22 17:18:49 otto Exp $	*/
+/*	$OpenBSD: util.h,v 1.8 2003/07/22 17:21:07 deraadt Exp $	*/
 
 char		*fetchname(char *, int, int);
 int		move_file(char *, char *);
 void		copy_file(char *, char *);
 void		say(char *, ...);
 void		fatal(char *, ...)
-	__attribute__((__format__(__printf__, 1, 2)));
+		    __attribute__((__format__(__printf__, 1, 2)));
 void		pfatal(char *, ...)
-	__attribute__((__format__(__printf__, 1, 2)));
+		    __attribute__((__format__(__printf__, 1, 2)));
 void		ask(char *, ...)
-	__attribute__((__format__(__printf__, 1, 2)));
+		    __attribute__((__format__(__printf__, 1, 2)));
 char		*savestr(char *);
 void		set_signals(int);
 void		ignore_signals(void);
