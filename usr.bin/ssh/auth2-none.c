63b7cde4f70349651d1823f76a0137d2b4eac241
markus <markus@openbsd.org>
2003-07-31 09:21:02 +0000

    check whether passwd auth is allowd, similar to proto 1; rob@pitman.co.za ok henning

--- usr.bin/ssh/auth2-none.c
+++ usr.bin/ssh/auth2-none.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: auth2-none.c,v 1.4 2002/06/27 10:35:47 deraadt Exp $");
+RCSID("$OpenBSD: auth2-none.c,v 1.5 2003/07/31 09:21:02 markus Exp $");
 
 #include "auth.h"
 #include "xmalloc.h"
@@ -96,7 +96,9 @@ userauth_none(Authctxt *authctxt)
 	none_enabled = 0;
 	packet_check_eom();
 	userauth_banner();
-	return (authctxt->valid ? PRIVSEP(auth_password(authctxt, "")) : 0);
+	if (options.password_authentication && authctxt->valid)
+		return (PRIVSEP(auth_password(authctxt, "")));
+	return (0);
 }
 
 Authmethod method_none = {
