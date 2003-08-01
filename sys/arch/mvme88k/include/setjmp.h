c7a66f54591207777c2f19618efdd3010194c6d7
miod <miod@openbsd.org>
2003-08-01 07:41:33 +0000

    Fix the *longjmp() behaviour - it is legal to reuse a jmp_buf several times. Gets us a working perl 5.8.

--- sys/arch/mvme88k/include/setjmp.h
+++ sys/arch/mvme88k/include/setjmp.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD: setjmp.h,v 1.6 2001/08/12 12:03:02 heko Exp $	*/
+/*	$OpenBSD: setjmp.h,v 1.7 2003/08/01 07:41:33 miod Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 #ifndef __MACHINE_SETJMP_H__
 #define __MACHINE_SETJMP_H__
-#define	_JBLEN	22		/* size, in longs, of a jmp_buf */
+#define	_JBLEN	21		/* size, in longs, of a jmp_buf */
 #endif /* __MACHINE_SETJMP_H__ */
