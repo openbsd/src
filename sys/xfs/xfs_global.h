ce44ba73726164d0e42143d77a459018ca1d62cd
hin <hin@openbsd.org>
2003-08-12 07:43:50 +0000

    Import xfs from arla-current as of 5 Aug 2003.
    ok deraadt@

--- sys/xfs/xfs_global.h
+++ sys/xfs/xfs_global.h
@@ -1,9 +1,9 @@
-#ifndef __XFS_GLOBAL_H
-#define __XFS_GLOBAL_H
+#ifndef __NNPFS_GLOBAL_H
+#define __NNPFS_GLOBAL_H
 
 #ifndef RCSID
 #define RCSID(msg) \
 static /**/const char *const rcsid[] = { (char *)rcsid, "\100(#)" msg }
 #endif /* RCSID */
 
-#endif  /* __XFS_GLOBAL_H */
+#endif  /* __NNPFS_GLOBAL_H */
