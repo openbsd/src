0713cdde1b0dbb7dda23e83fcfeca500fce17e07
tedu <tedu@openbsd.org>
2003-08-18 01:51:57 +0000

    spelling in comment, jonathon gray

--- sys/kern/vfs_init.c
+++ sys/kern/vfs_init.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_init.c,v 1.15 2003/07/24 22:00:24 mickey Exp $	*/
+/*	$OpenBSD: vfs_init.c,v 1.16 2003/08/18 01:51:57 tedu Exp $	*/
 /*	$NetBSD: vfs_init.c,v 1.6 1996/02/09 19:00:58 christos Exp $	*/
 
 /*
@@ -131,7 +131,7 @@ vfs_opv_init_explicit(vfs_opv_desc)
 		/*
 		 * Sanity check:  is this operation listed
 		 * in the list of operations?  We check this
-		 * by seeing if its offest is zero.  Since
+		 * by seeing if its offset is zero.  Since
 		 * the default routine should always be listed
 		 * first, it should be the only one with a zero
 		 * offset.  Any other operation with a zero
