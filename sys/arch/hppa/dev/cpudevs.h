d7690bb5317d719c1ae6f0f3b9e90baab0e4db2d
mickey <mickey@openbsd.org>
2003-08-20 23:49:39 +0000

    regen

--- sys/arch/hppa/dev/cpudevs.h
+++ sys/arch/hppa/dev/cpudevs.h
@@ -1,7 +1,7 @@
 /*
  * THIS FILE AUTOMATICALLY GENERATED. DO NOT EDIT.
  * generated from:
- *	OpenBSD: cpudevs,v 1.26 2003/08/20 18:52:40 mickey Exp 
+ *	OpenBSD: cpudevs,v 1.27 2003/08/20 23:49:23 mickey Exp 
  */
 
 #define	HPPA_TYPE_NPROC	0x00
