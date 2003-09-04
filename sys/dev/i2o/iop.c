5bad91e72ba71a8c1e81be0522d373fc5f46a741
avsm <avsm@openbsd.org>
2003-09-04 03:45:53 +0000

    bzero the full malloced space, not the sizeof the pointer ok tedu fgsch deraadt

--- sys/dev/i2o/iop.c
+++ sys/dev/i2o/iop.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iop.c,v 1.23 2003/04/27 11:22:52 ho Exp $	*/
+/*	$OpenBSD: iop.c,v 1.24 2003/09/04 03:45:53 avsm Exp $	*/
 /*	$NetBSD: iop.c,v 1.12 2001/03/21 14:27:05 ad Exp $	*/
 
 /*-
@@ -690,7 +690,7 @@ iop_reconfigure(struct iop_softc *sc, u_int chgind)
 		DPRINTF(("iop_reconfigure: out of memory\n"));
 		return (ENOMEM);
 	}
-	bzero(sc->sc_tidmap, sizeof(sc->sc_tidmap));
+	bzero(sc->sc_tidmap, sc->sc_nlctent * sizeof(struct iop_tidmap));
 
 	/* Allow 1 queued command per device while we're configuring. */
 	iop_adjqparam(sc, 1);
