082bf777c52fa1bfa28224f4501b808145dd72de
deraadt <deraadt@openbsd.org>
2003-09-15 23:29:31 +0000

    tree unlock, as we move to 3.4-current PLEASE NOTE: the tree is unlocking early for developers who were involved in the release process.  for other developers the tree is NOT UNLOCKED YET. the following people are unlocked:     beck, tdeval, nick, krw, pb, fries, matthieu, marcm, cedric, mdw,     drahn, sturm, millert, andreas, markus, jason, avsm, wim, frantzen,     fgs, henning, mcbride, hugh, tedu, nate, weingart, canacar, nordin,     miod, danh, wilfried, deraadt, itojun, mickey, miod, deraadt Other developers are not unlocked.  If I missed anyone, talk to me. Sorry, but our development process is oriented around "hack for 5 months, then do 1 month of release engineering.  If a developer hacks for 5 months and then does not show up to help with release engineering, then we take that to mean they are less serious, and can give a bit of pause to the serious developers who now want to get things into the tree post-unlock.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.56 2003/09/02 21:49:49 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.57 2003/09/15 23:29:31 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -68,9 +68,9 @@ const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
 const char osversion[] = "${id}#${v}";
 const char sccs[] =
-    "    @(#)${ost} ${osr} (${id}) #${v}: ${t}\n";
+    "    @(#)${ost} ${osr}-current (${id}) #${v}: ${t}\n";
 const char version[] =
-    "${ost} ${osr} (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
+    "${ost} ${osr}-current (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
 eof
 
 expr ${v} + 1 > version
