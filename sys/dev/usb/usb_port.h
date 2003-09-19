d0f6d9f731a9807499f82272b69755aefd6aa8ec
avsm <avsm@openbsd.org>
2003-09-19 12:07:20 +0000

    Disable USB_USE_SOFTINTR until we have generic support for soft interrupts. Solves a number of hangs on ohci (and possibly uhci) machines.
    Found by me prodding around, and drahn@ more cluefully
    ok pvalchev@, nate@

--- sys/dev/usb/usb_port.h
+++ sys/dev/usb/usb_port.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb_port.h,v 1.43 2003/07/08 13:19:09 nate Exp $ */
+/*	$OpenBSD: usb_port.h,v 1.44 2003/09/19 12:07:20 avsm Exp $ */
 /*	$NetBSD: usb_port.h,v 1.62 2003/02/15 18:33:30 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_port.h,v 1.21 1999/11/17 22:33:47 n_hibma Exp $	*/
 
@@ -202,7 +202,7 @@ int __CONCAT(dname,_detach)(struct device *self, int flags)
  */
 #include <sys/timeout.h>
 
-#define USB_USE_SOFTINTR
+#undef USB_USE_SOFTINTR
 
 #ifdef USB_DEBUG
 #define UKBD_DEBUG 1
