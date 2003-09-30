fc4eb1eecef0b2ef4ebdc9f57155ee5de17bfb9e
mickey <mickey@openbsd.org>
2003-09-30 02:24:44 +0000

    quantum atlas iv 9 wls lies about tags

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: scsiconf.c,v 1.73 2003/05/18 16:06:35 mickey Exp $	*/
+/*	$OpenBSD: scsiconf.c,v 1.74 2003/09/30 02:24:44 mickey Exp $	*/
 /*	$NetBSD: scsiconf.c,v 1.57 1996/05/02 01:09:01 neil Exp $	*/
 
 /*
@@ -501,6 +501,8 @@ const struct scsi_quirk_inquiry_pattern scsi_quirk_patterns[] = {
 	 "MST     ", "SnapLink        ", ""},     SDEV_NOLUNS},
 	{{T_DIRECT, T_FIXED,
 	 "NEC     ", "D3847           ", "0307"}, SDEV_NOLUNS},
+	{{T_DIRECT, T_FIXED,
+	 "QUANTUM ", "ATLAS IV 9 WLS",   ""},	  SDEV_NOTAGS},
 	{{T_DIRECT, T_FIXED,
 	 "QUANTUM ", "ELS85S          ", ""},	  SDEV_AUTOSAVE},
 	{{T_DIRECT, T_FIXED,
