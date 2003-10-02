57c5091a98e9268e854962f7b92e33a36619da10
miod <miod@openbsd.org>
2003-10-02 13:24:39 +0000

    Upon invoking the freshly loaded binary, populate registers via embedded asm statements, and be sure of what we get, rather than use "hinted" register local variables; bugcrt can now be compiled with compiler optimization enabled.

--- sys/arch/mvme88k/stand/bugcrt/crt.c
+++ sys/arch/mvme88k/stand/bugcrt/crt.c
@@ -1,26 +1,18 @@
-/*	$OpenBSD: crt.c,v 1.4 2001/12/16 23:49:47 miod Exp $ */
+/*	$OpenBSD: crt.c,v 1.5 2003/10/02 13:24:39 miod Exp $ */
 
 #include <sys/types.h>
 #include <machine/prom.h>
 
-struct mvmeprom_args bugargs = { 1};	       /* not BSS */
+struct mvmeprom_args bugargs = { 1 };	       /* not BSS */
 
 asm (".text");
 /* pseudo reset vector */
 asm (STACK_ASM_OP);	/* initial sp value */
 asm (".long _start");	/* initial ip value */
+
+void
 start()
 {
-	register int dev_lun asm (MVMEPROM_REG_DEVLUN);
-	register int ctrl_lun asm (MVMEPROM_REG_CTRLLUN);
-	register int flags asm (MVMEPROM_REG_FLAGS);
-	register int ctrl_addr asm (MVMEPROM_REG_CTRLADDR);
-	register int entry asm (MVMEPROM_REG_ENTRY);
-	register int conf_blk asm (MVMEPROM_REG_CONFBLK);
-	register char *arg_start asm (MVMEPROM_REG_ARGSTART);
-	register char *arg_end asm (MVMEPROM_REG_ARGEND);
-	register char *nbarg_start asm (MVMEPROM_REG_NBARGSTART);
-	register char *nbarg_end asm (MVMEPROM_REG_NBARGEND);
 	extern int edata, end;
 	struct mvmeprom_brdid *id, *mvmeprom_brdid();
 
@@ -31,20 +23,30 @@ start()
 	 * the netboot args.  Not cool at all... r25 seems free. 
 	 */
 	asm("|	enable SFU1");
-	asm("	ldcr	r25,cr1");
+	asm("	ldcr	r25,cr1" ::: "r25");
 	asm("	clr	r25,r25,1<3>"); /* bit 3 is SFU1D */
 	asm("	stcr	r25,cr1");
 
-	bugargs.dev_lun = dev_lun;
-	bugargs.ctrl_lun = ctrl_lun;
-	bugargs.flags = flags;
-	bugargs.ctrl_addr = ctrl_addr;
-	bugargs.entry = entry;
-	bugargs.conf_blk = conf_blk;
-	bugargs.arg_start = arg_start;
-	bugargs.arg_end = arg_end;
-	bugargs.nbarg_start = nbarg_start;
-	bugargs.nbarg_end = nbarg_end;
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_DEVLUN :
+	    "=r" (bugargs.dev_lun));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_CTRLLUN :
+	    "=r" (bugargs.ctrl_lun));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_FLAGS :
+	    "=r" (bugargs.flags));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_CTRLADDR :
+	    "=r" (bugargs.ctrl_addr));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_ENTRY :
+	    "=r" (bugargs.entry));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_CONFBLK :
+	    "=r" (bugargs.conf_blk));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_ARGSTART :
+	    "=r" (bugargs.arg_start));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_ARGEND :
+	    "=r" (bugargs.arg_end));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_NBARGSTART :
+	    "=r" (bugargs.nbarg_start));
+	__asm__ __volatile__ ("or %0, r0, " MVMEPROM_REG_NBARGEND :
+	    "=r" (bugargs.nbarg_end));
 	*bugargs.arg_end = 0;      
 
 	id = mvmeprom_brdid();
@@ -87,37 +89,33 @@ start()
 	/* NOTREACHED */
 }
 
+void
 __main()
 {
 }
 
 void
-bugexec(addr)
-
-void (*addr)();
-
+bugexec(void (*addr)(void))
 {
-	register int dev_lun asm (MVMEPROM_REG_DEVLUN);
-	register int ctrl_lun asm (MVMEPROM_REG_CTRLLUN);
-	register int flags asm (MVMEPROM_REG_FLAGS);
-	register int ctrl_addr asm (MVMEPROM_REG_CTRLADDR);
-	register int entry asm (MVMEPROM_REG_ENTRY);
-	register int conf_blk asm (MVMEPROM_REG_CONFBLK);
-	register char *arg_start asm (MVMEPROM_REG_ARGSTART);
-	register char *arg_end asm (MVMEPROM_REG_ARGEND);
-
-	dev_lun = bugargs.dev_lun;
-	ctrl_lun = bugargs.ctrl_lun;
-	flags = bugargs.flags;
-	ctrl_addr = bugargs.ctrl_addr;
-	entry = bugargs.entry;
-	conf_blk = bugargs.conf_blk;
-	arg_start = bugargs.arg_start;
-	arg_end = bugargs.arg_end;
+	__asm__ __volatile__ ("or " MVMEPROM_REG_DEVLUN ", r0, %0" ::
+	    "r" (bugargs.dev_lun));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_CTRLLUN ", r0, %0" ::
+	    "r" (bugargs.ctrl_lun));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_FLAGS ", r0, %0" ::
+	    "r" (bugargs.flags));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_CTRLADDR ", r0, %0" ::
+	    "r" (bugargs.ctrl_addr));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_ENTRY ", r0, %0" ::
+	    "r" (bugargs.entry));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_CONFBLK ", r0, %0" ::
+	    "r" (bugargs.conf_blk));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_ARGSTART ", r0, %0" ::
+	    "r" (bugargs.arg_start));
+	__asm__ __volatile__ ("or " MVMEPROM_REG_ARGEND ", r0, %0" ::
+	    "r" (bugargs.arg_end));
 
 	(*addr)();
 	printf("bugexec: 0x%x returned!\n", addr);
 
 	_rtt();
 }
-
