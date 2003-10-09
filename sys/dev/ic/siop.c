e4b01b228ab59e5defdc66a25af7f2e7d3c5b72f
mickey <mickey@openbsd.org>
2003-10-09 17:32:42 +0000

    go back to non-raw busdma load as some archs still do not have load_raw

--- sys/dev/ic/siop.c
+++ sys/dev/ic/siop.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siop.c,v 1.29 2003/09/29 18:52:53 mickey Exp $ */
+/*	$OpenBSD: siop.c,v 1.30 2003/10/09 17:32:42 mickey Exp $ */
 /*	$NetBSD: siop.c,v 1.65 2002/11/08 22:04:41 bouyer Exp $	*/
 
 /*
@@ -1739,8 +1739,8 @@ siop_morecbd(sc)
 		    sc->sc_c.sc_dev.dv_xname, error);
 		goto bad1;
 	}
-	error = bus_dmamap_load_raw(sc->sc_c.sc_dmat, newcbd->xferdma, &seg,
-	    rseg, PAGE_SIZE, BUS_DMA_NOWAIT);
+	error = bus_dmamap_load(sc->sc_c.sc_dmat, newcbd->xferdma, newcbd->xfers,
+	    PAGE_SIZE, NULL, BUS_DMA_NOWAIT);
 	if (error) {
 		printf("%s: unable to load cbd DMA map, error = %d\n",
 		    sc->sc_c.sc_dev.dv_xname, error);
