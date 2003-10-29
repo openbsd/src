4e240d5a3779dc243bbbe690082f8cc700a9da83
deraadt <deraadt@openbsd.org>
2003-10-29 16:41:13 +0000

    PD

--- include/poll.h
+++ include/poll.h
@@ -1,6 +1,8 @@
-/*	$OpenBSD: poll.h,v 1.2 2000/03/15 03:56:48 todd Exp $ */
+/*	$OpenBSD: poll.h,v 1.3 2003/10/29 16:41:13 deraadt Exp $ */
 
 /*
+ * Written by Theo de Raadt, Public Domain
+ *
  * Typical poll() implementations expect poll.h to be in /usr/include. 
  * However this is not a convenient place for the real definitions.
  */
