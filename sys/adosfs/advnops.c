89bd0ff0780f94e3587c7f6aceb612d315e173d8
drahn <drahn@openbsd.org>
2003-11-13 17:19:40 +0000

    Couple of typos in poll changes. ok millert@

--- sys/adosfs/advnops.c
+++ sys/adosfs/advnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: advnops.c,v 1.27 2003/10/17 18:25:06 tedu Exp $	*/
+/*	$OpenBSD: advnops.c,v 1.28 2003/11/13 17:19:40 drahn Exp $	*/
 /*	$NetBSD: advnops.c,v 1.32 1996/10/13 02:52:09 christos Exp $	*/
 
 /*
@@ -384,7 +384,7 @@ adosfs_poll(v)
 		struct vnode *a_vp;
 		int  a_events;
 		struct proc *a_p;
-	} */ *sp = v;
+	} */ *ap = v;
 #ifdef ADOSFS_DIAGNOSTIC
 	/*
 	 * sure there's something to read...
--- sys/msdosfs/msdosfs_vnops.c
+++ sys/msdosfs/msdosfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: msdosfs_vnops.c,v 1.42 2003/09/23 16:51:13 millert Exp $	*/
+/*	$OpenBSD: msdosfs_vnops.c,v 1.43 2003/11/13 17:19:40 drahn Exp $	*/
 /*	$NetBSD: msdosfs_vnops.c,v 1.63 1997/10/17 11:24:19 ws Exp $	*/
 
 /*-
@@ -714,7 +714,7 @@ msdosfs_poll(v)
 		struct vnode *a_vp;
 		int a_events;
 		struct proc *a_p;
-	} */ *ap;
+	} */ *ap = v;
 
 	return (seltrue(ap->a_vp->v_rdev, ap->a_events, ap->a_p));
 }
