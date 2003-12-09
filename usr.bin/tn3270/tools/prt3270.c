b896ee2f18816d52c3e4bb250733c2a5676db108
mickey <mickey@openbsd.org>
2003-12-09 11:58:42 +0000

    missing printf arg; from Alf Schlichting

--- usr.bin/tn3270/tools/prt3270.c
+++ usr.bin/tn3270/tools/prt3270.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: prt3270.c,v 1.5 2003/06/03 02:56:19 millert Exp $	*/
+/*	$OpenBSD: prt3270.c,v 1.6 2003/12/09 11:58:42 mickey Exp $	*/
 
 /*-
  * Copyright (c) 1988 The Regents of the University of California.
@@ -37,7 +37,7 @@ char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)prt3270.c	4.2 (Berkeley) 4/26/91";*/
-static char rcsid[] = "$OpenBSD: prt3270.c,v 1.5 2003/06/03 02:56:19 millert Exp $";
+static char rcsid[] = "$OpenBSD: prt3270.c,v 1.6 2003/12/09 11:58:42 mickey Exp $";
 #endif /* not lint */
 
 #if defined(unix)
@@ -518,7 +518,7 @@ int	c;
 {
     if (!isascii(c)) {
 	fflush(stdout);
-	fprintf(stderr, "Non-hex digit 0x%x.\n");
+	fprintf(stderr, "Non-hex digit 0x%x.\n", c);
 	fflush(stderr);
 	return 0;
     } else {
