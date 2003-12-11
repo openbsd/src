419c73786298f837bfdb5d3fa6ca0bc0824cd232
mcbride <mcbride@openbsd.org>
2003-12-11 20:49:20 +0000

    Sync usage() with SYNOPSIS in grep(1).
    ok deraadt@ jmc@

--- usr.bin/grep/grep.c
+++ usr.bin/grep/grep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: grep.c,v 1.23 2003/09/07 19:40:54 millert Exp $	*/
+/*	$OpenBSD: grep.c,v 1.24 2003/12/11 20:49:20 mcbride Exp $	*/
 
 /*-
  * Copyright (c) 1999 James Howard and Dag-Erling Coïdan Smørgrav
@@ -105,11 +105,12 @@ usage(void)
 {
 	fprintf(stderr,
 #ifdef NOZ
-	    "usage: %s [-[AB] num] [-CEFGHILPRSUVabchilnoqsvwx]"
+	    "usage: %s [-AB num] [-CEFGHILPRSUVabchilnoqsvwx]\n"
 #else
-	    "usage: %s [-[AB] num] [-CEFGHILPRSUVZabchilnoqsvwx]"
+	    "usage: %s [-AB num] [-CEFGHILPRSUVZabchilnoqsvwx]\n"
 #endif
-	    " [-e pattern] [-f file] [file ...]\n", __progname);
+	    "\t[--context[=num]] [--binary-files=value]\n"
+	    "\t[-e pattern] [-f file] [pattern] [file ...]\n", __progname);
 	exit(2);
 }
 
