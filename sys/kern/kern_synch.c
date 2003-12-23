c7a7f4d4e84e66d96423720f868aee0404597266
mickey <mickey@openbsd.org>
2003-12-23 00:15:14 +0000

    print tv_usec fields correctly in reporting conmonotonic time

--- sys/kern/kern_synch.c
+++ sys/kern/kern_synch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_synch.c,v 1.51 2003/12/19 04:51:44 millert Exp $	*/
+/*	$OpenBSD: kern_synch.c,v 1.52 2003/12/23 00:15:14 mickey Exp $	*/
 /*	$NetBSD: kern_synch.c,v 1.37 1996/04/22 01:38:37 christos Exp $	*/
 
 /*-
@@ -708,7 +708,8 @@ mi_switch()
 	microtime(&tv);
 	if (timercmp(&tv, &runtime, <)) {
 #ifdef DIAGNOSTIC
-		printf("time is not monotonic! tv=%ld.%ld, runtime=%ld.%ld\n",
+		printf("time is not monotonic! "
+		    "tv=%ld.%06ld, runtime=%ld.%06ld\n",
 		    tv.tv_sec, tv.tv_usec, runtime.tv_sec, runtime.tv_usec);
 #endif
 	} else {
