a8f411c8356c93c13f80c43cf5e3a4b0f6aa25e2
millert <millert@openbsd.org>
2003-12-31 00:13:24 +0000

    wait4(2) takes and returns pid_t, not int.  OK deraadt@ and miod@

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exit.c,v 1.47 2003/08/03 19:25:49 millert Exp $	*/
+/*	$OpenBSD: kern_exit.c,v 1.48 2003/12/31 00:13:24 millert Exp $	*/
 /*	$NetBSD: kern_exit.c,v 1.39 1996/04/22 01:38:25 christos Exp $	*/
 
 /*
@@ -379,14 +379,14 @@ reaper(void)
 	}
 }
 
-int
+pid_t
 sys_wait4(q, v, retval)
 	register struct proc *q;
 	void *v;
 	register_t *retval;
 {
 	register struct sys_wait4_args /* {
-		syscallarg(int) pid;
+		syscallarg(pid_t) pid;
 		syscallarg(int *) status;
 		syscallarg(int) options;
 		syscallarg(struct rusage *) rusage;
