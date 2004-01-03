17d5a5cb51ceac19b35c31959cc6f3feee982b04
brad <brad@openbsd.org>
2004-01-03 07:16:25 +0000

    sync the list of acceptable getopt_long options with reality. Fixes PR 3623
    From: binutils CVS 2.11 branch, based on rev 1.15.2.3
    
    ok otto@

--- gnu/usr.bin/binutils/binutils/objcopy.c
+++ gnu/usr.bin/binutils/binutils/objcopy.c
@@ -1831,7 +1831,7 @@ strip_main (argc, argv)
   struct section_list *p;
   char *output_file = NULL;
 
-  while ((c = getopt_long (argc, argv, "b:i:I:j:K:N:s:O:d:F:L:G:R:SpgxXVvW:",
+  while ((c = getopt_long (argc, argv, "I:O:F:K:N:R:o:sSpdgxXhVv",
 			   strip_options, (int *) 0)) != EOF)
     {
       switch (c)
@@ -1963,7 +1963,7 @@ copy_main (argc, argv)
   struct section_list *p;
   struct stat statbuf;
 
-  while ((c = getopt_long (argc, argv, "b:i:I:j:K:N:s:O:d:F:L:R:SpgxXVvW:",
+  while ((c = getopt_long (argc, argv, "b:i:I:j:K:N:s:O:d:F:L:G:R:SpgxXhVvW:",
 			   copy_options, (int *) 0)) != EOF)
     {
       switch (c)
