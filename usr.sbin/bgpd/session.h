065afdacee5ed69e07e075391a8ed055915dadb9
henning <henning@openbsd.org>
2004-01-06 19:21:22 +0000

    lower the initial value for the IdleHold timer from 90 to 30 seconds. 90 is too much, and now that the punishment works as intended oscillating peers get slowed down by that

--- usr.sbin/bgpd/session.h
+++ usr.sbin/bgpd/session.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.h,v 1.14 2004/01/06 19:19:21 henning Exp $ */
+/*	$OpenBSD: session.h,v 1.15 2004/01/06 19:21:22 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -23,7 +23,7 @@
 #define	INTERVAL_CONNECTRETRY		120
 #define	INTERVAL_HOLD_INITIAL		240
 #define	INTERVAL_HOLD			90
-#define	INTERVAL_IDLE_HOLD_INITIAL	90
+#define	INTERVAL_IDLE_HOLD_INITIAL	30
 #define	MAX_IDLE_HOLD			3600
 #define	MSGSIZE_HEADER			19
 #define	MSGSIZE_HEADER_MARKER		16
