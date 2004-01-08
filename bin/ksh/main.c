7ce501a7bdd06c61b8d50dee3d8f5b44b2a10f19
jmc <jmc@openbsd.org>
2004-01-08 05:43:14 +0000

    typo; from ray at cyth dot net (PR 3632);

--- bin/ksh/main.c
+++ bin/ksh/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.25 2003/06/26 00:09:45 deraadt Exp $	*/
+/*	$OpenBSD: main.c,v 1.26 2004/01/08 05:43:14 jmc Exp $	*/
 
 /*
  * startup, main loop, environments and error handling
@@ -177,7 +177,7 @@ main(int argc, char *argv[])
 	}
 
 
-	/* Turn on nohup by default for how - will change to off
+	/* Turn on nohup by default for now - will change to off
 	 * by default once people are aware of its existence
 	 * (at&t ksh does not have a nohup option - it always sends
 	 * the hup).
