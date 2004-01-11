d0840be5e003ffeb92b7b9784f563eb241791dd1
henning <henning@openbsd.org>
2004-01-11 21:59:45 +0000

    set address family...

--- usr.sbin/bgpd/rde_rib.c
+++ usr.sbin/bgpd/rde_rib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_rib.c,v 1.19 2004/01/11 21:47:20 claudio Exp $ */
+/*	$OpenBSD: rde_rib.c,v 1.20 2004/01/11 21:59:45 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Claudio Jeker <claudio@openbsd.org>
@@ -1040,6 +1040,7 @@ nexthop_add(struct rde_aspath *asp)
 	if (nh == NULL) {
 		nh = nexthop_alloc();
 		nh->state = NEXTHOP_LOOKUP;
+		nh->exit_nexthop.af = AF_INET;
 		nh->exit_nexthop.v4.s_addr = asp->flags.nexthop;
 		LIST_INSERT_HEAD(NEXTHOP_HASH(asp->flags.nexthop), nh,
 		    nexthop_l);
