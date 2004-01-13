847ea0c5ac9a2fee625feea13e6c2f551f200cad
markus <markus@openbsd.org>
2004-01-13 19:45:15 +0000

    cast for portability; millert@

--- usr.bin/ssh/compress.c
+++ usr.bin/ssh/compress.c
@@ -12,7 +12,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: compress.c,v 1.20 2004/01/13 19:23:15 markus Exp $");
+RCSID("$OpenBSD: compress.c,v 1.21 2004/01/13 19:45:15 markus Exp $");
 
 #include "log.h"
 #include "buffer.h"
@@ -57,11 +57,13 @@ void
 buffer_compress_uninit(void)
 {
 	debug("compress outgoing: raw data %llu, compressed %llu, factor %.2f",
-	    outgoing_stream.total_in, outgoing_stream.total_out,
+	    (unsigned long long)outgoing_stream.total_in,
+	    (unsigned long long)outgoing_stream.total_out,
 	    outgoing_stream.total_in == 0 ? 0.0 :
 	    (double) outgoing_stream.total_out / outgoing_stream.total_in);
 	debug("compress incoming: raw data %llu, compressed %llu, factor %.2f",
-	    incoming_stream.total_out, incoming_stream.total_in,
+	    (unsigned long long)incoming_stream.total_out,
+	    (unsigned long long)incoming_stream.total_in,
 	    incoming_stream.total_out == 0 ? 0.0 :
 	    (double) incoming_stream.total_in / incoming_stream.total_out);
 	if (compress_init_recv_called == 1 && inflate_failed == 0)
