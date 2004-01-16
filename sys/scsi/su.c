0598ed7c5ace0ea534817b49e973466b5870520f
miod <miod@openbsd.org>
2004-01-16 21:11:21 +0000

    Remove this farce.

--- sys/scsi/su.c
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: su.c,v 1.2 1996/10/31 01:09:26 niklas Exp $	*/
-/*	$NetBSD: su.c,v 1.2 1994/06/29 06:43:24 cgd Exp $	*/
-
-/* this will be a special user scsi device */
-/* not written yet */
-
