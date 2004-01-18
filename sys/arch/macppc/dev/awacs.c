cbeddb02b7b9e20a7a46a754d3f142d44e0d19bb
miod <miod@openbsd.org>
2004-01-18 13:51:35 +0000

    Silence match() unless DEBUG.

--- sys/arch/macppc/dev/awacs.c
+++ sys/arch/macppc/dev/awacs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: awacs.c,v 1.16 2003/10/16 03:31:25 drahn Exp $	*/
+/*	$OpenBSD: awacs.c,v 1.17 2004/01/18 13:51:35 miod Exp $	*/
 /*	$NetBSD: awacs.c,v 1.4 2001/02/26 21:07:51 wiz Exp $	*/
 
 /*-
@@ -248,8 +248,10 @@ awacs_match(struct device *parent, void *match, void *aux)
 	    strcmp(ca->ca_name, "davbus") != 0)
 		return 0;
 
+#ifdef DEBUG
 	printf("awacs: matched %s nreg %d nintr %d\n",
 		ca->ca_name, ca->ca_nreg, ca->ca_nintr);
+#endif
 
 	if (ca->ca_nreg < 24 || ca->ca_nintr < 12)
 		return 0;
