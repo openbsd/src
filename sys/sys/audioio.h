c95ad42f35d61ea342f23fa5fa4aa6e50f0c9482
jmc <jmc@openbsd.org>
2004-01-31 17:00:50 +0000

    typo from Jared Yanovich;

--- sys/sys/audioio.h
+++ sys/sys/audioio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: audioio.h,v 1.14 2001/10/24 16:27:20 mickey Exp $	*/
+/*	$OpenBSD: audioio.h,v 1.15 2004/01/31 17:00:50 jmc Exp $	*/
 /*	$NetBSD: audioio.h,v 1.24 1998/08/13 06:28:41 mrg Exp $	*/
 
 /*
@@ -56,7 +56,7 @@ struct audio_prinfo {
 	u_int	samples;	/* number of samples */
 	u_int	eof;		/* End Of File (zero-size writes) counter */
 	u_char	pause;		/* non-zero if paused, zero to resume */
-	u_char	error;		/* non-zero if underflow/overflow ocurred */
+	u_char	error;		/* non-zero if underflow/overflow occurred */
 	u_char	waiting;	/* non-zero if another process hangs in open */
 	u_char	balance;	/* stereo channel balance */
 	u_char	cspare[2];
