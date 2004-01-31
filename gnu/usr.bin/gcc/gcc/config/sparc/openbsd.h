a1cf9fdecb7cfbad42bc4138143bf1732dcc90ff
miod <miod@openbsd.org>
2004-01-31 18:18:56 +0000

    Sparc doesn't use old gas, obviously; ok espie@

--- gnu/usr.bin/gcc/gcc/config/sparc/openbsd.h
+++ gnu/usr.bin/gcc/gcc/config/sparc/openbsd.h
@@ -19,7 +19,6 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 /* Get generic OpenBSD definitions.  */
-#define OBSD_OLD_GAS
 #include <openbsd.h>
 
 /* Run-time target specifications.  */
