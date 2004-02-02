0b941c7c5a98fc4c18f2fb8fb0a6d7f25d40b369
drahn <drahn@openbsd.org>
2004-02-02 07:03:21 +0000

    sigh, watch for compile errors...

--- lib/libc/arch/arm/gen/infinity.c
+++ lib/libc/arch/arm/gen/infinity.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: infinity.c,v 1.2 2004/02/01 05:40:52 drahn Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.3 2004/02/02 07:03:21 drahn Exp $	*/
 /*	$NetBSD: infinity.c,v 1.3 2002/02/19 20:08:19 bjh21 Exp $	*/
 
 /*
@@ -11,11 +11,11 @@
 
 char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
 #if BYTE_ORDER == BIG_ENDIAN
-	{ { 0x7f, 0xf0,    0,    0, 0, 0,    0,    0} };
+	{ 0x7f, 0xf0,    0,    0, 0, 0,    0,    0};
 #else
 #ifdef __VFP_FP__
-	{ {    0,    0,    0,    0, 0, 0, 0xf0, 0x7f} };
+	{    0,    0,    0,    0, 0, 0, 0xf0, 0x7f};
 #else
-	{ {    0,    0, 0xf0, 0x7f, 0, 0,    0,    0} };
+	{    0,    0, 0xf0, 0x7f, 0, 0,    0,    0};
 #endif
 #endif
