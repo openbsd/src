d5415a444101e123b58939504afeabec5a48ba7e
deraadt <deraadt@openbsd.org>
2004-02-07 05:44:14 +0000

    new fxp; cmetz

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_fxp_pci.c,v 1.23 2004/01/09 21:32:24 brad Exp $	*/
+/*	$OpenBSD: if_fxp_pci.c,v 1.24 2004/02/07 05:44:14 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1995, David Greenman
@@ -98,6 +98,7 @@ const struct pci_matchid fxp_pci_devices[] = {
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VE_3 },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VE_4 },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VE_5 },
+	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VE_6 },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VM_0 },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VM_1 },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_PRO_100_VM_2 },
