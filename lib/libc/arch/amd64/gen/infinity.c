26a892eb37f29ab69d7cb5dd459d0184b750e55f
deraadt <deraadt@openbsd.org>
2004-02-08 17:31:44 +0000

    16 byte align for performance, as on other architectures

--- lib/libc/arch/amd64/gen/infinity.c
+++ lib/libc/arch/amd64/gen/infinity.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: infinity.c,v 1.1 2004/01/28 01:44:45 mickey Exp $";
+static char rcsid[] = "$OpenBSD: infinity.c,v 1.2 2004/02/08 17:31:44 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 /* infinity.c */
@@ -7,4 +7,6 @@ static char rcsid[] = "$OpenBSD: infinity.c,v 1.1 2004/01/28 01:44:45 mickey Exp
 #include <math.h>
 
 /* bytes for +Infinity on a 387 */
-char __infinity[] = { 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
+char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
+	{ 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
+
