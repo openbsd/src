23d6d5329b31fe0c0ac28dba347101ee761ec980
deraadt <deraadt@openbsd.org>
2004-02-08 07:09:56 +0000

    amd64 needs the same hack as powerpc; but this code really needs to be deleted/replaced.  anyone want to step up?

--- usr.sbin/pppd/main.c
+++ usr.sbin/pppd/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.42 2003/04/25 23:24:04 deraadt Exp $	*/
+/*	$OpenBSD: main.c,v 1.43 2004/02/08 07:09:56 deraadt Exp $	*/
 
 /*
  * main.c - Point-to-Point Protocol main module
@@ -46,7 +46,7 @@
 #if 0
 static char rcsid[] = "Id: main.c,v 1.49 1998/05/05 05:24:17 paulus Exp $";
 #else
-static char rcsid[] = "$OpenBSD: main.c,v 1.42 2003/04/25 23:24:04 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: main.c,v 1.43 2004/02/08 07:09:56 deraadt Exp $";
 #endif
 #endif
 
@@ -1523,7 +1523,7 @@ vfmtmsg(buf, buflen, fmt, args)
 	    break;
 	case 'r':
 	    f = va_arg(args, char *);
-#ifndef __powerpc__
+#if !defined(__powerpc__) && !defined(__x86_64__)
 	    n = vfmtmsg(buf, buflen + 1, f, va_arg(args, va_list));
 #else
 	    /* On the powerpc, a va_list is an array of 1 structure */
