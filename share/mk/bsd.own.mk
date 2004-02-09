8d594515d2fa8fb33d4a7225966b1ddcdef55726
drahn <drahn@openbsd.org>
2004-02-09 02:33:20 +0000

    do not build AFS on arm, no process.arm.S file (yet) arm now has shared library support.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.74 2004/02/07 23:49:05 deraadt Exp $
+#	$OpenBSD: bsd.own.mk,v 1.75 2004/02/09 02:33:20 drahn Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -20,7 +20,8 @@ YP?=		yes
 # integrated support for libwrap.
 TCP_WRAPPERS?=	yes
 # Set `AFS` to `yes' to build with AFS support.
-.if (${MACHINE_ARCH} == "m88k") || (${MACHINE_ARCH} == "x86_64")
+.if (${MACHINE_ARCH} == "arm") || (${MACHINE_ARCH} == "m88k") || \
+   (${MACHINE_ARCH} == "x86_64")
 AFS?=		no
 .else
 AFS?=		yes
@@ -118,8 +119,7 @@ STATIC?=	-static
 # don't try to generate PIC versions of libraries on machines
 # which don't support PIC.
 .if (${MACHINE_ARCH} == "vax") || (${MACHINE_ARCH} == "x86_64") || \
-    (${MACHINE_ARCH} == "hppa") || (${MACHINE_ARCH} == "m88k") \
-    || (${MACHINE_ARCH} == "arm")
+    (${MACHINE_ARCH} == "hppa") || (${MACHINE_ARCH} == "m88k")
 NOPIC=
 .endif
 
