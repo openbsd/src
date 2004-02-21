1b8e8464d33acc2ba6079bd00a2d44909cdbbefc
deraadt <deraadt@openbsd.org>
2004-02-21 03:04:18 +0000

    move sparc64 to gcc3. to move up please install a snapshot (or, install this, build the right gcc, do a make build and pray)

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.78 2004/02/12 05:28:02 drahn Exp $
+#	$OpenBSD: bsd.own.mk,v 1.79 2004/02/21 03:04:18 deraadt Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -39,7 +39,7 @@ ELF_TOOLCHAIN?=	no
 
 # do the dew
 .if (${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "hppa64" || \
-    ${MACHINE_ARCH} == "x86_64")
+    ${MACHINE_ARCH} == "x86_64" || ${MACHINE_ARCH} == "sparc64")
 USE_GCC3?=yes
 .else
 USE_GCC3?=no
