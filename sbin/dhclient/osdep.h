598e3778ec31e0a363e9a7c9e9ff26e9fb6c681c
henning <henning@openbsd.org>
2004-02-23 20:13:33 +0000

    scratch the PTRSIZE64 define, the gunk that was using it was in some of the files killed in the last hour

--- sbin/dhclient/osdep.h
+++ sbin/dhclient/osdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: osdep.h,v 1.10 2004/02/23 20:09:02 deraadt Exp $	*/
+/*	$OpenBSD: osdep.h,v 1.11 2004/02/23 20:13:33 henning Exp $	*/
 
 /* Operating system dependencies... */
 
@@ -65,11 +65,6 @@ extern int h_errno;
 
 #define EOL	'\n'
 #define VOIDPTR void *
-
-
-#if defined(__alpha__) || defined(__sparc64__) || defined(__amd64__)
-#define PTRSIZE_64BIT
-#endif
 
 #if !defined (TIME_MAX)
 # define TIME_MAX 2147483647
