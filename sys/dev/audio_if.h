5c1534d0f4235801e3b696d622ae4a87298935ef
deraadt <deraadt@openbsd.org>
2004-02-24 20:42:40 +0000

    same gory splaudio hack for amd64, sigh

--- sys/dev/audio_if.h
+++ sys/dev/audio_if.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: audio_if.h,v 1.23 2004/01/31 15:21:18 drahn Exp $	*/
+/*	$OpenBSD: audio_if.h,v 1.24 2004/02/24 20:42:40 deraadt Exp $	*/
 /*	$NetBSD: audio_if.h,v 1.24 1998/01/10 14:07:25 tv Exp $	*/
 
 /*
@@ -158,7 +158,7 @@ int	       audioprint(void *, const char *);
 #define ISDEVAUDIOCTL(x)	(AUDIODEV((x)) == AUDIOCTL_DEVICE)
 #define ISDEVMIXER(x)		(AUDIODEV((x)) == MIXER_DEVICE)
 
-#if !defined(__i386__) && !defined(__sparc64__) && !defined(__powerpc__) && !defined(__hppa__) && !defined(__arm__)
+#if !defined(__i386__) && !defined(__sparc64__) && !defined(__powerpc__) && !defined(__hppa__) && !defined(__arm__) && !defined(__x86_64__)
 #define splaudio splbio		/* XXX */
 #define IPL_AUDIO IPL_BIO	/* XXX */
 #endif
