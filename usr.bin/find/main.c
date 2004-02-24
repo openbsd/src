8a322dbba9ee6ad1eda744060a599f8ad25c4230
jmc <jmc@openbsd.org>
2004-02-24 07:20:38 +0000

    - remove double spaces in the examples - `W' before `X' - sync usage()
    ok millert@

--- usr.bin/find/main.c
+++ usr.bin/find/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.17 2003/09/26 22:22:26 tedu Exp $	*/
+/*	$OpenBSD: main.c,v 1.18 2004/02/24 07:20:38 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -31,7 +31,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/6/93";*/
-static char rcsid[] = "$OpenBSD: main.c,v 1.17 2003/09/26 22:22:26 tedu Exp $";
+static char rcsid[] = "$OpenBSD: main.c,v 1.18 2004/02/24 07:20:38 jmc Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -141,6 +141,6 @@ static void
 usage(void)
 {
 	(void)fprintf(stderr,
-	    "usage: find [-dHhLXxW] [-f file] file [...] [expression]\n");
+	    "usage: find [-dHhLWXx] [-f file] file [...] [expression]\n");
 	exit(1);
 }
