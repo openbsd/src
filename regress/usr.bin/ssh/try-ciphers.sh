b0fa8ad96a361714acce766561813bcd73e0cdcd
dtucker <dtucker@openbsd.org>
2004-02-28 13:44:45 +0000

    Test acss too; ok markus@

--- regress/usr.bin/ssh/try-ciphers.sh
+++ regress/usr.bin/ssh/try-ciphers.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: try-ciphers.sh,v 1.8 2003/06/12 15:40:01 markus Exp $
+#	$OpenBSD: try-ciphers.sh,v 1.9 2004/02/28 13:44:45 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="try ciphers"
@@ -26,5 +26,16 @@ for c in $ciphers; do
 	${SSH} -F $OBJ/ssh_proxy -1 -c $c somehost true
 	if [ $? -ne 0 ]; then
 		fail "ssh -1 failed with cipher $c"
+	fi
+done
+
+echo "Ciphers acss@openssh.org" >> $OBJ/sshd_proxy
+c=acss@openssh.org
+for m in $macs; do
+	trace "proto 2 $c mac $m"
+	verbose "test $tid: proto 2 cipher $c mac $m"
+	${SSH} -F $OBJ/ssh_proxy -2 -m $m -c $c somehost true
+	if [ $? -ne 0 ]; then
+		fail "ssh -2 failed with mac $m cipher $c"
 	fi
 done
