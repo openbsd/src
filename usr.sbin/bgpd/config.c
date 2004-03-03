e005ea11d9bd51e6ea2a334a773b149bbbbf32aa
henning <henning@openbsd.org>
2004-03-03 10:13:48 +0000

    missing freeaddrinfo() From: Patrick Latifi <pat@eyeo.org> Patrick is doing an _excellent_ job in finding all these little omissions, thank you very very much!

--- usr.sbin/bgpd/config.c
+++ usr.sbin/bgpd/config.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: config.c,v 1.30 2004/03/02 19:45:04 henning Exp $ */
+/*	$OpenBSD: config.c,v 1.31 2004/03/03 10:13:48 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -198,6 +198,7 @@ host_v6(const char *s, struct bgpd_addr *h)
 		h->scope_id =
 		    ((struct sockaddr_in6 *)res->ai_addr)->sin6_scope_id;
 
+		freeaddrinfo(res);
 		return (1);
 	}
 
