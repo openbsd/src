102e70c58279e3116f275f90b4092624411b897d
deraadt <deraadt@openbsd.org>
2004-03-10 00:47:50 +0000

    new vscom puc; toni.tiveron@tiscali.it

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pucdata.c,v 1.31 2003/12/09 23:06:56 deraadt Exp $	*/
+/*	$OpenBSD: pucdata.c,v 1.32 2004/03/10 00:47:50 deraadt Exp $	*/
 /*	$NetBSD: pucdata.c,v 1.6 1999/07/03 05:55:23 cgd Exp $	*/
 
 /*
@@ -507,6 +507,20 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, 0x20, 0x08, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x20, 0x10, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x20, 0x18, COM_FREQ },
+	    },
+	},
+
+	/*
+	 * VScom PCI-400S, based on PLX 9050 Chip, 16k buffer
+	 */
+	{ /* "VScom PCI-400S", */
+	    { PCI_VENDOR_PLX, PCI_PRODUCT_PLX_1077, 0x10b5, 0x1077 },
+	    { 0xffff, 0xffff, 0xffff, 0xffff },
+	    {
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x08, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x10, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x18, COM_FREQ * 8 },
 	    },
 	},
 
