f6a879d02f7d05bdfacc13b290b6d8f5f54f375b
sturm <sturm@openbsd.org>
2004-03-30 15:43:20 +0000

    if a string considered a filename is too long, don't exit but just let the syscall fail, it might not actually be a filename
    fixes pr 3140, ok provos@

--- bin/systrace/intercept.c
+++ bin/systrace/intercept.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intercept.c,v 1.45 2004/01/30 17:21:16 sturm Exp $	*/
+/*	$OpenBSD: intercept.c,v 1.46 2004/03/30 15:43:20 sturm Exp $	*/
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
@@ -639,12 +639,12 @@ normalize_filename(int fd, pid_t pid, char *name, int userp)
 	/* Need concatenated path for simplifypath */
 	if (havecwd && name[0] != '/') {
 		if (strlcat(cwd, "/", sizeof(cwd)) >= sizeof(cwd))
-			goto error;
+			return (NULL);
 		if (strlcat(cwd, name, sizeof(cwd)) >= sizeof(cwd))
-			goto error;
+			return (NULL);
 	} else {
 		if (strlcpy(cwd, name, sizeof(cwd)) >= sizeof(cwd))
-			goto error;
+			return (NULL);
 	}
 
 	if (userp != ICLINK_NONE) {
