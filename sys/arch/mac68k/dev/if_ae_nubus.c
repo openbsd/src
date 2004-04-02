8c9540997205cd21d63de24aaa359acc2f35ccc7
xsa <xsa@openbsd.org>
2004-04-02 21:01:44 +0000

    set correct MAC address for Farallon eth' cards. Tested by Martin Reindl <mreindl at catai.org> ok miod@.

--- sys/arch/mac68k/dev/if_ae_nubus.c
+++ sys/arch/mac68k/dev/if_ae_nubus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ae_nubus.c,v 1.8 2002/03/14 01:26:35 millert Exp $	*/
+/*	$OpenBSD: if_ae_nubus.c,v 1.9 2004/04/02 21:01:44 xsa Exp $	*/
 /*	$NetBSD: if_ae_nubus.c,v 1.17 1997/05/01 18:17:16 briggs Exp $	*/
 
 /*
@@ -43,6 +43,8 @@
  *	Asante MacCon II/E
  *	Kinetics EtherPort SE/30
  */
+
+#define	AE_OLD_GET_ENADDR
 
 #include <sys/param.h>
 #include <sys/device.h>
