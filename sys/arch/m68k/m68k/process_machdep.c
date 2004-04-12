e8a5f4715dbb71822a6e77be78109f8e933b7948
kettenis <kettenis@openbsd.org>
2004-04-12 15:28:38 +0000

    Fix process_frame macro. ok miod@

--- sys/arch/m68k/m68k/process_machdep.c
+++ sys/arch/m68k/m68k/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: process_machdep.c,v 1.5 2002/03/14 00:42:24 miod Exp $	*/
+/*	$OpenBSD: process_machdep.c,v 1.6 2004/04/12 15:28:38 kettenis Exp $	*/
 /*	$NetBSD: process_machdep.c,v 1.17 1996/05/06 20:05:24 gwr Exp $	*/
 
 /*
@@ -63,7 +63,7 @@
 #include <machine/psl.h>
 #include <machine/reg.h>
 
-#define	process_frame(p)	(struct frame *)&((p)->p_md.md_regs)
+#define	process_frame(p)	(struct frame *)((p)->p_md.md_regs)
 #define	process_fpframe(p)	&((p)->p_addr->u_pcb.pcb_fpregs)
 
 int
