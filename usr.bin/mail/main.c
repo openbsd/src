56ee374dca9ba9a612e0e2b9c4339c1467d21de0
jmc <jmc@openbsd.org>
2004-04-19 10:17:18 +0000

    - sort SYNOPSIS - remove a load of unnecessary quoting and some groff cruft - sort options list - add usd doc to SEE ALSO - sync usage()

--- usr.bin/mail/main.c
+++ usr.bin/mail/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.18 2003/06/03 02:56:11 millert Exp $	*/
+/*	$OpenBSD: main.c,v 1.19 2004/04/19 10:17:18 jmc Exp $	*/
 /*	$NetBSD: main.c,v 1.7 1997/05/13 06:15:57 mikel Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ static const char copyright[] =
 #if 0
 static const char sccsid[] = "@(#)main.c	8.2 (Berkeley) 4/20/95";
 #else
-static const char rcsid[] = "$OpenBSD: main.c,v 1.18 2003/06/03 02:56:11 millert Exp $";
+static const char rcsid[] = "$OpenBSD: main.c,v 1.19 2004/04/19 10:17:18 jmc Exp $";
 #endif
 #endif /* not lint */
 
@@ -290,11 +290,11 @@ __dead void
 usage(void)
 {
 
-	fprintf(stderr, "usage: %s [-iInv] [-s subject] [-c cc-addr] "
-	    "[-b bcc-addr] to-addr ...\n", __progname);
-	fprintf(stderr, "       %*s [- sendmail-options ...]\n",
+	fprintf(stderr, "usage: %s [-Iinv] [-b list] [-c list] "
+	    "[-s subject] to-addr [...]\n", __progname);
+	fprintf(stderr, "       %*s [-sendmail-options [...]]\n",
 	    (int)strlen(__progname), "");
-	fprintf(stderr, "       %s [-iInNv] -f [name]\n", __progname);
-	fprintf(stderr, "       %s [-iInNv] [-u user]\n", __progname);
+	fprintf(stderr, "       %s [-IiNnv] -f [name]\n", __progname);
+	fprintf(stderr, "       %s [-IiNnv] [-u user]\n", __progname);
 	exit(1);
 }
