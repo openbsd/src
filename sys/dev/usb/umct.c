04f307d0770b567232e78146fd50fc224d4eaf14
pvalchev <pvalchev@openbsd.org>
2004-04-20 16:28:36 +0000

    Belkin F5U109; from NetBSD, ok henning

--- sys/dev/usb/umct.c
+++ sys/dev/usb/umct.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: umct.c,v 1.9 2003/11/07 20:22:35 jmc Exp $	*/
+/*	$OpenBSD: umct.c,v 1.10 2004/04/20 16:28:36 pvalchev Exp $	*/
 /*	$NetBSD: umct.c,v 1.10 2003/02/23 04:20:07 simonb Exp $	*/
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -149,6 +149,8 @@ static const struct usb_devno umct_devs[] = {
 	{ USB_VENDOR_MCT, USB_PRODUCT_MCT_SITECOM_USB232 },
 	/* D-Link DU-H3SP USB BAY Hub Products */
 	{ USB_VENDOR_MCT, USB_PRODUCT_MCT_DU_H3SP_USB232 },
+	/* BELKIN F5U109 */
+	{ USB_VENDOR_BELKIN, USB_PRODUCT_BELKIN_F5U109 },
 };
 #define umct_lookup(v, p) usb_lookup(umct_devs, v, p)
 
@@ -445,7 +447,8 @@ umct_set_baudrate(struct umct_softc *sc, u_int rate)
 	uDWord arate;
 	u_int val;
 
-	if (sc->sc_product == USB_PRODUCT_MCT_SITECOM_USB232) {
+	if (sc->sc_product == USB_PRODUCT_MCT_SITECOM_USB232 ||
+	    sc->sc_product == USB_PRODUCT_BELKIN_F5U109) {
 		switch (rate) {
 		case    300: val = 0x01; break;
 		case    600: val = 0x02; break;
