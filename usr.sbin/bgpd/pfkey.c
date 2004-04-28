5a87fe7b68e27b70c639d796e9c6d1e16f186870
henning <henning@openbsd.org>
2004-04-28 03:34:38 +0000

    make this at least compile

--- usr.sbin/bgpd/pfkey.c
+++ usr.sbin/bgpd/pfkey.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfkey.c,v 1.24 2004/04/28 03:19:52 markus Exp $ */
+/*	$OpenBSD: pfkey.c,v 1.25 2004/04/28 03:34:38 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -424,8 +424,12 @@ pfkey_reply(int sd, u_int32_t *spip)
 	}
 	if (hdr.sadb_msg_errno != 0) {
 		errno = hdr.sadb_msg_errno;
-		log_warn("pfkey");
-		return (-1);
+		if (errno == ESRCH)
+			return (0);
+		else {
+			log_warn("pfkey");
+			return (-1);
+		}
 	}
 	len = hdr.sadb_msg_len * PFKEY2_CHUNK;
 	if ((data = malloc(len)) == NULL) {
--- usr.sbin/bgpd/rde.c
+++ usr.sbin/bgpd/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.106 2004/04/28 02:57:27 claudio Exp $ */
+/*	$OpenBSD: rde.c,v 1.107 2004/04/28 03:34:38 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -1056,14 +1056,18 @@ peer_down(u_int32_t id)
 void
 peer_dump(u_int32_t id, u_int16_t afi, u_int8_t safi)
 {
-	if (afi == AFI_ALL | afi == AFI_IPv4)
+	if (afi == AFI_ALL || afi == AFI_IPv4)
 		if (safi == SAFI_ALL || safi == SAFI_UNICAST ||
 		    safi == SAFI_BOTH) {
+#if 0
 			pt_dump(up_dump_upcall, peer);
+#endif
 			return;
 		}
 
+#if 0
 	log_peer_warnx(&peer->conf, "unsupported AFI, SAFI combination");
+#endif
 }
 
 /*
