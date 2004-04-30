33b4f39fbeffad07bc3206f173cff9f3c9901cd1
espie <espie@openbsd.org>
2004-04-30 17:15:12 +0000

    Add tests for strerror.

---
+++ regress/lib/libc/strerror/strerror_test.c
@@ -0,0 +1,28 @@
+/* $OpenBSD: strerror_test.c,v 1.1 2004/04/30 17:15:12 espie Exp $ */
+/*
+ * Copyright (c) 2004 Marc Espie <espie@cvs.openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+	printf("%s\n", strerror(21345));
+	printf("%s\n", strerror(-21345));
+	printf("%s\n", strerror(0));
+	printf("%s\n", strerror(INT_MAX));
+	printf("%s\n", strerror(INT_MIN));
+	return 0;
+}
