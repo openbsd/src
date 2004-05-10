2277d5f82acd63e9c5ecb26cbd2ec92bc7bc2b82
millert <millert@openbsd.org>
2004-05-10 17:03:23 +0000

    Check select in addition to poll (they should produce the same results). Run tests 4 times: with and without blocking and with and without a timeout for poll/select.  Results match Solaris.

--- regress/sys/fifofs/fifotest.c
+++ regress/sys/fifofs/fifotest.c
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/types.h>
+#include <sys/param.h>
 #include <sys/stat.h>
 #include <sys/poll.h>
 #include <errno.h>
@@ -25,9 +25,15 @@
 #include <string.h>
 #include <unistd.h>
 
+#ifndef INFTIM
+#define	INFTIM	-1
+#endif
+
 void usage(void);
 void sigalrm(int);
-void dopoll(int, int, char *);
+void dopoll(int, int, char *, int);
+void doselect(int, int, int);
+void runtest(char *, int, int);
 
 #if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || \
     defined(__linux__)
@@ -39,21 +45,32 @@ char *__progname;
 /*
  * Test FIFOs and poll(2) both with an emtpy and full FIFO.
  */
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	struct sigaction sa;
-	ssize_t nread;
-	int fd;
-	char *fifo, buf[BUFSIZ];
-
-#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || \
-    defined(__linux__)
+#if !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(__NetBSD__) && \
+    !defined(__linux__)
 	__progname = argv[0];
 #endif
 	if (argc != 2)
 		usage();
 
-	fifo = argv[1];
+	runtest(argv[1], 0, 0);
+	runtest(argv[1], 0, INFTIM);
+	runtest(argv[1], O_NONBLOCK, 0);
+	runtest(argv[1], O_NONBLOCK, INFTIM);
+
+	exit(0);
+}
+
+void
+runtest(char *fifo, int flags, int timeout)
+{
+	struct sigaction sa;
+	ssize_t nread;
+	int fd;
+	char buf[BUFSIZ];
+
 	(void)unlink(fifo);
 	if (mkfifo(fifo, 0644) != 0) {
 		printf("mkfifo %s: %s\n", fifo, strerror(errno));
@@ -66,18 +83,22 @@ int main(int argc, char **argv)
 	sigaction(SIGALRM, &sa, NULL);
 
 	alarm(2);
-	if ((fd = open(fifo, O_RDWR, 0644)) == -1) {
+	if ((fd = open(fifo, O_RDWR | flags, 0644)) == -1) {
 		printf("open %s: %s\n", fifo, strerror(errno));
 		exit(1);
 	}
 	alarm(0);
 	(void)unlink(fifo);
-	printf("sucessfully opened fifo\n");
+	printf("\nOpened fifo %s%s\n", fifo,
+	    (flags & O_NONBLOCK) ? " (nonblocking)" : "");
 
 	printf("\nTesting empty FIFO:\n");
-	dopoll(fd, POLLIN|POLLOUT, "POLLIN|POLLOUT");
-	dopoll(fd, POLLIN, "POLLIN");
-	dopoll(fd, POLLOUT, "POLLOUT");
+	dopoll(fd, POLLIN|POLLOUT, "POLLIN|POLLOUT", timeout);
+	dopoll(fd, POLLIN, "POLLIN", timeout);
+	dopoll(fd, POLLOUT, "POLLOUT", timeout);
+	doselect(fd, fd, timeout);
+	doselect(fd, -1, timeout);
+	doselect(-1, fd, timeout);
 
 	if (write(fd, "test", 4) != 4) {
 		printf("write error: %s\n", strerror(errno));
@@ -85,9 +106,12 @@ int main(int argc, char **argv)
 	}
 
 	printf("\nTesting full FIFO:\n");
-	dopoll(fd, POLLIN|POLLOUT, "POLLIN|POLLOUT");
-	dopoll(fd, POLLIN, "POLLIN");
-	dopoll(fd, POLLOUT, "POLLOUT");
+	dopoll(fd, POLLIN|POLLOUT, "POLLIN|POLLOUT", timeout);
+	dopoll(fd, POLLIN, "POLLIN", timeout);
+	dopoll(fd, POLLOUT, "POLLOUT", timeout);
+	doselect(fd, fd, timeout);
+	doselect(fd, -1, timeout);
+	doselect(-1, fd, timeout);
 
 	if ((nread = read(fd, buf, sizeof(buf))) <= 0) {
 		printf("read error: %s\n", (nread == 0) ? "EOF" : strerror(errno));
@@ -95,12 +119,10 @@ int main(int argc, char **argv)
 	}
 	buf[nread] = '\0';
 	printf("\treceived '%s' from FIFO\n", buf);
-
-	exit(0);
 }
 
 void
-dopoll(int fd, int events, char *str)
+dopoll(int fd, int events, char *str, int timeout)
 {
 	struct pollfd pfd;
 	int nready;
@@ -108,10 +130,10 @@ dopoll(int fd, int events, char *str)
 	pfd.fd = fd;
 	pfd.events = events;
 
-	printf("\ttesting %s\n", str);
+	printf("\tpoll %s, timeout=%d\n", str, timeout);
 	pfd.events = events;
 	alarm(2);
-	nready = poll(&pfd, 1, 0);
+	nready = poll(&pfd, 1, timeout);
 	alarm(0);
 	if (nready < 0) {
 		printf("poll: %s\n", strerror(errno));
@@ -129,6 +151,60 @@ dopoll(int fd, int events, char *str)
 	if (pfd.revents & POLLNVAL)
 		printf(" POLLNVAL");
 	printf("\n");
+}
+
+void
+doselect(int rfd, int wfd, int timeout)
+{
+	struct timeval tv, *tvp;
+	fd_set *rfds = NULL, *wfds = NULL;
+	int nready, maxfd;
+
+	if (timeout == INFTIM)
+		tvp = NULL;
+	else {
+		tv.tv_sec = timeout / 1000;
+		tv.tv_usec = (timeout % 1000) * 1000;
+		tvp = &tv;
+	}
+	tv.tv_sec = tv.tv_usec = 0;
+	maxfd = rfd > wfd ? rfd : wfd;
+	if (rfd != -1) {
+		rfds = calloc(howmany(maxfd + 1, NFDBITS), sizeof(fd_mask));
+		if (rfds == NULL) {
+			printf("unable to allocate memory\n");
+			exit(1);
+		}
+		FD_SET(rfd, rfds);
+	}
+	if (wfd != -1) {
+		wfds = calloc(howmany(maxfd + 1, NFDBITS), sizeof(fd_mask));
+		if (wfds == NULL) {
+			printf("unable to allocate memory\n");
+			exit(1);
+		}
+		FD_SET(wfd, wfds);
+	}
+
+	printf("\tselect%s%s\n", rfds ? " read" : "",
+	    wfds ? " write" : "");
+
+	alarm(2);
+	nready = select(maxfd + 1, rfds, wfds, NULL, &tv);
+	alarm(0);
+	if (nready < 0) {
+		printf("select: %s\n", strerror(errno));
+		goto cleanup;
+	}
+	printf("\t\t%d fd(s) ready", nready);
+	if (rfds != NULL && FD_ISSET(rfd, rfds))
+		printf(", readable");
+	if (wfds != NULL && FD_ISSET(wfd, wfds))
+		printf(", writeable");
+	printf("\n");
+cleanup:
+	free(rfds);
+	free(wfds);
 }
 
 void
