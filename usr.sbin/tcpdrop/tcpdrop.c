9d298371af95c08be3f09ece6eeb295f746b10e4
deraadt <deraadt@openbsd.org>
2004-05-22 23:55:22 +0000

    missing header

--- usr.sbin/tcpdrop/tcpdrop.c
+++ usr.sbin/tcpdrop/tcpdrop.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tcpdrop.c,v 1.3 2004/05/09 03:22:39 deraadt Exp $ */
+/* $OpenBSD: tcpdrop.c,v 1.4 2004/05/22 23:55:22 deraadt Exp $ */
 
 /*
  * Copyright (c) 2004 Markus Friedl <markus@openbsd.org>
@@ -29,6 +29,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <netdb.h>
 
 extern char *__progname;
