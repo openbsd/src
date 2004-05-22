3847979c56800e06ad18dae4e7a9d1e5eb3d4aa1
drahn <drahn@openbsd.org>
2004-05-22 19:42:52 +0000

    Dont print symcache statistics if no symbol lookups have occurred, ok mickey@

--- libexec/ld.so/dlfcn.c
+++ libexec/ld.so/dlfcn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dlfcn.c,v 1.33 2003/10/01 02:55:23 millert Exp $ */
+/*	$OpenBSD: dlfcn.c,v 1.34 2004/05/22 19:42:52 drahn Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -321,9 +321,11 @@ _dl_show_objects(void)
 	{
 		extern int _dl_symcachestat_hits;
 		extern int _dl_symcachestat_lookups;
-	DL_DEB(("symcache lookups %d hits %d ratio %d% hits\n",
-	    _dl_symcachestat_lookups, _dl_symcachestat_hits,
-	    (_dl_symcachestat_hits * 100) / _dl_symcachestat_lookups));
+		if (_dl_symcachestat_lookups != 0)
+			DL_DEB(("symcache lookups %d hits %d ratio %d% hits\n",
+			    _dl_symcachestat_lookups, _dl_symcachestat_hits,
+			    (_dl_symcachestat_hits * 100) /
+			    _dl_symcachestat_lookups));
 	}
 }
 
