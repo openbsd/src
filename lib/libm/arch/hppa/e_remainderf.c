ccf4823aff87654f0c4a37bd1da1947e7b90618f
mickey <mickey@openbsd.org>
2004-05-22 23:36:27 +0000

    evil pasto

--- lib/libm/arch/hppa/e_remainderf.c
+++ lib/libm/arch/hppa/e_remainderf.c
@@ -3,13 +3,13 @@
  */
 
 #if defined(LIBM_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: e_remainderf.c,v 1.1 2002/05/22 21:34:56 mickey Exp $";
+static char rcsid[] = "$OpenBSD: e_remainderf.c,v 1.2 2004/05/22 23:36:27 mickey Exp $";
 #endif
 
 #include "math.h"
 
 float
-__ieee754_remainder(float x, float p)
+__ieee754_remainderf(float x, float p)
 {
 	__asm__ __volatile__("frem,sgl %0,%1,%0" : "+f" (x) : "f" (p));
 
