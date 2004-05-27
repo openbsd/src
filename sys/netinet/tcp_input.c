0bb7306b264f4e1fa9f99c9f963faacc79eba134
markus <markus@openbsd.org>
2004-05-27 08:17:31 +0000

    the tcp header might be in a different mbuf after pulldown(); fixes tcp corruption on rl(4); ok itojun, cedric

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_input.c,v 1.169 2004/05/26 22:47:40 markus Exp $	*/
+/*	$OpenBSD: tcp_input.c,v 1.170 2004/05/27 08:17:31 markus Exp $	*/
 /*	$NetBSD: tcp_input.c,v 1.23 1996/02/13 23:43:44 christos Exp $	*/
 
 /*
@@ -551,8 +551,6 @@ tcp_input(struct mbuf *m, ...)
 #endif
 	}
 #endif /* TUBA_INCLUDE */
-
-	th = (struct tcphdr *)(mtod(m, caddr_t) + iphlen);
 
 	/*
 	 * Check that TCP offset makes sense,
