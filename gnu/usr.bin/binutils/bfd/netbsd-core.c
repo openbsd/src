eb020bae8a86d6d0bc81ea352d8af0886930e351
kettenis <kettenis@openbsd.org>
2004-05-30 17:04:01 +0000

    Recognize m88k core files. ok miod@

--- gnu/usr.bin/binutils/bfd/netbsd-core.c
+++ gnu/usr.bin/binutils/bfd/netbsd-core.c
@@ -182,6 +182,10 @@ netbsd_core_file_p (abfd)
       bfd_default_set_arch_mach (abfd, bfd_arch_m68k, 0);
       break;
 
+    case M_88K_OPENBSD:
+      bfd_default_set_arch_mach (abfd, bfd_arch_m88k, 0);
+      break;
+
     case M_POWERPC_NETBSD:
       bfd_default_set_arch_mach (abfd, bfd_arch_powerpc, bfd_mach_ppc);
       break;
