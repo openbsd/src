3b49ed98bd43e705e6da7cc1ac6dee93e47cf7e2
millert <millert@openbsd.org>
2004-06-04 18:04:38 +0000

    regen for ASUS SpaceLink WL-100

--- sys/dev/pcmcia/pcmciadevs.h
+++ sys/dev/pcmcia/pcmciadevs.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: pcmciadevs.h,v 1.116 2004/06/04 16:02:47 millert Exp $	*/
+/*	$OpenBSD: pcmciadevs.h,v 1.117 2004/06/04 18:04:38 millert Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *		OpenBSD: pcmciadevs,v 1.113 2004/06/04 16:02:34 millert Exp 
+ *		OpenBSD: pcmciadevs,v 1.114 2004/06/04 18:03:35 millert Exp 
  */
 /* $NetBSD: pcmciadevs,v 1.13 1998/08/17 23:10:12 thorpej Exp $ */
 
@@ -92,6 +92,7 @@
 #define	PCMCIA_VENDOR_BUFFALO	0x026f	/* BUFFALO (Melco Corporation) */
 #define	PCMCIA_VENDOR_LINKSYS2	0x0274	/* The Linksys Group */
 #define	PCMCIA_VENDOR_IODATA2	0x028a	/* I-O DATA */
+#define	PCMCIA_VENDOR_ASUS	0x02aa	/* ASUS */
 #define	PCMCIA_VENDOR_MICROSOFT	0x02d2	/* Microsoft */
 #define	PCMCIA_VENDOR_NWN	0x0602	/* No Wires Needed */
 #define	PCMCIA_VENDOR_BREEZECOM	0x0a02	/* BreezeCOM */
@@ -173,6 +174,10 @@
 /* Archos */
 #define	PCMCIA_CIS_ARCHOS_ARC_ATAPI	{ NULL, NULL, NULL, NULL }
 #define	PCMCIA_PRODUCT_ARCHOS_ARC_ATAPI	0x0043
+
+/* ASUS */
+#define	PCMCIA_CIS_ASUS_WL_100	{ NULL, NULL, NULL, NULL }
+#define	PCMCIA_PRODUCT_ASUS_WL_100	0x0002
 
 /* Bay Networks */
 #define	PCMCIA_CIS_BAY_STACK_650	{ NULL, NULL, NULL, NULL }
