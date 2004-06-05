f3f774043a4b20c86267076fcb0ee0d69b87c037
mcbride <mcbride@openbsd.org>
2004-06-05 20:24:30 +0000

    - replace handrolled crc32 with ether_crc32_be() - make multicast ranges work
    ok deraadt@

--- sys/dev/pci/if_lge.c
+++ sys/dev/pci/if_lge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_lge.c,v 1.14 2004/04/09 21:52:17 henning Exp $	*/
+/*	$OpenBSD: if_lge.c,v 1.15 2004/06/05 20:24:30 mcbride Exp $	*/
 /*
  * Copyright (c) 2001 Wind River Systems
  * Copyright (c) 1997, 1998, 1999, 2000, 2001
@@ -153,7 +153,6 @@ void lge_miibus_writereg(struct device *, int, int, int);
 void lge_miibus_statchg(struct device *);
 
 void lge_setmulti(struct lge_softc *);
-u_int32_t lge_crc(struct lge_softc *, caddr_t);
 void lge_reset(struct lge_softc *);
 int lge_list_rx_init(struct lge_softc *);
 int lge_list_tx_init(struct lge_softc *);
@@ -332,34 +331,6 @@ void lge_miibus_statchg(dev)
 	return;
 }
 
-u_int32_t lge_crc(sc, addr)
-	struct lge_softc	*sc;
-	caddr_t			addr;
-{
-	u_int32_t		crc, carry;
-	int			i, j;
-	u_int8_t		c;
-
-	/* Compute CRC for the address value. */
-	crc = 0xFFFFFFFF; /* initial value */
-
-	for (i = 0; i < 6; i++) {
-		c = *(addr + i);
-		for (j = 0; j < 8; j++) {
-			carry = ((crc & 0x80000000) ? 1 : 0) ^ (c & 0x01);
-			crc <<= 1;
-			c >>= 1;
-			if (carry)
-				crc = (crc ^ 0x04c11db6) | carry;
-		}
-	}
-
-	/*
-	 * return the filter bit position
-	 */
-	return((crc >> 26) & 0x0000003F);
-}
-
 void lge_setmulti(sc)
 	struct lge_softc	*sc;
 {
@@ -372,6 +343,7 @@ void lge_setmulti(sc)
 	/* Make sure multicast hash table is enabled. */
 	CSR_WRITE_4(sc, LGE_MODE1, LGE_MODE1_SETRST_CTL1|LGE_MODE1_RX_MCAST);
 
+allmulti:
 	if (ifp->if_flags & IFF_ALLMULTI || ifp->if_flags & IFF_PROMISC) {
 		CSR_WRITE_4(sc, LGE_MAR0, 0xFFFFFFFF);
 		CSR_WRITE_4(sc, LGE_MAR1, 0xFFFFFFFF);
@@ -385,8 +357,12 @@ void lge_setmulti(sc)
 	/* now program new ones */
 	ETHER_FIRST_MULTI(step, ac, enm);
 	while (enm != NULL) {
-		if (memcmp(enm->enm_addrlo, enm->enm_addrhi, 6) != 0)
-			continue;
+		if (bcmp(enm->enm_addrlo, enm->enm_addrhi, ETHER_ADDR_LEN)) {
+			ifp->if_flags |= IFF_ALLMULTI;
+			goto allmulti;
+		}
+		h = ( ether_crc32_be(enm->enm_addrlo, ETHER_ADDR_LEN) >> 26) &
+		    0x0000003F;
 		h = lge_crc(sc, LLADDR((struct sockaddr_dl *)enm->enm_addrlo));
 		if (h < 32)
 			hashes[0] |= (1 << h);
