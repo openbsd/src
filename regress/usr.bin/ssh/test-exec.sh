579b0c1b950ae082834cc57c49da68ffc5637071
djm <djm@openbsd.org>
2004-06-13 15:16:54 +0000

    remove duplicate setting of $SCP; spotted by markus@

--- regress/usr.bin/ssh/test-exec.sh
+++ regress/usr.bin/ssh/test-exec.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: test-exec.sh,v 1.18 2004/06/13 15:04:08 djm Exp $
+#	$OpenBSD: test-exec.sh,v 1.19 2004/06/13 15:16:54 djm Exp $
 #	Placed in the Public Domain.
 
 PORT=4242
@@ -35,7 +35,6 @@ unset SSH_AUTH_SOCK
 # defaults
 SSH=ssh
 SSHD=sshd
-SCP=scp
 SSHAGENT=ssh-agent
 SSHADD=ssh-add
 SSHKEYGEN=ssh-keygen
@@ -49,9 +48,6 @@ if [ "x$TEST_SSH_SSH" != "x" ]; then
 fi
 if [ "x$TEST_SSH_SSHD" != "x" ]; then
 	SSHD="${TEST_SSH_SSHD}"
-fi
-if [ "x$TEST_SSH_SCP" != "x" ]; then
-	SCP="${TEST_SSH_SCP}"
 fi
 if [ "x$TEST_SSH_SSHAGENT" != "x" ]; then
 	SSHAGENT="${TEST_SSH_SSHAGENT}"
