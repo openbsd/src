b5ab232b08d8a65a4d0aaa074d6b4e6900a1c6fc
henning <henning@openbsd.org>
2004-06-22 07:42:41 +0000

    missing bzero in a function for this strange other address family

--- usr.sbin/bgpd/session.c
+++ usr.sbin/bgpd/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.178 2004/06/22 03:44:26 claudio Exp $ */
+/*	$OpenBSD: session.c,v 1.179 2004/06/22 07:42:41 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -2334,6 +2334,7 @@ session_match_mask(struct peer *p, struct sockaddr *ip)
 	}
 
 	if (p->conf.remote_addr.af == AF_INET6) {
+		bzero(&mask, sizeof(mask));
 		for (i = 0; i < p->conf.remote_masklen / 8; i++)
 			mask.s6_addr[i] = 0xff;
 		i = p->conf.remote_masklen % 8;
