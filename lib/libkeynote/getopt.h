67f73dfa0fff0bc6b8efb43075c54c80ee94fb91
msf <msf@openbsd.org>
2004-06-24 23:08:23 +0000

    Remove unused getopt implementation. These files are not used. ok hshoexer@ theo@

--- lib/libkeynote/getopt.h
+++
@@ -1,5 +0,0 @@
-/* $OpenBSD: getopt.h,v 1.1 1999/10/01 01:08:30 angelos Exp $ */
-extern int   opterr;   /* flag:error message on unrecognzed options */
-extern int   optind;   /* last touched cmdline argument */
-extern char  *optarg;  /* argument to optopt */
-int getopt(int argc, char **argv, char *opts);
