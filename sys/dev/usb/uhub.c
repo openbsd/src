0964fbcce032b153aae054f1b8e0b154bf3f0298
deraadt <deraadt@openbsd.org>
2004-07-11 05:32:38 +0000

    from netbsd via loki@animata.net, revision 1.68 Ignore a port error that happens to come in at the same time as a connect status change.  Some root hubs seem to report both.

--- sys/dev/usb/uhub.c
+++ sys/dev/usb/uhub.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uhub.c,v 1.25 2004/07/11 05:29:16 deraadt Exp $ */
+/*	$OpenBSD: uhub.c,v 1.26 2004/07/11 05:32:38 deraadt Exp $ */
 /*	$NetBSD: uhub.c,v 1.64 2003/02/08 03:32:51 ichiro Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/uhub.c,v 1.18 1999/11/17 22:33:43 n_hibma Exp $	*/
 
@@ -358,7 +358,10 @@ uhub_explore(usbd_device_handle dev)
 		if (change & UPS_C_PORT_ENABLED) {
 			DPRINTF(("uhub_explore: C_PORT_ENABLED\n"));
 			usbd_clear_port_feature(dev, port, UHF_C_PORT_ENABLE);
-			if (status & UPS_PORT_ENABLED) {
+			if (change & UPS_C_CONNECT_STATUS) {
+				/* Ignore the port error if the device
+				   vanished. */
+			} else if (status & UPS_PORT_ENABLED) {
 				printf("%s: illegal enable change, port %d\n",
 				       USBDEVNAME(sc->sc_dev), port);
 			} else {
