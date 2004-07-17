05f229c1744c848d9c72d2f0132bd842fd34f1de
marc <marc@openbsd.org>
2004-07-17 20:38:43 +0000

    add PD statement.   Requested by espie@, OK millert@

--- lib/libc/gen/errno.c
+++ lib/libc/gen/errno.c
@@ -1,5 +1,7 @@
+/* PUBLIC DOMAIN: No Rights Reserved.   Marco S Hyman <marc@snafu.org> */
+
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: errno.c,v 1.3 2004/07/13 21:03:35 marc Exp $";
+static char rcsid[] = "$OpenBSD: errno.c,v 1.4 2004/07/17 20:38:43 marc Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <errno.h>
