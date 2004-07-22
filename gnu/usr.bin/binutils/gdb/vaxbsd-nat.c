bf8695e87ca46acb4c81e205b88c5b8a30afa8d8
kettenis <kettenis@openbsd.org>
2004-07-22 19:14:52 +0000

    Add vax libkvm kernel memory image debugging support. ok miod@, wow deraadt@

--- gnu/usr.bin/binutils/gdb/vaxbsd-nat.c
+++ gnu/usr.bin/binutils/gdb/vaxbsd-nat.c
@@ -54,7 +54,7 @@ vaxbsd_collect_gregset (const struct regcache *regcache,
   for (i = 0; i <= VAX_NUM_REGS; i++)
     {
       if (regnum == -1 || regnum == i)
-	regcache_raw_collect (regcache, regnum, regs + i * 4);
+	regcache_raw_collect (regcache, i, regs + i * 4);
     }
 }
 
@@ -91,4 +91,48 @@ store_inferior_registers (int regnum)
   if (ptrace (PT_SETREGS, PIDGET (inferior_ptid),
 	      (PTRACE_ARG3_TYPE) &regs, 0) == -1)
     perror_with_name ("Couldn't write registers");
+}
+
+
+/* Support for debugging kernel virtual memory images.  */
+
+#include <sys/types.h>
+#include <machine/pcb.h>
+
+#include "bsd-kvm.h"
+
+static int
+vaxbsd_supply_pcb (struct regcache *regcache, struct pcb *pcb)
+{
+  int regnum;
+
+  /* The following is true for OpenBSD 3.5:
+
+     The pcb contains the register state at the context switch inside
+     cpu_switch().  */
+
+  /* The stack pointer shouldn't be zero.  */
+  if (pcb->KSP == 0)
+    return 0;
+
+  for (regnum = VAX_R0_REGNUM; regnum < VAX_AP_REGNUM; regnum++)
+    regcache_raw_supply (regcache, regnum, &pcb->R[regnum - VAX_R0_REGNUM]);
+  regcache_raw_supply (regcache, VAX_AP_REGNUM, &pcb->AP);
+  regcache_raw_supply (regcache, VAX_FP_REGNUM, &pcb->FP);
+  regcache_raw_supply (regcache, VAX_SP_REGNUM, &pcb->KSP);
+  regcache_raw_supply (regcache, VAX_PC_REGNUM, &pcb->PC);
+  regcache_raw_supply (regcache, VAX_PS_REGNUM, &pcb->PSL);
+
+  return 1;
+}
+
+
+/* Provide a prototype to silence -Wmissing-prototypes.  */
+void _initialize_vaxbsd_nat (void);
+
+void
+_initialize_vaxbsd_nat (void)
+{
+  /* Support debugging kernel virtual memory images.  */
+  bsd_kvm_add_target (vaxbsd_supply_pcb);
 }
