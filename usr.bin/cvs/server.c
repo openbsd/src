ec4f5a20a0a409fa1c7fc376ee151ac14e6e3a77
jfb <jfb@openbsd.org>
2004-07-28 02:02:50 +0000

    Fix $OpenBSD$ tag

--- usr.bin/cvs/server.c
+++ usr.bin/cvs/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.1.1.1 2004/07/13 22:02:40 jfb Exp $	*/
+/*	$OpenBSD: server.c,v 1.2 2004/07/28 02:02:50 jfb Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved. 
