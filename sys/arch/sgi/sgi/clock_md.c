80ff2c4b123279cc95f29197c14e6f698ca0bbee
pefo <pefo@openbsd.org>
2004-08-07 10:24:41 +0000

    remove localbus.h include

--- sys/arch/sgi/sgi/clock_md.c
+++ sys/arch/sgi/sgi/clock_md.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock_md.c,v 1.1 2004/08/06 21:12:19 pefo Exp $ */
+/*	$OpenBSD: clock_md.c,v 1.2 2004/08/07 10:24:41 pefo Exp $ */
 
 /*
  * Copyright (c) 2001-2004 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -36,8 +36,6 @@
 
 #include <dev/ic/mc146818reg.h>
 #include <machine/m48t37.h>
-
-#include <pmonmips/localbus/localbus.h>
 
 #include <mips64/archtype.h>
 
