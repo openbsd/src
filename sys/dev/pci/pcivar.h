6d6e96a5c22ac1be204d4fab8d5653108d7fa127
pefo <pefo@openbsd.org>
2004-08-09 22:26:24 +0000

    remove some old stuff and clean up. arc and galileo is gone. ok mickey@

--- sys/dev/pci/pcivar.h
+++ sys/dev/pci/pcivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcivar.h,v 1.37 2004/08/06 21:54:09 mickey Exp $	*/
+/*	$OpenBSD: pcivar.h,v 1.38 2004/08/09 22:26:24 pefo Exp $	*/
 /*	$NetBSD: pcivar.h,v 1.23 1997/06/06 23:48:05 thorpej Exp $	*/
 
 /*
@@ -54,7 +54,7 @@ struct pcibus_attach_args;
 /*
  * Machine-dependent definitions.
  */
-#if (__alpha__ + __atari__ + __i386__ + __cats__ + __powerpc__ + __galileo__ + __sparc64__ + __hppa__ +__amd64__ + __sgi__ != 1)
+#if (__alpha__ + __atari__ + __i386__ + __cats__ + __powerpc__ + __sparc64__ + __hppa__ +__amd64__ + __sgi__ != 1)
 #error COMPILING FOR UNSUPPORTED MACHINE, OR MORE THAN ONE.
 #endif
 #if __alpha__
@@ -66,29 +66,23 @@ struct pcibus_attach_args;
 #if __i386__
 #include <i386/pci/pci_machdep.h>
 #endif
-#if __arc__
-#include <arc/pci/pci_machdep.h>
-#endif
 #if __cats__
 #include <cats/pci/pci_machdep.h>
 #endif
 #if __powerpc__
 #include <powerpc/pci/pci_machdep.h>
 #endif
-#if __galileo__
-#include <galileo/pci/pci_machdep.h>
-#endif
-#if __sgi__
-#include <sgi/pci/pci_machdep.h>
+#if __sparc64__
+#include <sparc64/include/pci_machdep.h>
 #endif
 #if __hppa__
 #include <hppa/include/pci_machdep.h>
 #endif
-#if __sparc64__
-#include <sparc64/include/pci_machdep.h>
-#endif
 #if __amd64__
 #include <amd64/include/pci_machdep.h>
+#endif
+#if __sgi__
+#include <sgi/pci/pci_machdep.h>
 #endif
 
 /*
