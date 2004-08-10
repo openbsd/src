b1ec829b2222e8ca0ffb4c263c4a8f39841df491
pefo <pefo@openbsd.org>
2004-08-10 20:34:59 +0000

    zap ECOFF

--- sys/arch/sgi/include/exec.h
+++ sys/arch/sgi/include/exec.h
@@ -1,8 +1,13 @@
-/*	$OpenBSD: exec.h,v 1.3 2004/08/10 18:52:58 deraadt Exp $	*/
+/*	$OpenBSD: exec.h,v 1.4 2004/08/10 20:34:59 pefo Exp $	*/
 
 /*
+<<<<<<< exec.h
+ * Copyright (c) 1996-2004 Per Fogelstrom, Opsycon AB
+ * 
+=======
  * Copyright (c) 1996-2003 Per Fogelstrom
  *
+>>>>>>> 1.3
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -11,12 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed under OpenBSD by
- *	Per Fogelstrom.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -54,7 +53,6 @@
 #define ELF_TARG_MACH		EM_MIPS
 
 #define _NLIST_DO_ELF
-#define _NLIST_DO_ECOFF
 
 #define _KERN_DO_ELF
 #if defined(_LP64)
