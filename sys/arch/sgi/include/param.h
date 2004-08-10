d028587b222a543d38c503f10576e96d3c04cd41
pefo <pefo@openbsd.org>
2004-08-10 21:11:42 +0000

    copyrights

--- sys/arch/sgi/include/param.h
+++ sys/arch/sgi/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.2 2004/08/10 18:51:29 deraadt Exp $ */
+/*	$OpenBSD: param.h,v 1.3 2004/08/10 21:11:42 pefo Exp $ */
 
 /*
  * Copyright (c) 2003 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Opsycon AB, Sweden.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
