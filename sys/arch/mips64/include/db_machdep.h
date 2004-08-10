d5809d58ebd959d64308e6724f233a56b6fa0f08
pefo <pefo@openbsd.org>
2004-08-10 20:07:15 +0000

    remove MID_MACHINE from here. one is enough

--- sys/arch/mips64/include/db_machdep.h
+++ sys/arch/mips64/include/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_machdep.h,v 1.1 2004/08/06 20:56:01 pefo Exp $ */
+/*	$OpenBSD: db_machdep.h,v 1.2 2004/08/10 20:07:15 pefo Exp $ */
 
 /*
  * Copyright (c) 1998-2003 Opsycon AB (www.opsycon.se)
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Opsycon AB, Sweden.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -38,7 +33,6 @@
 #include <machine/trap.h>
 #include <uvm/uvm_param.h>
 
-#define	MID_MACHINE 0		/* XXX booo... */
 #define DB_MACHINE_COMMANDS	/* We have machine specific commands */
 #define	DB_ELF_SYMBOLS		/* Elf style symbol table support */
 #define	DB_NO_AOUT		/* For crying out loud! */
