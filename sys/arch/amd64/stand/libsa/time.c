17af5266ee9267e2857c344d6715fabb8c19db0c
tom <tom@openbsd.org>
2004-08-18 19:13:07 +0000

    Calculate time_t correctly from (day, month, year): 1/1/70 is 0, not 1, as recently fixed on i386.
    requested by, and ok, deraadt@

--- sys/arch/amd64/stand/libsa/time.c
+++ sys/arch/amd64/stand/libsa/time.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: time.c,v 1.1 2004/02/03 12:09:47 mickey Exp $	*/
+/*	$OpenBSD: time.c,v 1.2 2004/08/18 19:13:07 tom Exp $	*/
 
 /*
  * Copyright (c) 1997 Michael Shalayeff
@@ -62,7 +62,7 @@ compute(int year, u_int8_t month, u_int8_t day, u_int8_t hour,
 	register time_t tt;
 
 	/* Compute days */
-	tt = (year - 1970) * 365 + monthcount[month] + day;
+	tt = (year - 1970) * 365 + monthcount[month] + day - 1;
 
 	/* Compute for leap year */
 	for(month <= 2? year--:0;year >= 1970;year--)
