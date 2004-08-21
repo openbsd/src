4da98914fc6e704420b5875e4add40c84ca0ab81
tom <tom@openbsd.org>
2004-08-21 19:09:42 +0000

    Fix typo in comment

--- sys/arch/i386/stand/libsa/libsa.h
+++ sys/arch/i386/stand/libsa/libsa.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: libsa.h,v 1.41 2004/06/26 05:19:37 tom Exp $	*/
+/*	$OpenBSD: libsa.h,v 1.42 2004/08/21 19:09:42 tom Exp $	*/
 
 /*
  * Copyright (c) 1996-1999 Michael Shalayeff
@@ -77,4 +77,4 @@ extern void (*devboot_p)(dev_t, char *);
 extern bios_diskinfo_t bios_diskinfo[];
 extern u_int32_t bios_cksumlen;
 
-#define MACHINE_CMD	cmd_machine /* we have i386 specific sommands */
+#define MACHINE_CMD	cmd_machine /* we have i386-specific commands */
