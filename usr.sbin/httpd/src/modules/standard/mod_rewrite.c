83284c088ef02f083ebb204459ee46a6a11de0ae
henning <henning@openbsd.org>
2004-09-08 16:12:30 +0000

    security fix: Apache's mod_rewrite module can be made to write one zero byte in an arbitrary memory position outside of a char array, causing DoS or possibly buffer overflows. The function lookup_map_dbmfile() in modules/mappers/mod_rewrite.c copies data from a DBM file to the char array buf in a _secure_ manner, but it zero-terminates the array afterwards in an _insecure_ manner. If the key that is looked up has an n bytes long value, a zero byte will be written in the memory position n bytes from the start of the char array buf. exploiting would require enabling dbm for mod_rewrite and getting it to use a malicious dbm file. reported by Ulf.Harnhammar.9485@student.uu.se fix by me ok otto, deraadt

--- usr.sbin/httpd/src/modules/standard/mod_rewrite.c
+++ usr.sbin/httpd/src/modules/standard/mod_rewrite.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mod_rewrite.c,v 1.21 2004/05/16 18:36:02 otto Exp $ */
+/*	$OpenBSD: mod_rewrite.c,v 1.22 2004/09/08 16:12:30 henning Exp $ */
 
 /* ====================================================================
  * The Apache Software License, Version 1.1
@@ -3080,16 +3080,17 @@ static char *lookup_map_dbmfile(request_rec *r, char *file, char *key)
     datum dbmval;
     char *value = NULL;
     char buf[MAX_STRING_LEN];
+    size_t len;
 
     dbmkey.dptr  = key;
     dbmkey.dsize = strlen(key);
     if ((dbmfp = dbm_open(file, O_RDONLY, 0666)) != NULL) {
         dbmval = dbm_fetch(dbmfp, dbmkey);
         if (dbmval.dptr != NULL) {
-            memcpy(buf, dbmval.dptr, 
-                   dbmval.dsize < sizeof(buf)-1 ? 
-                   dbmval.dsize : sizeof(buf)-1  );
-            buf[dbmval.dsize] = '\0';
+            len = dbmval.dsize < sizeof(buf)-1 ? 
+                  dbmval.dsize : sizeof(buf)-1;
+            memcpy(buf, dbmval.dptr, len);
+            buf[len] = '\0';
             value = ap_pstrdup(r->pool, buf);
         }
         dbm_close(dbmfp);
