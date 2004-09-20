901650d986827e39e7e6d70f7517973c3bee0478
deraadt <deraadt@openbsd.org>
2004-09-20 06:01:04 +0000

    another unit; http://www2.elecom.co.jp/products/UC-SGT.html

--- sys/dev/usb/uplcom.c
+++ sys/dev/usb/uplcom.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uplcom.c,v 1.13 2004/07/08 22:18:44 deraadt Exp $	*/
+/*	$OpenBSD: uplcom.c,v 1.14 2004/09/20 06:01:04 deraadt Exp $	*/
 /*	$NetBSD: uplcom.c,v 1.29 2002/09/23 05:51:23 simonb Exp $	*/
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -159,6 +159,7 @@ static const struct usb_devno uplcom_devs[] = {
 	{ USB_VENDOR_PROLIFIC, USB_PRODUCT_PROLIFIC_PL2303 },
 	/* ELECOM UC-SGT */
 	{ USB_VENDOR_ELECOM, USB_PRODUCT_ELECOM_UCSGT },
+	{ USB_VENDOR_ELECOM, USB_PRODUCT_ELECOM_UCSGT0 },
 	/* RATOC REX-USB60 */
 	{ USB_VENDOR_RATOC, USB_PRODUCT_RATOC_REXUSB60 },
 	/* TDK USB-PHS Adapter UHA6400 */
