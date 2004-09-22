10a6005b9a83a18c41ef6f3419b19d7de151cf56
espie <espie@openbsd.org>
2004-09-22 21:36:54 +0000

    OpenBSD infrastructure to compile simply, and install include files.

---
+++ lib/libexpat/expat_config.h
@@ -0,0 +1,8 @@
+/* $OpenBSD: expat_config.h,v 1.1 2004/09/22 21:36:54 espie Exp $ */
+
+/* quick and dirty conf for OpenBSD */
+
+#define HAVE_MEMMOVE 1
+#define XML_CONTEXT_BYTES 1024
+#define XML_DTD 1
+#define XML_NS 1
