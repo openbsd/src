afeb966f91756426b22308228cc996e7492e4eaa
grange <grange@openbsd.org>
2004-10-05 19:11:01 +0000

    Support for 6300ESB (timecounter, not SpeedStep of course).

--- sys/arch/i386/pci/ichpcib.c
+++ sys/arch/i386/pci/ichpcib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ichpcib.c,v 1.6 2004/10/05 18:58:40 grange Exp $	*/
+/*	$OpenBSD: ichpcib.c,v 1.7 2004/10/05 19:11:01 grange Exp $	*/
 /*
  * Copyright (c) 2004 Alexander Yurchenko <grange@openbsd.org>
  *
@@ -91,7 +91,8 @@ const struct pci_matchid ichpcib_devices[] = {
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801CAM_LPC },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801DB_LPC },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801DBM_LPC },
-	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801EB_LPC }
+	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801EB_LPC },
+	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_6300ESB_LPC }
 };
 
 int
