08056efdb5261f6226a3f890a0f11c35a69ff868
mickey <mickey@openbsd.org>
2004-10-05 18:43:42 +0000

    the file is just a copy from 4.4bsd and thus ucb copyright is enough

--- sys/arch/hppa/include/limits.h
+++ sys/arch/hppa/include/limits.h
@@ -1,27 +1,4 @@
-/*	$OpenBSD: limits.h,v 1.8 2003/06/02 23:27:46 millert Exp $	*/
-
-/* 
- * Copyright (c) 1992-1994, The University of Utah and
- * the Computer Systems Laboratory at the University of Utah (CSL).
- * All rights reserved.
- *
- * Permission to use, copy, modify and distribute this software is hereby
- * granted provided that (1) source code retains these copyright, permission,
- * and disclaimer notices, and (2) redistributions including binaries
- * reproduce the notices in supporting documentation, and (3) all advertising
- * materials mentioning features or use of this software display the following
- * acknowledgement: ``This product includes software developed by the
- * Computer Systems Laboratory at the University of Utah.''
- *
- * THE UNIVERSITY OF UTAH AND CSL ALLOW FREE USE OF THIS SOFTWARE IN ITS "AS
- * IS" CONDITION.  THE UNIVERSITY OF UTAH AND CSL DISCLAIM ANY LIABILITY OF
- * ANY KIND FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- *
- * CSL requests users of this software to return to csl-dist@cs.utah.edu any
- * improvements that they make and grant CSL redistribution rights.
- *
- * 	Utah $Hdr: limits.h 1.6 94/12/16$
- */
+/*	$OpenBSD: limits.h,v 1.9 2004/10/05 18:43:42 mickey Exp $	*/
 
 /*
  * Copyright (c) 1988, 1993
