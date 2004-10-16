010e35ab58670d2a7a7a6ea829ff3bd52b49fc15
henning <henning@openbsd.org>
2004-10-16 12:24:23 +0000

    oups, unrelated changes crept in

--- usr.sbin/bgpd/kroute.c
+++ usr.sbin/bgpd/kroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kroute.c,v 1.108 2004/10/16 09:33:03 henning Exp $ */
+/*	$OpenBSD: kroute.c,v 1.109 2004/10/16 12:24:23 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -398,19 +398,6 @@ kr_show_route(struct imsg *imsg)
 	}
 
 	send_imsg_session(IMSG_CTL_END, imsg->hdr.pid, NULL, 0);
-}
-
-void
-kr_ifinfo(char *ifname)
-{
-	struct kif_node	*kif;
-
-	RB_FOREACH(kif, kif_tree, &kit)
-		if (!strcmp(ifname, kif->k.ifname)) {
-			send_imsg_session(IMSG_IFINFO, 0,
-			    &kif->k, sizeof(kif->k));
-			return;
-		}
 }
 
 /*
@@ -964,8 +951,6 @@ if_change(u_short ifindex, int flags, struct if_data *ifd)
 	kif->k.link_state = ifd->ifi_link_state;
 	kif->k.media_type = ifd->ifi_type;
 	kif->k.baudrate = ifd->ifi_baudrate;
-
-	send_imsg_session(IMSG_IFINFO, 0, &kif->k, sizeof(kif->k));
 
 	if ((reachable = (flags & IFF_UP) &&
 	    (ifd->ifi_link_state != LINK_STATE_DOWN)) == kif->k.nh_reachable)
