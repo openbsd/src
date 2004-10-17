a5043c9b708699c479b4f038c19aa548d94374bc
grange <grange@openbsd.org>
2004-10-17 08:44:41 +0000

    Define number of ide dma registers; from NetBSD.

--- sys/dev/pci/pciidereg.h
+++ sys/dev/pci/pciidereg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pciidereg.h,v 1.6 2004/09/24 07:38:38 grange Exp $	*/
+/*	$OpenBSD: pciidereg.h,v 1.7 2004/10/17 08:44:41 grange Exp $	*/
 /*	$NetBSD: pciidereg.h,v 1.6 2000/11/14 18:42:58 thorpej Exp $	*/
 
 /*
@@ -87,6 +87,7 @@
 
 /* secondary channel registers offset */
 #define IDEDMA_SCH_OFFSET 0x08
+#define IDEDMA_NREGS 8
 
 /* Bus master command register (per channel) */
 #define IDEDMA_CMD(chan) (0x00 + IDEDMA_SCH_OFFSET * (chan))
