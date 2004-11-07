55298341cc74fbbeb5b91a791ad1502a3c51e791
espie <espie@openbsd.org>
2004-11-07 13:29:29 +0000

    clone() library entry so that md5 and modes/owners get copied along.

--- usr.sbin/pkg_add/OpenBSD/Update.pm
+++ usr.sbin/pkg_add/OpenBSD/Update.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Update.pm,v 1.12 2004/11/07 13:19:48 espie Exp $
+# $OpenBSD: Update.pm,v 1.13 2004/11/07 13:29:29 espie Exp $
 #
 # Copyright (c) 2004 Marc Espie <espie@openbsd.org>
 #
@@ -211,7 +211,7 @@ sub split_libs
 	for my $item (@{$plist->{items}}) {
 		if ($item->isa("OpenBSD::PackingElement::Lib") &&
 		    defined $to_split->{$item->fullname()}) {
-		    	OpenBSD::PackingElement::Lib->add($splitted, $item->{name});
+		    	OpenBSD::PackingElement::Lib->add($splitted, $item->clone());
 		} elsif ($item->isa("OpenBSD::PackingElement::Cwd")) {
 			OpenBSD::PackingElement::Cwd->add($splitted, $item->{name});
 		} else {
