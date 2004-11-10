7399cdf98211dd07fa94441643f6d8f6a4915c48
claudio <claudio@openbsd.org>
2004-11-10 15:18:11 +0000

    It is not needed to mask the value wit 0xff as it is < 255. OK henning@

--- usr.sbin/bgpd/rde_attr.c
+++ usr.sbin/bgpd/rde_attr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_attr.c,v 1.43 2004/08/10 13:08:13 claudio Exp $ */
+/*	$OpenBSD: rde_attr.c,v 1.44 2004/11/10 15:18:11 claudio Exp $ */
 
 /*
  * Copyright (c) 2004 Claudio Jeker <claudio@openbsd.org>
@@ -53,7 +53,7 @@ attr_write(void *p, u_int16_t p_len, u_int8_t flags, u_int8_t type,
 		memcpy(b, &tmp, sizeof(tmp));
 		b += 2;
 	} else
-		*b++ = (u_char)(data_len & 0xff);
+		*b++ = (u_char)data_len;
 
 	if (data_len != 0)
 		memcpy(b, data, data_len);
