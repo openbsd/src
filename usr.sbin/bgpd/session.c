0b41c2d5248feba6518bddfee6502469778969d5
claudio <claudio@openbsd.org>
2004-11-11 16:56:08 +0000

    Unbreak link local IPv6 sessions by setting the correct scope_id in addr2sa. OK henning@

--- usr.sbin/bgpd/session.c
+++ usr.sbin/bgpd/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.198 2004/11/10 14:48:25 claudio Exp $ */
+/*	$OpenBSD: session.c,v 1.199 2004/11/11 16:56:08 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -2546,6 +2546,7 @@ addr2sa(struct bgpd_addr *addr, u_int16_t port)
 		memcpy(&sa_in6->sin6_addr, &addr->v6,
 		    sizeof(sa_in6->sin6_addr));
 		sa_in6->sin6_port = htons(port);
+		sa_in6->sin6_scope_id = addr->scope_id;
 		break;
 	}
 
