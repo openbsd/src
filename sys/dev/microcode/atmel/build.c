25a3ed59c108ed35b21f3ea55f94cb0c080cbc95
deraadt <deraadt@openbsd.org>
2004-11-17 15:03:47 +0000

    rename to atmel

--- sys/dev/microcode/atmel/build.c
+++ sys/dev/microcode/atmel/build.c
@@ -1,10 +1,10 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-#include "atuwi_intersil_fw.h"
-#include "atuwi_rfmd2958-smc_fw.h"
-#include "atuwi_rfmd2958_fw.h"
-#include "atuwi_rfmd_fw.h"
+#include "atmel_intersil_fw.h"
+#include "atmel_rfmd2958-smc_fw.h"
+#include "atmel_rfmd2958_fw.h"
+#include "atmel_rfmd_fw.h"
 
 void
 output(const char *name, char *buf, int buflen)
@@ -24,24 +24,24 @@ output(const char *name, char *buf, int buflen)
 
 main(int argc, char *argv[])
 {
-	output("atu-intersil-int", atuwi_fw_intersil_int,
-	    sizeof atuwi_fw_intersil_int);
-	output("atu-intersil-ext", atuwi_fw_intersil_ext,
-	    sizeof atuwi_fw_intersil_ext);
+	output("atu-intersil-int", atmel_fw_intersil_int,
+	    sizeof atmel_fw_intersil_int);
+	output("atu-intersil-ext", atmel_fw_intersil_ext,
+	    sizeof atmel_fw_intersil_ext);
 
-	output("atu-rfmd2958smc-int", atuwi_fw_rfmd2958_smc_int,
-	    sizeof atuwi_fw_rfmd2958_smc_int);
-	output("atu-rfmd2958smc-ext", atuwi_fw_rfmd2958_smc_ext,
-	    sizeof atuwi_fw_rfmd2958_smc_ext);
+	output("atu-rfmd2958smc-int", atmel_fw_rfmd2958_smc_int,
+	    sizeof atmel_fw_rfmd2958_smc_int);
+	output("atu-rfmd2958smc-ext", atmel_fw_rfmd2958_smc_ext,
+	    sizeof atmel_fw_rfmd2958_smc_ext);
 
-	output("atu-rfmd2958-int", atuwi_fw_rfmd2958_int,
-	    sizeof atuwi_fw_rfmd2958_int);
-	output("atu-rfmd2958-ext", atuwi_fw_rfmd2958_ext,
-	    sizeof atuwi_fw_rfmd2958_ext);
+	output("atu-rfmd2958-int", atmel_fw_rfmd2958_int,
+	    sizeof atmel_fw_rfmd2958_int);
+	output("atu-rfmd2958-ext", atmel_fw_rfmd2958_ext,
+	    sizeof atmel_fw_rfmd2958_ext);
 
-	output("atu-rfmd-int", atuwi_fw_rfmd_int,
-	    sizeof atuwi_fw_rfmd_int);
-	output("atu-rfmd-ext", atuwi_fw_rfmd_ext,
-	    sizeof atuwi_fw_rfmd_ext);
+	output("atu-rfmd-int", atmel_fw_rfmd_int,
+	    sizeof atmel_fw_rfmd_int);
+	output("atu-rfmd-ext", atmel_fw_rfmd_ext,
+	    sizeof atmel_fw_rfmd_ext);
 
 }
