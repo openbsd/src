f88928fa4ba2f72cce579aefb1865ec372742b61
markus <markus@openbsd.org>
2004-11-17 09:22:43 +0000

    off-by-one in process_states[], fixes display of memory; ok deraadt, aaron

--- usr.bin/top/machine.c
+++ usr.bin/top/machine.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: machine.c,v 1.44 2004/10/07 06:26:12 otto Exp $	 */
+/* $OpenBSD: machine.c,v 1.45 2004/11/17 09:22:43 markus Exp $	 */
 
 /*-
  * Copyright (c) 1994 Thorsten Lockert <tholo@sigmasoft.com>
@@ -101,10 +101,10 @@ static long     cp_old[CPUSTATES];
 static long     cp_diff[CPUSTATES];
 
 /* these are for detailing the process states */
-int process_states[7];
+int process_states[8];
 char *procstatenames[] = {
 	"", " starting, ", " running, ", " idle, ",
-	" stopped, ", " zombie, ",
+	" stopped, ", " zombie, ", "dead, ", " on processor, ",
 	NULL
 };
 
