b183d2849f41c8c90b2363b292abffdfffbb1fe9
damien <damien@openbsd.org>
2004-11-24 21:20:58 +0000

    add missing field in struct iwi_associate

--- sys/dev/pci/if_iwireg.h
+++ sys/dev/pci/if_iwireg.h
@@ -1,4 +1,4 @@
-/*	$Id: if_iwireg.h,v 1.4 2004/11/23 21:28:22 damien Exp $ */
+/*	$Id: if_iwireg.h,v 1.5 2004/11/24 21:20:58 damien Exp $ */
 
 /*-
  * Copyright (c) 2004
@@ -288,6 +288,7 @@ struct iwi_associate {
 	u_int16_t	intval;
 	u_int8_t	dst[IEEE80211_ADDR_LEN];
 	u_int32_t	reserved3;
+	u_int16_t	reserved4;
 } __attribute__((__packed__));
 
 /* structure for command IWI_CMD_SCAN */
