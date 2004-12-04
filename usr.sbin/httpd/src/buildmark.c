7b6c2b5c8d93f87a6a86574282fd23bd51f4a361
henning <henning@openbsd.org>
2004-12-04 00:53:45 +0000

    incredible what for shit is in this code adding time and date the binary was built is a really really really important and worthwile information, rrrrriiiiiiiigggggghhhht. and for sure a nice way to make the binary changes every time it is built workaround for now (return "unnown") since this stupid piece of crap function is part of the API and modules etc might (ab)use it. king bula sez: what an incredible nonsense pointed out by John Kerbawy <kerbawy@gmail.com>

--- usr.sbin/httpd/src/buildmark.c
+++ usr.sbin/httpd/src/buildmark.c
@@ -59,13 +59,7 @@
 #include "ap_config.h"
 #include "httpd.h"
 
-#if defined(__DATE__) && defined(__TIME__)
-static const char server_built[] = __DATE__ " " __TIME__;
-#else
-static const char server_built[] = "unknown";
-#endif
-
 API_EXPORT(const char *) ap_get_server_built(void)
 {
-    return server_built;
+    return "unknown";
 }
