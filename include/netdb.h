6de6e2769bc50c01609f0f4387f20c5ac3d2c0e0
millert <millert@openbsd.org>
2004-12-20 22:35:32 +0000

    gai_strerror() should return const char *; OK deraadt@

--- include/netdb.h
+++ include/netdb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netdb.h,v 1.22 2004/11/17 03:57:53 itojun Exp $	*/
+/*	$OpenBSD: netdb.h,v 1.23 2004/12/20 22:35:32 millert Exp $	*/
 
 /*
  * ++Copyright++ 1980, 1983, 1988, 1993
@@ -311,7 +311,7 @@ int		getaddrinfo(const char *, const char *,
 void		freeaddrinfo(struct addrinfo *);
 int		getnameinfo(const struct sockaddr *, socklen_t,
 		    char *, size_t, char *, size_t, int);
-char		*gai_strerror(int);
+const char	*gai_strerror(int);
 int		net_addrcmp(struct sockaddr *, struct sockaddr *);
 int		getrrsetbyname(const char *, unsigned int, unsigned int, unsigned int, struct rrsetinfo **);
 void		freerrset(struct rrsetinfo *);
--- lib/libc/net/gai_strerror.c
+++ lib/libc/net/gai_strerror.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gai_strerror.c,v 1.5 2001/06/05 02:31:34 deraadt Exp $	*/
+/*	$OpenBSD: gai_strerror.c,v 1.6 2004/12/20 22:35:32 millert Exp $	*/
 
 /*
  * Copyright (c) 1997-1999, Craig Metz, All rights reserved.
@@ -38,7 +38,7 @@
 #include <netdb.h>
 #include <errno.h>
 
-char *
+const char *
 gai_strerror(int errnum)
 {
 	switch (errnum) {
