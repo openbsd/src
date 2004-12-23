d4152dd4deb086287bf5e645554d237b0c067184
espie <espie@openbsd.org>
2004-12-23 23:58:36 +0000

    give a complete path during install, so that we don't accidentally create a file with the wrong name if the directory doesn't exist. Shouldn't happen, ... but when it does it's hard to figure out.
    `sounds sane' theo@.
    
    ran through make build without issue.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.prog.mk,v 1.42 2004/10/20 09:28:17 espie Exp $
+#	$OpenBSD: bsd.prog.mk,v 1.43 2004/12/23 23:58:36 espie Exp $
 #	$NetBSD: bsd.prog.mk,v 1.55 1996/04/08 21:19:26 jtc Exp $
 #	@(#)bsd.prog.mk	5.26 (Berkeley) 6/25/91
 
@@ -149,7 +149,7 @@ afterinstall:
 realinstall:
 .if defined(PROG)
 	${INSTALL} ${INSTALL_COPY} ${INSTALL_STRIP} -o ${BINOWN} -g ${BINGRP} \
-	    -m ${BINMODE} ${PROG} ${DESTDIR}${BINDIR}
+	    -m ${BINMODE} ${PROG} ${DESTDIR}${BINDIR}/${PROG}
 .endif
 .endif
 
