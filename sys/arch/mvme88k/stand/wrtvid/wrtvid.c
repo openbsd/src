915a33a7b2ce4defa2079187b2f4f76a4d8bc4ca
drahn <drahn@openbsd.org>
2004-12-27 15:23:45 +0000

    Finally put copyrights on files I wrote _long_ ago. prodded by miod@

--- sys/arch/mvme88k/stand/wrtvid/wrtvid.c
+++ sys/arch/mvme88k/stand/wrtvid/wrtvid.c
@@ -1,4 +1,21 @@
-/*	$OpenBSD: wrtvid.c,v 1.4 2003/06/01 17:00:37 deraadt Exp $ */
+/*	$OpenBSD: wrtvid.c,v 1.5 2004/12/27 15:23:46 drahn Exp $ */
+
+/*
+ * Copyright (c) 1995 Dale Rahn <drahn@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
