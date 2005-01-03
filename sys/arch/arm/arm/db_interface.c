e4f2e6e29b990c9abbd4932c07b45e613ffa78f2
miod <miod@openbsd.org>
2005-01-03 16:58:50 +0000

    Don't forget to register "machine" commands for ddb.

--- sys/arch/arm/arm/db_interface.c
+++ sys/arch/arm/arm/db_interface.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_interface.c,v 1.3 2005/01/03 16:49:56 miod Exp $	*/
+/*	$OpenBSD: db_interface.c,v 1.4 2005/01/03 16:58:50 miod Exp $	*/
 /*	$NetBSD: db_interface.c,v 1.34 2003/10/26 23:11:15 chris Exp $	*/
 
 /* 
@@ -345,7 +345,7 @@ Debugger(void)
 	asm(".word	0xe7ffffff");
 }
 
-const struct db_command db_machine_command_table[] = {
+struct db_command db_machine_command_table[] = {
 	{ "frame",	db_show_frame_cmd,	0, NULL },
 #ifdef ARM32_DB_COMMANDS
 	ARM32_DB_COMMANDS,
@@ -382,6 +382,8 @@ db_machine_init(void)
 	 */
 	db_uh.uh_handler = db_trapper;
 	install_coproc_handler_static(0, &db_uh);
+
+	db_machine_commands_install(db_machine_command_table);
 }
 
 u_int
