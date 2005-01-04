b1e3c2a03c672571bc6b6adbbff7089b265c4388
miod <miod@openbsd.org>
2005-01-04 08:57:44 +0000

    No PCI on zaurus.

--- sys/arch/zaurus/include/conf.h
+++ sys/arch/zaurus/include/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.2 2004/12/31 14:57:13 drahn Exp $	*/
+/*	$OpenBSD: conf.h,v 1.3 2005/01/04 08:57:44 miod Exp $	*/
 /*	$NetBSD: conf.h,v 1.8 2002/02/10 12:26:03 chris Exp $	*/
 
 #ifndef _ZAURUS_CONF_H
@@ -9,7 +9,6 @@
  */
 #include "fcom.h"
 
-#define	CONF_HAVE_PCI
 #define	CONF_HAVE_USB
 #define	CONF_HAVE_SCSIPI
 #define	CONF_HAVE_WSCONS
