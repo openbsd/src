8a625b6212825512fc8cee6304d5a42de35e7ffb
jmc <jmc@openbsd.org>
2005-01-09 16:39:55 +0000

    typo in comment; ok many people from freebsd kern/75923;

--- sys/arch/i386/include/joystick.h
+++ sys/arch/i386/include/joystick.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: joystick.h,v 1.4 1997/09/16 07:52:33 deraadt Exp $ */
+/*	$OpenBSD: joystick.h,v 1.5 2005/01/09 16:39:55 jmc Exp $ */
 /*	$NetBSD: joystick.h,v 1.1 1996/03/27 19:18:56 perry Exp $	*/
 
 #ifndef _JOY_IOCTL_H_
@@ -16,7 +16,7 @@ struct joystick {
 #define JOY_SETTIMEOUT    _IOW('J', 1, int)    /* set timeout */
 #define JOY_GETTIMEOUT    _IOR('J', 2, int)    /* get timeout */
 #define JOY_SET_X_OFFSET  _IOW('J', 3, int)    /* set offset on X-axis */
-#define JOY_SET_Y_OFFSET  _IOW('J', 4, int)    /* set offset on X-axis */
+#define JOY_SET_Y_OFFSET  _IOW('J', 4, int)    /* set offset on Y-axis */
 #define JOY_GET_X_OFFSET  _IOR('J', 5, int)    /* get offset on X-axis */
 #define JOY_GET_Y_OFFSET  _IOR('J', 6, int)    /* get offset on Y-axis */
 
