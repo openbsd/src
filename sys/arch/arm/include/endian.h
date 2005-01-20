b610f1e003ac92c88a83dbe1d1a2be067e272976
drahn <drahn@openbsd.org>
2005-01-20 15:04:54 +0000

    ARM is strict alignment (practically) so let the kernel know this. ok miod@

--- sys/arch/arm/include/endian.h
+++ sys/arch/arm/include/endian.h
@@ -1,8 +1,9 @@
-/*	$OpenBSD: endian.h,v 1.1 2004/02/01 05:09:49 drahn Exp $	*/
+/*	$OpenBSD: endian.h,v 1.2 2005/01/20 15:04:54 drahn Exp $	*/
 
 #ifdef __ARMEB__
 #define BYTE_ORDER BIG_ENDIAN
 #else
 #define BYTE_ORDER LITTLE_ENDIAN
 #endif
+#define	__STRICT_ALIGNMENT
 #include <sys/endian.h>
