e4d8d522aaea856cb942bc633eb012766f93e275
jfb <jfb@openbsd.org>
2005-01-27 20:45:18 +0000

    get rid of event.h, it won't be used

--- usr.bin/cvs/sock.c
+++ usr.bin/cvs/sock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sock.c,v 1.8 2004/12/07 17:10:56 tedu Exp $	*/
+/*	$OpenBSD: sock.c,v 1.9 2005/01/27 20:45:18 jfb Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -38,7 +38,6 @@
 #include "log.h"
 #include "sock.h"
 #include "cvsd.h"
-#include "event.h"
 
 
 volatile sig_atomic_t  cvs_sock_doloop;
