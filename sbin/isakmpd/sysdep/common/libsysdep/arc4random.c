1423e6ce6485e89aaddcc72bc6308e661ec439d7
ho <ho@openbsd.org>
2005-02-10 14:43:14 +0000

    Make it compile.

--- sbin/isakmpd/sysdep/common/libsysdep/arc4random.c
+++ sbin/isakmpd/sysdep/common/libsysdep/arc4random.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: arc4random.c,v 1.6 2004/10/08 15:18:26 hshoexer Exp $	*/
+/*	$OpenBSD: arc4random.c,v 1.7 2005/02/10 14:43:14 ho Exp $	*/
 
 /*
  * Arc4 random number generator for OpenBSD.
@@ -78,7 +78,7 @@ arc4_addrandom(struct arc4_stream *as, u_char *dat, int datlen)
 static void
 arc4_stir(struct arc4_stream *as)
 {
-	int     fd;
+	int     fd, i;
 	struct {
 		struct timeval tv;
 		u_int8_t rnd[128 - sizeof(struct timeval)];
