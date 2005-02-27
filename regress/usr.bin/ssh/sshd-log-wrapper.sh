6457f4aee51aabc2ccd71843d99f561ab860bdb9
dtucker <dtucker@openbsd.org>
2005-02-27 11:40:30 +0000

    Don't truncate logfile here either

--- regress/usr.bin/ssh/sshd-log-wrapper.sh
+++ regress/usr.bin/ssh/sshd-log-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#       $OpenBSD: sshd-log-wrapper.sh,v 1.1 2005/02/27 11:33:30 dtucker Exp $
+#       $OpenBSD: sshd-log-wrapper.sh,v 1.2 2005/02/27 11:40:30 dtucker Exp $
 #       Placed in the Public Domain.
 #
 # simple wrapper for sshd proxy mode to catch stderr output
@@ -10,4 +10,4 @@ log=$2
 shift
 shift
 
-exec $sshd $@ -e 2>$log
+exec $sshd $@ -e 2>>$log
