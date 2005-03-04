87443f3b0e7f5276a1349aee5022f5b520b03330
pedro <pedro@openbsd.org>
2005-03-04 11:43:37 +0000

    Fix handling of names bigger than NCHNAMLEN in cache_enter(), okay tedu@

--- sys/kern/vfs_cache.c
+++ sys/kern/vfs_cache.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_cache.c,v 1.13 2004/12/26 21:22:13 miod Exp $	*/
+/*	$OpenBSD: vfs_cache.c,v 1.14 2005/03/04 11:43:37 pedro Exp $	*/
 /*	$NetBSD: vfs_cache.c,v 1.13 1996/02/04 02:18:09 christos Exp $	*/
 
 /*
@@ -257,12 +257,9 @@ cache_enter(dvp, vp, cnp)
 	register struct namecache *ncp;
 	register struct nchashhead *ncpp;
 
-#ifdef DIAGNOSTIC
-	if (cnp->cn_namelen > NCHNAMLEN)
-		panic("cache_enter: name too long");
-#endif
-	if (!doingcache)
+	if (!doingcache || cnp->cn_namelen > NCHNAMLEN)
 		return;
+
 	/*
 	 * Free the cache slot at head of lru chain.
 	 */
