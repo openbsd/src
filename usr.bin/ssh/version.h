7650806c2a47edd83284b9495360fbd1303bd794
djm <djm@openbsd.org>
2005-03-08 23:49:48 +0000

    OpenSSH 4.0

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.42 2004/08/16 08:17:01 markus Exp $ */
+/* $OpenBSD: version.h,v 1.43 2005/03/08 23:49:48 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_3.9"
+#define SSH_VERSION	"OpenSSH_4.0"
