f89063b8d5d9ea3e3b75ac741123b5b088f49b8b
markus <markus@openbsd.org>
2005-03-11 14:59:06 +0000

    typo, missing \n; mpech

--- usr.bin/ssh/ssh-keygen.c
+++ usr.bin/ssh/ssh-keygen.c
@@ -12,7 +12,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-keygen.c,v 1.121 2005/03/10 22:01:06 deraadt Exp $");
+RCSID("$OpenBSD: ssh-keygen.c,v 1.122 2005/03/11 14:59:06 markus Exp $");
 
 #include <openssl/evp.h>
 #include <openssl/pem.h>
@@ -707,7 +707,7 @@ do_known_hosts(struct passwd *pw, const char *name)
 		    identity_file);
 		if (inplace) {
 			fprintf(stderr, "Not replacing existing known_hosts "
-			    "file beacuse of errors");
+			    "file because of errors\n");
 			fclose(out);
 			unlink(tmp);
 		}
