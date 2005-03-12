42d42a2b8d60b60c2c729cd59a3f57315d466d06
tom <tom@openbsd.org>
2005-03-12 21:52:02 +0000

    On return from real mode, reload the GDT using a 16-bit pointer rather than a 32-bit value.  Found by Tim Fletcher <tim (at) parrswood (dot) manchester (dot) sch (dot) uk> using Etherboot; thanks to Tim and the Etherboot developers who narrowed this down.
    Also bump the pxeboot version to 1.01.
    
    ok weingart@, "go ahead" deraadt@

--- sys/arch/i386/stand/pxeboot/conf.c
+++ sys/arch/i386/stand/pxeboot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.1 2004/03/19 13:48:19 tom Exp $	*/
+/*	$OpenBSD: conf.c,v 1.2 2005/03/12 21:52:02 tom Exp $	*/
 
 /*
  * Copyright (c) 2004 Tom Cosgrove
@@ -46,7 +46,7 @@
 #include "pxeboot.h"
 #include "pxe_net.h"
 
-const char version[] = "1.00";
+const char version[] = "1.01";
 int	debug = 1;
 
 #undef _TEST
