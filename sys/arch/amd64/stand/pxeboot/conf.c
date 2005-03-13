4336f4f9f21796d4d615a5ec6516290db70f5eab
tom <tom@openbsd.org>
2005-03-13 22:31:49 +0000

    Only use bangpxe calling on PXE 2.1 and above.  Makes no difference on current-spec PXE systems, but should allow some older ones to get a bit further.
    Diff from krw@.  Tested by krw@ on several PXE 2.1 cards.  Many thanks.
    
    ok krw@; "get these changes into the amd64" deraadt@

--- sys/arch/amd64/stand/pxeboot/conf.c
+++ sys/arch/amd64/stand/pxeboot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.2 2005/03/13 22:14:54 tom Exp $	*/
+/*	$OpenBSD: conf.c,v 1.3 2005/03/13 22:31:49 tom Exp $	*/
 
 /*
  * Copyright (c) 2004 Tom Cosgrove
@@ -45,7 +45,7 @@
 #include "pxeboot.h"
 #include "pxe_net.h"
 
-const char version[] = "1.01";
+const char version[] = "1.02";
 int	debug = 0;
 
 #undef _TEST
