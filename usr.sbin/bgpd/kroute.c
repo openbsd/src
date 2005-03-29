f04d882f4210284ec50c88b0d50a25fc0d3a3ad7
henning <henning@openbsd.org>
2005-03-29 11:20:01 +0000

    explicitely check interface flags for IFF_UP in kroute_validate

--- usr.sbin/bgpd/kroute.c
+++ usr.sbin/bgpd/kroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kroute.c,v 1.119 2005/03/28 14:40:04 henning Exp $ */
+/*	$OpenBSD: kroute.c,v 1.120 2005/03/29 11:20:01 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -762,8 +762,12 @@ kroute_validate(struct kroute *kr)
 		    kr->ifindex, inet_ntoa(kr->prefix),
 		    kr->prefixlen);
 		return (1);
-	} else
-		return (kif->k.link_state != LINK_STATE_DOWN);
+	}
+
+	if (!(kif->k.flags & IFF_UP))
+		return (0);
+
+	return (kif->k.link_state != LINK_STATE_DOWN);
 }
 
 void
