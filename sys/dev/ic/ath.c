241421de6c83260f4f5e3cb94190c4964ae8262e
uwe <uwe@openbsd.org>
2005-04-03 08:06:14 +0000

    remove redundant suser() checks

--- sys/dev/ic/ath.c
+++ sys/dev/ic/ath.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: ath.c,v 1.17 2005/04/02 22:09:31 uwe Exp $  */
+/*      $OpenBSD: ath.c,v 1.18 2005/04/03 08:06:14 uwe Exp $  */
 /*	$NetBSD: ath.c,v 1.37 2004/08/18 21:59:39 dyoung Exp $	*/
 
 /*-
@@ -1034,8 +1034,6 @@ ath_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 	s = splnet();
 	switch (cmd) {
 	case SIOCSIFADDR:
-		if ((error = suser(curproc, 0)) != 0)
-			break;
 		ifp->if_flags |= IFF_UP;
 #ifdef INET
 		if (ifa->ifa_addr->sa_family == AF_INET) {
@@ -1044,8 +1042,6 @@ ath_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 #endif  /* INET */
 		/* FALLTHROUGH */
 	case SIOCSIFFLAGS:
-		if ((error = suser(curproc, 0)) != 0)
-			break;
 		if (ifp->if_flags & IFF_UP) {
 			if (ifp->if_flags & IFF_RUNNING) {
 				/*
@@ -1072,8 +1068,6 @@ ath_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 		break;
 	case SIOCADDMULTI:
 	case SIOCDELMULTI:
-		if ((error = suser(curproc, 0)) != 0)
-			break;
 #ifdef __FreeBSD__
 		/*
 		 * The upper layer has already installed/removed
