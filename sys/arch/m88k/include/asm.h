45fb3ff3d54bf9d8773491dc8288a9ef4fa649bd
miod <miod@openbsd.org>
2005-04-30 16:46:49 +0000

    Move CALL() from <machine/asm.h> to eh.S which is the only user of the macro, and replace it with faster constructs in some cases; also drop the unused CALLP() macro.

--- sys/arch/m88k/include/asm.h
+++ sys/arch/m88k/include/asm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: asm.h,v 1.4 2005/04/27 21:12:44 miod Exp $	*/
+/*	$OpenBSD: asm.h,v 1.5 2005/04/30 16:46:49 miod Exp $	*/
 
 /*
  * Mach Operating System
@@ -165,22 +165,6 @@
 #define	NOP_STRING		"or	r0, r0, r0"
 
 #define RTE	NOP ; rte
-
-/*
- * Useful in some situations.
- */
-#define CALL(NAME, ARG1, ARG2) \
-	or	r2, r0, ARG1; \
-	bsr.n	NAME; \
-	 or	r3, r0, ARG2
-
-/* This define is similar to CALL, but accepts a function pointer XXX smurph */
-#define CALLP(NAME, ARG1, ARG2) \
-	or.u	r5, r0, hi16(NAME); \
-	ld	r4, r5, lo16(NAME); \
-	or	r2, r0, ARG1; \
-	jsr.n	r4; \
-	 or	r3, r0, ARG2
 
 /*
  * Fields in cr18. More bits are used privately in the exception handling
