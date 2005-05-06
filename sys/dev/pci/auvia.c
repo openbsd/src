27fced9b68db3984c1c0c810e2f0ab7dd42310b8
miod <miod@openbsd.org>
2005-05-06 01:45:22 +0000

    Remove debug printf which crept in; spotted by drahn@

--- sys/dev/pci/auvia.c
+++ sys/dev/pci/auvia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auvia.c,v 1.32 2005/04/16 21:57:23 mickey Exp $ */
+/*	$OpenBSD: auvia.c,v 1.33 2005/05/06 01:45:22 miod Exp $ */
 /*	$NetBSD: auvia.c,v 1.7 2000/11/15 21:06:33 jdolecek Exp $	*/
 
 /*-
@@ -843,7 +843,6 @@ auvia_build_dma_ops(struct auvia_softc *sc, struct auvia_softc_chan *ch,
 	dp = ch->sc_dma_ops_dma;
 	op = ch->sc_dma_ops;
 
-	printf("auvia: op = %p\n", op);
 	while (l) {
 		op->ptr = htole32(s);
 		l = l - min(l, blksize);
