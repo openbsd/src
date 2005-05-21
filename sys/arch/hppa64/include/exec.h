83d51fefd6ca3eb1fbed7f7e5913f446c24e93fb
mickey <mickey@openbsd.org>
2005-05-21 18:04:01 +0000

    elf64 of course!

--- sys/arch/hppa64/include/exec.h
+++ sys/arch/hppa64/include/exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.1 2005/04/01 10:40:48 mickey Exp $	*/
+/*	$OpenBSD: exec.h,v 1.2 2005/05/21 18:04:01 mickey Exp $	*/
 
 /* 
  * Copyright (c) 1994, The University of Utah and
@@ -39,7 +39,7 @@
 
 #define NATIVE_EXEC_ELF
 #define _NLIST_DO_ELF
-#define	_KERN_DO_ELF
+#define	_KERN_DO_ELF64
 
 /*
  * the following MD ELF values defenitions are from the:
