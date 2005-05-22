846c180e520ef4263bb1937acd0638c928410656
brad <brad@openbsd.org>
2005-05-22 04:38:54 +0000

    The printf(9) `%p' conversion specifier puts an "0x" in front of the pointer value.  Therefore, remove the "0x" from the format string.
    From FreeBSD
    
    ok millert@

--- sys/ntfs/ntfs_subr.c
+++ sys/ntfs/ntfs_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ntfs_subr.c,v 1.5 2005/05/21 18:05:58 brad Exp $	*/
+/*	$OpenBSD: ntfs_subr.c,v 1.6 2005/05/22 04:38:54 brad Exp $	*/
 /*	$NetBSD: ntfs_subr.c,v 1.4 2003/04/10 21:37:32 jdolecek Exp $	*/
 
 /*-
@@ -1276,7 +1276,7 @@ ntfs_ntreaddir(
 	blsize = fp->f_dirblsz;
 	rdbuf = fp->f_dirblbuf;
 
-	dprintf(("ntfs_ntreaddir: rdbuf: 0x%p, blsize: %d\n", rdbuf, blsize));
+	dprintf(("ntfs_ntreaddir: rdbuf: %p, blsize: %d\n", rdbuf, blsize));
 
 	if (vap->va_a_iroot->ir_flag & NTFS_IRFLAG_INDXALLOC) {
 		error = ntfs_ntvattrget(ntmp, ip, NTFS_A_INDXBITMAP, "$I30",
@@ -2050,7 +2050,7 @@ ntfs_runtocn(
 
 #if NTFS_DEBUG
 	int             i;
-	printf("ntfs_runtocn: run: 0x%p, %ld bytes, vcn:%ld\n",
+	printf("ntfs_runtocn: run: %p, %ld bytes, vcn:%ld\n",
 		run, len, (u_long) vcn);
 	printf("ntfs_runtocn: run: ");
 	for (i = 0; i < len; i++)
