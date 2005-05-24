8d0ee81c847073d846d3a205ce0810d972861305
henning <henning@openbsd.org>
2005-05-24 03:33:01 +0000

    'nother

--- usr.bin/mg/theo.c
+++ usr.bin/mg/theo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: theo.c,v 1.85 2005/05/22 23:40:16 todd Exp $	*/
+/*	$OpenBSD: theo.c,v 1.86 2005/05/24 03:33:01 henning Exp $	*/
 /*
  * Copyright (c) 2002 Artur Grabowski <art@openbsd.org>
  * All rights reserved.
@@ -174,7 +174,8 @@ static const char *talk[] = {
 	"they are manual pages, not tea time chit-chats",
 	"Whiners.  They scale really well.",
 	"in your world, you would have a checklist of 50 fucking workarounds just to make a coffee.",
-	"for once, I have nothing to say."
+	"for once, I have nothing to say.",
+	"You have no idea how fucked we are"
 };
 
 static const int ntalk = sizeof(talk)/sizeof(talk[0]);
