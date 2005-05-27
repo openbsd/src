6a8224c183281ebdb4dd64cde24dbdb8d71a09af
miod <miod@openbsd.org>
2005-05-27 21:29:00 +0000

    Kill __HAVE_NWSCONS harder.

--- sys/arch/zaurus/include/types.h
+++ sys/arch/zaurus/include/types.h
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: types.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
+/* 	$OpenBSD: types.h,v 1.2 2005/05/27 21:29:00 miod Exp $	*/
 /*	$NetBSD: types.h,v 1.4 2002/02/28 03:17:26 simonb Exp $	*/
 
 #ifndef _ARM32_TYPES_H_
@@ -6,6 +6,5 @@
 
 #include <arm/types.h>
 #define __HAVE_GENERIC_SOFT_INTERRUPTS
-#define __HAVE_NWSCONS
 
 #endif
