6c15d9987a9923496dc5f7578dd736e5cb773c52
markus <markus@openbsd.org>
2005-06-10 15:56:01 +0000

    make sure the IP packet contains a full struct ip_timestamp from art; ok deraadt, claudio, henning

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_input.c,v 1.127 2005/05/24 04:20:25 markus Exp $	*/
+/*	$OpenBSD: ip_input.c,v 1.128 2005/06/10 15:56:01 markus Exp $	*/
 /*	$NetBSD: ip_input.c,v 1.30 1996/03/16 23:53:58 christos Exp $	*/
 
 /*
@@ -1122,6 +1122,8 @@ ip_dooptions(m)
 
 		case IPOPT_TS:
 			code = cp - (u_char *)ip;
+			if (optlen < sizeof(struct ip_timestamp))
+				goto bad;
 			bcopy(cp, &ipt, sizeof(struct ip_timestamp));
 			if (ipt.ipt_ptr < 5 || ipt.ipt_len < 5)
 				goto bad;
