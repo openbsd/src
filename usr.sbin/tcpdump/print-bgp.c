9cb0e842078a769a228f588c87fe1d484f78af83
moritz <moritz@openbsd.org>
2005-06-14 17:57:21 +0000

    add missing error check for decode_prefix4. ok deraadt@

--- usr.sbin/tcpdump/print-bgp.c
+++ usr.sbin/tcpdump/print-bgp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-bgp.c,v 1.5 2005/04/27 23:03:01 cloder Exp $	*/
+/*	$OpenBSD: print-bgp.c,v 1.6 2005/06/14 17:57:21 moritz Exp $	*/
 
 /*
  * Copyright (C) 1999 WIDE Project.
@@ -609,6 +609,7 @@ bgp_update_print(const u_char *dat, int length)
 		printf(" (Withdrawn routes: %d bytes)", len);
 #else	
 		char buf[MAXHOSTNAMELEN + 100];
+		int wpfx;
 
 		TCHECK2(p[2], len);
  		i = 2;
@@ -616,7 +617,12 @@ bgp_update_print(const u_char *dat, int length)
 		printf(" (Withdrawn routes:");
 			
 		while(i < 2 + len) {
-			i += decode_prefix4(&p[i], buf, sizeof(buf));
+			wpfx = decode_prefix4(&p[i], buf, sizeof(buf));
+			if (wpfx < 0) {
+				printf(" (illegal prefix length)");
+				break;
+			}
+			i += wpfx;
 			printf(" %s", buf);
 		}
 		printf(")\n");
