2f36b2e5ab7fb2f084b0da04c3464312af011487
mickey <mickey@openbsd.org>
2005-06-23 14:26:37 +0000

    iface description is not an array of ptr but rather just a string; jcs@ ok

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.145 2005/06/12 00:42:55 henning Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.146 2005/06/23 14:26:37 mickey Exp $	*/
 /*	$NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $	*/
 
 /*
@@ -2120,7 +2120,7 @@ status(int link, struct sockaddr_dl *sdl)
 	struct ifmediareq ifmr;
 	struct ifreq ifrdesc;
 	int *media_list, i;
-	char *ifdescr[IFDESCRSIZE];
+	char ifdescr[IFDESCRSIZE];
 
 	printf("%s: ", name);
 	printb("flags", flags, IFFBITS);
