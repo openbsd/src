9e49c7e5060fb15e6d91bf8045538c082be710e9
jmc <jmc@openbsd.org>
2005-07-06 21:45:44 +0000

    alter spacing of usage() so we don;t get a line break if skey is invoked as otp-rmd160;

--- usr.bin/skey/skey.c
+++ usr.bin/skey/skey.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: skey.c,v 1.22 2005/07/06 21:39:31 jmc Exp $	*/
+/*	$OpenBSD: skey.c,v 1.23 2005/07/06 21:45:44 jmc Exp $	*/
 /*
  * OpenBSD S/Key (skey.c)
  *
@@ -150,7 +150,7 @@ void
 usage(char *s)
 {
 	fprintf(stderr,
-	    "Usage: %s [-x] [-md4 | -md5 | -rmd160 | -sha1] [-n count] "
-	    "[-p passphrase]\n\t<sequence#>[/] key\n", s);
+	    "Usage: %s [-x] [-md4 | -md5 | -rmd160 | -sha1] [-n count]\n\t"
+	    "[-p passphrase] <sequence#>[/] key\n", s);
 	exit(1);
 }
