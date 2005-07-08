3246820a62f1506c2a8181a6ec38a705d3f62473
marc <marc@openbsd.org>
2005-07-08 02:26:07 +0000

    Fix mismatched brace and compile when NWSMUX and NWSDISPLAY not defined. OK uwe@

--- sys/dev/wscons/wskbd.c
+++ sys/dev/wscons/wskbd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: wskbd.c,v 1.44 2005/06/02 07:31:17 miod Exp $ */
+/* $OpenBSD: wskbd.c,v 1.45 2005/07/08 02:26:07 marc Exp $ */
 /* $NetBSD: wskbd.c,v 1.80 2005/05/04 01:52:16 augustss Exp $ */
 
 /*
@@ -120,9 +120,7 @@ int	wskbddebug = 0;
 #define DPRINTF(x)
 #endif
 
-#if NWSMUX > 0 || NWSDISPLAY > 0
 #include <dev/wscons/wsmuxvar.h>
-#endif
 
 struct wskbd_internal {
 	const struct wskbd_mapdata *t_keymap;
@@ -1470,8 +1468,8 @@ internal_command(struct wskbd_softc *sc, u_int *type, keysym_t ksym,
 		    ksym == KS_Cmd_ContrastDown ? -1 : 1,
 		    ksym == KS_Cmd_ContrastRotate ? 1 : 0);
 		return (1);
-#endif
 	}
+#endif
 	return (0);
 }
 
