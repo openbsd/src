f5f56308f8099829217081d22a9180125276747c
otto <otto@openbsd.org>
2005-07-13 07:08:00 +0000

    Test for fp save/restore in sig handlers.

---
+++ regress/lib/libm/fpsig/fpsig.c
@@ -0,0 +1,54 @@
+/*	$OpenBSD: fpsig.c,v 1.1 2005/07/13 07:08:00 otto Exp $	*/
+
+/*
+ * Public domain.  2005, Otto Moerbeek
+ *
+ * Try to check if fp registers are properly saved and restored while
+ * calling a signal hander.  This is not supposed to catch all that
+ * can go wrong, but trashed fp regsiters will typically get caught.
+ */
+ 
+#include <err.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define LIMIT	10.0
+
+volatile sig_atomic_t count;
+
+double g1;
+double g2;
+
+void
+handler(int signo)
+{
+	double a, b, c = 0.0;
+
+	if (signo)
+		alarm(1);
+	for (a = 0.0; a < LIMIT; a++)
+		for (b = 0.0; b < LIMIT; b++)
+			c += a * a + b * b;
+
+	if (signo) {
+		g1 = c;
+		count++;
+	} else
+		g2 = c;
+}
+
+int
+main()
+{
+	signal(SIGALRM, handler);
+	/* initialize global vars */
+	handler(0);
+	handler(1);
+	
+	while (count < 10) {
+		handler(0);
+		if (g1 != g2)
+			errx(1, "%f %f", g1, g2);
+	}
+	return (0);
+}
