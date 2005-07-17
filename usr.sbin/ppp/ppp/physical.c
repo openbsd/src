224036de907ea939dbca68f1fad7109f3a60b7bf
brad <brad@openbsd.org>
2005-07-17 20:10:53 +0000

    rev 1.55
    Report the number of bytes not written when complaining about failed writes
    
    From brian FreeBSD

--- usr.sbin/ppp/ppp/physical.c
+++ usr.sbin/ppp/ppp/physical.c
@@ -16,7 +16,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *  $OpenBSD: physical.c,v 1.39 2005/07/17 20:08:55 brad Exp $
+ *  $OpenBSD: physical.c,v 1.40 2005/07/17 20:10:53 brad Exp $
  *
  */
 
@@ -427,8 +427,8 @@ physical_DescriptorWrite(struct fdescriptor *d, struct bundle *bundle,
       if (errno == EAGAIN)
         result = 1;
       else if (errno != ENOBUFS) {
-	log_Printf(LogPHASE, "%s: write (%d): %s\n", p->link.name,
-                   p->fd, strerror(errno));
+	log_Printf(LogPHASE, "%s: write (fd %d, len %d): %s\n", p->link.name,
+                   p->fd, p->out->m_len, strerror(errno));
         datalink_Down(p->dl, CLOSE_NORMAL);
       }
     }
