7f25256fe5ca7d84dd57695123802f9ab67c8c9e
jmc <jmc@openbsd.org>
2005-07-18 14:38:39 +0000

    fix spelling mistake (in comment); from ray lai;

--- lib/libc/net/getaddrinfo.c
+++ lib/libc/net/getaddrinfo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getaddrinfo.c,v 1.52 2005/03/30 02:58:28 tedu Exp $	*/
+/*	$OpenBSD: getaddrinfo.c,v 1.53 2005/07/18 14:38:39 jmc Exp $	*/
 /*	$KAME: getaddrinfo.c,v 1.31 2000/08/31 17:36:43 itojun Exp $	*/
 
 /*
@@ -425,7 +425,7 @@ getaddrinfo(const char *hostname, const char *servname,
 
 	/*
 	 * XXX
-	 * If numreic representation of AF1 can be interpreted as FQDN
+	 * If numeric representation of AF1 can be interpreted as FQDN
 	 * representation of AF2, we need to think again about the code below.
 	 */
 	if (sentinel.ai_next)
