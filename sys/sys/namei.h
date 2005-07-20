69d2eb38f02e66537a9ebac8af3b6d2bc64b3d84
miod <miod@openbsd.org>
2005-07-20 17:01:14 +0000

    Typo in kern.nchstats sysctl names.

--- sys/sys/namei.h
+++ sys/sys/namei.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: namei.h,v 1.15 2005/06/18 18:09:43 millert Exp $	*/
+/*	$OpenBSD: namei.h,v 1.16 2005/07/20 17:01:14 miod Exp $	*/
 /*	$NetBSD: namei.h,v 1.11 1996/02/09 18:25:20 christos Exp $	*/
 
 /*
@@ -227,6 +227,6 @@ struct	nchstats {
 	{ "pass2", CTLTYPE_INT },		\
 	{ "2passes", CTLTYPE_INT },		\
 	{ "ncs_revhits", CTLTYPE_INT },		\
-	{ "nfs_revmiss", CTLTYPE_INT },		\
+	{ "ncs_revmiss", CTLTYPE_INT },		\
 }
 #endif /* !_SYS_NAMEI_H_ */
