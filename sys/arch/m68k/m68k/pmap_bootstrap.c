e1f0c379399414eca7ac879dd3ccbcd453a9b7e2
miod <miod@openbsd.org>
2005-08-01 12:06:39 +0000

    Thinko in 68060-only #ifdef line.

--- sys/arch/m68k/m68k/pmap_bootstrap.c
+++ sys/arch/m68k/m68k/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap_bootstrap.c,v 1.11 2005/01/14 19:57:48 miod Exp $	*/
+/*	$OpenBSD: pmap_bootstrap.c,v 1.12 2005/08/01 12:06:39 miod Exp $	*/
 
 /* 
  * Copyright (c) 1995 Theo de Raadt
@@ -391,7 +391,7 @@ pmap_bootstrap(nextpa, firstpa)
 	 */
 	if (RELOC(mmutype, int) == MMU_68040)
 		protopte |= PG_CCB;
-#ifdef CPU_68060
+#ifdef M68060
 	else if (RELOC(mmutype, int) == MMU_68060)
 		protopte |= PG_CWT;
 #endif
