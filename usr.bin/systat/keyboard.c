0dd7619152ced362a4c95a442e3c6514f2f98cef
millert <millert@openbsd.org>
2005-08-01 22:51:50 +0000

    Send ourselves SIGTSTP, not SIGSTOP on ^Z.  Noticed and OK deraadt@

--- usr.bin/systat/keyboard.c
+++ usr.bin/systat/keyboard.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: keyboard.c,v 1.16 2004/07/11 05:24:56 pvalchev Exp $	*/
+/*	$OpenBSD: keyboard.c,v 1.17 2005/08/01 22:51:50 millert Exp $	*/
 /*	$NetBSD: keyboard.c,v 1.2 1995/01/20 08:51:59 jtc Exp $	*/
 
 /*-
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)keyboard.c	8.1 (Berkeley) 6/6/93";
 #endif
-static char rcsid[] = "$OpenBSD: keyboard.c,v 1.16 2004/07/11 05:24:56 pvalchev Exp $";
+static char rcsid[] = "$OpenBSD: keyboard.c,v 1.17 2005/08/01 22:51:50 millert Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -61,7 +61,10 @@ keyboard(void)
 		do {
 			if (gottstp) {
 				endwin();
-				kill(getpid(), SIGSTOP);
+				signal(SIGTSTP, SIG_DFL);
+				kill(getpid(), SIGTSTP);
+				signal(SIGTSTP, sigtstp);
+				siginterrupt(SIGTSTP, 1);
 				gotwinch = 1;
 				gottstp = 0;
 			}
