b6bea444c687008314f7d3a4d79402e6aee5d775
moritz <moritz@openbsd.org>
2005-08-02 18:26:49 +0000

    add missing break; ok henning@

--- sbin/dhclient/conflex.c
+++ sbin/dhclient/conflex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conflex.c,v 1.9 2005/07/26 19:14:57 krw Exp $	*/
+/*	$OpenBSD: conflex.c,v 1.10 2005/08/02 18:26:49 moritz Exp $	*/
 
 /* Lexical scanner for dhcpd config file... */
 
@@ -338,6 +338,7 @@ intern(char *atom, int dfv)
 			return (BOOTING);
 		if (!strcasecmp(atom + 1, "oot-unknown-clients"))
 			return (BOOT_UNKNOWN_CLIENTS);
+		break;
 	case 'c':
 		if (!strcasecmp(atom + 1, "lass"))
 			return (CLASS);
