0bf9cfc2c24a6c6aa5c724d5eae30c8380a234e9
deraadt <deraadt@openbsd.org>
2005-08-04 16:00:06 +0000

    HW_PHYSMEM and HW_USERMEM should be unsigned; ok tdeval marco

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysctl.c,v 1.126 2005/08/04 13:00:44 jsg Exp $	*/
+/*	$OpenBSD: sysctl.c,v 1.127 2005/08/04 16:00:06 deraadt Exp $	*/
 /*	$NetBSD: sysctl.c,v 1.9 1995/09/30 07:12:50 thorpej Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ static const char copyright[] =
 #if 0
 static const char sccsid[] = "@(#)sysctl.c	8.5 (Berkeley) 5/9/95";
 #else
-static const char rcsid[] = "$OpenBSD: sysctl.c,v 1.126 2005/08/04 13:00:44 jsg Exp $";
+static const char rcsid[] = "$OpenBSD: sysctl.c,v 1.127 2005/08/04 16:00:06 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -474,6 +474,10 @@ parse(char *string, int flags)
 			len = sysctl_sensors(string, &bufp, mib, flags, &type);
 			if (len < 0)
 				return;
+			break;
+		case HW_PHYSMEM:
+		case HW_USERMEM:
+			special |= UNSIGNED;
 			break;
 		}
 		break;
