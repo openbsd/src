53aa784a41578b06c923d9b3a5590acbd3bd0b73
miod <miod@openbsd.org>
2005-08-07 07:29:44 +0000

    Remove advertising clause from UCB licenses; ok deraad@

--- sys/arch/mips64/include/param.h
+++ sys/arch/mips64/include/param.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: param.h,v 1.7 2005/07/31 15:31:15 miod Exp $ */
+/*      $OpenBSD: param.h,v 1.8 2005/08/07 07:29:44 miod Exp $ */
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -17,11 +17,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
--- sys/arch/mips64/include/pmap.h
+++ sys/arch/mips64/include/pmap.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: pmap.h,v 1.6 2004/09/27 17:40:24 pefo Exp $ */
+/*      $OpenBSD: pmap.h,v 1.7 2005/08/07 07:29:44 miod Exp $ */
 
 /*
  * Copyright (c) 1987 Carnegie-Mellon University
@@ -16,11 +16,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
