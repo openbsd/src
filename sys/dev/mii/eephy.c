18a6b78f7dd08f9fb82a7fb8647d371290e1ea19
jsg <jsg@openbsd.org>
2005-08-14 01:33:18 +0000

    Correct one of the unknown marvell phys which is actually 88E1111-RCJ. ok brad@

--- sys/dev/mii/eephy.c
+++ sys/dev/mii/eephy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eephy.c,v 1.20 2005/08/02 00:21:32 jsg Exp $	*/
+/*	$OpenBSD: eephy.c,v 1.21 2005/08/14 01:35:09 jsg Exp $	*/
 /*
  * Principal Author: Parag Patel
  * Copyright (c) 2001
@@ -97,8 +97,8 @@ static const struct mii_phydesc eephys[] = {
 	  MII_STR_MARVELL_E1000_5 },
 	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1000_6,
 	  MII_STR_MARVELL_E1000_6 },
-	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1000_7,
-	  MII_STR_MARVELL_E1000_7 },
+	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1111RCJ,
+	  MII_STR_MARVELL_E1111RCJ },
 
 	{ 0,				0,
 	  NULL },
