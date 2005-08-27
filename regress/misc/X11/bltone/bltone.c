f2618c1bf25121732559a628446be09614fcd281
deraadt <deraadt@openbsd.org>
2005-08-27 06:50:03 +0000

    backout unapproved diff

--- regress/misc/X11/bltone/bltone.c
+++ regress/misc/X11/bltone/bltone.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bltone.c,v 1.2 2005/08/25 19:41:23 kettenis Exp $	*/
+/*	$OpenBSD: bltone.c,v 1.3 2005/08/27 06:50:03 deraadt Exp $	*/
 /*
  *	Written by Mark Kettenis <kettenis@openbsd.org> 2004 Public Domain
  */
@@ -13,45 +13,45 @@ typedef unsigned FbStip;
 typedef unsigned FbBits;
 typedef int FbStride;
 
-extern void fbBltOne (FbStip *, FbStride, int, FbBits *, FbStride, int, int,
-		      int, int, FbBits, FbBits, FbBits, FbBits);
+extern void fbBltOne24 (FbStip *, FbStride, int, FbBits *, FbStride, int, int,
+			int, int, FbBits, FbBits, FbBits, FbBits);
 
 FbStip mask[] = { 0x77ff7700, 0x11335577 };
 
 int
 main (void)
 {
-	int pagesize;
-	FbStip *src;
-	FbBits *dst;
-	int srcX, dstX;
-	int dstBpp;
+  int pagesize;
+  FbStip *src;
+  FbBits *dst;
+  int srcX, dstX;
+  int dstBpp;
 
-	pagesize = getpagesize();
+  pagesize = getpagesize();
 
-	src = mmap(NULL, 2 * pagesize, PROT_READ|PROT_WRITE, MAP_ANON, -1, 0);
-	assert(src);
+  src = mmap(NULL, 2 * pagesize, PROT_READ|PROT_WRITE, MAP_ANON, -1, 0);
+  assert (src);
 
-	dst = mmap(NULL, 2 * pagesize, PROT_READ|PROT_WRITE, MAP_ANON, -1, 0);
-	assert(dst);
+  dst = mmap(NULL, 2 * pagesize, PROT_READ|PROT_WRITE, MAP_ANON, -1, 0);
+  assert (dst);
 
-	mprotect((char *)src + pagesize, pagesize, PROT_NONE);
-	src = (FbStip *)((char *)src + (pagesize - sizeof mask));
-	memcpy (src, mask, sizeof mask);
+  mprotect((char *)src + pagesize, pagesize, PROT_NONE);
+  src = (FbStip *)((char *)src + (pagesize - sizeof mask));
+  memcpy (src, mask, sizeof mask);
 
-	for (dstBpp = 8; dstBpp <= 32; dstBpp += 8)
-		for (dstX = 0; dstX < 64; dstX += 8)
-			for (srcX = 0; srcX < 32; srcX++)
-				fbBltOne(src, 1, srcX, dst, 128, dstX, dstBpp,
-				    (32 - srcX) * dstBpp, 2,
-				    0, 0, 0xffffffff, 0);
+  for (dstBpp = 8; dstBpp <= 32; dstBpp += 8)
+    for (dstX = 0; dstX < 64; dstX += 8)
+      for (srcX = 0; srcX < 32; srcX++)
+	fbBltOne(src, 1, srcX, dst, 128, dstX, dstBpp,
+		 (32 - srcX) * dstBpp, 2,
+		 0, 0, 0xffffffff, 0);
 
-	for (dstBpp = 8; dstBpp <= 32; dstBpp += 8)
-		for (dstX = 0; dstX < 64; dstX += 8)
-			for (srcX = 0; srcX < 32; srcX++)
-				fbBltOne(src, 1, srcX, dst, 128, dstX, dstBpp,
-				    (64 - srcX) * dstBpp, 1,
-				    0, 0, 0xffffffff, 0);
+  for (dstBpp = 8; dstBpp <= 32; dstBpp += 8)
+    for (dstX = 0; dstX < 64; dstX += 8)
+      for (srcX = 0; srcX < 32; srcX++)
+	fbBltOne(src, 1, srcX, dst, 128, dstX, dstBpp,
+		 (64 - srcX) * dstBpp, 1,
+		 0, 0, 0xffffffff, 0);
 
   return 0;
 }
