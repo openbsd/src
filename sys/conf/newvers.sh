a56e0144d642209033f41f8605e2982faf176a04
miod <miod@openbsd.org>
2005-09-05 20:43:25 +0000

    On the road again.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.71 2005/08/27 16:47:04 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.72 2005/09/05 20:43:25 miod Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -67,10 +67,10 @@ ost="OpenBSD"
 osr="3.8"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-current"
 #if 0
 #define STATUS "-beta"
-#define STATUS "-current"
+#define STATUS ""			/* release */
 #endif
 
 const char ostype[] = "${ost}";
