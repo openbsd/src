600c703d9f16cf7af01095bda461b47a4dda291d
pedro <pedro@openbsd.org>
2005-09-09 15:48:43 +0000

    typos

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.46 2005/05/24 21:11:47 tedu Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.47 2005/09/09 15:48:43 pedro Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
@@ -850,7 +850,7 @@ uvm_km_alloc_poolpage1(map, obj, waitok)
 	int s;
 
 	/*
-	 * NOTE: We may be called with a map that doens't require splvm
+	 * NOTE: We may be called with a map that doesn't require splvm
 	 * protection (e.g. kernel_map).  However, it does not hurt to
 	 * go to splvm in this case (since unprocted maps will never be
 	 * accessed in interrupt context).
@@ -884,7 +884,7 @@ uvm_km_free_poolpage1(map, addr)
 	int s;
 
 	/*
-	 * NOTE: We may be called with a map that doens't require splvm
+	 * NOTE: We may be called with a map that doesn't require splvm
 	 * protection (e.g. kernel_map).  However, it does not hurt to
 	 * go to splvm in this case (since unprocted maps will never be
 	 * accessed in interrupt context).
