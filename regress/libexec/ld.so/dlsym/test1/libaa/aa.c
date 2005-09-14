71a6ea9eebc0767baa85069db308afeaf966b532
kurt <kurt@openbsd.org>
2005-09-14 15:59:37 +0000

    Add a regress test for dlsym special handles. Checks that duplicate symbols are handled right for a simple case.

---
+++ regress/libexec/ld.so/dlsym/test1/libaa/aa.c
@@ -0,0 +1,84 @@
+/*	$OpenBSD: aa.c,v 1.1.1.1 2005/09/14 15:59:37 kurt Exp $	*/
+
+/*
+ * Copyright (c) 2005 Kurt Miller <kurt@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include "aa.h"
+
+void
+sigprocmask() {
+}
+
+/*
+ * aaTest verifies dlsym works as expected with a simple case of duplicate
+ * symbols. prog1, libaa and libc all have the sigprocmask symbol and are
+ * linked with prog1 with libaa before libc. Depending on how dlsym is called
+ * the symbol for sigprocmask should come from prog1 libaa or libc.
+ */
+int
+aaTest()
+{
+	int ret = 0;
+	void *value;
+	void *libaa_sigprocmask = dlsym(dlopen("libaa.so", RTLD_LAZY), "sigprocmask");
+	void *libc_sigprocmask = dlsym(dlopen("libc.so", RTLD_LAZY), "sigprocmask");
+
+	printf("sigprocmask       == %p\n", &sigprocmask);
+	printf("libaa_sigprocmask == %p\n", libaa_sigprocmask);
+	printf("libc_sigprocmask  == %p\n", libc_sigprocmask);
+
+	/* basic sanity check */
+	if (libaa_sigprocmask == &sigprocmask || libc_sigprocmask == &sigprocmask ||
+	    libc_sigprocmask == libaa_sigprocmask || libaa_sigprocmask == NULL ||
+	    libc_sigprocmask == NULL) {
+		printf("dlsym(handle, ...)\n FAILED\n");
+		return (-1);
+	}
+
+	value = dlsym(RTLD_DEFAULT, "sigprocmask");
+	if (value != &sigprocmask) {
+		printf("dlsym(RTLD_DEFAULT, \"sigprocmask\") == %p FAILED\n", value);
+		printf("\twas expecting == %p (&sigprocmask)\n", &sigprocmask);
+		ret = -1;
+	}
+
+	value = dlsym(RTLD_SELF, "sigprocmask");
+	if (value != libaa_sigprocmask) {
+		printf("dlsym(RTLD_SELF, \"sigprocmask\") == %p FAILED\n", value);
+		printf("\twas expecting == %p (libaa_sigprocmask)\n", libaa_sigprocmask);
+		printf("FAILED\n");
+		ret = -1;
+	}
+
+	value = dlsym(RTLD_NEXT, "sigprocmask");
+	if (value != libc_sigprocmask) {
+		printf("dlsym(RTLD_NEXT, \"sigprocmask\") == %p FAILED\n", value);
+		printf("\twas expecting == %p (libc_sigprocmask)\n", libc_sigprocmask);
+		ret = -1;
+	}
+
+	value = dlsym(NULL, "sigprocmask");
+	if (value != libaa_sigprocmask) {
+		printf("dlsym(NULL, \"sigprocmask\") == %p FAILED\n", value);
+		printf("\twas expecting == %p (libaa_sigprocmask)\n", libaa_sigprocmask);
+		ret = -1;
+	}
+
+	return (ret);
+}
