dca5c156df6c5836470966b370c9dec3d0f5b804
joris <joris@openbsd.org>
2005-09-20 05:03:02 +0000

    missing 'ident' command in the programs table.

--- usr.bin/rcs/rcsprog.c
+++ usr.bin/rcs/rcsprog.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsprog.c,v 1.6 2005/09/20 05:01:31 joris Exp $	*/
+/*	$OpenBSD: rcsprog.c,v 1.7 2005/09/20 05:03:02 joris Exp $	*/
 /*
  * Copyright (c) 2005 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -60,6 +60,7 @@ struct rcs_prog {
 	{ "co",		NULL,		NULL		},
 	{ "rcsclean",	NULL,		NULL		},
 	{ "rcsdiff",	NULL,		NULL		},
+	{ "ident",	NULL,		NULL		},
 };
 
 
