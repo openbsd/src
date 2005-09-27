d07f48d1fbe1d01a9098d0be09f924e3f48c6349
dlg <dlg@openbsd.org>
2005-09-27 23:40:07 +0000

    add a -z option that allows you to split records on the nul character ('\0'). useful in combination with things like "find . -print0" and "xargs -0". for dewi, a chump.
    ok deraadt@

--- usr.bin/sort/sort.c
+++ usr.bin/sort/sort.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sort.c,v 1.25 2005/04/11 21:04:48 moritz Exp $	*/
+/*	$OpenBSD: sort.c,v 1.26 2005/09/27 23:40:07 dlg Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -42,7 +42,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)sort.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: sort.c,v 1.25 2005/04/11 21:04:48 moritz Exp $";
+static char rcsid[] = "$OpenBSD: sort.c,v 1.26 2005/09/27 23:40:07 dlg Exp $";
 #endif
 #endif /* not lint */
 
@@ -125,7 +125,7 @@ main(int argc, char *argv[])
 	fixit(&argc, argv);
 	if (!issetugid() && (outfile = getenv("TMPDIR")))
 		tmpdir = outfile;
-	while ((ch = getopt(argc, argv, "bcdfik:mHno:rR:t:T:uy:")) != -1) {
+	while ((ch = getopt(argc, argv, "bcdfik:mHno:rR:t:T:uy:z")) != -1) {
 		switch (ch) {
 		case 'b': fldtab->flags |= BI | BT;
 			break;
@@ -184,6 +184,13 @@ main(int argc, char *argv[])
 			break;
 		case 'y':
 			/* accept -y for backwards compat. */
+			break;
+		case 'z':
+			if (REC_D != '\n')
+				usage("multiple record delimiters");
+			REC_D = '\0';
+			d_mask['\n'] = d_mask[' '];
+			d_mask[REC_D] = REC_D_F;
 			break;
 		case '?':
 		default:
@@ -328,7 +335,7 @@ usage(char *msg)
 
 	if (msg != NULL)
 		warnx("%s", msg);
-	(void)fprintf(stderr, "usage: %s [-T dir] [-o output] [-cmubdfinrH] "
+	(void)fprintf(stderr, "usage: %s [-T dir] [-o output] [-cmubdfinrHz] "
 	    "[-t char] [-R char] [-k keydef] ... [files]\n", __progname);
 	exit(2);
 }
