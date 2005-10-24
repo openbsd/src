c669c3dc81234f09195d6e72fd1b29eaf38afe9b
henning <henning@openbsd.org>
2005-10-24 16:02:40 +0000

    don't touch via 8237 for now, until it is fixed. what mickey committed is obviosuly NOT what I tested, since that worked.

--- sys/arch/i386/pci/pci_intr_fixup.c
+++ sys/arch/i386/pci/pci_intr_fixup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_intr_fixup.c,v 1.43 2005/10/20 15:12:27 mickey Exp $	*/
+/*	$OpenBSD: pci_intr_fixup.c,v 1.44 2005/10/24 16:02:40 henning Exp $	*/
 /*	$NetBSD: pci_intr_fixup.c,v 1.10 2000/08/10 21:18:27 soda Exp $	*/
 
 /*
@@ -181,8 +181,6 @@ const struct pciintr_icu_table {
 	  via82c586_init },
 
 	{ PCI_VENDOR_VIATECH,	PCI_PRODUCT_VIATECH_VT8231_ISA,
-	  via8231_init },
-	{ PCI_VENDOR_VIATECH,	PCI_PRODUCT_VIATECH_VT8237_ISA,
 	  via8231_init },
 
 	{ PCI_VENDOR_SIS,	PCI_PRODUCT_SIS_85C503,
