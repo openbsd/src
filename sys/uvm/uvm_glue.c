c0dd0e791fd278d504b8e3bd18904b14c66c6c02
aaron <aaron@openbsd.org>
2005-11-01 14:18:14 +0000

    In uvm_swapout(), protect "p->p_swtime = 0;" with SCHED_LOCK() as is already done in uvm_swapin().  Looks like this was a mistake made while editing.  No response from art@.  deraadt@, miod@, pedro@ ok

--- sys/uvm/uvm_glue.c
+++ sys/uvm/uvm_glue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_glue.c,v 1.42 2005/05/31 11:35:33 art Exp $	*/
+/*	$OpenBSD: uvm_glue.c,v 1.43 2005/11/01 14:18:14 aaron Exp $	*/
 /*	$NetBSD: uvm_glue.c,v 1.44 2001/02/06 19:54:44 eeh Exp $	*/
 
 /* 
@@ -586,8 +586,8 @@ uvm_swapout(p)
 	p->p_flag &= ~P_INMEM;
 	if (p->p_stat == SRUN)
 		remrunqueue(p);
-	SCHED_UNLOCK(s);
 	p->p_swtime = 0;
+	SCHED_UNLOCK(s);
 	++uvmexp.swapouts;
 
 	/*
