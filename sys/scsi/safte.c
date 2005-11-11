1122f506b592629a0ddc452c4a2f76671b4c6c25
fgsch <fgsch@openbsd.org>
2005-11-11 01:19:47 +0000

    include sys/device.h. builds on sparc. dlg@ ok.

--- sys/scsi/safte.c
+++ sys/scsi/safte.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: safte.c,v 1.19 2005/11/10 22:27:03 dlg Exp $ */
+/*	$OpenBSD: safte.c,v 1.20 2005/11/11 01:19:47 fgsch Exp $ */
 
 /*
  * Copyright (c) 2005 David Gwynne <dlg@openbsd.org>
@@ -20,6 +20,7 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/device.h>
 #include <sys/scsiio.h>
 #include <sys/malloc.h>
 #include <sys/queue.h>
