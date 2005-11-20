4fe3ad25295a14717e27a74ba8be995efa718634
kjell <kjell@openbsd.org>
2005-11-20 04:17:25 +0000

    Don't bind help to a null function

--- usr.bin/mg/funmap.c
+++ usr.bin/mg/funmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: funmap.c,v 1.19 2005/11/20 04:16:34 kjell Exp $	*/
+/*	$OpenBSD: funmap.c,v 1.20 2005/11/20 04:17:25 kjell Exp $	*/
 /*
  * Copyright (c) 2001 Artur Grabowski <art@openbsd.org>.  All rights reserved.
  *
@@ -115,7 +115,6 @@ static struct funmap functnames[] = {
 	{unbindtokey, "global-unset-key",},
 	{gotoline, "goto-line",},
 #ifndef NO_HELP
-	{NULL, "help",},
 	{help_help, "help-help",},
 #endif /* !NO_HELP */
 	{insert, "insert",},
