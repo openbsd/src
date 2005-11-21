d27e60bc03cbfc5dea511d1829e74c8f94e6b928
cloder <cloder@openbsd.org>
2005-11-21 19:37:11 +0000

    Place in public domain

--- regress/usr.bin/xlint/test-1.c
+++ regress/usr.bin/xlint/test-1.c
@@ -1,7 +1,7 @@
-/*      $OpenBSD: test-1.c,v 1.1 2005/11/21 18:25:57 cloder Exp $ */
+/*      $OpenBSD: test-1.c,v 1.2 2005/11/21 19:37:11 cloder Exp $ */
 
 /*
- * This program is in the public domain.
+ * Placed in the public domain by Chad Loder <cloder@openbsd.org>.
  *
  * Test the ARGSUSED feature of lint.
  */
