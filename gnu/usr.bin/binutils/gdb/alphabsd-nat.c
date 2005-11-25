9e9f261f3fbb7984a90ffd08dba5f469520a3396
kettenis <kettenis@openbsd.org>
2005-11-25 18:27:26 +0000

    Cut alpha over to inf-ptrace.c, making fork following work. tested by miod@

--- gnu/usr.bin/binutils/gdb/alphabsd-nat.c
+++ gnu/usr.bin/binutils/gdb/alphabsd-nat.c
@@ -1,6 +1,6 @@
 /* Native-dependent code for Alpha BSD's.
 
-   Copyright 2000, 2001, 2002, 2004 Free Software Foundation, Inc.
+   Copyright 2000, 2001, 2002, 2004, 2005 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
@@ -25,6 +25,7 @@
 
 #include "alpha-tdep.h"
 #include "alphabsd-tdep.h"
+#include "inf-ptrace.h"
 
 #include <sys/types.h>
 #include <sys/ptrace.h>
@@ -83,8 +84,8 @@ getregs_supplies (int regno)
 /* Fetch register REGNO from the inferior.  If REGNO is -1, do this
    for all registers (including the floating point registers).  */
 
-void
-fetch_inferior_registers (int regno)
+static void
+alphabsd_fetch_inferior_registers (int regno)
 {
   if (regno == -1 || getregs_supplies (regno))
     {
@@ -92,7 +93,7 @@ fetch_inferior_registers (int regno)
 
       if (ptrace (PT_GETREGS, PIDGET (inferior_ptid),
 		  (PTRACE_TYPE_ARG3) &gregs, 0) == -1)
-	perror_with_name ("Couldn't get registers");
+	perror_with_name (_("Couldn't get registers"));
 
       alphabsd_supply_reg ((char *) &gregs, regno);
       if (regno != -1)
@@ -105,7 +106,7 @@ fetch_inferior_registers (int regno)
 
       if (ptrace (PT_GETFPREGS, PIDGET (inferior_ptid),
 		  (PTRACE_TYPE_ARG3) &fpregs, 0) == -1)
-	perror_with_name ("Couldn't get floating point status");
+	perror_with_name (_("Couldn't get floating point status"));
 
       alphabsd_supply_fpreg ((char *) &fpregs, regno);
     }
@@ -114,21 +115,21 @@ fetch_inferior_registers (int regno)
 /* Store register REGNO back into the inferior.  If REGNO is -1, do
    this for all registers (including the floating point registers).  */
 
-void
-store_inferior_registers (int regno)
+static void
+alphabsd_store_inferior_registers (int regno)
 {
   if (regno == -1 || getregs_supplies (regno))
     {
       struct reg gregs;
       if (ptrace (PT_GETREGS, PIDGET (inferior_ptid),
                   (PTRACE_TYPE_ARG3) &gregs, 0) == -1)
-        perror_with_name ("Couldn't get registers");
+        perror_with_name (_("Couldn't get registers"));
 
       alphabsd_fill_reg ((char *) &gregs, regno);
 
       if (ptrace (PT_SETREGS, PIDGET (inferior_ptid),
                   (PTRACE_TYPE_ARG3) &gregs, 0) == -1)
-        perror_with_name ("Couldn't write registers");
+        perror_with_name (_("Couldn't write registers"));
 
       if (regno != -1)
 	return;
@@ -140,12 +141,26 @@ store_inferior_registers (int regno)
 
       if (ptrace (PT_GETFPREGS, PIDGET (inferior_ptid),
 		  (PTRACE_TYPE_ARG3) &fpregs, 0) == -1)
-	perror_with_name ("Couldn't get floating point status");
+	perror_with_name (_("Couldn't get floating point status"));
 
       alphabsd_fill_fpreg ((char *) &fpregs, regno);
 
       if (ptrace (PT_SETFPREGS, PIDGET (inferior_ptid),
 		  (PTRACE_TYPE_ARG3) &fpregs, 0) == -1)
-	perror_with_name ("Couldn't write floating point status");
+	perror_with_name (_("Couldn't write floating point status"));
     }
+}
+
+/* Provide a prototype to silence -Wmissing-prototypes.  */
+void _initialize_alphabsd_nat (void);
+
+void
+_initialize_alphabsd_nat (void)
+{
+  struct target_ops *t;
+
+  t = inf_ptrace_target ();
+  t->to_fetch_registers = alphabsd_fetch_inferior_registers;
+  t->to_store_registers = alphabsd_store_inferior_registers;
+  add_target (t);
 }
