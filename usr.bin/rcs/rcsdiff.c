33e5c3dcb53f63824ce3fbde36f276163240fb04
xsa <xsa@openbsd.org>
2005-11-25 15:56:10 +0000

    add support for `-kmode';

--- usr.bin/rcs/rcsdiff.c
+++ usr.bin/rcs/rcsdiff.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsdiff.c,v 1.19 2005/11/23 09:39:20 xsa Exp $	*/
+/*	$OpenBSD: rcsdiff.c,v 1.20 2005/11/25 15:56:10 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -126,6 +126,9 @@ rcsdiff_main(int argc, char **argv)
 
 		if ((file = rcs_open(fpath, RCS_READ)) == NULL)
 			continue;
+
+		if (kflag != RCS_KWEXP_ERR)
+			rcs_kwexp_set(file, kflag);
 
 		if (rev == RCS_HEAD_REV)
 			frev = file->rf_head;
