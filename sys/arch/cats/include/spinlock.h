d50ab096cc6801c6e6472c8a90f086c6e6f653f9
deraadt <deraadt@openbsd.org>
2005-12-01 22:09:44 +0000

    fix #endif crud

--- sys/arch/cats/include/spinlock.h
+++ sys/arch/cats/include/spinlock.h
@@ -1,6 +1,6 @@
 
-/*	$OpenBSD: spinlock.h,v 1.1 2004/02/01 05:12:54 drahn Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.2 2005/12/01 22:09:44 deraadt Exp $	*/
 #ifndef _CATS_SPINLOCK_H_
 #define _CATS_SPINLOCK_H_
 #include <arm/spinlock.h>
-#endif _CATS_SPINLOCK_H_
+#endif /* _CATS_SPINLOCK_H_ */
--- sys/arch/zaurus/include/spinlock.h
+++ sys/arch/zaurus/include/spinlock.h
@@ -1,6 +1,7 @@
 
-/*	$OpenBSD: spinlock.h,v 1.2 2004/12/31 14:57:13 drahn Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.3 2005/12/01 22:09:59 deraadt Exp $	*/
 #ifndef _ZAURUS_SPINLOCK_H_
 #define _ZAURUS_SPINLOCK_H_
 #include <arm/spinlock.h>
-#endif _ZAURUS_SPINLOCK_H_
+#endif /* _ZAURUS_SPINLOCK_H_ */
+
