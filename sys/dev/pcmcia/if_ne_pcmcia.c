de6adcdd5d70f38a0969f278585e78b1b550a214
fgsch <fgsch@openbsd.org>
2005-12-05 18:07:27 +0000

    trendnet TE-CF100 support; tested and ok uwe@.

--- sys/dev/pcmcia/if_ne_pcmcia.c
+++ sys/dev/pcmcia/if_ne_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ne_pcmcia.c,v 1.84 2005/11/23 11:39:37 mickey Exp $	*/
+/*	$OpenBSD: if_ne_pcmcia.c,v 1.85 2005/12/05 18:07:27 fgsch Exp $	*/
 /*	$NetBSD: if_ne_pcmcia.c,v 1.17 1998/08/15 19:00:04 thorpej Exp $	*/
 
 /*
@@ -266,6 +266,10 @@ const struct ne2000dev {
     { PCMCIA_VENDOR_LINKSYS, PCMCIA_PRODUCT_LINKSYS_COMBO_ECARD,
       PCMCIA_CIS_SVEC_PN650TX,
       0, -1, { 0x00, 0xe0, 0x98 } },
+
+    { PCMCIA_VENDOR_LINKSYS, PCMCIA_PRODUCT_LINKSYS_COMBO_ECARD,
+      PCMCIA_CIS_TRENDNET_TECF100,
+      0, -1, { 0x00, 0x12, 0x0e } },
 
     /*
      * This entry should be here so that above two cards doesn't
