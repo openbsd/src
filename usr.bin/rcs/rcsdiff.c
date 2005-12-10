f91c22bcf60dbb8da5f70c6b135ad7891c9263a4
niallo <niallo@openbsd.org>
2005-12-10 19:44:36 +0000

    - its wrong to use exit() here; return instead.
    ok joris@

--- usr.bin/rcs/rcsdiff.c
+++ usr.bin/rcs/rcsdiff.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsdiff.c,v 1.25 2005/12/05 14:47:24 xsa Exp $	*/
+/*	$OpenBSD: rcsdiff.c,v 1.26 2005/12/10 19:44:36 niallo Exp $	*/
 /*
  * Copyright (c) 2005 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -160,7 +160,7 @@ rcsdiff_main(int argc, char **argv)
 		rcs_close(file);
 	}
 
-	exit(status);
+	return (status);
 }
 
 void
