6e500a9b4cd0444a760e06bf95cc71ccc7819d26
cloder <cloder@openbsd.org>
2005-12-12 23:37:51 +0000

    Regression test for constant in conditional context

---
+++ regress/usr.bin/xlint/test-14.c
@@ -0,0 +1,43 @@
+/*	$OpenBSD: test-14.c,v 1.1 2005/12/12 23:37:51 cloder Exp $	*/
+
+/*
+ * Placed in the public domain by Chad Loder <cloder@openbsd.org>.
+ *
+ * Test lint warnings regarding constant in conditional contexts.
+ */
+
+/* ARGSUSED */
+int
+main(int argc, char *argv[])
+{
+	do {
+		argc++;
+	} while (0);			/* do not warn */
+
+	do {
+		if (argc++)
+			break;
+	} while (1);			/* do not warn */
+
+
+	do {
+		if (argc++)
+			break;
+	} while (2);			/* warn because of 2 */
+
+	if (0) {			/* do not warn */
+		argc++;
+	}
+
+	if (1) {			/* do not warn */
+		argc++;
+	}
+
+	if (argc && 1) {		/* warn because of compound expression */
+		argc++;
+	}
+
+	return 0;
+}
+
+
