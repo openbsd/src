4e1c8ed82a225108f6b226f03df71998d082676f
kettenis <kettenis@openbsd.org>
2005-12-12 18:25:47 +0000

    Pass interrupt type and level in the correct order. ok drahn@, deraadt@

--- sys/arch/macppc/pci/pciide_machdep.c
+++ sys/arch/macppc/pci/pciide_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pciide_machdep.c,v 1.3 2003/10/15 23:00:57 drahn Exp $	*/
+/*	$OpenBSD: pciide_machdep.c,v 1.4 2005/12/12 18:25:47 kettenis Exp $	*/
 /*	$NetBSD: pciide_machdep.c,v 1.2 1999/02/19 18:01:27 mycroft Exp $	*/
 
 /*
@@ -60,7 +60,8 @@ pciide_machdep_compat_intr_establish(struct device *dev,
 	void *cookie;
 
 	irq = PCIIDE_COMPAT_IRQ(chan);
-	cookie = mac_intr_establish(NULL, irq, IPL_BIO, IST_EDGE, func, arg, dev->dv_xname);
+	cookie = mac_intr_establish(NULL, irq, IST_EDGE, IPL_BIO,
+	    func, arg, dev->dv_xname);
 
 	return (cookie);
 }
