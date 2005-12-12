d451e0c1553566570fc1cb2c3b5d3021278479b7
jsg <jsg@openbsd.org>
2005-12-12 13:19:15 +0000

    Drop clause 3/4 as per i386 version this was based on. ok niklas@ (Copyright holder).

--- sys/arch/amd64/include/endian.h
+++ sys/arch/amd64/include/endian.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: endian.h,v 1.1 2004/01/28 01:39:39 mickey Exp $	*/
+/*	$OpenBSD: endian.h,v 1.2 2005/12/12 13:19:15 jsg Exp $	*/
 
 /*-
  * Copyright (c) 1997 Niklas Hallqvist.  All rights reserved.
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Niklas Hallqvist.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
