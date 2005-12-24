8d8293255121443a0bb4ea4c2c63df2924bb53fd
brad <brad@openbsd.org>
2005-12-24 19:16:31 +0000

    clear RUNNING here to do a full init.

--- sys/dev/pci/if_lge.c
+++ sys/dev/pci/if_lge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_lge.c,v 1.35 2005/11/23 11:30:14 mickey Exp $	*/
+/*	$OpenBSD: if_lge.c,v 1.36 2005/12/24 19:16:31 brad Exp $	*/
 /*
  * Copyright (c) 2001 Wind River Systems
  * Copyright (c) 1997, 1998, 1999, 2000, 2001
@@ -1436,6 +1436,7 @@ lge_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 				CSR_WRITE_4(sc, LGE_MODE1,
 				    LGE_MODE1_RX_PROMISC);
 			} else {
+				ifp->if_flags &= ~IFF_RUNNING;
 				lge_init(sc);
 			}
 		} else {
