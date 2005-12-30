a4c917d31ccbb96999f145be3f02e02cf2e0a240
grange <grange@openbsd.org>
2005-12-30 16:40:08 +0000

    Allow to disable lint by setting WANTLIB=no
    ok deraadt@ espie@

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.lib.mk,v 1.52 2005/12/29 14:44:05 espie Exp $
+#	$OpenBSD: bsd.lib.mk,v 1.53 2005/12/30 16:40:08 grange Exp $
 #	$NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 #	@(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
 
@@ -168,7 +168,7 @@ _LIBS+=lib${LIB}.so.${SHLIB_MAJOR}.${SHLIB_MINOR}
 .endif
 .endif
 
-.if defined(WANTLINT)
+.if defined(WANTLINT) && ${WANTLINT:L} != "no"
 _LIBS+=llib-l${LIB}.ln
 .endif
 
@@ -285,7 +285,7 @@ realinstall:
 	${INSTALL} ${INSTALL_COPY} -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
 	    lib${LIB}.so.${SHLIB_MAJOR}.${SHLIB_MINOR} ${DESTDIR}${LIBDIR}
 .endif
-.if defined(WANTLINT)
+.if defined(WANTLINT) && ${WANTLINT:L} != "no"
 	${INSTALL} ${INSTALL_COPY} -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
 	    llib-l${LIB}.ln ${DESTDIR}${LINTLIBDIR}
 .endif
