3d9d9691a65d35c78b4ed05a8fc82b94641e4474
jolan <jolan@openbsd.org>
2006-01-10 18:17:15 +0000

    need sys/types.h for vaddr_t

--- sys/arch/zaurus/stand/zbsdmod/compat_linux.h
+++ sys/arch/zaurus/stand/zbsdmod/compat_linux.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: compat_linux.h,v 1.3 2005/01/14 08:10:17 uwe Exp $	*/
+/*	$OpenBSD: compat_linux.h,v 1.4 2006/01/10 18:17:15 jolan Exp $	*/
 
 /*
  * Copyright (c) 2005 Uwe Stuehler <uwe@bsdx.de>
@@ -95,6 +95,7 @@ extern	void *memcpy(void *, const void *, size_t);
 
 /* BSD headers */
 #include <sys/exec_elf.h>
+#include <sys/types.h>
 #include <errno.h>
 
 /* Linux LKM support */
