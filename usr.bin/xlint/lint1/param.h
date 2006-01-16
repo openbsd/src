e79480e0f33dd39a2b7047287a879e4a110107fe
otto <otto@openbsd.org>
2006-01-16 08:07:31 +0000

    Kill spurious warnings when using size_t as an index; since size_t and ptrdiff_T are now alays (u)long, tell lint that. ok millert@ deraadt@

--- usr.bin/xlint/lint1/param.h
+++ usr.bin/xlint/lint1/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.12 2004/02/07 23:27:32 millert Exp $	*/
+/*	$OpenBSD: param.h,v 1.13 2006/01/16 08:07:31 otto Exp $	*/
 /*	$NetBSD: param.h,v 1.6 1996/04/01 21:47:57 mark Exp $	*/
 
 /*
@@ -61,13 +61,8 @@
  * Should be set to 1 if the difference of two pointers is of type long
  * or the value of sizeof is of type unsigned long.
  */
-#ifdef __LP64__
 #define PTRDIFF_IS_LONG		1
 #define SIZEOF_IS_ULONG		1
-#else
-#define PTRDIFF_IS_LONG		0
-#define SIZEOF_IS_ULONG		0
-#endif
 
 /*
  * Make sure this matches wchar_t.
