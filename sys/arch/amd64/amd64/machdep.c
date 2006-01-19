7ce37d319e3807aa587cb72c05e53858e819852f
grange <grange@openbsd.org>
2006-01-19 18:21:03 +0000

    Fix NACPI > 1 (should be >0). Spotted by form@.

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.44 2006/01/06 10:53:16 grange Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.45 2006/01/19 18:21:03 grange Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
@@ -138,7 +138,7 @@
 #include "ksyms.h"
 
 #include "acpi.h"
-#if NACPI > 1
+#if NACPI > 0
 #include <dev/acpi/acpivar.h>
 #endif
 
