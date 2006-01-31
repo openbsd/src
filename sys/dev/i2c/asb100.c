3da0678c5f6347f8e39d808a00bd35c4321d9903
djm <djm@openbsd.org>
2006-01-31 11:18:28 +0000

    fix error message

--- sys/dev/i2c/asb100.c
+++ sys/dev/i2c/asb100.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: asb100.c,v 1.4 2006/01/19 17:08:39 grange Exp $	*/
+/*	$OpenBSD: asb100.c,v 1.5 2006/01/31 11:18:28 djm Exp $	*/
 
 /*
- * Copyright (c) 2005 Damien Miller
+ * Copyright (c) 2005 Damien Miller <djm@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -321,7 +321,8 @@ asbtm_refresh(void *arg)
 	iic_acquire_bus(sc->sc_tag, 0);
 
 	if (asbtm_banksel(sc, 0, &orig_bank) == -1) {
-		printf(": cannot get/set register bank\n");
+		printf("%s: cannot get/set register bank\n",
+		    sc->sc_dev.dv_xname);
 		iic_release_bus(sc->sc_tag, 0);
 		return;
 	}
