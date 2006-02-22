09bddadd45fa67b808e0d0b8cdf4ca8c1450afac
otto <otto@openbsd.org>
2006-02-22 07:26:08 +0000

    Append two string using strlcpy()/strlcat() instead of snprintf() to avoid having to check for encoding errors returned by snprintf(). From Ray Lai; ok millert@ jaredy@

--- usr.bin/diff/diffreg.c
+++ usr.bin/diff/diffreg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: diffreg.c,v 1.63 2006/02/16 08:15:05 otto Exp $	*/
+/*	$OpenBSD: diffreg.c,v 1.64 2006/02/22 07:26:08 otto Exp $	*/
 
 /*
  * Copyright (C) Caldera International Inc.  2001-2002.
@@ -65,7 +65,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$OpenBSD: diffreg.c,v 1.63 2006/02/16 08:15:05 otto Exp $";
+static const char rcsid[] = "$OpenBSD: diffreg.c,v 1.64 2006/02/22 07:26:08 otto Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -511,8 +511,10 @@ opentemp(const char *file)
 
 	if ((tempdir = getenv("TMPDIR")) == NULL)
 		tempdir = _PATH_TMP;
-	if (snprintf(tempfile, sizeof(tempfile), "%s/diff.XXXXXXXX",
-	    tempdir) >= sizeof(tempfile)) {
+	
+	if (strlcpy(tempfile, tempdir, sizeof(tempfile)) >= sizeof(tempfile) ||
+	    strlcat(tempfile, "/diff.XXXXXXXX", sizeof(tempfile)) >=
+	    sizeof(tempfile)) {
 		close(ifd);
 		errno = ENAMETOOLONG;
 		return (NULL);
