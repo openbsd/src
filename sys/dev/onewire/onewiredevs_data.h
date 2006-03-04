4bb8ce0a64549f2fc4d25dc18a94e7489423f621
grange <grange@openbsd.org>
2006-03-04 16:33:33 +0000

    sync

---
+++ sys/dev/onewire/onewiredevs_data.h
@@ -0,0 +1,21 @@
+/*	$OpenBSD: onewiredevs_data.h,v 1.1 2006/03/04 16:33:33 grange Exp $	*/
+
+/*
+ * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
+ *
+ * Generated from:
+ *	OpenBSD: onewiredevs,v 1.1 2006/03/04 16:27:03 grange Exp 
+ */
+
+static const struct onewire_family onewire_familytab[] = {
+	{ ONEWIRE_FAMILY_DS1990, "ID" },
+	{ ONEWIRE_FAMILY_DS1991, "MultiKey" },
+	{ ONEWIRE_FAMILY_DS1994, "4kb NVRAM + RTC" },
+	{ ONEWIRE_FAMILY_DS1993, "4kb NVRAM" },
+	{ ONEWIRE_FAMILY_DS1992, "1kb NVRAM" },
+	{ ONEWIRE_FAMILY_DS1982, "1kb EPROM" },
+	{ ONEWIRE_FAMILY_DS1995, "16kb NVRAM" },
+	{ ONEWIRE_FAMILY_DS1996, "64kb NVRAM" },
+	{ ONEWIRE_FAMILY_DS1920, "Temperature" },
+	{ 0, NULL }
+};
