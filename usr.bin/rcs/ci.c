c465ac3cde6c761b137cf8b9c357ef7ef9d33d8d
niallo <niallo@openbsd.org>
2006-03-05 17:20:35 +0000

    checkin_parsekeyword() returns void so zap unnecessary return.

--- usr.bin/rcs/ci.c
+++ usr.bin/rcs/ci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ci.c,v 1.104 2006/03/05 17:17:27 niallo Exp $	*/
+/*	$OpenBSD: ci.c,v 1.105 2006/03/05 17:20:35 niallo Exp $	*/
 /*
  * Copyright (c) 2005, 2006 Niall O'Higgins <niallo@openbsd.org>
  * All rights reserved.
@@ -1067,6 +1067,4 @@ checkin_parsekeyword(char *keystring,  RCSNUM **rev, time_t *date,
 			fatal("could not parse rcsnum");
 		break;
 	}
-
-	return;
 }
