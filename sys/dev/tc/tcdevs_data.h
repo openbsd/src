cb2cba2879fba8afe9a8c95b7c96ec8d54a384ce
miod <miod@openbsd.org>
2006-03-13 22:00:42 +0000

    Regen

--- sys/dev/tc/tcdevs_data.h
+++ sys/dev/tc/tcdevs_data.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcdevs_data.h,v 1.7 2002/05/03 20:27:58 miod Exp $	*/
+/*	$OpenBSD: tcdevs_data.h,v 1.8 2006/03/13 22:00:42 miod Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
@@ -6,259 +6,183 @@
  * generated from:
  *	OpenBSD: tcdevs,v 1.7 2002/05/03 20:27:44 miod Exp 
  */
-/* $NetBSD: tcdevs,v 1.17 2000/12/17 13:56:05 ad Exp $ */
-
-/*
- * Copyright (c) 1996 Christopher G. Demetriou
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Christopher G. Demetriou
- *	for the NetBSD Project.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 struct tc_knowndev tc_knowndevs[] = {
 	{
 	    "AV01B-AA",
-	    TC_DEVICE_AV01B_AA,
-	    TC_DESCRIPTION_AV01B_AA,
+	    "DecAudio \"LoFi\" audio/isdn"
 	},
 	{
 	    "AV300-AA",
-	    TC_DEVICE_AV300_AA,
-	    TC_DESCRIPTION_AV300_AA,
+	    "Video capture option card"
 	},
 	{
 	    "DGLTA-FA",
-	    TC_DEVICE_DGLTA_FA,
-	    TC_DESCRIPTION_DGLTA_FA,
+	    "DGLTA ATM"
 	},
 	{
 	    "FORE_ATM",
-	    TC_DEVICE_FORE_ATM,
-	    TC_DESCRIPTION_FORE_ATM,
+	    "Fore TCA-100 ATM"
 	},
 	{
 	    "KZTSA-AA",
-	    TC_DEVICE_KZTSA_AA,
-	    TC_DESCRIPTION_KZTSA_AA,
+	    "TZA FWD SCSI"
 	},
 	{
 	    "OTTO    ",
-	    TC_DEVICE_OTTO,
-	    TC_DESCRIPTION_OTTO,
+	    "DEC SRC \"OTTO\" ATM"
 	},
 	{
 	    "PMAD-AA ",
-	    TC_DEVICE_PMAD_AA,
-	    TC_DESCRIPTION_PMAD_AA,
+	    "LANCE Ethernet"
 	},
 	{
 	    "PMAD-AB ",
-	    TC_DEVICE_PMAD_AB,
-	    TC_DESCRIPTION_PMAD_AB,
+	    "LANCE Ethernet"
 	},
 	{
 	    "PMAF-AA ",
-	    TC_DEVICE_PMAF_AA,
-	    TC_DESCRIPTION_PMAF_AA,
+	    "DEC FDDIcontroller 700 (DEFZA; fiber optic)"
 	},
 	{
 	    "PMAF-FA ",
-	    TC_DEVICE_PMAF_FA,
-	    TC_DESCRIPTION_PMAF_FA,
+	    "DEFTA FDDI"
 	},
 	{
 	    "PMAG-AA ",
-	    TC_DEVICE_PMAG_AA,
-	    TC_DESCRIPTION_PMAG_AA,
+	    "Monochrome Frame Buffer"
 	},
 	{
 	    "PMAG-BA ",
-	    TC_DEVICE_PMAG_BA,
-	    TC_DESCRIPTION_PMAG_BA,
+	    "Color Frame Buffer"
 	},
 	{
 	    "PMAG-CA ",
-	    TC_DEVICE_PMAG_CA,
-	    TC_DESCRIPTION_PMAG_CA,
+	    "2D Graphics (PX 2DA)"
 	},
 	{
 	    "PMAG-CB ",
-	    TC_DEVICE_PMAG_CB,
-	    TC_DESCRIPTION_PMAG_CB,
+	    "2D Graphics (PX 2DA)"
 	},
 	{
 	    "PMAG-DA ",
-	    TC_DEVICE_PMAG_DA,
-	    TC_DESCRIPTION_PMAG_DA,
+	    "3D Graphics (PXG LM-3DA)"
 	},
 	{
 	    "PMAG-DB ",
-	    TC_DEVICE_PMAG_DB,
-	    TC_DESCRIPTION_PMAG_DB,
+	    "3D Graphics (PXG LM-3DA)"
 	},
 	{
 	    "PMAG-EB ",
-	    TC_DEVICE_PMAG_EB,
-	    TC_DESCRIPTION_PMAG_EB,
+	    "3D Graphics (PXG MID-3DA)"
 	},
 	{
 	    "PMAG-FA ",
-	    TC_DEVICE_PMAG_FA,
-	    TC_DESCRIPTION_PMAG_FA,
+	    "3D Graphics (PXG HE-3DA)"
 	},
 	{
 	    "PMAG-FB ",
-	    TC_DEVICE_PMAG_FB,
-	    TC_DESCRIPTION_PMAG_FB,
+	    "3D Graphics (PXG HE+3DA)"
 	},
 	{
 	    "PMAGB-BA",
-	    TC_DEVICE_PMAGB_BA,
-	    TC_DESCRIPTION_PMAGB_BA,
+	    "Smart Frame Buffer (HX8)"
 	},
 	{
 	    "PMAGB-FA",
-	    TC_DEVICE_PMAGB_FA,
-	    TC_DESCRIPTION_PMAGB_FA,
+	    "3D Graphics (PXG HE+3DA)"
 	},
 	{
 	    "PMAGB-FB",
-	    TC_DEVICE_PMAGB_FB,
-	    TC_DESCRIPTION_PMAGB_FB,
+	    "3D Graphics (PXG HE+3DA)"
 	},
 	{
 	    "PMAGC-AA",
-	    TC_DEVICE_PMAGC_AA,
-	    TC_DESCRIPTION_PMAGC_AA,
+	    "2D Graphics (PV-LO)"
 	},
 	{
 	    "PMAGC-BA",
-	    TC_DEVICE_PMAGC_BA,
-	    TC_DESCRIPTION_PMAGC_BA,
+	    "2D Graphics (PV-MID)"
 	},
 	{
 	    "PMAGD-AA",
-	    TC_DEVICE_PMAGD_AA,
-	    TC_DESCRIPTION_PMAGD_AA,
+	    "Smart Frame Buffer Plus, 8bpp (ZLX-E1)"
 	},
 	{
 	    "PMAGD-BA",
-	    TC_DEVICE_PMAGD_BA,
-	    TC_DESCRIPTION_PMAGD_BA,
+	    "Smart Frame Buffer Plus, 32bpp (ZLX-E2)"
 	},
 	{
 	    "PMAGD-CA",
-	    TC_DEVICE_PMAGD_CA,
-	    TC_DESCRIPTION_PMAGD_CA,
+	    "Smart Frame Buffer Plus, 32bpp (ZLX-E3)"
 	},
 	{
 	    "PMAZ-AB ",
-	    TC_DEVICE_PMAZ_AB,
-	    TC_DESCRIPTION_PMAZ_AB,
+	    "53c94 SCSI Controller"
 	},
 	{
 	    "KWS_TD  ",
-	    TC_DEVICE_KWS_TD,
-	    TC_DESCRIPTION_KWS_TD,
+	    "Kubota Denali"
 	},
 	{
 	    "PMABV-AA",
-	    TC_DEVICE_PMABV_AA,
-	    TC_DESCRIPTION_PMABV_AA,
+	    "VME Adapter"
 	},
 	{
 	    "PMAG-DV ",
-	    TC_DEVICE_PMAG_DV,
-	    TC_DESCRIPTION_PMAG_DV,
+	    "Maxine Color Frame Buffer"
 	},
 	{
 	    "PMAG-JA ",
-	    TC_DEVICE_PMAG_JA,
-	    TC_DESCRIPTION_PMAG_JA,
+	    "24-plane True Color Frame Buffer (TX)"
 	},
 	{
 	    "PMAGD   ",
-	    TC_DEVICE_PMAGD,
-	    TC_DESCRIPTION_PMAGD,
+	    "Smart Frame Buffer Plus, unknown bpp"
 	},
 	{
 	    "PMAP-AA ",
-	    TC_DEVICE_PMAP_AA,
-	    TC_DESCRIPTION_PMAP_AA,
+	    "Prestoserve"
 	},
 	{
 	    "PMAT-AA ",
-	    TC_DEVICE_PMAT_AA,
-	    TC_DESCRIPTION_PMAT_AA,
+	    "DEC TurboChannel Token Ring Controller"
 	},
 	{
 	    "PMAZ-AA ",
-	    TC_DEVICE_PMAZ_AA,
-	    TC_DESCRIPTION_PMAZ_AA,
+	    "53c94 SCSI Controller"
 	},
 	{
 	    "PMAZ-DS ",
-	    TC_DEVICE_PMAZ_DS,
-	    TC_DESCRIPTION_PMAZ_DS,
+	    "53c94 TCDS SCSI Controller (baseboard)"
 	},
 	{
 	    "PMAZ-FS ",
-	    TC_DEVICE_PMAZ_FS,
-	    TC_DESCRIPTION_PMAZ_FS,
+	    "53c94 TCDS Fast SCSI Controller (baseboard)"
 	},
 	{
 	    "PMAZB-AA",
-	    TC_DEVICE_PMAZB_AA,
-	    TC_DESCRIPTION_PMAZB_AA,
+	    "53c94 TCDS SCSI Controller option card"
 	},
 	{
 	    "PMAZB-AB",
-	    TC_DEVICE_PMAZB_AB,
-	    TC_DESCRIPTION_PMAZB_AB,
+	    "53c94 TCDS SCSI Controller option card"
 	},
 	{
 	    "PMAZC-AA",
-	    TC_DEVICE_PMAZC_AA,
-	    TC_DESCRIPTION_PMAZC_AA,
+	    "53c94 TCDS Fast SCSI Controller option card"
 	},
 	{
 	    "PMTNV-AA",
-	    TC_DEVICE_PMTNV_AA,
-	    TC_DESCRIPTION_PMTNV_AA,
+	    "Non-volatile RAM option card"
 	},
 	{
 	    "T1D4PKT ",
-	    TC_DEVICE_T1D4PKT,
-	    TC_DESCRIPTION_T1D4PKT,
+	    "DECWRL Turbochannel T1"
 	},
 	{
 	    "T3PKT   ",
-	    TC_DEVICE_T3PKT,
-	    TC_DESCRIPTION_T3PKT,
+	    "DECWRL Turbochannel T3"
 	},
-	{ NULL, NULL, NULL, }
+	{ NULL, NULL }
 };
