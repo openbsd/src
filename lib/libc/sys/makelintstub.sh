6d0863a2d5e0dbba7ff00a3fb6589617857a8322
moritz <moritz@openbsd.org>
2006-03-13 18:59:32 +0000

    Correctly check for the exit value of getopt(1) and don't overwrite it with the value from ``set''. ok millert@

--- lib/libc/sys/makelintstub.sh
+++ lib/libc/sys/makelintstub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -
-#	$OpenBSD: makelintstub.sh,v 1.5 2006/03/12 20:12:14 deraadt Exp $
+#	$OpenBSD: makelintstub.sh,v 1.6 2006/03/13 18:59:32 moritz Exp $
 #	$NetBSD: makelintstub,v 1.2 1997/11/05 05:46:18 thorpej Exp $
 #
 # Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
@@ -152,16 +152,17 @@ trailer()
 	__EOF__
 }
 
-set -- `getopt no:ps: $*`
-
 pflag=NO
 nflag=NO
 oarg=""
 syscallhdr=/usr/include/sys/syscall.h
 
+args=`getopt no:ps: $*`
 if test $? -ne 0; then
 	usage
 fi
+set -- $args
+
 for i; do
 	case "$i" in
 	-n)	nflag=YES; shift;;
