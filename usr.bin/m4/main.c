f3a8ebd782b50dfa300903c6aa6954a36225c19f
espie <espie@openbsd.org>
2006-03-23 07:57:05 +0000

    back-out format for now, need to be active ONLY in GNU-m4 mode.

--- usr.bin/m4/main.c
+++ usr.bin/m4/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.71 2006/03/20 20:27:45 espie Exp $	*/
+/*	$OpenBSD: main.c,v 1.72 2006/03/23 07:57:05 espie Exp $	*/
 /*	$NetBSD: main.c,v 1.12 1997/02/08 23:54:49 cgd Exp $	*/
 
 /*-
@@ -111,7 +111,6 @@ struct keyblk keywrds[] = {	/* m4 keywords to be installed */
 	{ "esyscmd",	  ESYSCMDTYPE},
 	{ "__file__",	  FILENAMETYPE | NOARGS},
 	{ "__line__",	  LINETYPE | NOARGS},
-	{ "format",	  FORMATTYPE},
 #endif
 	{ "popdef",       POPDTYPE },
 	{ "pushdef",      PUSDTYPE },
