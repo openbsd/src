989e2504d97ce93f70229b3c7af8c9971dcd9ab0
drahn <drahn@openbsd.org>
2006-03-31 04:28:37 +0000

    Gently cast to make lint happy.

--- lib/libc/arch/powerpc/gen/infinity.c
+++ lib/libc/arch/powerpc/gen/infinity.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: infinity.c,v 1.5 2005/08/07 16:40:15 espie Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.6 2006/03/31 04:28:37 drahn Exp $	*/
 
 /* infinity.c */
 
@@ -6,4 +6,4 @@
 
 /* bytes for +Infinity on a PowerPC */
 char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
-    { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
+    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
