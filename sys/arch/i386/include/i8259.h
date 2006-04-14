d48761789ea914a05a1aa8cebe3bf92a7934e16c
weingart <weingart@openbsd.org>
2006-04-14 22:26:46 +0000

    Remove final DUMMY_NOPS vestige of a macro as well. ok marco@

--- sys/arch/i386/include/i8259.h
+++ sys/arch/i386/include/i8259.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i8259.h,v 1.3 2006/04/14 21:52:47 weingart Exp $	*/
+/*	$OpenBSD: i8259.h,v 1.4 2006/04/14 22:26:46 weingart Exp $	*/
 /*	$NetBSD: i8259.h,v 1.3 2003/05/04 22:01:56 fvdl Exp $	*/
 
 /*-
@@ -116,24 +116,24 @@ extern void i8259_reinit(void);
 #define	i8259_asm_ack2(num)
 #endif
 
-#ifndef PIC_MASKDELAY
-#define PIC_MASKDELAY	pushl %eax ; inb $0x84,%al ; popl %eax
-#endif
-
 #ifdef ICU_HARDWARE_MASK
 
 #define	i8259_asm_mask(num) \
 	movb	CVAROFF(imen, IRQ_BYTE(num)),%al			;\
 	orb	$IRQ_BIT(num),%al					;\
 	movb	%al,CVAROFF(imen, IRQ_BYTE(num))			;\
-	PIC_MASKDELAY							;\
+	pushl	%eax							;\
+	inb	$0x84,%al						;\
+	popl	%eax							;\
 	outb	%al,$(ICUADDR+1)
 #define	i8259_asm_unmask(num) \
 	cli								;\
 	movb	CVAROFF(imen, IRQ_BYTE(num)),%al			;\
 	andb	$~IRQ_BIT(num),%al					;\
 	movb	%al,CVAROFF(imen, IRQ_BYTE(num))			;\
-	PIC_MASKDELAY							;\
+	pushl	%eax							;\
+	inb	$0x84,%al						;\
+	popl	%eax							;\
 	outb	%al,$(ICUADDR+1)					;\
 	sti
 
