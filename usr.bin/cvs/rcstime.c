0bb684483808a8073a20bc55a793b16cdda7dfbc
ray <ray@openbsd.org>
2006-04-17 06:33:22 +0000

    Remove unused variable.
    Found by lint.
    
    OK joris@

--- usr.bin/cvs/rcstime.c
+++ usr.bin/cvs/rcstime.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcstime.c,v 1.4 2006/04/14 02:49:44 deraadt Exp $	*/
+/*	$OpenBSD: rcstime.c,v 1.5 2006/04/17 06:33:22 ray Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -33,7 +33,7 @@ void
 rcs_set_tz(char *tz, struct rcs_delta *rdp, struct tm *tb)
 {
 	int tzone;
-	int neg, pos;
+	int pos;
 	char *h, *m;
 	struct tm *ltb;
 	time_t now;
@@ -44,10 +44,9 @@ rcs_set_tz(char *tz, struct rcs_delta *rdp, struct tm *tb)
 		ltb->tm_hour += ((int)ltb->tm_gmtoff/3600);
 		memcpy(tb, ltb, sizeof(struct tm));
 	} else {
-		neg = pos = 0;
+		pos = 0;
 		switch (*tz) {
 		case '-':
-			neg = 1;
 			break;
 		case '+':
 			pos = 1;
