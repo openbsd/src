6711b70023d55e486391e8c7b384197c9f88bdaf
claudio <claudio@openbsd.org>
2006-04-25 08:44:08 +0000

    Mark fatal and fatalx as __dead from deraadt@

--- usr.sbin/bgpd/bgpd.h
+++ usr.sbin/bgpd/bgpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpd.h,v 1.197 2006/04/21 08:51:46 claudio Exp $ */
+/*	$OpenBSD: bgpd.h,v 1.198 2006/04/25 08:44:08 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -702,8 +702,8 @@ void		 log_warn(const char *, ...);
 void		 log_warnx(const char *, ...);
 void		 log_info(const char *, ...);
 void		 log_debug(const char *, ...);
-void		 fatal(const char *);
-void		 fatalx(const char *);
+void		 fatal(const char *) __dead;
+void		 fatalx(const char *) __dead;
 
 /* parse.y */
 int	 cmdline_symset(char *);
