026ecae97ca2385404dadb380d519b6a8c2c5b01
brad <brad@openbsd.org>
2006-05-01 15:59:31 +0000

    increase the Tx ring count from 64 to 256.
    ok jsg@ damien@

--- sys/dev/pci/if_nfereg.h
+++ sys/dev/pci/if_nfereg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_nfereg.h,v 1.17 2006/04/26 02:07:30 jsg Exp $	*/
+/*	$OpenBSD: if_nfereg.h,v 1.18 2006/05/01 15:59:31 brad Exp $	*/
 
 /*-
  * Copyright (c) 2005 Jonathan Gray <jsg@openbsd.org>
@@ -19,7 +19,7 @@
 #define NFE_PCI_BA		0x10
 
 #define NFE_RX_RING_COUNT	128
-#define NFE_TX_RING_COUNT	64
+#define NFE_TX_RING_COUNT	256
 
 #define NFE_JBYTES		(ETHER_MAX_LEN_JUMBO + ETHER_ALIGN)
 #define NFE_JPOOL_COUNT		(NFE_RX_RING_COUNT + 64)
