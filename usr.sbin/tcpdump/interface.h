f9a530d7bd90d54a7de5fd8856ec5c271efbadd8
stevesk <stevesk@openbsd.org>
2006-05-23 14:20:19 +0000

    add VLAN Query Protocol (VQP) dissector; ok canacar@ markus@

--- usr.sbin/tcpdump/interface.h
+++ usr.sbin/tcpdump/interface.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: interface.h,v 1.52 2006/03/28 15:48:33 reyk Exp $	*/
+/*	$OpenBSD: interface.h,v 1.53 2006/05/23 14:20:19 stevesk Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -20,7 +20,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: /home/cvs/src/usr.sbin/tcpdump/interface.h,v 1.52 2006/03/28 15:48:33 reyk Exp $ (LBL)
+ * @(#) $Header: /home/cvs/src/usr.sbin/tcpdump/interface.h,v 1.53 2006/05/23 14:20:19 stevesk Exp $ (LBL)
  */
 
 #ifndef tcpdump_interface_h
@@ -236,6 +236,7 @@ extern void l2tp_print(const u_char *dat, u_int length);
 extern void vrrp_print(const u_char *bp, u_int len, int ttl);
 extern void carp_print(const u_char *bp, u_int len, int ttl);
 extern void hsrp_print(const u_char *, u_int);
+extern void vqp_print(const u_char *, u_int);
 extern void nfsreply_print(const u_char *, u_int, const u_char *);
 extern void nfsreq_print(const u_char *, u_int, const u_char *);
 extern void ns_print(const u_char *, u_int, int);
--- usr.sbin/tcpdump/print-udp.c
+++ usr.sbin/tcpdump/print-udp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-udp.c,v 1.26 2006/03/10 18:17:41 reyk Exp $	*/
+/*	$OpenBSD: print-udp.c,v 1.27 2006/05/23 14:20:19 stevesk Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996
@@ -23,7 +23,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/print-udp.c,v 1.26 2006/03/10 18:17:41 reyk Exp $ (LBL)";
+    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/print-udp.c,v 1.27 2006/05/23 14:20:19 stevesk Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -361,6 +361,7 @@ static int udp_cksum(register const struct ip *ip,
 #define RADIUS_AUTH_PORT     1812
 #define RADIUS_ACCT_PORT     1813
 #define HSRP_PORT 1985		/*XXX*/
+#define VQP_PORT 1589
 #define LWRES_PORT 921
 #define MULTICASTDNS_PORT 5353
 
@@ -609,6 +610,8 @@ udp_print(register const u_char *bp, u_int length, register const u_char *bp2)
 			vat_print((const void *)(up + 1), length, up);
 		else if (ISPORT(IAPP_PORT) || ISPORT(IAPP_OLD_PORT))
 			iapp_print((const u_char *)(up + 1), length);
+		else if (ISPORT(VQP_PORT))
+			vqp_print((const u_char *)(up + 1), length);
 #ifdef INET6
 		else if (ISPORT(RIPNG_PORT))
 			ripng_print((const u_char *)(up + 1), length);
