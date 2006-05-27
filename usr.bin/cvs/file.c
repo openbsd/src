6b03c42ceeffb2e1ff8b1417084e938ab1f3095b
joris <joris@openbsd.org>
2006-05-27 15:15:43 +0000

    remove debug output

--- usr.bin/cvs/file.c
+++ usr.bin/cvs/file.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.c,v 1.140 2006/05/27 15:14:27 joris Exp $	*/
+/*	$OpenBSD: file.c,v 1.141 2006/05/27 15:15:43 joris Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
@@ -433,7 +433,6 @@ cvs_file_walkdir(struct cvs_file *cf, struct cvs_recursion *cr)
 
 			if (!(cr->flags & CR_RECURSE_DIRS) &&
 			    dp->d_type == DT_DIR) {
-				printf("Skipping %s\n", dp->d_name);
 				cp += dp->d_reclen;
 				continue;
 			}
