10e098a8609f724fab29ff74da43dd5402e6a8b9
claudio <claudio@openbsd.org>
2006-05-28 04:30:16 +0000

    Fix multicast and broadcast over gre(4) tunnels. Clear the M_MCAST and M_BCAST mbuf flags so that the packet is sent out with a normal link local address instead of a multicast or broadcast address. OK brad@

--- sys/net/if_gre.c
+++ sys/net/if_gre.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: if_gre.c,v 1.40 2006/03/25 22:41:47 djm Exp $ */
+/*      $OpenBSD: if_gre.c,v 1.41 2006/05/28 04:30:16 claudio Exp $ */
 /*	$NetBSD: if_gre.c,v 1.9 1999/10/25 19:18:11 drochner Exp $ */
 
 /*
@@ -230,6 +230,8 @@ gre_output(struct ifnet *ifp, struct mbuf *m, struct sockaddr *dst,
 	}
 	bcopy(&ifp, (caddr_t)(mtag + 1), sizeof(struct ifnet *));
 	m_tag_prepend(m, mtag);
+
+	m->m_flags &= ~(M_BCAST|M_MCAST);
 
 #if NBPFILTER >0
 	if (ifp->if_bpf)
