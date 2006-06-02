c93d272975fb2e54de786436154c26ab9aab0ec9
uwe <uwe@openbsd.org>
2006-06-02 21:17:56 +0000

    regen

--- sys/dev/sdmmc/sdmmcdevs.h
+++ sys/dev/sdmmc/sdmmcdevs.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: sdmmcdevs.h,v 1.2 2006/06/01 21:45:09 uwe Exp $	*/
+/*	$OpenBSD: sdmmcdevs.h,v 1.3 2006/06/02 21:17:56 uwe Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *		OpenBSD: sdmmcdevs,v 1.2 2006/06/01 21:44:55 uwe Exp 
+ *		OpenBSD: sdmmcdevs,v 1.3 2006/06/02 21:17:36 uwe Exp 
  */
 
 /*
@@ -27,17 +27,47 @@
  * List of known SD card vendors
  */
 
+#define	SDMMC_VENDOR_CGUYS	0x0092	/* C-guys, Inc. */
+#define	SDMMC_VENDOR_TOSHIBA	0x0098	/* Toshiba */
+#define	SDMMC_VENDOR_SOCKETCOM	0x0104	/* Socket Communications, Inc. */
 #define	SDMMC_VENDOR_SYCHIP	0x02db	/* SyChip Inc. */
 #define	SDMMC_VENDOR_SPECTEC	0x02fe	/* Spectec Computer Co., Ltd */
+#define	SDMMC_VENDOR_GLOBALSAT	0x0501	/* Globalsat Technology Co. */
 
 /*
  * List of known products, grouped by vendor
  */
 
-/* SyChip Inc. */
-#define	SDMMC_CID_SYCHIP_WLAN6060SD	{ NULL, NULL, NULL, NULL }
-#define	SDMMC_PRODUCT_SYCHIP_WLAN6060SD	0x544d
+/* C-guys, Inc. */
+#define	SDMMC_CIS_CGUYS_TIACX100	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_CGUYS_TIACX100	0x0001
+#define	SDMMC_CIS_CGUYS_SDFMRADIO2	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_CGUYS_SDFMRADIO2	0x0005
+#define	SDMMC_CIS_CGUYS_SDFMRADIO	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_CGUYS_SDFMRADIO	0x5544
+
+/* Globalsat Technology Co. */
+#define	SDMMC_CIS_GLOBALSAT_SD501	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_GLOBALSAT_SD501	0xf501
 
 /* Spectec Computer Co., Ltd */
-#define	SDMMC_CID_SPECTEC_SDW820	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_CIS_SPECTEC_SDW820	{ NULL, NULL, NULL, NULL }
 #define	SDMMC_PRODUCT_SPECTEC_SDW820	0x2128
+
+/* SyChip Inc. */
+#define	SDMMC_CIS_SYCHIP_WLAN6060SD	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_SYCHIP_WLAN6060SD	0x544d
+
+/* Toshiba */
+#define	SDMMC_CIS_TOSHIBA_SDBTCARD1	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_TOSHIBA_SDBTCARD1	0x0001
+#define	SDMMC_CIS_TOSHIBA_SDBTCARD2	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_TOSHIBA_SDBTCARD2	0x0002
+#define	SDMMC_CIS_TOSHIBA_SDBTCARD3	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_TOSHIBA_SDBTCARD3	0x0003
+
+/* Socket Communications, Inc. */
+#define	SDMMC_CIS_SOCKET_SDSCANNER	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_SOCKET_SDSCANNER	0x005e
+#define	SDMMC_CIS_SOCKET_BTCARD	{ NULL, NULL, NULL, NULL }
+#define	SDMMC_PRODUCT_SOCKET_BTCARD	0x00c5
