b5b3e5716f6d33a81ce5bf1211aac87d7f086d8a
hshoexer <hshoexer@openbsd.org>
2006-06-17 10:06:21 +0000

    Do not leak file descriptor in error path.  From Andrey Matveev <evol at online dot ptt dot ru>, thanks!

--- sbin/isakmpd/policy.c
+++ sbin/isakmpd/policy.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: policy.c,v 1.87 2005/11/14 23:25:11 deraadt Exp $	 */
+/* $OpenBSD: policy.c,v 1.88 2006/06/17 10:06:21 hshoexer Exp $	 */
 /* $EOM: policy.c,v 1.49 2000/10/24 13:33:39 niklas Exp $ */
 
 /*
@@ -2200,6 +2200,7 @@ keynote_cert_obtain(u_int8_t *id, size_t id_len, void *data, u_int8_t **cert,
 		log_error("keynote_cert_obtain: failed to allocate %lu bytes",
 		    (unsigned long)size);
 		free(file);
+		close(fd);
 		return 0;
 	}
 
