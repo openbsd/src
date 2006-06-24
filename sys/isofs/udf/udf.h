559cf1c3c0764ce6ddf8dc0c1969f9db5b202148
pedro <pedro@openbsd.org>
2006-06-24 21:47:50 +0000

    Shave 8 bytes off struct udf_mnt

--- sys/isofs/udf/udf.h
+++ sys/isofs/udf/udf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udf.h,v 1.3 2006/06/24 15:09:17 pedro Exp $	*/
+/*	$OpenBSD: udf.h,v 1.4 2006/06/24 21:47:50 pedro Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Scott Long <scottl@freebsd.org>
@@ -56,7 +56,6 @@ struct udf_mnt {
 	int			bmask;
 	uint32_t		part_start;
 	uint32_t		part_len;
-	uint64_t		root_id;
 	struct vnode		*root_vp;
 	struct long_ad		root_icb;
 	LIST_HEAD(udf_hash_lh, udf_node)	*hashtbl;
