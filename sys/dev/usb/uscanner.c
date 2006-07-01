7a8bfb812597c601287d26a05573e39a3489f279
miod <miod@openbsd.org>
2006-07-01 10:33:12 +0000

    More Canon scanners, from NetBSD via Matthieu Baptiste.

--- sys/dev/usb/uscanner.c
+++ sys/dev/usb/uscanner.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uscanner.c,v 1.20 2006/06/23 06:27:12 miod Exp $ */
+/*	$OpenBSD: uscanner.c,v 1.21 2006/07/01 10:33:12 miod Exp $ */
 /*	$NetBSD: uscanner.c,v 1.40 2003/01/27 00:32:44 wiz Exp $	*/
 
 /*
@@ -107,6 +107,9 @@ static const struct uscan_info uscanner_devs[] = {
 
   /* Canon */
  {{ USB_VENDOR_CANON, USB_PRODUCT_CANON_N656U }, 0 },
+ {{ USB_VENDOR_CANON, USB_PRODUCT_CANON_N670U }, 0 },
+ {{ USB_VENDOR_CANON, USB_PRODUCT_CANON_N1220U }, 0 },
+ {{ USB_VENDOR_CANON, USB_PRODUCT_CANON_N1240U }, 0 },
 
   /* Kye */
  {{ USB_VENDOR_KYE, USB_PRODUCT_KYE_VIVIDPRO }, 0 },
