4f300d63908bfee02eed7f17903e1405c5182ed6
brad <brad@openbsd.org>
2006-07-08 18:47:34 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.1092 2006/07/08 06:09:33 brad Exp 
+ *	OpenBSD: pcidevs,v 1.1093 2006/07/08 18:45:17 brad Exp 
  */
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
 
@@ -4740,7 +4740,7 @@ static const struct pci_known_product pci_known_products[] = {
 	},
 	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82870P2_PPB,
-	    "82870P2 PCI-PCI",
+	    "82870P2 PCIX-PCIX",
 	},
 	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82870P2_IOxAPIC,
@@ -5963,7 +5963,11 @@ static const struct pci_known_product pci_known_products[] = {
 	    "RCU31L I2O RAID",
 	},
 	{
-	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_21152,
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_21152_1,
+	    "S21152BB PCI-PCI",
+	},
+	{
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_21152_2,
 	    "S21152BB PCI-PCI",
 	},
 	{
