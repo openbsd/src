336db8467677430029f85107a53cc31469d2ce37
deraadt <deraadt@openbsd.org>
2006-07-12 05:55:22 +0000

    fake structs to let kernels compile on architectures that some german companies do not use.  Mickey, why did you break the tree for us like this, all after not showing the diffs to lots of people? Are you that special that you can get away with this?  Do you really want to bet on that, again?

--- sys/ddb/db_interface.h
+++ sys/ddb/db_interface.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_interface.h,v 1.8 2006/07/11 21:17:58 mickey Exp $	*/
+/*	$OpenBSD: db_interface.h,v 1.9 2006/07/12 05:55:22 deraadt Exp $	*/
 /*	$NetBSD: db_interface.h,v 1.1 1996/02/05 01:57:03 christos Exp $	*/
 
 /*
@@ -44,6 +44,9 @@ void db_show_all_procs(db_expr_t, int, db_expr_t, char *);
 
 /* kern/kern_timeout.c */
 void db_show_callout(db_expr_t, int, db_expr_t, char *);
+
+struct mount;
+struct vnode;
 
 /* kern/vfs_subr.c */
 void vfs_buf_print(struct buf *, int, int (*)(const char *, ...));
