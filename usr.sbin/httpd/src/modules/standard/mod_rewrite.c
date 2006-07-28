b28f9eb938e2e5f585f139c1756a9fb8a5b61a7e
henning <henning@openbsd.org>
2006-07-28 13:52:30 +0000

    A vulnerability has been reported in Apache HTTP Server, which potentially can be exploited by malicious people to compromise a vulnerable system. The vulnerability is caused by a off-by-one error in mod_rewrite and can be exploited to cause a one-byte buffer overflow. http://secunia.com/advisories/21197/ report "Alexey E. Suslikov" <cruel@texnika.com.ua>, the obvious fix mailed in by Stuart Henderson <stu@spacehopper.org>

--- usr.sbin/httpd/src/modules/standard/mod_rewrite.c
+++ usr.sbin/httpd/src/modules/standard/mod_rewrite.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mod_rewrite.c,v 1.24 2005/02/09 12:13:10 henning Exp $ */
+/*	$OpenBSD: mod_rewrite.c,v 1.25 2006/07/28 13:52:30 henning Exp $ */
 
 /* ====================================================================
  * The Apache Software License, Version 1.1
@@ -2762,7 +2762,7 @@ static char *escape_absolute_uri(ap_pool *p, char *uri, unsigned scheme)
             int c = 0;
 
             token[0] = cp = ap_pstrdup(p, cp);
-            while (*cp && c < 5) {
+            while (*cp && c < 4) {
                 if (*cp == '?') {
                     token[++c] = cp + 1;
                     *cp = '\0';
