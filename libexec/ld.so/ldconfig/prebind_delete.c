909f35838c62c1cffaef78e877ac7e83156b6d60
drahn <drahn@openbsd.org>
2006-07-28 21:38:05 +0000

    Only stop processing prebind deletion if an actual error occurs.

--- libexec/ld.so/ldconfig/prebind_delete.c
+++ libexec/ld.so/ldconfig/prebind_delete.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: prebind_delete.c,v 1.7 2006/06/26 23:26:12 drahn Exp $ */
+/* $OpenBSD: prebind_delete.c,v 1.8 2006/07/28 21:38:05 drahn Exp $ */
 
 /*
  * Copyright (c) 2006 Dale Rahn <drahn@dalerahn.com>
@@ -72,8 +72,9 @@ strip_file_or_dir(char *name)
 		break;
 	default:
 		; /* links and other files we skip */
+		ret = 0;
 	}
-	return -1;
+	return ret;
 }
 
 int
