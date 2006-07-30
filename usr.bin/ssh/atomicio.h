b177e5b3be9d24605530261334bd006cb2c887d8
stevesk <stevesk@openbsd.org>
2006-07-30 20:15:19 +0000

    order includes to KNF

--- usr.bin/ssh/atomicio.h
+++ usr.bin/ssh/atomicio.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: atomicio.h,v 1.8 2006/04/16 00:52:55 djm Exp $ */
+/* $OpenBSD: atomicio.h,v 1.9 2006/07/30 20:15:19 stevesk Exp $ */
 
 /*
  * Copyright (c) 2006 Damien Miller.  All rights reserved.
@@ -30,8 +30,9 @@
 #define _ATOMICIO_H
 
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/uio.h>
+
+#include <unistd.h>
 
 /*
  * Ensure all of data on socket comes through. f==read || f==vwrite
