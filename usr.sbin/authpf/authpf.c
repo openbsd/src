19338c8e6f73dda50f768ad529e7d01806a7d967
dhartmei <dhartmei@openbsd.org>
2006-08-09 16:21:39 +0000

    handle SIGQUIT instead of SIGSTOP, from Stefan Krah

--- usr.sbin/authpf/authpf.c
+++ usr.sbin/authpf/authpf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: authpf.c,v 1.98 2006/03/17 22:00:27 deraadt Exp $	*/
+/*	$OpenBSD: authpf.c,v 1.99 2006/08/09 16:21:39 dhartmei Exp $	*/
 
 /*
  * Copyright (C) 1998 - 2002 Bob Beck (beck@openbsd.org).
@@ -308,7 +308,7 @@ main(int argc, char *argv[])
 	signal(SIGALRM, need_death);
 	signal(SIGPIPE, need_death);
 	signal(SIGHUP, need_death);
-	signal(SIGSTOP, need_death);
+	signal(SIGQUIT, need_death);
 	signal(SIGTSTP, need_death);
 	while (1) {
 		printf("\r\nHello %s. ", luser);
