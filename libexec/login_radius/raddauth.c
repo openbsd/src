f39c81ec92279bc4da6a8ce398c938c0c3d430f4
millert <millert@openbsd.org>
2006-08-14 13:53:34 +0000

    Fix typo introduced in rev 1.16; fixes "login" authentication style.

--- libexec/login_radius/raddauth.c
+++ libexec/login_radius/raddauth.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: raddauth.c,v 1.20 2005/11/19 23:02:25 millert Exp $	*/
+/*	$OpenBSD: raddauth.c,v 1.21 2006/08/14 13:53:34 millert Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997 Berkeley Software Design, Inc. All rights reserved.
@@ -189,7 +189,7 @@ raddauth(char *username, char *class, char *style, char *challenge,
 			passwd = getpass("Password:");
 	} else
 		passwd = password;
-	if (password == NULL)
+	if (passwd == NULL)
 		passwd = "";
 
 	if ((v = login_getcapstr(lc, "radius-server", NULL, NULL)) == NULL){
