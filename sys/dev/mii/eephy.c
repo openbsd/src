e8083195370cf85d6a856a0871ce1c28f9c495f7
brad <brad@openbsd.org>
2006-08-21 03:06:06 +0000

    another id.

--- sys/dev/mii/eephy.c
+++ sys/dev/mii/eephy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eephy.c,v 1.28 2006/07/28 15:50:17 brad Exp $	*/
+/*	$OpenBSD: eephy.c,v 1.29 2006/08/21 03:06:06 brad Exp $	*/
 /*
  * Principal Author: Parag Patel
  * Copyright (c) 2001
@@ -100,6 +100,8 @@ static const struct mii_phydesc eephys[] = {
 	  MII_STR_MARVELL_E1000_6 },
 	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1000_7,
 	  MII_STR_MARVELL_E1000_7 },
+	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1000_8,
+	  MII_STR_MARVELL_E1000_8 },
 	{ MII_OUI_MARVELL,		MII_MODEL_MARVELL_E1111,
 	  MII_STR_MARVELL_E1111 },
 
