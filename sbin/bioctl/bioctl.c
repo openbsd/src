e42b60f25b1b58239213bf5412b98b5c03b92e88
deraadt <deraadt@openbsd.org>
2006-08-28 02:12:59 +0000

    Except I disagree strongly, and I was not asked

--- sbin/bioctl/bioctl.c
+++ sbin/bioctl/bioctl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bioctl.c,v 1.46 2006/08/27 22:25:16 mickey Exp $       */
+/* $OpenBSD: bioctl.c,v 1.47 2006/08/28 02:12:59 deraadt Exp $       */
 
 /*
  * Copyright (c) 2004, 2005 Marco Peereboom
@@ -258,7 +258,7 @@ bio_inq(char *name)
 
 		if (!volheader) {
 			volheader = 1;
-			printf("%-7s %-10s %14s %-8s\n",
+			printf("%-7s %-10s %-14s %-8s\n",
 			    "Volume", "Status", "Size", "Device");
 		}
 
