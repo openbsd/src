1e0d71c056dfe07618acb66c889da3ad3ea959a9
brad <brad@openbsd.org>
2006-10-02 17:07:28 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.1154 2006/10/01 00:20:12 brad Exp 
+ *	OpenBSD: pcidevs,v 1.1156 2006/10/02 17:06:23 brad Exp 
  */
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
 
@@ -9343,6 +9343,18 @@ static const struct pci_known_product pci_known_products[] = {
 	    "HT-2000 PCIE",
 	},
 	{
+	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_2100_PCIE_1,
+	    "HT-2100 PCIE",
+	},
+	{
+	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_2100_PCIE_2,
+	    "HT-2100 PCIE",
+	},
+	{
+	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_2100_PCIE_3,
+	    "HT-2100 PCIE",
+	},
+	{
 	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_OSB4,
 	    "OSB4",
 	},
@@ -9375,7 +9387,7 @@ static const struct pci_known_product pci_known_products[] = {
 	    "CSB6 RAID/IDE",
 	},
 	{
-	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_1000_IDE,
+	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_1000_IDE_1,
 	    "HT-1000 IDE",
 	},
 	{
@@ -9427,8 +9439,8 @@ static const struct pci_known_product pci_known_products[] = {
 	    "HT-1000 SATA",
 	},
 	{
-	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_SATA,
-	    "SATA",
+	    PCI_VENDOR_RCC, PCI_PRODUCT_RCC_HT_1000_IDE_2,
+	    "HT-1000 IDE",
 	},
 	{
 	    PCI_VENDOR_RENDITION, PCI_PRODUCT_RENDITION_V1000,
