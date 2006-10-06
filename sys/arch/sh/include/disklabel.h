d88bc18adc740f5c0bc79bc868284beefd1aed1b
krw <krw@openbsd.org>
2006-10-06 22:26:29 +0000

    Sync with recent disklabel.h changes. Zap dkbad.
    "Free commits" miod@

--- sys/arch/sh/include/disklabel.h
+++ sys/arch/sh/include/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.h,v 1.1.1.1 2006/10/06 21:02:55 miod Exp $	*/
+/*	$OpenBSD: disklabel.h,v 1.2 2006/10/06 22:26:29 krw Exp $	*/
 /*	$NetBSD: disklabel.h,v 1.2 2001/11/25 19:02:03 thorpej Exp $	*/
 
 /*
@@ -52,7 +52,6 @@
 #define MAXPARTITIONS	16		/* number of partitions */
 #define RAW_PART	2		/* raw partition: XX?c */
 
-#include <sys/dkbad.h>
 #if 0
 #include <arm/disklabel_acorn.h>
 #include <sys/disklabel_mbr.h>
@@ -106,7 +105,6 @@ struct dos_partition {
 
 struct cpu_disklabel {
 	struct dos_partition dosparts[NDOSPART];
-	struct dkbad bad;
 };
 
 #endif /* _ARM_DISKLABEL_H_ */
