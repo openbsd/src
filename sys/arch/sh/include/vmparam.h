4813dc9a7c6f6e3144913968f6efd90d918486c9
drahn <drahn@openbsd.org>
2006-10-14 18:40:06 +0000

    ps uses KERNBASE so define it to the correct value (pointed out by miod)

--- sys/arch/sh/include/vmparam.h
+++ sys/arch/sh/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmparam.h,v 1.1.1.1 2006/10/06 21:02:55 miod Exp $	*/
+/*	$OpenBSD: vmparam.h,v 1.2 2006/10/14 18:40:06 drahn Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.17 2006/03/04 01:55:03 uwe Exp $	*/
 
 /*-
@@ -50,6 +50,7 @@
 
 /* top of stack */
 #define	USRSTACK		VM_MAXUSER_ADDRESS
+#define	KERNBASE		VM_MIN_KERNEL_ADDRESS
 
 /* Virtual memory resoruce limit. */
 #define	MAXTSIZ			(64 * 1024 * 1024)	/* max text size */
