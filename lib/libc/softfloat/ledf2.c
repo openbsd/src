08daf0c23d348e44ad9e5fdcb608738a28c9c404
drahn <drahn@openbsd.org>
2006-11-06 15:11:37 +0000

    Copy MI softfloat bits from arm to here, so that they can be shared.

---
+++ lib/libc/softfloat/ledf2.c
@@ -0,0 +1,22 @@
+/*	$OpenBSD: ledf2.c,v 1.1 2006/11/06 15:11:37 drahn Exp $	*/
+/* $NetBSD: ledf2.c,v 1.1 2000/06/06 08:15:06 bjh21 Exp $ */
+
+/*
+ * Written by Ben Harris, 2000.  This file is in the Public Domain.
+ */
+
+#include "softfloat-for-gcc.h"
+#include "milieu.h"
+#include "softfloat.h"
+
+#include <sys/cdefs.h>
+
+flag __ledf2(float64, float64);
+
+flag
+__ledf2(float64 a, float64 b)
+{
+
+	/* libgcc1.c says 1 - (a <= b) */
+	return 1 - float64_le(a, b);
+}
