c7005bdb2440a4b159e1505f1cd3613e7751d5a1
otto <otto@openbsd.org>
2006-11-12 21:18:28 +0000

    fix infinity, taking into account endianess. ok drahn@ miod@

--- lib/libc/arch/sh/gen/infinity.c
+++ lib/libc/arch/sh/gen/infinity.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: infinity.c,v 1.1.1.1 2006/10/10 22:07:10 miod Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.2 2006/11/12 21:18:28 otto Exp $	*/
 
 /* infinity.c */
 
@@ -6,4 +6,8 @@
 
 /* bytes for +Infinity on a SH4 FPU (double precision) */
 char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
-    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+    { 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
+#else
+    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 }
+#endif
