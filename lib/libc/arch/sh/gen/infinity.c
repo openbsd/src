1d8cadea0c225af7f8a22d0781f25b2a8f3474a7
otto <otto@openbsd.org>
2006-11-13 07:00:59 +0000

    missing ;

--- lib/libc/arch/sh/gen/infinity.c
+++ lib/libc/arch/sh/gen/infinity.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: infinity.c,v 1.2 2006/11/12 21:18:28 otto Exp $	*/
+/*	$OpenBSD: infinity.c,v 1.3 2006/11/13 07:00:59 otto Exp $	*/
 
 /* infinity.c */
 
@@ -9,5 +9,5 @@ char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
 #if _BYTE_ORDER == _LITTLE_ENDIAN
     { 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
 #else
-    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 }
+    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
 #endif
