56dfe5a4d6c3415111bb55c04abaa75ace8039d8
deraadt <deraadt@openbsd.org>
2006-11-15 20:21:46 +0000

    remove double define; dunceor@gmail.com

--- usr.sbin/ripd/ripd.h
+++ usr.sbin/ripd/ripd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ripd.h,v 1.3 2006/10/31 23:43:11 michele Exp $ */
+/*	$OpenBSD: ripd.h,v 1.4 2006/11/15 20:21:46 deraadt Exp $ */
 
 /*
  * Copyright (c) 2004 Esben Norby <norby@openbsd.org>
@@ -31,7 +31,6 @@
 #define	CONF_FILE		"/etc/ripd.conf"
 #define	RIPD_SOCKET		"/var/run/ripd.sock"
 #define	RIPD_USER		"_ripd"
-#define	READ_BUF_SIZE		65535
 
 #define	NBR_HASHSIZE		128
 #define	NBR_IDSELF		1
