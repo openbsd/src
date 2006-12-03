9055441ba6a4d2e629f72ba1f1e62d69306a3ca6
damien <damien@openbsd.org>
2006-12-03 17:51:41 +0000

    don't include <net80211/ieee80211_amrr.h> rate control is handled by firmware.

--- sys/dev/usb/if_uath.c
+++ sys/dev/usb/if_uath.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_uath.c,v 1.13 2006/12/03 16:09:21 damien Exp $	*/
+/*	$OpenBSD: if_uath.c,v 1.14 2006/12/03 17:51:41 damien Exp $	*/
 
 /*-
  * Copyright (c) 2006
@@ -62,7 +62,6 @@
 #include <netinet/ip.h>
 
 #include <net80211/ieee80211_var.h>
-#include <net80211/ieee80211_amrr.h>
 #include <net80211/ieee80211_radiotap.h>
 
 #include <dev/rndvar.h>
