569fac94e21364033fc205fc43781440c037fa8f
miod <miod@openbsd.org>
2006-12-10 16:12:41 +0000

    Fix typo in intersil clock programming.

--- sys/arch/sparc/sparc/clock.c
+++ sys/arch/sparc/sparc/clock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock.c,v 1.23 2006/06/26 20:21:02 kettenis Exp $	*/
+/*	$OpenBSD: clock.c,v 1.24 2006/12/10 16:12:41 miod Exp $	*/
 /*	$NetBSD: clock.c,v 1.52 1997/05/24 20:16:05 pk Exp $ */
 
 /*
@@ -917,7 +917,7 @@ oclk_set_secs(secs)
 
 	clock_secs_to_ymdhms(secs, &dt);
 	
-	idt.dt_hour = dt.dt_sec;
+	idt.dt_hour = dt.dt_hour;
 	idt.dt_min = dt.dt_min;
 	idt.dt_sec = dt.dt_sec;
 	idt.dt_month = dt.dt_mon;
