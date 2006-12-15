2a597364610fbeb34c40209ba3c19b41c60d9093
jmc <jmc@openbsd.org>
2006-12-15 12:48:29 +0000

    sort options; sync usage;

--- sbin/mount_msdos/mount_msdos.c
+++ sbin/mount_msdos/mount_msdos.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mount_msdos.c,v 1.20 2006/12/15 03:04:24 krw Exp $	*/
+/*	$OpenBSD: mount_msdos.c,v 1.21 2006/12/15 12:48:29 jmc Exp $	*/
 /*	$NetBSD: mount_msdos.c,v 1.16 1996/10/24 00:12:50 cgd Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: mount_msdos.c,v 1.20 2006/12/15 03:04:24 krw Exp $";
+static char rcsid[] = "$OpenBSD: mount_msdos.c,v 1.21 2006/12/15 12:48:29 jmc Exp $";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -216,6 +216,6 @@ usage(void)
 {
 
 	fprintf(stderr,
-	    "usage: mount_msdos [-o options] [-u user] [-g group] [-m mask] bdev dir\n");
+	    "usage: mount_msdos [-9lsx] [-g gid] [-m mask] [-o options] [-u uid] special node\n");
 	exit(1);
 }
