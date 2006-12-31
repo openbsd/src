d452b86bc41eaed43e1a1ac3ba3c7edf2dd99e58
matthieu <matthieu@openbsd.org>
2006-12-31 10:38:18 +0000

    - XORG_PREFIX is now X11BASE - define X11ETC here to avoid bootstrap problems noticed by Pierre-Yves   Dampure and Frederick C. Druseikis.

--- share/mk/bsd.xorg.mk
+++ share/mk/bsd.xorg.mk
@@ -1,5 +1,7 @@
+# $OpenBSD: bsd.xorg.mk,v 1.2 2006/12/31 10:38:18 matthieu Exp $
 .include <bsd.own.mk>
-XORG_PREFIX?=   /usr/X11R6
-.if exists(${XORG_PREFIX}/share/mk/bsd.xorg.mk)
-. include       "${XORG_PREFIX}/share/mk/bsd.xorg.mk"
+X11BASE?=	/usr/X11R6
+X11ETC?=	/etc/X11
+.if exists(${X11BASE}/share/mk/bsd.xorg.mk)
+. include	"${X11BASE}/share/mk/bsd.xorg.mk"
 .endif
