64832050d3d0d07646ab7e348f0f5eca748b6d3f
deraadt <deraadt@openbsd.org>
2007-01-08 19:37:04 +0000

    term 3 here can be dropped too

--- usr.sbin/ospfd/in_cksum.c
+++ usr.sbin/ospfd/in_cksum.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_cksum.c,v 1.5 2006/09/26 11:56:22 claudio Exp $	*/
+/*	$OpenBSD: in_cksum.c,v 1.6 2007/01/08 19:37:04 deraadt Exp $	*/
 /*	$NetBSD: in_cksum.c,v 1.3 1995/04/22 13:53:48 cgd Exp $	*/
 
 /*
@@ -17,11 +17,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratory and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
