0305bdea8399736a8e779615c923d7111bdfd907
otto <otto@openbsd.org>
2007-01-14 19:13:35 +0000

    ragge@netbsd fixed the copyright, thanks. ok miod@

--- sys/arch/vax/vax/random.s
+++ sys/arch/vax/vax/random.s
@@ -1,5 +1,31 @@
-/*	$OpenBSD: random.s,v 1.4 2005/05/06 18:55:02 miod Exp $ */
-/*	$NetBSD: random.s,v 1.2 1994/10/26 08:03:24 cgd Exp $	*/
+/*	$OpenBSD: random.s,v 1.5 2007/01/14 19:13:35 otto Exp $ */
+/*	$NetBSD: random.S,v 1.4 2007/01/14 13:26:18 ragge Exp $	*/
+
+/*
+ * Copyright (c) 1994 Ludd, University of Lule}, Sweden. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1990,1993 The Regents of the University of California.
@@ -28,11 +54,6 @@
  * understanding of the algorithm.  It's trickier than you think.  If
  * you do change it, make sure that its 10,000'th invocation returns
  * 1043618065.
- *
- * This implementation Copyright (c) 1994 Ludd, University of Lule}, Sweden
- * All rights reserved.
- *
- * All bugs subject to removal without further notice.
  *
  * Here is easier-to-decipher pseudocode:
  *
