e22ebbd4d607c8ae587cfbecef6ccaf1892a4539
mk <mk@openbsd.org>
2007-01-27 16:15:03 +0000

    Unbreak build with ACPI_DEBUG.

--- sys/dev/acpi/acpidock.c
+++ sys/dev/acpi/acpidock.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpidock.c,v 1.10 2007/01/27 11:48:33 mk Exp $ */
+/* $OpenBSD: acpidock.c,v 1.11 2007/01/27 16:15:03 mk Exp $ */
 /*
  * Copyright (c) 2006,2007 Michael Knudsen <mk@openbsd.org>
  *
@@ -213,12 +213,12 @@ acpidock_eject(struct acpidock_softc *sc)
 	if (aml_evalname(sc->sc_acpi, sc->sc_devnode->parent, "_EJ0", 1, &cmd,
 	    &res) != 0) {
 		/* XXX */
-		dnprintf(15, "%s: _EJ0 %d failed\n", DEVNAME(sc), dock);
+		dnprintf(15, "%s: _EJ0 failed\n", DEVNAME(sc));
 
 		aml_freevalue(&res);
 		return (0);
 	} else {
-		dnprintf(15, "%s: _EJ0 %d successful\n", DEVNAME(sc), dock);
+		dnprintf(15, "%s: _EJ0 successful\n", DEVNAME(sc));
 
 		sc->sc_docked = 0;
 		aml_freevalue(&res);
