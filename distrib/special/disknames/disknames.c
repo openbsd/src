f8c9788be2c05c645ced0a6a3e48fc9145308f1b
deraadt <deraadt@openbsd.org>
2007-02-14 22:53:01 +0000

    append \n at end of line, and make this more normal userland code

--- distrib/special/disknames/disknames.c
+++ distrib/special/disknames/disknames.c
@@ -1,11 +1,14 @@
 #include <sys/param.h>
 #include <sys/sysctl.h>
-#include <sys/malloc.h>
+#include <sys/uio.h>
 
+#include <unistd.h>
 #include <stdlib.h>
 
 int
-main() {
+main(int argc, char *argv[])
+{
+	struct iovec iov[2];
 	int mib[2];
 	size_t len;
 	char *p;
@@ -14,10 +17,14 @@ main() {
 	mib[1] = HW_DISKNAMES;
 
 	if (sysctl(mib, 2, NULL, &len, NULL, 0) != -1)
-		if ((p = (char *)malloc(len)) != NULL)
+		if ((p = malloc(len)) != NULL)
 			if (sysctl(mib, 2, p, &len, NULL, 0) != -1) {
-				write(1, p, len);
-				_exit(0);
-			}	
-	_exit(1);
+				iov[0].iov_base = p;
+				iov[0].iov_len = len;
+				iov[1].iov_base = "\n";
+				iov[1].iov_len = 1;
+				writev(STDOUT_FILENO, iov, 2);
+				exit(0);
+			}
+	exit(1);
 }	
