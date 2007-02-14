f408c4777b5cd1613cee77d41c341833f0c272a8
krw <krw@openbsd.org>
2007-02-14 23:05:59 +0000

    /* not $*.

--- distrib/special/disknames/disknames.c
+++ distrib/special/disknames/disknames.c
@@ -1,4 +1,4 @@
-$*	$OpenBSD: disknames.c,v 1.3 2007/02/14 23:02:03 krw Exp $	*/
+/*	$OpenBSD: disknames.c,v 1.4 2007/02/14 23:05:59 krw Exp $	*/
 
 /*
  * Copyright (c) 2007 Kenneth R. Westerback <krw@openbsd.org>
