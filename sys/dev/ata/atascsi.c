f3068372f577bad17eecbf62c13f421c5a91cdf0
dlg <dlg@openbsd.org>
2007-02-19 12:04:01 +0000

    code to set up a global pool for allocating ata_xfers out of, and for accessing that pool.

--- sys/dev/ata/atascsi.c
+++ sys/dev/ata/atascsi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atascsi.c,v 1.5 2007/02/19 11:57:45 dlg Exp $ */
+/*	$OpenBSD: atascsi.c,v 1.6 2007/02/19 12:04:01 dlg Exp $ */
 
 /*
  * Copyright (c) 2007 David Gwynne <dlg@openbsd.org>
@@ -23,6 +23,7 @@
 #include <sys/malloc.h>
 #include <sys/device.h>
 #include <sys/proc.h>
+#include <sys/pool.h>
 #include <sys/queue.h>
 
 #include <scsi/scsi_all.h>
@@ -43,8 +44,6 @@ struct atascsi {
 	struct scsibus_softc	*as_scsibus;
 };
 
-int		atascsi_probe(struct atascsi *, int);
-
 int		atascsi_cmd(struct scsi_xfer *);
 
 /* template */
@@ -60,6 +59,8 @@ struct scsi_device atascsi_device = {
 	NULL, NULL, NULL, NULL
 };
 
+int		atascsi_probe(struct atascsi *, int);
+
 int		atascsi_disk_cmd(struct scsi_xfer *);
 int		atascsi_disk_inq(struct scsi_xfer *);
 int		atascsi_disk_capacity(struct scsi_xfer *);
@@ -69,6 +70,16 @@ int		atascsi_disk_sense(struct scsi_xfer *);
 int		atascsi_atapi_cmd(struct scsi_xfer *);
 
 int		atascsi_stuffup(struct scsi_xfer *);
+
+
+int		ata_running = 0;
+struct pool	ata_xfer_pool;
+
+void		ata_init(void);
+void		ata_destroy(void);
+
+struct ata_xfer	*ata_get_xfer(int);
+void		ata_put_xfer(struct ata_xfer *);
 
 struct atascsi *
 atascsi_attach(struct device *self, struct atascsi_attach_args *aaa)
@@ -249,3 +260,40 @@ atascsi_stuffup(struct scsi_xfer *xs)
 	splx(s);
 	return (COMPLETE);
 }
+
+void
+ata_init(void)
+{
+	if (ata_running++)
+		return;
+
+	pool_init(&ata_xfer_pool, sizeof(struct ata_xfer), 0, 0, 0, "xapl",
+	    NULL);
+}
+
+void
+ata_destroy(void)
+{
+	if (--ata_running)
+		return;
+
+	pool_destroy(&ata_xfer_pool);
+}
+
+struct ata_xfer *
+ata_get_xfer(int nosleep)
+{
+	struct ata_xfer		*xa;
+
+	xa = pool_get(&ata_xfer_pool, nosleep ? PR_NOWAIT : PR_WAITOK);
+
+	return (xa);
+}
+
+void
+ata_put_xfer(struct ata_xfer *xa)
+{
+	pool_put(&ata_xfer_pool, xa);
+}
+
+
