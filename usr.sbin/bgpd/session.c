d30aca78e1891f9a03b63f97b423f0fcca06909f
henning <henning@openbsd.org>
2007-02-22 08:26:45 +0000

    do not try to set IP_TTL on non-INET listening sockets, it's not going to work on INET6... non-fatal error, so in pratice this just avoids a misleading error msg

--- usr.sbin/bgpd/session.c
+++ usr.sbin/bgpd/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.268 2007/01/26 17:40:49 claudio Exp $ */
+/*	$OpenBSD: session.c,v 1.269 2007/02/22 08:26:45 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004, 2005 Henning Brauer <henning@openbsd.org>
@@ -150,8 +150,8 @@ setup_listeners(u_int *la_cnt)
 		}
 
 		/* set ttl to 255 so that ttl-security works */
-		if (setsockopt(la->fd, IPPROTO_IP, IP_TTL, &ttl,
-		    sizeof(ttl)) == -1) {
+		if (la->sa.ss_family == AF_INET && setsockopt(la->fd,
+		    IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1) {
 			log_warn("setup_listeners setsockopt TTL");
 			continue;
 		}
