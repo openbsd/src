363ee7a4df9f3484dd012ddccf65e5cee740f473
deraadt <deraadt@openbsd.org>
2007-02-23 15:18:59 +0000

    support for "Auto-Submitted: no" from Tamas TEVESZ; small tweak by millert, ok millert (man page update being done by jmc)

--- usr.bin/vacation/vacation.c
+++ usr.bin/vacation/vacation.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vacation.c,v 1.24 2006/08/14 15:49:28 millert Exp $	*/
+/*	$OpenBSD: vacation.c,v 1.25 2007/02/23 15:18:59 deraadt Exp $	*/
 /*	$NetBSD: vacation.c,v 1.7 1995/04/29 05:58:27 cgd Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)vacation.c	8.2 (Berkeley) 1/26/94";
 #endif
-static char rcsid[] = "$OpenBSD: vacation.c,v 1.24 2006/08/14 15:49:28 millert Exp $";
+static char rcsid[] = "$OpenBSD: vacation.c,v 1.25 2007/02/23 15:18:59 deraadt Exp $";
 #endif /* not lint */
 
 /*
@@ -206,6 +206,24 @@ readheaders(void)
 	cont = tome = 0;
 	while (fgets(buf, sizeof(buf), stdin) && *buf != '\n')
 		switch (*buf) {
+		case 'A':		/* "Auto-Submitted:" */
+		case 'a':
+			cont = 0;
+			if (strncasecmp(buf, "Auto-Submitted:", 15))
+				break;
+			for (p = buf + 15; *p && isspace(*p); ++p)
+				;
+			/*
+			 * RFC 3834 section 2:
+			 * Automatic responses SHOULD NOT be issued in response
+			 * to any message which contains an Auto-Submitted
+			 * header where the field has any value other than "no".
+			 */
+			if (!*p)
+				exit(0);
+			if (strncasecmp(p, "no", 2))
+				exit(0);
+			break;
 		case 'F':		/* "From " */
 		case 'f':
 			cont = 0;
@@ -242,13 +260,9 @@ readheaders(void)
 		case 'P':		/* "Precedence:" */
 		case 'p':
 			cont = 0;
-			if (strncasecmp(buf, "Precedence", 10) ||
-			    (buf[10] != ':' && buf[10] != ' ' &&
-			    buf[10] != '\t'))
+			if (strncasecmp(buf, "Precedence:", 11))
 				break;
-			if (!(p = strchr(buf, ':')))
-				break;
-			while (*++p && isspace(*p))
+			for (p = buf + 11; *p && isspace(*p); ++p)
 				;
 			if (!*p)
 				break;
