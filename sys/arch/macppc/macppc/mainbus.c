a0d1b6b5a9749e97bc6c560b3eefde66d3b5aaf8
gwk <gwk@openbsd.org>
2007-02-25 04:13:48 +0000

    Change how we determin if were running on a new world versus old world machine for the purposes of printing model information durring boot and the hw.vendor/product sysctls. Turns out the apple didnt use the same format in the early imacs as they did in the latter new world machines resulting in NULL strings as found by drahn@.  In this case and all future cases where we dont understand the format the default is to print whats found in the name property without interpertation.
    ok drahn@

--- sys/arch/macppc/macppc/mainbus.c
+++ sys/arch/macppc/macppc/mainbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mainbus.c,v 1.16 2007/02/12 22:22:39 gwk Exp $	*/
+/*	$OpenBSD: mainbus.c,v 1.17 2007/02/25 04:13:48 gwk Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -93,23 +93,18 @@ mbattach(struct device *parent, struct device *self, void *aux)
 	len = OF_getprop(node, "compatible", name, sizeof(name));
 	if (len > 1) {
 		name[len] = '\0';
-		if ((strncmp(t, name, strlen(t))) == 0) {
-			/* New World Macintosh */
+		/* Old World Macintosh */
+		if ((strncmp(name, "AAPL", 4)) == 0) {
+			hw_vendor = "Apple Computer, Inc.";
+			slen = strlen(t) + strlen(name) - 3;
+			if ((hw_prod = malloc(slen, M_DEVBUF, M_NOWAIT)) != NULL) {
+				snprintf(hw_prod, slen, "%s %s", t, name + 5);
+				free(t, M_DEVBUF);
+			}
+		} else {
+			/* New World Macintosh or Unknown */
 			hw_vendor = "Apple Computer, Inc.";
 			hw_prod = t;
-		} else {
-			/* Old World Macintosh */
-			if ((strncmp(name, "AAPL", 4)) == 0) {
-				hw_vendor = "Apple Computer, Inc.";
-				slen = strlen(t) + strlen(name) - 3;
-				if ((hw_prod = malloc(slen, M_DEVBUF,
-				    M_NOWAIT)) != NULL) {
-					snprintf(hw_prod, slen, "%s %s", t,
-					    name + 5);
-					free(t, M_DEVBUF);
-				}
-			}
-
 		}
 	}
 	printf(": model %s\n", hw_prod);
