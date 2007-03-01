3d64423f1d2301d294b963c3194554ffea816956
pvalchev <pvalchev@openbsd.org>
2007-03-01 16:33:08 +0000

    strip off -beta; ok deraadt

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.81 2007/02/12 13:10:02 henning Exp $
+#	$OpenBSD: newvers.sh,v 1.82 2007/03/01 16:33:08 pvalchev Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -67,10 +67,10 @@ ost="OpenBSD"
 osr="4.1"
 
 cat >vers.c <<eof
-#define STATUS "-beta"
-#if 0
-#define STATUS "-current"
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"
+#define STATUS "-current"
 #endif
 
 const char ostype[] = "${ost}";
