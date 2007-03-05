9253bd1e3d39b4568f5a3c834b7ec5f0e9d35414
henning <henning@openbsd.org>
2007-03-05 16:40:10 +0000

    better error msg

--- usr.sbin/bgpctl/irr_prefix.c
+++ usr.sbin/bgpctl/irr_prefix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: irr_prefix.c,v 1.9 2007/03/05 16:16:42 henning Exp $ */
+/*	$OpenBSD: irr_prefix.c,v 1.10 2007/03/05 16:40:10 henning Exp $ */
 
 /*
  * Copyright (c) 2007 Henning Brauer <henning@openbsd.org>
@@ -83,7 +83,7 @@ prefixset_addmember(char *s)
 
 	if ((len = inet_net_pton(AF_INET, s, &pfx->addr.in,
 	    sizeof(pfx->addr.in))) == -1)
-		err(1, "inet_net_pton %s", s);
+		err(1, "prefixset_addmember inet_net_pton \"%s\"", s);
 
 	pfx->af = AF_INET;
 	pfx->len = len;
