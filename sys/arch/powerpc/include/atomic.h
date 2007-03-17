b1e784d09ac71603114d8954462b43f6d0596907
kettenis <kettenis@openbsd.org>
2007-03-17 22:10:04 +0000

    Implement proper atomic.h for powerpc.  With help from art@ and drahn@.
    ok drahn@

--- sys/arch/powerpc/include/atomic.h
+++ sys/arch/powerpc/include/atomic.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atomic.h,v 1.2 2007/02/19 17:18:43 deraadt Exp $	*/
+/*	$OpenBSD: atomic.h,v 1.3 2007/03/17 22:10:04 kettenis Exp $	*/
 
 /* Public Domain */
 
@@ -10,13 +10,27 @@
 static __inline void
 atomic_setbits_int(__volatile unsigned int *uip, unsigned int v)
 {
-	*uip |= v;
+	unsigned int tmp;
+
+	__asm volatile (
+	    "1:	lwarx	%0, 0, %2	\n"
+	    "	or	%0, %1, %0	\n"
+	    "	stwcx.	%0, 0, %2	\n"
+	    "	bne-	1b		\n"
+	    "	sync" : "=&r" (tmp) : "r" (v), "r" (uip) : "memory");
 }
 
 static __inline void
 atomic_clearbits_int(__volatile unsigned int *uip, unsigned int v)
 {
-	*uip &= ~v;
+	unsigned int tmp;
+
+	__asm volatile (
+	    "1:	lwarx	%0, 0, %2	\n"
+	    "	andc	%0, %0, %1	\n"
+	    "	stwcx.	%0, 0, %2	\n"
+	    "	bne-	1b		\n"
+	    "	sync" : "=&r" (tmp) : "r" (v), "r" (uip) : "memory");
 }
 
 #endif /* defined(_KERNEL) */
