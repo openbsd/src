77e61ec49ce86bb62e563e0956275a7da2e836ed
kettenis <kettenis@openbsd.org>
2007-03-22 19:26:27 +0000

    Move powerpc to __HAVE_MUTEX.  With help from drahn@.  Tested by nick@, xsa@, deraadt@.
    "reads right" deraadt@

--- sys/arch/powerpc/include/_types.h
+++ sys/arch/powerpc/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _types.h,v 1.3 2007/03/20 20:59:53 kettenis Exp $	*/
+/*	$OpenBSD: _types.h,v 1.4 2007/03/22 19:26:28 kettenis Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -117,5 +117,6 @@ typedef	void *			__wctype_t;
 
 /* Feature test macros */
 #define __HAVE_CPUINFO
+#define __HAVE_MUTEX
 
 #endif	/* _POWERPC__TYPES_H_ */
