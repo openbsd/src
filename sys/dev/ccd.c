8779fc788f5baa9fbb795cd852fb6ca95c3fe993
deraadt <deraadt@openbsd.org>
2007-03-30 15:19:05 +0000

    accept interleave of 0; pr 5431, my patch matches the author's

--- sys/dev/ccd.c
+++ sys/dev/ccd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ccd.c,v 1.68 2006/11/29 15:03:42 krw Exp $	*/
+/*	$OpenBSD: ccd.c,v 1.69 2007/03/30 15:19:05 deraadt Exp $	*/
 /*	$NetBSD: ccd.c,v 1.33 1996/05/05 04:21:14 thorpej Exp $	*/
 
 /*-
@@ -1151,7 +1151,7 @@ ccdioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 			return (EBUSY);
 
 		if (ccio->ccio_ndisks == 0 || ccio->ccio_ndisks > INT_MAX ||
-		    ccio->ccio_ileave <= 0)
+		    ccio->ccio_ileave < 0)
 			return (EINVAL);
 
 		if ((error = ccdlock(cs)) != 0)
