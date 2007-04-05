03f5b7b7df4f1dc31e933ee1b91b1476ce452aea
tedu <tedu@openbsd.org>
2007-04-05 02:32:45 +0000

    laurence tratt says his cable mostly works with this ok millert

--- sys/dev/usb/if_cdce.c
+++ sys/dev/usb/if_cdce.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_cdce.c,v 1.23 2007/02/23 01:19:15 drahn Exp $ */
+/*	$OpenBSD: if_cdce.c,v 1.24 2007/04/05 02:32:45 tedu Exp $ */
 
 /*
  * Copyright (c) 1997, 1998, 1999, 2000-2003 Bill Paul <wpaul@windriver.com>
@@ -101,6 +101,7 @@ Static const struct cdce_type cdce_devs[] = {
     {{ USB_VENDOR_GMATE, USB_PRODUCT_GMATE_YP3X00 }, CDCE_NO_UNION },
     {{ USB_VENDOR_NETCHIP, USB_PRODUCT_NETCHIP_ETHERNETGADGET }, CDCE_NO_UNION },
     {{ USB_VENDOR_COMPAQ, USB_PRODUCT_COMPAQ_IPAQLINUX }, CDCE_NO_UNION },
+    {{ USB_VENDOR_AMBIT, USB_PRODUCT_AMBIT_NTL_250 }, CDCE_NO_UNION },
 };
 #define cdce_lookup(v, p) ((const struct cdce_type *)usb_lookup(cdce_devs, v, p))
 
