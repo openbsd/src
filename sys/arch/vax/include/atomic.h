1b4bdea42ea4a04751c2638ed13c9c9309859c56
miod <miod@openbsd.org>
2007-04-05 17:33:50 +0000

    Wrap bit operations between splhigh()/splx() for atomicity wrt interrupts. Not enough for multiprocessor, but we're not there yet anyway.

--- sys/arch/vax/include/atomic.h
+++ sys/arch/vax/include/atomic.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atomic.h,v 1.2 2007/02/19 17:18:43 deraadt Exp $	*/
+/*	$OpenBSD: atomic.h,v 1.3 2007/04/05 17:33:50 miod Exp $	*/
 
 /* Public Domain */
 
@@ -7,16 +7,26 @@
 
 #if defined(_KERNEL)
 
+#include <machine/mtpr.h>
+
 static __inline void
 atomic_setbits_int(__volatile unsigned int *uip, unsigned int v)
 {
+	int s;
+
+	s = splhigh();
 	*uip |= v;
+	splx(s);
 }
 
 static __inline void
 atomic_clearbits_int(__volatile unsigned int *uip, unsigned int v)
 {
+	int s;
+
+	s = splhigh();
 	*uip &= ~v;
+	splx(s);
 }
 
 #endif /* defined(_KERNEL) */
