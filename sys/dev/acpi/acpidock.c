3ebb5e6b5ee1d76400be2259f136966cd143b6cd
mk <mk@openbsd.org>
2007-04-06 01:12:06 +0000

    Fix a strange typo and don't set sc_docked in acpidock_dockctl().  It's being handled by acpidock_status().
    From Matthew R. Dempsky who had another chunk in his diff that I didn't
    include.  It's correct, but it seems to cause some strange regression on
    my X60 that I need to look into first.

--- sys/dev/acpi/acpidock.c
+++ sys/dev/acpi/acpidock.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpidock.c,v 1.17 2007/02/15 21:02:17 mk Exp $ */
+/* $OpenBSD: acpidock.c,v 1.18 2007/04/06 01:12:06 mk Exp $ */
 /*
  * Copyright (c) 2006,2007 Michael Knudsen <mk@openbsd.org>
  *
@@ -141,7 +141,7 @@ acpidock_docklock(struct acpidock_softc *sc, int lock)
 	cmd.type = AML_OBJTYPE_INTEGER;
 	if (aml_evalname(sc->sc_acpi, sc->sc_devnode, "_LCK", 1, &cmd,
 	    &res) != 0) {
-		dnprintf(20, "%s: _LCD %d failed\n", DEVNAME(sc), lock);
+		dnprintf(20, "%s: _LCK %d failed\n", DEVNAME(sc), lock);
 
 		rv = 0;
 	} else {
@@ -178,8 +178,6 @@ acpidock_dockctl(struct acpidock_softc *sc, int dock)
 	}
 
 	aml_freevalue(&res);
-
-	sc->sc_docked = rv;
 
 	return rv;
 }
