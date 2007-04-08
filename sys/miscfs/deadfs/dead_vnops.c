61a98e28c0652e4a6506d1c94a766b6573c32cab
pedro <pedro@openbsd.org>
2007-04-08 16:37:10 +0000

    ansify and deregister, no binary change, okay mickey@ bluhm@

--- sys/miscfs/deadfs/dead_vnops.c
+++ sys/miscfs/deadfs/dead_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dead_vnops.c,v 1.16 2007/03/21 17:29:32 thib Exp $	*/
+/*	$OpenBSD: dead_vnops.c,v 1.17 2007/04/08 16:37:10 pedro Exp $	*/
 /*	$NetBSD: dead_vnops.c,v 1.16 1996/02/13 13:12:48 mycroft Exp $	*/
 
 /*
@@ -131,8 +131,7 @@ struct vnodeopv_desc dead_vnodeop_opv_desc =
  */
 /* ARGSUSED */
 int
-dead_lookup(v)
-	void *v;
+dead_lookup(void *v)
 {
 	struct vop_lookup_args /* {
 		struct vnode * a_dvp;
@@ -149,10 +148,8 @@ dead_lookup(v)
  */
 /* ARGSUSED */
 int
-dead_open(v)
-	void *v;
+dead_open(void *v)
 {
-
 	return (ENXIO);
 }
 
@@ -161,8 +158,7 @@ dead_open(v)
  */
 /* ARGSUSED */
 int
-dead_read(v)
-	void *v;
+dead_read(void *v)
 {
 	struct vop_read_args /* {
 		struct vnode *a_vp;
@@ -186,8 +182,7 @@ dead_read(v)
  */
 /* ARGSUSED */
 int
-dead_write(v)
-	void *v;
+dead_write(void *v)
 {
 	struct vop_write_args /* {
 		struct vnode *a_vp;
@@ -206,8 +201,7 @@ dead_write(v)
  */
 /* ARGSUSED */
 int
-dead_ioctl(v)
-	void *v;
+dead_ioctl(void *v)
 {
 	struct vop_ioctl_args /* {
 		struct vnode *a_vp;
@@ -225,8 +219,7 @@ dead_ioctl(v)
 
 /* ARGSUSED */
 int
-dead_poll(v)
-	void *v;
+dead_poll(void *v)
 {
 #if 0
 	struct vop_poll_args /* {
@@ -246,10 +239,8 @@ dead_poll(v)
  * Just call the device strategy routine
  */
 int
-dead_strategy(v)
-	void *v;
+dead_strategy(void *v)
 {
-
 	struct vop_strategy_args /* {
 		struct buf *a_bp;
 	} */ *ap = v;
@@ -269,8 +260,7 @@ dead_strategy(v)
  * Wait until the vnode has finished changing state.
  */
 int
-dead_lock(v)
-	void *v;
+dead_lock(void *v)
 {
 	struct vop_lock_args /* {
 		struct vnode *a_vp;
@@ -289,8 +279,7 @@ dead_lock(v)
  * Wait until the vnode has finished changing state.
  */
 int
-dead_bmap(v)
-	void *v;
+dead_bmap(void *v)
 {
 	struct vop_bmap_args /* {
 		struct vnode *a_vp;
@@ -310,8 +299,7 @@ dead_bmap(v)
  */
 /* ARGSUSED */
 int
-dead_print(v)
-	void *v;
+dead_print(void *v)
 {
 	printf("tag VT_NON, dead vnode\n");
 	return 0;
@@ -322,10 +310,8 @@ dead_print(v)
  */
 /*ARGSUSED*/
 int
-dead_ebadf(v)
-	void *v;
+dead_ebadf(void *v)
 {
-
 	return (EBADF);
 }
 
@@ -334,10 +320,8 @@ dead_ebadf(v)
  */
 /*ARGSUSED*/
 int
-dead_badop(v)
-	void *v;
+dead_badop(void *v)
 {
-
 	panic("dead_badop called");
 	/* NOTREACHED */
 }
@@ -347,8 +331,7 @@ dead_badop(v)
  * in a state of change.
  */
 int
-chkvnlock(vp)
-	register struct vnode *vp;
+chkvnlock(struct vnode *vp)
 {
 	int locked = 0;
 
