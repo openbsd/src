f0665ece41f3ccdb6dcc4fbaaf74bfcd609f205f
miod <miod@openbsd.org>
2007-04-10 18:10:31 +0000

    ``prob ably'' can be written in one word.

--- sys/arch/vax/if/sgecreg.h
+++ sys/arch/vax/if/sgecreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sgecreg.h,v 1.2 2003/06/02 23:27:57 millert Exp $	*/
+/*	$OpenBSD: sgecreg.h,v 1.3 2007/04/10 18:10:31 miod Exp $	*/
 /*	$NetBSD: sgecreg.h,v 1.1 1999/08/08 11:41:29 ragge Exp $	*/
 /*
  * Copyright (c) 1988 Regents of the University of California.
@@ -35,7 +35,7 @@
 
 
 /* Driver for SGEC (second generation Ethernet controller) chip, type DC-541,
-   found on the KA670 (and prob ably other) CPU.
+   found on the KA670 (and probably other) CPU.
 
    17 May 1998...Jay Maynard, jmaynard@phoenix.net
 */
