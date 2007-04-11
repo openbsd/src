34d9ef7e9e44cc6357b63ebdbe4317f4ef2db8bf
thib <thib@openbsd.org>
2007-04-11 12:18:54 +0000

    remove duplicate prototype;
    ok pedro@

--- sys/nfs/nfs_var.h
+++ sys/nfs/nfs_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nfs_var.h,v 1.23 2004/08/03 17:11:48 marius Exp $	*/
+/*	$OpenBSD: nfs_var.h,v 1.24 2007/04/11 12:18:54 thib Exp $	*/
 /*	$NetBSD: nfs_var.h,v 1.3 1996/02/18 11:53:54 fvdl Exp $	*/
 
 /*
@@ -70,7 +70,6 @@ int nfs_asyncio(struct buf *);
 int nfs_doio(struct buf *, struct proc *);
 
 /* nfs_boot.c */
-int nfs_boot_init(struct nfs_diskless *, struct proc *);
 int nfs_boot_init(struct nfs_diskless *, struct proc *);
 
 /* nfs_node.c */
