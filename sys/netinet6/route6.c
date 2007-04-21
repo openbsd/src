41432a3896fc6b0d11a224eabd10a2b721d63f0b
itojun <itojun@openbsd.org>
2007-04-21 16:47:42 +0000

    did you know that IPv6 packets are actually carried by turtles? because turtles are slow but reliable and trustworthy, packets stays on the net for a long period of time.  bigger turtles can stay much longer. that is the hidden secret reason for the name of KAME project (i'm lying).
    j> some IETFers need to be sent to bondage/SM club and spanked/whipped
    j> by thousands of dominas and then chopped into million peaces by samurai
    j> swords.
    t> maybe that is what they actually want, and that is why they
    t> fucked RFC1883 and put rosemary's baby into RFC2460.
    j> I am king of IETF now, and tomorrow i may become beggar on the IETF venue
    j> hotel corridor.
    
    http://www.secdev.org/conf/IPv6_RH_security-csw07.pdf
    
    ok by myself, deraadt@, <samurais at kame.net>

--- sys/netinet6/route6.c
+++ sys/netinet6/route6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: route6.c,v 1.14 2006/12/27 05:12:13 itojun Exp $	*/
+/*	$OpenBSD: route6.c,v 1.15 2007/04/21 16:47:42 itojun Exp $	*/
 /*	$KAME: route6.c,v 1.22 2000/12/03 00:54:00 itojun Exp $	*/
 
 /*
@@ -44,7 +44,9 @@
 
 #include <netinet/icmp6.h>
 
+#if 0
 static int ip6_rthdr0(struct mbuf *, struct ip6_hdr *, struct ip6_rthdr0 *);
+#endif
 
 int
 route6_input(mp, offp, proto)
@@ -64,6 +66,7 @@ route6_input(mp, offp, proto)
 	}
 
 	switch (rh->ip6r_type) {
+#if 0
 	case IPV6_RTHDR_TYPE_0:
 		rhlen = (rh->ip6r_len + 1) << 3;
 		if (rh->ip6r_segleft == 0)
@@ -84,6 +87,7 @@ route6_input(mp, offp, proto)
 		if (ip6_rthdr0(m, ip6, (struct ip6_rthdr0 *)rh))
 			return (IPPROTO_DONE);
 		break;
+#endif
 	default:
 		/* unknown routing type */
 		if (rh->ip6r_segleft == 0) {
@@ -100,6 +104,7 @@ route6_input(mp, offp, proto)
 	return (rh->ip6r_nxt);
 }
 
+#if 0
 /*
  * Type0 routing header processing
  *
@@ -180,3 +185,4 @@ ip6_rthdr0(m, ip6, rh0)
 	m_freem(m);
 	return (-1);
 }
+#endif
