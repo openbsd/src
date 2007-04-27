2330ec0023c9730bf406bf66762e02c70a6c69bd
dlg <dlg@openbsd.org>
2007-04-27 01:57:16 +0000

    remove duplicate defines.
    found by lint, ok deraadt@

--- sys/arch/amd64/include/specialreg.h
+++ sys/arch/amd64/include/specialreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: specialreg.h,v 1.7 2007/02/17 17:35:43 tom Exp $	*/
+/*	$OpenBSD: specialreg.h,v 1.8 2007/04/27 01:57:16 dlg Exp $	*/
 /*	$NetBSD: specialreg.h,v 1.1 2003/04/26 18:39:48 fvdl Exp $	*/
 /*	$NetBSD: x86/specialreg.h,v 1.2 2003/04/25 21:54:30 fvdl Exp $	*/
 
@@ -620,24 +620,3 @@
 #define	K7_BP1_MATCH			0xdd
 #define	K7_BP2_MATCH			0xde
 #define	K7_BP3_MATCH			0xdf
-
-/*
- * Extended Feature Enable Register of the x86-64
- */
-
-#define MSR_EFER	0xc0000080
-
-#define EFER_SCE	0x00000001	/* SYSCALL extension */
-#define EFER_LME	0x00000100	/* Long Mode Enabled */
-#define EFER_LMA	0x00000400	/* Long Mode Active */
-#define EFER_NXE	0x00000800	/* No-Execute Enable */
-#define EFER_FFXSR	0x00004000	/* Fast FXSAVE/FXRSTOR */
-
-#define MSR_STAR	0xc0000081		/* 32 bit syscall gate addr */
-#define MSR_LSTAR	0xc0000082		/* 64 bit syscall gate addr */
-#define MSR_CSTAR	0xc0000083		/* compat syscall gate addr */
-#define MSR_SFMASK	0xc0000084		/* flags to clear on syscall */
-
-#define MSR_FSBASE		0xc0000100	/* 64bit offset for fs: */
-#define MSR_GSBASE		0xc0000101	/* 64bit offset for gs: */
-#define MSR_KERNELGSBASE	0xc0000102	/* storage for swapgs ins */
