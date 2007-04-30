5e3b30454e14bf890931f1c7da49d7a049a57044
deraadt <deraadt@openbsd.org>
2007-04-30 00:02:12 +0000

    fix dmesg; ok dlg

--- sys/dev/pci/if_tht.c
+++ sys/dev/pci/if_tht.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tht.c,v 1.86 2007/04/29 21:44:32 dlg Exp $ */
+/*	$OpenBSD: if_tht.c,v 1.87 2007/04/30 00:02:12 deraadt Exp $ */
 
 /*
  * Copyright (c) 2007 David Gwynne <dlg@openbsd.org>
@@ -788,7 +788,7 @@ tht_attach(struct device *parent, struct device *self, void *aux)
 	if_attach(ifp);
 	ether_ifattach(ifp);
 
-	printf(" address %s\n", ether_sprintf(sc->sc_ac.ac_enaddr));
+	printf(": address %s\n", ether_sprintf(sc->sc_ac.ac_enaddr));
 
 	mountroothook_establish(tht_mountroot, sc);
 }
