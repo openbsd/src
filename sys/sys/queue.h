284c345fe237b40bfff79f186bbcdeda1541da4b
pedro <pedro@openbsd.org>
2007-04-30 18:42:34 +0000

    Enable QUEUE_MACRO_DEBUG on DIAGNOSTIC kernels. Input and okays from krw@, millert@, otto@, deraadt@, miod@.

--- sys/sys/queue.h
+++ sys/sys/queue.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue.h,v 1.31 2005/11/25 08:06:25 otto Exp $	*/
+/*	$OpenBSD: queue.h,v 1.32 2007/04/30 18:42:34 pedro Exp $	*/
 /*	$NetBSD: queue.h,v 1.11 1996/05/16 05:17:14 mycroft Exp $	*/
 
 /*
@@ -82,7 +82,7 @@
  * For details on the use of these macros, see the queue(3) manual page.
  */
 
-#ifdef QUEUE_MACRO_DEBUG
+#if defined(QUEUE_MACRO_DEBUG) || (defined(_KERNEL) && defined(DIAGNOSTIC))
 #define _Q_INVALIDATE(a) (a) = ((void *)-1)
 #else
 #define _Q_INVALIDATE(a)
