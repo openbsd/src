d90f86bf1b307bc40a6e450d9f5c4170ed2f2d3e
espie <espie@openbsd.org>
2007-05-01 18:09:44 +0000

    allows us to register anything, and not just a class, with the factory.

--- usr.sbin/pkg_add/OpenBSD/PackingElement.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingElement.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackingElement.pm,v 1.92 2007/05/01 18:07:00 espie Exp $
+# $OpenBSD: PackingElement.pm,v 1.93 2007/05/01 18:09:44 espie Exp $
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -45,11 +45,14 @@ sub Factory
 
 sub register_with_factory 
 {
-	my ($class, $k) = @_;
+	my ($class, $k, $o) = @_;
 	if (!defined $k) {
 		$k = $class->keyword;
 	}
-	$keyword{$k} = $class;
+	if (!defined $o) {
+		$o = $class;
+	} 
+	$keyword{$k} = $o;
 }
 
 sub category() { 'items' }
