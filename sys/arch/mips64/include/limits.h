b5759c39e524a389f824e26bbdad24235e479cd3
kettenis <kettenis@openbsd.org>
2007-05-07 20:51:07 +0000

    Remove symbols already defined in <sys/limits.h>; unbreaks build on sgi.
    ok miod

--- sys/arch/mips64/include/limits.h
+++ sys/arch/mips64/include/limits.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: limits.h,v 1.4 2006/01/06 22:48:47 millert Exp $	*/
+/*	$OpenBSD: limits.h,v 1.5 2007/05/07 20:51:07 kettenis Exp $	*/
 
 /*
  * Copyright (c) 1988, 1993
@@ -36,40 +36,11 @@
 
 #include <sys/cdefs.h>
 
-#define	CHAR_BIT	8		/* number of bits in a char */
 #define	MB_LEN_MAX	6		/* Allow 31 bit UTF2 */
-
-/*
- * According to ANSI (section 2.2.4.2), the values below must be usable by
- * #if preprocessing directives.  Additionally, the expression must have the
- * same type as would an expression that is an object of the corresponding
- * type converted according to the integral promotions.  The subtraction for
- * INT_MIN and LONG_MIN is so the value is not unsigned; 2147483648 is an
- * unsigned int for 32-bit two's complement ANSI compilers (section 3.1.3.2).
- * These numbers work for pcc as well.  The UINT_MAX and ULONG_MAX values
- * are written as hex so that GCC will be quiet about large integer constants.
- */
-#define	SCHAR_MAX	0x7f		/* min value for a signed char */
-#define	SCHAR_MIN	(-0x7f-1)	/* max value for a signed char */
-
-#define	UCHAR_MAX	0xffU		/* max value for an unsigned char */
-#define	CHAR_MAX	0x7f		/* max value for a char */
-#define	CHAR_MIN	(-0x7f-1)	/* min value for a char */
-
-#define	USHRT_MAX	0xffffU		/* max value for an unsigned short */
-#define	SHRT_MAX	0x7fff		/* max value for a short */
-#define	SHRT_MIN	(-0x7fff-1)	/* min value for a short */
-
-#define	UINT_MAX	0xffffffffU	/* max value for an unsigned int */
-#define	INT_MAX		0x7fffffff	/* max value for an int */
-#define	INT_MIN		(-0x7fffffff-1)	/* min value for an int */
-
-#define	UID_MAX		UINT_MAX	/* max value for a uid_t */
-#define	GID_MAX		UINT_MAX	/* max value for a gid_t */
 
 #if __POSIX_VISIBLE || __XPG_VISIBLE
 #ifndef	SIZE_MAX
-#define SIZE_MAX	ULONG_MAX	/* max value for a size_t */
+#define	SIZE_MAX	ULONG_MAX	/* max value for a size_t */
 #endif
 #define	SSIZE_MAX	LONG_MAX	/* max value for a ssize_t */
 #endif
@@ -77,11 +48,10 @@
 #if __BSD_VISIBLE
 #define	SIZE_T_MAX	ULONG_MAX	/* max value for a size_t (historic) */
 
-/* GCC requires that quad constants be written as expressions. */
-#define	UQUAD_MAX	((u_quad_t)0-1)	/* max value for a uquad_t */
-					/* max value for a quad_t */
-#define	QUAD_MAX	((quad_t)(UQUAD_MAX >> 1))
-#define	QUAD_MIN	(-QUAD_MAX-1)	/* min value for a quad_t */
+/* Quads and longs are the same on mips64 */
+#define	UQUAD_MAX	(ULONG_MAX)	/* max value for a uquad_t */
+#define	QUAD_MAX	(LONG_MAX)	/* max value for a quad_t */
+#define	QUAD_MIN	(LONG_MIN)	/* min value for a quad_t */
 
 #endif /* __BSD_VISIBLE */
 
