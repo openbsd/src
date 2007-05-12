1f6b10d186fe79f94dde4ba9feeb067bf2214112
krw <krw@openbsd.org>
2007-05-12 12:19:23 +0000

    Use VNDLABELDEV() and not DISKLABELDEV() in writedisklabel() since VNDLABELDEV() is used in readdisklabel(). VNDLABELDEV() preserves the vndsimple() property encoded in the device.

--- sys/dev/vnd.c
+++ sys/dev/vnd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vnd.c,v 1.73 2007/04/29 18:59:37 krw Exp $	*/
+/*	$OpenBSD: vnd.c,v 1.74 2007/05/12 12:19:23 krw Exp $	*/
 /*	$NetBSD: vnd.c,v 1.26 1996/03/30 23:06:11 christos Exp $	*/
 
 /*
@@ -914,7 +914,7 @@ vndioctl(dev_t dev, u_long cmd, caddr_t addr, int flag, struct proc *p)
 		    vnd->sc_dk.dk_cpulabel);
 		if (error == 0) {
 			if (cmd == DIOCWDINFO)
-				error = writedisklabel(DISKLABELDEV(dev),
+				error = writedisklabel(VNDLABELDEV(dev),
 				    vndstrategy, vnd->sc_dk.dk_label,
 				    vnd->sc_dk.dk_cpulabel);
 		}
