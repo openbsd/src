ed6d0e48213d2a02d1f5431ceef74b5e9770fe60
dlg <dlg@openbsd.org>
2007-05-19 10:24:18 +0000

    command ids for mailbox commands

--- sys/dev/ic/slireg.h
+++ sys/dev/ic/slireg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: slireg.h,v 1.3 2007/05/16 09:31:01 dlg Exp $ */
+/*	$OpenBSD: slireg.h,v 1.4 2007/05/19 10:24:18 dlg Exp $ */
 
 /*
  * Copyright (c) 2007 David Gwynne <dlg@openbsd.org>
@@ -16,11 +16,17 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+/*
+ * PCI BARs
+ */
 #define SLI_PCI_BAR_SLIM		0x10
 #define SLI_PCI_BAR_REGISTER		0x18
 #define SLI_PCI_BAR_BIU			0x20
 #define SLI_PCI_BAR_REGISTER_IO		0x24
 
+/*
+ * Registers in the REGISTER BAR
+ */
 #define SLI_REG_HA		0x0 /* Host Attention */
 #define  SLI_REG_HA_ERATT		(1<<31)
 #define  SLI_REG_HA_MBATT		(1<<30)
@@ -91,3 +97,53 @@
 				    "\034INITMB" "\033INITHBI" "\005R3INT" \
 				    "\004R2INT" "\003R1INT" "\002R0INT" \
 				    "\001MBINT"
+
+/*
+ * Mailbox commands
+ */
+#define SLI_CMD_SHUTDOWN	0x00
+#define SLI_CMD_LOAD_SM		0x01
+#define SLI_CMD_READ_NV		0x02
+#define SLI_CMD_WRITE_NV	0x03
+#define SLI_CMD_RUN_BIU_DIAG	0x04
+#define SLI_CMD_INIT_LINK	0x05
+#define SLI_CMD_DOWN_LINK	0x06
+#define SLI_CMD_CONFIG_LINK	0x07
+#define SLI_CMD_CONFIG_RING	0x09
+#define SLI_CMD_RESET_RING	0x0a
+#define SLI_CMD_READ_CONFIG	0x0b
+#define SLI_CMD_READ_RCONFIG	0x0c
+#define SLI_CMD_READ_SPARM	0x0d
+#define SLI_CMD_READ_STATUS	0x0e
+#define SLI_CMD_READ_RPI	0x0f
+#define SLI_CMD_READ_XRI	0x10
+#define SLI_CMD_READ_REV	0x11
+#define SLI_CMD_READ_LNK_STAT	0x12
+#define SLI_CMD_REG_LOGIN	0x13
+#define SLI_CMD_UNREG_LOGIN	0x14
+#define SLI_CMD_READ_LA		0x15
+#define SLI_CMD_CLEAR_LA	0x16
+#define SLI_CMD_DUMP_MEMORY	0x17
+#define SLI_CMD_DUMP_CONTEXT	0x18
+#define SLI_CMD_RUN_DIAGS	0x19
+#define SLI_CMD_RESTART		0x1a
+#define SLI_CMD_UPDATE_CFG	0x1b
+#define SLI_CMD_DOWN_LOAD	0x1c
+#define SLI_CMD_DEL_LD_ENTRY	0x1d
+#define SLI_CMD_RUN_PROGRAM	0x1e
+#define SLI_CMD_SET_MASK	0x20
+#define SLI_CMD_SEL_SLIM	0x21
+#define SLI_CMD_UNREG_D_ID	0x23
+#define SLI_CMD_KILL_BOARD	0x24
+#define SLI_CMD_CONFIG_FARP	0x25
+#define SLI_CMD_LOAD_AREA	0x81
+#define SLI_CMD_RUN_BIU_DIAG64	0x84
+#define SLI_CMD_CONFIG_PORT	0x88
+#define SLI_CMD_READ_SPARM64	0x8d
+#define SLI_CMD_READ_RPI64	0x8f
+#define SLI_CMD_REG_LOGIN64	0x93
+#define SLI_CMD_READ_LA64	0x95
+#define SLI_CMD_FLAGS_WR_ULA	0x98
+#define SLI_CMD_FLSET_DEBUG	0x99
+#define SLI_CMD_LOAD_EXP_ROM	0x9c
+#define SLI_CMD_MAX_CMDS	0x9d
