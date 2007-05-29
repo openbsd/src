878b4c6a9a929c154ca5cc66d7fd80c9b7ffc678
david <david@openbsd.org>
2007-05-29 17:58:02 +0000

    the MSIZE mbuf constant moved from MD param.h to MI param.h ok deraadt@ millert@

--- regress/sys/kern/mbuf/mbuftest.c
+++ regress/sys/kern/mbuf/mbuftest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mbuftest.c,v 1.1 2003/04/23 21:45:57 jason Exp $	*/
+/*	$OpenBSD: mbuftest.c,v 1.2 2007/05/29 17:58:02 david Exp $	*/
 
 /*
  * Copyright (c) 2003 Jason L. Wright (jason@thought.net)
@@ -34,7 +34,7 @@
  */
 
 #include <sys/types.h>
-#include <machine/param.h>
+#include <sys/param.h>
 #include <sys/mbuf.h>
 #include <stdio.h>
 #include <err.h>
