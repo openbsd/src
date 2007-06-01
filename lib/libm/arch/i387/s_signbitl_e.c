8c5eef9afb56f4b4bae33c6b93bafabc86659201
jason <jason@openbsd.org>
2007-06-01 05:52:42 +0000

    add __signbitl for i387 (not used yet)

---
+++ lib/libm/arch/i387/s_signbitl_e.c
@@ -0,0 +1,19 @@
+/*	$OpenBSD: s_signbitl_e.c,v 1.1 2007/06/01 05:52:42 jason Exp $	*/
+
+/*
+ * Written by Jason L. Wright (jason@thought.net) in 2007 and placed
+ * into the public domain.
+ */
+
+#include "math.h"
+#include "math_private.h"
+
+/* __signbitl for extended long double */
+
+int
+__signbitl(long double l)
+{
+	ieee_extended_shape_type e;
+	e.value = l;
+	return (e.parts.msw & 0x8000);
+}
