1142e0a0005759beba9fc08e2f46d38da04cfc86
todd <todd@openbsd.org>
2007-06-01 18:19:40 +0000

    Move a useless warning to the only case that uses it (-f) from canacar@ ok beck@

--- usr.sbin/tcpdump/tcpdump.c
+++ usr.sbin/tcpdump/tcpdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcpdump.c,v 1.55 2006/06/16 16:55:46 deraadt Exp $	*/
+/*	$OpenBSD: tcpdump.c,v 1.56 2007/06/01 18:19:40 todd Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -26,7 +26,7 @@ static const char copyright[] =
     "@(#) Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997\n\
 The Regents of the University of California.  All rights reserved.\n";
 static const char rcsid[] =
-    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/tcpdump.c,v 1.55 2006/06/16 16:55:46 deraadt Exp $ (LBL)";
+    "@(#) $Header: /home/cvs/src/usr.sbin/tcpdump/tcpdump.c,v 1.56 2007/06/01 18:19:40 todd Exp $ (LBL)";
 #endif
 
 /*
@@ -423,7 +423,8 @@ main(int argc, char **argv)
 
 		/* state: STATE_BPF */
 		if (pcap_lookupnet(device, &localnet, &netmask, ebuf)) {
-			warning("%s", ebuf);
+			if (fflag)
+				warning("%s", ebuf);
 			localnet = 0;
 			netmask = 0;
 		}
