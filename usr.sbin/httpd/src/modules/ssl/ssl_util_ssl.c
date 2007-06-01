2bdbc5a6b3b7103820177218c9448e294e8a82d3
pvalchev <pvalchev@openbsd.org>
2007-06-01 21:53:34 +0000

    paranoid snprintf length calculation to avoid a possible buffer overflow in SSL session id parsing (by reaching a negative size arg) ok henning

--- usr.sbin/httpd/src/modules/ssl/ssl_util_ssl.c
+++ usr.sbin/httpd/src/modules/ssl/ssl_util_ssl.c
@@ -503,7 +503,7 @@ char *SSL_SESSION_id2sz(unsigned char *id, int idlen)
     cp = str;
     for (n = 0; n < idlen && n < SSL_MAX_SSL_SESSION_ID_LENGTH; n++) {
         ap_snprintf(cp, sizeof(str)-(cp-str), "%02X", id[n]);
-        cp += 2;
+        cp += strlen(cp);
     }
     *cp = NUL;
     return str;
