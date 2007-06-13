7cbc328344a4b6622e57a33a4495f121c2489ac9
claudio <claudio@openbsd.org>
2007-06-13 17:23:36 +0000

    Don't RB_REMOVE() vertices in area_del() this will be done by vertex_free(). With this the RDE no longer crashes on shutdown.

--- usr.sbin/ospfd/area.c
+++ usr.sbin/ospfd/area.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: area.c,v 1.5 2007/05/29 22:08:25 claudio Exp $ */
+/*	$OpenBSD: area.c,v 1.6 2007/06/13 17:23:36 claudio Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Esben Norby <norby@openbsd.org>
@@ -68,7 +68,6 @@ area_del(struct area *area)
 
 	for (v = RB_MIN(lsa_tree, &area->lsa_tree); v != NULL; v = nv) {
 		nv = RB_NEXT(lsa_tree, &area->lsa_tree, v);
-		RB_REMOVE(lsa_tree, &area->lsa_tree, v);
 		vertex_free(v);
 	}
 
