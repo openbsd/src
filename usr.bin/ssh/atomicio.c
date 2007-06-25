99289681ababee1e441b94b2751651ad22a40d36
dtucker <dtucker@openbsd.org>
2007-06-25 12:02:27 +0000

    Include <poll.h> like the man page says rather than <sys/poll.h>.  ok djm@

--- usr.bin/ssh/atomicio.c
+++ usr.bin/ssh/atomicio.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: atomicio.c,v 1.24 2007/06/19 02:04:43 djm Exp $ */
+/* $OpenBSD: atomicio.c,v 1.25 2007/06/25 12:02:27 dtucker Exp $ */
 /*
  * Copyright (c) 2006 Damien Miller. All rights reserved.
  * Copyright (c) 2005 Anil Madhavapeddy. All rights reserved.
@@ -28,9 +28,9 @@
 
 #include <sys/param.h>
 #include <sys/uio.h>
-#include <sys/poll.h>
 
 #include <errno.h>
+#include <poll.h>
 #include <string.h>
 #include <unistd.h>
 
