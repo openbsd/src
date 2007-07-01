bbd71afa53b509be7424f73796e7caf7bd8c9794
miod <miod@openbsd.org>
2007-07-01 19:09:20 +0000

    Move more fpe-related stuff in #ifdef ARMFPU blocks.

--- sys/arch/arm/arm/cpu.c
+++ sys/arch/arm/arm/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.7 2006/05/29 17:01:42 drahn Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.8 2007/07/01 19:09:20 miod Exp $	*/
 /*	$NetBSD: cpu.c,v 1.56 2004/04/14 04:01:49 bsh Exp $	*/
 
 
@@ -73,9 +73,11 @@ void identify_arm_cpu(struct device *dv, struct cpu_info *);
 void
 cpu_attach(struct device *dv)
 {
+#ifdef ARMFPE
 	int usearmfpe;
 
 	usearmfpe = 1;	/* when compiled in, its enabled by default */
+#endif
 
 	curcpu()->ci_dev = dv;
 
