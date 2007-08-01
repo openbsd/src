8d52b3c96316105908ae7236d81fb2c377ce6fdd
fgsch <fgsch@openbsd.org>
2007-08-01 10:08:56 +0000

    escape [, ], and ? as well. from cbiere@netbsd. millert@ ok.

--- bin/ksh/edit.c
+++ bin/ksh/edit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: edit.c,v 1.31 2005/12/11 20:31:21 otto Exp $	*/
+/*	$OpenBSD: edit.c,v 1.32 2007/08/01 10:08:56 fgsch Exp $	*/
 
 /*
  * Command line editing - common code
@@ -841,7 +841,7 @@ x_escape(const char *s, size_t len, int (*putbuf_func) (const char *, size_t))
 	int rval=0;
 
 	for (add = 0, wlen = len; wlen - add > 0; add++) {
-		if (strchr("\\$(){}*&;#|<>\"'`", s[add]) || strchr(ifs, s[add])) {
+		if (strchr("\\$(){}[]?*&;#|<>\"'`", s[add]) || strchr(ifs, s[add])) {
 			if (putbuf_func(s, add) != 0) {
 				rval = -1;
 				break;
