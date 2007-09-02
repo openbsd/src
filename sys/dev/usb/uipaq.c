2c0ee96d65ade5078d982e3fb0f43005b5a462cc
jsg <jsg@openbsd.org>
2007-09-02 04:20:25 +0000

    Add support for Alltel UTStarcom PPC6700. Tested by Sam Fourman Jr. <sfourman@gmail.com>

--- sys/dev/usb/uipaq.c
+++ sys/dev/usb/uipaq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipaq.c,v 1.12 2007/06/14 10:11:16 mbalmer Exp $	*/
+/*	$OpenBSD: uipaq.c,v 1.13 2007/09/02 04:20:25 jsg Exp $	*/
 
 /*
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -121,11 +121,12 @@ struct uipaq_type {
 };
 
 static const struct uipaq_type uipaq_devs[] = {
+	{{ USB_VENDOR_ASUS, USB_PRODUCT_ASUS_MYPAL_A730} , 0},
+	{{ USB_VENDOR_CASIO, USB_PRODUCT_CASIO_BE300} , 0},
+	{{ USB_VENDOR_COMPAQ, USB_PRODUCT_COMPAQ_IPAQPOCKETPC} , 0},
 	{{ USB_VENDOR_HP, USB_PRODUCT_HP_2215 }, 0 },
 	{{ USB_VENDOR_HP, USB_PRODUCT_HP_568J }, 0},
-	{{ USB_VENDOR_COMPAQ, USB_PRODUCT_COMPAQ_IPAQPOCKETPC} , 0},
-	{{ USB_VENDOR_CASIO, USB_PRODUCT_CASIO_BE300} , 0},
-	{{ USB_VENDOR_ASUS, USB_PRODUCT_ASUS_MYPAL_A730} , 0}
+	{{ USB_VENDOR_HTC, USB_PRODUCT_HTC_PPC6700MODEM }, 0}
 };
 
 #define uipaq_lookup(v, p) ((struct uipaq_type *)usb_lookup(uipaq_devs, v, p))
