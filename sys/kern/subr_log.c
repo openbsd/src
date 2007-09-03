2e4accc306005d1ed446f7f546c99be47285ea55
thib <thib@openbsd.org>
2007-09-03 17:51:03 +0000

    unsupported ioctl's should be returned with ENOTTY not -1;
    ok miod@

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_log.c,v 1.14 2005/12/09 09:09:52 jsg Exp $	*/
+/*	$OpenBSD: subr_log.c,v 1.15 2007/09/03 17:51:03 thib Exp $	*/
 /*	$NetBSD: subr_log.c,v 1.11 1996/03/30 22:24:44 christos Exp $	*/
 
 /*
@@ -306,7 +306,7 @@ logioctl(dev_t dev, u_long com, caddr_t data, int flag, struct proc *p)
 		break;
 
 	default:
-		return (-1);
+		return (ENOTTY);
 	}
 	return (0);
 }
