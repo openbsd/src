764c630e243161be6a12d3af37ae16b1183a7b18
stevesk <stevesk@openbsd.org>
2007-09-11 04:36:29 +0000

    sort #include

--- usr.bin/ssh/sshpty.c
+++ usr.bin/ssh/sshpty.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshpty.c,v 1.26 2006/08/03 03:34:42 deraadt Exp $ */
+/* $OpenBSD: sshpty.c,v 1.27 2007/09/11 04:36:29 stevesk Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -15,16 +15,17 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+
 #include <errno.h>
 #include <fcntl.h>
 #include <grp.h>
 #include <paths.h>
 #include <pwd.h>
+#include <stdarg.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <util.h>
-#include <stdarg.h>
 
 #include "sshpty.h"
 #include "log.h"
