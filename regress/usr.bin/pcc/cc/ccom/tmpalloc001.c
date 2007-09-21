c74f76ead90bc8e220b32f38b15e8335eaeabb15
otto <otto@openbsd.org>
2007-09-21 06:17:00 +0000

    add the other case Ted mentioned

--- regress/usr.bin/pcc/cc/ccom/tmpalloc001.c
+++ regress/usr.bin/pcc/cc/ccom/tmpalloc001.c
@@ -1,10 +1,17 @@
 /* From Ted Unangst */
 int a() { return 1; }
 
+int f()
+{
+	int b = 0;
+	a() + ++b;
+	return 0;
+}
+
 int main()
 {
 	int b = 0;
 	a() + ++b;
-	printf("b %d\n", b);
+	printf("%d\n", b);
 	return 0;
 }
