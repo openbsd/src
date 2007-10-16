d55a1c24149aa2c54a45b281db6b919374a43282
otto <otto@openbsd.org>
2007-10-16 12:49:53 +0000

    += and sign extend problem

---
+++ regress/usr.bin/pcc/ccom/arith001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+         long long foo = 10;
+         unsigned int d = 0xffffffffUL;
+
+         if (foo + d != 0x100000009LL)
+		return 1;
+
+         foo += d;
+         if (foo != 0x100000009LL)
+		return 1;
+	 return 0;
+}
+
