6cb0f96f4713438bae2194dd3896df9286753aac
ragge <ragge@openbsd.org>
2007-10-27 14:20:58 +0000

    From master repo: Remove lineid() prototype.

--- usr.bin/pcc/mip/protos.h
+++ usr.bin/pcc/mip/protos.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: protos.h,v 1.2 2007/09/15 22:04:39 ray Exp $	*/
+/*	$OpenBSD: protos.h,v 1.3 2007/10/27 14:20:58 ragge Exp $	*/
 
 struct optab;
 struct symtab;
@@ -53,7 +53,6 @@ int noinit(void);
 void eprint(NODE *, int, int *, int *);
 int uclass(int class);
 int fixclass(int, TWORD type);
-void lineid(int, char *);
 void mycanon(NODE *);
 void delay(NODE *);
 int delay1(NODE *);
