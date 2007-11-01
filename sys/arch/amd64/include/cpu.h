cae4a59f02f52f5b52d87a2c91d1b43b8aee3392
mikeb <mikeb@openbsd.org>
2007-11-01 23:30:46 +0000

    Remove curproc definition.  It is supposed to be in proc.h only.
    ok kettenis

--- sys/arch/amd64/include/cpu.h
+++ sys/arch/amd64/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.31 2007/05/29 18:53:52 deraadt Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.32 2007/11/01 23:30:46 mikeb Exp $	*/
 /*	$NetBSD: cpu.h,v 1.1 2003/04/26 18:39:39 fvdl Exp $	*/
 
 /*-
@@ -215,7 +215,6 @@ extern struct cpu_info cpu_info_primary;
 extern u_int32_t cpus_attached;
 
 #define curpcb		curcpu()->ci_curpcb
-#define curproc		curcpu()->ci_curproc
 
 /*
  * Arguments to hardclock, softclock and statclock
