2c8fd010925c2f357d29b005c3e2c2c14482f60a
dlg <dlg@openbsd.org>
2007-11-05 07:06:02 +0000

    recognise (and use) ahci 1.2 controllers.
    diff from Henrik Gustafsson

--- sys/dev/pci/ahci.c
+++ sys/dev/pci/ahci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ahci.c,v 1.131 2007/11/04 01:38:54 dlg Exp $ */
+/*	$OpenBSD: ahci.c,v 1.132 2007/11/05 07:06:02 dlg Exp $ */
 
 /*
  * Copyright (c) 2006 David Gwynne <dlg@openbsd.org>
@@ -94,6 +94,7 @@ int ahcidebug = AHCI_D_VERBOSE;
 #define  AHCI_REG_VS_0_95		0x00000905 /* 0.95 */
 #define  AHCI_REG_VS_1_0		0x00010000 /* 1.0 */
 #define  AHCI_REG_VS_1_1		0x00010100 /* 1.1 */
+#define  AHCI_REG_VS_1_2		0x00010200 /* 1.2 */
 #define AHCI_REG_CCC_CTL	0x014 /* Coalescing Control */
 #define  AHCI_REG_CCC_CTL_INT(_r)	(((_r) & 0xf8) >> 3) /* CCC INT slot */
 #define AHCI_REG_CCC_PORTS	0x018 /* Coalescing Ports */
@@ -835,6 +836,9 @@ ahci_init(struct ahci_softc *sc)
 		break;
 	case AHCI_REG_VS_1_1:
 		revision = "1.1";
+		break;
+	case AHCI_REG_VS_1_2:
+		revision = "1.2";
 		break;
 
 	default:
