d892f82890cb797b85215960b268c0b64e1c145b
tobias <tobias@openbsd.org>
2007-11-09 16:27:38 +0000

    Parse CVSROOT/config and set umask on server-side, too.

--- usr.bin/cvs/server.c
+++ usr.bin/cvs/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.72 2007/09/22 16:01:22 joris Exp $	*/
+/*	$OpenBSD: server.c,v 1.73 2007/11/09 16:27:38 tobias Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -15,6 +15,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <sys/types.h>
 #include <sys/stat.h>
 
 #include <errno.h>
@@ -165,6 +166,9 @@ cvs_server_root(char *data)
 
 	if (data[0] != '/' || (current_cvsroot = cvsroot_get(data)) == NULL)
 		fatal("Invalid Root specified!");
+
+	cvs_parse_configfile();
+	umask(cvs_umask);
 }
 
 void
