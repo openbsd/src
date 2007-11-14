c71b043f9250b9efbd0cdad03a63571ebd9cc056
deraadt <deraadt@openbsd.org>
2007-11-14 12:13:32 +0000

    CCU-550 EVDO; Alexey Vatchenko

--- sys/dev/usb/usb_quirks.c
+++ sys/dev/usb/usb_quirks.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb_quirks.c,v 1.30 2007/08/28 09:45:46 henning Exp $ */
+/*	$OpenBSD: usb_quirks.c,v 1.31 2007/11/14 12:13:56 deraadt Exp $ */
 /*	$NetBSD: usb_quirks.c,v 1.45 2003/05/10 17:47:14 hamajima Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_quirks.c,v 1.30 2003/01/02 04:15:55 imp Exp $	*/
 
@@ -102,6 +102,8 @@ const struct usbd_quirk_entry {
  { USB_VENDOR_SUNTAC, USB_PRODUCT_SUNTAC_AS64LX,
 	0x100, { UQ_ASSUME_CM_OVER_DATA }},
  { USB_VENDOR_CMOTECH, USB_PRODUCT_CMOTECH_CM5100P,
+	ANY, { UQ_ASSUME_CM_OVER_DATA }},
+ { USB_VENDOR_CMOTECH, USB_PRODUCT_CMOTECH_CCU550,
 	ANY, { UQ_ASSUME_CM_OVER_DATA }},
  { USB_VENDOR_SIEMENS2, USB_PRODUCT_SIEMENS2_ES75,
 	ANY, { UQ_ASSUME_CM_OVER_DATA }},
