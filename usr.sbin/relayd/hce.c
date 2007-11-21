f98352d819b071d236f3f4401a9ec3cad15b11cf
pyr <pyr@openbsd.org>
2007-11-21 09:54:39 +0000

    make sure all hosts are checked when hoststatectl poll is issued, now that check interval can differ from table to table. ok reyk@

--- usr.sbin/relayd/hce.c
+++ usr.sbin/relayd/hce.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hce.c,v 1.34 2007/11/20 15:44:21 pyr Exp $	*/
+/*	$OpenBSD: hce.c,v 1.35 2007/11/21 09:54:39 pyr Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -403,6 +403,8 @@ hce_dispatch_imsg(int fd, short event, void *ptr)
 			break;
 		case IMSG_CTL_POLL:
 			evtimer_del(&env->ev);
+			TAILQ_FOREACH(table, env->tables, entry)
+				table->skipped = 0;
 			hce_launch_checks(-1, EV_TIMEOUT, env);
 			break;
 		default:
