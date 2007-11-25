70a9d0b0a60b892043b6406fe84ebbe3dea722c3
deraadt <deraadt@openbsd.org>
2007-11-25 22:35:21 +0000

    missing line from previous commit; return 1 for interrupts we do handle

--- sys/dev/pci/pccbb.c
+++ sys/dev/pci/pccbb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pccbb.c,v 1.51 2007/11/25 18:59:12 deraadt Exp $	*/
+/*	$OpenBSD: pccbb.c,v 1.52 2007/11/25 22:35:21 deraadt Exp $	*/
 /*	$NetBSD: pccbb.c,v 1.96 2004/03/28 09:49:31 nakayama Exp $	*/
 
 /*
@@ -1010,6 +1010,7 @@ pccbbintr(arg)
 			timeout_add(&sc->sc_ins_tmo, hz / 10);
 			sc->sc_flags |= CBB_INSERTING;
 		}
+		return (1);
 	}
 
 	return (0);
