a6f3c67d8fb1c3d58e9fff52eca4284464de78ff
fgsch <fgsch@openbsd.org>
2007-12-04 11:06:49 +0000

    catch SIGQUIT too. ckuethe@ and henning@ ok.

--- usr.sbin/ntpd/ntpd.c
+++ usr.sbin/ntpd/ntpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ntpd.c,v 1.53 2007/11/22 10:22:30 otto Exp $ */
+/*	$OpenBSD: ntpd.c,v 1.54 2007/12/04 11:06:49 fgsch Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -56,6 +56,7 @@ sighdlr(int sig)
 	switch (sig) {
 	case SIGTERM:
 	case SIGINT:
+	case SIGQUIT:
 		quit = 1;
 		break;
 	case SIGCHLD:
@@ -162,6 +163,7 @@ main(int argc, char *argv[])
 
 	signal(SIGTERM, sighdlr);
 	signal(SIGINT, sighdlr);
+	signal(SIGQUIT, sighdlr);
 	signal(SIGHUP, sighdlr);
 
 	close(pipe_chld[1]);
