31cdd5f8bad3139315b930b05d104863b37e0642
kettenis <kettenis@openbsd.org>
2007-12-11 22:09:29 +0000

    Don't store arguments and environment in the space reserved for stackgap on machines where the stack grows up.  Fixes systrace(1) on hppa.
    ok deraadt@

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exec.c,v 1.104 2007/10/29 14:12:19 chl Exp $	*/
+/*	$OpenBSD: kern_exec.c,v 1.105 2007/12/11 22:09:29 kettenis Exp $	*/
 /*	$NetBSD: kern_exec.c,v 1.75 1996/02/09 18:59:28 christos Exp $	*/
 
 /*-
@@ -435,8 +435,8 @@ sys_execve(struct proc *p, void *v, register_t *retval)
 	arginfo.ps_nenvstr = envc;
 
 #ifdef MACHINE_STACK_GROWS_UP
-	stack = (char *)USRSTACK + sizeof(arginfo);
-	slen = len - sizeof(arginfo);
+	stack = (char *)USRSTACK + sizeof(arginfo) + ALIGN(sgap);
+	slen = len - sizeof(arginfo) - ALIGN(sgap);
 #else
 	stack = (char *)(USRSTACK - len);
 #endif
