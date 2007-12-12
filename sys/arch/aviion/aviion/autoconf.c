f03252cda2e406cf0d673b80354cf87ceb17af92
miod <miod@openbsd.org>
2007-12-12 20:36:40 +0000

    Remove possible trailing \r from the PROM commandline arguments.

--- sys/arch/aviion/aviion/autoconf.c
+++ sys/arch/aviion/aviion/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.6 2007/11/21 22:05:18 miod Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.7 2007/12/12 20:36:40 miod Exp $	*/
 /*
  * Copyright (c) 1998 Steve Murphree, Jr.
  * Copyright (c) 1996 Nivas Madhur
@@ -128,6 +128,16 @@ void
 cmdline_parse(void)
 {
 	char *p;
+
+	/*
+	 * If the boot commandline has been manually entered, it
+	 * may end with a '\r' character.
+	 */
+	for (p = bootargs; *p != '\0'; p++)
+		;
+	if (p != bootargs)
+		if (*--p == '\r')
+			*p = '\0';
 
 	/*
 	 * Skip boot device ``foo(ctrl,dev,lun)'' and filename,
