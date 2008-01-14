5ef93e83acea01a90314bb4bc2a95fde0b9b336b
kettenis <kettenis@openbsd.org>
2008-01-14 22:05:18 +0000

    Remove extra whitespace after comment; makes file identical to i386 version (except for RCS string of course).

--- sys/arch/amd64/amd64/dkcsum.c
+++ sys/arch/amd64/amd64/dkcsum.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dkcsum.c,v 1.10 2007/12/10 17:36:56 deraadt Exp $	*/
+/*	$OpenBSD: dkcsum.c,v 1.11 2008/01/14 22:05:18 kettenis Exp $	*/
 
 /*-
  * Copyright (c) 1997 Niklas Hallqvist.  All rights reserved.
@@ -175,7 +175,7 @@ dkcsumattach(void)
 		 * out in the bootblocks.
 		 */
 
-		/* B_TYPE dependent hd unit counting bootblocks */ 
+		/* B_TYPE dependent hd unit counting bootblocks */
 		if ((B_TYPE(bootdev) == B_TYPE(hit->bsd_dev)) &&
 		    (B_UNIT(bootdev) == B_UNIT(hit->bsd_dev))) {
 			int type, ctrl, adap, part, unit;
