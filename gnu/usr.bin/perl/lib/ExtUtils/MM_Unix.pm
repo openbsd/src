b95de1209f59967b381ac8400e4300c920055721
espie <espie@openbsd.org>
2008-01-29 00:31:31 +0000

    fix another make -j race, avoid rebuilding some archives twice in parallel. okay millert@, went thru several full builds including >500 perl packages...

--- gnu/usr.bin/perl/lib/ExtUtils/MM_Unix.pm
+++ gnu/usr.bin/perl/lib/ExtUtils/MM_Unix.pm
@@ -3579,7 +3579,7 @@ sub top_targets {
 pure_all :: config pm_to_blib subdirs linkext
 	$(NOECHO) $(NOOP)
 
-subdirs :: $(MYEXTLIB)
+subdirs ::
 	$(NOECHO) $(NOOP)
 
 config :: $(FIRST_MAKEFILE) blibdirs
