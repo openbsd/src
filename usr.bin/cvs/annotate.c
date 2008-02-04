33b84c4913d68fcdad21d63e507593c774c76276
joris <joris@openbsd.org>
2008-02-04 19:12:31 +0000

    trailing space police;

--- usr.bin/cvs/annotate.c
+++ usr.bin/cvs/annotate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: annotate.c,v 1.50 2008/02/03 16:59:11 tobias Exp $	*/
+/*	$OpenBSD: annotate.c,v 1.51 2008/02/04 19:12:31 joris Exp $	*/
 /*
  * Copyright (c) 2007 Tobias Stoeckmann <tobias@openbsd.org>
  * Copyright (c) 2006 Xavier Santolaria <xsa@openbsd.org>
@@ -161,10 +161,10 @@ cvs_annotate_local(struct cvs_file *cf)
 				/* Stick at weird GNU cvs, ignore error. */
 				return;
 
-			/* -f is not allowed for unknown symbols */   
-			rev = rcsnum_parse(cvs_specified_tag);   
-			if (rev == NULL)   
-				fatal("no such tag %s", cvs_specified_tag);   
+			/* -f is not allowed for unknown symbols */
+			rev = rcsnum_parse(cvs_specified_tag);
+			if (rev == NULL)
+				fatal("no such tag %s", cvs_specified_tag);
                         rcsnum_free(rev);
 			rev = rcsnum_alloc();
 			rcsnum_cpy(cf->file_rcs->rf_head, rev, 0);
