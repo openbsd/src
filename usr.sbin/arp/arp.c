df8e2a83df9bf62073b33573f6c8606a5b9b8913
thib <thib@openbsd.org>
2008-02-07 15:57:07 +0000

    make arp(8) default to -na when called with no arguments;
    man page bit from jmc@
    
    ok millert@, oga@, laurent@

--- usr.sbin/arp/arp.c
+++ usr.sbin/arp/arp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: arp.c,v 1.40 2007/08/24 13:12:16 claudio Exp $ */
+/*	$OpenBSD: arp.c,v 1.41 2008/02/07 15:57:07 thib Exp $ */
 /*	$NetBSD: arp.c,v 1.12 1995/04/24 13:25:18 cgd Exp $ */
 
 /*
@@ -133,6 +133,9 @@ main(int argc, char *argv[])
 	}
 	argc -= optind;
 	argv += optind;
+
+	if (argc == 0)
+		aflag = nflag = 1;
 
 	if (!func)
 		func = F_GET;
