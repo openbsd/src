9f67c8d0a9da31c445018c367627cf4f8934d2b2
deraadt <deraadt@openbsd.org>
2008-02-26 18:32:04 +0000

    Add support for Axesstel MV100H, as used on CDMA network operated by O2 CZ; from Pav Lucistnik <pav@FreeBSD.org>

--- sys/dev/usb/ubsa.c
+++ sys/dev/usb/ubsa.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ubsa.c,v 1.31 2007/10/11 18:33:14 deraadt Exp $ 	*/
+/*	$OpenBSD: ubsa.c,v 1.32 2008/02/26 18:32:06 deraadt Exp $ 	*/
 /*	$NetBSD: ubsa.c,v 1.5 2002/11/25 00:51:33 fvdl Exp $	*/
 /*-
  * Copyright (c) 2002, Alexander Kabaev <kan.FreeBSD.org>.
@@ -212,6 +212,8 @@ struct	ucom_methods ubsa_methods = {
 const struct usb_devno ubsa_devs[] = {
 	/* AnyDATA ADU-E100H */
 	{ USB_VENDOR_ANYDATA, USB_PRODUCT_ANYDATA_ADU_E100H },
+	/* Axesstel MV100H */
+	{ USB_VENDOR_AXESSTEL, USB_PRODUCT_AXESSTEL_DATAMODEM },
 	/* BELKIN F5U103 */
 	{ USB_VENDOR_BELKIN, USB_PRODUCT_BELKIN_F5U103 },
 	/* BELKIN F5U120 */
