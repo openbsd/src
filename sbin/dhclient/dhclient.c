b215a882fd7e98ff38505d62bfc539bee124e0ee
hugh <hugh@openbsd.org>
2008-03-12 13:31:22 +0000

    Code was not passing script exit status; fix with macro. deraadt@ OK

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.116 2007/10/16 20:19:26 sobrado Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.117 2008/03/12 13:31:22 hugh Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -1807,7 +1807,7 @@ priv_script_go(void)
 
 	script_flush_env();
 
-	return (wstatus & 0xff);
+	return (WEXITSTATUS(wstatus));
 }
 
 void
