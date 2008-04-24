afcbddc548d689e37caa0f92346ed8caa5ddb0fd
thib <thib@openbsd.org>
2008-04-24 20:47:05 +0000

    oops. unbreak build by removing the #include for amd8131reg.h

--- sys/arch/amd64/pci/aapic.c
+++ sys/arch/amd64/pci/aapic.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aapic.c,v 1.4 2008/04/24 17:44:54 thib Exp $	*/
+/*	$OpenBSD: aapic.c,v 1.5 2008/04/24 20:47:05 thib Exp $	*/
 /* 	$NetBSD: aapic.c,v 1.3 2005/01/13 23:40:01 fvdl Exp $	*/
 
 /*
@@ -12,8 +12,6 @@
 #include <dev/pci/pcireg.h>
 #include <dev/pci/pcivar.h>
 #include <dev/pci/pcidevs.h>
-
-#include <arch/amd64/pci/amd8131reg.h>
 
 #include "ioapic.h"
 
