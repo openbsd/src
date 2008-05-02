a7628e10f87f8d9fb4d2ae15edf6941434da1a97
pyr <pyr@openbsd.org>
2008-05-02 04:28:51 +0000

    do not always delay log initialization, when no configuration file is present, which is the most common use of rtadvd, it would otherwise always print messages to the console though the situation is valid.
    this fixes a regression introduced with the new logging code.
    
    ok rainer@, todd@, fgsch@

--- usr.sbin/rtadvd/rtadvd.c
+++ usr.sbin/rtadvd/rtadvd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtadvd.c,v 1.33 2008/04/23 10:17:50 pyr Exp $	*/
+/*	$OpenBSD: rtadvd.c,v 1.34 2008/05/02 04:28:51 pyr Exp $	*/
 /*	$KAME: rtadvd.c,v 1.66 2002/05/29 14:18:36 itojun Exp $	*/
 
 /*
@@ -199,13 +199,17 @@ main(argc, argv)
 	/* get iflist block from kernel */
 	init_iflist();
 
+	if (conffile == NULL)
+		log_init(dflag);
+
 	while (argc--)
 		getconfig(*argv++);
 
 	if (inet_pton(AF_INET6, ALLNODES, &sin6_allnodes.sin6_addr) != 1)
 		fatal("inet_pton failed");
 
-	log_init(dflag);
+	if (conffile != NULL)
+		log_init(dflag);
 
 	if (!dflag)
 		daemon(1, 0);
