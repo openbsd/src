a07ad480367871189fd2f7b87d01eab553414da4
beck <beck@openbsd.org>
2008-05-11 08:50:54 +0000

    fix sync.c to not puke if sync_lease called without sync_init. from henric.

--- usr.sbin/dhcpd/sync.c
+++ usr.sbin/dhcpd/sync.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sync.c,v 1.4 2008/05/08 14:15:40 deraadt Exp $	*/
+/*	$OpenBSD: sync.c,v 1.5 2008/05/11 08:50:54 beck Exp $	*/
 
 /*
  * Copyright (c) 2008 Bob Beck <beck@openbsd.org>
@@ -416,6 +416,9 @@ sync_lease(struct lease *lease)
 	int i = 0;
 	HMAC_CTX ctx;
 	u_int hmac_len;
+
+	if (sync_key == NULL)
+		return;
 
 	bzero(&hdr, sizeof(hdr));
 	bzero(&ld, sizeof(ld));
