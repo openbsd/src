80cf86b562d8fe0a78eb828a7f365b8f13f24ebb
thib <thib@openbsd.org>
2008-05-17 17:27:24 +0000

    KNF. No names for arguments in function prototypes, regroup functions and make some comments nicer...

--- sys/nfs/krpc.h
+++ sys/nfs/krpc.h
@@ -1,24 +1,18 @@
-/*	$OpenBSD: krpc.h,v 1.5 2002/03/14 01:27:13 millert Exp $	*/
+/*	$OpenBSD: krpc.h,v 1.6 2008/05/17 17:27:24 thib Exp $	*/
 /*	$NetBSD: krpc.h,v 1.4 1995/12/19 23:07:11 cgd Exp $	*/
 
 #include <sys/cdefs.h>
 
-int krpc_call(struct sockaddr_in *sin,
-	u_int prog, u_int vers, u_int func,
-	struct mbuf **data, struct mbuf **from, int retries);
+int krpc_call(struct sockaddr_in *, u_int, u_int, u_int, struct mbuf **,
+    struct mbuf **, int);
+int krpc_portmap(struct sockaddr_in *, u_int, u_int, u_int16_t *);
 
-int krpc_portmap(struct sockaddr_in *sin,
-	u_int prog, u_int vers, u_int16_t *portp);
+struct mbuf *xdr_string_encode(char *, int);
+struct mbuf *xdr_string_decode(struct mbuf *, char *, int *);
+struct mbuf *xdr_inaddr_encode(struct in_addr *);
+struct mbuf *xdr_inaddr_decode(struct mbuf *, struct in_addr *);
 
-struct mbuf *xdr_string_encode(char *str, int len);
-struct mbuf *xdr_string_decode(struct mbuf *m, char *str, int *len_p);
-struct mbuf *xdr_inaddr_encode(struct in_addr *ia);
-struct mbuf *xdr_inaddr_decode(struct mbuf *m, struct in_addr *ia);
-
-
-/*
- * RPC definitions for the portmapper
- */
+/* RPC definitions for the portmapper. */
 #define	PMAPPORT		111
 #define	PMAPPROG		100000
 #define	PMAPVERS		2
@@ -29,10 +23,7 @@ struct mbuf *xdr_inaddr_decode(struct mbuf *m, struct in_addr *ia);
 #define	PMAPPROC_DUMP		4
 #define	PMAPPROC_CALLIT		5
 
-
-/*
- * RPC definitions for bootparamd
- */
+/* RPC definitions for bootparamd. */
 #define	BOOTPARAM_PROG		100026
 #define	BOOTPARAM_VERS		1
 #define BOOTPARAM_WHOAMI	1
