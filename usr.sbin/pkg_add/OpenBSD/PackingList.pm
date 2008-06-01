6a8d296f20f082bb706b03e6f8dd3380e5ab8a83
espie <espie@openbsd.org>
2008-06-01 10:09:39 +0000

    provision for reading only prelink related stuff from a package (maybe a bit more, we'll trim it as fitting).

--- usr.sbin/pkg_add/OpenBSD/PackingList.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingList.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackingList.pm,v 1.81 2007/12/08 15:54:45 espie Exp $
+# $OpenBSD: PackingList.pm,v 1.82 2008/06/01 10:09:39 espie Exp $
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -152,6 +152,17 @@ sub FilesOnly
 	local $_;
 	while (<$fh>) {
 	    	next unless m/^\@(?:cwd|name|info|man|file|lib|shell|sample)\b/o || !m/^\@/o;
+		&$cont($_);
+	}
+}
+
+sub PrelinkStuffOnly
+{
+	my ($fh, $cont) = @_;
+	local $_;
+	while (<$fh>) {
+		next unless m/^\@(?:cwd|bin|lib|name|depend|wantlib)\b/o ||
+			m/^\@comment\s+subdir\=/o;
 		&$cont($_);
 	}
 }
