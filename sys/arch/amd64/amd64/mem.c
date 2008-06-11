46c1739908fad457b4ef70725fcceeb519e00e27
phessler <phessler@openbsd.org>
2008-06-11 09:46:38 +0000

    unbreak RAMDISK
    no cookie for me

--- sys/arch/amd64/amd64/mem.c
+++ sys/arch/amd64/amd64/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.11 2008/06/11 09:22:38 phessler Exp $ */
+/*	$OpenBSD: mem.c,v 1.12 2008/06/11 09:46:38 phessler Exp $ */
 /*
  * Copyright (c) 1988 University of Utah.
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -63,7 +63,9 @@
 
 #include <uvm/uvm_extern.h>
 
+#if NMTRR > 0
 #include "mtrr.h"
+#endif
 
 caddr_t zeropage;
 extern int start, end, etext;
