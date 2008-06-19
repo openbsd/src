907b209cd1bb8e933024645b8cf2b1c2fdc810ae
reyk <reyk@openbsd.org>
2008-06-19 08:39:33 +0000

    add 10GbaseT media type, 10 Gigabit Ethernet over CAT 6 unshielded or shielded twisted pair cables. this is an upcoming standard.
    ok brad@

--- sys/net/if_media.h
+++ sys/net/if_media.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_media.h,v 1.27 2007/10/05 19:28:26 brad Exp $	*/
+/*	$OpenBSD: if_media.h,v 1.28 2008/06/19 08:39:33 reyk Exp $	*/
 /*	$NetBSD: if_media.h,v 1.22 2000/02/17 21:53:16 sommerfeld Exp $	*/
 
 /*-
@@ -184,6 +184,7 @@ u_int64_t	ifmedia_baudrate(int);
 #define	IFM_10G_SR	19		/* 10GBase-SR - multi-mode fiber */
 #define	IFM_10G_CX4	20		/* 10GBase-CX4 - copper */
 #define	IFM_2500_SX	21		/* 2500baseSX - multi-mode fiber */
+#define	IFM_10G_T	22		/* 10GbaseT cat 6 */
 
 #define	IFM_ETH_MASTER	0x00000100	/* master mode (1000baseT) */
 #define	IFM_ETH_RXPAUSE	0x00000200	/* receive PAUSE frames */
@@ -425,6 +426,9 @@ struct ifmedia_description {
 	{ IFM_ETHER|IFM_10G_CX4,	"10GBASE-CX4" },		\
 	{ IFM_ETHER|IFM_2500_SX,	"2500baseSX" },			\
 	{ IFM_ETHER|IFM_2500_SX,	"2500SX" },			\
+	{ IFM_ETHER|IFM_10G_T,		"10GbaseT" },			\
+	{ IFM_ETHER|IFM_10G_T,		"10GT" },			\
+	{ IFM_ETHER|IFM_10G_T,		"10GBASE-T" },			\
 									\
 	{ IFM_FDDI|IFM_FDDI_SMF,	"Single-mode" },		\
 	{ IFM_FDDI|IFM_FDDI_SMF,	"SMF" },			\
@@ -540,6 +544,7 @@ struct ifmedia_baudrate {
 	{ IFM_ETHER|IFM_10G_SR,		IF_Gbps(10) },			\
 	{ IFM_ETHER|IFM_10G_CX4,	IF_Gbps(10) },			\
 	{ IFM_ETHER|IFM_2500_SX,	IF_Mbps(2500) },		\
+	{ IFM_ETHER|IFM_10G_T,		IF_Gbps(10) },			\
 									\
 	{ IFM_FDDI|IFM_FDDI_SMF,	IF_Mbps(100) },			\
 	{ IFM_FDDI|IFM_FDDI_MMF,	IF_Mbps(100) },			\
