acaa7177caf31ef9c84028263eed9a80efb029da
martynas <martynas@openbsd.org>
2008-06-21 07:46:46 +0000

    use optopt to get invalid flag, instead of return value of getopt, which is always '?';  ok djm@

--- usr.bin/ssh/sftp.c
+++ usr.bin/ssh/sftp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sftp.c,v 1.101 2008/06/08 20:15:29 dtucker Exp $ */
+/* $OpenBSD: sftp.c,v 1.102 2008/06/21 07:46:46 martynas Exp $ */
 /*
  * Copyright (c) 2001-2004 Damien Miller <djm@openbsd.org>
  *
@@ -343,7 +343,7 @@ infer_path(const char *p, char **ifp)
 static int
 parse_getput_flags(const char *cmd, char **argv, int argc, int *pflag)
 {
-	extern int optind, optreset, opterr;
+	extern int opterr, optind, optopt, optreset;
 	int ch;
 
 	optind = optreset = 1;
@@ -357,7 +357,7 @@ parse_getput_flags(const char *cmd, char **argv, int argc, int *pflag)
 			*pflag = 1;
 			break;
 		default:
-			error("%s: Invalid flag -%c", cmd, ch);
+			error("%s: Invalid flag -%c", cmd, optopt);
 			return -1;
 		}
 	}
@@ -368,7 +368,7 @@ parse_getput_flags(const char *cmd, char **argv, int argc, int *pflag)
 static int
 parse_ls_flags(char **argv, int argc, int *lflag)
 {
-	extern int optind, optreset, opterr;
+	extern int opterr, optind, optopt, optreset;
 	int ch;
 
 	optind = optreset = 1;
@@ -407,7 +407,7 @@ parse_ls_flags(char **argv, int argc, int *lflag)
 			*lflag |= LS_TIME_SORT;
 			break;
 		default:
-			error("ls: Invalid flag -%c", ch);
+			error("ls: Invalid flag -%c", optopt);
 			return -1;
 		}
 	}
@@ -418,7 +418,7 @@ parse_ls_flags(char **argv, int argc, int *lflag)
 static int
 parse_df_flags(const char *cmd, char **argv, int argc, int *hflag, int *iflag)
 {
-	extern int optind, optreset, opterr;
+	extern int opterr, optind, optopt, optreset;
 	int ch;
 
 	optind = optreset = 1;
@@ -434,7 +434,7 @@ parse_df_flags(const char *cmd, char **argv, int argc, int *hflag, int *iflag)
 			*iflag = 1;
 			break;
 		default:
-			error("%s: Invalid flag -%c", cmd, ch);
+			error("%s: Invalid flag -%c", cmd, optopt);
 			return -1;
 		}
 	}
