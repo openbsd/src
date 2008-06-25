7c859c40c6464d9b76e0e44f5847f63275b2eebf
deraadt <deraadt@openbsd.org>
2008-06-25 15:26:33 +0000

    handle sparc64 seperately; ok miod

--- gnu/usr.bin/cvs/src/import.c
+++ gnu/usr.bin/cvs/src/import.c
@@ -899,6 +899,9 @@ static const struct compair comtable[] =
 #ifdef __sparc__
     {"s", "! "},			/* assembler	 */
 #endif
+#ifdef __sparc64__
+    {"s", "! "},			/* assembler	 */
+#endif
 #ifdef __mc68000__
     {"s", "| "},			/* assembler	 */
 #endif
