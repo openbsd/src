3c72c0751485ebfd618e6b6f1a38737525976b34
ray <ray@openbsd.org>
2008-06-27 06:06:13 +0000

    Remove clauses 3 and 4 from license copyrighted by millert, deraadt, krw, and NetBSD.
    OK millert, krw, and deraadt

--- distrib/miniroot/upgrade.sh
+++ distrib/miniroot/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: upgrade.sh,v 1.64 2006/08/29 01:02:49 krw Exp $
+#	$OpenBSD: upgrade.sh,v 1.65 2008/06/27 06:06:13 ray Exp $
 #	$NetBSD: upgrade.sh,v 1.2.4.5 1996/08/27 18:15:08 gwr Exp $
 #
 # Copyright (c) 1997-2004 Todd Miller, Theo de Raadt, Ken Westerback
@@ -19,13 +19,6 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#        This product includes software developed by the NetBSD
-#        Foundation, Inc. and its contributors.
-# 4. Neither the name of The NetBSD Foundation nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
 # ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
