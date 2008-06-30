8855ecc598d1d5475aa87a598bba8a48f25362ee
djm <djm@openbsd.org>
2008-06-30 10:43:03 +0000

    explicitly disable conch options that could interfere with the test

--- regress/usr.bin/ssh/conch-ciphers.sh
+++ regress/usr.bin/ssh/conch-ciphers.sh
@@ -1,12 +1,10 @@
-#	$OpenBSD: conch-ciphers.sh,v 1.1 2008/06/28 13:57:25 djm Exp $
+#	$OpenBSD: conch-ciphers.sh,v 1.2 2008/06/30 10:43:03 djm Exp $
 #	Placed in the Public Domain.
 
 tid="conch ciphers"
 
 DATA=/bin/ls
 COPY=${OBJ}/copy
-
-set -e
 
 if test "x$REGRESS_INTEROP_CONCH" != "xyes" ; then
 	fatal "conch interop tests not enabled"
@@ -18,9 +16,11 @@ for c in aes256-ctr aes256-cbc aes192-ctr aes192-cbc aes128-ctr aes128-cbc \
          cast128-cbc blowfish 3des-cbc ; do
 	verbose "$tid: cipher $c"
 	rm -f ${COPY}
-	${CONCH} --identity $OBJ/rsa --port $PORT --user $USER \
-	         --known-hosts $OBJ/known_hosts \
-	         127.0.0.1 cat ${DATA} > ${COPY} 2>/dev/null
+	# XXX the 2nd "cat" seems to be needed because of buggy FD handling
+	# in conch
+	${CONCH} --identity $OBJ/rsa --port $PORT --user $USER  -e none \
+	    --known-hosts $OBJ/known_hosts --notty --noagent --nox11 -n \
+	    127.0.0.1 "cat ${DATA}" 2>/dev/null | cat > ${COPY}
 	if [ $? -ne 0 ]; then
 		fail "ssh cat $DATA failed"
 	fi
