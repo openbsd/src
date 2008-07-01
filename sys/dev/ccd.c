86c8ec090020164c16dbc7b8bb403cc44b61e115
ray <ray@openbsd.org>
2008-07-01 04:15:59 +0000

    Remove clauses 3 and 4 from BSD license owned by mickey, niklas, and NetBSD.
    OK niklas, mickey, deraadt.

--- sys/dev/ccd.c
+++ sys/dev/ccd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ccd.c,v 1.84 2008/06/15 00:36:40 krw Exp $	*/
+/*	$OpenBSD: ccd.c,v 1.85 2008/07/01 04:15:59 ray Exp $	*/
 /*	$NetBSD: ccd.c,v 1.33 1996/05/05 04:21:14 thorpej Exp $	*/
 
 /*-
@@ -18,13 +18,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
