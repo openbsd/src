5088440718083bde599de10cfa93d52543b2e31d
miod <miod@openbsd.org>
2008-07-22 18:15:48 +0000

    Do not define VM_MIN_ADDRESS as __LDPGSZ, but provide its value, to prevent uvm_map.c from requiring <machine/exec.h>.

--- sys/arch/sparc/include/vmparam.h
+++ sys/arch/sparc/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmparam.h,v 1.32 2008/06/24 21:24:03 deraadt Exp $	*/
+/*	$OpenBSD: vmparam.h,v 1.33 2008/07/22 18:15:48 miod Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.13 1997/07/12 16:20:03 perry Exp $	*/
 
 /*
@@ -89,7 +89,7 @@
  * IO space virtual base, which must be the same as VM_MAX_KERNEL_ADDRESS:
  * tread with care.
  */
-#define VM_MIN_ADDRESS		((vaddr_t)__LDPGSZ)
+#define VM_MIN_ADDRESS		((vaddr_t)0x2000)
 #define VM_MAX_ADDRESS		((vaddr_t)VM_MIN_KERNEL_ADDRESS)
 #define VM_MAXUSER_ADDRESS	((vaddr_t)VM_MIN_KERNEL_ADDRESS)
 #define VM_MIN_KERNEL_ADDRESS	((vaddr_t)KERNBASE)
