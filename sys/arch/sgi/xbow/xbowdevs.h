0d2830b4a9f026683852404faf5dc6c363e10701
miod <miod@openbsd.org>
2008-07-28 18:51:11 +0000

    regen

--- sys/arch/sgi/xbow/xbowdevs.h
+++ sys/arch/sgi/xbow/xbowdevs.h
@@ -20,28 +20,28 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#define	PCI_VENDOR_SGI	0x0000
-#define	PCI_VENDOR_SGI2	0x0023
-#define	PCI_VENDOR_SGI3	0x0024
-#define	PCI_VENDOR_SGI4	0x0036
-#define	PCI_VENDOR_SGI5	0x02aa
+#define	XBOW_VENDOR_SGI	0x0000
+#define	XBOW_VENDOR_SGI2	0x0023
+#define	XBOW_VENDOR_SGI3	0x0024
+#define	XBOW_VENDOR_SGI4	0x0036
+#define	XBOW_VENDOR_SGI5	0x02aa
 
 /*
  * List of known products.  Grouped by ``manufacturer''.
  */
 
-#define	PCI_PRODUCT_SGI_XBOW	0x0000		/* XBow */
-#define	PCI_PRODUCT_SGI_XXBOW	0xd000		/* XXBow */
+#define	XBOW_PRODUCT_SGI_XBOW	0x0000		/* XBow */
+#define	XBOW_PRODUCT_SGI_XXBOW	0xd000		/* XXBow */
 
-#define	PCI_PRODUCT_SGI2_ODYSSEY	0xc013		/* Odyssey */
+#define	XBOW_PRODUCT_SGI2_ODYSSEY	0xc013		/* Odyssey */
 
-#define	PCI_PRODUCT_SGI3_TPU	0xc202		/* TPU */
-#define	PCI_PRODUCT_SGI3_XBRIDGE	0xd002		/* XBridge */
+#define	XBOW_PRODUCT_SGI3_TPU	0xc202		/* TPU */
+#define	XBOW_PRODUCT_SGI3_XBRIDGE	0xd002		/* XBridge */
 
-#define	PCI_PRODUCT_SGI4_HEART	0xc001		/* Heart */
-#define	PCI_PRODUCT_SGI4_BRIDGE	0xc002		/* Bridge */
-#define	PCI_PRODUCT_SGI4_HUB	0xc101		/* Hub */
-#define	PCI_PRODUCT_SGI4_BEDROCK	0xc110		/* Bedrock */
+#define	XBOW_PRODUCT_SGI4_HEART	0xc001		/* Heart */
+#define	XBOW_PRODUCT_SGI4_BRIDGE	0xc002		/* Bridge */
+#define	XBOW_PRODUCT_SGI4_HUB	0xc101		/* Hub */
+#define	XBOW_PRODUCT_SGI4_BEDROCK	0xc110		/* Bedrock */
 
-#define	PCI_PRODUCT_SGI5_IMPACT	0xc003		/* ImpactSR */
-#define	PCI_PRODUCT_SGI5_KONA	0xc102		/* Kona */
+#define	XBOW_PRODUCT_SGI5_IMPACT	0xc003		/* ImpactSR */
+#define	XBOW_PRODUCT_SGI5_KONA	0xc102		/* Kona */
