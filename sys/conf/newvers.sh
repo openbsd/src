39765da9402bf7adf65496799cccfab4371fa9ea
deraadt <deraadt@openbsd.org>
2008-08-07 17:18:03 +0000

    we are at 4.4-current

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.91 2008/08/06 03:56:53 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.92 2008/08/07 17:18:03 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -67,10 +67,10 @@ ost="OpenBSD"
 osr="4.4"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
-#define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
+#define STATUS "-beta"			/* just before a release */
 #endif
 
 const char ostype[] = "${ost}";
