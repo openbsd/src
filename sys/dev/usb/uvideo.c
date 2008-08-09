d416fa2d1df35ea70b016292aa6c79022ad9c063
mglocker <mglocker@openbsd.org>
2008-08-09 08:42:03 +0000

    Revert last commit, it doesn't always help, we need to find another solution.

--- sys/dev/usb/uvideo.c
+++ sys/dev/usb/uvideo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvideo.c,v 1.76 2008/08/09 07:57:48 mglocker Exp $ */
+/*	$OpenBSD: uvideo.c,v 1.77 2008/08/09 08:42:03 mglocker Exp $ */
 
 /*
  * Copyright (c) 2008 Robert Nagy <robert@openbsd.org>
@@ -1035,9 +1035,6 @@ uvideo_vs_negotiation(struct uvideo_softc *sc, int commit)
 	bzero(probe_data, sizeof(probe_data));
 	error = uvideo_vs_get_probe(sc, probe_data, GET_DEF);
 	if (error != USBD_NORMAL_COMPLETION) {
-		/* relax a moment and retry with GET_CUR */
-		usbd_delay_ms(sc->sc_udev, 100);
-
 		error = uvideo_vs_get_probe(sc, probe_data, GET_CUR);
 		if (error != USBD_NORMAL_COMPLETION)
 			return (error);
