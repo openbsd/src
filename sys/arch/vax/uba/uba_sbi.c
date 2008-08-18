913a8a5513c338dc1b5e8ac409b18227b02bb2b4
miod <miod@openbsd.org>
2008-08-18 23:09:42 +0000

    Increment device interrupt event counter when redispatching an interrupt.

--- sys/arch/vax/uba/uba_sbi.c
+++ sys/arch/vax/uba/uba_sbi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uba_sbi.c,v 1.4 2005/11/24 04:55:18 brad Exp $	*/
+/*	$OpenBSD: uba_sbi.c,v 1.5 2008/08/18 23:09:42 miod Exp $	*/
 /*	$NetBSD: uba_sbi.c,v 1.1 1999/06/21 16:23:01 ragge Exp $	   */
 /*
  * Copyright (c) 1996 Jonathan Stone.
@@ -224,6 +224,7 @@ uba_dw780int(uba)
 		func = scb_vec[vec/4].hoppaddr;
 		arg = scb_vec[vec/4].pushlarg;
 		(*func)(arg);
+		scb_vec[vec/4].ev->ec_count++;
 	}
 }
 
