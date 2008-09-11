252697cb9d368d5a5521085e7fbbe4efef361a29
brad <brad@openbsd.org>
2008-09-11 20:52:24 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.1408 2008/09/11 19:43:56 kettenis Exp 
+ *	OpenBSD: pcidevs,v 1.1409 2008/09/11 20:51:52 brad Exp 
  */
 
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
@@ -3344,8 +3344,8 @@ static const struct pci_known_product pci_known_products[] = {
 	    "BCM4306",
 	},
 	{
-	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4322,
-	    "BCM4322",
+	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_SERIAL_2,
+	    "Serial",
 	},
 	{
 	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4309,
@@ -3356,8 +3356,16 @@ static const struct pci_known_product pci_known_products[] = {
 	    "BCM43XG",
 	},
 	{
-	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4328,
-	    "BCM4328",
+	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4321,
+	    "BCM4321",
+	},
+	{
+	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4321_2,
+	    "BCM4321",
+	},
+	{
+	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_BCM4322,
+	    "BCM4322",
 	},
 	{
 	    PCI_VENDOR_BROADCOM, PCI_PRODUCT_BROADCOM_SERIAL,
