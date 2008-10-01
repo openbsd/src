84c6ddd7467463faa142ba52625d469ce3470749
drahn <drahn@openbsd.org>
2008-10-01 14:59:18 +0000

    Assert my copyright on files I gave to d@ back in 1998.

--- lib/librthread/arch/powerpc/_atomic_lock.c
+++ lib/librthread/arch/powerpc/_atomic_lock.c
@@ -1,4 +1,20 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.3 2006/01/05 22:33:24 marc Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.4 2008/10/01 14:59:18 drahn Exp $	*/
+/*
+ * Copyright (c) 1998 Dale Rahn <drahn@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 /*
  * Atomic lock for powerpc
  */
@@ -23,7 +39,7 @@ _atomic_lock(volatile _spinlock_lock_t *lock)
 	/*
 	 * Dale <drahn@openbsd.org> says:
 	 *   Side note. to prevent two processes from accessing
-	 *   the same address with the lwarx in one instrution
+	 *   the same address with the lwarx in one instruction
 	 *   and the stwcx in another process, the current powerpc
 	 *   kernel uses a stwcx instruction without the corresponding
 	 *   lwarx which causes any reservation of a process
