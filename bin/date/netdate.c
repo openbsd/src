8bf64ad5068f919c9e55841ccb0729e5e1a30d18
otto <otto@openbsd.org>
2008-10-01 08:46:58 +0000

    give a less confusing error message on bindresvport() failure; ok matthieu@ millert@

--- bin/date/netdate.c
+++ bin/date/netdate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netdate.c,v 1.16 2006/04/25 15:41:07 deraadt Exp $	*/
+/*	$OpenBSD: netdate.c,v 1.17 2008/10/01 08:46:58 otto Exp $	*/
 /*	$NetBSD: netdate.c,v 1.10 1995/09/07 06:21:06 jtc Exp $	*/
 
 /*-
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)netdate.c	8.2 (Berkeley) 4/28/95";
 #else
-static char rcsid[] = "$OpenBSD: netdate.c,v 1.16 2006/04/25 15:41:07 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: netdate.c,v 1.17 2008/10/01 08:46:58 otto Exp $";
 #endif
 #endif /* not lint */
 
@@ -104,7 +104,7 @@ netsettime(time_t tval)
 	sin.sin_len = sizeof(struct sockaddr_in);
 	sin.sin_family = AF_INET;
 	if (bindresvport(s, &sin) < 0) {
-		warnx("all ports in use");
+		warn("bindresvport");
 		goto bad;
 	}
 	msg.tsp_type = TSP_SETDATE;
