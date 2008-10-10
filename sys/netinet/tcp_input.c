2ea1a7e1feb88eeb87ce3d426d93b27b2fa0d8e6
dhill <dhill@openbsd.org>
2008-10-10 15:35:59 +0000

    Comment out statekey code to stop 'panic: soreceive 3', which happens with IPv6 TCP traffic, until a better fix is found.
    patch from henning@
    proded by deraadt@

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_input.c,v 1.221 2008/09/09 15:26:12 mpf Exp $	*/
+/*	$OpenBSD: tcp_input.c,v 1.222 2008/10/10 15:35:59 dhill Exp $	*/
 /*	$NetBSD: tcp_input.c,v 1.23 1996/02/13 23:43:44 christos Exp $	*/
 
 /*
@@ -594,10 +594,12 @@ tcp_input(struct mbuf *m, ...)
 	/*
 	 * Locate pcb for segment.
 	 */
+/*
 #if NPF > 0
 	if (m->m_pkthdr.pf.statekey)
 		inp = ((struct pf_state_key *)m->m_pkthdr.pf.statekey)->inp;
 #endif
+*/
 findpcb:
 	if (inp == NULL) {
 		switch (af) {
