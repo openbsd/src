8243e63cd355d3e18c5c57d2e054ba8f514493e4
kettenis <kettenis@openbsd.org>
2008-10-12 11:51:08 +0000

    Reduce diff to macppc version.

--- sys/arch/socppc/socppc/clock.c
+++ sys/arch/socppc/socppc/clock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock.c,v 1.1 2008/05/10 12:02:21 kettenis Exp $	*/
+/*	$OpenBSD: clock.c,v 1.2 2008/10/12 11:51:08 kettenis Exp $	*/
 /*	$NetBSD: clock.c,v 1.1 1996/09/30 16:34:40 ws Exp $	*/
 
 /*
@@ -202,7 +202,7 @@ decr_intr(struct clockframe *frame)
 	 */
 	ppc_mtdec(nextevent - tb);
 
-	if (curcpu()->ci_cpl & SPL_CLOCK) {
+	if (ci->ci_cpl >= IPL_CLOCK) {
 		ci->ci_statspending += nstats;
 	} else {
 		KERNEL_LOCK();
