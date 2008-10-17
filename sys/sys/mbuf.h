8ba05fea37ea64864a910cc460da6136020d7df9
claudio <claudio@openbsd.org>
2008-10-17 19:06:01 +0000

    reorder mbuf flags so they are in numerical order again. brad@ concurs

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mbuf.h,v 1.104 2008/09/18 15:16:30 naddy Exp $	*/
+/*	$OpenBSD: mbuf.h,v 1.105 2008/10/17 19:06:01 claudio Exp $	*/
 /*	$NetBSD: mbuf.h,v 1.19 1996/02/09 18:25:14 christos Exp $	*/
 
 /*
@@ -152,17 +152,17 @@ struct mbuf {
 #define	M_PROTO1	0x0010	/* protocol-specific */
 
 /* mbuf pkthdr flags, also in m_flags */
-#define	M_BCAST		0x0100	/* send/received as link-level broadcast */
-#define	M_MCAST		0x0200	/* send/received as link-level multicast */
-#define M_CONF		0x0400  /* payload was encrypted (ESP-transport) */
-#define M_AUTH		0x0800  /* payload was authenticated (AH or ESP auth) */
-#define M_AUTH_AH	0x2000  /* header was authenticated (AH) */
-#define M_TUNNEL	0x1000  /* IP-in-IP added by tunnel mode IPsec */
-#define M_ANYCAST6	0x4000	/* received as IPv6 anycast */
-#define M_LINK0		0x8000	/* link layer specific flag */
 #define M_VLANTAG	0x0020	/* ether_vtag is valid */
 #define M_LOOP		0x0040	/* for Mbuf statistics */
 #define M_FILDROP	0x0080	/* dropped by bpf filter */
+#define M_BCAST		0x0100	/* send/received as link-level broadcast */
+#define M_MCAST		0x0200	/* send/received as link-level multicast */
+#define M_CONF		0x0400  /* payload was encrypted (ESP-transport) */
+#define M_AUTH		0x0800  /* payload was authenticated (AH or ESP auth) */
+#define M_TUNNEL	0x1000  /* IP-in-IP added by tunnel mode IPsec */
+#define M_AUTH_AH	0x2000  /* header was authenticated (AH) */
+#define M_ANYCAST6	0x4000	/* received as IPv6 anycast */
+#define M_LINK0		0x8000	/* link layer specific flag */
 
 /* flags copied when copying m_pkthdr */
 #define	M_COPYFLAGS	(M_PKTHDR|M_EOR|M_PROTO1|M_BCAST|M_MCAST|M_CONF|\
