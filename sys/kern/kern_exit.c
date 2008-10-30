b6af5ebeb9c3778536b7c678a9a17f8debd8b384
deraadt <deraadt@openbsd.org>
2008-10-30 17:09:13 +0000

    Use msleep() in the reaper to make it not lose events.  Based on discussion PR 5609, and revisited with dlg.  Tested on all platforms. ok miod

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exit.c,v 1.76 2008/10/14 18:27:29 guenther Exp $	*/
+/*	$OpenBSD: kern_exit.c,v 1.77 2008/10/30 17:09:13 deraadt Exp $	*/
 /*	$NetBSD: kern_exit.c,v 1.39 1996/04/22 01:38:25 christos Exp $	*/
 
 /*
@@ -388,17 +388,13 @@ reaper(void)
 
 	for (;;) {
 		mtx_enter(&deadproc_mutex);
-		p = LIST_FIRST(&deadproc);
-		if (p == NULL) {
-			/* No work for us; go to sleep until someone exits. */
-			mtx_leave(&deadproc_mutex);
-			(void) tsleep(&deadproc, PVM, "reaper", 0);
-			continue;
-		}
+		while ((p = LIST_FIRST(&deadproc)) == NULL)
+			msleep(&deadproc, &deadproc_mutex, PVM, "reaper", 0);
 
 		/* Remove us from the deadproc list. */
 		LIST_REMOVE(p, p_hash);
 		mtx_leave(&deadproc_mutex);
+
 		KERNEL_PROC_LOCK(curproc);
 
 		/*
