14dcb8d43774050f21147fa8ef9487e118366eaf
ratchov <ratchov@openbsd.org>
2008-11-09 12:22:27 +0000

    sio_revents() should return 0 poll events if the device isn't started

--- lib/libsndio/sndio.c
+++ lib/libsndio/sndio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sndio.c,v 1.3 2008/11/07 21:01:15 ratchov Exp $	*/
+/*	$OpenBSD: sndio.c,v 1.4 2008/11/09 12:22:27 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -451,6 +451,8 @@ sio_revents(struct sio_hdl *hdl, struct pollfd *pfd)
 #endif
 	if (hdl->eof)
 		return POLLHUP;
+	if (!hdl->started)
+		return 0;
 	hdl->pollcnt++;
 	revents = hdl->ops->revents(hdl, pfd);
 #ifdef DEBUG
