faa68cee3feeac27d4683943914380df9ce2ae26
jmc <jmc@openbsd.org>
2008-11-17 07:36:43 +0000

    tweak previous man page changes, and sync usage();

--- usr.bin/aucat/aucat.c
+++ usr.bin/aucat/aucat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aucat.c,v 1.43 2008/11/17 07:04:13 ratchov Exp $	*/
+/*	$OpenBSD: aucat.c,v 1.44 2008/11/17 07:36:43 jmc Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -113,8 +113,8 @@ usage(void)
 	fprintf(stderr,
 	    "usage: %s [-lu] [-b nsamples] [-C min:max] [-c min:max] [-e enc] "
 	    "[-f device]\n"
-	    "\t[-h fmt] [-i file] [-o file] [-r rate] [-s path] [-v volume] "
-	    "[-x policy]\n",
+	    "\t[-h fmt] [-i file] [-m mode] [-o file] [-r rate] [-s file]\n"
+	    "\t[-v volume] [-x policy]\n",
 	    __progname);
 }
 
