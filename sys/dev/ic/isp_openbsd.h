492f1c3beacf7ea4c58aeefbeb5cafd53e80a2c8
krw <krw@openbsd.org>
2008-11-17 00:22:13 +0000

    NETBSD -> OPENBSD in #endif comment.

--- sys/dev/ic/isp_openbsd.h
+++ sys/dev/ic/isp_openbsd.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: isp_openbsd.h,v 1.29 2008/11/17 00:18:13 krw Exp $ */
+/*      $OpenBSD: isp_openbsd.h,v 1.30 2008/11/17 00:22:13 krw Exp $ */
 /*
  * OpenBSD Specific definitions for the QLogic ISP Host Adapter
  */
@@ -429,4 +429,4 @@ isp_wait_complete(struct ispsoftc *isp)
 
 #include <dev/ic/isp_library.h>
 
-#endif	/* _ISP_NETBSD_H */
+#endif	/* _ISP_OPENBSD_H */
