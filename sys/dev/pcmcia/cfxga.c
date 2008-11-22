81388344445f44a17dc02280991198a5a50718b3
deraadt <deraadt@openbsd.org>
2008-11-22 11:35:58 +0000

    com not pccom; ok miod

--- sys/dev/pcmcia/cfxga.c
+++ sys/dev/pcmcia/cfxga.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cfxga.c,v 1.16 2007/09/11 13:39:33 gilles Exp $	*/
+/*	$OpenBSD: cfxga.c,v 1.17 2008/11/22 11:35:58 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006, Matthieu Herrb and Miodrag Vallat
@@ -306,7 +306,7 @@ out:
 	cfxga_remove_function(pf);
 
 	/*
-	 * Be sure to return a value greater than pccom's if we match,
+	 * Be sure to return a value greater than com's if we match,
 	 * otherwise it can win due to the way config(8) will order devices...
 	 */
 	return (id == PRODUCT_S1D13806 ? 10 : 0);
