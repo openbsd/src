5aad79a83d75e19387556e29a0182d132d0f775b
kurt <kurt@openbsd.org>
2008-11-24 19:19:46 +0000

    init uvm_km_page memory a bit earlier to reduce pressure on pmap bootstrap pages.
    "looks good/no problems with it" tedu@ miod@ art@

--- sys/uvm/uvm_init.c
+++ sys/uvm/uvm_init.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_init.c,v 1.18 2008/10/18 12:11:30 kettenis Exp $	*/
+/*	$OpenBSD: uvm_init.c,v 1.19 2008/11/24 19:19:46 kurt Exp $	*/
 /*	$NetBSD: uvm_init.c,v 1.14 2000/06/27 17:29:23 mrg Exp $	*/
 
 /*
@@ -140,6 +140,11 @@ uvm_init()
 	amap_init();		/* init amap module */
 
 	/*
+	 * step 9: init uvm_km_page allocator memory.
+	 */
+	uvm_km_page_init();
+
+	/*
 	 * the VM system is now up!  now that malloc is up we can resize the
 	 * <obj,off> => <page> hash table for general use and enable paging
 	 * of kernel objects.
@@ -148,8 +153,6 @@ uvm_init()
 	uvm_page_rehash();
 	uao_create(VM_MAX_KERNEL_ADDRESS - VM_MIN_KERNEL_ADDRESS,
 	    UAO_FLAG_KERNSWAP);
-
-	uvm_km_page_init();
 
 	/*
 	 * reserve some unmapped space for malloc/pool use after free usage
