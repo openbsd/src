749d97f8ba2d83254d6be9043e3018470cce8052
brad <brad@openbsd.org>
2008-11-28 06:46:13 +0000

    Garbage collect now unused field in the softc struct.

--- sys/dev/pci/if_em.h
+++ sys/dev/pci/if_em.h
@@ -32,7 +32,7 @@ POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************/
 
 /* $FreeBSD: if_em.h,v 1.26 2004/09/01 23:22:41 pdeuskar Exp $ */
-/* $OpenBSD: if_em.h,v 1.38 2008/11/26 00:14:48 dlg Exp $ */
+/* $OpenBSD: if_em.h,v 1.39 2008/11/28 06:46:13 brad Exp $ */
 
 #ifndef _EM_H_DEFINED_
 #define _EM_H_DEFINED_
@@ -385,7 +385,6 @@ struct em_softc {
 	u_int16_t		num_rx_desc;
 	struct em_buffer	*rx_buffer_area;
 	bus_dma_tag_t		rxtag;
-	bus_dmamap_t		rx_sparemap;
 
 	/*
 	 * First/last mbuf pointers, for
