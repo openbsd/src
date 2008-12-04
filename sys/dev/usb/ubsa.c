20cc36d9271582f62941f90fe5eb368a8cda8d97
fkr <fkr@openbsd.org>
2008-12-04 00:23:00 +0000

    move Option GlobeTrotter 3G Quad / 3G Quad Plus and the Vodafone Mobile Connect from ubsa(4) to umsm(4) as these should work better with that driver. discussed with yuo@, jsg@ and deraadt@.

--- sys/dev/usb/ubsa.c
+++ sys/dev/usb/ubsa.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ubsa.c,v 1.41 2008/11/23 14:32:34 fkr Exp $ 	*/
+/*	$OpenBSD: ubsa.c,v 1.42 2008/12/04 00:23:00 fkr Exp $ 	*/
 /*	$NetBSD: ubsa.c,v 1.5 2002/11/25 00:51:33 fvdl Exp $	*/
 /*-
  * Copyright (c) 2002, Alexander Kabaev <kan.FreeBSD.org>.
@@ -217,12 +217,6 @@ const struct usb_devno ubsa_devs[] = {
 	{ USB_VENDOR_GOHUBS, USB_PRODUCT_GOHUBS_GOCOM232 },
 	/* Novatel Wireless U740 */
 	{ USB_VENDOR_NOVATEL, USB_PRODUCT_NOVATEL_MERLINU740 },
-	/* Option Vodafone Mobile Connect 3G */
-	{ USB_VENDOR_OPTION, USB_PRODUCT_OPTION_VODAFONEMC3G },
-	/* Option GlobeTrotter 3G QUAD */
-	{ USB_VENDOR_OPTION, USB_PRODUCT_OPTION_GT3GQUAD },
-	/* Option GlobeTrotter 3G QUAD PLUS */
-	{ USB_VENDOR_OPTION, USB_PRODUCT_OPTION_GT3GQUADPLUS },
 	/* Peracom */
 	{ USB_VENDOR_PERACOM, USB_PRODUCT_PERACOM_SERIAL1 },
 	/* Qualcomm Inc. ZTE CMDMA MSM modem */
