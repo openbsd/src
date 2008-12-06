54e7f0b936f8f9e7351f555bfe5230fea4f5ac0b
tedu <tedu@openbsd.org>
2008-12-06 14:36:49 +0000

    the not MP version didn't compile, mglocker found

--- sys/arch/amd64/amd64/intr.c
+++ sys/arch/amd64/amd64/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.c,v 1.20 2008/12/06 04:31:24 tedu Exp $	*/
+/*	$OpenBSD: intr.c,v 1.21 2008/12/06 14:36:49 tedu Exp $	*/
 /*	$NetBSD: intr.c,v 1.3 2003/03/03 22:16:20 fvdl Exp $	*/
 
 /*
@@ -553,6 +553,7 @@ intr_disestablish(struct intrhand *ih)
 	simple_unlock(&ci->ci_slock);
 }
 
+#ifdef MULTIPROCESSOR
 int
 intr_biglock_wrap(void *v)
 {
@@ -565,6 +566,7 @@ intr_biglock_wrap(void *v)
 
 	return (ret);
 }
+#endif
 
 #define CONCAT(x,y)	__CONCAT(x,y)
 
