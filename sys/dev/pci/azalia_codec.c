7deb8fe8bca7637632c447da1d079572aa25a3d4
jakemsr <jakemsr@openbsd.org>
2009-01-02 20:35:05 +0000

    * add GPIO quirk for Dell Vostro 1400 (from FreeBSD) * move quirk for Dell E6400 into the group of quirks that set GPIO 0
    from Alexey Suslikov, thanks

--- sys/dev/pci/azalia_codec.c
+++ sys/dev/pci/azalia_codec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: azalia_codec.c,v 1.103 2009/01/02 20:18:18 jakemsr Exp $	*/
+/*	$OpenBSD: azalia_codec.c,v 1.104 2009/01/02 20:35:05 jakemsr Exp $	*/
 /*	$NetBSD: azalia_codec.c,v 1.8 2006/05/10 11:17:27 kent Exp $	*/
 
 /*-
@@ -61,6 +61,8 @@
 #define STAC9205_DELL_V1500	0x02281028
 #define IDT_92HD71B7		0x111d76b2
 #define IDT92HD71B7_DELL_E6400	0x02331028
+#define SIGMATEL_STAC9228X	0x83847616
+#define STAC9228X_DELL_V1400	0x02271028
 
 int	azalia_generic_codec_init_dacgroup(codec_t *);
 int	azalia_generic_codec_fnode(codec_t *, nid_t, int, int);
@@ -2065,6 +2067,7 @@ azalia_codec_gpio_quirks(codec_t *this)
 	    (this->vid == REALTEK_ALC882 && this->subid == ALC882_ASUS_W2J) ||
 	    (this->vid == REALTEK_ALC885 && this->subid == ALC885_APPLE_MB3) ||
 	    (this->vid == REALTEK_ALC885 && this->subid == ALC885_APPLE_MB4) ||
+	    (this->vid == IDT_92HD71B7 && this->subid == IDT92HD71B7_DELL_E6400) ||
 	    (this->vid == SIGMATEL_STAC9205 && this->subid == STAC9205_DELL_D630) ||
 	    (this->vid == SIGMATEL_STAC9205 && this->subid == STAC9205_DELL_V1500)) {
 		azalia_gpio_unmute(this, 0);
@@ -2072,8 +2075,8 @@ azalia_codec_gpio_quirks(codec_t *this)
 	if (this->vid == REALTEK_ALC880 && this->subid == ALC880_MEDION_MD95257) {
 		azalia_gpio_unmute(this, 1);
 	}
-	if (this->vid == IDT_92HD71B7 && this->subid == IDT92HD71B7_DELL_E6400) {
-		azalia_gpio_unmute(this, 0);
+	if (this->vid == SIGMATEL_STAC9228X && this->subid == STAC9228X_DELL_V1400) {
+		azalia_gpio_unmute(this, 2);
  	}
 	return 0;
 }
