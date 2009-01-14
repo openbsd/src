9b37955ff33673affeb81c33c4d98799ed58e878
grange <grange@openbsd.org>
2009-01-14 07:58:45 +0000

    int cpuspeed is global. ok markus@

--- sys/arch/i386/pci/elan520.c
+++ sys/arch/i386/pci/elan520.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: elan520.c,v 1.15 2008/06/26 05:42:11 ray Exp $	*/
+/*	$OpenBSD: elan520.c,v 1.16 2009/01/14 07:58:45 grange Exp $	*/
 /*	$NetBSD: elan520.c,v 1.4 2002/10/02 05:47:15 thorpej Exp $	*/
 
 /*-
@@ -90,8 +90,6 @@ struct cfattach elansc_ca = {
 struct cfdriver elansc_cd = {
 	NULL, "elansc", DV_DULL
 };
-
-static int cpuspeed;
 
 int
 elansc_match(struct device *parent, void *match, void *aux)
