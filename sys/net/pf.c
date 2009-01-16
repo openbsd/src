6dcfee2baad76a7f2ce5efc1ef0b86cd516a474f
david <david@openbsd.org>
2009-01-16 23:43:01 +0000

    In pf_test_rule(), if we don't create a state, free any state keys that may have been allocated earlier by pf_get_translation().  Fixes a pf_state_key_pl leak triggered by certain ICMP types matching a NAT rule.  Finally located with the help of pool_walk(). ok henning@ dlg@ mcbride@

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.627 2008/11/24 13:22:09 mikeb Exp $ */
+/*	$OpenBSD: pf.c,v 1.628 2009/01/16 23:43:01 david Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -3400,6 +3400,11 @@ pf_test_rule(struct pf_rule **rm, struct pf_state **sm, int direction,
 		    bip_sum, hdrlen);
 		if (action != PF_PASS)
 			return (action);
+	} else {
+		if (sk != NULL)
+			pool_put(&pf_state_key_pl, sk);
+		if (nk != NULL)
+			pool_put(&pf_state_key_pl, nk);
 	}
 
 	/* copy back packet headers if we performed NAT operations */
