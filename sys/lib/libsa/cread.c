63e8a98adbc4b54540649bf61e3db114d6eeeae9
miod <miod@openbsd.org>
2009-01-18 21:46:50 +0000

    If open() failed because the file does not exist, a struct sd for this file won't be allocated. Check for this in close() so as not to free() bogus pointers.

--- sys/lib/libsa/cread.c
+++ sys/lib/libsa/cread.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cread.c,v 1.12 2004/04/02 04:39:51 deraadt Exp $	*/
+/*	$OpenBSD: cread.c,v 1.13 2009/01/18 21:46:50 miod Exp $	*/
 /*	$NetBSD: cread.c,v 1.2 1997/02/04 18:38:20 thorpej Exp $	*/
 
 /*
@@ -250,11 +250,12 @@ close(int fd)
 		return(oclose(fd));
 
 	s = ss[fd];
+	if (s != NULL) {
+		inflateEnd(&(s->stream));
 
-	inflateEnd(&(s->stream));
-
-	free(s->inbuf, Z_BUFSIZE);
-	free(s, sizeof(struct sd));
+		free(s->inbuf, Z_BUFSIZE);
+		free(s, sizeof(struct sd));
+	}
 
 	return(oclose(fd));
 }
