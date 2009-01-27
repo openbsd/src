81937d42804b53a6737473434bf2d87f7393c3fb
michele <michele@openbsd.org>
2009-01-27 12:45:52 +0000

    No need to check if neighbor exists before adding it as it is already done in rde_nbr_new().
    ok claudio@ stsp@

--- usr.sbin/ospfd/rde.c
+++ usr.sbin/ospfd/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.76 2009/01/07 21:16:36 claudio Exp $ */
+/*	$OpenBSD: rde.c,v 1.77 2009/01/27 12:45:52 michele Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Claudio Jeker <claudio@openbsd.org>
@@ -270,10 +270,9 @@ rde_dispatch_imsg(int fd, short event, void *bula)
 				fatalx("invalid size of OE request");
 			memcpy(&rn, imsg.data, sizeof(rn));
 
-			if (rde_nbr_find(imsg.hdr.peerid))
+			if (rde_nbr_new(imsg.hdr.peerid, &rn) == NULL)
 				fatalx("rde_dispatch_imsg: "
 				    "neighbor already exists");
-			rde_nbr_new(imsg.hdr.peerid, &rn);
 			break;
 		case IMSG_NEIGHBOR_DOWN:
 			rde_nbr_del(rde_nbr_find(imsg.hdr.peerid));
