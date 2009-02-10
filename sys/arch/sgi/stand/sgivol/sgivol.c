53188a8f6b79195ed5ba36d822e7fa8df1eb7c44
jsing <jsing@openbsd.org>
2009-02-10 03:13:20 +0000

    Unbreak sgivol. When writing to the raw device use the native blocksize. Whilst here, check the return value of fread() and improve error messages.
    ok krw@ miod@

--- sys/arch/sgi/stand/sgivol/sgivol.c
+++ sys/arch/sgi/stand/sgivol/sgivol.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sgivol.c,v 1.12 2008/08/25 14:53:34 jsing Exp $	*/
+/*	$OpenBSD: sgivol.c,v 1.13 2009/02/10 03:13:20 jsing Exp $	*/
 /*	$NetBSD: sgivol.c,v 1.8 2003/11/08 04:59:00 sekiya Exp $	*/
 
 /*-
@@ -422,10 +422,12 @@ write_file(void)
 	i = st.st_size;
 	fp = fopen(ufilename, "r");
 	while (i > 0) {
+		bzero(fbuf, fbufsize);
 		fsize = i > fbufsize ? fbufsize : i;
-		fread(fbuf, 1, fsize, fp);
-		if (write(fd, fbuf, fsize) != fsize)
-			err(1, "write file");
+		if (fread(fbuf, 1, fsize, fp) != fsize)
+			err(1, "reading file from disk");
+		if (write(fd, fbuf, fbufsize) != fbufsize)
+			err(1, "writing file to SGI volume header");
 		i -= fsize;
 	}
 	fclose(fp);
