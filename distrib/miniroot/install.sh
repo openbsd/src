33ce06f9630780b86bc184cbe14a7ca0f5eba7cc
krw <krw@openbsd.org>
2009-02-19 02:11:32 +0000

    Third time lucky. Proper use of quotes when comparing the first and second copies of the entered password will preserve leading and trailing spaces.
    Prodded by dale@. Proper clue pointed out by todd@.

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: install.sh,v 1.160 2009/02/15 15:56:45 krw Exp $
+#	$OpenBSD: install.sh,v 1.161 2009/02/19 02:11:32 krw Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2004 Todd Miller, Theo de Raadt, Ken Westerback
@@ -334,7 +334,9 @@ while :; do
 	_password=$resp
 
 	askpass "Password for root account? (again)"
-	[[ $resp == $_password ]] && break
+	# N.B.: Need quotes around $resp and $_password to preserve leading
+	#       or trailing spaces.
+	[[ "$resp" == "$_password" ]] && break
 
 	echo "Passwords do not match, try again."
 done
