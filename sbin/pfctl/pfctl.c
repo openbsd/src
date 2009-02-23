5adf98b67837167a2d3c63ab34c11171f99c5930
deraadt <deraadt@openbsd.org>
2009-02-23 06:58:30 +0000

    back out last commit since it breaks a few regress tests -- this will be looked at post-release -- out of time for these kinds of problems david@ says: pfopt6 and f91.ok; pfopt6 change looks ok, but pf91 is of concern.

--- sbin/pfctl/pfctl.c
+++ sbin/pfctl/pfctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfctl.c,v 1.279 2009/02/16 23:53:59 sthen Exp $ */
+/*	$OpenBSD: pfctl.c,v 1.280 2009/02/23 06:58:30 deraadt Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1352,7 +1352,8 @@ pfctl_load_rule(struct pfctl *pf, char *path, struct pf_rule *r, int depth)
 
 	if (pf->opts & PF_OPT_VERBOSE) {
 		INDENT(depth, !(pf->opts & PF_OPT_VERBOSE2));
-		print_rule(r, name, pf->opts & PF_OPT_VERBOSE2);
+		print_rule(r, r->anchor ? r->anchor->name : "",
+		    pf->opts & PF_OPT_VERBOSE2);
 	}
 	path[len] = '\0';
 	pfctl_clear_pool(&r->rpool);
