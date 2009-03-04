2eaae6b33975e4f6c567f4dccfc8a74d6d884f98
stevesk <stevesk@openbsd.org>
2009-03-04 16:54:42 +0000

    print alarm condition from leap indicator; ok henning@ otto@

--- usr.sbin/tcpdump/print-ntp.c
+++ usr.sbin/tcpdump/print-ntp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-ntp.c,v 1.13 2007/10/07 16:41:05 deraadt Exp $	*/
+/*	$OpenBSD: print-ntp.c,v 1.14 2009/03/04 16:54:42 stevesk Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -27,7 +27,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Id: print-ntp.c,v 1.13 2007/10/07 16:41:05 deraadt Exp $ (LBL)";
+    "@(#) $Id: print-ntp.c,v 1.14 2009/03/04 16:54:42 stevesk Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -79,6 +79,10 @@ ntp_print(register const u_char *cp, u_int length)
 	switch (leapind) {
 
 	case NO_WARNING:
+		break;
+
+	case ALARM:
+		fputs(" alarm", stdout);
 		break;
 
 	case PLUS_SEC:
