e74b632f5fc23e91c35c677f82547a2a6ea6e6b5
joris <joris@openbsd.org>
2009-03-23 17:53:17 +0000

    simplify

--- usr.bin/cvs/init.h
+++ usr.bin/cvs/init.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: init.h,v 1.1 2006/06/12 13:56:00 xsa Exp $       */
+/*      $OpenBSD: init.h,v 1.2 2009/03/23 17:53:17 joris Exp $       */
 /*
  * Copyright (c) 2006 Xavier Santolaria <xsa@openbsd.org>
  *
@@ -18,7 +18,7 @@
 #ifndef INIT_H
 #define INIT_H
 
-static const char *const config_contents[] = {
+const char *config_contents[] = {
 	"# Set name of the local tag to use in addition to `Id'\n",
 	"#tag=OpenBSD\n",
 	"# Set default file creation mode mask\n",
