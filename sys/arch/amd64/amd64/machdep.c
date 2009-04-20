d3bfedd763e2415aaa5bdf4f8a26cd09d4651477
reyk <reyk@openbsd.org>
2009-04-20 15:44:49 +0000

    bigmem is not tweakable by config(8), it never was.
    ok oga@

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.88 2009/02/15 02:03:40 marco Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.89 2009/04/20 15:44:49 reyk Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
@@ -1238,7 +1238,6 @@ extern vector IDTVEC(osyscall);
 extern vector IDTVEC(oosyscall);
 extern vector *IDTVEC(exceptions)[];
 
-/* Tweakable by config(8) */
 int bigmem = 0;
 
 void
