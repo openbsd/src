6378b430396562f0098cf904fa7491e065ec4799
stevesk <stevesk@openbsd.org>
2009-05-06 16:05:20 +0000

    remove erroneous fldcnt test.  fldcnt can never be 13 here.  this is apparently a leftover from tty_nmea.c

--- sys/kern/tty_msts.c
+++ sys/kern/tty_msts.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tty_msts.c,v 1.13 2009/04/26 02:25:36 cnst Exp $ */
+/*	$OpenBSD: tty_msts.c,v 1.14 2009/05/06 16:05:20 stevesk Exp $ */
 
 /*
  * Copyright (c) 2008 Marc Balmer <mbalmer@openbsd.org>
@@ -281,8 +281,7 @@ msts_decode(struct msts *np, struct tty *tp, char *fld[], int fldcnt)
 	if (np->time.status == SENSOR_S_UNKNOWN) {
 		np->time.status = SENSOR_S_OK;
 		np->time.flags &= ~SENSOR_FINVALID;
-		if (fldcnt != 13)
-			strlcpy(np->time.desc, "MSTS", sizeof(np->time.desc));
+		strlcpy(np->time.desc, "MSTS", sizeof(np->time.desc));
 	}
 	/*
 	 * only update the timeout if the clock reports the time a valid,
