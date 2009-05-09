ce9568e914a0ba5576e8ee1bbe323c9be5b270fb
mglocker <mglocker@openbsd.org>
2009-05-09 20:34:25 +0000

    Need to add new display type to wsconsctl(8), too.
    Noted by miod@

--- sbin/wsconsctl/util.c
+++ sbin/wsconsctl/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.40 2009/03/12 19:10:22 kettenis Exp $ */
+/*	$OpenBSD: util.c,v 1.41 2009/05/09 20:34:25 mglocker Exp $ */
 /*	$NetBSD: util.c,v 1.8 2000/03/14 08:11:53 sato Exp $ */
 
 /*-
@@ -143,7 +143,8 @@ static const struct nameint dpytype_tab[] = {
 	{ WSDISPLAY_TYPE_GBE,		"gbe" },
 	{ WSDISPLAY_TYPE_LEGSS,		"dec-legss" },
 	{ WSDISPLAY_TYPE_IFB,		"ifb" },
-	{ WSDISPLAY_TYPE_RAPTOR,	"raptor" }
+	{ WSDISPLAY_TYPE_RAPTOR,	"raptor" },
+	{ WSDISPLAY_TYPE_DL,		"displaylink" }
 };
 
 static const struct nameint kbdenc_tab[] = {
