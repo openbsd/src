9690cfb6311b2167c8107db1c6c984b0c8fe2839
ray <ray@openbsd.org>
2009-06-02 11:14:08 +0000

    Don't dereference NULL if buf winds up being empty. Can't happen right now according to NicM but better safe than sorry.
    OK nicm@

--- usr.bin/tmux/attributes.c
+++ usr.bin/tmux/attributes.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: attributes.c,v 1.1 2009/06/01 22:58:49 nicm Exp $ */
+/* $OpenBSD: attributes.c,v 1.2 2009/06/02 11:14:08 ray Exp $ */
 
 /*
  * Copyright (c) 2009 Joshua Elsasser <josh@elsasser.org>
@@ -45,7 +45,8 @@ attributes_tostring(u_char ch)
 		strlcat(buf, "hidden,", sizeof (buf));
 	if (ch & GRID_ATTR_ITALICS)
 		strlcat(buf, "italics,", sizeof (buf));
-	*(strrchr(buf, ',')) = '\0';
+	if (*buf)
+		*(strrchr(buf, ',')) = '\0';
 
 	return (buf);
 }
