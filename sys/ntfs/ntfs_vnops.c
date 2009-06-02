d7aec474b6e5c7ffd3e40ea84b9407f495103fe5
jasper <jasper@openbsd.org>
2009-06-02 20:48:48 +0000

    - make sure biodone() gets run at IPL_BIO
    OK thib@

--- sys/ntfs/ntfs_vnops.c
+++ sys/ntfs/ntfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ntfs_vnops.c,v 1.13 2008/05/13 02:24:08 brad Exp $	*/
+/*	$OpenBSD: ntfs_vnops.c,v 1.14 2009/06/02 20:48:48 jasper Exp $	*/
 /*	$NetBSD: ntfs_vnops.c,v 1.6 2003/04/10 21:57:26 jdolecek Exp $	*/
 
 /*
@@ -320,7 +320,7 @@ ntfs_strategy(ap)
 	struct fnode *fp = VTOF(vp);
 	struct ntnode *ip = FTONT(fp);
 	struct ntfsmount *ntmp = ip->i_mp;
-	int error;
+	int error, s;
 
 #ifdef __FreeBSD__
 	dprintf(("ntfs_strategy: offset: %d, blkno: %d, lblkno: %d\n",
@@ -384,7 +384,9 @@ ntfs_strategy(ap)
 			}
 		}
 	}
+	s = splbio();
 	biodone(bp);
+	splx(s);
 	return (error);
 }
 
