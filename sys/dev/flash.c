860223f8491fa1141e2a97af16a9df2489e9de63
deraadt <deraadt@openbsd.org>
2009-06-04 23:13:21 +0000

    remove variable left behind by thib

--- sys/dev/flash.c
+++ sys/dev/flash.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: flash.c,v 1.10 2009/06/03 22:09:30 thib Exp $	*/
+/*	$OpenBSD: flash.c,v 1.11 2009/06/04 23:13:21 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2005 Uwe Stuehler <uwe@openbsd.org>
@@ -877,7 +877,7 @@ flashsize(dev_t dev)
 void
 flashstart(struct flash_softc *sc)
 {
-	struct buf *dp, *bp;
+	struct buf *bp;
 
 	while (1) {
 		/* Remove the next buffer from the queue or stop. */
