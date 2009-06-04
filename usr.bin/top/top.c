7a443088e17bdfb7e7f3ba26b52f4e41677c84b2
sthen <sthen@openbsd.org>
2009-06-04 19:01:30 +0000

    with -d1, switch interactive mode off, so the screen isn't restored immediately afterwards on a smart terminal. found by claudio@. "I like that diff" deraadt "yes please!" phessler "I think that makes sense" claudio

--- usr.bin/top/top.c
+++ usr.bin/top/top.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: top.c,v 1.65 2007/11/29 10:06:30 otto Exp $	*/
+/*	$OpenBSD: top.c,v 1.66 2009/06/04 19:01:30 sthen Exp $	*/
 
 /*
  *  Top users/processes display for Unix
@@ -190,6 +190,8 @@ parseargs(int ac, char **av)
 		case 'd':	/* number of displays to show */
 			if ((i = atoiwi(optarg)) != Invalid && i != 0) {
 				displays = i;
+				if (displays == 1)
+					interactive = No;
 				break;
 			}
 			new_message(MT_delayed,
