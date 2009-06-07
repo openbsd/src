baede216d50237534825cf13a59fdbb691179570
sobrado <sobrado@openbsd.org>
2009-06-07 21:48:16 +0000

    improve spacing in usage()

--- usr.sbin/pcidump/pcidump.c
+++ usr.sbin/pcidump/pcidump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcidump.c,v 1.18 2009/06/07 09:50:52 jmc Exp $	*/
+/*	$OpenBSD: pcidump.c,v 1.19 2009/06/07 21:48:16 sobrado Exp $	*/
 
 /*
  * Copyright (c) 2006, 2007 David Gwynne <loki@animata.net>
@@ -48,7 +48,8 @@ usage(void)
 {
 	extern char *__progname;
 
-	fprintf(stderr, "usage: %s [-v] [-x|-xx] [-d pcidev] [bus:dev:func]\n",
+	fprintf(stderr,
+	    "usage: %s [-v] [-x | -xx] [-d pcidev] [bus:dev:func]\n",
 	    __progname);
 	exit(1);
 }
