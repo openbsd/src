fc9c78967bbe4e6bb39b48366ed11a21419930cb
thib <thib@openbsd.org>
2009-06-22 10:51:06 +0000

    sync m_inithdr() with m_gethdr() after the addition of rdomain to struct pkthdr.
    "Doh!" claudio@
    ok henning@

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_mbuf.c,v 1.121 2009/06/05 00:05:21 claudio Exp $	*/
+/*	$OpenBSD: uipc_mbuf.c,v 1.122 2009/06/22 10:51:06 thib Exp $	*/
 /*	$NetBSD: uipc_mbuf.c,v 1.15.4.1 1996/06/13 17:11:44 cgd Exp $	*/
 
 /*
@@ -248,6 +248,7 @@ m_inithdr(struct mbuf *m)
 	m->m_data = m->m_pktdat;
 	m->m_flags = M_PKTHDR;
 	m->m_pkthdr.rcvif = NULL;
+	m->m_pkthdr.rdomain = 0;
 	SLIST_INIT(&m->m_pkthdr.tags);
 	m->m_pkthdr.csum_flags = 0;
 	m->m_pkthdr.ether_vtag = 0;
