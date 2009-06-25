27d0e24a5e71d615a420649399963766c46784cf
okan <okan@openbsd.org>
2009-06-25 20:40:33 +0000

    sync the SENSOR_DRIVE_* names with sysctl(8)'s
    ok canacar@

--- usr.bin/systat/sensors.c
+++ usr.bin/systat/sensors.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sensors.c,v 1.15 2009/06/25 20:39:02 okan Exp $	*/
+/*	$OpenBSD: sensors.c,v 1.16 2009/06/25 20:40:33 okan Exp $	*/
 
 /*
  * Copyright (c) 2007 Deanna Phillips <deanna@openbsd.org>
@@ -59,8 +59,8 @@ int select_sn(void);
 
 const char *drvstat[] = {
 	NULL,
-	"empty", "ready", "powerup", "online", "idle", "active",
-	"rebuild", "powerdown", "fail", "pfail"
+	"empty", "ready", "powering up", "online", "idle", "active",
+	"rebuilding", "powering down", "failed", "degraded"
 };
 
 
