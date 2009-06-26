3a5cd88d1a43021b942e819ca0b59a0823cc06a4
nicm <nicm@openbsd.org>
2009-06-26 10:55:37 +0000

    Also #ifndef nitems here, caught by Thomas Jeunet.

--- usr.bin/tmux/procname.c
+++ usr.bin/tmux/procname.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: procname.c,v 1.1 2009/06/01 22:58:49 nicm Exp $ */
+/* $OpenBSD: procname.c,v 1.2 2009/06/26 10:55:37 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -25,7 +25,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#ifndef nitems
 #define nitems(_a) (sizeof((_a)) / sizeof((_a)[0]))
+#endif
 
 #define is_runnable(p) \
 	((p)->p_stat == SRUN || (p)->p_stat == SIDL || (p)->p_stat == SONPROC)
