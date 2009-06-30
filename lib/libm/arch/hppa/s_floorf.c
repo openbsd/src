7089d81aec865bbbb25ab3cf8ebec870772a27f2
kettenis <kettenis@openbsd.org>
2009-06-30 20:20:20 +0000

    floor(3) should round towards -inf instead of towards zero.
    ok martynas@, deraadt@

--- lib/libm/arch/hppa/s_floorf.c
+++ lib/libm/arch/hppa/s_floorf.c
@@ -3,7 +3,7 @@
  */
 
 #if defined(LIBM_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: s_floorf.c,v 1.4 2003/10/31 23:22:37 mickey Exp $";
+static char rcsid[] = "$OpenBSD: s_floorf.c,v 1.5 2009/06/30 20:20:20 kettenis Exp $";
 #endif
 
 #include <sys/types.h>
@@ -16,8 +16,7 @@ floorf(float x)
 	u_int64_t ofpsr, fpsr;
 
 	__asm__ __volatile__("fstds %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
-	fpsr = (ofpsr & ~((u_int64_t)FP_RM << (9 + 32))) |
-	    ((u_int64_t)FP_RZ << (9 + 32));
+	fpsr = ofpsr | ((u_int64_t)FP_RM << (9 + 32));
 	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&fpsr));
 
 	__asm__ __volatile__("frnd,sgl %0,%0" : "+f" (x));
