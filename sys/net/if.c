c2ea215e3b4e857f3078e258bdc1a985ae213000
blambert <blambert@openbsd.org>
2009-07-09 06:40:20 +0000

    unsigned -> unsigned int
    ok claudio@, henning@

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.196 2009/06/06 12:31:17 rainer Exp $	*/
+/*	$OpenBSD: if.c,v 1.197 2009/07/09 06:40:20 blambert Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -307,7 +307,7 @@ if_attachsetup(struct ifnet *ifp)
 void
 if_alloc_sadl(struct ifnet *ifp)
 {
-	unsigned socksize, ifasize;
+	unsigned int socksize, ifasize;
 	int namelen, masklen;
 	struct sockaddr_dl *sdl;
 	struct ifaddr *ifa;
