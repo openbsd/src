7200feb2e8b78341674e118168d1cfcede258444
martynas <martynas@openbsd.org>
2009-07-15 20:08:43 +0000

    round, roundf, trunc, truncf for hppa;  ok kettenis@

---
+++ lib/libm/arch/hppa/s_roundf.c
@@ -0,0 +1,27 @@
+/*
+ * Written by Michael Shalayeff. Public Domain
+ */
+
+#if defined(LIBM_SCCS) && !defined(lint)
+static char rcsid[] = "$OpenBSD: s_roundf.c,v 1.1 2009/07/15 20:08:43 martynas Exp $";
+#endif
+
+#include <sys/types.h>
+#include <machine/ieeefp.h>
+#include "math.h"
+
+float
+roundf(float x)
+{
+	u_int64_t ofpsr, fpsr;
+
+	__asm__ __volatile__("fstds %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
+	fpsr = (ofpsr & ~((u_int64_t)FP_RM << (9 + 32))) |
+	    ((u_int64_t)FP_RN << (9 + 32));
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&fpsr));
+
+	__asm__ __volatile__("frnd,sgl %0,%0" : "+f" (x));
+
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&ofpsr));
+	return (x);
+}
