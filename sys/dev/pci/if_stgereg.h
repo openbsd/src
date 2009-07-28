eea1fe6b68581dd568b69a3274fc49c9ac478b94
sthen <sthen@openbsd.org>
2009-07-28 22:15:44 +0000

    Garbage-collect softc member stge_if_flags, unused since if_stge.c r1.46. From Brad.

--- sys/dev/pci/if_stgereg.h
+++ sys/dev/pci/if_stgereg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_stgereg.h,v 1.9 2008/06/26 05:42:17 ray Exp $	*/
+/*	$OpenBSD: if_stgereg.h,v 1.10 2009/07/28 22:15:44 sthen Exp $	*/
 /*	$NetBSD: if_stgereg.h,v 1.3 2003/02/10 21:10:07 christos Exp $	*/
 
 /*-
@@ -531,7 +531,6 @@ struct stge_softc {
 	struct arpcom sc_arpcom;	/* ethernet common data */
 	void *sc_sdhook;		/* shutdown hook */
 	int sc_rev;			/* silicon revision */ 
-	int stge_if_flags;
 	void *sc_ih;			/* interrupt cookie */
 
 	struct mii_data sc_mii;		/* MII/media information */
