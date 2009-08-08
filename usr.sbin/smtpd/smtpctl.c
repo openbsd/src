0cc8978a25988a6e2b6243b04e4b7107ca832b8c
ian <ian@openbsd.org>
2009-08-08 19:16:41 +0000

    mailq is a common sendmail alias for showing the queue; make smtpctl implement this, ignoring sendmail options for now. OK gilles@, jackekm@

--- usr.sbin/smtpd/smtpctl.c
+++ usr.sbin/smtpd/smtpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpctl.c,v 1.32 2009/08/08 00:16:49 gilles Exp $	*/
+/*	$OpenBSD: smtpctl.c,v 1.33 2009/08/08 19:16:41 ian Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -96,7 +96,12 @@ main(int argc, char *argv[])
 	/* parse options */
 	if (strcmp(__progname, "sendmail") == 0 || strcmp(__progname, "send-mail") == 0)
 		sendmail = 1;
-	else if (strcmp(__progname, "smtpctl") == 0) {
+	else if (strcmp(__progname, "mailq") == 0) {
+		if (geteuid())
+			errx(1, "need root privileges");
+		show_queue(PATH_QUEUE, 0);
+		return 0;
+	} else if (strcmp(__progname, "smtpctl") == 0) {
 		/* check for root privileges */
 		if (geteuid())
 			errx(1, "need root privileges");
