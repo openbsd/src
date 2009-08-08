6e18f1005a354d2fec127ad1500655ca702a4a80
nicm <nicm@openbsd.org>
2009-08-08 18:33:40 +0000

    Make imsg_clear close and free any fds in the received fd queue as well as freeing the msgbuf.
    While here also remove an unnecessary while loop.
    
    ok eric pyr

--- usr.sbin/ldpd/imsg.c
+++ usr.sbin/ldpd/imsg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: imsg.c,v 1.6 2009/06/08 08:30:06 dlg Exp $	*/
+/*	$OpenBSD: imsg.c,v 1.7 2009/08/08 18:33:40 nicm Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -263,6 +263,9 @@ imsg_flush(struct imsgbuf *ibuf)
 void
 imsg_clear(struct imsgbuf *ibuf)
 {
-	while (ibuf->w.queued)
-		msgbuf_clear(&ibuf->w);
+	int	fd;
+
+	msgbuf_clear(&ibuf->w);
+	while ((fd = imsg_get_fd(ibuf)) != -1)
+		close(fd);
 }
