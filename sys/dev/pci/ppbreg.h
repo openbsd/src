2f6010e7b73a2c51ad8b5b276c2968344840e040
deraadt <deraadt@openbsd.org>
2009-08-09 09:59:23 +0000

    spelling error

--- sys/dev/pci/ppbreg.h
+++ sys/dev/pci/ppbreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ppbreg.h,v 1.2 2001/11/14 21:04:46 mickey Exp $	*/
+/*	$OpenBSD: ppbreg.h,v 1.3 2009/08/09 09:59:23 deraadt Exp $	*/
 /*	$NetBSD: ppbreg.h,v 1.3 2001/07/06 18:07:16 mcr Exp $	*/
 
 /*
@@ -92,7 +92,7 @@
 /* 
  * bridge control register (see table 3.9 of ppb rev. 1.1)
  *
- * Note these are in the *upper* 16 bits if the Bridge Control
+ * Note these are in the *upper* 16 bits of the Bridge Control
  * Register (the bottom 16 are Interrupt Line and Interrupt Pin).
  */
 #define	PPB_BC_BITBASE			   16
