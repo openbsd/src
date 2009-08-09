1b1073a6ec0f83fd5fbb6d413fffee9cc1361bf8
martynas <martynas@openbsd.org>
2009-08-09 21:58:00 +0000

    indicate success for setting directory attributes;  first step towards making directory moves work...  discussed w. & makes sense - millert@

--- sys/msdosfs/msdosfs_vnops.c
+++ sys/msdosfs/msdosfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: msdosfs_vnops.c,v 1.67 2008/06/09 23:38:37 millert Exp $	*/
+/*	$OpenBSD: msdosfs_vnops.c,v 1.68 2009/08/09 21:58:00 martynas Exp $	*/
 /*	$NetBSD: msdosfs_vnops.c,v 1.63 1997/10/17 11:24:19 ws Exp $	*/
 
 /*-
@@ -370,7 +370,7 @@ msdosfs_setattr(v)
 	 * Directories must not ever get their attributes modified
 	 */
 	if (ap->a_vp->v_type == VDIR)
-		return EISDIR;
+		return (0);
 
 	if (vap->va_size != VNOVAL) {
 		error = detrunc(dep, (uint32_t)vap->va_size, 0, cred, ap->a_p);
