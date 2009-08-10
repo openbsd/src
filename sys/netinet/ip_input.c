99aee2909fe84780235c6c6f9529b03318d026fb
henning <henning@openbsd.org>
2009-08-10 15:26:33 +0000

    we need to null mcopy, gotos bite. theo and i both missed them, theo ok

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_input.c,v 1.168 2009/08/10 13:20:08 henning Exp $	*/
+/*	$OpenBSD: ip_input.c,v 1.169 2009/08/10 15:26:33 henning Exp $	*/
 /*	$NetBSD: ip_input.c,v 1.30 1996/03/16 23:53:58 christos Exp $	*/
 
 /*
@@ -1426,7 +1426,7 @@ ip_forward(m, srcrt)
 	int error, type = 0, code = 0, destmtu = 0, len;
 	u_int rtableid = 0;
 	n_long dest;
-	struct mbuf mfake, *mcopy;
+	struct mbuf mfake, *mcopy = NULL;
 
 	dest = 0;
 #ifdef DIAGNOSTIC
