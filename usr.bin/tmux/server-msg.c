1f2f8f5165f4250103a120ae4b2c705316ed2303
nicm <nicm@openbsd.org>
2009-08-13 12:15:45 +0000

    If the client passes zero for the window size in the identify message (which it can, for example on serial terminals), reset it to 80x25, same as for resize messages. Problem reported by kettenis@.

--- usr.bin/tmux/server-msg.c
+++ usr.bin/tmux/server-msg.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server-msg.c,v 1.13 2009/08/11 21:28:11 nicm Exp $ */
+/* $OpenBSD: server-msg.c,v 1.14 2009/08/13 12:15:45 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -238,7 +238,11 @@ void
 server_msg_identify(struct client *c, struct msg_identify_data *data, int fd)
 {
 	c->tty.sx = data->sx;
+	if (c->tty.sx == 0)
+		c->tty.sx = 80;
 	c->tty.sy = data->sy;
+	if (c->tty.sy == 0)
+		c->tty.sy = 25;
 
 	c->cwd = NULL;
 	data->cwd[(sizeof data->cwd) - 1] = '\0';
