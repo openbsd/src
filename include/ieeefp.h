22dda6cb9f78f50847825274be50c2797fe49e26
martynas <martynas@openbsd.org>
2009-09-27 21:23:55 +0000

    use c calling conventions so that g++ doesn't mangle fpgetmask & friends' names unbreaking at least enblend-enfuse & inkscape on macppc & alpha reported, tested: ajacoutot@, sthen@ ok: millert@, otto@

--- include/ieeefp.h
+++ include/ieeefp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieeefp.h,v 1.3 2002/02/16 21:27:17 millert Exp $	*/
+/*	$OpenBSD: ieeefp.h,v 1.4 2009/09/27 21:23:55 martynas Exp $	*/
 
 /* 
  * Written by J.T. Conklin, Apr 6, 1995
@@ -11,11 +11,13 @@
 #include <sys/cdefs.h>
 #include <machine/ieeefp.h>
 
+__BEGIN_DECLS
 extern fp_rnd    fpgetround(void);
 extern fp_rnd    fpsetround(fp_rnd);
 extern fp_except fpgetmask(void);
 extern fp_except fpsetmask(fp_except);
 extern fp_except fpgetsticky(void);
 extern fp_except fpsetsticky(fp_except);
+__END_DECLS
 
 #endif /* _IEEEFP_H_ */
