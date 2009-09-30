f6f3b5e045689d955ad1fd661f83da97b8d1d71a
claudio <claudio@openbsd.org>
2009-09-30 14:39:07 +0000

    Oups, these changes should not have been commited. Revert.

--- usr.sbin/ospfd/neighbor.c
+++ usr.sbin/ospfd/neighbor.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: neighbor.c,v 1.38 2009/09/30 14:37:11 claudio Exp $ */
+/*	$OpenBSD: neighbor.c,v 1.39 2009/09/30 14:39:07 claudio Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -211,11 +211,6 @@ nbr_fsm(struct nbr *nbr, enum nbr_event event)
 
 			gettimeofday(&now, NULL);
 			nbr->uptime = now.tv_sec;
-
-			/* demote P2P links if the neighbor resets */
-			if (nbr->iface->type == IF_TYPE_POINTOPOINT)
-				ospfe_demote_iface(nbr->iface,
-				    !(old_state & NBR_STA_FULL));
 		}
 
 		/* bidirectional communication lost */
