116493d327f393797ad5158534f5e8e80a4890f2
sobrado <sobrado@openbsd.org>
2009-10-11 15:50:34 +0000

    makes source code fit on a 80-column display, no binary change.

--- usr.bin/vacation/vacation.c
+++ usr.bin/vacation/vacation.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vacation.c,v 1.30 2007/09/11 15:47:17 gilles Exp $	*/
+/*	$OpenBSD: vacation.c,v 1.31 2009/10/11 15:50:34 sobrado Exp $	*/
 /*	$NetBSD: vacation.c,v 1.7 1995/04/29 05:58:27 cgd Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)vacation.c	8.2 (Berkeley) 1/26/94";
 #endif
-static char rcsid[] = "$OpenBSD: vacation.c,v 1.30 2007/09/11 15:47:17 gilles Exp $";
+static char rcsid[] = "$OpenBSD: vacation.c,v 1.31 2009/10/11 15:50:34 sobrado Exp $";
 #endif /* not lint */
 
 /*
@@ -319,7 +319,8 @@ findme:			for (cur = names; !tome && cur; cur = cur->next)
 	if (!tome)
 		exit(0);
 	if (!*from) {
-		syslog(LOG_NOTICE, "no initial \"From\" or \"Return-Path\"line.");
+		syslog(LOG_NOTICE,
+		    "no initial \"From\" or \"Return-Path\"line.");
 		exit(1);
 	}
 }
