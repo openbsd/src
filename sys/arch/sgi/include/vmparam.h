bca357705698462c58c3d6eeb2c616037e0cdd0e
miod <miod@openbsd.org>
2009-10-14 20:18:26 +0000

    Add some comments to explain why the DMA32 physseg is really 2**31 bytes long. Prompted by deraadt@ long ago.

--- sys/arch/sgi/include/vmparam.h
+++ sys/arch/sgi/include/vmparam.h
@@ -1,12 +1,23 @@
-/*	$OpenBSD: vmparam.h,v 1.3 2009/05/08 18:42:04 miod Exp $ */
-
+/*	$OpenBSD: vmparam.h,v 1.4 2009/10/14 20:18:26 miod Exp $ */
+/* public domain */
 #ifndef _SGI_VMPARAM_H_
 #define _SGI_VMPARAM_H_
 
 #define	VM_PHYSSEG_MAX	32	/* Max number of physical memory segments */
 
+/*
+ * On Origin and Octane families, DMA to 32-bit PCI devices is restricted.
+ *
+ * Systems with physical memory after the 2GB boundary needs to ensure
+ * memory which may used for DMA transfers is allocated from the low
+ * memory range.
+ *
+ * Other systems, like the O2, do not have such a restriction, but can not
+ * have more than 2GB of physical memory, so this doesn't affect them.
+ */
+
 #define	VM_NFREELIST		2
-#define	VM_FREELIST_DMA32	1	/* memory under 2GB suitable for DMA */
+#define	VM_FREELIST_DMA32	1	/* memory suitable for 32-bit DMA */
 
 #include <mips64/vmparam.h>
 
