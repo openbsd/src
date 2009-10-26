ec32666053187384e10cef116fe6acd8f7923928
jasper <jasper@openbsd.org>
2009-10-26 21:09:33 +0000

    add ENOTSUP and bump libc minor
    discussed with a few
    ok deraadt@

--- sys/sys/errno.h
+++ sys/sys/errno.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: errno.h,v 1.20 2007/09/03 14:37:52 millert Exp $	*/
+/*	$OpenBSD: errno.h,v 1.21 2009/10/26 21:09:33 jasper Exp $	*/
 /*	$NetBSD: errno.h,v 1.10 1996/01/20 01:33:53 jtc Exp $	*/
 
 /*
@@ -166,8 +166,9 @@
 #endif /* __BSD_VISIBLE */
 #define	EIDRM		89		/* Identifier removed */
 #define	ENOMSG		90		/* No message of desired type */
+#define ENOTSUP		91		/* Operation not supported */
 #if __BSD_VISIBLE
-#define	ELAST		90		/* Must be equal largest errno */
+#define	ELAST		91		/* Must be equal largest errno */
 #endif /* __BSD_VISIBLE */
 
 #ifdef _KERNEL
