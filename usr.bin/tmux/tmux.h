bb2f750c1dadf2dd13659e9546ef581df27bfdff
nicm <nicm@openbsd.org>
2009-11-02 20:18:22 +0000

    Double the escape timer (the time after a \033 is received before tmux gives up waiting to see if it is part of a key sequence and passes it through) to 500 ms, the previous setting was too fast. Suggested by naddy.

--- usr.bin/tmux/tmux.h
+++ usr.bin/tmux/tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.153 2009/11/01 23:20:37 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.154 2009/11/02 20:18:22 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -60,7 +60,7 @@ extern char   **environ;
 #define NAME_INTERVAL 500
 
 /* Escape timer period, in milliseconds. */
-#define ESCAPE_PERIOD 250
+#define ESCAPE_PERIOD 500
 
 /* Maximum poll timeout (when attached). */
 #define POLL_TIMEOUT 50
