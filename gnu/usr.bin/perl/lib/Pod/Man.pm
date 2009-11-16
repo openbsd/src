16446b861575786f90d8f4c9a5fe669e6bc43b76
millert <millert@openbsd.org>
2009-11-16 15:10:18 +0000

    Fix generation of .IX entries that include characters escaped with a backslash.  Fix from Russ Allbery, the podlators maintainer.

--- gnu/usr.bin/perl/lib/Pod/Man.pm
+++ gnu/usr.bin/perl/lib/Pod/Man.pm
@@ -712,6 +712,7 @@ sub outindex {
     for (@output) {
         my ($type, $entry) = @$_;
         $entry =~ s/\"/\"\"/g;
+	$entry =~ s/\\/\\\\/g;
         $self->output (".IX $type " . '"' . $entry . '"' . "\n");
     }
 }
