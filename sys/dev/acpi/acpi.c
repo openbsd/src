513569d7d2d9836b53c698a88c82f5f8086b3b59
deraadt <deraadt@openbsd.org>
2009-11-24 16:12:37 +0000

    abort suspends on SMP systems until that gets written; ok mlarkin

--- sys/dev/acpi/acpi.c
+++ sys/dev/acpi/acpi.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpi.c,v 1.148 2009/11/23 22:34:23 mlarkin Exp $ */
+/* $OpenBSD: acpi.c,v 1.149 2009/11/24 16:12:37 deraadt Exp $ */
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  * Copyright (c) 2005 Jordan Hargrave <jordan@openbsd.org>
@@ -1818,6 +1818,10 @@ acpi_sleep_state(struct acpi_softc *sc, int state)
 {
 	int ret;
 
+#ifdef MULTIPROCESSOR
+	if (ncpus > 1)	/* cannot suspend MP yet */
+		return (0);
+#endif
 	switch (state) {
 	case ACPI_STATE_S0:
 		return (0);
