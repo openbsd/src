43ef72a2850f518fbd206b28aaba6105982c7a52
chl <chl@openbsd.org>
2009-12-10 00:20:38 +0000

    remove dead assignment and newly created unused variable.
    Found by LLVM/Clang Static Analyzer.
    
    ok dlg@ marco@

--- sys/dev/ic/mpi.c
+++ sys/dev/ic/mpi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpi.c,v 1.123 2009/12/09 04:59:41 marco Exp $ */
+/*	$OpenBSD: mpi.c,v 1.124 2009/12/10 00:20:38 chl Exp $ */
 
 /*
  * Copyright (c) 2005, 2006, 2009 David Gwynne <dlg@openbsd.org>
@@ -2256,7 +2256,6 @@ mpi_portenable(struct mpi_softc *sc)
 {
 	struct mpi_ccb				*ccb;
 	struct mpi_msg_portenable_request	*peq;
-	struct mpi_msg_portenable_repy		*pep;
 	int					s;
 
 	DNPRINTF(MPI_D_MISC, "%s: mpi_portenable\n", DEVNAME(sc));
@@ -2287,7 +2286,6 @@ mpi_portenable(struct mpi_softc *sc)
 		    DEVNAME(sc));
 		return (1);
 	}
-	pep = ccb->ccb_rcb->rcb_reply;
 
 	mpi_push_reply(sc, ccb->ccb_rcb->rcb_reply_dva);
 	mpi_put_ccb(sc, ccb);
