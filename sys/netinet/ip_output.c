3edb4ffb4dc2b14fb84578ae7bdc91af81d3b4f3
deraadt <deraadt@openbsd.org>
2009-12-11 17:50:57 +0000

    Two cases of IPSEC getsockopt() returning two bytes of uninitialialized kernel stack content instead of proper information; found by Clement LECIGNE

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_output.c,v 1.200 2009/11/27 20:05:50 guenther Exp $	*/
+/*	$OpenBSD: ip_output.c,v 1.201 2009/12/11 17:50:57 deraadt Exp $	*/
 /*	$NetBSD: ip_output.c,v 1.28 1996/02/13 23:43:07 christos Exp $	*/
 
 /*
@@ -1593,9 +1593,11 @@ ip_ctloutput(op, so, level, optname, mp)
 			case IP_IPSEC_LOCAL_AUTH:
 				if (inp->inp_ipo != NULL)
 					ipr = inp->inp_ipo->ipo_local_auth;
+				opt16val = IPSP_AUTH_NONE;
 				break;
 			case IP_IPSEC_REMOTE_AUTH:
 				ipr = inp->inp_ipsec_remoteauth;
+				opt16val = IPSP_AUTH_NONE;
 				break;
 			}
 			if (ipr == NULL)
