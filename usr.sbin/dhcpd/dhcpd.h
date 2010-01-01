b387e28ca1a4577ca073cf5b7d3499689f8063a4
krw <krw@openbsd.org>
2010-01-01 02:38:02 +0000

    Delete unused declarations.

--- usr.sbin/dhcpd/dhcpd.h
+++ usr.sbin/dhcpd/dhcpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcpd.h,v 1.37 2010/01/01 01:47:41 krw Exp $ */
+/*	$OpenBSD: dhcpd.h,v 1.38 2010/01/01 02:38:02 krw Exp $ */
 
 /*
  * Copyright (c) 1995, 1996, 1997, 1998, 1999
@@ -604,8 +604,6 @@ void write_leases(void);
 /* alloc.c */
 void * dmalloc(int, char *);
 void dfree(void *, char *);
-struct packet *new_packet(char *);
-struct dhcp_packet *new_dhcp_packet(char *);
 struct tree *new_tree(char *);
 struct tree_cache *new_tree_cache(char *);
 struct hash_table *new_hash_table(int, char *);
@@ -615,18 +613,8 @@ struct lease *new_leases(int, char *);
 struct subnet *new_subnet(char *);
 struct shared_network *new_shared_network(char *);
 struct group *new_group(char *);
-struct protocol *new_protocol(char *);
 struct lease_state *new_lease_state(char *);
-struct domain_search_list *new_domain_search_list(char *);
-struct name_server *new_name_server(char *);
-void free_name_server(struct name_server *, char *);
-void free_domain_search_list(struct domain_search_list *, char *);
 void free_lease_state(struct lease_state *, char *);
-void free_protocol(struct protocol *, char *);
-void free_group(struct group *, char *);
-void free_shared_network(struct shared_network *, char *);
-void free_class(struct class *, char *);
-void free_subnet(struct subnet *, char *);
 void free_lease(struct lease *, char *);
 void free_hash_bucket(struct hash_bucket *, char *);
 void free_tree_cache(struct tree_cache *);
