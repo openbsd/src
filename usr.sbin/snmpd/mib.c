1b0ce453ea2a9729e1de4767a9c96176744aeafe
deraadt <deraadt@openbsd.org>
2010-01-11 06:38:41 +0000

    use calloc() instead of multiplying for malloc(); ok reyk

--- usr.sbin/snmpd/mib.c
+++ usr.sbin/snmpd/mib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mib.c,v 1.32 2009/12/16 22:17:53 deraadt Exp $	*/
+/*	$OpenBSD: mib.c,v 1.33 2010/01/11 06:38:41 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008 Reyk Floeter <reyk@vantronix.net>
@@ -723,7 +723,7 @@ kinfo_proc(u_int32_t idx, struct kinfo_proc2 **kinfo)
 		nkp = count;
 	}
 
-	klist = malloc(count * sizeof(*klist));
+	klist = calloc(count, sizeof(*klist));
 	if (klist == NULL)
 		return (-1);
 
