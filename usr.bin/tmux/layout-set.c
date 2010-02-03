b54d9662adeb0f19fead342c3b69ea9fee27bcee
nicm <nicm@openbsd.org>
2010-02-03 22:24:34 +0000

    Fix divide by zero on small windows with main-* layouts.

--- usr.bin/tmux/layout-set.c
+++ usr.bin/tmux/layout-set.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: layout-set.c,v 1.4 2009/12/03 22:50:10 nicm Exp $ */
+/* $OpenBSD: layout-set.c,v 1.5 2010/02/03 22:24:34 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -244,6 +244,8 @@ layout_set_main_h(struct window *w)
 
 	/* How many rows and columns will be needed? */
 	columns = w->sx / (PANE_MINIMUM + 1);	/* maximum columns */
+	if (columns == 0)
+		columns = 1;
 	rows = 1 + (n - 1) / columns;
 	columns = 1 + (n - 1) / rows;
 	width = w->sx / columns;
@@ -353,6 +355,8 @@ layout_set_main_v(struct window *w)
 
 	/* How many rows and columns will be needed? */
 	rows = w->sy / (PANE_MINIMUM + 1);	/* maximum rows */
+	if (rows == 0)
+		rows = 1;
 	columns = 1 + (n - 1) / rows;
 	rows = 1 + (n - 1) / columns;
 	height = w->sy / rows;
