b2b27e1965ea2b2031a3ae729708a02445062f3d
djm <djm@openbsd.org>
2010-03-16 16:36:49 +0000

    crank version to openssh-5.5 since we have a few fixes since 5.4; requested deraadt@ kettenis@

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.57 2010/03/07 22:01:32 djm Exp $ */
+/* $OpenBSD: version.h,v 1.58 2010/03/16 16:36:49 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_5.4"
+#define SSH_VERSION	"OpenSSH_5.5"
