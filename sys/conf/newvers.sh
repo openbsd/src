855ac2478c09f0f8d34ce4c18334372faeea1142
otto <otto@openbsd.org>
2010-03-18 21:17:48 +0000

    move to 4.7-current

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.103 2010/03/05 10:59:35 miod Exp $
+#	$OpenBSD: newvers.sh,v 1.104 2010/03/18 21:17:48 otto Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -59,15 +59,17 @@ id=`basename ${d}`
 # -current and -beta tagging:
 #	For release, select STATUS ""
 #	Right after release unlock, select STATUS "-current"
+#	and enable POOL_DEBUG in sys/conf/GENERIC
 #	A month or so before release, select STATUS "-beta"
+#	and disable POOL_DEBUG in sys/conf/GENERIC
 
 ost="OpenBSD"
 osr="4.7"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #endif
 
