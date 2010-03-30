38dcaba1e0cfb22b690596d4bfb0a0a417462ddd
claudio <claudio@openbsd.org>
2010-03-30 15:43:30 +0000

    Hmpf, aid2af() leftover from the time I did the transition that should not be there. network X set nexthop Y failed because of this.

--- usr.sbin/bgpd/rde.c
+++ usr.sbin/bgpd/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.289 2010/03/29 09:06:56 claudio Exp $ */
+/*	$OpenBSD: rde.c,v 1.290 2010/03/30 15:43:30 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -3007,7 +3007,7 @@ network_add(struct network_config *nc, int flagstatic)
 	if (!flagstatic)
 		asp->flags |= F_ANN_DYNAMIC;
 
-	rde_apply_set(asp, &nc->attrset, aid2af(nc->prefix.aid), peerself, peerself);
+	rde_apply_set(asp, &nc->attrset, nc->prefix.aid, peerself, peerself);
 	for (i = 1; i < rib_size; i++)
 		path_update(&ribs[i], peerself, asp, &nc->prefix,
 		    nc->prefixlen);
