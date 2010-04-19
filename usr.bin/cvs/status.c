7d7c52be4c4357e59dfa97425b8d3e224d8a1250
millert <millert@openbsd.org>
2010-04-19 13:03:10 +0000

    Increase buffer size to support large pathnames.  Committing on behalf of stsp@ who is travelling.  OK miod@ deraadt@ xsa@ jsg@

--- usr.bin/cvs/status.c
+++ usr.bin/cvs/status.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: status.c,v 1.92 2009/03/21 15:33:22 joris Exp $	*/
+/*	$OpenBSD: status.c,v 1.93 2010/04/19 13:03:10 millert Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  * Copyright (c) 2005-2008 Xavier Santolaria <xsa@openbsd.org>
@@ -121,7 +121,8 @@ cvs_status_local(struct cvs_file *cf)
 	size_t len;
 	RCSNUM *head;
 	const char *status;
-	char buf[128], timebuf[CVS_TIME_BUFSZ], revbuf[CVS_REV_BUFSZ];
+	char buf[MAXPATHLEN + CVS_REV_BUFSZ + 128];
+	char timebuf[CVS_TIME_BUFSZ], revbuf[CVS_REV_BUFSZ];
 	struct rcs_sym *sym;
 
 	cvs_log(LP_TRACE, "cvs_status_local(%s)", cf->file_path);
