0f2ad55e9e78444104d782e1eb64411e210e24f6
ratchov <ratchov@openbsd.org>
2010-04-24 14:33:46 +0000

    if there are no files to play in legacy mode, print the usage message

--- usr.bin/aucat/aucat.c
+++ usr.bin/aucat/aucat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aucat.c,v 1.87 2010/04/24 06:18:23 ratchov Exp $	*/
+/*	$OpenBSD: aucat.c,v 1.88 2010/04/24 14:33:46 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -547,21 +547,22 @@ aucat_main(int argc, char **argv)
 		dopar = ipar;
 		dipar = opar;
 	}
-	if (!l_flag && SLIST_EMPTY(&ifiles) &&
-	    SLIST_EMPTY(&ofiles) && argc > 0) {
-		/*
-		 * Legacy mode: if no -i or -o options are provided, and
-		 * there are arguments then assume the arguments are files
-		 * to play.
-		 */
-		for (c = 0; c < argc; c++)
-			if (legacy_play(devpath, argv[c]) != 0) {
-				errx(1, "%s: could not play\n", argv[c]);
-			}
-		exit(0);
-	} else if (argc > 0) {
-		aucat_usage();
-		exit(1);
+	if (!l_flag && SLIST_EMPTY(&ifiles) && SLIST_EMPTY(&ofiles)) {
+		if (argc > 0) {
+			/*
+			 * Legacy mode: if no -i or -o options are provided, and
+			 * there are arguments then assume the arguments are files
+			 * to play.
+			 */
+			for (c = 0; c < argc; c++)
+				if (legacy_play(devpath, argv[c]) != 0) {
+					errx(1, "%s: could not play\n", argv[c]);
+				}
+			exit(0);
+		} else {
+			aucat_usage();
+			exit(1);
+		}
 	}
 
 	if (!l_flag && (!SLIST_EMPTY(&sfiles) || unit >= 0))
