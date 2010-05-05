0eb761ddf8014aba46c4edb3968babf6e74958d7
kettenis <kettenis@openbsd.org>
2010-05-05 12:01:14 +0000

    Tweak inline asm to prevent gcc4 from optimizing away crucial bits of it.
    ok miod@

--- lib/libm/arch/hppa/s_trunc.c
+++ lib/libm/arch/hppa/s_trunc.c
@@ -11,13 +11,13 @@ trunc(double x)
 {
 	u_int64_t ofpsr, fpsr;
 
-	__asm__ __volatile__("fstds %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
+	__asm__ __volatile__("fstds %%fr0,0(%0)" :: "r" (&ofpsr) : "memory");
 	fpsr = (ofpsr & ~((u_int64_t)FP_RM << (9 + 32))) |
 	    ((u_int64_t)FP_RZ << (9 + 32));
-	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&fpsr));
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&fpsr) : "memory");
 
 	__asm__ __volatile__("frnd,dbl %0,%0" : "+f" (x));
 
-	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&ofpsr));
+	__asm__ __volatile__("fldds 0(%0), %%fr0" :: "r" (&ofpsr) : "memory");
 	return (x);
 }
