3a2f028aae207a2e105e9701322a9dc580ea6a33
jsg <jsg@openbsd.org>
2010-05-16 12:49:00 +0000

    Don't include -ftree-vrp in -O2 and higher as it has known bugs in gcc 4.2. Fixes jdk builds among other things.
    ok robert@ espie@

--- gnu/gcc/gcc/opts.c
+++ gnu/gcc/gcc/opts.c
@@ -505,7 +505,6 @@ decode_options (unsigned int argc, const char **argv)
       flag_reorder_functions = 1;
       flag_tree_store_ccp = 1;
       flag_tree_store_copy_prop = 1;
-      flag_tree_vrp = 1;
 
       if (!optimize_size)
 	{
