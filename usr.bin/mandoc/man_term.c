4c3277263eaa5160ffda688f5b0ea140155f9643
schwarze <schwarze@openbsd.org>
2010-05-24 01:36:22 +0000

    sync to bsd.lv: modern groff produces three blank lines before the man(7) footer; from Joerg Sonnenberger.

--- usr.bin/mandoc/man_term.c
+++ usr.bin/mandoc/man_term.c
@@ -1,4 +1,4 @@
-/*	$Id: man_term.c,v 1.37 2010/05/23 22:45:00 schwarze Exp $ */
+/*	$Id: man_term.c,v 1.38 2010/05/24 01:36:22 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -868,6 +868,8 @@ print_man_foot(struct termp *p, const struct man_meta *meta)
 
 	time2a(meta->date, buf, DATESIZ);
 
+	term_vspace(p);
+	term_vspace(p);
 	term_vspace(p);
 
 	p->flags |= TERMP_NOSPACE | TERMP_NOBREAK;
