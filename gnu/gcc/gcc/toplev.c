02ddaac851b0e7bd777fc8342f4e291092301e0a
kettenis <kettenis@openbsd.org>
2010-05-30 22:04:10 +0000

    Only enable -fstack-protector by default on targets that support it.
    ok marco@

--- gnu/gcc/gcc/toplev.c
+++ gnu/gcc/gcc/toplev.c
@@ -1833,6 +1833,8 @@ process_options (void)
 
   /* Targets must be able to place spill slots at lower addresses.  If the
      target already uses a soft frame pointer, the transition is trivial.  */
+  if (flag_stack_protect == -1)
+    flag_stack_protect = FRAME_GROWS_DOWNWARD ? 1 : 0;
   if (!FRAME_GROWS_DOWNWARD && flag_stack_protect)
     {
       warning (0, "-fstack-protector not supported for this target");
