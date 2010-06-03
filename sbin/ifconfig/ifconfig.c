7692a1fdf4f44a2739870b35e4672381116712ec
stsp <stsp@openbsd.org>
2010-06-03 21:57:12 +0000

    Stop requiring the 'inet6' keyword when the 'autoconfprivacy' option is used. Simplifies enabling autoconf privacy from hostname.if files. A line such as 'rtsol autoconfprivacy' will now work, as documented in ifconfig(8). Pointed out by steven@. ok deraadt@ steven@ todd@

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.233 2010/05/28 12:20:06 claudio Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.234 2010/06/03 21:57:12 stsp Exp $	*/
 /*	$NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $	*/
 
 /*
@@ -1197,12 +1197,6 @@ void
 setifxflags(const char *vname, int value)
 {
 	struct ifreq my_ifr;
-
-	if ((value == IFXF_INET6_PRIVACY || value == -IFXF_INET6_PRIVACY)
-	    && afp->af_af != AF_INET6) {
-		errx(1, "autoconfprivacy needs AF inet6, current AF is `%s'",
-		    afp->af_name);
-	}
 
 	bcopy((char *)&ifr, (char *)&my_ifr, sizeof(struct ifreq));
 
