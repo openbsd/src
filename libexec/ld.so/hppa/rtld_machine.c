5d04f0bf5dd33dab5ec9e985ae22d77df76764b3
miod <miod@openbsd.org>
2010-06-05 21:40:58 +0000

    Argh, revert previous change, for older binutils do not grok the fic() change.

--- libexec/ld.so/hppa/rtld_machine.c
+++ libexec/ld.so/hppa/rtld_machine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtld_machine.c,v 1.21 2010/06/05 17:04:44 miod Exp $	*/
+/*	$OpenBSD: rtld_machine.c,v 1.22 2010/06/05 21:40:58 miod Exp $	*/
 
 /*
  * Copyright (c) 2004 Michael Shalayeff
@@ -368,8 +368,13 @@ _dl_md_reloc_got(elf_object_t *object, int lazy)
 		__asm __volatile("fdc 0(%0)" :: "r" (&got[-7]));
 		__asm __volatile("fdc 0(%0)" :: "r" (&got[-6]));
 		__asm __volatile("sync");
+#if 0
 		__asm __volatile("fic 0(%%sr0,%0)" :: "r" (&got[-7]));
 		__asm __volatile("fic 0(%%sr0,%0)" :: "r" (&got[-6]));
+#else
+		__asm __volatile("fic 0(%0)" :: "r" (&got[-7]));
+		__asm __volatile("fic 0(%0)" :: "r" (&got[-6]));
+#endif
 		__asm __volatile("sync");
 
 		/*
