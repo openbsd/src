4091c10409e44c56858110d65cafe64b1fc26e64
bluhm <bluhm@openbsd.org>
2010-06-12 09:48:39 +0000

    Print additional new line after 'Number of Links' in show database router.  This separates the router and link blocks like ospfctl. ok claudio@

--- usr.sbin/ospf6ctl/ospf6ctl.c
+++ usr.sbin/ospf6ctl/ospf6ctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospf6ctl.c,v 1.34 2010/02/25 16:40:23 claudio Exp $ */
+/*	$OpenBSD: ospf6ctl.c,v 1.35 2010/06/12 09:48:39 bluhm Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -851,7 +851,7 @@ show_db_msg_detail(struct imsg *imsg)
 
 		nlinks = (ntohs(lsa->hdr.len) - sizeof(struct lsa_hdr)
 		    - sizeof(u_int32_t)) / sizeof(struct lsa_rtr_link);
-		printf("Number of Links: %d\n", nlinks);
+		printf("Number of Links: %d\n\n", nlinks);
 
 		off = sizeof(lsa->hdr) + sizeof(struct lsa_rtr);
 
