5f0cba21cd72dd86ab7c5ff7ae57ee4b6592f2a6
damien <damien@openbsd.org>
2010-06-14 17:51:43 +0000

    fix my previous commit, i forgot BIP, so instead of enumerating what is not supported as a group cipher, enumerate what is supported.

--- sys/net80211/ieee80211_node.c
+++ sys/net80211/ieee80211_node.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieee80211_node.c,v 1.58 2010/06/14 17:39:20 damien Exp $	*/
+/*	$OpenBSD: ieee80211_node.c,v 1.59 2010/06/14 17:51:43 damien Exp $	*/
 /*	$NetBSD: ieee80211_node.c,v 1.14 2004/05/09 09:18:47 dyoung Exp $	*/
 
 /*-
@@ -439,8 +439,10 @@ ieee80211_match_bss(struct ieee80211com *ic, struct ieee80211_node *ni)
 			if (!(ic->ic_flags & IEEE80211_F_PSK))
 				fail |= 0x40;
 		}
-		if (ni->ni_rsngroupcipher == IEEE80211_CIPHER_NONE ||
-		    ni->ni_rsngroupcipher == IEEE80211_CIPHER_USEGROUP)
+		if (ni->ni_rsngroupcipher != IEEE80211_CIPHER_WEP40 &&
+		    ni->ni_rsngroupcipher != IEEE80211_CIPHER_TKIP &&
+		    ni->ni_rsngroupcipher != IEEE80211_CIPHER_CCMP &&
+		    ni->ni_rsngroupcipher != IEEE80211_CIPHER_WEP104)
 			fail |= 0x40;
 		if ((ni->ni_rsnciphers & ic->ic_rsnciphers) == 0)
 			fail |= 0x40;
