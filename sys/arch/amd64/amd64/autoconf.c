42549d2445a851bae58ac510aaf7b8ec4c1c2d1a
deraadt <deraadt@openbsd.org>
2010-06-28 22:20:12 +0000

    Ah, this will not help the netboot case

--- sys/arch/amd64/amd64/autoconf.c
+++ sys/arch/amd64/amd64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.32 2010/06/27 19:21:20 deraadt Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.33 2010/06/28 22:20:12 deraadt Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.1 2003/04/26 18:39:26 fvdl Exp $	*/
 
 /*-
@@ -194,11 +194,13 @@ diskconf(void)
 				break;
 		}
 		if (ifp) {
+#if defined(NFSCLIENT)
 			printf("PXE boot MAC address %s, interface %s\n",
 			    ether_sprintf(bios_bootmac->mac), ifp->if_xname);
 			bootdv = parsedisk(ifp->if_xname, strlen(ifp->if_xname),
 			    0, &tmpdev);
 			part = 0;
+#endif
 		} else
 			printf("PXE boot MAC address %s, interface %s\n",
 			    ether_sprintf(bios_bootmac->mac), "unknown");
--- sys/arch/i386/i386/autoconf.c
+++ sys/arch/i386/i386/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.83 2010/06/27 19:21:19 deraadt Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.84 2010/06/28 22:20:12 deraadt Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.20 1996/05/03 19:41:56 christos Exp $	*/
 
 /*-
@@ -205,11 +205,13 @@ diskconf(void)
 				break;
 		}
 		if (ifp) {
+#if defined(NFSCLIENT)
 			printf("PXE boot MAC address %s, interface %s\n",
 			    ether_sprintf(bios_bootmac->mac), ifp->if_xname);
 			bootdv = parsedisk(ifp->if_xname, strlen(ifp->if_xname),
 			    0, &tmpdev);
 			part = 0;
+#endif
 		} else
 			printf("PXE boot MAC address %s, interface %s\n",
 			    ether_sprintf(bios_bootmac->mac), "unknown");
