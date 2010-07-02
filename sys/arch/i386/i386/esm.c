b47e89aac2e18105fc36e8bd9ba79be5d0ab72ba
tedu <tedu@openbsd.org>
2010-07-02 01:35:13 +0000

    initialize values to something sensible. ok deraadt pirofti

--- sys/arch/i386/i386/esm.c
+++ sys/arch/i386/i386/esm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: esm.c,v 1.51 2010/06/30 15:22:05 blambert Exp $ */
+/*	$OpenBSD: esm.c,v 1.52 2010/07/02 01:35:13 tedu Exp $ */
 
 /*
  * Copyright (c) 2005 Jordan Hargrave <jordan@openbsd.org>
@@ -685,9 +685,9 @@ struct esm_sensor_map esm_sensors_powerunit[] = {
 void
 esm_devmap(struct esm_softc *sc, struct esm_devmap *devmap)
 {
-	struct esm_sensor_map	*sensor_map;
+	struct esm_sensor_map	*sensor_map = NULL;
 	const char		*name = NULL, *fname = NULL;
-	int			mapsize;
+	int			mapsize = 0;
 
 	switch (devmap->dev_major) {
 	case ESM2_DEV_ESM2:
