263e6d5c900d2be217b0c14d92971fe5b166bb06
marco <marco@openbsd.org>
2010-07-06 16:16:48 +0000

    Enable FIFO IO for sd devices.  This time committed against the actual tree.
    Tested by lots of people during c2k10
    
    ok phessler krw thib

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sd.c,v 1.202 2010/07/03 01:40:12 kettenis Exp $	*/
+/*	$OpenBSD: sd.c,v 1.203 2010/07/06 16:16:48 marco Exp $	*/
 /*	$NetBSD: sd.c,v 1.111 1997/04/02 02:29:41 mycroft Exp $	*/
 
 /*-
@@ -175,7 +175,11 @@ sdattach(struct device *parent, struct device *self, void *aux)
 	 */
 	sc->sc_dk.dk_driver = &sddkdriver;
 	sc->sc_dk.dk_name = sc->sc_dev.dv_xname;
-	sc->sc_bufq = bufq_init(BUFQ_DEFAULT);
+
+	if (SCSISPC(sc_link->inqdata.version) >= 2)
+		sc->sc_bufq = bufq_init(BUFQ_FIFO);
+	else
+		sc->sc_bufq = bufq_init(BUFQ_DEFAULT);
 
 	if ((sc_link->flags & SDEV_ATAPI) && (sc_link->flags & SDEV_REMOVABLE))
 		sc_link->quirks |= SDEV_NOSYNCCACHE;
