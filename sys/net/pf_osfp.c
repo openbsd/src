fc6dd41fe36a572041d5211519d191c1165a8e82
henning <henning@openbsd.org>
2010-07-13 12:56:06 +0000

    -                       find.fp_wsize *= find.fp_mss, 1; +                       find.fp_wsize *= find.fp_mss; From: Ed Schouten <ed at FreeBSD.org>

--- sys/net/pf_osfp.c
+++ sys/net/pf_osfp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_osfp.c,v 1.17 2010/04/09 20:58:06 oga Exp $ */
+/*	$OpenBSD: pf_osfp.c,v 1.18 2010/07/13 12:56:06 henning Exp $ */
 
 /*
  * Copyright (c) 2003 Mike Frantzen <frantzen@w4g.org>
@@ -560,7 +560,7 @@ pf_osfp_validate(void)
 		if (find.fp_mss == 0)
 			find.fp_mss = 128;
 		if (f->fp_flags & PF_OSFP_WSIZE_MSS)
-			find.fp_wsize *= find.fp_mss, 1;
+			find.fp_wsize *= find.fp_mss;
 		else if (f->fp_flags & PF_OSFP_WSIZE_MTU)
 			find.fp_wsize *= (find.fp_mss + 40);
 		else if (f->fp_flags & PF_OSFP_WSIZE_MOD)
