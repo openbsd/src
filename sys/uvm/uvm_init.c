7cfce8f0cbe53991ecd18ebda24c151fe4f40996
deraadt <deraadt@openbsd.org>
2010-07-13 16:49:16 +0000

    need pool.h to initialize the dma allocator

--- sys/uvm/uvm_init.c
+++ sys/uvm/uvm_init.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_init.c,v 1.26 2010/07/13 16:47:03 deraadt Exp $	*/
+/*	$OpenBSD: uvm_init.c,v 1.27 2010/07/13 16:49:16 deraadt Exp $	*/
 /*	$NetBSD: uvm_init.c,v 1.14 2000/06/27 17:29:23 mrg Exp $	*/
 
 /*
@@ -48,6 +48,7 @@
 #include <sys/proc.h>
 #include <sys/malloc.h>
 #include <sys/vnode.h>
+#include <sys/pool.h>
 
 #include <uvm/uvm.h>
 
