999b1f879279b5fa528946a0b972cfee14da5bb9
deraadt <deraadt@openbsd.org>
2010-07-22 14:26:38 +0000

    Don't do anything on suspend/resume for now (there is a diff brewing, but it has some problems still..)

--- sys/dev/pckbc/pckbd.c
+++ sys/dev/pckbc/pckbd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pckbd.c,v 1.26 2010/07/21 20:10:17 miod Exp $ */
+/* $OpenBSD: pckbd.c,v 1.27 2010/07/22 14:26:38 deraadt Exp $ */
 /* $NetBSD: pckbd.c,v 1.24 2000/06/05 22:20:57 sommerfeld Exp $ */
 
 /*-
@@ -123,14 +123,13 @@ static int pckbd_is_console(pckbc_tag_t, pckbc_slot_t);
 
 int pckbdprobe(struct device *, void *, void *);
 void pckbdattach(struct device *, struct device *, void *);
-int pckbd_activate(struct device *, int);
 
 struct cfattach pckbd_ca = {
 	sizeof(struct pckbd_softc), 
 	pckbdprobe, 
 	pckbdattach, 
 	NULL, 
-	pckbd_activate
+	NULL
 };
 
 int	pckbd_enable(void *, int);
@@ -416,20 +415,6 @@ pckbdattach(parent, self, aux)
 	 * Attach the wskbd, saving a handle to it.
 	 */
 	sc->sc_wskbddev = config_found(self, &a, wskbddevprint);
-}
-
-int
-pckbd_activate(struct device *self, int act)
-{
-	switch (act) {
-	case DVACT_SUSPEND:
-		pckbd_enable(self, 0);
-		break;
-	case DVACT_RESUME:
-		pckbd_enable(self, 1);
-		break;
-	}
-	return (0);
 }
 
 int
