61cec9357e4fbd3322dacdc4b73ec59d3ccf9bf4
mk <mk@openbsd.org>
2010-07-31 11:51:45 +0000

    The code referencing urndis_watchdog() is #if 0'd out, but the actual function is not.
    This change #if 0's urndis_watchdog() and some functions that are only
    called from here.
    
    At some point we probably want to use the watchdog functionality but the
    current code is completely untested so disable it entirely rather than
    enabling it this close to release.

--- sys/dev/usb/if_urndis.c
+++ sys/dev/usb/if_urndis.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_urndis.c,v 1.24 2010/07/31 10:59:37 mk Exp $ */
+/*	$OpenBSD: if_urndis.c,v 1.25 2010/07/31 11:51:45 mk Exp $ */
 
 /*
  * Copyright (c) 2010 Jonathan Armani <armani@openbsd.org>
@@ -72,7 +72,9 @@
 int urndis_newbuf(struct urndis_softc *, struct urndis_chain *);
 
 int urndis_ioctl(struct ifnet *, u_long, caddr_t);
+#if 0
 void urndis_watchdog(struct ifnet *);
+#endif
 
 void urndis_start(struct ifnet *);
 void urndis_rxeof(usbd_xfer_handle, usbd_private_handle, usbd_status);
@@ -104,8 +106,10 @@ u_int32_t urndis_ctrl_query(struct urndis_softc *, u_int32_t, void *, size_t,
 u_int32_t urndis_ctrl_set(struct urndis_softc *, u_int32_t, void *, size_t);
 u_int32_t urndis_ctrl_set_param(struct urndis_softc *, const char *, u_int32_t,
     void *, size_t);
+#if 0
 u_int32_t urndis_ctrl_reset(struct urndis_softc *);
 u_int32_t urndis_ctrl_keepalive(struct urndis_softc *);
+#endif
 
 int urndis_encap(struct urndis_softc *, struct mbuf *, int);
 void urndis_decap(struct urndis_softc *, struct urndis_chain *, u_int32_t);
@@ -645,6 +649,7 @@ urndis_ctrl_set_param(struct urndis_softc *sc,
 	return rval;
 }
 
+#if 0
 /* XXX : adrreset, get it from response */
 u_int32_t
 urndis_ctrl_reset(struct urndis_softc *sc)
@@ -729,6 +734,7 @@ urndis_ctrl_keepalive(struct urndis_softc *sc)
 
 	return rval;
 }
+#endif
 
 int
 urndis_encap(struct urndis_softc *sc, struct mbuf *m, int idx)
@@ -1025,6 +1031,7 @@ urndis_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 	return (error);
 }
 
+#if 0
 void
 urndis_watchdog(struct ifnet *ifp)
 {
@@ -1040,6 +1047,7 @@ urndis_watchdog(struct ifnet *ifp)
 
 	urndis_ctrl_keepalive(sc);
 }
+#endif
 
 void
 urndis_init(struct urndis_softc *sc)
