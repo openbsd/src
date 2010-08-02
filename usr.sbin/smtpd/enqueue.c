ff7ba44c49eac5c086a012da3eb4140c4898e3da
jacekm <jacekm@openbsd.org>
2010-08-02 11:49:02 +0000

    Ignore SIGPIPE, the delivery of which prevented /usr/sbin/sendmail from relaying the server diagnostic back to the user (eg. "500 Line too long").

--- usr.sbin/smtpd/enqueue.c
+++ usr.sbin/smtpd/enqueue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: enqueue.c,v 1.37 2010/06/02 19:16:53 chl Exp $	*/
+/*	$OpenBSD: enqueue.c,v 1.38 2010/08/02 11:49:02 jacekm Exp $	*/
 
 /*
  * Copyright (c) 2005 Henning Brauer <henning@bulabula.org>
@@ -188,6 +188,7 @@ enqueue(int argc, char *argv[])
 	}
 
 	signal(SIGALRM, sighdlr);
+	signal(SIGPIPE, SIG_IGN);
 	alarm(300);
 
 	fp = tmpfile();
