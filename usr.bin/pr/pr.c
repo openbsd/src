8a4376a09c120a27a3c69cb721d711a184ced0c8
chl <chl@openbsd.org>
2010-08-25 18:20:11 +0000

    fix potential use of uninitialized variable.
    reported by gcc.
    
    ok jasper@

--- usr.bin/pr/pr.c
+++ usr.bin/pr/pr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pr.c,v 1.29 2010/08/17 21:37:11 jasper Exp $	*/
+/*	$OpenBSD: pr.c,v 1.30 2010/08/25 18:20:11 chl Exp $	*/
 
 /*-
  * Copyright (c) 1991 Keith Muller.
@@ -186,7 +186,7 @@ onecol(int argc, char *argv[])
     char *nbuf;
     char *hbuf = NULL;
     char *ohbuf;
-    FILE *inf;
+    FILE *inf = NULL;
     char *fname;
     int mor;
     int error = 1;
