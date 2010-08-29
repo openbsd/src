e52a13e522da8a7d32440b33ac3248b992a58027
gollo <gollo@openbsd.org>
2010-08-29 09:24:38 +0000

    Fix kernel compiling with disabled IPSEC and enabled GIF/MPLS
    OK: claudio@, bob@, sthen@, thib@

--- sys/netinet/in_proto.c
+++ sys/netinet/in_proto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_proto.c,v 1.53 2010/05/11 09:36:07 claudio Exp $	*/
+/*	$OpenBSD: in_proto.c,v 1.54 2010/08/29 09:24:38 gollo Exp $	*/
 /*	$NetBSD: in_proto.c,v 1.14 1996/02/18 18:58:32 christos Exp $	*/
 
 /*
@@ -149,9 +149,9 @@
 
 #ifdef IPSEC
 #include <netinet/ip_ipsp.h>
-#include <netinet/ip_ether.h>
 #endif
 
+#include <netinet/ip_ether.h>
 #include <netinet/ip_ipip.h>
 
 #include "gre.h"
