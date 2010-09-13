49e2ffbf59cd5e2503ecbae3611df27353bb4cf5
schwarze <schwarze@openbsd.org>
2010-09-13 20:10:11 +0000

    Prevent .Pp and .Lp from looping endlessly on punctuation. Fix for .Pp from kristaps@, i applied the same to .Lp. Bug reported by espie@.

--- usr.bin/mandoc/mdoc_argv.c
+++ usr.bin/mandoc/mdoc_argv.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_argv.c,v 1.33 2010/07/25 18:05:54 schwarze Exp $ */
+/*	$Id: mdoc_argv.c,v 1.34 2010/09/13 20:10:11 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -91,7 +91,7 @@ static	int mdoc_argflags[MDOC_MAX] = {
 	0, /* Os */
 	0, /* Sh */
 	0, /* Ss */ 
-	ARGS_DELIM, /* Pp */ 
+	0, /* Pp */ 
 	ARGS_DELIM, /* D1 */
 	ARGS_DELIM, /* Dl */
 	0, /* Bd */
@@ -192,7 +192,7 @@ static	int mdoc_argflags[MDOC_MAX] = {
 	0, /* Fr */
 	0, /* Ud */
 	0, /* Lb */
-	ARGS_DELIM, /* Lp */
+	0, /* Lp */
 	ARGS_DELIM, /* Lk */
 	ARGS_DELIM, /* Mt */
 	ARGS_DELIM, /* Brq */
