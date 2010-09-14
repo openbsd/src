8ff9afc85e2ec32e7c9863b237c7b222adc59e3c
dlg <dlg@openbsd.org>
2010-09-14 04:02:43 +0000

    scsi_xsh_del the xs handler on DEACTIVATE.
    same change as sd.c r1.212. matthew@ pointed out that cd(4) and
    st(4) would need the same change.

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cd.c,v 1.191 2010/09/12 01:54:48 krw Exp $	*/
+/*	$OpenBSD: cd.c,v 1.192 2010/09/14 04:02:43 dlg Exp $	*/
 /*	$NetBSD: cd.c,v 1.100 1997/04/02 02:29:30 mycroft Exp $	*/
 
 /*
@@ -255,6 +255,7 @@ cdactivate(struct device *self, int act)
 	case DVACT_DEACTIVATE:
 		sc->sc_flags |= CDF_DYING;
 		bufq_drain(&sc->sc_bufq);
+		scsi_xsh_del(&sc->sc_xsh);
 		break;
 	}
 	return (rv);
--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: st.c,v 1.110 2010/09/14 01:43:38 krw Exp $	*/
+/*	$OpenBSD: st.c,v 1.111 2010/09/14 04:02:43 dlg Exp $	*/
 /*	$NetBSD: st.c,v 1.71 1997/02/21 23:03:49 thorpej Exp $	*/
 
 /*
@@ -358,6 +358,7 @@ stactivate(struct device *self, int act)
 	case DVACT_DEACTIVATE:
 		st->flags |= ST_DYING;
 		bufq_drain(&st->sc_bufq);
+		scsi_xsh_del(&st->sc_xsh);
 		break;
 	}
 
