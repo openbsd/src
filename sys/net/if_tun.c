191feee13dd5b33d2da884c7ebf618da98f6de39
matthew <matthew@openbsd.org>
2010-09-22 05:44:27 +0000

    Don't bother calling suser() in tunopen().
    "Sure" deraadt@

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tun.c,v 1.107 2010/07/08 08:40:29 yasuoka Exp $	*/
+/*	$OpenBSD: if_tun.c,v 1.108 2010/09/22 05:44:27 matthew Exp $	*/
 /*	$NetBSD: if_tun.c,v 1.24 1996/05/07 02:40:48 thorpej Exp $	*/
 
 /*
@@ -342,9 +342,6 @@ tunopen(dev_t dev, int flag, int mode, struct proc *p)
 	struct tun_softc	*tp;
 	struct ifnet		*ifp;
 	int			 error, s;
-
-	if ((error = suser(p, 0)) != 0)
-		return (error);
 
 	if ((tp = tun_lookup(minor(dev))) == NULL) {	/* create on demand */
 		char	xname[IFNAMSIZ];
