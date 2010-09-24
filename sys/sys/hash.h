b0c646fbdd1d131ff6f17ac88f4415a6eddabf92
claudio <claudio@openbsd.org>
2010-09-24 13:24:53 +0000

    Make the endpointer also const so that there is no warning from gcc4 about discarding a qualifier. OK deraadt@ krw@

--- sys/sys/hash.h
+++ sys/sys/hash.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hash.h,v 1.4 2004/05/25 18:37:23 jmc Exp $	*/
+/*	$OpenBSD: hash.h,v 1.5 2010/09/24 13:24:53 claudio Exp $	*/
 
 /*
  * Copyright (c) 2001 Tobias Weingartner
@@ -93,7 +93,7 @@ hash32_strn(const void *buf, size_t len, uint32_t hash)
  * namei() hashing of path name parts.
  */
 static __inline uint32_t
-hash32_stre(const void *buf, int end, char **ep, uint32_t hash)
+hash32_stre(const void *buf, int end, const char **ep, uint32_t hash)
 {
 	const unsigned char *p = buf;
 
@@ -101,7 +101,7 @@ hash32_stre(const void *buf, int end, char **ep, uint32_t hash)
 		hash = HASHSTEP(hash, *p++);
 
 	if (ep)
-		*ep = (char *)p;
+		*ep = p;
 
 	return hash;
 }
@@ -112,7 +112,8 @@ hash32_stre(const void *buf, int end, char **ep, uint32_t hash)
  * as a helper for the namei() hashing of path name parts.
  */
 static __inline uint32_t
-hash32_strne(const void *buf, size_t len, int end, char **ep, uint32_t hash)
+hash32_strne(const void *buf, size_t len, int end, const char **ep,
+    uint32_t hash)
 {
 	const unsigned char *p = buf;
 
@@ -120,7 +121,7 @@ hash32_strne(const void *buf, size_t len, int end, char **ep, uint32_t hash)
 		hash = HASHSTEP(hash, *p++);
 
 	if (ep)
-		*ep = (char *)p;
+		*ep = p;
 
 	return hash;
 }
