2197a9870892b855c85d31517412fe7ad8d0e180
claudio <claudio@openbsd.org>
2010-09-30 08:57:37 +0000

    Only 6 elements of mib are used so do allocate 6 not 7.

--- usr.sbin/bgpd/kroute.c
+++ usr.sbin/bgpd/kroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kroute.c,v 1.184 2010/09/02 14:03:21 sobrado Exp $ */
+/*	$OpenBSD: kroute.c,v 1.185 2010/09/30 08:57:37 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -421,7 +421,7 @@ ktable_exists(u_int rtableid, u_int *rdomid)
 {
 	size_t			 len;
 	struct rt_tableinfo	 info;
-	int			 mib[7];
+	int			 mib[6];
 
 	mib[0] = CTL_NET;
 	mib[1] = AF_ROUTE;
