0c2225f1e2d175d737e65742c79d70653b333273
espie <espie@openbsd.org>
2010-10-19 07:29:41 +0000

    before running groff, create dir when needed. fixes tcl and the few ports with non-standard man locations that can't rely on mtree fake.
    as noticed by brad@

--- usr.sbin/pkg_add/OpenBSD/PackingElement.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingElement.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackingElement.pm,v 1.183 2010/08/03 14:07:27 espie Exp $
+# $OpenBSD: PackingElement.pm,v 1.184 2010/10/19 07:29:41 espie Exp $
 #
 # Copyright (c) 2003-2010 Marc Espie <espie@openbsd.org>
 #
@@ -478,6 +478,7 @@ sub keyword() { "shell" }
 __PACKAGE__->register_with_factory;
 
 package OpenBSD::PackingElement::Manpage;
+use File::Basename;
 our @ISA=qw(OpenBSD::PackingElement::FileBase);
 
 sub keyword() { "man" }
@@ -528,6 +529,10 @@ sub format
 		}
 	}
 	open my $oldout, '>&STDOUT';
+	my $dir = dirname("$base/$out");
+	unless (-d $dir) {
+		mkdir($dir);
+	}
 	open STDOUT, '>', "$base/$out" or die "Can't write to $base/$out";
 	system(OpenBSD::Paths->groff,
 	    '-Tascii', '-mandoc', '-Wall', '-mtty-char', @extra, '--', $fname);
