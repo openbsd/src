f29863e364b1321301b8b644100c169acbb1d3ff
espie <espie@openbsd.org>
2010-12-13 20:51:33 +0000

    rename() is the atomic operation, unlink() is just there to create races between ldconfig and ld.so
    (simple commit, hours of bug hunt...)
    
    okay kettenis@, deraad@, miod@

--- libexec/ld.so/ldconfig/ldconfig.c
+++ libexec/ld.so/ldconfig/ldconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldconfig.c,v 1.27 2010/03/30 17:42:50 zinovik Exp $	*/
+/*	$OpenBSD: ldconfig.c,v 1.28 2010/12/13 20:51:33 espie Exp $	*/
 
 /*
  * Copyright (c) 1993,1995 Paul Kranenburg
@@ -430,12 +430,6 @@ buildhints(void)
 		goto out;
 	}
 	if (close(fd) != 0) {
-		warn("%s", _PATH_LD_HINTS);
-		goto out;
-	}
-
-	/* Install it */
-	if (unlink(_PATH_LD_HINTS) != 0 && errno != ENOENT) {
 		warn("%s", _PATH_LD_HINTS);
 		goto out;
 	}
