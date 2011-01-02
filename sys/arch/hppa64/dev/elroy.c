059714db596559b410da66b6767c1569c1ebb96e
jasper <jasper@openbsd.org>
2011-01-02 13:36:09 +0000

    Only store bus/dev/func numbers when the corresponding pointer argument isn't NULL.
    Original to hppa's elroy(4) by, and ok kettenis@

--- sys/arch/hppa64/dev/elroy.c
+++ sys/arch/hppa64/dev/elroy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: elroy.c,v 1.10 2010/12/04 17:06:31 miod Exp $	*/
+/*	$OpenBSD: elroy.c,v 1.11 2011/01/02 13:36:09 jasper Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -233,9 +233,12 @@ elroy_make_tag(void *v, int bus, int dev, int func)
 void
 elroy_decompose_tag(void *v, pcitag_t tag, int *bus, int *dev, int *func)
 {
-	*bus = (tag >> 16) & 0xff;
-	*dev = (tag >> 11) & 0x1f;
-	*func= (tag >>  8) & 0x07;
+	if (bus)
+		*bus = (tag >> 16) & 0xff;
+	if (dev)
+		*dev = (tag >> 11) & 0x1f;
+	if (func)
+		*func= (tag >>  8) & 0x07;
 }
 
 int
