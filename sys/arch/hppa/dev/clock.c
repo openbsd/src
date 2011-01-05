b024cdb98bc13c912c498e63d349ac6be2073384
jasper <jasper@openbsd.org>
2011-01-05 16:30:50 +0000

    - remove unused headers wrapped in #if defined(DDB)
    ok jsing@ kettenis@

--- sys/arch/hppa/dev/clock.c
+++ sys/arch/hppa/dev/clock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock.c,v 1.26 2011/01/01 19:00:56 jasper Exp $	*/
+/*	$OpenBSD: clock.c,v 1.27 2011/01/05 16:30:50 jasper Exp $	*/
 
 /*
  * Copyright (c) 1998-2003 Michael Shalayeff
@@ -40,13 +40,6 @@
 #include <machine/reg.h>
 #include <machine/cpufunc.h>
 #include <machine/autoconf.h>
-
-#if defined(DDB)
-#include <uvm/uvm_extern.h>
-#include <machine/db_machdep.h>
-#include <ddb/db_sym.h>
-#include <ddb/db_extern.h>
-#endif
 
 u_long	cpu_hzticks;
 
