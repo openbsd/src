a4cc8db039942c88908ac8b22825b763cd57987e
deraadt <deraadt@openbsd.org>
2011-01-07 06:17:33 +0000

    do not rely on rndvar.h leaking shit into the namespace

--- regress/sys/kern/kqueue/kqueue-random.c
+++ regress/sys/kern/kqueue/kqueue-random.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kqueue-random.c,v 1.4 2003/07/31 21:48:08 deraadt Exp $	*/
+/*	$OpenBSD: kqueue-random.c,v 1.5 2011/01/07 06:17:33 deraadt Exp $	*/
 /*	Written by Michael Shalayeff, 2002, Public Domain	*/
 
 #include <stdlib.h>
@@ -21,7 +21,7 @@ do_random(void)
 	int n, fd, kq;
 	struct timespec ts;
 	struct kevent ev;
-	u_int32_t buf[POOLWORDS];
+	u_int32_t buf[BUFSIZ];
 
 	if ((fd = open("/dev/srandom", O_RDONLY)) < 0) {
 		warn("open: /dev/srandom");
