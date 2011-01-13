73746b6cb0ebeb1cedd370221e13ff3cc6c70771
jsg <jsg@openbsd.org>
2011-01-13 14:29:26 +0000

    Add an entry for "Advanced Capabilities for Conventional PCI" to the list of capabilities, present in recent Intel chipsets.
    ok kettenis@

--- usr.sbin/pcidump/pcidump.c
+++ usr.sbin/pcidump/pcidump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcidump.c,v 1.28 2011/01/13 14:17:21 jsg Exp $	*/
+/*	$OpenBSD: pcidump.c,v 1.29 2011/01/13 14:29:26 jsg Exp $	*/
 
 /*
  * Copyright (c) 2006, 2007 David Gwynne <loki@animata.net>
@@ -91,7 +91,8 @@ const char *pci_capnames[] = {
 	"Secure",
 	"PCI Express",
 	"Extended Message Signaled Interrupts (MSI-X)",
-	"SATA"
+	"SATA",
+	"PCI Advanced Features"
 };
 
 int
