6428b005451c18f0cda22ed604c6c3c636f54d36
espie <espie@openbsd.org>
2011-01-21 20:37:26 +0000

    forgot to pass this. bug-report from "Michael W. Lucas" <mwlucas@blackhelicopters.org>

--- usr.sbin/pkg_add/OpenBSD/Add.pm
+++ usr.sbin/pkg_add/OpenBSD/Add.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Add.pm,v 1.119 2010/12/29 13:03:05 espie Exp $
+# $OpenBSD: Add.pm,v 1.120 2011/01/21 20:37:26 espie Exp $
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -93,7 +93,7 @@ sub record_partial_installation
 			undef $last->{d};
 		}
 	}
-	register_installation($n);
+	register_installation($n, $state);
 	return $borked;
 }
 
