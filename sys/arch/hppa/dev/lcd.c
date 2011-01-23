2038bf0aea9fc9ea54f200beacceedcd2b6ae5b7
jsing <jsing@openbsd.org>
2011-01-23 09:39:15 +0000

    Spinning up secondary CPUs on an MP kernel results in PDC output on the system LCD. Move LCD initialisation to a mountroot hook in order to avoid PDC output after initialisation.
    Committing on behalf of kettenis@

--- sys/arch/hppa/dev/lcd.c
+++ sys/arch/hppa/dev/lcd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lcd.c,v 1.2 2007/07/20 22:13:45 kettenis Exp $	*/
+/*	$OpenBSD: lcd.c,v 1.3 2011/01/23 09:39:15 jsing Exp $	*/
 
 /*
  * Copyright (c) 2007 Mark Kettenis
@@ -56,6 +56,7 @@ struct cfdriver lcd_cd = {
 	NULL, "lcd", DV_DULL
 };
 
+void	lcd_mountroot(void *);
 void	lcd_write(struct lcd_softc *, const char *);
 void	lcd_blink(void *, int);
 void	lcd_blink_finish(void *);
@@ -99,18 +100,26 @@ lcd_attach(struct device *parent, struct device *self, void *aux)
 	for (i = 0; i < 3; i++)
 		sc->sc_heartbeat[i] = pdc_lcd->heartbeat[i];
 
+	timeout_set(&sc->sc_to, lcd_blink_finish, sc);
+
+	sc->sc_blink.bl_func = lcd_blink;
+	sc->sc_blink.bl_arg = sc;
+	blink_led_register(&sc->sc_blink);
+
+	mountroothook_establish(lcd_mountroot, sc);
+}
+
+void
+lcd_mountroot(void *arg)
+{
+	struct lcd_softc *sc = arg;
+
 	bus_space_write_1(sc->sc_iot, sc->sc_cmdh, 0, LCD_CLS);
 	delay(100 * sc->sc_delay);
 
 	bus_space_write_1(sc->sc_iot, sc->sc_cmdh, 0, LCD_LOCATE(0, 0));
 	delay(sc->sc_delay);
 	lcd_write(sc, "OpenBSD/" MACHINE);
-
-	timeout_set(&sc->sc_to, lcd_blink_finish, sc);
-
-	sc->sc_blink.bl_func = lcd_blink;
-	sc->sc_blink.bl_arg = sc;
-	blink_led_register(&sc->sc_blink);
 }
 
 void
