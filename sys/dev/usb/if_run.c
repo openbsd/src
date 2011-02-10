5ad57547d96f2a9debd2cee4b86a6c5bc0861b50
jakemsr <jakemsr@openbsd.org>
2011-02-10 17:26:40 +0000

    now that usb_wait_task() is back, bring back recently reverted changes:
    * use usb_ref_{incr,decr,wait}() to avoid detaching the driver while a
      process is still using the hardware.
    * don't add timeout(9)s if the device is detached.
    * add checks to see if the device has been detached before running
      ioctls, timeouts, and tasks.
    * use usb_wait_task() to wait for tasks to complete.
    
    ok damien@

--- sys/dev/usb/if_run.c
+++ sys/dev/usb/if_run.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_run.c,v 1.87 2011/02/09 04:25:32 jakemsr Exp $	*/
+/*	$OpenBSD: if_run.c,v 1.88 2011/02/10 17:26:40 jakemsr Exp $	*/
 
 /*-
  * Copyright (c) 2008-2010 Damien Bergamini <damien.bergamini@free.fr>
@@ -600,21 +600,7 @@ run_detach(struct device *self, int flags)
 		timeout_del(&sc->calib_to);
 
 	/* wait for all queued asynchronous commands to complete */
-#if 0
-	while (sc->cmdq.queued > 0)
-		tsleep(&sc->cmdq, 0, "cmdq", 0);
-#endif
-	/* the async commands are run in a task */
 	usb_rem_wait_task(sc->sc_udev, &sc->sc_task);
-
-	/* but the task might not have run if it did not start before
-	 * usbd_deactivate() was called, so wakeup now.  we're
-	 * detaching, no need to try to run more commands.
-	 */
-	if (sc->cmdq.queued > 0) {
-		sc->cmdq.queued = 0;
-		wakeup(&sc->cmdq);
-	}
 
 	usbd_ref_wait(sc->sc_udev);
 
@@ -1485,7 +1471,6 @@ run_task(void *arg)
 		ring->queued--;
 		ring->next = (ring->next + 1) % RUN_HOST_CMD_RING_COUNT;
 	}
-	wakeup(ring);
 	splx(s);
 }
 
@@ -3534,8 +3519,7 @@ run_stop(struct ifnet *ifp, int disable)
 	s = splusb();
 	ieee80211_new_state(ic, IEEE80211_S_INIT, -1);
 	/* wait for all queued asynchronous commands to complete */
-	while (sc->cmdq.queued > 0)
-		tsleep(&sc->cmdq, 0, "cmdq", 0);
+	usb_wait_task(sc->sc_udev, &sc->sc_task);
 	splx(s);
 
 	/* disable Tx/Rx */
