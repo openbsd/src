a96ea958bf6eb75e28ae56fadad4adacfaa66a8c
mikeb <mikeb@openbsd.org>
2011-02-16 10:37:45 +0000

    allow users to enable access to the /dev/crypto, but keep it turned off by default, adjust comments in sysctl.conf;  ok markus, tedu, djm, miod

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sysctl.c,v 1.196 2010/12/14 20:26:44 mikeb Exp $	*/
+/*	$OpenBSD: kern_sysctl.c,v 1.197 2011/02/16 10:37:45 mikeb Exp $	*/
 /*	$NetBSD: kern_sysctl.c,v 1.17 1996/05/20 17:49:05 mrg Exp $	*/
 
 /*-
@@ -511,17 +511,11 @@ kern_sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp, void *newp,
 #endif
 #ifdef CRYPTO
 	case KERN_USERCRYPTO:
-		if (newp && newlen)
-			return (EINVAL);
 		return (sysctl_int(oldp, oldlenp, newp, newlen, &usercrypto));
 	case KERN_USERASYMCRYPTO:
-		if (newp && newlen)
-			return (EINVAL);
 		return (sysctl_int(oldp, oldlenp, newp, newlen,
 			    &userasymcrypto));
 	case KERN_CRYPTODEVALLOWSOFT:
-		if (newp && newlen)
-			return (EINVAL);
 		return (sysctl_int(oldp, oldlenp, newp, newlen,
 			    &cryptodevallowsoft));
 #endif
