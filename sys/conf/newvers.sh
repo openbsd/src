ce7ec9bec6188ef925541add58cfda808ec93094
deraadt <deraadt@openbsd.org>
2011-03-02 01:58:39 +0000

    we are now hacking on 4.9-current

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.110 2011/02/15 07:14:45 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.111 2011/03/02 01:58:39 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,10 +64,10 @@ ost="OpenBSD"
 osr="4.9"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
-#define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
+#define STATUS "-beta"			/* just before a release */
 #endif
 
 const char ostype[] = "${ost}";
