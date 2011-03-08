d8c325e90d90ac3cde24296db313be39c1399e52
bluhm <bluhm@openbsd.org>
2011-03-08 01:16:31 +0000

    Add regression test that checks ENOTSOCK error for non-socket drain.

---
+++ regress/sys/kern/splice/error-splice-ENOTSOCK.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# test ENOTSOCK for splicing with non-socket
+
+use Errno;
+use IO::Socket;
+use constant SO_SPLICE => 0x1023;
+
+my $sl = IO::Socket::INET->new(
+    Proto => "tcp",
+    Listen => 5,
+    LocalAddr => "127.0.0.1",
+) or die "socket listen failed: $!";
+
+my $s = IO::Socket::INET->new(
+    Proto => "tcp",
+    PeerAddr => $sl->sockhost(),
+    PeerPort => $sl->sockport(),
+) or die "socket failed: $!";
+
+$s->setsockopt(SOL_SOCKET, SO_SPLICE, pack('i', 0))
+    and die "splice with non non-socket fileno succeeded";
+$!{ENOTSOCK}
+    or die "error not ENOTSOCK: $!"
