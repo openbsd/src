720fec5d32d8660fa56ab37ba6983867112e6927
deraadt <deraadt@openbsd.org>
2011-03-13 22:12:37 +0000

    add a regress test for the vis and unvis functions.  after finding one bug, this then found a 2nd bug.. worked on with guenther

--- regress/lib/libc/vis/vis_test.c
+++ regress/lib/libc/vis/vis_test.c
@@ -1,23 +1,109 @@
-/*	$OpenBSD: vis_test.c,v 1.2 2009/06/21 00:38:22 martynas Exp $	*/
+/*	$OpenBSD: vis_test.c,v 1.3 2011/03/13 22:12:37 deraadt Exp $	*/
 
 /* Public domain. 2005, Otto Moerbeek */
 
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <vis.h>
 
-int
-main()
+#define NTESTS	8000
+#define NCH	800
+
+char ibuf[NCH];
+char obuf[NCH * 4];
+char rbuf[NCH * 4];
+
+int flags[] = {
+	VIS_ALL,
+	VIS_GLOB,
+	VIS_TAB,
+	VIS_NL,
+	VIS_WHITE,
+	VIS_SAFE
+};
+
+char *flagname[] = {
+	"VIS_ALL",
+	"VIS_GLOB",
+	"VIS_TAB",
+	"VIS_NL",
+	"VIS_WHITE",
+	"VIS_SAFE"
+};
+
+int title;
+
+void
+dotitle(int i, int j)
 {
+	if (title == 0)
+		printf("%d %s:", i, flagname[j]);
+	title = 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+
 	char inp[UCHAR_MAX + 1];
 	char out[4 * UCHAR_MAX + 1];
-	int i;
+	int i, j, fail = 0;
+	ssize_t owant, o, r;
 
 	for (i = 0; i <= UCHAR_MAX; i++) {
 		inp[i] = i;
 	}
 	strvisx(out, inp, UCHAR_MAX + 1, 0);
 	printf("%s\n", out);
-	exit(0);
+
+	for (i = 0; i < NTESTS; i++) {
+		arc4random_buf(ibuf, sizeof(ibuf) - 1);
+		ibuf[sizeof(ibuf) - 1] = '\0';
+		title = 0;
+
+		for (j = 0; j < sizeof(flags)/sizeof(flags[0]); j++) {
+			owant = sizeof(ibuf);
+			o = strnvis(obuf, ibuf, owant, flags[j]);
+			if (o >= owant) {
+				owant = o + 1;
+				o = strnvis(obuf, ibuf, owant, flags[j]);
+				if (o > owant) {
+					dotitle(i, j);
+					printf("HUGE overflow\n");
+				}
+				if (o < owant - 1) {
+					dotitle(i, j);
+					printf("over-estimate of overflow\n");
+				}
+			} else if (o > strlen(ibuf) * 4) {
+				dotitle(i, j);
+				printf("wants too much %d %d\n", o, strlen(ibuf) * 4);
+				continue;
+			}
+
+			r = strnunvis(rbuf, obuf, sizeof rbuf);
+
+			if (r == -1) {
+				dotitle(i, j);
+				printf("cannot decode\n");
+				printf("%s\n", obuf);
+				fail = 1;
+			} else if (r != strlen(ibuf)) {
+				dotitle(i, j);
+				printf("rlen %d != inlen %d\n", r, strlen(ibuf));
+				printf("%s\n", obuf);
+				printf("%s\n", rbuf);
+				fail = 1;
+			} else if (bcmp(ibuf, rbuf, r)) {
+				dotitle(i, j);
+				printf("strings are different\n");
+				printf("%s\n", ibuf);
+				printf("%s\n", rbuf);
+				fail = 1;
+			}
+		}
+	}
+	exit(fail);
 }
