c8c4dd3640c9f8be80961cda0fd4d3bd28d55959
okan <okan@openbsd.org>
2011-03-16 09:49:11 +0000

    spelling.
    ok yasuoka@

--- usr.sbin/npppd/l2tp/l2tpd.c
+++ usr.sbin/npppd/l2tp/l2tpd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: l2tpd.c,v 1.5 2010/09/24 14:50:30 yasuoka Exp $ */
+/* $OpenBSD: l2tpd.c,v 1.6 2011/03/16 09:49:11 okan Exp $ */
 
 /*-
  * Copyright (c) 2009 Internet Initiative Japan Inc.
@@ -26,7 +26,7 @@
  * SUCH DAMAGE.
  */
 /**@file L2TP(Layer Two Tunneling Protocol "L2TP") / RFC2661 */
-/* $Id: l2tpd.c,v 1.5 2010/09/24 14:50:30 yasuoka Exp $ */
+/* $Id: l2tpd.c,v 1.6 2011/03/16 09:49:11 okan Exp $ */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
@@ -590,7 +590,7 @@ l2tpd_stop(l2tpd *_this)
 	if (l2tpd_is_stopped(_this))
 		return;
 	if (l2tpd_is_shutting_down(_this)) {
-		/* terminate immidiatly, when 2nd call */
+		/* terminate immediately, when 2nd call */
 		l2tpd_stop_immediatly(_this);
 		return;
 	}
