88e905b94a879008c2583cdeccc7ce865689338a
claudio <claudio@openbsd.org>
2011-03-23 13:40:42 +0000

    Don't process ICMP6 redirects by default anymore. This is in line with what we do for IPv4. rtsol will turn it back on if -F is used. After discussion with bluhm@, fgsch@, sthen@ and deraadt@ OK sthen@

--- sys/netinet6/in6_proto.c
+++ sys/netinet6/in6_proto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6_proto.c,v 1.60 2011/01/07 17:50:42 bluhm Exp $	*/
+/*	$OpenBSD: in6_proto.c,v 1.61 2011/03/23 13:40:42 claudio Exp $	*/
 /*	$KAME: in6_proto.c,v 1.66 2000/10/10 15:35:47 itojun Exp $	*/
 
 /*
@@ -310,7 +310,7 @@ u_long	rip6_sendspace = RIPV6SNDQ;
 u_long	rip6_recvspace = RIPV6RCVQ;
 
 /* ICMPV6 parameters */
-int	icmp6_rediraccept = 1;		/* accept and process redirects */
+int	icmp6_rediraccept = 0;		/* don't process redirects by default */
 int	icmp6_redirtimeout = 10 * 60;	/* 10 minutes */
 int	icmp6errppslim = 100;		/* 100pps */
 int	icmp6_nodeinfo = 1;		/* enable/disable NI response */
