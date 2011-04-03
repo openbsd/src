0e62a8e57ec6a7c244a36994a59f242d0ca693c7
dlg <dlg@openbsd.org>
2011-04-03 16:53:20 +0000

    get rid of NO_CCB by moving to iopools.
    "be brave" k2k11

--- sys/dev/i2o/ioprbsvar.h
+++ sys/dev/i2o/ioprbsvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ioprbsvar.h,v 1.3 2007/10/17 15:07:37 deraadt Exp $	*/
+/*	$OpenBSD: ioprbsvar.h,v 1.4 2011/04/03 16:53:20 dlg Exp $	*/
 
 /*
  * Copyright (c) 2001 Niklas Hallqvist
@@ -73,6 +73,8 @@ struct ioprbs_softc {
 	/* commands which have been returned by the controller */
 	LIST_HEAD(, scsi_xfer) sc_queue;
 	struct scsi_xfer *sc_queuelast;
+	struct scsi_iopool sc_iopool;
+	struct mutex sc_ccb_mtx;
 };
 
 #define	IOPRBS_CLAIMED		0x01
