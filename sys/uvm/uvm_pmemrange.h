db9d8d58357e631e50c31e3f58043fcdb71d38c2
guenther <guenther@openbsd.org>
2011-04-03 14:16:42 +0000

    Fix an #ifdef: uvm_pmr_isfree() is also used when DEBUG is defined Pointed out by Fred Crowson.  ok ariane@

--- sys/uvm/uvm_pmemrange.h
+++ sys/uvm/uvm_pmemrange.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_pmemrange.h,v 1.5 2010/04/22 19:02:55 oga Exp $	*/
+/*	$OpenBSD: uvm_pmemrange.h,v 1.6 2011/04/03 14:16:42 guenther Exp $	*/
 
 /*
  * Copyright (c) 2009 Ariane van der Steldt <ariane@stack.nl>
@@ -76,7 +76,7 @@ int	uvm_pmr_getpages(psize_t, paddr_t, paddr_t, paddr_t, paddr_t,
 	    int, int, struct pglist *);
 void	uvm_pmr_init(void);
 
-#ifdef DDB
+#if defined(DDB) || defined(DEBUG)
 int	uvm_pmr_isfree(struct vm_page *pg);
 #endif
 
