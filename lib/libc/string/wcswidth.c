074b56922fa7d2c33d6603e252d323595229c6c3
stsp <stsp@openbsd.org>
2011-04-04 18:16:24 +0000

    Add a wcswidth man page (based on FreeBSD), and fix the implementation to return -1 in case of an unprintable character. ok nicm jmc

--- lib/libc/string/wcswidth.c
+++ lib/libc/string/wcswidth.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wcswidth.c,v 1.3 2005/08/08 08:05:37 espie Exp $	*/
+/*	$OpenBSD: wcswidth.c,v 1.4 2011/04/04 18:16:24 stsp Exp $	*/
 /*	$NetBSD: wcswidth.c,v 1.2 2001/01/03 14:29:37 lukem Exp $	*/
 
 /*-
@@ -34,11 +34,14 @@
 int
 wcswidth(const wchar_t *s, size_t n)
 {
-	int w;
+	int w, q;
 
 	w = 0;
 	while (n && *s) {
-		w += wcwidth(*s);
+		q = wcwidth(*s);
+		if (q == -1)
+			return (-1);
+		w += q;
 		s++;
 		n--;
 	}
