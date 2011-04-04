a28b77b3ed0298c42116a7191400b208929314c8
ajacoutot <ajacoutot@openbsd.org>
2011-04-04 14:37:25 +0000

    If we choose to create a user during installation, put it into /root/.forward.
    "makes sense" kettenis, ok pirofti@ deraadt@

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: install.sh,v 1.217 2011/02/07 18:01:08 miod Exp $
+#	$OpenBSD: install.sh,v 1.218 2011/04/04 14:37:25 ajacoutot Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2009 Todd Miller, Theo de Raadt, Ken Westerback
@@ -285,6 +285,7 @@ if [[ -n $user ]]; then
 	uline="${user}:${_encr}:1000:1000:staff:0:0:${username}:/home/${user}:/bin/ksh"
 	echo "$uline" >> /mnt/etc/master.passwd
 	echo "${user}:*:1000:" >> /mnt/etc/group
+	echo ${user} > /mnt/root/.forward
 
 	mkdir -p /mnt/home/$user
 	(cd /mnt/etc/skel; cp -pR . /mnt/home/$user)
