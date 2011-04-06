90f7131b38c80e6df6b69e2c053dc56ccf1e8731
miod <miod@openbsd.org>
2011-04-06 18:01:50 +0000

    Fix a typo causing a sanity check in m88110_syscall() to never trigger.

--- sys/arch/m88k/m88k/trap.c
+++ sys/arch/m88k/m88k/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.74 2011/04/03 14:56:28 guenther Exp $	*/
+/*	$OpenBSD: trap.c,v 1.75 2011/04/06 18:01:50 miod Exp $	*/
 /*
  * Copyright (c) 2004, Miodrag Vallat.
  * Copyright (c) 1998 Steve Murphree, Jr.
@@ -1365,7 +1365,7 @@ m88110_syscall(register_t code, struct trapframe *tf)
 		callp += code;
 
 	i = callp->sy_argsize / sizeof(register_t);
-	if (i > sizeof(args) > sizeof(register_t))
+	if (i > sizeof(args) / sizeof(register_t))
 		panic("syscall nargs");
 	if (i > nap) {
 		bcopy((caddr_t)ap, (caddr_t)args, nap * sizeof(register_t));
