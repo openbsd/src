1540486af55b32c8068123ac309feab491efe5e4
martynas <martynas@openbsd.org>
2011-04-23 21:34:02 +0000

    Sysarch is for userland, not kernel.  ifdef _KERNEL -> ifndef _KERNEL so that the userland actually gets the proto. OK kettenis@, miod@.

--- sys/arch/alpha/include/sysarch.h
+++ sys/arch/alpha/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysarch.h,v 1.6 2011/03/23 16:54:34 pirofti Exp $	*/
+/*	$OpenBSD: sysarch.h,v 1.7 2011/04/23 21:34:02 martynas Exp $	*/
 /*	$NetBSD: sysarch.h,v 1.8 2001/04/26 03:10:46 ross Exp $	*/
 
 /*-
@@ -54,8 +54,8 @@ struct alpha_fp_c_args {
 	uint64_t fp_c;
 };
 
-#ifdef _KERNEL
+#ifndef _KERNEL
 int	sysarch(int, void *);
-#endif /* _KERNEL */
+#endif /* !_KERNEL */
 
 #endif /* !_MACHINE_SYSARCH_H_ */
