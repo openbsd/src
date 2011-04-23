b19afbad11a8f5e66208100cdaf58d3e86166f40
nicm <nicm@openbsd.org>
2011-04-23 22:08:15 +0000

    Enable the SIGWINCH handler in ncurses.
    Prompted by mail from Mikolaj Kucharski on tech@ and discussion with
    deraadt@ and millert@.
    
    A library installing signal handlers without being asked is seriously
    wrong but it appears quite a few applications now depend on the ncurses
    SIGWINCH and it looks to be safe. Safer than the other signal handlers
    it installs (apparently without request... boke).
    
    ok millert

--- lib/libcurses/ncurses_cfg.h
+++ lib/libcurses/ncurses_cfg.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ncurses_cfg.h,v 1.25 2010/09/06 17:26:17 nicm Exp $ */
+/* $OpenBSD: ncurses_cfg.h,v 1.26 2011/04/23 22:08:15 nicm Exp $ */
 
 /* include/ncurses_cfg.h.  Generated automatically by configure.  */
 /****************************************************************************
@@ -33,7 +33,7 @@
  *  Author: Thomas E. Dickey <dickey@clark.net> 1997                        *
  ****************************************************************************/
 /*
- * $Id: ncurses_cfg.h,v 1.25 2010/09/06 17:26:17 nicm Exp $
+ * $Id: ncurses_cfg.h,v 1.26 2011/04/23 22:08:15 nicm Exp $
  *
  * This is a template-file used to generate the "ncurses_cfg.h" file.
  *
@@ -171,12 +171,12 @@
 #define USE_LINKS 1
 #define USE_OPENPTY_HEADER <util.h>
 #define USE_ROOT_ENVIRON 1
+#define USE_SIGWINCH 1
 #define USE_WIDEC_SUPPORT 1
 #define USE_XTERM_PTY 1
 /* #define HAVE_LIBFORM 1 */
 /* #define HAVE_LIBMENU 1 */
 /* #define HAVE_LIBPANEL 1 */
-/* #define USE_SIGWINCH 1 */
 
 #include <ncurses_def.h>
 
