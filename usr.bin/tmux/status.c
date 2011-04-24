9eb504ef515790c90369fb9a006cd57a9ca9d9a3
nicm <nicm@openbsd.org>
2011-04-24 21:32:07 +0000

    Provide #h for short hostname (no domain) from Michal Mazurek.

--- usr.bin/tmux/status.c
+++ usr.bin/tmux/status.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: status.c,v 1.73 2011/04/18 19:49:05 nicm Exp $ */
+/* $OpenBSD: status.c,v 1.74 2011/04/24 21:32:07 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -408,6 +408,13 @@ status_replace1(struct client *c, struct session *s, struct winlink *wl,
 	case 'H':
 		if (gethostname(tmp, sizeof tmp) != 0)
 			fatal("gethostname failed");
+		ptr = tmp;
+		goto do_replace;
+	case 'h':
+		if (gethostname(tmp, sizeof tmp) != 0)
+			fatal("gethostname failed");
+		if ((ptr = strchr(tmp, '.')) != NULL)
+			*ptr = '\0';
 		ptr = tmp;
 		goto do_replace;
 	case 'I':
