e89b951e17d2ba1c186af017b7f7bf32678e6fb2
ariane <ariane@openbsd.org>
2011-04-28 20:43:41 +0000

    Expose pmap_prefer parameters. This will enable future uvm_map to make intelligent allocation decisions.

--- sys/arch/hppa64/include/pmap.h
+++ sys/arch/hppa64/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.4 2010/12/26 15:40:59 miod Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.5 2011/04/28 20:43:41 ariane Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -67,6 +67,11 @@ pmap_prefer(vaddr_t offs, vaddr_t hint)
 		pmap_prefer_hint += HPPA_PGALIAS;
 	return pmap_prefer_hint;
 }
+
+/* pmap prefer alignment */
+#define PMAP_PREFER_ALIGN()	(HPPA_PGALIAS)
+/* pmap prefer offset within alignment */
+#define PMAP_PREFER_OFFSET(of)	((of) & HPPA_PGAOFF)
 
 #define	PMAP_GROWKERNEL
 #define	PMAP_STEAL_MEMORY
