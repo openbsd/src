69c136712beacae74a8d6ab401855b9f55ca23cc
ariane <ariane@openbsd.org>
2011-04-28 20:50:58 +0000

    Expose pmap_prefer parameters. This enables future uvm_map allocator to behave intelligently.

--- sys/arch/arm/include/pmap.h
+++ sys/arch/arm/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.17 2011/03/23 16:54:34 pirofti Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.18 2011/04/28 20:50:58 ariane Exp $	*/
 /*	$NetBSD: pmap.h,v 1.76 2003/09/06 09:10:46 rearnsha Exp $	*/
 
 /*
@@ -620,6 +620,14 @@ vaddr_t	pmap_prefer(vaddr_t, vaddr_t);
 
 extern uint32_t pmap_alias_dist;
 extern uint32_t pmap_alias_bits;
+
+/* pmap prefer alias alignment. */
+#define PMAP_PREFER_ALIGN()	(pmap_alias_dist)
+/* pmap prefer offset withing alignment. */
+#define PMAP_PREFER_OFFSET(of)						\
+    (PMAP_PREFER_ALIGN() == 0 ? 0 : ((of) & (PMAP_PREFER_ALIGN() - 1)))
+
+
 #endif /* _LOCORE */
 
 #endif /* _KERNEL */
