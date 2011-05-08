2c4ee07c2e21f5164a0d7e2cf4c021acbd54c41d
nicm <nicm@openbsd.org>
2011-05-08 19:53:06 +0000

    Use the tsl and fsl terminfo(5) capabilities to update terminal title and automatically fill them in on terminals with the XT capability (which means their title setting is xterm-compatible). From hsim at gmx.li.

--- usr.bin/tmux/options-table.c
+++ usr.bin/tmux/options-table.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: options-table.c,v 1.5 2011/04/18 19:49:05 nicm Exp $ */
+/* $OpenBSD: options-table.c,v 1.6 2011/05/08 19:53:06 nicm Exp $ */
 
 /*
  * Copyright (c) 2011 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -355,7 +355,7 @@ const struct options_table_entry session_options_table[] = {
 
 	{ .name = "terminal-overrides",
 	  .type = OPTIONS_TABLE_STRING,
-	  .default_str = "*88col*:colors=88,*256col*:colors=256"
+	  .default_str = "*88col*:colors=88,*256col*:colors=256,xterm*:XT"
 	},
 
 	{ .name = "update-environment",
