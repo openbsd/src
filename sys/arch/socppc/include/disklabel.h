dc1c1999194fd60dfe4af9cf3b880bb376647d3b
deraadt <deraadt@openbsd.org>
2011-05-22 23:37:37 +0000

    do not need the DPME parts anymore

--- sys/arch/socppc/include/disklabel.h
+++ sys/arch/socppc/include/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.h,v 1.1 2008/05/10 12:02:21 kettenis Exp $	*/
+/*	$OpenBSD: disklabel.h,v 1.2 2011/05/22 23:37:37 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1994 Christopher G. Demetriou
@@ -36,55 +36,5 @@
 #define	LABELSECTOR	1		/* sector containing label */
 #define	LABELOFFSET	0		/* offset of label in sector */
 #define	MAXPARTITIONS	16		/* number of partitions */
-
-/* HFS/DPME */
-
-/* partition map structure from Inside Macintosh: Devices, SCSI Manager
- * pp. 13-14.  The partition map always begins on physical block 1.  
- * 
- * With the exception of block 0, all blocks on the disk must belong to
- * exactly one partition.  The partition map itself belongs to a partition
- * of type `APPLE_PARTITION_MAP', and is not limited in size by anything
- * other than available disk space.  The partition map is not necessarily
- * the first partition listed.
- */
-struct part_map_entry {
-#define PART_ENTRY_MAGIC        0x504d
-	u_int16_t       pmSig;          /* partition signature */   
-	u_int16_t       pmSigPad;       /* (reserved) */
-	u_int32_t       pmMapBlkCnt;    /* number of blocks in partition map */
-	u_int32_t       pmPyPartStart;  /* first physical block of partition */
-	u_int32_t       pmPartBlkCnt;   /* number of blocks in partition */
-	char            pmPartName[32]; /* partition name */
-	char            pmPartType[32]; /* partition type */
-	u_int32_t       pmLgDataStart;  /* first logical block of data area */
-	u_int32_t       pmDataCnt;      /* number of blocks in data area */
-	u_int32_t       pmPartStatus;   /* partition status information */
-	u_int32_t       pmLgBootStart;  /* first logical block of boot code */
-	u_int32_t       pmBootSize;     /* size of boot code, in bytes */
-	u_int32_t       pmBootLoad;     /* boot code load address */
-	u_int32_t       pmBootLoad2;    /* (reserved) */
-	u_int32_t       pmBootEntry;    /* boot code entry point */
-	u_int32_t       pmBootEntry2;   /* (reserved) */
-	u_int32_t       pmBootCksum;    /* boot code checksum */
-	char            pmProcessor[16]; /* processor type (e.g. "68020") */
-	u_int8_t        pmBootArgs[128]; /* A/UX boot arguments */
-	/* we do not index the disk image as an array,
-	 * leave out the on disk padding
-	 */
-#if 0
-	u_int8_t        pad[248];       /* pad to end of block */
-#endif
-};
-
-#define PART_TYPE_DRIVER        "APPLE_DRIVER"
-#define PART_TYPE_DRIVER43      "APPLE_DRIVER43"
-#define PART_TYPE_DRIVERATA     "APPLE_DRIVER_ATA"
-#define PART_TYPE_DRIVERIOKIT   "APPLE_DRIVER_IOKIT"
-#define PART_TYPE_FWDRIVER      "APPLE_FWDRIVER"
-#define PART_TYPE_FWB_COMPONENT "FWB DRIVER COMPONENTS"
-#define PART_TYPE_FREE          "APPLE_FREE"
-#define PART_TYPE_MAC           "APPLE_HFS"
-#define PART_TYPE_OPENBSD       "OPENBSD"
 
 #endif /* _MACHINE_DISKLABEL_H_ */
