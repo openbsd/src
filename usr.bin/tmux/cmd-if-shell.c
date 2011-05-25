4f99d3bd95e92026da61862ff1122a8deb755f54
nicm <nicm@openbsd.org>
2011-05-25 17:50:52 +0000

    Nuke a redundant if statement, from Tiago Cunha.

--- usr.bin/tmux/cmd-if-shell.c
+++ usr.bin/tmux/cmd-if-shell.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-if-shell.c,v 1.11 2011/01/26 01:54:56 nicm Exp $ */
+/* $OpenBSD: cmd-if-shell.c,v 1.12 2011/05/25 17:50:52 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Tiago Cunha <me@tiagocunha.org>
@@ -88,11 +88,7 @@ cmd_if_shell_callback(struct job *job)
 		return;
 	}
 
-	if (cmd_list_exec(cmdlist, ctx) < 0) {
-		cmd_list_free(cmdlist);
-		return;
-	}
-
+	cmd_list_exec(cmdlist, ctx);
 	cmd_list_free(cmdlist);
 }
 
