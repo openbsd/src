1eb55be3605a7edaa8d83b2f4950137526805de1
miod <miod@openbsd.org>
2011-06-25 19:38:47 +0000

    Remove irrelevant comments borrowed from loongson.

--- sys/arch/octeon/include/param.h
+++ sys/arch/octeon/include/param.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: param.h,v 1.2 2010/10/11 15:51:06 syuu Exp $ */
+/* $OpenBSD: param.h,v 1.3 2011/06/25 19:38:47 miod Exp $ */
 /* public domain */
 
 #ifndef _MACHINE_PARAM_H_
@@ -8,19 +8,9 @@
 #define	_MACHINE	octeon
 #define MACHINE_ARCH	"mips64"
 #define _MACHINE_ARCH	mips64
-/* not the canonical endianness */
-#define	MACHINE_CPU	"mips64"
-#define	_MACHINE_CPU	mips64
 
 #define MID_MACHINE	MID_MIPS64
 
-/*
- * The Loongson level 1 cache expects software to prevent virtual
- * aliases. Unfortunately, since this cache is physically tagged,
- * this would require all virtual address to have the same bits 14
- * and 13 as their physical addresses, which is not something the
- * kernel can guarantee unless the page size is at least 16KB.
- */
 #define	PAGE_SHIFT	14
 
 #include <mips64/param.h>
