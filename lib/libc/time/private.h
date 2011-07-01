dd91d8afd6842df9c3038b0bb2f13e4c70a89f4e
millert <millert@openbsd.org>
2011-07-01 13:34:18 +0000

    Enable the knob to dynamically allocate (large) state structures.

--- lib/libc/time/private.h
+++ lib/libc/time/private.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: private.h,v 1.22 2008/01/07 01:09:37 millert Exp $	*/
+/*	$OpenBSD: private.h,v 1.23 2011/07/01 13:34:18 millert Exp $	*/
 #ifndef PRIVATE_H
 
 #define PRIVATE_H
@@ -13,6 +13,7 @@
 #define TM_ZONE			tm_zone
 #define PCTS			1
 #define XPG4_1994_04_09		1
+#define ALL_STATE		1
 #define STD_INSPIRED		1
 #define HAVE_STRERROR		1
 #define HAVE_STDINT_H		1
