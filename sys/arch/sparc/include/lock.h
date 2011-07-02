7d99efdec5f9e1ffc6c249a0bd1223acbdb8b571
guenther <guenther@openbsd.org>
2011-07-02 22:19:16 +0000

    Per recommandation in the the sparc docs, use unlocked reads when spinning on a contended lock.
    ok kettenis@

--- sys/arch/sparc/include/lock.h
+++ sys/arch/sparc/include/lock.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lock.h,v 1.4 2011/03/23 16:54:37 pirofti Exp $	*/
+/*	$OpenBSD: lock.h,v 1.5 2011/07/02 22:19:16 guenther Exp $	*/
 
 /* public domain */
 
@@ -18,27 +18,30 @@ __cpu_simple_lock_init(__cpu_simple_lock_t *l)
 	*l = __SIMPLELOCK_UNLOCKED;
 }
 
+static __inline__ u_int8_t
+__cpu_ldstub(__cpu_simple_lock_t *l)
+{
+	u_int8_t old;
+
+	__asm__ __volatile__
+	    ("ldstub [%1], %0" : "=&r" (old) : "r" (l) : "memory");
+	return old;
+}
+
 static __inline__ void
 __cpu_simple_lock(__cpu_simple_lock_t *l)
 {
-	__cpu_simple_lock_t old;
-
-	do {
-		old = __SIMPLELOCK_LOCKED;
-		__asm__ __volatile__
-		    ("ldstub [%0], %1" : "=r" (l), "=r" (old) : "0" (l));
-	} while (old != __SIMPLELOCK_UNLOCKED);
+	while (__cpu_ldstub(l) != __SIMPLELOCK_UNLOCKED)
+		while (*l != __SIMPLELOCK_UNLOCKED)
+			;
 }
 
 static __inline__ int
 __cpu_simple_lock_try(__cpu_simple_lock_t *l)
 {
-	__cpu_simple_lock_t old = __SIMPLELOCK_LOCKED;
-
-	__asm__ __volatile__
-	    ("ldstub [%0], %1" : "=r" (l), "=r" (old) : "0" (l));
-
-	return (old == __SIMPLELOCK_UNLOCKED);
+	if (__cpu_ldstub(l) != __SIMPLELOCK_UNLOCKED)
+		return (0);
+	return (1);
 }
 
 static __inline__ void
