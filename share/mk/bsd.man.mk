b17f0a03d6615b56e1348ac1aff7621454941bf5
schwarze <schwarze@openbsd.org>
2011-07-05 04:49:35 +0000

    Allow parallel linting of manuals, by using one mandoc process and one timestamp file per man page. This might be slightly slower on single-processor machines, but it's considerably faster with make -j.
    "I think so" deraadt@

--- share/mk/bsd.man.mk
+++ share/mk/bsd.man.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.man.mk,v 1.35 2011/06/28 23:50:46 schwarze Exp $
+#	$OpenBSD: bsd.man.mk,v 1.36 2011/07/05 04:49:35 schwarze Exp $
 #	$NetBSD: bsd.man.mk,v 1.23 1996/02/10 07:49:33 jtc Exp $
 #	@(#)bsd.man.mk	5.2 (Berkeley) 5/11/90
 
@@ -17,17 +17,15 @@ MANSUBDIR:=${MANSUBDIR:S,^,/,:S,$,/,}
 MANSUBDIR=/
 .endif
 
-CLEANFILES+= .man-linted
+CLEANFILES+=.man-linted *.manlint
 
-.if defined(MAN) && !empty(MAN)
-.man-linted: ${MAN}
+.for page in ${MAN}
+${page}.manlint: ${page}
 	mandoc -Tlint -Wfatal ${.ALLSRC}
 	@touch ${.TARGET}
 
-all: .man-linted
-.endif
+all: ${page}.manlint
 
-.for page in ${MAN}
 .  for sub in ${MANSUBDIR}
 _MAN_INST=${DESTDIR}${MANDIR}${page:E}${sub}${page:T}
 ${_MAN_INST}: ${page}
