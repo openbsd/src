a269d1c9059b659a04ab887c2e7b61929099a335
tedu <tedu@openbsd.org>
2011-07-05 20:00:18 +0000

    Remove a broken optimization found by the new pool_chk code.  It leaves an empty page in curpage, and this inconsistency slowly spreads until finally one of the other pool checks freaks out. ok art deraadt

--- sys/kern/subr_pool.c
+++ sys/kern/subr_pool.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_pool.c,v 1.105 2011/07/05 16:36:15 tedu Exp $	*/
+/*	$OpenBSD: subr_pool.c,v 1.106 2011/07/05 20:00:18 tedu Exp $	*/
 /*	$NetBSD: subr_pool.c,v 1.61 2001/09/26 07:14:56 chs Exp $	*/
 
 /*-
@@ -783,10 +783,7 @@ pool_do_put(struct pool *pp, void *v)
 
 	if (pp->pr_flags & PR_WANTED) {
 		pp->pr_flags &= ~PR_WANTED;
-		if (ph->ph_nmissing == 0)
-			pp->pr_nidle++;
 		wakeup(pp);
-		return;
 	}
 
 	/*
