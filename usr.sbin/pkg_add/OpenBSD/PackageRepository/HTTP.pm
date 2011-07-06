6dac1782f24bac0e86818f222f9d80169a6cb824
espie <espie@openbsd.org>
2011-07-06 13:05:52 +0000

    future code to handle HTTP 1.1 connection: need to establish a somewhat persistant connection, will use the same client code for scp and http.

---
+++ usr.sbin/pkg_add/OpenBSD/PackageRepository/HTTP.pm
@@ -0,0 +1,23 @@
+#! /usr/bin/perl
+# ex:ts=8 sw=4:
+# $OpenBSD: HTTP.pm,v 1.1 2011/07/06 13:05:52 espie Exp $
+#
+# Copyright (c) 2011 Marc Espie <espie@openbsd.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+use strict;
+use warnings;
+
+# to be done
+1;
