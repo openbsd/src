63b9fd3a51fd0d72dddb705eab92978a9627e391
schwarze <schwarze@openbsd.org>
2011-07-06 04:10:27 +0000

    Make sure ${BEFOREMAN} is finished before ${MANLINT} is started; otherwise, stuff gets done in the wrong order in make -j. While here, also make sure ${BEFOREMAN} is finished before any manual source code is generated.
    tested in make build / make release by krw@

--- share/mk/bsd.man.mk
+++ share/mk/bsd.man.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.man.mk,v 1.36 2011/07/05 04:49:35 schwarze Exp $
+#	$OpenBSD: bsd.man.mk,v 1.37 2011/07/06 04:10:27 schwarze Exp $
 #	$NetBSD: bsd.man.mk,v 1.23 1996/02/10 07:49:33 jtc Exp $
 #	@(#)bsd.man.mk	5.2 (Berkeley) 5/11/90
 
@@ -10,6 +10,10 @@
 .MAIN: all
 .endif
 
+BEFOREMAN?=
+MANLINT=${MAN:S/$/.manlint/}
+CLEANFILES+=.man-linted ${MANLINT}
+
 # Add / so that we don't have to specify it.
 .if defined(MANSUBDIR) && !empty(MANSUBDIR)
 MANSUBDIR:=${MANSUBDIR:S,^,/,:S,$,/,}
@@ -17,15 +21,32 @@ MANSUBDIR:=${MANSUBDIR:S,^,/,:S,$,/,}
 MANSUBDIR=/
 .endif
 
-CLEANFILES+=.man-linted *.manlint
-
+# Files contained in ${BEFOREMAN} must be built before generating any
+# manual page source code.  However, static manual page files contained
+# in the source tree must not appear as targets, or the ${.IMPSRC} in
+# the .man.manlint suffix rule below will not find them in the .PATH.
 .for page in ${MAN}
-${page}.manlint: ${page}
-	mandoc -Tlint -Wfatal ${.ALLSRC}
+.  if target(${page})
+${page}: ${BEFOREMAN}
+.  endif
+.endfor
+
+# In any case, ${BEFOREMAN} must be finished before linting any manuals.
+.if !empty(MANLINT)
+${MANLINT}: ${BEFOREMAN}
+.endif
+
+# Set up the suffix rules for checking manuals.
+_MAN_SUFFIXES=1 2 3 3p 4 5 6 7 8 9
+.for s in ${_MAN_SUFFIXES}
+.SUFFIXES: .${s} .${s}.manlint
+.${s}.${s}.manlint:
+	mandoc -Tlint -Wfatal ${.IMPSRC}
 	@touch ${.TARGET}
+.endfor
 
-all: ${page}.manlint
-
+# Install the real manuals.
+.for page in ${MAN}
 .  for sub in ${MANSUBDIR}
 _MAN_INST=${DESTDIR}${MANDIR}${page:E}${sub}${page:T}
 ${_MAN_INST}: ${page}
@@ -36,6 +57,7 @@ maninstall: ${_MAN_INST}
 .  endfor
 .endfor
 
+# Install the manual hardlinks, if any.
 maninstall:
 .if defined(MLINKS) && !empty(MLINKS)
 .  for sub in ${MANSUBDIR}
@@ -48,5 +70,5 @@ maninstall:
 .  endfor
 .endif
 
-BEFOREMAN?=
-all: ${BEFOREMAN} ${MAN}
+# Explicitly list ${BEFOREMAN} to get it done even if ${MAN} is empty.
+all: ${BEFOREMAN} ${MAN} ${MANLINT}
