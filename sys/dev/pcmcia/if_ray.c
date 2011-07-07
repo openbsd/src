467fa03b3308ff83920259ef7e2fcb28ec828e38
henning <henning@openbsd.org>
2011-07-07 19:13:29 +0000

    get rid of a debug printf that keeps giving me flase psotives because it is the only place in the tree (minus if.{c,h} which are allowed to) that reads ifq_maxlen without setting it, ryan ok

--- sys/dev/pcmcia/if_ray.c
+++ sys/dev/pcmcia/if_ray.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ray.c,v 1.47 2011/07/03 15:47:17 matthew Exp $	*/
+/*	$OpenBSD: if_ray.c,v 1.48 2011/07/07 19:13:29 henning Exp $	*/
 /*	$NetBSD: if_ray.c,v 1.21 2000/07/05 02:35:54 onoe Exp $	*/
 
 /*
@@ -1103,10 +1103,6 @@ ray_intr_start(struct ray_softc *sc)
 	u_int8_t *d;
 
 	ifp = &sc->sc_if;
-
-	RAY_DPRINTF(("%s: start free %d qlen %d qmax %d\n",
-	    ifp->if_xname, sc->sc_txfree, ifp->if_snd.ifq_len,
-	    ifp->if_snd.ifq_maxlen));
 
 	ray_cmd_cancel(sc, SCP_IFSTART);
 
