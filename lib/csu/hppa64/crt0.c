8e7074c91e0d9bc179b865f7fb418d82de82c2c7
kettenis <kettenis@openbsd.org>
2011-07-09 00:12:56 +0000

    On hppa64 we need to use __gp instead of $global$.

--- lib/csu/hppa64/crt0.c
+++ lib/csu/hppa64/crt0.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: crt0.c,v 1.2 2005/08/04 16:33:05 espie Exp $	*/
+/*	$OpenBSD: crt0.c,v 1.3 2011/07/09 00:12:56 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2001 Michael Shalayeff
@@ -75,10 +75,10 @@ __asm(
 	".proc\n\t"
 	".callinfo frame=0, calls\n\t"
 	".entry\n\t"
-	"ldil	L%$global$, %r27\n\t"
+	"ldil	L%__gp, %r27\n\t"
 	".call\n\t"
 	"b	___start\n\t"
-	"ldo	R%$global$(%r27), %r27\n\t"
+	"ldo	R%__gp(%r27), %r27\n\t"
 	".exit\n\t"
 	".procend\n\t");
 
