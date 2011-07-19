7d4e60de7a1690bc1c76823e0dc33328d7fcb447
espie <espie@openbsd.org>
2011-07-19 20:04:23 +0000

    tag fuzzy stuff for manual-installs too

--- usr.sbin/pkg_add/OpenBSD/Update.pm
+++ usr.sbin/pkg_add/OpenBSD/Update.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Update.pm,v 1.148 2011/06/19 01:28:43 espie Exp $
+# $OpenBSD: Update.pm,v 1.149 2011/07/19 20:04:23 espie Exp $
 #
 # Copyright (c) 2004-2010 Marc Espie <espie@openbsd.org>
 #
@@ -254,6 +254,7 @@ sub process_hint
 	my $r = $state->choose_location($hint_name, $l);
 	if (defined $r) {
 		$self->add_location($set, $hint, $r);
+		OpenBSD::Add::tag_user_packages($set);
 		return 1;
 	} else {
 		return 0;
