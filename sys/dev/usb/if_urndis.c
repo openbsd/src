37eb2b39279ca70a62715d8f0772bb33f4200fbb
jasper <jasper@openbsd.org>
2011-07-20 13:11:41 +0000

    this works on the galaxy s2 too.
    ok dlg@

--- sys/dev/usb/if_urndis.c
+++ sys/dev/usb/if_urndis.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_urndis.c,v 1.31 2011/07/03 15:47:17 matthew Exp $ */
+/*	$OpenBSD: if_urndis.c,v 1.32 2011/07/20 13:11:41 jasper Exp $ */
 
 /*
  * Copyright (c) 2010 Jonathan Armani <armani@openbsd.org>
@@ -136,7 +136,8 @@ struct cfattach urndis_ca = {
  */
 static const struct usb_devno urndis_devs[] = {
 	{ USB_VENDOR_HTC,	USB_PRODUCT_HTC_ANDROID },
-	{ USB_VENDOR_SAMSUNG2,	USB_PRODUCT_SAMSUNG2_ANDROID }
+	{ USB_VENDOR_SAMSUNG2,	USB_PRODUCT_SAMSUNG2_ANDROID },
+	{ USB_VENDOR_SAMSUNG2,	USB_PRODUCT_SAMSUNG2_ANDROID2 }
 };
 
 usbd_status
