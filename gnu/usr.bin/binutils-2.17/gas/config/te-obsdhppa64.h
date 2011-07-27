8ad7eaad46cf54ba66fb5f63461598910cc79b02
kettenis <kettenis@openbsd.org>
2011-07-27 14:46:30 +0000

    Add support for OpenBSD/hppa64.

---
+++ gnu/usr.bin/binutils-2.17/gas/config/te-obsdhppa64.h
@@ -0,0 +1,26 @@
+/* te-obsdhppa64.h -- OpenBSD target environment declarations.
+   Copyright 2005
+   Free Software Foundation, Inc.
+
+   This file is part of GAS, the GNU Assembler.
+
+   GAS is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GAS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GAS; see the file COPYING.  If not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
+
+#define TE_OpenBSD	1
+#define LOCAL_LABELS_FB 1
+#define LABELS_WITHOUT_COLONS 1
+#define TARGET_ARCH_SIZE 64
+#include "obj-format.h"
