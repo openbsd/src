8a5e6023393aca94f6d4daa11bb03e816f23bbac
nicm <nicm@openbsd.org>
2011-08-16 09:36:23 +0000

    Correctly skip existing numbers when generating the name for a new session.

--- usr.bin/tmux/session.c
+++ usr.bin/tmux/session.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: session.c,v 1.31 2011/04/06 21:51:31 nicm Exp $ */
+/* $OpenBSD: session.c,v 1.32 2011/08/16 09:36:23 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -119,11 +119,18 @@ session_create(const char *name, const char *cmd, const char *cwd,
 	s->sx = sx;
 	s->sy = sy;
 
-	s->idx = next_session++;
-	if (name != NULL)
+	if (name != NULL) {
 		s->name = xstrdup(name);
-	else
-		xasprintf(&s->name, "%u", s->idx);
+		s->idx = next_session++;
+	} else {
+		s->name = NULL;
+		do {
+			s->idx = next_session++;
+			if (s->name != NULL)
+				xfree (s->name);
+			xasprintf(&s->name, "%u", s->idx);
+		} while (RB_FIND(sessions, &sessions, s) != NULL);
+	}
 	RB_INSERT(sessions, &sessions, s);
 
 	if (cmd != NULL) {
