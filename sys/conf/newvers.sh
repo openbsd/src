91947d09f2730a8d4a869ac5c437a30556f3dfbe
kettenis <kettenis@openbsd.org>
2011-08-16 21:00:48 +0000

    we are now hacking on 5.0-current
    requested by deraadt@

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.113 2011/08/03 18:45:55 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.114 2011/08/16 21:00:48 kettenis Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,10 +64,10 @@ ost="OpenBSD"
 osr="5.0"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
-#define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
+#define STATUS "-beta"			/* just before a release */
 #endif
 
 const char ostype[] = "${ost}";
