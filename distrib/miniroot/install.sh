eb7974526435ee4b3f0c19aea0192ddb9435bf32
todd <todd@openbsd.org>
2011-08-17 03:42:14 +0000

    make sure to set two rtsol sysctls, not just the last one ok krw@

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: install.sh,v 1.223 2011/07/24 15:33:41 fgsch Exp $
+#	$OpenBSD: install.sh,v 1.224 2011/08/17 03:42:14 todd Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2009 Todd Miller, Theo de Raadt, Ken Westerback
@@ -315,8 +315,7 @@ fi
 
 if grep -qs '^rtsol' /mnt/etc/hostname.*; then
 	sed -e "/^#\(net\.inet6\.ip6\.accept_rtadv\)/s//\1/" \
-		/mnt/etc/sysctl.conf >/tmp/sysctl.conf
-	sed -e "/^#\(net\.inet6\.icmp6\.rediraccept\)/s//\1/" \
+	    -e "/^#\(net\.inet6\.icmp6\.rediraccept\)/s//\1/" \
 		/mnt/etc/sysctl.conf >/tmp/sysctl.conf
 	cp /tmp/sysctl.conf /mnt/etc/sysctl.conf
 fi
