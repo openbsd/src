e30e335c553edf6c29af9c80dd46e1c09e058c47
kettenis <kettenis@openbsd.org>
2011-08-19 15:44:36 +0000

    Bring libc and libm in line with the compiler now that we no longer have a 128-bit long double.  Diff committed on behalf of martynas@

--- lib/libc/arch/hppa64/gen/fabs.c
+++ lib/libc/arch/hppa64/gen/fabs.c
@@ -1,8 +1,10 @@
-/*	$OpenBSD: fabs.c,v 1.3 2011/07/08 22:28:33 martynas Exp $	*/
+/*	$OpenBSD: fabs.c,v 1.4 2011/08/19 15:44:36 kettenis Exp $	*/
 
 /*
  * Written by Miodrag Vallat.  Public domain
  */
+
+#include <sys/cdefs.h>
 
 double
 fabs(double val)
@@ -11,3 +13,5 @@ fabs(double val)
 	__asm__ __volatile__("fabs,dbl %0,%0" : "+f" (val));
 	return (val);
 }
+
+__weak_alias(fabsl, fabs);
--- sys/arch/hppa64/include/ieee.h
+++ sys/arch/hppa64/include/ieee.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieee.h,v 1.2 2008/09/07 20:36:06 martynas Exp $	*/
+/*	$OpenBSD: ieee.h,v 1.3 2011/08/19 15:44:36 kettenis Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -87,22 +87,6 @@
 #define	E80_FRACBITS	64
 #endif
 
-#define	EXT_EXPBITS	15
-#define	EXT_FRACHBITS	16
-#define	EXT_FRACHMBITS	32
-#define	EXT_FRACLMBITS	32
-#define	EXT_FRACLBITS	32
-#define	EXT_FRACBITS	112
-
-#define	EXT_IMPLICIT_NBIT
-
-#define	EXT_TO_ARRAY32(p, a) do {		\
-	(a)[0] = (uint32_t)(p)->ext_fracl;	\
-	(a)[1] = (uint32_t)(p)->ext_fraclm;	\
-	(a)[2] = (uint32_t)(p)->ext_frachm;	\
-	(a)[3] = (uint32_t)(p)->ext_frach;	\
-} while(0)
-
 struct ieee_single {
 	u_int	sng_sign:1;
 	u_int	sng_exp:8;
@@ -116,15 +100,6 @@ struct ieee_double {
 	u_int	dbl_fracl;
 };
 
-struct ieee_ext {
-	u_int	ext_sign:1;
-	u_int	ext_exp:15;
-	u_int	ext_frach:16;
-	u_int	ext_frachm;
-	u_int	ext_fraclm;
-	u_int	ext_fracl;
-};
-
 /*
  * Floats whose exponent is in [1..INFNAN) (of whatever type) are
  * `normal'.  Floats whose exponent is INFNAN are either Inf or NaN.
@@ -136,12 +111,10 @@ struct ieee_ext {
  */
 #define	SNG_EXP_INFNAN	255
 #define	DBL_EXP_INFNAN	2047
-#define	EXT_EXP_INFNAN	32767
 
 #if 0
 #define	SNG_QUIETNAN	(1 << 22)
 #define	DBL_QUIETNAN	(1 << 19)
-#define	EXT_QUIETNAN	(1 << 15)
 #endif
 
 /*
@@ -149,4 +122,3 @@ struct ieee_ext {
  */
 #define	SNG_EXP_BIAS	127
 #define	DBL_EXP_BIAS	1023
-#define	EXT_EXP_BIAS	16383
