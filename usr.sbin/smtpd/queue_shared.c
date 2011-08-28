69d9e0965c735c86697427d3799fd970ddc12708
chl <chl@openbsd.org>
2011-08-28 22:20:27 +0000

    Clear bounce envelope structure, to avoid wrong use when bounce_record_message() returns 0.
    Initial report from me, tweak from gilles@
    
    ok gilles@

--- usr.sbin/smtpd/queue_shared.c
+++ usr.sbin/smtpd/queue_shared.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue_shared.c,v 1.49 2011/05/16 21:05:52 gilles Exp $	*/
+/*	$OpenBSD: queue_shared.c,v 1.50 2011/08/28 22:20:27 chl Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -62,6 +62,8 @@ int
 bounce_record_message(struct envelope *e, struct envelope *bounce)
 {
 	u_int32_t msgid;
+
+	bzero(bounce, sizeof(*bounce));
 
 	if (e->delivery.type == D_BOUNCE) {
 		log_debug("mailer daemons loop detected !");
