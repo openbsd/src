e6b2243fc6667ae17ee5dee84a6fe96787da91e7
yuo <yuo@openbsd.org>
2011-09-16 13:36:18 +0000

    add Vaisala's "USB instrument cable" entry

--- sys/dev/usb/uslcom.c
+++ sys/dev/usb/uslcom.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uslcom.c,v 1.23 2011/07/03 15:47:17 matthew Exp $	*/
+/*	$OpenBSD: uslcom.c,v 1.24 2011/09/16 13:36:18 yuo Exp $	*/
 
 /*
  * Copyright (c) 2006 Jonathan Gray <jsg@openbsd.org>
@@ -142,7 +142,8 @@ static const struct usb_devno uslcom_devs[] = {
 	{ USB_VENDOR_SILABS,		USB_PRODUCT_SILABS_USBWAVE12 },
 	{ USB_VENDOR_SILABS2,		USB_PRODUCT_SILABS2_DCU11CLONE },
 	{ USB_VENDOR_SILABS3,		USB_PRODUCT_SILABS3_GPRS_MODEM },
-	{ USB_VENDOR_USI,		USB_PRODUCT_USI_MC60 }
+	{ USB_VENDOR_USI,		USB_PRODUCT_USI_MC60 },
+	{ USB_VENDOR_VAISALA,		USB_PRODUCT_VAISALA_USBINSTCABLE }
 };
 
 int uslcom_match(struct device *, void *, void *); 
