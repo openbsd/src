e4e9858d335bdc7fb534a1549897026ded3a3847
miod <miod@openbsd.org>
2011-09-17 15:38:43 +0000

    Fix a truncated DPRINTF.

--- sys/dev/pcmcia/pcmcia.c
+++ sys/dev/pcmcia/pcmcia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcmcia.c,v 1.44 2011/07/03 15:47:17 matthew Exp $	*/
+/*	$OpenBSD: pcmcia.c,v 1.45 2011/09/17 15:38:43 miod Exp $	*/
 /*	$NetBSD: pcmcia.c,v 1.9 1998/08/13 02:10:55 eeh Exp $	*/
 
 /*
@@ -448,7 +448,8 @@ pcmcia_function_enable(pf)
 		/*
 		 * Don't do anything if we're already enabled.
 		 */
-		DPRINTF(("%s: pcmcia_function_enable on enabled func\n"));
+		DPRINTF(("%s: pcmcia_function_enable on enabled func\n",
+		    pf->sc->dev.dv_xname));
 		return (0);
 	}
 
