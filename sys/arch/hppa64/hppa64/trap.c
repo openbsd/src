52373dbb100ac0d8b593b111ed777d33fc5f397d
kettenis <kettenis@openbsd.org>
2011-09-19 08:49:19 +0000

    Remove (annoying) debug printf.

--- sys/arch/hppa64/hppa64/trap.c
+++ sys/arch/hppa64/hppa64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.22 2011/08/07 15:49:34 kettenis Exp $	*/
+/*	$OpenBSD: trap.c,v 1.23 2011/09/19 08:49:19 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -444,7 +444,6 @@ trap(int type, struct trapframe *frame)
 			break;
 		}
 
-printf("here\n");
 		ret = uvm_fault(map, trunc_page(va), fault, vftype);
 
 		/*
