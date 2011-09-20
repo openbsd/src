2cc3da1234fb695d0a6b95e5d7bf112e694bc92e
miod <miod@openbsd.org>
2011-09-20 15:56:11 +0000

    In find_barrier(), be less aggressive when deciding how many instructions can be put between two set of pc-relative loaded data if we are compiling PIC code; subsequent optimization passes are not as benign as what find_barrier() expects, leading to dreaded ``pcrel too far'' as(1) errors.

--- gnu/usr.bin/gcc/gcc/config/sh/sh.c
+++ gnu/usr.bin/gcc/gcc/config/sh/sh.c
@@ -2687,6 +2687,13 @@ find_barrier (num_mova, mova, from)
 
   si_limit = 1018;
   hi_limit = 510;
+#if defined(OPENBSD_NATIVE) || defined(OPENBSD_CROSS)
+  if (flag_pic)
+    {
+      si_limit -= 16;
+      hi_limit -= 16;
+    }
+#endif
 
   while (from && count_si < si_limit && count_hi < hi_limit)
     {
