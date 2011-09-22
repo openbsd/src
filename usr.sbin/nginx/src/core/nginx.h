d2bbe503305ff6b033a901010508488faf7e9c70
robert <robert@openbsd.org>
2011-09-22 23:38:25 +0000

    thank you CVS for missing the "core" directory

---
+++ usr.sbin/nginx/src/core/nginx.h
@@ -0,0 +1,19 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ */
+
+
+#ifndef _NGINX_H_INCLUDED_
+#define _NGINX_H_INCLUDED_
+
+
+#define nginx_version      1000006
+#define NGINX_VERSION      "1.0.6"
+#define NGINX_VER          "nginx/" NGINX_VERSION
+
+#define NGINX_VAR          "NGINX"
+#define NGX_OLDPID_EXT     ".oldbin"
+
+
+#endif /* _NGINX_H_INCLUDED_ */
