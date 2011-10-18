b55c44ac602063c09b62077acf79fdd101126753
matthew <matthew@openbsd.org>
2011-10-18 20:01:28 +0000

    Bump intercept_get_string() internal buffer to allow strings up to ARG_MAX long, so that ic_trargv can execute reliably.
    Thanks to Olivier Cherrier for reporting and helping to diagnose the
    problem.
    
    ok deraadt@

--- bin/systrace/intercept.c
+++ bin/systrace/intercept.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intercept.c,v 1.57 2011/09/18 23:24:14 matthew Exp $	*/
+/*	$OpenBSD: intercept.c,v 1.58 2011/10/18 20:01:28 matthew Exp $	*/
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
@@ -548,7 +548,7 @@ intercept_replace(int fd, pid_t pid, u_int16_t seqnr,
 char *
 intercept_get_string(int fd, pid_t pid, void *addr)
 {
-	static char name[8192];
+	static char name[ARG_MAX];
 	int off = 0, done = 0, stride;
 
 	if (addr == NULL)
