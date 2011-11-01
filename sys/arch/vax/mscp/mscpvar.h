675f646a5ba3442c90ee93c1af26839cfbde22b0
miod <miod@openbsd.org>
2011-11-01 20:50:11 +0000

    Remove orphaned comments.

--- sys/arch/vax/mscp/mscpvar.h
+++ sys/arch/vax/mscp/mscpvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mscpvar.h,v 1.6 2003/06/02 23:27:57 millert Exp $	*/
+/*	$OpenBSD: mscpvar.h,v 1.7 2011/11/01 20:50:11 miod Exp $	*/
 /*	$NetBSD: mscpvar.h,v 1.7 1999/06/06 19:16:18 ragge Exp $	*/
 /*
  * Copyright (c) 1996 Ludd, University of Lule}, Sweden.
@@ -210,8 +210,6 @@ struct mscp_softc {
  */
 #define MSCP_WAIT	1
 #define MSCP_DONTWAIT	0
-
-	/* get a command packet */
 
 /*
  * Unit flags
