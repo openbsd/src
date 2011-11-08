d4ed8d8fd744970fd4fdd64f4e19cc3434e8fc05
deraadt <deraadt@openbsd.org>
2011-11-08 12:21:29 +0000

    use -A with traceroute; ok sthen

--- usr.bin/bgplg/bgplg.h
+++ usr.bin/bgplg/bgplg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgplg.h,v 1.7 2011/07/04 18:11:53 claudio Exp $	*/
+/*	$OpenBSD: bgplg.h,v 1.8 2011/11/08 12:21:29 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Reyk Floeter <reyk@vantronix.net>
@@ -63,7 +63,7 @@ struct cmd {
 	    { BGPCTL, "show", "nexthop", NULL } },			\
 	{ "show version", 0, 0, NULL, { NULL }, lg_show_version },	\
 	{ "traceroute", 1, 1, "&lt;address&gt;",			\
-	    { TRACEROUTE, "-Sl", NULL } },				\
+	    { TRACEROUTE, "-ASl", NULL } },				\
 	{ "ping", 1, 1, "&lt;address&gt;",				\
 	    { PING, "-c4", "-w2", NULL } },				\
 	{ "traceroute6", 1, 1, "&lt;address&gt;",			\
