d50edf33c49fab12be24646f2716fa7ca8b4be60
jasper <jasper@openbsd.org>
2011-11-09 14:56:13 +0000

    openbsd will never support __ARMEB__, so remove it here
    ok deraadt@

--- gnu/usr.bin/cc/cc_tools/size-host.h
+++ gnu/usr.bin/cc/cc_tools/size-host.h
@@ -1,5 +1,5 @@
 /* $FreeBSD: src/gnu/usr.bin/cc/cc_tools/size-host.h,v 1.1.8.1 2009/04/15 03:14:26 kensmith Exp $ */
-/* $OpenBSD: size-host.h,v 1.3 2010/08/01 21:23:02 kettenis Exp $ */
+/* $OpenBSD: size-host.h,v 1.4 2011/11/09 14:56:13 jasper Exp $ */
 
 #ifdef	SIZEOF_INT
 # undef	SIZEOF_INT
@@ -37,6 +37,6 @@
 #undef WORDS_BIGENDIAN
 #endif
 
-#if defined(__sparc__) || defined(__sparc64__) || defined(__ARMEB__)
+#if defined(__sparc__) || defined(__sparc64__)
 #define WORDS_BIGENDIAN		1
 #endif
