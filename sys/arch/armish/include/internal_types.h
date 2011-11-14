12bf0fe1be6f6077cfee2c70e0c3406b6f857908
deraadt <deraadt@openbsd.org>
2011-11-14 14:30:38 +0000

    use include properly

--- sys/arch/armish/include/internal_types.h
+++ sys/arch/armish/include/internal_types.h
@@ -1,7 +1,7 @@
-/* $OpenBSD: internal_types.h,v 1.1 2006/05/29 17:13:19 drahn Exp $ */
+/* $OpenBSD: internal_types.h,v 1.2 2011/11/14 14:30:38 deraadt Exp $ */
 /* Public domain */
 #ifndef _MACHINE_INTERNAL_TYPES_H_
 #define _MACHINE_INTERNAL_TYPES_H_
 
-#include "arm/internal_types.h"
+#include <arm/internal_types.h>
 #endif
--- sys/arch/zaurus/include/internal_types.h
+++ sys/arch/zaurus/include/internal_types.h
@@ -1,7 +1,7 @@
-/* $OpenBSD: internal_types.h,v 1.1 2004/12/31 00:04:35 drahn Exp $ */
-/* Public domain */
+/* $OpenBSD: internal_types.h,v 1.2 2011/11/14 14:30:38 deraadt Exp $ */
+
 #ifndef _MACHINE_INTERNAL_TYPES_H_
 #define _MACHINE_INTERNAL_TYPES_H_
 
-#include "arm/internal_types.h"
+#include <arm/internal_types.h>
 #endif
