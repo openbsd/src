9f8aa9c4e5078d2aac9cc1dd859f824f426c8ef3
nicm <nicm@openbsd.org>
2011-12-01 20:42:31 +0000

    Make M-f and M-b work the same at the command prompt as in copy mode, pointed out by Romain Francoise.

--- usr.bin/tmux/status.c
+++ usr.bin/tmux/status.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: status.c,v 1.81 2011/11/15 23:34:12 nicm Exp $ */
+/* $OpenBSD: status.c,v 1.82 2011/12/01 20:42:31 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -1156,11 +1156,8 @@ status_prompt_key(struct client *c, int key)
 		/* Find the separator at the end of the word. */
 		while (c->prompt_index != size) {
 			c->prompt_index++;
-			if (strchr(wsep, c->prompt_buffer[c->prompt_index])) {
-				/* Go back to the word. */
-				c->prompt_index--;
+			if (strchr(wsep, c->prompt_buffer[c->prompt_index]))
 				break;
-			}
 		}
 
 		c->flags |= CLIENT_STATUS;
