7e02d87a9c611923f7eee87eea061930b86c12bc
schwarze <schwarze@openbsd.org>
2011-12-04 14:11:55 +0000

    Fix parsing of file names given on the command line; i broke it when adding support for formatted manual pages.

--- usr.bin/mandoc/mandocdb.c
+++ usr.bin/mandoc/mandocdb.c
@@ -1,4 +1,4 @@
-/*	$Id: mandocdb.c,v 1.17 2011/12/03 14:53:12 schwarze Exp $ */
+/*	$Id: mandocdb.c,v 1.18 2011/12/04 14:11:55 schwarze Exp $ */
 /*
  * Copyright (c) 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011 Ingo Schwarze <schwarze@openbsd.org>
@@ -1405,13 +1405,12 @@ ofile_argbuild(int argc, char *argv[], struct of **of)
 				*p = '\0';
 				continue;
 			}
-			if (strncmp("man", p + 1, 3)) {
+			if (0 == strncmp("man", p + 1, 3))
 				src_form |= MANDOC_SRC;
-				arch = p + 1;
-			} else if (strncmp("cat", p + 1, 3)) {
+			else if (0 == strncmp("cat", p + 1, 3))
 				src_form |= MANDOC_FORM;
+			else
 				arch = p + 1;
-			}
 			break;
 		}
 		if (NULL == title)
