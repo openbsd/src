fd171617623c7b9cb066b131922b2975afab3d1f
schwarze <schwarze@openbsd.org>
2011-12-09 00:53:10 +0000

    In default mode, use realpath(3) on the "dir" arguments and put canonicalized absolute pathnames into the database; from kristaps@.

--- usr.bin/mandoc/mandocdb.c
+++ usr.bin/mandoc/mandocdb.c
@@ -1,4 +1,4 @@
-/*	$Id: mandocdb.c,v 1.20 2011/12/08 02:14:00 schwarze Exp $ */
+/*	$Id: mandocdb.c,v 1.21 2011/12/09 00:53:10 schwarze Exp $ */
 /*
  * Copyright (c) 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011 Ingo Schwarze <schwarze@openbsd.org>
@@ -260,7 +260,9 @@ mandocdb(int argc, char *argv[])
 	struct manpaths	 dirs;
 	enum op		 op; /* current operation */
 	const char	*dir;
-	char		 ibuf[MAXPATHLEN], /* index fname */
+	char		*cp;
+	char		 pbuf[PATH_MAX],
+			 ibuf[MAXPATHLEN], /* index fname */
 			 fbuf[MAXPATHLEN];  /* btree fname */
 	int		 ch, i, flags;
 	DB		*idx, /* index database */
@@ -402,10 +404,15 @@ mandocdb(int argc, char *argv[])
 	 */
 
 	if (argc > 0) {
-		dirs.paths = mandoc_malloc(argc * sizeof(char *));
+		dirs.paths = mandoc_calloc(argc, sizeof(char *));
 		dirs.sz = argc;
-		for (i = 0; i < argc; i++)
-			dirs.paths[i] = mandoc_strdup(argv[i]);
+		for (i = 0; i < argc; i++) {
+			if (NULL == (cp = realpath(argv[i], pbuf))) {
+				perror(argv[i]);
+				goto out;
+			}
+			dirs.paths[i] = mandoc_strdup(cp);
+		}
 	} else
 		manpath_parse(&dirs, NULL, NULL);
 
