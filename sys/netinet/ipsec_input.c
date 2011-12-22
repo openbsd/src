a49e5cf1607f48c8141f372ef2935ab4edba8ee8
sperreault <sperreault@openbsd.org>
2011-12-22 13:36:06 +0000

    Fix RFC reference section
    spotted by bluhm@, ok yasuoka@

--- sys/netinet/ipsec_input.c
+++ sys/netinet/ipsec_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ipsec_input.c,v 1.105 2011/12/21 14:53:26 sperreault Exp $	*/
+/*	$OpenBSD: ipsec_input.c,v 1.106 2011/12/22 13:36:06 sperreault Exp $	*/
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -558,7 +558,7 @@ ipsec_common_input_cb(struct mbuf *m, struct tdb *tdbp, int skip, int protoff,
 
 	/*
 	 * Fix TCP/UDP checksum of UDP encapsulated transport mode ESP packet.
-	 * (RFC3948 3.1.1)
+	 * (RFC3948 3.1.2)
 	 */
 	if ((af == AF_INET || af == AF_INET6) &&
 	    (tdbp->tdb_flags & TDBF_UDPENCAP) &&
