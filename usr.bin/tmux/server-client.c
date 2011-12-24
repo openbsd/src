a9613af05773d23e8ff0cf0a451571fc2c51efd7
nicm <nicm@openbsd.org>
2011-12-24 08:26:59 +0000

    Fix so that when mouse-select-pane and mouse-select-window are both enabled, clicking on the status line does not change the current pane. From Romain Francoise.

--- usr.bin/tmux/server-client.c
+++ usr.bin/tmux/server-client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server-client.c,v 1.61 2011/08/20 20:37:30 nicm Exp $ */
+/* $OpenBSD: server-client.c,v 1.62 2011/12/24 08:26:59 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -320,6 +320,8 @@ server_client_handle_key(int key, struct mouse_event *mouse, void *data)
 		if (c->flags & CLIENT_READONLY)
 			return;
 		if (options_get_number(oo, "mouse-select-pane") &&
+		    (!(options_get_number(oo, "status") &&
+		       mouse->y + 1 == c->tty.sy)) &&
 		    ((!(mouse->b & MOUSE_DRAG) && mouse->b != MOUSE_UP) ||
 		    wp->mode != &window_copy_mode)) {
 			/*
