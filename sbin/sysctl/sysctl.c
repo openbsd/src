2b4e548bdeaaa8efbb71568517a5f2ccc89f51b0
dhill <dhill@openbsd.org>
2012-01-15 16:00:50 +0000

    add missing sysctl_pipex declaration
    ok yasuoka

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysctl.c,v 1.180 2011/09/16 20:52:48 yuo Exp $	*/
+/*	$OpenBSD: sysctl.c,v 1.181 2012/01/15 16:00:50 dhill Exp $	*/
 /*	$NetBSD: sysctl.c,v 1.9 1995/09/30 07:12:50 thorpej Exp $	*/
 
 /*
@@ -192,6 +192,7 @@ int sysctl_inet6(char *, char **, int *, int, int *);
 #endif
 int sysctl_bpf(char *, char **, int *, int, int *);
 int sysctl_mpls(char *, char **, int *, int, int *);
+int sysctl_pipex(char *, char **, int *, int, int *);
 int sysctl_fs(char *, char **, int *, int, int *);
 static int sysctl_vfs(char *, char **, int[], int, int *);
 static int sysctl_vfsgen(char *, char **, int[], int, int *);
