6e67a9254cd6d0b6846551cb0bd464ed03253eb4
deraadt <deraadt@openbsd.org>
2012-02-07 17:30:00 +0000

    move out of -beta

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.116 2012/01/12 00:35:59 sthen Exp $
+#	$OpenBSD: newvers.sh,v 1.117 2012/02/07 17:30:00 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,9 +64,9 @@ ost="OpenBSD"
 osr="5.1"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
 #endif
 
