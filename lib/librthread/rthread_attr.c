6918e75c4e8ee4d998959013260fd7c24a12a35e
guenther <guenther@openbsd.org>
2012-02-18 21:50:27 +0000

    Before using _thread_pagesize, make sure _rthread_init() has been called

--- lib/librthread/rthread_attr.c
+++ lib/librthread/rthread_attr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rthread_attr.c,v 1.12 2012/02/18 21:12:09 guenther Exp $ */
+/*	$OpenBSD: rthread_attr.c,v 1.13 2012/02/18 21:50:27 guenther Exp $ */
 /*
  * Copyright (c) 2004,2005 Ted Unangst <tedu@openbsd.org>
  * All Rights Reserved.
@@ -144,6 +144,8 @@ pthread_attr_getstacksize(const pthread_attr_t *attrp, size_t *stacksize)
 int
 pthread_attr_setstacksize(pthread_attr_t *attrp, size_t stacksize)
 {
+	if (!_threads_ready)
+		_rthread_init();
 	if (stacksize < PTHREAD_STACK_MIN ||
 	    stacksize > ROUND_TO_PAGE(stacksize))
 		return (EINVAL);
@@ -163,6 +165,8 @@ pthread_attr_getstackaddr(const pthread_attr_t *attrp, void **stackaddr)
 int
 pthread_attr_setstackaddr(pthread_attr_t *attrp, void *stackaddr)
 {
+	if (!_threads_ready)
+		_rthread_init();
 	if (stackaddr == NULL || (uintptr_t)stackaddr & (_thread_pagesize - 1))
 		return EINVAL;
 	(*attrp)->stack_addr = stackaddr;
