ae7eb20509475c2503cc42d73a6e60fb34764540
deraadt <deraadt@openbsd.org>
2012-03-26 04:19:55 +0000

    clear junk in p_comm before copying shorter names into it ok jsing guenther miod

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exec.c,v 1.126 2012/03/23 15:51:26 guenther Exp $	*/
+/*	$OpenBSD: kern_exec.c,v 1.127 2012/03/26 04:19:55 deraadt Exp $	*/
 /*	$NetBSD: kern_exec.c,v 1.75 1996/02/09 18:59:28 christos Exp $	*/
 
 /*-
@@ -472,9 +472,9 @@ sys_execve(struct proc *p, void *v, register_t *retval)
 	TCB_SET(p, NULL);	/* reset the TCB address */
 
 	/* set command name & other accounting info */
+	bzero(p->p_comm, sizeof(p->p_comm));
 	len = min(nid.ni_cnd.cn_namelen, MAXCOMLEN);
 	bcopy(nid.ni_cnd.cn_nameptr, p->p_comm, len);
-	p->p_comm[len] = 0;
 	p->p_acflag &= ~AFORK;
 
 	/* record proc's vnode, for use by procfs and others */
