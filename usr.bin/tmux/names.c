91e805305873c59ade38b8c0eccf06a1cfa511ad
nicm <nicm@openbsd.org>
2012-04-11 07:45:30 +0000

    Turn automatic-rename off properly if turned off by renaming a window. Reported by Romain Francoise.

--- usr.bin/tmux/names.c
+++ usr.bin/tmux/names.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: names.c,v 1.14 2012/04/08 06:47:26 nicm Exp $ */
+/* $OpenBSD: names.c,v 1.15 2012/04/11 07:45:30 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -49,7 +49,12 @@ window_name_callback(unused int fd, unused short events, void *data)
 	struct window	*w = data;
 	char		*name, *wname;
 
-	queue_window_name(w); /* stopped when option turned off */
+	if (!options_get_number(&w->options, "automatic-rename")) {
+		if (event_initialized(&w->name_timer))
+			event_del(&w->name_timer);
+		return;
+	}
+	queue_window_name(w);
 
 	if (w->active->screen != &w->active->base)
 		name = NULL;
