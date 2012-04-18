b56f4376216beaffb0c52d56018e21daafe3a86a
miod <miod@openbsd.org>
2012-04-18 17:19:07 +0000

    regen

--- sys/arch/sgi/gio/giodevs.h
+++ sys/arch/sgi/gio/giodevs.h
@@ -2,25 +2,26 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: giodevs,v 1.1 2012/03/28 20:44:23 miod Exp 
+ *	OpenBSD: giodevs,v 1.2 2012/04/18 17:18:50 miod Exp 
  */
 
 
-#define XPI	0x01	/* XPI low cost FDDI */
-#define GTR	0x02	/* GTR TokenRing */
-#define ISDN	0x04	/* Synchronous ISDN */
-#define CANON	0x06	/* Canon Interface */
-#define JPEG_D	0x08	/* JPEG (Double Wide) */
-#define JPEG_S	0x09	/* JPEG (Single Wide) */
-#define XPI_M0	0x0a	/* XPI mez. FDDI device 0 */
-#define XPI_M1	0x0b	/* XPI mez. FDDI device 1 */
-#define EP	0x0e	/* E-Plex 8-port Ethernet */
-#define IVAS	0x30	/* Lyon Lamb IVAS */
-#define PHOBOS_G160	0x35	/* Phobos G160 10/100 Ethernet */
-#define PHOBOS_G130	0x36	/* Phobos G130 10/100 Ethernet */
-#define PHOBOS_G100	0x37	/* Phobos G100 100baseTX Fast Ethernet */
-#define SETENG_GFE	0x38	/* Set Engineering GFE 10/100 Ethernet */
-#define ATM	0x85	/* ATM board */
-#define SCSI	0x87	/* 16 bit SCSI Card */
-#define SMPTE	0x8c	/* SMPTE 259M Video */
-#define BABBLE	0x8d	/* Babblefish Compression */
+#define GIO_PRODUCT_XPI	0x01	/* XPI low cost FDDI */
+#define GIO_PRODUCT_GTR	0x02	/* GTR TokenRing */
+#define GIO_PRODUCT_ISDN	0x04	/* Synchronous ISDN */
+#define GIO_PRODUCT_CANON	0x06	/* Canon Interface */
+#define GIO_PRODUCT_JPEG_D	0x08	/* JPEG (Double Wide) */
+#define GIO_PRODUCT_JPEG_S	0x09	/* JPEG (Single Wide) */
+#define GIO_PRODUCT_XPI_M0	0x0a	/* XPI mez. FDDI device 0 */
+#define GIO_PRODUCT_XPI_M1	0x0b	/* XPI mez. FDDI device 1 */
+#define GIO_PRODUCT_EP	0x0e	/* E-Plex 8-port Ethernet */
+#define GIO_PRODUCT_IMPACT	0x10	/* Impact */
+#define GIO_PRODUCT_IVAS	0x30	/* Lyon Lamb IVAS */
+#define GIO_PRODUCT_PHOBOS_G160	0x35	/* Phobos G160 10/100 Ethernet */
+#define GIO_PRODUCT_PHOBOS_G130	0x36	/* Phobos G130 10/100 Ethernet */
+#define GIO_PRODUCT_PHOBOS_G100	0x37	/* Phobos G100 100baseTX Fast Ethernet */
+#define GIO_PRODUCT_SETENG_GFE	0x38	/* Set Engineering GFE 10/100 Ethernet */
+#define GIO_PRODUCT_ATM	0x85	/* ATM board */
+#define GIO_PRODUCT_SCSI	0x87	/* 16 bit SCSI Card */
+#define GIO_PRODUCT_SMPTE	0x8c	/* SMPTE 259M Video */
+#define GIO_PRODUCT_BABBLE	0x8d	/* Babblefish Compression */
--- sys/arch/sgi/gio/giodevs_data.h
+++ sys/arch/sgi/gio/giodevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: giodevs,v 1.1 2012/03/28 20:44:23 miod Exp 
+ *	OpenBSD: giodevs,v 1.2 2012/04/18 17:18:50 miod Exp 
  */
 
 /*	$NetBSD: giodevs,v 1.8 2007/02/19 04:46:33 rumble Exp $	*/
@@ -14,23 +14,24 @@ struct gio_knowndev {
 };
 
 struct gio_knowndev gio_knowndevs[] = {
-	{ 0x01, "XPI low cost FDDI" },
-	{ 0x02, "GTR TokenRing" },
-	{ 0x04, "Synchronous ISDN" },
-	{ 0x06, "Canon Interface" },
-	{ 0x08, "JPEG (Double Wide)" },
-	{ 0x09, "JPEG (Single Wide)" },
-	{ 0x0a, "XPI mez. FDDI device 0" },
-	{ 0x0b, "XPI mez. FDDI device 1" },
-	{ 0x0e, "E-Plex 8-port Ethernet" },
-	{ 0x30, "Lyon Lamb IVAS" },
-	{ 0x35, "Phobos G160 10/100 Ethernet" },
-	{ 0x36, "Phobos G130 10/100 Ethernet" },
-	{ 0x37, "Phobos G100 100baseTX Fast Ethernet" },
-	{ 0x38, "Set Engineering GFE 10/100 Ethernet" },
-	{ 0x85, "ATM board" },
-	{ 0x87, "16 bit SCSI Card" },
-	{ 0x8c, "SMPTE 259M Video" },
-	{ 0x8d, "Babblefish Compression" },
+	{ GIO_PRODUCT_XPI, "XPI low cost FDDI" },
+	{ GIO_PRODUCT_GTR, "GTR TokenRing" },
+	{ GIO_PRODUCT_ISDN, "Synchronous ISDN" },
+	{ GIO_PRODUCT_CANON, "Canon Interface" },
+	{ GIO_PRODUCT_JPEG_D, "JPEG (Double Wide)" },
+	{ GIO_PRODUCT_JPEG_S, "JPEG (Single Wide)" },
+	{ GIO_PRODUCT_XPI_M0, "XPI mez. FDDI device 0" },
+	{ GIO_PRODUCT_XPI_M1, "XPI mez. FDDI device 1" },
+	{ GIO_PRODUCT_EP, "E-Plex 8-port Ethernet" },
+	{ GIO_PRODUCT_IMPACT, "Impact" },
+	{ GIO_PRODUCT_IVAS, "Lyon Lamb IVAS" },
+	{ GIO_PRODUCT_PHOBOS_G160, "Phobos G160 10/100 Ethernet" },
+	{ GIO_PRODUCT_PHOBOS_G130, "Phobos G130 10/100 Ethernet" },
+	{ GIO_PRODUCT_PHOBOS_G100, "Phobos G100 100baseTX Fast Ethernet" },
+	{ GIO_PRODUCT_SETENG_GFE, "Set Engineering GFE 10/100 Ethernet" },
+	{ GIO_PRODUCT_ATM, "ATM board" },
+	{ GIO_PRODUCT_SCSI, "16 bit SCSI Card" },
+	{ GIO_PRODUCT_SMPTE, "SMPTE 259M Video" },
+	{ GIO_PRODUCT_BABBLE, "Babblefish Compression" },
 	{ 0, NULL }
 };
