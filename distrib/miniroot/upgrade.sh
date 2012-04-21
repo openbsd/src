4cd3b9a913dbc0edd97b3b7b5cd0e76e82409c1f
henning <henning@openbsd.org>
2012-04-21 10:17:26 +0000

    remove the "Do you want to do any manual network configuration?" question. there is barely ever a need to do so these days, I for one don't remember ever using it, and we have a well documented way of escaping to a shell at any point in the install process, which the installer conveniently points out right at the beginning. stumbled upon while reviewing the upcoming 2nd edition of Michael Lucas' "Absolute OpenBSD" ok sthen deraadt halex krw

--- distrib/miniroot/upgrade.sh
+++ distrib/miniroot/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: upgrade.sh,v 1.71 2011/07/06 21:12:05 ajacoutot Exp $
+#	$OpenBSD: upgrade.sh,v 1.72 2012/04/21 10:17:26 henning Exp $
 #	$NetBSD: upgrade.sh,v 1.2.4.5 1996/08/27 18:15:08 gwr Exp $
 #
 # Copyright (c) 1997-2009 Todd Miller, Theo de Raadt, Ken Westerback
@@ -71,7 +71,6 @@ THESETS="$THESETS site$VERSION-$(hostname -s).tgz"
 
 # Configure the network.
 enable_network
-manual_net_cfg
 
 startftplist
 
