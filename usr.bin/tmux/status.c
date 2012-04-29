7c5e5a5e86ff8c99f40063f4a16a0c08006e9e8e
nicm <nicm@openbsd.org>
2012-04-29 07:33:41 +0000

    Use int not u_char for colours from options since they may have bit 8 set to mark them as 256-colour. Reported by Chris Johnson.

--- usr.bin/tmux/status.c
+++ usr.bin/tmux/status.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: status.c,v 1.91 2012/04/23 22:23:14 nicm Exp $ */
+/* $OpenBSD: status.c,v 1.92 2012/04/29 07:33:41 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -81,7 +81,7 @@ status_redraw_get_left(struct client *c,
 {
 	struct session	*s = c->session;
 	char		*left;
-	u_char		 fg, bg, attr;
+	int		 fg, bg, attr;
 	size_t		 leftlen;
 
 	fg = options_get_number(&s->options, "status-left-fg");
@@ -111,7 +111,7 @@ status_redraw_get_right(struct client *c,
 {
 	struct session	*s = c->session;
 	char		*right;
-	u_char		 fg, bg, attr;
+	int		 fg, bg, attr;
 	size_t		 rightlen;
 
 	fg = options_get_number(&s->options, "status-right-fg");
@@ -683,7 +683,7 @@ status_print(
 	struct session	*s = c->session;
 	const char	*fmt;
 	char   		*text;
-	u_char		 fg, bg, attr;
+	int		 fg, bg, attr;
 
 	fg = options_get_number(oo, "window-status-fg");
 	if (fg != 8)
