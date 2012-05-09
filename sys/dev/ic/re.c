fd5365304415d9ac31ff5c4205de95e22033316c
jsg <jsg@openbsd.org>
2012-05-09 13:30:12 +0000

    8401E/8402/8105E/8105E_SPIN1 don't support jumbo frames from Brad

--- sys/dev/ic/re.c
+++ sys/dev/ic/re.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: re.c,v 1.138 2012/05/09 13:14:35 jsg Exp $	*/
+/*	$OpenBSD: re.c,v 1.139 2012/05/09 13:30:12 jsg Exp $	*/
 /*	$FreeBSD: if_re.c,v 1.31 2004/09/04 07:54:05 ru Exp $	*/
 /*
  * Copyright (c) 1997, 1998-2003
@@ -849,7 +849,8 @@ re_attach(struct rl_softc *sc, const char *intrstr)
 	case RL_HWREV_8105E_SPIN1:
 		sc->rl_flags |= RL_FLAG_INVMAR | RL_FLAG_PHYWAKE |
 		    RL_FLAG_PHYWAKE_PM | RL_FLAG_PAR | RL_FLAG_DESCV2 |
-		    RL_FLAG_MACSTAT | RL_FLAG_CMDSTOP | RL_FLAG_AUTOPAD;
+		    RL_FLAG_MACSTAT | RL_FLAG_CMDSTOP | RL_FLAG_AUTOPAD |
+		    RL_FLAG_NOJUMBO;
 		break;
 	case RL_HWREV_8168_SPIN1:
 	case RL_HWREV_8168_SPIN2:
