c9a384ac3d9e10a619daccd72a93454e65785535
miod <miod@openbsd.org>
2012-05-15 18:17:50 +0000

    revert a chunk introduced in 1.7 which was not supposed to be there yet

--- sys/arch/sgi/sgi/ip22_machdep.c
+++ sys/arch/sgi/sgi/ip22_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip22_machdep.c,v 1.7 2012/05/12 16:48:27 miod Exp $	*/
+/*	$OpenBSD: ip22_machdep.c,v 1.8 2012/05/15 18:17:50 miod Exp $	*/
 
 /*
  * Copyright (c) 2012 Miodrag Vallat.
@@ -101,7 +101,8 @@ ip22_arcbios_walk_component(arc_config_t *cf)
 		 * SS is Log2(cache size in 4KB units) (should be 0007)
 		 */
 		ci->ci_l2size = (1 << 12) << (key & 0x0000ffff);
-		ci->ci_l2line = 1 << ((key >> 16) & 0xff);
+		/* L2 line size */
+		ci->ci_cacheconfiguration = 1 << ((key >> 16) & 0xff);
 
 		ip22_arcwalk_results |= IP22_HAS_L2;
 	}
