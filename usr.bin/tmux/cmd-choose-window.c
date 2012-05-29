a5c2646b9bb6cea29eae94a2fb08142131540a01
nicm <nicm@openbsd.org>
2012-05-29 08:15:45 +0000

    Add pane title to choose-window template and use the right index for the pane, from Romain Francoise.

--- usr.bin/tmux/cmd-choose-window.c
+++ usr.bin/tmux/cmd-choose-window.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-choose-window.c,v 1.19 2012/05/22 11:35:37 nicm Exp $ */
+/* $OpenBSD: cmd-choose-window.c,v 1.20 2012/05/29 08:15:45 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -72,7 +72,7 @@ cmd_choose_window_exec(struct cmd *self, struct cmd_ctx *ctx)
 		return (0);
 
 	if ((template = args_get(args, 'F')) == NULL)
-		template = DEFAULT_WINDOW_TEMPLATE;
+		template = DEFAULT_WINDOW_TEMPLATE " \"#{pane_title}\"";
 
 	cur = idx = 0;
 	RB_FOREACH(wm, winlinks, &s->windows) {
@@ -84,9 +84,10 @@ cmd_choose_window_exec(struct cmd *self, struct cmd_ctx *ctx)
 		format_add(ft, "line", "%u", idx);
 		format_session(ft, s);
 		format_winlink(ft, s, wm);
+		format_window_pane(ft, wm->window->active);
 
 		line = format_expand(ft, template);
-		window_choose_add(wl->window->active, idx, "%s", line);
+		window_choose_add(wl->window->active, wm->idx, "%s", line);
 
 		xfree(line);
 		format_free(ft);
