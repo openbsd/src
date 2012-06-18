e61e2d342dd0ab2734395ac00d353560d5caac89
chl <chl@openbsd.org>
2012-06-18 10:21:16 +0000

    fix potential use of uninitialized variable.
    found with valgrind on -portable.
    
    ok gilles@

--- usr.sbin/smtpd/queue.c
+++ usr.sbin/smtpd/queue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue.c,v 1.117 2012/01/28 11:33:07 gilles Exp $	*/
+/*	$OpenBSD: queue.c,v 1.118 2012/06/18 10:21:16 chl Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -74,6 +74,7 @@ queue_imsg(struct imsgev *iev, struct imsg *imsg)
 
 		case IMSG_QUEUE_COMMIT_MESSAGE:
 			ss.id = e->session_id;
+			ss.code = 250;
 			if (queue_message_commit(Q_INCOMING, evpid_to_msgid(e->id)))
 				stat_increment(e->flags & DF_ENQUEUED ?
 				    STATS_QUEUE_LOCAL : STATS_QUEUE_REMOTE);
