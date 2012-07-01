13cd5609c6ac68fb725d20d404e5118283d0e64a
sthen <sthen@openbsd.org>
2012-07-01 11:55:13 +0000

    typo in comment, s/withdrawl/withdrawal/

--- usr.sbin/bgpd/rde_rib.c
+++ usr.sbin/bgpd/rde_rib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_rib.c,v 1.132 2012/05/22 20:44:06 claudio Exp $ */
+/*	$OpenBSD: rde_rib.c,v 1.133 2012/07/01 11:55:13 sthen Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Claudio Jeker <claudio@openbsd.org>
@@ -874,7 +874,7 @@ prefix_updateall(struct rde_aspath *asp, enum nexthop_state state,
 		 * If the prefix is the active one remove it first,
 		 * this has to be done because we can not detect when
 		 * the active prefix changes its state. In this case
-		 * we know that this is a withdrawl and so the second
+		 * we know that this is a withdrawal and so the second
 		 * prefix_evaluate() will generate no update because
 		 * the nexthop is unreachable or ineligible.
 		 */
