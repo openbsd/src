70fc49e38e5b05687f88c0db9294a1d3063b03b5
dtucker <dtucker@openbsd.org>
2012-07-02 14:37:06 +0000

    remove exit from end of test since it prevents reporting failure

--- regress/usr.bin/ssh/connect-privsep.sh
+++ regress/usr.bin/ssh/connect-privsep.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: connect-privsep.sh,v 1.3 2012/06/26 12:06:59 dtucker Exp $
+#	$OpenBSD: connect-privsep.sh,v 1.4 2012/07/02 14:37:06 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="proxy connect with privsep"
@@ -33,5 +33,3 @@ for m in '' A F G H J P R S X Z '<' '>'; do
 	fi
     done
 done
-
-exit
