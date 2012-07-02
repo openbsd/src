fb3ad1b2ffa1a7c9434575a745855c1272cc9d1a
lum <lum@openbsd.org>
2012-07-02 08:08:31 +0000

    Maintain mg's Public Domain license heritage.
    Sunil Nimmagadda agrees.

--- usr.bin/mg/cscope.c
+++ usr.bin/mg/cscope.c
@@ -1,19 +1,9 @@
-/*	$OpenBSD: cscope.c,v 1.2 2012/06/26 16:59:23 jasper Exp $	*/
+/*	$OpenBSD: cscope.c,v 1.3 2012/07/02 08:08:31 lum Exp $	*/
 
 /*
- * Copyright (c) 2012 Sunil Nimmagadda <sunil@sunilnimmagadda.com>
+ * This file is in the public domain.
  *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * Author: Sunil Nimmagadda <sunil@sunilnimmagadda.com>
  */
 
 #include <sys/types.h>
