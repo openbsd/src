7690362d6c939f38efd8d167f80bd7910a6b8b5e
ratchov <ratchov@openbsd.org>
2012-07-08 17:55:50 +0000

    Include sys/types.h rather than sys/param.h to pull BYTE_ORDER macros. suggested by deraadt@

--- include/sndio.h
+++ include/sndio.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sndio.h,v 1.6 2012/05/23 19:25:11 ratchov Exp $	*/
+/*	$OpenBSD: sndio.h,v 1.7 2012/07/08 17:55:50 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -17,7 +17,7 @@
 #ifndef SNDIO_H
 #define SNDIO_H
 
-#include <sys/param.h>
+#include <sys/types.h>
 
 /*
  * default audio device and MIDI port
