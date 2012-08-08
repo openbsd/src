38be147e09c89cb39bf75bfbd1224069b2cafed2
jsg <jsg@openbsd.org>
2012-08-08 12:05:22 +0000

    remove mac68k leftovers ok miod@ mpi@ deraadt@ martin@

--- sbin/pdisk/pdisk.c
+++ sbin/pdisk/pdisk.c
@@ -102,9 +102,6 @@ static int first_get = 1;
 // Forward declarations
 //
 void do_change_map_size(partition_map_header *map);
-#ifdef __m68k__
-void do_update_dpme(partition_map *entry);
-#endif
 void do_create_partition(partition_map_header *map, int get_type);
 void do_delete_partition(partition_map_header *map);
 void do_display_block(partition_map_header *map, char *alt_name);
@@ -526,28 +523,6 @@ finis:
     close_partition_map(map);
 }
 
-#ifdef __m68k__
-void
-do_update_dpme(partition_map *entry)
-{
-    int slice = 0;
-    if (!entry) return;
-    dpme_init_flags(entry->data);
-    entry->HFS_name = get_HFS_name(entry, &entry->HFS_kind);
-    if (istrncmp(entry->data->dpme_type, kUnixType, DPISTRLEN) == 0) {
-	printf("Available partition slices for %s:\n",entry->data->dpme_type);
-	printf("  a   root partition\n");
-	printf("  b   swap partition\n");
-	printf("  c   do not set any bzb bits\n");
-	printf("  g   user partition\n");
-	printf("Other lettered values will create user partitions\n");
-	get_command("Select a slice for default bzb values: ",0,&slice);
-    }
-    bzb_init_slice((BZB *)entry->data->dpme_bzb,slice);
-    entry->the_map->changed = 1;
-}
-#endif
-
 void
 do_create_partition(partition_map_header *map, int get_type)
 {
@@ -591,9 +566,6 @@ do_create_partition(partition_map_header *map, int get_type)
 	}
 	add_partition_to_map(name, type_name, base, length, map);
     }
-#ifdef __m68k__
-    do_update_dpme(find_entry_by_base(base,map));
-#endif
 xit2:
     if (type_name)
         free(type_name);
@@ -731,9 +703,6 @@ do_change_type(partition_map_header *map)
     }
 
     strncpy(entry->data->dpme_type, type, DPISTRLEN);
-#ifdef __m68k__
-    do_update_dpme(entry);
-#endif
     map->changed = 1;
 
 out:
