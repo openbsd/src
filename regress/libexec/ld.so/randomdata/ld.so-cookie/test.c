b113c0bf3ac8e642fa6e14ffc88183c98d5edd35
matthew <matthew@openbsd.org>
2012-08-17 15:11:12 +0000

    Double check that we're not testing libc's __guard.  Changing ld.so's __guard symbol to HIDDEN causes dlsym() to return a pointer to libc's __guard instead, which can cause the test to wrongly pass even if ld.so's random data segments are not being initialized properly.

--- regress/libexec/ld.so/randomdata/ld.so-cookie/test.c
+++ regress/libexec/ld.so/randomdata/ld.so-cookie/test.c
@@ -8,11 +8,13 @@ main()
 	void *dso;
 	long *guardptr;
 	long guard;
+	extern long __guard[];
 
 	dso = dlopen("ld.so", RTLD_LOCAL|RTLD_LAZY);
 	assert(dso != NULL);
 	guardptr = dlsym(dso, "__guard");
 	assert(guardptr != NULL);
+	assert(guardptr != &__guard[0]);
 
 	guard = *guardptr;
 	assert(guard != 0);
