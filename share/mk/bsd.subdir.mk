7df21d541d895f3227f1721e51fce8492f4edf49
espie <espie@openbsd.org>
2012-08-18 10:36:51 +0000

    allow other targets to work like install -> realinstall (to be used in xenocara for build)
    okay matthieu@

--- share/mk/bsd.subdir.mk
+++ share/mk/bsd.subdir.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.subdir.mk,v 1.17 2012/04/08 15:56:28 jsg Exp $
+#	$OpenBSD: bsd.subdir.mk,v 1.18 2012/08/18 10:36:51 espie Exp $
 #	$NetBSD: bsd.subdir.mk,v 1.11 1996/04/04 02:05:06 jtc Exp $
 #	@(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
 
@@ -43,7 +43,7 @@ _SUBDIRUSE: .USE
 			exec ${MAKE} SKIPDIR="$${subskipdir}" \
 			    $${_makefile_spec_} _THISDIR_="$${_nextdir_}" \
 			    ${MAKE_FLAGS} \
-			    ${.TARGET:S/realinstall/install/:S/subdirdepend/depend/}; \
+			    ${.TARGET:S/^real//:S/subdirdepend/depend/}; \
 		fi); \
 	done
 
