f20666721f3e5d90e525c3cd4c516ff481ab89f9
jsg <jsg@openbsd.org>
2012-08-22 14:39:43 +0000

    recognise rrx mnemonic, from GPLv2 apple cctools via FreeBSD ok miod@

--- gnu/usr.bin/binutils-2.17/gas/config/tc-arm.c
+++ gnu/usr.bin/binutils-2.17/gas/config/tc-arm.c
@@ -7644,6 +7644,13 @@ do_t_rbit (void)
 }
 
 static void
+do_t_rd_rm (void)
+{
+  inst.instruction |= inst.operands[0].reg << 8;
+  inst.instruction |= inst.operands[1].reg;
+}
+
+static void
 do_t_rev (void)
 {
   if (inst.operands[0].reg <= 7 && inst.operands[1].reg <= 7
@@ -9333,6 +9340,9 @@ static const struct asm_opcode insns[] =
 #define THUMB_VARIANT &arm_ext_v6_notm
  TCE(ldrexd,	1b00f9f, e8d0007f, 3, (RRnpc, oRRnpc, RRnpcb),        ldrexd, t_ldrexd),
  TCE(strexd,	1a00f90, e8c00070, 4, (RRnpc, RRnpc, oRRnpc, RRnpcb), strexd, t_strexd),
+
+ TCE(rrx,      1a00060, ea4f0030, 2, (RR, RR),      rd_rm, t_rd_rm),
+ TCE(rrxs,     1b00060, ea5f0030, 2, (RR, RR),      rd_rm, t_rd_rm),
 
 #undef THUMB_VARIANT
 #define THUMB_VARIANT &arm_ext_v6t2
