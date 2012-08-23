341df58dac89086036044214ab1fa463800a7616
deraadt <deraadt@openbsd.org>
2012-08-23 06:19:48 +0000

    no more nnpfspioctl() system call; ok guenther

--- lib/libc/sys/makelintstub.sh
+++ lib/libc/sys/makelintstub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -
-#	$OpenBSD: makelintstub.sh,v 1.9 2010/07/24 23:32:52 guenther Exp $
+#	$OpenBSD: makelintstub.sh,v 1.10 2012/08/23 06:19:48 deraadt Exp $
 #	$NetBSD: makelintstub,v 1.2 1997/11/05 05:46:18 thorpej Exp $
 #
 # Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
@@ -63,7 +63,6 @@ header()
 	#include <sys/ktrace.h>
 	#include <sys/mman.h>
 	#include <sys/event.h>
-	#include <nnpfs/nnpfs_pioctl.h>
 	#include <sys/wait.h>
 	#include <stdio.h>
 	#undef DIRBLKSIZ
