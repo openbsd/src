76ac2d199133f05296b0a39fab70b12197282a56
matthew <matthew@openbsd.org>
2012-08-31 16:37:14 +0000

    Switch GCC to use __guard_local instead of __guard.  This allows GCC to emit -fstack-protector code that doesn't need GOT indirection for accessing __guard.
    Tested on amd64, i386, sparc64, hppa, loongson, and sgi (thanks lteo,
    djm, sthen, todd, naddy, kettenis, phessler, jasper, and anyone else
    that I'm missing who tested).

--- gnu/gcc/gcc/targhooks.c
+++ gnu/gcc/gcc/targhooks.c
@@ -372,7 +372,7 @@ default_stack_protect_guard (void)
 
   if (t == NULL)
     {
-      t = build_decl (VAR_DECL, get_identifier ("__guard"), ptr_type_node);
+      t = build_decl (VAR_DECL, get_identifier ("__guard_local"), ptr_type_node);
       TREE_STATIC (t) = 1;
       TREE_PUBLIC (t) = 1;
       DECL_EXTERNAL (t) = 1;
@@ -380,6 +380,8 @@ default_stack_protect_guard (void)
       TREE_THIS_VOLATILE (t) = 1;
       DECL_ARTIFICIAL (t) = 1;
       DECL_IGNORED_P (t) = 1;
+      DECL_VISIBILITY (t) = VISIBILITY_HIDDEN;
+      DECL_VISIBILITY_SPECIFIED (t) = 1;
 
       stack_chk_guard_decl = t;
     }
