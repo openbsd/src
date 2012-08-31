efe83d5a654cfb1159bbc7c075b1a5481a6f4c9d
pascal <pascal@openbsd.org>
2012-08-31 17:16:21 +0000

    Enable PIE on amd64, mips64(el) and sparc64.
    Most work done by kurt@ some years ago; also, most work committed over the last
    weeks was based on a diff by him.  Cheers, kurt@!
    
    Lots and lots of helpful input, comments, feedback and diffs from deraadt@,
    kettenis@ and matthew@, miod@ and others.  Thanks!
    
    Bulk builds done by naddy@, sthen@, landry@ and in progress from pea@ and
    jasper@.
    
    Pending architectures are alpha, i386, hppa, arm, sparc and landisk.
    
    ok deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.124 2012/08/28 16:45:03 pascal Exp $
+#	$OpenBSD: bsd.own.mk,v 1.125 2012/08/31 17:16:21 pascal Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -32,7 +32,7 @@ ELF_TOOLCHAIN?=	yes
 GCC2_ARCH=m68k m88k vax
 GCC4_ARCH=alpha amd64 arm avr32 hppa hppa64 i386 ia64 mips64 mips64el powerpc sparc sparc64 sh
 BINUTILS217_ARCH=avr32 hppa64 ia64
-PIE_ARCH=
+PIE_ARCH=amd64 mips64 mips64el sparc64
 
 .for _arch in ${MACHINE_ARCH}
 .if !empty(GCC2_ARCH:M${_arch})
