51380a07d756f4fbb426d9e8b229fcd2cd120f2b
deraadt <deraadt@openbsd.org>
2012-09-02 03:44:19 +0000

    enable PIE on alpha, and document the situation for other architectures not running PIE yet.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.126 2012/09/01 03:12:16 deraadt Exp $
+#	$OpenBSD: bsd.own.mk,v 1.127 2012/09/02 03:44:19 deraadt Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -32,7 +32,12 @@ ELF_TOOLCHAIN?=	yes
 GCC2_ARCH=m68k m88k vax
 GCC4_ARCH=alpha amd64 arm hppa hppa64 i386 ia64 mips64 mips64el powerpc sparc sparc64 sh
 BINUTILS217_ARCH=hppa64 ia64
-PIE_ARCH=amd64 mips64 mips64el sparc64
+
+# i386 PIE works; but there are RAMDISK fitting problems.
+# sparc & powerpc needs consideration of -fpic/-fPIC vs -fpie/-fPIE
+# arm needs binutils-2.17, after that regains W^X support
+# sh has register spill problems in gcc (see usr.bin/sort) with -fpie
+PIE_ARCH=alpha amd64 mips64 mips64el sparc64
 
 .for _arch in ${MACHINE_ARCH}
 .if !empty(GCC2_ARCH:M${_arch})
