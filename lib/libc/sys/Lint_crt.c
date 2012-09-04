ac9b4aacc1da35008afea06a5d23c2f2dea9b93e
okan <okan@openbsd.org>
2012-09-04 03:10:42 +0000

    remove lint leftovers; ok guenther@

--- lib/libc/sys/Lint_crt.c
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD: Lint_crt.c,v 1.2 2007/09/08 19:42:36 otto Exp $	*/
-
-/* Public domain, Theo de Raadt, 2005 */
-
-#include <unistd.h>
-
-char *__progname;
--- lib/libc/sys/Lint_environ.c
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD: Lint_environ.c,v 1.1 2007/09/09 18:46:34 otto Exp $	*/
-
-/* Public domain, Otto Moerbeek, 2007 */
-
-#include <stdlib.h>
-
-char **environ;
