476cbaa3fe588cd840e6aaa919cb556b5545c33a
matthew <matthew@openbsd.org>
2012-09-06 22:13:44 +0000

    Add a (currently failing) regress test for GNU C++'s init_priority attribute.  See "info --index-search=init_priority gcc" for details on how this feature is supposed to work.

---
+++ regress/lib/csu/init_priority/init_priority_test.cc
@@ -0,0 +1,45 @@
+/* $OpenBSD: init_priority_test.cc,v 1.1 2012/09/06 22:13:44 matthew Exp $ */
+
+#include <cassert>
+
+namespace {
+const int kNumTests = 10;
+int counter = 0;
+int log[kNumTests];
+
+struct Test {
+	Test(int x);
+};
+
+Test::Test(int x)
+{
+	if (counter < kNumTests)
+		log[counter] = x;
+	counter++;
+}
+
+#define TEST(n) Test test_##n __attribute__((init_priority(n))) (n);
+TEST(12597);
+TEST(20840);
+TEST(31319);
+TEST(17071);
+TEST(47220);
+TEST(40956);
+TEST(28373);
+TEST(8742);
+TEST(14117);
+TEST(6407);
+#undef TEST
+}
+
+int
+main()
+{
+	int i;
+
+	assert(counter == kNumTests);
+	for (i = 1; i < kNumTests; i++)
+		assert(log[i] >= log[i - 1]);
+
+	return (0);
+}
