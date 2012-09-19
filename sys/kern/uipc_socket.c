88b888f645e83b56b33117e3c6c61790b61e69f2
bluhm <bluhm@openbsd.org>
2012-09-19 19:41:29 +0000

    In somove() make the call to pr_usrreq(PRU_RCVD) under the same conditions as in soreceive().  My goal is to make socket splicing less protocol dependent. ok claudio@

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.105 2012/09/17 14:33:56 bluhm Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.106 2012/09/19 19:41:29 bluhm Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
@@ -1236,7 +1236,7 @@ somove(struct socket *so, int wait)
 #endif
 
 	/* Send window update to source peer if receive buffer has changed. */
-	if (m)
+	if (m && so->so_proto->pr_flags & PR_WANTRCVD && so->so_pcb)
 		(so->so_proto->pr_usrreq)(so, PRU_RCVD, NULL,
 		    (struct mbuf *)0L, NULL, NULL);
 
