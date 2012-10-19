ba330dff103148df08dc6b4416faf67dbb3b69e6
deraadt <deraadt@openbsd.org>
2012-10-19 02:14:47 +0000

    Document the current situation with who is PIE, and who is not.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.130 2012/09/11 04:38:38 deraadt Exp $
+#	$OpenBSD: bsd.own.mk,v 1.131 2012/10/19 02:14:47 deraadt Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -34,9 +34,8 @@ GCC4_ARCH=alpha amd64 arm hppa hppa64 i386 ia64 mips64 mips64el powerpc sh sparc
 BINUTILS217_ARCH=hppa64 ia64
 
 # i386 PIE works; but there are RAMDISK fitting problems.
-# sparc & powerpc needs consideration of -fpic/-fPIC vs -fpie/-fPIE
-# arm needs binutils-2.17, after that regains W^X support
-# sh has register spill problems in gcc (see usr.bin/sort) with -fpie
+# powerpc has problems on some "embedded" powerpc.
+# arm needs binutils-2.17, which still lacks W^X support
 PIE_ARCH=alpha amd64 hppa mips64 mips64el sh sparc64
 
 .for _arch in ${MACHINE_ARCH}
