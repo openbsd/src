a99cba7ee5ecbdd23019286f8779873348ae8a53
gsoares <gsoares@openbsd.org>
2012-10-30 20:03:16 +0000

    fdopen(3) return a FILE * not an int, so compare against NULL. no functional change
    OK millert@

--- usr.bin/sort/sort.c
+++ usr.bin/sort/sort.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sort.c,v 1.39 2009/12/22 19:47:02 schwarze Exp $	*/
+/*	$OpenBSD: sort.c,v 1.40 2012/10/30 20:03:16 gsoares Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -281,7 +281,7 @@ main(int argc, char *argv[])
 		(void)umask(um);
 		if ((outfd = mkstemp(toutpath)) == -1 ||
 		    fchmod(outfd, DEFFILEMODE & ~um) == -1 ||
-		    (outfp = fdopen(outfd, "w")) == 0)
+		    (outfp = fdopen(outfd, "w")) == NULL)
 			err(2, "%s", toutpath);
 		outfile = toutpath;
 
