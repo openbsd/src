f32c56c73d107253028c800524e566a9ae969786
florian <florian@openbsd.org>
2012-11-03 16:26:32 +0000

    kill dead assignment, found by llvm
    ok haesbaert, lum, benno

--- usr.bin/mg/echo.c
+++ usr.bin/mg/echo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: echo.c,v 1.52 2012/10/22 08:31:42 florian Exp $	*/
+/*	$OpenBSD: echo.c,v 1.53 2012/11/03 16:26:32 florian Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -268,7 +268,6 @@ veread(const char *fp, char *buf, size_t nbuf, int flag, va_list ap)
 		case CCHR('D'):
 			if (cpos != epos) {
 				tteeol();
-				y = buf[cpos];
 				epos--;
 				rr = ttrow;
 				cc = ttcol;
