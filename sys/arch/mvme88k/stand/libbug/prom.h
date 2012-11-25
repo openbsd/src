785fe7530309c255d4fcc04966829c4752af2084
miod <miod@openbsd.org>
2012-11-25 14:08:14 +0000

    Sync register clobber information in MVMEPROM_CALL() with those used in the kernel (in dev/bugio.c)

--- sys/arch/mvme88k/stand/libbug/prom.h
+++ sys/arch/mvme88k/stand/libbug/prom.h
@@ -1,5 +1,31 @@
-/*	$OpenBSD: prom.h,v 1.4 2012/07/05 04:23:33 guenther Exp $	*/
+/*	$OpenBSD: prom.h,v 1.5 2012/11/25 14:08:14 miod Exp $	*/
+/*
+ * Copyright (c) 2006, 2010, Miodrag Vallat.
+ * Copyright (c) 1998 Steve Murphree, Jr.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#define MVMEPROM_CALL(x) \
-	__asm__ __volatile__ ("or r9,r0," __STRING(x)); \
-	__asm__ __volatile__ ("tb0 0,r0,496")
+#define MVMEPROM_CALL(x)						\
+	__asm__ __volatile__ ("or r9,r0," __STRING(x) "; tb0 0,r0,496"	\
+	    :::	"r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",		\
+	        "r9", "r10", "r11", "r12", "r13", "memory")
