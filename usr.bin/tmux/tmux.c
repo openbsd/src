351943552a7c4230d5f1bf5bcdf131b6d72fd745
nicm <nicm@openbsd.org>
2012-11-26 11:35:28 +0000

    Call realpath earlier on the socket directory path rather than on the socket file path because the latter may not exist yet and in that case realpath is allowed to fail. From Romain Francoise.

--- usr.bin/tmux/tmux.c
+++ usr.bin/tmux/tmux.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.c,v 1.112 2012/07/10 11:53:01 nicm Exp $ */
+/* $OpenBSD: tmux.c,v 1.113 2012/11/26 11:35:28 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -159,7 +159,7 @@ parseenvironment(void)
 char *
 makesocketpath(const char *label)
 {
-	char		base[MAXPATHLEN], *path, *s;
+	char		base[MAXPATHLEN], realbase[MAXPATHLEN], *path, *s;
 	struct stat	sb;
 	u_int		uid;
 
@@ -183,7 +183,10 @@ makesocketpath(const char *label)
 		return (NULL);
 	}
 
-	xasprintf(&path, "%s/%s", base, label);
+	if (realpath(base, realbase) == NULL)
+		strlcpy(realbase, base, sizeof realbase);
+
+	xasprintf(&path, "%s/%s", realbase, label);
 	return (path);
 }
 
@@ -384,8 +387,7 @@ main(int argc, char **argv)
 		}
 	}
 	free(label);
-	if (realpath(path, socket_path) == NULL)
-		strlcpy(socket_path, path, sizeof socket_path);
+	strlcpy(socket_path, path, sizeof socket_path);
 	free(path);
 
 	/* Set process title. */
