140c00f1ff23375cccb83c6115bf8e71300f9315
nicm <nicm@openbsd.org>
2012-11-27 14:26:48 +0000

    Revert last, after discussion it isn't necessary.

--- usr.bin/tmux/input-keys.c
+++ usr.bin/tmux/input-keys.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: input-keys.c,v 1.28 2012/11/27 14:22:43 nicm Exp $ */
+/* $OpenBSD: input-keys.c,v 1.29 2012/11/27 14:26:48 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -224,8 +224,7 @@ input_mouse(struct window_pane *wp, struct session *s, struct mouse_event *m)
 	}
 
 	if ((m->xb & 3) != 1 &&
-	    options_get_number(&wp->window->options, "mode-mouse") == 1 &&
-	    (wp->base.mode & ALL_MOUSE_MODES) == 0) {
+	    options_get_number(&wp->window->options, "mode-mouse") == 1) {
 		if (window_pane_set_mode(wp, &window_copy_mode) == 0) {
 			window_copy_init_from_pane(wp);
 			if (wp->mode->mouse != NULL)
