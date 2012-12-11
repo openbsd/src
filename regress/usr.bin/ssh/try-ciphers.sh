aea242ff0ea2eeaa7361c5e607785fa8c959a7fe
markus <markus@openbsd.org>
2012-12-11 22:32:56 +0000

    add etm modes

--- regress/usr.bin/ssh/try-ciphers.sh
+++ regress/usr.bin/ssh/try-ciphers.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: try-ciphers.sh,v 1.15 2012/10/05 02:20:48 dtucker Exp $
+#	$OpenBSD: try-ciphers.sh,v 1.16 2012/12/11 22:32:56 markus Exp $
 #	Placed in the Public Domain.
 
 tid="try ciphers"
@@ -8,7 +8,11 @@ ciphers="aes128-cbc 3des-cbc blowfish-cbc cast128-cbc
 	aes192-cbc aes256-cbc rijndael-cbc@lysator.liu.se
 	aes128-ctr aes192-ctr aes256-ctr"
 macs="hmac-sha1 hmac-md5 umac-64@openssh.com umac-128@openssh.com
-	hmac-sha1-96 hmac-md5-96 hmac-sha2-256 hmac-sha2-512"
+	hmac-sha1-96 hmac-md5-96 hmac-sha2-256 hmac-sha2-512
+	hmac-sha1-etm@openssh.com hmac-md5-etm@openssh.com
+	umac-64-etm@openssh.com umac-128-etm@openssh.com
+	hmac-sha1-96-etm@openssh.com hmac-md5-96-etm@openssh.com
+	hmac-sha2-256-etm@openssh.com hmac-sha2-512-etm@openssh.com"
 
 for c in $ciphers; do
 	for m in $macs; do
