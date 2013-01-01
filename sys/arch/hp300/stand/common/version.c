a686fbfe9a02ff1eb8e59bc53c0e12135177d7fd
miod <miod@openbsd.org>
2013-01-01 17:18:49 +0000

    Switch hp300 boot blocks to MI loadfile code.

--- sys/arch/hp300/stand/common/version.c
+++ sys/arch/hp300/stand/common/version.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: version.c,v 1.10 2011/08/18 20:02:58 miod Exp $	*/
+/*	$OpenBSD: version.c,v 1.11 2013/01/01 17:18:49 miod Exp $	*/
 
 /*
  * Record major changes in the boot code here, and increment the version
@@ -6,21 +6,21 @@
  */
 
 /*
- * 2.0			INITIAL REVISION
- * 2.1			APCI support, config changes from NetBSD.
- * 2.2			Grand reorganization.
- * 2.3			Added CD9660 boot support.
- * 2.4			Added/fixed 425e support.
- * 2.5			Added SYS_CDBOOT.
- * 2.6			Fixed RTC reading for 2001.
- * 2.7			Minor syncs with the kernel (recognize more models
- *			and use the same logic to pick the console on 425e).
- * 2.8			TurboVRX frame buffer support.
- * 2.9			SGC frame buffers support, bug fixes and code cleanup.
- * 2.10			blind SGC support on models 362 and 382, turned out
- *			to be useless.
- * 2.11			sti@dio frame buffer support (for models 362 and 382),
- *			and various cleanups.
+ * 2.0	INITIAL REVISION
+ * 2.1	APCI support, config changes from NetBSD.
+ * 2.2	Grand reorganization.
+ * 2.3	Added CD9660 boot support.
+ * 2.4	Added/fixed 425e support.
+ * 2.5	Added SYS_CDBOOT.
+ * 2.6	Fixed RTC reading for 2001.
+ * 2.7	Minor syncs with the kernel (recognize more models and use the same
+ *	logic to pick the console on 425e).
+ * 2.8	TurboVRX frame buffer support.
+ * 2.9	SGC frame buffers support, bug fixes and code cleanup.
+ * 2.10	Blind SGC support on models 362 and 382, turned out to be useless.
+ * 2.11	sti@dio frame buffer support (for models 362 and 382), and various
+ *	cleanups.
+ * 2.12	Switch to MI loadfile code.
  */
 
-const char version[] = "2.11";
+const char version[] = "2.12";
