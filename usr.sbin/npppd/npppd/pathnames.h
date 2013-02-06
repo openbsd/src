1049b6f43ff67f79026928cb5ed32c561de0cbc4
giovanni <giovanni@openbsd.org>
2013-02-06 11:47:18 +0000

    remove unused constant definition ok yasuoka@

--- usr.sbin/npppd/npppd/pathnames.h
+++ usr.sbin/npppd/npppd/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pathnames.h,v 1.5 2012/05/08 13:15:12 yasuoka Exp $ */
+/*	$OpenBSD: pathnames.h,v 1.6 2013/02/06 11:47:18 giovanni Exp $ */
 
 /*-
  * Copyright (c) 2009 Internet Initiative Japan Inc.
@@ -30,10 +30,6 @@
 
 #ifndef	DEFAULT_NPPPD_CONF
 #define	DEFAULT_NPPPD_CONF		"/etc/npppd/npppd.conf"
-#endif
-
-#ifndef	DEFAULT_NPPPD_USERSFILE
-#define	DEFAULT_NPPPD_USERSFILE		"/etc/npppd/npppd-users.csv"
 #endif
 
 #ifndef DEFAULT_NPPPD_PIDFILE
