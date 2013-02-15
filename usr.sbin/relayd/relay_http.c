412ad10bface586d9b2a12aab8ae26b66a5cc085
bluhm <bluhm@openbsd.org>
2013-02-15 12:15:12 +0000

    Fix the toread check in the lateconnect case.  This allows Content-Length: 0 in HTTP GET request when a request filter is used. This was broken by my previous commit. OK reyk@ benno@

--- usr.sbin/relayd/relay_http.c
+++ usr.sbin/relayd/relay_http.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relay_http.c,v 1.8 2013/02/05 21:36:33 bluhm Exp $	*/
+/*	$OpenBSD: relay_http.c,v 1.9 2013/02/15 12:15:12 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2012 Reyk Floeter <reyk@openbsd.org>
@@ -357,7 +357,7 @@ relay_read_http(struct bufferevent *bev, void *arg)
 		relay_http_request_close(cre);
 
  done:
-		if (cre->dir == RELAY_DIR_REQUEST && cre->toread < 0 &&
+		if (cre->dir == RELAY_DIR_REQUEST && cre->toread <= 0 &&
 		    proto->lateconnect && cre->dst->bev == NULL) {
 			if (rlay->rl_conf.fwdmode == FWD_TRANS) {
 				relay_bindanyreq(con, 0, IPPROTO_TCP);
