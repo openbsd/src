10b64716de34bde39703e2a59e2bdd53be885104
miod <miod@openbsd.org>
2013-03-08 18:35:46 +0000

    In pcic_intr_find(), pass a correct device name to isa_intr_establish(), so that we can get reasonable debug or panic messages when things go wrong.

--- sys/dev/isa/i82365_isasubr.c
+++ sys/dev/isa/i82365_isasubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i82365_isasubr.c,v 1.22 2009/07/23 15:58:37 miod Exp $	*/
+/*	$OpenBSD: i82365_isasubr.c,v 1.23 2013/03/08 18:35:46 miod Exp $	*/
 /*	$NetBSD: i82365_isasubr.c,v 1.1 1998/06/07 18:28:31 sommerfe Exp $  */
 
 /*
@@ -313,7 +313,7 @@ pcic_intr_find(sc, ist)
 				 * higher.
 				 */
 				ih = isa_intr_establish(ic, irq, ist, IPL_VM,
-				    pcic_intr_probe, 0, NULL);
+				    pcic_intr_probe, 0, sc->dev.dv_xname);
 				if (ih == NULL)
 					continue;
 				pcic_intr_seen = 0;
