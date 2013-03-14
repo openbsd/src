423e3a9c4463c04d81621ccba8bb81591961b173
millert <millert@openbsd.org>
2013-03-14 13:20:57 +0000

    Fix indentation

--- usr.bin/mktemp/mktemp.c
+++ usr.bin/mktemp/mktemp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mktemp.c,v 1.17 2013/03/12 19:09:23 millert Exp $	*/
+/*	$OpenBSD: mktemp.c,v 1.18 2013/03/14 13:20:57 millert Exp $	*/
 
 /*
  * Copyright (c) 1996, 1997, 2001-2003, 2013
@@ -89,8 +89,8 @@ main(int argc, char *argv[])
 		len = strlen(template);
 		if (len < 6 || strcmp(&template[len - 6], "XXXXXX")) {
 		    if (!quiet) {
-			warnx("insufficient number of Xs in template `%s'",
-			    template);
+			    warnx("insufficient number of Xs in template `%s'",
+				template);
 		    }
 		    exit(1);
 		}
