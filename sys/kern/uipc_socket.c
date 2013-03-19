4291a85a4d4b7e98662f0efa6874ba719eb5976c
bluhm <bluhm@openbsd.org>
2013-03-19 20:07:14 +0000

    After a socket splicing timeout is fired, a network interrupt can unsplice() the sockets before soidle() goes to splsoftnet.  In this case, unsplice() was called twice.  So check wether splicing still exists within the splsoftnet protection. Uvm fault in sounsplice() reported by keith at scott-land dot net. OK claudio@

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.114 2013/02/16 14:34:52 bluhm Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.115 2013/03/19 20:07:14 bluhm Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
@@ -1443,8 +1443,10 @@ soidle(void *arg)
 	int s;
 
 	s = splsoftnet();
-	so->so_error = ETIMEDOUT;
-	sounsplice(so, so->so_splice, 1);
+	if (so->so_splice) {
+		so->so_error = ETIMEDOUT;
+		sounsplice(so, so->so_splice, 1);
+	}
 	splx(s);
 }
 #endif /* SOCKET_SPLICE */
