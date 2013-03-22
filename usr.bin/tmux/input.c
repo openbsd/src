84c47e4a250f5c9bab794d2d86cafb4699d1f7e8
nicm <nicm@openbsd.org>
2013-03-22 10:36:53 +0000

    Implement DECAWM (SM/RM 7) using existing MODE_WRAP flag.

--- usr.bin/tmux/input.c
+++ usr.bin/tmux/input.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: input.c,v 1.60 2013/03/22 10:33:50 nicm Exp $ */
+/* $OpenBSD: input.c,v 1.61 2013/03/22 10:36:53 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -1248,6 +1248,9 @@ input_csi_dispatch(struct input_ctx *ictx)
 			screen_write_cursormove(&ictx->ctx, 0, 0);
 			screen_write_clearscreen(&ictx->ctx);
 			break;
+		case 7:		/* DECAWM */
+			screen_write_mode_clear(&ictx->ctx, MODE_WRAP);
+			break;
 		case 25:	/* TCEM */
 			screen_write_mode_clear(&ictx->ctx, MODE_CURSOR);
 			break;
@@ -1304,6 +1307,9 @@ input_csi_dispatch(struct input_ctx *ictx)
 		case 3:		/* DECCOLM */
 			screen_write_cursormove(&ictx->ctx, 0, 0);
 			screen_write_clearscreen(&ictx->ctx);
+			break;
+		case 7:		/* DECAWM */
+			screen_write_mode_set(&ictx->ctx, MODE_WRAP);
 			break;
 		case 25:	/* TCEM */
 			screen_write_mode_set(&ictx->ctx, MODE_CURSOR);
