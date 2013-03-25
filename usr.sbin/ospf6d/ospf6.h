c1df15f7d94258888f54a12644d58dd37ff0e13f
markus <markus@openbsd.org>
2013-03-25 14:22:02 +0000

    sync w/ospfd: allow two minutes until neighbor adjacencies are formed; ok claudio@ sthen@

--- usr.sbin/ospf6d/ospf6.h
+++ usr.sbin/ospf6d/ospf6.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospf6.h,v 1.19 2013/03/22 12:00:57 sthen Exp $ */
+/*	$OpenBSD: ospf6.h,v 1.20 2013/03/25 14:22:02 markus Exp $ */
 
 /*
  * Copyright (c) 2004, 2005, 2007 Esben Norby <norby@openbsd.org>
@@ -57,7 +57,7 @@
 #define MIN_TRANSMIT_DELAY	1
 #define MAX_TRANSMIT_DELAY	3600
 
-#define DEFAULT_ADJ_TMOUT	60
+#define DEFAULT_ADJ_TMOUT	120
 
 #define DEFAULT_NBR_TMOUT	86400	/* 24 hours */
 
