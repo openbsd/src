a67c559c2f12ad6fe0a483084e3d099794245e58
mcbride <mcbride@openbsd.org>
2013-03-27 13:32:28 +0000

    Use the correct src/dst ports depending on direction (one of src or dst was wrong in each direction).
    Report and fix from UMEZAWA Takeshi <umezawa@iij.ad.jp>

--- sys/net/pf_ioctl.c
+++ sys/net/pf_ioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_ioctl.c,v 1.257 2013/02/26 14:56:05 mikeb Exp $ */
+/*	$OpenBSD: pf_ioctl.c,v 1.258 2013/03/27 13:32:28 mcbride Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1354,14 +1354,14 @@ pfioctl(dev_t dev, u_long cmd, caddr_t addr, int flags, struct proc *p)
 				sk = s->key[PF_SK_STACK];
 				srcaddr = &sk->addr[1];
 				dstaddr = &sk->addr[0];
-				srcport = sk->port[0];
+				srcport = sk->port[1];
 				dstport = sk->port[0];
 			} else {
 				sk = s->key[PF_SK_WIRE];
 				srcaddr = &sk->addr[0];
 				dstaddr = &sk->addr[1];
 				srcport = sk->port[0];
-				dstport = sk->port[0];
+				dstport = sk->port[1];
 			}
 			if ((!psk->psk_af || sk->af == psk->psk_af)
 			    && (!psk->psk_proto || psk->psk_proto ==
