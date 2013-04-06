de6a4eadff9b208d5ba0b0970271ad75146e8272
tedu <tedu@openbsd.org>
2013-04-06 06:08:20 +0000

    i missed a file, crap.  found by deraadt

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sig.c,v 1.149 2013/03/28 16:55:25 deraadt Exp $	*/
+/*	$OpenBSD: kern_sig.c,v 1.150 2013/04/06 06:08:20 tedu Exp $	*/
 /*	$NetBSD: kern_sig.c,v 1.54 1996/04/22 01:38:32 christos Exp $	*/
 
 /*
@@ -1611,9 +1611,6 @@ sys___thrsigdivert(struct proc *p, void *v, register_t *retval)
 	sigset_t *m;
 	long long to_ticks = 0;
 	int error;
-
-	if (!rthreads_enabled)
-		return (ENOTSUP);
 
 	m = NULL;
 	mask = SCARG(uap, sigmask) &~ sigcantmask;
