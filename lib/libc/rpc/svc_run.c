8b010f33d68c2f42382d5251a6807e866c6cad1a
millert <millert@openbsd.org>
2013-04-16 12:15:55 +0000

    Replace a realloc() + memcpy() with calloc() + memcpy(). We don't need to zero pfd but using calloc() gets us overflow protection for free.  OK chl@ deraadt@

--- lib/libc/rpc/svc_run.c
+++ lib/libc/rpc/svc_run.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: svc_run.c,v 1.19 2010/09/01 14:43:34 millert Exp $ */
+/*	$OpenBSD: svc_run.c,v 1.20 2013/04/16 12:15:55 millert Exp $ */
 
 /*
  * Copyright (c) 2010, Oracle America, Inc.
@@ -45,18 +45,17 @@
 void
 svc_run(void)
 {
-	struct pollfd *pfd = NULL, *newp;
+	struct pollfd *pfd = NULL;
 	int nready, saved_max_pollfd = 0;
 
 	for (;;) {
 		if (svc_max_pollfd > saved_max_pollfd) {
-			newp = realloc(pfd, sizeof(*pfd) * svc_max_pollfd);
-			if (newp == NULL) {
-				free(pfd);
+			free(pfd);
+			pfd = calloc(sizeof(*pfd), svc_max_pollfd);
+			if (pfd == NULL) {
 				perror("svc_run");	/* XXX */
 				return;			/* XXX */
 			}
-			pfd = newp;
 			saved_max_pollfd = svc_max_pollfd;
 		}
 		memcpy(pfd, svc_pollfd, sizeof(*pfd) * svc_max_pollfd);
