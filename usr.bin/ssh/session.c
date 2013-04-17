ea2cbbacbc7e4e2fdd452c24cb0c5e3a9a27c858
dtucker <dtucker@openbsd.org>
2013-04-17 09:04:09 +0000

    revert rev 1.262; it fails because uid is already set here.  ok djm@

--- usr.bin/ssh/session.c
+++ usr.bin/ssh/session.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: session.c,v 1.262 2013/03/06 23:35:23 djm Exp $ */
+/* $OpenBSD: session.c,v 1.263 2013/04/17 09:04:09 dtucker Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -1216,10 +1216,7 @@ do_setusercontext(struct passwd *pw)
 			perror("unable to set user context (setuser)");
 			exit(1);
 		}
-	} else if (options.chroot_directory != NULL &&
-	    strcasecmp(options.chroot_directory, "none") != 0)
-		fatal("server lacks privileges to chroot to ChrootDirectory");
-
+	}
 	if (getuid() != pw->pw_uid || geteuid() != pw->pw_uid)
 		fatal("Failed to set uids to %u.", (u_int) pw->pw_uid);
 }
