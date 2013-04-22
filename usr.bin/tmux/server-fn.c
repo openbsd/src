fb4c71a6a64b580015ca5bde49fa87b2f7da744a
nicm <nicm@openbsd.org>
2013-04-22 13:35:18 +0000

    Call recalculate_sizes() after killing window in case it is in a grouped session, from Daniel Ralston.

--- usr.bin/tmux/server-fn.c
+++ usr.bin/tmux/server-fn.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server-fn.c,v 1.68 2013/03/25 11:36:59 nicm Exp $ */
+/* $OpenBSD: server-fn.c,v 1.69 2013/04/22 13:35:18 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -283,6 +283,7 @@ server_kill_window(struct window *w)
 		if (options_get_number(&s->options, "renumber-windows"))
 			session_renumber_windows(s);
 	}
+	recalculate_sizes();
 }
 
 int
