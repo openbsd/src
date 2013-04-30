b3a7da0c53540c29bf77fc8b2ba13e2cee43f23a
patrick <patrick@openbsd.org>
2013-04-30 13:10:45 +0000

    Switch from pmap to pmap7.
    ok bmercer@

--- sys/arch/beagle/include/pmap.h
+++ sys/arch/beagle/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.1 2009/05/08 03:13:26 drahn Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.2 2013/04/30 13:10:45 patrick Exp $	*/
 /* $NetBSD: pmap.h,v 1.2 2001/11/23 17:29:01 thorpej Exp $ */
 
-#include <arm/pmap.h>
+#include <arm/pmap7.h>
