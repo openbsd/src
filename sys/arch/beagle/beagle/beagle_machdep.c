7c3dc09bd9564f7bd23c7b67b627fdb48e375503
patrick <patrick@openbsd.org>
2013-05-01 14:09:50 +0000

    Add a secure monitor call function, so that a secondary cache controller driver can talk to its controller properly.
    From drahn at dalerahn.com.
    
    ok bmercer@

--- sys/arch/beagle/beagle/beagle_machdep.c
+++ sys/arch/beagle/beagle/beagle_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: beagle_machdep.c,v 1.16 2013/05/01 13:49:18 patrick Exp $ */
+/*	$OpenBSD: beagle_machdep.c,v 1.17 2013/05/01 14:09:50 patrick Exp $ */
 /*	$NetBSD: lubbock_machdep.c,v 1.2 2003/07/15 00:25:06 lukem Exp $ */
 
 /*
@@ -144,6 +144,7 @@
 #include <arm/armv7/armv7reg.h>
 #include <arm/armv7/armv7var.h>
 
+#include <arm/cortex/smc.h>
 #include <machine/machine_reg.h>
 #include <beagle/dev/omapvar.h>
 
@@ -1013,4 +1014,21 @@ board_startup(void)
 		printf("kernel does not support -c; continuing..\n");
 #endif
 	}
+}
+
+void
+platform_smc_write(bus_space_tag_t iot, bus_space_handle_t ioh, bus_size_t off,
+    uint32_t op, uint32_t val)
+{
+	extern void omap4_smc_call(uint32_t, uint32_t);
+
+	switch (op) {
+	case 0x100:	/* PL310 DEBUG */
+	case 0x102:	/* PL310 CTL */
+		break;
+	default:
+		panic("platform_smc_write: invalid operation %d", op);
+	}
+
+	omap4_smc_call(op, val);
 }
