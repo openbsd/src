9cea5a4d20b399784136c8ceaebbc1e93a61aec9
guenther <guenther@openbsd.org>
2013-05-06 00:23:49 +0000

    If the lock is contended, such that the 'sc' fails, then we need to reset the %2 register with _SPINLOCK_LOCKED before retrying.
    tobiasu@ hit the problem and figured out that ".set noreorder" is
    needed in the ASM

--- lib/librthread/arch/mips64/_atomic_lock.c
+++ lib/librthread/arch/mips64/_atomic_lock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.4 2009/06/01 23:17:53 miod Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.5 2013/05/06 00:23:49 guenther Exp $	*/
 
 /*
  * Atomic lock for mips
@@ -13,12 +13,15 @@ _atomic_lock(volatile _spinlock_lock_t *lock)
 	_spinlock_lock_t old;
 
 	__asm__ __volatile__ (
+	".set	noreorder\n"
 	"1:	ll	%0,	0(%1)\n"
 	"	sc	%2,	0(%1)\n"
 	"	beqz	%2,	1b\n"
-	"	 nop\n" :
-		"=r"(old) :
-		"r"(lock), "r"(_SPINLOCK_LOCKED) : "memory");
+	"	 addi	%2,	$0, %3\n"
+	".set	reorder\n"
+		: "=&r"(old)
+		: "r"(lock), "r"(_SPINLOCK_LOCKED), "i"(_SPINLOCK_LOCKED)
+		: "memory");
 
 	return (old != _SPINLOCK_UNLOCKED);
 }
