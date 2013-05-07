1318a170f298fbe04becaa354175d2332b462a07
jsg <jsg@openbsd.org>
2013-05-07 09:32:58 +0000

    alloc the size of the struct not the size of a pointer to it ok reyk@

--- usr.sbin/snmpctl/snmpctl.c
+++ usr.sbin/snmpctl/snmpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: snmpctl.c,v 1.14 2012/09/17 16:43:59 reyk Exp $	*/
+/*	$OpenBSD: snmpctl.c,v 1.15 2013/05/07 09:32:58 jsg Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008 Reyk Floeter <reyk@openbsd.org>
@@ -89,7 +89,7 @@ main(int argc, char *argv[])
 	int			 ch;
 	const char		*sock = SNMPD_SOCKET;
 
-	if ((env = calloc(1, sizeof(struct snmpd *))) == NULL)
+	if ((env = calloc(1, sizeof(struct snmpd))) == NULL)
 		err(1, "calloc");
 	gettimeofday(&env->sc_starttime, NULL);
 
