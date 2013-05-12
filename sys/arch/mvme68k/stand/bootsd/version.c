6efe90da7443994bcb088bb78aaaf1a096364820
miod <miod@openbsd.org>
2013-05-12 08:10:07 +0000

    Lower heap location and load address by 1MB for the secondary boot loaders, to fit 8MB boards.

--- sys/arch/mvme68k/stand/bootsd/version.c
+++ sys/arch/mvme68k/stand/bootsd/version.c
@@ -1,7 +1,8 @@
-/*	$OpenBSD: version.c,v 1.9 2012/12/31 21:35:32 miod Exp $ */
+/*	$OpenBSD: version.c,v 1.10 2013/05/12 08:10:07 miod Exp $ */
 
 /*
+ *	1.10	lower load address and heap location by 1MB to fit 8MB boards
  *	1.9	kernel loaded with loadfile, a.out and ELF formats
  */
 
-char *version = "1.9";
+char *version = "1.10";
--- sys/arch/mvme68k/stand/bootst/version.c
+++ sys/arch/mvme68k/stand/bootst/version.c
@@ -1,8 +1,9 @@
-/*	$OpenBSD: version.c,v 1.5 2013/04/14 19:05:19 miod Exp $ */
+/*	$OpenBSD: version.c,v 1.6 2013/05/12 08:10:07 miod Exp $ */
 
 /*
+ *	1.6	lower load address and heap location by 1MB to fit 8MB boards
  *	1.5	do not load kernel symbols to avoid seeking backwards
  *	1.4	kernel loaded with loadfile, a.out and ELF formats
  */
 
-char *version = "1.5";
+char *version = "1.6";
