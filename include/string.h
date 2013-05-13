2eb94c6c3499348a5df101b750fafbd483f53db9
ajacoutot <ajacoutot@openbsd.org>
2013-05-13 10:37:02 +0000

    Add an implementation of memmem() -- from FreeBSD with some tweaks. Requires a libc minor bump, committing now so that we have up-to-date snapshots for the upcoming hackathon.
    joint work with millert@
    man page bits ok jmc@
    input and ok millert@, guenther@, deraadt@

--- include/string.h
+++ include/string.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: string.h,v 1.25 2013/04/11 03:08:47 guenther Exp $	*/
+/*	$OpenBSD: string.h,v 1.26 2013/05/13 10:37:02 ajacoutot Exp $	*/
 /*	$NetBSD: string.h,v 1.6 1994/10/26 00:56:30 cgd Exp $	*/
 
 /*-
@@ -126,6 +126,7 @@ char	*strsignal(int);
 #endif
 
 #if __BSD_VISIBLE
+void	*memmem(const void *, size_t, const void *, size_t);
 void	*memrchr(const void *, int, size_t);
 char	*strcasestr(const char *, const char *);
 size_t	 strlcat(char *, const char *, size_t)
