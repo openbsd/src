9a62a23dffa07971021f08f6326549533be4761e
dtucker <dtucker@openbsd.org>
2013-05-16 03:33:30 +0000

    test rekeying when there's no data being transferred

--- regress/usr.bin/ssh/rekey.sh
+++ regress/usr.bin/ssh/rekey.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: rekey.sh,v 1.4 2013/05/16 02:10:35 dtucker Exp $
+#	$OpenBSD: rekey.sh,v 1.5 2013/05/16 03:33:30 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="rekey during transfer data"
@@ -11,7 +11,7 @@ rm -f ${COPY} ${LOG} ${DATA}
 dd if=/dev/zero of=${DATA} bs=1k count=512 > /dev/null 2>&1
 
 for s in 16 1k 128k 256k; do
-	trace "rekeylimit ${s}"
+	verbose "rekeylimit ${s}"
 	rm -f ${COPY} ${LOG}
 	cat $DATA | \
 		${SSH} -oCompression=no -oRekeyLimit=$s \
@@ -28,16 +28,32 @@ for s in 16 1k 128k 256k; do
 	fi
 done
 
-for s in 5 10 15 ; do
-	trace "rekeylimit default ${s}"
+for s in 5 10; do
+	verbose "rekeylimit default ${s}"
 	rm -f ${COPY} ${LOG}
 	cat $DATA | \
 		${SSH} -oCompression=no -oRekeyLimit="default $s" -F \
-			$OBJ/ssh_proxy somehost "cat >${COPY};sleep $s;sleep 5"
+			$OBJ/ssh_proxy somehost "cat >${COPY};sleep $s;sleep 3"
 	if [ $? -ne 0 ]; then
 		fail "ssh failed"
 	fi
 	cmp $DATA ${COPY}		|| fail "corrupted copy"
+	n=`grep 'NEWKEYS sent' ${LOG} | wc -l`
+	n=`expr $n - 1`
+	trace "$n rekeying(s)"
+	if [ $n -lt 1 ]; then
+		fail "no rekeying occured"
+	fi
+done
+
+for s in 5 10; do
+	verbose "rekeylimit default ${s} no data"
+	rm -f ${COPY} ${LOG}
+	${SSH} -oCompression=no -oRekeyLimit="default $s" -F \
+		$OBJ/ssh_proxy somehost "sleep $s;sleep 3"
+	if [ $? -ne 0 ]; then
+		fail "ssh failed"
+	fi
 	n=`grep 'NEWKEYS sent' ${LOG} | wc -l`
 	n=`expr $n - 1`
 	trace "$n rekeying(s)"
