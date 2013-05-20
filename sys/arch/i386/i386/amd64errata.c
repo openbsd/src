13cb06c920f1b1eea2758ce73804a53aa56c5ccf
kettenis <kettenis@openbsd.org>
2013-05-20 17:34:08 +0000

    de-static-ize, such that people don't get confused by aesni failures on AMD hardware the next time somebody tries to run on a broken KVM hypervisor.

--- sys/arch/i386/i386/amd64errata.c
+++ sys/arch/i386/i386/amd64errata.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: amd64errata.c,v 1.3 2012/03/27 05:59:46 jsg Exp $	*/
+/*	$OpenBSD: amd64errata.c,v 1.4 2013/05/20 17:34:08 kettenis Exp $	*/
 /*	$NetBSD: errata.c,v 1.6 2007/02/05 21:05:45 ad Exp $	*/
 
 /*-
@@ -243,7 +243,7 @@ static errata_t errata[] = {
 	},
 };
 
-static int 
+int 
 amd64_errata_testmsr(struct cpu_info *ci, errata_t *e)
 {
 	uint64_t val;
@@ -258,7 +258,7 @@ amd64_errata_testmsr(struct cpu_info *ci, errata_t *e)
 	return 1;			/* found */
 }
 
-static int 
+int 
 amd64_errata_setmsr(struct cpu_info *ci, errata_t *e)
 {
 	uint64_t val;
