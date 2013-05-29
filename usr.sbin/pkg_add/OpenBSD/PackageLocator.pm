8052eb10f2cb55a4d8d23cbe418e7745b09bb36a
espie <espie@openbsd.org>
2013-05-29 09:35:16 +0000

    allow installpath to use +=, requested by quite a few people, but did require testing.

--- usr.sbin/pkg_add/OpenBSD/PackageLocator.pm
+++ usr.sbin/pkg_add/OpenBSD/PackageLocator.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackageLocator.pm,v 1.100 2012/07/13 07:50:13 espie Exp $
+# $OpenBSD: PackageLocator.pm,v 1.101 2013/05/29 09:35:16 espie Exp $
 #
 # Copyright (c) 2003-2010 Marc Espie <espie@openbsd.org>
 #
@@ -42,7 +42,7 @@ sub build_default_path
 	$default_path->add(OpenBSD::PackageRepository->new("./", $state)->can_be_empty);
 	return if $state->defines('NOINSTALLPATH');
 
-	if (my $i = $state->config->value("installpath")) {
+	for my $i ($state->config->value("installpath")) {
 		$default_path->add(OpenBSD::PackageRepository->new($i, $state));
 	}
 }
