15ffba1e8a1a63ed8ffd128fa09cb1295fd1cb62
tedu <tedu@openbsd.org>
2013-05-31 21:12:54 +0000

    fixie

--- lib/libc/gen/setprogname.c
+++ lib/libc/gen/setprogname.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: setprogname.c,v 1.1 2013/05/31 20:59:24 ajacoutot Exp $ */
+/* $OpenBSD: setprogname.c,v 1.2 2013/05/31 21:12:54 tedu Exp $ */
 /*
  * Copyright (c) 2013 Antoine Jacoutot <ajacoutot@openbsd.org>
  *
@@ -29,5 +29,5 @@ setprogname(const char *progname)
 	if (tmpn == NULL)
 		__progname = progname;
 	else
-		tmpn + 1;
+		__progname = tmpn + 1;
 }
