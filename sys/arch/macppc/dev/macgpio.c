c767eae2a6d1b39511e274225e19885e3331ff39
mpi <mpi@openbsd.org>
2013-06-03 20:10:50 +0000

    Be coherent when printing irq numbers.

--- sys/arch/macppc/dev/macgpio.c
+++ sys/arch/macppc/dev/macgpio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: macgpio.c,v 1.6 2007/04/22 22:31:14 deraadt Exp $	*/
+/*	$OpenBSD: macgpio.c,v 1.7 2013/06/03 20:10:50 mpi Exp $	*/
 /*	$NetBSD: gpio.c,v 1.2 2001/02/27 05:16:33 matt Exp $	*/
 
 /*-
@@ -164,7 +164,7 @@ macgpio_gpio_attach(struct device *parent, struct device *self, void *aux)
 	mac_intr_establish(parent, ca->ca_intr[0], IST_LEVEL, IPL_HIGH,
 	    gpio_intr, sc, sc->sc_dev.dv_xname);
 
-	printf(" irq %d\n", ca->ca_intr[0]);
+	printf(": irq %d\n", ca->ca_intr[0]);
 }
 
 #if NADB > 0
