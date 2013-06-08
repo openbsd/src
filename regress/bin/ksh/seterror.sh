d73392faed85e3ab5cd25c2df5045fbb8a3c3fa3
millert <millert@openbsd.org>
2013-06-08 21:36:50 +0000

    POSIX specifies that for an AND/OR list, only the last command's exit status matters for "set -e".  OK espie@

--- regress/bin/ksh/seterror.sh
+++ regress/bin/ksh/seterror.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $OpenBSD: seterror.sh,v 1.2 2009/01/29 23:27:26 jaredy Exp $
+# $OpenBSD: seterror.sh,v 1.3 2013/06/08 21:36:50 millert Exp $
 
 # set -e is supposed to abort the script for errors that are not caught
 # otherwise.
@@ -11,8 +11,8 @@ if true; then if true; then false && false; fi; fi
 
 for i in 1 2 3
 do
-	true && false
-	false || false
+	false && true
+	true || false
 done
 
 ! true | false
