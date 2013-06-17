4167e41ced9a2ad26acbd7d1eb84a43384c1b59b
schwarze <schwarze@openbsd.org>
2013-06-17 20:04:21 +0000

    Do not misinterpret "--" at the end of a NAME section input line as hyphenation.  Fixes parsing of some manuals preformatted with outlandish versions of groff.
    Issue found by kerberos-robert@ in krb5_appdefault_time.0
    
    Okay espie@

--- libexec/makewhatis/OpenBSD/Makewhatis/Formated.pm
+++ libexec/makewhatis/OpenBSD/Makewhatis/Formated.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Formated.pm,v 1.7 2013/01/29 11:08:56 espie Exp $
+# $OpenBSD: Formated.pm,v 1.8 2013/06/17 20:04:21 schwarze Exp $
 # Copyright (c) 2000-2004 Marc Espie <espie@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -117,7 +117,7 @@ sub handle
 		    s/^\s*//;
 		}
 		# more troff hyphenation
-		if (defined $subject and $subject =~ m/\S(?:\-\cH)*\-$/) {
+		if (defined $subject and $subject =~ m/[^\s-](?:\-\cH)*\-$/) {
 		    $subject =~ s/(?:\-\cH)*\-$//;
 		    s/^\s*//;
 		}
