b8137792690cc721a3114c6cef83de2617324d0e
espie <espie@openbsd.org>
2013-07-05 05:41:09 +0000

    implement floorf and ceilf: same technique as floor and ceil, just with appropriate constant to the mantissa size (and document what L is along the way).
    okay martynas@, tested by miod@ for results

--- lib/libm/noieee_src/n_floor.c
+++ lib/libm/noieee_src/n_floor.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: n_floor.c,v 1.17 2013/03/28 18:09:38 martynas Exp $	*/
+/*	$OpenBSD: n_floor.c,v 1.18 2013/07/05 05:41:09 espie Exp $	*/
 /*	$NetBSD: n_floor.c,v 1.1 1995/10/10 23:36:48 ragge Exp $	*/
 /*
  * Copyright (c) 1985, 1993
@@ -33,7 +33,8 @@
 
 #include "mathimpl.h"
 
-static const double L = 36028797018963968.0E0;
+static const double L = 36028797018963968.0E0;	/* 2**55 */
+static const float F = 8388608E0f;		/* 2**23 */
 
 /*
  * floor(x) := the largest integer no larger than x;
@@ -74,6 +75,37 @@ ceil(double x)
 	}
 }
 
+float
+floorf(float x)
+{
+	volatile float y;
+
+	if (isnanf(x) || x >= F)	/* already an even integer */
+		return x;
+	else if (x < (float)0)
+		return -ceilf(-x);
+	else {			/* now 0 <= x < F */
+		y = F+x;		/* destructive store must be forced */
+		y -= F;			/* an integer, and |x-y| < 1 */
+		return x < y ? y-(float)1 : y;
+	}
+}
+
+float
+ceilf(float x)
+{
+	volatile float y;
+
+	if (isnanf(x) || x >= F)	/* already an even integer */
+		return x;
+	else if (x < (float)0)
+		return -floorf(-x);
+	else {			/* now 0 <= x < F */
+		y = F+x;		/* destructive store must be forced */
+		y -= F;			/* an integer, and |x-y| < 1 */
+		return x > y ? y+(float)1 : y;
+	}
+}
 /*
  * algorithm for rint(x) in pseudo-pascal form ...
  *
