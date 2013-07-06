2bcbef651d7590ee7d0f5601a653500785f21f31
espie <espie@openbsd.org>
2013-07-06 23:06:36 +0000

    implement some simple check that make sure typedefs don't change in a bad way on us.
    Prompted by some recent work on types.h by guenther, to be fleshed
    out as people require.

---
+++ regress/misc/c++abi/nm1.C
@@ -0,0 +1,31 @@
+/* $OpenBSD: nm1.C,v 1.1 2013/07/06 23:06:36 espie Exp $ */
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define D(T)	void witness_##T(T) {}
+
+D(cpuid_t)
+D(dev_t)
+D(fixpt_t)
+D(fsblkcnt_t)
+D(gid_t)
+D(id_t)
+D(in_addr_t)
+D(in_port_t)
+D(ino_t)
+D(key_t)
+D(mode_t)
+D(nlink_t)
+D(pid_t)
+D(rlim_t)
+D(sa_family_t)
+D(segsz_t)
+D(socklen_t)
+D(suseconds_t)
+D(swblk_t)
+D(uid_t)
+D(uint64_t)
+D(uint32_t)
+D(size_t)
+D(off_t)
+D(useconds_t)
