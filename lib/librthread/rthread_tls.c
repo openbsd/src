ff687106fc277670f790b38db9a1f004631c43b9
guenther <guenther@openbsd.org>
2013-07-30 16:19:33 +0000

    Free the list used for tracking the pthread_{get,set}specific() data after calling all the destructors.
    noted by kettenis@
    diff by tedu@

--- lib/librthread/rthread_tls.c
+++ lib/librthread/rthread_tls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rthread_tls.c,v 1.14 2013/06/01 20:47:40 tedu Exp $ */
+/*	$OpenBSD: rthread_tls.c,v 1.15 2013/07/30 16:19:33 guenther Exp $ */
 /*
  * Copyright (c) 2004,2005 Ted Unangst <tedu@openbsd.org>
  * All Rights Reserved.
@@ -175,6 +175,10 @@ _rthread_tls_destructors(pthread_t thread)
 				_spinlock(&rkeyslock);
 			}
 		}
+	}
+	for (rs = thread->local_storage; rs; rs = thread->local_storage) {
+		thread->local_storage = rs->next;
+		free(rs);
 	}
 	_spinunlock(&rkeyslock);
 }
