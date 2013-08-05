5ab22b455965cf9ec958c2b926f13b591e4803dc
kettenis <kettenis@openbsd.org>
2013-08-05 18:13:19 +0000

    tgamma(+-0) == +-Inf
    ok martynas@

--- lib/libm/src/ld80/e_tgammal.c
+++ lib/libm/src/ld80/e_tgammal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: e_tgammal.c,v 1.2 2011/07/20 21:02:51 martynas Exp $	*/
+/*	$OpenBSD: e_tgammal.c,v 1.3 2013/08/05 18:13:19 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2008 Stephen L. Moshier <steve@moshier.net>
@@ -229,6 +229,8 @@ if(x == INFINITY)
 	return(INFINITY);
 if(x == -INFINITY)
 	return(x - x);
+if( x == 0.0L )
+	return( 1.0L / x );
 q = fabsl(x);
 
 if( q > 13.0L )
