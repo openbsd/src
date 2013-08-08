28079d48c54d89d60ff9f00a57e55a097194fde1
kettenis <kettenis@openbsd.org>
2013-08-08 21:14:23 +0000

    Add PMAP_NOCACHE as an alias for PMAP_NC for use in semi-MI code.

--- sys/arch/sparc64/include/pmap.h
+++ sys/arch/sparc64/include/pmap.h
@@ -135,6 +135,7 @@ struct prom_map {
 
 #define PMAP_NC		0x001	/* Set the E bit in the page */
 #define PMAP_NVC	0x002	/* Don't enable the virtual cache */
+#define PMAP_NOCACHE	PMAP_NC
 #define PMAP_LITTLE	0x004	/* Map in little endian mode */
 /* Large page size hints -- we really should use another param to pmap_enter() */
 #define PMAP_8K		0x000
