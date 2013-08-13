c9f4afa883c7d34e9b9ee410a33b1fceb0014f10
sthen <sthen@openbsd.org>
2013-08-13 09:36:22 +0000

    addition for 64bit time_t

--- usr.bin/mg/theo.c
+++ usr.bin/mg/theo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: theo.c,v 1.125 2013/05/30 20:15:40 florian Exp $	*/
+/*	$OpenBSD: theo.c,v 1.126 2013/08/13 09:36:22 sthen Exp $	*/
 /*
  * Copyright (c) 2002 Artur Grabowski <art@openbsd.org>
  * All rights reserved.
@@ -167,7 +167,8 @@ static const char *talk[] = {
 	"if you assign that responsibility to non-hikers I will walk over and cripple you now.",
 	"i ojbect two yoru splelng of achlhlocis.",
 	"We have two kinds of developers - those that deal with their own shit and those that deal with other people's shit.",
-	"If people keep adding such huge stuff, soon mg will be bigger than emacs."
+	"If people keep adding such huge stuff, soon mg will be bigger than emacs.",
+	"this change comes down to: This year, next year, 5 years from now, 10 years from now, or Oh fuck."
 };
 
 static const int ntalk = sizeof(talk)/sizeof(talk[0]);
