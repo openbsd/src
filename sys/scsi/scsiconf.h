f94c91d179ba19edac3fd0f93dfbdbc5fc06137a
krw <krw@openbsd.org>
2013-09-27 11:43:19 +0000

    scsi_size() is now used only by cd(4). So move it from scsi_base.c to cd.c and call it cd_size(), like sd_size() lives in sd.c.
    Tweak some daddr_t variables to u_int64_t on the way, when they are
    for disk sector numbers, not 512-byte block numbers.

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: scsiconf.h,v 1.156 2013/08/29 02:54:37 dlg Exp $	*/
+/*	$OpenBSD: scsiconf.h,v 1.157 2013/09/27 11:43:19 krw Exp $	*/
 /*	$NetBSD: scsiconf.h,v 1.35 1997/04/02 02:29:38 mycroft Exp $	*/
 
 /*
@@ -472,7 +472,6 @@ const void *scsi_inqmatch(struct scsi_inquiry_data *, const void *, int,
     workq_add_task(NULL, (_fl), (_f), (_a1), (_a2))
 
 void	scsi_init(void);
-daddr_t scsi_size(struct scsi_link *, int, u_int32_t *);
 int	scsi_test_unit_ready(struct scsi_link *, int, int);
 int	scsi_inquire(struct scsi_link *, struct scsi_inquiry_data *, int);
 int	scsi_inquire_vpd(struct scsi_link *, void *, u_int, u_int8_t, int);
