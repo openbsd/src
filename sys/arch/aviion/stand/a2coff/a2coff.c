72178805754f3bc867c588be0281881934224638
miod <miod@openbsd.org>
2013-10-08 21:49:40 +0000

    Try to be a bit more cross-compile friendly.

--- sys/arch/aviion/stand/a2coff/a2coff.c
+++ sys/arch/aviion/stand/a2coff/a2coff.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: a2coff.c,v 1.5 2013/09/21 21:00:02 miod Exp $	*/
+/*	$OpenBSD: a2coff.c,v 1.6 2013/10/08 21:49:40 miod Exp $	*/
 /*
  * Copyright (c) 2006, 2013, Miodrag Vallat
  *
@@ -47,6 +47,8 @@
 #undef	__LDPGSZ
 #define	__LDPGSZ	0x1000
 #endif	/* m88k */
+
+#define	ELFSIZE		32
 #include <sys/exec_elf.h>
 
 #define	ECOFF_ALIGN	0x200
