4f8ec555fa7e6cc2fa965e7a6a2411079ba43949
mpi <mpi@openbsd.org>
2013-10-09 09:44:19 +0000

    Fix a possible mbuf leak, from NetBSD via Loganaden Velvindron.
    ok claudio@, henning@

--- sys/netinet6/ip6_mroute.c
+++ sys/netinet6/ip6_mroute.c
@@ -503,7 +503,7 @@ ip6_mrouter_done(void)
 			for (rte = rt->mf6c_stall; rte != NULL; ) {
 				struct rtdetq *n = rte->next;
 
-				m_free(rte->m);
+				m_freem(rte->m);
 				free(rte, M_MRTABLE);
 				rte = n;
 			}
