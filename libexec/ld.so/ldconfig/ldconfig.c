f077f2b9afbc27e44fc1712c3dd23e8027971fe5
deraadt <deraadt@openbsd.org>
2013-10-18 14:44:39 +0000

    does not need stab.h

--- libexec/ld.so/ldconfig/ldconfig.c
+++ libexec/ld.so/ldconfig/ldconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldconfig.c,v 1.29 2012/09/11 21:20:14 espie Exp $	*/
+/*	$OpenBSD: ldconfig.c,v 1.30 2013/10/18 14:44:39 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1993,1995 Paul Kranenburg
@@ -45,7 +45,6 @@
 #include <ar.h>
 #include <ranlib.h>
 #include <a.out.h>
-#include <stab.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
