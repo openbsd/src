ea7f633ca299f11ee7e9df86135513f45a3bc43d
deraadt <deraadt@openbsd.org>
2013-10-19 14:50:21 +0000

    remove confusing comments referencing the vax

--- sys/netinet/in_systm.h
+++ sys/netinet/in_systm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_systm.h,v 1.5 2003/06/02 23:28:14 millert Exp $	*/
+/*	$OpenBSD: in_systm.h,v 1.6 2013/10/19 14:50:21 deraadt Exp $	*/
 /*	$NetBSD: in_systm.h,v 1.8 1995/04/13 06:29:22 cgd Exp $	*/
 
 /*
@@ -35,26 +35,13 @@
 #ifndef _NETINET_IN_SYSTM_H_
 #define _NETINET_IN_SYSTM_H_
 
-/*
- * Miscellaneous internetwork
- * definitions for kernel.
- */
-
-/*
- * Network types.
- *
- * Internally the system keeps counters in the headers with the bytes
- * swapped so that VAX instructions will work on them.  It reverses
- * the bytes before transmission at each protocol level.  The n_ types
- * represent the types with the bytes in ``high-ender'' order.
- */
-typedef u_int16_t n_short;		/* short as received from the net */
-typedef u_int32_t n_long;		/* long as received from the net */
-
-typedef u_int32_t n_time;		/* ms since 00:00 GMT, byte rev */
+/* network byte order */
+typedef u_int16_t n_short;
+typedef u_int32_t n_long;
+typedef u_int32_t n_time;		/* ms since 00:00 GMT */
 
 #ifdef _KERNEL
 n_time	 iptime(void);
 #endif /* _KERNEL */
-#endif /* _NETINET_IN_SYSTM_H_ */
 
+#endif /* _NETINET_IN_SYSTM_H_ */
