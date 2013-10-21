a7055e1f89c0e3b222eb7d13968f720d46d8dc53
phessler <phessler@openbsd.org>
2013-10-21 09:02:37 +0000

    Allow a user to switch to rdomain zero

--- usr.bin/telnet/commands.c
+++ usr.bin/telnet/commands.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: commands.c,v 1.53 2011/06/21 17:31:07 mikeb Exp $	*/
+/*	$OpenBSD: commands.c,v 1.54 2013/10/21 09:02:37 phessler Exp $	*/
 /*	$NetBSD: commands.c,v 1.14 1996/03/24 22:03:48 jtk Exp $	*/
 
 /*
@@ -2396,11 +2396,9 @@ tn(argc, argv)
 	if (net < 0)
 	    continue;
 
-	if (rtableid) {
-		if (setsockopt(net, SOL_SOCKET, SO_RTABLE, &rtableid,
-		    sizeof(rtableid)) == -1)
-			perror("setsockopt (SO_RTABLE)");
-	}
+	if (setsockopt(net, SOL_SOCKET, SO_RTABLE, &rtableid,
+	    sizeof(rtableid)) == -1)
+		perror("setsockopt (SO_RTABLE)");
 
 	if (aliasp) {
 	    struct addrinfo ahints, *ares;
