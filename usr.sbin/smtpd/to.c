d699b7bdb09b0bb43702fefe4405706c3f9c3bff
eric <eric@openbsd.org>
2013-10-28 10:32:17 +0000

    handle the case where the filter string is quoted

--- usr.sbin/smtpd/to.c
+++ usr.sbin/smtpd/to.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: to.c,v 1.10 2013/10/26 20:55:09 eric Exp $	*/
+/*	$OpenBSD: to.c,v 1.11 2013/10/28 10:32:17 eric Exp $	*/
 
 /*
  * Copyright (c) 2009 Jacek Masiulaniec <jacekm@dobremiasto.net>
@@ -693,14 +693,26 @@ expandnode_to_text(struct expandnode *expandnode)
 static int
 alias_is_filter(struct expandnode *alias, const char *line, size_t len)
 {
-	if (*line == '|') {
-		if (strlcpy(alias->u.buffer, line + 1,
-			sizeof(alias->u.buffer)) >= sizeof(alias->u.buffer))
+	int	v = 0;
+
+	if (*line == '"')
+		v = 1;
+	if (*(line+v) == '|') {
+		if (strlcpy(alias->u.buffer, line + v + 1,
+		    sizeof(alias->u.buffer)) >= sizeof(alias->u.buffer))
 			return 0;
+		if (v) {
+			v = strlen(alias->u.buffer);
+			if (v == 0)
+				return (0);
+			if (alias->u.buffer[v-1] != '"')
+				return (0);
+			alias->u.buffer[v-1] = '\0';
+		}
 		alias->type = EXPAND_FILTER;
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
 static int
