f5fb5798b8f23653d5c4440e16bd84e115564c51
miod <miod@openbsd.org>
2013-11-01 07:43:09 +0000

    Better report of the system type, to match the OMRON spelling; aoyama@

--- sys/arch/luna88k/stand/boot/init_main.c
+++ sys/arch/luna88k/stand/boot/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.4 2013/10/29 22:13:28 miod Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.5 2013/11/01 07:43:09 miod Exp $	*/
 /*	$NetBSD: init_main.c,v 1.6 2013/03/05 15:34:53 tsutsui Exp $	*/
 
 /*
@@ -171,17 +171,17 @@ main(void)
 	 * Initialize the console before we print anything out.
 	 */
 	if (machtype == LUNA_88K) {
-		machstr  = "luna88k";
+		machstr  = "LUNA-88K";
 		cpuspeed = MHZ_25;
 	} else {
-		machstr  = "luna88k-2";
+		machstr  = "LUNA-88K2";
 		cpuspeed = MHZ_33;
 	}
 
 	nplane = get_plane_numbers();
 	cninit();
 
-	printf("\nOpenBSD/%s boot 0.3\n\n", machstr);
+	printf("\nOpenBSD/" MACHINE " (%s) boot 0.3\n\n", machstr);
 
 #ifdef SUPPORT_ETHERNET
 	try_bootp = 1;
