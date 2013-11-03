0265d62142526d1090da7d93d1faadb542f655ab
rapha <rapha@openbsd.org>
2013-11-03 14:08:56 +0000

    Include "gpio.h"
    ok jasper@

--- sys/arch/armv7/sunxi/sxipio.c
+++ sys/arch/armv7/sunxi/sxipio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sxipio.c,v 1.3 2013/10/31 17:15:19 jasper Exp $	*/
+/*	$OpenBSD: sxipio.c,v 1.4 2013/11/03 14:08:56 rapha Exp $	*/
 /*
  * Copyright (c) 2010 Miodrag Vallat.
  * Copyright (c) 2013 Artturi Alm
@@ -29,6 +29,8 @@
 
 #include <armv7/sunxi/sunxivar.h>
 #include <armv7/sunxi/sxipiovar.h>
+
+#include "gpio.h"
 
 #define	SXIPIO_NPORT		9
 #define	SXIPIO_PA_NPIN		18
