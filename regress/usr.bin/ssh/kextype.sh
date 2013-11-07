25f6e71af0269fb67fb45097832254b209a11af8
dtucker <dtucker@openbsd.org>
2013-11-07 04:26:56 +0000

    trailing space

--- regress/usr.bin/ssh/kextype.sh
+++ regress/usr.bin/ssh/kextype.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: kextype.sh,v 1.3 2013/11/07 03:55:41 dtucker Exp $
+#	$OpenBSD: kextype.sh,v 1.4 2013/11/07 04:26:56 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="login with different key exchange algorithms"
@@ -8,7 +8,7 @@ cp $OBJ/sshd_proxy $OBJ/sshd_proxy_bak
 cp $OBJ/ssh_proxy $OBJ/ssh_proxy_bak
 
 tries="1 2 3 4"
-for k in `${SSH} -Q kex`; do 
+for k in `${SSH} -Q kex`; do
 	verbose "kex $k"
 	for i in $tries; do
 		${SSH} -F $OBJ/ssh_proxy -o KexAlgorithms=$k x true
