c416fe4b6f6399fb74e119d7c2ca55bfedcbdfbc
deraadt <deraadt@openbsd.org>
2013-11-12 05:04:29 +0000

    a missing prototypes and don't act like an unknown vnode type is "ok"

--- lib/libkvm/kvm_file2.c
+++ lib/libkvm/kvm_file2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kvm_file2.c,v 1.28 2013/10/22 16:40:26 guenther Exp $	*/
+/*	$OpenBSD: kvm_file2.c,v 1.29 2013/11/12 05:04:29 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2009 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -122,6 +122,8 @@ static struct kinfo_file *kvm_deadfile_byid(kvm_t *, int, int,
 static int fill_file(kvm_t *, struct kinfo_file *, struct file *, u_long,
     struct vnode *, struct proc *, int, pid_t);
 static int filestat(kvm_t *, struct kinfo_file *, struct vnode *);
+
+mode_t	_kvm_getftype(enum vtype v_type);
 
 LIST_HEAD(proclist, proc);
 
@@ -899,6 +901,9 @@ filestat(kvm_t *kd, struct kinfo_file *kf, struct vnode *vp)
 		case VT_NON:
 			if (vp->v_flag & VCLONE)
 				ret = spec_filestat(kd, kf, vp);
+			break;
+		default:
+			ret = -1;
 			break;
 		}
 	}
