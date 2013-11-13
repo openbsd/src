7e8c0af04431f59c0d954ae28d0240d6724053ff
bluhm <bluhm@openbsd.org>
2013-11-13 23:13:04 +0000

    Add a check-setup target to make sure that all addresses and routes are configured correctly.  Add a scapy test that creates a packet without routing header to check the setup.

---
+++ regress/sys/netinet6/rh0/rh0_none.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python2.7
+# send a ping6 packet without routing header type 0
+# we expect an echo reply, as there is no routing header
+
+import os
+from addr import *
+from scapy.all import *
+
+pid=os.getpid()
+payload="ABCDEFGHIJKLMNOP"
+packet=IPv6(src=SRC_OUT6, dst=DST_IN6)/\
+    ICMPv6EchoRequest(id=pid, data=payload)
+eth=Ether(src=SRC_MAC, dst=DST_MAC)/packet
+
+if os.fork() == 0:
+	time.sleep(1)
+	sendp(eth, iface=SRC_IF)
+	os._exit(0)
+
+ans=sniff(iface=SRC_IF, timeout=3, filter=
+    "ip6 and dst "+SRC_OUT6+" and icmp6")
+for a in ans:
+	if a and a.type == scapy.layers.dot11.ETHER_TYPES.IPv6 and \
+	    ipv6nh[a.payload.nh] == 'ICMPv6' and \
+	    icmp6types[a.payload.payload.type] == 'Echo Reply':
+		reply=a.payload.payload
+		id=reply.id
+		print "id=%#x" % (id)
+		if id != pid:
+			print "WRONG ECHO REPLY ID"
+			exit(2)
+		data=reply.data
+		print "payload=%s" % (data)
+		if data != payload:
+			print "WRONG PAYLOAD"
+			exit(2)
+		exit(0)
+print "NO ICMP6 ECHO REPLY"
+exit(1)
