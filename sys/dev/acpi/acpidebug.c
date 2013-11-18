e4da60bba89dc641a2d073b143cd98d805b6c943
deraadt <deraadt@openbsd.org>
2013-11-18 23:44:57 +0000

    format string cleanup

--- sys/dev/acpi/acpidebug.c
+++ sys/dev/acpi/acpidebug.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpidebug.c,v 1.27 2010/07/21 19:35:15 deraadt Exp $ */
+/* $OpenBSD: acpidebug.c,v 1.28 2013/11/18 23:44:57 deraadt Exp $ */
 /*
  * Copyright (c) 2006 Marco Peereboom <marco@openbsd.org>
  *
@@ -304,7 +304,7 @@ void db_disprint(void *arg, const char *fmt, ...)
 	vsnprintf(stre, sizeof(stre), fmt, ap);
 	va_end(ap);
 
-	db_printf(stre);
+	db_printf("%s", stre);
 }
 
 void
