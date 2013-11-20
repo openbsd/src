1b878602b8a2e14ff016da6c5487569615c07b43
deraadt <deraadt@openbsd.org>
2013-11-20 21:00:33 +0000

    str derives from argv; so use an unsigned char cast for isspace() ok ratchov

--- usr.bin/w/w.c
+++ usr.bin/w/w.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: w.c,v 1.53 2013/10/31 02:00:12 deraadt Exp $	*/
+/*	$OpenBSD: w.c,v 1.54 2013/11/20 21:00:33 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1991, 1993, 1994
@@ -350,7 +350,8 @@ pr_args(struct kinfo_proc *kp)
 			if ((str = strchr(*argv + 5, ':')) != NULL)
 				str = strchr(str + 1, ':');
 			if (str != NULL) {
-				if ((str[0] == ':') && isspace(str[1]))
+				if ((str[0] == ':') &&
+				    isspace((unsigned char)str[1]))
 					str += 2;
 				fmt_puts(str, &left);
 			} else
