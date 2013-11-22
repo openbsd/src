b0c34719d23583e4139a8a06a014c4275a40bcab
espie <espie@openbsd.org>
2013-11-22 15:43:18 +0000

    remove unused goo, okay guenther@

--- share/mk/bsd.obj.mk
+++ share/mk/bsd.obj.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.obj.mk,v 1.13 2007/05/01 08:31:13 espie Exp $
+#	$OpenBSD: bsd.obj.mk,v 1.14 2013/11/22 15:43:18 espie Exp $
 #	$NetBSD: bsd.obj.mk,v 1.9 1996/04/10 21:08:05 thorpej Exp $
 
 .if !target(obj)
@@ -7,27 +7,9 @@ obj:
 .  else
 
 .  if defined(MAKEOBJDIR)
-__baseobjdir=	${MAKEOBJDIR}
+__objdir=	${MAKEOBJDIR}
 .  else
-__baseobjdir=	obj
-.  endif
-
-.  if defined(OBJMACHINE)
-__objdir=	${__baseobjdir}.${MACHINE}
-.  else
-__objdir=	${__baseobjdir}
-.  endif
-
-.  if defined(USR_OBJMACHINE)
-__usrobjdir=	${BSDOBJDIR}.${MACHINE}
-__usrobjdirpf=	
-.  else
-__usrobjdir=	${BSDOBJDIR}
-.    if defined(OBJMACHINE)
-__usrobjdirpf=	.${MACHINE}
-.    else
-__usrobjdirpf=
-.    endif
+__objdir=	obj
 .  endif
 
 _SUBDIRUSE:
@@ -37,7 +19,7 @@ obj! _SUBDIRUSE
 	here=`/bin/pwd`; bsdsrcdir=`cd ${BSDSRCDIR}; /bin/pwd`; \
 	subdir=$${here#$${bsdsrcdir}/}; \
 	if test $$here != $$subdir ; then \
-		dest=${__usrobjdir}/$$subdir${__usrobjdirpf} ; \
+		dest=${BSDOBJDIR}/$$subdir ; \
 		echo "$$here/${__objdir} -> $$dest"; \
 		if test ! -L ${__objdir} -o \
 		    X`readlink ${__objdir}` != X$$dest; \
@@ -45,13 +27,13 @@ obj! _SUBDIRUSE
 			if test -e ${__objdir}; then rm -rf ${__objdir}; fi; \
 			ln -sf $$dest ${__objdir}; \
 		fi; \
-		if test -d ${__usrobjdir}; then \
+		if test -d ${BSDOBJDIR}; then \
 			test -d $$dest || mkdir -p $$dest; \
 		else \
-			if test -e ${__usrobjdir}; then \
-				echo "${__usrobjdir} is not a directory"; \
+			if test -e ${BSDOBJDIR}; then \
+				echo "${BSDOBJDIR} is not a directory"; \
 			else \
-				echo "${__usrobjdir} does not exist"; \
+				echo "${BSDOBJDIR} does not exist"; \
 			fi; \
 		fi; \
 	else \
