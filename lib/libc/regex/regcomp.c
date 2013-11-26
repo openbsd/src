9596d54da03efb1970383a8fb2db9d5a8f7821dd
deraadt <deraadt@openbsd.org>
2013-11-26 13:16:18 +0000

    unsigned char cast for ctype; ok guenther

--- lib/libc/regex/regcomp.c
+++ lib/libc/regex/regcomp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: regcomp.c,v 1.22 2013/04/17 17:39:29 tedu Exp $ */
+/*	$OpenBSD: regcomp.c,v 1.23 2013/11/26 13:16:18 deraadt Exp $ */
 /*-
  * Copyright (c) 1992, 1993, 1994 Henry Spencer.
  * Copyright (c) 1992, 1993, 1994
@@ -759,7 +759,7 @@ p_b_cclass(struct parse *p, cset *cs)
 	char *u;
 	char c;
 
-	while (MORE() && isalpha(PEEK()))
+	while (MORE() && isalpha((uch)PEEK()))
 		NEXT();
 	len = p->next - sp;
 	for (cp = cclasses; cp->name != NULL; cp++)
