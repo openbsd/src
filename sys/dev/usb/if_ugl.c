da28f5dbdad1ceba05fc439155c95dc736909397
sasano <sasano@openbsd.org>
2013-12-03 21:06:59 +0000

    add copyright notice/code modifier's name.
    ok by deraadt@

--- sys/dev/usb/if_ugl.c
+++ sys/dev/usb/if_ugl.c
@@ -1,5 +1,21 @@
-/*	$OpenBSD: if_ugl.c,v 1.1 2013/11/15 19:42:20 sasano Exp $	*/
+/*	$OpenBSD: if_ugl.c,v 1.2 2013/12/03 21:06:59 sasano Exp $	*/
 /*	$NetBSD: if_upl.c,v 1.19 2002/07/11 21:14:26 augustss Exp $	*/
+/*
+ * Copyright (c) 2013 SASANO Takayoshi <uaa@uaa.org.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 /*
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
