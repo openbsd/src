1daa5381dd067f37e48dd2b615ed036118549ba2
mikeb <mikeb@openbsd.org>
2013-12-04 16:27:56 +0000

    ifp must be initialized in ip_setmoptions;  from oga via mpi, ok mpi

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_output.c,v 1.251 2013/11/27 08:37:08 mpi Exp $	*/
+/*	$OpenBSD: ip_output.c,v 1.252 2013/12/04 16:27:56 mikeb Exp $	*/
 /*	$NetBSD: ip_output.c,v 1.28 1996/02/13 23:43:07 christos Exp $	*/
 
 /*
@@ -1704,7 +1704,7 @@ ip_setmoptions(int optname, struct ip_moptions **imop, struct mbuf *m,
 	struct in_addr addr;
 	struct in_ifaddr *ia;
 	struct ip_mreq *mreq;
-	struct ifnet *ifp;
+	struct ifnet *ifp = NULL;
 	struct ip_moptions *imo = *imop;
 	struct in_multi **immp;
 	struct route ro;
