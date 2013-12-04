5dd2055ac7cc377116eadb4a34ea51a3f51707a6
syl <syl@openbsd.org>
2013-12-04 12:31:14 +0000

    Looks like the eMMC does no like block read request lower than block size. Rever the bsize to default 512 to fix the problem while a proper fix is worked on.
    Spotted by rapha@
    ok rapha@, fgsch@

--- sys/arch/armv7/omap/ommmc.c
+++ sys/arch/armv7/omap/ommmc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ommmc.c,v 1.11 2013/11/12 17:51:52 fgsch Exp $	*/
+/*	$OpenBSD: ommmc.c,v 1.12 2013/12/04 12:31:14 syl Exp $	*/
 
 /*
  * Copyright (c) 2009 Dale Rahn <drahn@openbsd.org>
@@ -381,6 +381,10 @@ ommmc_attach(struct device *parent, struct device *self, void *args)
 		printf("invalid capability blocksize in capa %08x,"
 		    " trying 512\n", HREAD4(sc, MMCHS_CAPA));
 	}
+	/*
+	 * MMC does not support blksize > 512 yet
+	 */
+	sc->maxblklen = 512;
 	/*
 	 * Attach the generic SD/MMC bus driver.  (The bus driver must
 	 * not invoke any chipset functions before it is attached.)
