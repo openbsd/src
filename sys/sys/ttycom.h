0919e16ba69b4dc259a8856211f3177724f51a13
naddy <naddy@openbsd.org>
2013-12-15 21:09:48 +0000

    update comment, 4.3BSD tty compatibility and the associated ioctls are gone

--- sys/sys/ttycom.h
+++ sys/sys/ttycom.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ttycom.h,v 1.11 2009/05/06 18:21:23 stevesk Exp $	*/
+/*	$OpenBSD: ttycom.h,v 1.12 2013/12/15 21:09:48 naddy Exp $	*/
 /*	$NetBSD: ttycom.h,v 1.4 1996/05/19 17:17:53 jonathan Exp $	*/
 
 /*-
@@ -42,10 +42,7 @@
 
 #include <sys/ioccom.h>
 
-/*
- * Tty ioctl's except for those supported only for backwards compatibility
- * with the old tty driver.
- */
+/* Tty ioctl's. */
 
 /*
  * Window/terminal size structure.  This information is stored by the kernel
