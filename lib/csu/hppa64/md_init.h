8f8cbdf6c7737375a267f68d9ef40e67084b4c3c
tobiasu <tobiasu@openbsd.org>
2013-12-20 16:23:43 +0000

    Fix typo, allow hppa64 to build a little further

--- lib/csu/hppa64/md_init.h
+++ lib/csu/hppa64/md_init.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: md_init.h,v 1.4 2013/12/03 06:21:41 guenther Exp $ */
+/* $OpenBSD: md_init.h,v 1.5 2013/12/20 16:23:43 tobiasu Exp $ */
 
 /*
  * Copyright (c) 2003 Dale Rahn. All rights reserved.
@@ -77,7 +77,7 @@
 	"	.label _start					\n" \
 	"	.label __start					\n" \
 	"	.proc						\n" \
-	"	.callin_fo frame=0, calls			\n" \
+	"	.callinfo frame=0, calls			\n" \
 	"	.entry						\n" \
 	"	ldil	L%__gp, %r27				\n" \
 	"	.call						\n" \
