05f91f63a37ef5a1f362a08c7e373afd283ded3e
kettenis <kettenis@openbsd.org>
2013-12-23 10:48:43 +0000

    Revert previous commit.  It makes resume hang on some MP systems running GENERIC.MP.

--- sys/dev/acpi/acpi.c
+++ sys/dev/acpi/acpi.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpi.c,v 1.249 2013/12/16 19:11:52 kettenis Exp $ */
+/* $OpenBSD: acpi.c,v 1.250 2013/12/23 10:48:43 kettenis Exp $ */
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  * Copyright (c) 2005 Jordan Hargrave <jordan@openbsd.org>
@@ -30,7 +30,6 @@
 #include <sys/workq.h>
 #include <sys/sched.h>
 #include <sys/reboot.h>
-#include <sys/sysctl.h>
 
 #ifdef HIBERNATE
 #include <sys/hibernate.h>
@@ -2085,7 +2084,6 @@ acpi_indicator(struct acpi_softc *sc, int led_state)
 int
 acpi_sleep_state(struct acpi_softc *sc, int state)
 {
-	extern int perflevel;
 	int error = ENXIO;
 	int s;
 
@@ -2185,10 +2183,6 @@ fail_quiesce:
 #if NWSDISPLAY > 0
 	wsdisplay_resume();
 #endif /* NWSDISPLAY > 0 */
-
-	/* Restore hw.setperf */
-	if (cpu_setperf != NULL)
-		cpu_setperf(perflevel);
 
 	acpi_record_event(sc, APM_NORMAL_RESUME);
 	acpi_indicator(sc, ACPI_SST_WORKING);
