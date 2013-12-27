53d9ab368510d7e7acb42757d4cb314f6f2b4868
deraadt <deraadt@openbsd.org>
2013-12-27 21:40:16 +0000

    correct comment about why first 16 pages are skipped: SMI corruption is the real cause discussed with mlarkin

--- sys/arch/i386/i386/machdep.c
+++ sys/arch/i386/i386/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.528 2013/11/01 17:36:19 krw Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.529 2013/12/27 21:40:16 deraadt Exp $	*/
 /*	$NetBSD: machdep.c,v 1.214 1996/11/10 03:16:17 thorpej Exp $	*/
 
 /*-
@@ -3220,7 +3220,7 @@ init386(paddr_t first_avail)
 				e = 0xfffff000;
 			}
 
-			/* skip first 16 pages for tramps and hibernate */
+			/* skip first 16 pages for due to SMI corruption */
 			if (a < 16 * NBPG)
 				a = 16 * NBPG;
 
