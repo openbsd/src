1e345a1a3abcc7b7ac957277ef98c80a7945f28b
jsing <jsing@openbsd.org>
2013-12-28 12:03:57 +0000

    Add installboot support for vax.

---
+++ usr.sbin/installboot/vax/vax_installboot.c
@@ -0,0 +1,38 @@
+/*	$OpenBSD: vax_installboot.c,v 1.1 2013/12/28 12:03:57 jsing Exp $	*/
+
+/*
+ * Copyright (c) 2013 Joel Sing <jsing@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "installboot.h"
+
+void
+md_init(void)
+{
+	stages = 2;
+	stage1 = "/usr/mdec/xxboot";
+	stage2 = "/boot";
+}
+
+void
+md_loadboot(void)
+{
+}
+
+void
+md_installboot(int devfd, char *dev)
+{
+	bootstrap(devfd, dev, stage1);
+}
