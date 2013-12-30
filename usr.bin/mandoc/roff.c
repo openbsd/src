c702e99688b461af100de6de1277dfe901fa7dff
schwarze <schwarze@openbsd.org>
2013-12-30 18:42:55 +0000

    Oops, missed one: Remove duplicate const specifier from a call to mandoc_escape(). Found by Thomas Klausner <wiz at NetBSD dot org> using clang. No functional change.

--- usr.bin/mandoc/roff.c
+++ usr.bin/mandoc/roff.c
@@ -1,4 +1,4 @@
-/*	$Id: roff.c,v 1.61 2013/12/25 00:50:03 schwarze Exp $ */
+/*	$Id: roff.c,v 1.62 2013/12/30 18:42:55 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010, 2011, 2012, 2013 Ingo Schwarze <schwarze@openbsd.org>
@@ -642,8 +642,7 @@ roff_parsetext(char **bufp, size_t *szp, int pos, int *offs)
 		if ('\\' == *p) {
 			/* Skip over escapes. */
 			p++;
-			esc = mandoc_escape
-				((const char const **)&p, NULL, NULL);
+			esc = mandoc_escape((const char **)&p, NULL, NULL);
 			if (ESCAPE_ERROR == esc)
 				break;
 			continue;
