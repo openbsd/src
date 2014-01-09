cc1d26938081653e73d897c56e87de782ca24661
tedu <tedu@openbsd.org>
2014-01-09 16:22:04 +0000

    test signing too

--- regress/usr.bin/signify/signify.sh
+++ regress/usr.bin/signify/signify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: signify.sh,v 1.1 2014/01/09 16:13:44 tedu Exp $
+# $OpenBSD: signify.sh,v 1.2 2014/01/09 16:22:04 tedu Exp $
 
 srcdir=$1
 
@@ -11,7 +11,12 @@ forgery="$srcdir/forgery.txt"
 
 set -e
 
+signify -s $seckey -o test.sig -S $orders 
+cmp test.sig "$orders.sig"
+rm test.sig
+
 signify -p $pubkey -V $orders > /dev/null
+
 signify -p $pubkey -V $forgery 2> /dev/null && exit 1
 
 true
