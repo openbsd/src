2778d96683e830d6c62d23013b1b48f949fc0d0d
guenther <guenther@openbsd.org>
2014-01-11 06:28:46 +0000

    optarg/optind are declared in <unistd.h>, so kill the externs here

--- games/fortune/fortune/fortune.c
+++ games/fortune/fortune/fortune.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fortune.c,v 1.30 2011/01/03 17:38:24 jasper Exp $	*/
+/*	$OpenBSD: fortune.c,v 1.31 2014/01/11 06:28:46 guenther Exp $	*/
 /*	$NetBSD: fortune.c,v 1.8 1995/03/23 08:28:40 cgd Exp $	*/
 
 /*-
@@ -265,8 +265,6 @@ getargs(int argc, char *argv[])
 # ifndef NO_REGEX
 	char	*pat = NULL;
 # endif	/* NO_REGEX */
-	extern char *optarg;
-	extern int optind;
 	int ch;
 
 	ignore_case = FALSE;
