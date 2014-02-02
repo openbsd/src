5326cabb03104aeb561ddfbc8d3a86991d88c68d
rpe <rpe@openbsd.org>
2014-02-02 09:32:00 +0000

    remove -e sed option where only one command is used
    ok halex@ krw@

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: install.sh,v 1.240 2014/01/05 01:56:52 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.241 2014/02/02 09:32:00 rpe Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2009 Todd Miller, Theo de Raadt, Ken Westerback
@@ -233,7 +233,7 @@ if [[ -s $SERVERLISTALL ]]; then
 fi
 
 # Ensure an enabled console has the correct speed in /etc/ttys.
-sed -e "/^console.*on.*secure.*$/s/std\.[0-9]*/std.$(stty speed </dev/console)/" \
+sed "/^console.*on.*secure.*$/s/std\.[0-9]*/std.$(stty speed </dev/console)/" \
 	/mnt/etc/ttys >/tmp/ttys
 mv /tmp/ttys /mnt/etc/ttys
 
