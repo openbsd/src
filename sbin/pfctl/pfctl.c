4258316b273011b979afca39b362893a53a7a833
lteo <lteo@openbsd.org>
2014-02-17 04:52:25 +0000

    Remove a stray debug printf that crept in via one of the newqueue commits.
    ok henning@

--- sbin/pfctl/pfctl.c
+++ sbin/pfctl/pfctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfctl.c,v 1.321 2013/11/01 22:55:39 pelikan Exp $ */
+/*	$OpenBSD: pfctl.c,v 1.322 2014/02/17 04:52:25 lteo Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1955,7 +1955,6 @@ pfctl_debug(int dev, u_int32_t level, int opts)
 
 	memset(&t, 0, sizeof(t));
 	t.pfrb_type = PFRB_TRANS;
-printf("pfctl_debug DIOCXCOMMIT\n");
 	if (pfctl_trans(dev, &t, DIOCXBEGIN, 0) ||
 	    ioctl(dev, DIOCSETDEBUG, &level) ||
 	    pfctl_trans(dev, &t, DIOCXCOMMIT, 0))
