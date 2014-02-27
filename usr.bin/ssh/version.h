3856c438a231ecc91d8a5040462b91cf9e774b1b
djm <djm@openbsd.org>
2014-02-27 22:57:40 +0000

    openssh-6.6

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.69 2014/01/16 07:32:00 djm Exp $ */
+/* $OpenBSD: version.h,v 1.70 2014/02/27 22:57:40 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.5"
+#define SSH_VERSION	"OpenSSH_6.6"
