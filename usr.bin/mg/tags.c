110109aa07a16c0647e3d0275647f7b726c5ffaa
jasper <jasper@openbsd.org>
2014-03-06 14:51:48 +0000

    add missing stdio.h include for fparseln().
    from Ulrich Mueller, via Han Boetes
    ok florian@

--- usr.bin/mg/tags.c
+++ usr.bin/mg/tags.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tags.c,v 1.6 2012/10/20 09:05:33 jasper Exp $	*/
+/*	$OpenBSD: tags.c,v 1.7 2014/03/06 14:51:48 jasper Exp $	*/
 
 /*
  * This file is in the public domain.
@@ -15,6 +15,7 @@
 #include <err.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <util.h>
 
 #include "def.h"
