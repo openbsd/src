e1375b2e8092d6f55244cb7ca578d8e08cf31ec9
espie <espie@openbsd.org>
2014-03-07 09:44:11 +0000

    the IO::Uncompress framework can auto-detect compression type, turn that on to make sure things are okay all the time.

--- usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
+++ usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackageRepository.pm,v 1.108 2014/02/08 11:07:33 espie Exp $
+# $OpenBSD: PackageRepository.pm,v 1.109 2014/03/07 09:44:11 espie Exp $
 #
 # Copyright (c) 2003-2010 Marc Espie <espie@openbsd.org>
 #
@@ -301,8 +301,8 @@ sub did_it_fork
 sub uncompress
 {
 	my $self = shift;
-	require IO::Uncompress::Gunzip;
-	return IO::Uncompress::Gunzip->new(@_, MultiStream => 1);
+	require IO::Uncompress::AnyUncompress;
+	return IO::Uncompress::AnyUncompress->new(@_, MultiStream => 1);
 }
 
 package OpenBSD::PackageRepository::Local;
