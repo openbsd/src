cf318fd4e1cd3b0a05b3c91a809c409af806cc94
gerhard <gerhard@openbsd.org>
2014-03-07 07:19:42 +0000

    If allocation of 'id' fails, don't try to deref it after 'goto fail'.
    ok millert@

--- sbin/isakmpd/x509.c
+++ sbin/isakmpd/x509.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509.c,v 1.115 2013/11/14 15:44:06 deraadt Exp $	 */
+/* $OpenBSD: x509.c,v 1.116 2014/03/07 07:19:42 gerhard Exp $	 */
 /* $EOM: x509.c,v 1.54 2001/01/16 18:42:16 ho Exp $	 */
 
 /*
@@ -1153,6 +1153,7 @@ x509_cert_get_subjects(void *scert, int *cnt, u_int8_t ***id,
 	if (!*id) {
 		log_print("x509_cert_get_subject: malloc (%lu) failed",
 		    *cnt * (unsigned long)sizeof **id);
+		*cnt = 0;
 		goto fail;
 	}
 	*id_len = calloc(*cnt, sizeof **id_len);
