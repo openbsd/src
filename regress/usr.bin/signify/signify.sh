740fd9c80b9aff7a966cd8dcae74844f6a986ecb
tedu <tedu@openbsd.org>
2014-03-17 15:18:55 +0000

    use quiet signify

--- regress/usr.bin/signify/signify.sh
+++ regress/usr.bin/signify/signify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: signify.sh,v 1.6 2014/03/17 02:49:02 tedu Exp $
+# $OpenBSD: signify.sh,v 1.7 2014/03/17 15:18:55 tedu Exp $
 
 srcdir=$1
 
@@ -14,18 +14,18 @@ set -e
 cat $seckey | signify -S -s - -x test.sig -m $orders 
 diff -u "$orders.sig" test.sig
 
-signify -V -p $pubkey -m $orders
+signify -V -q -p $pubkey -m $orders
 
-signify -V -p $pubkey -m $forgery 2> /dev/null && exit 1
+signify -V -q -p $pubkey -m $forgery 2> /dev/null && exit 1
 
 signify -S -s $seckey -x confirmorders.sig -e -m $orders 
-signify -V -p $pubkey -e -m confirmorders
+signify -V -q -p $pubkey -e -m confirmorders
 diff -u $orders confirmorders
 
 sha256 $pubkey $seckey > HASH
 sha512 $orders $forgery >> HASH
 signify -S -e -s $seckey -m HASH
 rm HASH
-signify -q -C -p $pubkey -x HASH.sig
+signify -C -q -p $pubkey -x HASH.sig
 
 true
