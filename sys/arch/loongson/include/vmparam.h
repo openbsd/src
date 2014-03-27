8ba1c5ccf85694a5f4b738f565b478412067ee7b
miod <miod@openbsd.org>
2014-03-27 21:58:13 +0000

    Increase VM_PHYSSEG_MAX, necessary for systems with non-contiguous memory (such as 2E and 3A systems).

--- sys/arch/loongson/include/vmparam.h
+++ sys/arch/loongson/include/vmparam.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD: vmparam.h,v 1.3 2011/03/23 16:54:35 pirofti Exp $ */
+/*	$OpenBSD: vmparam.h,v 1.4 2014/03/27 21:58:13 miod Exp $ */
 /* public domain */
 #ifndef _MACHINE_VMPARAM_H_
 #define _MACHINE_VMPARAM_H_
 
-#define	VM_PHYSSEG_MAX		2 /* Max number of physical memory segments */
+#define	VM_PHYSSEG_MAX		3 /* Max number of physical memory segments */
 #define	VM_PHYSSEG_STRAT	VM_PSTRAT_BIGFIRST
 
 #include <mips64/vmparam.h>
