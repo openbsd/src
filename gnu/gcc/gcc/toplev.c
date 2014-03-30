c7057713a6f47a04db938481c6d3d3f5cab83cf6
martynas <martynas@openbsd.org>
2014-03-30 01:11:00 +0000

    Change the heuristics of -fstack-protector to select to protect additional functions --- those that have local array definitions, or have references to local frame addresses.
    Note that upstream uses -fstack-protector-strong and misleads people:
    -fstack-protector, -fstack-protector-all, -fstack-protector-strong
    can you tell which one is safe?
    
    Luckily, OpenBSD has its own compiler and is able to do the right
    thing for security:  this is enabled by default, and called
    -fstack-protector.
    
    OK deraadt@, miod@.  Tested for 3 months.

--- gnu/gcc/gcc/toplev.c
+++ gnu/gcc/gcc/toplev.c
@@ -1834,7 +1834,7 @@ process_options (void)
   /* Targets must be able to place spill slots at lower addresses.  If the
      target already uses a soft frame pointer, the transition is trivial.  */
   if (flag_stack_protect == -1)
-    flag_stack_protect = FRAME_GROWS_DOWNWARD ? 1 : 0;
+    flag_stack_protect = FRAME_GROWS_DOWNWARD ? 3 : 0;
   if (!FRAME_GROWS_DOWNWARD && flag_stack_protect)
     {
       warning (0, "-fstack-protector not supported for this target");
