a38b157bce1d9eb91d76881ec88e1720aac93e18
jsg <jsg@openbsd.org>
2014-04-12 15:57:50 +0000

    fix a bit test from FreeBSD rev 241829

--- usr.sbin/memconfig/memconfig.c
+++ usr.sbin/memconfig/memconfig.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: memconfig.c,v 1.15 2011/09/20 08:28:51 deraadt Exp $ */
+/* $OpenBSD: memconfig.c,v 1.16 2014/04/12 15:57:50 jsg Exp $ */
 
 /*-
  * Copyright (c) 1999 Michael Smith <msmith@freebsd.org>
@@ -242,7 +242,7 @@ setfunc(int memfd, int argc, char *argv[])
 	while(argc--) {
 		for (i = 0; attrnames[i].name != NULL; i++) {
 			if (!strcmp(attrnames[i].name, argv[0])) {
-				if (!attrnames[i].kind & MDF_SETTABLE)
+				if (!(attrnames[i].kind & MDF_SETTABLE))
 					help("flags");
 				mrd.mr_flags |= attrnames[i].val;
 				break;
