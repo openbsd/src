fa6b5173b89baa5c079de1616daab56f21efa9cc
miod <miod@openbsd.org>
2014-04-16 05:16:39 +0000

    Upgrade from des_old misery

--- usr.bin/gzsig/ssh.c
+++ usr.bin/gzsig/ssh.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh.c,v 1.2 2005/05/29 09:10:23 djm Exp $ */
+/* $OpenBSD: ssh.c,v 1.3 2014/04/16 05:16:39 miod Exp $ */
 
 /*
  * ssh.c
@@ -56,8 +56,8 @@
 extern int sign_passwd_cb(char *, int, int, void *);
 
 struct des3_state {
-	des_key_schedule	k1, k2, k3;
-	des_cblock		iv1, iv2, iv3;
+	DES_key_schedule	k1, k2, k3;
+	DES_cblock		iv1, iv2, iv3;
 };
 
 static int
@@ -153,13 +153,13 @@ des3_init(u_char *sesskey, int len)
 	if ((state = malloc(sizeof(*state))) == NULL)
 		return (NULL);
 
-	des_set_key((void *)sesskey, state->k1);
-	des_set_key((void *)(sesskey + 8), state->k2);
+	DES_set_key((const_DES_cblock *)sesskey, &state->k1);
+	DES_set_key((const_DES_cblock *)(sesskey + 8), &state->k2);
 
 	if (len <= 16)
-		des_set_key((void *)sesskey, state->k3);
+		DES_set_key((const_DES_cblock *)sesskey, &state->k3);
 	else
-		des_set_key((void *)(sesskey + 16), state->k3);
+		DES_set_key((const_DES_cblock *)(sesskey + 16), &state->k3);
 	
 	memset(state->iv1, 0, 8);
 	memset(state->iv2, 0, 8);
@@ -176,9 +176,9 @@ des3_decrypt(u_char *src, u_char *dst, int len, void *state)
 	dstate = (struct des3_state *)state;
 	memcpy(dstate->iv1, dstate->iv2, 8);
 	
-	des_ncbc_encrypt(src, dst, len, dstate->k3, &dstate->iv3, DES_DECRYPT);
-	des_ncbc_encrypt(dst, dst, len, dstate->k2, &dstate->iv2, DES_ENCRYPT);
-	des_ncbc_encrypt(dst, dst, len, dstate->k1, &dstate->iv1, DES_DECRYPT);
+	DES_ncbc_encrypt(src, dst, len, &dstate->k3, &dstate->iv3, DES_DECRYPT);
+	DES_ncbc_encrypt(dst, dst, len, &dstate->k2, &dstate->iv2, DES_ENCRYPT);
+	DES_ncbc_encrypt(dst, dst, len, &dstate->k1, &dstate->iv1, DES_DECRYPT);
 }
 
 static int
