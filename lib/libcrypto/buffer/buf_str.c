6c86449927f9f86b9676a2dd98777a7c36483724
beck <beck@openbsd.org>
2014-04-17 13:45:44 +0000

    simply wrap around intrinsics, and knf cleanup. ok miod@ deraadt@

--- lib/libcrypto/buffer/buf_str.c
+++ lib/libcrypto/buffer/buf_str.c
@@ -5,21 +5,21 @@
  * This package is an SSL implementation written
  * by Eric Young (eay@cryptsoft.com).
  * The implementation was written so as to conform with Netscapes SSL.
- * 
+ *
  * This library is free for commercial and non-commercial use as long as
  * the following conditions are aheared to.  The following conditions
  * apply to all code found in this distribution, be it the RC4, RSA,
  * lhash, DES, etc., code; not just the SSL code.  The SSL documentation
  * included with this distribution is covered by the same copyright terms
  * except that the holder is Tim Hudson (tjh@cryptsoft.com).
- * 
+ *
  * Copyright remains Eric Young's, and as such any Copyright notices in
  * the code are not to be removed.
  * If this package is used in a product, Eric Young should be given attribution
  * as the author of the parts of the library used.
  * This can be in the form of a textual message at program startup or
  * in documentation (online or textual) provided with the package.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -34,10 +34,10 @@
  *     Eric Young (eay@cryptsoft.com)"
  *    The word 'cryptographic' can be left out if the rouines from the library
  *    being used are not cryptographic related :-).
- * 4. If you include any Windows specific code (or a derivative thereof) from 
+ * 4. If you include any Windows specific code (or a derivative thereof) from
  *    the apps directory (application code) you must include an acknowledgement:
  *    "This product includes software written by Tim Hudson (tjh@cryptsoft.com)"
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY ERIC YOUNG ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -49,71 +49,68 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- * 
+ *
  * The licence and distribution terms for any publically available version or
  * derivative of this code cannot be changed.  i.e. this code cannot simply be
  * copied and put under another distribution licence
  * [including the GNU Public Licence.]
  */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "cryptlib.h"
 #include <openssl/buffer.h>
 
-char *BUF_strdup(const char *str)
-	{
-	if (str == NULL) return(NULL);
-	return BUF_strndup(str, strlen(str));
+/*
+ * XXX these functions accept a NULL arg and return NULL
+ * when the standard ones do not. we should at an appropriate
+ * time change these to find the bad callers
+ */
+
+char *
+BUF_strdup(const char *str)
+{
+	char *ret = NULL;
+	if (str != NULL) {
+		if (!(ret = strdup(str)))
+			BUFerr(BUF_F_BUF_STRDUP,ERR_R_MALLOC_FAILURE);
 	}
+	return ret;
+}
 
-char *BUF_strndup(const char *str, size_t siz)
-	{
-	char *ret;
-
-	if (str == NULL) return(NULL);
-
-	ret=malloc(siz+1);
-	if (ret == NULL) 
-		{
-		BUFerr(BUF_F_BUF_STRNDUP,ERR_R_MALLOC_FAILURE);
-		return(NULL);
-		}
-	BUF_strlcpy(ret,str,siz+1);
-	return(ret);
+char *
+BUF_strndup(const char *str, size_t siz)
+{
+	char *ret = NULL;
+	if (str != NULL) {
+		if (!(ret = strndup(str, siz)))
+			BUFerr(BUF_F_BUF_STRNDUP,ERR_R_MALLOC_FAILURE);
 	}
+	return ret;
+}
 
-void *BUF_memdup(const void *data, size_t siz)
-	{
-	void *ret;
-
-	if (data == NULL) return(NULL);
-
-	ret=malloc(siz);
-	if (ret == NULL) 
-		{
-		BUFerr(BUF_F_BUF_MEMDUP,ERR_R_MALLOC_FAILURE);
-		return(NULL);
-		}
-	return memcpy(ret, data, siz);
-	}	
-
-size_t BUF_strlcpy(char *dst, const char *src, size_t size)
-	{
-	size_t l = 0;
-	for(; size > 1 && *src; size--)
-		{
-		*dst++ = *src++;
-		l++;
-		}
-	if (size)
-		*dst = '\0';
-	return l + strlen(src);
+void *
+BUF_memdup(const void *data, size_t siz)
+{
+	void *ret = NULL;
+	if (data != NULL) {
+		if (!(ret = malloc(siz)))
+			BUFerr(BUF_F_BUF_MEMDUP,ERR_R_MALLOC_FAILURE);
+		else
+			(void) memcpy(ret, data, siz);
 	}
+	return ret;
+}
 
-size_t BUF_strlcat(char *dst, const char *src, size_t size)
-	{
-	size_t l = 0;
-	for(; size > 0 && *dst; size--, dst++)
-		l++;
-	return l + BUF_strlcpy(dst, src, size);
-	}
+size_t
+BUF_strlcpy(char *dst, const char *src, size_t size)
+{
+	return strlcpy(dst, src, size);
+}
+
+size_t
+BUF_strlcat(char *dst, const char *src, size_t size)
+{
+	return strlcat(dst, src, size);
+}
