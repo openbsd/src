0ec6e02f1a19a18673e350e8fe6aaf6a78224fc0
tedu <tedu@openbsd.org>
2014-04-18 18:03:26 +0000

    another

--- lib/libssl/src/apps/testdsa.h
+++ lib/libssl/src/apps/testdsa.h
@@ -212,6 +212,3 @@ DSA *get_dsa2048()
 		return(NULL);
 	return(dsa);
 	}
-
-static const char rnd_seed[] = "string to make the random number generator think it has entropy";
-static int rnd_fake = 0;
