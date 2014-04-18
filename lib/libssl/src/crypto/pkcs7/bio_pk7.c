567206c133687a17da42aef3fb0b0e19dc053992
tedu <tedu@openbsd.org>
2014-04-18 02:56:48 +0000

    -netware

--- lib/libssl/src/crypto/pkcs7/bio_pk7.c
+++ lib/libssl/src/crypto/pkcs7/bio_pk7.c
@@ -56,14 +56,10 @@
 #include <openssl/pkcs7.h>
 #include <openssl/bio.h>
 
-#if !defined(OPENSSL_SYSNAME_NETWARE) && !defined(OPENSSL_SYSNAME_VXWORKS)
-#include <memory.h>
-#endif
 #include <stdio.h>
 
 /* Streaming encode support for PKCS#7 */
-
 BIO *BIO_new_PKCS7(BIO *out, PKCS7 *p7) 
-	{
+{
 	return BIO_new_NDEF(out, (ASN1_VALUE *)p7, ASN1_ITEM_rptr(PKCS7));
-	}
+}
