3cf2799588c77511aa9a35344eecb1c1157a946e
gilles <gilles@openbsd.org>
2014-04-19 11:31:06 +0000

    cast (void) this strlcpy(), it can't fail copying a small constant string into a much larger buffer

--- usr.sbin/smtpd/delivery_mbox.c
+++ usr.sbin/smtpd/delivery_mbox.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: delivery_mbox.c,v 1.9 2013/05/24 17:03:14 eric Exp $	*/
+/*	$OpenBSD: delivery_mbox.c,v 1.10 2014/04/19 11:31:06 gilles Exp $	*/
 
 /*
  * Copyright (c) 2011 Gilles Chehade <gilles@poolp.org>
@@ -57,7 +57,7 @@ delivery_mbox_open(struct deliver *deliver)
 	environ = environ_new;
 
 	if (deliver->from[0] == '\0')
-		strlcpy(deliver->from, "MAILER-DAEMON", sizeof deliver->from);
+		(void)strlcpy(deliver->from, "MAILER-DAEMON", sizeof deliver->from);
 	execle(PATH_MAILLOCAL, PATH_MAILLOCAL, "-f", deliver->from,
 	    deliver->to, (char *)NULL, environ_new);
 	perror("execle");
