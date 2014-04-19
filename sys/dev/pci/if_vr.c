bdcaae737ce61fac34267c1d3f2519bd06e9be63
henning <henning@openbsd.org>
2014-04-19 14:47:51 +0000

    sigh, another driver fiddling with altq outside #ifdef ALTQ

--- sys/dev/pci/if_vr.c
+++ sys/dev/pci/if_vr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_vr.c,v 1.132 2013/12/28 03:34:54 deraadt Exp $	*/
+/*	$OpenBSD: if_vr.c,v 1.133 2014/04/19 14:47:51 henning Exp $	*/
 
 /*
  * Copyright (c) 1997, 1998
@@ -1350,10 +1350,7 @@ vr_start(struct ifnet *ifp)
 		head_tx = cur_tx;
 		if (vr_encap(sc, &cur_tx, m_head)) {
 			/* Rollback, send what we were able to encap. */
-			if (ALTQ_IS_ENABLED(&ifp->if_snd))
-				m_freem(m_head);
-			else
-				IF_PREPEND(&ifp->if_snd, m_head);
+			IF_PREPEND(&ifp->if_snd, m_head);
 			break;
 		}
 		queued++;
