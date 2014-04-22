1571cfed7586976b53448a46f510adc1129af81a
miod <miod@openbsd.org>
2014-04-22 22:21:32 +0000

    unifdef -UAES_LONG for we do not intend to run on platforms where int is smaller than 32 bits.

--- lib/libssl/src/crypto/aes/aes_locl.h
+++ lib/libssl/src/crypto/aes/aes_locl.h
@@ -65,11 +65,7 @@
 #define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ ((u32)(pt)[2] <<  8) ^ ((u32)(pt)[3]))
 #define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); (ct)[1] = (u8)((st) >> 16); (ct)[2] = (u8)((st) >>  8); (ct)[3] = (u8)(st); }
 
-#ifdef AES_LONG
-typedef unsigned long u32;
-#else
 typedef unsigned int u32;
-#endif
 typedef unsigned short u16;
 typedef unsigned char u8;
 
