aaa200608dfb44e5f2fe34c912145ead8b518948
stsp <stsp@openbsd.org>
2014-04-23 12:45:18 +0000

    Document RTS5227 support in rtsx(4).

--- sys/dev/ic/rtsx.c
+++ sys/dev/ic/rtsx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtsx.c,v 1.7 2013/12/08 18:31:03 stsp Exp $	*/
+/*	$OpenBSD: rtsx.c,v 1.8 2014/04/23 12:45:18 stsp Exp $	*/
 
 /*
  * Copyright (c) 2006 Uwe Stuehler <uwe@openbsd.org>
@@ -18,7 +18,7 @@
  */
 
 /*
- * Realtek RTS5209/RTS5229 Card Reader driver.
+ * Realtek RTS5209/RTS5227/RTS5229 Card Reader driver.
  */
 
 #include <sys/param.h>
