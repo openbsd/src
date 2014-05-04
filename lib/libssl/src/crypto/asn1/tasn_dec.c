fee20d6336ae737dbb28e9f2039be59c1ba09b25
tedu <tedu@openbsd.org>
2014-05-04 19:06:40 +0000

    i give up. reuse problem is unfixable. dlg says puppet crashes.

--- lib/libssl/src/crypto/asn1/tasn_dec.c
+++ lib/libssl/src/crypto/asn1/tasn_dec.c
@@ -382,14 +382,10 @@ ASN1_item_ex_d2i(ASN1_VALUE **pval, const unsigned char **in, long len,
 			goto err;
 		}
 
-		if (!*pval) {
-			if (!ASN1_item_ex_new(pval, it)) {
-				ASN1err(ASN1_F_ASN1_ITEM_EX_D2I,
-				    ERR_R_NESTED_ASN1_ERROR);
-				goto err;
-			}
-		} else {
-			memset(*pval, 0, it->size);
+		if (!*pval && !ASN1_item_ex_new(pval, it)) {
+			ASN1err(ASN1_F_ASN1_ITEM_EX_D2I,
+			    ERR_R_NESTED_ASN1_ERROR);
+			goto err;
 		}
 
 		if (asn1_cb && !asn1_cb(ASN1_OP_D2I_PRE, pval, it, NULL))
