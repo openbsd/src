6bbdf2101d4fbe760c9574d33d212447f81204c7
espie <espie@openbsd.org>
2014-05-05 16:29:32 +0000

    work-around the same location being reinstalled thru a different path later... no need to do anything on sets consisting purely of "kept" handles
    This is not fully satisfying, I expect I need to tie some "done already" to
    locations as we do with sets, so that we can wipe update_info properly and
    be done once and for all...
    
    Issue found by stsp@ while trying to use FETCH_PACKAGES, which is still not
    100% in good shape with this.

--- usr.sbin/pkg_add/OpenBSD/PkgAdd.pm
+++ usr.sbin/pkg_add/OpenBSD/PkgAdd.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: PkgAdd.pm,v 1.64 2014/03/18 18:53:29 espie Exp $
+# $OpenBSD: PkgAdd.pm,v 1.65 2014/05/05 16:29:32 espie Exp $
 #
 # Copyright (c) 2003-2014 Marc Espie <espie@openbsd.org>
 #
@@ -901,6 +901,9 @@ sub process_set
 	$set->figure_out_kept($state);
 
 	if (newer_has_errors($set, $state)) {
+		return ();
+	}
+	if ($set->newer == 0 && $set->older_to_do == 0) {
 		return ();
 	}
 
