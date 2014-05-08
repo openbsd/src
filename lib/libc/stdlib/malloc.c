91c2a70715ef3ba4fe719d27609670f8fca87f6b
deraadt <deraadt@openbsd.org>
2014-05-08 21:43:49 +0000

    move reallocarray() to a seperate file so that -portable applications can avoid reinventing the wheel ok guenther schwarze

--- lib/libc/stdlib/malloc.c
+++ lib/libc/stdlib/malloc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malloc.c,v 1.160 2014/05/07 20:07:59 halex Exp $	*/
+/*	$OpenBSD: malloc.c,v 1.161 2014/05/08 21:43:49 deraadt Exp $	*/
 /*
  * Copyright (c) 2008, 2010, 2011 Otto Moerbeek <otto@drijf.net>
  * Copyright (c) 2012 Matthew Dempsky <matthew@openbsd.org>
@@ -1422,17 +1422,6 @@ calloc(size_t nmemb, size_t size)
 	if (r != NULL)
 		errno = saved_errno;
 	return r;
-}
-
-void *
-reallocarray(void *optr, size_t nmemb, size_t size)
-{
-	if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) &&
-	    nmemb > 0 && SIZE_MAX / nmemb < size) {
-		errno = ENOMEM;
-		return NULL;
-	}
-	return realloc(optr, size * nmemb);
 }
 
 static void *
