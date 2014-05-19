f2245ce3ad535d15027f81e188675d4e34341d2e
jsing <jsing@openbsd.org>
2014-05-19 12:18:23 +0000

    More whack-a-mole^Wfips.
    ok miod@

--- lib/libssl/src/apps/dgst.c
+++ lib/libssl/src/apps/dgst.c
@@ -127,7 +127,6 @@ dgst_main(int argc, char **argv)
 #endif
 	char *hmac_key = NULL;
 	char *mac_name = NULL;
-	int non_fips_allow = 0;
 	STACK_OF(OPENSSL_STRING) * sigopts = NULL, *macopts = NULL;
 
 	signal(SIGPIPE, SIG_IGN);
@@ -204,10 +203,6 @@ dgst_main(int argc, char **argv)
 			out_bin = 1;
 		else if (strcmp(*argv, "-d") == 0)
 			debug = 1;
-		else if (!strcmp(*argv, "-fips-fingerprint"))
-			hmac_key = "etaonrishdlcupfm";
-		else if (strcmp(*argv, "-non-fips-allow") == 0)
-			non_fips_allow = 1;
 		else if (!strcmp(*argv, "-hmac")) {
 			if (--argc < 1)
 				break;
@@ -353,11 +348,6 @@ mac_end:
 			EVP_PKEY_CTX_free(mac_ctx);
 		if (r == 0)
 			goto end;
-	}
-	if (non_fips_allow) {
-		EVP_MD_CTX *md_ctx;
-		BIO_get_md_ctx(bmd, &md_ctx);
-		EVP_MD_CTX_set_flags(md_ctx, EVP_MD_CTX_FLAG_NON_FIPS_ALLOW);
 	}
 	if (hmac_key) {
 		sigkey = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, e,
