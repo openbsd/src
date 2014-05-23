43ee1c707d8d0364b1e888da4086c8cc4fd22d59
guenther <guenther@openbsd.org>
2014-05-23 21:58:26 +0000

    Archives written with a non-standard blocksize currently show up as truncated reads.  Until better detection of that case can is implemented, back out that part of rev 1.45
    problem noted by sthen@

--- bin/pax/ar_io.c
+++ bin/pax/ar_io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ar_io.c,v 1.46 2014/05/23 19:47:49 guenther Exp $	*/
+/*	$OpenBSD: ar_io.c,v 1.47 2014/05/23 21:58:26 guenther Exp $	*/
 /*	$NetBSD: ar_io.c,v 1.5 1996/03/26 23:54:13 mrg Exp $	*/
 
 /*-
@@ -571,7 +571,7 @@ ar_read(char *buf, int cnt)
 	if (res < 0)
 		syswarn(1, errno, "Failed read on archive volume %d", arvol);
 	else
-		paxwarn(1, "End of archive volume %d reached", arvol);
+		paxwarn(0, "End of archive volume %d reached", arvol);
 	return(res);
 }
 
