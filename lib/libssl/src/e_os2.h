65af98848fc7a42e34d470d10fc1db8e23f9db93
jsing <jsing@openbsd.org>
2014-05-24 15:03:39 +0000

    We have shared global variables - unifdef OPENSSL_EXPORT_VAR_AS_FUNCTION.
    ok miod@ beck@

--- lib/libssl/src/e_os2.h
+++ lib/libssl/src/e_os2.h
@@ -80,17 +80,9 @@ extern "C" {
 	OPENSSL_DECLARE_GLOBAL(int,foobar);
 	#define foobar OPENSSL_GLOBAL_REF(foobar)
 */
-#ifdef OPENSSL_EXPORT_VAR_AS_FUNCTION
-# define OPENSSL_IMPLEMENT_GLOBAL(type,name,value)			\
-	type *_shadow_##name(void)					\
-	{ static type _hide_##name=value; return &_hide_##name; }
-# define OPENSSL_DECLARE_GLOBAL(type,name) type *_shadow_##name(void)
-# define OPENSSL_GLOBAL_REF(name) (*(_shadow_##name()))
-#else
 # define OPENSSL_IMPLEMENT_GLOBAL(type,name,value) OPENSSL_GLOBAL type _shadow_##name=value;
 # define OPENSSL_DECLARE_GLOBAL(type,name) OPENSSL_EXPORT type _shadow_##name
 # define OPENSSL_GLOBAL_REF(name) _shadow_##name
-#endif
 #  define ossl_ssize_t ssize_t
 #ifdef  __cplusplus
 }
