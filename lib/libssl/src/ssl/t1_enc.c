c198e78d61e0e4c04aca575d3f895aa1d9a0ad37
jsing <jsing@openbsd.org>
2014-05-28 14:05:35 +0000

    More KNF.

--- lib/libssl/src/ssl/t1_enc.c
+++ lib/libssl/src/ssl/t1_enc.c
@@ -928,27 +928,28 @@ tls1_generate_master_secret(SSL *s, unsigned char *out, unsigned char *p,
 	const void *co = NULL, *so = NULL;
 	int col = 0, sol = 0;
 
-
-
 #ifdef TLSEXT_TYPE_opaque_prf_input
-	if (s->s3->client_opaque_prf_input != NULL && s->s3->server_opaque_prf_input != NULL &&
+	if (s->s3->client_opaque_prf_input != NULL &&
+	    s->s3->server_opaque_prf_input != NULL &&
 	    s->s3->client_opaque_prf_input_len > 0 &&
-	    s->s3->client_opaque_prf_input_len == s->s3->server_opaque_prf_input_len) {
+	    s->s3->client_opaque_prf_input_len ==
+	    s->s3->server_opaque_prf_input_len) {
+		/*
+		 * sol must be same as col - see section 3.1 of
+		 * draft-rescorla-tls-opaque-prf-input-00.txt.
+		 */
 		co = s->s3->client_opaque_prf_input;
 		col = s->s3->server_opaque_prf_input_len;
 		so = s->s3->server_opaque_prf_input;
-		sol = s->s3->client_opaque_prf_input_len; /* must be same as col (see draft-rescorla-tls-opaque-prf-input-00.txt, section 3.1) */
+		sol = s->s3->client_opaque_prf_input_len;
 	}
 #endif
 
 	tls1_PRF(ssl_get_algorithm2(s),
 	    TLS_MD_MASTER_SECRET_CONST, TLS_MD_MASTER_SECRET_CONST_SIZE,
-	    s->s3->client_random, SSL3_RANDOM_SIZE,
-	    co, col,
-	    s->s3->server_random, SSL3_RANDOM_SIZE,
-	    so, sol,
-	    p, len,
-	    s->session->master_key, buff, sizeof buff);
+	    s->s3->client_random, SSL3_RANDOM_SIZE, co, col,
+	    s->s3->server_random, SSL3_RANDOM_SIZE, so, sol,
+	    p, len, s->session->master_key, buff, sizeof buff);
 
 	return (SSL3_MASTER_SECRET_SIZE);
 }
