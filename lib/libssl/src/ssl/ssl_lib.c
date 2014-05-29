7aed837606fe293db660cdb85dbb3ff126cf726e
jsing <jsing@openbsd.org>
2014-05-29 14:43:33 +0000

    When you have functions that perform specific functions, use them.
    EVP_CIPHER_CTX_free() does a NULL check, then calls EVP_CIPHER_CTX_cleanup()
    and frees the memory. COMP_CTX_free() also had its own NULL check, so there
    is no point in duplicating that here.
    
    ok beck@

--- lib/libssl/src/ssl/ssl_lib.c
+++ lib/libssl/src/ssl/ssl_lib.c
@@ -2726,25 +2726,16 @@ SSL_dup(SSL *s)
 void
 ssl_clear_cipher_ctx(SSL *s)
 {
-	if (s->enc_read_ctx != NULL) {
-		EVP_CIPHER_CTX_cleanup(s->enc_read_ctx);
-		free(s->enc_read_ctx);
-		s->enc_read_ctx = NULL;
-	}
-	if (s->enc_write_ctx != NULL) {
-		EVP_CIPHER_CTX_cleanup(s->enc_write_ctx);
-		free(s->enc_write_ctx);
-		s->enc_write_ctx = NULL;
-	}
+	EVP_CIPHER_CTX_free(s->enc_read_ctx);
+	s->enc_read_ctx = NULL;
+	EVP_CIPHER_CTX_free(s->enc_write_ctx);
+	s->enc_write_ctx = NULL;
+
 #ifndef OPENSSL_NO_COMP
-	if (s->expand != NULL) {
-		COMP_CTX_free(s->expand);
-		s->expand = NULL;
-	}
-	if (s->compress != NULL) {
-		COMP_CTX_free(s->compress);
-		s->compress = NULL;
-	}
+	COMP_CTX_free(s->expand);
+	s->expand = NULL;
+	COMP_CTX_free(s->compress);
+	s->compress = NULL;
 #endif
 }
 
