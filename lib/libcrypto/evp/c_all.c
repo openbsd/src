7a59c9a977d3d1b29bad28f3f13f7bebe5e18a9e
deraadt <deraadt@openbsd.org>
2014-06-10 16:15:19 +0000

    Abandon the auto-ENGINE /dev/crypto interface.  VIA 3des cbc receives collateral damage.
    The syncronous nature of this mechanism has hampered performance for
    symmetric crypto relative to brute-force cpu. The assymetric crypto
    support never really materialized in drivers.
    
    So abandon the complexity.
    ok tedu beck mikeb
    some disagrement from djm but if he wants to test /dev/crypto ciphers
    he should do it without this this gigantic API in the way

--- lib/libcrypto/evp/c_all.c
+++ lib/libcrypto/evp/c_all.c
@@ -73,9 +73,6 @@ OPENSSL_add_all_algorithms_noconf(void)
 	OPENSSL_cpuid_setup();
 	OpenSSL_add_all_ciphers();
 	OpenSSL_add_all_digests();
-#ifndef OPENSSL_NO_ENGINE
-	ENGINE_setup_bsd_cryptodev();
-#endif
 }
 
 void
--- lib/libssl/src/crypto/engine/eng_all.c
+++ lib/libssl/src/crypto/engine/eng_all.c
@@ -71,9 +71,7 @@ ENGINE_load_builtin_engines(void)
 	 * *no* builtin implementations). */
 	ENGINE_load_openssl();
 #endif
-#if !defined(OPENSSL_NO_HW) && (defined(__OpenBSD__) || defined(__FreeBSD__) || defined(HAVE_CRYPTODEV))
-	ENGINE_load_cryptodev();
-#endif
+
 #ifndef OPENSSL_NO_RSAX
 	ENGINE_load_rsax();
 #endif
@@ -87,17 +85,3 @@ ENGINE_load_builtin_engines(void)
 #endif
 	ENGINE_register_all_complete();
 }
-
-#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(HAVE_CRYPTODEV)
-void
-ENGINE_setup_bsd_cryptodev(void)
-{
-	static int bsd_cryptodev_default_loaded = 0;
-
-	if (!bsd_cryptodev_default_loaded) {
-		ENGINE_load_cryptodev();
-		ENGINE_register_all_complete();
-	}
-	bsd_cryptodev_default_loaded = 1;
-}
-#endif
