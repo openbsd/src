14e3048027c0fad5c8ed39b8306bfffad6ef6c66
tobias <tobias@openbsd.org>
2014-06-16 18:33:33 +0000

    Merge NetBSD commit: Move to 2 clause license, approved by Wolfgang Solfrank.
    ok deraadt@, krw@

--- sbin/fsck_msdos/fat.c
+++ sbin/fsck_msdos/fat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fat.c,v 1.22 2014/06/14 19:32:25 tobias Exp $	*/
+/*	$OpenBSD: fat.c,v 1.23 2014/06/16 18:33:33 tobias Exp $	*/
 /*	$NetBSD: fat.c,v 1.8 1997/10/17 11:19:53 ws Exp $	*/
 
 /*
@@ -13,13 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Martin Husemann
- *	and Wolfgang Solfrank.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
