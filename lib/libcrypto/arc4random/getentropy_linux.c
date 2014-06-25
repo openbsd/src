11014432cd5f073d8ad364e1c178a4af3f57c54b
beck <beck@openbsd.org>
2014-06-25 16:45:49 +0000

    get the page of data at AT_SYSINFO_EHDR ok deraadt@

--- lib/libcrypto/arc4random/getentropy_linux.c
+++ lib/libcrypto/arc4random/getentropy_linux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getentropy_linux.c,v 1.11 2014/06/25 16:31:03 beck Exp $	*/
+/*	$OpenBSD: getentropy_linux.c,v 1.12 2014/06/25 16:45:49 beck Exp $	*/
 
 /*
  * Copyright (c) 2014 Theo de Raadt <deraadt@openbsd.org>
@@ -460,7 +460,7 @@ getentropy_fallback(void *buf, size_t len)
 #ifdef AT_SYSINFO_EHDR
 		p = (char *) getauxval(AT_SYSINFO_EHDR);
 		if (p)
-			HR(p, sizeof(p));
+			HR(p, pgs);
 #endif
 #ifdef AT_BASE
 		p = (char *) getauxval(AT_BASE);
