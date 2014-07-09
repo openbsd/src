6dc6a75eaadc3e476ba5e1e7e963a2ce38bf9729
benno <benno@openbsd.org>
2014-07-09 08:07:53 +0000

    Set default of net.inet6.icmp6.nodeinfo to 0, disables responses to RFC4620 IPv6 Node Information Queries. ok florian henning bluhm

--- sys/netinet6/in6_proto.c
+++ sys/netinet6/in6_proto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6_proto.c,v 1.69 2013/10/21 12:27:15 deraadt Exp $	*/
+/*	$OpenBSD: in6_proto.c,v 1.70 2014/07/09 08:07:53 benno Exp $	*/
 /*	$KAME: in6_proto.c,v 1.66 2000/10/10 15:35:47 itojun Exp $	*/
 
 /*
@@ -308,5 +308,5 @@ u_long	rip6_recvspace = RIPV6RCVQ;
 int	icmp6_rediraccept = 0;		/* don't process redirects by default */
 int	icmp6_redirtimeout = 10 * 60;	/* 10 minutes */
 int	icmp6errppslim = 100;		/* 100pps */
-int	icmp6_nodeinfo = 1;		/* enable/disable NI response */
+int	icmp6_nodeinfo = 0;		/* enable/disable NI response */
 int	ip6_mtudisc_timeout = IPMTUDISCTIMEOUT;
