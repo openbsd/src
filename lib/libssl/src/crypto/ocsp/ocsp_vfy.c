8543bd0a222c68b2c4a49627c508b29867283553
tedu <tedu@openbsd.org>
2014-07-09 19:08:10 +0000

    ocsp_check_ids says "If algoritm mismatch let caller deal with it" before returning 2. The one and only caller doesn't check for that, so... Stop returning it. ok miod

--- lib/libssl/src/crypto/ocsp/ocsp_vfy.c
+++ lib/libssl/src/crypto/ocsp/ocsp_vfy.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ocsp_vfy.c,v 1.11 2014/06/12 15:49:30 deraadt Exp $ */
+/* $OpenBSD: ocsp_vfy.c,v 1.12 2014/07/09 19:08:10 tedu Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2000.
  */
@@ -283,11 +283,6 @@ ocsp_check_ids(STACK_OF(OCSP_SINGLERESP) *sresp, OCSP_CERTID **ret)
 		tmpid = sk_OCSP_SINGLERESP_value(sresp, i)->certId;
 		/* Check to see if IDs match */
 		if (OCSP_id_issuer_cmp(cid, tmpid)) {
-			/* If algoritm mismatch let caller deal with it */
-			if (OBJ_cmp(tmpid->hashAlgorithm->algorithm,
-			    cid->hashAlgorithm->algorithm))
-				return 2;
-			/* Else mismatch */
 			return 0;
 		}
 	}
