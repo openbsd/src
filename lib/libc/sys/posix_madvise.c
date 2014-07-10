7ab35e9cc9e7553eb4aa519de495f5739d7e0035
tedu <tedu@openbsd.org>
2014-07-10 12:46:28 +0000

    prototype for _thread_sys_madvise

--- lib/libc/sys/posix_madvise.c
+++ lib/libc/sys/posix_madvise.c
@@ -1,8 +1,10 @@
-/*	$OpenBSD: posix_madvise.c,v 1.1 2010/05/18 22:24:55 tedu Exp $ */
+/*	$OpenBSD: posix_madvise.c,v 1.2 2014/07/10 12:46:28 tedu Exp $ */
 /*
  * Ted Unangst wrote this file and placed it into the public domain.
  */
 #include <sys/mman.h>
+
+int _thread_sys_madvise(void *addr, size_t len, int behav);
 
 int
 posix_madvise(void *addr, size_t len, int behav)
