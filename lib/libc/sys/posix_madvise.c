228d32200194209ef31f85d91a4f4da3083f4bc7
guenther <guenther@openbsd.org>
2014-07-10 13:42:53 +0000

    posix_madvise() should return the errno value on error, not -1
    ok tedu@ jmc@

--- lib/libc/sys/posix_madvise.c
+++ lib/libc/sys/posix_madvise.c
@@ -1,13 +1,14 @@
-/*	$OpenBSD: posix_madvise.c,v 1.2 2014/07/10 12:46:28 tedu Exp $ */
+/*	$OpenBSD: posix_madvise.c,v 1.3 2014/07/10 13:42:53 guenther Exp $ */
 /*
  * Ted Unangst wrote this file and placed it into the public domain.
  */
 #include <sys/mman.h>
+#include <errno.h>
 
 int _thread_sys_madvise(void *addr, size_t len, int behav);
 
 int
 posix_madvise(void *addr, size_t len, int behav)
 {
-	return (_thread_sys_madvise(addr, len, behav));
+	return (_thread_sys_madvise(addr, len, behav) ? errno : 0);
 }
