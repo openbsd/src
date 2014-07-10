ee91c906f7a31876c2e033129febbb750fa7002f
deraadt <deraadt@openbsd.org>
2014-07-10 14:33:18 +0000

    struct ucred; for fstat _KERNEL block

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.h,v 1.31 2013/06/05 01:26:00 guenther Exp $	*/
+/*	$OpenBSD: file.h,v 1.32 2014/07/10 14:33:18 deraadt Exp $	*/
 /*	$NetBSD: file.h,v 1.11 1995/03/26 20:24:13 jtc Exp $	*/
 
 /*
@@ -43,6 +43,7 @@ struct uio;
 struct knote;
 struct stat;
 struct file;
+struct ucred;
 
 struct	fileops {
 	int	(*fo_read)(struct file *, off_t *, struct uio *,
