5e5c016ed07331adde9d77d6745a6e2a3f77889b
tedu <tedu@openbsd.org>
2014-07-11 14:01:38 +0000

    move all the feature settings to a common header. probably ok beck jsing miod

--- lib/libcrypto/arch/ia64/opensslconf.h
+++ lib/libcrypto/arch/ia64/opensslconf.h
@@ -1,3 +1,4 @@
-/* $OpenBSD: opensslconf.h,v 1.1 2014/05/27 10:10:59 tobiasu Exp $ */
+#include <opensslfeatures.h>
+/* $OpenBSD: opensslconf.h,v 1.2 2014/07/11 14:01:38 tedu Exp $ */
 
 #error "Dummy header, create a proper ia64 config"
