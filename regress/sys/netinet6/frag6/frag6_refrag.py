fcb190aafc71da09927b98c3aa48552289b6b807
bluhm <bluhm@openbsd.org>
2014-07-11 18:25:50 +0000

    Add a test that sends a large ICMP6 packet in many small fragments. The echo reply has to be fragmented.

---
+++ regress/sys/netinet6/frag6/frag6_refrag.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python2.7
+# send fragments of a large packet that has to be refragmented by reflector
+
+# |--------|
+#          |------------------|
+#                              ...
+#                                 |------------------|
+#                                                    |----|
+
+import os
+from addr import *
+from scapy.all import *
+
+pid=os.getpid()
+payload=100 * "ABCDEFGHIJKLMNOP"
+packet=IPv6(src=SRC_OUT6, dst=DST_IN6)/ICMPv6EchoRequest(id=pid, data=payload)
+frag=[]
+frag.append(IPv6ExtHdrFragment(nh=58, id=pid, m=1)/str(packet)[40:56])
+offset=2
+chunk=4
+while 40+8*(offset+chunk) < len(payload):
+	frag.append(IPv6ExtHdrFragment(nh=58, id=pid, offset=offset, m=1)/
+	    str(packet)[40+(8*offset):40+8*(offset+chunk)])
+	offset+=chunk
+frag.append(IPv6ExtHdrFragment(nh=58, id=pid, offset=offset)/
+    str(packet)[40+(8*offset):])
+eth=[]
+for f in frag:
+	pkt=IPv6(src=SRC_OUT6, dst=DST_IN6)/f
+	eth.append(Ether(src=SRC_MAC, dst=DST_MAC)/pkt)
+
+if os.fork() == 0:
+	time.sleep(1)
+	sendp(eth, iface=SRC_IF)
+	os._exit(0)
+
+ans=sniff(iface=SRC_IF, timeout=3, filter=
+    "ip6 and src "+DST_IN6+" and dst "+SRC_OUT6+" and proto ipv6-frag")
+for a in ans:
+	if a and a.type == scapy.layers.dot11.ETHER_TYPES.IPv6 and \
+	    ipv6nh[a.payload.nh] == 'Fragment Header' and \
+	    a.payload.payload.offset == 0 and \
+	    ipv6nh[a.payload.payload.nh] == 'ICMPv6' and \
+	    icmp6types[a.payload.payload.payload.type] == 'Echo Reply':
+		id=a.payload.payload.payload.id
+		print "id=%#x" % (id)
+		if id != pid:
+			print "WRONG ECHO REPLY ID"
+			exit(2)
+		exit(0)
+print "NO ECHO REPLY"
+exit(2)
--- regress/sys/netinet6/frag6/frag6_zerofirst.py
+++ regress/sys/netinet6/frag6/frag6_zerofirst.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python2.7
-# send ping6 fragment without payload with other proto before first fragment 
+# send ping6 fragment without payload with other proto before first fragment
 
 # ||
 # |---------|
