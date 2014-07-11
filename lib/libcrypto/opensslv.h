ab91d4b06864edd1757bb08f94a38a199dc1e9ca
beck <beck@openbsd.org>
2014-07-11 21:21:59 +0000

    Provide LIBRESSL_VERSION_NUMBER for people who use such things to detect versions distinct from OPENSSL_BLAH_WOOF.. ok jsing@ tedu@ deraadt@

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,7 +1,8 @@
-/* $OpenBSD: opensslv.h,v 1.26 2014/07/11 11:43:49 jsing Exp $ */
+/* $OpenBSD: opensslv.h,v 1.27 2014/07/11 21:21:59 beck Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
+#define LIBRESSL_VERSION_NUMBER	0x20000000L
 #define OPENSSL_VERSION_NUMBER	0x20000000L
 #define OPENSSL_VERSION_TEXT	"LibreSSL 2.0"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
