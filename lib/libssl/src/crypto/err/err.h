fbc627cc42304bdfa3eac86e9f0470915e7b6123
jsing <jsing@openbsd.org>
2014-07-11 09:25:24 +0000

    Revert change that snuck into previous commit.

--- lib/libssl/src/crypto/err/err.h
+++ lib/libssl/src/crypto/err/err.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: err.h,v 1.20 2014/07/11 08:44:48 jsing Exp $ */
+/* $OpenBSD: err.h,v 1.21 2014/07/11 09:25:24 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -114,10 +114,8 @@
 
 #include <openssl/opensslconf.h>
 
-#ifndef LIBRESSL_INTERNAL
 #include <stdio.h>
 #include <stdlib.h>
-#endif
 
 #include <openssl/ossl_typ.h>
 #ifndef OPENSSL_NO_BIO
