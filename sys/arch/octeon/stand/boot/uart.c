7fd79dbc59470201f3cf48996184e878f51b8ab7
jasper <jasper@openbsd.org>
2014-07-12 12:16:36 +0000

    - rewrite register reading/writing to use octeon_xkphys_{read,write}_8 which   is the right approach - write into _THR instead of _RBR for cnputc() as per miod's suggestion
    this doesn't fix the first character that gets eaten, but it's the right way
    to move forward.
    
    with pirofti@

--- sys/arch/octeon/stand/boot/uart.c
+++ sys/arch/octeon/stand/boot/uart.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uart.c,v 1.5 2014/07/11 15:38:20 jasper Exp $	*/
+/*	$OpenBSD: uart.c,v 1.6 2014/07/12 12:16:36 jasper Exp $	*/
 
 /*
  * Copyright (c) 2001-2004 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -31,25 +31,28 @@
 #include <sys/tty.h>
 
 #include <machine/bus.h>
+#include <machine/cpu.h>
 
 #include <dev/ic/comreg.h>
 #include <dev/ic/comvar.h>
 #include <dev/cons.h>
+#include <machine/octeonvar.h>
 
 #include "libsa.h"
 
-#define  OCTEON_MIO_UART0               0x8001180000000800ull
-#define  OCTEON_MIO_UART0_LSR           0x8001180000000828ull
-#define  OCTEON_MIO_UART0_RBR           0x8001180000000800ull
-#define  OCTEON_MIO_UART0_IER           0x8001180000000808ull
-#define  OCTEON_MIO_UART0_IIR           0x8001180000000810ull
-#define  OCTEON_MIO_UART0_MSR           0x8001180000000830ull
-#define  OCTEON_MIO_UART0_USR           0x8001180000000938ull
-#define  OCTEON_MIO_UART0_LCR           0x8001180000000818ull
-#define  OCTEON_MIO_UART0_MCR           0x8001180000000820ull
-#define  OCTEON_MIO_UART0_FCR           0x8001180000000850ull
-#define  OCTEON_MIO_UART0_DLL           0x8001180000000880ull
-#define  OCTEON_MIO_UART0_DLH           0x8001180000000888ull
+#define  OCTEON_MIO_UART0_RBR           0x0001180000000800ull
+#define  OCTEON_MIO_UART0_IER           0x0001180000000808ull
+#define  OCTEON_MIO_UART0_IIR           0x0001180000000810ull
+#define  OCTEON_MIO_UART0_LCR           0x0001180000000818ull
+#define  OCTEON_MIO_UART0_MCR           0x0001180000000820ull
+#define  OCTEON_MIO_UART0_LSR           0x0001180000000828ull
+#define  OCTEON_MIO_UART0_MSR           0x0001180000000830ull
+#define  OCTEON_MIO_UART0_THR           0x0001180000000840ull
+#define  OCTEON_MIO_UART0_USR           0x0001180000000938ull
+#define  OCTEON_MIO_UART0_FCR           0x0001180000000850ull
+#define  OCTEON_MIO_UART0_DLL           0x0001180000000880ull
+#define  OCTEON_MIO_UART0_DLH           0x0001180000000888ull
+
 #define  USR_TXFIFO_NOTFULL		2
 
 int cn30xxuart_delay(void);
@@ -62,22 +65,20 @@ int
 cn30xxuart_delay(void)
 {
 	int divisor;
-	u_char lcr;
+	uint8_t lcr;
+	lcr = octeon_xkphys_read_8(OCTEON_MIO_UART0_LCR);
+	octeon_xkphys_write_8(OCTEON_MIO_UART0_LCR, lcr | LCR_DLAB);
+	divisor = *(int *) (octeon_xkphys_read_8(OCTEON_MIO_UART0_DLL) | octeon_xkphys_read_8(OCTEON_MIO_UART0_DLH) << 8);
+	octeon_xkphys_write_8(OCTEON_MIO_UART0_LCR, lcr);
 
-	lcr = (u_char)*(uint64_t*)OCTEON_MIO_UART0_LCR;
-	*(uint64_t*)OCTEON_MIO_UART0_LCR = lcr | LCR_DLAB;
-	divisor = (int)(*(uint64_t*)OCTEON_MIO_UART0_DLL |
-		*(uint64_t*)OCTEON_MIO_UART0_DLH << 8);
-	*(uint64_t*)OCTEON_MIO_UART0_LCR = lcr;
-
-	return 10; /* return an approx delay value */
+	return (10);
 }
 
 void
 cn30xxuart_wait_txhr_empty(int d)
 {
-	while (((*(uint64_t*)OCTEON_MIO_UART0_LSR & LSR_TXRDY) == 0) &&
-               ((*(uint64_t*)OCTEON_MIO_UART0_USR & USR_TXFIFO_NOTFULL) == 0))
+	while(((octeon_xkphys_read_8(OCTEON_MIO_UART0_LSR) & LSR_TXRDY) == 0) &&
+	        ((octeon_xkphys_read_8(OCTEON_MIO_UART0_USR) & USR_TXFIFO_NOTFULL) == 0))
 		delay(d);
 }
 
@@ -101,7 +102,7 @@ cn30xxuartcnputc (dev_t dev, int c)
 	/* 1/10th the time to transmit 1 character (estimate). */
 	d = cn30xxuart_delay();
         cn30xxuart_wait_txhr_empty(d);
-	*(uint64_t*)OCTEON_MIO_UART0_RBR = (uint8_t)c;
+	octeon_xkphys_write_8(OCTEON_MIO_UART0_THR, c);
         cn30xxuart_wait_txhr_empty(d);
 }
 
@@ -113,10 +114,8 @@ cn30xxuartcngetc (dev_t dev)
 	/* 1/10th the time to transmit 1 character (estimate). */
 	d = cn30xxuart_delay();
 
-	while ((*(uint64_t*)OCTEON_MIO_UART0_LSR & LSR_RXRDY) == 0)
+	while ((octeon_xkphys_read_8(OCTEON_MIO_UART0_LSR) & LSR_RXRDY) == 0)
 		delay(d);
-
-	c = (uint8_t)*(uint64_t*)OCTEON_MIO_UART0_RBR;
-
+	c = octeon_xkphys_read_8(OCTEON_MIO_UART0_RBR);
 	return (c);
 }
