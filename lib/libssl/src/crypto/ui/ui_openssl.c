38bc216985d4d4d188db42ca08b0b9d95125869f
deraadt <deraadt@openbsd.org>
2014-07-13 00:10:47 +0000

    remove silly cast

--- lib/libssl/src/crypto/ui/ui_openssl.c
+++ lib/libssl/src/crypto/ui/ui_openssl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ui_openssl.c,v 1.22 2014/07/11 08:44:49 jsing Exp $ */
+/* $OpenBSD: ui_openssl.c,v 1.23 2014/07/13 00:10:47 deraadt Exp $ */
 /* Written by Richard Levitte (richard@levitte.org) and others
  * for the OpenSSL project 2001.
  */
@@ -267,7 +267,7 @@ read_string_inner(UI *ui, UI_STRING *uis, int echo, int strip_nl)
 		goto error;
 	if (ferror(tty_in))
 		goto error;
-	if ((p = (char *) strchr(result, '\n')) != NULL) {
+	if ((p = strchr(result, '\n')) != NULL) {
 		if (strip_nl)
 			*p = '\0';
 	} else if (!read_till_nl(tty_in))
