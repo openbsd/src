c80209b5f06135ee44290e6929d2f588256705bd
miod <miod@openbsd.org>
2014-07-13 15:48:32 +0000

    Remove stale comment which used to explain why we had a special 31-bit freelist for DMA, back when this was applicable.

--- sys/arch/sgi/include/vmparam.h
+++ sys/arch/sgi/include/vmparam.h
@@ -1,20 +1,9 @@
-/*	$OpenBSD: vmparam.h,v 1.9 2011/05/30 22:25:22 oga Exp $ */
+/*	$OpenBSD: vmparam.h,v 1.10 2014/07/13 15:48:32 miod Exp $ */
 /* public domain */
 #ifndef _MACHINE_VMPARAM_H_
 #define _MACHINE_VMPARAM_H_
 
 #define	VM_PHYSSEG_MAX	32	/* Max number of physical memory segments */
-
-/*
- * On Origin and Octane families, DMA to 32-bit PCI devices is restricted.
- *
- * Systems with physical memory after the 2GB boundary need to ensure
- * memory which may used for DMA transfers is allocated from the low
- * memory range.
- *
- * Other systems, like the O2, do not have such a restriction, but can not
- * have more than 2GB of physical memory, so this doesn't affect them.
- */
 
 #include <mips64/vmparam.h>
 
