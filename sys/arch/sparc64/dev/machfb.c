6de0776383802ee51cb75d8c946d28c942891536
kettenis <kettenis@openbsd.org>
2014-07-28 06:21:29 +0000

    Hardware supported by machfb(4) needs the aperture (for now) so make ramdisks print a message to that extent such that the install scripts can do their magic.
    ok miod@, deraadt@

--- sys/arch/sparc64/dev/machfb.c
+++ sys/arch/sparc64/dev/machfb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machfb.c,v 1.8 2013/10/20 20:07:27 miod Exp $	*/
+/*	$OpenBSD: machfb.c,v 1.9 2014/07/28 06:21:29 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2009 Mark Kettenis.
@@ -294,6 +294,10 @@ machfb_attach(struct device *parent, struct device *self, void *aux)
 	}
 
 	printf(", %dx%d\n", sc->sc_sunfb.sf_width, sc->sc_sunfb.sf_height);
+
+#ifdef SMALL_KERNEL
+	printf("%s: aperture needed\n", self->dv_xname);
+#endif
 
 	ri = &sc->sc_sunfb.sf_ro;
 	ri->ri_bits = bus_space_vaddr(sc->sc_memt, sc->sc_memh);
