c2235d37adbf8264f37d9012501da605fa859ca5
schwarze <schwarze@openbsd.org>
2014-08-08 17:17:42 +0000

    mansearch.h uses uint64_t, so it needs stdint.h; found on Linux

--- usr.bin/mandoc/cgi.c
+++ usr.bin/mandoc/cgi.c
@@ -1,4 +1,4 @@
-/*	$Id: cgi.c,v 1.31 2014/07/25 21:05:38 schwarze Exp $ */
+/*	$Id: cgi.c,v 1.32 2014/08/08 17:17:42 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014 Ingo Schwarze <schwarze@usta.de>
@@ -19,6 +19,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
