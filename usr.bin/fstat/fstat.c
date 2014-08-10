61c1eafe6c069b602917536764011898e60c8f6b
guenther <guenther@openbsd.org>
2014-08-10 07:29:45 +0000

    Don't need to know how to format bluetooth socket info here

--- usr.bin/fstat/fstat.c
+++ usr.bin/fstat/fstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fstat.c,v 1.77 2014/05/30 07:14:46 guenther Exp $	*/
+/*	$OpenBSD: fstat.c,v 1.78 2014/08/10 07:29:45 guenther Exp $	*/
 
 /*
  * Copyright (c) 2009 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -787,12 +787,6 @@ socktrans(struct kinfo_file *kf)
 	case AF_ROUTE:
 		/* print protocol number and socket address */
 		printf("* route %s", stype);
-		printf(" %d ", kf->so_protocol);
-		hide((void *)(uintptr_t)kf->f_data);
-		break;
-	case AF_BLUETOOTH:
-		/* print protocol number and socket address */
-		printf("* bluetooth %s", stype);
 		printf(" %d ", kf->so_protocol);
 		hide((void *)(uintptr_t)kf->f_data);
 		break;
