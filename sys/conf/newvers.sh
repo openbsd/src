57bdc2b5043c4a3a436edf725821842a2dfc554b
miod <miod@openbsd.org>
2014-08-11 18:33:36 +0000

    -current dammit

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.133 2014/07/29 12:56:41 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.134 2014/08/11 18:33:36 miod Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,11 +64,11 @@ ost="OpenBSD"
 osr="5.6"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
 #if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
-#define STATUS "-current"		/* just after a release */
 #endif
+#define STATUS "-current"		/* just after a release */
 
 const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
