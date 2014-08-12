4eb958b1ede356b2d47cbd28dc3f2b1351fc5c6c
miod <miod@openbsd.org>
2014-08-12 19:35:28 +0000

    Lower VM_PHYSSEG_MAX from the mips64 default to a generous 4, and change allocation strategy to VM_PSTRAT_BIGFIRST.

--- sys/arch/octeon/include/vmparam.h
+++ sys/arch/octeon/include/vmparam.h
@@ -1,3 +1,11 @@
-/*	$OpenBSD: vmparam.h,v 1.1 2010/09/20 06:32:30 syuu Exp $ */
+/*	$OpenBSD: vmparam.h,v 1.2 2014/08/12 19:35:28 miod Exp $ */
 /* public domain */
+#ifndef _MACHINE_VMPARAM_H_
+#define _MACHINE_VMPARAM_H_
+
+#define	VM_PHYSSEG_MAX		4 /* Max number of physical memory segments */
+#define	VM_PHYSSEG_STRAT	VM_PSTRAT_BIGFIRST
+
 #include <mips64/vmparam.h>
+
+#endif	/* _MACHINE_VMPARAM_H_ */
