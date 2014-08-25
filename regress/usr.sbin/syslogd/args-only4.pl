edb6f5c369dbe7a28fd0481a60c7a313932ecc95
bluhm <bluhm@openbsd.org>
2014-08-25 19:32:29 +0000

    Add tests where the IP address family has been chosen by -4 or -6 command line switch and udp4:// or udp6:// loghost prefix.

---
+++ regress/usr.sbin/syslogd/args-only4.pl
@@ -0,0 +1,33 @@
+# The client writes a message to a localhost IPv4 UDP socket.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd -4 passes it via IPv4 UDP to the loghost.
+# The server receives the message on its UDP socket.
+# Find the message in client, file, pipe, syslogd, server log.
+# Check that the syslogd has no IPv6 socket in fstat output.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	connect => { domain => AF_INET, addr => "127.0.0.1", port => 514 },
+    },
+    syslogd => {
+	fstat => 1,
+	loghost => '@127.0.0.1:$connectport',
+	options => ["-4nu"],
+    },
+    server => {
+	listen => { domain => AF_INET, addr => "127.0.0.1" },
+    },
+    file => {
+	loggrep => qr/ 127.0.0.1 /. get_log(),
+    },
+    fstat => {
+	loggrep => {
+	    qr/ internet6 / => 0,
+	},
+    },
+);
+
+1;
---
+++ regress/usr.sbin/syslogd/args-proto-udp.pl
@@ -0,0 +1,24 @@
+# The client writes a message to Sys::Syslog native method.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via explicit UDP to the loghost.
+# The server receives the message on its UDP socket.
+# Find the message in client, file, pipe, syslogd, server log.
+# Check that syslogd log contains the 127.0.0.1 address.
+
+use strict;
+use warnings;
+
+our %args = (
+    syslogd => {
+	loghost => '@udp://127.0.0.1:$connectport',
+	loggrep => {
+	    qr/Logging to FORW \@udp:\/\/127.0.0.1:\d+/ => 5,
+	    get_log() => 1,
+	},
+    },
+    server => {
+	listen => { domain => AF_INET, addr => "127.0.0.1" },
+    },
+);
+
+1;
