82328041b46184907d0e8ec060af0a72978d211d
kspillner <kspillner@openbsd.org>
2014-08-27 15:22:40 +0000

    Ensure diff -uw always produces valid output when one file doesn't end with a newline.  Issue reported by guenther@.
    ok guenther@

--- usr.bin/diff/diffreg.c
+++ usr.bin/diff/diffreg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: diffreg.c,v 1.82 2012/07/08 15:48:56 stsp Exp $	*/
+/*	$OpenBSD: diffreg.c,v 1.83 2014/08/27 15:22:40 kspillner Exp $	*/
 
 /*
  * Copyright (C) Caldera International Inc.  2001-2002.
@@ -778,10 +778,14 @@ check(FILE *f1, FILE *f2, int flags)
 				 * GNU diff ignores a missing newline
 				 * in one file for -b or -w.
 				 */
-				if ((flags & (D_FOLDBLANKS|D_IGNOREBLANKS)) &&
-				    ((c == EOF && d == '\n') ||
-				    (c == '\n' && d == EOF))) {
-					break;
+				if (flags & (D_FOLDBLANKS|D_IGNOREBLANKS)) {
+					if (c == EOF && d == '\n') {
+						ctnew++;
+						break;
+					} else if (c == '\n' && d == EOF) {
+						ctold++;
+						break;
+					}
 				}
 				ctold++;
 				ctnew++;
