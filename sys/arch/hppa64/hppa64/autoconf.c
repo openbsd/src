7c967a7383bf3c76ae4009df2df35f2003827b9d
miod <miod@openbsd.org>
2014-08-29 21:21:39 +0000

    Use the load average from averunnable.ldavg[] instead of bogus cp_time[] arithmetic to display the load average in leds 5-8 of 8 led systems.

--- sys/arch/hppa64/hppa64/autoconf.c
+++ sys/arch/hppa64/hppa64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.18 2014/07/12 18:44:41 tedu Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.19 2014/08/29 21:21:39 miod Exp $	*/
 
 /*
  * Copyright (c) 1998-2005 Michael Shalayeff
@@ -148,22 +148,16 @@ diskconf(void)
 void
 heartbeat(void *v)
 {
-	static u_int hbcnt = 0, ocp_total, ocp_idle;
-	int toggle, cp_mask, cp_total, cp_idle;
+	static u_int hbcnt = 0;
+	int toggle, cp_mask;
 
 	timeout_add(&heartbeat_tmo, hz / 16);
 
-	cp_idle = cp_time[CP_IDLE];
-	cp_total = cp_time[CP_USER] + cp_time[CP_NICE] + cp_time[CP_SYS] +
-	    cp_time[CP_INTR] + cp_time[CP_IDLE];
-	if (cp_total == ocp_total)
-		cp_total = ocp_total + 1;
-	if (cp_idle == ocp_idle)
-		cp_idle = ocp_idle + 1;
-	cp_mask = 0xf0 >> (cp_idle - ocp_idle) * 4 / (cp_total - ocp_total);
-	cp_mask &= 0xf0;
-	ocp_total = cp_total;
-	ocp_idle = cp_idle;
+	/* display the load average in the upper leds if any */
+	cp_mask = averunnable.ldavg[0] >> FSHIFT;
+	if (cp_mask > 0x0f)
+		cp_mask = 0x0f;
+	cp_mask <<= 4;
 	/*
 	 * do this:
 	 *
