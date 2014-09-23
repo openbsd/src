3e3129e8fd16be6441472cf92b970812ffebbd50
dlg <dlg@openbsd.org>
2014-09-23 02:53:59 +0000

    dont need to call bpfdetach cos if_detach does it for you.
    delete some stupid comments while there.
    
    ok deraadt@ brad@

--- sys/dev/ic/smc91cxx.c
+++ sys/dev/ic/smc91cxx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smc91cxx.c,v 1.35 2014/07/22 13:12:12 mpi Exp $	*/
+/*	$OpenBSD: smc91cxx.c,v 1.36 2014/09/23 02:53:59 dlg Exp $	*/
 /*	$NetBSD: smc91cxx.c,v 1.11 1998/08/08 23:51:41 mycroft Exp $	*/
 
 /*-
@@ -697,7 +697,6 @@ smc91cxx_start(ifp)
 	ifp->if_timer = 5;
 
 #if NBPFILTER > 0
-	/* Hand off a copy to the bpf. */
 	if (ifp->if_bpf)
 		bpf_mtap(ifp->if_bpf, top, BPF_DIRECTION_OUT);
 #endif
@@ -997,10 +996,6 @@ smc91cxx_read(sc)
 	ifp->if_ipackets++;
 
 #if NBPFILTER > 0
-	/*
-	 * Hand the packet off to bpf listeners.  If there's a bpf listener,
-	 * we need to check if the packet is ours.
-	 */
 	if (ifp->if_bpf)
 		bpf_mtap(ifp->if_bpf, m, BPF_DIRECTION_IN);
 #endif
@@ -1239,9 +1234,6 @@ smc91cxx_detach(self, flags)
 	/* Delete all media. */
 	ifmedia_delete_instance(&sc->sc_mii.mii_media, IFM_INST_ANY);
 
-#if NBPFILTER > 0
-	bpfdetach(ifp);
-#endif
 	ether_ifdetach(ifp);
 	if_detach(ifp);
 
