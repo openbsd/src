595769cc1d80b08e2d2e05e04f1db6f1fa22530b
reyk <reyk@openbsd.org>
2014-09-28 14:26:42 +0000

    Remove a limitation that ignored IPv6 link-local addresses on carp(4). This workaround was needed when the interface link-local address was still enabled by default to prevent immediate state transition on interfaces without any configured addresses.  This is not needed anymore and removing the limitation allows to use carp(4) with addresses like fe80::2%carp0.
    discussed with some
    ok mpi@ pelikan@

--- sys/netinet/ip_carp.c
+++ sys/netinet/ip_carp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_carp.c,v 1.234 2014/09/08 06:24:13 jsg Exp $	*/
+/*	$OpenBSD: ip_carp.c,v 1.235 2014/09/28 14:26:42 reyk Exp $	*/
 
 /*
  * Copyright (c) 2002 Michael Shalayeff. All rights reserved.
@@ -1807,8 +1807,7 @@ carp_addr_updated(void *v)
 		if (ifa->ifa_addr->sa_family == AF_INET)
 			new_naddrs++;
 #ifdef INET6
-		else if (ifa->ifa_addr->sa_family == AF_INET6 &&
-		    !IN6_IS_ADDR_LINKLOCAL(&ifatoia6(ifa)->ia_addr.sin6_addr))
+		else if (ifa->ifa_addr->sa_family == AF_INET6)
 			new_naddrs6++;
 #endif /* INET6 */
 	}
