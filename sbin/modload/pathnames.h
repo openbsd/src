82ecdc0202b48ac03ea19c0d9e2886d84816430c
tedu <tedu@openbsd.org>
2014-10-09 03:43:34 +0000

    no more modules

--- sbin/modload/pathnames.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: pathnames.h,v 1.4 2002/01/08 21:28:38 ericj Exp $	*/
-/*	$NetBSD: pathnames.h,v 1.2 1995/03/18 14:56:46 cgd Exp $	*/
-
-#include <paths.h>
-
-#define	_PATH_LKM	"/dev/lkm"
--- sbin/modunload/pathnames.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: pathnames.h,v 1.2 1996/06/23 14:31:08 deraadt Exp $	*/
-/*	$NetBSD: pathnames.h,v 1.2 1995/03/18 14:56:51 cgd Exp $	*/
-
-#include <paths.h>
-
-#define	_PATH_LKM	"/dev/lkm"
