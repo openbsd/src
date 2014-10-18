24767d70a034d00f013c1354a004ee30b66a50ee
kettenis <kettenis@openbsd.org>
2014-10-18 17:28:34 +0000

    Make sure the direct map isn't executable on hardware that allows us to do so. Enforcing W^X in the kernel like this mitigates at least some ret2dir attacks.
    ok mlarkin@, deraadt@

--- sys/arch/amd64/amd64/pmap.c
+++ sys/arch/amd64/amd64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.74 2014/10/06 20:34:58 sf Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.75 2014/10/18 17:28:34 kettenis Exp $	*/
 /*	$NetBSD: pmap.c,v 1.3 2003/05/08 18:13:13 thorpej Exp $	*/
 
 /*
@@ -643,7 +643,7 @@ pmap_bootstrap(paddr_t first_avail, paddr_t max_pa)
 	}
 
 	kpm->pm_pdir[PDIR_SLOT_DIRECT] = dmpdp | PG_V | PG_KW | PG_U |
-	    PG_M;
+	    PG_M | pg_nx;
 
 	tlbflush();
 
