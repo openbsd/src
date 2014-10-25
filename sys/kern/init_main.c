c965d262f847c2562bd6726044a30e171e316f8b
miod <miod@openbsd.org>
2014-10-25 12:53:14 +0000

    Do not launch the page zeroing thread on MULTIPROCESSOR m88k systems. This causes a deadlock between reaper and zerothread I am currently investigating.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.222 2014/10/17 01:51:39 tedu Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.223 2014/10/25 12:53:14 miod Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -531,7 +531,7 @@ main(void *framep)
 	if (kthread_create(uvm_aiodone_daemon, NULL, NULL, "aiodoned"))
 		panic("fork aiodoned");
 
-#ifndef __hppa__
+#if !defined(__hppa__) && !(defined(__m88k__) && defined(MULTIPROCESSOR))
 	/* Create the page zeroing kernel thread. */
 	if (kthread_create(uvm_pagezero_thread, NULL, NULL, "zerothread"))
 		panic("fork zerothread");
