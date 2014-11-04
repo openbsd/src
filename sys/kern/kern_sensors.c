5473e273779ba226529207e7e4319a7b9b9fdacf
jasper <jasper@openbsd.org>
2014-11-04 18:29:32 +0000

    panic when a sensordev gets installed twice, instead of blowing up amazingly later on
    ok deraadt@

--- sys/kern/kern_sensors.c
+++ sys/kern/kern_sensors.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sensors.c,v 1.30 2014/11/01 23:58:28 tedu Exp $	*/
+/*	$OpenBSD: kern_sensors.c,v 1.31 2014/11/04 18:29:32 jasper Exp $	*/
 
 /*
  * Copyright (c) 2005 David Gwynne <dlg@openbsd.org>
@@ -50,7 +50,7 @@ sensordev_install(struct ksensordev *sensdev)
 		for (v = SLIST_FIRST(&sensordev_list);
 		    (nv = SLIST_NEXT(v, list)) != NULL; v = nv)
 			if (nv->num - v->num > 1)
-				break;
+				panic("sensor installed twice");
 		sensdev->num = v->num + 1;
 		SLIST_INSERT_AFTER(v, sensdev, list);
 	}
