a876f2470c0e9a7d6cfcbf53be0fc9a225c42d52
nicm <nicm@openbsd.org>
2014-12-01 22:22:14 +0000

    Another fix for insertion from Balazs Kezes. On insertion, size the line just enough for the inserted characters.

--- usr.bin/tmux/grid-view.c
+++ usr.bin/tmux/grid-view.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: grid-view.c,v 1.19 2014/11/12 22:59:45 nicm Exp $ */
+/* $OpenBSD: grid-view.c,v 1.20 2014/12/01 22:22:14 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -184,9 +184,10 @@ grid_view_insert_cells(struct grid *gd, u_int px, u_int py, u_int nx)
 	px = grid_view_x(gd, px);
 	py = grid_view_y(gd, py);
 
-	sx = grid_view_x(gd, gd->linedata[py].cellsize);
-	if (sx < px + nx)
-		sx = px + nx;
+	if (gd->linedata[py].cellsize + nx < gd->sx)
+		sx = grid_view_x(gd, gd->linedata[py].cellsize + nx);
+	else
+		sx = grid_view_x(gd, gd->sx);
 
 	if (px == sx - 1)
 		grid_clear(gd, px, py, 1, 1);
