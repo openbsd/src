a2e5b9cfbd50117e35e6a895dc523efcc4631c22
dcoppa <dcoppa@openbsd.org>
2014-12-04 08:19:03 +0000

    Disallow file allocations on directories that have been removed (tn_links == 0). Failure to enforce such a check can lead to the violation of the assumption that removed directories should not contain directory entries and thus trigger a kernel diagnostic assertion (panic).
    Fix provided by Pedro Martelletto, thanks!
    
    OK millert@

--- sys/tmpfs/tmpfs_subr.c
+++ sys/tmpfs/tmpfs_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tmpfs_subr.c,v 1.8 2014/11/18 02:37:31 tedu Exp $	*/
+/*	$OpenBSD: tmpfs_subr.c,v 1.9 2014/12/04 08:19:03 dcoppa Exp $	*/
 /*	$NetBSD: tmpfs_subr.c,v 1.79 2012/03/13 18:40:50 elad Exp $	*/
 
 /*
@@ -395,6 +395,11 @@ tmpfs_alloc_file(struct vnode *dvp, struct vnode **vpp, struct vattr *vap,
 
 	if (TMPFS_DIRSEQ_FULL(dnode)) {
 		error = ENOSPC;
+		goto out;
+	}
+
+	if (dnode->tn_links == 0) {
+		error = ENOENT;
 		goto out;
 	}
 
