31216e72ae899621e9431c6a94ea8866f5231e6d
eric <eric@openbsd.org>
2014-12-24 08:43:58 +0000

    missing include

--- usr.sbin/smtpd/ca.c
+++ usr.sbin/smtpd/ca.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ca.c,v 1.11 2014/10/02 19:14:56 gilles Exp $	*/
+/*	$OpenBSD: ca.c,v 1.12 2014/12/24 08:43:58 eric Exp $	*/
 
 /*
  * Copyright (c) 2014 Reyk Floeter <reyk@openbsd.org>
@@ -21,6 +21,7 @@
 #include <sys/queue.h>
 #include <sys/socket.h>
 
+#include <signal.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
