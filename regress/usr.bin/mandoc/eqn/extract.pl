08d5dbf19571f4526e107d63ae260831c1e79b6f
schwarze <schwarze@openbsd.org>
2015-01-01 12:53:46 +0000

    eqn(7) tests

---
+++ regress/usr.bin/mandoc/eqn/extract.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my ($begun, $ended);
+
+while (<>) {
+	chomp;
+	if (not $begun) {
+		s/.*<math class="eqn">// or next;
+		$begun = 1;
+		next unless length;
+	}
+	s/<\/math>.*// and $ended = 1;
+	print "$_\n" if length;
+	exit 0 if $ended;
+}
+
+die "unexpected end of file";
