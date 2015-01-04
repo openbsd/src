860049aa036e846a53695d3c86fffb9b6001955c
dlg <dlg@openbsd.org>
2015-01-04 08:54:01 +0000

    back out r1.173, aka the "* 8" diff. it tickles a problem on some landisk machines. we've been unable to figure out due to a lack of hardware (on my part) or time.
    discussed with and ok miod@

--- sys/kern/subr_pool.c
+++ sys/kern/subr_pool.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_pool.c,v 1.175 2015/01/04 02:53:19 jsg Exp $	*/
+/*	$OpenBSD: subr_pool.c,v 1.176 2015/01/04 08:54:01 dlg Exp $	*/
 /*	$NetBSD: subr_pool.c,v 1.61 2001/09/26 07:14:56 chs Exp $	*/
 
 /*-
@@ -232,7 +232,7 @@ pool_init(struct pool *pp, size_t size, u_int align, u_int ioff, int flags,
 	size = roundup(size, align);
 
 	if (palloc == NULL) {
-		while (size * 8 > pgsize)
+		while (size > pgsize)
 			pgsize <<= 1;
 
 		if (pgsize > PAGE_SIZE) {
