b110652143650128975fb4186ff7426234f958e8
djm <djm@openbsd.org>
2015-01-14 13:54:13 +0000

    update sftp client and server to new buffer API. pretty much just mechanical changes; with & ok markus

--- usr.bin/ssh/sftp.c
+++ usr.bin/ssh/sftp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sftp.c,v 1.168 2014/11/26 18:34:51 millert Exp $ */
+/* $OpenBSD: sftp.c,v 1.169 2015/01/14 13:54:13 djm Exp $ */
 /*
  * Copyright (c) 2001-2004 Damien Miller <djm@openbsd.org>
  *
@@ -44,7 +44,8 @@
 #include "misc.h"
 
 #include "sftp.h"
-#include "buffer.h"
+#include "ssherr.h"
+#include "sshbuf.h"
 #include "sftp-common.h"
 #include "sftp-client.h"
 
