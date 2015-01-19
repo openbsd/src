f153452a7b92c3ebdd7f56e6f93752fbf08d76ac
deraadt <deraadt@openbsd.org>
2015-01-19 07:41:01 +0000

    If SIGUSR1 and SIGUSR2 are unavailable, don't invent something crazy honouring systems before 4.3BSD.

--- usr.sbin/rbootd/defs.h
+++ usr.sbin/rbootd/defs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: defs.h,v 1.9 2015/01/16 06:40:19 deraadt Exp $	*/
+/*	$OpenBSD: defs.h,v 1.10 2015/01/19 07:41:01 deraadt Exp $	*/
 /*	$NetBSD: defs.h,v 1.5 1995/10/06 05:12:14 thorpej Exp $	*/
 
 /*
@@ -50,16 +50,6 @@
 **  Common #define's and external variables.  All other files should
 **  include this.
 */
-
-/*
- *  SIGUSR1 and SIGUSR2 are defined in <signal.h> for 4.3BSD systems.
- */
-#ifndef SIGUSR1
-#define	SIGUSR1 SIGEMT
-#endif
-#ifndef SIGUSR2
-#define	SIGUSR2 SIGFPE
-#endif
 
 /*
  *  These can be faster & more efficient than strcmp()/strncmp()...
