48cb37502010d2dfde09b114ecc62378b9e4adad
jsg <jsg@openbsd.org>
2015-02-07 01:02:25 +0000

    remove linux int types in the drm header kdump includes all headers with ioctls and the v4l videoio.h already defines these.  Found the hard way by deraadt. The types will be patched out of future libdrm updates.

--- sys/dev/pci/drm/drm.h
+++ sys/dev/pci/drm/drm.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: drm.h,v 1.17 2015/02/06 23:42:33 jsg Exp $ */
+/* $OpenBSD: drm.h,v 1.18 2015/02/07 01:02:25 jsg Exp $ */
 /**
  * \file drm.h
  * Header for the Direct Rendering Manager
@@ -49,16 +49,6 @@
 
 #include <sys/ioccom.h>
 #include <sys/types.h>
-#ifndef _KERNEL
-typedef int8_t   __s8;
-typedef uint8_t  __u8;
-typedef int16_t  __s16;
-typedef uint16_t __u16;
-typedef int32_t  __s32;
-typedef uint32_t __u32;
-typedef int64_t  __s64;
-typedef uint64_t __u64;
-#endif
 typedef unsigned long drm_handle_t;
 
 #define DRM_IOCTL_NR(n)		((n) & 0xff)
