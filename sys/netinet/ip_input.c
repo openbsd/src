d24fa91a2cefaf4ac5dda7c060ceac9ed29ba634
claudio <claudio@openbsd.org>
2015-02-08 04:14:47 +0000

    Just use sysctl_rdstruct() to read out some structs. Only difference to current code is that you can no longer call this with a NULL oldlenp which does not make any sense. OK phessler, henning Behaviour change pointed out by miod@

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_input.c,v 1.244 2015/01/12 13:51:45 mpi Exp $	*/
+/*	$OpenBSD: ip_input.c,v 1.245 2015/02/08 04:14:47 claudio Exp $	*/
 /*	$NetBSD: ip_input.c,v 1.30 1996/03/16 23:53:58 christos Exp $	*/
 
 /*
@@ -1620,15 +1620,11 @@ ip_sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp, void *newp,
 	        return (sysctl_ifq(name + 1, namelen - 1,
 		    oldp, oldlenp, newp, newlen, &ipintrq));
 	case IPCTL_STATS:
-		if (newp != NULL)
-			return (EPERM);
-		return (sysctl_struct(oldp, oldlenp, newp, newlen,
+		return (sysctl_rdstruct(oldp, oldlenp, newp,
 		    &ipstat, sizeof(ipstat)));
 	case IPCTL_MRTSTATS:
 #ifdef MROUTING
-		if (newp != NULL)
-			return (EPERM);
-		return (sysctl_struct(oldp, oldlenp, newp, newlen,
+		return (sysctl_rdstruct(oldp, oldlenp, newp,
 		    &mrtstat, sizeof(mrtstat)));
 #else
 		return (EOPNOTSUPP);
