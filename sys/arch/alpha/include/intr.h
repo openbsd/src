958f93b58241ba85180fd867997df1b893f58c8f
dlg <dlg@openbsd.org>
2015-02-11 01:13:22 +0000

    alpha (nor any other arch) uses any types or api provided by sys/lock.h.

--- sys/arch/alpha/include/intr.h
+++ sys/arch/alpha/include/intr.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: intr.h,v 1.43 2014/02/01 21:19:39 miod Exp $ */
+/* $OpenBSD: intr.h,v 1.44 2015/02/11 01:13:22 dlg Exp $ */
 /* $NetBSD: intr.h,v 1.26 2000/06/03 20:47:41 thorpej Exp $ */
 
 /*-
@@ -62,7 +62,6 @@
 #define _MACHINE_INTR_H_
 
 #include <sys/evcount.h>
-#include <sys/lock.h>
 #include <sys/mutex.h>
 #include <sys/queue.h>
 #include <machine/atomic.h>
--- sys/arch/alpha/include/pmap.h
+++ sys/arch/alpha/include/pmap.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: pmap.h,v 1.34 2015/02/09 22:35:06 miod Exp $ */
+/* $OpenBSD: pmap.h,v 1.35 2015/02/11 01:13:22 dlg Exp $ */
 /* $NetBSD: pmap.h,v 1.37 2000/11/19 03:16:35 thorpej Exp $ */
 
 /*-
@@ -74,7 +74,6 @@
 
 #ifdef _KERNEL
 
-#include <sys/lock.h>
 #include <sys/queue.h>
 
 /*
