74be6e735dd6b93bcadf7f2c8d4a7817e155bb57
miod <miod@openbsd.org>
2015-02-11 00:16:07 +0000

    zero ecx before cpuid(1), for some Cyrix processors do not really support cpuid with other values than zero, and leave the ecx register unchanged. ok kettenis@

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.149 2015/02/07 00:26:37 deraadt Exp $	*/
+/*	$OpenBSD: locore.s,v 1.150 2015/02/11 00:16:07 miod Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -398,6 +398,7 @@ try586:	/* Use the `cpuid' instruction. */
 	movl	$0,  RELOC(_C_LABEL(cpu_vendor))+12
 
 	movl	$1,%eax
+	xorl	%ecx,%ecx
 	cpuid
 	movl	%eax,RELOC(_C_LABEL(cpu_id))	# store cpu_id and features
 	movl	%ebx,RELOC(_C_LABEL(cpu_miscinfo))
