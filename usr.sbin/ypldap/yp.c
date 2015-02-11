ed29f1e0be343b13023eb3a384e45c3ffc4779e5
pelikan <pelikan@openbsd.org>
2015-02-11 01:26:00 +0000

    YPPROC_XFR will trigger a crash, even if using it doesn't make sense.
    ok deraadt miod

--- usr.sbin/ypldap/yp.c
+++ usr.sbin/ypldap/yp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yp.c,v 1.13 2015/01/16 06:40:22 deraadt Exp $ */
+/*	$OpenBSD: yp.c,v 1.14 2015/02/11 01:26:00 pelikan Exp $ */
 /*
  * Copyright (c) 2008 Pierre-Yves Ritschard <pyr@openbsd.org>
  *
@@ -209,7 +209,7 @@ yp_dispatch(struct svc_req *req, SVCXPRT *trans)
 		if (yp_check(req) == -1)
 			return;
 		svcerr_noproc(trans);
-		break;
+		return;
 	case YPPROC_CLEAR:
 		log_debug("ypproc_clear");
 		if (yp_check(req) == -1)
