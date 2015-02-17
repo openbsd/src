c527ae4232df9c2086338eac4890e89e758a2614
schwarze <schwarze@openbsd.org>
2015-02-17 17:55:12 +0000

    Cope with another one of the many kinds of DocBook stupidity:
    Instead of just using .br, DocBook sometimes fiddles with the
    utterly unportable internal register \n[an-break-flag] that is
    only available in the GNU implementation of man(7) and then arms
    an input line trap to call the equally unportable internal macro
    .an-trap that, in the GNU implementation, inspects that variable;
    all the world is GNU, isn't it?
    
    Since naddy@ reports that quite a few ports manuals suffer from
    this insanity, let's just translate it to the intended .br.
    
    Et ceterum censeo DocBookem esse delendam.

--- usr.bin/mandoc/roff.c
+++ usr.bin/mandoc/roff.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: roff.c,v 1.133 2015/02/17 17:16:12 schwarze Exp $ */
+/*	$OpenBSD: roff.c,v 1.134 2015/02/17 17:55:12 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2011, 2012, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -2335,10 +2335,19 @@ roff_it(ROFF_ARGS)
 		return(ROFF_IGN);
 	}
 
-	/* Arm the input line trap. */
+	while (isspace((unsigned char)buf->buf[pos]))
+		pos++;
+
+	/*
+	 * Arm the input line trap.
+	 * Special-casing "an-trap" is an ugly workaround to cope
+	 * with DocBook stupidly fiddling with man(7) internals.
+	 */
 
 	roffit_lines = iv;
-	roffit_macro = mandoc_strdup(buf->buf + pos);
+	roffit_macro = mandoc_strdup(iv != 1 ||
+	    strcmp(buf->buf + pos, "an-trap") ?
+	    buf->buf + pos : "br");
 	return(ROFF_IGN);
 }
 
