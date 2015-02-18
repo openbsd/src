be71ea879ff499d354a252a4099f77e3e8d83fc6
dlg <dlg@openbsd.org>
2015-02-18 10:08:19 +0000

    we're not using these macros

--- sys/dev/pci/if_myxreg.h
+++ sys/dev/pci/if_myxreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_myxreg.h,v 1.6 2014/03/24 01:18:28 dlg Exp $	*/
+/*	$OpenBSD: if_myxreg.h,v 1.7 2015/02/18 10:08:19 dlg Exp $	*/
 
 /*
  * Copyright (c) 2007 Reyk Floeter <reyk@openbsd.org>
@@ -33,8 +33,6 @@
 #define MYX_NTXDESC_MIN		2
 #define MYX_IRQCOALDELAY	60
 #define MYX_IRQDEASSERTWAIT	1
-#define MYX_MAX_MTU_SMALL	(ETHERMTU + ETHER_HDR_LEN + 4)
-#define MYX_MAX_MTU_BIG		PAGE_SIZE
 
 #define MYXALIGN_CMD		64
 #define MYXALIGN_DATA		PAGE_SIZE
