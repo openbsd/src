bc195cc885fa4eb1faee372f83ae375d2104929b
miod <miod@openbsd.org>
2015-03-09 20:08:55 +0000

    If my calculations are correct, when this baby hits 5.8... you're gonna see some serious shit.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.136 2015/03/04 14:31:13 jsg Exp $
+#	$OpenBSD: newvers.sh,v 1.137 2015/03/09 20:08:55 miod Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,11 +64,11 @@ ost="OpenBSD"
 osr="5.7"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
 #if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
-#define STATUS "-current"		/* just after a release */
 #endif
+#define STATUS "-current"		/* just after a release */
 
 const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
