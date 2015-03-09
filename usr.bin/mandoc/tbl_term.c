1eb4ab006be23747751335659afdf8ec64a0e4f1
schwarze <schwarze@openbsd.org>
2015-03-09 17:30:29 +0000

    Flush the line preceding a table before clearing the right margin, such that that line isn't output with unlimited width. Problem reported and fix OK by kristaps@.

--- usr.bin/mandoc/tbl_term.c
+++ usr.bin/mandoc/tbl_term.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tbl_term.c,v 1.26 2015/01/31 00:11:52 schwarze Exp $ */
+/*	$OpenBSD: tbl_term.c,v 1.27 2015/03/09 17:30:29 schwarze Exp $ */
 /*
  * Copyright (c) 2009, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2014, 2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -64,6 +64,9 @@ term_tbl(struct termp *tp, const struct tbl_span *sp)
 	size_t			 rmargin, maxrmargin, tsz;
 	int			 ic, horiz, spans, vert;
 
+	if (tp->tbl.cols == NULL)
+		term_flushln(tp);
+
 	rmargin = tp->rmargin;
 	maxrmargin = tp->maxrmargin;
 
@@ -80,8 +83,6 @@ term_tbl(struct termp *tp, const struct tbl_span *sp)
 	 */
 
 	if (tp->tbl.cols == NULL) {
-		term_flushln(tp);
-
 		tp->tbl.len = term_tbl_len;
 		tp->tbl.slen = term_tbl_strlen;
 		tp->tbl.arg = tp;
