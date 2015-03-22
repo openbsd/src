a2dce8d0a96bb45cee0f15a9277e47cfbb1dcdf3
bcook <bcook@openbsd.org>
2015-03-22 13:28:03 +0000

    sys/sysctl.h includes linux/sysctl.h, remove the extra unchecked include

--- lib/libcrypto/arc4random/getentropy_linux.c
+++ lib/libcrypto/arc4random/getentropy_linux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getentropy_linux.c,v 1.36 2014/10/11 16:44:05 bcook Exp $	*/
+/*	$OpenBSD: getentropy_linux.c,v 1.37 2015/03/22 13:28:03 bcook Exp $	*/
 
 /*
  * Copyright (c) 2014 Theo de Raadt <deraadt@openbsd.org>
@@ -51,7 +51,6 @@
 
 #include <linux/types.h>
 #include <linux/random.h>
-#include <linux/sysctl.h>
 #ifdef HAVE_GETAUXVAL
 #include <sys/auxv.h>
 #endif
