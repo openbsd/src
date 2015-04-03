7fc0018a3eef37edefcb2dbfa7723105f6fec5d9
jsg <jsg@openbsd.org>
2015-04-03 12:22:55 +0000

    Translate PAGE_MASK usage for the way it is defined in our kernel. This code is not called.

--- sys/dev/pci/drm/ttm/ttm_bo_vm.c
+++ sys/dev/pci/drm/ttm/ttm_bo_vm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ttm_bo_vm.c,v 1.5 2015/02/11 07:01:37 jsg Exp $	*/
+/*	$OpenBSD: ttm_bo_vm.c,v 1.6 2015/04/03 12:22:55 jsg Exp $	*/
 /**************************************************************************
  *
  * Copyright (c) 2006-2009 VMware, Inc., Palo Alto, CA., USA
@@ -403,7 +403,7 @@ ssize_t ttm_bo_io(struct ttm_bo_device *bdev, struct file *filp,
 		goto out_unref;
 	}
 
-	page_offset = *f_pos & ~PAGE_MASK;
+	page_offset = *f_pos & PAGE_MASK;
 	io_size = bo->num_pages - kmap_offset;
 	io_size = (io_size << PAGE_SHIFT) - page_offset;
 	if (count < io_size)
@@ -472,7 +472,7 @@ ssize_t ttm_bo_fbdev_io(struct ttm_buffer_object *bo, const char __user *wbuf,
 	if (unlikely(kmap_offset >= bo->num_pages))
 		return -EFBIG;
 
-	page_offset = *f_pos & ~PAGE_MASK;
+	page_offset = *f_pos & PAGE_MASK;
 	io_size = bo->num_pages - kmap_offset;
 	io_size = (io_size << PAGE_SHIFT) - page_offset;
 	if (count < io_size)
