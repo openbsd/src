43f7d9d2f70721dccf869edb3960a56fb7a6c4a0
guenther <guenther@openbsd.org>
2015-04-21 01:38:41 +0000

    The ELF psABI for PPC specifies that the stack shall always be 16-byte aligned.
    ok mpi@ deraadt@

--- sys/arch/powerpc/include/_types.h
+++ sys/arch/powerpc/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _types.h,v 1.20 2014/03/19 05:14:13 guenther Exp $	*/
+/*	$OpenBSD: _types.h,v 1.21 2015/04/21 01:38:41 guenther Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -52,7 +52,7 @@ typedef struct label_t {
  * (within reasonable limits).
  */
 #define	_ALIGNBYTES		(sizeof(double) - 1)
-#define	_STACKALIGNBYTES	_ALIGNBYTES
+#define	_STACKALIGNBYTES	15
 #define	_ALIGN(p)		(((unsigned long)(p) + _ALIGNBYTES) & ~_ALIGNBYTES)
 #define	_ALIGNED_POINTER(p,t)	((((unsigned long)(p)) & (sizeof(t) - 1)) == 0)
 
