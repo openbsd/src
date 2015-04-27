d1ee572c0237d917802e26383cc3945518497c42
djm <djm@openbsd.org>
2015-04-27 23:17:11 +0000

    Rework sshd enable root login questions in light of sshd PermitRootLogin default change. The new default is not to ask to enable root logins when a non-root user has been addedi. There is some additional sublety for auto-installs that provide root ssh keys.
    patch by myself and rpe@ with feedback from sthen@;
    ok rpe@ deraadt@ sthen@

--- distrib/miniroot/install.sh
+++ distrib/miniroot/install.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: install.sh,v 1.265 2015/04/13 21:27:07 deraadt Exp $
+#	$OpenBSD: install.sh,v 1.266 2015/04/27 23:17:11 djm Exp $
 #	$NetBSD: install.sh,v 1.5.2.8 1996/08/27 18:15:05 gwr Exp $
 #
 # Copyright (c) 1997-2015 Todd Miller, Theo de Raadt, Ken Westerback
@@ -95,6 +95,7 @@ questions
 
 # Gather information for setting up the initial user account.
 user_setup
+ask_root_sshd
 
 # Set TZ variable based on zonefile and user selection.
 set_timezone /var/tzlist
