3e3a5746bf2e82123111ef7a676cdb9ce1e1ea13
bcook <bcook@openbsd.org>
2015-04-27 03:37:06 +0000

    Not all Linux libc's include linux/sysctl.h in sys/sysctl.h.
    Include it if we have the sysctl syscall.

--- lib/libcrypto/crypto/getentropy_linux.c
+++ lib/libcrypto/crypto/getentropy_linux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getentropy_linux.c,v 1.37 2015/03/22 13:28:03 bcook Exp $	*/
+/*	$OpenBSD: getentropy_linux.c,v 1.38 2015/04/27 03:37:06 bcook Exp $	*/
 
 /*
  * Copyright (c) 2014 Theo de Raadt <deraadt@openbsd.org>
@@ -27,8 +27,9 @@
 #include <sys/ioctl.h>
 #include <sys/resource.h>
 #include <sys/syscall.h>
-#ifdef HAVE_SYS_SYSCTL_H
+#ifdef SYS__sysctl
 #include <sys/sysctl.h>
+#include <linux/sysctl.h>
 #endif
 #include <sys/statvfs.h>
 #include <sys/socket.h>
