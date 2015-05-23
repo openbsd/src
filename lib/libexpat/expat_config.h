d528a8b35042cd995c5cc3c89337c943ba51bb39
jsg <jsg@openbsd.org>
2015-05-23 00:53:25 +0000

    define BYTEORDER so the endian tests will work ok deraadt@ miod@

--- lib/libexpat/expat_config.h
+++ lib/libexpat/expat_config.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: expat_config.h,v 1.1 2004/09/22 21:36:54 espie Exp $ */
+/* $OpenBSD: expat_config.h,v 1.2 2015/05/23 00:53:25 jsg Exp $ */
 
 /* quick and dirty conf for OpenBSD */
 
@@ -6,3 +6,12 @@
 #define XML_CONTEXT_BYTES 1024
 #define XML_DTD 1
 #define XML_NS 1
+
+#include <endian.h>
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define BYTEORDER 1234
+#elif BYTE_ORDER == BIG_ENDIAN
+#define BYTEORDER 4321
+#else
+#error "unknown byte order"
+#endif
