1161300643452158ad75752650a1480ce4b29af9
bcook <bcook@openbsd.org>
2015-05-26 03:05:26 +0000

    Add OPENSSL_NO_EGD to opensslfeatures.h.
    Since RAND_egd has been removed from LibreSSL, simplify porting software that
    relies on it. See https://github.com/libressl-portable/openbsd/pull/34
    
    from Bernard Spil, ok deraadt@

--- lib/libssl/src/crypto/opensslfeatures.h
+++ lib/libssl/src/crypto/opensslfeatures.h
@@ -1,6 +1,7 @@
 # define OPENSSL_NO_EC_NISTP_64_GCC_128
 # define OPENSSL_NO_CMS
 # define OPENSSL_NO_COMP
+# define OPENSSL_NO_EGD
 # define OPENSSL_NO_GMP
 # define OPENSSL_NO_JPAKE
 # define OPENSSL_NO_KRB5
