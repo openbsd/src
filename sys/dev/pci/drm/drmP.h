8bcd2dd3b47aa6a4874b571e9049900b8d73730d
jsg <jsg@openbsd.org>
2015-05-30 18:09:26 +0000

    DRMDEBUG not DRM_DEBUG.  From frantisek holop.

--- sys/dev/pci/drm/drmP.h
+++ sys/dev/pci/drm/drmP.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: drmP.h,v 1.195 2015/04/18 14:47:34 jsg Exp $ */
+/* $OpenBSD: drmP.h,v 1.196 2015/05/30 18:09:26 jsg Exp $ */
 /* drmP.h -- Private header for Direct Rendering Manager -*- linux-c -*-
  * Created: Mon Jan  4 10:05:05 1999 by faith@precisioninsight.com
  */
@@ -188,7 +188,7 @@ drm_can_sleep(void)
 	    curproc->p_pid, __func__ , ## arg)
 
 
-#ifdef DRM_DEBUG
+#ifdef DRMDEBUG
 #define DRM_INFO(fmt, arg...)  printf("drm: " fmt, ## arg)
 #else
 #define DRM_INFO(fmt, arg...) do { } while(/* CONSTCOND */ 0)
