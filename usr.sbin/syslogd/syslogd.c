3fa6eb4b43dee3aeb775ab5dbbbe41b032e1e8ed
bluhm <bluhm@openbsd.org>
2015-06-12 19:20:43 +0000

    Close the lock pipe before dupping /dev/null to stdio. This works even if the file descriptor is 0 or 1 or 2. input and OK millert@

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.162 2015/06/12 00:54:28 bluhm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.163 2015/06/12 19:20:43 bluhm Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -629,10 +629,10 @@ main(int argc, char *argv[])
 	reply_text = ctl_reply + CTL_HDR_LEN;
 
 	if (!Debug) {
+		close(lockpipe[1]);
 		dup2(nullfd, STDIN_FILENO);
 		dup2(nullfd, STDOUT_FILENO);
 		dup2(nullfd, STDERR_FILENO);
-		close(lockpipe[1]);
 	}
 	if (nullfd > 2)
 		close(nullfd);
