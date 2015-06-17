282907d51bb847565f9cc225ef91f51bbea402cc
deraadt <deraadt@openbsd.org>
2015-06-17 22:35:08 +0000

    my keyboard is conspiring against me

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.116 2015/06/17 22:32:08 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.117 2015/06/17 22:35:08 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -42,7 +42,7 @@
 #define BSD4_4	1
 
 #define OpenBSD	201510		/* OpenBSD version (year & month). */
-c#define OpenBSD5_8 1		/* OpenBSD 5.8 */
+#define OpenBSD5_8 1		/* OpenBSD 5.8 */
 
 #ifndef NULL
 #ifdef 	__GNUG__
