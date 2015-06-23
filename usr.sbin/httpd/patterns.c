b426d426db75cef05af42131708bee7a429712ca
semarie <semarie@openbsd.org>
2015-06-23 15:35:20 +0000

    remove a deprecated character class.
    it was deprecated in lua code, but here the code is new. The documentation
    don't mention it either.
    
    ok reyk@

--- usr.sbin/httpd/patterns.c
+++ usr.sbin/httpd/patterns.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patterns.c,v 1.1 2015/06/23 15:23:14 reyk Exp $	*/
+/*	$OpenBSD: patterns.c,v 1.2 2015/06/23 15:35:20 semarie Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -26,7 +26,7 @@
 
 /*
  * Derived from Lua 5.3.1:
- * $Id: patterns.c,v 1.1 2015/06/23 15:23:14 reyk Exp $
+ * $Id: patterns.c,v 1.2 2015/06/23 15:35:20 semarie Exp $
  * Standard library for string operations and pattern-matching
  */
 
@@ -156,9 +156,6 @@ match_class(int c, int cl)
 	case 'x':
 		res = isxdigit(c);
 		break;
-	case 'z':
-		res = (c == 0);
-		break;		/* deprecated option */
 	default:
 		return (cl == c);
 	}
