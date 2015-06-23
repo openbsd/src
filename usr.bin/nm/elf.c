642c3e06fd11b59af4550a00ec59dd254d055316
semarie <semarie@openbsd.org>
2015-06-23 13:43:08 +0000

    add some check before accessing data:  - if section header table is be present  - consistency of section header table size
    ok miod@

--- usr.bin/nm/elf.c
+++ usr.bin/nm/elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: elf.c,v 1.28 2015/05/17 20:19:08 guenther Exp $	*/
+/*	$OpenBSD: elf.c,v 1.29 2015/06/23 13:43:08 semarie Exp $	*/
 
 /*
  * Copyright (c) 2003 Michael Shalayeff
@@ -148,6 +148,16 @@ elf_load_shdrs(const char *name, FILE *fp, off_t foff, Elf_Ehdr *head)
 	Elf_Shdr *shdr;
 
 	elf_fix_header(head);
+
+	if (head->e_shnum == 0) {
+		warnx("%s: no section header table", name);
+		return (NULL);
+	}
+
+	if (head->e_shstrndx >= head->e_shnum) {
+		warnx("%s: inconsistent section header table", name);
+		return (NULL);
+	}
 
 	if ((shdr = calloc(head->e_shentsize, head->e_shnum)) == NULL) {
 		warn("%s: malloc shdr", name);
