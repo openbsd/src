779baed039cdcc7d5d8bf4f343bd33a50bce75ad
djm <djm@openbsd.org>
2015-07-01 01:55:00 +0000

    reset default PermitRootLogin to 'yes' (momentarily, for release)

--- usr.bin/ssh/servconf.c
+++ usr.bin/ssh/servconf.c
@@ -1,5 +1,5 @@
 
-/* $OpenBSD: servconf.c,v 1.271 2015/05/22 03:50:02 djm Exp $ */
+/* $OpenBSD: servconf.c,v 1.272 2015/07/01 01:55:00 djm Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -206,7 +206,7 @@ fill_default_server_options(ServerOptions *options)
 	if (options->key_regeneration_time == -1)
 		options->key_regeneration_time = 3600;
 	if (options->permit_root_login == PERMIT_NOT_SET)
-		options->permit_root_login = PERMIT_NO;
+		options->permit_root_login = PERMIT_YES;
 	if (options->ignore_rhosts == -1)
 		options->ignore_rhosts = 1;
 	if (options->ignore_user_known_hosts == -1)
