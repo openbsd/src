b21dd182f03386cd7620755c38dd2c66c137cf0b
dlg <dlg@openbsd.org>
2015-07-02 08:58:16 +0000

    copy MUTEX_ASSERT_LOCKED and MUTEX_ASSERT_UNLOCKED from alpha.
    the previous asserts checked if the mutex was locked by any cpu or
    not when they should have been checking if the current cpu has the
    lock or not.
    
    found by miod after i enabled pool_gc again.
    ok miod@

--- sys/arch/m88k/include/mutex.h
+++ sys/arch/m88k/include/mutex.h
@@ -1,6 +1,6 @@
 #ifndef _M88K_MUTEX_H_
 #define _M88K_MUTEX_H_
-/*	$OpenBSD: mutex.h,v 1.2 2014/02/02 20:31:10 kettenis Exp $	*/
+/*	$OpenBSD: mutex.h,v 1.3 2015/07/02 08:58:16 dlg Exp $	*/
 
 /*
  * Copyright (c) 2005, Miodrag Vallat.
@@ -55,15 +55,13 @@ void __mtx_init(struct mutex *, int);
 
 #ifdef DIAGNOSTIC
 
-#define	MUTEX_ASSERT_LOCKED(mtx)					\
-do {									\
-	if ((mtx)->mtx_lock == 0 || (mtx)->mtx_cpu != curcpu())		\
+#define MUTEX_ASSERT_LOCKED(mtx) do {					\
+	if ((mtx)->mtx_owner != curcpu())				\
 		panic("mutex %p not held in %s", (mtx), __func__);	\
 } while (0)
 
-#define	MUTEX_ASSERT_UNLOCKED(mtx)					\
-do {									\
-	if ((mtx)->mtx_lock != 0)					\
+#define MUTEX_ASSERT_UNLOCKED(mtx) do {					\
+	if ((mtx)->mtx_owner == curcpu())				\
 		panic("mutex %p held in %s", (mtx), __func__);		\
 } while (0)
 
