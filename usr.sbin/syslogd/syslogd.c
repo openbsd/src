319825b05071d393b1d554f38e378dbc4900174c
bluhm <bluhm@openbsd.org>
2015-07-09 16:01:48 +0000

    Set f_hostname to NULL after free() to avoid a double free when both !host and memory buffer are used. OK jung@

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.171 2015/07/07 17:53:04 bluhm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.172 2015/07/09 16:01:48 bluhm Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -1869,6 +1869,7 @@ init(void)
 		free(f->f_hostname);
 		if (f->f_type == F_MEMBUF) {
 			f->f_program = NULL;
+			f->f_hostname = NULL;
 			dprintf("add %p to mb\n", f);
 			SIMPLEQ_INSERT_HEAD(&mb, f, f_next);
 		} else
