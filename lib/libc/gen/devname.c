88939d51d2fe2d83d35fd8968a4145096c240f49
millert <millert@openbsd.org>
2015-07-13 12:41:54 +0000

    Do not write a warning to stderr if the db cannot be opened, just return an error.  Avoids bogus warnings in chroots. OK deraadt@ ajacoutot@

--- lib/libc/gen/devname.c
+++ lib/libc/gen/devname.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: devname.c,v 1.7 2006/03/31 00:41:20 deraadt Exp $ */
+/*	$OpenBSD: devname.c,v 1.8 2015/07/13 12:41:54 millert Exp $ */
 /*
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -52,7 +52,6 @@ devname(int dev, mode_t type)
 
 	if (!db && !failure &&
 	    !(db = dbopen(_PATH_DEVDB, O_RDONLY, 0, DB_HASH, NULL))) {
-		_warn("warning: %s", _PATH_DEVDB);
 		failure = 1;
 	}
 	if (failure)
