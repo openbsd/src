093bca227a770c2992746c99d92e89b01fb08105
bluhm <bluhm@openbsd.org>
2015-07-16 15:19:55 +0000

    The expected behavior of Perl srand(0) is a deterministic pseudo random stream.  Only srand() should give real random values.  Make our pimped Perl more compatible with the rest of the world. OK afresh1@ millert@

--- gnu/usr.bin/perl/pp.c
+++ gnu/usr.bin/perl/pp.c
@@ -2783,15 +2783,13 @@ PP(pp_srand)
                              "Integer overflow in srand");
             anum = UV_MAX;
         }
+        (void)srand48_deterministic((Rand_seed_t)anum);
     }
     else {
-        anum = 0;
+        anum = seed();
+        (void)seedDrand01((Rand_seed_t)anum);
     }
 
-    if (anum)
-        (void)srand48_deterministic((Rand_seed_t)anum);
-    else
-    (void)seedDrand01((Rand_seed_t)anum);
     PL_srand_called = TRUE;
     if (anum)
 	XPUSHu(anum);
