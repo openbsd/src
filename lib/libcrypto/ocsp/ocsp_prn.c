965ef27266ab6d83d35ac65bfd799cb83615cc92
miod <miod@openbsd.org>
2015-07-16 02:16:19 +0000

    Check return value of all used functions in OCSP_REQUEST_print(); covers Coverity CID 78796; ok beck@

--- lib/libcrypto/ocsp/ocsp_prn.c
+++ lib/libcrypto/ocsp/ocsp_prn.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ocsp_prn.c,v 1.7 2014/06/12 15:49:30 deraadt Exp $ */
+/* $OpenBSD: ocsp_prn.c,v 1.8 2015/07/16 02:16:19 miod Exp $ */
 /* Written by Tom Titchener <Tom_Titchener@groove.net> for the OpenSSL
  * project. */
 
@@ -174,11 +174,15 @@ OCSP_REQUEST_print(BIO *bp, OCSP_REQUEST* o, unsigned long flags)
 	    inf->requestExtensions, flags, 4))
 		goto err;
 	if (sig) {
-		X509_signature_print(bp, sig->signatureAlgorithm,
-		    sig->signature);
+		if (X509_signature_print(bp, sig->signatureAlgorithm,
+		    sig->signature) == 0)
+			goto err;
 		for (i = 0; i < sk_X509_num(sig->certs); i++) {
-			X509_print(bp, sk_X509_value(sig->certs, i));
-			PEM_write_bio_X509(bp, sk_X509_value(sig->certs, i));
+			if (X509_print(bp, sk_X509_value(sig->certs, i)) == 0)
+				goto err;
+			if (PEM_write_bio_X509(bp,
+			    sk_X509_value(sig->certs, i)) == 0)
+				goto err;
 		}
 	}
 	return 1;
