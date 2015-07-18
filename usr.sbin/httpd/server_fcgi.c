8fe5fc8c095d2d0db69b5e1a0f8954445f5d1cbc
blambert <blambert@openbsd.org>
2015-07-18 16:42:39 +0000

    treat asprintf failure in REQUEST_URI case as a fatal error
    ok florian@

--- usr.sbin/httpd/server_fcgi.c
+++ usr.sbin/httpd/server_fcgi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server_fcgi.c,v 1.55 2015/07/17 20:44:57 reyk Exp $	*/
+/*	$OpenBSD: server_fcgi.c,v 1.56 2015/07/18 16:42:39 blambert Exp $	*/
 
 /*
  * Copyright (c) 2014 Florian Obser <florian@openbsd.org>
@@ -306,8 +306,12 @@ server_fcgi(struct httpd *env, struct client *clt)
 			errstr = "failed to encode param";
 			goto fail;
 		}
-	} else if (asprintf(&str, "%s?%s", desc->http_path,
-	    desc->http_query) != -1) {
+	} else {
+		if (asprintf(&str, "%s?%s", desc->http_path,
+		    desc->http_query) == -1) {
+			errstr = "failed to encode param";
+			goto fail;
+		}
 		ret = fcgi_add_param(&param, "REQUEST_URI", str, clt);
 		free(str);
 		if (ret == -1) {
