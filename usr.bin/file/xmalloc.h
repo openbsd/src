8269d3287b200bb0e07d0b28db47a0dc2e264d1c
nicm <nicm@openbsd.org>
2015-07-20 08:51:41 +0000

    Remove leftover xfree() prototype.

--- usr.bin/file/xmalloc.h
+++ usr.bin/file/xmalloc.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: xmalloc.h,v 1.1 2015/04/24 16:24:11 nicm Exp $ */
+/* $OpenBSD: xmalloc.h,v 1.2 2015/07/20 08:51:41 nicm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -22,7 +22,6 @@
 void	*xmalloc(size_t);
 void	*xcalloc(size_t, size_t);
 void	*xreallocarray(void *, size_t, size_t);
-void     xfree(void *);
 char	*xstrdup(const char *);
 int	 xasprintf(char **, const char *, ...)
                 __attribute__((__format__ (printf, 2, 3)))
