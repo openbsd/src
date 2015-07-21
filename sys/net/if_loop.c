d0710e2c8844afb90aff48d2fb55494f71d038df
mpi <mpi@openbsd.org>
2015-07-21 02:46:54 +0000

    Put the mbuf_list inside "#ifdef MPLS".
    reported by rpe@

--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_loop.c,v 1.68 2015/07/20 22:54:29 mpi Exp $	*/
+/*	$OpenBSD: if_loop.c,v 1.69 2015/07/21 02:46:54 mpi Exp $	*/
 /*	$NetBSD: if_loop.c,v 1.15 1996/05/07 02:40:33 thorpej Exp $	*/
 
 /*
@@ -204,7 +204,9 @@ looutput(struct ifnet *ifp, struct mbuf *m, struct sockaddr *dst,
     struct rtentry *rt)
 {
 	struct niqueue *ifq = NULL;
+#ifdef MPLS
 	struct mbuf_list ml = MBUF_LIST_INITIALIZER();
+#endif /* MPLS */
 
 	if ((m->m_flags & M_PKTHDR) == 0)
 		panic("looutput: no header mbuf");
