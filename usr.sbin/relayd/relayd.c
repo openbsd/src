a94502bfe4b5cb0ee298183fa38bb32199576724
benno <benno@openbsd.org>
2015-07-29 20:55:43 +0000

    fix bug where other than the last of multiple forward rules in http protocols would be ignored, reported and fixed by J. Fischer (lists -AT- mistrust -DOT- net) and reminded by (trondd -AT- kagu-tsuchi -DOT- com), thanks! ok deraadt@

--- usr.sbin/relayd/relayd.c
+++ usr.sbin/relayd/relayd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relayd.c,v 1.142 2015/06/03 02:24:36 millert Exp $	*/
+/*	$OpenBSD: relayd.c,v 1.143 2015/07/29 20:55:43 benno Exp $	*/
 
 /*
  * Copyright (c) 2007 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -1021,11 +1021,8 @@ rule_settable(struct relay_rules *rules, struct relay_table *rlt)
 
 	TAILQ_FOREACH(r, rules, rule_entry) {
 		if (r->rule_tablename[0] &&
-		    strcmp(pname, r->rule_tablename) == 0) {
+		    strcmp(pname, r->rule_tablename) == 0)
 			r->rule_table = rlt;
-		} else {
-			r->rule_table = NULL;
-		}
 	}
 }
 
