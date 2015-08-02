22a2c0aeb4f533538f2ef27c98645a1334911a01
djm <djm@openbsd.org>
2015-08-02 09:56:42 +0000

    openssh 7.0; ok deraadt@

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.73 2015/07/01 01:55:13 djm Exp $ */
+/* $OpenBSD: version.h,v 1.74 2015/08/02 09:56:42 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.9"
+#define SSH_VERSION	"OpenSSH_7.0"
