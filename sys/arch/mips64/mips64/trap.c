f11c78bfd1836c2c2b05470f91710ca898e5f734
visa <visa@openbsd.org>
2015-08-03 13:32:15 +0000

    On mips64, enable IPIs before calling refreshcreds() in trap(), to avoid a potential deadlock.
    ok miod@, deraadt@

--- sys/arch/mips64/mips64/trap.c
+++ sys/arch/mips64/mips64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.107 2015/07/19 17:00:39 visa Exp $	*/
+/*	$OpenBSD: trap.c,v 1.108 2015/08/03 13:32:15 visa Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -181,10 +181,8 @@ trap(struct trap_frame *trapframe)
 	if (type != T_SYSCALL)
 #endif
 		atomic_inc_int(&uvmexp.traps);
-	if (USERMODE(trapframe->sr)) {
+	if (USERMODE(trapframe->sr))
 		type |= T_USER;
-		refreshcreds(p);
-	}
 
 	/*
 	 * Enable hardware interrupts if they were on before the trap;
@@ -207,6 +205,9 @@ trap(struct trap_frame *trapframe)
 		}
 		break;
 	}
+
+	if (type & T_USER)
+		refreshcreds(p);
 
 #ifdef CPU_R8000
 	/*
