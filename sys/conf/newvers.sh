76a014aec89c3f1aaca2c6565b931d80c5faf4b1
jca <jca@openbsd.org>
2015-08-10 20:31:00 +0000

    Back to -current.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.139 2015/07/23 16:26:57 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.140 2015/08/10 20:31:00 jca Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,11 +64,11 @@ ost="OpenBSD"
 osr="5.8"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
 #if 0
-#define STATUS "-current"		/* just after a release */
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #endif
+#define STATUS "-current"		/* just after a release */
 
 const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
