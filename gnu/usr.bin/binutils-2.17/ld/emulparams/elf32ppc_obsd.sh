e7fcfa0e53596a085c8a6fe1f4fdae750451f32a
kettenis <kettenis@openbsd.org>
2015-08-23 15:19:32 +0000

    Enable missing code to fully enable the Secure-PLT ABI and clean up the remaining commented out variable settings.  We'll continue to use the same layout for both BSS-PLT and Secure-PLT since supporting the more relro friendly layout that Linux uses isn't compatible with our way of making the PLT and GOT read-only.

--- gnu/usr.bin/binutils-2.17/ld/emulparams/elf32ppc_obsd.sh
+++ gnu/usr.bin/binutils-2.17/ld/emulparams/elf32ppc_obsd.sh
@@ -1,14 +1,7 @@
-# . ${srcdir}/emulparams/elf32ppc.sh
 . ${srcdir}/emulparams/elf32ppccommon.sh
-## # Yes, we want duplicate .got and .plt sections.  The linker chooses the
-## # appropriate one magically in ppc_after_open
-## DATA_GOT=
-## SDATA_GOT=
-## SEPARATE_GOTPLT=0
+# We deliberately keep the traditional OpenBSD W^X layout for both the
+# old BSS-PLT and the new Secure-PLT ABI.
 BSS_PLT=
-## GOT=".got          ${RELOCATING-0} : SPECIAL { *(.got) }"
-## PLT=".plt          ${RELOCATING-0} : SPECIAL { *(.plt) }"
-## GOTPLT="${PLT}"
-## OTHER_TEXT_SECTIONS="*(.glink)"
-## EXTRA_EM_FILE=ppc32elf
+OTHER_TEXT_SECTIONS="*(.glink)"
+EXTRA_EM_FILE=ppc32elf
 . ${srcdir}/emulparams/elf_obsd.sh
