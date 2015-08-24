f674f844308d1a91e5d7cf5d36cc4ba4b3551ed9
bluhm <bluhm@openbsd.org>
2015-08-24 22:14:01 +0000

    Enable path MTU test with ping for IPv6.  Scapy srp1() does not accept inner IPv6 packets in ICMP6 with bad checksum created by pf. Use same workaround as in pf_forward tests and fork a process for sniffing.

--- regress/sys/net/pf_fragment/ping6_mtu_1300.py
+++ regress/sys/net/pf_fragment/ping6_mtu_1300.py
@@ -8,8 +8,22 @@ from scapy.all import *
 dstaddr=sys.argv[1]
 pid=os.getpid()
 payload="a" * 1452
-a=srp1(Ether(src=SRC_MAC, dst=PF_MAC)/IPv6(src=SRC_OUT6, dst=dstaddr)/
-    ICMPv6EchoRequest(id=pid, data=payload), iface=SRC_IF, timeout=2)
+eth=Ether(src=SRC_MAC, dst=PF_MAC)/IPv6(src=SRC_OUT6, dst=dstaddr)/\
+    ICMPv6EchoRequest(id=pid, data=payload)
+
+# work around the broken sniffing of packages with bad checksum
+#a=srp1(eth, iface=SRC_IF, timeout=2)
+if os.fork() == 0:
+	time.sleep(1)
+	sendp(eth, iface=SRC_IF)
+	os._exit(0)
+ans=sniff(iface=SRC_IF, timeout=3, filter=
+    "ip6 and dst "+SRC_OUT6+" and icmp6")
+if len(ans) == 0:
+	print "no packet sniffed"
+	exit(2)
+a=ans[0]
+
 if a and a.type == ETH_P_IPV6 and \
     ipv6nh[a.payload.nh] == 'ICMPv6' and \
     icmp6types[a.payload.payload.type] == 'Packet too big':
