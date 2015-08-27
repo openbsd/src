f6c28b3a06335ff63f898a194a6bea37f414e96a
deraadt <deraadt@openbsd.org>
2015-08-27 19:51:36 +0000

    size for free()

--- sys/dev/pv/vmt.c
+++ sys/dev/pv/vmt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmt.c,v 1.4 2015/08/12 09:15:52 uebayasi Exp $ */
+/*	$OpenBSD: vmt.c,v 1.5 2015/08/27 19:51:36 deraadt Exp $ */
 
 /*
  * Copyright (c) 2007 David Crawshaw <david@zentus.com>
@@ -375,7 +375,7 @@ vmt_attach(struct device *parent, struct device *self, void *aux)
 	return;
 
 free:
-	free(sc->sc_rpc_buf, M_DEVBUF, 0);
+	free(sc->sc_rpc_buf, M_DEVBUF, VMT_RPC_BUFLEN);
 }
 
 void
