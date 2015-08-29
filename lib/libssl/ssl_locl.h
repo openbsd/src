a4b2bab798fb2d65d5169920cb7f9daaf58490f8
doug <doug@openbsd.org>
2015-08-29 17:15:52 +0000

    Now that SSLv3 is going away, TLS_* and SSLv23 are equivalent.
    Remove the TLS method data and tls_any_* functions.  Point to SSLv23.
    
    ok jsing@

--- lib/libssl/ssl_locl.h
+++ lib/libssl/ssl_locl.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_locl.h,v 1.100 2015/07/24 07:57:48 doug Exp $ */
+/* $OpenBSD: ssl_locl.h,v 1.101 2015/08/29 17:15:52 doug Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -757,8 +757,6 @@ int ssl23_accept(SSL *s);
 int ssl23_connect(SSL *s);
 int ssl23_read_bytes(SSL *s, int n);
 int ssl23_write_bytes(SSL *s);
-int tls_any_accept(SSL *s);
-int tls_any_connect(SSL *s);
 
 int tls1_new(SSL *s);
 void tls1_free(SSL *s);
