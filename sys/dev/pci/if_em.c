de1f09503bf4f64fc255e730efe54a403fde6edf
mpi <mpi@openbsd.org>
2015-09-01 09:48:50 +0000

    Use the correct free(9) size for the RX ring.
    ok dlg@, phessler@

--- sys/dev/pci/if_em.c
+++ sys/dev/pci/if_em.c
@@ -31,7 +31,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 ***************************************************************************/
 
-/* $OpenBSD: if_em.c,v 1.302 2015/09/01 07:09:55 deraadt Exp $ */
+/* $OpenBSD: if_em.c,v 1.303 2015/09/01 09:48:50 mpi Exp $ */
 /* $FreeBSD: if_em.c,v 1.46 2004/09/29 18:28:28 mlaier Exp $ */
 
 #include <dev/pci/if_em.h>
@@ -2770,7 +2770,7 @@ em_free_receive_structures(struct em_softc *sc)
 	}
 	if (sc->rx_buffer_area != NULL) {
 		free(sc->rx_buffer_area, M_DEVBUF,
-		    sc->num_tx_desc * sizeof(struct em_buffer));
+		    sc->num_rx_desc * sizeof(struct em_buffer));
 		sc->rx_buffer_area = NULL;
 	}
 	if (sc->rxtag != NULL)
