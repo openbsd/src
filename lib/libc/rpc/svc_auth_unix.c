5e36bd8c13f95c8ef4ec108384bd3b7be499a7ff
deraadt <deraadt@openbsd.org>
2015-09-02 02:52:26 +0000

    do not printf() on error from libc; ok millert

--- lib/libc/rpc/svc_auth_unix.c
+++ lib/libc/rpc/svc_auth_unix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: svc_auth_unix.c,v 1.10 2010/09/01 14:43:34 millert Exp $ */
+/*	$OpenBSD: svc_auth_unix.c,v 1.11 2015/09/02 02:52:26 deraadt Exp $ */
 
 /*
  * Copyright (c) 2010, Oracle America, Inc.
@@ -97,8 +97,6 @@ _svcauth_unix(struct svc_req *rqst, struct rpc_msg *msg)
 		 * timestamp, hostname len (0), uid, gid, and gids len (0).
 		 */
 		if ((5 + gid_len) * BYTES_PER_XDR_UNIT + str_len > auth_len) {
-			(void) printf("bad auth_len gid %u str %u auth %u\n",
-			    gid_len, str_len, auth_len);
 			stat = AUTH_BADCRED;
 			goto done;
 		}
