c6a916fdec0d0296b1e6c64f3fa9f0931ca76c0b
deraadt <deraadt@openbsd.org>
2015-09-03 15:52:16 +0000

    match amd64; bios_efi_info_t is required here as well, because acpidump compiles on more than one platform.

--- sys/arch/i386/include/biosvar.h
+++ sys/arch/i386/include/biosvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: biosvar.h,v 1.63 2015/07/17 21:36:56 mlarkin Exp $	*/
+/*	$OpenBSD: biosvar.h,v 1.64 2015/09/03 15:52:16 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997-1999 Michael Shalayeff
@@ -222,6 +222,21 @@ typedef struct _bios_bootsr {
 	u_int8_t	maskkey[BOOTSR_CRYPTO_MAXKEYBYTES];
 } __packed bios_bootsr_t;
 
+#define	BOOTARG_EFIINFO 11
+typedef struct _bios_efiinfo {
+	uint64_t	config_acpi;
+	uint64_t	config_smbios;
+	uint64_t	fb_addr;
+	uint64_t	fb_size;
+	uint32_t	fb_height;
+	uint32_t	fb_width;
+	uint32_t	fb_pixpsl;	/* pixels per scan line */
+	uint32_t	fb_red_mask;
+	uint32_t	fb_green_mask;
+	uint32_t	fb_blue_mask;
+	uint32_t	fb_reserved_mask;
+} __packed bios_efiinfo_t;
+
 #if defined(_KERNEL) || defined (_STANDALONE)
 
 #ifdef _LOCORE
@@ -272,6 +287,7 @@ void bios32_cleanup(void);
 
 extern u_int bootapiver;
 extern bios_memmap_t *bios_memmap;
+extern bios_efiinfo_t *bios_efiinfo;
 extern void *bios_smpinfo;
 extern bios_pciinfo_t *bios_pciinfo;
 
