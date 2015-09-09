d12add8d6d7758de3e006f747ce63261912cd7cd
dlg <dlg@openbsd.org>
2015-09-09 20:19:26 +0000

    if_put after if_get
    ok mpi@

--- sys/netinet/ip_ipip.c
+++ sys/netinet/ip_ipip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ipip.c,v 1.65 2015/08/24 22:04:06 mpi Exp $ */
+/*	$OpenBSD: ip_ipip.c,v 1.66 2015/09/09 20:19:26 dlg Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -297,6 +297,8 @@ ipip_input(struct mbuf *m, int iphlen, struct ifnet *gifp, int proto)
 	    !(ifp->if_flags & IFF_LOOPBACK)) && ipip_allow != 2) {
 		struct sockaddr_storage ss;
 		struct rtentry *rt;
+
+		if_put(ifp);
 
 		memset(&ss, 0, sizeof(ss));
 
