af64ca04ddea5268314889383049c2fb82e3f450
mpi <mpi@openbsd.org>
2015-09-10 08:55:03 +0000

    Remove link_addr(3).  A function to encode the name of an interface in a sockaddr_dl is a questionnable interface.  But now it makes it harder to properly reference ifp becauses of this.
    Set sdl_index to the index of the corresponding interface when
    constructing a routing message.
    
    Ridding previous libc crank.
    
    ok guenther@, deraadt@, dlg@

--- lib/libc/net/linkaddr.c
+++ lib/libc/net/linkaddr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: linkaddr.c,v 1.5 2005/08/06 20:30:03 espie Exp $ */
+/*	$OpenBSD: linkaddr.c,v 1.6 2015/09/10 08:55:03 mpi Exp $ */
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -32,84 +32,6 @@
 #include <sys/socket.h>
 #include <net/if_dl.h>
 #include <string.h>
-
-/* States*/
-#define NAMING	0
-#define GOTONE	1
-#define GOTTWO	2
-#define RESET	3
-/* Inputs */
-#define	DIGIT	(4*0)
-#define	END	(4*1)
-#define DELIM	(4*2)
-#define LETTER	(4*3)
-
-void
-link_addr(const char *addr, struct sockaddr_dl *sdl)
-{
-	char *cp = sdl->sdl_data;
-	char *cplim = sdl->sdl_len + (char *)sdl;
-	int byte = 0, state = NAMING, new;
-
-	bzero((char *)&sdl->sdl_family, sdl->sdl_len - 1);
-	sdl->sdl_family = AF_LINK;
-	do {
-		state &= ~LETTER;
-		if ((*addr >= '0') && (*addr <= '9')) {
-			new = *addr - '0';
-		} else if ((*addr >= 'a') && (*addr <= 'f')) {
-			new = *addr - 'a' + 10;
-		} else if ((*addr >= 'A') && (*addr <= 'F')) {
-			new = *addr - 'A' + 10;
-		} else if (*addr == 0) {
-			state |= END;
-		} else if (state == NAMING &&
-			   (((*addr >= 'A') && (*addr <= 'Z')) ||
-			   ((*addr >= 'a') && (*addr <= 'z'))))
-			state |= LETTER;
-		else
-			state |= DELIM;
-		addr++;
-		switch (state /* | INPUT */) {
-		case NAMING | DIGIT:
-		case NAMING | LETTER:
-			*cp++ = addr[-1];
-			continue;
-		case NAMING | DELIM:
-			state = RESET;
-			sdl->sdl_nlen = cp - sdl->sdl_data;
-			continue;
-		case GOTTWO | DIGIT:
-			*cp++ = byte;
-			/* FALLTHROUGH */
-		case RESET | DIGIT:
-			state = GOTONE;
-			byte = new;
-			continue;
-		case GOTONE | DIGIT:
-			state = GOTTWO;
-			byte = new + (byte << 4);
-			continue;
-		default: /* | DELIM */
-			state = RESET;
-			*cp++ = byte;
-			byte = 0;
-			continue;
-		case GOTONE | END:
-		case GOTTWO | END:
-			*cp++ = byte;
-			/* FALLTHROUGH */
-		case RESET | END:
-			break;
-		}
-		break;
-	} while (cp < cplim); 
-	sdl->sdl_alen = cp - LLADDR(sdl);
-	new = cp - (char *)sdl;
-	if (new > sizeof(*sdl))
-		sdl->sdl_len = new;
-	return;
-}
 
 static char hexlist[] = "0123456789abcdef";
 
