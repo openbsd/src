93d7a446307c6a236e07e1f60cd4f98205015f39
semarie <semarie@openbsd.org>
2015-09-10 11:16:08 +0000

    convert tame(2) regress to new API

--- regress/sys/kern/tame/sigabrt/sigabrt.c
+++ regress/sys/kern/tame/sigabrt/sigabrt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sigabrt.c,v 1.2 2015/08/23 04:59:31 semarie Exp $ */
+/*	$OpenBSD: sigabrt.c,v 1.3 2015/09/10 11:16:08 semarie Exp $ */
 /*
  * Copyright (c) 2015 Sebastien Marie <semarie@openbsd.org>
  *
@@ -15,11 +15,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/tame.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 void
 handler(int sigraised)
@@ -37,7 +36,7 @@ main(int argc, char *argv[])
 	printf("permitted STDIO\n");
 	fflush(stdout);
 
-	tame(TAME_ABORT, NULL);
+	tame("abort", NULL);
 
 	/* this will triggered tame_fail() */
 	printf("forbidden STDIO 1\n");
--- regress/sys/kern/tame/sigkill/sigkill.c
+++ regress/sys/kern/tame/sigkill/sigkill.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sigkill.c,v 1.2 2015/08/23 04:59:31 semarie Exp $ */
+/*	$OpenBSD: sigkill.c,v 1.3 2015/09/10 11:16:08 semarie Exp $ */
 /*
  * Copyright (c) 2015 Sebastien Marie <semarie@openbsd.org>
  *
@@ -15,11 +15,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/tame.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 void
 handler(int sigraised)
@@ -38,7 +37,7 @@ main(int argc, char *argv[])
 	printf("permitted STDIO\n");
 	fflush(stdout);
 
-	tame(0, NULL);
+	tame("", NULL);
 
 	/* this will triggered tame_fail() */
 	printf("forbidden STDIO 1\n");
