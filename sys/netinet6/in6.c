02ca528816a15bdd37da530539b995d716a727a4
claudio <claudio@openbsd.org>
2015-09-10 08:45:32 +0000

    Trivial if_put addition. OK dlg@

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6.c,v 1.171 2015/09/09 15:51:40 mpi Exp $	*/
+/*	$OpenBSD: in6.c,v 1.172 2015/09/10 08:45:32 claudio Exp $	*/
 /*	$KAME: in6.c,v 1.372 2004/06/14 08:14:21 itojun Exp $	*/
 
 /*
@@ -1385,6 +1385,7 @@ in6_delmulti(struct in6_multi *in6m)
 			    ifma_list);
 			splx(s);
 		}
+		if_put(ifp);
 
 		free(in6m, M_IPMADDR, 0);
 	}
