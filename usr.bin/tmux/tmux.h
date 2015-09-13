47f0136494ac7c4768d11d4de16a0e44e94fe94c
nicm <nicm@openbsd.org>
2015-09-13 13:31:40 +0000

    Add copy-mode -e to exit copy mode when scrolling off the bottom, useful for quick view of history, from Cam Hutchison.

--- usr.bin/tmux/tmux.h
+++ usr.bin/tmux/tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.556 2015/09/11 14:41:50 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.557 2015/09/13 13:31:40 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -2068,7 +2068,7 @@ extern const char window_clock_table[14][5][5];
 
 /* window-copy.c */
 extern const struct window_mode window_copy_mode;
-void		 window_copy_init_from_pane(struct window_pane *);
+void		 window_copy_init_from_pane(struct window_pane *, u_int);
 void		 window_copy_init_for_output(struct window_pane *);
 void printflike(2, 3) window_copy_add(struct window_pane *, const char *, ...);
 void		 window_copy_vadd(struct window_pane *, const char *, va_list);
