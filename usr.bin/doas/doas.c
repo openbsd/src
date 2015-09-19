af293d1a7d13610d468b604ec6fde97ee8861fb6
tedu <tedu@openbsd.org>
2015-09-19 02:47:46 +0000

    doas doesn't need any files to be passed in. closefrom STDERR+1. ok benno

--- usr.bin/doas/doas.c
+++ usr.bin/doas/doas.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: doas.c,v 1.41 2015/09/03 20:05:58 tedu Exp $ */
+/* $OpenBSD: doas.c,v 1.42 2015/09/19 02:47:46 tedu Exp $ */
 /*
  * Copyright (c) 2015 Ted Unangst <tedu@openbsd.org>
  *
@@ -322,6 +322,8 @@ main(int argc, char **argv, char **envp)
 	int nflag = 0;
 	char cwdpath[PATH_MAX];
 	const char *cwd;
+
+	closefrom(STDERR_FILENO + 1);
 
 	uid = getuid();
 
