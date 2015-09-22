daa5cd37aa86d0b6f3d6ab9f5ad1faa35878b6bf
nicm <nicm@openbsd.org>
2015-09-22 21:56:16 +0000

    Don't update last session when the session is unchanged, from Sina Siadat.

--- usr.bin/tmux/cmd-switch-client.c
+++ usr.bin/tmux/cmd-switch-client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-switch-client.c,v 1.30 2015/09/10 08:58:14 nicm Exp $ */
+/* $OpenBSD: cmd-switch-client.c,v 1.31 2015/09/22 21:56:16 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -124,7 +124,7 @@ cmd_switch_client_exec(struct cmd *self, struct cmd_q *cmdq)
 		environ_update(update, &c->environ, &s->environ);
 	}
 
-	if (c->session != NULL)
+	if (c->session != NULL && c->session != s)
 		c->last_session = c->session;
 	c->session = s;
 	status_timer_start(c);
