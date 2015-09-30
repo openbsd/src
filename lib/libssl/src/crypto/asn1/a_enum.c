bb53bdea91980dc6413475ad0b55d7d0c00d2c9c
jsing <jsing@openbsd.org>
2015-09-30 18:45:56 +0000

    Replace M_ASN1_ENUMERATED_(free|new) with ASN1_ENUMERATED_(free|new).

--- lib/libssl/src/crypto/asn1/a_enum.c
+++ lib/libssl/src/crypto/asn1/a_enum.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: a_enum.c,v 1.16 2014/09/21 12:17:42 miod Exp $ */
+/* $OpenBSD: a_enum.c,v 1.17 2015/09/30 18:45:56 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -140,7 +140,7 @@ BN_to_ASN1_ENUMERATED(BIGNUM *bn, ASN1_ENUMERATED *ai)
 	int len, j;
 
 	if (ai == NULL)
-		ret = M_ASN1_ENUMERATED_new();
+		ret = ASN1_ENUMERATED_new();
 	else
 		ret = ai;
 	if (ret == NULL) {
@@ -172,7 +172,7 @@ BN_to_ASN1_ENUMERATED(BIGNUM *bn, ASN1_ENUMERATED *ai)
 
 err:
 	if (ret != ai)
-		M_ASN1_ENUMERATED_free(ret);
+		ASN1_ENUMERATED_free(ret);
 	return (NULL);
 }
 
