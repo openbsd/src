a493b653384e6a9c3688a64b0d8be50eabb95584
deraadt <deraadt@openbsd.org>
2015-10-03 23:34:01 +0000

    tame "stdio rpath" is sufficient for all the operations done by test(1)

--- bin/test/test.c
+++ bin/test/test.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: test.c,v 1.13 2014/12/02 18:32:05 tedu Exp $	*/
+/*	$OpenBSD: test.c,v 1.14 2015/10/03 23:34:01 deraadt Exp $	*/
 /*	$NetBSD: test.c,v 1.15 1995/03/21 07:04:06 cgd Exp $	*/
 
 /*
@@ -157,6 +157,9 @@ main(int argc, char *argv[])
 {
 	extern char *__progname;
 	int	res;
+
+	if (tame("stdio rpath", NULL) == -1)
+		err(1, "tame");
 
 	if (strcmp(__progname, "[") == 0) {
 		if (strcmp(argv[--argc], "]"))
