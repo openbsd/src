4c01e3ae4bdec46c7de2ca55a6275a7ea6044ca3
deraadt <deraadt@openbsd.org>
2015-10-04 18:11:22 +0000

    remove tame "proc".  it is not useful, because the "ed" diffs require fork+execve, and execve is not going to become available in this fashion. ed diffs should be handled using a built-in handler, and various folks have been discussing this behind the scenes.

--- usr.bin/patch/patch.c
+++ usr.bin/patch/patch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patch.c,v 1.56 2015/10/03 13:49:51 deraadt Exp $	*/
+/*	$OpenBSD: patch.c,v 1.57 2015/10/04 18:11:22 deraadt Exp $	*/
 
 /*
  * patch - a program to apply diffs to original files
@@ -147,7 +147,7 @@ main(int argc, char *argv[])
 	const	char *tmpdir;
 	char	*v;
 
-	if (tame("stdio rpath wpath cpath tmppath fattr proc", NULL) == -1)
+	if (tame("stdio rpath wpath cpath tmppath fattr", NULL) == -1)
 		perror("tame");
 
 	setvbuf(stdout, NULL, _IOLBF, 0);
