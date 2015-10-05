16c00009ff907ff99833265d5dd7f143d38cb7cf
deraadt <deraadt@openbsd.org>
2015-10-05 06:43:31 +0000

    tame "stdio getpw" right at the top; this runs the whole gamut of getpw/gr type functions allowed in that set. ok doug

--- usr.bin/id/id.c
+++ usr.bin/id/id.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: id.c,v 1.24 2015/10/05 06:30:37 deraadt Exp $	*/
+/*	$OpenBSD: id.c,v 1.25 2015/10/05 06:43:31 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -57,6 +57,9 @@ main(int argc, char *argv[])
 	uid_t uid;
 	gid_t gid;
 	const char *opts;
+
+	if (tame("stdio getpw", NULL) == -1)
+		err(1, "tame");
 
 	cflag = Gflag = gflag = nflag = pflag = rflag = uflag = 0;
 
