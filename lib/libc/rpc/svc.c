3077ba8bf774fcd5f23cae979be200bf53277031
deraadt <deraadt@openbsd.org>
2015-10-05 01:23:17 +0000

    user land -> userland; from Rob Pierce

--- lib/libc/rpc/svc.c
+++ lib/libc/rpc/svc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: svc.c,v 1.28 2015/09/13 15:36:56 guenther Exp $ */
+/*	$OpenBSD: svc.c,v 1.29 2015/10/05 01:23:17 deraadt Exp $ */
 
 /*
  * Copyright (c) 2010, Oracle America, Inc.
@@ -500,7 +500,7 @@ DEF_WEAK(svcerr_progvers);
  *   a) the structure is contiguous (no pointers), and
  *   b) the cred structure size does not exceed RQCRED_SIZE bytes. 
  * In all events, all three parameters are freed upon exit from this routine.
- * The storage is trivially management on the call stack in user land, but
+ * The storage is trivially management on the call stack in userland, but
  * is mallocated in kernel land.
  */
 
