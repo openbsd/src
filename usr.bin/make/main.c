89b686069c1d24df745739be7b47d87e615df6d7
deraadt <deraadt@openbsd.org>
2015-10-07 14:16:09 +0000

    Oops, not quite ready for tame() here.  People need time to update their kernels, before it starts using the new "exec" primitive. HINT: everyone, update your kernels, tame is coming to make really soon.

--- usr.bin/make/main.c
+++ usr.bin/make/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.111 2015/10/07 14:14:30 deraadt Exp $ */
+/*	$OpenBSD: main.c,v 1.112 2015/10/07 14:16:09 deraadt Exp $ */
 /*	$NetBSD: main.c,v 1.34 1997/03/24 20:56:36 gwr Exp $	*/
 
 /*
@@ -191,9 +191,6 @@ MainParseArgs(int argc, char **argv)
 
 #define OPTFLAGS "BC:D:I:SV:d:ef:ij:km:npqrst"
 #define OPTLETTERS "BSiknpqrst"
-
-	if (tame("stdio rpath wpath cpath proc exec", NULL) == -1)
-		err(1, "tame");
 
 	optind = 1;	/* since we're called more than once */
 	optreset = 1;
