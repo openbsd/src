6cfa7bda3af08bb75bd2821541765b5ceaf9cbf9
deraadt <deraadt@openbsd.org>
2015-10-07 04:03:57 +0000

    We continue our tour through obscure BSD <word escapes me>.   This stdio-based program may open files arbitrarily, so tame "stdio rpath" it from the start.

--- usr.bin/lam/lam.c
+++ usr.bin/lam/lam.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lam.c,v 1.17 2015/01/16 06:40:09 deraadt Exp $	*/
+/*	$OpenBSD: lam.c,v 1.18 2015/10/07 04:03:57 deraadt Exp $	*/
 /*	$NetBSD: lam.c,v 1.2 1994/11/14 20:27:42 jtc Exp $	*/
 
 /*-
@@ -70,6 +70,9 @@ int
 main(int argc, char *argv[])
 {
 	int i;
+
+	if (tame("stdio rpath", NULL) == -1)
+		err(1, "tame");
 
 	/* Process arguments, set numfiles to file argument count. */
 	getargs(argc, argv);
