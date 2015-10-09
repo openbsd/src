0d02dc9f799e0a1bd4299646c051141d9afe81a1
bluhm <bluhm@openbsd.org>
2015-10-09 16:44:55 +0000

    A fork(2) is used in ttymsg() to delay the message to a tty if it blocks.  Fix the potential syslogd's death, add "proc" to pledge. OK deraadt@

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.192 2015/10/09 16:29:17 deraadt Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.193 2015/10/09 16:44:55 bluhm Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -593,7 +593,7 @@ main(int argc, char *argv[])
 	if (priv_init(ConfFile, NoDNS, lockpipe[1], nullfd, argv) < 0)
 		errx(1, "unable to privsep");
 
-	if (pledge("stdio rpath unix inet recvfd", NULL) == -1)
+	if (pledge("stdio rpath unix inet recvfd proc", NULL) == -1)
 		err(1, "pledge");
 
 	/* Process is now unprivileged and inside a chroot */
