57ae68919325b77be9ad4af9b2b44fac8156f600
deraadt <deraadt@openbsd.org>
2015-10-10 17:59:15 +0000

    pledge "stdio rpath tty".  rpath for the configuration reading done by login* subsystem, tty for readpassphase() ok beck

--- usr.bin/encrypt/encrypt.c
+++ usr.bin/encrypt/encrypt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: encrypt.c,v 1.40 2015/02/26 17:46:15 tedu Exp $	*/
+/*	$OpenBSD: encrypt.c,v 1.41 2015/10/10 17:59:15 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996, Jason Downs.  All rights reserved.
@@ -92,6 +92,9 @@ main(int argc, char **argv)
 	int prompt = 0;
 	char *extra = NULL;	/* Store login class or number of rounds */
 	const char *errstr;
+
+	if (pledge("stdio rpath tty", NULL) == -1)
+		err(1, "pledge");
 
 	while ((opt = getopt(argc, argv, "pb:c:")) != -1) {
 		switch (opt) {
