b808fb874b0c1a9ccbbc3a78e03e9fcd2cc7cdba
deraadt <deraadt@openbsd.org>
2015-10-10 18:58:53 +0000

    pledge "stdio getpw rpath wpath cpath fattr".  doug pointed out getpw* use, and fattr for chmod. doug

--- usr.bin/rcs/rcsprog.c
+++ usr.bin/rcs/rcsprog.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsprog.c,v 1.156 2015/06/13 20:15:21 nicm Exp $	*/
+/*	$OpenBSD: rcsprog.c,v 1.157 2015/10/10 18:58:53 deraadt Exp $	*/
 /*
  * Copyright (c) 2005 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -127,6 +127,9 @@ main(int argc, char **argv)
 	u_int i;
 	char **cmd_argv;
 	int ret, cmd_argc;
+
+	if (pledge("stdio getpw rpath wpath cpath fattr", NULL) == -1)
+		err(1, "pledge");
 
 	ret = -1;
 	rcs_optind = 1;
