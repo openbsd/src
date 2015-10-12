b154e2c0a99bad776cb43391283323adbae0c908
deraadt <deraadt@openbsd.org>
2015-10-12 05:54:18 +0000

    fstat() of opened fd, rather than stat(), to avoid TOCTOU ok jsg

--- usr.sbin/mkuboot/mkuboot.c
+++ usr.sbin/mkuboot/mkuboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkuboot.c,v 1.4 2014/05/18 21:18:07 miod Exp $	*/
+/*	$OpenBSD: mkuboot.c,v 1.5 2015/10/12 05:54:18 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2008 Mark Kettenis
@@ -223,14 +223,12 @@ main(int argc, char *argv[])
 	ifd = open(iname, O_RDONLY);
 	if (ifd < 0)
 		err(1, "%s", iname);
+	if (fstat(ifd, &sb) == -1)
+		err(1, "%s", iname);
 
 	ofd = open(oname, O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if (ofd < 0)
 		err(1, "%s", oname);
-
-	if (stat(iname, &sb) == -1) {
-		err(1, "%s", oname);
-	}
 
 	/* Write initial header. */
 	if (write(ofd, &ih, sizeof ih) != sizeof ih)
