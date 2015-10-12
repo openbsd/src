43df60e4e4359956898847648e809b43940a489f
deraadt <deraadt@openbsd.org>
2015-10-12 06:24:28 +0000

    pledge "stdio" after opening up the input & output files. ok jsg

--- usr.sbin/mkuboot/mkuboot.c
+++ usr.sbin/mkuboot/mkuboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkuboot.c,v 1.5 2015/10/12 05:54:18 deraadt Exp $	*/
+/*	$OpenBSD: mkuboot.c,v 1.6 2015/10/12 06:24:28 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2008 Mark Kettenis
@@ -229,6 +229,9 @@ main(int argc, char *argv[])
 	ofd = open(oname, O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if (ofd < 0)
 		err(1, "%s", oname);
+
+	if (pledge("stdio", NULL) == -1)
+		err(1, "pledge");
 
 	/* Write initial header. */
 	if (write(ofd, &ih, sizeof ih) != sizeof ih)
