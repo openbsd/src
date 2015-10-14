ce808d91e2cc4a886aa58a893d638593c3d1c1a1
deraadt <deraadt@openbsd.org>
2015-10-14 15:54:49 +0000

    unfortunately rewritelabel() just before termination does a non-permitted ioctl to rewrite the label, in support of the old-school "frag info in the disklabel" concept.  disklabel folk, please come talk to me...

--- sbin/newfs_ext2fs/newfs_ext2fs.c
+++ sbin/newfs_ext2fs/newfs_ext2fs.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: newfs_ext2fs.c,v 1.16 2015/10/13 15:15:30 deraadt Exp $ */
+/* $OpenBSD: newfs_ext2fs.c,v 1.17 2015/10/14 15:54:49 deraadt Exp $ */
 /*	$NetBSD: newfs_ext2fs.c,v 1.8 2009/03/02 10:38:13 tsutsui Exp $	*/
 
 /*
@@ -296,10 +296,6 @@ main(int argc, char *argv[])
 		}
 
 		pp = getpartition(fsi, special, argv, &lp);
-
-		if (pledge("stdio", NULL) == -1)
-			err(1, "pledge");
-
 		if (!Iflag) {
 			static const char m[] =
 			    "%s partition type is not `%s' (or use -I)";
