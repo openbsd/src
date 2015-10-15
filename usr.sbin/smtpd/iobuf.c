cdd750812e40a7be0a15cc8a523ad0055423b288
gilles <gilles@openbsd.org>
2015-10-15 08:29:41 +0000

    iobuf_queue() should return the number of bytes it queued on success, not 0
    ok eric@

--- usr.sbin/smtpd/iobuf.c
+++ usr.sbin/smtpd/iobuf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iobuf.c,v 1.6 2015/09/03 06:26:17 jsg Exp $	*/
+/*	$OpenBSD: iobuf.c,v 1.7 2015/10/15 08:29:41 gilles Exp $	*/
 /*      
  * Copyright (c) 2012 Eric Faurot <eric@openbsd.org>
  *
@@ -287,7 +287,7 @@ iobuf_queue(struct iobuf *io, const void *data, size_t len)
 
 	memmove(buf, data, len);
 
-	return (0);
+	return (len);
 }
 
 int
