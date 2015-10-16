be435c80b502130f928c3861850787ebba993703
mmcc <mmcc@openbsd.org>
2015-10-16 18:21:43 +0000

    Cast isspace() argument to unsigned char.
    ok jca@

--- usr.bin/mail/fio.c
+++ usr.bin/mail/fio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fio.c,v 1.35 2015/10/16 17:56:07 mmcc Exp $	*/
+/*	$OpenBSD: fio.c,v 1.36 2015/10/16 18:21:43 mmcc Exp $	*/
 /*	$NetBSD: fio.c,v 1.8 1997/07/07 22:57:55 phil Exp $	*/
 
 /*
@@ -142,7 +142,7 @@ setptr(FILE *ibuf, off_t offset)
 		} else if (inhead) {
 			for (cp = linebuf, cp2 = "status";; cp++) {
 				if ((c = (unsigned char)*cp2++) == 0) {
-					while (isspace(*cp++))
+					while (isspace((unsigned char)*cp++))
 						;
 					if (cp[-1] != ':')
 						break;
