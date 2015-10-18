9532a95439a4d0fe7f47d96facab9cfbc3d01d6b
bluhm <bluhm@openbsd.org>
2015-10-18 16:35:06 +0000

    Add "id" pledge to syslogd privsep process.  Needed for logging to pipe. OK deraadt@

--- usr.sbin/syslogd/privsep.c
+++ usr.sbin/syslogd/privsep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: privsep.c,v 1.57 2015/10/16 16:10:10 bluhm Exp $	*/
+/*	$OpenBSD: privsep.c,v 1.58 2015/10/18 16:35:06 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2003 Anil Madhavapeddy <anil@recoil.org>
@@ -144,7 +144,7 @@ priv_init(char *conf, int numeric, int lockfd, int nullfd, char *argv[])
 		return 0;
 	}
 
-	if (pledge("stdio rpath wpath cpath inet dns getpw sendfd proc exec",
+	if (pledge("stdio rpath wpath cpath inet dns getpw sendfd id proc exec",
 	    NULL) == -1)
 		err(1, "pledge priv");
 
