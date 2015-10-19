0e18bff87fdcaf90f47abd0fdb04e2079a362009
reyk <reyk@openbsd.org>
2015-10-19 11:27:35 +0000

    Fix control_imsg_forward() by changing imsg_compose() to imsg_compose_event().  This was done by pyr@ in relayd/control.c -r1.32 (2009/06/05, ok eric@) but somehow didn't slip into other daemons that imported control.c.

--- sbin/iked/control.c
+++ sbin/iked/control.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: control.c,v 1.17 2015/10/19 11:25:35 reyk Exp $	*/
+/*	$OpenBSD: control.c,v 1.18 2015/10/19 11:27:35 reyk Exp $	*/
 
 /*
  * Copyright (c) 2010-2013 Reyk Floeter <reyk@openbsd.org>
@@ -311,7 +311,7 @@ control_imsg_forward(struct imsg *imsg)
 
 	TAILQ_FOREACH(c, &ctl_conns, entry)
 		if (c->flags & CTL_CONN_NOTIFY)
-			imsg_compose(&c->iev.ibuf, imsg->hdr.type,
+			imsg_compose_event(&c->iev, imsg->hdr.type,
 			    0, imsg->hdr.pid, -1, imsg->data,
 			    imsg->hdr.len - IMSG_HEADER_SIZE);
 }
