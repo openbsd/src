03dec1288e42c92c53f9932a76830880a7788fb0
bluhm <bluhm@openbsd.org>
2015-10-24 12:58:32 +0000

    Handling of bogus CA file has changed in syslogd.  Adapt tests to new error messages.

--- regress/usr.sbin/syslogd/args-tls-cafile-noexist.pl
+++ regress/usr.sbin/syslogd/args-tls-cafile-noexist.pl
@@ -16,7 +16,7 @@ our %args = (
 	    qr/open CAfile: No such file or directory/ => 2,
 	    qr/Logging to FORWTLS \@tls:\/\/localhost:\d+/ => '>=4',
 	    qr/syslogd: tls_connect_socket .*: /.
-		qr/ssl verify setup failure/ => 2,
+		qr/ssl verify memory setup failure/ => 2,
 	    get_testgrep() => 1,
 	},
 	cacrt => "noexist",
--- regress/usr.sbin/syslogd/args-tls-cafile-toobig.pl
+++ regress/usr.sbin/syslogd/args-tls-cafile-toobig.pl
@@ -16,7 +16,7 @@ our %args = (
 	    qr/CAfile larger than 50MB/ => 2,
 	    qr/Logging to FORWTLS \@tls:\/\/localhost:\d+/ => '>=4',
 	    qr/syslogd: tls_connect_socket .*: /.
-		qr/ssl verify setup failure/ => 2,
+		qr/ssl verify memory setup failure/ => 2,
 	    get_testgrep() => 1,
 	},
 	cacrt => "toobig",
