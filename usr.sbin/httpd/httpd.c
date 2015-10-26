648805e437cb01d64287697d59f106497714607f
jung <jung@openbsd.org>
2015-10-26 11:03:29 +0000

    fix PATH_INFO for / requests
    diff from Denis Fondras
    
    ok reyk

--- usr.sbin/httpd/httpd.c
+++ usr.sbin/httpd/httpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.c,v 1.41 2015/10/14 08:02:38 reyk Exp $	*/
+/*	$OpenBSD: httpd.c,v 1.42 2015/10/26 11:03:29 jung Exp $	*/
 
 /*
  * Copyright (c) 2014 Reyk Floeter <reyk@openbsd.org>
@@ -696,7 +696,7 @@ path_info(char *path)
 
 	for (p = end; p > start; p--) {
 		/* Scan every path component from the end and at each '/' */
-		if (p < end && *p != '/')
+		if (p <= end && *p != '/')
 			continue;
 
 		/* Temporarily cut the path component out */
