12618d23f6c01aaa5e64f03ab4e25cca6723af0e
gilles <gilles@openbsd.org>
2015-10-28 14:30:03 +0000

    masquerade and senders map require being able to lookup mailaddr structures in tables, prepare for such features by bringing the helpers + smtpd.h part
    ok millert@

--- usr.sbin/smtpd/smtpd.h
+++ usr.sbin/smtpd/smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.h,v 1.481 2015/10/28 07:25:30 gilles Exp $	*/
+/*	$OpenBSD: smtpd.h,v 1.482 2015/10/28 14:30:03 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -464,6 +464,15 @@ struct expand {
 	size_t				 nb_nodes;
 	struct rule			*rule;
 	struct expandnode		*parent;
+};
+
+struct maddrnode {
+	TAILQ_ENTRY(maddrnode)		entries;
+	struct mailaddr			mailaddr;
+};
+
+struct maddrmap {
+	TAILQ_HEAD(xmaddr, maddrnode)	queue;
 };
 
 #define DSN_SUCCESS 0x01
@@ -1182,6 +1191,13 @@ void logit(int, const char *, ...) __attribute__((format (printf, 2, 3)));
 void mda_postfork(void);
 void mda_postprivdrop(void);
 void mda_imsg(struct mproc *, struct imsg *);
+
+
+/* mailaddr.c */
+int mailaddr_line(struct maddrmap *, const char *);
+void maddrmap_init(struct maddrmap *);
+void maddrmap_insert(struct maddrmap *, struct maddrnode *);
+void maddrmap_free(struct maddrmap *);
 
 
 /* mproc.c */
