780e07573caa32980ce36e7b4db1db606070fae9
mmcc <mmcc@openbsd.org>
2015-10-29 03:58:55 +0000

    (struct timezone *)0 -> NULL, while I'm here.

--- lib/libc/gen/time.c
+++ lib/libc/gen/time.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: time.c,v 1.6 2015/09/12 14:35:40 guenther Exp $ */
+/*	$OpenBSD: time.c,v 1.7 2015/10/29 03:58:55 mmcc Exp $ */
 /*
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -36,7 +36,7 @@ time(time_t *t)
 {
 	struct timeval tt;
 
-	if (gettimeofday(&tt, (struct timezone *)0) < 0)
+	if (gettimeofday(&tt, NULL) < 0)
 		return (-1);
 	if (t)
 		*t = (time_t)tt.tv_sec;
