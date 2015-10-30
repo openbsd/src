ef29e497cb6dd1e7ca69d8bdc46e4a1649553fb9
deraadt <deraadt@openbsd.org>
2015-10-30 13:57:33 +0000

    top renice command requires pledge "id", from Michael Lesniewski

--- usr.bin/top/top.c
+++ usr.bin/top/top.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: top.c,v 1.85 2015/10/25 09:39:00 deraadt Exp $	*/
+/*	$OpenBSD: top.c,v 1.86 2015/10/30 13:57:33 deraadt Exp $	*/
 
 /*
  *  Top users/processes display for Unix
@@ -328,7 +328,7 @@ main(int argc, char *argv[])
 		preset_argc = 0;
 	} while (i != 0);
 
-	if (pledge("stdio rpath getpw tty ps vminfo", NULL) == -1)
+	if (pledge("stdio rpath getpw tty id ps vminfo", NULL) == -1)
 		err(1, "pledge");
 
 	/* set constants for username/uid display correctly */
