711661c76fc454c09dffdc6c35cf95469b353fa4
schwarze <schwarze@openbsd.org>
2015-11-05 20:55:46 +0000

    Use include files "header.html" and "footer.html" rather than a compiled-in string.  This is not a security risk, we read the file manpath.conf from the same directory, anyway.  No error handling is needed; even if the files are absent, that's not an error.
    This is more flexible without causing complication of the code or
    the user interface.  It helps the upcoming revamp of the online
    manual pages on man.NetBSD.org.
    
    Based on an idea by Jean-Yves Migeon <jeanyves dot migeon at free dot fr>,
    but implemented in a much simpler way.

--- usr.bin/mandoc/cgi.c
+++ usr.bin/mandoc/cgi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cgi.c,v 1.52 2015/11/05 17:47:53 schwarze Exp $ */
+/*	$OpenBSD: cgi.c,v 1.53 2015/11/05 20:55:46 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014, 2015 Ingo Schwarze <schwarze@usta.de>
@@ -75,6 +75,7 @@ static	void		 pg_searchres(const struct req *,
 static	void		 pg_show(struct req *, const char *);
 static	void		 resp_begin_html(int, const char *);
 static	void		 resp_begin_http(int, const char *);
+static	void		 resp_copy(const char *);
 static	void		 resp_end_html(void);
 static	void		 resp_searchform(const struct req *);
 static	void		 resp_show(const struct req *, const char *);
@@ -366,6 +367,20 @@ resp_begin_http(int code, const char *msg)
 }
 
 static void
+resp_copy(const char *filename)
+{
+	char	 buf[4096];
+	ssize_t	 sz;
+	int	 fd;
+
+	if ((fd = open(filename, O_RDONLY)) != -1) {
+		fflush(stdout);
+		while ((sz = read(fd, buf, sizeof(buf))) > 0)
+			write(STDOUT_FILENO, buf, sz);
+	}
+}
+
+static void
 resp_begin_html(int code, const char *msg)
 {
 
@@ -382,11 +397,15 @@ resp_begin_html(int code, const char *msg)
 	       "<BODY>\n"
 	       "<!-- Begin page content. //-->\n",
 	       CSS_DIR, CUSTOMIZE_TITLE);
+
+	resp_copy(MAN_DIR "/header.html");
 }
 
 static void
 resp_end_html(void)
 {
+
+	resp_copy(MAN_DIR "/footer.html");
 
 	puts("</BODY>\n"
 	     "</HTML>");
@@ -397,7 +416,6 @@ resp_searchform(const struct req *req)
 {
 	int		 i;
 
-	puts(CUSTOMIZE_BEGIN);
 	puts("<!-- Begin search form. //-->");
 	printf("<DIV ID=\"mancgi\">\n"
 	       "<FORM ACTION=\"%s\" METHOD=\"get\">\n"
