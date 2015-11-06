6993d9b5cd21b4fc385537a6406220796dbb99a9
espie <espie@openbsd.org>
2015-11-06 08:53:12 +0000

    the Solver uses kept_names, so we need these there too found by aja@

--- usr.sbin/pkg_add/OpenBSD/PkgCreate.pm
+++ usr.sbin/pkg_add/OpenBSD/PkgCreate.pm
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 # ex:ts=8 sw=4:
-# $OpenBSD: PkgCreate.pm,v 1.117 2015/08/13 08:13:44 espie Exp $
+# $OpenBSD: PkgCreate.pm,v 1.118 2015/11/06 08:53:12 espie Exp $
 #
 # Copyright (c) 2003-2014 Marc Espie <espie@openbsd.org>
 #
@@ -1023,6 +1023,11 @@ sub older_names
 }
 
 sub kept
+{
+	return ();
+}
+
+sub kept_names
 {
 	return ();
 }
