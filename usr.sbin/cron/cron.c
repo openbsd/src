e8d6cae41246077dcf943e1dd68c52f3fc43a390
deraadt <deraadt@openbsd.org>
2015-11-11 03:20:19 +0000

    exit on pledge failure, oops

--- usr.sbin/cron/cron.c
+++ usr.sbin/cron/cron.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cron.c,v 1.66 2015/11/09 16:37:07 millert Exp $	*/
+/*	$OpenBSD: cron.c,v 1.67 2015/11/11 03:20:19 deraadt Exp $	*/
 
 /* Copyright 1988,1990,1993,1994 by Paul Vixie
  * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
@@ -100,8 +100,10 @@ main(int argc, char *argv[])
 	set_cron_cwd();
 
 	if (pledge("stdio rpath wpath cpath fattr getpw unix flock id dns proc exec",
-	    NULL) == -1)
+	    NULL) == -1) {
 		log_it("CRON", "pledge", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
 	cronSock = open_socket();
 
