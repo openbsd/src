6f793eeffb48a4b8243f4c7f1f5be1ea245ea571
mmcc <mmcc@openbsd.org>
2015-11-12 16:30:30 +0000

    Remove xfree()'s prototype. tedu@ removed its definition a few weeks ago. Pointed out by Ricardo Mestre.

--- usr.bin/diff/xmalloc.h
+++ usr.bin/diff/xmalloc.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: xmalloc.h,v 1.3 2015/04/29 04:00:25 deraadt Exp $ */
+/* $OpenBSD: xmalloc.h,v 1.4 2015/11/12 16:30:30 mmcc Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -22,7 +22,6 @@
 void	*xmalloc(size_t);
 void	*xcalloc(size_t, size_t);
 void	*xreallocarray(void *, size_t, size_t);
-void     xfree(void *);
 char	*xstrdup(const char *);
 int	 xasprintf(char **, const char *, ...)
                 __attribute__((__format__ (printf, 2, 3)))
