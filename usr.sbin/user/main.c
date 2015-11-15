66401f6bb94723fc1ede5e7b0818084c1b19c3c1
deraadt <deraadt@openbsd.org>
2015-11-15 23:14:21 +0000

    pledge "stdio rpath wpath cpath fattr getpw flock id proc exec" at the top.  It is a lot, maybe someone will take the time to find smaller chunks later in the program. tested by jca

--- usr.sbin/user/main.c
+++ usr.sbin/user/main.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: main.c,v 1.7 2011/12/31 14:05:51 ajacoutot Exp $ */
+/* $OpenBSD: main.c,v 1.8 2015/11/15 23:14:21 deraadt Exp $ */
 /* $NetBSD: main.c,v 1.3 2002/07/09 10:34:16 tron Exp $ */
 
 /*
@@ -79,6 +79,10 @@ main(int argc, char **argv)
 	cmd_t	*cmdp;
 	int	matched;
 	int	i;
+
+	if (pledge("stdio rpath wpath cpath fattr getpw flock id proc exec",
+	    NULL) == -1)
+		err(1, "pledge");
 
 	for (cmdp = cmds ; cmdp->c_wc > 0 ; cmdp++) {
 		for (matched = i = 0 ; i < cmdp->c_wc && i < MaxCmdWords ; i++) {
