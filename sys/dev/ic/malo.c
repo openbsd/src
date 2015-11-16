a3d594ecd3017fa28441c837415f4aa818e22b52
mpi <mpi@openbsd.org>
2015-11-16 10:03:01 +0000

    Check for space on the ring before dequeuing packets.
    Allows us to get rid of mq_requeue(9) and IFQ_POLL(9) because wireless
    drivers use a special queue for management frames.
    
    Tested by stsp@, ok dlg@, stsp@

--- sys/dev/ic/malo.c
+++ sys/dev/ic/malo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malo.c,v 1.109 2015/11/04 12:11:59 dlg Exp $ */
+/*	$OpenBSD: malo.c,v 1.110 2015/11/16 10:03:01 mpi Exp $ */
 
 /*
  * Copyright (c) 2006 Claudio Jeker <claudio@openbsd.org>
@@ -1010,14 +1010,13 @@ malo_start(struct ifnet *ifp)
 		return;
 
 	for (;;) {
+		if (sc->sc_txring.queued >= MALO_TX_RING_COUNT - 1) {
+			ifp->if_flags |= IFF_OACTIVE;
+			break;
+		}
+
 		m0 = mq_dequeue(&ic->ic_mgtq);
 		if (m0 != NULL) {
-			if (sc->sc_txring.queued >= MALO_TX_RING_COUNT) {
-				ifp->if_flags |= IFF_OACTIVE;
-				mq_requeue(&ic->ic_mgtq, m0);
-				break;
-			}
-
 			ni = m0->m_pkthdr.ph_cookie;
 #if NBPFILTER > 0
 			if (ic->ic_rawbpf != NULL)
@@ -1028,14 +1027,10 @@ malo_start(struct ifnet *ifp)
 		} else {
 			if (ic->ic_state != IEEE80211_S_RUN)
 				break;
-			IFQ_POLL(&ifp->if_snd, m0);
+
+			IFQ_DEQUEUE(&ifp->if_snd, m0);
 			if (m0 == NULL)
 				break;
-			if (sc->sc_txring.queued >= MALO_TX_RING_COUNT - 1) {
-				ifp->if_flags |= IFF_OACTIVE;
-				break;
-			}
-			IFQ_DEQUEUE(&ifp->if_snd, m0);
 #if NBPFILTER > 0
 			if (ifp->if_bpf != NULL)
 				bpf_mtap(ifp->if_bpf, m0, BPF_DIRECTION_OUT);
