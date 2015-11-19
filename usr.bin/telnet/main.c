16ef2ce26ab5b175d54b4cc079382a551f8a494f
deraadt <deraadt@openbsd.org>
2015-11-19 22:01:33 +0000

    how did the pledge "dns" get forgotten?!?! wow..

--- usr.bin/telnet/main.c
+++ usr.bin/telnet/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.34 2015/11/13 18:05:37 schwarze Exp $	*/
+/*	$OpenBSD: main.c,v 1.35 2015/11/19 22:01:33 deraadt Exp $	*/
 /*	$NetBSD: main.c,v 1.5 1996/02/28 21:04:05 thorpej Exp $	*/
 
 /*
@@ -169,7 +169,7 @@ main(int argc, char *argv[])
 			exit(1);
 		}
 
-	if (pledge("stdio rpath wpath getpw inet tty", NULL) == -1) {
+	if (pledge("stdio rpath wpath getpw dns inet tty", NULL) == -1) {
 		perror("pledge");
 		exit(1);
 	}
