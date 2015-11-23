bd193770fa77752195aff28e232b6f400bdc3982
ratchov <ratchov@openbsd.org>
2015-11-23 09:48:25 +0000

    Insert new ports at the head of the list and stop assuming its ordered.

--- usr.bin/sndiod/midi.c
+++ usr.bin/sndiod/midi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: midi.c,v 1.11 2015/11/22 16:42:22 ratchov Exp $	*/
+/*	$OpenBSD: midi.c,v 1.12 2015/11/23 09:48:25 ratchov Exp $	*/
 /*
  * Copyright (c) 2008-2012 Alexandre Ratchov <alex@caoua.org>
  *
@@ -426,7 +426,7 @@ port_exit(void *arg)
 struct port *
 port_new(char *path, unsigned int mode, int hold)
 {
-	struct port *c, **pc;
+	struct port *c;
 
 	c = xmalloc(sizeof(struct port));
 	c->path = path;
@@ -434,10 +434,8 @@ port_new(char *path, unsigned int mode, int hold)
 	c->hold = hold;
 	c->midi = midi_new(&port_midiops, c, mode);
 	c->num = midi_portnum++;
-	for (pc = &port_list; *pc != NULL; pc = &(*pc)->next)
-		; /* nothing */
-	c->next = NULL;
-	*pc = c;
+	c->next = port_list;
+	port_list = c;
 	return c;
 }
 
@@ -502,7 +500,7 @@ port_bynum(int num)
 	struct port *p;
 
 	for (p = port_list; p != NULL; p = p->next) {
-		if (num-- == 0)
+		if (p->num == num)
 			return p;
 	}
 	return NULL;
