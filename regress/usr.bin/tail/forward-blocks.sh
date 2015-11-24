69739939091738cfa4bd59f7f33b6c7d0c664d12
tedu <tedu@openbsd.org>
2015-11-24 04:04:19 +0000

    add some more tail tests, from Martijn van Duren

---
+++ regress/usr.bin/tail/forward-blocks.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# $OpenBSD: forward-blocks.sh,v 1.1 2015/11/24 04:04:19 tedu Exp $
+
+# test if tail grep the correct number of blocks from a file.
+
+DIR=$(mktemp -d)
+echo DIR=${DIR}
+
+NAME=${0##*/}
+OUT=${DIR}/${NAME%%.sh}.out
+i=0
+while [ ${i} -lt 512 ]; do
+	echo ${i} >> ${DIR}/bar
+	i=$((i+1))
+done
+
+tail -b +1 ${DIR}/bar > ${OUT}
+
+diff -u ${OUT} ${0%%.sh}.out || exit 1
+
+# cleanup if okay
+rm -Rf ${DIR}
---
+++ regress/usr.bin/tail/reverse-lines.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# $OpenBSD: reverse-lines.sh,v 1.1 2015/11/24 04:04:19 tedu Exp $
+
+# test if tail grep the correct number of blocks from a file.
+
+DIR=$(mktemp -d)
+echo DIR=${DIR}
+
+NAME=${0##*/}
+OUT=${DIR}/${NAME%%.sh}.out
+i=0
+while [ ${i} -lt 512 ]; do
+	echo ${i} >> ${DIR}/bar
+	i=$((i+1))
+done
+
+tail -n 5 ${DIR}/bar > ${OUT}
+tail -n -5 ${DIR}/bar > ${OUT}1
+tail -5 ${DIR}/bar > ${OUT}2
+
+diff -u ${OUT} ${OUT}1 || exit 1
+diff -u ${OUT} ${OUT}2 || exit 1
+diff -u ${OUT} ${0%%.sh}.out || exit 1
+
+# cleanup if okay
+rm -Rf ${DIR}
