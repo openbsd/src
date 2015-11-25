4ef6f0cf3e570e584af44670f50d2a1456c81c5e
kettenis <kettenis@openbsd.org>
2015-11-25 21:13:28 +0000

    Don't bother keeping track of the resident count for the kernel pmap.

--- sys/arch/sparc64/sparc64/pmap.c
+++ sys/arch/sparc64/sparc64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.94 2015/09/02 21:59:29 kettenis Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.95 2015/11/25 21:13:28 kettenis Exp $	*/
 /*	$NetBSD: pmap.c,v 1.107 2001/08/31 16:47:41 eeh Exp $	*/
 #undef	NO_VCACHE /* Don't forget the locked TLB in dostart */
 /*
@@ -305,7 +305,6 @@ pmap_enter_kpage(vaddr_t va, int64_t data)
 			prom_printf("pmap_enter_kpage: out of pages\n");
 			panic("pmap_enter_kpage");
 		}
-		atomic_inc_long(&pmap_kernel()->pm_stats.resident_count);
 
 		BDPRINTF(PDB_BOOT1, 
 			 ("pseg_set: pm=%p va=%p data=%lx newp %lx\r\n",
@@ -1718,8 +1717,6 @@ pmap_kenter_pa(vaddr_t va, paddr_t pa, vm_prot_t prot)
 	if (pseg_set(pm, va, tte.data, 0) != 0)
 		panic("%s: no pseg", __func__);
 
-	atomic_inc_long(&pm->pm_stats.resident_count);
-
 	/* this is correct */
 	dcache_flush_page(pa);
 }
@@ -1756,7 +1753,6 @@ pmap_kremove(vaddr_t va, vsize_t size)
 				/* panic? */
 			}
 
-			atomic_dec_long(&pm->pm_stats.resident_count);
 			tsb_invalidate(pm->pm_ctx, va);
 			/* Here we assume nothing can get into the TLB unless it has a PTE */
 			tlb_flush_pte(va, pm->pm_ctx);
@@ -2568,6 +2564,7 @@ pmap_page_protect(struct vm_page *pg, vm_prot_t prot)
 				    pv->pv_pmap->pm_ctx);
 			}
 			atomic_dec_long(&pv->pv_pmap->pm_stats.resident_count);
+
 			KASSERT(pv->pv_next == NULL);
 			/* dump the first pv */
 			pv->pv_pmap = NULL;
