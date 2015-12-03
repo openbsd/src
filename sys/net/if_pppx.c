8ae6a40a7bd2f7752a1d18b60b5de6c60fdd7c12
bluhm <bluhm@openbsd.org>
2015-12-03 17:03:33 +0000

    When enqueuing a mbuf chain, take the top instead of the last element.  Otherwise the kernel paniced with "ipintr no HDR" if the data passed to pppxwrite() had a certain length. Bug reported and fix tested by Sevan / Venture37. Debugged with and OK mikeb@

--- sys/net/if_pppx.c
+++ sys/net/if_pppx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_pppx.c,v 1.46 2015/11/20 05:31:24 dlg Exp $ */
+/*	$OpenBSD: if_pppx.c,v 1.47 2015/12/03 17:03:33 bluhm Exp $ */
 
 /*
  * Copyright (c) 2010 Claudio Jeker <claudio@openbsd.org>
@@ -383,7 +383,7 @@ pppxwrite(dev_t dev, struct uio *uio, int ioflag)
 		return (EAFNOSUPPORT);
 	}
 
-	if (niq_enqueue(ifq, m) != 0)
+	if (niq_enqueue(ifq, top) != 0)
 		return (ENOBUFS);
 
 	return (error);
