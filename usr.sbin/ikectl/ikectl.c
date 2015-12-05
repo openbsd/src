a220363262d74c3e41ee91ed7bf59a55b4a952e2
claudio <claudio@openbsd.org>
2015-12-05 13:11:18 +0000

    EAGAIN handling for imsg_read. OK henning@ benno@

--- usr.sbin/ikectl/ikectl.c
+++ usr.sbin/ikectl/ikectl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ikectl.c,v 1.22 2015/11/06 06:29:11 jsg Exp $	*/
+/*	$OpenBSD: ikectl.c,v 1.23 2015/12/05 13:11:18 claudio Exp $	*/
 
 /*
  * Copyright (c) 2007-2013 Reyk Floeter <reyk@openbsd.org>
@@ -324,7 +324,7 @@ main(int argc, char *argv[])
 			err(1, "write error");
 
 	while (!done) {
-		if ((n = imsg_read(ibuf)) == -1)
+		if ((n = imsg_read(ibuf)) == -1 && errno != EAGAIN)
 			errx(1, "imsg_read error");
 		if (n == 0)
 			errx(1, "pipe closed");
