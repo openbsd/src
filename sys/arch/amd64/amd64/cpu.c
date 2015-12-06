d43058c8a004ff08a62769f15a4e96b604b397e6
dlg <dlg@openbsd.org>
2015-12-06 03:14:55 +0000

    unbreak kernel link if vmm isnt being built as part of the kernel.

--- sys/arch/amd64/amd64/cpu.c
+++ sys/arch/amd64/amd64/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.92 2015/12/06 01:16:58 mlarkin Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.93 2015/12/06 03:14:55 dlg Exp $	*/
 /* $NetBSD: cpu.c,v 1.1 2003/04/26 18:39:26 fvdl Exp $ */
 
 /*-
@@ -517,9 +517,11 @@ cpu_init(struct cpu_info *ci)
 		fpu_save_len = ebx;
 	}
 
+#if NVMM > 0
 	/* Re-enable VMM if needed */
 	if (ci->ci_flags & CPUF_VMM)
 		start_vmm_on_cpu(ci);
+#endif /* NVMM > 0 */
 
 #ifdef MULTIPROCESSOR
 	ci->ci_flags |= CPUF_RUNNING;
