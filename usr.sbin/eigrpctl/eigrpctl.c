d139f9217901c1e3d6347e589a1ac039a5434cd4
mmcc <mmcc@openbsd.org>
2015-12-07 20:28:18 +0000

    No need to check for NULL before free().

--- usr.sbin/eigrpctl/eigrpctl.c
+++ usr.sbin/eigrpctl/eigrpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eigrpctl.c,v 1.5 2015/12/05 15:48:40 claudio Exp $ */
+/*	$OpenBSD: eigrpctl.c,v 1.6 2015/12/07 20:28:18 mmcc Exp $ */
 
 /*
  * Copyright (c) 2015 Renato Westphal <renato@openbsd.org>
@@ -607,10 +607,8 @@ show_topology_detail_msg(struct imsg *imsg, struct parse_result *res)
 		}
 
 		printf("\n");
-		if (dstnet)
-			free(dstnet);
-		if (state)
-			free(state);
+		free(dstnet);
+		free(state);
 		free(type);
 		free(nexthop);
 		break;
