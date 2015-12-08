f5bc4a8bb049273dd28d8c9d203bd443d49685c2
mikeb <mikeb@openbsd.org>
2015-12-08 18:46:25 +0000

    Xen basic infrastructure files and pvbus(4) attachment.
    With input from and OK mpi, mlarkin, reyk

---
+++ sys/dev/pv/xenvar.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Mike Belopuhov
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _XENVAR_H_
+#define _XENVAR_H_
+
+struct xen_softc {
+	struct device		 sc_dev;
+	uint32_t		 sc_base;
+};
+
+#endif	/* _XENVAR_H_ */
