f80fdc7e33e0a0329d1396a0b1bba516e11da3b4
vgross <vgross@openbsd.org>
2015-12-09 14:52:59 +0000

    add multicast test suites

--- regress/sys/netinet/in_pcbbind/runtest.c
+++ regress/sys/netinet/in_pcbbind/runtest.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: runtest.c,v 1.1 2015/12/07 17:05:52 vgross Exp $ */
+/* $OpenBSD: runtest.c,v 1.2 2015/12/09 14:52:59 vgross Exp $ */
 /*
  * Copyright (c) 2015 Vincent Gross <vincent.gross@kilob.yt>
  *
@@ -25,13 +25,16 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <netinet/in.h>
+#include <net/if.h>
+#include <ifaddrs.h>
 
 int
 runtest(int *sockp, struct addrinfo *ai, int reuseaddr, int reuseport,
-    int expected)
+    void *mreq, int expected)
 {
 	int error, optval;
+	struct ip_mreq imr;
 
 	*sockp = socket(ai->ai_family, ai->ai_socktype, 0);
 	if (*sockp == -1) {
@@ -59,16 +62,44 @@ runtest(int *sockp, struct addrinfo *ai, int reuseaddr, int reuseport,
 		}
 	}
 
+	if (mreq) {
+		switch (ai->ai_family) {
+		case AF_INET6:
+			error = setsockopt(*sockp, IPPROTO_IPV6, IPV6_JOIN_GROUP,
+			    mreq, sizeof(struct ipv6_mreq));
+			if (error) {
+				warn("%s : setsockopt(IPV6_JOIN_GROUP)",
+				    ai->ai_canonname);
+				return (2);
+			}
+			break;
+		case AF_INET:
+			error = setsockopt(*sockp, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+			    mreq, sizeof(struct ip_mreq));
+			if (error) {
+				warn("%s : setsockopt(IP_ADD_MEMBERSHIP)",
+				    ai->ai_canonname);
+				return (2);
+			}
+			break;
+		default:
+			warnx("%s : trying to join multicast group in unknown AF",
+			    ai->ai_canonname);
+			return (2);
+		}
+	}
+
+
 	error = bind(*sockp, ai->ai_addr, ai->ai_addrlen);
 	if (error && (expected == 0 || expected != errno)) {
 		warn("bind(%s,%s,%s)", ai->ai_canonname,
-				reuseaddr ? "REUSEADDR" : "",
-				reuseport ? "REUSEPORT" : "");
+		    reuseaddr ? "REUSEADDR" : "", reuseport ? "REUSEPORT" : "");
 		return (1);
 	}
 	if (error == 0 && expected != 0) {
-		warnx("%s : bind() succeeded, expected : %s",
-		    ai->ai_canonname, strerror(errno));
+		warnx("bind(%s,%s,%s) succeeded, expected : %s", ai->ai_canonname,
+		    reuseaddr ? "REUSEADDR" : "", reuseport ? "REUSEPORT" : "",
+		    strerror(errno));
 		return (1);
 	}
 
@@ -84,85 +115,326 @@ cleanup(int *fds, int num_fds)
 }
 
 int
+unicast_testsuite(struct addrinfo *local, struct addrinfo *any)
+{
+	int test_rc, rc, *s;
+	int sockets[4];
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 0, NULL, 0);
+	rc |= runtest(s++, any,   0, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, any,   1, 0, NULL, 0);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 1);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any,   0, 0, NULL, 0);
+	rc |= runtest(s++, local, 0, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, local, 1, 0, NULL, 0);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 2);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 1, NULL, 0);
+	rc |= runtest(s++, local, 0, 1, NULL, 0);
+	rc |= runtest(s++, local, 1, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, local, 0, 0, NULL, EADDRINUSE);
+	cleanup(sockets, 4);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 3);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any, 0, 1, NULL, 0);
+	rc |= runtest(s++, any, 0, 1, NULL, 0);
+	rc |= runtest(s++, any, 1, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, any, 0, 0, NULL, EADDRINUSE);
+	cleanup(sockets, 4);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 4);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 0, NULL, 0);
+	rc |= runtest(s++, local, 1, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, local, 0, 1, NULL, EADDRINUSE);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 5);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any, 1, 0, NULL, 0);
+	rc |= runtest(s++, any, 1, 0, NULL, EADDRINUSE);
+	rc |= runtest(s++, any, 0, 1, NULL, EADDRINUSE);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 6);
+
+	return (test_rc);
+}
+
+int
+mcast_reuse_testsuite(struct addrinfo *local, void *mr)
+{
+	int test_rc, rc, *s;
+	int sockets[6];
+	int testnum = 1;
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, EADDRINUSE);
+	rc |= runtest(s++, local, 0, 1, mr, EADDRINUSE);
+	rc |= runtest(s++, local, 1, 1, mr, EADDRINUSE);
+	cleanup(sockets, 4);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 1);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	rc |= runtest(s++, local, 0, 0, mr, EADDRINUSE);
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	cleanup(sockets, 5);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 2);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 0, 0, mr, EADDRINUSE);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	cleanup(sockets, 5);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 3);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 0, 0, mr, EADDRINUSE);
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	cleanup(sockets, 5);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 4);
+
+#if 1
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 5);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 0, 1, mr, 0);
+	cleanup(sockets, 5);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 6);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 1, 1, mr, 0);
+	rc |= runtest(s++, local, 1, 0, mr, 0);
+	rc |= runtest(s++, local, 0, 1, mr, 0); /* XXX - EADDRINUSE */
+	cleanup(sockets, 5);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 7);
+#endif
+	return (test_rc);
+}
+
+int
+mcast6_testsuite(struct addrinfo *local, struct ipv6_mreq *local_mreq,
+    struct addrinfo *any, struct ipv6_mreq *any_mreq)
+{
+	int test_rc, rc, *s;
+	int sockets[4];
+	int testnum = 1;
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 0, local_mreq, 0);
+	rc |= runtest(s++, any,   0, 0, any_mreq,   EADDRINUSE);
+	rc |= runtest(s++, any,   1, 0, any_mreq,   0);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 1);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any,   0, 0, any_mreq,   0);
+	rc |= runtest(s++, local, 0, 0, local_mreq, EADDRINUSE);
+	rc |= runtest(s++, local, 1, 0, local_mreq, 0);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 2);
+	
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 0, 1, local_mreq, 0);
+	rc |= runtest(s++, local, 0, 1, local_mreq, 0);
+	rc |= runtest(s++, local, 1, 0, local_mreq, 0);
+	rc |= runtest(s++, local, 0, 0, local_mreq, EADDRINUSE);
+	cleanup(sockets, 4);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 3);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any, 0, 1, any_mreq, 0);
+	rc |= runtest(s++, any, 0, 1, any_mreq, 0);
+	rc |= runtest(s++, any, 1, 0, any_mreq, 0); /* XXX - EADDRINUSE */
+	rc |= runtest(s++, any, 0, 0, any_mreq, EADDRINUSE);
+	cleanup(sockets, 4);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 4);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, local, 1, 0, local_mreq, 0);
+	rc |= runtest(s++, local, 1, 0, local_mreq, 0);
+	rc |= runtest(s++, local, 0, 1, local_mreq, 0); /* XXX - EADDRINUSE */
+	rc |= runtest(s++, local, 0, 0, local_mreq, EADDRINUSE);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 5);
+
+	rc = 0; s = sockets;
+	rc |= runtest(s++, any, 1, 0, any_mreq, 0);
+	rc |= runtest(s++, any, 1, 0, any_mreq, 0); /* XXX - EADDRINUSE */
+	rc |= runtest(s++, any, 0, 1, any_mreq, 0); /* XXX - EADDRINUSE */
+	rc |= runtest(s++, any, 0, 0, any_mreq, EADDRINUSE);
+	cleanup(sockets, 3);
+	test_rc |= rc;
+	if (rc)
+		warnx("%s : test #%d failed", __func__, 6);
+
+	return (test_rc);
+}
+
+int
 main(int argc, char *argv[])
 {
-	int error, i, rc, test_rc;
-	struct addrinfo hints, *local, *any;
-	int sockets[4];
+	int error, rc;
+	struct addrinfo hints, *local, *any, *mifa;
+	struct ifaddrs *ifap, *curifa;
+	struct ip_mreq local_imr, any_imr;
+	struct ipv6_mreq local_i6mr, any_i6mr;
+	struct sockaddr_in *sin;
+	struct sockaddr_in6 *sin6;
 	int *s;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags = AI_ADDRCONFIG | AI_NUMERICHOST | AI_NUMERICSERV | \
 	    AI_PASSIVE;
 	hints.ai_socktype = SOCK_DGRAM;
-	if ((error = getaddrinfo(argv[2], argv[1], &hints, &local)))
-		errx(1, "getaddrinfo(%s,%s): %s", argv[2], argv[1],
-		    gai_strerror(error));
-	local->ai_canonname = argv[2];
 
-	hints.ai_family = local->ai_family;
-	if ((error = getaddrinfo(NULL, argv[1], &hints, &any)))
-		errx(1, "getaddrinfo(NULL,%s): %s", argv[1],
-		    gai_strerror(error));
-	any->ai_canonname = "ANY";
+	if (strcmp(argv[1],"unicast") == 0) {
+		if ((error = getaddrinfo(argv[3], argv[2], &hints, &local)))
+			errx(2, "getaddrinfo(%s,%s): %s", argv[3], argv[2],
+			    gai_strerror(error));
+		local->ai_canonname = argv[3];
 
-	test_rc = 0;
+		hints.ai_family = local->ai_family;
+		if ((error = getaddrinfo(NULL, argv[2], &hints, &any)))
+			errx(2, "getaddrinfo(NULL,%s): %s", argv[2],
+			    gai_strerror(error));
+		any->ai_canonname = "ANY";
 
-	rc = 0; s = sockets;
-	rc |= runtest(s++, local, 0, 0, 0);
-	rc |= runtest(s++, any,   0, 0, EADDRINUSE);
-	rc |= runtest(s++, any,   1, 0, 0);
-	cleanup(sockets, 3);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #1 failed");
+		return unicast_testsuite(local, any);
+	}
 
-	rc = 0; s = sockets;
-	rc |= runtest(s++, any,   0, 0, 0);
-	rc |= runtest(s++, local, 0, 0, EADDRINUSE);
-	rc |= runtest(s++, local, 1, 0, 0);
-	cleanup(sockets, 3);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #2 failed");
-	
-	rc = 0; s = sockets;
-	rc |= runtest(s++, local, 0, 1, 0);
-	rc |= runtest(s++, local, 0, 1, 0);
-	rc |= runtest(s++, local, 1, 0, EADDRINUSE); /* 0 if multicast */
-	rc |= runtest(s++, local, 0, 0, EADDRINUSE);
-	cleanup(sockets, 4);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #3 failed");
 
-	rc = 0; s = sockets;
-	rc |= runtest(s++, any, 0, 1, 0);
-	rc |= runtest(s++, any, 0, 1, 0);
-	rc |= runtest(s++, any, 1, 0, EADDRINUSE); /* 0 if multicast */
-	rc |= runtest(s++, any, 0, 0, EADDRINUSE);
-	cleanup(sockets, 4);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #4 failed");
+	if (strcmp(argv[1], "mcast") == 0) {
+		if ((error = getaddrinfo(argv[3], argv[2], &hints, &local)))
+			errx(2, "getaddrinfo(%s,%s): %s", argv[3], argv[2],
+			    gai_strerror(error));
+		local->ai_canonname = argv[3];
 
-	rc = 0; s = sockets;
-	rc |= runtest(s++, local, 1, 0, 0);
-	rc |= runtest(s++, local, 1, 0, EADDRINUSE); /* 0 if multicast */
-	rc |= runtest(s++, local, 0, 1, EADDRINUSE); /* 0 if multicast */
-	cleanup(sockets, 3);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #5 failed");
+		hints.ai_family = local->ai_family;
+		if ((error = getaddrinfo(NULL, argv[2], &hints, &any)))
+			errx(2, "getaddrinfo(NULL,%s): %s", argv[2],
+			    gai_strerror(error));
+		any->ai_canonname = "ANY";
 
-	rc = 0; s = sockets;
-	rc |= runtest(s++, local, 1, 0, 0);
-	rc |= runtest(s++, local, 1, 0, EADDRINUSE); /* 0 if multicast */
-	rc |= runtest(s++, local, 0, 1, EADDRINUSE); /* 0 if multicast */
-	cleanup(sockets, 3);
-	test_rc |= rc;
-	if (rc)
-		warnx("test #6 failed");
+		hints.ai_flags = AI_ADDRCONFIG | AI_NUMERICHOST;
+		if ((error = getaddrinfo(argv[4], NULL, &hints, &mifa)))
+			errx(2, "getaddrinfo(%s,NULL): %s", argv[4],
+			    gai_strerror(error));
 
-	return (test_rc);
+
+		switch (hints.ai_family) {
+		case AF_INET:
+			sin = (struct sockaddr_in *)(mifa->ai_addr);
+			local_imr.imr_interface = sin->sin_addr;
+			sin = (struct sockaddr_in *)(local->ai_addr);
+			local_imr.imr_multiaddr = sin->sin_addr;
+
+			/* no 'any' mcast group in ipv4 */
+			return mcast_reuse_testsuite(local, &local_imr);
+
+		case AF_INET6:
+			if (getifaddrs(&ifap))
+				err(2, "getifaddrs()");
+			curifa = ifap;
+			while (curifa) {
+				if (memcmp(curifa->ifa_addr,
+				    mifa->ai_addr,
+				    mifa->ai_addrlen) == 0)
+					break;
+				curifa = curifa->ifa_next;
+			}
+			if (curifa == NULL)
+				errx(2, "no interface configured with %s", argv[4]);
+			local_i6mr.ipv6mr_interface =
+			    if_nametoindex(curifa->ifa_name);
+			if (local_i6mr.ipv6mr_interface == 0)
+				errx(2, "unable to get \"%s\" index",
+				    curifa->ifa_name);
+			freeifaddrs(ifap);
+
+			sin6 = (struct sockaddr_in6 *)(local->ai_addr);
+			local_i6mr.ipv6mr_multiaddr = sin6->sin6_addr;
+
+			any_i6mr.ipv6mr_interface = local_i6mr.ipv6mr_interface;
+			sin6 = (struct sockaddr_in6 *)(any->ai_addr);
+			any_i6mr.ipv6mr_multiaddr = sin6->sin6_addr;
+
+			rc = 0;
+			rc |= mcast_reuse_testsuite(local, &local_i6mr);
+			if (geteuid() == 0)
+				rc |= mcast6_testsuite(local, &local_i6mr, any, &any_i6mr);
+			else
+				warnx("skipping mcast6_testsuite() due to insufficient privs, please run again as root");
+			return (rc);
+
+		default:
+			errx(2, "no multicast test suite for af %d", hints.ai_family);
+		}
+
+	}
+
+
+	return (2);
 }
