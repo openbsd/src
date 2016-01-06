af7719fe4ff82bf7b0e967971f3ebaf9dc5e647f
krw <krw@openbsd.org>
2016-01-06 16:12:20 +0000

    We want the first, not the last, OpenBSD partition in the MBR.

--- sys/arch/amd64/stand/libsa/softraid.c
+++ sys/arch/amd64/stand/libsa/softraid.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: softraid.c,v 1.24 2016/01/06 02:10:03 krw Exp $	*/
+/*	$OpenBSD: softraid.c,v 1.25 2016/01/06 16:12:20 krw Exp $	*/
 
 /*
  * Copyright (c) 2012 Joel Sing <jsing@openbsd.org>
@@ -556,8 +556,10 @@ sr_getdisklabel(struct sr_boot_volume *bv, struct disklabel *label)
 			dp = &mbr.dmbr_parts[i];
 			if (!dp->dp_size)
 				continue;
-			if (dp->dp_typ == DOSPTYP_OPENBSD)
+			if (dp->dp_typ == DOSPTYP_OPENBSD) {
 				start = dp->dp_start;
+				break;
+			}
 		}
 	}
 
