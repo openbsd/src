5547a3bd6ebd6e964899bfac0ce5694504b7eff0
stefan <stefan@openbsd.org>
2016-01-08 07:54:02 +0000

    Use uiomove() instead of uiomovei(). Diff from Martin Natano, thanks!
    ok kettenis@, deraadt@

--- sys/dev/rnd.c
+++ sys/dev/rnd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rnd.c,v 1.177 2015/12/28 05:21:53 tedu Exp $	*/
+/*	$OpenBSD: rnd.c,v 1.178 2016/01/08 07:54:02 stefan Exp $	*/
 
 /*
  * Copyright (c) 2011 Theo de Raadt.
@@ -840,7 +840,7 @@ randomread(dev_t dev, struct uio *uio, int ioflag)
 	}
 
 	while (ret == 0 && uio->uio_resid > 0) {
-		int	n = ulmin(POOLBYTES, uio->uio_resid);
+		size_t	n = ulmin(POOLBYTES, uio->uio_resid);
 
 		if (myctx) {
 #ifndef KEYSTREAM_ONLY
@@ -849,7 +849,7 @@ randomread(dev_t dev, struct uio *uio, int ioflag)
 			chacha_encrypt_bytes(&lctx, buf, buf, n);
 		} else
 			arc4random_buf(buf, n);
-		ret = uiomovei(buf, n, uio);
+		ret = uiomove(buf, n, uio);
 		if (ret == 0 && uio->uio_resid > 0)
 			yield();
 	}
@@ -872,9 +872,9 @@ randomwrite(dev_t dev, struct uio *uio, int flags)
 	buf = malloc(POOLBYTES, M_TEMP, M_WAITOK);
 
 	while (ret == 0 && uio->uio_resid > 0) {
-		int	n = ulmin(POOLBYTES, uio->uio_resid);
+		size_t	n = ulmin(POOLBYTES, uio->uio_resid);
 
-		ret = uiomovei(buf, n, uio);
+		ret = uiomove(buf, n, uio);
 		if (ret != 0)
 			break;
 		while (n % sizeof(u_int32_t))
