9b8ad5878cd3d81f9857d5201a0c8a1fcdd7ff64
jcs <jcs@openbsd.org>
2016-01-08 16:57:26 +0000

    pull in dev/hid/hid.h for moved HIO_* vars

--- lib/libusbhid/usbhid.h
+++ lib/libusbhid/usbhid.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usbhid.h,v 1.6 2014/05/12 17:03:28 mpi Exp $	*/
+/*	$OpenBSD: usbhid.h,v 1.7 2016/01/08 16:57:26 jcs Exp $	*/
 /*	$NetBSD: usbhid.h,v 1.1 2001/12/28 17:45:27 augustss Exp $	*/
 
 /*
@@ -26,6 +26,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <dev/hid/hid.h>
 
 typedef struct report_desc *report_desc_t;
 
