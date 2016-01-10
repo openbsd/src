242982a07040de6fa8d5e4066cedc522e56aa5d3
schwarze <schwarze@openbsd.org>
2016-01-10 14:48:28 +0000

    Make the test file 7-bit clean and improve diagnostics with hexdump(1), both in the same way as it was done for ul(1). No functional change.

--- regress/usr.bin/colrm/colrm.sh
+++ regress/usr.bin/colrm/colrm.sh
@@ -25,8 +25,11 @@ test_colrm()
 	result=`echo -n "$stdin" | colrm $args ; echo -n .`
 	if [ "$result" != "${expected}" ]; then
 		echo "echo -n \"$stdin\" | colrm $args"
+		echo -n "$stdin" | hexdump -C
 		echo "expected: \"$expected\""
+		echo -n "$expected" | hexdump -C
 		echo "result: \"$result\""
+		echo -n "$result" | hexdump -C
 		exit 1;
 	fi
 
@@ -37,8 +40,11 @@ test_colrm()
 	result=`echo -n "$stdin" | colrm $args ; echo -n .`
 	if [ "$result" != "${expected}" ]; then
 		echo "[C] echo -n \"$stdin\" | colrm $args"
+		echo -n "$stdin" | hexdump -C
 		echo "expected: \"$expected\""
+		echo -n "$expected" | hexdump -C
 		echo "result: \"$result\""
+		echo -n "$result" | hexdump -C
 		exit 1;
 	fi
 }
@@ -60,38 +66,42 @@ test_colrm "7 9" "abcd\txe" "abcd  e"
 test_colrm "3 6" "abcd\tef" "ab  ef"
 
 # zero width
-test_colrm "2 2" "axÌ€b" "ab" "aÌ€b"
-test_colrm "3 3" "axÌ€bxÌ€c" "axÌ€xÌ€c" \
-		 "ax€bxÌ€c"
+test_colrm "2 2" "ax\0314\0200b" "ab" "a\0314\0200b"
+test_colrm "3 3" "ax\0314\0200bx\0314\0200c" "ax\0314\0200x\0314\0200c" \
+		 "ax\0200bx\0314\0200c"
 
 # double width
-test_colrm "2 3" "aì¿¿b" "ab" "a¿b"
-test_colrm "2 2" "aì¿¿b" "a b" "a¿¿b"
-test_colrm "3 3" "aì¿¿b" "a b" "aì¿b"
-test_colrm "4 4" "aì¿¿bì¿¿c" "aì¿¿ì¿¿c" \
-		 "aì¿bì¿¿c"
+test_colrm "2 3" "a\0354\0277\0277b" "ab" "a\0277b"
+test_colrm "2 2" "a\0354\0277\0277b" "a b" "a\0277\0277b"
+test_colrm "3 3" "a\0354\0277\0277b" "a b" "a\0354\0277b"
+test_colrm "4 4" "a\0354\0277\0277b\0354\0277\0277c" \
+		 "a\0354\0277\0277\0354\0277\0277c" \
+		 "a\0354\0277b\0354\0277\0277c"
 
 # backspaces
-test_colrm "3 3" "ab_cd_e" "ab_d_e"
-test_colrm "2 2" "ab_c" "ac"
-test_colrm "2 2" "axÌ€b" "ab" "aÌ€b"
-test_colrm "3 3" "axÌ€bxÌ€c" "axÌ€xÌ€c" \
-		 "ax€bxÌ€c"
-test_colrm "2 3" "aì¿¿bcde" "ade" "a¿bcde"
-test_colrm "2 2" "aì¿¿bcde" "acde" "a¿¿bcde"
-test_colrm "3 3" "aì¿¿bcde" "abde" "aì¿bcde"
-test_colrm "4 4" "aì¿¿bì¿¿c" "aì¿¿ì¿¿c" \
-		 "aì¿bì¿¿c"
+test_colrm "3 3" "ab\b_cd\b_e" "ab\b_d\b_e"
+test_colrm "2 2" "ab\b_c" "ac"
+test_colrm "2 2" "ax\0314\0200\bb" "ab" "a\0314\0200\bb"
+test_colrm "3 3" "ax\0314\0200\bbx\0314\0200\bc" \
+		 "ax\0314\0200\bx\0314\0200\bc" \
+		 "ax\0200\bbx\0314\0200\bc"
+test_colrm "2 3" "a\0354\0277\0277\bbcde" "ade" "a\0277\bbcde"
+test_colrm "2 2" "a\0354\0277\0277\bbcde" "acde" "a\0277\0277\bbcde"
+test_colrm "3 3" "a\0354\0277\0277\bbcde" "abde" "a\0354\0277\bbcde"
+test_colrm "4 4" "a\0354\0277\0277b\0354\0277\0277c" \
+		 "a\0354\0277\0277\0354\0277\0277c" \
+		 "a\0354\0277b\0354\0277\0277c"
 test_colrm "" "\bx" "\bx"
 test_colrm "1" "\bx" "\b"
 
 # invalid bytes and non-printable characters
-test_colrm "3 3" "aÿbÿc" "aÿÿc"
-test_colrm "2 2" "aÿb" "ab"
-test_colrm "3 3" "abc" "ac"
-test_colrm "2 2" "ab" "ab"
-test_colrm "3 3" "aÍ¸bÍ¸c" "aÍ¸Í¸c" "aÍbÍ¸c"
-test_colrm "2 2" "aÍ¸b" "ab" "a¸b"
+test_colrm "3 3" "a\0377b\0377c" "a\0377\0377c"
+test_colrm "2 2" "a\0377b" "ab"
+test_colrm "3 3" "a\01b\01c" "a\01\01c"
+test_colrm "2 2" "a\01b" "ab"
+test_colrm "3 3" "a\0315\0270b\0315\0270c" "a\0315\0270\0315\0270c" \
+		 "a\0315b\0315\0270c"
+test_colrm "2 2" "a\0315\0270b" "ab" "a\0270b"
 
 # edge cases
 test_colrm "" "" ""
