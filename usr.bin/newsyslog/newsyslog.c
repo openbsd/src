a7f93d5a1c4b6a9ca677959a34e27ead591816d5
tb <tb@openbsd.org>
2016-01-11 19:26:04 +0000

    ajacoutot@ reported seeing
    newsyslog: preposterous process number in pid file: /var/run/syslog.pid
    
    in his log since syslogd ran as pid 2.
    
    Nowadays only init runs with a reserved low pid, so decrease MIN_PID
    from 4 to 2, as suggested by espie@.
    
    ok kettenis@

--- usr.bin/newsyslog/newsyslog.c
+++ usr.bin/newsyslog/newsyslog.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: newsyslog.c,v 1.99 2015/12/09 19:28:34 mmcc Exp $	*/
+/*	$OpenBSD: newsyslog.c,v 1.100 2016/01/11 19:26:04 tb Exp $	*/
 
 /*
  * Copyright (c) 1999, 2002, 2003 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -117,7 +117,7 @@
 #define CE_FOLLOW	0x10		/* Follow symbolic links */
 #define CE_TRIMAT	0x20		/* Trim at a specific time */
 
-#define	MIN_PID		4		/* Don't touch pids lower than this */
+#define	MIN_PID		2		/* Don't touch pids lower than this */
 #define	MIN_SIZE	256		/* Don't rotate if smaller (in bytes) */
 
 #define	DPRINTF(x)	do { if (verbose) printf x ; } while (0)
