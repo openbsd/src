e6b41cc5b916a53d90d9d325fa0c7bf9cb3d16ab
schwarze <schwarze@openbsd.org>
2016-01-18 20:31:36 +0000

    UTF-8 support: Cut by display columns rather than by character positions because the latter would be useless in the presence of combining zero-width characters.  Similarly, let tab advance to display columns rather than to character positions.
    For compatibility with nroff and man(1) output, let backspace back
    up one character rather than on display column.  But for compatibility
    with POSIX fold(1), *if* two backspaces follow a double-width character,
    ignore the second one.
    
    Fix some bugs while here:  Delete backspaces that immediately follow
    deleted characters.  Expand tabs intersecting deletions, such that
    part of the blanks can be removed.  Expand tabs following deletions,
    or they would no longer align with adjacent lines without tabs.
    
    OK jmc@ on a previous version of the manual.
    No opposition when shown on tech@.

--- usr.bin/colrm/colrm.c
+++ usr.bin/colrm/colrm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: colrm.c,v 1.11 2015/10/09 01:37:06 deraadt Exp $	*/
+/*	$OpenBSD: colrm.c,v 1.12 2016/01/18 20:31:36 schwarze Exp $	*/
 /*	$NetBSD: colrm.c,v 1.4 1995/09/02 05:51:37 jtc Exp $	*/
 
 /*-
@@ -35,22 +35,27 @@
 #include <err.h>
 #include <errno.h>
 #include <limits.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <wchar.h>
 
 #define	TAB	8
 
-void check(FILE *);
 void usage(void);
 
 int
 main(int argc, char *argv[])
 {
-	u_long column, start, stop;
-	int ch;
-	char *p;
+	char	 *line, *p;
+	ssize_t	  linesz;
+	wchar_t	  wc;
+	u_long	  column, newcol, start, stop;
+	int	  ch, len, width;
+
+	setlocale(LC_ALL, "");
 
 	if (pledge("stdio", NULL) == -1)
 		err(1, "pledge");
@@ -85,39 +90,95 @@ main(int argc, char *argv[])
 	if (stop && start > stop)
 		err(1, "illegal start and stop columns");
 
-	for (column = 0;;) {
-		switch (ch = getchar()) {
-		case EOF:
-			check(stdin);
-			break;
-		case '\b':
-			if (column)
-				--column;
-			break;
-		case '\n':
-			column = 0;
-			break;
-		case '\t':
-			column = (column + TAB) & ~(TAB - 1);
-			break;
-		default:
-			++column;
-			break;
+	line = NULL;
+	while (getline(&line, &linesz, stdin) != -1) {
+		column = 0;
+		width = 0;
+		for (p = line; *p != '\0'; p += len) {
+			len = 1;
+			switch (*p) {
+			case '\n':
+				putchar('\n');
+				continue;
+			case '\b':
+				/*
+				 * Pass it through if the previous character
+				 * was in scope, still represented by the
+				 * current value of "column".
+				 * Allow an optional second backspace
+				 * after a double-width character.
+				 */
+				if (start == 0 || column < start ||
+				    (stop > 0 &&
+				     column > stop + (width > 1))) {
+					putchar('\b');
+					if (width > 1 && p[1] == '\b')
+						putchar('\b');
+				} 
+				if (width > 1 && p[1] == '\b')
+					p++;
+				column -= width;
+				continue;
+			case '\t':
+				newcol = (column + TAB) & ~(TAB - 1);
+				if (start == 0 || newcol < start) {
+					putchar('\t');
+					column = newcol;
+				} else
+					/*
+					 * Expand tabs that intersect or
+					 * follow deleted columns.
+					 */
+					while (column < newcol)
+						if (++column < start ||
+						    (stop > 0 &&
+						     column > stop))
+				 			putchar(' ');
+				continue;
+			default:
+				break;
+			}
+
+			/*
+			 * Handle the three cases of invalid bytes,
+			 * non-printable, and printable characters.
+			 */
+
+			if ((len = mbtowc(&wc, p, MB_CUR_MAX)) == -1) {
+				(void)mbtowc(NULL, NULL, MB_CUR_MAX);
+				len = 1;
+				width = 1;
+			} else if ((width = wcwidth(wc)) == -1)
+				width = 1;
+
+			/*
+			 * If the character completely fits before or
+			 * after the cut, keep it; otherwise, skip it.
+			 */
+
+			if ((start == 0 || column + width < start ||
+			    (stop > 0 && column + (width > 0) > stop)))
+			    	fwrite(p, 1, len, stdout);
+
+			/*
+			 * If the cut cuts the character in half
+			 * and no backspace follows,
+			 * print a blank for correct columnation.
+			 */
+
+			else if (width > 1 && p[len] != '\b' &&
+			    (start == 0 || column + 1 < start ||
+			    (stop > 0 && column + width > stop)))
+				putchar(' ');
+
+			column += width;
 		}
-
-		if ((!start || column < start || (stop && column > stop)) &&
-		    putchar(ch) == EOF)
-			check(stdout);
 	}
-}
-
-void
-check(FILE *stream)
-{
-	if (feof(stream))
-		exit(0);
-	if (ferror(stream))
-		err(1, "%s", stream == stdin ? "stdin" : "stdout");
+	if (ferror(stdin))
+		err(1, "stdin");
+	if (ferror(stdout))
+		err(1, "stdout");
+	return 0;
 }
 
 void
