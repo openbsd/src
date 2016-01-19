f28a080d198a701c81a0cd617229b96ee4bd6550
krw <krw@openbsd.org>
2016-01-19 17:55:19 +0000

    Remove unused global 'io_buffer'.

--- sbin/pdisk/io.c
+++ sbin/pdisk/io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: io.c,v 1.17 2016/01/18 15:03:18 krw Exp $	*/
+/*	$OpenBSD: io.c,v 1.18 2016/01/19 17:55:19 krw Exp $	*/
 
 /*
  * io.c - simple io and input parsing routines
@@ -45,7 +45,6 @@ const long	kDefault = -1;
 
 short		unget_buf[UNGET_MAX_COUNT + 1];
 int		unget_count;
-char		io_buffer[MAXIOSIZE];
 
 long		get_number(int);
 char           *get_string(int);
