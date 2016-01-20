d88f57029e5acaaaf028633c7fa15c5d7325c5cc
jasper <jasper@openbsd.org>
2016-01-20 19:09:52 +0000

    After nearly 20 years of warning that malloc.h is obsolete, it's about time we actually remove this header. It was originally added for ports, which is malloc.h-free now.
    additional ports bulk by aja@
    ok bently@ dcoppa@ millert@ sthen@

--- include/malloc.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: malloc.h,v 1.2 1996/10/12 03:13:56 tholo Exp $	*/
-/*	$NetBSD: malloc.h,v 1.3 1994/10/26 00:56:03 cgd Exp $	*/
-
-#warning "<malloc.h> is obsolete, use <stdlib.h>"
-
-#include <stdlib.h>
