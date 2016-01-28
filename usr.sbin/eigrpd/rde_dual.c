7d45d066ef38396a4b0f596077efbb35f4f15e01
jsg <jsg@openbsd.org>
2016-01-28 13:25:14 +0000

    Correct bit flag tests to work as intended. ok renato@

--- usr.sbin/eigrpd/rde_dual.c
+++ usr.sbin/eigrpd/rde_dual.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_dual.c,v 1.18 2016/01/15 12:56:12 renato Exp $ */
+/*	$OpenBSD: rde_dual.c,v 1.19 2016/01/28 13:25:14 jsg Exp $ */
 
 /*
  * Copyright (c) 2015 Renato Westphal <renato@openbsd.org>
@@ -666,8 +666,8 @@ rt_update_fib(struct rt_node *rn)
 
 			installed++;
 
-			if (route->flags & (F_EIGRP_ROUTE_INSTALLED |
-			    !F_EIGRP_ROUTE_M_CHANGED))
+			if ((route->flags & F_EIGRP_ROUTE_INSTALLED) &&
+			    !(route->flags & F_EIGRP_ROUTE_M_CHANGED))
 				continue;
 
 			rde_send_change_kroute(rn, route);
