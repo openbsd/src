007f6abc65dd39bba01b60c5b07d8467eb73522c
martijn <martijn@openbsd.org>
2016-01-30 21:34:57 +0000

    /var/tmp is dead, long live /tmp
    OK zhuk@ and tb@

--- usr.bin/vi/build/pathnames.h
+++ usr.bin/vi/build/pathnames.h
@@ -1,8 +1,8 @@
-/*	$OpenBSD: pathnames.h,v 1.5 2016/01/06 22:34:45 millert Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.6 2016/01/30 21:34:57 martijn Exp $	*/
 /* @(#)pathnames.h.in	8.4 (Berkeley) 6/26/96 */
 
 #define	_PATH_EXRC	".exrc"
 #define	_PATH_NEXRC	".nexrc"
-#define	_PATH_PRESERVE	"/var/tmp/vi.recover"
+#define	_PATH_PRESERVE	"/tmp/vi.recover"
 #define	_PATH_SYSEXRC	"/etc/vi.exrc"
 #define	_PATH_TAGS	"tags"
