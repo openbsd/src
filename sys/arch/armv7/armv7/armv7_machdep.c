a5e3965e91f4771064f1bc2f50850abb14b0a7be
jsg <jsg@openbsd.org>
2016-01-31 01:04:24 +0000

    Leonardo Guardati discovered a u-boot built with "Enable workarounds for booting old kernels" was required to boot sunxi kernels with newer versions of u-boot.
    Unless built with OLD_SUNXI_KERNEL_COMPAT u-boot now uses the top four
    bits of the board id for a 'compatibility revision'.  This was
    supposedly for different handling of the pll5 dram clock, but we don't
    touch that currently.  Mask these bits away so the kernel will boot from
    a u-boot that doesn't have the option set.
    
    Tested by Patrick Wildt on a Banana Pi R1 with current u-boot sources.

--- sys/arch/armv7/armv7/armv7_machdep.c
+++ sys/arch/armv7/armv7/armv7_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: armv7_machdep.c,v 1.24 2015/05/19 03:30:54 jsg Exp $ */
+/*	$OpenBSD: armv7_machdep.c,v 1.25 2016/01/31 01:04:24 jsg Exp $ */
 /*	$NetBSD: lubbock_machdep.c,v 1.2 2003/07/15 00:25:06 lukem Exp $ */
 
 /*
@@ -397,6 +397,12 @@ initarm(void *arg0, void *arg1, void *arg2)
 	    bus_space_handle_t *);
 
 	board_id = (uint32_t)arg1;
+	/*
+	 * u-boot has decided the top four bits are
+	 * 'compatibility revision' for sunxi
+	 */
+	if (board_id != 0xffffffff)
+		board_id &= 0x0fffffff;
 
 	/*
 	 * Heads up ... Setup the CPU / MMU / TLB functions
