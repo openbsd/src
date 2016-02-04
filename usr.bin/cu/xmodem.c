169f012f6dd604ce5dfb9f685252148bfe4122d5
millert <millert@openbsd.org>
2016-02-04 18:33:30 +0000

    Include stdint.h for the uint{8,16}_t types.  OK nicm@

--- usr.bin/cu/xmodem.c
+++ usr.bin/cu/xmodem.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: xmodem.c,v 1.8 2015/10/05 23:15:31 nicm Exp $ */
+/* $OpenBSD: xmodem.c,v 1.9 2016/02/04 18:33:30 millert Exp $ */
 
 /*
  * Copyright (c) 2012 Nicholas Marriott <nicm@openbsd.org>
@@ -20,6 +20,7 @@
 
 #include <errno.h>
 #include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
