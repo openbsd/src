853039567776d2debd3023fd1b0e2562d0f543ab
bluhm <bluhm@openbsd.org>
2016-02-18 15:33:24 +0000

    Fix format string of a warning. from markus@; OK claudio@

--- usr.sbin/ospfd/database.c
+++ usr.sbin/ospfd/database.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: database.c,v 1.32 2015/03/13 02:31:29 claudio Exp $ */
+/*	$OpenBSD: database.c,v 1.33 2016/02/18 15:33:24 bluhm Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -167,7 +167,7 @@ recv_db_description(struct nbr *nbr, char *buf, u_int16_t len)
 	int			 dupe = 0;
 
 	if (len < sizeof(dd_hdr)) {
-		log_warnx("recv_db_description: neighbor ID %s: ",
+		log_warnx("recv_db_description: neighbor ID %s: "
 		    "bad packet size", inet_ntoa(nbr->id));
 		return;
 	}
