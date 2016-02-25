f062910fb880fb1608109fc6d3cb55de92aced87
semarie <semarie@openbsd.org>
2016-02-25 07:39:09 +0000

    corrects a misleading comment about SS_DNS (and adjust tab)
    SS_DNS is now created by using SOCK_DNS with socket(2). dnssocket(2) was removed since October 28.

--- sys/sys/socketvar.h
+++ sys/sys/socketvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: socketvar.h,v 1.59 2015/10/18 00:04:43 deraadt Exp $	*/
+/*	$OpenBSD: socketvar.h,v 1.60 2016/02/25 07:39:09 semarie Exp $	*/
 /*	$NetBSD: socketvar.h,v 1.18 1996/02/09 18:25:38 christos Exp $	*/
 
 /*-
@@ -146,7 +146,7 @@ struct socket {
 #define	SS_ASYNC		0x200	/* async i/o notify */
 #define	SS_CONNECTOUT		0x1000	/* connect, not accept, at this end */
 #define	SS_ISSENDING		0x2000	/* hint for lower layer */
-#define SS_DNS			0x4000	/* created using dnssocket() */
+#define	SS_DNS			0x4000	/* created using SOCK_DNS socket(2) */
 
 #ifdef _KERNEL
 /*
