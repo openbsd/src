737ef790dccd168e3cdd98e5be66f681bff40db2
krw <krw@openbsd.org>
2016-03-11 17:12:17 +0000

    "if (_3btol(cmd->len) != 0)" is more likely to be a useful test than "if (cmd->len) != 0)", as cmd->len is a 3 byte array.
    Broken since media_blkno accounting was introduced in r1.72 (2007!).
    Might fix some obscure tape issues around miscounting variable sized blocks.
    
    Spotted by mmcc@ and his friend clang.

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: st.c,v 1.129 2015/06/07 19:13:27 krw Exp $	*/
+/*	$OpenBSD: st.c,v 1.130 2016/03/11 17:12:17 krw Exp $	*/
 /*	$NetBSD: st.c,v 1.71 1997/02/21 23:03:49 thorpej Exp $	*/
 
 /*
@@ -1021,7 +1021,7 @@ ststart(struct scsi_xfer *xs)
 		/* Update block count now, errors will set it to -1. */
 		if (st->flags & ST_FIXEDBLOCKS)
 			st->media_blkno += _3btol(cmd->len);
-		else if (cmd->len != 0)
+		else if (_3btol(cmd->len) != 0)
 			st->media_blkno++;
 	}
 
