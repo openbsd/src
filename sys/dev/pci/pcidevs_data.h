ccd400035db12f90c5952594356380f0e21e7152
mlarkin <mlarkin@openbsd.org>
2016-03-15 07:06:20 +0000

    sync

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.1788 2016/02/20 14:34:27 deraadt Exp 
+ *	OpenBSD: pcidevs,v 1.1789 2016/03/15 06:54:49 mlarkin Exp 
  */
 
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
