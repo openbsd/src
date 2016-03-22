c31e5583884fb684208bd887743d70d052a70362
dlg <dlg@openbsd.org>
2016-03-22 23:53:01 +0000

    dont leak an mbuf when copying the packet in pipex_mppe_output fails
    ok yasuoka@

--- sys/net/pipex.c
+++ sys/net/pipex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pipex.c,v 1.86 2016/03/22 23:51:01 dlg Exp $	*/
+/*	$OpenBSD: pipex.c,v 1.87 2016/03/22 23:53:01 dlg Exp $	*/
 
 /*-
  * Copyright (c) 2009 Internet Initiative Japan Inc.
@@ -2569,9 +2569,9 @@ pipex_mppe_output(struct mbuf *m0, struct pipex_session *session,
 	for (m = m0; m != NULL; m = m->m_next) {
 		if (M_READONLY(m)) {
 			m = m_dup_pkt(m0, max_linkhdr, M_NOWAIT);
+			m_freem(m0);
 			if (m == NULL)
 				goto drop;
-			m_freem(m0);
 			m0 = m;
 			break;
 		}
