03c22ace7c31cb3a2b4bfff994b264255a01f875
semarie <semarie@openbsd.org>
2016-04-26 09:37:59 +0000

    add "dns" to openssl ocsp
    problem reported by Alexandre (kAworu)
    
    ok beck@ deraadt@ sthen@

--- usr.bin/openssl/ocsp.c
+++ usr.bin/openssl/ocsp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ocsp.c,v 1.7 2015/10/17 15:00:11 doug Exp $ */
+/* $OpenBSD: ocsp.c,v 1.8 2016/04/26 09:37:59 semarie Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2000.
  */
@@ -147,7 +147,7 @@ ocsp_main(int argc, char **argv)
 	const char *errstr = NULL;
 
 	if (single_execution) {
-		if (pledge("stdio inet rpath wpath cpath", NULL) == -1) {
+		if (pledge("stdio inet dns rpath wpath cpath", NULL) == -1) {
 			perror("pledge");
 			exit(1);
 		}
