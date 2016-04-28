2dbc8ef5f3aefde4feba4973f637ed7ee7ed7c93
jsing <jsing@openbsd.org>
2016-04-28 16:09:09 +0000

    Update AEAD regress to match EVP_aead_chacha20_poly1305() changes.

--- regress/lib/libcrypto/aead/aeadtest.c
+++ regress/lib/libcrypto/aead/aeadtest.c
@@ -134,11 +134,11 @@ aead_from_name(const EVP_AEAD **aead, const char *name)
 #else
 		fprintf(stderr, "No chacha20-poly1305 support.\n");
 #endif
-	} else if (strcmp(name, "chacha20-poly1305-ietf") == 0) {
+	} else if (strcmp(name, "chacha20-poly1305-old") == 0) {
 #if !defined(OPENSSL_NO_CHACHA) && !defined(OPENSSL_NO_POLY1305)
-		*aead = EVP_aead_chacha20_poly1305_ietf();
+		*aead = EVP_aead_chacha20_poly1305_old();
 #else
-		fprintf(stderr, "No chacha20-poly1305-ietf support.\n");
+		fprintf(stderr, "No chacha20-poly1305-old support.\n");
 #endif
 	} else {
 		fprintf(stderr, "Unknown AEAD: %s\n", name);
