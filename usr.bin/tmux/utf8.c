6973c2658a02ce6b995b9dc6a124a4a24dbb7d55
nicm <nicm@openbsd.org>
2016-04-29 09:11:19 +0000

    OpenBSD wcwidth() is sensible and complete so if it returns -1 it means that a character is not printable, so return to ignoring such characters.

--- usr.bin/tmux/utf8.c
+++ usr.bin/tmux/utf8.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: utf8.c,v 1.31 2016/04/27 09:36:25 nicm Exp $ */
+/* $OpenBSD: utf8.c,v 1.32 2016/04/29 09:11:19 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -119,14 +119,6 @@ utf8_width(wchar_t wc)
 	width = wcwidth(wc);
 	if (width < 0 || width > 0xff) {
 		log_debug("Unicode %04x, wcwidth() %d", wc, width);
-
-		/*
-		 * Many platforms have no width for relatively common
-		 * characters (wcwidth() returns -1); assume width 1 in this
-		 * case and hope for the best.
-		 */
-		if (width < 0)
-			return (1);
 		return (-1);
 	}
 	return (width);
