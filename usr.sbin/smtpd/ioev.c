32e1408cf2f21fe4b9083b9424de934f92491774
millert <millert@openbsd.org>
2016-05-16 21:43:16 +0000

    No need to store the return value of fcntl(fd, F_SETFL, flags). OK jung@

--- usr.sbin/smtpd/ioev.c
+++ usr.sbin/smtpd/ioev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ioev.c,v 1.25 2016/03/25 15:06:58 krw Exp $	*/
+/*	$OpenBSD: ioev.c,v 1.26 2016/05/16 21:43:16 millert Exp $	*/
 /*
  * Copyright (c) 2012 Eric Faurot <eric@openbsd.org>
  *
@@ -140,7 +140,7 @@ io_set_nonblocking(int fd)
 
 	flags |= O_NONBLOCK;
 
-	if ((flags = fcntl(fd, F_SETFL, flags)) == -1)
+	if (fcntl(fd, F_SETFL, flags) == -1)
 		err(1, "io_set_blocking:fcntl(F_SETFL)");
 }
 
