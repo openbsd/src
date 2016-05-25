b01a7407d5cd6927ce003f4eff4e6369a9540ead
ratchov <ratchov@openbsd.org>
2016-05-25 10:24:24 +0000

    Don't warn when read or write block at cycle boundary, this may happen when remote devices are used and is not an error.

--- usr.bin/sndiod/siofile.c
+++ usr.bin/sndiod/siofile.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siofile.c,v 1.11 2016/01/08 16:17:31 ratchov Exp $	*/
+/*	$OpenBSD: siofile.c,v 1.12 2016/05/25 10:24:24 ratchov Exp $	*/
 /*
  * Copyright (c) 2008-2012 Alexandre Ratchov <alex@caoua.org>
  *
@@ -354,10 +354,6 @@ dev_sio_run(void *arg)
 			n = sio_read(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (n == 0 && data == base && !sio_eof(d->sio.hdl)) {
-				dev_log(d);
-				log_puts(": read blocked at cycle start\n");
-			}
 			if (log_level >= 4) {
 				dev_log(d);
 				log_puts(": read ");
@@ -437,10 +433,6 @@ dev_sio_run(void *arg)
 			n = sio_write(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (n == 0 && data == base && !sio_eof(d->sio.hdl)) {
-				dev_log(d);
-				log_puts(": write blocked at cycle start\n");
-			}
 			if (log_level >= 4) {
 				dev_log(d);
 				log_puts(": wrote ");
