05f61546870bcaab7deb825cb81375bbf9ef3872
ratchov <ratchov@openbsd.org>
2016-06-02 05:29:35 +0000

    remove forgotten debug printfs

--- usr.bin/aucat/aucat.c
+++ usr.bin/aucat/aucat.c
@@ -821,10 +821,6 @@ dev_mmcloc(int hr, int min, int sec, int fr, int cent, int fps)
 		log_puts(" at ");
 		log_putu(fps);
 		log_puts("fps\n");
-
-		log_puts("pos: ");
-		log_putu(pos);
-		log_puts("\n");
 	}
 	if (dev_pstate == DEV_START) {
 		dev_mmcstop();
