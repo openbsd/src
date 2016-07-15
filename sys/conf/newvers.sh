896deeddbdbfdd1b4dce1bb371efe4b37cb7f34e
deraadt <deraadt@openbsd.org>
2016-07-15 05:06:24 +0000

    take us out of -beta

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.145 2016/05/11 18:01:33 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.146 2016/07/15 05:06:24 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -66,10 +66,10 @@ ost="OpenBSD"
 osr="6.0"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
-#define STATUS "-current"		/* just after a release */
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
+#define STATUS "-current"		/* just after a release */
 #endif
 
 const char ostype[] = "${ost}";
