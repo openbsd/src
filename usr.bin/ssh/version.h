801b28b82d1bb765557de634ad0a71e60a736a14
djm <djm@openbsd.org>
2016-07-24 11:45:36 +0000

    openssh-7.3

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.76 2016/02/23 09:14:34 djm Exp $ */
+/* $OpenBSD: version.h,v 1.77 2016/07/24 11:45:36 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_7.2"
+#define SSH_VERSION	"OpenSSH_7.3"
