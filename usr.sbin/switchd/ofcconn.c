cec48c93a6ebf747f03c9295e6ee862f0cd4209a
yasuoka <yasuoka@openbsd.org>
2016-08-08 08:22:29 +0000

    Fix previous.  Obvously the condition is reversed.

--- usr.sbin/switchd/ofcconn.c
+++ usr.sbin/switchd/ofcconn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ofcconn.c,v 1.7 2016/08/08 07:24:27 yasuoka Exp $	*/
+/*	$OpenBSD: ofcconn.c,v 1.8 2016/08/08 08:22:29 yasuoka Exp $	*/
 
 /*
  * Copyright (c) 2016 YASUOKA Masahiko <yasuoka@openbsd.org>
@@ -214,7 +214,7 @@ ofsw_close(struct ofsw *os)
 void
 ofsw_free(struct ofsw *os)
 {
-	if (os != NULL)
+	if (os == NULL)
 		return;
 
 	TAILQ_REMOVE(&ofsw_list, os, os_next);
