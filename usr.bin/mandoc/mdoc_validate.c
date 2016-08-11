611d9138e480c02845497603ebf714305c67df68
schwarze <schwarze@openbsd.org>
2016-08-11 11:39:19 +0000

    oops, fix stupid typo in previous

--- usr.bin/mandoc/mdoc_validate.c
+++ usr.bin/mandoc/mdoc_validate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mdoc_validate.c,v 1.222 2016/08/11 10:46:27 schwarze Exp $ */
+/*	$OpenBSD: mdoc_validate.c,v 1.223 2016/08/11 11:39:19 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2016 Ingo Schwarze <schwarze@openbsd.org>
@@ -878,7 +878,8 @@ post_display(POST_ARGS)
 	switch (n->type) {
 	case ROFFT_BODY:
 		if (n->end != ENDBODY_NOT) {
-			if (n->tok == MDOC_Bd && n->parent->args == NULL)
+			if (n->tok == MDOC_Bd &&
+			    n->body->parent->args == NULL)
 				roff_node_delete(mdoc, n);
 		} else if (n->child == NULL)
 			mandoc_msg(MANDOCERR_BLK_EMPTY, mdoc->parse,
