efffed83f916d9fed9a4e890dbc8b3c0aa756246
guenther <guenther@openbsd.org>
2016-08-26 09:10:11 +0000

    Pull in <time.h> for time()
    ok deraadt@

--- usr.bin/systat/cache.c
+++ usr.bin/systat/cache.c
@@ -1,4 +1,4 @@
-/* $Id: cache.c,v 1.6 2016/01/16 03:30:26 canacar Exp $ */
+/* $Id: cache.c,v 1.7 2016/08/26 09:10:11 guenther Exp $ */
 /*
  * Copyright (c) 2001, 2007 Can Erkin Acar <canacar@openbsd.org>
  *
@@ -32,6 +32,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <assert.h>
 
