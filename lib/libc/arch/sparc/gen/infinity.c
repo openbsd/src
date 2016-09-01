3d1107806c4789012a8da1e6d187128842366f16
tedu <tedu@openbsd.org>
2016-09-01 09:26:04 +0000

    retire the sparc library code

--- lib/libc/arch/sparc/gen/infinity.c
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD: infinity.c,v 1.3 2005/08/07 16:40:15 espie Exp $ */
-/* infinity.c */
-
-#include <math.h>
-
-/* bytes for +Infinity on a sparc */
-char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
