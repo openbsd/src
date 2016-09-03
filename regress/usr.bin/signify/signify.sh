6f4af37c4e034283a47a5bfb7fec08fc2535cd9b
espie <espie@openbsd.org>
2016-09-03 12:23:02 +0000

    check -z mode, just by creating an archive and checking it's still proper gzip.

--- regress/usr.bin/signify/signify.sh
+++ regress/usr.bin/signify/signify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: signify.sh,v 1.7 2014/03/17 15:18:55 tedu Exp $
+# $OpenBSD: signify.sh,v 1.8 2016/09/03 12:23:02 espie Exp $
 
 srcdir=$1
 
@@ -28,4 +28,10 @@ signify -S -e -s $seckey -m HASH
 rm HASH
 signify -C -q -p $pubkey -x HASH.sig
 
+tar zcPf archive.tgz $srcdir 
+signify -zS -s $seckey -m archive.tgz -x signed.tgz
+# check it's still valid gzip
+gunzip -t signed.tgz
+# verify it
+signify -zV -p $pubkey <signed.tgz|gunzip -t
 true
