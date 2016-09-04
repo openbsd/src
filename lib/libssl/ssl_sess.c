077818223fb36c7a470097145ea6e82bc24bf7de
jsing <jsing@openbsd.org>
2016-09-04 16:11:47 +0000

    Expand IMPLEMENT_PEM macros.
    No change in generated assembly.

--- lib/libssl/ssl_sess.c
+++ lib/libssl/ssl_sess.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_sess.c,v 1.49 2016/03/11 07:08:45 mmcc Exp $ */
+/* $OpenBSD: ssl_sess.c,v 1.50 2016/09/04 16:11:47 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1096,4 +1096,31 @@ SSL_CTX_set_cookie_verify_cb(SSL_CTX *ctx,
 	ctx->app_verify_cookie_cb = cb;
 }
 
-IMPLEMENT_PEM_rw(SSL_SESSION, SSL_SESSION, PEM_STRING_SSL_SESSION, SSL_SESSION)
+
+SSL_SESSION *
+PEM_read_bio_SSL_SESSION(BIO *bp, SSL_SESSION **x, pem_password_cb *cb, void *u)
+{
+	return PEM_ASN1_read_bio((d2i_of_void *)d2i_SSL_SESSION, PEM_STRING_SSL_SESSION, bp,
+	    (void **)x, cb, u);
+}
+
+SSL_SESSION *
+PEM_read_SSL_SESSION(FILE *fp, SSL_SESSION **x, pem_password_cb *cb, void *u)
+{
+	return PEM_ASN1_read((d2i_of_void *)d2i_SSL_SESSION, PEM_STRING_SSL_SESSION, fp,
+	    (void **)x, cb, u);
+}
+
+int
+PEM_write_bio_SSL_SESSION(BIO *bp, SSL_SESSION *x)
+{
+	return PEM_ASN1_write_bio((i2d_of_void *)i2d_SSL_SESSION, PEM_STRING_SSL_SESSION, bp,
+	    x, NULL, NULL, 0, NULL, NULL);
+}
+
+int
+PEM_write_SSL_SESSION(FILE *fp, SSL_SESSION *x)
+{
+	return PEM_ASN1_write((i2d_of_void *)i2d_SSL_SESSION, PEM_STRING_SSL_SESSION, fp,
+	    x, NULL, NULL, 0, NULL, NULL);
+}
