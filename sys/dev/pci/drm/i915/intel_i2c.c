bc6b9cfeb12c7b6ab02ff8f1afcc28b1597ddad3
kettenis <kettenis@openbsd.org>
2016-09-24 17:03:37 +0000

    Assert that bus->force_bit >= 0 instead of force_bit.  Found by clang.

--- sys/dev/pci/drm/i915/intel_i2c.c
+++ sys/dev/pci/drm/i915/intel_i2c.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intel_i2c.c,v 1.8 2016/04/16 17:39:44 kettenis Exp $	*/
+/*	$OpenBSD: intel_i2c.c,v 1.9 2016/09/24 17:03:37 kettenis Exp $	*/
 /*
  * Copyright (c) 2012, 2013 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -436,7 +436,7 @@ intel_gmbus_force_bit(struct i2c_controller *controller, bool force_bit)
 	else
 		bus->force_bit--;
 
-	KASSERT(force_bit >= 0);
+	KASSERT(bus->force_bit >= 0);
 
 	if (bus->force_bit) {
 		bus->controller.ic_exec = NULL;
