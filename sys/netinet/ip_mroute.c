daeec585f35e6e3f0399a92048d0a543b5d8a654
tedu <tedu@openbsd.org>
2016-09-24 18:39:48 +0000

    use hashfree. from Mathieu - ok guenther

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_mroute.c,v 1.90 2016/03/07 18:44:00 naddy Exp $	*/
+/*	$OpenBSD: ip_mroute.c,v 1.91 2016/09/24 18:39:48 tedu Exp $	*/
 /*	$NetBSD: ip_mroute.c,v 1.85 2004/04/26 01:31:57 matt Exp $	*/
 
 /*
@@ -611,7 +611,7 @@ ip_mrouter_done(void)
 	}
 
 	memset(nexpire, 0, sizeof(nexpire));
-	free(mfchashtbl, M_MRTABLE, 0);
+	hashfree(mfchashtbl, MFCTBLSIZ, M_MRTABLE);
 	mfchashtbl = NULL;
 
 	ip_mrouter = NULL;
