024da9c32955294b66339888c19cb16013e41985
nicm <nicm@openbsd.org>
2016-09-29 08:50:43 +0000

    Check padding when writing any character with width > 1, in case they overlap after the first character (for example with cells xy and ab, y is replacing a).

--- usr.bin/tmux/screen-write.c
+++ usr.bin/tmux/screen-write.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: screen-write.c,v 1.93 2016/09/02 20:57:20 nicm Exp $ */
+/* $OpenBSD: screen-write.c,v 1.94 2016/09/29 08:50:43 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1267,10 +1267,10 @@ screen_write_overwrite(struct screen_write_ctx *ctx, struct grid_cell *gc,
 	}
 
 	/*
-	 * Overwrite any padding cells that belong to a UTF-8 character
-	 * we'll be overwriting with the current character.
+	 * Overwrite any padding cells that belong to any UTF-8 characters we'll be
+	 * overwriting with the current character.
 	 */
-	if (gc->data.width != 1 || gc->flags & GRID_FLAG_PADDING) {
+	if (width != 1 || gc->data.width != 1 || gc->flags & GRID_FLAG_PADDING) {
 		xx = s->cx + width - 1;
 		while (++xx < screen_size_x(s)) {
 			grid_view_get_cell(gd, xx, s->cy, &tmp_gc);
