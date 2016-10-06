c3c02032166e1a14490da727473718651f7ab1a5
jsg <jsg@openbsd.org>
2016-10-06 04:39:30 +0000

    In pledge_namei_wlpath() if resolvpath() errors out early it will not set variables that will be later used as the size argument to free(NULL calls.  This should be harmless as free returns early if the address is NULL without checking the size.  Initialise these variables before the call to ensure they are never passed to another function uninitialised.
    ok tedu@ millert@ deraadt@

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.184 2016/10/05 05:22:02 guenther Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.185 2016/10/06 04:39:30 jsg Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -752,7 +752,7 @@ pledge_namei_wlpath(struct proc *p, struct nameidata *ni)
 {
 	struct whitepaths *wl = p->p_p->ps_pledgepaths;
 	char *rdir = NULL, *cwd = NULL, *resolved = NULL;
-	size_t rdirlen, cwdlen, resolvedlen;
+	size_t rdirlen = 0, cwdlen = 0, resolvedlen = 0;
 	int i, error, pardir_found;
 
 	/*
