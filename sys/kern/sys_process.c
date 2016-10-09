6c0fe18513b8ea36a39875b869508a3f01be57cd
guenther <guenther@openbsd.org>
2016-10-09 22:46:48 +0000

    With systrace and procfs gone, process_checkioperm() and process_domem() are for option PTRACE only
    ok kettenis@

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_process.c,v 1.70 2016/09/01 12:47:18 akfaew Exp $	*/
+/*	$OpenBSD: sys_process.c,v 1.71 2016/10/09 22:46:48 guenther Exp $	*/
 /*	$NetBSD: sys_process.c,v 1.55 1996/05/15 06:17:47 tls Exp $	*/
 
 /*-
@@ -669,7 +669,6 @@ sys_ptrace(struct proc *p, void *v, register_t *retval)
 #endif
 	return 0;
 }
-#endif	/* PTRACE */
 
 /*
  * Check if a process is allowed to fiddle with the memory of another.
@@ -741,7 +740,6 @@ process_domem(struct proc *curp, struct proc *p, struct uio *uio, int req)
 	return (error);
 }
 
-#ifdef PTRACE
 int
 process_auxv_offset(struct proc *curp, struct proc *p, struct uio *uiop)
 {
