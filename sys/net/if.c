90ccfa08ea0673701e0e34b5610c8a8a2f7e3b08
bluhm <bluhm@openbsd.org>
2016-10-16 21:45:17 +0000

    m_resethdr() clears information attached to a mbuf that has been accumulated during processing, especially pf(4) data is removed. When a packet is reinserted to local input processing, we have to start from scratch with a clean mbuf header.  Also the packet has to be in the routing doamin of the interface where it is inserted. Incoming packet with stale inp pointer seen by patrick@ on lo(4); OK sashan@ mpi@

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.454 2016/10/09 20:05:10 claudio Exp $	*/
+/*	$OpenBSD: if.c,v 1.455 2016/10/16 21:45:17 bluhm Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -665,7 +665,9 @@ if_input_local(struct ifnet *ifp, struct mbuf *m, sa_family_t af)
 			bpf_mtap_af(if_bpf, af, m, BPF_DIRECTION_OUT);
 	}
 #endif
+	m_resethdr(m);
 	m->m_pkthdr.ph_ifidx = ifp->if_index;
+	m->m_pkthdr.ph_rtableid = ifp->if_rdomain;
 
 	ifp->if_opackets++;
 	ifp->if_obytes += m->m_pkthdr.len;
