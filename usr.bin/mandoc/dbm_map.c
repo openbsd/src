71a346d715832fd6ccb93514ce4104745226b3f7
schwarze <schwarze@openbsd.org>
2016-10-20 16:12:14 +0000

    st_size is off_t, which is signed, so use %lld rather than %llu; noticed by Ed Maste when compiling on FreeBSD

--- usr.bin/mandoc/dbm_map.c
+++ usr.bin/mandoc/dbm_map.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dbm_map.c,v 1.3 2016/08/30 21:36:54 schwarze Exp $ */
+/*	$OpenBSD: dbm_map.c,v 1.4 2016/10/20 16:12:14 schwarze Exp $ */
 /*
  * Copyright (c) 2016 Ingo Schwarze <schwarze@openbsd.org>
  *
@@ -90,7 +90,7 @@ dbm_map(const char *fname)
 	}
 	max_offset = be32toh(*dbm_getint(3)) + sizeof(int32_t);
 	if (st.st_size != max_offset) {
-		warnx("dbm_map(%s): Inconsistent file size %llu (expected %d)",
+		warnx("dbm_map(%s): Inconsistent file size %lld (expected %d)",
 		    fname, st.st_size, max_offset);
 		errno = EFTYPE;
 		goto fail;
