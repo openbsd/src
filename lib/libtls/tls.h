62518615be3e10eb1a6af0978720aad556a6a116
jsing <jsing@openbsd.org>
2016-11-11 14:03:33 +0000

    Bump TLS_API due to recent feature additions and changes.

--- lib/libtls/tls.h
+++ lib/libtls/tls.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls.h,v 1.42 2016/11/11 14:02:24 jsing Exp $ */
+/* $OpenBSD: tls.h,v 1.43 2016/11/11 14:03:33 jsing Exp $ */
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
@@ -27,7 +27,7 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-#define TLS_API	20160904
+#define TLS_API	20161111
 
 #define TLS_PROTOCOL_TLSv1_0	(1 << 1)
 #define TLS_PROTOCOL_TLSv1_1	(1 << 2)
