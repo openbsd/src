8a9d3c5f757a8b903881552b03625ce06b85c33c
mpi <mpi@openbsd.org>
2016-11-14 09:21:41 +0000

    Fix build now that ftd.h move to a MI directory.

--- sys/arch/socppc/stand/boot/fdt.c
+++ sys/arch/socppc/stand/boot/fdt.c
@@ -1,4 +1,4 @@
-/*     $OpenBSD: fdt.c,v 1.5 2015/12/14 03:25:59 mmcc Exp $       */
+/*     $OpenBSD: fdt.c,v 1.6 2016/11/14 09:21:41 mpi Exp $       */
 
 /*
  * Copyright (c) 2009 Dariusz Swiderski <sfires@sfires.net>
@@ -19,8 +19,8 @@
 
 #include <sys/types.h>
 #include <sys/param.h>
-#include <machine/fdt.h>
 
+#include "fdt.h"
 #include "libsa.h"
 
 
