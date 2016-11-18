740d69f48eecef314e6083a7183d19040fe044f6
visa <visa@openbsd.org>
2016-11-18 15:38:14 +0000

    Use the ErrorEPC register for curcpu().

--- sys/arch/loongson/include/asm.h
+++ sys/arch/loongson/include/asm.h
@@ -1,3 +1,9 @@
-/* $OpenBSD: asm.h,v 1.1.1.1 2009/07/31 09:26:25 miod Exp $ */
+/* $OpenBSD: asm.h,v 1.2 2016/11/18 15:38:14 visa Exp $ */
 /* public domain */
+
+#ifdef MULTIPROCESSOR
+#define HW_GET_CPU_INFO(ci, tmp)	\
+	dmfc0	ci, COP_0_ERROR_PC
+#endif
+
 #include <mips64/asm.h>
