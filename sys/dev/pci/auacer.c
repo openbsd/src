cbc520ef11e0a0d42df241899e53d37bf76477a9
ratchov <ratchov@openbsd.org>
2016-12-12 06:43:01 +0000

    Simplify auacer_activate(). From Michael W. Bombardieri <mb at ii.net>

--- sys/dev/pci/auacer.c
+++ sys/dev/pci/auacer.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auacer.c,v 1.20 2016/09/19 06:46:44 ratchov Exp $	*/
+/*	$OpenBSD: auacer.c,v 1.21 2016/12/12 06:43:01 ratchov Exp $	*/
 /*	$NetBSD: auacer.c,v 1.3 2004/11/10 04:20:26 kent Exp $	*/
 
 /*-
@@ -899,16 +899,8 @@ int
 auacer_activate(struct device *self, int act)
 {
 	struct auacer_softc *sc = (struct auacer_softc *)self;
-	int rv = 0;
 
-	switch (act) {
-	case DVACT_RESUME:
+	if (act == DVACT_RESUME)
 		ac97_resume(&sc->host_if, sc->codec_if);
-		rv = config_activate_children(self, act);
-		break;
-	default:
-		rv = config_activate_children(self, act);
-		break;
-	}
-	return (rv);
+	return (config_activate_children(self, act));
 }
