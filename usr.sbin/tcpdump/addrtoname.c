f80901e9d7fa101a900faf44981fb79c10c6ba20
jca <jca@openbsd.org>
2016-12-14 19:12:16 +0000

    Sync extern decl of eproto_db with what's in libpcap.
    For some reason this crashes on armv7.  Issue reported by Lars Nooden.

--- usr.sbin/tcpdump/addrtoname.c
+++ usr.sbin/tcpdump/addrtoname.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: addrtoname.c,v 1.36 2015/11/18 15:36:20 mmcc Exp $	*/
+/*	$OpenBSD: addrtoname.c,v 1.37 2016/12/14 19:12:16 jca Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -693,11 +693,11 @@ init_ipprotoarray(void)
 		}
 }
 
-/*XXX from libbpfc.a */
-extern struct eproto {
+/* XXX from libpcap */
+extern const struct eproto {
 	char *s;
 	u_short p;
-} eproto_db[];
+} * const eproto_db;
 
 static void
 init_eprotoarray(void)
