4477e5dc5a003c00e5ebeb81168de607d53b00eb
patrick <patrick@openbsd.org>
2016-12-18 22:19:36 +0000

    Consolidate bus space tags into generic bus space code.  Especially considering we don't have a separate iobus and the a4x iobus wasn't even compiled it does not make sense to declare those in iobus. Improves code readability as well.

--- sys/arch/arm64/dev/arm64_bus_space.c
+++ sys/arch/arm64/dev/arm64_bus_space.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: arm64_bus_space.c,v 1.1 2016/12/17 23:38:33 patrick Exp $ */
+/*	$OpenBSD: arm64_bus_space.c,v 1.2 2016/12/18 22:19:36 patrick Exp $ */
 
 /*
  * Copyright (c) 2001-2003 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -34,6 +34,53 @@
 #include <sys/systm.h>
 
 #include <machine/bus.h>
+#include <uvm/uvm_extern.h>
+
+bus_space_t arm64_bs_tag = {
+	.bus_base = 0ULL, // XXX
+	.bus_private = NULL,
+	._space_read_1 =	generic_space_read_1,
+	._space_write_1 =	generic_space_write_1,
+	._space_read_2 =	generic_space_read_2,
+	._space_write_2 =	generic_space_write_2,
+	._space_read_4 =	generic_space_read_4,
+	._space_write_4 =	generic_space_write_4,
+	._space_read_8 =	generic_space_read_8,
+	._space_write_8 =	generic_space_write_8,
+	._space_read_raw_2 =	generic_space_read_raw_2,
+	._space_write_raw_2 =	generic_space_write_raw_2,
+	._space_read_raw_4 =	generic_space_read_raw_4,
+	._space_write_raw_4 =	generic_space_write_raw_4,
+	._space_read_raw_8 =	generic_space_read_raw_8,
+	._space_write_raw_8 =	generic_space_write_raw_8,
+	._space_map =		generic_space_map,
+	._space_unmap =		generic_space_unmap,
+	._space_subregion =	generic_space_region,
+	._space_vaddr =		generic_space_vaddr
+};
+
+bus_space_t arm64_a4x_bs_tag = {
+	.bus_base = 0ULL, // XXX
+	.bus_private = NULL,
+	._space_read_1 =	a4x_space_read_1,
+	._space_write_1 =	a4x_space_write_1,
+	._space_read_2 =	a4x_space_read_2,
+	._space_write_2 =	a4x_space_write_2,
+	._space_read_4 =	a4x_space_read_4,
+	._space_write_4 =	a4x_space_write_4,
+	._space_read_8 =	a4x_space_read_8,
+	._space_write_8 =	a4x_space_write_8,
+	._space_read_raw_2 =	a4x_space_read_raw_2,
+	._space_write_raw_2 =	a4x_space_write_raw_2,
+	._space_read_raw_4 =	a4x_space_read_raw_4,
+	._space_write_raw_4 =	a4x_space_write_raw_4,
+	._space_read_raw_8 =	a4x_space_read_raw_8,
+	._space_write_raw_8 =	a4x_space_write_raw_8,
+	._space_map =		generic_space_map,
+	._space_unmap =		generic_space_unmap,
+	._space_subregion =	generic_space_region,
+	._space_vaddr =		generic_space_vaddr
+};
 
 uint8_t
 generic_space_read_1(bus_space_tag_t t, bus_space_handle_t h, bus_size_t o)
@@ -163,13 +210,39 @@ int
 generic_space_map(bus_space_tag_t t, bus_addr_t offs, bus_size_t size,
     int flags, bus_space_handle_t *bshp)
 {
-	*bshp = t->bus_base + offs;
-	return 0;
+	u_long startpa, endpa, pa;
+	vaddr_t va;
+	int cache = flags & BUS_SPACE_MAP_CACHEABLE ?
+	    PMAP_CACHE_WB : PMAP_CACHE_CI;
+
+	startpa = trunc_page(offs);
+	endpa = round_page(offs + size);
+
+	va = (vaddr_t)km_alloc(endpa - startpa, &kv_any, &kp_none, &kd_nowait);
+	if (! va)
+		return(ENOMEM);
+
+	*bshp = (bus_space_handle_t)(va + (offs - startpa));
+
+	for (pa = startpa; pa < endpa; pa += PAGE_SIZE, va += PAGE_SIZE) {
+		pmap_kenter_cache(va, pa, PROT_READ | PROT_WRITE, cache);
+	}
+	pmap_update(pmap_kernel());
+
+	return(0);
 }
 
 void
 generic_space_unmap(bus_space_tag_t t, bus_space_handle_t bsh, bus_size_t size)
 {
+	vaddr_t	va, endva;
+
+	va = trunc_page((vaddr_t)bsh);
+	endva = round_page((vaddr_t)bsh + size);
+
+	pmap_kremove(va, endva - va);
+	pmap_update(pmap_kernel());
+	km_free((void *)va, endva - va, &kv_any, &kp_none);
 }
 
 int
@@ -309,4 +382,3 @@ a4x_space_write_raw_8(bus_space_tag_t t, bus_space_handle_t h, bus_addr_t o,
 		buf += 8;
 	}
 }
-
