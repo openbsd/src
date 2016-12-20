298d98f11ad3925cf40f1eac6912f64e2f397b43
bluhm <bluhm@openbsd.org>
2016-12-20 12:07:14 +0000

    Put a write memory barrier into counters_enter().  This ensures that the generation number increment is written before the function returns and anything else is written. OK patrick@ mpi@ dlg@

--- sys/sys/percpu.h
+++ sys/sys/percpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: percpu.h,v 1.4 2016/11/14 03:26:31 dlg Exp $ */
+/*	$OpenBSD: percpu.h,v 1.5 2016/12/20 12:07:14 bluhm Exp $ */
 
 /*
  * Copyright (c) 2016 David Gwynne <dlg@openbsd.org>
@@ -122,6 +122,7 @@ counters_enter(struct counters_ref *ref, struct cpumem *cm)
 	ref->c = cpumem_enter(cm);
 #ifdef MULTIPROCESSOR
 	ref->g = ++(*ref->c); /* make the generation number odd */
+	membar_producer();
 	return (ref->c + 1);
 #else
 	return (ref->c);
