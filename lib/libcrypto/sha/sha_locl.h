b636a65c63275e4981d29a7f8a07b4a6c9936d61
patrick <patrick@openbsd.org>
2016-12-23 23:22:25 +0000

    Move __BEGIN_HIDDEN_DECLS out of the middle of a function declaration.
    ok jca@

--- lib/libcrypto/sha/sha_locl.h
+++ lib/libcrypto/sha/sha_locl.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: sha_locl.h,v 1.22 2016/12/21 15:49:29 jsing Exp $ */
+/* $OpenBSD: sha_locl.h,v 1.23 2016/12/23 23:22:25 patrick Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -85,11 +85,11 @@
 					  ix=(a)=ROTATE((a),1)	\
 					)
 
+__BEGIN_HIDDEN_DECLS
+
 #ifndef SHA1_ASM
 static
 #endif
-
-__BEGIN_HIDDEN_DECLS
 
 void sha1_block_data_order (SHA_CTX *c, const void *p,size_t num);
 
