449bb0f40da0fc5e2457fab9be08f289cee7dd6d
jsing <jsing@openbsd.org>
2016-12-30 16:28:53 +0000

    Expand ASN1_ITEM_rptr macros here as well... used with NETSCAPE_X509 of all things...

--- usr.bin/openssl/apps.c
+++ usr.bin/openssl/apps.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: apps.c,v 1.40 2016/09/04 09:00:14 guenther Exp $ */
+/* $OpenBSD: apps.c,v 1.41 2016/12/30 16:28:53 jsing Exp $ */
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
@@ -619,7 +619,7 @@ load_cert(BIO *err, const char *file, int format, const char *pass,
 		x = d2i_X509_bio(cert, NULL);
 	else if (format == FORMAT_NETSCAPE) {
 		NETSCAPE_X509 *nx;
-		nx = ASN1_item_d2i_bio(ASN1_ITEM_rptr(NETSCAPE_X509),
+		nx = ASN1_item_d2i_bio(&NETSCAPE_X509_it,
 		    cert, NULL);
 		if (nx == NULL)
 			goto end;
--- usr.bin/openssl/x509.c
+++ usr.bin/openssl/x509.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509.c,v 1.12 2015/10/17 15:00:11 doug Exp $ */
+/* $OpenBSD: x509.c,v 1.13 2016/12/30 16:28:53 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -893,7 +893,7 @@ bad:
 		nx.header = &hdr;
 		nx.cert = x;
 
-		i = ASN1_item_i2d_bio(ASN1_ITEM_rptr(NETSCAPE_X509), out, &nx);
+		i = ASN1_item_i2d_bio(&NETSCAPE_X509_it, out, &nx);
 	} else {
 		BIO_printf(bio_err, "bad output format specified for outfile\n");
 		goto end;
