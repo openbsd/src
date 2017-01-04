c531d0408286c90f6dbf0c501ca0de4ba127168d
millert <millert@openbsd.org>
2017-01-04 16:27:01 +0000

    Avoid an out of bounds read when the environment variable LESSBINFMT is set to "*".  Patch from Tobias Stoeckmann.  OK tb@

--- usr.bin/less/charset.c
+++ usr.bin/less/charset.c
@@ -37,7 +37,7 @@ checkfmt(const char *s)
 
 	if (*s == '*') {		/* skip leading attribute if there */
 		s++;
-		if (strchr("dksu", *s) == NULL) {
+		if (*s == '\0' || strchr("dksu", *s) == NULL) {
 			return (-1);
 		}
 		s++;
@@ -57,7 +57,8 @@ checkfmt(const char *s)
 		if (seen) {
 			return (-1);	/* 2nd % format item! */
 		}
-		while (strchr(" '+-0#", *s) != NULL) {	/* skip flags */
+		/* skip flags */
+		while (*s != '\0' && strchr(" '+-0#", *s) != NULL) {
 			s++;
 		}
 		while (isdigit(*s)) {			/* skip width */
@@ -78,7 +79,7 @@ checkfmt(const char *s)
 				s++;
 		}
 
-		if (strchr("cCdiouxX", *s) == NULL) {
+		if (*s == '\0' || strchr("cCdiouxX", *s) == NULL) {
 			/* bad or evil format character (%s, %n, etc.) */
 			return (-1);
 		}
