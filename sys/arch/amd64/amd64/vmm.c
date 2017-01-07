74a2a75afe45feb2993269d06ccad75db3eafa2e
mlarkin <mlarkin@openbsd.org>
2017-01-07 22:37:03 +0000

    use 16 bit segment selectors. also reduces differences with i386 vmm

--- sys/arch/amd64/amd64/vmm.c
+++ sys/arch/amd64/amd64/vmm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmm.c,v 1.99 2017/01/03 09:48:15 mlarkin Exp $	*/
+/*	$OpenBSD: vmm.c,v 1.100 2017/01/07 22:37:03 mlarkin Exp $	*/
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -1365,7 +1365,8 @@ vcpu_writeregs_vmx(struct vcpu *vcpu, uint64_t regmask, int loadvmcs,
     struct vcpu_reg_state *vrs)
 {
 	int i, ret = 0;
-	uint64_t sel, limit, ar;
+	uint16_t sel;
+	uint64_t limit, ar;
 	uint64_t *gprs = vrs->vrs_gprs;
 	uint64_t *crs = vrs->vrs_crs;
 	struct vcpu_segment_info *sregs = vrs->vrs_sregs;
