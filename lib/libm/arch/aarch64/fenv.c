3c6363cf8bb94e6527044bc52d6c77cd85f71439
patrick <patrick@openbsd.org>
2017-01-11 13:38:13 +0000

    Add support for AArch64.  Based on FreeBSD's msun header, modified to look and feel like our code.

---
+++ lib/libm/arch/aarch64/fenv.c
@@ -0,0 +1,284 @@
+/* $OpenBSD: fenv.c,v 1.1 2017/01/11 13:38:13 patrick Exp $ */
+/*-
+ * Copyright (c) 2004-2005 David Schultz <das@FreeBSD.ORG>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD: head/lib/msun/aarch64/fenv.h 280857 2015-03-30 16:42:08Z emaste $
+ */
+
+#include <fenv.h>
+#include <machine/ieeefp.h>
+
+/* We need to be able to map status flag positions to mask flag positions */
+#define	_FPUSW_SHIFT	8
+#define	_ENABLE_MASK	(FE_ALL_EXCEPT << _FPUSW_SHIFT)
+
+#define	__mrs_fpcr(r)	__asm __volatile("mrs %x0, fpcr" : "=r" (r))
+#define	__msr_fpcr(r)	__asm __volatile("msr fpcr, %x0" : : "r" (r))
+
+#define	__mrs_fpsr(r)	__asm __volatile("mrs %x0, fpsr" : "=r" (r))
+#define	__msr_fpsr(r)	__asm __volatile("msr fpsr, %x0" : : "r" (r))
+
+/*
+ * The following constant represents the default floating-point environment
+ * (that is, the one installed at program startup) and has type pointer to
+ * const-qualified fenv_t.
+ *
+ * It can be used as an argument to the functions within the <fenv.h> header
+ * that manage the floating-point environment, namely fesetenv() and
+ * feupdateenv().
+ */
+fenv_t __fe_dfl_env = 0;
+
+/*
+ * The feclearexcept() function clears the supported floating-point exceptions
+ * represented by `excepts'.
+ */
+int
+feclearexcept(int excepts)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	r &= ~excepts;
+	__msr_fpsr(r);
+	return (0);
+}
+DEF_STD(feclearexcept);
+
+/*
+ * The fegetexceptflag() function stores an implementation-defined
+ * representation of the states of the floating-point status flags indicated by
+ * the argument excepts in the object pointed to by the argument flagp.
+ */
+int
+fegetexceptflag(fexcept_t *flagp, int excepts)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	*flagp = r & excepts;
+	return (0);
+}
+DEF_STD(fegetexceptflag);
+
+/*
+ * The feraiseexcept() function raises the supported floating-point exceptions
+ * represented by the argument `excepts'.
+ */
+int
+feraiseexcept(int excepts)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	r |= excepts;
+	__msr_fpsr(r);
+	return (0);
+}
+DEF_STD(feraiseexcept);
+
+/*
+ * This function sets the floating-point status flags indicated by the argument
+ * `excepts' to the states stored in the object pointed to by `flagp'. It does
+ * NOT raise any floating-point exceptions, but only sets the state of the flags.
+ */
+int
+fesetexceptflag(const fexcept_t *flagp, int excepts)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	r &= ~excepts;
+	r |= *flagp & excepts;
+	__msr_fpsr(r);
+	return (0);
+}
+DEF_STD(fesetexceptflag);
+
+/*
+ * The fetestexcept() function determines which of a specified subset of the
+ * floating-point exception flags are currently set. The `excepts' argument
+ * specifies the floating-point status flags to be queried.
+ */
+int
+fetestexcept(int excepts)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	return (r & excepts);
+}
+DEF_STD(fetestexcept);
+
+/*
+ * The fegetround() function gets the current rounding direction.
+ */
+int
+fegetround(void)
+{
+	fenv_t r;
+
+	__mrs_fpcr(r);
+	return ((r >> _ROUND_SHIFT) & _ROUND_MASK);
+}
+DEF_STD(fegetround);
+
+/*
+ * The fesetround() function establishes the rounding direction represented by
+ * its argument `round'. If the argument is not equal to the value of a rounding
+ * direction macro, the rounding direction is not changed.
+ */
+int
+fesetround(int round)
+{
+	fenv_t r;
+
+	if (round & ~_ROUND_MASK)
+		return (-1);
+	__mrs_fpcr(r);
+	r &= ~(_ROUND_MASK << _ROUND_SHIFT);
+	r |= round << _ROUND_SHIFT;
+	__msr_fpcr(r);
+	return (0);
+}
+DEF_STD(fesetround);
+
+/*
+ * The fegetenv() function attempts to store the current floating-point
+ * environment in the object pointed to by envp.
+ */
+int
+fegetenv(fenv_t *envp)
+{
+	fenv_t r;
+
+	__mrs_fpcr(r);
+	*envp = r & _ENABLE_MASK;
+
+	__mrs_fpsr(r);
+	*envp |= r & (FE_ALL_EXCEPT | (_ROUND_MASK << _ROUND_SHIFT));
+
+	return (0);
+}
+DEF_STD(fegetenv);
+
+/*
+ * The feholdexcept() function saves the current floating-point environment
+ * in the object pointed to by envp, clears the floating-point status flags, and
+ * then installs a non-stop (continue on floating-point exceptions) mode, if
+ * available, for all floating-point exceptions.
+ */
+int
+feholdexcept(fenv_t *envp)
+{
+	fenv_t r;
+
+	__mrs_fpcr(r);
+	*envp = r & _ENABLE_MASK;
+	r &= ~(_ENABLE_MASK);
+	__msr_fpcr(r);
+
+	__mrs_fpsr(r);
+	*envp |= r & (FE_ALL_EXCEPT | (_ROUND_MASK << _ROUND_SHIFT));
+	r &= ~(_ENABLE_MASK);
+	__msr_fpsr(r);
+	return (0);
+}
+DEF_STD(feholdexcept);
+
+/*
+ * The fesetenv() function attempts to establish the floating-point environment
+ * represented by the object pointed to by envp. The argument `envp' points
+ * to an object set by a call to fegetenv() or feholdexcept(), or equal a
+ * floating-point environment macro. The fesetenv() function does not raise
+ * floating-point exceptions, but only installs the state of the floating-point
+ * status flags represented through its argument.
+ */
+int
+fesetenv(const fenv_t *envp)
+{
+
+	__msr_fpcr((*envp) & _ENABLE_MASK);
+	__msr_fpsr((*envp) & (FE_ALL_EXCEPT | (_ROUND_MASK << _ROUND_SHIFT)));
+	return (0);
+}
+DEF_STD(fesetenv);
+
+/*
+ * The feupdateenv() function saves the currently raised floating-point
+ * exceptions in its automatic storage, installs the floating-point environment
+ * represented by the object pointed to by `envp', and then raises the saved
+ * floating-point exceptions. The argument `envp' shall point to an object set
+ * by a call to feholdexcept() or fegetenv(), or equal a floating-point
+ * environment macro.
+ */
+int
+feupdateenv(const fenv_t *envp)
+{
+	fexcept_t r;
+
+	__mrs_fpsr(r);
+	fesetenv(envp);
+	feraiseexcept(r & FE_ALL_EXCEPT);
+	return (0);
+}
+DEF_STD(feupdateenv);
+
+/*
+ * The following functions are extentions to the standard
+ */
+int
+feenableexcept(int mask)
+{
+	fenv_t old_r, new_r;
+
+	__mrs_fpcr(old_r);
+	new_r = old_r | ((mask & FE_ALL_EXCEPT) << _FPUSW_SHIFT);
+	__msr_fpcr(new_r);
+	return ((old_r >> _FPUSW_SHIFT) & FE_ALL_EXCEPT);
+}
+DEF_STD(feenableexcept);
+
+int
+fedisableexcept(int mask)
+{
+	fenv_t old_r, new_r;
+
+	__mrs_fpcr(old_r);
+	new_r = old_r & ~((mask & FE_ALL_EXCEPT) << _FPUSW_SHIFT);
+	__msr_fpcr(new_r);
+	return ((old_r >> _FPUSW_SHIFT) & FE_ALL_EXCEPT);
+}
+DEF_STD(fedisableexcept);
+
+int
+fegetexcept(void)
+{
+	fenv_t r;
+
+	__mrs_fpcr(r);
+	return ((r & _ENABLE_MASK) >> _FPUSW_SHIFT);
+}
+DEF_STD(fegetexcept);
