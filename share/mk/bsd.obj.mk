7ba043aa1ce9515383a0058ea7033c788489d61d
tb <tb@openbsd.org>
2017-01-21 06:55:52 +0000

    Only do chown :wobj when this command will succeed. Allows using bsd.prog.mk outside /usr/src by users not belonging to group wobj.
    Requested by many after a report by Sebastien Marie.
    ok deraadt

--- share/mk/bsd.obj.mk
+++ share/mk/bsd.obj.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.obj.mk,v 1.16 2016/11/19 14:20:58 tb Exp $
+#	$OpenBSD: bsd.obj.mk,v 1.17 2017/01/21 06:55:52 tb Exp $
 #	$NetBSD: bsd.obj.mk,v 1.9 1996/04/10 21:08:05 thorpej Exp $
 
 .if !target(obj)
@@ -27,7 +27,7 @@ obj! _SUBDIRUSE
 			}; \
 			MKDIRS=_mkdirs; \
 		fi; \
-	elif [[ $$here == $$subdir ]]; then \
+	elif [[ `id` == *'('${WOBJGROUP}')'* && $$here == $$subdir ]]; then \
 		SETOWNER="chown :${WOBJGROUP}"; \
 	else \
 		SETOWNER=:; \
