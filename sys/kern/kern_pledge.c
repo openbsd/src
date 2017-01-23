970b68f44ed8e4e7b172a2be3a344d4d82793ee8
deraadt <deraadt@openbsd.org>
2017-01-23 03:17:55 +0000

    whitespace

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.189 2017/01/21 05:42:03 guenther Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.190 2017/01/23 03:17:55 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -1601,11 +1601,11 @@ canonpath(const char *input, char *buf, size_t bufsize)
 			*q++ = *p++;
 		}
 	}
-        if ((*p == '\0') && (q - buf < bufsize)) {
-                *q = 0;
-                return 0;
-        } else
-                return ENAMETOOLONG;
+	if ((*p == '\0') && (q - buf < bufsize)) {
+		*q = 0;
+		return 0;
+	} else
+		return ENAMETOOLONG;
 }
 
 int
