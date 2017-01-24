b49434870bad0bd5a63597df4624dd85a7c17f2f
tb <tb@openbsd.org>
2017-01-24 03:22:13 +0000

    Modernize and unify test idioms for the sake of readability and consistency. Done jointly with rpe.
    ok deraadt, phessler, rpe;
    also looks good to guenther despite a slight change in semantics

--- share/mk/bsd.obj.mk
+++ share/mk/bsd.obj.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.obj.mk,v 1.18 2017/01/24 02:56:50 tb Exp $
+#	$OpenBSD: bsd.obj.mk,v 1.19 2017/01/24 03:22:13 tb Exp $
 #	$NetBSD: bsd.obj.mk,v 1.9 1996/04/10 21:08:05 thorpej Exp $
 
 .if !target(obj)
@@ -33,20 +33,19 @@ obj! _SUBDIRUSE
 		SETOWNER=:; \
 	fi; \
 	[[ -z $$MKDIRS ]] && MKDIRS="mkdir -p"; \
-	if test $$here != $$subdir ; then \
+	if [[ $$here != $$subdir ]]; then \
 		dest=${BSDOBJDIR}/$$subdir ; \
 		echo "$$here/${__objdir} -> $$dest"; \
-		if test ! -L ${__objdir} -o \
-		    X`readlink ${__objdir}` != X$$dest; \
+		if [[ ! -L ${__objdir} || `readlink ${__objdir}` != $$dest ]]; \
 		    then \
-			if test -e ${__objdir}; then rm -rf ${__objdir}; fi; \
+			[[ -e ${__objdir} ]] && rm -rf ${__objdir}; \
 			ln -sf $$dest ${__objdir}; \
 			$$SETOWNER ${__objdir}; \
 		fi; \
-		if test -d ${BSDOBJDIR}; then \
-			test -d $$dest || $$MKDIRS $$dest; \
+		if [[ -d ${BSDOBJDIR} ]]; then \
+			[[ -d $$dest ]] || $$MKDIRS $$dest; \
 		else \
-			if test -e ${BSDOBJDIR}; then \
+			if [[ -e ${BSDOBJDIR} ]]; then \
 				echo "${BSDOBJDIR} is not a directory"; \
 			else \
 				echo "${BSDOBJDIR} does not exist"; \
@@ -54,7 +53,7 @@ obj! _SUBDIRUSE
 		fi; \
 	else \
 		dest=$$here/${__objdir} ; \
-		if test ! -d ${__objdir} ; then \
+		if [[ ! -d ${__objdir} ]]; then \
 			echo "making $$dest" ; \
 			$$MKDIRS $$dest; \
 			$$SETOWNER $$dest; \
