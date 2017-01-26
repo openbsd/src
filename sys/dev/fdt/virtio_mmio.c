304ad86a4b0d3de73f3e37374f5f6710dcef83ba
jsg <jsg@openbsd.org>
2017-01-26 01:46:19 +0000

    The only difference between armv7 and arm64 fdt virtio attachments is an uneeded include.  Remove the include and move to MI fdt directory.

--- sys/dev/fdt/virtio_mmio.c
+++ sys/dev/fdt/virtio_mmio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: virtio_mmio.c,v 1.6 2017/01/21 11:29:52 reyk Exp $	*/
+/*	$OpenBSD: virtio_mmio.c,v 1.1 2017/01/26 01:46:19 jsg Exp $	*/
 /*	$NetBSD: virtio.c,v 1.3 2011/11/02 23:05:52 njoly Exp $	*/
 
 /*
@@ -38,7 +38,6 @@
 #include <dev/pv/virtiovar.h>
 
 #include <machine/fdt.h>
-#include <armv7/armv7/armv7var.h>
 
 #include <dev/ofw/fdt.h>
 #include <dev/ofw/openfirm.h>
