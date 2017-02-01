dc0b7803a7748f727fd59deaa0ccd5b3fc6472d2
beck <beck@openbsd.org>
2017-02-01 18:54:37 +0000

    netinet/in.h should be included, and freebsd and some others don't have EAI_NODATA, so make this easier for people from bernard spill

--- usr.sbin/ocspcheck/http.c
+++ usr.sbin/ocspcheck/http.c
@@ -1,4 +1,4 @@
-/*	$Id: http.c,v 1.6 2017/01/25 13:31:01 benno Exp $ */
+/*	$Id: http.c,v 1.7 2017/02/01 18:54:37 beck Exp $ */
 /*
  * Copyright (c) 2016 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -15,9 +15,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/param.h>
-#include <arpa/inet.h>
 
 #include <ctype.h>
 #include <err.h>
--- usr.sbin/ocspcheck/ocspcheck.c
+++ usr.sbin/ocspcheck/ocspcheck.c
@@ -15,6 +15,7 @@
  */
 
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 
@@ -76,7 +77,9 @@ host_dns(const char *s, struct addr vec[MAX_SERVERS_DNS])
 	error = getaddrinfo(s, NULL, &hints, &res0);
 
 	if (error == EAI_AGAIN ||
+#ifdef EAI_NODATA
 	    error == EAI_NODATA ||
+#endif
 	    error == EAI_NONAME)
 		return(0);
 
