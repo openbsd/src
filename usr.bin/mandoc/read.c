610cce7facbf76c7f517a4e339dbbf002d568582
schwarze <schwarze@openbsd.org>
2017-02-03 17:55:36 +0000

    If an application parses multiple files with mparse_readfd(3) but without using mparse_open(3) to open the files, and if one of the files includes a gzip'ed file with .so, then the gzip flag remains set and the next main file will be expected to be gzip'ed. Fix this by clearing the gzip flag in mparse_reset(3).
    Bug found and patch provided by Michael <Stapelberg at debian dot org>.

--- usr.bin/mandoc/read.c
+++ usr.bin/mandoc/read.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: read.c,v 1.131 2017/01/28 23:26:56 schwarze Exp $ */
+/*	$OpenBSD: read.c,v 1.132 2017/02/03 17:55:36 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2017 Ingo Schwarze <schwarze@openbsd.org>
@@ -818,6 +818,7 @@ mparse_reset(struct mparse *curp)
 
 	free(curp->sodest);
 	curp->sodest = NULL;
+	curp->gzip = 0;
 }
 
 void
