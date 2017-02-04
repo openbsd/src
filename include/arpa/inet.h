02a6e1c4ea06ce742b98675a7f1bb66d52865030
guenther <guenther@openbsd.org>
2017-02-04 19:16:25 +0000

    The macro versions of htonl et al don't require them, but POSIX says <arpa/inet.h> needs to provide uint16_t and uint32_t.
    ok millert@ krw@ naddy@

--- include/arpa/inet.h
+++ include/arpa/inet.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: inet.h,v 1.20 2017/02/04 02:54:33 guenther Exp $	*/
+/*	$OpenBSD: inet.h,v 1.21 2017/02/04 19:16:25 guenther Exp $	*/
 
 /*
  * ++Copyright++ 1983, 1993
@@ -69,6 +69,16 @@
 #define htonl(x)	__htobe32(x)
 #define ntohs(x)	__htobe16(x)
 #define ntohl(x)	__htobe32(x)
+#endif
+
+#ifndef	_UINT16_T_DEFINED_
+#define	_UINT16_T_DEFINED_
+typedef	__uint16_t	uint16_t;
+#endif
+
+#ifndef	_UINT32_T_DEFINED_
+#define	_UINT32_T_DEFINED_
+typedef	__uint32_t	uint32_t;
 #endif
 
 #ifndef	_SOCKLEN_T_DEFINED_
