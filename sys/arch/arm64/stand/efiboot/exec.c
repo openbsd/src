4141bc49c37389e6f3ec8a65fcd26cc25a422b64
patrick <patrick@openbsd.org>
2017-02-05 13:11:06 +0000

    Since turning of the MMU in the beginning of the kernel removes the caches' contents, we lose data about the freshly copied and created kernel.  This leads to bootup failures.  Make sure to clean the caches to make sure that the data is in memory before we turn off the MMU.

--- sys/arch/arm64/stand/efiboot/exec.c
+++ sys/arch/arm64/stand/efiboot/exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.c,v 1.3 2017/02/03 13:06:20 patrick Exp $	*/
+/*	$OpenBSD: exec.c,v 1.4 2017/02/05 13:11:06 patrick Exp $	*/
 
 /*
  * Copyright (c) 2006, 2016 Mark Kettenis
@@ -27,10 +27,65 @@
 #include <efi.h>
 #include <stand/boot/cmd.h>
 
+#include <machine/armreg.h>
+
 #include "efiboot.h"
 #include "libsa.h"
+#include "fdt.h"
 
 typedef void (*startfuncp)(void *, void *, void *) __attribute__ ((noreturn));
+
+unsigned int cpu_get_dcache_line_size(void);
+void cpu_flush_dcache(vaddr_t, vsize_t);
+void cpu_inval_icache(void);
+
+unsigned int
+cpu_get_dcache_line_size(void)
+{
+	uint64_t ctr;
+	unsigned int dcl_size;
+
+	/* Accessible from all security levels */
+	ctr = READ_SPECIALREG(ctr_el0);
+
+	/*
+	 * Relevant field [19:16] is LOG2
+	 * of the number of words in DCache line
+	 */
+	dcl_size = CTR_DLINE_SIZE(ctr);
+
+	/* Size of word shifted by cache line size */
+	return (sizeof(int) << dcl_size);
+}
+
+void
+cpu_flush_dcache(vaddr_t addr, vsize_t len)
+{
+	uint64_t cl_size;
+	vaddr_t end;
+
+	cl_size = cpu_get_dcache_line_size();
+
+	/* Calculate end address to clean */
+	end = addr + len;
+	/* Align start address to cache line */
+	addr = addr & ~(cl_size - 1);
+
+	for (; addr < end; addr += cl_size)
+		__asm volatile("dc civac, %0" :: "r" (addr) : "memory");
+
+	/* Full system DSB */
+	__asm volatile("dsb sy" ::: "memory");
+}
+
+void
+cpu_inval_icache(void)
+{
+	__asm volatile(
+	    "ic		ialluis	\n"
+	    "dsb	ish	\n"
+	    : : : "memory");
+}
 
 void
 run_loadfile(u_long *marks, int howto)
@@ -80,6 +135,11 @@ run_loadfile(u_long *marks, int howto)
 	fdt = efi_makebootargs(args);
 
 	efi_cleanup();
+
+	cpu_flush_dcache(marks[MARK_ENTRY], marks[MARK_END] - marks[MARK_ENTRY]);
+	cpu_inval_icache();
+
+	cpu_flush_dcache((vaddr_t)fdt, fdt_get_size(fdt));
 
 	(*(startfuncp)(marks[MARK_ENTRY]))((void *)esym, 0, fdt);
 
