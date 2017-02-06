0bfdceb3e26ceb1cf36c61a3940f9131aa271641
dlg <dlg@openbsd.org>
2017-02-06 03:52:35 +0000

    add a license for the code in here, all of which i seem to have written.
    this has been pointed out to me by a couple of people now.

--- sys/arch/arm/include/endian.h
+++ sys/arch/arm/include/endian.h
@@ -1,4 +1,20 @@
-/*	$OpenBSD: endian.h,v 1.9 2017/01/06 00:46:49 jsg Exp $	*/
+/*	$OpenBSD: endian.h,v 1.10 2017/02/06 03:52:35 dlg Exp $	*/
+
+/*
+ * Copyright (c) 2015 David Gwynne <dlg@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #ifndef _ARM_ENDIAN_H_
 #define _ARM_ENDIAN_H_
