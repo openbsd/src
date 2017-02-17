8d8a71b4ddab57ba221d57ad2ef6c004c0460cd8
ajacoutot <ajacoutot@openbsd.org>
2017-02-17 16:46:17 +0000

    ypbind(8) is not started automatically at boot time by /etc/rc if domainname is set and the directory var/yp/binding exists.
    ok deraadt@

--- usr.sbin/ypbind/ypbind.c
+++ usr.sbin/ypbind/ypbind.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ypbind.c,v 1.68 2016/07/08 19:32:26 millert Exp $ */
+/*	$OpenBSD: ypbind.c,v 1.69 2017/02/17 16:46:17 ajacoutot Exp $ */
 
 /*
  * Copyright (c) 1992, 1993, 1996, 1997, 1998 Theo de Raadt <deraadt@openbsd.org>
@@ -375,9 +375,6 @@ main(int argc, char *argv[])
 		}
 		closedir(dirp);
 	} else {
-		printf("Enabling yp client subsystem.\n");
-		printf("To disable: kill ypbind and remove %s\n",
-		    BINDINGDIR);
 		(void)mkdir(BINDINGDIR, 0755);
 	}
 
