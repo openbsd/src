c23358d657e06a72e442b9bc98d361334b7c6ce6
reyk <reyk@openbsd.org>
2017-02-22 09:56:03 +0000

    The pcap people gave us ID 267 for DLT_OPENFLOW
    Via https://github.com/the-tcpdump-group/libpcap/issues/542
    
    OK sthen@ jsg@

--- sys/net/bpf.h
+++ sys/net/bpf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bpf.h,v 1.61 2016/11/17 15:41:14 sthen Exp $	*/
+/*	$OpenBSD: bpf.h,v 1.62 2017/02/22 09:56:03 reyk Exp $	*/
 /*	$NetBSD: bpf.h,v 1.15 1996/12/13 07:57:33 mikel Exp $	*/
 
 /*
@@ -202,7 +202,7 @@ struct bpf_hdr {
 #define DLT_USER14		161	/* Reserved for private use */
 #define DLT_USER15		162	/* Reserved for private use */
 #define DLT_MPLS		219	/* MPLS Provider Edge header */
-#define DLT_OPENFLOW		DLT_USER0
+#define DLT_OPENFLOW		267	/* in-kernel OpenFlow, by pcap */
 
 /*
  * The instruction encodings.
