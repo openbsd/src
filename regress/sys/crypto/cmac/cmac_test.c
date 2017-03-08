9be3be63a592c35ed46d93b842b18a3fd605929b
deraadt <deraadt@openbsd.org>
2017-03-08 19:33:12 +0000

    format string mishandling

--- regress/sys/crypto/cmac/cmac_test.c
+++ regress/sys/crypto/cmac/cmac_test.c
@@ -26,7 +26,7 @@ print_hex(char *str, unsigned char *buf, int len)
       int     i;
 
       for ( i=0; i<len; i++ ) {
-          if ( (i % 16) == 0 && i != 0 ) printf(str);
+          if ( (i % 16) == 0 && i != 0 ) printf("%s", str);
           printf("%02x", buf[i]);
           if ( (i % 4) == 3 ) printf(" ");
           if ( (i % 16) == 15 ) printf("\n");
