386a4b3ba93765dda1bad4b25395c4d3a975fb4d
kettenis <kettenis@openbsd.org>
2017-03-10 08:42:08 +0000

    Set VM_PHYSSEG_MAX to 32 and use VM_PSTRAT_BSEARCH as the sort/search option like we do on macppc and sparc64.  This makes our kernel recognize all the memory on the Odroid XU4.
    ok jsg@

--- sys/arch/armv7/include/vmparam.h
+++ sys/arch/armv7/include/vmparam.h
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: vmparam.h,v 1.5 2017/01/24 13:33:06 jca Exp $	*/
+/* 	$OpenBSD: vmparam.h,v 1.6 2017/03/10 08:42:08 kettenis Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.23 2003/05/22 05:47:07 thorpej Exp $	*/
 
 /*
@@ -83,8 +83,8 @@
  * max number of non-contig chunks of physical RAM you can have
  */
 
-#define	VM_PHYSSEG_MAX		2
-#define	VM_PHYSSEG_STRAT	VM_PSTRAT_RANDOM
+#define	VM_PHYSSEG_MAX		32
+#define	VM_PHYSSEG_STRAT	VM_PSTRAT_BSEARCH
 
 /*
  * this indicates that we can't add RAM to the VM system after the
