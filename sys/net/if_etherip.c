51b38662647aaeb901f04cfd1bb5abe26747eebc
jca <jca@openbsd.org>
2017-03-27 23:49:03 +0000

    Don't reject etherip packets if they are protected with IPsec.
    This aligns code with documentation & matches what was available before
    etherip(4) was split from gif(4).  sysctl net.inet.etherip.allow=1 is
    still needed to accept etherip packets not protected with IPsec.
    
    Reported by at least Jason Tubnor, ok mikeb@

--- sys/net/if_etherip.c
+++ sys/net/if_etherip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_etherip.c,v 1.15 2017/03/07 23:35:06 jca Exp $	*/
+/*	$OpenBSD: if_etherip.c,v 1.16 2017/03/27 23:49:03 jca Exp $	*/
 /*
  * Copyright (c) 2015 Kazuya GODA <goda@openbsd.org>
  *
@@ -423,7 +423,7 @@ ip_etherip_input(struct mbuf **mp, int *offp, int proto)
 		return IPPROTO_DONE;
 	}
 
-	if (!etherip_allow) {
+	if (!etherip_allow && (m->m_flags & (M_AUTH|M_CONF)) == 0) {
 		m_freem(m);
 		etheripstat.etherips_pdrops++;
 		return IPPROTO_DONE;
@@ -579,7 +579,7 @@ ip6_etherip_input(struct mbuf **mp, int *offp, int proto)
 	struct ifnet *ifp = NULL;
 
 
-	if (!etherip_allow) {
+	if (!etherip_allow && (m->m_flags & (M_AUTH|M_CONF)) == 0) {
 		m_freem(m);
 		etheripstat.etherips_pdrops++;
 		return IPPROTO_NONE;
