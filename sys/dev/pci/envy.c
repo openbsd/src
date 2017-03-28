94c25ab859f53d6c33c591e54a26dc5606424eaa
ratchov <ratchov@openbsd.org>
2017-03-28 05:23:15 +0000

    Make set_params() return the rate the device is using. Fixes a wrong rate being reported when a unsupported rate was requested.

--- sys/dev/pci/envy.c
+++ sys/dev/pci/envy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: envy.c,v 1.69 2017/02/10 08:05:14 ratchov Exp $	*/
+/*	$OpenBSD: envy.c,v 1.70 2017/03/28 05:23:15 ratchov Exp $	*/
 /*
  * Copyright (c) 2007 Alexandre Ratchov <alex@caoua.org>
  *
@@ -1852,6 +1852,7 @@ envy_set_params(void *self, int setmode, int usemode,
 	reg |= envy_rates[i].reg;
 	envy_mt_write_1(sc, ENVY_MT_RATE, reg);
 	if (setmode & AUMODE_PLAY) {
+		p->sample_rate = envy_rates[i].rate;
 		p->encoding = AUDIO_ENCODING_SLINEAR_LE;
 		p->precision = 24;
 		p->bps = 4;
@@ -1859,6 +1860,7 @@ envy_set_params(void *self, int setmode, int usemode,
 		p->channels = sc->isht ? sc->card->noch : ENVY_PCHANS;
 	}
 	if (setmode & AUMODE_RECORD) {
+		r->sample_rate = envy_rates[i].rate;
 		r->encoding = AUDIO_ENCODING_SLINEAR_LE;
 		r->precision = 24;
 		r->bps = 4;
