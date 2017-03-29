6fe788edf4c0030595f8bfc180a7fc9f70af12ce
deraadt <deraadt@openbsd.org>
2017-03-29 01:39:27 +0000

    move to 6.1 release, drop -beta tag

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.151 2017/03/04 16:52:47 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.152 2017/03/29 01:39:27 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -66,10 +66,10 @@ ost="OpenBSD"
 osr="6.1"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
-#define STATUS "-current"		/* just after a release */
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
+#define STATUS "-current"		/* just after a release */
 #endif
 
 const char ostype[] = "${ost}";
