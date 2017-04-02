4f54a3e538c0e2ddbd184e40a26f2dc39aae4562
visa <visa@openbsd.org>
2017-04-02 15:25:05 +0000

    Drop address conversion cruft.

--- sys/arch/octeon/dev/if_cnmac.c
+++ sys/arch/octeon/dev/if_cnmac.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_cnmac.c,v 1.62 2017/01/24 03:57:34 dlg Exp $	*/
+/*	$OpenBSD: if_cnmac.c,v 1.63 2017/04/02 15:25:05 visa Exp $	*/
 
 /*
  * Copyright (c) 2007 Internet Initiative Japan, Inc.
@@ -626,7 +626,7 @@ octeon_eth_buf_free_work(struct octeon_eth_softc *sc, uint64_t *work)
 	    PIP_WQE_WORD2_IP_BUFS_SHIFT;
 	word3 = work[3];
 	while (nbufs-- > 0) {
-		addr = word3 & PIP_WQE_WORD3_ADDR, CCA_CACHED;
+		addr = word3 & PIP_WQE_WORD3_ADDR;
 		back = (word3 & PIP_WQE_WORD3_BACK) >>
 		    PIP_WQE_WORD3_BACK_SHIFT;
 		pktbuf = (addr & ~(CACHE_LINE_SIZE - 1)) -
