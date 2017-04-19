da3dc5d4481b2306643992ad1d38c04dba1fcbb9
nicm <nicm@openbsd.org>
2017-04-19 16:59:54 +0000

    load_cfg returns < 0 on error, not != 0. Problem reported by Kaushal Modi.

--- usr.bin/tmux/cmd-source-file.c
+++ usr.bin/tmux/cmd-source-file.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-source-file.c,v 1.34 2017/02/14 18:13:05 nicm Exp $ */
+/* $OpenBSD: cmd-source-file.c,v 1.35 2017/04/19 16:59:54 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Tiago Cunha <me@tiagocunha.org>
@@ -79,7 +79,7 @@ cmd_source_file_exec(struct cmd *self, struct cmdq_item *item)
 	free(pattern);
 
 	for (i = 0; i < (u_int)g.gl_pathc; i++) {
-		if (load_cfg(g.gl_pathv[i], c, item, quiet) != 0)
+		if (load_cfg(g.gl_pathv[i], c, item, quiet) < 0)
 			retval = CMD_RETURN_ERROR;
 	}
 	if (cfg_finished) {
