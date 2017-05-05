d2daf580bc29092a7741bba30af64ada731e73c2
kettenis <kettenis@openbsd.org>
2017-05-05 20:13:54 +0000

    Move dwge(4) glue into dev/fdt.

--- sys/dev/fdt/if_dwge_fdt.c
+++ sys/dev/fdt/if_dwge_fdt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_dwge_fdt.c,v 1.3 2016/08/22 19:31:27 kettenis Exp $	*/
+/*	$OpenBSD: if_dwge_fdt.c,v 1.1 2017/05/05 20:13:54 kettenis Exp $	*/
 /*
  * Copyright (c) 2016 Patrick Wildt <patrick@blueri.se>
  * Copyright (c) 2016 Mark Kettenis <kettenis@openbsd.org>
