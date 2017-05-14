6b6657674e255ae4591a7103b9b01dde5e389351
schwarze <schwarze@openbsd.org>
2017-05-14 12:26:59 +0000

    Tweak previous: tb@ noticed that some browser/font combinations have so amazingly wide bold fonts (for the same nominal font size) that adding 15% to the column width still isn't sufficient to make text reliably fit, so go for 20%.

--- usr.bin/mandoc/html.c
+++ usr.bin/mandoc/html.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: html.c,v 1.81 2017/05/12 17:56:39 schwarze Exp $ */
+/*	$OpenBSD: html.c,v 1.82 2017/05/14 12:26:59 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2011, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011-2015, 2017 Ingo Schwarze <schwarze@openbsd.org>
@@ -613,7 +613,7 @@ print_otag(struct html *h, enum htmltag tag, const char *fmt, ...)
 			a2width(arg2, su);
 			if (*fmt == '+') {
 				/* Increase to make even bold text fit. */
-				su->scale *= 1.15;
+				su->scale *= 1.2;
 				/* Add padding. */
 				su->scale += 3.0;
 				fmt++;
