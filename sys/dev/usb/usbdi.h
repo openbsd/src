9d644903e1b08eb9cf074eaea2f656378d3f01dd
mpi <mpi@openbsd.org>
2017-05-15 11:05:51 +0000

    Use splraise() rather than splsoftnet() which is going away.

--- sys/dev/usb/usbdi.h
+++ sys/dev/usb/usbdi.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usbdi.h,v 1.68 2017/05/15 10:52:08 mpi Exp $ */
+/*	$OpenBSD: usbdi.h,v 1.69 2017/05/15 11:05:51 mpi Exp $ */
 /*	$NetBSD: usbdi.h,v 1.62 2002/07/11 21:14:35 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usbdi.h,v 1.18 1999/11/17 22:33:49 n_hibma Exp $	*/
 
@@ -250,10 +250,11 @@ struct usb_attach_arg {
 /* No match */
 #define UMATCH_NONE					 0
 
-#define splusb splsoftnet
-#define splhardusb splbio
-
 #define	IPL_USB		IPL_BIO
 #define	IPL_SOFTUSB	IPL_SOFTNET
+
+#define splusb()	splraise(IPL_SOFTUSB)
+#define splhardusb()	splraise(IPL_USB)
+
 
 #endif /* _USBDI_H_ */
