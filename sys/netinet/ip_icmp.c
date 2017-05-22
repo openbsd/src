b011ee520b1c3520b4eb68721df0a59e4880719d
bluhm <bluhm@openbsd.org>
2017-05-22 14:26:08 +0000

    Fix a mbuf leak when reflecting an ICMP packet with IP options. Free the options in icmp_input_if() after a successful call to icmp_reflect(). bug report and analysis by Hendrik Gerlach OK krw@ claudio@ phessler@

--- sys/netinet/ip_icmp.c
+++ sys/netinet/ip_icmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.c,v 1.167 2017/05/04 17:58:46 bluhm Exp $	*/
+/*	$OpenBSD: ip_icmp.c,v 1.168 2017/05/22 14:26:08 bluhm Exp $	*/
 /*	$NetBSD: ip_icmp.c,v 1.19 1996/02/13 23:42:22 christos Exp $	*/
 
 /*
@@ -591,8 +591,10 @@ reflect:
 
 		icmpstat_inc(icps_reflect);
 		icmpstat_inc(icps_outhist + icp->icmp_type);
-		if (!icmp_reflect(m, &opts, NULL))
+		if (!icmp_reflect(m, &opts, NULL)) {
 			icmp_send(m, opts);
+			m_free(opts);
+		}
 		return IPPROTO_DONE;
 
 	case ICMP_REDIRECT:
