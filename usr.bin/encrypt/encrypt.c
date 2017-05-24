e02741f9c39b4b7b8e134cc98e2700e9646a206e
mestre <mestre@openbsd.org>
2017-05-24 09:19:55 +0000

    Use freezero instead of explicit_bzero+free
    OK tb@

--- usr.bin/encrypt/encrypt.c
+++ usr.bin/encrypt/encrypt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: encrypt.c,v 1.46 2017/05/03 09:51:39 mestre Exp $	*/
+/*	$OpenBSD: encrypt.c,v 1.47 2017/05/24 09:19:55 mestre Exp $	*/
 
 /*
  * Copyright (c) 1996, Jason Downs.  All rights reserved.
@@ -169,8 +169,7 @@ main(int argc, char **argv)
 		(void)fputc('\n', stdout);
 
 		/* Wipe our copy, before we free it. */
-		explicit_bzero(string, strlen(string));
-		free(string);
+		freezero(string, strlen(string));
 	}
 	return 0;
 }
