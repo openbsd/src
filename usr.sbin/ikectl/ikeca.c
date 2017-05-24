2174bbc4d374c98290b0014ebcaedaa678f861bc
jsg <jsg@openbsd.org>
2017-05-24 04:55:57 +0000

    Set REQ_EXT in req section so ikectl ca certificate revoke will work again.

--- usr.sbin/ikectl/ikeca.c
+++ usr.sbin/ikectl/ikeca.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ikeca.c,v 1.43 2017/05/21 02:37:52 deraadt Exp $	*/
+/*	$OpenBSD: ikeca.c,v 1.44 2017/05/24 04:55:57 jsg Exp $	*/
 
 /*
  * Copyright (c) 2010 Jonathan Gray <jsg@openbsd.org>
@@ -900,6 +900,7 @@ ca_revoke(struct ca *ca, char *keyname)
 
 	ca_setenv("$ENV::CADB", ca->index);
 	ca_setenv("$ENV::CASERIAL", ca->serial);
+	ca_setenv("$ENV::REQ_EXT", "req");
 	ca_setcnf(ca, "ca-revoke");
 
 	if (keyname) {
