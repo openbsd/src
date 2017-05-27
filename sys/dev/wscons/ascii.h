d3118a24d5c7f2b2e39268129c55c4c063a10bcc
fcambus <fcambus@openbsd.org>
2017-05-27 12:00:28 +0000

    Add missing comments for CAN and SUB, for consistency.
    OK tb@

--- sys/dev/wscons/ascii.h
+++ sys/dev/wscons/ascii.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ascii.h,v 1.5 2009/01/12 20:43:53 miod Exp $ */
+/* $OpenBSD: ascii.h,v 1.6 2017/05/27 12:00:28 fcambus Exp $ */
 /* $NetBSD: ascii.h,v 1.3 1998/06/20 19:11:04 drochner Exp $ */
 
 #define ASCII_NUL	0x00		/* nul */
@@ -11,6 +11,6 @@
 #define ASCII_CR	0x0d		/* carriage return */
 #define ASCII_SO	0x0e		/* shift out */
 #define ASCII_SI	0x0f		/* shift in */
-#define	ASCII_CAN	0x18
-#define	ASCII_SUB	0x1a
+#define ASCII_CAN	0x18		/* cancel */
+#define ASCII_SUB	0x1a		/* substitute */
 #define ASCII_ESC	0x1b		/* escape */
