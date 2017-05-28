9606f8b798abce8ff5b4e66ec7b5d81e6220cffb
mpi <mpi@openbsd.org>
2017-05-28 15:03:53 +0000

    Add missing NET_UNLOCK() in error path.
    Spotted by sashan@

--- sys/net/if_trunk.c
+++ sys/net/if_trunk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_trunk.c,v 1.131 2017/05/28 14:26:38 mpi Exp $	*/
+/*	$OpenBSD: if_trunk.c,v 1.132 2017/05/28 15:03:53 mpi Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006, 2007 Reyk Floeter <reyk@openbsd.org>
@@ -218,8 +218,10 @@ trunk_clone_destroy(struct ifnet *ifp)
 	/* Shutdown and remove trunk ports, return on error */
 	NET_LOCK(s);
 	while ((tp = SLIST_FIRST(&tr->tr_ports)) != NULL) {
-		if ((error = trunk_port_destroy(tp)) != 0)
+		if ((error = trunk_port_destroy(tp)) != 0) {
+			NET_UNLOCK(s);
 			return (error);
+		}
 	}
 	NET_UNLOCK(s);
 
