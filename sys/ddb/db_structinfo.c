b47f84309291da48e092576d16de5db2e0f8afe6
bluhm <bluhm@openbsd.org>
2017-05-30 18:37:19 +0000

    Header sys/scanio.h has been deleted, do not include it here.

--- sys/ddb/db_structinfo.c
+++ sys/ddb/db_structinfo.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: db_structinfo.c,v 1.14 2016/05/21 02:11:02 guenther Exp $ */
+/* $OpenBSD: db_structinfo.c,v 1.15 2017/05/30 18:37:19 bluhm Exp $ */
 /* public domain */
 /*
  * This file is intended to be compiled with debug information,
@@ -102,7 +102,6 @@
 #include <sys/mtio.h>
 #include <sys/pciio.h>
 #include <sys/radioio.h>
-#include <sys/scanio.h>
 #include <sys/scsiio.h>
 #include <sys/sockio.h>
 #include <sys/videoio.h>
