9aecda2c521d54d6d8810fc14de953b00a0dac6f
mikeb <mikeb@openbsd.org>
2017-06-06 20:33:28 +0000

    Call xbf_intr for polled transfers that can't sleep

--- sys/dev/pv/xbf.c
+++ sys/dev/pv/xbf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: xbf.c,v 1.28 2017/03/19 16:37:19 mikeb Exp $	*/
+/*	$OpenBSD: xbf.c,v 1.29 2017/06/06 20:33:28 mikeb Exp $	*/
 
 /*
  * Copyright (c) 2016 Mike Belopuhov
@@ -688,6 +688,7 @@ xbf_poll_cmd(struct scsi_xfer *xs, int desc, int timo)
 			delay(10);
 		else
 			tsleep(xs, PRIBIO, "xbfpoll", 1);
+		xbf_intr(xs->sc_link->adapter_softc);
 	} while(--timo > 0);
 
 	return (0);
