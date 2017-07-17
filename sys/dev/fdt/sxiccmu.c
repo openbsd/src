5c793bcf6108718b5d7e474113007768503242f1
kettenis <kettenis@openbsd.org>
2017-07-17 19:28:35 +0000

    The Linux kernel device trees use "allwinner,sun50i-h5-ccu" as the compatible propert for the clock controller module.  Prepare for the day when the u-boot device trees get update.

--- sys/dev/fdt/sxiccmu.c
+++ sys/dev/fdt/sxiccmu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sxiccmu.c,v 1.3 2017/07/16 19:30:56 kettenis Exp $	*/
+/*	$OpenBSD: sxiccmu.c,v 1.4 2017/07/17 19:28:35 kettenis Exp $	*/
 /*
  * Copyright (c) 2007,2009 Dale Rahn <drahn@openbsd.org>
  * Copyright (c) 2013 Artturi Alm
@@ -103,15 +103,16 @@ sxiccmu_match(struct device *parent, void *match, void *aux)
 		return (OF_is_compatible(node, "allwinner,sun4i-a10") ||
 		    OF_is_compatible(node, "allwinner,sun5i-a10s") ||
 		    OF_is_compatible(node, "allwinner,sun5i-r8") ||
-		    OF_is_compatible(node, "allwinner,sun50i-a64") ||
-		    OF_is_compatible(node, "allwinner,sun50i-h5") ||
 		    OF_is_compatible(node, "allwinner,sun7i-a20") ||
 		    OF_is_compatible(node, "allwinner,sun8i-h3") ||
-		    OF_is_compatible(node, "allwinner,sun9i-a80"));
+		    OF_is_compatible(node, "allwinner,sun9i-a80") ||
+		    OF_is_compatible(node, "allwinner,sun50i-a64") ||
+		    OF_is_compatible(node, "allwinner,sun50i-h5"));
 	}
 
-	return (OF_is_compatible(faa->fa_node, "allwinner,sun50i-a64-ccu") ||
-	    OF_is_compatible(faa->fa_node, "allwinner,sun8i-h3-ccu"));
+	return (OF_is_compatible(faa->fa_node, "allwinner,sun8i-h3-ccu") ||
+	    OF_is_compatible(faa->fa_node, "allwinner,sun50i-a64-ccu") ||
+	    OF_is_compatible(faa->fa_node, "allwinner,sun50i-h5-ccu"));
 }
 
 void
@@ -137,7 +138,8 @@ sxiccmu_attach(struct device *parent, struct device *self, void *aux)
 		sc->sc_nresets = nitems(sun50i_a64_resets);
 		sc->sc_get_frequency = sxiccmu_a64_get_frequency;
 		sc->sc_set_frequency = sxiccmu_a64_set_frequency;
-	} else if (OF_is_compatible(sc->sc_node, "allwinner,sun8i-h3-ccu")) {
+	} else if (OF_is_compatible(sc->sc_node, "allwinner,sun8i-h3-ccu") ||
+	    OF_is_compatible(sc->sc_node, "allwinner,sun50i-h5-ccu") {
 		KASSERT(faa->fa_nreg > 0);
 		sc->sc_gates = sun8i_h3_gates;
 		sc->sc_ngates = nitems(sun8i_h3_gates);
