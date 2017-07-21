6f2e209c22cc59cbde0f9232b3a2d3af050c9fcc
krw <krw@openbsd.org>
2017-07-21 18:43:05 +0000

    whitespace botch.

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.472 2017/07/20 17:44:13 krw Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.473 2017/07/21 18:43:05 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -195,7 +195,7 @@ get_ifa_family(char *cp, int n)
 		if ((i & n) != 0) {
 			sa = (struct sockaddr *)cp;
 			if (i == RTA_IFA)
-				    return sa->sa_family;
+				return sa->sa_family;
 			ADVANCE(cp, sa);
 		}
 	}
