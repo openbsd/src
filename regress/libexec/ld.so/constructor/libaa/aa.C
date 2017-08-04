1baba19217f7d3398f70716c64a5526798c65fd1
kettenis <kettenis@openbsd.org>
2017-08-04 18:23:52 +0000

    Use volatile to keep clang from optimizing things too much.  Force linking with the c++ command instead of cc by setting CC=${CXX}.  This is a hack and a better solution is under development.  Linking with c++ is necessary to make the shared libraries depend up libc++.so such that  the iostream objects are constructed before we use them.
    ok bluhm@

--- regress/libexec/ld.so/constructor/libaa/aa.C
+++ regress/libexec/ld.so/constructor/libaa/aa.C
@@ -1,11 +1,11 @@
 /*
  * Public Domain 2003 Dale Rahn
  *
- * $OpenBSD: aa.C,v 1.2 2016/09/27 06:52:50 kettenis Exp $
+ * $OpenBSD: aa.C,v 1.3 2017/08/04 18:23:52 kettenis Exp $
  */
 
 #include "aa.h"
-int a;
+volatile int a;
 
 
 AA::AA(char *arg)
