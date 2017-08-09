0a659a0e948644ad9aec658615914aae6c215965
dcoppa <dcoppa@openbsd.org>
2017-08-09 10:15:31 +0000

    Add "PNP0303" (8042 PS/2 Controller) to acpi_isa_hids[]
    ok kettenis@

--- sys/dev/acpi/acpi.c
+++ sys/dev/acpi/acpi.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpi.c,v 1.330 2017/07/22 21:06:17 jcs Exp $ */
+/* $OpenBSD: acpi.c,v 1.331 2017/08/09 10:15:31 dcoppa Exp $ */
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  * Copyright (c) 2005 Jordan Hargrave <jordan@openbsd.org>
@@ -2801,6 +2801,7 @@ const char *acpi_skip_hids[] = {
 
 /* ISA devices for which we attach a driver later */
 const char *acpi_isa_hids[] = {
+	"PNP0303",	/* 8042 PS/2 Controller */
 	"PNP0501",	/* 16550A-compatible COM Serial Port */
 	NULL
 };
