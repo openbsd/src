d662984cd789759c637533064054a6a2752c763a
jsing <jsing@openbsd.org>
2017-08-11 21:13:12 +0000

    doug@ added code in here as well.

--- regress/lib/libssl/tlsext/tlsexttest.c
+++ regress/lib/libssl/tlsext/tlsexttest.c
@@ -1,6 +1,7 @@
-/* $OpenBSD: tlsexttest.c,v 1.6 2017/08/11 21:12:39 jsing Exp $ */
+/* $OpenBSD: tlsexttest.c,v 1.7 2017/08/11 21:13:12 jsing Exp $ */
 /*
  * Copyright (c) 2017 Joel Sing <jsing@openbsd.org>
+ * Copyright (c) 2017 Doug Hogan <doug@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
