c3b48e336b5d87e4a647e525451891b114329f5e
bcook <bcook@openbsd.org>
2017-08-12 21:47:23 +0000

    bump to 2.6.1

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,10 +1,10 @@
-/* $OpenBSD: opensslv.h,v 1.40 2017/04/06 10:38:24 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.41 2017/08/12 21:47:23 bcook Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER	0x2060000fL
-#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.6.0"
+#define LIBRESSL_VERSION_NUMBER	0x2060100fL
+#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.6.1"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
