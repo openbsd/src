7d70b5d74c7e9b9fdb6481efee0ce3b59f89b733
visa <visa@openbsd.org>
2017-08-12 15:33:41 +0000

    Add a dummy (for now) <machine/reloc.h> for mips64 to fix build.
    OK guenther@

---
+++ sys/arch/mips64/include/reloc.h
@@ -0,0 +1,10 @@
+/*	$OpenBSD: reloc.h,v 1.3 2017/08/12 15:33:41 visa Exp $	*/
+
+/* public domain */
+
+#ifndef _MIPS64_RELOC_H_
+#define _MIPS64_RELOC_H_
+
+/* nothing */
+
+#endif /* !_MIPS64_RELOC_H_ */
