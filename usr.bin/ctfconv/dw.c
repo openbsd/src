b7b99cbe2de1144a5136f67f85c0ce929bf4bb55
anton <anton@openbsd.org>
2017-09-04 12:56:01 +0000

    Fix typo in comment; ok mpi@

--- usr.bin/ctfconv/dw.c
+++ usr.bin/ctfconv/dw.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dw.c,v 1.2 2017/08/11 14:58:56 jasper Exp $ */
+/*	$OpenBSD: dw.c,v 1.3 2017/09/04 12:56:01 anton Exp $ */
 
 /*
  * Copyright (c) 2016 Martin Pieuchot
@@ -570,7 +570,7 @@ dw_cu_parse(struct dwbuf *info, struct dwbuf *abbrev, size_t seglen,
 	if (length >= 0xfffffff0 || length > info->len)
 		return EOVERFLOW;
 
-	/* Offset of the next Compule Unit. */
+	/* Offset of the next Compile Unit. */
 	nextoff = segoff + length + sizeof(uint32_t);
 
 	if (dw_read_buf(info, &dwbuf, length))
