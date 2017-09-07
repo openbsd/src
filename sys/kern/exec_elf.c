01352abefd52196b710c7e6439551a98559e5da0
bluhm <bluhm@openbsd.org>
2017-09-07 11:32:14 +0000

    In elf_load_file() to not call free(9) with an uninitialized size even if the pointer is NULL.  This is not a real bug as free(9) checks the addr pointer before the size value, but the compiler cannot know that. found by clang -Wuninitialized; OK deraadt@

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_elf.c,v 1.140 2017/03/20 00:05:21 kettenis Exp $	*/
+/*	$OpenBSD: exec_elf.c,v 1.141 2017/09/07 11:32:14 bluhm Exp $	*/
 
 /*
  * Copyright (c) 1996 Per Fogelstrom
@@ -318,7 +318,7 @@ elf_load_file(struct proc *p, char *path, struct exec_package *epp,
 	struct nameidata nd;
 	Elf_Ehdr eh;
 	Elf_Phdr *ph = NULL;
-	u_long phsize;
+	u_long phsize = 0;
 	Elf_Addr addr;
 	struct vnode *vp;
 	Elf_Phdr *base_ph = NULL;
