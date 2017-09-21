dccf4c1b2d51190a0b8137f387f2ab28e04a7630
markus <markus@openbsd.org>
2017-09-21 19:18:12 +0000

    test reverse dynamic forwarding with SOCKS

--- regress/usr.bin/ssh/dynamic-forward.sh
+++ regress/usr.bin/ssh/dynamic-forward.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: dynamic-forward.sh,v 1.12 2017/04/30 23:34:55 djm Exp $
+#	$OpenBSD: dynamic-forward.sh,v 1.13 2017/09/21 19:18:12 markus Exp $
 #	Placed in the Public Domain.
 
 tid="dynamic forwarding"
@@ -17,42 +17,45 @@ trace "will use ProxyCommand $proxycmd"
 
 start_sshd
 
-n=0
-error="1"
-trace "start dynamic forwarding, fork to background"
-while [ "$error" -ne 0 -a "$n" -lt 3 ]; do
-	n=`expr $n + 1`
-	${SSH} -F $OBJ/ssh_config -f -D $FWDPORT -q \
-	    -oExitOnForwardFailure=yes somehost exec sh -c \
-		\'"echo \$\$ > $OBJ/remote_pid; exec sleep 444"\'
-	error=$?
+for d in D R; do
+	n=0
+	error="1"
+	trace "start dynamic forwarding, fork to background"
+
+	while [ "$error" -ne 0 -a "$n" -lt 3 ]; do
+		n=`expr $n + 1`
+		${SSH} -F $OBJ/ssh_config -f -$d $FWDPORT -q \
+		    -oExitOnForwardFailure=yes somehost exec sh -c \
+			\'"echo \$\$ > $OBJ/remote_pid; exec sleep 444"\'
+		error=$?
+		if [ "$error" -ne 0 ]; then
+			trace "forward failed attempt $n err $error"
+			sleep $n
+		fi
+	done
 	if [ "$error" -ne 0 ]; then
-		trace "forward failed attempt $n err $error"
-		sleep $n
+		fatal "failed to start dynamic forwarding"
 	fi
-done
-if [ "$error" -ne 0 ]; then
-	fatal "failed to start dynamic forwarding"
-fi
 
-for s in 4 5; do
-    for h in 127.0.0.1 localhost; do
-	trace "testing ssh socks version $s host $h"
-	${SSH} -F $OBJ/ssh_config \
-		-o "ProxyCommand ${proxycmd}${s} $h $PORT" \
-		somehost cat ${DATA} > ${COPY}
-	test -f ${COPY}	 || fail "failed copy ${DATA}"
-	cmp ${DATA} ${COPY} || fail "corrupted copy of ${DATA}"
-    done
-done
+	for s in 4 5; do
+	    for h in 127.0.0.1 localhost; do
+		trace "testing ssh socks version $s host $h (-$d)"
+		${SSH} -F $OBJ/ssh_config \
+			-o "ProxyCommand ${proxycmd}${s} $h $PORT" \
+			somehost cat ${DATA} > ${COPY}
+		test -f ${COPY}	 || fail "failed copy ${DATA}"
+		cmp ${DATA} ${COPY} || fail "corrupted copy of ${DATA}"
+	    done
+	done
 
-if [ -f $OBJ/remote_pid ]; then
-	remote=`cat $OBJ/remote_pid`
-	trace "terminate remote shell, pid $remote"
-	if [ $remote -gt 1 ]; then
-		kill -HUP $remote
+	if [ -f $OBJ/remote_pid ]; then
+		remote=`cat $OBJ/remote_pid`
+		trace "terminate remote shell, pid $remote"
+		if [ $remote -gt 1 ]; then
+			kill -HUP $remote
+		fi
+	else
+		fail "no pid file: $OBJ/remote_pid"
 	fi
-else
-	fail "no pid file: $OBJ/remote_pid"
-fi
 
+done
