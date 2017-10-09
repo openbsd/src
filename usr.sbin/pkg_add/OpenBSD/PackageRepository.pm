2f58182f683848b9f246e99b705a8e8e2083f8bd
espie <espie@openbsd.org>
2017-10-09 15:35:06 +0000

    correct calls to this constructor *must* include a state. If they don't you won't notice until the repository wants to output an error message, which is bad.
    if code breaks thanks to this commit, the call site needs to be fixed.

--- usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
+++ usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackageRepository.pm,v 1.146 2017/08/04 11:53:03 sthen Exp $
+# $OpenBSD: PackageRepository.pm,v 1.147 2017/10/09 15:35:06 espie Exp $
 #
 # Copyright (c) 2003-2010 Marc Espie <espie@openbsd.org>
 #
@@ -52,6 +52,10 @@ sub baseurl
 sub new
 {
 	my ($class, $baseurl, $state) = @_;
+	if (!defined $state) {
+		require Carp;
+		Carp::croak "fatal: old api call to $class: needs state";
+	}
 	my $o = $class->parse(\$baseurl, $state);
 	if ($baseurl ne '') {
 		return undef;
