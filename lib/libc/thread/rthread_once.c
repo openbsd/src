eb59ee9a45e0f19979fa0d4ef567f683d048aafb
guenther <guenther@openbsd.org>
2017-10-28 21:25:24 +0000

    If the init_routine used with pthread_once() is canceled, then we need to unlock the mutex inside the pthread_once_t.  Push a cleanup wrapper for that.
    Diff from Scott Cheloha <scottcheloha@gmail.com>
    ok mpi@

--- lib/libc/thread/rthread_once.c
+++ lib/libc/thread/rthread_once.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rthread_once.c,v 1.1 2017/08/15 06:13:24 guenther Exp $ */
+/*	$OpenBSD: rthread_once.c,v 1.2 2017/10/28 21:25:24 guenther Exp $ */
 /*
  * Copyright (c) 2004,2005 Ted Unangst <tedu@openbsd.org>
  * All Rights Reserved.
@@ -17,13 +17,23 @@
  */
 
 #include <pthread.h>
+#include <stdlib.h>
+
+static void
+mutex_unlock(void *arg)
+{
+	if (pthread_mutex_unlock(arg))
+		abort();
+}
 
 int
 pthread_once(pthread_once_t *once_control, void (*init_routine)(void))
 {
 	pthread_mutex_lock(&once_control->mutex);
 	if (once_control->state == PTHREAD_NEEDS_INIT) {
+		pthread_cleanup_push(mutex_unlock, &once_control->mutex);
 		init_routine();
+		pthread_cleanup_pop(0);
 		once_control->state = PTHREAD_DONE_INIT;
 	}
 	pthread_mutex_unlock(&once_control->mutex);
