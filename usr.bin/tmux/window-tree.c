34f4cdc844f196aff5120ec997559667a0201960
nicm <nicm@openbsd.org>
2017-11-02 18:52:05 +0000

    Format for group list of "other sessions" is a bit weird, just list all the sessions in the group.

--- usr.bin/tmux/window-tree.c
+++ usr.bin/tmux/window-tree.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: window-tree.c,v 1.24 2017/11/02 18:43:51 nicm Exp $ */
+/* $OpenBSD: window-tree.c,v 1.25 2017/11/02 18:52:05 nicm Exp $ */
 
 /*
  * Copyright (c) 2017 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -44,10 +44,8 @@ static void		 window_tree_key(struct window_pane *,
 		"," \
 			"#{session_windows} windows" \
 			"#{?session_grouped, " \
-				"(group #{session_group}" \
-				"#{?session_group_others," \
-					" with #{session_group_others}," \
-				"})," \
+				"(group #{session_group}: " \
+				"#{session_group_list})," \
 			"}" \
 			"#{?session_attached, (attached),}" \
 		"}" \
