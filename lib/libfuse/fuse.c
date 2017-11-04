34272175f5be72da85b257db3cf58d30d8c6b0e6
mpi <mpi@openbsd.org>
2017-11-04 13:17:18 +0000

    fuse_loop_mt() isn't implemented so return an error value.
    From Helg Bredow.

--- lib/libfuse/fuse.c
+++ lib/libfuse/fuse.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: fuse.c,v 1.33 2017/11/04 13:04:08 mpi Exp $ */
+/* $OpenBSD: fuse.c,v 1.34 2017/11/04 13:17:18 mpi Exp $ */
 /*
  * Copyright (c) 2013 Sylvestre Gallon <ccna.syl@gmail.com>
  *
@@ -236,7 +236,7 @@ fuse_get_session(struct fuse *f)
 int
 fuse_loop_mt(unused struct fuse *fuse)
 {
-	return (0);
+	return (-1);
 }
 
 struct fuse *
