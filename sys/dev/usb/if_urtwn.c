39ab79cf254785ae195582100d8e44ec50250c10
stsp <stsp@openbsd.org>
2017-11-07 21:20:05 +0000

    Prevent a potential stack buffer overrun when a urtwn(4) device reports more than 3 bulk-out endpoints.
    Problem found by Pierre Pronchery (khorben) during code inspection.
    
    ok mpi@ kevlo@

--- sys/dev/usb/if_urtwn.c
+++ sys/dev/usb/if_urtwn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_urtwn.c,v 1.75 2017/08/23 09:25:17 kevlo Exp $	*/
+/*	$OpenBSD: if_urtwn.c,v 1.76 2017/11/07 21:20:05 stsp Exp $	*/
 
 /*-
  * Copyright (c) 2010 Damien Bergamini <damien.bergamini@free.fr>
@@ -535,7 +535,8 @@ urtwn_open_pipes(struct urtwn_softc *sc)
 			rx_no = ed->bEndpointAddress;
 			nrx++;
 		} else {
-			epaddr[sc->ntx] = ed->bEndpointAddress;
+			if (sc->ntx < R92C_MAX_EPOUT)
+				epaddr[sc->ntx] = ed->bEndpointAddress;
 			sc->ntx++;
 		}
 	}
