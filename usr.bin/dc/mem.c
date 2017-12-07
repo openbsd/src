9e55baca979a2df7b75e41cd738a559420896dfd
otto <otto@openbsd.org>
2017-12-07 06:34:05 +0000

    zap a few stray backslashes from the time thesse two were #defines; from Ilya Kaliman

--- usr.bin/dc/mem.c
+++ usr.bin/dc/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.7 2015/02/16 20:53:34 jca Exp $	*/
+/*	$OpenBSD: mem.c,v 1.8 2017/12/07 06:34:05 otto Exp $	*/
 
 /*
  * Copyright (c) 2003, Otto Moerbeek <otto@drijf.net>
@@ -90,14 +90,14 @@ bstrdup(const char *p)
 }
 
 void
-bn_check(int x)						\
+bn_check(int x)
 {
 	if (x == 0)
 		err(1, "big number failure %lx", ERR_get_error());
 }
 
 void
-bn_checkp(const void *p)						\
+bn_checkp(const void *p)
 {
 	if (p == NULL)
 		err(1, "allocation failure %lx", ERR_get_error());
