8d713102296e374bdfd7db88a1300cb06e44f802
benno <benno@openbsd.org>
2017-12-14 09:26:11 +0000

    let urtwn(4) find my TP-LINK TL-WN722N v2 ok deraadt@ phessler@ kettenis@

--- sys/dev/usb/if_urtwn.c
+++ sys/dev/usb/if_urtwn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_urtwn.c,v 1.76 2017/11/07 21:20:05 stsp Exp $	*/
+/*	$OpenBSD: if_urtwn.c,v 1.77 2017/12/14 09:26:11 benno Exp $	*/
 
 /*-
  * Copyright (c) 2010 Damien Bergamini <damien.bergamini@free.fr>
@@ -310,7 +310,8 @@ static const struct urtwn_type {
 	URTWN_DEV_8188EU(DLINK,		DWA125D1),
 	URTWN_DEV_8188EU(ELECOM,	WDC150SU2M),
 	URTWN_DEV_8188EU(REALTEK,	RTL8188ETV),
-	URTWN_DEV_8188EU(REALTEK,	RTL8188EU)
+	URTWN_DEV_8188EU(REALTEK,	RTL8188EU),
+	URTWN_DEV_8188EU(TPLINK,	RTL8188EUS)
 };
 
 #define urtwn_lookup(v, p)	\
