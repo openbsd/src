3860a9d15615a4970b389a032ccada79736ae915
otto <otto@openbsd.org>
2017-12-17 08:21:10 +0000

    Use proper BN functions for error reporting; from kshe

--- usr.bin/dc/mem.c
+++ usr.bin/dc/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.9 2017/12/12 19:08:57 otto Exp $	*/
+/*	$OpenBSD: mem.c,v 1.10 2017/12/17 08:21:10 otto Exp $	*/
 
 /*
  * Copyright (c) 2003, Otto Moerbeek <otto@drijf.net>
@@ -91,13 +91,19 @@ bstrdup(const char *p)
 void
 bn_check(int x)
 {
-	if (x == 0)
-		err(1, "big number failure %lx", ERR_get_error());
+	if (x == 0) {
+		ERR_load_BN_strings();
+		errx(1, "BN failure: %s",
+		    ERR_reason_error_string(ERR_get_error()));
+	}
 }
 
 void
 bn_checkp(const void *p)
 {
-	if (p == NULL)
-		err(1, "allocation failure %lx", ERR_get_error());
+	if (p == NULL) {
+		ERR_load_BN_strings();
+		errx(1, "BN failure: %s",
+		    ERR_reason_error_string(ERR_get_error()));
+	}
 }
