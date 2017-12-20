cd187a97da7d683c6a3e843cc98b3055da72c382
mpi <mpi@openbsd.org>
2017-12-20 11:08:44 +0000

    Switch x86 architectures to the common C mutex implementation.
    This is a step towards MI mutexes.
    
    ok kettenis@

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.180 2017/08/25 19:28:48 guenther Exp $	*/
+/*	$OpenBSD: locore.s,v 1.181 2017/12/20 11:08:44 mpi Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -1318,8 +1318,6 @@ ENTRY(cpu_paenable)
 #if NLAPIC > 0
 #include <i386/i386/apicvec.s>
 #endif
-
-#include <i386/i386/mutex.S>
 
 	.section .rodata
 	.globl _C_LABEL(_stac)
