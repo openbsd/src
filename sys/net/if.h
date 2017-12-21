a6a7f80333366bc447f4bbbe1305559317e2bda7
dlg <dlg@openbsd.org>
2017-12-21 01:11:47 +0000

    prototype if_attach_iqueues so drivers can configure multiple iqs.

--- sys/net/if.h
+++ sys/net/if.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.h,v 1.188 2017/11/09 09:07:01 tb Exp $	*/
+/*	$OpenBSD: if.h,v 1.189 2017/12/21 01:11:47 dlg Exp $	*/
 /*	$NetBSD: if.h,v 1.23 1996/05/07 02:40:27 thorpej Exp $	*/
 
 /*
@@ -463,6 +463,7 @@ void	if_alloc_sadl(struct ifnet *);
 void	if_free_sadl(struct ifnet *);
 void	if_attach(struct ifnet *);
 void	if_attach_queues(struct ifnet *, unsigned int);
+void	if_attach_iqueues(struct ifnet *, unsigned int);
 void	if_attach_ifq(struct ifnet *, const struct ifq_ops *, void *);
 void	if_attachtail(struct ifnet *);
 void	if_attachhead(struct ifnet *);
