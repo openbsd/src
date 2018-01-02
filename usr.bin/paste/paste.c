1ada7db47ea8f19e0fef96fa06dd66a7954c3611
guenther <guenther@openbsd.org>
2018-01-02 06:56:41 +0000

    When invoked with no file arguments, display usage and fail instead of silently doing nothing.
    suggested by Klemens Nanni (kl3 (at) posteo.org), from FreeBSD r92772
    ok millert@

--- usr.bin/paste/paste.c
+++ usr.bin/paste/paste.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: paste.c,v 1.22 2015/12/09 19:39:10 mmcc Exp $	*/
+/*	$OpenBSD: paste.c,v 1.23 2018/01/02 06:56:41 guenther Exp $	*/
 
 /*
  * Copyright (c) 1989 The Regents of the University of California.
@@ -76,6 +76,9 @@ main(int argc, char *argv[])
 	}
 	argc -= optind;
 	argv += optind;
+
+	if (argc == 0)
+		usage();
 
 	if (!delim) {
 		delimcnt = 1;
