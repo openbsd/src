0dfa4009674e3444e346b13939fa4d22eb92befe
stsp <stsp@openbsd.org>
2018-01-14 11:51:34 +0000

    Pass our currently configured SSID to iwm(4) scan commands.
    This makes connecting to silly "hidden" networks work, which was a long
    standing problem with iwm(4). My past observations where other SSIDs were
    filtered from scan results while associated can no longer be reproduced.
    
    ok robert phessler

--- sys/dev/pci/if_iwm.c
+++ sys/dev/pci/if_iwm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwm.c,v 1.222 2017/12/10 20:34:41 stsp Exp $	*/
+/*	$OpenBSD: if_iwm.c,v 1.223 2018/01/14 11:51:34 stsp Exp $	*/
 
 /*
  * Copyright (c) 2014, 2016 genua gmbh <info@genua.de>
@@ -4741,10 +4741,8 @@ iwm_lmac_scan_fill_channels(struct iwm_softc *sc,
 		chan->iter_count = htole16(1);
 		chan->iter_interval = 0;
 		chan->flags = htole32(IWM_UNIFIED_SCAN_CHANNEL_PARTIAL);
-#if 0 /* makes scanning while associated less useful */
 		if (n_ssids != 0)
 			chan->flags |= htole32(1 << 1); /* select SSID 0 */
-#endif
 		chan++;
 		nchan++;
 	}
@@ -4770,10 +4768,8 @@ iwm_umac_scan_fill_channels(struct iwm_softc *sc,
 		chan->channel_num = ieee80211_mhz2ieee(c->ic_freq, 0);
 		chan->iter_count = 1;
 		chan->iter_interval = htole16(0);
-#if 0 /* makes scanning while associated less useful */
 		if (n_ssids != 0)
 			chan->flags = htole32(1 << 0); /* select SSID 0 */
-#endif
 		chan++;
 		nchan++;
 	}
