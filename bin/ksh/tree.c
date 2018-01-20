81a95e3f81cc7fb05308aec6e97fbdfac84dafc3
anton <anton@openbsd.org>
2018-01-20 15:32:20 +0000

    Remove code that has been disabled since 1999. From Michael W. Bombardieri who also took the time to provide an excellent explanation on why the code isn't useful on tech@.
    ok jca@

--- bin/ksh/tree.c
+++ bin/ksh/tree.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tree.c,v 1.31 2018/01/16 22:52:32 jca Exp $	*/
+/*	$OpenBSD: tree.c,v 1.32 2018/01/20 15:32:20 anton Exp $	*/
 
 /*
  * command tree climbing
@@ -47,25 +47,8 @@ ptree(struct op *t, int indent, struct shf *shf)
 			fptreef(shf, indent, "#no-args# ");
 		break;
 	case TEXEC:
-#if 0 /* ?not useful - can't be called? */
-		/* Print original vars */
-		if (t->left->vars)
-			for (w = t->left->vars; *w != NULL; )
-				fptreef(shf, indent, "%S ", *w++);
-		else
-			fptreef(shf, indent, "#no-vars# ");
-		/* Print expanded vars */
-		if (t->args)
-			for (w = t->args; *w != NULL; )
-				fptreef(shf, indent, "%s ", *w++);
-		else
-			fptreef(shf, indent, "#no-args# ");
-		/* Print original io */
-		t = t->left;
-#else
 		t = t->left;
 		goto Chain;
-#endif
 	case TPAREN:
 		fptreef(shf, indent + 2, "( %T) ", t->left);
 		break;
