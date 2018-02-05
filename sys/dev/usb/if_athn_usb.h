9f962d91a6e340229bbcbc30078185776c5ec74d
stsp <stsp@openbsd.org>
2018-02-05 09:52:03 +0000

    claim copyright

--- sys/dev/usb/if_athn_usb.h
+++ sys/dev/usb/if_athn_usb.h
@@ -1,7 +1,8 @@
-/*	$OpenBSD: if_athn_usb.h,v 1.9 2018/02/05 08:48:57 stsp Exp $	*/
+/*	$OpenBSD: if_athn_usb.h,v 1.10 2018/02/05 09:52:03 stsp Exp $	*/
 
 /*-
  * Copyright (c) 2011 Damien Bergamini <damien.bergamini@free.fr>
+ * Copyright (c) 2018 Stefan Sperling <stsp@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
