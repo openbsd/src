71ca718426c3f92ede99df873d2c877ff85491ff
phessler <phessler@openbsd.org>
2018-02-06 09:04:45 +0000

    remove extern from function prototypes, remove some variable names, and a left-over kernel malloc feature we don't use.
    OK deraadt@

--- sys/net80211/ieee80211_node.h
+++ sys/net80211/ieee80211_node.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieee80211_node.h,v 1.70 2017/12/08 21:16:01 stsp Exp $	*/
+/*	$OpenBSD: ieee80211_node.h,v 1.71 2018/02/06 09:04:45 phessler Exp $	*/
 /*	$NetBSD: ieee80211_node.h,v 1.9 2004/04/30 22:57:32 dyoung Exp $	*/
 
 /*-
@@ -345,64 +345,61 @@ ieee80211_unref_node(struct ieee80211_node **ni)
 
 struct ieee80211com;
 
-#ifdef MALLOC_DECLARE
-MALLOC_DECLARE(M_80211_NODE);
-#endif
+typedef void ieee80211_iter_func(void *, struct ieee80211_node *);
 
-extern	void ieee80211_node_attach(struct ifnet *);
-extern	void ieee80211_node_lateattach(struct ifnet *);
-extern	void ieee80211_node_detach(struct ifnet *);
+void ieee80211_node_attach(struct ifnet *);
+void ieee80211_node_lateattach(struct ifnet *);
+void ieee80211_node_detach(struct ifnet *);
 
-extern	void ieee80211_begin_scan(struct ifnet *);
-extern	void ieee80211_next_scan(struct ifnet *);
-extern	void ieee80211_end_scan(struct ifnet *);
-extern	void ieee80211_reset_scan(struct ifnet *);
-extern	struct ieee80211_node *ieee80211_alloc_node(struct ieee80211com *,
+void ieee80211_begin_scan(struct ifnet *);
+void ieee80211_next_scan(struct ifnet *);
+void ieee80211_end_scan(struct ifnet *);
+void ieee80211_reset_scan(struct ifnet *);
+struct ieee80211_node *ieee80211_alloc_node(struct ieee80211com *,
 		const u_int8_t *);
-extern	struct ieee80211_node *ieee80211_dup_bss(struct ieee80211com *,
+struct ieee80211_node *ieee80211_dup_bss(struct ieee80211com *,
 		const u_int8_t *);
-extern	struct ieee80211_node *ieee80211_find_node(struct ieee80211com *,
+struct ieee80211_node *ieee80211_find_node(struct ieee80211com *,
 		const u_int8_t *);
-extern	struct ieee80211_node *ieee80211_find_rxnode(struct ieee80211com *,
+struct ieee80211_node *ieee80211_find_rxnode(struct ieee80211com *,
 		const struct ieee80211_frame *);
-extern	struct ieee80211_node *ieee80211_find_txnode(struct ieee80211com *,
+struct ieee80211_node *ieee80211_find_txnode(struct ieee80211com *,
 		const u_int8_t *);
-extern	struct ieee80211_node *
+struct ieee80211_node *
 		ieee80211_find_node_for_beacon(struct ieee80211com *,
 		const u_int8_t *, const struct ieee80211_channel *,
 		const char *, u_int8_t);
-extern	void ieee80211_release_node(struct ieee80211com *,
+void ieee80211_release_node(struct ieee80211com *,
 		struct ieee80211_node *);
-extern	void ieee80211_free_allnodes(struct ieee80211com *);
-typedef void ieee80211_iter_func(void *, struct ieee80211_node *);
-extern	void ieee80211_iterate_nodes(struct ieee80211com *ic,
+void ieee80211_free_allnodes(struct ieee80211com *);
+void ieee80211_iterate_nodes(struct ieee80211com *,
 		ieee80211_iter_func *, void *);
-extern	void ieee80211_clean_cached(struct ieee80211com *ic);
-extern	void ieee80211_clean_nodes(struct ieee80211com *, int);
+void ieee80211_clean_cached(struct ieee80211com *);
+void ieee80211_clean_nodes(struct ieee80211com *, int);
 void ieee80211_setup_htcaps(struct ieee80211_node *, const uint8_t *,
     uint8_t);
 void ieee80211_clear_htcaps(struct ieee80211_node *);
 int ieee80211_setup_htop(struct ieee80211_node *, const uint8_t *,
     uint8_t);
-extern	int ieee80211_setup_rates(struct ieee80211com *,
+int ieee80211_setup_rates(struct ieee80211com *,
 	    struct ieee80211_node *, const u_int8_t *, const u_int8_t *, int);
-extern  int ieee80211_iserp_sta(const struct ieee80211_node *);
-extern void ieee80211_count_longslotsta(void *, struct ieee80211_node *);
-extern void ieee80211_count_nonerpsta(void *, struct ieee80211_node *);
-extern void ieee80211_count_pssta(void *, struct ieee80211_node *);
-extern void ieee80211_count_rekeysta(void *, struct ieee80211_node *);
-extern	void ieee80211_node_join(struct ieee80211com *,
+int ieee80211_iserp_sta(const struct ieee80211_node *);
+void ieee80211_count_longslotsta(void *, struct ieee80211_node *);
+void ieee80211_count_nonerpsta(void *, struct ieee80211_node *);
+void ieee80211_count_pssta(void *, struct ieee80211_node *);
+void ieee80211_count_rekeysta(void *, struct ieee80211_node *);
+void ieee80211_node_join(struct ieee80211com *,
 		struct ieee80211_node *, int);
-extern	void ieee80211_node_leave(struct ieee80211com *,
+void ieee80211_node_leave(struct ieee80211com *,
 		struct ieee80211_node *);
-extern	int ieee80211_match_bss(struct ieee80211com *,
+int ieee80211_match_bss(struct ieee80211com *,
 		struct ieee80211_node *);
-extern	void ieee80211_create_ibss(struct ieee80211com* ,
+void ieee80211_create_ibss(struct ieee80211com* ,
 		struct ieee80211_channel *);
-extern	void ieee80211_notify_dtim(struct ieee80211com *);
-extern	void ieee80211_set_tim(struct ieee80211com *, int, int);
+void ieee80211_notify_dtim(struct ieee80211com *);
+void ieee80211_set_tim(struct ieee80211com *, int, int);
 
-extern	int ieee80211_node_cmp(const struct ieee80211_node *,
+int ieee80211_node_cmp(const struct ieee80211_node *,
 		const struct ieee80211_node *);
 RBT_PROTOTYPE(ieee80211_tree, ieee80211_node, ni_node, ieee80211_node_cmp);
 
