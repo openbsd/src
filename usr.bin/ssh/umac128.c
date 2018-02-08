8d4c5dd784efe9d35fc378ffdbf6f2a5dc1670f9
dtucker <dtucker@openbsd.org>
2018-02-08 04:12:32 +0000

    Rename struct umac_ctx to umac128_ctx too.  In portable some linkers complain about two symbols with the same name having differing sizes.  ok djm@

--- usr.bin/ssh/umac128.c
+++ usr.bin/ssh/umac128.c
@@ -1,9 +1,10 @@
-/* $OpenBSD: umac128.c,v 1.1 2017/12/12 15:06:12 naddy Exp $ */
+/* $OpenBSD: umac128.c,v 1.2 2018/02/08 04:12:32 dtucker Exp $ */
 
 #define UMAC_OUTPUT_LEN	16
 #define umac_new	umac128_new
 #define umac_update	umac128_update
 #define umac_final	umac128_final
 #define umac_delete	umac128_delete
+#define umac_ctx	umac128_ctx
 
 #include "umac.c"
