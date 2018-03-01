42804c79f7a7bae0a71d58c8cb491860d930f860
markus <markus@openbsd.org>
2018-03-01 20:32:16 +0000

    missing #ifdef for _PATH_HOST_XMSS_KEY_FILE; report by jmc@

--- usr.bin/ssh/servconf.c
+++ usr.bin/ssh/servconf.c
@@ -1,5 +1,5 @@
 
-/* $OpenBSD: servconf.c,v 1.325 2018/02/23 15:58:37 markus Exp $ */
+/* $OpenBSD: servconf.c,v 1.326 2018/03/01 20:32:16 markus Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -232,8 +232,10 @@ fill_default_server_options(ServerOptions *options)
 		    _PATH_HOST_ECDSA_KEY_FILE);
 		servconf_add_hostkey("[default]", 0, options,
 		    _PATH_HOST_ED25519_KEY_FILE);
+#ifdef WITH_XMSS
 		servconf_add_hostkey("[default]", 0, options,
 		    _PATH_HOST_XMSS_KEY_FILE);
+#endif /* WITH_XMSS */
 	}
 	/* No certificates by default */
 	if (options->num_ports == 0)
