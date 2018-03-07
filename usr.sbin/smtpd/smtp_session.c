da29cb6e30ec528892c05146e3db75137d1eef30
gilles <gilles@openbsd.org>
2018-03-07 21:37:14 +0000

    rfc4954 provides more than one method to submit the same credentials and in a recent cleanup we accidentally removed one of the ways breaking some .net lib apparently, sorry.
    this commit reintroduces the "pass password in the same line as username"
    method
    
    ok millert@

--- usr.sbin/smtpd/smtp_session.c
+++ usr.sbin/smtpd/smtp_session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtp_session.c,v 1.317 2018/02/16 20:57:30 eric Exp $	*/
+/*	$OpenBSD: smtp_session.c,v 1.318 2018/03/07 21:37:14 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -1609,6 +1609,10 @@ smtp_rfc4954_auth_login(struct smtp_session *s, char *arg)
 	switch (s->state) {
 	case STATE_HELO:
 		smtp_enter_state(s, STATE_AUTH_USERNAME);
+		if (arg != NULL && *arg != '\0') {
+			smtp_rfc4954_auth_login(s, arg);
+			return;
+		}
 		smtp_reply(s, "334 VXNlcm5hbWU6");
 		return;
 
