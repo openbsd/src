d9a63ec70417aa1ee3d88defe8fe071683410f3c
guenther <guenther@openbsd.org>
2018-03-13 05:10:40 +0000

    We don't save+restore FS.base and GS.base on every context switch but rather reset GS.base and restore FS.base to what was set via __tcb_set(), so we can't really support the wr{fs,gs}base instructions by userspace. Enabling CR4_FSGSBASE is therefore incorrect so stop doing so.
    ok jsg@ krw@

--- sys/arch/amd64/amd64/cpu.c
+++ sys/arch/amd64/amd64/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.112 2018/02/21 19:24:15 guenther Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.113 2018/03/13 05:10:40 guenther Exp $	*/
 /* $NetBSD: cpu.c,v 1.1 2003/04/26 18:39:26 fvdl Exp $ */
 
 /*-
@@ -520,8 +520,6 @@ cpu_init(struct cpu_info *ci)
 		cr4 |= CR4_SMEP;
 	if (ci->ci_feature_sefflags_ebx & SEFF0EBX_SMAP)
 		cr4 |= CR4_SMAP;
-	if (ci->ci_feature_sefflags_ebx & SEFF0EBX_FSGSBASE)
-		cr4 |= CR4_FSGSBASE;
 	if (ci->ci_feature_sefflags_ecx & SEFF0ECX_UMIP)
 		cr4 |= CR4_UMIP;
 	if (cpu_ecxfeature & CPUIDECX_XSAVE)
