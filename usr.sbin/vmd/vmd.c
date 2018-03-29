18e8598b8a69755cdefe1491a8dd1817c8cfeb1e
martijn <martijn@openbsd.org>
2018-03-29 18:29:24 +0000

    Make sure that the global config is send out immediately when it is loaded. This makes sure that the local prefix specied in the config is always used.
    OK ccardenas@

--- usr.sbin/vmd/vmd.c
+++ usr.sbin/vmd/vmd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmd.c,v 1.81 2018/03/14 07:29:34 mlarkin Exp $	*/
+/*	$OpenBSD: vmd.c,v 1.82 2018/03/29 18:29:24 martijn Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -828,6 +828,10 @@ vmd_configure(void)
 		exit(0);
 	}
 
+	/* Send shared global configuration to all children */
+	if (config_setconfig(env) == -1)
+		return (-1);
+
 	TAILQ_FOREACH(vsw, env->vmd_switches, sw_entry) {
 		if (vsw->sw_running)
 			continue;
@@ -849,10 +853,6 @@ vmd_configure(void)
 		if (config_setvm(&env->vmd_ps, vm, -1, vm->vm_params.vmc_uid) == -1)
 			return (-1);
 	}
-
-	/* Send shared global configuration to all children */
-	if (config_setconfig(env) == -1)
-		return (-1);
 
 	return (0);
 }
@@ -893,16 +893,18 @@ vmd_reload(unsigned int reset, const char *filename)
 					vm_remove(vm);
 				}
 			}
-
-			/* Update shared global configuration in all children */
-			if (config_setconfig(env) == -1)
-				return (-1);
 		}
 
 		if (parse_config(filename) == -1) {
 			log_debug("%s: failed to load config file %s",
 			    __func__, filename);
 			return (-1);
+		}
+
+		if (reload) {
+			/* Update shared global configuration in all children */
+			if (config_setconfig(env) == -1)
+				return (-1);
 		}
 
 		TAILQ_FOREACH(vsw, env->vmd_switches, sw_entry) {
