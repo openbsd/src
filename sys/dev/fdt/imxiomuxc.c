865cbd1047f0dab04470b2d7b28117fef7c51a53
patrick <patrick@openbsd.org>
2018-04-02 17:49:58 +0000

    Attach imxiomuxc(4) to i.MX8M as well.

--- sys/dev/fdt/imxiomuxc.c
+++ sys/dev/fdt/imxiomuxc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: imxiomuxc.c,v 1.1 2018/04/02 17:45:38 patrick Exp $ */
+/* $OpenBSD: imxiomuxc.c,v 1.2 2018/04/02 17:49:58 patrick Exp $ */
 /*
  * Copyright (c) 2013 Patrick Wildt <patrick@blueri.se>
  * Copyright (c) 2016 Mark Kettenis <kettenis@openbsd.org>
@@ -68,7 +68,8 @@ imxiomuxc_match(struct device *parent, void *match, void *aux)
 	    OF_is_compatible(faa->fa_node, "fsl,imx6dl-iomuxc") ||
 	    OF_is_compatible(faa->fa_node, "fsl,imx6sl-iomuxc") ||
 	    OF_is_compatible(faa->fa_node, "fsl,imx6sx-iomuxc") ||
-	    OF_is_compatible(faa->fa_node, "fsl,imx6ul-iomuxc"));
+	    OF_is_compatible(faa->fa_node, "fsl,imx6ul-iomuxc") ||
+	    OF_is_compatible(faa->fa_node, "fsl,imx8mq-iomuxc"));
 }
 
 void
