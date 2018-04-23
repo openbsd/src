1bb2e9d88d1af542d7fc32b5655c51ae97eb5111
tb <tb@openbsd.org>
2018-04-23 08:09:57 +0000

    About half of the listed functions are wrappers of ASN1_item_free(). Annotate them.

--- regress/lib/libcrypto/free/freenull.c
+++ regress/lib/libcrypto/free/freenull.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: freenull.c,v 1.8 2018/04/23 08:07:50 tb Exp $ */
+/* $OpenBSD: freenull.c,v 1.9 2018/04/23 08:09:57 tb Exp $ */
 /*
  * Copyright (c) 2017 Bob Beck <beck@openbsd.org>
  *
@@ -40,31 +40,31 @@
 int
 main(int argc, char **argv)
 {
-	ACCESS_DESCRIPTION_free(NULL);
-	ASN1_BIT_STRING_free(NULL);
-	ASN1_BMPSTRING_free(NULL);
-	ASN1_ENUMERATED_free(NULL);
-	ASN1_GENERALIZEDTIME_free(NULL);
-	ASN1_GENERALSTRING_free(NULL);
-	ASN1_IA5STRING_free(NULL);
-	ASN1_INTEGER_free(NULL);
-	ASN1_NULL_free(NULL);
+	ACCESS_DESCRIPTION_free(NULL);			/* ASN1_item_free */
+	ASN1_BIT_STRING_free(NULL);			/* ASN1_item_free */
+	ASN1_BMPSTRING_free(NULL);			/* ASN1_item_free */
+	ASN1_ENUMERATED_free(NULL);			/* ASN1_item_free */
+	ASN1_GENERALIZEDTIME_free(NULL);		/* ASN1_item_free */
+	ASN1_GENERALSTRING_free(NULL);			/* ASN1_item_free */
+	ASN1_IA5STRING_free(NULL);			/* ASN1_item_free */
+	ASN1_INTEGER_free(NULL);			/* ASN1_item_free */
+	ASN1_NULL_free(NULL);				/* ASN1_item_free */
 	ASN1_OBJECT_free(NULL);
-	ASN1_OCTET_STRING_free(NULL);
+	ASN1_OCTET_STRING_free(NULL);			/* ASN1_item_free */
 	ASN1_PCTX_free(NULL);
-	ASN1_PRINTABLESTRING_free(NULL);
-	ASN1_PRINTABLE_free(NULL);
+	ASN1_PRINTABLESTRING_free(NULL);		/* ASN1_item_free */
+	ASN1_PRINTABLE_free(NULL);			/* ASN1_item_free */
 	ASN1_STRING_free(NULL);
-	ASN1_T61STRING_free(NULL);
-	ASN1_TIME_free(NULL);
-	ASN1_TYPE_free(NULL);
-	ASN1_UNIVERSALSTRING_free(NULL);
-	ASN1_UTCTIME_free(NULL);
-	ASN1_UTF8STRING_free(NULL);
-	ASN1_VISIBLESTRING_free(NULL);
-	AUTHORITY_INFO_ACCESS_free(NULL);
-	AUTHORITY_KEYID_free(NULL);
-	BASIC_CONSTRAINTS_free(NULL);
+	ASN1_T61STRING_free(NULL);			/* ASN1_item_free */
+	ASN1_TIME_free(NULL);				/* ASN1_item_free */
+	ASN1_TYPE_free(NULL);				/* ASN1_item_free */
+	ASN1_UNIVERSALSTRING_free(NULL);		/* ASN1_item_free */
+	ASN1_UTCTIME_free(NULL);			/* ASN1_item_free */
+	ASN1_UTF8STRING_free(NULL);			/* ASN1_item_free */
+	ASN1_VISIBLESTRING_free(NULL);			/* ASN1_item_free */
+	AUTHORITY_INFO_ACCESS_free(NULL);		/* ASN1_item_free */
+	AUTHORITY_KEYID_free(NULL);			/* ASN1_item_free */
+	BASIC_CONSTRAINTS_free(NULL);			/* ASN1_item_free */
 	BIO_free(NULL);
 	BIO_free_all(NULL);
 	BIO_meth_free(NULL);
@@ -76,138 +76,138 @@ main(int argc, char **argv)
 	BN_clear_free(NULL);
 	BN_free(NULL);
 	BUF_MEM_free(NULL);
-	CERTIFICATEPOLICIES_free(NULL);
+	CERTIFICATEPOLICIES_free(NULL);			/* ASN1_item_free */
 	CMAC_CTX_free(NULL);
 	COMP_CTX_free(NULL);
 	CONF_free(NULL);
-	CRL_DIST_POINTS_free(NULL);
+	CRL_DIST_POINTS_free(NULL);			/* ASN1_item_free */
 	DH_free(NULL);
-	DIRECTORYSTRING_free(NULL);
-	DISPLAYTEXT_free(NULL);
-	DIST_POINT_NAME_free(NULL);
-	DIST_POINT_free(NULL);
+	DIRECTORYSTRING_free(NULL);			/* ASN1_item_free */
+	DISPLAYTEXT_free(NULL);				/* ASN1_item_free */
+	DIST_POINT_NAME_free(NULL);			/* ASN1_item_free */
+	DIST_POINT_free(NULL);				/* ASN1_item_free */
 	DSA_SIG_free(NULL);
 	DSA_free(NULL);
 	DSA_meth_free(NULL);
-	ECDSA_SIG_free(NULL);
+	ECDSA_SIG_free(NULL);				/* ASN1_item_free */
 	EC_GROUP_clear_free(NULL);
 	EC_GROUP_free(NULL);
 	EC_KEY_free(NULL);
 	EC_POINT_clear_free(NULL);
 	EC_POINT_free(NULL);
-	EDIPARTYNAME_free(NULL);
+	EDIPARTYNAME_free(NULL);			/* ASN1_item_free */
 #ifndef OPENSSL_NO_ENGINE
 	ENGINE_free(NULL);
 #endif
-	ESS_CERT_ID_free(NULL);
-	ESS_ISSUER_SERIAL_free(NULL);
-	ESS_SIGNING_CERT_free(NULL);
+	ESS_CERT_ID_free(NULL);				/* ASN1_item_free */
+	ESS_ISSUER_SERIAL_free(NULL);			/* ASN1_item_free */
+	ESS_SIGNING_CERT_free(NULL);			/* ASN1_item_free */
 	EVP_CIPHER_CTX_free(NULL);
 	EVP_MD_CTX_free(NULL);
 	EVP_PKEY_CTX_free(NULL);
 	EVP_PKEY_asn1_free(NULL);
 	EVP_PKEY_free(NULL);
 	EVP_PKEY_meth_free(NULL);
-	EXTENDED_KEY_USAGE_free(NULL);
-	GENERAL_NAMES_free(NULL);
-	GENERAL_NAME_free(NULL);
-	GENERAL_SUBTREE_free(NULL);
-	GOST_CIPHER_PARAMS_free(NULL);
+	EXTENDED_KEY_USAGE_free(NULL);			/* ASN1_item_free */
+	GENERAL_NAMES_free(NULL);			/* ASN1_item_free */
+	GENERAL_NAME_free(NULL);			/* ASN1_item_free */
+	GENERAL_SUBTREE_free(NULL);			/* ASN1_item_free */
+	GOST_CIPHER_PARAMS_free(NULL);			/* ASN1_item_free */
 	GOST_KEY_free(NULL);
 	HMAC_CTX_free(NULL);
-	ISSUING_DIST_POINT_free(NULL);
-	NAME_CONSTRAINTS_free(NULL);
+	ISSUING_DIST_POINT_free(NULL);			/* ASN1_item_free */
+	NAME_CONSTRAINTS_free(NULL);			/* ASN1_item_free */
 	NCONF_free(NULL);
 	NCONF_free_data(NULL);
-	NETSCAPE_CERT_SEQUENCE_free(NULL);
-	NETSCAPE_SPKAC_free(NULL);
-	NETSCAPE_SPKI_free(NULL);
-	NETSCAPE_X509_free(NULL);
-	NOTICEREF_free(NULL);
-	OCSP_BASICRESP_free(NULL);
-	OCSP_CERTID_free(NULL);
-	OCSP_CERTSTATUS_free(NULL);
-	OCSP_CRLID_free(NULL);
-	OCSP_ONEREQ_free(NULL);
-	OCSP_REQINFO_free(NULL);
-	OCSP_REQUEST_free(NULL);
+	NETSCAPE_CERT_SEQUENCE_free(NULL);		/* ASN1_item_free */
+	NETSCAPE_SPKAC_free(NULL);			/* ASN1_item_free */
+	NETSCAPE_SPKI_free(NULL);			/* ASN1_item_free */
+	NETSCAPE_X509_free(NULL);			/* ASN1_item_free */
+	NOTICEREF_free(NULL);				/* ASN1_item_free */
+	OCSP_BASICRESP_free(NULL);			/* ASN1_item_free */
+	OCSP_CERTID_free(NULL);				/* ASN1_item_free */
+	OCSP_CERTSTATUS_free(NULL);			/* ASN1_item_free */
+	OCSP_CRLID_free(NULL);				/* ASN1_item_free */
+	OCSP_ONEREQ_free(NULL);				/* ASN1_item_free */
+	OCSP_REQINFO_free(NULL);			/* ASN1_item_free */
+	OCSP_REQUEST_free(NULL);			/* ASN1_item_free */
 	OCSP_REQ_CTX_free(NULL);
-	OCSP_RESPBYTES_free(NULL);
-	OCSP_RESPDATA_free(NULL);
-	OCSP_RESPID_free(NULL);
-	OCSP_RESPONSE_free(NULL);
-	OCSP_REVOKEDINFO_free(NULL);
-	OCSP_SERVICELOC_free(NULL);
-	OCSP_SIGNATURE_free(NULL);
-	OCSP_SINGLERESP_free(NULL);
-	OTHERNAME_free(NULL);
-	PBEPARAM_free(NULL);
-	PBKDF2PARAM_free(NULL);
-	PKCS12_BAGS_free(NULL);
-	PKCS12_MAC_DATA_free(NULL);
-	PKCS12_SAFEBAG_free(NULL);
-	PKCS12_free(NULL);
-	PKCS7_DIGEST_free(NULL);
-	PKCS7_ENCRYPT_free(NULL);
-	PKCS7_ENC_CONTENT_free(NULL);
-	PKCS7_ENVELOPE_free(NULL);
-	PKCS7_ISSUER_AND_SERIAL_free(NULL);
-	PKCS7_RECIP_INFO_free(NULL);
-	PKCS7_SIGNED_free(NULL);
-	PKCS7_SIGNER_INFO_free(NULL);
-	PKCS7_SIGN_ENVELOPE_free(NULL);
-	PKCS7_free(NULL);
-	PKCS8_PRIV_KEY_INFO_free(NULL);
-	PKEY_USAGE_PERIOD_free(NULL);
-	POLICYINFO_free(NULL);
-	POLICYQUALINFO_free(NULL);
-	POLICY_CONSTRAINTS_free(NULL);
-	POLICY_MAPPING_free(NULL);
-	PROXY_CERT_INFO_EXTENSION_free(NULL);
-	PROXY_POLICY_free(NULL);
-	RSA_PSS_PARAMS_free(NULL);
+	OCSP_RESPBYTES_free(NULL);			/* ASN1_item_free */
+	OCSP_RESPDATA_free(NULL);			/* ASN1_item_free */
+	OCSP_RESPID_free(NULL);				/* ASN1_item_free */
+	OCSP_RESPONSE_free(NULL);			/* ASN1_item_free */
+	OCSP_REVOKEDINFO_free(NULL);			/* ASN1_item_free */
+	OCSP_SERVICELOC_free(NULL);			/* ASN1_item_free */
+	OCSP_SIGNATURE_free(NULL);			/* ASN1_item_free */
+	OCSP_SINGLERESP_free(NULL);			/* ASN1_item_free */
+	OTHERNAME_free(NULL);				/* ASN1_item_free */
+	PBEPARAM_free(NULL);				/* ASN1_item_free */
+	PBKDF2PARAM_free(NULL);				/* ASN1_item_free */
+	PKCS12_BAGS_free(NULL);				/* ASN1_item_free */
+	PKCS12_MAC_DATA_free(NULL);			/* ASN1_item_free */
+	PKCS12_SAFEBAG_free(NULL);			/* ASN1_item_free */
+	PKCS12_free(NULL);				/* ASN1_item_free */
+	PKCS7_DIGEST_free(NULL);			/* ASN1_item_free */
+	PKCS7_ENCRYPT_free(NULL);			/* ASN1_item_free */
+	PKCS7_ENC_CONTENT_free(NULL);			/* ASN1_item_free */
+	PKCS7_ENVELOPE_free(NULL);			/* ASN1_item_free */
+	PKCS7_ISSUER_AND_SERIAL_free(NULL);		/* ASN1_item_free */
+	PKCS7_RECIP_INFO_free(NULL);			/* ASN1_item_free */
+	PKCS7_SIGNED_free(NULL);			/* ASN1_item_free */
+	PKCS7_SIGNER_INFO_free(NULL);			/* ASN1_item_free */
+	PKCS7_SIGN_ENVELOPE_free(NULL);			/* ASN1_item_free */
+	PKCS7_free(NULL);				/* ASN1_item_free */
+	PKCS8_PRIV_KEY_INFO_free(NULL);			/* ASN1_item_free */
+	PKEY_USAGE_PERIOD_free(NULL);			/* ASN1_item_free */
+	POLICYINFO_free(NULL);				/* ASN1_item_free */
+	POLICYQUALINFO_free(NULL);			/* ASN1_item_free */
+	POLICY_CONSTRAINTS_free(NULL);			/* ASN1_item_free */
+	POLICY_MAPPING_free(NULL);			/* ASN1_item_free */
+	PROXY_CERT_INFO_EXTENSION_free(NULL);		/* ASN1_item_free */
+	PROXY_POLICY_free(NULL);			/* ASN1_item_free */
+	RSA_PSS_PARAMS_free(NULL);			/* ASN1_item_free */
 	RSA_free(NULL);
 	RSA_meth_free(NULL);
-	SXNETID_free(NULL);
-	SXNET_free(NULL);
-	TS_ACCURACY_free(NULL);
-	TS_MSG_IMPRINT_free(NULL);
+	SXNETID_free(NULL);				/* ASN1_item_free */
+	SXNET_free(NULL);				/* ASN1_item_free */
+	TS_ACCURACY_free(NULL);				/* ASN1_item_free */
+	TS_MSG_IMPRINT_free(NULL);			/* ASN1_item_free */
 	TS_REQ_ext_free(NULL);
-	TS_REQ_free(NULL);
+	TS_REQ_free(NULL);				/* ASN1_item_free */
 	TS_RESP_CTX_free(NULL);
-	TS_RESP_free(NULL);
-	TS_STATUS_INFO_free(NULL);
+	TS_RESP_free(NULL);				/* ASN1_item_free */
+	TS_STATUS_INFO_free(NULL);			/* ASN1_item_free */
 	TS_TST_INFO_ext_free(NULL);
-	TS_TST_INFO_free(NULL);
+	TS_TST_INFO_free(NULL);				/* ASN1_item_free */
 	TS_VERIFY_CTX_free(NULL);
 	TXT_DB_free(NULL);
 	UI_free(NULL);
-	USERNOTICE_free(NULL);
+	USERNOTICE_free(NULL);				/* ASN1_item_free */
 	X509V3_conf_free(NULL);
-	X509_ALGOR_free(NULL);
-	X509_ATTRIBUTE_free(NULL);
-	X509_CERT_AUX_free(NULL);
-	X509_CERT_PAIR_free(NULL);
-	X509_CINF_free(NULL);
-	X509_CRL_INFO_free(NULL);
-	X509_CRL_free(NULL);
-	X509_EXTENSION_free(NULL);
+	X509_ALGOR_free(NULL);				/* ASN1_item_free */
+	X509_ATTRIBUTE_free(NULL);			/* ASN1_item_free */
+	X509_CERT_AUX_free(NULL);			/* ASN1_item_free */
+	X509_CERT_PAIR_free(NULL);			/* ASN1_item_free */
+	X509_CINF_free(NULL);				/* ASN1_item_free */
+	X509_CRL_INFO_free(NULL);			/* ASN1_item_free */
+	X509_CRL_free(NULL);				/* ASN1_item_free */
+	X509_EXTENSION_free(NULL);			/* ASN1_item_free */
 	X509_INFO_free(NULL);
 	X509_LOOKUP_free(NULL);
-	X509_NAME_ENTRY_free(NULL);
-	X509_NAME_free(NULL);
+	X509_NAME_ENTRY_free(NULL);			/* ASN1_item_free */
+	X509_NAME_free(NULL);				/* ASN1_item_free */
 	X509_PKEY_free(NULL);
-	X509_PUBKEY_free(NULL);
-	X509_REQ_INFO_free(NULL);
-	X509_REQ_free(NULL);
-	X509_REVOKED_free(NULL);
-	X509_SIG_free(NULL);
+	X509_PUBKEY_free(NULL);				/* ASN1_item_free */
+	X509_REQ_INFO_free(NULL);			/* ASN1_item_free */
+	X509_REQ_free(NULL);				/* ASN1_item_free */
+	X509_REVOKED_free(NULL);			/* ASN1_item_free */
+	X509_SIG_free(NULL);				/* ASN1_item_free */
 	X509_STORE_CTX_free(NULL);
 	X509_STORE_free(NULL);
-	X509_VAL_free(NULL);
+	X509_VAL_free(NULL);				/* ASN1_item_free */
 	X509_VERIFY_PARAM_free(NULL);
 	X509_email_free(NULL);
-	X509_free(NULL);
+	X509_free(NULL);				/* ASN1_item_free */
 	X509_policy_tree_free(NULL);
 	_CONF_free_data(NULL);
 
