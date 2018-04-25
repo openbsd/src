a354b791415658526784b7fc18ddb2e422b3cd3f
mlarkin <mlarkin@openbsd.org>
2018-04-25 15:49:48 +0000

    vmd(8)'s early error messages weren't visible when started via /etc/rc (such as errors relating to not having VMX/etc). Change the log_init to log to syslog so at least we have some chance of seeing these errors.
    requested and ok beck@

--- usr.sbin/vmd/vmd.c
+++ usr.sbin/vmd/vmd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmd.c,v 1.83 2018/04/21 07:03:06 mlarkin Exp $	*/
+/*	$OpenBSD: vmd.c,v 1.84 2018/04/25 15:49:48 mlarkin Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -662,8 +662,7 @@ main(int argc, char **argv)
 	const char		*errp, *title = NULL;
 	int			 argc0 = argc;
 
-	/* log to stderr until daemonized */
-	log_init(1, LOG_DAEMON);
+	log_init(0, LOG_DAEMON);
 
 	if ((env = calloc(1, sizeof(*env))) == NULL)
 		fatal("calloc: env");
