9f2c39383ee470c5b76fb828f64ea58341b214e9
kettenis <kettenis@openbsd.org>
2018-05-02 13:20:12 +0000

    Revert previous commit; the XR17V35X probe that was added accesses registers that aren't guaranteed to be there and may even belong to a different device. This triggers a fault on hppa machines like the C3000 for example.

--- sys/dev/ic/comreg.h
+++ sys/dev/ic/comreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: comreg.h,v 1.18 2018/04/15 15:07:25 jcs Exp $	*/
+/*	$OpenBSD: comreg.h,v 1.19 2018/05/02 13:20:12 kettenis Exp $	*/
 /*	$NetBSD: comreg.h,v 1.8 1996/02/05 23:01:50 scottr Exp $	*/
 
 /*
@@ -181,11 +181,6 @@
 #define ISR_RXPL	0x10	/* negative receive data polarity */
 
 #define	COM_NPORTS	8
-
-/* Exar XR17V35X */
-#define UART_EXAR_INT0	0x80
-#define UART_EXAR_SLEEP	0x8b	/* Sleep mode */
-#define UART_EXAR_DVID	0x8d	/* Device identification */
 
 /*
  * WARNING: Serial console is assumed to be at COM1 address
