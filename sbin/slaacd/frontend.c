4b1b7c91ccf7efa0b119c4a4ead123c1825d223a
florian <florian@openbsd.org>
2018-05-18 11:19:03 +0000

    dead stores; from llvm scan-build

--- sbin/slaacd/frontend.c
+++ sbin/slaacd/frontend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: frontend.c,v 1.17 2018/05/17 13:39:00 florian Exp $	*/
+/*	$OpenBSD: frontend.c,v 1.18 2018/05/18 11:19:03 florian Exp $	*/
 
 /*
  * Copyright (c) 2017 Florian Obser <florian@openbsd.org>
@@ -678,7 +678,7 @@ handle_route_message(struct rt_msghdr *rtm, struct sockaddr **rti_info)
 	struct sockaddr_rtlabel		*rl;
 	struct in6_addr			*in6;
 	int64_t				 id, pid;
-	int				 flags, xflags, if_index;
+	int				 xflags, if_index;
 	char				 ifnamebuf[IFNAMSIZ];
 	char				*if_name;
 	char				**ap, *argv[4], *p;
@@ -695,7 +695,6 @@ handle_route_message(struct rt_msghdr *rtm, struct sockaddr **rti_info)
 			    &if_index, sizeof(if_index));
 		} else {
 			xflags = get_xflags(if_name);
-			flags = get_flags(if_name);
 			if (!(xflags & IFXF_AUTOCONF6)) {
 				log_debug("RTM_IFINFO: %s(%d) no(longer) "
 				   "autoconf6", if_name, ifm->ifm_index);
@@ -901,9 +900,6 @@ icmp6_receive(int fd, short events, void *arg)
 	ssize_t			 len;
 	int			 if_index = 0, *hlimp = NULL;
 	char			 ntopbuf[INET6_ADDRSTRLEN], ifnamebuf[IFNAMSIZ];
-	uint8_t			*p;
-
-	p = icmp6ev.answer;
 
 	if ((len = recvmsg(fd, &icmp6ev.rcvmhdr, 0)) < 0) {
 		log_warn("recvmsg");
