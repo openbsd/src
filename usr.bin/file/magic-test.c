e554841d642d1a9918f7773c47f4d54b22ce0d43
nicm <nicm@openbsd.org>
2018-06-14 19:51:11 +0000

    Return from magic_test does not need to be copied because the caller will do so anyway and it was just being leaked. ok brynet deraadt

--- usr.bin/file/magic-test.c
+++ usr.bin/file/magic-test.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: magic-test.c,v 1.25 2017/04/18 14:16:48 nicm Exp $ */
+/* $OpenBSD: magic-test.c,v 1.26 2018/06/14 19:51:11 nicm Exp $ */
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -1404,10 +1404,10 @@ magic_test(struct magic *m, const void *base, size_t size, int flags)
 	if (*ms.out != '\0') {
 		if (flags & MAGIC_TEST_MIME) {
 			if (ms.mimetype != NULL)
-				return (xstrdup(ms.mimetype));
+				return (ms.mimetype);
 			return (NULL);
 		}
-		return (xstrdup(ms.out));
+		return (ms.out);
 	}
 	return (NULL);
 }
