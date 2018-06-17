472dffbc012f90084c8c71f0da051734d8d6d609
deraadt <deraadt@openbsd.org>
2018-06-17 14:58:27 +0000

    incorrect pointer used, hence gecos expansion was ignored ok millert

--- usr.bin/finger/util.c
+++ usr.bin/finger/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.32 2015/12/26 20:51:35 guenther Exp $	*/
+/*	$OpenBSD: util.c,v 1.33 2018/06/17 14:58:27 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1989 The Regents of the University of California.
@@ -104,7 +104,7 @@ userinfo(PERSON *pn, struct passwd *pw)
 	if (!(p = strsep(&bp, ",")))
 		return;
 	expandusername(p, pw->pw_name, name, sizeof(name));
-	if (stravis(&pn->realname, p, VIS_SAFE|VIS_NOSLASH) == -1)
+	if (stravis(&pn->realname, name, VIS_SAFE|VIS_NOSLASH) == -1)
 		err(1, "stravis");
 	if ((p = strsep(&bp, ",")) && *p) {
 		if (stravis(&pn->office, p, VIS_SAFE|VIS_NOSLASH) == -1)
