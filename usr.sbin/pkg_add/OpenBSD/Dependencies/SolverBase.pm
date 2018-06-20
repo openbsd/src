caf3361305896a399d1c5b4b102fa1887fe8e6d9
espie <espie@openbsd.org>
2018-06-20 08:53:49 +0000

    prepare to split half of the dependencies code into a manageable subclass

---
+++ usr.sbin/pkg_add/OpenBSD/Dependencies/SolverBase.pm
@@ -0,0 +1,22 @@
+# ex:ts=8 sw=4:
+# $OpenBSD: SolverBase.pm,v 1.1 2018/06/20 08:53:49 espie Exp $
+#
+# Copyright (c) 2005-2018 Marc Espie <espie@openbsd.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+
+use strict;
+use warnings;
+
+# placeholder for common dependencies code between pkg_add/pkg_delete/pkg_create
+
+1;
