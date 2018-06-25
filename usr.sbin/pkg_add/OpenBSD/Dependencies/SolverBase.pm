e18c1491dea16ec44dcf5d6080d6594a4176e86b
espie <espie@openbsd.org>
2018-06-25 18:18:00 +0000

    fix ::lookup interface return, oops

--- usr.sbin/pkg_add/OpenBSD/Dependencies/SolverBase.pm
+++ usr.sbin/pkg_add/OpenBSD/Dependencies/SolverBase.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: SolverBase.pm,v 1.6 2018/06/25 12:34:40 espie Exp $
+# $OpenBSD: SolverBase.pm,v 1.7 2018/06/25 18:18:00 espie Exp $
 #
 # Copyright (c) 2005-2018 Marc Espie <espie@openbsd.org>
 #
@@ -213,8 +213,9 @@ sub find_in_already_done
 		$obj->{definition_list} = $d;
 		$state->say("Found tag #1 in #2", $obj->stringize, $dep)
 		    if $state->verbose >= 3;
+		return $dep;
 	}
-	return $r;
+	return undef;
 }
 
 sub find_in_plist
