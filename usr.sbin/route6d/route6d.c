0cd16294720f80951b217b38812828124814ec15
deraadt <deraadt@openbsd.org>
2018-07-01 15:42:37 +0000

    more RTM_LOCK removals

--- usr.sbin/route6d/route6d.c
+++ usr.sbin/route6d/route6d.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: route6d.c,v 1.95 2018/07/01 08:53:03 mpi Exp $	*/
+/*	$OpenBSD: route6d.c,v 1.96 2018/07/01 15:42:37 deraadt Exp $	*/
 /*	$KAME: route6d.c,v 1.111 2006/10/25 06:38:13 jinmei Exp $	*/
 
 /*
@@ -1645,7 +1645,6 @@ rtrecv(void)
 	case RTM_MISS:
 	case RTM_RESOLVE:
 	case RTM_GET:
-	case RTM_LOCK:
 		/* should already be handled */
 		fatalx("rtrecv: never reach here");
 		/*NOTREACHED*/
@@ -2260,7 +2259,6 @@ do { \
 	RTTYPE("LOSING", RTM_LOSING);
 	RTTYPE("REDIRECT", RTM_REDIRECT);
 	RTTYPE("MISS", RTM_MISS);
-	RTTYPE("LOCK", RTM_LOCK);
 	RTTYPE("RESOLVE", RTM_RESOLVE);
 	RTTYPE("NEWADDR", RTM_NEWADDR);
 	RTTYPE("DELADDR", RTM_DELADDR);
