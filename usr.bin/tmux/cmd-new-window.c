5f8a0990b2cd540e904359fde88920d87beb3399
nicm <nicm@openbsd.org>
2018-07-15 06:57:13 +0000

    Fix new-window -k, GitHub issue 1403.

--- usr.bin/tmux/cmd-new-window.c
+++ usr.bin/tmux/cmd-new-window.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-new-window.c,v 1.75 2018/05/24 09:42:49 nicm Exp $ */
+/* $OpenBSD: cmd-new-window.c,v 1.76 2018/07/15 06:57:13 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -103,7 +103,6 @@ cmd_new_window_exec(struct cmd *self, struct cmdq_item *item)
 	else
 		name = NULL;
 
-	wl = NULL;
 	if (idx != -1)
 		wl = winlink_find_by_index(&s->windows, idx);
 	if (wl != NULL && args_has(args, 'k')) {
