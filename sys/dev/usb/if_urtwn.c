ccaa33a796f42d15c0934bb150b64659790b55a0
sthen <sthen@openbsd.org>
2018-07-19 17:34:22 +0000

    attach RTL8188EU under AboCom's vendor id. From FreeBSD via Mikhail <mp39590 at gmail>

--- sys/dev/usb/if_urtwn.c
+++ sys/dev/usb/if_urtwn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_urtwn.c,v 1.77 2017/12/14 09:26:11 benno Exp $	*/
+/*	$OpenBSD: if_urtwn.c,v 1.78 2018/07/19 17:34:22 sthen Exp $	*/
 
 /*-
  * Copyright (c) 2010 Damien Bergamini <damien.bergamini@free.fr>
@@ -306,6 +306,7 @@ static const struct urtwn_type {
 	URTWN_DEV_8192CU(TRENDNET,	RTL8192CU),
 	URTWN_DEV_8192CU(ZYXEL,		RTL8192CU),
 	/* URTWN_RTL8188E */
+	URTWN_DEV_8188EU(ABOCOM,	RTL8188EU),
 	URTWN_DEV_8188EU(DLINK,		DWA123D1),
 	URTWN_DEV_8188EU(DLINK,		DWA125D1),
 	URTWN_DEV_8188EU(ELECOM,	WDC150SU2M),
