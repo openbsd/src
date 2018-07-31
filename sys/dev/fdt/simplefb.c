f3d31b3e2e9e6d810d80ce71b8798fed7b3f1bf1
fcambus <fcambus@openbsd.org>
2018-07-31 17:25:55 +0000

    Display color depth alongside resolution when attaching simplefb(4).
    OK kettenis@, deraadt@

--- sys/dev/fdt/simplefb.c
+++ sys/dev/fdt/simplefb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: simplefb.c,v 1.3 2017/12/18 10:13:45 kettenis Exp $	*/
+/*	$OpenBSD: simplefb.c,v 1.4 2018/07/31 17:25:55 fcambus Exp $	*/
 /*
  * Copyright (c) 2016 Mark Kettenis
  *
@@ -154,7 +154,7 @@ simplefb_attach(struct device *parent, struct device *self, void *aux)
 		ri->ri_flg &= ~RI_CLEAR;
 	}
 
-	printf(": %dx%d\n", ri->ri_width, ri->ri_height);
+	printf(": %dx%d, %dbpp\n", ri->ri_width, ri->ri_height, ri->ri_depth);
 
 	ri->ri_flg |= RI_VCONS;
 	rasops_init(ri, SIMPLEFB_HEIGHT, SIMPLEFB_WIDTH);
