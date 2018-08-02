5195589a9b8cbe53f3470d2b36c8c469d63ba772
mestre <mestre@openbsd.org>
2018-08-02 06:43:31 +0000

    Actually order the promises in their canonical form, missed that in my previous commit.
    heads up and OK tb@

--- usr.sbin/eigrpd/eigrpd.c
+++ usr.sbin/eigrpd/eigrpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eigrpd.c,v 1.22 2018/08/02 06:28:35 mestre Exp $ */
+/*	$OpenBSD: eigrpd.c,v 1.23 2018/08/02 06:43:31 mestre Exp $ */
 
 /*
  * Copyright (c) 2015 Renato Westphal <renato@openbsd.org>
@@ -271,7 +271,7 @@ main(int argc, char *argv[])
 	    eigrpd_conf->rdomain) == -1)
 		fatalx("kr_init failed");
 
-	if (pledge("inet rpath cpath stdio sendfd", NULL) == -1)
+	if (pledge("stdio rpath cpath inet sendfd", NULL) == -1)
 		fatal("pledge");
 
 	event_dispatch();
