3a412390be15d48afa55754d7987d2990770626e
schwarze <schwarze@openbsd.org>
2018-08-18 20:17:58 +0000

    If a tbl(7) column contains both text cells and numeric cells, horizontally align the numbers in the same way as groff does.

--- usr.bin/mandoc/out.c
+++ usr.bin/mandoc/out.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: out.c,v 1.43 2018/08/18 16:44:52 schwarze Exp $ */
+/*	$OpenBSD: out.c,v 1.44 2018/08/18 20:17:58 schwarze Exp $ */
 /*
  * Copyright (c) 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011,2014,2015,2017,2018 Ingo Schwarze <schwarze@openbsd.org>
@@ -166,6 +166,7 @@ tblcalc(struct rofftbl *tbl, const struct tbl_span *sp,
 	}
 
 	/*
+	 * Align numbers with text.
 	 * Count columns to equalize and columns to maximize.
 	 * Find maximum width of the columns to equalize.
 	 * Find total width of the columns *not* to maximize.
@@ -175,6 +176,10 @@ tblcalc(struct rofftbl *tbl, const struct tbl_span *sp,
 	ewidth = xwidth = 0;
 	for (icol = 0; icol <= maxcol; icol++) {
 		col = tbl->cols + icol;
+		if (col->width > col->nwidth)
+			col->decimal += (col->width - col->nwidth) / 2;
+		else
+			col->width = col->nwidth;
 		if (col->spacing == SIZE_MAX || icol == maxcol)
 			col->spacing = 3;
 		if (col->flags & TBL_CELL_EQUAL) {
@@ -356,10 +361,10 @@ tblcalc_number(struct rofftbl *tbl, struct roffcol *col,
 		sz += col->decimal - d;
 		d = col->decimal;
 	} else
-		col->width += d - col->decimal;
+		col->nwidth += d - col->decimal;
 
-	if (sz > col->width)
-		col->width = sz;
+	if (sz > col->nwidth)
+		col->nwidth = sz;
 	if (d > col->decimal)
 		col->decimal = d;
 }
