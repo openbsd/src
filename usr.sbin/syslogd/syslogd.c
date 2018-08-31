2091c1cbd81c9eb0437f36d35cf798b135db1069
bluhm <bluhm@openbsd.org>
2018-08-31 19:06:08 +0000

    Unsetting Initialized during syslogd die() is a relic from the time when we had real signal handlers.  But now we use libevent, so remove the old logic. OK deraadt@ millert@

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.255 2018/07/17 13:51:47 djm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.256 2018/08/31 19:06:08 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2014-2017 Alexander Bluhm <bluhm@genua.de>
@@ -2277,9 +2277,7 @@ __dead void
 die(int signo)
 {
 	struct filed *f;
-	int was_initialized = Initialized;
 
-	Initialized = 0;		/* Don't log SIGCHLDs */
 	SIMPLEQ_FOREACH(f, &Files, f_next) {
 		/* flush any pending output */
 		if (f->f_prevcount)
@@ -2294,7 +2292,6 @@ die(int signo)
 			f->f_dropped = 0;
 		}
 	}
-	Initialized = was_initialized;
 	dropped_warn(&init_dropped, "during initialization");
 	dropped_warn(&file_dropped, "to file");
 	dropped_warn(&tcpbuf_dropped, "to remote loghost");
