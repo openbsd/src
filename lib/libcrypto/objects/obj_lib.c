3e238a8b223d4aaeab8cd5927c9512200a705f14
tb <tb@openbsd.org>
2018-09-08 10:31:24 +0000

    indent labels

--- lib/libcrypto/objects/obj_lib.c
+++ lib/libcrypto/objects/obj_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: obj_lib.c,v 1.14 2017/01/29 17:49:23 beck Exp $ */
+/* $OpenBSD: obj_lib.c,v 1.15 2018/09/08 10:31:24 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -109,7 +109,7 @@ OBJ_dup(const ASN1_OBJECT *o)
 	    ASN1_OBJECT_FLAG_DYNAMIC_STRINGS | ASN1_OBJECT_FLAG_DYNAMIC_DATA);
 	return (r);
 
-err:
+ err:
 	OBJerror(ERR_R_MALLOC_FAILURE);
 	free(ln);
 	free(sn);
