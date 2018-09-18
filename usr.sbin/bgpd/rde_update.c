442d622cc0ae3ae55876bdb50429370af9ab6bcc
sthen <sthen@openbsd.org>
2018-09-18 16:54:01 +0000

    s/mulitprotocol/multiprotocol/ in error text

--- usr.sbin/bgpd/rde_update.c
+++ usr.sbin/bgpd/rde_update.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_update.c,v 1.98 2018/09/09 12:33:51 claudio Exp $ */
+/*	$OpenBSD: rde_update.c,v 1.99 2018/09/18 16:54:01 sthen Exp $ */
 
 /*
  * Copyright (c) 2004 Claudio Jeker <claudio@openbsd.org>
@@ -1167,7 +1167,7 @@ up_dump_mp_reach(u_char *buf, u_int16_t *len, struct rde_peer *peer,
 		return (-2);
 
 	if (upa->mpattr_len == 0 || upa->mpattr == NULL)
-		fatalx("mulitprotocol update without MP attrs");
+		fatalx("multiprotocol update without MP attrs");
 
 	datalen += upa->mpattr_len;
 	wpos -= upa->mpattr_len;
