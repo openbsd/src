7a9e63a7a3358b1942dc6d7128de38847820e17d
fcambus <fcambus@openbsd.org>
2018-09-24 11:10:34 +0000

    Add "Spleen 5x8" to wsfont, a font targetted at small OLED displays to be used with devices handled by ssdfb(4). It contains all printable ASCII characters (96 glyphes).
    The font is 2-Clause BSD licensed and is my original creation.
    
    OK patrick@

--- sys/dev/wsfont/wsfont.c
+++ sys/dev/wsfont/wsfont.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wsfont.c,v 1.52 2017/09/08 05:36:53 deraadt Exp $ */
+/*	$OpenBSD: wsfont.c,v 1.53 2018/09/24 11:10:34 fcambus Exp $ */
 /*	$NetBSD: wsfont.c,v 1.17 2001/02/07 13:59:24 ad Exp $	*/
 
 /*-
@@ -42,6 +42,11 @@
 #include "wsfont_glue.h"	/* NRASOPS_ROTATION */
 
 #undef HAVE_FONT
+
+#ifdef FONT_SPLEEN5x8
+#define HAVE_FONT 1
+#include <dev/wsfont/spleen5x8.h>
+#endif
 
 #ifdef FONT_BOLD8x16
 #define HAVE_FONT 1
@@ -105,6 +110,9 @@ static struct font builtin_fonts[] = {
 #endif
 #ifdef FONT_GALLANT12x22
 	BUILTIN_FONT(gallant12x22, 3),
+#endif
+#ifdef FONT_SPLEEN5x8
+	BUILTIN_FONT(spleen5x8, 4),
 #endif
 #undef BUILTIN_FONT
 };
