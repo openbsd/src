470b5f92fd7cd5285f551d01caae23c1035c8e76
benno <benno@openbsd.org>
2018-09-27 12:34:06 +0000

    whitepspace fix

--- usr.sbin/ospfd/ospfe.c
+++ usr.sbin/ospfd/ospfe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospfe.c,v 1.102 2018/08/29 08:43:17 remi Exp $ */
+/*	$OpenBSD: ospfe.c,v 1.103 2018/09/27 12:34:06 benno Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -466,7 +466,7 @@ ospfe_dispatch_main(int fd, short event, void *bula)
 			TAILQ_INIT(&ctl_conns);
 			control_listen();
 			if (pledge("stdio inet mcast", NULL) == -1)
-                		fatal("pledge");
+				fatal("pledge");
 			break;
 		default:
 			log_debug("ospfe_dispatch_main: error handling imsg %d",
