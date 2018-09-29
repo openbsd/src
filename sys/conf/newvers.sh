8dd19e7b62c5225d627ed07bd334147b265c4882
deraadt <deraadt@openbsd.org>
2018-09-29 16:00:44 +0000

    unmark -beta.  There is still development happening, and we aren't locked in stone yet, but the clock starts ticking...

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.163 2018/08/10 20:27:01 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.164 2018/09/29 16:00:44 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,10 +71,10 @@ ost="OpenBSD"
 osr="6.4"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
-#define STATUS "-current"		/* just after a release */
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
+#define STATUS "-current"		/* just after a release */
 #endif
 
 const char ostype[] = "${ost}";
