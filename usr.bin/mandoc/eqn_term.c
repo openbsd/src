eb9ded0c4a3992333233370d6b726e4fe954d7d9
schwarze <schwarze@openbsd.org>
2018-10-02 12:14:44 +0000

    Render the eqn(7) "sqrt" function as U+221A in UTF-8 output. This also agrees with what groff does. Suggested by an attendee of EuroBSDCon 2018 in Bucuresti. Written on the plane Bucuresti-Frankfurt returning from EuroBSDCon.

--- usr.bin/mandoc/eqn_term.c
+++ usr.bin/mandoc/eqn_term.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eqn_term.c,v 1.13 2017/08/23 21:56:04 schwarze Exp $ */
+/*	$OpenBSD: eqn_term.c,v 1.14 2018/10/02 12:14:44 schwarze Exp $ */
 /*
  * Copyright (c) 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014, 2015, 2017 Ingo Schwarze <schwarze@openbsd.org>
@@ -104,7 +104,7 @@ eqn_box(struct termp *p, const struct eqn_box *bp)
 	/* Special box types. */
 
 	if (bp->pos == EQNPOS_SQRT) {
-		term_word(p, "sqrt");
+		term_word(p, "\\(sr");
 		if (bp->first != NULL) {
 			p->flags |= TERMP_NOSPACE;
 			eqn_box(p, bp->first);
