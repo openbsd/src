38768d0456291d2405ef62e25da4de509bd23d95
espie <espie@openbsd.org>
2018-10-04 09:17:00 +0000

    fix stupidity

--- usr.sbin/pkg_add/OpenBSD/Temp.pm
+++ usr.sbin/pkg_add/OpenBSD/Temp.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Temp.pm,v 1.28 2018/02/10 10:08:05 espie Exp $
+# $OpenBSD: Temp.pm,v 1.29 2018/10/04 09:17:00 espie Exp $
 #
 # Copyright (c) 2003-2005 Marc Espie <espie@openbsd.org>
 #
@@ -90,7 +90,8 @@ sub fh_file
 
 sub file
 {
-	return (fh_file("pkgout", sub { $files->{shift} = $$; })) [1];
+	return (fh_file("pkgout", 
+	    sub { my $n = shift; $files->{$n} = $$; })) [1];
 }
 
 sub reclaim
