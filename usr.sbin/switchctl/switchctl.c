b042077087a11405d83f26a5d0ba7b2ba95cb536
akoshibe <akoshibe@openbsd.org>
2018-10-24 18:06:21 +0000

    As per advice from claudio@, further restrict the pledge in switchctl(8). Only stdio is needed when running the 'show *' commands. Everything else is only needed while parsing args and opening sockets.
    OK claudio@ deraadt@ phessler@

--- usr.sbin/switchctl/switchctl.c
+++ usr.sbin/switchctl/switchctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: switchctl.c,v 1.8 2018/10/21 21:10:24 akoshibe Exp $	*/
+/*	$OpenBSD: switchctl.c,v 1.9 2018/10/24 18:06:21 akoshibe Exp $	*/
 
 /*
  * Copyright (c) 2007-2015 Reyk Floeter <reyk@openbsd.org>
@@ -180,6 +180,9 @@ main(int argc, char *argv[])
 		}
 		err(1, "connect: %s", sock);
 	}
+
+	if (pledge("stdio", NULL) == -1)
+		err(1, "pledge");
 
 	if (res->ibuf != NULL)
 		ibuf = res->ibuf;
