b5b96391a4250ba122c3f04eef03f4d68dd9f381
krw <krw@openbsd.org>
2018-11-04 19:10:34 +0000

    Kill obsolete comment about the constraints imposed by the need to bounce the interface link state. We no longer bounce.

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.580 2018/11/04 16:32:11 krw Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.581 2018/11/04 19:10:34 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -598,12 +598,6 @@ main(int argc, char *argv[])
 		close(tailfd);
 	}
 
-	/*
-	 * Do the initial status check and possible force up before creating
-	 * the routing socket. If we bounce the interface down and up while
-	 * the routing socket is listening, the RTM_IFINFO message with the
-	 * RTF_UP flag reset will cause premature exit.
-	 */
 	interface_state(ifi);
 	if (!LINK_STATE_IS_UP(ifi->link_state))
 		interface_link_forceup(ifi->name, ioctlfd);
