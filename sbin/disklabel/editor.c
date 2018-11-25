e4a1d39e47744bf36c637c371e77c49b6a48a0b7
krw <krw@openbsd.org>
2018-11-25 17:01:20 +0000

    FS_BOOT partitions are just partitions. Not free(ish) space.
    ok deraadt@ as part of larger diff

--- sbin/disklabel/editor.c
+++ sbin/disklabel/editor.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: editor.c,v 1.351 2018/09/21 14:07:34 solene Exp $	*/
+/*	$OpenBSD: editor.c,v 1.352 2018/11/25 17:01:20 krw Exp $	*/
 
 /*
  * Copyright (c) 1997-2000 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -1443,7 +1443,6 @@ sort_partitions(struct disklabel *lp)
 
 	for (npartitions = 0, i = 0; i < lp->d_npartitions; i++) {
 		if (lp->d_partitions[i].p_fstype != FS_UNUSED &&
-		    lp->d_partitions[i].p_fstype != FS_BOOT &&
 		    DL_GETPSIZE(&lp->d_partitions[i]) != 0)
 			spp[npartitions++] = &lp->d_partitions[i];
 	}
