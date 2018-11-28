d95f7c87ff6b2619583da16f9be20d794c669703
bluhm <bluhm@openbsd.org>
2018-11-28 13:34:06 +0000

    This test does not terminate if malloc conf is preconfigured with J.  Clear that option to allow running full regress with paranoid malloc flags.  Also fix whitespace. OK otto@

--- regress/lib/libc/malloc/malloc_ulimit1/malloc_ulimit1.c
+++ regress/lib/libc/malloc/malloc_ulimit1/malloc_ulimit1.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malloc_ulimit1.c,v 1.3 2017/07/27 15:08:37 bluhm Exp $	*/
+/*	$OpenBSD: malloc_ulimit1.c,v 1.4 2018/11/28 13:34:06 bluhm Exp $	*/
 
 /* Public Domain, 2006, Otto Moerbeek <otto@drijf.net> */
 
@@ -14,13 +14,13 @@
  * 2006) where the allocation of the region itself succeeds, but the
  * page dir entry pages fails.
  * This in turn trips a "hole in directories" error.
- * Having a large (512M) ulimit -m helps a lot in triggering the 
+ * Having a large (512M) ulimit -m helps a lot in triggering the
  * problem. Note that you may need to run this test multiple times to
  * see the error.
 */
 
-#define	STARTI	1300
-#define FACTOR 	1024
+#define STARTI	1300
+#define FACTOR	1024
 
 int
 main()
@@ -29,6 +29,10 @@ main()
 	size_t sz;
 	int i;
 	void *p;
+	extern char *malloc_options;
+
+	/* This test takes forever with junking turned on. */
+	malloc_options = "jj";
 
 	if (getrlimit(RLIMIT_DATA, &lim) == -1)
 		err(1, "getrlimit");
