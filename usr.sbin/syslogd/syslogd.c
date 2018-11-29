86b998b1370a68c1496922c1c25ff658a11143c6
kn <kn@openbsd.org>
2018-11-29 21:10:51 +0000

    Honor SIGINT/QUIT when running in foreground
    Allows stopping `syslogd -F' with ^C.
    
    OK bluhm

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.256 2018/08/31 19:06:08 bluhm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.257 2018/11/29 21:10:51 kn Exp $	*/
 
 /*
  * Copyright (c) 2014-2017 Alexander Bluhm <bluhm@genua.de>
@@ -880,7 +880,7 @@ main(int argc, char *argv[])
 
 	signal_add(ev_hup, NULL);
 	signal_add(ev_term, NULL);
-	if (Debug) {
+	if (Debug || Foreground) {
 		signal_add(ev_int, NULL);
 		signal_add(ev_quit, NULL);
 	} else {
