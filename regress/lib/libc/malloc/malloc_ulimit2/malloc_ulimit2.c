a3144fb430d1ea50bbe9aa008f655315e6d576ac
bluhm <bluhm@openbsd.org>
2018-11-29 22:23:22 +0000

    This test does not terminate if malloc conf is preconfigured with J.  Clear that option to allow running full regress with paranoid malloc flags.  This is the same fix as for malloc_ulimit1.

--- regress/lib/libc/malloc/malloc_ulimit2/malloc_ulimit2.c
+++ regress/lib/libc/malloc/malloc_ulimit2/malloc_ulimit2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malloc_ulimit2.c,v 1.3 2017/07/27 15:08:37 bluhm Exp $	*/
+/*	$OpenBSD: malloc_ulimit2.c,v 1.4 2018/11/29 22:23:22 bluhm Exp $	*/
 
 /* Public Domain, 2006, Otto Moerbeek <otto@drijf.net> */
 
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define FACTOR 	1024
+#define FACTOR	1024
 
 int
 main()
@@ -18,6 +18,10 @@ main()
 	size_t sz;
 	int i;
 	void *p;
+	extern char *malloc_options;
+
+	/* This test takes forever with junking turned on. */
+	malloc_options = "jj";
 
 	if (getrlimit(RLIMIT_DATA, &lim) == -1)
 		err(1, "getrlimit");
