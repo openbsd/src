6a36bd7497ddfd09e0db88bd7c552372a48b482f
kettenis <kettenis@openbsd.org>
2019-01-05 18:46:36 +0000

    fix previous commit
    "please commit asap" deraadt@

--- sys/dev/pci/if_em_hw.h
+++ sys/dev/pci/if_em_hw.h
@@ -31,7 +31,7 @@
 
 *******************************************************************************/
 
-/* $OpenBSD: if_em_hw.h,v 1.77 2019/01/05 11:55:46 jsg Exp $ */
+/* $OpenBSD: if_em_hw.h,v 1.78 2019/01/05 18:46:36 kettenis Exp $ */
 /* $FreeBSD: if_em_hw.h,v 1.15 2005/05/26 23:32:02 tackerman Exp $ */
 
 /* if_em_hw.h
@@ -88,7 +88,7 @@ typedef enum {
 #define IS_ICH8(t) \
 	(t == em_ich8lan || t == em_ich9lan || t == em_ich10lan || \
 	 t == em_pchlan || t == em_pch2lan || t == em_pch_lpt || \
-	 t == em_pch_spt || em_pch_cnp)
+	 t == em_pch_spt || t == em_pch_cnp)
 
 typedef enum {
     em_eeprom_uninitialized = 0,
