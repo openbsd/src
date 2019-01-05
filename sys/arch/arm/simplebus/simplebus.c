ad434fd9e6cb47723d3e5ed46887978b88f72dd7
jsg <jsg@openbsd.org>
2019-01-05 03:42:19 +0000

    match arm64 and print unconfigured simplebus devices on armv7 ok dlg@ kettenis@

--- sys/arch/arm/simplebus/simplebus.c
+++ sys/arch/arm/simplebus/simplebus.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: simplebus.c,v 1.13 2017/04/27 22:41:46 kettenis Exp $ */
+/* $OpenBSD: simplebus.c,v 1.14 2019/01/05 03:42:19 jsg Exp $ */
 /*
  * Copyright (c) 2016 Patrick Wildt <patrick@blueri.se>
  *
@@ -115,6 +115,26 @@ simplebus_submatch(struct device *self, void *match, void *aux)
 	return 0;
 }
 
+int
+simplebus_print(void *aux, const char *pnp)
+{
+	struct fdt_attach_args *fa = aux;
+	char name[32];
+
+	if (!pnp)
+		return (QUIET);
+
+	if (OF_getprop(fa->fa_node, "name", name, sizeof(name)) > 0) {
+		name[sizeof(name) - 1] = 0;
+		printf("\"%s\"", name);
+	} else
+		printf("node %u", fa->fa_node);
+
+	printf(" at %s", pnp);
+
+	return (UNCONF);
+}
+
 /*
  * Look for a driver that wants to be attached to this node.
  */
@@ -180,7 +200,8 @@ simplebus_attach_node(struct device *self, int node)
 		OF_getpropintarray(node, "interrupts", fa.fa_intr, len);
 	}
 
-	config_found_sm(self, &fa, NULL, simplebus_submatch);
+	config_found_sm(self, &fa, sc->sc_early ? NULL : simplebus_print,
+	    simplebus_submatch);
 
 	free(fa.fa_reg, M_DEVBUF, fa.fa_nreg * sizeof(struct fdt_reg));
 	free(fa.fa_intr, M_DEVBUF, fa.fa_nintr * sizeof(uint32_t));
