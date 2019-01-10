7f72a6609de129767b9f4c5b76f078a437143dd0
kettenis <kettenis@openbsd.org>
2019-01-10 20:26:34 +0000

    Hold a read lock on the map while doing the actual device I/O during in physio(9) to prevent another thread from unmapping the memory and triggering an assertion or even corruption random physical memory pages.
    ok deraadt@
    
    Should fix:
    Reported-by: syzbot+b8e7faf688f8c9d341b1@syzkaller.appspotmail.com
    Reported-by: syzbot+b6a9255faa0605669432@syzkaller.appspotmail.com

--- sys/uvm/uvm_glue.c
+++ sys/uvm/uvm_glue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_glue.c,v 1.73 2017/05/08 09:32:19 mpi Exp $	*/
+/*	$OpenBSD: uvm_glue.c,v 1.74 2019/01/10 20:26:34 kettenis Exp $	*/
 /*	$NetBSD: uvm_glue.c,v 1.44 2001/02/06 19:54:44 eeh Exp $	*/
 
 /* 
@@ -103,33 +103,27 @@ uvm_kernacc(caddr_t addr, size_t len, int rw)
 /*
  * uvm_vslock: wire user memory for I/O
  *
- * - called from physio and sys_sysctl
+ * - called from sys_sysctl
  */
-
 int
 uvm_vslock(struct proc *p, caddr_t addr, size_t len, vm_prot_t access_type)
 {
-	struct vm_map *map;
+	struct vm_map *map = &p->p_vmspace->vm_map;
 	vaddr_t start, end;
-	int rv;
 
-	map = &p->p_vmspace->vm_map;
 	start = trunc_page((vaddr_t)addr);
 	end = round_page((vaddr_t)addr + len);
 	if (end <= start)
 		return (EINVAL);
 
-	rv = uvm_fault_wire(map, start, end, access_type);
-
-	return (rv);
+	return uvm_fault_wire(map, start, end, access_type);
 }
 
 /*
  * uvm_vsunlock: unwire user memory wired by uvm_vslock()
  *
- * - called from physio and sys_sysctl
+ * - called from sys_sysctl
  */
-
 void
 uvm_vsunlock(struct proc *p, caddr_t addr, size_t len)
 {
@@ -137,8 +131,7 @@ uvm_vsunlock(struct proc *p, caddr_t addr, size_t len)
 
 	start = trunc_page((vaddr_t)addr);
 	end = round_page((vaddr_t)addr + len);
-	if (end <= start)
-		return;
+	KASSERT(end > start);
 
 	uvm_fault_unwire(&p->p_vmspace->vm_map, start, end);
 }
@@ -146,19 +139,21 @@ uvm_vsunlock(struct proc *p, caddr_t addr, size_t len)
 /*
  * uvm_vslock_device: wire user memory, make sure it's device reachable
  *  and bounce if necessary.
- * Always bounces for now.
+ *
+ * - called from physio
  */
 int
 uvm_vslock_device(struct proc *p, void *addr, size_t len,
     vm_prot_t access_type, void **retp)
 {
+	struct vm_map *map = &p->p_vmspace->vm_map;
 	struct vm_page *pg;
 	struct pglist pgl;
 	int npages;
 	vaddr_t start, end, off;
 	vaddr_t sva, va;
 	vsize_t sz;
-	int error, i;
+	int error, mapv, i;
 
 	start = trunc_page((vaddr_t)addr);
 	end = round_page((vaddr_t)addr + len);
@@ -167,17 +162,23 @@ uvm_vslock_device(struct proc *p, void *addr, size_t len,
 	if (end <= start)
 		return (EINVAL);
 
-	if ((error = uvm_fault_wire(&p->p_vmspace->vm_map, start, end,
-	    access_type))) {
+	vm_map_lock_read(map);
+retry:
+	mapv = map->timestamp;
+	vm_map_unlock_read(map);
+
+	if ((error = uvm_fault_wire(map, start, end, access_type)))
 		return (error);
-	}
+
+	vm_map_lock_read(map);
+	if (mapv != map->timestamp)
+		goto retry;
 
 	npages = atop(sz);
 	for (i = 0; i < npages; i++) {
 		paddr_t pa;
 
-		if (!pmap_extract(p->p_vmspace->vm_map.pmap,
-		    start + ptoa(i), &pa)) {
+		if (!pmap_extract(map->pmap, start + ptoa(i), &pa)) {
 			error = EFAULT;
 			goto out_unwire;
 		}
@@ -219,10 +220,16 @@ uvm_vslock_device(struct proc *p, void *addr, size_t len,
 out_unmap:
 	uvm_km_free(kernel_map, sva, sz);
 out_unwire:
-	uvm_fault_unwire(&p->p_vmspace->vm_map, start, end);
+	uvm_fault_unwire_locked(map, start, end);
+	vm_map_unlock_read(map);
 	return (error);
 }
 
+/*
+ * uvm_vsunlock_device: unwire user memory wired by uvm_vslock_device()
+ *
+ * - called from physio
+ */
 void
 uvm_vsunlock_device(struct proc *p, void *addr, size_t len, void *map)
 {
@@ -232,13 +239,14 @@ uvm_vsunlock_device(struct proc *p, void *addr, size_t len, void *map)
 
 	start = trunc_page((vaddr_t)addr);
 	end = round_page((vaddr_t)addr + len);
+	KASSERT(end > start);
 	sz = end - start;
-	if (end <= start)
-		return;
 
 	if (map)
 		copyout(map, addr, len);
-	uvm_fault_unwire(&p->p_vmspace->vm_map, start, end);
+
+	uvm_fault_unwire_locked(&p->p_vmspace->vm_map, start, end);
+	vm_map_unlock_read(&p->p_vmspace->vm_map);
 
 	if (!map)
 		return;
