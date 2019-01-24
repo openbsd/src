3dd0b9bc5c8fb1cd4e4f37491d6800cb07a7e702
beck <beck@openbsd.org>
2019-01-24 15:50:47 +0000

    Set pointer variables in tls13 handshake to NULL on clear ok jsing@ bcook@

--- lib/libssl/s3_lib.c
+++ lib/libssl/s3_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: s3_lib.c,v 1.182 2019/01/24 02:56:41 beck Exp $ */
+/* $OpenBSD: s3_lib.c,v 1.183 2019/01/24 15:50:47 beck Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1603,9 +1603,13 @@ ssl3_clear(SSL *s)
 	S3I(s)->tmp.x25519 = NULL;
 
 	tls13_secrets_destroy(S3I(s)->hs_tls13.secrets);
+	S3I(s)->hs_tls13.secrets = NULL;
 	freezero(S3I(s)->hs_tls13.x25519_private, X25519_KEY_LENGTH);
+	S3I(s)->hs_tls13.x25519_private = NULL;
 	freezero(S3I(s)->hs_tls13.x25519_public, X25519_KEY_LENGTH);
+	S3I(s)->hs_tls13.x25519_public = NULL;
 	freezero(S3I(s)->hs_tls13.x25519_peer_public, X25519_KEY_LENGTH);
+	S3I(s)->hs_tls13.x25519_peer_public = NULL;
 	freezero(S3I(s)->hs_tls13.cookie, S3I(s)->hs_tls13.cookie_len);
 	S3I(s)->hs_tls13.cookie = NULL;
 	S3I(s)->hs_tls13.cookie_len = 0;
