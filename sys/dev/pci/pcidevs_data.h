8f0bc74e5217d62dc2ca6bae3dc4159ddfc87b07
dlg <dlg@openbsd.org>
2019-02-20 03:11:56 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.1876 2019/02/18 08:59:43 kevlo Exp 
+ *	OpenBSD: pcidevs,v 1.1877 2019/02/20 03:11:38 dlg Exp 
  */
 
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
