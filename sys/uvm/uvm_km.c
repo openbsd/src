bde9ef78a91201526e96333c2b3c052fa9cffbdb
tedu <tedu@openbsd.org>
2019-02-22 07:53:56 +0000

    at some point the uvm_km_thread learned to free memory, but the comment was never updated. from Amit Kulkarni

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.130 2017/05/11 06:55:47 dlg Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.131 2019/02/22 07:53:56 tedu Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
@@ -700,9 +700,7 @@ uvm_km_createthread(void *arg)
 
 /*
  * Endless loop.  We grab pages in increments of 16 pages, then
- * quickly swap them into the list.  At some point we can consider
- * returning memory to the system if we have too many free pages,
- * but that's not implemented yet.
+ * quickly swap them into the list.
  */
 void
 uvm_km_thread(void *arg)
