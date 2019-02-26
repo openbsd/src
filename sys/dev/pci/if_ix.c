84b7d9735cab3735af1ac856d23c70f96ab343e7
dlg <dlg@openbsd.org>
2019-02-26 03:09:50 +0000

    put the rx buffer at the end of the mbuf
    this should give a bit of space if the packet needs an m_pullup.
    care is taken to ensure the packet is still aligned for the ip
    stack.
    
    ok and tweaks claudio@

--- sys/dev/pci/if_ix.c
+++ sys/dev/pci/if_ix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ix.c,v 1.153 2019/02/21 03:16:47 dlg Exp $	*/
+/*	$OpenBSD: if_ix.c,v 1.154 2019/02/26 03:09:50 dlg Exp $	*/
 
 /******************************************************************************
 
@@ -2389,8 +2389,8 @@ ixgbe_get_buf(struct rx_ring *rxr, int i)
 	if (!mp)
 		return (ENOBUFS);
 
+	mp->m_data += (mp->m_ext.ext_size - sc->rx_mbuf_sz);
 	mp->m_len = mp->m_pkthdr.len = sc->rx_mbuf_sz;
-	m_adj(mp, ETHER_ALIGN);
 
 	error = bus_dmamap_load_mbuf(rxr->rxdma.dma_tag, rxbuf->map,
 	    mp, BUS_DMA_NOWAIT);
