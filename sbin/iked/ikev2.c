86d7e43bac2a2d598bc5b2a380ba73ac367b14be
patrick <patrick@openbsd.org>
2019-02-26 18:05:22 +0000

    Fix sending IKEV2_CFG_INTERNAL_IP6_DNS, IKEV2_CFG_INTERNAL_IP6_NBNS, IKEV2_CFG_INTERNAL_IP6_DHCP and IKEV2_CFG_INTERNAL_IP6_SERVER by using the correct member in the iked_addr struct for the address.
    From Aram Havarnean

--- sbin/iked/ikev2.c
+++ sbin/iked/ikev2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ikev2.c,v 1.166 2018/03/05 14:30:30 patrick Exp $	*/
+/*	$OpenBSD: ikev2.c,v 1.167 2019/02/26 18:05:22 patrick Exp $	*/
 
 /*
  * Copyright (c) 2010-2013 Reyk Floeter <reyk@openbsd.org>
@@ -1935,7 +1935,7 @@ ikev2_add_cp(struct iked *env, struct iked_sa *sa, struct ibuf *buf)
 		case IKEV2_CFG_INTERNAL_IP6_DHCP:
 		case IKEV2_CFG_INTERNAL_IP6_SERVER:
 			/* 16 bytes IPv6 address */
-			in6 = (struct sockaddr_in6 *)&ikecfg->cfg.address;
+			in6 = (struct sockaddr_in6 *)&ikecfg->cfg.address.addr;
 			cfg->cfg_length = htobe16(16);
 			if (ibuf_add(buf, &in6->sin6_addr.s6_addr, 16) == -1)
 				return (-1);
