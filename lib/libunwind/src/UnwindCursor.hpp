003688bd4aad94c6ca348a09289c56eb30eb1c34
pirofti <pirofti@openbsd.org>
2019-03-13 08:44:32 +0000

    Initial IP-based caching implementation with O(logn) lookup.
    Caching implemented via red-black trees, this can be improved and
    further work is on-going to bring it closer to GNU's performance that
    uses a LRU-MRU 8-entries based caching algorithm.
    
    Prompted by robert@ who run into a runtime of an executed macro of
    5 minutes in libreoffice. With this the execution is reduced to 58 seconds.
    
    C++11 tips from espie@, rsadowski@
    Tested by robert@
    OK mortimer@, kettenis@.

--- lib/libunwind/src/UnwindCursor.hpp
+++ lib/libunwind/src/UnwindCursor.hpp
@@ -36,6 +36,8 @@
 
 namespace libunwind {
 
+static UnwindInfoSectionsCache uwis_cache;
+
 #if defined(_LIBUNWIND_SUPPORT_DWARF_UNWIND)
 /// Cache of recently found FDEs.
 template <typename A>
@@ -1286,7 +1288,14 @@ void UnwindCursor<A, R>::setInfoBasedOnIPRegister(bool isReturnAddress) {
 
   // Ask address space object to find unwind sections for this pc.
   UnwindInfoSections sects;
-  if (_addressSpace.findUnwindSections(pc, sects)) {
+  bool have_sects = false;
+  if (uwis_cache.getUnwindInfoSectionsForPC(pc, sects))
+    have_sects = true;
+  else if (_addressSpace.findUnwindSections(pc, sects)) {
+    uwis_cache.setUnwindInfoSectionsForPC(pc, sects);
+    have_sects = true;
+  }
+  if (have_sects) {
 #if defined(_LIBUNWIND_SUPPORT_COMPACT_UNWIND)
     // If there is a compact unwind encoding table, look there first.
     if (sects.compact_unwind_section != 0) {
