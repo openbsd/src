321c52b2e59a648b025fb9b11a5032c0a76ff568
fcambus <fcambus@openbsd.org>
2019-03-15 10:38:14 +0000

    When calling rasops_init() in efifb_cnremap() and efifb_attach(), pass EFIFB_HEIGHT and EFIFB_WIDTH instead of efifb_std_descr.n{rows,cols}.
    Because the efifb resolution doesn't change, this ensures 'ri_emuwidth'
    and 'ri_emuheight' will always get the same value when we remap and
    later when we attach, so the text area is always displayed at the same
    position.
    
    This fixes display glitches happening on smaller screens or with larger
    fonts, which caused the content previously displayed in the area that
    was becoming margins when remapping to remain there.
    
    OK kettenis@

--- sys/arch/amd64/amd64/efifb.c
+++ sys/arch/amd64/amd64/efifb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: efifb.c,v 1.19 2019/01/10 01:16:29 jsg Exp $	*/
+/*	$OpenBSD: efifb.c,v 1.20 2019/03/15 10:38:14 fcambus Exp $	*/
 
 /*
  * Copyright (c) 2015 YASUOKA Masahiko <yasuoka@yasuoka.net>
@@ -219,7 +219,7 @@ efifb_attach(struct device *parent, struct device *self, void *aux)
 		ri->ri_flg &= ~RI_CLEAR;
 		ri->ri_flg |= RI_VCONS | RI_WRONLY;
 
-		rasops_init(ri, efifb_std_descr.nrows, efifb_std_descr.ncols);
+		rasops_init(ri, EFIFB_HEIGHT, EFIFB_WIDTH);
 
 		ri->ri_ops.alloc_attr(ri->ri_active, 0, 0, 0, &defattr);
 		wsdisplay_cnattach(&efifb_std_descr, ri->ri_active, ccol, crow,
@@ -467,7 +467,7 @@ efifb_cnremap(void)
 	ri->ri_flg &= ~RI_CLEAR;
 	ri->ri_flg |= RI_CENTER | RI_WRONLY;
 
-	rasops_init(ri, efifb_std_descr.nrows, efifb_std_descr.ncols);
+	rasops_init(ri, EFIFB_HEIGHT, EFIFB_WIDTH);
 }
 
 int
