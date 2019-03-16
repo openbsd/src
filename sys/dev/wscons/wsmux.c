4906ff4f5db053754bfe228dabc0a3e0e02e3fc4
anton <anton@openbsd.org>
2019-03-16 15:04:19 +0000

    Clear the me_evp pointer after calling wsmux_do_close() as opposed of before. The pointer indicates if a wsmux device is attached to another wsmux, clearing it therefore allows same device to attached again. This can be problematic since wsmux_do_close() can sleep. Swapping the order of operations prevents attaching a wsmux which is in a closing state.
    ok deraadt@ mpi@ visa@

--- sys/dev/wscons/wsmux.c
+++ sys/dev/wscons/wsmux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wsmux.c,v 1.43 2019/03/02 07:42:03 anton Exp $	*/
+/*	$OpenBSD: wsmux.c,v 1.44 2019/03/16 15:04:19 anton Exp $	*/
 /*      $NetBSD: wsmux.c,v 1.37 2005/04/30 03:47:12 augustss Exp $      */
 
 /*
@@ -303,8 +303,8 @@ wsmuxclose(dev_t dev, int flags, int mode, struct proc *p)
 int
 wsmux_mux_close(struct wsevsrc *me)
 {
-	me->me_evp = NULL;
 	wsmux_do_close((struct wsmux_softc *)me);
+	me->me_evp = NULL;
 	return (0);
 }
 
