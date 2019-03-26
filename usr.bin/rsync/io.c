4fbea98972e91fab0499e506e66173f3b55a5b20
deraadt <deraadt@openbsd.org>
2019-03-26 17:13:42 +0000

    Same INT32_MAX vs -1 confusion on the write-side (previous commit was for read-side)

--- usr.bin/rsync/io.c
+++ usr.bin/rsync/io.c
@@ -1,4 +1,4 @@
-/*	$Id: io.c,v 1.13 2019/03/26 16:20:03 deraadt Exp $ */
+/*	$Id: io.c,v 1.14 2019/03/26 17:13:42 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -407,7 +407,7 @@ io_write_long(struct sess *sess, int fd, int64_t val)
 
 	nv = htole64(val);
 
-	if (!io_write_int(sess, fd, INT32_MAX))
+	if (!io_write_int(sess, fd, -1))
 		ERRX1(sess, "io_write_int");
 	else if (!io_write_buf(sess, fd, &nv, sizeof(int64_t)))
 		ERRX1(sess, "io_write_buf");
