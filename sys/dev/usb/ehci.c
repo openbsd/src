de3d60f82d4f21dc512afd62cdf4d20ce4acd796
mglocker <mglocker@openbsd.org>
2019-03-31 06:16:38 +0000

    Fix comment typo: 'trasnfer' -> 'transfer'.

--- sys/dev/usb/ehci.c
+++ sys/dev/usb/ehci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ehci.c,v 1.203 2019/03/11 17:50:08 mpi Exp $ */
+/*	$OpenBSD: ehci.c,v 1.204 2019/03/31 06:16:38 mglocker Exp $ */
 /*	$NetBSD: ehci.c,v 1.66 2004/06/30 03:11:56 mycroft Exp $	*/
 
 /*
@@ -1431,7 +1431,7 @@ ehci_open(struct usbd_pipe *pipe)
 		);
 		/*
 		 * To reduce conflict with split isochronous transfer,
-		 * schedule (split) interrupt trasnfer at latter half of
+		 * schedule (split) interrupt transfer at latter half of
 		 * 1ms frame:
 		 *
 		 *         |<-------------- H-Frame -------------->|
