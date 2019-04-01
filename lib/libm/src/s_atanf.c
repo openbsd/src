37dcc47f52ae9206c8370e1a51d72d72ffd26313
naddy <naddy@openbsd.org>
2019-04-01 04:39:55 +0000

    repair confusing "} if" indentation as suggested by deraadt@

--- lib/libm/src/s_atanf.c
+++ lib/libm/src/s_atanf.c
@@ -61,7 +61,8 @@ atanf(float x)
 		return x+x;		/* NaN */
 	    if(hx>0) return  atanhi[3]+atanlo[3];
 	    else     return -atanhi[3]-atanlo[3];
-	} if (ix < 0x3ee00000) {	/* |x| < 0.4375 */
+	}
+	if (ix < 0x3ee00000) {	/* |x| < 0.4375 */
 	    if (ix < 0x31000000) {	/* |x| < 2^-29 */
 		if(huge+x>one) return x;	/* raise inexact */
 	    }
