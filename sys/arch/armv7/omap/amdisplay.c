92ab049d89a04203c95ef8337968d4f0e6390c0e
ians <ians@openbsd.org>
2019-04-10 22:18:39 +0000

    KNF per naddy@

--- sys/arch/armv7/omap/amdisplay.c
+++ sys/arch/armv7/omap/amdisplay.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: amdisplay.c,v 1.8 2018/09/19 08:12:39 claudio Exp $ */
+/* $OpenBSD: amdisplay.c,v 1.9 2019/04/10 22:18:39 ians Exp $ */
 /*
  * Copyright (c) 2016 Ian Sutton <ians@openbsd.org>
  *
@@ -411,21 +411,33 @@ amdisplay_intr(void *arg)
 		HCLR4(sc, LCD_RASTER_CTRL, LCD_RASTER_CTRL_PALMODE);
 		HSET4(sc, LCD_RASTER_CTRL, 0x02 << LCD_RASTER_CTRL_PALMODE_SHAMT);
 		HSET4(sc, LCD_RASTER_CTRL, LCD_RASTER_CTRL_LCDEN);
-	} if (ISSET(reg, LCD_IRQ_FUF)) { 
+	}
+
+	if (ISSET(reg, LCD_IRQ_FUF)) { 
 		DPRINTF(15, ("%s: FIFO underflow\n", DEVNAME(sc)));
-	} if (ISSET(reg, LCD_IRQ_SYNC)) {
+	}
+	
+	if (ISSET(reg, LCD_IRQ_SYNC)) {
 		sc->sc_flags |= LCD_RESET_PENDING;
 		DPRINTF(18, ("%s: sync lost\n", DEVNAME(sc)));
-	} if (ISSET(reg, LCD_IRQ_RR_DONE)) { 
+	}
+
+	if (ISSET(reg, LCD_IRQ_RR_DONE)) { 
 		DPRINTF(21, ("%s: frame done\n", DEVNAME(sc)));
 		HWRITE4(sc, LCD_LCDDMA_FB0, sc->sc_fb0_dma_segs[0].ds_addr);
 		HWRITE4(sc, LCD_LCDDMA_FB0_CEIL, (sc->sc_fb0_dma_segs[0].ds_addr
 		    + sc->sc_fb_size) - 1);
-	} if (ISSET(reg, LCD_IRQ_EOF0)) {
+	}
+
+	if (ISSET(reg, LCD_IRQ_EOF0)) {
 		DPRINTF(21, ("%s: framebuffer 0 done\n", DEVNAME(sc)));
-	} if (ISSET(reg, LCD_IRQ_EOF1)) {
+	}
+
+	if (ISSET(reg, LCD_IRQ_EOF1)) {
 		DPRINTF(21, ("%s: framebuffer 1 done\n", DEVNAME(sc)));
-	} if (ISSET(reg, LCD_IRQ_DONE)) {
+	}
+
+	if (ISSET(reg, LCD_IRQ_DONE)) {
 		if (ISSET(sc->sc_flags, LCD_RESET_PENDING)) {
 			HWRITE4(sc, LCD_IRQSTATUS, 0xFFFFFFFF);
 			HSET4(sc, LCD_CLKC_RESET, LCD_CLKC_RESET_MAIN_RST);
@@ -435,7 +447,9 @@ amdisplay_intr(void *arg)
 			sc->sc_flags &= ~LCD_RESET_PENDING;
 		}
 		DPRINTF(15, ("%s: last frame done\n", DEVNAME(sc)));
-	} if (ISSET(reg, LCD_IRQ_ACB)) {
+	}
+
+	if (ISSET(reg, LCD_IRQ_ACB)) {
 		DPRINTF(15, ("%s: AC bias event\n", DEVNAME(sc)));
 	}
 
