fb4d85023675bc7da402da96b2bb84fd12905dbf
jsg <jsg@openbsd.org>
2019-05-21 22:40:06 +0000

    add amdgpu from linux 4.19.44 for recent AMD Radeon parts
    committing now so this can be worked on in tree
    
    Thanks to the OpenBSD Foundation for sponsoring this work and kettenis@
    for helping.

--- sys/dev/pci/drm/include/linux/kconfig.h
+++ sys/dev/pci/drm/include/linux/kconfig.h
@@ -21,8 +21,10 @@
 #define CONFIG_PM				0
 #define CONFIG_DRM_AMD_DC			1
 #define CONFIG_DRM_AMD_DC_DCN1_0		1
+#if 0
 #define CONFIG_DRM_AMDGPU_CIK			1
 #define CONFIG_DRM_AMDGPU_SI			1
+#endif
 
 #if BYTE_ORDER == BIG_ENDIAN
 #define __BIG_ENDIAN
