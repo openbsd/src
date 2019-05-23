743da4bd6095b2266114cf2ae351d3f599361dbd
florian <florian@openbsd.org>
2019-05-23 15:09:17 +0000

    typo

--- sbin/unwind/resolver.c
+++ sbin/unwind/resolver.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: resolver.c,v 1.40 2019/05/14 14:51:31 florian Exp $	*/
+/*	$OpenBSD: resolver.c,v 1.41 2019/05/23 15:09:17 florian Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -1161,7 +1161,8 @@ best_resolver(void)
 	    uw_resolver_state_str[resolvers[UW_RES_DOT]->state] :
 	    "NA", captive_portal_state_str[captive_portal_state]);
 
-	if (captive_portal_state == UNKNOWN || captive_portal_state == BEHIND) {
+	if (captive_portal_state == PORTAL_UNKNOWN || captive_portal_state ==
+	    BEHIND) {
 		if (resolvers[UW_RES_DHCP] != NULL) {
 			res = resolvers[UW_RES_DHCP];
 			goto out;
