65148de87e2ed4f5ba9076c868267c7181bc1db1
kettenis <kettenis@openbsd.org>
2019-05-31 14:18:27 +0000

    Deimos is Cortex-A77.

--- sys/arch/arm64/arm64/cpu.c
+++ sys/arch/arm64/arm64/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.29 2019/04/22 18:52:56 kettenis Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.30 2019/05/31 14:18:27 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2016 Dale Rahn <drahn@dalerahn.com>
@@ -55,7 +55,7 @@
 #define CPU_PART_CORTEX_A75	0xd0a
 #define CPU_PART_CORTEX_A76	0xd0b
 #define CPU_PART_NEOVERSE_N1	0xd0c
-#define CPU_PART_CORTEX_DEIMOS	0xd0d
+#define CPU_PART_CORTEX_A77	0xd0d
 #define CPU_PART_CORTEX_A76AE	0xd0e
 #define CPU_PART_NEOVERSE_E1	0xd4a
 
@@ -88,7 +88,7 @@ struct cpu_cores cpu_cores_arm[] = {
 	{ CPU_PART_CORTEX_A75, "Cortex-A75" },
 	{ CPU_PART_CORTEX_A76, "Cortex-A76" },
 	{ CPU_PART_CORTEX_A76AE, "Cortex-A76AE" },
-	{ CPU_PART_CORTEX_DEIMOS, "Cortex-Deimos" },
+	{ CPU_PART_CORTEX_A77, "Cortex-A77" },
 	{ CPU_PART_NEOVERSE_E1, "Neoverse E1" },
 	{ CPU_PART_NEOVERSE_N1, "Neoverse N1" },
 	{ 0 },
@@ -249,7 +249,7 @@ cpu_identify(struct cpu_info *ci)
 		case CPU_PART_CORTEX_A55:
 		case CPU_PART_CORTEX_A76:
 		case CPU_PART_CORTEX_A76AE:
-		case CPU_PART_CORTEX_DEIMOS:
+		case CPU_PART_CORTEX_A77:
 		case CPU_PART_NEOVERSE_E1:
 		case CPU_PART_NEOVERSE_N1:
 			/* Not vulnerable. */
