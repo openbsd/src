70dbe2957938f0a4b14a60c4c5ae9a0d4ec2b5a6
mortimer <mortimer@openbsd.org>
2019-06-01 15:20:08 +0000

    Do not eliminate frame pointers on i386. The entire i386 ecosystem assumes it will be there. problem found by naddy@, "heck yeah" kettenis@

--- gnu/llvm/tools/clang/lib/Driver/ToolChains/Clang.cpp
+++ gnu/llvm/tools/clang/lib/Driver/ToolChains/Clang.cpp
@@ -532,7 +532,6 @@ static bool useFramePointerForTargetByDefault(const ArgList &Args,
     switch (Triple.getArch()) {
     case llvm::Triple::mips64:
     case llvm::Triple::mips64el:
-    case llvm::Triple::x86:
     case llvm::Triple::x86_64:
       return !areOptimizationsEnabled(Args);
     default:
