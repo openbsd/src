57d08c5e9fb936e61ae2d9462cca6ee4f147639a
deraadt <deraadt@openbsd.org>
2019-06-02 14:30:51 +0000

    have_md is initialized at startup, don't need to do it again later from Hiltjo Posthuma

--- usr.bin/rsync/blocks.c
+++ usr.bin/rsync/blocks.c
@@ -1,4 +1,4 @@
-/*	$Id: blocks.c,v 1.16 2019/05/08 21:30:11 benno Exp $ */
+/*	$Id: blocks.c,v 1.17 2019/06/02 14:30:51 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -55,7 +55,6 @@ blk_find(struct sess *sess, const void *buf, off_t size, off_t offs,
 	assert(remain);
 	osz = remain < (off_t)blks->len ? remain : (off_t)blks->len;
 	fhash = hash_fast(buf + offs, (size_t)osz);
-	have_md = 0;
 
 	/*
 	 * Start with our match hint.
