9865b3a930919c1bb4b9e1ffba8fd4306af70465
asou <asou@openbsd.org>
2019-06-07 10:55:41 +0000

    fix print not specified process.
    ok guenther@ yasuoka@

--- lib/libkvm/kvm_proc2.c
+++ lib/libkvm/kvm_proc2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kvm_proc2.c,v 1.27 2016/11/07 00:26:33 guenther Exp $	*/
+/*	$OpenBSD: kvm_proc2.c,v 1.28 2019/06/07 10:55:41 asou Exp $	*/
 /*	$NetBSD: kvm_proc.c,v 1.30 1999/03/24 05:50:50 mrg Exp $	*/
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -196,7 +196,7 @@ kvm_proclist(kvm_t *kd, int op, int arg, struct process *pr,
 
 		switch (op) {
 		case KERN_PROC_PID:
-			if (parent_pid != (pid_t)arg)
+			if (process.ps_pid != (pid_t)arg)
 				continue;
 			break;
 
