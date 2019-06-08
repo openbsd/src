9864457bc4ebff253d37bf662568b1023915c899
dlg <dlg@openbsd.org>
2019-06-08 10:27:02 +0000

    disambiguate memory extent conflicts for membars and rom addresses
    there's some more work to do in this space, but this helps us start.
    
    ok kettenis@

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci.c,v 1.112 2018/07/28 15:28:51 kettenis Exp $	*/
+/*	$OpenBSD: pci.c,v 1.113 2019/06/08 10:27:02 dlg Exp $	*/
 /*	$NetBSD: pci.c,v 1.31 1997/06/06 23:48:04 thorpej Exp $	*/
 
 /*
@@ -911,7 +911,7 @@ pci_reserve_resources(struct pci_attach_args *pa)
 			    base, size, EX_NOWAIT) &&
 			    pa->pa_memex && extent_alloc_region(pa->pa_memex,
 			    base, size, EX_NOWAIT)) {
-				printf("%d:%d:%d: mem address conflict 0x%lx/0x%lx\n",
+				printf("%d:%d:%d: rom address conflict 0x%lx/0x%lx\n",
 				    bus, dev, func, base, size);
 				pci_conf_write(pc, tag, PCI_ROM_REG, 0);
 			}
