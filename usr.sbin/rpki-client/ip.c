28d71d2576f129b08eb2757da37f9db427f5caa5
deraadt <deraadt@openbsd.org>
2019-06-17 15:08:08 +0000

    system includes first, always.

--- usr.sbin/rpki-client/ip.c
+++ usr.sbin/rpki-client/ip.c
@@ -1,4 +1,4 @@
-/*	$Id: ip.c,v 1.2 2019/06/17 15:04:59 deraadt Exp $ */
+/*	$Id: ip.c,v 1.3 2019/06/17 15:08:08 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -15,8 +15,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include <assert.h>
 #include <err.h>
--- usr.sbin/rpki-client/rsync.c
+++ usr.sbin/rpki-client/rsync.c
@@ -1,4 +1,4 @@
-/*	$Id: rsync.c,v 1.2 2019/06/17 15:04:59 deraadt Exp $ */
+/*	$Id: rsync.c,v 1.3 2019/06/17 15:08:08 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -15,9 +15,9 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <netinet/in.h>
 #include <assert.h>
 #include <err.h>
-#include <netinet/in.h>
 #include <resolv.h>
 #include <stdio.h>
 #include <stdlib.h>
