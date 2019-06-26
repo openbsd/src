e75df510872593510780f5aa14b5c417a6944d46
claudio <claudio@openbsd.org>
2019-06-26 14:39:12 +0000

    In LLVM 8 somebody had the glorious idea to enable the integrated assembler for OpenBSD sparc64. The problem is that the integrated assembler is not even able to compile the .S files in lib/csu or lib/libc so revert this and use gas again. Fixes build issues with clang on sparc64. Issue identified by jca@ OK deraadt@, patrick@, jca@

--- gnu/llvm/tools/clang/lib/Driver/ToolChains/Gnu.cpp
+++ gnu/llvm/tools/clang/lib/Driver/ToolChains/Gnu.cpp
@@ -2512,7 +2512,7 @@ bool Generic_GCC::IsIntegratedAssemblerDefault() const {
   case llvm::Triple::sparc:
   case llvm::Triple::sparcel:
   case llvm::Triple::sparcv9:
-    if (getTriple().isOSSolaris() || getTriple().isOSOpenBSD())
+    if (getTriple().isOSSolaris())
       return true;
     return false;
   default:
