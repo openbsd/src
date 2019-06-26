423d763413f5d09950ac49680c6a700572f8d1ed
robert <robert@openbsd.org>
2019-06-26 17:04:55 +0000

    allow more video(4) ioctls for the video pledge (required by chromium)
    ok deraadt@

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.253 2019/06/16 21:00:42 deraadt Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.254 2019/06/26 17:04:55 robert Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -1176,6 +1176,19 @@ pledge_ioctl(struct proc *p, long com, struct file *fp)
 		case VIDIOC_STREAMOFF:
 		case VIDIOC_ENUM_FRAMESIZES:
 		case VIDIOC_ENUM_FRAMEINTERVALS:
+		case VIDIOC_DQEVENT:
+		case VIDIOC_ENCODER_CMD:
+		case VIDIOC_EXPBUF:
+		case VIDIOC_G_CROP:
+		case VIDIOC_G_EXT_CTRLS:
+		case VIDIOC_G_FMT:
+		case VIDIOC_G_SELECTION:
+		case VIDIOC_QUERYMENU:
+		case VIDIOC_SUBSCRIBE_EVENT:
+		case VIDIOC_S_EXT_CTRLS:
+		case VIDIOC_S_SELECTION:
+		case VIDIOC_TRY_DECODER_CMD:
+		case VIDIOC_TRY_ENCODER_CMD:
 			if (fp->f_type == DTYPE_VNODE &&
 			    vp->v_type == VCHR &&
 			    cdevsw[major(vp->v_rdev)].d_open == videoopen)
