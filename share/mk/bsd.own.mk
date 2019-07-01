e3a1b03f3477b2164bfbf04433b63f4e6ed00e66
visa <visa@openbsd.org>
2019-07-01 15:20:16 +0000

    Switch the default compiler on octeon to clang.
    OK deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.197 2019/04/21 15:20:24 kettenis Exp $
+#	$OpenBSD: bsd.own.mk,v 1.198 2019/07/01 15:20:16 visa Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -16,9 +16,13 @@ SKEY?=		yes
 YP?=		yes
 
 CLANG_ARCH=aarch64 amd64 arm i386 mips64 mips64el powerpc sparc64
-GCC4_ARCH=alpha hppa mips64 mips64el powerpc sh sparc64
+GCC4_ARCH=alpha hppa mips64el powerpc sh sparc64
 GCC3_ARCH=m88k
 LLD_ARCH=aarch64 amd64 arm i386
+
+.if ${MACHINE} == "sgi"
+GCC4_ARCH+=mips64
+.endif
 
 # m88k: ?
 PIE_ARCH=aarch64 alpha amd64 arm hppa i386 mips64 mips64el powerpc sh sparc64
@@ -39,7 +43,8 @@ BUILD_GCC3?=yes
 BUILD_GCC3?=no
 .endif
 .if !empty(GCC4_ARCH:M${_arch}) || ${MACHINE_ARCH} == "amd64" || \
-    ${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "i386"
+    ${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "i386" || \
+    ${MACHINE_ARCH} == "mips64"
 BUILD_GCC4?=yes
 .else
 BUILD_GCC4?=no
