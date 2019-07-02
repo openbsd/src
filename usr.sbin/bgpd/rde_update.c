40ea128b6609939704969444f9b9ae5f3b3cec01
claudio <claudio@openbsd.org>
2019-07-02 12:07:00 +0000

    up_generate_default() needs to fake a prefix entry so that the outbound filtering can happen. rde_filter no longer reads the re pointer but instead the prefix entry pointer pt. So instead of faking a re entry allocate a pt entry including the refcount dance. Also make sure that the validation state is set to ROA_NOTFOUND. Problem found by dlg@

--- usr.sbin/bgpd/rde_update.c
+++ usr.sbin/bgpd/rde_update.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_update.c,v 1.118 2019/06/22 05:44:05 claudio Exp $ */
+/*	$OpenBSD: rde_update.c,v 1.119 2019/07/02 12:07:00 claudio Exp $ */
 
 /*
  * Copyright (c) 2004 Claudio Jeker <claudio@openbsd.org>
@@ -189,7 +189,7 @@ up_generate_default(struct filter_head *rules, struct rde_peer *peer,
 	struct filterstate	 state;
 	struct rde_aspath	*asp;
 	struct prefix		 p;
-	struct rib_entry	*re;
+	struct pt_entry		*pte;
 	struct bgpd_addr	 addr;
 
 	if (peer->capa.mp[aid] == 0)
@@ -210,18 +210,20 @@ up_generate_default(struct filter_head *rules, struct rde_peer *peer,
 	/*
 	 * XXX this is ugly because we need to have a prefix for rde_filter()
 	 * but it will be added after filtering. So fake it till we make it.
+	 * rde_filter() only accesses prefix_peer(), prefix_vstate() and the
+	 * pt pointer.
 	 */
 	bzero(&p, sizeof(p));
 	bzero(&addr, sizeof(addr));
 	addr.aid = aid;
-	re = rib_get(rib_byid(peer->loc_rib_id), &addr, 0);
-	if (re == NULL)
-		re = rib_add(rib_byid(peer->loc_rib_id), &addr, 0);
-	p.re = re;
-	p.aspath = asp;
-	p.peer = peer; /* XXX should be peerself */
+	pte = pt_get(&addr, 0);
+	if (pte == NULL)
+		pte = pt_add(&addr, 0);
+	p.pt = pt_ref(pte);
+	p.validation_state = ROA_NOTFOUND;
+	p.peer = peer;		/* XXX should be peerself */
 
-	/* filter as usual */
+	/* outbound filter as usual */
 	if (rde_filter(rules, peer, &p, &state) == ACTION_DENY) {
 		rde_filterstate_clean(&state);
 		return;
@@ -236,8 +238,7 @@ up_generate_default(struct filter_head *rules, struct rde_peer *peer,
 	/* no longer needed */
 	rde_filterstate_clean(&state);
 
-	if (rib_empty(re))
-		rib_remove(re);
+	pt_unref(pte);
 }
 
 /* only for IPv4 */
