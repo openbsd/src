3b11bbaaee5711a3b5059b6df2d2680e52dfb6f1
kettenis <kettenis@openbsd.org>
2019-07-02 20:42:45 +0000

    Drop % from register name used for register variable since it makes clang unhappy.
    ok deraadt@, visa@

--- sys/arch/powerpc/include/tcb.h
+++ sys/arch/powerpc/include/tcb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcb.h,v 1.5 2016/09/04 08:42:47 guenther Exp $	*/
+/*	$OpenBSD: tcb.h,v 1.6 2019/07/02 20:42:45 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2011 Philip Guenther <guenther@openbsd.org>
@@ -34,7 +34,7 @@
 /* powerpc offsets the TCB pointer 0x7000 bytes after the data */
 #define TCB_OFFSET	0x7000
 
-register void *__tcb __asm__ ("%r2");
+register void *__tcb __asm__ ("r2");
 #define TCB_GET()		(__tcb)
 #define TCB_SET(tcb)		((__tcb) = (tcb))
 
