b764efda0543b1b3bc5df86127045ddbd161641c
lum <lum@openbsd.org>
2019-07-18 15:52:11 +0000

    The 'list' diff I committed this morning, was missing a couple of calls to a clean-up variable function.

--- usr.bin/mg/extend.c
+++ usr.bin/mg/extend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extend.c,v 1.70 2019/07/18 05:57:48 lum Exp $	*/
+/*	$OpenBSD: extend.c,v 1.71 2019/07/18 15:52:11 lum Exp $	*/
 /* This file is in the public domain. */
 
 /*
@@ -603,9 +603,12 @@ evalbuffer(int f, int n)
 
 		/* make sure it's terminated */
 		excbuf[llength(lp)] = '\0';
-		if ((s = excline(excbuf)) != TRUE)
+		if ((s = excline(excbuf)) != TRUE) {
+			(void) clearvars();
 			return (s);
+		}
 	}
+	(void) clearvars();
 	return (TRUE);
 }
 
