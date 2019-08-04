2f6dee1761d44f0447ac59b8b19cb1a7f8ca525a
visa <visa@openbsd.org>
2019-08-04 08:56:15 +0000

    Remove the old, unfinished octeon bootloader.

--- sys/arch/octeon/stand/boot/exec.c
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: exec.c,v 1.3 2019/04/10 04:17:36 deraadt Exp $	*/
-#include <sys/param.h>
-#include "libsa.h"
-#include <lib/libsa/loadfile.h>
-
-void
-run_loadfile(uint64_t *marks, int howto)
-{
-}
