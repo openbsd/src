ab1289e9a0cccf2a8b50c0cf310cd9098e513014
mortimer <mortimer@openbsd.org>
2019-08-04 15:27:37 +0000

    Add arm64 asm for stackpivot regress test.

--- regress/sys/kern/stackpivot/pivot.h
+++ regress/sys/kern/stackpivot/pivot.h
@@ -2,7 +2,9 @@
 #define REGRESS_PIVOT_H
 
 static void pivot(size_t *newstack) {
-#if defined(__amd64__)
+#if defined(__aarch64__)
+    asm("mov sp, %0; ldr lr, [sp]; ret;" ::"r"(newstack));
+#elif defined(__amd64__)
     asm("mov %0, %%rsp; retq;" ::"r"(newstack));
 #elif defined(__i386__)
     asm("mov %0, %%esp; retl;" ::"r"(newstack));
