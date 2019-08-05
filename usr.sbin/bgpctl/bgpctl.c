143a9790363bcf535f6ef7e641e947b44d451699
claudio <claudio@openbsd.org>
2019-08-05 12:51:32 +0000

    Add a comment why network_bulk() is not using send_filterset(). Using send_filterset() would result in a use after free because send_filterset() frees the set after sending but network_bulk() runs in a loop.

--- usr.sbin/bgpctl/bgpctl.c
+++ usr.sbin/bgpctl/bgpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpctl.c,v 1.242 2019/07/03 06:15:48 claudio Exp $ */
+/*	$OpenBSD: bgpctl.c,v 1.243 2019/08/05 12:51:32 claudio Exp $ */
 
 /*
  * Copyright (c) 2003 Henning Brauer <henning@openbsd.org>
@@ -2036,6 +2036,10 @@ network_bulk(struct parse_result *res)
 			if (res->action == NETWORK_BULK_ADD) {
 				imsg_compose(ibuf, IMSG_NETWORK_ADD,
 				    0, 0, -1, &net, sizeof(net));
+				/*
+				 * can't use send_filterset since that
+				 * would free the set.
+				 */
 				TAILQ_FOREACH(s, &res->set, entry) {
 					imsg_compose(ibuf,
 					    IMSG_FILTER_SET,
