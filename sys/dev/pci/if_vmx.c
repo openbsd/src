3867ce6a490f459643b1092b19785bac76a0e34e
dlg <dlg@openbsd.org>
2019-08-06 07:21:29 +0000

    use ifiq_input so we can call if_rxr_livelocked to apply backpressure

--- sys/dev/pci/if_vmx.c
+++ sys/dev/pci/if_vmx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_vmx.c,v 1.45 2017/01/22 10:17:38 dlg Exp $	*/
+/*	$OpenBSD: if_vmx.c,v 1.46 2019/08/06 07:21:29 dlg Exp $	*/
 
 /*
  * Copyright (c) 2013 Tsubai Masanari
@@ -785,7 +785,8 @@ skip_buffer:
 		}
 	}
 
-	if_input(ifp, &ml);
+	if (ifiq_input(&ifp->if_rcv, &ml))
+		if_rxr_livelocked(&ring->rxr);
 
 	/* XXX Should we (try to) allocate buffers for ring 2 too? */
 	ring = &rq->cmd_ring[0];
