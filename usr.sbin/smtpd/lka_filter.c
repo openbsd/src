40e5b96044234b547bbcfad0cb05dca8a9327405
gilles <gilles@openbsd.org>
2019-08-11 16:51:00 +0000

    teach builtin filter rdns how to match a session that has or lacks rdns

--- usr.sbin/smtpd/lka_filter.c
+++ usr.sbin/smtpd/lka_filter.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lka_filter.c,v 1.38 2019/07/01 07:40:43 martijn Exp $	*/
+/*	$OpenBSD: lka_filter.c,v 1.39 2019/08/11 16:51:00 gilles Exp $	*/
 
 /*
  * Copyright (c) 2018 Gilles Chehade <gilles@poolp.org>
@@ -966,10 +966,15 @@ filter_check_rdns(struct filter *filter, const char *hostname)
 	if (!filter->config->rdns)
 		return 0;
 
+	/* this is a hack until smtp session properly deals with lack of rdns */
+	ret = strcmp("<unknown>", hostname);
+	if (ret == 0)
+		return filter->config->not_rdns < 0 ? !ret : ret;
+
 	/* if text_to_netaddress succeeds,
 	 * we don't have an rDNS so the filter should match
 	 */
-	ret = text_to_netaddr(&netaddr, hostname);
+	ret = !text_to_netaddr(&netaddr, hostname);
 	return filter->config->not_rdns < 0 ? !ret : ret;
 }
 
