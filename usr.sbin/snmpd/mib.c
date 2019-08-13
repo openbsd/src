cf873db3210dc006985f5f637ff6dddfc7b67e09
claudio <claudio@openbsd.org>
2019-08-13 12:46:19 +0000

    Report if_iqdrops (input queue drops) as ifInDiscards. This is pretty much what this counter is for. For sure better than net.inet.ip.ifq.drops which no longer exists. Found by and OK martijn@ and OK sthen@

--- usr.sbin/snmpd/mib.c
+++ usr.sbin/snmpd/mib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mib.c,v 1.94 2019/06/28 13:32:51 deraadt Exp $	*/
+/*	$OpenBSD: mib.c,v 1.95 2019/08/13 12:46:19 claudio Exp $	*/
 
 /*
  * Copyright (c) 2012 Joel Knight <joel@openbsd.org>
@@ -1195,15 +1195,7 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 13:
-		mib[3] = IPCTL_IFQUEUE;
-		mib[4] = IFQCTL_DROPS;
-		len = sizeof(ifq);
-		if (sysctl(mib, sizeofa(mib), &ifq, &len, 0, 0) == -1) {
-			log_info("mib_iftable: %s: invalid ifq: %s",
-			    kif->if_name, strerror(errno));
-			return (-1);
-		}
-		ber = ber_add_integer(ber, ifq);
+		ber = ber_add_integer(ber, (u_int32_t)kif->if_iqdrops);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 14:
