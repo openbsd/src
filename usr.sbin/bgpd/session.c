6676f720df31a506723fb60ca7e9fb5579c19967
claudio <claudio@openbsd.org>
2019-08-13 12:13:26 +0000

    Do not use the SE global conf struct for the bgpd_config but actually the bgpd_config pointer passed to these functions. Luckily the affected functions were not used outside of the SE. While there also use getpeerbyid() to check if an peer id is in use instead of the rather dumb linear loop. OK benno@

--- usr.sbin/bgpd/session.c
+++ usr.sbin/bgpd/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.389 2019/08/12 14:15:27 claudio Exp $ */
+/*	$OpenBSD: session.c,v 1.390 2019/08/13 12:13:26 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004, 2005 Henning Brauer <henning@openbsd.org>
@@ -2894,7 +2894,7 @@ getpeerbydesc(struct bgpd_config *c, const char *descr)
 	struct peer	*p, *res = NULL;
 	int		 match = 0;
 
-	RB_FOREACH(p, peer_head, &conf->peers)
+	RB_FOREACH(p, peer_head, &c->peers)
 		if (!strcmp(p->conf.descr, descr)) {
 			res = p;
 			match++;
@@ -2920,13 +2920,13 @@ getpeerbyip(struct bgpd_config *c, struct sockaddr *ip)
 	sa2addr(ip, &addr, NULL);
 
 	/* we might want a more effective way to find peers by IP */
-	RB_FOREACH(p, peer_head, &conf->peers)
+	RB_FOREACH(p, peer_head, &c->peers)
 		if (!p->conf.template &&
 		    !memcmp(&addr, &p->conf.remote_addr, sizeof(addr)))
 			return (p);
 
 	/* try template matching */
-	RB_FOREACH(p, peer_head, &conf->peers)
+	RB_FOREACH(p, peer_head, &c->peers)
 		if (p->conf.template &&
 		    p->conf.remote_addr.aid == addr.aid &&
 		    session_match_mask(p, &addr))
@@ -2940,10 +2940,7 @@ getpeerbyip(struct bgpd_config *c, struct sockaddr *ip)
 			fatal(NULL);
 		memcpy(newpeer, loose, sizeof(struct peer));
 		for (id = PEER_ID_DYN_MAX; id > PEER_ID_STATIC_MAX; id--) {
-			RB_FOREACH(p, peer_head, &conf->peers)
-				if (p->conf.id == id)
-					break;
-			if (p == NULL)		/* we found a free id */
+			if (getpeerbyid(c, id) == NULL)	/* we found a free id */
 				break;
 		}
 		newpeer->template = loose;
