6959e8e10e8853600abc3d9759673a15ce8675bc
florian <florian@openbsd.org>
2019-08-13 13:34:43 +0000

    Silence warnings about short reads. They happen, we deal with it, it's just noise. OK claudio

--- usr.sbin/rpki-client/io.c
+++ usr.sbin/rpki-client/io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: io.c,v 1.4 2019/06/28 13:32:50 deraadt Exp $ */
+/*	$OpenBSD: io.c,v 1.5 2019/08/13 13:34:43 florian Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -135,7 +135,7 @@ io_str_write(int fd, const char *p)
 /*
  * Read of a binary buffer that must be on a blocking descriptor.
  * Does nothing if "sz" is zero.
- * This will fail and exit on EOF or short reads.
+ * This will fail and exit on EOF.
  */
 void
 io_simple_read(int fd, void *res, size_t sz)
@@ -151,7 +151,6 @@ again:
 		errx(EXIT_FAILURE, "read: unexpected end of file");
 	else if ((size_t)ssz == sz)
 		return;
-	warnx("read: short read: %zu remain", sz - (size_t)ssz);
 	sz -= ssz;
 	res += ssz;
 	goto again;
