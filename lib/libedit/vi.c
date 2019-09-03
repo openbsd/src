bb90466644c9635fe89fb5f3b6e98a4549b941b4
asou <asou@openbsd.org>
2019-09-03 02:28:25 +0000

    Correct the length for argument of reallocarray(). This fix the SEGV when launch the vi editor in vi mode. This fix is similar 1.63 from NetBSD lib/libedit/vi.c.
    OK schwarze@ yasuoka@

--- lib/libedit/vi.c
+++ lib/libedit/vi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vi.c,v 1.26 2019/06/28 05:35:34 deraadt Exp $	*/
+/*	$OpenBSD: vi.c,v 1.27 2019/09/03 02:28:25 asou Exp $	*/
 /*	$NetBSD: vi.c,v 1.33 2011/02/17 16:44:48 joerg Exp $	*/
 
 /*-
@@ -1027,7 +1027,7 @@ vi_histedit(EditLine *el, wint_t c __attribute__((__unused__)))
 		unlink(tempfile);
 		return CC_ERROR;
 	}
-	line = reallocarray(NULL, len, sizeof(*line));
+	line = reallocarray(NULL, len + 1, sizeof(*line));
 	if (line == NULL) {
 		close(fd);
 		unlink(tempfile);
