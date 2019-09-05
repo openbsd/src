52c7a68a914e50748162a8440be12cde426574ba
jsing <jsing@openbsd.org>
2019-09-05 16:05:36 +0000

    Remove ECDH_KDF_X9_62 wrapper.

--- lib/libcrypto/ecdh/ecdh_kdf.c
+++ lib/libcrypto/ecdh/ecdh_kdf.c
@@ -67,15 +67,3 @@ int ecdh_KDF_X9_63(unsigned char *out, size_t outlen,
     EVP_MD_CTX_free(mctx);
     return rv;
 }
-
-/*-
- * The old name for ecdh_KDF_X9_63
- * Retained for ABI compatibility
- */
-int ECDH_KDF_X9_62(unsigned char *out, size_t outlen,
-                   const unsigned char *Z, size_t Zlen,
-                   const unsigned char *sinfo, size_t sinfolen,
-                   const EVP_MD *md)
-{
-    return ecdh_KDF_X9_63(out, outlen, Z, Zlen, sinfo, sinfolen, md);
-}
