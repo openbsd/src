2c8a22d88bfd24f21fd3ea68d5f4f3a3eed1ddfe
krw <krw@openbsd.org>
2019-09-05 17:35:28 +0000

    Devices that have no quirks don't need to appear in the list of devices with quirks.

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: st.c,v 1.143 2019/09/05 03:04:45 krw Exp $	*/
+/*	$OpenBSD: st.c,v 1.144 2019/09/05 17:35:28 krw Exp $	*/
 /*	$NetBSD: st.c,v 1.71 1997/02/21 23:03:49 thorpej Exp $	*/
 
 /*
@@ -117,9 +117,6 @@ const struct st_quirk_inquiry_pattern st_quirk_patterns[] = {
 	{{T_SEQUENTIAL, T_REMOV,
 		 "        ", "                ", "    "}, {ST_Q_FORCE_BLKSIZE,
 							   {512, 0}}},
-	{{T_SEQUENTIAL, T_REMOV,
-		 "TANDBERG", " TDC 3600       ", ""},     {0,
-							   {0, 0}}},
  	{{T_SEQUENTIAL, T_REMOV,
 		 "TANDBERG", " TDC 3800       ", ""},     {ST_Q_FORCE_BLKSIZE,
 							   {512, 0}}},
@@ -141,9 +138,6 @@ const struct st_quirk_inquiry_pattern st_quirk_patterns[] = {
 		 "ANRITSU ", "DMT780          ", ""},     {ST_Q_FORCE_BLKSIZE,
 							   {512, 0}}},
 	{{T_SEQUENTIAL, T_REMOV,
-		 "ARCHIVE ", "VIPER 150  21247", ""},     {0,
-							   {0, 0}}},
-	{{T_SEQUENTIAL, T_REMOV,
 		 "ARCHIVE ", "VIPER 150  21531", ""},     {ST_Q_SENSE_HELP,
 							   {0, 0}}},
 	{{T_SEQUENTIAL, T_REMOV,
@@ -153,22 +147,8 @@ const struct st_quirk_inquiry_pattern st_quirk_patterns[] = {
 		 "WANGTEK ", "5150ES SCSI", ""},          {ST_Q_FORCE_BLKSIZE,
 							   {512, 0}}},
 	{{T_SEQUENTIAL, T_REMOV,
-		 "WANGTEK ", "5525ES SCSI REV7", ""},     {0,
-							   {0, 0}}},
-	{{T_SEQUENTIAL, T_REMOV,
-		 "WangDAT ", "Model 1300      ", ""},     {0,
-							   {0, 0}}},
-	{{T_SEQUENTIAL, T_REMOV,
-		 "EXABYTE ", "EXB-8200        ", "263H"}, {0,
-							   {0, 0}}},
-	{{T_SEQUENTIAL, T_REMOV,
 		 "HP      ", "T4000s          ", ""},     {ST_Q_UNIMODAL,
 							   {0, QIC_3095}}},
-#if 0
-	{{T_SEQUENTIAL, T_REMOV,
-		 "EXABYTE ", "EXB-8200        ", ""},     {0,
-							   {0, 0}}},
-#endif
 	{{T_SEQUENTIAL, T_REMOV,
 		 "WANGTEK ", "5150ES SCSI FA15\0""01 A", "????"}, {ST_Q_IGNORE_LOADS,
 								   {0, 0}}},
