8c2d8af6c78b634bf647ac7483afa1ed9cdced90
florian <florian@openbsd.org>
2019-09-25 14:40:59 +0000

    Be more robust when dealing with malformed lease files. Do not assume that required tokens have been generated by strsep. (toks[0] cannot be NULL but it doesn't hurt to be explicit about it.) Found by llvm's scan-build. OK deraadt, kn

--- sbin/unwind/frontend.c
+++ sbin/unwind/frontend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: frontend.c,v 1.22 2019/06/28 13:32:46 deraadt Exp $	*/
+/*	$OpenBSD: frontend.c,v 1.23 2019/09/25 14:40:59 florian Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -1009,8 +1009,12 @@ parse_dhcp_lease(int fd)
 			if (**tok != '\0')
 				tok++;
 		}
+		if (toks[0] == NULL)
+			continue;
 		*tok = NULL;
 		if (strcmp(toks[0], "option") == 0) {
+			if (toks[1] == NULL || toks[2] == NULL)
+				continue;
 			if (strcmp(toks[1], "domain-name-servers") == 0) {
 				if((p = strchr(toks[2], ';')) != NULL) {
 					*p='\0';
@@ -1025,6 +1029,8 @@ parse_dhcp_lease(int fd)
 				}
 			}
 		} else if (strcmp(toks[0], "epoch") == 0) {
+			if (toks[1] == NULL)
+				continue;
 			if((p = strchr(toks[1], ';')) != NULL) {
 				*p='\0';
 				epoch = strtonum(toks[1], 0,
