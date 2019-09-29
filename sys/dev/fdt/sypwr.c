8f4013c930d47d3b54faa8664dcc89cc33451719
kettenis <kettenis@openbsd.org>
2019-09-29 13:27:48 +0000

    Fix pasto.

--- sys/dev/fdt/sypwr.c
+++ sys/dev/fdt/sypwr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sypwr.c,v 1.2 2018/08/03 21:07:34 kettenis Exp $	*/
+/*	$OpenBSD: sypwr.c,v 1.3 2019/09/29 13:27:48 kettenis Exp $	*/
 /*
  * Copyright (c) 2017 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -56,7 +56,7 @@ struct cfdriver sypwr_cd = {
 uint8_t	sypwr_read(struct sypwr_softc *, int);
 void	sypwr_write(struct sypwr_softc *, int, uint8_t);
 uint32_t sypwr_get_voltage(void *);
-int	sypwr_set_voltage(void *, uint32_t voltage);
+int	sypwr_set_voltage(void *, uint32_t);
 
 int
 sypwr_match(struct device *parent, void *match, void *aux)
