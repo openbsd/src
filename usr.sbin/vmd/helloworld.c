93c1e0a07b97957a10abca30519bbb5766dac762
Hemaprasanthi <prasanthi3426@gmail.com>
2019-10-18 14:51:43 -0700

    path_change

--- usr.sbin/vmd/helloworld.c
+++ usr.sbin/vmd/helloworld.c
@@ -1,7 +1,9 @@
     #include <stdio.h>
+
     int main()
     {
        // printf() displays the string inside quotation
        printf("Hello, World!");
+	printf("The integer is: %d\n", RDOMAIN);
        return 0;
     }
