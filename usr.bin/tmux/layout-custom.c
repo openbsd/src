38c82affcda982e5e2c68bc40d91559f36603ee7
nicm <nicm@openbsd.org>
2019-10-19 12:40:42 +0000

    Do not crash trying to fix layout size if only one cell, from Azat Khuzhin.

--- usr.bin/tmux/layout-custom.c
+++ usr.bin/tmux/layout-custom.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: layout-custom.c,v 1.17 2019/10/14 09:16:48 nicm Exp $ */
+/* $OpenBSD: layout-custom.c,v 1.18 2019/10/19 12:40:42 nicm Exp $ */
 
 /*
  * Copyright (c) 2010 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -210,7 +210,7 @@ layout_parse(struct window *w, const char *layout)
 		}
 		break;
 	}
-	if (lc->sx != sx || lc->sy != sy) {
+	if (lc->type != LAYOUT_WINDOWPANE && (lc->sx != sx || lc->sy != sy)) {
 		log_debug("fix layout %u,%u to %u,%u", lc->sx, lc->sy, sx,sy);
 		layout_print_cell(lc, __func__, 0);
 		lc->sx = sx - 1; lc->sy = sy - 1;
