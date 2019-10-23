8bcaa1af7f22de57ab3c79b8eafe0dc278121430
deraadt <deraadt@openbsd.org>
2019-10-23 16:50:14 +0000

    retract 1.151, now that ftp reports the failing url itself. with jca, ok aja

--- usr.sbin/syspatch/syspatch.sh
+++ usr.sbin/syspatch/syspatch.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 #
-# $OpenBSD: syspatch.sh,v 1.153 2019/10/20 09:10:43 ajacoutot Exp $
+# $OpenBSD: syspatch.sh,v 1.154 2019/10/23 16:50:14 deraadt Exp $
 #
 # Copyright (c) 2016, 2017 Antoine Jacoutot <ajacoutot@openbsd.org>
 #
@@ -164,9 +164,7 @@ ls_missing()
 	local _c _d _f _cmd _l="$(ls_installed)" _p _r _sha=${_TMP}/SHA256
 
 	# don't output anything on stdout to prevent corrupting the patch list
-	unpriv -f "${_sha}.sig" ftp -MVo "${_sha}.sig" "${_MIRROR}/SHA256.sig" \
-		>/dev/null 2>&1 ||
-		sp_err "failed to fetch ${_MIRROR}/SHA256.sig"
+	unpriv -f "${_sha}.sig" ftp -MVo "${_sha}.sig" "${_MIRROR}/SHA256.sig"
 	unpriv -f "${_sha}" signify -Veq -x ${_sha}.sig -m ${_sha} -p \
 		/etc/signify/openbsd-${_OSrev}-syspatch.pub >/dev/null
 
