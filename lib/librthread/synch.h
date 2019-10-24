bc52b28af8fc6fad0f918198d9b7bd543eb7a5fd
sthen <sthen@openbsd.org>
2019-10-24 12:32:18 +0000

    Backout previous synch.h commit (r1.5, "Use process-private futexes to avoid the uvm_map lookup overhead"). This causes hangs with Python, seen easily by trying to build ports/graphics/py-Pillow.

--- lib/librthread/synch.h
+++ lib/librthread/synch.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: synch.h,v 1.5 2019/10/21 10:06:31 mpi Exp $ */
+/*	$OpenBSD: synch.h,v 1.6 2019/10/24 12:32:18 sthen Exp $ */
 /*
  * Copyright (c) 2017 Martin Pieuchot
  *
@@ -22,7 +22,14 @@
 static inline int
 _wake(volatile uint32_t *p, int n)
 {
-	return futex(p, FUTEX_WAKE_PRIVATE, n, NULL, NULL);
+	return futex(p, FUTEX_WAKE, n, NULL, NULL);
+}
+
+static inline void
+_wait(volatile uint32_t *p, int val)
+{
+	while (*p != (uint32_t)val)
+		futex(p, FUTEX_WAIT, val, NULL, NULL);
 }
 
 static inline int
@@ -31,7 +38,7 @@ _twait(volatile uint32_t *p, int val, clockid_t clockid, const struct timespec *
 	struct timespec rel;
 
 	if (abs == NULL)
-		return futex(p, FUTEX_WAIT_PRIVATE, val, NULL, NULL);
+		return futex(p, FUTEX_WAIT, val, NULL, NULL);
 
 	if (abs->tv_nsec >= 1000000000 || clock_gettime(clockid, &rel))
 		return (EINVAL);
@@ -44,11 +51,11 @@ _twait(volatile uint32_t *p, int val, clockid_t clockid, const struct timespec *
 	if (rel.tv_sec < 0)
 		return (ETIMEDOUT);
 
-	return futex(p, FUTEX_WAIT_PRIVATE, val, &rel, NULL);
+	return futex(p, FUTEX_WAIT, val, &rel, NULL);
 }
 
 static inline int
 _requeue(volatile uint32_t *p, int n, int m, volatile uint32_t *q)
 {
-	return futex(p, FUTEX_REQUEUE_PRIVATE, n, (void *)(long)m, q);
+	return futex(p, FUTEX_REQUEUE, n, (void *)(long)m, q);
 }
