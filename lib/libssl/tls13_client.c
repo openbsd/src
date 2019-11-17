9dc7da83854cedf6e36cea4499d30fe36d645697
beck <beck@openbsd.org>
2019-11-17 00:16:58 +0000

    Drop back to the legacy tls method if we are doing client authenticaiton from a tls 1.3 connection, for now.
    ok jsing@

--- lib/libssl/tls13_client.c
+++ lib/libssl/tls13_client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls13_client.c,v 1.16 2019/04/05 20:23:38 tb Exp $ */
+/* $OpenBSD: tls13_client.c,v 1.17 2019/11/17 00:16:58 beck Exp $ */
 /*
  * Copyright (c) 2018, 2019 Joel Sing <jsing@openbsd.org>
  *
@@ -62,6 +62,12 @@ tls13_legacy_connect(SSL *ssl)
 {
 	struct tls13_ctx *ctx = ssl->internal->tls13;
 	int ret;
+
+	/* XXX drop back to legacy for client auth for now */
+	if (ssl->cert->key != NULL) {
+		ssl->method = tls_legacy_client_method();
+		return ssl->method->internal->ssl_connect(ssl);
+	}
 
 	if (ctx == NULL) {
 		if ((ctx = tls13_ctx_new(TLS13_HS_CLIENT)) == NULL) {
