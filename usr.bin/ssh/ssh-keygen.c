847074b14be9117e004e22e6330d2abe194c84b8
djm <djm@openbsd.org>
2019-11-18 01:59:48 +0000

    missing break in getopt switch; spotted by Sebastian Kinne

--- usr.bin/ssh/ssh-keygen.c
+++ usr.bin/ssh/ssh-keygen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-keygen.c,v 1.364 2019/11/14 21:27:30 djm Exp $ */
+/* $OpenBSD: ssh-keygen.c,v 1.365 2019/11/18 01:59:48 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1994 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -2972,6 +2972,7 @@ main(int argc, char **argv)
 			break;
 		case 'Y':
 			sign_op = optarg;
+			break;
 		case 'w':
 			sk_provider = optarg;
 			break;
