edb49af2f15d19e9e71f0ef66a4e5fdec4f66b89
kevlo <kevlo@openbsd.org>
2019-11-19 06:32:46 +0000

    Don't assign ifp twice in the re_txeof().
    ok deraadt@

--- sys/dev/ic/re.c
+++ sys/dev/ic/re.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: re.c,v 1.202 2017/06/19 09:36:27 mpi Exp $	*/
+/*	$OpenBSD: re.c,v 1.203 2019/11/19 06:32:46 kevlo Exp $	*/
 /*	$FreeBSD: if_re.c,v 1.31 2004/09/04 07:54:05 ru Exp $	*/
 /*
  * Copyright (c) 1997, 1998-2003
@@ -1415,8 +1415,6 @@ re_txeof(struct rl_softc *sc)
 	unsigned int	prod, cons;
 	unsigned int	idx;
 	int		free = 0;
-
-	ifp = &sc->sc_arpcom.ac_if;
 
 	prod = sc->rl_ldata.rl_txq_prodidx;
 	cons = sc->rl_ldata.rl_txq_considx;
