ab4d4874e20c93e1406e23479c89f22f491f4354
benno <benno@openbsd.org>
2019-11-29 05:11:18 +0000

    commited at minus 21 degC s/EXIT_FAILURE/1/ ok claudio@

--- usr.sbin/rpki-client/cert.c
+++ usr.sbin/rpki-client/cert.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cert.c,v 1.12 2019/11/28 20:36:17 claudio Exp $ */
+/*	$OpenBSD: cert.c,v 1.13 2019/11/29 05:11:18 benno Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -88,7 +88,7 @@ append_ip(struct parse *p, const struct cert_ip *ip)
 	res->ips = reallocarray(res->ips, res->ipsz + 1,
 	    sizeof(struct cert_ip));
 	if (res->ips == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 	res->ips[res->ipsz++] = *ip;
 	return 1;
 }
@@ -107,7 +107,7 @@ append_as(struct parse *p, const struct cert_as *as)
 	p->res->as = reallocarray(p->res->as, p->res->asz + 1,
 	    sizeof(struct cert_as));
 	if (p->res->as == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 	p->res->as[p->res->asz++] = *as;
 	return 1;
 }
@@ -207,7 +207,7 @@ sbgp_sia_resource_mft(struct parse *p,
 		goto out;
 
 	if ((p->res->mft = strndup((const char *)d, plen)) == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 
 	/* Make sure it's an MFT rsync address. */
 
@@ -944,7 +944,7 @@ cert_parse_inner(X509 **xp, const char *fn, const unsigned char *dgst, int ta)
 	memset(&p, 0, sizeof(struct parse));
 	p.fn = fn;
 	if ((p.res = calloc(1, sizeof(struct cert))) == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 
 	/*
 	 * If we have a digest specified, create an MD chain that will
@@ -1271,20 +1271,20 @@ cert_read(int fd)
 	int		 has_crl, has_aki;
 
 	if ((p = calloc(1, sizeof(struct cert))) == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 
 	io_simple_read(fd, &p->valid, sizeof(int));
 	io_simple_read(fd, &p->ipsz, sizeof(size_t));
 	p->ips = calloc(p->ipsz, sizeof(struct cert_ip));
 	if (p->ips == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 	for (i = 0; i < p->ipsz; i++)
 		cert_ip_read(fd, &p->ips[i]);
 
 	io_simple_read(fd, &p->asz, sizeof(size_t));
 	p->as = calloc(p->asz, sizeof(struct cert_as));
 	if (p->as == NULL)
-		err(EXIT_FAILURE, NULL);
+		err(1, NULL);
 	for (i = 0; i < p->asz; i++)
 		cert_as_read(fd, &p->as[i]);
 
