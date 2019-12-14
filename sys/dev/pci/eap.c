959bbc3fefb15d26a548df6e907615a6a5bddac5
fcambus <fcambus@openbsd.org>
2019-12-14 12:48:32 +0000

    Add sizes for free() in eap(4).
    OK tedu@

--- sys/dev/pci/eap.c
+++ sys/dev/pci/eap.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: eap.c,v 1.56 2018/09/14 08:37:34 miko Exp $ */
+/*      $OpenBSD: eap.c,v 1.57 2019/12/14 12:48:32 fcambus Exp $ */
 /*	$NetBSD: eap.c,v 1.46 2001/09/03 15:07:37 reinoud Exp $ */
 
 /*
@@ -1471,7 +1471,7 @@ eap_malloc(void *addr, int direction, size_t size, int pool, int flags)
 		return (0);
 	error = eap_allocmem(sc, size, 16, p);
 	if (error) {
-		free(p, pool, 0);
+		free(p, pool, sizeof(*p));
 		return (0);
 	}
 	p->next = sc->sc_dmas;
@@ -1489,7 +1489,7 @@ eap_free(void *addr, void *ptr, int pool)
 		if (KERNADDR(p) == ptr) {
 			eap_freemem(sc, p);
 			*pp = p->next;
-			free(p, pool, 0);
+			free(p, pool, sizeof(*p));
 			return;
 		}
 	}
