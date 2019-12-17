6fc3cb5247be4bc0461ed5b124198628bb3dc070
millert <millert@openbsd.org>
2019-12-17 19:41:51 +0000

    Use correct size when zeroing out dinode. Fixes a bug on file systems using the rev1 inode structure where the size of the in-memory and on-disk dinode differ. From Chris Waddey; OK deraadt@

--- sbin/fsck_ext2fs/pass2.c
+++ sbin/fsck_ext2fs/pass2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pass2.c,v 1.15 2016/04/28 12:17:15 krw Exp $	*/
+/*	$OpenBSD: pass2.c,v 1.16 2019/12/17 19:41:51 millert Exp $	*/
 /*	$NetBSD: pass2.c,v 1.6 2000/01/28 16:01:46 bouyer Exp $	*/
 
 /*
@@ -145,7 +145,7 @@ pass2(void)
 				inodirty();
 			}
 		}
-		memset(&dino, 0, EXT2_DINODE_SIZE(&sblock));
+		memset(&dino, 0, sizeof(dino));
 		dino.e2di_mode = htole16(IFDIR);
 		inossize(&dino, inp->i_isize);
 		memcpy(&dino.e2di_blocks[0], &inp->i_blks[0], (size_t)inp->i_numblks);
