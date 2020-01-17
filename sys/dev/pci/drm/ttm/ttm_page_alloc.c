5e68f046c95120f127c92100998bc71a942aae3b
jsg <jsg@openbsd.org>
2020-01-17 23:20:14 +0000

    drm/ttm: fix start page for huge page check in ttm_put_pages()
    From Christian Koenig
    83a88fb92950ef34b9924c39ff7554ae92c9af69 in linux 4.19.y/4.19.97
    ac1e516d5a4c56bf0cb4a3dfc0672f689131cfd4 in mainline linux

--- sys/dev/pci/drm/ttm/ttm_page_alloc.c
+++ sys/dev/pci/drm/ttm/ttm_page_alloc.c
@@ -758,7 +758,7 @@ static void ttm_put_pages(struct vm_page **pages, unsigned npages, int flags,
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 			if (!(flags & TTM_PAGE_FLAG_DMA32) &&
 			    (npages - i) >= HPAGE_PMD_NR) {
-				for (j = 0; j < HPAGE_PMD_NR; ++j)
+				for (j = 1; j < HPAGE_PMD_NR; ++j)
 					if (p++ != pages[i + j])
 					    break;
 
@@ -795,7 +795,7 @@ static void ttm_put_pages(struct vm_page **pages, unsigned npages, int flags,
 			if (!p)
 				break;
 
-			for (j = 0; j < HPAGE_PMD_NR; ++j)
+			for (j = 1; j < HPAGE_PMD_NR; ++j)
 				if (p++ != pages[i + j])
 				    break;
 
