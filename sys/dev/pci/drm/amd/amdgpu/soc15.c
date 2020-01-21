674237b1330b72ca99a9e3a4ed21ddd645fff818
jsg <jsg@openbsd.org>
2020-01-21 07:43:48 +0000

    Enable mmhub power gating on picasso.  Matches linux 4.20. mmhub on soc15 is a memory controller hub for sdma, uvd and vce.
    ok kettenis@

--- sys/dev/pci/drm/amd/amdgpu/soc15.c
+++ sys/dev/pci/drm/amd/amdgpu/soc15.c
@@ -713,7 +713,9 @@ static int soc15_common_early_init(void *handle)
 				AMD_CG_SUPPORT_SDMA_MGCG |
 				AMD_CG_SUPPORT_SDMA_LS;
 
-			adev->pg_flags = AMD_PG_SUPPORT_SDMA | AMD_PG_SUPPORT_VCN;
+			adev->pg_flags = AMD_PG_SUPPORT_SDMA |
+				AMD_PG_SUPPORT_MMHUB |
+				AMD_PG_SUPPORT_VCN;
 		} else {
 			adev->cg_flags = AMD_CG_SUPPORT_GFX_MGCG |
 				AMD_CG_SUPPORT_GFX_MGLS |
