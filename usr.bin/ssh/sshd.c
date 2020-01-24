90d55c550393a285b40475bb85858f3a2a75b406
djm <djm@openbsd.org>
2020-01-24 23:56:01 +0000

    tweak proctitle to include sshd arguments, as these are frequently used to distinguish between multiple independent instances of the server. New proctitle looks like this:
    $ pgrep -lf sshd
    12844 sshd: /usr/sbin/sshd -f /etc/ssh/sshd_config [listener] 0 of 10-100 startups
    
    requested by sthen@ and aja@; ok aja@

--- usr.bin/ssh/sshd.c
+++ usr.bin/ssh/sshd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshd.c,v 1.544 2020/01/23 07:10:22 dtucker Exp $ */
+/* $OpenBSD: sshd.c,v 1.545 2020/01/24 23:56:01 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -239,6 +239,8 @@ struct sshbuf *loginmsg;
 void destroy_sensitive_data(void);
 void demote_sensitive_data(void);
 static void do_ssh2_kex(struct ssh *);
+
+static char *listener_proctitle;
 
 /*
  * Close all listening sockets
@@ -1032,9 +1034,9 @@ server_accept_loop(int *sock_in, int *sock_out, int *newsock, int *config_s)
 	 */
 	for (;;) {
 		if (ostartups != startups) {
-			setproctitle("[listener] %d of %d-%d startups",
-			    startups, options.max_startups_begin,
-			    options.max_startups);
+			setproctitle("%s [listener] %d of %d-%d startups",
+			    listener_proctitle, startups,
+			    options.max_startups_begin, options.max_startups);
 			ostartups = startups;
 		}
 		if (received_sighup) {
@@ -1345,6 +1347,17 @@ accumulate_host_timing_secret(struct sshbuf *server_cfg,
 		fatal("%s: ssh_digest_update", __func__);
 	sshbuf_reset(buf);
 	sshbuf_free(buf);
+}
+
+static char *
+prepare_proctitle(int ac, char **av)
+{
+	char *ret = NULL;
+	int i;
+
+	for (i = 0; i < ac; i++)
+		xextendf(&ret, " ", "%s", av[i]);
+	return ret;
 }
 
 /*
@@ -1774,6 +1787,7 @@ main(int ac, char **av)
 		rexec_argv[rexec_argc] = "-R";
 		rexec_argv[rexec_argc + 1] = NULL;
 	}
+	listener_proctitle = prepare_proctitle(ac, av);
 
 	/* Ensure that umask disallows at least group and world write */
 	new_umask = umask(0077) | 0022;
