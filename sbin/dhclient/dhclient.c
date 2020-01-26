c5fb3b0f56faec5f0dfedf1e7e21a61fbe50bebf
krw <krw@openbsd.org>
2020-01-26 10:31:03 +0000

    Don't validate '-c' with S_ISREG(). The install script uses /dev/stdin.
    Noticed by sebastia@

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.658 2020/01/24 00:54:16 krw Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.659 2020/01/26 10:31:03 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -533,9 +533,7 @@ main(int argc, char *argv[])
 			 */
 			if (errno != ENOENT)
 				fatal("lstat(%s)", path_dhclient_conf);
-		} else if (S_ISREG(sb.st_mode) == 0)
-			fatalx("'%s' is not a regular file",
-			    path_dhclient_conf);
+		}
 	}
 
 	if ((cmd_opts & (OPT_FOREGROUND | OPT_NOACTION)) != 0)
