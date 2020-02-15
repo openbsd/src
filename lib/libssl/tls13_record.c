2c5c7241e8f7a8afafe86d205bdddceaa587fd2b
jsing <jsing@openbsd.org>
2020-02-15 14:36:58 +0000

    Remove #include that is not needed.

--- lib/libssl/tls13_record.c
+++ lib/libssl/tls13_record.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls13_record.c,v 1.3 2019/01/21 00:24:19 jsing Exp $ */
+/* $OpenBSD: tls13_record.c,v 1.4 2020/02/15 14:36:58 jsing Exp $ */
 /*
  * Copyright (c) 2018, 2019 Joel Sing <jsing@openbsd.org>
  *
@@ -16,8 +16,6 @@
  */
 
 #include "ssl_locl.h"
-
-#include <openssl/curve25519.h>
 
 #include "tls13_internal.h"
 #include "tls13_record.h"
