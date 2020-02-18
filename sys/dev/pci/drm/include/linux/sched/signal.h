a69d255ff42b6fc403b5e90bb14d2252613a77ce
claudio <claudio@openbsd.org>
2020-02-18 07:40:49 +0000

    Use SIGPENDING() instead of CURSIG() for emulating signal_pending(). There is no need for the additional work done by CURSIG(). seems reasonable jsg@

--- sys/dev/pci/drm/include/linux/sched/signal.h
+++ sys/dev/pci/drm/include/linux/sched/signal.h
@@ -6,7 +6,7 @@
 #include <sys/systm.h>
 #include <sys/signalvar.h>
 
-#define signal_pending_state(x, y) CURSIG(curproc)
-#define signal_pending(y) CURSIG(curproc)
+#define signal_pending_state(x, y) SIGPENDING(curproc)
+#define signal_pending(y) SIGPENDING(curproc)
 
 #endif
