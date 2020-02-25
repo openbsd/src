7aea8869d4f9c04df569916eb4f12ee0e4d28265
cheloha <cheloha@openbsd.org>
2020-02-25 02:09:22 +0000

    pcic(4): i82365.c: tsleep(9) -> tsleep_nsec(9)
    Yet another straightforward ticks-to-milliseconds conversion.
    
    I am honestly unsure what particular driver this file belongs to.  It
    seems to be related to pcic(4) and/or pcmcia(4) so I've marked this
    diff with pcic(4) in the summary line.
    
    No complaints on tech@ after six weeks.

--- sys/dev/ic/i82365.c
+++ sys/dev/ic/i82365.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i82365.c,v 1.37 2019/12/31 10:05:32 mpi Exp $	*/
+/*	$OpenBSD: i82365.c,v 1.38 2020/02/25 02:09:22 cheloha Exp $	*/
 /*	$NetBSD: i82365.c,v 1.10 1998/06/09 07:36:55 thorpej Exp $	*/
 
 /*
@@ -429,8 +429,8 @@ pcic_event_thread(arg)
 		} else {
 			splx(s);
 			/* sleep .25s to be enqueued chatterling interrupts */
-			(void) tsleep((caddr_t)pcic_event_thread, PWAIT,
-			    "pcicss", hz/4);
+			tsleep(pcic_event_thread, PWAIT, "pcicss",
+			    MSEC_TO_NSEC(250));
 		}
 		pcic_event_process(h, pe);
 	}
