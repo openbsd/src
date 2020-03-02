6e86b7cad6c94bf4210af71116c9723bb2358cba
nicm <nicm@openbsd.org>
2020-03-02 08:30:30 +0000

    Use current session for cwd of new sessions, not the new session which doesn't have one yet. GitHub issue 2091.

--- usr.bin/tmux/spawn.c
+++ usr.bin/tmux/spawn.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: spawn.c,v 1.14 2020/01/28 10:21:21 nicm Exp $ */
+/* $OpenBSD: spawn.c,v 1.15 2020/03/02 08:30:30 nicm Exp $ */
 
 /*
  * Copyright (c) 2019 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -230,9 +230,9 @@ spawn_pane(struct spawn_context *sc, char **cause)
 	 * the pane's stored one unless specified.
 	 */
 	if (sc->cwd != NULL)
-		cwd = format_single(item, sc->cwd, c, s, NULL, NULL);
+		cwd = format_single(item, sc->cwd, c, item->target.s, NULL, NULL);
 	else if (~sc->flags & SPAWN_RESPAWN)
-		cwd = xstrdup(server_client_get_cwd(c, s));
+		cwd = xstrdup(server_client_get_cwd(c, item->target.s));
 	else
 		cwd = NULL;
 
