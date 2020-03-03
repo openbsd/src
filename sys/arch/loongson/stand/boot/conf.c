ba3d37d536abecf25d8b260e6336f498b1e279fe
otto <otto@openbsd.org>
2020-03-03 19:04:28 +0000

    Allow loongson boot(8) to read from an ffs2 filesystem. ok deraadt@ visa@

--- sys/arch/loongson/stand/boot/conf.c
+++ sys/arch/loongson/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.13 2019/10/29 02:55:51 deraadt Exp $	*/
+/*	$OpenBSD: conf.c,v 1.14 2020/03/03 19:04:28 otto Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -37,9 +37,10 @@
 
 #include "libsa.h"
 #include <lib/libsa/ufs.h>
+#include <lib/libsa/ufs2.h>
 #include <lib/libsa/cd9660.h>
 
-const char version[] = "1.0";
+const char version[] = "1.1";
 #if 0	/* network code not compiled in */
 int	debug = 0;
 #endif
@@ -65,6 +66,8 @@ struct fs_ops file_system[] = {
 	/* ufs filesystem */
 	{	ufs_open,	ufs_close,	ufs_read,	ufs_write,
 		ufs_seek,	ufs_stat,	ufs_readdir,	ufs_fchmod },
+	{	ufs2_open,	ufs2_close,	ufs2_read,	ufs2_write,
+		ufs2_seek,	ufs2_stat,	ufs2_readdir,	ufs2_fchmod },
 	/* cd9660 filesystem - in case a cd image is dd'ed on non USB media */
 	{	cd9660_open,	cd9660_close,	cd9660_read,	cd9660_write,
 		cd9660_seek,	cd9660_stat,	cd9660_readdir	}
