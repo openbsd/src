eac155ac72782d875ecd3ecf6b5c26a186664c2c
otto <otto@openbsd.org>
2020-03-07 15:11:50 +0000

    Revert previous, there are BIOS that do not like these changes. Sorry for the inconvenience. With help from semarie@.

--- usr.sbin/installboot/i386_softraid.c
+++ usr.sbin/installboot/i386_softraid.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i386_softraid.c,v 1.13 2020/02/28 12:26:30 otto Exp $	*/
+/*	$OpenBSD: i386_softraid.c,v 1.14 2020/03/07 15:11:50 otto Exp $	*/
 /*
  * Copyright (c) 2012 Joel Sing <jsing@openbsd.org>
  * Copyright (c) 2010 Otto Moerbeek <otto@drijf.net>
@@ -190,7 +190,6 @@ sr_install_bootldr(int devfd, char *dev)
 	sym_set_value(pbr_symbols, "_inodeblk", inodeblk);
 	sym_set_value(pbr_symbols, "_inodedbl", inodedbl);
 	sym_set_value(pbr_symbols, "_nblocks", nblocks);
-	sym_set_value(pbr_symbols, "_blkincr", 0);
 
 	if (verbose)
 		fprintf(stderr, "%s is %d blocks x %d bytes\n",
