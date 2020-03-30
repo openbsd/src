1298765350c95a7852ee8f8f07a6e0684531e348
krw <krw@openbsd.org>
2020-03-30 22:29:04 +0000

    No point in checking the loop variable for NULL inside a LIST_FOREACH.
    CID 980279

--- sys/dev/usb/ehci.c
+++ sys/dev/usb/ehci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ehci.c,v 1.208 2020/03/21 12:08:31 patrick Exp $ */
+/*	$OpenBSD: ehci.c,v 1.209 2020/03/30 22:29:04 krw Exp $ */
 /*	$NetBSD: ehci.c,v 1.66 2004/06/30 03:11:56 mycroft Exp $	*/
 
 /*
@@ -2524,8 +2524,6 @@ ehci_alloc_itd(struct ehci_softc *sc)
 
 	freeitd = NULL;
 	LIST_FOREACH(itd, &sc->sc_freeitds, u.free_list) {
-		if (itd == NULL)
-			break;
 		if (itd->slot != frindex && itd->slot != previndex) {
 			freeitd = itd;
 			break;
