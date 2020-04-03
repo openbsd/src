8c0c56a30d6a6ec1d375ba5e4a281cd7e8147d8c
bluhm <bluhm@openbsd.org>
2020-04-03 12:01:56 +0000

    Make signify regress work without obj dir.  Avoid including the output archive by reducing the input file list to text files only. from Christian Ludwig; OK beck@

--- regress/usr.bin/signify/signify.sh
+++ regress/usr.bin/signify/signify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: signify.sh,v 1.9 2019/12/09 09:41:54 espie Exp $
+# $OpenBSD: signify.sh,v 1.10 2020/04/03 12:01:56 bluhm Exp $
 
 srcdir=$1
 
@@ -28,7 +28,7 @@ signify -S -e -s $seckey -m HASH
 rm HASH
 signify -C -q -p $pubkey -x HASH.sig
 
-tar zcPf archive.tgz $srcdir 
+tar zcPf archive.tgz $srcdir/*.txt
 signify -zS -s $seckey -m archive.tgz -x signed.tgz
 # check it's still valid gzip
 gunzip -t signed.tgz
