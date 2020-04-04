a412a45212c6539000c62bda6daad7c2c6b4a4a4
jca <jca@openbsd.org>
2020-04-04 23:46:30 +0000

    Make libcxxabi provide __register_frame, _Unwind_Backtrace and friends on sparc64
    Discrepancy spotted in
    
      http://build-failures.rhaalovely.net/sparc64/2020-04-03/devel/llvm,,-main.log
    
    Input from gkoehler@, ok kettenis@ deraadt@

--- lib/libunwind/src/config.h
+++ lib/libunwind/src/config.h
@@ -75,7 +75,7 @@
     defined(__ppc__) || defined(__ppc64__) || defined(__powerpc64__) ||        \
     (!defined(__APPLE__) && defined(__arm__)) ||                               \
     (defined(__arm64__) || defined(__aarch64__)) ||                            \
-    defined(__mips__)
+    defined(__mips__) || defined(__sparc64__)
 #if !defined(_LIBUNWIND_BUILD_SJLJ_APIS)
 #define _LIBUNWIND_BUILD_ZERO_COST_APIS
 #endif
