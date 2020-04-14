673bad01c9e38fe7515790b0ef3ffdcc53d9ce2c
kettenis <kettenis@openbsd.org>
2020-04-14 20:59:53 +0000

    Add flags to specify whether internal delays should be configured for RGMII.
    ok patrick@

--- sys/dev/mii/miivar.h
+++ sys/dev/mii/miivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: miivar.h,v 1.34 2015/09/12 09:49:20 miod Exp $	*/
+/*	$OpenBSD: miivar.h,v 1.35 2020/04/14 20:59:53 kettenis Exp $	*/
 /*	$NetBSD: miivar.h,v 1.17 2000/03/06 20:56:57 thorpej Exp $	*/
 
 /*-
@@ -152,6 +152,8 @@ typedef struct mii_softc mii_softc_t;
 #define	MIIF_DOPAUSE	0x0100		/* advertise PAUSE capability */
 #define	MIIF_IS_HPNA	0x0200		/* is a HomePNA device */
 #define	MIIF_FORCEANEG	0x0400		/* force autonegotiation */
+#define	MIIF_RXID	0x0800		/* add Rx delay */
+#define	MIIF_TXID	0x1000		/* add Tx delay */
 
 #define	MIIF_INHERIT_MASK	(MIIF_NOISOLATE|MIIF_NOLOOP|MIIF_AUTOTSLEEP)
 
