e13309270b236a45d4bfba62588f742ab5cfb67c
jsing <jsing@openbsd.org>
2020-04-22 17:05:53 +0000

    Revise regress to match state transition changes.

--- regress/lib/libssl/handshake/handshake_table.c
+++ regress/lib/libssl/handshake/handshake_table.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: handshake_table.c,v 1.12 2020/03/10 17:15:37 jsing Exp $	*/
+/*	$OpenBSD: handshake_table.c,v 1.13 2020/04/22 17:05:53 jsing Exp $	*/
 /*
  * Copyright (c) 2019 Theo Buehler <tb@openbsd.org>
  *
@@ -86,16 +86,16 @@ struct child {
 
 static struct child stateinfo[][TLS13_NUM_MESSAGE_TYPES] = {
 	[CLIENT_HELLO] = {
+		{SERVER_HELLO_RETRY_REQUEST, DEFAULT, 0, 0},
+		{SERVER_HELLO, WITHOUT_HRR, 0, 0},
+	},
+	[SERVER_HELLO_RETRY_REQUEST] = {
+		{CLIENT_HELLO_RETRY, DEFAULT, 0, 0},
+	},
+	[CLIENT_HELLO_RETRY] = {
 		{SERVER_HELLO, DEFAULT, 0, 0},
 	},
 	[SERVER_HELLO] = {
-		{SERVER_ENCRYPTED_EXTENSIONS, DEFAULT, 0, 0},
-		{CLIENT_HELLO_RETRY, WITH_HRR, 0, 0},
-	},
-	[CLIENT_HELLO_RETRY] = {
-		{SERVER_HELLO_RETRY, DEFAULT, 0, 0},
-	},
-	[SERVER_HELLO_RETRY] = {
 		{SERVER_ENCRYPTED_EXTENSIONS, DEFAULT, 0, 0},
 	},
 	[SERVER_ENCRYPTED_EXTENSIONS] = {
@@ -172,8 +172,8 @@ flag2str(uint8_t flag)
 	case WITHOUT_CR:
 		ret = "WITHOUT_CR";
 		break;
-	case WITH_HRR:
-		ret = "WITH_HRR";
+	case WITHOUT_HRR:
+		ret = "WITHOUT_HRR";
 		break;
 	case WITH_PSK:
 		ret = "WITH_PSK";
@@ -221,8 +221,8 @@ mt2str(enum tls13_message_type mt)
 	case SERVER_HELLO:
 		ret = "SERVER_HELLO";
 		break;
-	case SERVER_HELLO_RETRY:
-		ret = "SERVER_HELLO_RETRY";
+	case SERVER_HELLO_RETRY_REQUEST:
+		ret = "SERVER_HELLO_RETRY_REQUEST";
 		break;
 	case SERVER_ENCRYPTED_EXTENSIONS:
 		ret = "SERVER_ENCRYPTED_EXTENSIONS";
@@ -461,6 +461,8 @@ main(int argc, char *argv[])
 	    hs_table[UINT8_MAX][TLS13_NUM_MESSAGE_TYPES] = {
 		[INITIAL] = {
 			CLIENT_HELLO,
+			SERVER_HELLO_RETRY_REQUEST,
+			CLIENT_HELLO_RETRY,
 			SERVER_HELLO,
 		},
 	};
