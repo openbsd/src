0989b4a8a37afecf8059c6954bfde4c54a02bf0a
deraadt <deraadt@openbsd.org>
2020-05-04 15:00:35 +0000

    leave -beta.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.173 2020/04/05 06:34:20 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.174 2020/05/04 15:00:35 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,10 +71,10 @@ ost="OpenBSD"
 osr="6.7"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
+#define STATUS ""			/* release */
 #if 0
 #define STATUS "-current"		/* just after a release */
-#define STATUS ""			/* release */
+#define STATUS "-beta"			/* just before a release */
 #endif
 
 const char ostype[] = "${ost}";
