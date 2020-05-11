4bc1d1b706be164b5e6811da4cfc08f10c996940
claudio <claudio@openbsd.org>
2020-05-11 16:59:19 +0000

    There is no reason to limit the string length of log_reason() to REASON_LEN charactars. Also fix a long line. OK benno@ deraadt@

--- usr.sbin/bgpd/bgpd.c
+++ usr.sbin/bgpd/bgpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpd.c,v 1.228 2020/05/10 13:38:46 deraadt Exp $ */
+/*	$OpenBSD: bgpd.c,v 1.229 2020/05/11 16:59:19 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -829,10 +829,10 @@ dispatch_imsg(struct imsgbuf *ibuf, int idx, struct bgpd_config *conf)
 			else {
 				reconfig = 1;
 				reconfpid = imsg.hdr.pid;
-				if (imsg.hdr.len == IMSG_HEADER_SIZE + REASON_LEN &&
-				    ((char *)imsg.data)[0])
-					log_info("reload due to: %.*s",
-					    REASON_LEN, log_reason(imsg.data));
+				if (imsg.hdr.len == IMSG_HEADER_SIZE +
+				    REASON_LEN && ((char *)imsg.data)[0])
+					log_info("reload due to: %s",
+					    log_reason(imsg.data));
 			}
 			break;
 		case IMSG_CTL_FIB_COUPLE:
