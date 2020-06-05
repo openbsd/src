4bc3ebd4cc1b3e066bb8688ae48c1cb0789ffa00
djm <djm@openbsd.org>
2020-06-05 06:18:07 +0000

    unbreak "sshd -ddd" - close of config passing fd happened too early. ok markus@

--- usr.bin/ssh/sshd.c
+++ usr.bin/ssh/sshd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshd.c,v 1.555 2020/05/26 01:09:05 djm Exp $ */
+/* $OpenBSD: sshd.c,v 1.556 2020/06/05 06:18:07 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1256,7 +1256,6 @@ server_accept_loop(int *sock_in, int *sock_out, int *newsock, int *config_s)
 				startup_pipe = -1;
 				pid = getpid();
 				if (rexec_flag) {
-					close(config_s[1]);
 					send_rexec_state(config_s[0], cfg);
 					close(config_s[0]);
 				}
