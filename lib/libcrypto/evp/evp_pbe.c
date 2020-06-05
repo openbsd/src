0dc17585e21702d324c1a89284e68280ad013824
jsing <jsing@openbsd.org>
2020-06-05 17:30:41 +0000

    Allow GOST R 34.11-2012 in PBE/PBKDF2/PKCS#5.
    Diff from Dmitry Baryshkov <dbaryshkov@gmail.com>
    
    Sponsored by ROSA Linux
    
    ok inoguchi@ tb@

--- lib/libcrypto/evp/evp_pbe.c
+++ lib/libcrypto/evp/evp_pbe.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: evp_pbe.c,v 1.25 2017/01/29 17:49:23 beck Exp $ */
+/* $OpenBSD: evp_pbe.c,v 1.26 2020/06/05 17:30:41 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 1999.
  */
@@ -114,6 +114,8 @@ static const EVP_PBE_CTL builtin_pbe[] = {
 	{EVP_PBE_TYPE_PRF, NID_hmacWithSHA384, -1, NID_sha384, 0},
 	{EVP_PBE_TYPE_PRF, NID_hmacWithSHA512, -1, NID_sha512, 0},
 	{EVP_PBE_TYPE_PRF, NID_id_HMACGostR3411_94, -1, NID_id_GostR3411_94, 0},
+	{EVP_PBE_TYPE_PRF, NID_id_tc26_hmac_gost_3411_12_256, -1, NID_id_tc26_gost3411_2012_256, 0},
+	{EVP_PBE_TYPE_PRF, NID_id_tc26_hmac_gost_3411_12_512, -1, NID_id_tc26_gost3411_2012_512, 0},
 };
 
 int
