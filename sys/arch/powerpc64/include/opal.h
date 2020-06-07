a41737c68e621813df385481bdf4948448c968dd
kettenis <kettenis@openbsd.org>
2020-06-07 17:18:31 +0000

    Add more return codes.

--- sys/arch/powerpc64/include/opal.h
+++ sys/arch/powerpc64/include/opal.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: opal.h,v 1.3 2020/06/07 16:13:30 kettenis Exp $	*/
+/*	$OpenBSD: opal.h,v 1.4 2020/06/07 17:18:31 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2020 Mark Kettenis <kettenis@openbsd.org>
@@ -19,6 +19,7 @@
 #ifndef _MACHINE_OPAL_H_
 #define _MACHINE_OPAL_H_
 
+/* Tokens. */
 #define OPAL_TEST			0
 #define OPAL_CONSOLE_WRITE		1
 #define OPAL_CONSOLE_READ		2
@@ -28,7 +29,23 @@
 #define OPAL_PCI_CONFIG_READ_WORD	15
 #define OPAL_PCI_CONFIG_WRITE_WORD	18
 
+/* Return codes. */
 #define OPAL_SUCCESS			0
+#define OPAL_PARAMETER			-1
+#define OPAL_BUSY			-2
+#define OPAL_PARTIAL			-3
+#define OPAL_CONSTRAINED		-4
+#define OPAL_CLOSED			-5
+#define OPAL_HARDWARE			-6
+#define OPAL_UNSUPPORTED		-7
+#define OPAL_PERMISSION			-8
+#define OPAL_NO_MEM			-9
+#define OPAL_RESOURCE			-10
+#define OPAL_INTERNAL_ERROR		-11
+#define OPAL_BUSY_EVENT			-12
+#define OPAL_HARDWARE_FROZEN		-13
+#define OPAL_WRONG_STATE		-14
+#define OPAL_ASYNC_COMPLETION		-15
 
 #ifndef _LOCORE
 int64_t	opal_test(uint64_t);
