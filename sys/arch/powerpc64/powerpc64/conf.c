72d0ce5fe4f96ec8b6392b43bba15af60ec29f87
kettenis <kettenis@openbsd.org>
2020-06-07 09:34:20 +0000

    Add a few more devices.

--- sys/arch/powerpc64/powerpc64/conf.c
+++ sys/arch/powerpc64/powerpc64/conf.c
@@ -1,6 +1,40 @@
+/*	$OpenBSD: conf.c,v 1.2 2020/06/07 09:34:20 kettenis Exp $	*/
+
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *     @(#)conf.c	7.9 (Berkeley) 5/28/91
+ */
+
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/device.h>
+#include <sys/tty.h>
 #include <sys/vnode.h>
 
 #include <machine/conf.h>
@@ -11,9 +45,18 @@ struct bdevsw bdevsw[] =
 };
 int	nblkdev = nitems(bdevsw);
 
+#include "pty.h"
+
 struct cdevsw cdevsw[] =
 {
+	cdev_cn_init(1,cn),		/* 0: virtual console */
+	cdev_ctty_init(1,ctty),		/* 1: controlling terminal */
 	cdev_notdef(),
+	cdev_notdef(),
+	cdev_notdef(),
+	cdev_tty_init(NPTY,pts),	/* 5: pseudo-tty slave */
+	cdev_ptc_init(NPTY,ptc),	/* 6: pseudo-tty master */
+	cdev_ptm_init(NPTY,ptm),	/* XX: pseudo-tty ptm device */
 };
 int	nchrdev = nitems(cdevsw);
 
