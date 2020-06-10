e09158d2a79a4dfec9b83a5cca1eba2f5223928b
nicm <nicm@openbsd.org>
2020-06-10 07:27:10 +0000

    Instead of a buffer size limit on each pane, set a limit of 300 seconds of data for each client in control mode.

--- usr.bin/tmux/tmux.h
+++ usr.bin/tmux/tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.1065 2020/06/05 11:20:51 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.1066 2020/06/10 07:27:10 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1668,6 +1668,7 @@ struct client {
 	}		 exit_type;
 	enum msgtype	 exit_msgtype;
 	char		*exit_session;
+	char		*exit_message;
 
 	struct key_table *keytable;
 
