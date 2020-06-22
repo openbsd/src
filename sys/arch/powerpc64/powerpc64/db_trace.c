4d943fd61ab7ebc3390510da6f072b3fbd6ff841
kettenis <kettenis@openbsd.org>
2020-06-22 18:49:36 +0000

    The minimum frame size for the ELFv2 ABI is 32 bytes.  Using this value instead of 48 in the trap setup functions makes it possible to get rid of the magic offset in proc_trampoline.
    Suggested by gkoehler@

--- sys/arch/powerpc64/powerpc64/db_trace.c
+++ sys/arch/powerpc64/powerpc64/db_trace.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_trace.c,v 1.4 2020/06/21 21:57:35 kettenis Exp $	*/
+/*	$OpenBSD: db_trace.c,v 1.5 2020/06/22 18:49:36 kettenis Exp $	*/
 /*	$NetBSD: db_trace.c,v 1.15 1996/02/22 23:23:41 gwr Exp $	*/
 
 /*
@@ -153,7 +153,7 @@ db_stack_trace_print(db_expr_t addr, int have_addr, db_expr_t count,
 
 		if (lr == (vaddr_t)&trapexit) {
 			struct trapframe *frame =
-			    (struct trapframe *)(sp + 48);
+			    (struct trapframe *)(sp + 32);
 
 			if ((frame->srr1 & PSL_PR) && frame->exc == EXC_SC) {
 				(*pr)("--- syscall (number %ld) ---\n",
