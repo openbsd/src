c80f17de1083aa3c960ed97b5836066610f05eaf
kettenis <kettenis@openbsd.org>
2020-06-28 08:21:27 +0000

    Add powerpc64 bits.  From drahn@

---
+++ usr.bin/gprof/powerpc64.c
@@ -0,0 +1,8 @@
+/*	$OpenBSD: powerpc64.c,v 1.1 2020/06/28 08:21:27 kettenis Exp $	*/
+#include        "gprof.h"
+
+void
+findcall(nltype *parentp, unsigned long p_lowpc, unsigned long p_highpc)
+{
+	/* not supported. */
+}
