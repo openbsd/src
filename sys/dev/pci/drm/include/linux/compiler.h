114e233526ffafebcaeb80295f177b5626d65ff6
jsg <jsg@openbsd.org>
2020-06-29 05:04:02 +0000

    use attributes for __maybe_unused and __always_inline

--- sys/dev/pci/drm/include/linux/compiler.h
+++ sys/dev/pci/drm/include/linux/compiler.h
@@ -12,16 +12,16 @@
 #define __force
 #define __acquires(x)
 #define __releases(x)
-#define __always_unused __unused
-#define __maybe_unused
 #define __read_mostly
 #define __iomem
 #define __must_check
 #define __init
 #define __exit
 #define __deprecated
-#define __always_inline inline
-#define noinline __attribute__((noinline))
+#define __always_unused	__attribute__((__unused__))
+#define __maybe_unused	__attribute__((__unused__))
+#define __always_inline	__attribute__((__always_inline__))
+#define noinline	__attribute__((__noinline__))
 #define fallthrough	do {} while (0)
 
 #ifndef __user
