86e7d0549c97c62412292a01cf0500f5db121c18
jsg <jsg@openbsd.org>
2020-06-29 10:23:12 +0000

    use time since epoch not time since boot for 'real' interfaces

--- sys/dev/pci/drm/include/linux/timekeeping.h
+++ sys/dev/pci/drm/include/linux/timekeeping.h
@@ -3,15 +3,21 @@
 #ifndef _LINUX_TIMEKEEPING_H
 #define _LINUX_TIMEKEEPING_H
 
+#define ktime_get_boottime()	ktime_get()
 #define get_seconds()		gettime()
 
-#define ktime_get_real() ktime_get()
-#define ktime_get_boottime() ktime_get()
-
-static inline int64_t
+static inline time_t
 ktime_get_real_seconds(void)
 {
-	return ktime_get().tv_sec;
+	return gettime();
+}
+
+static inline struct timeval
+ktime_get_real(void)
+{
+	struct timeval tv;
+	getmicrotime(&tv);
+	return tv;
 }
 
 static inline uint64_t
