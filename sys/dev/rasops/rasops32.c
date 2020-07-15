34a5437d6d5eb86f949663a326352d321a644a36
fcambus <fcambus@openbsd.org>
2020-07-15 08:34:25 +0000

    Optimize character rendering in 32bpp mode, with unrolled rows and pairwise foreground / background pixel rendering.
    This does double-pixel rendering for the common font widths (8, 12,
    16, and 32 pixels wide), resulting in a significant speed-up.
    
    No regression noticed on 32-bit architectures.
    
    Tested by weerd@, and by abieber@ on macppc.
    
    From John Carmack, thanks!

--- sys/dev/rasops/rasops32.c
+++ sys/dev/rasops/rasops32.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rasops32.c,v 1.10 2020/05/25 09:55:49 jsg Exp $	*/
+/*	$OpenBSD: rasops32.c,v 1.11 2020/07/15 08:34:25 fcambus Exp $	*/
 /*	$NetBSD: rasops32.c,v 1.7 2000/04/12 14:22:29 pk Exp $	*/
 
 /*-
@@ -65,9 +65,14 @@ rasops32_init(struct rasops_info *ri)
 int
 rasops32_putchar(void *cookie, int row, int col, u_int uc, uint32_t attr)
 {
-	int width, height, cnt, fs, fb, clr[2];
+	int width, height, step, cnt, fs, b, f;
+	uint32_t fb, clr[2];
 	struct rasops_info *ri;
-	int32_t *dp, *rp;
+	int64_t *rp;
+	union {
+		int64_t q[4];
+		int32_t d[4][2];
+	} u;
 	u_char *fr;
 
 	ri = (struct rasops_info *)cookie;
@@ -81,47 +86,128 @@ rasops32_putchar(void *cookie, int row, int col, u_int uc, uint32_t attr)
 		return 0;
 #endif
 
-	rp = (int32_t *)(ri->ri_bits + row*ri->ri_yscale + col*ri->ri_xscale);
+	rp = (int64_t *)(ri->ri_bits + row*ri->ri_yscale + col*ri->ri_xscale);
 
 	height = ri->ri_font->fontheight;
 	width = ri->ri_font->fontwidth;
+	step = ri->ri_stride >> 3;
 
-	clr[0] = ri->ri_devcmap[(attr >> 16) & 0xf];
-	clr[1] = ri->ri_devcmap[(attr >> 24) & 0xf];
+	b = ri->ri_devcmap[(attr >> 16) & 0xf];
+	f = ri->ri_devcmap[(attr >> 24) & 0xf];
+	u.d[0][0] = b; u.d[0][1] = b;
+	u.d[1][0] = b; u.d[1][1] = f;
+	u.d[2][0] = f; u.d[2][1] = b;
+	u.d[3][0] = f; u.d[3][1] = f;
 
 	if (uc == ' ') {
 		while (height--) {
-			dp = rp;
-			DELTA(rp, ri->ri_stride, int32_t *);
-
-			for (cnt = width; cnt; cnt--)
-				*dp++ = clr[0];
+			/* the general, pixel-at-a-time case is fast enough */
+			for (cnt = 0; cnt < width; cnt++)
+				((int *)rp)[cnt] = b;
+			rp += step;
 		}
 	} else {
 		uc -= ri->ri_font->firstchar;
 		fr = (u_char *)ri->ri_font->data + uc * ri->ri_fontscale;
 		fs = ri->ri_font->stride;
 
-		while (height--) {
-			dp = rp;
-			fb = fr[3] | (fr[2] << 8) | (fr[1] << 16) |
-			    (fr[0] << 24);
-			fr += fs;
-			DELTA(rp, ri->ri_stride, int32_t *);
-
-			for (cnt = width; cnt; cnt--) {
-				*dp++ = clr[(fb >> 31) & 1];
-				fb <<= 1;
+		/* double-pixel special cases for the common widths */
+		switch (width) {
+		case 8:
+			while (height--) {
+				fb = fr[0];
+				rp[0] = u.q[fb >> 6];
+				rp[1] = u.q[(fb >> 4) & 3];
+				rp[2] = u.q[(fb >> 2) & 3];
+				rp[3] = u.q[fb & 3];
+				rp += step;
+				fr += 1;
 			}
+			break;
+
+		case 12:
+			while (height--) {
+				fb = fr[0];
+				rp[0] = u.q[fb >> 6];
+				rp[1] = u.q[(fb >> 4) & 3];
+				rp[2] = u.q[(fb >> 2) & 3];
+				rp[3] = u.q[fb & 3];
+				fb = fr[1];
+				rp[4] = u.q[fb >> 6];
+				rp[5] = u.q[(fb >> 4) & 3];
+				rp += step;
+				fr += 2;
+			}
+			break;
+
+		case 16:
+			while (height--) {
+				fb = fr[0];
+				rp[0] = u.q[fb >> 6];
+				rp[1] = u.q[(fb >> 4) & 3];
+				rp[2] = u.q[(fb >> 2) & 3];
+				rp[3] = u.q[fb & 3];
+				fb = fr[1];
+				rp[4] = u.q[fb >> 6];
+				rp[5] = u.q[(fb >> 4) & 3];
+				rp[6] = u.q[(fb >> 2) & 3];
+				rp[7] = u.q[fb & 3];
+				rp += step;
+				fr += 2;
+			}
+			break;
+
+		case 32:
+			while (height--) {
+				fb = fr[0];
+				rp[0] = u.q[fb >> 6];
+				rp[1] = u.q[(fb >> 4) & 3];
+				rp[2] = u.q[(fb >> 2) & 3];
+				rp[3] = u.q[fb & 3];
+				fb = fr[1];
+				rp[4] = u.q[fb >> 6];
+				rp[5] = u.q[(fb >> 4) & 3];
+				rp[6] = u.q[(fb >> 2) & 3];
+				rp[7] = u.q[fb & 3];
+				fb = fr[2];
+				rp[8] = u.q[fb >> 6];
+				rp[9] = u.q[(fb >> 4) & 3];
+				rp[10] = u.q[(fb >> 2) & 3];
+				rp[11] = u.q[fb & 3];
+				fb = fr[3];
+				rp[12] = u.q[fb >> 6];
+				rp[13] = u.q[(fb >> 4) & 3];
+				rp[14] = u.q[(fb >> 2) & 3];
+				rp[15] = u.q[fb & 3];
+				rp += step;
+				fr += 4;
+			}
+			break;
+
+		default: /* there is a 5x8 font, so fall back to per-pixel */
+			clr[0] = b;
+			clr[1] = f;
+
+			while (height--) {
+				fb = fr[3] | (fr[2] << 8) | (fr[1] << 16) |
+				    (fr[0] << 24);
+				fr += fs;
+
+				for (cnt = 0; cnt < width; cnt++) {
+					((int *)rp)[cnt] = clr[fb >> 31];
+					fb <<= 1;
+				}
+				rp += step;
+			}
+			break;
 		}
 	}
 
-	/* Do underline */
+	/* Do underline a pixel at a time */
 	if ((attr & 1) != 0) {
-		DELTA(rp, -(ri->ri_stride << 1), int32_t *);
-
-		while (width--)
-			*rp++ = clr[1];
+		rp -= step;
+		for (cnt = 0; cnt < width; cnt++)
+			((int *)rp)[cnt] = f;
 	}
 
 	return 0;
