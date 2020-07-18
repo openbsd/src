efa5b6a3f1d407c6752cedffe6a7420fa9d1600f
daniel <daniel@openbsd.org>
2020-07-18 02:53:47 +0000

    Properly escape a backslash.
    Found by CompCert which notes that \E is not a valid escape sequence.
    
    ok nicm@

--- usr.bin/tmux/tty-features.c
+++ usr.bin/tmux/tty-features.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty-features.c,v 1.16 2020/06/04 10:36:28 nicm Exp $ */
+/* $OpenBSD: tty-features.c,v 1.17 2020/07/18 02:53:47 daniel Exp $ */
 
 /*
  * Copyright (c) 2020 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -110,8 +110,8 @@ static const struct tty_feature tty_feature_overline = {
 
 /* Terminal supports underscore styles. */
 static const char *tty_feature_usstyle_capabilities[] = {
-	"Smulx=\E[4::%p1%dm",
-	"Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m",
+	"Smulx=\\E[4::%p1%dm",
+	"Setulc=\\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m",
 	NULL
 };
 static const struct tty_feature tty_feature_usstyle = {
