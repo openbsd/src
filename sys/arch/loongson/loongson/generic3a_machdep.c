681e01da43452d1c5dbd6de5334fc607d5a1cafe
visa <visa@openbsd.org>
2020-07-21 05:56:02 +0000

    Do not bother masking IPIs in the Loongson 3A specific IPI routine. The interrupt dispatcher interrupt() keeps interrupts disabled anyway. Also, the IPI code should not allow nesting of interrupts.

--- sys/arch/loongson/loongson/generic3a_machdep.c
+++ sys/arch/loongson/loongson/generic3a_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: generic3a_machdep.c,v 1.9 2020/07/06 13:33:07 pirofti Exp $	*/
+/*	$OpenBSD: generic3a_machdep.c,v 1.10 2020/07/21 05:56:02 visa Exp $	*/
 
 /*
  * Copyright (c) 2009, 2010, 2012 Miodrag Vallat.
@@ -353,14 +353,8 @@ generic3a_ipi_intr(uint32_t hwpend, struct trapframe *frame)
 {
 	cpuid_t cpuid = cpu_number();
 
-	/* Mask all IPIs. */
-	REGVAL32(ls3_ipi_base[cpuid] + LS3_IPI_IMR) = ~0u;
-
 	if (ls3_ipi_handler != NULL)
 		ls3_ipi_handler((void *)cpuid);
-
-	/* Enable the IPI. */
-	REGVAL32(ls3_ipi_base[cpuid] + LS3_IPI_IMR) = 1u;
 
 	return hwpend;
 }
