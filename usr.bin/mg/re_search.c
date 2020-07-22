8a6c6275cc78ce0e6eada5e17506088fed271200
tb <tb@openbsd.org>
2020-07-22 13:29:05 +0000

    Avoid running out of memory with query-replace-regex ^
    Choosing ! (replace rest) never advances beyond the current line
    and keeps inserting the replacement test, and mg eventually runs
    out of memory.
    
    Patch from Mark Willson with minor stylistic tweaks.
    
    Looks good to Hiltjo Posthuma who would have preferred making forward
    replacing on empty lines work. Mark argued that this makes it behave
    consistently with the current behaviour when searching for ^ and the
    point at the beginning of a non-empty line in which case the cursor does
    not move either.
    
    Also tested by krw

--- usr.bin/mg/re_search.c
+++ usr.bin/mg/re_search.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: re_search.c,v 1.34 2020/07/09 10:42:24 tb Exp $	*/
+/*	$OpenBSD: re_search.c,v 1.35 2020/07/22 13:29:05 tb Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -308,7 +308,7 @@ re_doreplace(RSIZE plen, char *st)
 static int
 re_forwsrch(void)
 {
-	int	 tbo, tdotline, error;
+	int	 	 re_flags, tbo, tdotline, error;
 	struct line	*clp;
 
 	clp = curwp->w_dotp;
@@ -318,9 +318,10 @@ re_forwsrch(void)
 	if (tbo == clp->l_used)
 		/*
 		 * Don't start matching past end of line -- must move to
-		 * beginning of next line, unless at end of file.
+		 * beginning of next line, unless line is empty or at
+		 * end of file.
 		 */
-		if (clp != curbp->b_headp) {
+		if (clp != curbp->b_headp && llength(clp) != 0) {
 			clp = lforw(clp);
 			tdotline++;
 			tbo = 0;
@@ -330,10 +331,13 @@ re_forwsrch(void)
 	 * always makes the last line empty so this is good.
 	 */
 	while (clp != (curbp->b_headp)) {
+		re_flags = REG_STARTEND;
+		if (tbo != 0)
+			re_flags |= REG_NOTBOL;
 		regex_match[0].rm_so = tbo;
 		regex_match[0].rm_eo = llength(clp);
 		error = regexec(&regex_buff, ltext(clp) ? ltext(clp) : "",
-		    RE_NMATCH, regex_match, REG_STARTEND);
+		    RE_NMATCH, regex_match, re_flags);
 		if (error != 0) {
 			clp = lforw(clp);
 			tdotline++;
