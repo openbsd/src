ef3cc1ee6ac3f08f9c1fd1730cb9dd983812a804
kettenis <kettenis@openbsd.org>
2020-07-22 20:39:02 +0000

    Actually try to evict a PTE from a different slot instead of failing to evict the same PTE 16 times because the PTE is wired.

--- sys/arch/powerpc64/powerpc64/pmap.c
+++ sys/arch/powerpc64/powerpc64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.34 2020/07/22 12:38:38 kettenis Exp $ */
+/*	$OpenBSD: pmap.c,v 1.35 2020/07/22 20:39:02 kettenis Exp $ */
 
 /*
  * Copyright (c) 2015 Martin Pieuchot
@@ -717,6 +717,8 @@ pte_insert(struct pte_desc *pted)
 
 		if ((pte->pte_hi & PTE_WIRED) == 0)
 			break;
+
+		off++;
 	}
 	/*
 	 * Since we only wire unmanaged kernel mappings, we should
