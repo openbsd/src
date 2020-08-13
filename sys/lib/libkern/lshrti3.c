49f00df61520a457c70db7bca65b2d6bc6acdd2a
jca <jca@openbsd.org>
2020-08-13 10:02:46 +0000

    Move compiler_rt type definitions to a separate header
    so that we can reuse them in other compiler_rt routines.
    ok kettenis@

--- sys/lib/libkern/lshrti3.c
+++ sys/lib/libkern/lshrti3.c
@@ -12,41 +12,11 @@
  * ===----------------------------------------------------------------------===
  */
 
+#include "crt_glue.h"
+
 /* Returns: logical a >> b */
 
 /* Precondition:  0 <= b < bits_in_tword */
-
-#include <sys/limits.h>
-#include <sys/endian.h>
-
-typedef int      si_int;
-typedef unsigned int su_int;
-typedef          long long di_int;
-typedef unsigned long long du_int;
-typedef int      ti_int __attribute__ ((mode (TI)));
-typedef int      tu_int __attribute__ ((mode (TI)));
-
-#if BYTE_ORDER == LITTLE_ENDIAN
-#define _YUGA_LITTLE_ENDIAN 0
-#else
-#define _YUGA_LITTLE_ENDIAN 1
-#endif
-
-typedef union
-{
-	tu_int all;
-	struct
-	{
-#if _YUGA_LITTLE_ENDIAN
-		du_int low;
-		du_int high;
-#else
-		du_int high;
-		du_int low;
-#endif /* _YUGA_LITTLE_ENDIAN */
-	}s;
-} utwords;
-
 
 ti_int
 __lshrti3(ti_int a, si_int b)
