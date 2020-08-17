a8dc0ba10d20a4e9a0011aa745717e6916159398
semarie <semarie@openbsd.org>
2020-08-17 18:12:12 +0000

    honor /etc/nologin by unveiled it
    ok deraadt@

--- usr.bin/su/su.c
+++ usr.bin/su/su.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: su.c,v 1.81 2020/07/08 15:36:35 jca Exp $	*/
+/*	$OpenBSD: su.c,v 1.82 2020/08/17 18:12:12 semarie Exp $	*/
 
 /*
  * Copyright (c) 1988 The Regents of the University of California.
@@ -169,6 +169,8 @@ main(int argc, char **argv)
 	if (unveil(_PATH_SHELLS, "r") == -1)
 		err(1, "unveil");
 	if (unveil(_PATH_DEVDB, "r") == -1)
+		err(1, "unveil");
+	if (unveil(_PATH_NOLOGIN, "r") == -1)
 		err(1, "unveil");
 
 	for (;;) {
