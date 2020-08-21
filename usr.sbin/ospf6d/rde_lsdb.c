26a5862ced4202e77aec15814575589ab04a8126
jan <jan@openbsd.org>
2020-08-21 08:32:23 +0000

    Remove redundent code.
    OK claudio@, looks fine from tb@

--- usr.sbin/ospf6d/rde_lsdb.c
+++ usr.sbin/ospf6d/rde_lsdb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_lsdb.c,v 1.43 2020/02/17 08:12:22 denis Exp $ */
+/*	$OpenBSD: rde_lsdb.c,v 1.44 2020/08/21 08:32:23 jan Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Claudio Jeker <claudio@openbsd.org>
@@ -763,9 +763,7 @@ lsa_dump(struct lsa_tree *tree, int imsg_type, pid_t pid)
 		lsa_age(v);
 		switch (imsg_type) {
 		case IMSG_CTL_SHOW_DATABASE:
-			rde_imsg_compose_ospfe(IMSG_CTL_SHOW_DATABASE, 0, pid,
-			    &v->lsa->hdr, ntohs(v->lsa->hdr.len));
-			continue;
+			break;
 		case IMSG_CTL_SHOW_DB_SELF:
 			if (v->lsa->hdr.adv_rtr == rde_router_id())
 				break;
