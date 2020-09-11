c38ae11dbcbd677414fbf5873ae345b3fd5bfd7a
bluhm <bluhm@openbsd.org>
2020-09-11 22:48:00 +0000

    Enable cert and cipher interop tests.  cert just works.  cipher has been fixed to work with libressl TLS 1.3.  Both libressl and openssl11 replace obsolete TLS 1.2 ciphers with AEAD-AES256-GCM-SHA384 or TLS_AES_256_GCM_SHA384 in TLS 1.3 respectively.  The test expects that now.  Currently GOST does not work with libressl and TLS 1.3 and is disabled.

--- regress/lib/libssl/interop/client.c
+++ regress/lib/libssl/interop/client.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: client.c,v 1.8 2019/03/21 17:52:26 bluhm Exp $	*/
+/*	$OpenBSD: client.c,v 1.9 2020/09/11 22:48:00 bluhm Exp $	*/
 /*
  * Copyright (c) 2018-2019 Alexander Bluhm <bluhm@openbsd.org>
  *
@@ -51,7 +51,6 @@ main(int argc, char *argv[])
 	char buf[256];
 	char *ca = NULL, *crt = NULL, *key = NULL, *ciphers = NULL;
 	char *host_port, *host = "127.0.0.1", *port = "0";
-
 
 	while ((ch = getopt(argc, argv, "C:c:k:Ll:sv")) != -1) {
 		switch (ch) {
