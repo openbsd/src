23a50ae63d5d971b92780260d49c9a299f18e819
tb <tb@openbsd.org>
2020-10-10 10:19:45 +0000

    Don't leak bundle_file and cert_file paths at the end.

--- regress/lib/libcrypto/x509/bettertls/verify.c
+++ regress/lib/libcrypto/x509/bettertls/verify.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: verify.c,v 1.7 2020/10/08 14:38:09 tb Exp $ */
+/* $OpenBSD: verify.c,v 1.8 2020/10/10 10:19:45 tb Exp $ */
 /*
  * Copyright (c) 2020 Joel Sing <jsing@openbsd.org>
  * Copyright (c) 2020 Bob Beck <beck@openbsd.org>
@@ -219,6 +219,8 @@ bettertls_cert_test(const char *certs_path)
 		free(bundle_file);
 		free(cert_file);
 	}
+	free(bundle_file);
+	free(cert_file);
 	free(roots_file);
 	X509_STORE_free(store);
 }
