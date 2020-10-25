5d65abadd09117ad2f064503355dab0d21c5852d
schwarze <schwarze@openbsd.org>
2020-10-25 18:21:07 +0000

    The GNU tbl(1) program contained in the groff package internally uses roff(7) tabulator settings to implement tables, and it used to leak the changed tabulator settings from tables to the subsequent roff(7) code.  In mandoc/tbl_term.c rev. 1.42 (June 17, 2017), code was added to be bug-compatible with groff.
    In commit d0e03cf6 (Oct 20, 2020), GNU tbl(1) changed behaviour
    to save the tabulator settings before starting a table and restore
    them afterwards.  Adjust mandoc for compatibility.
    
    Since mandoc implements tables without using roff(7) tabulator
    settings, saving and restoring tabulator settings is not needed in
    mandoc.  Simply deleting the code that changed tabulator settings
    by reverting tbl_term.c rev. 1.42 is sufficient in mandoc.
    Also adjust the desired output of the regression tests
    to match the new behaviour of both groff and mandoc.

--- usr.bin/mandoc/tbl_term.c
+++ usr.bin/mandoc/tbl_term.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tbl_term.c,v 1.61 2020/01/11 16:24:33 schwarze Exp $ */
+/*	$OpenBSD: tbl_term.c,v 1.62 2020/10/25 18:21:07 schwarze Exp $ */
 /*
  * Copyright (c) 2009, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011-2020 Ingo Schwarze <schwarze@openbsd.org>
@@ -187,17 +187,6 @@ term_tbl(struct termp *tp, const struct tbl_span *sp)
 		tp->tbl.arg = tp;
 
 		tblcalc(&tp->tbl, sp, tp->tcol->offset, tp->tcol->rmargin);
-
-		/* Tables leak .ta settings to subsequent text. */
-
-		term_tab_set(tp, NULL);
-		coloff = sp->opts->opts & (TBL_OPT_BOX | TBL_OPT_DBOX) ||
-		    sp->opts->lvert;
-		for (ic = 0; ic < sp->opts->cols; ic++) {
-			coloff += tp->tbl.cols[ic].width;
-			term_tab_iset(coloff);
-			coloff += tp->tbl.cols[ic].spacing;
-		}
 
 		/* Center the table as a whole. */
 
