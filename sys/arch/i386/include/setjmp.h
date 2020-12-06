3a26117014390486743ddc86d9fd1b9bca79fbd6
bluhm <bluhm@openbsd.org>
2020-12-06 15:31:30 +0000

    Introduce constants to access the setjmp(3) jmp_buf fields from i386 libc.  The assembler code is more readable than with magic numbers.  This brings i386 in line with amd64.  No change in object file. OK kettenis@

--- sys/arch/i386/include/setjmp.h
+++ sys/arch/i386/include/setjmp.h
@@ -1,8 +1,19 @@
-/*	$OpenBSD: setjmp.h,v 1.2 2000/08/05 22:07:32 niklas Exp $	*/
+/*	$OpenBSD: setjmp.h,v 1.3 2020/12/06 15:31:30 bluhm Exp $	*/
 /*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:36:43 cgd Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN	10		/* size, in longs, of a jmp_buf */
+#define _JB_EIP		0
+#define _JB_EBX		1
+#define _JB_ESP		2
+#define _JB_EBP		3
+#define _JB_ESI		4
+#define _JB_EDI		5
+#define _JB_SIGMASK	6
+#define _JB_SIGFLAG	7
+#define _JB_MXCSR	8
+#define _JB_FCW		9
+
+#define _JBLEN		10	/* size, in longs, of a jmp_buf */
