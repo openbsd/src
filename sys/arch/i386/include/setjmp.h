4856180f9a2b3d6bc7e69d3b31e067e957f4b8b5
bluhm <bluhm@openbsd.org>
2020-12-13 21:21:32 +0000

    Geode CPU does not support SSE, so MXCSR does not exists there.  As our i386 compiler does not generate SSE instructions by default, it is not strictly necessary to save MXCSR content between setjmp(3) and longjmp(3).  We do not want to end supporting such old processors now.  Remove the stmxcsr and ldmxcsr instructions from libc. reported by Johan Huldtgren; OK jsg@ kettenis@

--- sys/arch/i386/include/setjmp.h
+++ sys/arch/i386/include/setjmp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setjmp.h,v 1.3 2020/12/06 15:31:30 bluhm Exp $	*/
+/*	$OpenBSD: setjmp.h,v 1.4 2020/12/13 21:21:32 bluhm Exp $	*/
 /*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:36:43 cgd Exp $	*/
 
 /*
@@ -13,7 +13,6 @@
 #define _JB_EDI		5
 #define _JB_SIGMASK	6
 #define _JB_SIGFLAG	7
-#define _JB_MXCSR	8
-#define _JB_FCW		9
+#define _JB_FCW		8
 
 #define _JBLEN		10	/* size, in longs, of a jmp_buf */
