9dbe817d498564132d7cbf200f3355a7b4e551cf
mpi <mpi@openbsd.org>
2020-12-15 22:14:42 +0000

    Remove the assertion in uvm_km_pgremove().
    At least some initialization code on i386 calls it w/o KERNEL_LOCK().
    
    Found the hardway by jungle Boogie and Hrvoje Popovski.

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.138 2020/12/14 13:29:18 mpi Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.139 2020/12/15 22:14:42 mpi Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
@@ -243,7 +243,6 @@ uvm_km_pgremove(struct uvm_object *uobj, vaddr_t start, vaddr_t end)
 	voff_t curoff;
 	int slot;
 
-	KERNEL_ASSERT_LOCKED();
 	KASSERT(uobj->pgops == &aobj_pager);
 
 	for (curoff = start ; curoff < end ; curoff += PAGE_SIZE) {
