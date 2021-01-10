001f9a4bbb1e2e6dd349366b4c3c50ec20c8df04
visa <visa@openbsd.org>
2021-01-10 09:14:18 +0000

    Disable base-gcc on loongson and octeon.
    OK deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.205 2020/12/22 04:32:08 visa Exp $
+#	$OpenBSD: bsd.own.mk,v 1.206 2021/01/10 09:14:18 visa Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -43,7 +43,6 @@ BUILD_GCC3?=yes
 BUILD_GCC3?=no
 .endif
 .if !empty(GCC4_ARCH:M${_arch}) || ${MACHINE_ARCH} == "amd64" || \
-    ${MACHINE_ARCH} == "mips64" || ${MACHINE_ARCH} == "mips64el" || \
     ${MACHINE_ARCH} == "powerpc"
 BUILD_GCC4?=yes
 .else
