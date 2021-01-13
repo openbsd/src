b02e17298eca4b4d0cd32ef932c890f28087451e
jmatthew <jmatthew@openbsd.org>
2021-01-13 23:50:21 +0000

    put M_ZERO in the right place
    ok dlg@

--- sys/dev/pv/vmt.c
+++ sys/dev/pv/vmt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmt.c,v 1.19 2020/06/24 22:03:40 cheloha Exp $ */
+/*	$OpenBSD: vmt.c,v 1.20 2021/01/13 23:50:21 jmatthew Exp $ */
 
 /*
  * Copyright (c) 2007 David Crawshaw <david@zentus.com>
@@ -418,7 +418,7 @@ vmt_kvop(void *arg, int op, char *key, char *value, size_t valuelen)
 	int error = 0;
 
 	bufsz = VMT_RPC_BUFLEN;
-	buf = malloc(bufsz, M_TEMP|M_ZERO, M_WAITOK);
+	buf = malloc(bufsz, M_TEMP, M_WAITOK | M_ZERO);
 
 	switch (op) {
 	case PVBUS_KVWRITE:
