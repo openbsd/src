ec786feeeb6868735dfc2e233bdde499db5f7abd
claudio <claudio@openbsd.org>
2021-01-19 10:16:44 +0000

    Like in all other log.c file just use exit(1) in fatal() calls. This log.c should be further synced with the other log.c files.

--- usr.sbin/ripd/log.c
+++ usr.sbin/ripd/log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: log.c,v 1.7 2017/03/21 12:06:56 bluhm Exp $ */
+/*	$OpenBSD: log.c,v 1.8 2021/01/19 10:16:44 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -150,10 +150,7 @@ fatal(const char *emsg)
 			logit(LOG_CRIT, "fatal in %s: %s",
 			    log_procname, emsg);
 
-	if (ripd_process == PROC_MAIN)
-		exit(1);
-	else				/* parent copes via SIGCHLD */
-		_exit(1);
+	exit(1);
 }
 
 void
