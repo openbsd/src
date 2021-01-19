e2c2429cdd179c83e8c2a6c8dc50616746d5ce19
kettenis <kettenis@openbsd.org>
2021-01-19 12:29:07 +0000

    Remove some unused #defines and remove some commented-out variables.

--- sys/arch/armv7/omap/gptimer.c
+++ sys/arch/armv7/omap/gptimer.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: gptimer.c,v 1.9 2020/07/12 20:36:37 naddy Exp $ */
+/* $OpenBSD: gptimer.c,v 1.10 2021/01/19 12:29:07 kettenis Exp $ */
 /*
  * Copyright (c) 2007,2009 Dale Rahn <drahn@openbsd.org>
  *
@@ -98,10 +98,6 @@
 
 static struct evcount clk_count;
 static struct evcount stat_count;
-#define GPT1_IRQ  38
-#define GPTIMER0_IRQ	38
-
-//static int clk_irq = GPT1_IRQ; /* XXX 37 */
 
 void gptimer_attach(struct device *parent, struct device *self, void *args);
 int gptimer_intr(void *frame);
@@ -281,7 +277,6 @@ gptimer_intr(void *frame)
 void
 gptimer_cpu_initclocks()
 {
-//	u_int32_t now;
 	stathz = 128;
 	profhz = 1024;
 
