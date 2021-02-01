490e531a93b653cba1836d6c61936d91d79106eb
deraadt <deraadt@openbsd.org>
2021-02-01 05:49:48 +0000

    a regular function decl collides with an inline, due to C99 inline rules. We are never updating this sub-tree.  Knock out the collision in the simplest way.  diff from mortimer. This is the last change required for -fno-common on all architectures, thanks to mortimer for starting the effort and encouraging others.

--- gnu/gcc/gcc/toplev.h
+++ gnu/gcc/gcc/toplev.h
@@ -169,7 +169,7 @@ extern int floor_log2                  (unsigned HOST_WIDE_INT);
 #  define CLZ_HWI __builtin_clz
 #  define CTZ_HWI __builtin_ctz
 # endif
-
+/*
 extern inline int
 floor_log2 (unsigned HOST_WIDE_INT x)
 {
@@ -181,6 +181,7 @@ exact_log2 (unsigned HOST_WIDE_INT x)
 {
   return x == (x & -x) && x ? (int) CTZ_HWI (x) : -1;
 }
+*/
 #endif /* GCC_VERSION >= 3004 */
 
 /* Functions used to get and set GCC's notion of in what directory
