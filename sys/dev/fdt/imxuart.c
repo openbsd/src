d67a9733ec34dc73f2e8770f9e78cf952c58c605
patrick <patrick@openbsd.org>
2021-02-04 22:40:04 +0000

    Tedu unnecessary imxuartvar.h.
    ok kettenis@

--- sys/dev/fdt/imxuart.c
+++ sys/dev/fdt/imxuart.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: imxuart.c,v 1.8 2019/07/19 00:17:15 cheloha Exp $ */
+/* $OpenBSD: imxuart.c,v 1.9 2021/02/04 22:40:04 patrick Exp $ */
 /*
  * Copyright (c) 2005 Dale Rahn <drahn@motorola.com>
  *
@@ -39,7 +39,6 @@
 #endif
 
 #include <dev/fdt/imxuartreg.h>
-#include <dev/fdt/imxuartvar.h>
 
 #include <dev/ofw/openfirm.h>
 #include <dev/ofw/ofw_clock.h>
