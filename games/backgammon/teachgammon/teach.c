9edb0cbec17def024c21f16878acbde33cb6a13b
mestre <mestre@openbsd.org>
2021-02-06 21:42:30 +0000

    while learning (teachgammon(1)) you might want to save your game so "{w,c}path" pledge(2) permissions are required

--- games/backgammon/teachgammon/teach.c
+++ games/backgammon/teachgammon/teach.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: teach.c,v 1.18 2017/01/21 08:22:57 krw Exp $	*/
+/*	$OpenBSD: teach.c,v 1.19 2021/02/06 21:42:30 mestre Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
 {
 	int     i;
 
-	if (pledge("stdio rpath tty exec", NULL) == -1)
+	if (pledge("stdio rpath wpath cpath tty exec", NULL) == -1)
 		err(1, "pledge");
 
 	signal(SIGINT, getout);
