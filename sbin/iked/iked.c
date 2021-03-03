47ed2a289c6912127e200f176d7e9e5067283b73
tobhe <tobhe@openbsd.org>
2021-03-03 22:18:00 +0000

    Free sc_vroute on shutdown.

--- sbin/iked/iked.c
+++ sbin/iked/iked.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iked.c,v 1.55 2021/02/22 21:58:12 tobhe Exp $	*/
+/*	$OpenBSD: iked.c,v 1.56 2021/03/03 22:18:00 tobhe Exp $	*/
 
 /*
  * Copyright (c) 2019 Tobias Heider <tobias.heider@stusta.de>
@@ -479,6 +479,7 @@ parent_shutdown(struct iked *env)
 {
 	proc_kill(&env->sc_ps);
 
+	free(env->sc_vroute);
 	free(env);
 
 	log_warnx("parent terminating");
