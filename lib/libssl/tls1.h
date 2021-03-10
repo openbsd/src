5fb603068396a80293d0f411297e6119a4473d62
jsing <jsing@openbsd.org>
2021-03-10 18:32:38 +0000

    Guard TLS1_get_{client_,}version() macros with #ifndef LIBRESSL_INTERNAL.
    These are no longer used (and should not be used) internally.

--- lib/libssl/tls1.h
+++ lib/libssl/tls1.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls1.h,v 1.41 2020/06/05 18:14:05 jsing Exp $ */
+/* $OpenBSD: tls1.h,v 1.42 2021/03/10 18:32:38 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -177,11 +177,13 @@ extern "C" {
 #define TLS1_VERSION_MAJOR		0x03
 #define TLS1_VERSION_MINOR		0x01
 
+#ifndef LIBRESSL_INTERNAL
 #define TLS1_get_version(s) \
 		((s->version >> 8) == TLS1_VERSION_MAJOR ? s->version : 0)
 
 #define TLS1_get_client_version(s) \
 		((s->client_version >> 8) == TLS1_VERSION_MAJOR ? s->client_version : 0)
+#endif
 
 /*
  * TLS Alert codes.
