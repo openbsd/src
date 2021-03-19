3db7c0f63c899a7470bd6ee63aa74a089acc7bac
djm <djm@openbsd.org>
2021-03-19 04:23:50 +0000

    add a test for misc.c:argv_split(), currently fails

--- regress/usr.bin/ssh/unittests/misc/tests.c
+++ regress/usr.bin/ssh/unittests/misc/tests.c
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: tests.c,v 1.5 2021/03/19 03:25:01 djm Exp $ */
+/* 	$OpenBSD: tests.c,v 1.6 2021/03/19 04:23:50 djm Exp $ */
 /*
  * Regress test for misc helper functions.
  *
@@ -20,6 +20,7 @@
 void test_parse(void);
 void test_convtime(void);
 void test_expand(void);
+void test_argv(void);
 
 void
 tests(void)
@@ -27,4 +28,5 @@ tests(void)
 	test_parse();
 	test_convtime();
 	test_expand();
+	test_argv();
 }
