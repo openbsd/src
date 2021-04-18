eb43b0d362e4b4c1a52331b657c2867e36492069
deraadt <deraadt@openbsd.org>
2021-04-18 23:40:52 +0000

    post 6.9 development continues...

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.180 2021/04/04 23:03:07 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.181 2021/04/18 23:40:52 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,9 +71,9 @@ ost="OpenBSD"
 osr="6.9"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #endif
 
