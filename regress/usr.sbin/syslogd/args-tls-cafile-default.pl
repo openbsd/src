cee623ccd7642a1c73ce99110ce82a853cff7630
bluhm <bluhm@openbsd.org>
2021-04-19 10:58:31 +0000

    Fix TLS error message race.  Due to less handshakes in TLS 1.3 an EPIPE error may be reported instead of a TLS alert.

--- regress/usr.sbin/syslogd/args-tls-cafile-default.pl
+++ regress/usr.sbin/syslogd/args-tls-cafile-default.pl
@@ -7,7 +7,11 @@
 
 use strict;
 use warnings;
+use Errno ':POSIX';
 use Socket;
+
+my @errors = (EPIPE);
+my $errors = "(". join("|", map { $! = $_ } @errors). ")";
 
 our %args = (
     syslogd => {
@@ -32,8 +36,9 @@ our %args = (
 	exit => 255,
 	loggrep => {
 	    qr/listen sock: (127.0.0.1|::1) \d+/ => 1,
-	    qr/SSL accept attempt failed error:/.
-		qr/.*ST_ACCEPT:tlsv1 alert unknown ca/ => 1,
+	    qr/IO::Socket::SSL socket accept failed: /.
+		qr/.*,SSL accept attempt failed error:.*/.
+		qr/(ST_ACCEPT:tlsv1 alert unknown ca|$errors)/ => 1,
 	    get_testgrep() => 0,
 	},
     },
