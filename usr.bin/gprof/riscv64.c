7c92cdcd390f7f7d4f28f76f17706c36cec07d8e
drahn <drahn@openbsd.org>
2021-04-29 18:48:30 +0000

    *** empty log message ***

---
+++ usr.bin/gprof/riscv64.c
@@ -0,0 +1,8 @@
+/*	$OpenBSD: riscv64.c,v 1.1 2021/04/29 18:48:30 drahn Exp $	*/
+#include        "gprof.h"
+
+void
+findcall(nltype *parentp, unsigned long p_lowpc, unsigned long p_highpc)
+{
+	/* not supported. */
+}
