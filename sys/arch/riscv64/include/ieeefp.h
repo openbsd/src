8ff8b8d9b89466c8a9411414b0f03f2638b18809
drahn <drahn@openbsd.org>
2021-04-30 00:25:52 +0000

    Fix line swap resulting in misplaced ',' causing build error.

--- sys/arch/riscv64/include/ieeefp.h
+++ sys/arch/riscv64/include/ieeefp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieeefp.h,v 1.2 2021/04/29 17:19:18 kettenis Exp $	*/
+/*	$OpenBSD: ieeefp.h,v 1.3 2021/04/30 00:25:52 drahn Exp $	*/
 
 /*
  * Based on ieeefp.h written by J.T. Conklin, Apr 28, 1995
@@ -17,9 +17,9 @@ typedef int fp_except;
 
 typedef enum {
     FP_RN=0,			/* round to nearest representable number */
-    FP_RZ=1			/* round to zero (truncate) */
+    FP_RZ=1,			/* round to zero (truncate) */
     FP_RM=2,			/* round toward negative infinity */
-    FP_RP=3,			/* round toward positive infinity */
+    FP_RP=3			/* round toward positive infinity */
 } fp_rnd;
 
 #endif /* _MACHINE_IEEEFP_H_ */
