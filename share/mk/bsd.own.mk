9b5c53ead0651c9bf8eaadcbb830252cc329e4ec
naddy <naddy@openbsd.org>
2021-05-02 22:32:30 +0000

    disable base-gcc on amd64; ok jca@ deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.209 2021/05/01 16:11:10 visa Exp $
+#	$OpenBSD: bsd.own.mk,v 1.210 2021/05/02 22:32:31 naddy Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -38,7 +38,7 @@ BUILD_GCC3?=yes
 .else
 BUILD_GCC3?=no
 .endif
-.if !empty(GCC4_ARCH:M${_arch}) || ${MACHINE_ARCH} == "amd64"
+.if !empty(GCC4_ARCH:M${_arch})
 BUILD_GCC4?=yes
 .else
 BUILD_GCC4?=no
