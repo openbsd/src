0123979b79b22118ce557e35a8632f161f0937d6
inoguchi <inoguchi@openbsd.org>
2021-05-03 23:42:04 +0000

    Modify regress ssl_get_shared_ciphers for portable
    - Split out the intermediate path (../certs/) to Makefile
    - Change 'shutdown' to 'shutdown_all'
    
    ok tb@

--- regress/lib/libssl/unit/ssl_get_shared_ciphers.c
+++ regress/lib/libssl/unit/ssl_get_shared_ciphers.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ssl_get_shared_ciphers.c,v 1.8 2021/01/21 05:02:25 tb Exp $ */
+/*	$OpenBSD: ssl_get_shared_ciphers.c,v 1.9 2021/05/03 23:42:04 inoguchi Exp $ */
 /*
  * Copyright (c) 2021 Theo Buehler <tb@openbsd.org>
  *
@@ -353,7 +353,7 @@ handshake(SSL *client_ssl, SSL *server_ssl, const char *description)
 }
 
 static int
-shutdown(SSL *client_ssl, SSL *server_ssl, const char *description)
+shutdown_all(SSL *client_ssl, SSL *server_ssl, const char *description)
 {
 	int loops = 0, client_ret = 0, server_ret = 0;
 
@@ -442,7 +442,7 @@ test_get_shared_ciphers(const struct ssl_shared_ciphers_test_data *test)
 		goto err;
 	}
 
-	if (!shutdown(client_ssl, server_ssl, test->description))
+	if (!shutdown_all(client_ssl, server_ssl, test->description))
 		goto err;
 
 	failed = check_shared_ciphers(test, buf);
@@ -462,7 +462,7 @@ main(int argc, char **argv)
 	size_t i;
 	int failed = 0;
 
-	if (asprintf(&server_cert, "%s/../certs/server.pem", CURDIR) == -1) {
+	if (asprintf(&server_cert, "%s/server.pem", CERTSDIR) == -1) {
 		fprintf(stderr, "asprintf server_cert failed\n");
 		failed = 1;
 		goto err;
