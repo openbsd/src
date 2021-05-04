2795574053f7addabf6fb8e3ed5d47c8a33b3c7a
aoyama <aoyama@openbsd.org>
2021-05-04 14:05:12 +0000

    Fix 2nd and 3rd arguments' cast in atomic_cas_ptr().
    This fixes compile errors (actually warnings) on m88k in sys/net/pf.c
    revision 1.1116 changes.
    
    Diff from Miod Vallat, tested on GENERIC and GENERIC.MP by me.

--- sys/arch/m88k/include/atomic.h
+++ sys/arch/m88k/include/atomic.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atomic.h,v 1.14 2018/08/22 11:25:58 aoyama Exp $	*/
+/*	$OpenBSD: atomic.h,v 1.15 2021/05/04 14:05:12 aoyama Exp $	*/
 
 /* Public Domain */
 
@@ -137,7 +137,7 @@ atomic_clear_int(volatile unsigned int *uip)
 	((unsigned long)atomic_cas_uint((unsigned int *)p, (unsigned int)o, \
 	 (unsigned int)n))
 #define	atomic_cas_ptr(p,o,n) \
-	((void *)atomic_cas_uint((void *)p, (void *)o, (void *)n))
+	((void *)atomic_cas_uint((void *)p, (unsigned int)o, (unsigned int)n))
 
 #define	atomic_swap_ulong(p,o) \
 	((unsigned long)atomic_swap_uint((unsigned int *)p, (unsigned int)o)
