1cb19090bf0e66960b2e1fc2e5f52f7be1ca5353
drahn <drahn@openbsd.org>
2021-05-09 21:26:06 +0000

    Change offsets to (N * 8) to reduce chance of register clobber and mistakes. Simplify integer loading, use 'li <dest>, <value>' instead of x0/zero register Adjust _JB_SIGMASK to not collide with saved registers.

--- sys/arch/riscv64/include/setjmp.h
+++ sys/arch/riscv64/include/setjmp.h
@@ -1,8 +1,8 @@
-/* $OpenBSD: setjmp.h,v 1.1 2021/04/23 02:42:16 drahn Exp $ */
+/* $OpenBSD: setjmp.h,v 1.2 2021/05/09 21:26:06 drahn Exp $ */
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN		256	/* sp, ra, [f]s0-11, magic val, sigmask */
-#define	_JB_SIGMASK	27
+#define	_JBLEN		256 /* sp, ra, [f]s0-11, fscr, magic val, sigmask */
+#define	_JB_SIGMASK	28
