70fbd3f17097ff67ab6e722ee4577adcafd25d21
bluhm <bluhm@openbsd.org>
2021-05-21 23:00:02 +0000

    Ignore return value of execve(2) in ktrace output.  It is meaningless. This fixes syslogd regress on powerpc64. discussed with kettenis@

--- regress/usr.sbin/syslogd/args-sighup-privsep.pl
+++ regress/usr.sbin/syslogd/args-sighup-privsep.pl
@@ -20,7 +20,7 @@ our %args = (
     syslogd => {
 	ktrace => {
 	    qr/syslogd  PSIG  SIGHUP caught handler/ => 2,
-	    qr/syslogd  RET   execve 0/ => 2,
+	    qr/syslogd  RET   execve \d+/ => 2,
 	},
 	loggrep => {
 	    qr/syslogd: restarted/ => 1,
