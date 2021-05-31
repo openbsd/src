7be14faee88b1a8c060913ec06b63bd2e04ceee5
tb <tb@openbsd.org>
2021-05-31 17:39:24 +0000

    Revert previous. bluhm noted that it causes a regress failure.

--- lib/libm/src/s_lroundl.c
+++ lib/libm/src/s_lroundl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: s_lroundl.c,v 1.4 2021/05/28 15:16:43 tb Exp $	*/
+/*	$OpenBSD: s_lroundl.c,v 1.5 2021/05/31 17:39:24 tb Exp $	*/
 
 /*-
  * Copyright (c) 2005 David Schultz <das@FreeBSD.ORG>
@@ -47,9 +47,9 @@
  * that everything is in range.  At compile time, INRANGE(x) should reduce to
  * two floating-point comparisons in the former case, or TRUE otherwise.
  */
-static const type dtype_min = (type)DTYPE_MIN - 0.5;
-static const type dtype_max = (type)DTYPE_MAX + 0.5;
-#define	INRANGE(x)	(dtype_max - (type)DTYPE_MAX != 0.5 || \
+static const type dtype_min = DTYPE_MIN - 0.5;
+static const type dtype_max = DTYPE_MAX + 0.5;
+#define	INRANGE(x)	(dtype_max - DTYPE_MAX != 0.5 || \
 			 ((x) > dtype_min && (x) < dtype_max))
 
 dtype
