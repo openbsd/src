8837872478fa82b836caf00e4d668d8e2b8e142c
dtucker <dtucker@openbsd.org>
2021-06-08 07:02:46 +0000

    Check if IPQoS or TunnelDevice are already set before overriding. Prevents values in config files from overriding values supplied on the command line.  bz#3319, ok markus.

--- usr.bin/ssh/readconf.c
+++ usr.bin/ssh/readconf.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: readconf.c,v 1.354 2021/05/23 18:22:57 naddy Exp $ */
+/* $OpenBSD: readconf.c,v 1.355 2021/06/08 07:02:46 dtucker Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1728,7 +1728,7 @@ parse_pubkey_algos:
 			    filename, linenum);
 			return -1;
 		}
-		if (*activep) {
+		if (*activep && options->tun_local == -1) {
 			options->tun_local = value;
 			options->tun_remote = value2;
 		}
@@ -1836,7 +1836,7 @@ parse_pubkey_algos:
 			    filename, linenum, arg);
 			return -1;
 		}
-		if (*activep) {
+		if (*activep && options->ip_qos_interactive == -1) {
 			options->ip_qos_interactive = value;
 			options->ip_qos_bulk = value2;
 		}
