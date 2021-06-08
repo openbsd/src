7f3a6a269e639fda0e7dd8a5a682dd0aa1e8db2d
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-08 12:21:57 -0400

    Update memchr.c

--- lib/libc/string/memchr.c
+++ lib/libc/string/memchr.c
@@ -39,9 +39,13 @@ memchr(const void *s, int c, size_t n)
 	if (n != 0) {
 		const unsigned char *p = s;
 
+		const unsigned char uc = (unsigned char)c;
+
 		do {
-			if (*p++ == (unsigned char)c)
-				return ((void *)(p - 1));
+			if (*p == (unsigned char)c)
+				return ((void *)p);
+
+			p++;
 		} while (--n != 0);
 	}
 	return (NULL);
