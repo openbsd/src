85a98a23b2870b061e753517cb68bf593725bfba
matthieu <matthieu@openbsd.org>
2021-06-26 06:54:00 +0000

    Revert last change, which is under an #ifdef __linux__ block so no used.
    noticed by jsg@

--- sys/dev/pci/drm/ttm/ttm_bo_util.c
+++ sys/dev/pci/drm/ttm/ttm_bo_util.c
@@ -551,7 +551,7 @@ pgprot_t ttm_io_prot(uint32_t caching_flags, pgprot_t tmp)
 		tmp = pgprot_noncached(tmp);
 #endif
 #if defined(__ia64__) || defined(__arm__) || defined(__aarch64__) || \
-    defined(__powerpc__) || defined(__mips__) || defined(__riscv64__)
+    defined(__powerpc__) || defined(__mips__)
 	if (caching_flags & TTM_PL_FLAG_WC)
 		tmp = pgprot_writecombine(tmp);
 	else
