953b63135b62637cfd0b2d30fdcc0cb750699406
patrick <patrick@openbsd.org>
2021-06-29 12:46:36 +0000

    When configuring the USB3 phy, try the OFW PHY API first, and if there's no matching device, fall back to xhci(4)'s PHY configuration code.
    ok kettenis@

--- sys/dev/fdt/xhci_fdt.c
+++ sys/dev/fdt/xhci_fdt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: xhci_fdt.c,v 1.16 2020/12/18 18:13:17 patrick Exp $	*/
+/*	$OpenBSD: xhci_fdt.c,v 1.17 2021/06/29 12:46:36 patrick Exp $	*/
 /*
  * Copyright (c) 2017 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -263,6 +263,9 @@ xhci_init_phys(struct xhci_fdt_softc *sc)
 	 */
 	idx = OF_getindex(sc->sc_node, "usb3-phy", "phy-names");
 	if (idx < 0)
+		return;
+
+	if (phy_enable_idx(sc->sc_node, idx) != ENXIO)
 		return;
 
 	len = OF_getproplen(sc->sc_node, "phys");
