17715cfacbfb73508d6d985f866e6ea2003d2d0e
gkoehler <gkoehler@openbsd.org>
2021-08-21 03:00:02 +0000

    Switch macppc to ld.lld
    Add powerpc to LLD_ARCH.  Remove -Wl,-relax flag; ld.bfd needed it,
    but ld.lld can't take this flag.
    
    To avoid linker errors on macppc, you need a snapshot where
    /usr/bin/ld is LLD.
    
    ok kettenis@ deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.210 2021/05/02 22:32:31 naddy Exp $
+#	$OpenBSD: bsd.own.mk,v 1.211 2021/08/21 03:00:02 gkoehler Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -18,7 +18,7 @@ YP?=		yes
 CLANG_ARCH=aarch64 amd64 arm i386 mips64 mips64el powerpc powerpc64 riscv64 sparc64
 GCC4_ARCH=alpha hppa sh sparc64
 GCC3_ARCH=m88k
-LLD_ARCH=aarch64 amd64 arm i386 powerpc64 riscv64
+LLD_ARCH=aarch64 amd64 arm i386 powerpc powerpc64 riscv64
 
 # m88k: ?
 PIE_ARCH=aarch64 alpha amd64 arm hppa i386 mips64 mips64el powerpc powerpc64 riscv64 sh sparc64
