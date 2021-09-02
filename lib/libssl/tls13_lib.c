92bfa7b0f1ae397cd74dc373ca0f6f7b63b073b8
beck <beck@openbsd.org>
2021-09-02 11:58:30 +0000

    Call the ocsp callback if present and we get no response, instead of succeeding unconditionally.  Makes muststaple work with tls1.3 in nc
    ok tb@

--- lib/libssl/tls13_lib.c
+++ lib/libssl/tls13_lib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tls13_lib.c,v 1.60 2021/08/30 19:25:43 jsing Exp $ */
+/*	$OpenBSD: tls13_lib.c,v 1.61 2021/09/02 11:58:30 beck Exp $ */
 /*
  * Copyright (c) 2018, 2019 Joel Sing <jsing@openbsd.org>
  * Copyright (c) 2019 Bob Beck <beck@openbsd.org>
@@ -195,8 +195,7 @@ tls13_legacy_ocsp_status_recv_cb(void *arg)
 	SSL *s = ctx->ssl;
 	int ret;
 
-	if (s->ctx->internal->tlsext_status_cb == NULL ||
-	    s->internal->tlsext_ocsp_resp == NULL)
+	if (s->ctx->internal->tlsext_status_cb == NULL)
 		return 1;
 
 	ret = s->ctx->internal->tlsext_status_cb(s,
