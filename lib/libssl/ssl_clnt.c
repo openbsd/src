4d783bdb519709114ba8ae52a1552658c372a9d6
jsing <jsing@openbsd.org>
2021-09-03 13:18:17 +0000

    Ensure that a server hello does not have trailing data.
    Found by tlsfuzzer.
    
    ok beck@

--- lib/libssl/ssl_clnt.c
+++ lib/libssl/ssl_clnt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_clnt.c,v 1.110 2021/09/02 14:41:03 beck Exp $ */
+/* $OpenBSD: ssl_clnt.c,v 1.111 2021/09/03 13:18:17 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1064,6 +1064,9 @@ ssl3_get_server_hello(SSL *s)
 		SSLerror(s, SSL_R_PARSE_TLSEXT);
 		goto fatal_err;
 	}
+
+	if (CBS_len(&cbs) != 0)
+		goto decode_err;
 
 	/*
 	 * Determine if we need to see RI. Strictly speaking if we want to
