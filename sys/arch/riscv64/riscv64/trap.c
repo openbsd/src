8dc94f388f54450ea5ceff4f8d50534acf2ce71a
jca <jca@openbsd.org>
2021-09-03 14:58:25 +0000

    Zap a chatty printf
    ok mlarkin@ kettenis@

--- sys/arch/riscv64/riscv64/trap.c
+++ sys/arch/riscv64/riscv64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.16 2021/07/26 22:13:19 kettenis Exp $	*/
+/*	$OpenBSD: trap.c,v 1.17 2021/09/03 14:58:25 jca Exp $	*/
 
 /*
  * Copyright (c) 2020 Shivam Waghela <shivamwaghela@gmail.com>
@@ -159,7 +159,6 @@ do_trap_user(struct trapframe *frame)
 		trapsignal(p, SIGILL, 0, ILL_ILLTRP, sv);
 		break;
 	case EXCP_BREAKPOINT:
-		printf("BREAKPOINT\n");
 		sv.sival_ptr = (void *)frame->tf_stval;
 		trapsignal(p, SIGTRAP, 0, TRAP_BRKPT, sv);
 		break;
