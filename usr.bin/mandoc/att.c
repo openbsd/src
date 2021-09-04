ca3d78cb52dcb4d690754a9aa872b86caa08d37d
schwarze <schwarze@openbsd.org>
2021-09-04 20:24:40 +0000

    mdoc(7): improve output of .At 32v
    The official designation by AT&T was "UNIX/32V", so use that in the output.
    That also makes sense because "system/architecture" is a widespread
    convention to refer to the port of an operating system to a specific
    architecture, in this case 32V (32bit DEC VAX).
    
    The former wording "Version 32V AT&T UNIX" was misleading
    because 32V is not a version number.
    
    Even though UNIX/32V was not officially designated as Version 7 by AT&T,
    prepend "Version 7" because it was in fact a straightforward port of
    Version 7 AT&T UNIX.  That makes it easier to understand for 21st
    century readers of manual pages.
    
    Suggested by nabijaczleweli at nabijaczleweli dot xyz.
    Same change as in GNU troff commit 21d30728.
    OK  G dot Branden dot Robinson at gmail dot com  (gbranden@ in groff)

--- usr.bin/mandoc/att.c
+++ usr.bin/mandoc/att.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: att.c,v 1.14 2018/12/13 11:55:14 schwarze Exp $ */
+/*	$OpenBSD: att.c,v 1.15 2021/09/04 20:24:40 schwarze Exp $ */
 /*
  * Copyright (c) 2009 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -35,7 +35,7 @@ mdoc_a2att(const char *p)
 	LINE("v5",	"Version\\~5 AT&T UNIX");
 	LINE("v6",	"Version\\~6 AT&T UNIX");
 	LINE("v7",	"Version\\~7 AT&T UNIX");
-	LINE("32v",	"Version\\~32V AT&T UNIX");
+	LINE("32v",	"Version\\~7 AT&T UNIX/32V");
 	LINE("III",	"AT&T System\\~III UNIX");
 	LINE("V",	"AT&T System\\~V UNIX");
 	LINE("V.1",	"AT&T System\\~V Release\\~1 UNIX");
