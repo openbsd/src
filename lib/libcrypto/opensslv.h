77dc73d187110aaeda020dcbd3017de7cde3dc0f
tb <tb@openbsd.org>
2021-09-15 17:14:26 +0000

    bump to LibreSSL 3.4.1

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.65 2021/05/01 14:15:57 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.66 2021/09/15 17:14:26 tb Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3040000fL
+#define LIBRESSL_VERSION_NUMBER 0x3040100fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.0"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.1"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
