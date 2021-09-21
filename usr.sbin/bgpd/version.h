205f87b24612351c6e00d49d2a34c856e23a5edb
claudio <claudio@openbsd.org>
2021-09-21 11:59:47 +0000

    Move us to OpenBGPD 7.2

--- usr.sbin/bgpd/version.h
+++ usr.sbin/bgpd/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.2 2021/06/20 08:05:47 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.3 2021/09/21 11:59:47 claudio Exp $ */
 
-#define BGPD_VERSION	"7.1"
+#define BGPD_VERSION	"7.2"
