8650c5a0c2a65ce087bbdc381eb115b1cbf8941f
benno <benno@openbsd.org>
2021-09-21 12:41:05 +0000

    bump rpki-client version

--- usr.sbin/rpki-client/version.h
+++ usr.sbin/rpki-client/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.4 2021/07/28 15:45:34 benno Exp $ */
+/* $OpenBSD: version.h,v 1.5 2021/09/21 12:41:05 benno Exp $ */
 
-#define RPKI_VERSION	"7.2"
+#define RPKI_VERSION	"7.3"
