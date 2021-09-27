ebdefde75efc89da8dfcadaa0b292d42c38f8eaa
jsg <jsg@openbsd.org>
2021-09-27 03:34:34 +0000

    drm: serialize drm_file.master with a new spinlock
    From Desmond Cheong Zhi Xi
    06a553a99bacb00d3bc25f79e75c8e0fbf7a5025 in linux 5.10.y/5.10.67
    0b0860a3cf5eccf183760b1177a1dcdb821b0b66 in mainline linux

--- sys/dev/pci/drm/drm_file.c
+++ sys/dev/pci/drm/drm_file.c
@@ -179,6 +179,7 @@ struct drm_file *drm_file_alloc(struct drm_minor *minor)
 	init_waitqueue_head(&file->event_wait);
 	file->event_space = 4096; /* set aside 4k for event buffer */
 
+	mtx_init(&file->master_lookup_lock, IPL_NONE);
 	rw_init(&file->event_read_lock, "evread");
 
 	if (drm_core_check_feature(dev, DRIVER_GEM))
