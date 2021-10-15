93bee3e0b47605e159550139a1a0c7d3ccfcb376
job <job@openbsd.org>
2021-10-15 08:48:18 +0000

    Update json key names
    OK claudio@

--- usr.sbin/rpki-client/output-json.c
+++ usr.sbin/rpki-client/output-json.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: output-json.c,v 1.19 2021/10/12 15:16:45 job Exp $ */
+/*	$OpenBSD: output-json.c,v 1.20 2021/10/15 08:48:18 job Exp $ */
 /*
  * Copyright (c) 2019 Claudio Jeker <claudio@openbsd.org>
  *
@@ -46,8 +46,8 @@ outputheader_json(FILE *out, struct stats *st)
 	    "\t\t\"roas\": %zu,\n"
 	    "\t\t\"failedroas\": %zu,\n"
 	    "\t\t\"invalidroas\": %zu,\n"
-	    "\t\t\"bgpsec_router_keys\": %zu,\n"
-	    "\t\t\"invalidbgpsec_router_keys\": %zu,\n"
+	    "\t\t\"bgpsec_pubkeys\": %zu,\n"
+	    "\t\t\"invalidbgpsec_pubkeys\": %zu,\n"
 	    "\t\t\"certificates\": %zu,\n"
 	    "\t\t\"failcertificates\": %zu,\n"
 	    "\t\t\"invalidcertificates\": %zu,\n"
