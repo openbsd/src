1064d52924a06a491a959d21f11701699695d313
espie <espie@openbsd.org>
2021-11-16 15:38:52 +0000

    tweak the packages-specs regexp a bit: stem-* shouldn't have any cruft added.

--- usr.sbin/pkg_add/OpenBSD/PkgSpec.pm
+++ usr.sbin/pkg_add/OpenBSD/PkgSpec.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PkgSpec.pm,v 1.46 2019/10/13 16:22:30 espie Exp $
+# $OpenBSD: PkgSpec.pm,v 1.47 2021/11/16 15:38:52 espie Exp $
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -196,7 +196,7 @@ sub parse
 	my $r = {};
 
 	# let's try really hard to find the stem and the flavors
-	unless ($p =~ m/^(.*?)\-((?:(?:\>|\>\=|\<\=|\<|\=)?\d|\*)[^-]*)(.*)$/) {
+	unless ($p =~ m/^(.*?)\-((?:\>|\>\=|\<\=|\<|\=)?\d[^-]*|\*)(.*)$/) {
 		return undef;
 	}
 	($r->{stemspec}, $r->{vspec}, $r->{flavorspec}) = ($1, $2, $3);
