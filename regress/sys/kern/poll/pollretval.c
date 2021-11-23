42ebeb7d780d20b60531b537dc2393a21e3add6d
deraadt <deraadt@openbsd.org>
2021-11-23 01:14:26 +0000

    A weird little test which can expose buggy return value conditions in poll()

---
+++ regress/sys/kern/poll/pollretval.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <poll.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <err.h>
+
+int
+main()
+{
+        struct pollfd pfd[100];
+        int i, r, r2 = 0;
+
+        for (i = 0; i < 100; i++) {
+            pfd[i].fd = 0;
+            pfd[i].events = arc4random() % 0x177;
+        }
+
+	r = poll(pfd, 100, INFTIM);
+
+	if (r == -1)
+		errx(1, "poll failed unexpectedly");
+
+        for (i = 0; i < 100; i++)
+		if (pfd[i].revents)
+			r2++;
+	if (r != r2)
+		errx(1, "poll return value %d miscounts .revents %d", r, r2);
+}
