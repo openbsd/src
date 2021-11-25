4aefc49552e88a10359379b4b2560711c410d81b
claudio <claudio@openbsd.org>
2021-11-25 15:03:04 +0000

    Initialize the repo_timeout to timeout / 4 at start of main(). OK job@

--- usr.sbin/rpki-client/main.c
+++ usr.sbin/rpki-client/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.166 2021/11/25 14:03:40 job Exp $ */
+/*	$OpenBSD: main.c,v 1.167 2021/11/25 15:03:04 claudio Exp $ */
 /*
  * Copyright (c) 2021 Claudio Jeker <claudio@openbsd.org>
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
@@ -68,7 +68,7 @@ const char	*bird_tablename = "ROAS";
 int	verbose;
 int	noop;
 int	rrdpon = 1;
-int	repo_timeout = 15*60;
+int	repo_timeout;
 
 struct stats	 stats;
 
@@ -709,6 +709,7 @@ main(int argc, char *argv[])
 	}
 	cachedir = RPKI_PATH_BASE_DIR;
 	outputdir = RPKI_PATH_OUT_DIR;
+	repo_timeout = timeout / 4;
 
 	if (pledge("stdio rpath wpath cpath inet fattr dns sendfd recvfd "
 	    "proc exec unveil", NULL) == -1)
