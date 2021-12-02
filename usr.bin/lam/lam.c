b79c304ca963f01b3438f931c5630ceaad10c21a
jmc <jmc@openbsd.org>
2021-12-02 15:15:29 +0000

    list uppercase options along with lower in SYNOPSIS/options list/usage; suggested by/ok deraadt

--- usr.bin/lam/lam.c
+++ usr.bin/lam/lam.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lam.c,v 1.22 2018/07/29 11:27:14 schwarze Exp $	*/
+/*	$OpenBSD: lam.c,v 1.23 2021/12/02 15:15:29 jmc Exp $	*/
 /*	$NetBSD: lam.c,v 1.2 1994/11/14 20:27:42 jtc Exp $	*/
 
 /*-
@@ -263,7 +263,7 @@ usage(void)
 	extern char *__progname;
 
 	fprintf(stderr,
-	    "usage: %s [-f min.max] [-p min.max] [-s sepstring] [-t c] file ...\n",
+	    "usage: %s [-F|f min.max] [-P|p min.max] [-S|s sepstring] [-T|t c] file ...\n",
 	    __progname);
 	exit(1);
 }
