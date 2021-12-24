c89a19ac1276574ce7f5a2e0934f6cc808fda935
tb <tb@openbsd.org>
2021-12-24 02:22:16 +0000

    Remove assert from extract_min_max() (again)
    All callers ensure that aor != NULL, so this isn't necessary.
    
    ok jsing

--- lib/libcrypto/x509/x509_asid.c
+++ lib/libcrypto/x509/x509_asid.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: x509_asid.c,v 1.24 2021/12/24 02:17:27 tb Exp $ */
+/*	$OpenBSD: x509_asid.c,v 1.25 2021/12/24 02:22:16 tb Exp $ */
 /*
  * Contributed to the OpenSSL Project by the American Registry for
  * Internet Numbers ("ARIN").
@@ -474,8 +474,6 @@ X509v3_asid_add_id_or_range(ASIdentifiers *asid, int which, ASN1_INTEGER *min,
 static int
 extract_min_max(ASIdOrRange *aor, ASN1_INTEGER **min, ASN1_INTEGER **max)
 {
-	OPENSSL_assert(aor != NULL);
-
 	switch (aor->type) {
 	case ASIdOrRange_id:
 		*min = aor->u.id;
