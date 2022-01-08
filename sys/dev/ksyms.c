64eea40c0d2750ad2f012d420b45f6e99d63d72a
guenther <guenther@openbsd.org>
2022-01-08 22:54:49 +0000

    __LDPGSZ hasn't been used here since rev 1.23 (2013). Delete comment referring to it
    ok jsg@

--- sys/dev/ksyms.c
+++ sys/dev/ksyms.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ksyms.c,v 1.33 2020/07/18 20:21:01 kettenis Exp $	*/
+/*	$OpenBSD: ksyms.c,v 1.34 2022/01/08 22:54:49 guenther Exp $	*/
 /*
  * Copyright (c) 1998 Todd C. Miller <millert@openbsd.org>
  * Copyright (c) 2001 Artur Grabowski <art@openbsd.org>
@@ -48,10 +48,6 @@ static size_t ksym_head_size;
 static size_t ksym_syms_size;
 
 void	ksymsattach(int);
-
-/*
- * We assume __LDPGSZ is a multiple of PAGE_SIZE (it is)
- */
 
 void
 ksymsattach(int num)
