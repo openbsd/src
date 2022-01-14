b7df4fb64b6ddd23f40e7b8c8a3a1638320dbb75
tb <tb@openbsd.org>
2022-01-14 08:38:48 +0000

    Garbage collect last use of EVP_ecdsa()
    ok inoguchi jsing

--- lib/libssl/ssl_algs.c
+++ lib/libssl/ssl_algs.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_algs.c,v 1.29 2021/05/16 08:24:21 jsing Exp $ */
+/* $OpenBSD: ssl_algs.c,v 1.30 2022/01/14 08:38:48 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -113,7 +113,6 @@ SSL_library_init(void)
 	EVP_add_digest(EVP_sha256());
 	EVP_add_digest(EVP_sha384());
 	EVP_add_digest(EVP_sha512());
-	EVP_add_digest(EVP_ecdsa());
 #ifndef OPENSSL_NO_GOST
 	EVP_add_digest(EVP_gostr341194());
 	EVP_add_digest(EVP_gost2814789imit());
