735ddf26403efe5d8df41bd53f9c1edec0b627b3
gnezdo <gnezdo@openbsd.org>
2022-01-21 03:13:10 +0000

    Optimization for tiny x in cos and sin
    Subset of FreeBSD commit 4339c67c485f0b1f7699863fc29f6c06862d1dde:
    Moved the optimization for tiny x from __kernel_{cos,sin}[f](x) to
    {cos_sin}[f](x) so that x doesn't need to be reclassified in the
    "kernel" functions to determine if it is tiny (it still needs to be
    reclassified in the cosine case for other reasons that will go away).
    ...
    
    Parts of the original change were applied separately before.
    
    Suggested by kettenis@
    
    OK mbuhl@, tested by mbuhl@ and phessler@

--- lib/libm/src/s_cosf.c
+++ lib/libm/src/s_cosf.c
@@ -26,7 +26,12 @@ cosf(float x)
 
     /* |x| ~< pi/4 */
 	ix &= 0x7fffffff;
-	if(ix <= 0x3f490fd8) return __kernel_cosf(x,z);
+	if(ix <= 0x3f490fd8) {
+	    if(ix<0x39800000)			/* if x < 2**-12 */
+		if(((int)x)==0) return 1.0;	/* generate inexact */
+	    return __kernel_cosf(x,z);
+	}
+
 
     /* cos(Inf or NaN) is NaN */
 	else if (ix>=0x7f800000) return x-x;
