69ebe3e496e69cd8c77d0df8d473c1cdaf138c88
dtucker <dtucker@openbsd.org>
2022-02-04 07:53:44 +0000

    Add unit tests for hpdelim.

---
+++ regress/usr.bin/ssh/unittests/misc/test_hpdelim.c
@@ -0,0 +1,67 @@
+/* 	$OpenBSD: test_hpdelim.c,v 1.1 2022/02/04 07:53:44 dtucker Exp $ */
+/*
+ * Regress test for misc hpdelim() and co
+ *
+ * Placed in the public domain.
+ */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "test_helper.h"
+
+#include "log.h"
+#include "misc.h"
+#include "xmalloc.h"
+
+void test_hpdelim(void);
+
+void
+test_hpdelim(void)
+{
+	char *orig, *str, *cp, *port;
+
+#define START_STRING(x)	orig = str = xstrdup(x)
+#define DONE_STRING()	free(orig)
+
+	TEST_START("hpdelim host only");
+	START_STRING("host");
+	cp = hpdelim(&str);
+	ASSERT_STRING_EQ(cp, "host");
+	ASSERT_PTR_EQ(str, NULL);
+	DONE_STRING();
+	TEST_DONE();
+
+	TEST_START("hpdelim host:port");
+	START_STRING("host:1234");
+	cp = hpdelim(&str);
+	ASSERT_STRING_EQ(cp, "host");
+	ASSERT_PTR_NE(str, NULL);
+	port = hpdelim(&str);
+	ASSERT_STRING_EQ(port, "1234");
+	ASSERT_PTR_EQ(str, NULL);
+	DONE_STRING();
+	TEST_DONE();
+
+	TEST_START("hpdelim [host]:port");
+	START_STRING("[::1]:1234");
+	cp = hpdelim(&str);
+	ASSERT_STRING_EQ(cp, "[::1]");
+	ASSERT_PTR_NE(str, NULL);
+	port = hpdelim(&str);
+	ASSERT_STRING_EQ(port, "1234");
+	ASSERT_PTR_EQ(str, NULL);
+	DONE_STRING();
+	TEST_DONE();
+
+	TEST_START("hpdelim missing ] error");
+	START_STRING("[::1:1234");
+	cp = hpdelim(&str);
+	ASSERT_PTR_EQ(cp, NULL);
+	DONE_STRING();
+	TEST_DONE();
+
+}
--- regress/usr.bin/ssh/unittests/misc/tests.c
+++ regress/usr.bin/ssh/unittests/misc/tests.c
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: tests.c,v 1.8 2021/12/14 21:25:27 deraadt Exp $ */
+/* 	$OpenBSD: tests.c,v 1.9 2022/02/04 07:53:44 dtucker Exp $ */
 /*
  * Regress test for misc helper functions.
  *
@@ -21,6 +21,7 @@ void test_convtime(void);
 void test_expand(void);
 void test_argv(void);
 void test_strdelim(void);
+void test_hpdelim(void);
 
 void
 tests(void)
@@ -30,4 +31,5 @@ tests(void)
 	test_expand();
 	test_argv();
 	test_strdelim();
+	test_hpdelim();
 }
