917b5357f260d1b395eeb9e99125de4ea75d6d73
daniel <daniel@openbsd.org>
2022-02-05 20:31:26 +0000

    grow the dmesg buffer on i386 from 4 to 8 pages
    I have a machine where the dmesg is too big for 4 pages.
    
    ok deraadt@

--- sys/arch/i386/include/param.h
+++ sys/arch/i386/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.50 2018/08/25 17:24:22 kettenis Exp $	*/
+/*	$OpenBSD: param.h,v 1.51 2022/02/05 20:31:26 daniel Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -70,7 +70,7 @@
 #define	NMBCLUSTERS	(32 * 1024)		/* max cluster allocation */
 
 #ifndef	MSGBUFSIZE
-#define	MSGBUFSIZE	(4 * PAGE_SIZE)		/* default message buffer size */
+#define	MSGBUFSIZE	(8 * PAGE_SIZE)		/* default message buffer size */
 #endif
 
 /*
