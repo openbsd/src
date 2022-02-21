f7b0f15d77d860c9fe33fa0f7e2e22bf08ff5f55
jsg <jsg@openbsd.org>
2022-02-21 05:54:35 +0000

    tempterature -> temperature

--- sys/arch/armv7/imx/imxtemp.c
+++ sys/arch/armv7/imx/imxtemp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: imxtemp.c,v 1.7 2021/10/24 17:52:27 mpi Exp $	*/
+/*	$OpenBSD: imxtemp.c,v 1.8 2022/02/21 05:54:35 jsg Exp $	*/
 /*
  * Copyright (c) 2014 Patrick Wildt <patrick@blueri.se>
  * Copyright (c) 2016 Mark Kettenis <kettenis@openbsd.org>
@@ -162,7 +162,7 @@ imxtemp_calc_temp(struct imxtemp_softc *sc, uint32_t temp_cnt)
 	int32_t value;
 
 	/*
-	 * Calculate the calibrated tempterature based on the equation
+	 * Calculate the calibrated temperature based on the equation
 	 * provided in the i.MX6 reference manual:
 	 *
 	 * Tmeas = HOT_TEMP - (Nmeas - HOT_COUNT) * ((HOT_TEMP - 25.0) /
