a97d9365eb2842fc70e9e5a027c122bd9547b780
jsg <jsg@openbsd.org>
2022-02-21 11:20:34 +0000

    ignorint -> ignoring

--- sys/dev/pci/vmwpvs.c
+++ sys/dev/pci/vmwpvs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmwpvs.c,v 1.24 2020/09/22 19:32:53 krw Exp $ */
+/*	$OpenBSD: vmwpvs.c,v 1.25 2022/02/21 11:20:34 jsg Exp $ */
 
 /*
  * Copyright (c) 2013 David Gwynne <dlg@openbsd.org>
@@ -809,7 +809,7 @@ vmwpvs_msg_task(void *xsc)
 		case VMWPVS_MSG_T_REMOVED:
 			dvmsg = (struct vmwpvs_ring_msg_dev *)msg;
 			if (letoh32(dvmsg->bus) != 0) {
-				printf("%s: ignorint request to remove device"
+				printf("%s: ignoring request to remove device"
 				    " on bus %d\n", DEVNAME(sc),
 				    letoh32(msg->type));
 				break;
