a7992526e7aa0116ca9794a96c12b39fa87099fd
deraadt <deraadt@openbsd.org>
2022-02-23 19:01:00 +0000

    and we go back to testing sftp-scp after the 8.9 release...

--- usr.bin/ssh/scp.c
+++ usr.bin/ssh/scp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: scp.c,v 1.245 2022/02/10 04:12:38 djm Exp $ */
+/* $OpenBSD: scp.c,v 1.246 2022/02/23 19:01:00 deraadt Exp $ */
 /*
  * scp - secure remote copy.  This is basically patched BSD rcp which
  * uses ssh to do the data transfer (instead of using rcmd).
@@ -423,7 +423,7 @@ main(int argc, char **argv)
 	const char *errstr;
 	extern char *optarg;
 	extern int optind;
-	enum scp_mode_e mode = MODE_SCP;
+	enum scp_mode_e mode = MODE_SFTP;
 	char *sftp_direct = NULL;
 
 	/* Ensure that fds 0, 1 and 2 are open or directed to /dev/null */
