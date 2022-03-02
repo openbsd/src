1914e2079d8471fbf2cca3b02895415f67b45a63
tb <tb@openbsd.org>
2022-03-02 17:45:39 +0000

    whitespace

--- lib/libcrypto/asn1/a_object.c
+++ lib/libcrypto/asn1/a_object.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: a_object.c,v 1.38 2022/03/02 11:28:00 jsing Exp $ */
+/* $OpenBSD: a_object.c,v 1.39 2022/03/02 17:45:39 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -446,7 +446,7 @@ i2a_ASN1_OBJECT(BIO *bp, const ASN1_OBJECT *aobj)
 	}
 	if (!CBB_finish(&cbb, &data, &data_len))
 		goto err;
-		
+
 	ret = BIO_write(bp, data, data_len);
 
  err:
