2d8ffc66c3bedcfa856d3893486820bae563f4eb
deraadt <deraadt@openbsd.org>
2022-04-04 21:16:47 +0000

    move release date a little

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.137 2022/02/22 17:04:29 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.138 2022/04/04 21:16:47 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -41,7 +41,7 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define OpenBSD	202205		/* OpenBSD version (year & month). */
+#define OpenBSD	202204		/* OpenBSD version (year & month). */
 #define OpenBSD7_1 1		/* OpenBSD 7.1 */
 
 #include <sys/_null.h>
