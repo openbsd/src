32c3fcf8e3bcb01cae4948256c824755fcdb007f
claudio <claudio@openbsd.org>
2022-04-08 15:30:27 +0000

    Bump because of the last minute mktime -> timegm fix

--- usr.sbin/rpki-client/version.h
+++ usr.sbin/rpki-client/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.9 2022/04/04 08:37:35 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.10 2022/04/08 15:30:27 claudio Exp $ */
 
-#define RPKI_VERSION	"7.7"
+#define RPKI_VERSION	"7.8"
