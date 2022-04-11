f9c49ec7e13c27299b98dadda34543cab727793e
jsg <jsg@openbsd.org>
2022-04-11 03:24:33 +0000

    drm/amdgpu: move PX checking into amdgpu_device_ip_early_init
    From Alex Deucher
    0884abb259f180bc737cdb2c681d907b86f266e8 in linux 5.15.y/5.15.33
    901e2be20dc55079997ea1885ea77fc72e6826e7 in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/amdgpu_kms.c
+++ sys/dev/pci/drm/amd/amdgpu/amdgpu_kms.c
@@ -208,20 +208,9 @@ static void amdgpu_get_audio_func(struct amdgpu_device *adev)
 int amdgpu_driver_load_kms(struct amdgpu_device *adev, unsigned long flags)
 {
 	struct drm_device *dev;
-	struct pci_dev *parent;
 	int r, acpi_status;
 
 	dev = adev_to_drm(adev);
-
-	if (amdgpu_has_atpx() &&
-	    (amdgpu_is_atpx_hybrid() ||
-	     amdgpu_has_atpx_dgpu_power_cntl()) &&
-	    ((flags & AMD_IS_APU) == 0) &&
-	    !pci_is_thunderbolt_attached(to_pci_dev(dev->dev)))
-		flags |= AMD_IS_PX;
-
-	parent = pci_upstream_bridge(adev->pdev);
-	adev->has_pr3 = parent ? pci_pr3_present(parent) : false;
 
 	/* amdgpu_device_init should report only fatal error
 	 * like memory allocation failure or iomapping failure,
@@ -2134,13 +2123,6 @@ amdgpu_attachhook(struct device *self)
 	struct amdgpu_device	*adev = (struct amdgpu_device *)self;
 	struct drm_device	*dev = &adev->ddev;
 	int r, acpi_status;
-
-	if (amdgpu_has_atpx() &&
-	    (amdgpu_is_atpx_hybrid() ||
-	     amdgpu_has_atpx_dgpu_power_cntl()) &&
-	    ((adev->flags & AMD_IS_APU) == 0) &&
-	    !pci_is_thunderbolt_attached(dev->pdev))
-		adev->flags |= AMD_IS_PX;
 
 	/* amdgpu_device_init should report only fatal error
 	 * like memory allocation failure or iomapping failure,
