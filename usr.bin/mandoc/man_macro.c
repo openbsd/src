6369db7023f81f1bf5ae71175457e3e9c7b146f3
schwarze <schwarze@openbsd.org>
2022-04-13 14:37:34 +0000

    Do not die on an assertion if an input file contains no section whatsoever and ends with a broken next-line scope.  Obviously, this cannot happen in a real manual page, but mandoc(1) should not die even when fed absurd input.
    This bug was independently reported by both jsg@ and tb@ who both
    found it with afl(1).

--- usr.bin/mandoc/man_macro.c
+++ usr.bin/mandoc/man_macro.c
@@ -1,7 +1,7 @@
-/* $OpenBSD: man_macro.c,v 1.107 2020/09/09 16:57:05 schwarze Exp $ */
+/* $OpenBSD: man_macro.c,v 1.108 2022/04/13 14:37:34 schwarze Exp $ */
 /*
+ * Copyright (c) 2012-2015,2017-2020,2022 Ingo Schwarze <schwarze@openbsd.org>
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
- * Copyright (c) 2012-2015, 2017-2020 Ingo Schwarze <schwarze@openbsd.org>
  * Copyright (c) 2013 Franco Fichtner <franco@lastsummer.de>
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -106,7 +106,8 @@ man_unscope(struct roff_man *man, const struct roff_node *to)
 				    n->line, n->pos,
 				    "EOF breaks %s", roff_name[n->tok]);
 				if (man->flags & MAN_ELINE) {
-					if ((man_macro(n->parent->tok)->flags &
+					if (n->parent->type == ROFFT_ROOT ||
+					    (man_macro(n->parent->tok)->flags &
 					    MAN_ESCOPED) == 0)
 						man->flags &= ~MAN_ELINE;
 				} else {
