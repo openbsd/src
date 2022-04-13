7846bfe8500d7568349d53bb176df7d722aef2a8
millert <millert@openbsd.org>
2022-04-13 22:17:33 +0000

    Fix typo in last commit.

--- lib/libc/net/inet_net_pton.c
+++ lib/libc/net/inet_net_pton.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: inet_net_pton.c,v 1.12 2022/04/13 16:20:11 millert Exp $	*/
+/*	$OpenBSD: inet_net_pton.c,v 1.13 2022/04/13 22:17:33 millert Exp $	*/
 
 /*
  * Copyright (c) 2012 by Gilles Chehade <gilles@openbsd.org>
@@ -208,7 +208,7 @@ inet_net_pton_ipv6(const char *src, u_char *dst, size_t size)
 	struct in6_addr	 in6;
 	int		 ret;
 	int		 bits;
-	size_t		 bytes
+	size_t		 bytes;
 	char		 buf[INET6_ADDRSTRLEN + sizeof("/128")];
 	char		*sep;
 	const char	*errstr;
