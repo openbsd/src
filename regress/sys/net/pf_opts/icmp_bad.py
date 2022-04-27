b4d308b44d7e798031d566d6097e9322f7caef7d
bluhm <bluhm@openbsd.org>
2022-04-27 18:28:40 +0000

    Send packets with IPv4 option or IPv6 extension header over loopback interface and check which of the packets are dropped by pf.  The bad packets appear in pflog0 tcpdump.  This regress is testing rules with and without allow-opts.

---
+++ regress/sys/net/pf_opts/icmp_bad.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+print("send icmp unknown option")
+
+import os
+import sys
+from addr import *
+from scapy.all import *
+
+if len(sys.argv) != 2:
+	print("usage: icmp_bad.py Nn")
+	exit(2)
+
+N=sys.argv[1]
+IF=eval("IF_"+N);
+ADDR=eval("ADDR_"+N);
+
+pid=os.getpid()
+eid=pid & 0xffff
+payload=b"ABCDEFGHIJKLMNOP"
+packet=IP(src=ADDR, dst=ADDR, options=b"\003\004\000\000")/ \
+    ICMP(type=6, id=eid)/payload
+
+send(packet, iface=IF)
---
+++ regress/sys/net/pf_opts/icmp_ra.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+print("send icmp option router alert")
+
+import os
+import sys
+from addr import *
+from scapy.all import *
+
+if len(sys.argv) != 2:
+	print("usage: icmp_ra.py Nn")
+	exit(2)
+
+N=sys.argv[1]
+IF=eval("IF_"+N);
+ADDR=eval("ADDR_"+N);
+
+pid=os.getpid()
+eid=pid & 0xffff
+payload=b"ABCDEFGHIJKLMNOP"
+packet=IP(src=ADDR, dst=ADDR, options=b"\001\224\004\000\000\001\000\001")/ \
+    ICMP(type=6, id=eid)/payload
+
+send(packet, iface=IF)
