1dae0cf48afba75e7d0b4dd5509592bfd8f71506
jsg <jsg@openbsd.org>
2022-05-04 05:20:33 +0000

    drm/i915/rpl-s: Add PCI IDS for Raptor Lake S
    From Anusha Srivatsa
    52407c220c44c8dcc6aa8aa35ffc8a2db3c849a9 in mainline linux

--- sys/dev/pci/drm/i915/i915_pci.c
+++ sys/dev/pci/drm/i915/i915_pci.c
@@ -1115,6 +1115,7 @@ const struct pci_device_id pciidlist[] = {
 	INTEL_RKL_IDS(&rkl_info),
 	INTEL_ADLS_IDS(&adl_s_info),
 	INTEL_ADLP_IDS(&adl_p_info),
+	INTEL_RPLS_IDS(&adl_s_info),
 	{0, 0, 0}
 };
 MODULE_DEVICE_TABLE(pci, pciidlist);
