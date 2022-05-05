c7411d0fce764c003a56eba97332d9f277a9c4fb
tb <tb@openbsd.org>
2022-05-05 08:51:21 +0000

    Add hkdf_pkey_meth to the standard_methods[]
    ok beck jsing

--- lib/libcrypto/evp/pmeth_lib.c
+++ lib/libcrypto/evp/pmeth_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pmeth_lib.c,v 1.21 2022/05/05 08:42:27 tb Exp $ */
+/* $OpenBSD: pmeth_lib.c,v 1.22 2022/05/05 08:51:21 tb Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2006.
  */
@@ -84,6 +84,7 @@ extern const EVP_PKEY_METHOD rsa_pkey_meth, rsa_pss_pkey_meth;
 extern const EVP_PKEY_METHOD dh_pkey_meth, dsa_pkey_meth;
 extern const EVP_PKEY_METHOD ec_pkey_meth, hmac_pkey_meth, cmac_pkey_meth;
 extern const EVP_PKEY_METHOD gostimit_pkey_meth, gostr01_pkey_meth;
+extern const EVP_PKEY_METHOD hkdf_pkey_meth;
 
 static const EVP_PKEY_METHOD *standard_methods[] = {
 #ifndef OPENSSL_NO_RSA
@@ -107,6 +108,7 @@ static const EVP_PKEY_METHOD *standard_methods[] = {
 #ifndef OPENSSL_NO_RSA
 	&rsa_pss_pkey_meth,
 #endif
+	&hkdf_pkey_meth,
 };
 
 static int pmeth_cmp_BSEARCH_CMP_FN(const void *, const void *);
