49cc5d012c6912b1790309074eab984a6b5f9ca2
tb <tb@openbsd.org>
2022-05-05 08:24:14 +0000

    Ditch #defines for tls1_prf and scrypt. Drop unused errors and massage some const.
    ok beck jsing

--- lib/libcrypto/kdf/kdf_err.c
+++ lib/libcrypto/kdf/kdf_err.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kdf_err.c,v 1.7 2022/05/05 07:48:07 tb Exp $ */
+/*	$OpenBSD: kdf_err.c,v 1.8 2022/05/05 08:24:14 tb Exp $ */
 /* ====================================================================
  * Copyright (c) 1999-2018 The OpenSSL Project.  All rights reserved.
  *
@@ -62,44 +62,20 @@
 
 #ifndef OPENSSL_NO_ERR
 
-static const ERR_STRING_DATA KDF_str_functs[] = {
+static ERR_STRING_DATA KDF_str_functs[] = {
 	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_HKDF_CTRL_STR, 0), "pkey_hkdf_ctrl_str"},
 	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_HKDF_DERIVE, 0), "pkey_hkdf_derive"},
 	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_HKDF_INIT, 0), "pkey_hkdf_init"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_SCRYPT_CTRL_STR, 0),
-	 "pkey_scrypt_ctrl_str"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_SCRYPT_CTRL_UINT64, 0),
-	 "pkey_scrypt_ctrl_uint64"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_SCRYPT_DERIVE, 0), "pkey_scrypt_derive"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_SCRYPT_INIT, 0), "pkey_scrypt_init"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_SCRYPT_SET_MEMBUF, 0),
-	 "pkey_scrypt_set_membuf"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_TLS1_PRF_CTRL_STR, 0),
-	 "pkey_tls1_prf_ctrl_str"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_TLS1_PRF_DERIVE, 0),
-	 "pkey_tls1_prf_derive"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_PKEY_TLS1_PRF_INIT, 0), "pkey_tls1_prf_init"},
-	{ERR_PACK(ERR_LIB_KDF, KDF_F_TLS1_PRF_ALG, 0), "tls1_prf_alg"},
-	{0, NULL}
+	{0, NULL},
 };
 
-static const ERR_STRING_DATA KDF_str_reasons[] = {
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_INVALID_DIGEST), "invalid digest"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_ITERATION_COUNT),
-	 "missing iteration count"},
+static ERR_STRING_DATA KDF_str_reasons[] = {
 	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_KEY), "missing key"},
 	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_MESSAGE_DIGEST),
 	 "missing message digest"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_PARAMETER), "missing parameter"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_PASS), "missing pass"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_SALT), "missing salt"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_SECRET), "missing secret"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_MISSING_SEED), "missing seed"},
 	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_UNKNOWN_PARAMETER_TYPE),
 	 "unknown parameter type"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_VALUE_ERROR), "value error"},
-	{ERR_PACK(ERR_LIB_KDF, 0, KDF_R_VALUE_MISSING), "value missing"},
-	{0, NULL}
+	{0, NULL},
 };
 
 #endif
@@ -109,8 +85,8 @@ ERR_load_KDF_strings(void)
 {
 #ifndef OPENSSL_NO_ERR
 	if (ERR_func_error_string(KDF_str_functs[0].error) == NULL) {
-		ERR_load_strings_const(KDF_str_functs);
-		ERR_load_strings_const(KDF_str_reasons);
+		ERR_load_strings(0, KDF_str_functs);
+		ERR_load_strings(0, KDF_str_reasons);
 	}
 #endif
 	return 1;
