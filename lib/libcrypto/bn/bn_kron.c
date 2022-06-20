2f1d478512eaf9d1ce1d86f5a427ab97a0a2566f
tb <tb@openbsd.org>
2022-06-20 19:42:58 +0000

    Tweak a comment

--- lib/libcrypto/bn/bn_kron.c
+++ lib/libcrypto/bn/bn_kron.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bn_kron.c,v 1.8 2022/06/20 19:38:25 tb Exp $ */
+/* $OpenBSD: bn_kron.c,v 1.9 2022/06/20 19:42:58 tb Exp $ */
 /* ====================================================================
  * Copyright (c) 1998-2000 The OpenSSL Project.  All rights reserved.
  *
@@ -55,7 +55,7 @@
 
 #include "bn_lcl.h"
 
-/* least significant word */
+/* The least significant word of a BIGNUM. */
 #define BN_lsw(n) (((n)->top == 0) ? (BN_ULONG) 0 : (n)->d[0])
 
 /*
