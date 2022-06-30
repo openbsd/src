ce65d8e20f156678b17e1de9cb2e356af8574c4d
mlarkin <mlarkin@openbsd.org>
2022-06-30 13:51:24 +0000

    amd64/pmap: Change the wording of a panic string
    Change a panic string to make the text locatable in the code since there
    is another panic with the same text. Helps debugging when the line number
    information isn't available.
    
    no functional change.
    
    ok dv

--- sys/arch/amd64/amd64/pmap.c
+++ sys/arch/amd64/amd64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.152 2022/06/30 09:38:22 mlarkin Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.153 2022/06/30 13:51:24 mlarkin Exp $	*/
 /*	$NetBSD: pmap.c,v 1.3 2003/05/08 18:13:13 thorpej Exp $	*/
 
 /*
@@ -1787,8 +1787,8 @@ pmap_do_remove(struct pmap *pmap, vaddr_t sva, vaddr_t eva, int flags)
 				ptp = pmap_find_ptp(pmap, sva, ptppa, 1);
 #ifdef DIAGNOSTIC
 				if (ptp == NULL)
-					panic("%s: unmanaged PTP detected",
-					      __func__);
+					panic("%s: unmanaged PTP detected "
+					    "in shortcut path", __func__);
 #endif
 			}
 
