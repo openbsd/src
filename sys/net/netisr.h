4f290cc7e96086a9796d1b142eff92423ee42caf
mvs <mvs@openbsd.org>
2022-07-14 10:52:21 +0000

    Replace tabs by spaces after "#define". No functional changes, just prevent future diffs to be ugly.
    ok bluhm@

--- sys/net/netisr.h
+++ sys/net/netisr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netisr.h,v 1.59 2022/06/29 16:04:49 mvs Exp $	*/
+/*	$OpenBSD: netisr.h,v 1.60 2022/07/14 10:52:21 mvs Exp $	*/
 /*	$NetBSD: netisr.h,v 1.12 1995/08/12 23:59:24 mycroft Exp $	*/
 
 /*
@@ -41,14 +41,14 @@
  * interrupt used for scheduling the network code to calls
  * on the lowest level routine of each protocol.
  */
-#define	NETISR_IP	2		/* same as AF_INET */
-#define	NETISR_PFSYNC	5		/* for pfsync "immediate" tx */
-#define	NETISR_ARP	18		/* same as AF_LINK */
-#define	NETISR_IPV6	24		/* same as AF_INET6 */
-#define	NETISR_PIPEX	27		/* for pipex processing */
-#define	NETISR_PPP	28		/* for PPP processing */
-#define	NETISR_BRIDGE	29		/* for bridge processing */
-#define	NETISR_PPPOE	30		/* for pppoe processing */
+#define NETISR_IP	2		/* same as AF_INET */
+#define NETISR_PFSYNC	5		/* for pfsync "immediate" tx */
+#define NETISR_ARP	18		/* same as AF_LINK */
+#define NETISR_IPV6	24		/* same as AF_INET6 */
+#define NETISR_PIPEX	27		/* for pipex processing */
+#define NETISR_PPP	28		/* for PPP processing */
+#define NETISR_BRIDGE	29		/* for bridge processing */
+#define NETISR_PPPOE	30		/* for pppoe processing */
 
 #ifndef _LOCORE
 #ifdef _KERNEL
