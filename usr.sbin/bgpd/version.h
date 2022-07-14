1ed35d5c90155ce3b361403f0b14c3505abb0364
claudio <claudio@openbsd.org>
2022-07-14 09:16:09 +0000

    Time to bump version

--- usr.sbin/bgpd/version.h
+++ usr.sbin/bgpd/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.5 2022/06/14 07:33:58 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.6 2022/07/14 09:16:09 claudio Exp $ */
 
-#define BGPD_VERSION	"7.4"
+#define BGPD_VERSION	"7.5"
