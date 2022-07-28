b74974d393ef8225731b68c975a6f4b313aaf530
claudio <claudio@openbsd.org>
2022-07-28 10:41:18 +0000

    Remove F_DOWN, it is no longer used and reshuffle the other flags a bit to group them a bit better. OK tb@

--- usr.sbin/bgpd/bgpd.h
+++ usr.sbin/bgpd/bgpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpd.h,v 1.446 2022/07/27 17:23:17 claudio Exp $ */
+/*	$OpenBSD: bgpd.h,v 1.447 2022/07/28 10:41:18 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -76,14 +76,13 @@
 #define	F_BGPD			0x0001
 #define	F_BGPD_INSERTED		0x0002
 #define	F_CONNECTED		0x0004
-#define	F_NEXTHOP		0x0008
-#define	F_DOWN			0x0010
-#define	F_STATIC		0x0020
-#define	F_REJECT		0x0080
-#define	F_BLACKHOLE		0x0100
+#define	F_STATIC		0x0008
+#define	F_NEXTHOP		0x0010
+#define	F_REJECT		0x0020
+#define	F_BLACKHOLE		0x0040
+#define	F_MPLS			0x0080
 #define	F_LONGER		0x0200
 #define	F_SHORTER		0x0400
-#define	F_MPLS			0x0800
 #define	F_CTL_DETAIL		0x1000	/* only set on requests */
 #define	F_CTL_ADJ_IN		0x2000	/* only set on requests */
 #define	F_CTL_ADJ_OUT		0x4000	/* only set on requests */
