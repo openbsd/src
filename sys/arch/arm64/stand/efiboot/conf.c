09ce6c0661b90c6c2535c9b67b87f0366c17d369
kettenis <kettenis@openbsd.org>
2022-07-29 21:34:59 +0000

    Give to PE/COFF file proper .text and .data sections, and fill in the characteristics field in the COFF header.  Makes our bootloader work in the x13s.  With help from mlarkin@.
    ok mlarkin@

--- sys/arch/arm64/stand/efiboot/conf.c
+++ sys/arch/arm64/stand/efiboot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.37 2022/04/06 21:27:03 kettenis Exp $	*/
+/*	$OpenBSD: conf.c,v 1.38 2022/07/29 21:34:59 kettenis Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -46,7 +46,7 @@
 #include "efipxe.h"
 #include "softraid_arm64.h"
 
-const char version[] = "1.9";
+const char version[] = "1.10";
 int	debug = 0;
 
 struct fs_ops file_system[] = {
