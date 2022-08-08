6fd32186fd9130fe92cde28453f152d5f920beab
jsg <jsg@openbsd.org>
2022-08-08 15:28:59 +0000

    remove unused arm and armv7 headers ok miod@

--- sys/arch/armv7/include/fp.h
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: fp.h,v 1.1 2013/09/04 14:38:27 patrick Exp $	*/
-/*	$NetBSD: fp.h,v 1.3 2001/11/25 15:55:56 thorpej Exp $	*/
-
-#include <arm/fp.h>
--- sys/arch/armv7/include/rtc.h
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: rtc.h,v 1.1 2013/09/04 14:38:29 patrick Exp $	*/
-/*	$NetBSD: rtc.h,v 1.2 2001/11/25 15:55:58 thorpej Exp $	*/
-
-#include <arm/rtc.h>
