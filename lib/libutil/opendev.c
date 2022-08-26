531ea1fd1f29d4262dcc911182a2c7056587eb1f
kn <kn@openbsd.org>
2022-08-26 21:47:16 +0000

    Backout "Only return file descriptors to block or character devices"
    mount_mfs(8) no monger mounts "/build" with the following fstab(5) entry:
    	/dev/sd0b /build mfs rw,noperm,nodev,nosuid,-s=2064348,noauto
    
    as found out by bluhm the hard way.

--- lib/libutil/opendev.c
+++ lib/libutil/opendev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: opendev.c,v 1.16 2022/08/25 17:09:54 kn Exp $	*/
+/*	$OpenBSD: opendev.c,v 1.17 2022/08/26 21:47:16 kn Exp $	*/
 
 /*
  * Copyright (c) 2000, Todd C. Miller.  All rights reserved.
@@ -38,7 +38,6 @@
 #include <sys/limits.h>
 #include <sys/disk.h>
 #include <sys/dkio.h>
-#include <sys/stat.h>
 
 #include "util.h"
 
@@ -64,23 +63,8 @@ opendev(const char *path, int oflags, int dflags, char **realpath)
 		prefix = "r";			/* character device */
 
 	if ((slash = strchr(path, '/'))) {
-		struct stat sb;
-
 		strlcpy(namebuf, path, sizeof(namebuf));
 		fd = open(namebuf, oflags);
-
-		if (fd != -1) {
-			if (fstat(fd, &sb) == -1) {
-				close(fd);
-				fd = -1;
-			} else if ((dflags & OPENDEV_BLCK) ?
-			    !S_ISBLK(sb.st_mode) :
-			    !S_ISCHR(sb.st_mode)) {
-				close(fd);
-				fd = -1;
-				errno = ENOTBLK;
-			}
-		}
 	} else if (isduid(path, dflags)) {
 		strlcpy(namebuf, path, sizeof(namebuf));
 		if ((fd = open("/dev/diskmap", oflags)) != -1) {
