8ce2cf4efb02b30df7b7925008f7d2fa1b950ead
jsing <jsing@openbsd.org>
2022-09-03 18:59:09 +0000

    Remove duplicate prototype that just snuck in.

--- lib/libcrypto/asn1/asn1_locl.h
+++ lib/libcrypto/asn1/asn1_locl.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: asn1_locl.h,v 1.37 2022/09/03 18:52:18 jsing Exp $ */
+/* $OpenBSD: asn1_locl.h,v 1.38 2022/09/03 18:59:09 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2006.
  */
@@ -210,8 +210,6 @@ int asn1_tag2charwidth(int tag);
 
 int asn1_abs_set_unused_bits(ASN1_BIT_STRING *abs, uint8_t unused_bits);
 int c2i_ASN1_BIT_STRING_cbs(ASN1_BIT_STRING **out_abs, CBS *cbs);
-
-int c2i_ASN1_ENUMERATED_cbs(ASN1_ENUMERATED **out_aenum, CBS *cbs);
 
 int c2i_ASN1_ENUMERATED_cbs(ASN1_ENUMERATED **out_aenum, CBS *cbs);
 
