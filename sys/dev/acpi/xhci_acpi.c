cd1ddd29d31fa15b4cad89a0847dbcd5ddaad946
kettenis <kettenis@openbsd.org>
2022-09-09 08:30:32 +0000

    The DSDT on the snapdragon 7c based Acer Aspire 1 (A114-61) uses a bogus compatible ID.  Add its hardware ID to the list to work around this.
    ok patrick@, mlarkin@, mglocker@

--- sys/dev/acpi/xhci_acpi.c
+++ sys/dev/acpi/xhci_acpi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: xhci_acpi.c,v 1.9 2022/09/07 20:06:23 kettenis Exp $	*/
+/*	$OpenBSD: xhci_acpi.c,v 1.10 2022/09/09 08:30:32 kettenis Exp $	*/
 /*
  * Copyright (c) 2018 Mark Kettenis
  *
@@ -59,6 +59,7 @@ const char *xhci_hids[] = {
 	"QCOM0498",
 	"QCOM068B",		/* SC8280 URS */
 	"QCOM068C",
+	"QCOM0826",		/* SC7180 USB */
 	"QCOM24B6",		/* SDM850 URS */
 	"QCOM24B7",
 	NULL
