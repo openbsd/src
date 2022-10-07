c45c688b6d76ed90e26bc1de5d9b53536eda8a05
krw <krw@openbsd.org>
2022-10-07 19:34:40 +0000

    Nuke GPTDOSACTIVE which specified the wrong bit. It was superseded by GPTPARTATTR_BOOTABLE which specifies the correct one.
    Reminded by drahn@

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.h,v 1.82 2022/09/15 10:10:14 krw Exp $	*/
+/*	$OpenBSD: disklabel.h,v 1.83 2022/10/07 19:34:40 krw Exp $	*/
 /*	$NetBSD: disklabel.h,v 1.41 1996/05/10 23:07:37 mark Exp $	*/
 
 /*
@@ -348,7 +348,6 @@ struct partinfo {
 #define	GPTPARTATTR_MS_HIDDEN		(1ULL << 62)
 #define	GPTPARTATTR_MS_NOAUTOMOUNT	(1ULL << 63)
 
-#define	GPTDOSACTIVE		0x2
 #define	GPTMINHDRSIZE		92
 #define	GPTMINPARTSIZE		128
 #define	GPTPARTNAMESIZE		36
