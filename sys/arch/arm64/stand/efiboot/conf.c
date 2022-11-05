fb406bc0f85f203c40c8fe5b011983eb24d6da50
patrick <patrick@openbsd.org>
2022-11-05 19:01:51 +0000

    Bump version number so that it's easier to see if the running version has the bugfixes in.
    ok kettenis@

--- sys/arch/arm64/stand/efiboot/conf.c
+++ sys/arch/arm64/stand/efiboot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.40 2022/08/15 13:13:41 kn Exp $	*/
+/*	$OpenBSD: conf.c,v 1.41 2022/11/05 19:01:51 patrick Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -46,7 +46,7 @@
 #include "efipxe.h"
 #include "softraid_arm64.h"
 
-const char version[] = "1.12";
+const char version[] = "1.13";
 int	debug = 0;
 
 struct fs_ops file_system[] = {
