b75fe81950c5c4b26e4bb4efbcf7d8945687e2a6
deraadt <deraadt@openbsd.org>
2022-11-09 18:44:11 +0000

    dlopen() with RTLD_NODELETE should also set the object nodelete flag, so the mapping layer will know it can use mimmutable()

--- libexec/ld.so/dlfcn.c
+++ libexec/ld.so/dlfcn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dlfcn.c,v 1.112 2022/11/07 10:35:26 deraadt Exp $ */
+/*	$OpenBSD: dlfcn.c,v 1.113 2022/11/09 18:44:11 deraadt Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -97,8 +97,10 @@ dlopen(const char *libname, int flags)
 		goto loaded;
 	}
 
-	if (flags & RTLD_NODELETE)
+	if (flags & RTLD_NODELETE) {
 		object->obj_flags |= DF_1_NODELETE;
+		object->nodelete = 1;
+	}
 	
 	_dl_link_dlopen(object);
 
