ff827b62f389545cafa10fda63d504b800c320ec
tobhe <tobhe@openbsd.org>
2022-11-17 13:30:21 +0000

    Add apple-boot firmware for Apple arm64 machines in fw_update(8) patterns.
    The pattern matches the printed CPU_IMPL_APPLE name as in:
    cpu0 at mainbus0 mpidr 0: Apple Icestorm Pro r2p0
    cpu0 at mainbus0 mpidr 0: Apple Blizzard r1p0
    
    ok deraadt@ afresh@ kettenis@

--- usr.sbin/fw_update/patterns.c
+++ usr.sbin/fw_update/patterns.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patterns.c,v 1.4 2022/08/06 03:12:19 jsg Exp $	*/
+/*	$OpenBSD: patterns.c,v 1.5 2022/11/17 13:30:21 tobhe Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Christopher G. Demetriou.  All rights reserved.
@@ -90,6 +90,7 @@ main(void)
 	printf("%s\n", "acx");
 	printf("%s\n", "amdgpu");
 	print_devices("amdgpu", amdgpu_devices, nitems(amdgpu_devices));
+	printf("%s\n", "apple-boot ^cpu0*Apple");
 	printf("%s\n", "athn");
 	printf("%s\n", "bwfm");
 	printf("%s\n", "bwi");
