9320ea12de2d959fb77c8a390af558b3c5fffaec
sthen <sthen@openbsd.org>
2022-11-19 08:02:11 +0000

    Add O: to the getopt string for newfs in mount_mfs mode, allowing it to optionally use FFS2 (stu@ ran into a problem when running some tests on an MFS filesystem that involved future dates).
    Correct the manual which said that FFS2 is the default, which isn't the
    case for mount_mfs.
    
    lgtm miod@

--- sbin/newfs/newfs.c
+++ sbin/newfs/newfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: newfs.c,v 1.115 2021/06/03 06:42:03 otto Exp $	*/
+/*	$OpenBSD: newfs.c,v 1.116 2022/11/19 08:02:11 sthen Exp $	*/
 /*	$NetBSD: newfs.c,v 1.20 1996/05/16 07:13:03 thorpej Exp $	*/
 
 /*
@@ -203,7 +203,7 @@ main(int argc, char *argv[])
 		fatal("insane maxpartitions value %d", maxpartitions);
 
 	opstring = mfs ?
-	    "P:T:b:c:e:f:i:m:o:s:" :
+	    "O:P:T:b:c:e:f:i:m:o:s:" :
 	    "NO:S:T:b:c:e:f:g:h:i:m:o:qs:t:";
 	while ((ch = getopt(argc, argv, opstring)) != -1) {
 		switch (ch) {
