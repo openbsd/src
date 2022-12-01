e4d0e162690b26bac995be7e122c25ea97bd4067
dtucker <dtucker@openbsd.org>
2022-12-01 02:19:29 +0000

    Log output of ssh-agent and ssh-add to make debugging easier.

--- regress/usr.bin/ssh/agent-getpeereid.sh
+++ regress/usr.bin/ssh/agent-getpeereid.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: agent-getpeereid.sh,v 1.13 2021/09/01 00:50:27 dtucker Exp $
+#	$OpenBSD: agent-getpeereid.sh,v 1.14 2022/12/01 02:19:29 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="disallow agent attach from other uid"
@@ -6,6 +6,8 @@ tid="disallow agent attach from other uid"
 UNPRIV=nobody
 ASOCK=${OBJ}/agent
 SSH_AUTH_SOCK=/nonexistent
+>$OBJ/ssh-agent.log
+>$OBJ/ssh-add.log
 
 case "x$SUDO" in
 	xsudo) sudo=1;;
@@ -23,32 +25,33 @@ case "x$SUDO" in
 esac
 
 trace "start agent"
-eval `${SSHAGENT} ${EXTRA_AGENT_ARGS} -s -a ${ASOCK}` > /dev/null
+eval `${SSHAGENT} ${EXTRA_AGENT_ARGS} -s -a ${ASOCK}` >$OBJ/ssh-agent.log 2>&1
 r=$?
 if [ $r -ne 0 ]; then
 	fail "could not start ssh-agent: exit code $r"
 else
 	chmod 644 ${SSH_AUTH_SOCK}
 
-	${SSHADD} -l > /dev/null 2>&1
+	${SSHADD} -vvv -l >>$OBJ/ssh-add.log 2>&1
 	r=$?
 	if [ $r -ne 1 ]; then
 		fail "ssh-add failed with $r != 1"
 	fi
 	if test -z "$sudo" ; then
 		# doas
-		${SUDO} -n -u ${UNPRIV} ${SSHADD} -l 2>/dev/null
+		${SUDO} -n -u ${UNPRIV} ${SSHADD} -vvv -l >>$OBJ/ssh-add.log 2>&1
 	else
 		# sudo
-		< /dev/null ${SUDO} -S -u ${UNPRIV} ${SSHADD} -l 2>/dev/null
+		< /dev/null ${SUDO} -S -u ${UNPRIV} ${SSHADD} -vvv -l >>$OBJ/ssh-add.log 2>&1
 	fi
 	r=$?
 	if [ $r -lt 2 ]; then
 		fail "ssh-add did not fail for ${UNPRIV}: $r < 2"
+		cat $OBJ/ssh-add.log
 	fi
 
 	trace "kill agent"
-	${SSHAGENT} -k > /dev/null
+	${SSHAGENT} -vvv -k >>$OBJ/ssh-agent.log 2>&1
 fi
 
 rm -f ${OBJ}/agent
