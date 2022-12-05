531188d2161227cf7a8ad5b7a5ea4ac144c24dac
claudio <claudio@openbsd.org>
2022-12-05 21:02:52 +0000

    Prep for rpki-client 8.2

--- usr.sbin/rpki-client/version.h
+++ usr.sbin/rpki-client/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.13 2022/09/24 16:25:22 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.14 2022/12/05 21:02:52 claudio Exp $ */
 
-#define RPKI_VERSION	"8.1"
+#define RPKI_VERSION	"8.2"
