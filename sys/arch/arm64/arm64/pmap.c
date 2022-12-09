a6c8f9465544d24334598cf827c022c26c07ce42
kettenis <kettenis@openbsd.org>
2022-12-09 22:31:31 +0000

    Simplify early kernel bootstrap a bit more.  Just map the entire 64MB memory block that the bootloader allocates for us (minus the first 2MB).  This means we can get rid of a bunch of code in pmap_bootstrap().  It also makes sure that we don't accidentally enter mappings that cover secure memory just beyond where the kernel was loaded.
    ok patrick@

--- sys/arch/arm64/arm64/pmap.c
+++ sys/arch/arm64/arm64/pmap.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pmap.c,v 1.89 2022/11/21 20:19:21 kettenis Exp $ */
+/* $OpenBSD: pmap.c,v 1.90 2022/12/09 22:31:31 kettenis Exp $ */
 /*
  * Copyright (c) 2008-2009,2014-2016 Dale Rahn <drahn@dalerahn.com>
  *
@@ -1294,44 +1294,6 @@ pmap_bootstrap(long kvo, paddr_t lpt1, long kernelstart, long kernelend,
 	pa = pmap_steal_avail(Lx_TABLE_ALIGN, Lx_TABLE_ALIGN, &va);
 	memset((void *)pa, 0, Lx_TABLE_ALIGN);
 	pmap_kernel()->pm_pt0pa = pa;
-
-	/* now that we have mapping space for everything, lets map it */
-	/* all of these mappings are ram -> kernel va */
-
-	/*
-	 * enable mappings for existing 'allocated' mapping in the bootstrap
-	 * page tables
-	 */
-	extern uint64_t *pagetable;
-	extern char _end[];
-	vp2 = (void *)((long)&pagetable + kvo);
-	struct mem_region *mp;
-	ssize_t size;
-	for (mp = pmap_allocated; mp->size != 0; mp++) {
-		/* bounds may be kinda messed up */
-		for (pa = mp->start, size = mp->size & ~0xfff;
-		    size > 0;
-		    pa+= L2_SIZE, size -= L2_SIZE)
-		{
-			paddr_t mappa = pa & ~(L2_SIZE-1);
-			vaddr_t mapva = mappa - kvo;
-			int prot = PROT_READ | PROT_WRITE;
-
-			if (mapva < (vaddr_t)_end)
-				continue;
-
-			if (mapva >= (vaddr_t)__text_start &&
-			    mapva < (vaddr_t)_etext)
-				prot = PROT_READ | PROT_EXEC;
-			else if (mapva >= (vaddr_t)__rodata_start &&
-			    mapva < (vaddr_t)_erodata)
-				prot = PROT_READ;
-
-			vp2->l2[VP_IDX2(mapva)] = mappa | L2_BLOCK |
-			    ATTR_IDX(PTE_ATTR_WB) | ATTR_SH(SH_INNER) |
-			    ATTR_nG | ap_bits_kern[prot];
-		}
-	}
 
 	pmap_avail_fixup();
 
