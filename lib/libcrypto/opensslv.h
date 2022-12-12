ff193b36716f1753ba1a68f908f4c31993e8b59e
tb <tb@openbsd.org>
2022-12-12 14:16:58 +0000

    Bump to LibreSSL 3.7.1

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.71 2022/11/10 18:15:36 tb Exp $ */
+/* $OpenBSD: opensslv.h,v 1.72 2022/12/12 14:16:58 tb Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3070000fL
+#define LIBRESSL_VERSION_NUMBER 0x3070100fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.7.0"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.7.1"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
