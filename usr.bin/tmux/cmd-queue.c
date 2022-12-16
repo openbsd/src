c91c88b18252695d1bd3ff0a558ac8de55d5712c
nicm <nicm@openbsd.org>
2022-12-16 08:22:05 +0000

    Do not escape tabs in output (iTerm2 needs them). GitHub issue 3414.

--- usr.bin/tmux/cmd-queue.c
+++ usr.bin/tmux/cmd-queue.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-queue.c,v 1.111 2022/12/07 09:44:44 nicm Exp $ */
+/* $OpenBSD: cmd-queue.c,v 1.112 2022/12/16 08:22:05 nicm Exp $ */
 
 /*
  * Copyright (c) 2013 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -834,7 +834,7 @@ cmdq_print_data(struct cmdq_item *item, int parse, struct evbuffer *evb)
 	char				*sanitized, *msg, *line;
 
 	if (!parse) {
-		utf8_stravisx(&msg, data, size, VIS_OCTAL|VIS_CSTYLE|VIS_TAB);
+		utf8_stravisx(&msg, data, size, VIS_OCTAL|VIS_CSTYLE);
 		log_debug("%s: %s", __func__, msg);
 	} else {
 		msg = EVBUFFER_DATA(evb);
