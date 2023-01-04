60b454c967e80847f9512115d274d10b9d573723
jsg <jsg@openbsd.org>
2023-01-04 06:33:33 +0000

    Chuck Cranor rescinded the advertising clause of uvm_mmap.c in NetBSD rev 1.134 and confirmed with Mike Hibler that the University of Utah would do the same.
    https://mail-index.netbsd.org/source-changes/2011/02/02/msg018021.html
    
    ok deraadt@

--- sys/uvm/uvm_mmap.c
+++ sys/uvm/uvm_mmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_mmap.c,v 1.175 2022/11/17 18:53:05 deraadt Exp $	*/
+/*	$OpenBSD: uvm_mmap.c,v 1.176 2023/01/04 06:33:33 jsg Exp $	*/
 /*	$NetBSD: uvm_mmap.c,v 1.49 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -20,12 +20,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Charles D. Cranor,
- *	Washington University, University of California, Berkeley and
- *	its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
