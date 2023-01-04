fad6d19425b1a957794f23d76396489614b81eac
dlg <dlg@openbsd.org>
2023-01-04 03:47:10 +0000

    fix a comment, no functional change.

--- sys/net/ifq.h
+++ sys/net/ifq.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifq.h,v 1.35 2022/11/22 03:40:53 dlg Exp $ */
+/*	$OpenBSD: ifq.h,v 1.36 2023/01/04 03:47:10 dlg Exp $ */
 
 /*
  * Copyright (c) 2015 David Gwynne <dlg@openbsd.org>
@@ -283,7 +283,7 @@ struct ifiqueue {
  *		if_attach(ifp);
  *
  *		if_attach_queues(ifp, DRV_NUM_TX_RINGS);
- *		for (i = ; i < DRV_NUM_TX_RINGS; i++) {
+ *		for (i = 0; i < DRV_NUM_TX_RINGS; i++) {
  *			struct ifqueue *ifq = ifp->if_ifqs[i];
  *			struct drv_tx_ring *ring = &sc->sc_tx_rings[i];
  *
