b26ab6c35d92aee08b0eb8b23498e30c5a090cf1
jsg <jsg@openbsd.org>
2023-01-18 23:47:25 +0000

    add __list_add() for 6.1.7 drm

--- sys/dev/pci/drm/include/linux/list.h
+++ sys/dev/pci/drm/include/linux/list.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: list.h,v 1.6 2023/01/01 01:34:58 jsg Exp $	*/
+/*	$OpenBSD: list.h,v 1.7 2023/01/18 23:47:25 jsg Exp $	*/
 /* drm_linux_list.h -- linux list functions for the BSDs.
  * Created: Mon Apr 7 14:30:16 1999 by anholt@FreeBSD.org
  */
@@ -73,6 +73,16 @@ list_is_last(const struct list_head *list,
     const struct list_head *head)
 {
 	return list->next == head;
+}
+
+static inline void
+__list_add(struct list_head *new, struct list_head *prev,
+    struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
 }
 
 static inline void
