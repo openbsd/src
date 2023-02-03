be3178932f9d78afcc231eec89712e6b1dd7af27
deraadt <deraadt@openbsd.org>
2023-02-03 05:59:08 +0000

    default sparc64 ld.lld to --execute-only Note this doesn't actually work yet for anything big -- some unknown stuff needs to be fixed.  Also ld.lld isn't the default sparc64 ld yet. But at least having this available during normal builds, to test iterate upon, will speed up either of those desireable outcomes. ok claudio

--- gnu/llvm/lld/ELF/Driver.cpp
+++ gnu/llvm/lld/ELF/Driver.cpp
@@ -1487,6 +1487,7 @@ static void setConfigs(opt::InputArgList &args) {
   case EM_PPC:
   case EM_PPC64:
   case EM_RISCV:
+  case EM_SPARCV9:
   case EM_X86_64:
     config->executeOnly = true;
     break;
