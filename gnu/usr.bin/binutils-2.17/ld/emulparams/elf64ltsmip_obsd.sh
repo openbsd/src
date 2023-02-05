e09e767412aafd3a051128ad07e63ba8935b94e1
kettenis <kettenis@openbsd.org>
2023-02-05 15:26:36 +0000

    Switch the mips64 linker scripts over to the templates that order .rodata before .text.
    ok deraadt@

--- gnu/usr.bin/binutils-2.17/ld/emulparams/elf64ltsmip_obsd.sh
+++ gnu/usr.bin/binutils-2.17/ld/emulparams/elf64ltsmip_obsd.sh
@@ -3,6 +3,7 @@ MAXPAGESIZE=0x10000
 COMMONPAGESIZE=0x4000
 TEXT_START_ADDR="0x10000000"
 . ${srcdir}/emulparams/elf_obsd.sh
+SCRIPT_NAME=elf_obsd
 # XXX causes GOT oflows
 NO_PAD_CDTOR=y
 NOP=0x00000000
