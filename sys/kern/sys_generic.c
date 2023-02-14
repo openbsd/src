97b0c01557f5df5905132361238645084482ec94
mvs <mvs@openbsd.org>
2023-02-14 08:34:49 +0000

    Backout previous. Assertion was triggered within tsleep().

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_generic.c,v 1.153 2023/02/13 09:42:45 mvs Exp $	*/
+/*	$OpenBSD: sys_generic.c,v 1.154 2023/02/14 08:34:49 mvs Exp $	*/
 /*	$NetBSD: sys_generic.c,v 1.24 1996/03/29 00:25:32 cgd Exp $	*/
 
 /*
@@ -596,16 +596,15 @@ dopselect(struct proc *p, int nd, fd_set *in, fd_set *ou, fd_set *ex,
 	struct timespec zerots = {};
 	fd_mask bits[6];
 	fd_set *pibits[3], *pobits[3];
-	int error, nfiles, ncollected = 0, nevents = 0;
+	int error, ncollected = 0, nevents = 0;
 	u_int ni;
 
 	if (nd < 0)
 		return (EINVAL);
-
-	nfiles = READ_ONCE(p->p_fd->fd_nfiles);
-	if (nd > nfiles)
-		nd = nfiles;
-
+	if (nd > p->p_fd->fd_nfiles) {
+		/* forgiving; slightly wrong */
+		nd = p->p_fd->fd_nfiles;
+	}
 	ni = howmany(nd, NFDBITS) * sizeof(fd_mask);
 	if (ni > sizeof(bits[0])) {
 		caddr_t mbits;
@@ -644,11 +643,8 @@ dopselect(struct proc *p, int nd, fd_set *in, fd_set *ou, fd_set *ex,
 	}
 #endif
 
-	if (sigmask) {
-		KERNEL_LOCK();
+	if (sigmask)
 		dosigsuspend(p, *sigmask &~ sigcantmask);
-		KERNEL_UNLOCK();
-	}
 
 	/* Register kqueue events */
 	error = pselregister(p, pibits, pobits, nd, &nevents, &ncollected);
@@ -952,11 +948,8 @@ doppoll(struct proc *p, struct pollfd *fds, u_int nfds,
 	if ((error = copyin(fds, pl, sz)) != 0)
 		goto bad;
 
-	if (sigmask) {
-		KERNEL_LOCK();
+	if (sigmask)
 		dosigsuspend(p, *sigmask &~ sigcantmask);
-		KERNEL_UNLOCK();
-	}
 
 	/* Register kqueue events */
 	ppollregister(p, pl, nfds, &nevents, &ncollected);
