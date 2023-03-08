7c1e87a135a302fe789c0b6a9184628ec0243313
jsing <jsing@openbsd.org>
2023-03-08 07:15:42 +0000

    Fix a EC_GROUP_clear_free() that snuck through.
    Thanks to Mark Patruck for reporting.

--- lib/libcrypto/ec/ec_cvt.c
+++ lib/libcrypto/ec/ec_cvt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ec_cvt.c,v 1.9 2023/03/08 05:35:51 jsing Exp $ */
+/* $OpenBSD: ec_cvt.c,v 1.10 2023/03/08 07:15:42 jsing Exp $ */
 /*
  * Originally written by Bodo Moeller for the OpenSSL project.
  */
@@ -89,7 +89,7 @@ ec_group_new_curve(const EC_METHOD *method, const BIGNUM *p, const BIGNUM *a,
 	return group;
 
  err:
-	EC_GROUP_clear_free(group);
+	EC_GROUP_free(group);
 
 	return NULL;
 }
