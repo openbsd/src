ae980b2d31aa09bd6e87dd86e266f0363cf7b770
dtucker <dtucker@openbsd.org>
2023-03-14 07:28:47 +0000

    Free KRL itself in addition to its contents. From Coverity CID 291841, ok djm@

--- usr.bin/ssh/krl.c
+++ usr.bin/ssh/krl.c
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $OpenBSD: krl.c,v 1.54 2022/04/28 02:53:31 djm Exp $ */
+/* $OpenBSD: krl.c,v 1.55 2023/03/14 07:28:47 dtucker Exp $ */
 
 #include <sys/types.h>
 #include <sys/tree.h>
@@ -189,6 +189,7 @@ ssh_krl_free(struct ssh_krl *krl)
 		TAILQ_REMOVE(&krl->revoked_certs, rc, entry);
 		revoked_certs_free(rc);
 	}
+	free(krl);
 }
 
 void
