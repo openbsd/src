fb9222a7a4d8ab0301cfda2f5d5c4b11fc996629
jsing <jsing@openbsd.org>
2023-03-26 19:22:35 +0000

    Remove unnecessary HIDDEN_DECLS.

--- lib/libcrypto/sha/sha1dgst.c
+++ lib/libcrypto/sha/sha1dgst.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sha1dgst.c,v 1.20 2023/03/26 19:21:13 jsing Exp $ */
+/* $OpenBSD: sha1dgst.c,v 1.21 2023/03/26 19:22:35 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -89,15 +89,10 @@
 					  ix=(a)=ROTATE((a),1)	\
 					)
 
-__BEGIN_HIDDEN_DECLS
-
 #ifndef SHA1_ASM
 static
 #endif
-
 void sha1_block_data_order (SHA_CTX *c, const void *p, size_t num);
-
-__END_HIDDEN_DECLS
 
 #include "md32_common.h"
 
