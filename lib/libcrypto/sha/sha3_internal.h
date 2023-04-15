8a4ba2fcb6aa72c943522f6d78af9c86ef1d240a
jsing <jsing@openbsd.org>
2023-04-15 18:07:44 +0000

    Apply style(9) (first pass).

--- lib/libcrypto/sha/sha3_internal.h
+++ lib/libcrypto/sha/sha3_internal.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sha3_internal.h,v 1.2 2023/04/15 17:59:50 jsing Exp $	*/
+/*	$OpenBSD: sha3_internal.h,v 1.3 2023/04/15 18:07:44 jsing Exp $	*/
 /*
  * The MIT License (MIT)
  *
@@ -42,20 +42,20 @@
 
 // state context
 typedef struct {
-    union {                                 // state:
-        uint8_t b[200];                     // 8-bit bytes
-        uint64_t q[25];                     // 64-bit words
-    } st;
-    int pt, rsiz, mdlen;                    // these don't overflow
+	union {				// state:
+		uint8_t b[200];		// 8-bit bytes
+		uint64_t q[25];		// 64-bit words
+	} st;
+	int pt, rsiz, mdlen;		// these don't overflow
 } sha3_ctx_t;
 
 // Compression function.
 void sha3_keccakf(uint64_t st[25]);
 
 // OpenSSL - like interfece
-int sha3_init(sha3_ctx_t *c, int mdlen);    // mdlen = hash output in bytes
+int sha3_init(sha3_ctx_t *c, int mdlen);	// mdlen = hash output in bytes
 int sha3_update(sha3_ctx_t *c, const void *data, size_t len);
-int sha3_final(void *md, sha3_ctx_t *c);    // digest goes to md
+int sha3_final(void *md, sha3_ctx_t *c);	// digest goes to md
 
 // compute a sha3 hash (md) of given byte length from "in"
 void *sha3(const void *in, size_t inlen, void *md, int mdlen);
