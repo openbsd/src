b3eed87fdc320cefa6c6e92eb99bc50d0a9e4114
tb <tb@openbsd.org>
2023-04-16 07:36:43 +0000

    Make pcy_int.h pull in x509_local.h it will need it soon
    ok jsing

--- lib/libcrypto/x509/pcy_int.h
+++ lib/libcrypto/x509/pcy_int.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: pcy_int.h,v 1.1 2020/06/04 15:19:31 jsing Exp $ */
+/* $OpenBSD: pcy_int.h,v 1.2 2023/04/16 07:36:43 tb Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2004.
  */
@@ -55,6 +55,9 @@
  * Hudson (tjh@cryptsoft.com).
  *
  */
+
+/* Needed to pull in the typedefs for X509_POLICY_* */
+#include "x509_local.h"
 
 __BEGIN_HIDDEN_DECLS
 
