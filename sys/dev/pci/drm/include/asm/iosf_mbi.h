8c62f8dd253f113f51bb136bcc2f94db06a3e85e
dlg <dlg@openbsd.org>
2023-04-23 00:49:04 +0000

    iosf(4) implements functionality that was stubbed out in drm

--- sys/dev/pci/drm/include/asm/iosf_mbi.h
+++ sys/dev/pci/drm/include/asm/iosf_mbi.h
@@ -3,10 +3,11 @@
 #ifndef _ASM_IOSF_MBI_H
 #define _ASM_IOSF_MBI_H
 
-struct notifier_block;
+#include "iosf.h"
 
-#define MBI_PMIC_BUS_ACCESS_BEGIN	1
-#define MBI_PMIC_BUS_ACCESS_END		2
+#if NIOSF > 0
+#include <dev/ic/iosfvar.h>
+#else
 
 static inline void
 iosf_mbi_assert_punit_acquired(void)
@@ -22,6 +23,13 @@ static inline void
 iosf_mbi_punit_release(void)
 {
 }
+
+#endif /* NIOSF */
+
+struct notifier_block;
+
+#define MBI_PMIC_BUS_ACCESS_BEGIN	1
+#define MBI_PMIC_BUS_ACCESS_END		2
 
 static inline int
 iosf_mbi_register_pmic_bus_access_notifier(struct notifier_block *nb)
