8f4cde5df3b32decde8cdafaab24e1439df1fe72
tb <tb@openbsd.org>
2023-04-25 17:17:21 +0000

    Remove a couple of temporary hacks

--- regress/lib/libcrypto/bn/bn_mod_exp.c
+++ regress/lib/libcrypto/bn/bn_mod_exp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bn_mod_exp.c,v 1.36 2023/04/25 15:34:07 tb Exp $ */
+/*	$OpenBSD: bn_mod_exp.c,v 1.37 2023/04/25 17:17:21 tb Exp $ */
 
 /*
  * Copyright (c) 2022,2023 Theo Buehler <tb@openbsd.org>
@@ -27,9 +27,6 @@
 
 #define N_MOD_EXP_TESTS		100
 #define N_MOD_EXP2_TESTS	50
-
-int	BN_mod_exp_recp(BIGNUM *r, const BIGNUM *a, const BIGNUM *p,
-    const BIGNUM *m, BN_CTX *ctx);
 
 #define INIT_MOD_EXP_FN(f) { .name = #f, .mod_exp_fn = (f), }
 #define INIT_MOD_EXP_MONT_FN(f) { .name = #f, .mod_exp_mont_fn = (f), }
