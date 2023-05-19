144b3efa58c940d89ee2a0ec145dadd54adaf552
guenther <guenther@openbsd.org>
2023-05-19 01:12:23 +0000

    Since waitid(2) shares code with wait4(2) and doesn't expose any non-trivial new information or code-paths over wait4(), include it in pledge("stdio")
    discussed with deraadt@

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.304 2023/02/19 18:46:46 anton Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.305 2023/05/19 01:12:23 guenther Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -231,6 +231,7 @@ const uint64_t pledge_syscalls[SYS_MAXSYSCALL] = {
 	[SYS_socketpair] = PLEDGE_STDIO,
 
 	[SYS_wait4] = PLEDGE_STDIO,
+	[SYS_waitid] = PLEDGE_STDIO,
 
 	/*
 	 * Can kill self with "stdio".  Killing another pid
