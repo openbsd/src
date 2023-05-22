e8736803f12c30d8a6324971679943e4918f6aa8
tb <tb@openbsd.org>
2023-05-22 15:20:16 +0000

    Bye, bye, horrible reacharounds into libcrypto
    Now that rpki-client no longer uses LibreSSL-specific ASN1_time_* API,
    we can get rid of some of the gross hacks needed for testing against
    OpenSSL in regress. This simplifies things greatly.
    
    Unfortunately, the unistd.h hack needs to stay until someone unearths
    their STACK_OF compat diffs.

--- regress/usr.sbin/rpki-client/openssl11/unistd.h
+++ regress/usr.sbin/rpki-client/openssl11/unistd.h
@@ -6,11 +6,7 @@
 
 #include_next <unistd.h>
 
-#include <openssl/asn1.h>
 #include <openssl/stack.h>
-
-int ASN1_time_parse(const char *, size_t, struct tm *, int);
-int ASN1_time_tm_cmp(struct tm *, struct tm *);
 
 #ifndef DECLARE_STACK_OF
 #define DECLARE_STACK_OF DEFINE_STACK_OF
