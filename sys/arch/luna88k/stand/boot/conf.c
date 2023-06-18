15cd4de1daee7cf71c45b18ed4381928e83781f5
aoyama <aoyama@openbsd.org>
2023-06-18 13:13:00 +0000

    Add fchmod() entry in luna88k bootloader.
    Now 'chmod a-x /bsd.upgrade' works in order to prevent re-upgrade, like other architectures.
    
    Tested by me on LUNA-88K2.

--- sys/arch/luna88k/stand/boot/conf.c
+++ sys/arch/luna88k/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.5 2023/02/11 04:35:34 aoyama Exp $	*/
+/*	$OpenBSD: conf.c,v 1.6 2023/06/18 13:13:00 aoyama Exp $	*/
 /*	$NetBSD: conf.c,v 1.3 2013/01/16 15:46:20 tsutsui Exp $	*/
 
 /*
@@ -98,7 +98,8 @@ int	n_netif_drivers = sizeof(netif_drivers) / sizeof(netif_drivers[0]);
 	__CONCAT(fs,_write), \
 	__CONCAT(fs,_seek), \
 	__CONCAT(fs,_stat), \
-	__CONCAT(fs,_readdir) }
+	__CONCAT(fs,_readdir), \
+	__CONCAT(fs,_fchmod) }
 #ifdef SUPPORT_DISK
 struct fs_ops file_system_disk[] = {
 	FS_OPS(ufs),
@@ -106,6 +107,7 @@ struct fs_ops file_system_disk[] = {
 int	nfsys_disk = sizeof(file_system_disk) / sizeof(file_system_disk[0]);
 #endif
 #ifdef SUPPORT_ETHERNET
+#define	nfs_fchmod	NULL
 struct fs_ops file_system_nfs[] = { FS_OPS(nfs) };
 #endif
 
