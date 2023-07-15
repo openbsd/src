dd7ba58db8f0e46a12572483f40488a2cd49ebc9
tb <tb@openbsd.org>
2023-07-15 19:29:44 +0000

    Import a version of libcrypto's symbols test for libssl

---
+++ regress/lib/libssl/symbols/symbols.d
@@ -0,0 +1,137 @@
+symbols.o: symbols.c /usr/include/openssl/dtls1.h /usr/include/sys/time.h \
+  /usr/include/sys/select.h /usr/include/sys/types.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/machine/endian.h /usr/include/time.h \
+  /usr/include/sys/_null.h /usr/include/sys/_time.h /usr/include/stdio.h \
+  /usr/include/stdlib.h /usr/include/string.h /usr/include/strings.h \
+  /usr/include/openssl/opensslconf.h \
+  /usr/include/openssl/opensslfeatures.h /usr/include/openssl/buffer.h \
+  /usr/include/openssl/ossl_typ.h /usr/include/stddef.h \
+  /usr/include/openssl/ssl.h /usr/include/stdint.h \
+  /usr/include/openssl/hmac.h /usr/include/openssl/evp.h \
+  /usr/include/openssl/bio.h /usr/include/stdarg.h \
+  /usr/include/openssl/crypto.h /usr/include/openssl/stack.h \
+  /usr/include/openssl/safestack.h /usr/include/openssl/opensslv.h \
+  /usr/include/openssl/objects.h /usr/include/openssl/obj_mac.h \
+  /usr/include/openssl/asn1.h /usr/include/openssl/bn.h \
+  /usr/include/openssl/pem.h /usr/include/openssl/x509.h \
+  /usr/include/openssl/dh.h /usr/include/openssl/dsa.h \
+  /usr/include/openssl/ec.h /usr/include/openssl/ecdsa.h \
+  /usr/include/openssl/ecdh.h /usr/include/openssl/rsa.h \
+  /usr/include/openssl/sha.h /usr/include/openssl/x509_vfy.h \
+  /usr/include/openssl/lhash.h /usr/include/openssl/pkcs7.h \
+  /usr/include/openssl/pem2.h /usr/include/openssl/ssl2.h \
+  /usr/include/openssl/ssl3.h /usr/include/openssl/tls1.h \
+  /usr/include/openssl/ssl23.h /usr/include/openssl/srtp.h
+
+/usr/include/openssl/dtls1.h:
+
+/usr/include/sys/time.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/sys/types.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/machine/cdefs.h:
+
+/usr/include/sys/endian.h:
+
+/usr/include/sys/_endian.h:
+
+/usr/include/sys/_types.h:
+
+/usr/include/machine/_types.h:
+
+/usr/include/machine/endian.h:
+
+/usr/include/time.h:
+
+/usr/include/sys/_null.h:
+
+/usr/include/sys/_time.h:
+
+/usr/include/stdio.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/string.h:
+
+/usr/include/strings.h:
+
+/usr/include/openssl/opensslconf.h:
+
+/usr/include/openssl/opensslfeatures.h:
+
+/usr/include/openssl/buffer.h:
+
+/usr/include/openssl/ossl_typ.h:
+
+/usr/include/stddef.h:
+
+/usr/include/openssl/ssl.h:
+
+/usr/include/stdint.h:
+
+/usr/include/openssl/hmac.h:
+
+/usr/include/openssl/evp.h:
+
+/usr/include/openssl/bio.h:
+
+/usr/include/stdarg.h:
+
+/usr/include/openssl/crypto.h:
+
+/usr/include/openssl/stack.h:
+
+/usr/include/openssl/safestack.h:
+
+/usr/include/openssl/opensslv.h:
+
+/usr/include/openssl/objects.h:
+
+/usr/include/openssl/obj_mac.h:
+
+/usr/include/openssl/asn1.h:
+
+/usr/include/openssl/bn.h:
+
+/usr/include/openssl/pem.h:
+
+/usr/include/openssl/x509.h:
+
+/usr/include/openssl/dh.h:
+
+/usr/include/openssl/dsa.h:
+
+/usr/include/openssl/ec.h:
+
+/usr/include/openssl/ecdsa.h:
+
+/usr/include/openssl/ecdh.h:
+
+/usr/include/openssl/rsa.h:
+
+/usr/include/openssl/sha.h:
+
+/usr/include/openssl/x509_vfy.h:
+
+/usr/include/openssl/lhash.h:
+
+/usr/include/openssl/pkcs7.h:
+
+/usr/include/openssl/pem2.h:
+
+/usr/include/openssl/ssl2.h:
+
+/usr/include/openssl/ssl3.h:
+
+/usr/include/openssl/tls1.h:
+
+/usr/include/openssl/ssl23.h:
+
+/usr/include/openssl/srtp.h:
