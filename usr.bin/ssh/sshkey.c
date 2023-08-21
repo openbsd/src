38ab26de4514adde399b2afa4b31a44a7fe0441c
djm <djm@openbsd.org>
2023-08-21 04:36:46 +0000

    want stdlib.h for free(3)

--- usr.bin/ssh/sshkey.c
+++ usr.bin/ssh/sshkey.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshkey.c,v 1.137 2023/07/27 22:23:05 djm Exp $ */
+/* $OpenBSD: sshkey.c,v 1.138 2023/08/21 04:36:46 djm Exp $ */
 /*
  * Copyright (c) 2000, 2001 Markus Friedl.  All rights reserved.
  * Copyright (c) 2008 Alexander von Gernler.  All rights reserved.
@@ -38,6 +38,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <util.h>
 #include <limits.h>
