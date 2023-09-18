4a0a51f8839c30f0ad4ecbb6192bbca890e7f788
deraadt <deraadt@openbsd.org>
2023-09-18 13:16:13 +0000

    crank to 7.4-beta

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: sys.mk,v 1.93 2023/03/04 14:49:37 deraadt Exp $
+#	$OpenBSD: sys.mk,v 1.94 2023/09/18 13:16:13 deraadt Exp $
 #	$NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 #	@(#)sys.mk	5.11 (Berkeley) 3/13/91
 
@@ -10,7 +10,7 @@
 
 unix=		We run OpenBSD.
 OSMAJOR=	7
-OSMINOR=	3
+OSMINOR=	4
 OSREV=		$(OSMAJOR).$(OSMINOR)
 OSrev=		$(OSMAJOR)$(OSMINOR)
 
