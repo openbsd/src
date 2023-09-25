1100288745b4dbc564eb0f1ca3fd609a3e34acc1
deraadt <deraadt@openbsd.org>
2023-09-25 15:38:46 +0000

    match on Meinberg PZF180PEX; from Maurice Janssen

--- sys/dev/pci/mbg.c
+++ sys/dev/pci/mbg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mbg.c,v 1.34 2023/04/10 04:21:20 jsg Exp $ */
+/*	$OpenBSD: mbg.c,v 1.35 2023/09/25 15:38:46 deraadt Exp $ */
 
 /*
  * Copyright (c) 2006, 2007 Marc Balmer <mbalmer@openbsd.org>
@@ -160,7 +160,8 @@ const struct pci_matchid mbg_devices[] = {
 	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PCI32 },
 	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PCI509 },
 	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PCI511 },
-	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PEX511 }
+	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PEX511 },
+	{ PCI_VENDOR_MEINBERG, PCI_PRODUCT_MEINBERG_PZF180PEX }
 };
 
 int
@@ -246,6 +247,7 @@ mbg_attach(struct device *parent, struct device *self, void *aux)
 		sensor_task_register(sc, mbg_task, 10);
 		break;
 	case PCI_PRODUCT_MEINBERG_GPS170PCI:
+	case PCI_PRODUCT_MEINBERG_PZF180PEX:
 		t_trust = 4 * 24 * 60 * 60;	/* four days */
 		sc->sc_read = mbg_read_asic;
 		sensor_task_register(sc, mbg_task_hr, 1);
