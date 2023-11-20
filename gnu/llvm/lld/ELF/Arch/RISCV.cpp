7474b486ee6245367d3a67d178c6fda11adc52d5
jca <jca@openbsd.org>
2023-11-20 04:53:11 +0000

    Restore lost patch: downgrade RISCV ABI mismatch error to a warning
    This lets lld link code with object files created with ld -b binary,
    as used by several ports (mupdf, postgresql-pllua, various games).
    Upstream report with an unfinished diff: https://reviews.llvm.org/D106378
    
    ok gkoehler@ kettenis@

--- gnu/llvm/lld/ELF/Arch/RISCV.cpp
+++ gnu/llvm/lld/ELF/Arch/RISCV.cpp
@@ -144,7 +144,7 @@ uint32_t RISCV::calcEFlags() const {
       target |= EF_RISCV_RVC;
 
     if ((eflags & EF_RISCV_FLOAT_ABI) != (target & EF_RISCV_FLOAT_ABI))
-      error(
+      warn(
           toString(f) +
           ": cannot link object files with different floating-point ABI from " +
           toString(ctx.objectFiles[0]));
