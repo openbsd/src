50f17a8287e71ad65d323fb0081b7d9d79788633
florian <florian@openbsd.org>
2023-11-25 13:00:05 +0000

    whitespace; spotted by kn

--- sbin/dhcpleased/printconf.c
+++ sbin/dhcpleased/printconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: printconf.c,v 1.5 2023/11/25 12:00:39 florian Exp $	*/
+/*	$OpenBSD: printconf.c,v 1.6 2023/11/25 13:00:05 florian Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -127,7 +127,7 @@ print_config(struct dhcpleased_conf *conf)
 
 		}
 		if (iface->prefer_ipv6)
-			printf("\t prefer ipv6\n");
+			printf("\tprefer ipv6\n");
 		printf("}\n");
 	}
 }
