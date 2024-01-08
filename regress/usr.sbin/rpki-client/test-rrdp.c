43e17b8a33526238b60e691082a827a246e34fbe
tb <tb@openbsd.org>
2024-01-08 08:26:38 +0000

    Fix regress build since the reacharounds are fragile
    noted by anton

--- regress/usr.sbin/rpki-client/test-rrdp.c
+++ regress/usr.sbin/rpki-client/test-rrdp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: test-rrdp.c,v 1.8 2023/06/24 07:07:36 claudio Exp $ */
+/*	$OpenBSD: test-rrdp.c,v 1.9 2024/01/08 08:26:38 tb Exp $ */
 /*
  * Copyright (c) 2020 Nils Fisher <nils_fisher@hotmail.com>
  * Copyright (c) 2021 Claudio Jeker <claudio@openbsd.org>
@@ -35,8 +35,9 @@
 #include "extern.h"
 #include "rrdp.h"
 
-int verbose;
 int filemode;
+int outformats;
+int verbose;
 
 #define REGRESS_NOTIFY_URI	"https://rpki.example.com/notify.xml"
 
