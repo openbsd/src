3b5541ba8d82142735a6c0f1a8786a25401a1c58
deraadt <deraadt@openbsd.org>
2024-01-16 19:08:37 +0000

    print flag 'l' for base program or ld.so being under pinsyscalls enforcement, and 'L' for libc.so.  This flag printing may be deleted once we are entirely confident this is working correctly. ok kettenis

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print.c,v 1.86 2023/03/08 14:47:02 tobhe Exp $	*/
+/*	$OpenBSD: print.c,v 1.87 2024/01/16 19:08:37 deraadt Exp $	*/
 /*	$NetBSD: print.c,v 1.27 1995/09/29 21:58:12 cgd Exp $	*/
 
 /*-
@@ -303,6 +303,10 @@ printstate(const struct pinfo *pi, VARENT *ve)
 		*cp++ = '+';
 	if (kp->p_psflags & PS_PLEDGE)
 		*cp++ = 'p';
+	if (kp->p_psflags & PS_PIN)
+		*cp++ = 'l';
+	if (kp->p_psflags & PS_LIBCPIN)
+		*cp++ = 'L';
 	if (kp->p_eflag & EPROC_UNVEIL) {
 		if (kp->p_eflag & EPROC_LKUNVEIL)
 			*cp++ = 'U';
