7d75b92ac3cdabdec06a2a7839e63c9d5d7bef57
jsg <jsg@openbsd.org>
2024-01-18 07:52:37 +0000

    remove duplicate defines, merge error from local patches

--- sys/dev/pci/drm/i915/intel_device_info.h
+++ sys/dev/pci/drm/i915/intel_device_info.h
@@ -133,17 +133,6 @@ enum intel_platform {
 #define INTEL_SUBPLATFORM_M	0
 #define INTEL_SUBPLATFORM_P	1
 
-/* ADL */
-#define INTEL_SUBPLATFORM_RPL	0
-
-/* ADL-P */
-/*
- * As #define INTEL_SUBPLATFORM_RPL 0 will apply
- * here too, SUBPLATFORM_N will have different
- * bit set
- */
-#define INTEL_SUBPLATFORM_N    1
-
 enum intel_ppgtt_type {
 	INTEL_PPGTT_NONE = I915_GEM_PPGTT_NONE,
 	INTEL_PPGTT_ALIASING = I915_GEM_PPGTT_ALIASING,
