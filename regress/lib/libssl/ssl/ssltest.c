319de9d21c3312642723dcb88f2b8e69fe593177
tb <tb@openbsd.org>
2024-03-01 03:45:16 +0000

    ssltest: drop another use of CRYPTO_mem_leaks()

--- regress/lib/libssl/ssl/ssltest.c
+++ regress/lib/libssl/ssl/ssltest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ssltest.c,v 1.44 2023/11/19 13:12:06 tb Exp $ */
+/*	$OpenBSD: ssltest.c,v 1.45 2024/03/01 03:45:16 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -763,7 +763,6 @@ end:
 	ERR_free_strings();
 	ERR_remove_thread_state(NULL);
 	EVP_cleanup();
-	CRYPTO_mem_leaks(bio_err);
 	BIO_free(bio_err);
 
 	exit(ret);
