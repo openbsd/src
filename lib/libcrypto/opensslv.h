211229d1689008022e679e1bdfd3e685ebcb90ff
tb <tb@openbsd.org>
2024-03-02 11:53:30 +0000

    Crank libressl version to 3.9.0 (finally!)

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.77 2023/09/20 11:42:25 tb Exp $ */
+/* $OpenBSD: opensslv.h,v 1.78 2024/03/02 11:53:30 tb Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3080200fL
+#define LIBRESSL_VERSION_NUMBER 0x3090000fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.8.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.9.0"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
