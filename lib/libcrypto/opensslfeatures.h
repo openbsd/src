5e695025b1a75ec59cb3add0e4a53f502cc7734a
tb <tb@openbsd.org>
2024-03-02 11:25:46 +0000

    Update list of OPENSSL_NO_*
    This syncs the list with some version of upstream and exposes a few
    OPENSSL_NO_* that may now be relevant.
    
    from jsing (a long time ago)

--- lib/libcrypto/opensslfeatures.h
+++ lib/libcrypto/opensslfeatures.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: opensslfeatures.h,v 1.42 2024/03/02 11:17:27 tb Exp $ */
+/* $OpenBSD: opensslfeatures.h,v 1.43 2024/03/02 11:25:46 tb Exp $ */
 /*
  * Feature flags for LibreSSL... so you can actually tell when things
  * are enabled, rather than not being able to tell when things are
@@ -34,14 +34,19 @@
 /* #define OPENSSL_NO_AUTOERRINIT */
 /* #define OPENSSL_NO_AUTOLOAD_CONFIG */
 /* #define OPENSSL_NO_BF */
-/* #define OPENSSL_NO_BLAKE2 */
+#define OPENSSL_NO_BLAKE2
+#define OPENSSL_NO_BROTLI
+/* #define OPENSSL_NO_BUILTIN_OVERFLOW_CHECKING */
 /* #define OPENSSL_NO_CAMELLIA */
-/* #define OPENSSL_NO_CAPIENG */
+#define OPENSSL_NO_CAPIENG
 /* #define OPENSSL_NO_CAST */
 /* #define OPENSSL_NO_CHACHA */
 /* #define OPENSSL_NO_CMAC */
+/* #define OPENSSL_NO_CMP */
 /* #define OPENSSL_NO_CMS */
-#define OPENSSL_NO_COMP /* XXX */
+#define OPENSSL_NO_COMP
+/* #define OPENSSL_NO_COMP_ALG */
+/* #define OPENSSL_NO_CRMF */
 /* #define OPENSSL_NO_CRYPTO_MDEBUG */
 /* #define OPENSSL_NO_CRYPTO_MDEBUG_BACKTRACE */
 /* #define OPENSSL_NO_CT */
@@ -65,6 +70,7 @@
 #define OPENSSL_NO_EGD
 #define OPENSSL_NO_ENGINE
 /* #define OPENSSL_NO_ERR */
+/* #define OPENSSL_NO_FILENAMES */
 /* #define OPENSSL_NO_FUZZ_AFL */
 /* #define OPENSSL_NO_FUZZ_LIBFUZZER */
 #define OPENSSL_NO_GOST
@@ -73,6 +79,11 @@
 /* #define OPENSSL_NO_HW_PADLOCK */
 /* #define OPENSSL_NO_IDEA */
 /* #define OPENSSL_NO_INLINE_ASM */
+/* #define OPENSSL_NO_KEYPARAMS */
+#define OPENSSL_NO_KTLS
+/* #define OPENSSL_NO_KTLS_RX */
+/* #define OPENSSL_NO_KTLS_ZC_TX */
+/* #define OPENSSL_NO_LOCALE */
 #define OPENSSL_NO_MD2
 /* #define OPENSSL_NO_MD4 */
 /* #define OPENSSL_NO_MD5 */
@@ -81,10 +92,12 @@
 /* #define OPENSSL_NO_NEXTPROTONEG */
 /* #define OPENSSL_NO_OCB */
 /* #define OPENSSL_NO_OCSP */
+/* #define OPENSSL_NO_PADLOCKENG */
 /* #define OPENSSL_NO_PINSHARED */
 /* #define OPENSSL_NO_POLY1305 */
 /* #define OPENSSL_NO_POSIX_IO */
 #define OPENSSL_NO_PSK
+#define OPENSSL_NO_QUIC
 /* #define OPENSSL_NO_RC2 */
 /* #define OPENSSL_NO_RC4 */
 #define OPENSSL_NO_RC5
@@ -92,11 +105,12 @@
 /* #define OPENSSL_NO_RFC3779 */
 /* #define OPENSSL_NO_RMD160 */
 /* #define OPENSSL_NO_RSA */
-/* #define OPENSSL_NO_SCRYPT */
+#define OPENSSL_NO_SCRYPT
 #define OPENSSL_NO_SCTP
 /* #define OPENSSL_NO_SECURE_MEMORY */
 #define OPENSSL_NO_SEED
 /* #define OPENSSL_NO_SIPHASH */
+/* #define OPENSSL_NO_SIV */
 /* #define OPENSSL_NO_SM2 */
 /* #define OPENSSL_NO_SM3 */
 /* #define OPENSSL_NO_SM4 */
@@ -108,6 +122,7 @@
 #define OPENSSL_NO_SSL_TRACE
 /* #define OPENSSL_NO_STATIC_ENGINE */
 /* #define OPENSSL_NO_STDIO */
+/* #define OPENSSL_NO_THREAD_POOL */
 /* #define OPENSSL_NO_TLS */
 #define OPENSSL_NO_TLS1
 #define OPENSSL_NO_TLS1_1
@@ -119,8 +134,13 @@
 #define OPENSSL_NO_TLS1_3
 #endif
 /* #define OPENSSL_NO_TLS1_METHOD */
+/* #define OPENSSL_NO_TRACE */
 /* #define OPENSSL_NO_TS */
 /* #define OPENSSL_NO_UI_CONSOLE */
 /* #define OPENSSL_NO_UNIT_TEST */
+/* #define OPENSSL_NO_UNIX_SOCK */
 /* #define OPENSSL_NO_WEAK_SSL_CIPHERS */
 /* #define OPENSSL_NO_WHIRLPOOL */
+/* #define OPENSSL_NO_WINSTORE */
+#define OPENSSL_NO_ZLIB
+/* #define OPENSSL_NO_ZSTD */
