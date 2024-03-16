32b03391502ca53007d47df3bd83460d36c6cc99
kettenis <kettenis@openbsd.org>
2024-03-16 20:46:28 +0000

    Set the HCR_API and HCR_APK bits in the HCR_EL2 when CPUs boot in EL2. Otherwise using PAC instructions in EL1 will trigger a trap into EL2 that we don't handle.
    ok jsg@, deraadt@

--- sys/arch/arm64/include/hypervisor.h
+++ sys/arch/arm64/include/hypervisor.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: hypervisor.h,v 1.3 2021/02/21 13:14:47 kettenis Exp $ */
+/* $OpenBSD: hypervisor.h,v 1.4 2024/03/16 20:46:28 kettenis Exp $ */
 /*-
  * Copyright (c) 2013, 2014 Andrew Turner
  * All rights reserved.
@@ -82,6 +82,8 @@
 #define	HCR_CD		0x0000000100000000
 #define	HCR_ID		0x0000000200000000
 #define	HCR_E2H		0x0000000400000000
+#define	HCR_APK		0x0000010000000000
+#define	HCR_API		0x0000020000000000
 
 #endif
 
