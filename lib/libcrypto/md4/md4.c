13b5123909a2889f4eb9084b67dfaeffbb24c7cd
jsing <jsing@openbsd.org>
2024-03-26 06:54:20 +0000

    Expand HASH_* defines.
    No change to generated assembly.

--- lib/libcrypto/md4/md4.c
+++ lib/libcrypto/md4/md4.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: md4.c,v 1.9 2024/03/26 06:40:29 jsing Exp $ */
+/* $OpenBSD: md4.c,v 1.10 2024/03/26 06:54:20 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -93,47 +93,47 @@ __END_HIDDEN_DECLS
 #include "md32_common.h"
 
 int
-HASH_UPDATE(HASH_CTX *c, const void *data_, size_t len)
+MD4_Update(MD4_CTX *c, const void *data_, size_t len)
 {
 	const unsigned char *data = data_;
 	unsigned char *p;
-	HASH_LONG l;
+	MD4_LONG l;
 	size_t n;
 
 	if (len == 0)
 		return 1;
 
-	l = (c->Nl + (((HASH_LONG)len) << 3))&0xffffffffUL;
+	l = (c->Nl + (((MD4_LONG)len) << 3))&0xffffffffUL;
 	/* 95-05-24 eay Fixed a bug with the overflow handling, thanks to
 	 * Wei Dai <weidai@eskimo.com> for pointing it out. */
 	if (l < c->Nl) /* overflow */
 		c->Nh++;
-	c->Nh+=(HASH_LONG)(len>>29);	/* might cause compiler warning on 16-bit */
+	c->Nh+=(MD4_LONG)(len>>29);	/* might cause compiler warning on 16-bit */
 	c->Nl = l;
 
 	n = c->num;
 	if (n != 0) {
 		p = (unsigned char *)c->data;
 
-		if (len >= HASH_CBLOCK || len + n >= HASH_CBLOCK) {
-			memcpy (p + n, data, HASH_CBLOCK - n);
-			HASH_BLOCK_DATA_ORDER (c, p, 1);
-			n = HASH_CBLOCK - n;
+		if (len >= MD4_CBLOCK || len + n >= MD4_CBLOCK) {
+			memcpy (p + n, data, MD4_CBLOCK - n);
+			md4_block_data_order (c, p, 1);
+			n = MD4_CBLOCK - n;
 			data += n;
 			len -= n;
 			c->num = 0;
-			memset (p,0,HASH_CBLOCK);	/* keep it zeroed */
+			memset(p, 0, MD4_CBLOCK);	/* keep it zeroed */
 		} else {
-			memcpy (p + n, data, len);
+			memcpy(p + n, data, len);
 			c->num += (unsigned int)len;
 			return 1;
 		}
 	}
 
-	n = len/HASH_CBLOCK;
+	n = len / MD4_CBLOCK;
 	if (n > 0) {
-		HASH_BLOCK_DATA_ORDER (c, data, n);
-		n    *= HASH_CBLOCK;
+		md4_block_data_order(c, data, n);
+		n    *= MD4_CBLOCK;
 		data += n;
 		len -= n;
 	}
@@ -141,17 +141,19 @@ HASH_UPDATE(HASH_CTX *c, const void *data_, size_t len)
 	if (len != 0) {
 		p = (unsigned char *)c->data;
 		c->num = (unsigned int)len;
-		memcpy (p, data, len);
+		memcpy(p, data, len);
 	}
 	return 1;
 }
 
-void HASH_TRANSFORM (HASH_CTX *c, const unsigned char *data)
+void
+MD4_Transform(MD4_CTX *c, const unsigned char *data)
 {
-	HASH_BLOCK_DATA_ORDER (c, data, 1);
+	md4_block_data_order(c, data, 1);
 }
 
-int HASH_FINAL (unsigned char *md, HASH_CTX *c)
+int
+MD4_Final(unsigned char *md, MD4_CTX *c)
 {
 	unsigned char *p = (unsigned char *)c->data;
 	size_t n = c->num;
@@ -159,14 +161,14 @@ int HASH_FINAL (unsigned char *md, HASH_CTX *c)
 	p[n] = 0x80; /* there is always room for one */
 	n++;
 
-	if (n > (HASH_CBLOCK - 8)) {
-		memset (p + n, 0, HASH_CBLOCK - n);
+	if (n > (MD4_CBLOCK - 8)) {
+		memset(p + n, 0, MD4_CBLOCK - n);
 		n = 0;
-		HASH_BLOCK_DATA_ORDER (c, p, 1);
+		md4_block_data_order(c, p, 1);
 	}
-	memset (p + n, 0, HASH_CBLOCK - 8 - n);
+	memset(p + n, 0, MD4_CBLOCK - 8 - n);
 
-	p += HASH_CBLOCK - 8;
+	p += MD4_CBLOCK - 8;
 #if   defined(DATA_ORDER_IS_BIG_ENDIAN)
 	HOST_l2c(c->Nh, p);
 	HOST_l2c(c->Nl, p);
@@ -174,10 +176,10 @@ int HASH_FINAL (unsigned char *md, HASH_CTX *c)
 	HOST_l2c(c->Nl, p);
 	HOST_l2c(c->Nh, p);
 #endif
-	p -= HASH_CBLOCK;
-	HASH_BLOCK_DATA_ORDER (c, p, 1);
+	p -= MD4_CBLOCK;
+	md4_block_data_order(c, p, 1);
 	c->num = 0;
-	memset (p, 0, HASH_CBLOCK);
+	memset(p, 0, MD4_CBLOCK);
 
 #ifndef HASH_MAKE_STRING
 #error "HASH_MAKE_STRING must be defined!"
