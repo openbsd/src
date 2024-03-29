42558ebfa29005f50f0ecb8c5da2ad7d0f229b13
deraadt <deraadt@openbsd.org>
2024-03-29 07:53:32 +0000

    SYS_msyscall will go away soon.  kdump does not need a special handler for it. ok tb

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kdump.c,v 1.161 2023/12/15 15:12:08 deraadt Exp $	*/
+/*	$OpenBSD: kdump.c,v 1.162 2024/03/29 07:53:32 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1988, 1993
@@ -754,7 +754,6 @@ static const formatter scargs[][8] = {
     [SYS_access]	= { Ppath, Accessmodename },
     [SYS_chflags]	= { Ppath, Chflagsname },
     [SYS_fchflags]	= { Pfd, Chflagsname },
-    [SYS_msyscall]	= { Pptr, Pbigsize },
     [SYS_stat]		= { Ppath, Pptr },
     [SYS_lstat]		= { Ppath, Pptr },
     [SYS_dup]		= { Pfd },
