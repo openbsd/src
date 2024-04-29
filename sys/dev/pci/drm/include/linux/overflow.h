4d3df29298314c53d0d7547b3696f27078ee60a7
jsg <jsg@openbsd.org>
2024-04-29 06:06:46 +0000

    add check_add_overflow() for 6.6.29 drm

--- sys/dev/pci/drm/include/linux/overflow.h
+++ sys/dev/pci/drm/include/linux/overflow.h
@@ -8,4 +8,8 @@
 #define struct_size(p, member, n) \
 	(sizeof(*(p)) + ((n) * (sizeof(*(p)->member))))
 
+#if defined(__clang__) || (defined(__GNUC__) && __GNUC__ >= 5)
+#define check_add_overflow(x, y, sum)	__builtin_add_overflow(x, y, sum)
+#endif
+
 #endif
