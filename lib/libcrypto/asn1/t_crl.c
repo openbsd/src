7d86db2fa487952b9c80bdf13f0376547756fb70
tb <tb@openbsd.org>
2024-05-02 15:33:59 +0000

    Remove a useless OBJ_obj2nid() call from X509_CRL_print()
    ok beck (as part of a larger diff)

--- lib/libcrypto/asn1/t_crl.c
+++ lib/libcrypto/asn1/t_crl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: t_crl.c,v 1.24 2024/04/09 13:55:02 beck Exp $ */
+/* $OpenBSD: t_crl.c,v 1.25 2024/05/02 15:33:59 tb Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 1999.
  */
@@ -99,7 +99,6 @@ X509_CRL_print(BIO *out, X509_CRL *x)
 	if (l < 0 || l == LONG_MAX)
 		goto err;
 	BIO_printf(out, "%8sVersion %lu (0x%lx)\n", "", l + 1, l);
-	i = OBJ_obj2nid(x->sig_alg->algorithm);
 	if (X509_signature_print(out, x->sig_alg, NULL) == 0)
 		goto err;
 	p = X509_NAME_oneline(X509_CRL_get_issuer(x), NULL, 0);
