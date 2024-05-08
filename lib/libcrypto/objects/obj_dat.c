1b9cf3a3fa614bdeeea16307bec596261c50bf67
tb <tb@openbsd.org>
2024-05-08 16:35:05 +0000

    fix line wrapping in function definition

--- lib/libcrypto/objects/obj_dat.c
+++ lib/libcrypto/objects/obj_dat.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: obj_dat.c,v 1.89 2024/03/02 11:11:11 tb Exp $ */
+/* $OpenBSD: obj_dat.c,v 1.90 2024/05/08 16:35:05 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -167,7 +167,8 @@ cleanup1_doall(ADDED_OBJ *a)
 	    ASN1_OBJECT_FLAG_DYNAMIC_DATA;
 }
 
-static void cleanup2_doall(ADDED_OBJ *a)
+static void
+cleanup2_doall(ADDED_OBJ *a)
 {
 	a->obj->nid++;
 }
