8de813b20c11b72ef1082de25ecbd3a6ff157b5b
jsg <jsg@openbsd.org>
2024-05-10 03:50:12 +0000

    make pf_match_rule() prototype match the function

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.1194 2024/04/12 16:07:09 bluhm Exp $ */
+/*	$OpenBSD: pf.c,v 1.1195 2024/05/10 03:50:12 jsg Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -239,7 +239,7 @@ int			 pf_src_connlimit(struct pf_state **);
 int			 pf_match_rcvif(struct mbuf *, struct pf_rule *);
 int			 pf_step_into_anchor(struct pf_test_ctx *,
 			    struct pf_rule *);
-int			 pf_match_rule(struct pf_test_ctx *,
+enum pf_test_status	 pf_match_rule(struct pf_test_ctx *,
 			    struct pf_ruleset *);
 void			 pf_counters_inc(int, struct pf_pdesc *,
 			    struct pf_state *, struct pf_rule *,
