aeef08fb4ade59f068e33c61f1c1597cd7189446
jsg <jsg@openbsd.org>
2024-05-12 09:27:13 +0000

    remove uvm_swprekeyprint extern for var we never had

--- sys/uvm/uvm_swap_encrypt.h
+++ sys/uvm/uvm_swap_encrypt.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_swap_encrypt.h,v 1.10 2015/05/06 04:00:10 dlg Exp $	*/
+/*	$OpenBSD: uvm_swap_encrypt.h,v 1.11 2024/05/12 09:27:13 jsg Exp $	*/
 
 /*
  * Copyright 1999 Niels Provos <provos@citi.umich.edu>
@@ -79,7 +79,6 @@ void swap_key_create(struct swap_key *);
 void swap_key_delete(struct swap_key *);
 
 extern int uvm_doswapencrypt;		/* swapencrypt enabled/disabled */
-extern int uvm_swprekeyprint;
 extern u_int uvm_swpkeyexpire;		/* expiry time for keys (tR) */
 extern int swap_encrypt_initialized;
 
