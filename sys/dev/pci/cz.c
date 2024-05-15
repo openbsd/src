35659bf2453d7eeb12fd24089cf668ca13361c18
jsg <jsg@openbsd.org>
2024-05-15 12:57:36 +0000

    ansi style function decl

--- sys/dev/pci/cz.c
+++ sys/dev/pci/cz.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cz.c,v 1.27 2022/03/11 18:00:45 mpi Exp $ */
+/*	$OpenBSD: cz.c,v 1.28 2024/05/15 12:57:36 jsg Exp $ */
 /*	$NetBSD: cz.c,v 1.15 2001/01/20 19:10:36 thorpej Exp $	*/
 
 /*-
@@ -268,9 +268,7 @@ do {									\
  *	Determine if the given PCI device is a Cyclades-Z board.
  */
 int
-cz_match(parent, match, aux)
-	struct device *parent;
-	void *match, *aux;
+cz_match(struct device *parent, void *match, void *aux)
 {
 	struct pci_attach_args *pa = aux;
 
@@ -286,9 +284,7 @@ cz_match(parent, match, aux)
  *	A Cyclades-Z board was found; attach it.
  */
 void
-cz_attach(parent, self, aux)
-	struct device *parent, *self;
-	void *aux;
+cz_attach(struct device *parent, struct device *self, void *aux)
 {
 	struct cz_softc *cz = (void *) self;
 	struct pci_attach_args *pa = aux;
