d487c30473fe81d838da8d6f183bcfc53bd8dba2
deraadt <deraadt@openbsd.org>
2024-06-02 15:55:36 +0000

    Due to growth, crunchgen must also try to block -fret-clean, so also use COPTS+=-fno-ret-clean

--- usr.sbin/crunchgen/crunchgen.c
+++ usr.sbin/crunchgen/crunchgen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: crunchgen.c,v 1.27 2023/09/14 16:39:00 jca Exp $	 */
+/* $OpenBSD: crunchgen.c,v 1.28 2024/06/02 15:55:36 deraadt Exp $	 */
 
 /*
  * Copyright (c) 1994 University of Maryland
@@ -897,6 +897,7 @@ top_makefile_rules(FILE * outmk)
 	fprintf(outmk, "CFLAGS+=-fno-unwind-tables\n");
 	fprintf(outmk, ".if ${MACHINE} == \"amd64\"\n");
 	fprintf(outmk, "CFLAGS+=-fcf-protection=none\n");
+	fprintf(outmk, "CFLAGS+=-no-ret-clean\n");
 	fprintf(outmk, ".endif\n");
 	fprintf(outmk, ".if ${MACHINE} == \"arm64\"\n");
 	fprintf(outmk, "CFLAGS+=-mbranch-protection=none\n");
