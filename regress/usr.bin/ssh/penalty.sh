c64f50f7f80a81661e1389867e936db6d70f638b
djm <djm@openbsd.org>
2024-06-16 08:18:06 +0000

    penalty test is still a bit racy

--- regress/usr.bin/ssh/penalty.sh
+++ regress/usr.bin/ssh/penalty.sh
@@ -25,14 +25,17 @@ verbose "penalty for authentication failure"
 cat /dev/null > $OBJ/authorized_keys_${USER}
 ${SSH} -F $OBJ/ssh_config somehost true && fatal "noauth connect succeeded"
 cp $OBJ/authorized_keys_${USER}.bak $OBJ/authorized_keys_${USER}
+sleep 2
 
 # Should be below penalty threshold
 ${SSH} -F $OBJ/ssh_config somehost true || fatal "authfail not expired"
+sleep 2
 
 # Fail authentication again; penalty should activate
 cat /dev/null > $OBJ/authorized_keys_${USER}
 ${SSH} -F $OBJ/ssh_config somehost true && fatal "noauth connect succeeded"
 cp $OBJ/authorized_keys_${USER}.bak $OBJ/authorized_keys_${USER}
+sleep 2
 
 # These should be refused by the active penalty
 ${SSH} -F $OBJ/ssh_config somehost true && fail "authfail not rejected"
@@ -42,6 +45,7 @@ conf "noauth:100s"
 ${SSH} -F $OBJ/ssh_config somehost true || fatal "basic connect failed"
 verbose "penalty for no authentication"
 ${SSHKEYSCAN} -t ssh-ed25519 -p $PORT 127.0.0.1 >/dev/null || fatal "keyscan failed"
+sleep 2
 
 # Repeat attempt should be penalised
 ${SSHKEYSCAN} -t ssh-ed25519 -p $PORT 127.0.0.1 >/dev/null 2>&1 && fail "keyscan not rejected"
