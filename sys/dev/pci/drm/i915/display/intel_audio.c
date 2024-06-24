f6d48bfef753f50ff588f15b25ad7fce03be0434
jsg <jsg@openbsd.org>
2024-06-24 03:57:38 +0000

    drm/i915: Fix audio component initialization
    From Imre Deak
    f287b1e34f1dd910723ca720300548c27a9a72d7 in linux-6.6.y/6.6.35
    75800e2e4203ea83bbc9d4f63ad97ea582244a08 in mainline linux

--- sys/dev/pci/drm/i915/display/intel_audio.c
+++ sys/dev/pci/drm/i915/display/intel_audio.c
@@ -1256,17 +1256,6 @@ static void i915_audio_component_init(struct drm_i915_private *i915)
 {
 #ifdef notyet
 	u32 aud_freq, aud_freq_init;
-	int ret;
-
-	ret = component_add_typed(i915->drm.dev,
-				  &i915_audio_component_bind_ops,
-				  I915_COMPONENT_AUDIO);
-	if (ret < 0) {
-		drm_err(&i915->drm,
-			"failed to add audio component (%d)\n", ret);
-		/* continue with reduced functionality */
-		return;
-	}
 
 	if (DISPLAY_VER(i915) >= 9) {
 		aud_freq_init = intel_de_read(i915, AUD_FREQ_CNTRL);
@@ -1289,6 +1278,23 @@ static void i915_audio_component_init(struct drm_i915_private *i915)
 
 	/* init with current cdclk */
 	intel_audio_cdclk_change_post(i915);
+#endif
+}
+
+static void i915_audio_component_register(struct drm_i915_private *i915)
+{
+#ifdef notyet
+	int ret;
+
+	ret = component_add_typed(i915->drm.dev,
+				  &i915_audio_component_bind_ops,
+				  I915_COMPONENT_AUDIO);
+	if (ret < 0) {
+		drm_err(&i915->drm,
+			"failed to add audio component (%d)\n", ret);
+		/* continue with reduced functionality */
+		return;
+	}
 
 	i915->display.audio.component_registered = true;
 #endif
@@ -1320,6 +1326,12 @@ void intel_audio_init(struct drm_i915_private *i915)
 {
 	if (intel_lpe_audio_init(i915) < 0)
 		i915_audio_component_init(i915);
+}
+
+void intel_audio_register(struct drm_i915_private *i915)
+{
+	if (!i915->display.audio.lpe.platdev)
+		i915_audio_component_register(i915);
 }
 
 /**
