b88523ef2045b321520b6ae391d91fec3af962a2
djm <djm@openbsd.org>
2024-07-01 04:31:59 +0000

    openssh-9.8

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.101 2024/03/11 04:59:47 djm Exp $ */
+/* $OpenBSD: version.h,v 1.102 2024/07/01 04:31:59 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_9.7"
+#define SSH_VERSION	"OpenSSH_9.8"
