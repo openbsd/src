7407cf33afc84799fbb73196c5794f6384dca795
jsg <jsg@openbsd.org>
2024-07-02 04:42:43 +0000

    remove oga's copyright notice, none of those changes remain

--- sys/arch/i386/pci/agp_machdep.c
+++ sys/arch/i386/pci/agp_machdep.c
@@ -1,20 +1,5 @@
-/*	$OpenBSD: agp_machdep.c,v 1.23 2024/07/02 04:29:01 jsg Exp $	*/
+/*	$OpenBSD: agp_machdep.c,v 1.24 2024/07/02 04:42:43 jsg Exp $	*/
 
-/*
- * Copyright (c) 2008 - 2009 Owain G. Ainsworth <oga@openbsd.org>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 /*
  * Copyright (c) 2002 Michael Shalayeff
  * All rights reserved.
