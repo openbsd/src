32d9991986601f4a5abadcf9e260885cd21c5f9b
tb <tb@openbsd.org>
2024-07-09 16:30:54 +0000

    OPENSSL_free() -> free()

--- lib/libcrypto/kdf/tls1_prf.c
+++ lib/libcrypto/kdf/tls1_prf.c
@@ -51,7 +51,7 @@ static void pkey_tls1_prf_cleanup(EVP_PKEY_CTX *ctx)
     TLS1_PRF_PKEY_CTX *kctx = ctx->data;
     freezero(kctx->sec, kctx->seclen);
     explicit_bzero(kctx->seed, kctx->seedlen);
-    OPENSSL_free(kctx);
+    free(kctx);
 }
 
 static int pkey_tls1_prf_ctrl(EVP_PKEY_CTX *ctx, int type, int p1, void *p2)
