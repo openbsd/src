de9520ece7231f124544220ac8fb819bb56aa877
krw <krw@openbsd.org>
2024-07-10 09:12:11 +0000

    Sweep up another softdep crumb.
    Remove #if notyet/#endif chunk that references the never-defined STATFS_SOFTUPD.
    
    ok jsg@

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_syscalls.c,v 1.365 2024/05/18 05:20:22 guenther Exp $	*/
+/*	$OpenBSD: vfs_syscalls.c,v 1.366 2024/07/10 09:12:11 krw Exp $	*/
 /*	$NetBSD: vfs_syscalls.c,v 1.71 1996/04/23 10:29:02 mycroft Exp $	*/
 
 /*
@@ -697,10 +697,6 @@ sys_getfsstat(struct proc *p, void *v, register_t *retval)
 			}
 
 			sp->f_flags = mp->mnt_flag & MNT_VISFLAGMASK;
-#if notyet
-			if (mp->mnt_flag & MNT_SOFTDEP)
-				sp->f_eflags = STATFS_SOFTUPD;
-#endif
 			error = (copyout_statfs(sp, sfsp, p));
 			if (error) {
 				vfs_unbusy(mp);
