38b547d006ac13fb766c04113ad553880264b31a
tb <tb@openbsd.org>
2024-07-14 06:34:04 +0000

    Unbreak tree by defining log_getverbose() for SMALL builds
    ok anton florian

--- sbin/slaacd/log.h
+++ sbin/slaacd/log.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: log.h,v 1.3 2021/12/13 18:28:40 deraadt Exp $ */
+/*	$OpenBSD: log.h,v 1.4 2024/07/14 06:34:04 tb Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -46,6 +46,7 @@ __dead void fatalx(const char *, ...)
 #define log_init(x...)		do {} while(0)
 #define log_procinit(x...)	do {} while(0)
 #define log_setverbose(x...)	do {} while(0)
+#define log_getverbose()	0
 #define log_warn(x...)		do {} while(0)
 #define log_warnx(x...)		do {} while(0)
 #define log_info(x...)		do {} while(0)
