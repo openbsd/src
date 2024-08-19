421bf2dd52f9ccf1a5f7ebc8a7f452ce5e8a6d3d
deraadt <deraadt@openbsd.org>
2024-08-19 01:43:23 +0000

    move ed/tests files to regress/bin/ed, where they are used

--- regress/bin/ed/mkscripts.sh
+++ regress/bin/ed/mkscripts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -
-#	$OpenBSD: mkscripts.sh,v 1.2 1996/06/23 14:20:08 deraadt Exp $
+#	$OpenBSD: mkscripts.sh,v 1.1 2024/08/19 01:43:23 deraadt Exp $
 #	$NetBSD: mkscripts.sh,v 1.10 1995/04/23 10:07:36 cgd Exp $
 #
 # This script generates ed test scripts (.ed) from .t files
