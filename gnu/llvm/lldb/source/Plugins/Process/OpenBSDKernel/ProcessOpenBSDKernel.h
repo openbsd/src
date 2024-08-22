6e92fa473ce316b354d85f42a55432c67258f814
asou <asou@openbsd.org>
2024-08-22 08:22:13 +0000

    lldb: shut up the warning message on quit debugging kernel core file.
    The diff from Yuichiro NAITO.
    
    ok yasuoka

--- gnu/llvm/lldb/source/Plugins/Process/OpenBSDKernel/ProcessOpenBSDKernel.h
+++ gnu/llvm/lldb/source/Plugins/Process/OpenBSDKernel/ProcessOpenBSDKernel.h
@@ -43,6 +43,9 @@ public:
 
   lldb_private::DynamicLoader *GetDynamicLoader() override;
 
+  // Process Queries
+  bool WarnBeforeDetach() const override { return false; }
+
 protected:
   bool DoUpdateThreadList(lldb_private::ThreadList &old_thread_list,
 			  lldb_private::ThreadList &new_thread_list) override;
