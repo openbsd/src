43a6b7cb41659f6d4cf49620f1d154ce156d8ffa
tb <tb@openbsd.org>
2024-09-04 04:34:14 +0000

    Add regress against OpenSSL 3.2
    OpenSSL 1.1 is dead. This directory will use the default version in ports
    (currently 3.2) for regress testing.

---
+++ regress/usr.sbin/rpki-client/openssl/unistd.h
@@ -0,0 +1,28 @@
+/*
+ * Public domain
+ * compatibility shim for OpenSSL 3
+ * overloading unistd.h is a ugly guly hack for this issue but works here
+ */
+
+#include_next <unistd.h>
+
+#include <openssl/cms.h>
+#include <openssl/stack.h>
+
+#ifndef DECLARE_STACK_OF
+#define DECLARE_STACK_OF DEFINE_STACK_OF
+#endif
+
+static inline int
+CMS_get_version(CMS_ContentInfo *cms, long *version)
+{
+	*version = 3;
+	return 1;
+}
+
+static inline int
+CMS_SignerInfo_get_version(CMS_SignerInfo *si, long *version)
+{
+	*version = 3;
+	return 1;
+}
