ad37d21be2965392f092995ca1ef57693141d8d9
deraadt <deraadt@openbsd.org>
2024-09-23 20:50:47 +0000

    now hacking on 7.6-current

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.205 2024/09/17 13:39:17 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.206 2024/09/23 20:50:47 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -72,9 +72,9 @@ osr="7.6"
 
 cat >vers.c <<eof
 #define STATUS ""			/* release */
+#define STATUS "-current"		/* just after a release */
 #if 0
 #define STATUS "-beta"			/* just before a release */
-#define STATUS "-current"		/* just after a release */
 #define STATUS "-stable"		/* stable branch */
 #endif
 
