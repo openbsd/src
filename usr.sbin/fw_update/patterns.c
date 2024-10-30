4cfb78b57c0c7511ccb748bd7c990a86c201e3fd
bluhm <bluhm@openbsd.org>
2024-10-30 00:04:46 +0000

    Add fw_update pattern for AMD SEV firmware used with psp(4).
    from hshoexer@; input deraadt@

--- usr.sbin/fw_update/patterns.c
+++ usr.sbin/fw_update/patterns.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patterns.c,v 1.15 2024/10/13 00:00:41 deraadt Exp $	*/
+/*	$OpenBSD: patterns.c,v 1.16 2024/10/30 00:04:46 bluhm Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Christopher G. Demetriou.  All rights reserved.
@@ -94,6 +94,10 @@ main(void)
 	print_devices("amdgpu", amdgpu_devices, nitems(amdgpu_devices));
 	printf("%s\n", "amdgpu ^vga*vendor \"ATI\", unknown product"); 
 	printf("%s\n", "amdgpu ^vendor \"ATI\", unknown product*class display");
+	printf("%s\n", "amdsev ^\"AMD*Crypto\"");
+	printf("%s\n", "amdsev ^\"AMD*PSP\"");
+	printf("%s\n", "amdsev ccp");
+	printf("%s\n", "amdsev psp");
 	printf("%s\n", "apple-boot ^cpu0*Apple");
 	printf("%s\n", "arm64-qcom-dtb ^qcgpio0");
 	printf("%s\n", "athn");
