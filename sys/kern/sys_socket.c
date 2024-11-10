55a277b37f5b617980b4a6d672383e7c0fb66db1
bluhm <bluhm@openbsd.org>
2024-11-10 13:03:22 +0000

    Lock send socket buffer for fstat syscall.
    In soo_stat() lock send socket buffer mutex around access to sb_state.
    Althoug not strictly necessary as this is only a single read access,
    make clear where parallel variable access happens.  As this is not
    performance critical, use mutex instead of read once wrapper.  This
    is also consistent to the receive socket buffer a few lines above.
    
    OK mvs@

--- sys/kern/sys_socket.c
+++ sys/kern/sys_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_socket.c,v 1.65 2024/04/30 17:59:15 mvs Exp $	*/
+/*	$OpenBSD: sys_socket.c,v 1.66 2024/11/10 13:03:22 bluhm Exp $	*/
 /*	$NetBSD: sys_socket.c,v 1.13 1995/08/12 23:59:09 mycroft Exp $	*/
 
 /*
@@ -153,8 +153,10 @@ soo_stat(struct file *fp, struct stat *ub, struct proc *p)
 	    so->so_rcv.sb_cc != 0)
 		ub->st_mode |= S_IRUSR | S_IRGRP | S_IROTH;
 	mtx_leave(&so->so_rcv.sb_mtx);
+	mtx_enter(&so->so_snd.sb_mtx);
 	if ((so->so_snd.sb_state & SS_CANTSENDMORE) == 0)
 		ub->st_mode |= S_IWUSR | S_IWGRP | S_IWOTH;
+	mtx_leave(&so->so_snd.sb_mtx);
 	ub->st_uid = so->so_euid;
 	ub->st_gid = so->so_egid;
 	(void)pru_sense(so, ub);
