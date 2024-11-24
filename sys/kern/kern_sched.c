35bbce866312cda36088b371394842f8af281d5b
claudio <claudio@openbsd.org>
2024-11-24 13:05:14 +0000

    Add KASSERT on P_WSLEEP in setrunqueue() and sched_chooseproc().
    P_WSLEEP indicates that the thread is still on a CPU executing and
    has not yet mi_switched away to sleep. So it is a bug to make such
    a thread runnable or even worse try to switch to it.
    OK mpi@

--- sys/kern/kern_sched.c
+++ sys/kern/kern_sched.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sched.c,v 1.102 2024/10/08 11:57:59 claudio Exp $	*/
+/*	$OpenBSD: kern_sched.c,v 1.103 2024/11/24 13:05:14 claudio Exp $	*/
 /*
  * Copyright (c) 2007, 2008 Artur Grabowski <art@openbsd.org>
  *
@@ -277,6 +277,7 @@ setrunqueue(struct cpu_info *ci, struct proc *p, uint8_t prio)
 	KASSERT(ci != NULL);
 	SCHED_ASSERT_LOCKED();
 	KASSERT(p->p_wchan == NULL);
+	KASSERT(!ISSET(p->p_flag, P_WSLEEP));
 
 	p->p_cpu = ci;
 	p->p_stat = SRUN;
@@ -367,6 +368,7 @@ again:
 	} 
 
 	KASSERT(p->p_wchan == NULL);
+	KASSERT(!ISSET(p->p_flag, P_WSLEEP));
 	return (p);
 }
 
