c05259c4088db71d23b5e09af888e53ee0739cfe
dtucker <dtucker@openbsd.org>
2024-12-06 06:55:28 +0000

    Change "login again" to "log in again" in password change message.  From ThinLinc-Zeijlon via github PR#532.

--- usr.bin/ssh/session.c
+++ usr.bin/ssh/session.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: session.c,v 1.339 2024/10/14 01:57:50 djm Exp $ */
+/* $OpenBSD: session.c,v 1.340 2024/12/06 06:55:28 dtucker Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -1155,7 +1155,7 @@ do_pwchange(Session *s)
 	fprintf(stderr, "WARNING: Your password has expired.\n");
 	if (s->ttyfd != -1) {
 		fprintf(stderr,
-		    "You must change your password now and login again!\n");
+		    "You must change your password now and log in again!\n");
 		execl(_PATH_PASSWD_PROG, "passwd", (char *)NULL);
 		perror("passwd");
 	} else {
