86da87d93ea159b56c320c6d41df546cf6abe214
claudio <claudio@openbsd.org>
2024-12-16 16:14:46 +0000

    Bump version

--- usr.sbin/bgpd/version.h
+++ usr.sbin/bgpd/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.17 2024/09/05 20:28:42 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.18 2024/12/16 16:14:46 claudio Exp $ */
 
-#define BGPD_VERSION	"8.6"
+#define BGPD_VERSION	"8.7"
