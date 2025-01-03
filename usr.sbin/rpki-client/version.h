1675f48f1fdb8ed86ec0ed78a84c0d8095fa5cc0
job <job@openbsd.org>
2025-01-03 10:43:36 +0000

    Prepare to release rpki-client 9.4
    OK tb@

--- usr.sbin/rpki-client/version.h
+++ usr.sbin/rpki-client/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.25 2024/09/19 13:45:07 job Exp $ */
+/* $OpenBSD: version.h,v 1.26 2025/01/03 10:43:36 job Exp $ */
 
-#define RPKI_VERSION	"9.3"
+#define RPKI_VERSION	"9.4"
