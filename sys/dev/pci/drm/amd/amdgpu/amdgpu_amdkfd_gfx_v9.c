a664c55f825c7ebe1e18eda651dd96d80f8f9189
jsg <jsg@openbsd.org>
2025-02-10 01:44:13 +0000

    drm/amdgpu: fix gpu recovery disable with per queue reset
    From Jonathan Kim
    d9cc477e77c622f3b595c62b1f37563c7d641214 in linux-6.12.y/6.12.13
    86bde64cb7957be393f84e5d35fb8dfc91e4ae7e in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v9.c
+++ sys/dev/pci/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v9.c
@@ -1133,6 +1133,9 @@ uint64_t kgd_gfx_v9_hqd_get_pq_addr(struct amdgpu_device *adev,
 	uint32_t low, high;
 	uint64_t queue_addr = 0;
 
+	if (!amdgpu_gpu_recovery)
+		return 0;
+
 	kgd_gfx_v9_acquire_queue(adev, pipe_id, queue_id, inst);
 	amdgpu_gfx_rlc_enter_safe_mode(adev, inst);
 
@@ -1180,6 +1183,9 @@ uint64_t kgd_gfx_v9_hqd_reset(struct amdgpu_device *adev,
 {
 	uint32_t low, high, pipe_reset_data = 0;
 	uint64_t queue_addr = 0;
+
+	if (!amdgpu_gpu_recovery)
+		return 0;
 
 	kgd_gfx_v9_acquire_queue(adev, pipe_id, queue_id, inst);
 	amdgpu_gfx_rlc_enter_safe_mode(adev, inst);
