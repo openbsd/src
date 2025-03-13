4ae46dfd37dc636913edeca30705391d97f17ee1
jsg <jsg@openbsd.org>
2025-03-13 10:48:36 +0000

    don't error on explicit fence in drm_gem_plane_helper_prepare_fb()
    mglocker@ encountered this.  With a screen sometimes staying black when
    coming back from screen saver idle.

--- sys/dev/pci/drm/drm_gem_atomic_helper.c
+++ sys/dev/pci/drm/drm_gem_atomic_helper.c
@@ -20,14 +20,13 @@ drm_gem_plane_helper_prepare_fb(struct drm_plane *dp,
 		obj = dps->fb->obj[0];
 		if (obj == NULL)
 			return -EINVAL;
-		if (dps->fence) {
-			printf("%s: explicit fence not handled\n", __func__);
-			return -EINVAL;
-		}
 		r = dma_resv_get_singleton(obj->resv, DMA_RESV_USAGE_WRITE, &f);
 		if (r)
 			return r;
-		dps->fence = f;
+		if (dps->fence)
+			dma_fence_put(f);
+		else
+			dps->fence = f;
 	}
 
 	return 0;
