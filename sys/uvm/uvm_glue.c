818e6b57296330608942515e50567df4db757576
mpi <mpi@openbsd.org>
2025-03-21 13:19:33 +0000

    Make vslock(9) similar to mlock(2): silently ignore non-mapped ranges.
    Prevents panics triggerable by multi-threaded processes where a thread A
    munmap(2)s a page being used and wired, by sysctl(2) executed in thread B.
    
    This turns vslock(9) into a best-effort mechanism similar to doing
    mlock(2) on sysctl(2)'s arguments.
    
    Note that with this change, thread B might still fault during its copyout(9)
    if a sibling thread does munmap(2) or munlock(2) multiple times.
    
    Tested by and ok bluhm@, ok kettenis@
    
    Reported-by: syzbot+e8310909e2910c9cca08@syzkaller.appspotmail.com
    Reported-by: syzbot+d9b926edfd5f64a66c58@syzkaller.appspotmail.com
    Reported-by: syzbot+a12540517e3a76a6a490@syzkaller.appspotmail.com

--- sys/uvm/uvm_glue.c
+++ sys/uvm/uvm_glue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_glue.c,v 1.87 2024/10/28 08:25:32 mpi Exp $	*/
+/*	$OpenBSD: uvm_glue.c,v 1.88 2025/03/21 13:19:33 mpi Exp $	*/
 /*	$NetBSD: uvm_glue.c,v 1.44 2001/02/06 19:54:44 eeh Exp $	*/
 
 /* 
@@ -114,7 +114,7 @@ uvm_vslock(struct proc *p, caddr_t addr, size_t len, vm_prot_t access_type)
 	if (end <= start)
 		return (EINVAL);
 
-	return uvm_fault_wire(map, start, end, access_type);
+	return uvm_map_pageable(map, start, end, FALSE, 0);
 }
 
 /*
@@ -125,13 +125,14 @@ uvm_vslock(struct proc *p, caddr_t addr, size_t len, vm_prot_t access_type)
 void
 uvm_vsunlock(struct proc *p, caddr_t addr, size_t len)
 {
+	struct vm_map *map = &p->p_vmspace->vm_map;
 	vaddr_t start, end;
 
 	start = trunc_page((vaddr_t)addr);
 	end = round_page((vaddr_t)addr + len);
 	KASSERT(end > start);
 
-	uvm_fault_unwire(&p->p_vmspace->vm_map, start, end);
+	uvm_map_pageable(map, start, end, TRUE, 0);
 }
 
 /*
