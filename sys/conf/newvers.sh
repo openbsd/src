db0194835ccc17f119e47a372658b213d3ad6a97
deraadt <deraadt@openbsd.org>
2025-03-30 20:43:36 +0000

    head out of -beta to 7.7

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.208 2025/03/01 19:44:07 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.209 2025/03/30 20:43:36 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,10 +71,10 @@ ost="OpenBSD"
 osr="7.7"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
-#define STATUS "-current"		/* just after a release */
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
+#define STATUS "-current"		/* just after a release */
 #define STATUS "-stable"		/* stable branch */
 #endif
 
