60e47db49e73620f35c1693a436f5ef378d1b821
claudio <claudio@openbsd.org>
2025-04-01 11:36:40 +0000

    Run route get with -n to avoid DNS lookups.

--- regress/usr.sbin/bgpd/integrationtests/l3vpn.sh
+++ regress/usr.sbin/bgpd/integrationtests/l3vpn.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: l3vpn.sh,v 1.6 2024/10/28 12:11:05 claudio Exp $
+#	$OpenBSD: l3vpn.sh,v 1.7 2025/04/01 11:36:40 claudio Exp $
 
 set -e
 
@@ -99,24 +99,24 @@ route -T ${RDOMAIN1} exec bgpctl show
 route -T ${RDOMAIN1} exec bgpctl show rib
 route -T ${RDOMAIN1} exec bgpctl show fib table 13
 route -T ${RDOMAIN3} show
-route -T ${RDOMAIN3} get 192.168.44/24 > /dev/null
-route -T ${RDOMAIN4} get 192.168.42/24 > /dev/null
-route -T ${RDOMAIN3} get -inet6 2001:db8:42:44::/64 > /dev/null
-route -T ${RDOMAIN4} get -inet6 2001:db8:42:42::/64 > /dev/null
+route -T ${RDOMAIN3} -n get 192.168.44/24 > /dev/null
+route -T ${RDOMAIN4} -n get 192.168.42/24 > /dev/null
+route -T ${RDOMAIN3} -n get -inet6 2001:db8:42:44::/64 > /dev/null
+route -T ${RDOMAIN4} -n get -inet6 2001:db8:42:42::/64 > /dev/null
 
 echo Add new network
 route -T ${RDOMAIN2} exec bgpctl network add 192.168.45.0/24 rd 4200000002:14
 route -T ${RDOMAIN2} exec bgpctl network add 2001:db8:42:45::/64 rd 4200000002:14
 sleep 1
-route -T ${RDOMAIN3} get 192.168.45/24 > /dev/null
-route -T ${RDOMAIN3} get -inet6 2001:db8:42:45::/64 > /dev/null
+route -T ${RDOMAIN3} -n get 192.168.45/24 > /dev/null
+route -T ${RDOMAIN3} -n get -inet6 2001:db8:42:45::/64 > /dev/null
 
 echo Remove new network
 route -T ${RDOMAIN2} exec bgpctl network del 192.168.45.0/24 rd 4200000002:14
 route -T ${RDOMAIN2} exec bgpctl network del 2001:db8:42:45::/64 rd 4200000002:14
 sleep 1
 route -T ${RDOMAIN1} exec bgpctl show rib
-! route -T ${RDOMAIN3} get 192.168.45/24 > /dev/null
-! route -T ${RDOMAIN3} get -inet6 2001:db8:42:45::/64 > /dev/null
+! route -T ${RDOMAIN3} -n get 192.168.45/24 > /dev/null
+! route -T ${RDOMAIN3} -n get -inet6 2001:db8:42:45::/64 > /dev/null
 
 exit 0
