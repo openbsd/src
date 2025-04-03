235d2486e7633e1102c02a00754d8bdd6a69c9b6
tb <tb@openbsd.org>
2025-04-03 14:30:17 +0000

    rpki-client: bump version
    discussed with claudio

--- usr.sbin/rpki-client/version.h
+++ usr.sbin/rpki-client/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.26 2025/01/03 10:43:36 job Exp $ */
+/* $OpenBSD: version.h,v 1.27 2025/04/03 14:30:17 tb Exp $ */
 
-#define RPKI_VERSION	"9.4"
+#define RPKI_VERSION	"9.5"
