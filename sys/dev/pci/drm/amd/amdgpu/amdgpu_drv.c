9ecdddf007a710f429c0cb0f1527d6bb4dabb5a5
jsg <jsg@openbsd.org>
2025-04-14 09:09:07 +0000

    drm/amd: Keep display off while going into S4
    From Mario Limonciello
    cf1b90486748fc7251794074aeacb779ac9befb2 in linux-6.12.y/6.12.23
    4afacc9948e1f8fdbca401d259ae65ad93d298c0 in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/amdgpu_drv.c
+++ sys/dev/pci/drm/amd/amdgpu/amdgpu_drv.c
@@ -2638,7 +2638,6 @@ static int amdgpu_pmops_freeze(struct device *dev)
 
 	adev->in_s4 = true;
 	r = amdgpu_device_suspend(drm_dev, true);
-	adev->in_s4 = false;
 	if (r)
 		return r;
 
@@ -2652,8 +2651,13 @@ static int amdgpu_pmops_freeze(struct device *dev)
 static int amdgpu_pmops_thaw(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
+	struct amdgpu_device *adev = drm_to_adev(drm_dev);
+	int r;
 
-	return amdgpu_device_resume(drm_dev, true);
+	r = amdgpu_device_resume(drm_dev, true);
+	adev->in_s4 = false;
+
+	return r;
 }
 
 static int amdgpu_pmops_poweroff(struct device *dev)
@@ -2668,6 +2672,9 @@ static int amdgpu_pmops_poweroff(struct device *dev)
 static int amdgpu_pmops_restore(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
+	struct amdgpu_device *adev = drm_to_adev(drm_dev);
+
+	adev->in_s4 = false;
 
 	return amdgpu_device_resume(drm_dev, true);
 }
