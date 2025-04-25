1fc6364f0469ef2b7d33e7cad6ea60f67398533c
nicm <nicm@openbsd.org>
2025-04-25 12:25:32 +0000

    Revert change to handle command parsing failures in the client because it breaks aliases.

--- usr.bin/tmux/client.c
+++ usr.bin/tmux/client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: client.c,v 1.164 2025/02/26 07:47:46 nicm Exp $ */
+/* $OpenBSD: client.c,v 1.165 2025/04/25 12:25:32 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -267,13 +267,8 @@ client_main(struct event_base *base, int argc, char **argv, uint64_t flags,
 			if (cmd_list_any_have(pr->cmdlist, CMD_STARTSERVER))
 				flags |= CLIENT_STARTSERVER;
 			cmd_list_free(pr->cmdlist);
-		} else {
-			fprintf(stderr, "%s\n", pr->error);
-			args_free_values(values, argc);
-			free(values);
+		} else
 			free(pr->error);
-			return 1;
-		}
 		args_free_values(values, argc);
 		free(values);
 	}
