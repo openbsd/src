8f7105497afc31a1bdf10c8f7ac4d09687280101
tb <tb@openbsd.org>
2025-05-03 15:09:38 +0000

    import pkgconf 2.4.3
    Our homegrown Perl-based pkg-config cannot cope with the giant DAGs
    arising in modern software, especially from the abseil-cpp and protobuf
    family. Waiting minutes for configure to complete in some ports is just
    awful.
    
    Thus we're switching to the sanely-licenced, widely used pkgconf, which                          is actively maintained, written in a sensible dialect of C, and does not                         suffer from these performance issues.
    
    Work that should happen in tree during this cycle:
    * see what we want to do with our old manual and pkgconf's
    * add pledge and unveil.
    
    Initial work done by espie during or right after p2k23, support from many.
    
    ok semarie

---
+++ usr.bin/pkgconf/libpkgconf/config.h
@@ -0,0 +1,8 @@
+#define PACKAGE_NAME "pkgconf-lite"
+#define PACKAGE_BUGREPORT "https://git.dereferenced.org/pkgconf/pkgconf/issues"
+#define PACKAGE_VERSION "2.4.3"
+#define PACKAGE PACKAGE_NAME " " PACKAGE_VERSION
+#define HAVE_DECL_STRLCPY 1
+#define HAVE_STRLCAT
+#define HAVE_DECL_STRLCAT 1
+#define HAVE_DECL_STRNDUP 1
