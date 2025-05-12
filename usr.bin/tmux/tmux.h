072ecc2dce640e0f6d15bfdbc2f19540a8d245ff
nicm <nicm@openbsd.org>
2025-05-12 10:34:13 +0000

    Expand prompts when they are used rather than ahead of time, so the input can be used as part of the format.

--- usr.bin/tmux/tmux.h
+++ usr.bin/tmux/tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.1260 2025/05/12 10:16:42 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.1261 2025/05/12 10:34:13 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1990,6 +1990,7 @@ struct client {
 	struct event		 message_timer;
 
 	char			*prompt_string;
+	struct format_tree	*prompt_formats;
 	struct utf8_data	*prompt_buffer;
 	char			*prompt_last;
 	size_t			 prompt_index;
