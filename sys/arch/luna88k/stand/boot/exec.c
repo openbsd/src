a11cbf06623e6189c81d2890860e2ab16d45c5b9
aoyama <aoyama@openbsd.org>
2025-05-17 23:25:14 +0000

    Need type casts to print the values correctly.

--- sys/arch/luna88k/stand/boot/exec.c
+++ sys/arch/luna88k/stand/boot/exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.c,v 1.1 2023/01/10 17:10:57 miod Exp $	*/
+/*	$OpenBSD: exec.c,v 1.2 2025/05/17 23:25:14 aoyama Exp $	*/
 /*	$NetBSD: boot.c,v 1.3 2013/03/05 15:34:53 tsutsui Exp $	*/
 
 /*
@@ -89,9 +89,9 @@ void
 run_loadfile(uint64_t *marks, int howto)
 {
 #ifdef DEBUG
-	printf("entry = 0x%lx\n", marks[MARK_ENTRY]);
-	printf("ssym  = 0x%lx\n", marks[MARK_SYM]);
-	printf("esym  = 0x%lx\n", marks[MARK_END]);
+	printf("entry = 0x%lx\n", (uint32_t)marks[MARK_ENTRY]);
+	printf("ssym  = 0x%lx\n", (uint32_t)marks[MARK_SYM]);
+	printf("esym  = 0x%lx\n", (uint32_t)marks[MARK_END]);
 #endif
 
 	cpu_bootarg1 = BOOT_MAGIC;
