b7c26f81876a7ed9926fbdda2948486b360fd9c6
jsing <jsing@openbsd.org>
2025-05-19 03:55:09 +0000

    Simplify EVP AES code for OFB.
    Provide AES-NI with its own aesni_ofb_cipher() and switch aes_ofb_cipher()
    to call AES_ofb128_encrypt() directly.
    
    ok joshua@ tb@

--- lib/libcrypto/evp/e_aes.c
+++ lib/libcrypto/evp/e_aes.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: e_aes.c,v 1.66 2025/05/19 01:49:23 jsing Exp $ */
+/* $OpenBSD: e_aes.c,v 1.67 2025/05/19 03:55:09 jsing Exp $ */
 /* ====================================================================
  * Copyright (c) 2001-2011 The OpenSSL Project.  All rights reserved.
  *
@@ -278,6 +278,18 @@ aesni_ecb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
 }
 
 static int
+aesni_ofb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
+    const unsigned char *in, size_t len)
+{
+	EVP_AES_KEY *eak = ctx->cipher_data;
+
+	CRYPTO_ofb128_encrypt(in, out, len, &eak->ks, ctx->iv, &ctx->num,
+	    (block128_f)aesni_encrypt);
+
+	return 1;
+}
+
+static int
 aesni_gcm_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
     const unsigned char *iv, int enc)
 {
@@ -452,10 +464,10 @@ static int
 aes_ofb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
     const unsigned char *in, size_t len)
 {
-	EVP_AES_KEY *dat = (EVP_AES_KEY *)ctx->cipher_data;
+	EVP_AES_KEY *eak = ctx->cipher_data;
 
-	CRYPTO_ofb128_encrypt(in, out, len, &dat->ks, ctx->iv, &ctx->num,
-	    dat->block);
+	AES_ofb128_encrypt(in, out, len, &eak->ks, ctx->iv, &ctx->num);
+
 	return 1;
 }
 
@@ -602,7 +614,7 @@ static const EVP_CIPHER aesni_128_ofb = {
 	.iv_len = 16,
 	.flags = EVP_CIPH_FLAG_DEFAULT_ASN1 | EVP_CIPH_OFB_MODE,
 	.init = aesni_init_key,
-	.do_cipher = aes_ofb_cipher,
+	.do_cipher = aesni_ofb_cipher,
 	.ctx_size = sizeof(EVP_AES_KEY),
 };
 #endif
@@ -848,7 +860,7 @@ static const EVP_CIPHER aesni_192_ofb = {
 	.iv_len = 16,
 	.flags = EVP_CIPH_FLAG_DEFAULT_ASN1 | EVP_CIPH_OFB_MODE,
 	.init = aesni_init_key,
-	.do_cipher = aes_ofb_cipher,
+	.do_cipher = aesni_ofb_cipher,
 	.ctx_size = sizeof(EVP_AES_KEY),
 };
 #endif
@@ -1094,7 +1106,7 @@ static const EVP_CIPHER aesni_256_ofb = {
 	.iv_len = 16,
 	.flags = EVP_CIPH_FLAG_DEFAULT_ASN1 | EVP_CIPH_OFB_MODE,
 	.init = aesni_init_key,
-	.do_cipher = aes_ofb_cipher,
+	.do_cipher = aesni_ofb_cipher,
 	.ctx_size = sizeof(EVP_AES_KEY),
 };
 #endif
