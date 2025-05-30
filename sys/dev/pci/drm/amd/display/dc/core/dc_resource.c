671b4f3aa426a4bde0c2ffcf4b461eb8fdfc4c82
jsg <jsg@openbsd.org>
2025-05-30 02:26:07 +0000

    drm/amd/display: Fix BT2020 YCbCr limited/full range input
    From Ilya Bakoulin
    1e826acee1165a05c119366f4e902a4a11ee2460 in linux-6.12.y/6.12.31
    07bc2dcbcf403d47d6f305ef7f0d3d489491c5fb in mainline linux

--- sys/dev/pci/drm/amd/display/dc/core/dc_resource.c
+++ sys/dev/pci/drm/amd/display/dc/core/dc_resource.c
@@ -4215,7 +4215,7 @@ static void set_avi_info_frame(
 		break;
 	case COLOR_SPACE_2020_RGB_FULLRANGE:
 	case COLOR_SPACE_2020_RGB_LIMITEDRANGE:
-	case COLOR_SPACE_2020_YCBCR:
+	case COLOR_SPACE_2020_YCBCR_LIMITED:
 		hdmi_info.bits.EC0_EC2 = COLORIMETRYEX_BT2020RGBYCBCR;
 		hdmi_info.bits.C0_C1   = COLORIMETRY_EXTENDED;
 		break;
@@ -4229,7 +4229,7 @@ static void set_avi_info_frame(
 		break;
 	}
 
-	if (pixel_encoding && color_space == COLOR_SPACE_2020_YCBCR &&
+	if (pixel_encoding && color_space == COLOR_SPACE_2020_YCBCR_LIMITED &&
 			stream->out_transfer_func.tf == TRANSFER_FUNCTION_GAMMA22) {
 		hdmi_info.bits.EC0_EC2 = 0;
 		hdmi_info.bits.C0_C1 = COLORIMETRY_ITU709;
