a6893816bb5201b858b5e3936d90c05016d8c07e
claudio <claudio@openbsd.org>
2025-06-02 12:29:50 +0000

    In db_show_all_procs check that p->p_cpu is not NULL before calling __mp_lock_held(). This function is called on ddb entry and needs to be extra careful. OK mpi@ kettenis@

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_proc.c,v 1.102 2025/03/10 09:28:56 claudio Exp $	*/
+/*	$OpenBSD: kern_proc.c,v 1.103 2025/06/02 12:29:50 claudio Exp $	*/
 /*	$NetBSD: kern_proc.c,v 1.14 1996/02/09 18:59:41 christos Exp $	*/
 
 /*
@@ -585,7 +585,8 @@ db_show_all_procs(db_expr_t addr, int haddr, db_expr_t count, char *modif)
 
 		TAILQ_FOREACH(p, &pr->ps_threads, p_thr_link) {
 #ifdef MULTIPROCESSOR
-			if (__mp_lock_held(&kernel_lock, p->p_cpu))
+			if (p->p_cpu != NULL &&
+			    __mp_lock_held(&kernel_lock, p->p_cpu))
 				has_kernel_lock = 1;
 			else
 				has_kernel_lock = 0;
