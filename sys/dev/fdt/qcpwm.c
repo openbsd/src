0459ddd67240664b330651374f3859dbd1985d4c
kettenis <kettenis@openbsd.org>
2025-06-12 12:25:29 +0000

    Implement support for the "high resolution" PWMs as found on the x1e80100 machines.  Fix integer overflows that might already happen on the non high-res PWMs.
    ok landry@

--- sys/dev/fdt/qcpwm.c
+++ sys/dev/fdt/qcpwm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: qcpwm.c,v 1.2 2022/11/30 09:52:13 patrick Exp $	*/
+/*	$OpenBSD: qcpwm.c,v 1.3 2025/06/12 12:25:29 kettenis Exp $	*/
 /*
  * Copyright (c) 2022 Patrick Wildt <patrick@blueri.se>
  *
@@ -36,12 +36,18 @@
 #define PWM_SUBTYPE			0x05
 #define  PWM_SUBTYPE_LPG			0x2
 #define  PWM_SUBTYPE_PWM			0xb
+#define  PWM_SUBTYPE_HI_RES_PWM			0xc
 #define  PWM_SUBTYPE_LPG_LITE			0x11
 #define PWM_SIZE_CLK			0x41
 #define  PWM_SIZE_CLK_SELECT_SHIFT		0
 #define  PWM_SIZE_CLK_SELECT_MASK		0x3
+#define  PWM_SIZE_CLK_SIZE_SHIFT		2
+#define  PWM_SIZE_CLK_SIZE_MASK			0x1
+#define  PWM_SIZE_CLK_HI_RES_SELECT_SHIFT	0
+#define  PWM_SIZE_CLK_HI_RES_SELECT_MASK	0x7
+#define  PWM_SIZE_CLK_HI_RES_SIZE_SHIFT		4
+#define  PWM_SIZE_CLK_HI_RES_SIZE_MASK		0x7
 #define  PWM_SIZE_CLK_LPG_9BIT			(3 << 4)
-#define  PWM_SIZE_CLK_PWM_9BIT			(1 << 2)
 #define  PWM_SIZE_CLK_LPG_LITE_9BIT		(1 << 4)
 #define PWM_PREDIV_CLK			0x42
 #define  PWM_PREDIV_CLK_EXP_SHIFT		0
@@ -59,12 +65,12 @@
 #define PWM_SYNC			0x47
 #define  PWM_SYNC_PWM				(1 << 0)
 
-#define PWM_RESOLUTION			512
-
 #define NS_PER_S			1000000000LLU
 
-uint64_t qcpwm_clk_rates[] = { 0, 1024, 32768, 19200000 };
-uint64_t qcpwm_pre_divs[] = { 1, 3, 5, 6 };
+uint64_t qcpwm_clk_rates[8] = { 0, 1024, 32768, 19200000, 76800000 };
+uint64_t qcpwm_pre_divs[4] = { 1, 3, 5, 6 };
+uint64_t qcpwm_res[2] = { 64, 512 };
+uint64_t qcpwm_hi_res[8] = { 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 };
 
 struct qcpwm_softc {
 	struct device		sc_dev;
@@ -101,7 +107,8 @@ qcpwm_match(struct device *parent, void *match, void *aux)
 {
 	struct spmi_attach_args *saa = aux;
 
-	return OF_is_compatible(saa->sa_node, "qcom,pm8350c-pwm");
+	return (OF_is_compatible(saa->sa_node, "qcom,pm8350c-pwm") ||
+	    OF_is_compatible(saa->sa_node, "qcom,pmk8550-pwm"));
 }
 
 void
@@ -115,7 +122,10 @@ qcpwm_attach(struct device *parent, struct device *self, void *aux)
 	sc->sc_tag = saa->sa_tag;
 	sc->sc_sid = saa->sa_sid;
 
-	sc->sc_nchannel = 4;
+	if (OF_is_compatible(saa->sa_node, "qcom,pm8350c-pwm"))
+		sc->sc_nchannel = 4;
+	else
+		sc->sc_nchannel = 2;
 
 	printf("\n");
 
@@ -137,7 +147,7 @@ qcpwm_get_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 {
 	struct qcpwm_softc *sc = cookie;
 	int chan = cells[0];
-	uint64_t refclk, prediv, exp;
+	uint64_t refclk, prediv, exp, res;
 	uint64_t pcycles, dcycles;
 	uint16_t pwmval;
 	uint8_t reg;
@@ -151,9 +161,25 @@ qcpwm_get_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 	    PWM_ENABLE_CONTROL) & PWM_ENABLE_CONTROL_OUTPUT);
 
 	reg = qcpwm_read(sc, PWM_CHAN_OFF(chan) + PWM_SIZE_CLK);
-	refclk = (reg >> PWM_SIZE_CLK_SELECT_SHIFT) &
-	    PWM_SIZE_CLK_SELECT_MASK;
-	refclk = qcpwm_clk_rates[refclk];
+	switch (qcpwm_read(sc, PWM_CHAN_OFF(chan) + PWM_SUBTYPE)) {
+	case PWM_SUBTYPE_HI_RES_PWM:
+		refclk = (reg >> PWM_SIZE_CLK_HI_RES_SELECT_SHIFT) &
+		    PWM_SIZE_CLK_HI_RES_SELECT_MASK;
+		refclk = qcpwm_clk_rates[refclk];
+		res = (reg >> PWM_SIZE_CLK_HI_RES_SIZE_SHIFT) &
+		    PWM_SIZE_CLK_HI_RES_SIZE_MASK;
+		res = qcpwm_hi_res[res];
+		break;
+	default:
+		refclk = (reg >> PWM_SIZE_CLK_SELECT_SHIFT) &
+		    PWM_SIZE_CLK_SELECT_MASK;
+		refclk = qcpwm_clk_rates[refclk];
+		res = (reg >> PWM_SIZE_CLK_SIZE_SHIFT) &
+		    PWM_SIZE_CLK_SIZE_MASK;
+		res = qcpwm_res[res];
+		break;
+	}
+
 	if (refclk == 0)
 		return 0;
 
@@ -168,7 +194,7 @@ qcpwm_get_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 	    sc->sc_addr + PWM_CHAN_OFF(chan) + PWM_VALUE,
 	    &pwmval, sizeof(pwmval));
 
-	pcycles = (NS_PER_S * PWM_RESOLUTION * prediv * (1 << exp));
+	pcycles = (NS_PER_S * res * prediv * (1 << exp));
 	pcycles = (pcycles + refclk - 1) / refclk;
 	dcycles = (NS_PER_S * pwmval * prediv * (1 << exp));
 	dcycles = (dcycles + refclk - 1) / refclk;
@@ -183,11 +209,13 @@ qcpwm_set_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 {
 	struct qcpwm_softc *sc = cookie;
 	uint64_t dcycles, pcycles, acycles, diff = UINT64_MAX;
+	uint64_t *res;
 	int chan = cells[0];
-	int clksel, divsel, exp;
+	int clksel, divsel, ressel, exp;
+	int nclk, nres;
 	uint16_t pwmval;
 	uint8_t reg;
-	int i, j, m;
+	int i, j, k, m;
 
 	if (chan >= sc->sc_nchannel)
 		return ENXIO;
@@ -195,27 +223,43 @@ qcpwm_set_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 	pcycles = ps->ps_period;
 	dcycles = ps->ps_pulse_width;
 
-	for (i = 1; i < nitems(qcpwm_clk_rates); i++) {
-		for (j = 0; j < nitems(qcpwm_pre_divs); j++) {
-			if (pcycles * qcpwm_clk_rates[i] <
-			    NS_PER_S * qcpwm_pre_divs[j] * PWM_RESOLUTION)
-				continue;
+	reg = qcpwm_read(sc, PWM_CHAN_OFF(chan) + PWM_SIZE_CLK);
+	switch (qcpwm_read(sc, PWM_CHAN_OFF(chan) + PWM_SUBTYPE)) {
+	case PWM_SUBTYPE_HI_RES_PWM:
+		res = qcpwm_hi_res;
+		nres = nitems(qcpwm_hi_res);
+		nclk = 5;
+		break;
+	default:
+		res = qcpwm_res;
+		nres = nitems(qcpwm_res);
+		nclk = 4;
+		break;
+	}
 
-			m = flsl((pcycles * qcpwm_clk_rates[i]) /
-			    (NS_PER_S * qcpwm_pre_divs[j] * PWM_RESOLUTION))
-			    - 1;
-			if (m > PWM_PREDIV_CLK_EXP_MASK)
-				m = PWM_PREDIV_CLK_EXP_MASK;
+	for (i = 0; i < nres; i++) {
+		for (j = 1; j < nclk; j++) {
+			for (k = 0; k < nitems(qcpwm_pre_divs); k++) {
+				if (pcycles * qcpwm_clk_rates[j] <
+				    NS_PER_S * qcpwm_pre_divs[k] * res[i])
+					continue;
 
-			acycles = (NS_PER_S * qcpwm_pre_divs[j] *
-			    PWM_RESOLUTION * (1 << m)) /
-			    qcpwm_clk_rates[i];
-			if (pcycles - acycles < diff) {
-				diff = pcycles - acycles;
-				ps->ps_period = acycles;
-				clksel = i;
-				divsel = j;
-				exp = m;
+				m = flsl((pcycles * qcpwm_clk_rates[j]) /
+				    (NS_PER_S * qcpwm_pre_divs[k] * res[i]))
+				    - 1;
+				if (m > PWM_PREDIV_CLK_EXP_MASK)
+					m = PWM_PREDIV_CLK_EXP_MASK;
+
+				acycles = (NS_PER_S * qcpwm_pre_divs[k] *
+				    res[i] * (1 << m)) / qcpwm_clk_rates[j];
+				if (pcycles - acycles < diff) {
+					diff = pcycles - acycles;
+					ps->ps_period = acycles;
+					ressel = i;
+					clksel = j;
+					divsel = k;
+					exp = m;
+				}
 			}
 		}
 	}
@@ -239,7 +283,10 @@ qcpwm_set_state(void *cookie, uint32_t *cells, struct pwm_state *ps)
 		reg |= PWM_SIZE_CLK_LPG_9BIT;
 		break;
 	case PWM_SUBTYPE_PWM:
-		reg |= PWM_SIZE_CLK_PWM_9BIT;
+		reg |= (ressel << PWM_SIZE_CLK_SIZE_SHIFT);
+		break;
+	case PWM_SUBTYPE_HI_RES_PWM:
+		reg |= (ressel << PWM_SIZE_CLK_HI_RES_SIZE_SHIFT);
 		break;
 	case PWM_SUBTYPE_LPG_LITE:
 	default:
