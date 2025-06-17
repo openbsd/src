c5e46c7932a0678c6e3378c9fc518e0eeaa2762f
dlg <dlg@openbsd.org>
2025-06-17 01:35:07 +0000

    guard tcp stat updates with splnet now that interrupts can update them.
    some tcp stack lso/tso statistics are now updated by drivers that
    implement tso/lro, and this code can run from an interrupt handler.
    eg, ifq_restart from a transmit completion interrupt can call a
    drivers start routine, which can preempt the tcp stack running on
    behalf of a system call.
    
    the tcp stats are per cpu counters so they can be updated without
    coordinating with each other, but, as per the counters manpage,
    updates on a cpu have to be serialised. this is to prevent corruption
    of at least the generation number each cpu uses to version the
    counter updates. since all the tcp stats are a single set of counters,
    any update has to prevent preemption.
    
    ok claudio@ bluhm@

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_var.h,v 1.192 2025/06/08 17:06:19 bluhm Exp $	*/
+/*	$OpenBSD: tcp_var.h,v 1.193 2025/06/17 01:35:07 dlg Exp $	*/
 /*	$NetBSD: tcp_var.h,v 1.17 1996/02/13 23:44:24 christos Exp $	*/
 
 /*
@@ -640,20 +640,26 @@ extern struct cpumem *tcpcounters;
 static inline void
 tcpstat_inc(enum tcpstat_counters c)
 {
+	int s = splnet();
 	counters_inc(tcpcounters, c);
+	splx(s);
 }
 
 static inline void
 tcpstat_add(enum tcpstat_counters c, uint64_t v)
 {
+	int s = splnet();
 	counters_add(tcpcounters, c, v);
+	splx(s);
 }
 
 static inline void
 tcpstat_pkt(enum tcpstat_counters pcounter, enum tcpstat_counters bcounter,
     uint64_t v)
 {
+	int s = splnet();
 	counters_pkt(tcpcounters, pcounter, bcounter, v);
+	splx(s);
 }
 
 extern uint64_t tcp_starttime;
