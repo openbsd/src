2c2860715069de5b24a894fcaed4b49aa2bdd98d
miod <miod@openbsd.org>
2025-06-26 20:43:41 +0000

    Factor code responsible for flipping bits in the auxio registers. Only include the floppy routine if the floppy driver is configured.

--- sys/arch/sparc64/dev/auxio.c
+++ sys/arch/sparc64/dev/auxio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auxio.c,v 1.11 2025/06/26 20:39:34 miod Exp $	*/
+/*	$OpenBSD: auxio.c,v 1.12 2025/06/26 20:43:41 miod Exp $	*/
 /*	$NetBSD: auxio.c,v 1.1 2000/04/15 03:08:13 mrg Exp $	*/
 
 /*
@@ -47,6 +47,8 @@
 #include <sparc64/dev/auxioreg.h>
 #include <sparc64/dev/auxiovar.h>
 
+#include "fdc.h"
+
 #define	AUXIO_ROM_NAME		"auxio"
 
 /*
@@ -71,6 +73,7 @@ struct cfdriver auxio_cd = {
 };
 
 void auxio_led_blink(void *, int);
+void auxio_flip(struct auxio_softc *, uint32_t, uint32_t);
 
 int
 auxio_ebus_match(struct device *parent, void *cf, void *aux)
@@ -150,10 +153,9 @@ auxio_attach_common(struct auxio_softc *sc)
 }
 
 void
-auxio_led_blink(void *vsc, int on)
+auxio_flip(struct auxio_softc *sc, uint32_t mask, uint32_t set)
 {
-	struct auxio_softc *sc = vsc;
-	u_int32_t led;
+	uint32_t led;
 	int s;
 
 	s = splhigh();
@@ -163,10 +165,7 @@ auxio_led_blink(void *vsc, int on)
 	else
 		led = bus_space_read_1(sc->sc_tag, sc->sc_led, 0);
 
-	if (on)
-		led |= AUXIO_LED_LED;
-	else
-		led &= ~AUXIO_LED_LED;
+	led = (led & ~mask) | set;
 
 	if (sc->sc_flags & AUXIO_EBUS)
 		bus_space_write_4(sc->sc_tag, sc->sc_led, 0, htole32(led));
@@ -176,11 +175,18 @@ auxio_led_blink(void *vsc, int on)
 	splx(s);
 }
 
+void
+auxio_led_blink(void *vsc, int on)
+{
+	struct auxio_softc *sc = vsc;
+	auxio_flip(sc, AUXIO_LED_LED, on ? AUXIO_LED_LED : 0);
+}
+
+#if NFDC > 0
 int
 auxio_fd_control(u_int32_t bits)
 {
 	struct auxio_softc *sc;
-	u_int32_t led;
 
 	if (auxio_cd.cd_ndevs == 0) {
 		return ENXIO;
@@ -191,17 +197,7 @@ auxio_fd_control(u_int32_t bits)
 	 * We'll assume the floppy drive is tied to first auxio found.
 	 */
 	sc = (struct auxio_softc *)auxio_cd.cd_devs[0];
-	if (sc->sc_flags & AUXIO_EBUS)
-		led = letoh32(bus_space_read_4(sc->sc_tag, sc->sc_led, 0));
-	else
-		led = bus_space_read_1(sc->sc_tag, sc->sc_led, 0);
-
-	led = (led & ~AUXIO_LED_FLOPPY_MASK) | bits;
-
-	if (sc->sc_flags & AUXIO_EBUS)
-		bus_space_write_4(sc->sc_tag, sc->sc_led, 0, htole32(led));
-	else
-		bus_space_write_1(sc->sc_tag, sc->sc_led, 0, led);
-
+	auxio_flip(sc, AUXIO_LED_FLOPPY_MASK, bits);
 	return 0;
 }
+#endif
