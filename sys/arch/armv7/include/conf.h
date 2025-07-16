054bb2fd459c70075d0b11ae0cdf232d9ad4750a
jsg <jsg@openbsd.org>
2025-07-16 03:01:53 +0000

    remove outdated comment about BEAGLEBOARD config

--- sys/arch/armv7/include/conf.h
+++ sys/arch/armv7/include/conf.h
@@ -1,17 +1,10 @@
-/*	$OpenBSD: conf.h,v 1.1 2013/09/04 14:38:26 patrick Exp $	*/
+/*	$OpenBSD: conf.h,v 1.2 2025/07/16 03:01:53 jsg Exp $	*/
 /*	$NetBSD: conf.h,v 1.8 2002/02/10 12:26:03 chris Exp $	*/
 
 #ifndef _MACHINE_CONF_H_
 #define	_MACHINE_CONF_H_
 
 #include <sys/conf.h>
-
-/*
- * BEAGLEBOARD specific device includes go in here
- */
-
-/*#define	CONF_HAVE_GPIO*/
-
 #include <arm/conf.h>
 
 #endif	/* _MACHINE_CONF_H_ */
