f7689e02158bb007b530508bfc95ac16538796c3
jsg <jsg@openbsd.org>
2025-07-18 00:15:45 +0000

    drm/framebuffer: Acquire internal references on GEM handles
    From Thomas Zimmermann
    065bd940ee0a0033ea9a7dbb1a9110baef9415a4 in linux-6.12.y/6.12.39
    f6bfc9afc7510cb5e6fbe0a17c507917b0120280 in mainline linux

--- sys/dev/pci/drm/drm_internal.h
+++ sys/dev/pci/drm/drm_internal.h
@@ -183,7 +183,7 @@ drm_sysfs_connector_remove_early(struct drm_connector *connector)
 
 /* drm_gem.c */
 int drm_gem_init(struct drm_device *dev);
-void drm_gem_object_handle_get_unlocked(struct drm_gem_object *obj);
+bool drm_gem_object_handle_get_if_exists_unlocked(struct drm_gem_object *obj);
 void drm_gem_object_handle_put_unlocked(struct drm_gem_object *obj);
 int drm_gem_handle_create_tail(struct drm_file *file_priv,
 			       struct drm_gem_object *obj,
