831436ac1c29b4d9b429f5158bf635e3f23c1eb3
jca <jca@openbsd.org>
2025-08-02 12:20:40 +0000

    Also inherit PS_PROFILE on fork
    This lets child processes disable/reenable profiling when they deemed
    appropriate. Currently the profil(0) in _mcleanup fails with EPERM for
    child processes.
    
    Input and ok deraadt@

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_fork.c,v 1.274 2025/08/01 13:32:53 jca Exp $	*/
+/*	$OpenBSD: kern_fork.c,v 1.275 2025/08/02 12:20:40 jca Exp $	*/
 /*	$NetBSD: kern_fork.c,v 1.29 1996/02/09 18:59:34 christos Exp $	*/
 
 /*
@@ -252,7 +252,7 @@ process_new(struct proc *p, struct process *parent, int flags)
 
 	pr->ps_flags = parent->ps_flags &
 	    (PS_SUGID | PS_SUGIDEXEC | PS_PLEDGE | PS_EXECPLEDGE |
-	    PS_NOBTCFI | PS_WXNEEDED | PS_CHROOT);
+	    PS_NOBTCFI | PS_WXNEEDED | PS_CHROOT | PS_PROFILE);
 	if (parent->ps_session->s_ttyvp != NULL)
 		pr->ps_flags |= parent->ps_flags & PS_CONTROLT;
 
