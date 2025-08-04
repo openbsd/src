9a7afd8601f91bf892f0ef15e48524274cbe22c7
bluhm <bluhm@openbsd.org>
2025-08-04 14:03:32 +0000

    Avoid unsigned comparison in AH parsing.
    The check (skip - sizeof(struct ip6_hdr) > 0) did not work as
    expected because it is done as unsigned long.  Instead of substraction,
    add the small positive integers off and skip to size_t sizes.  Then
    do a comparison that does not rely on negative numbers.
    
    CID 1594927
    OK claudio@ tobhe@

--- sys/netinet/ip_ah.c
+++ sys/netinet/ip_ah.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ah.c,v 1.177 2025/07/08 00:47:41 jsg Exp $ */
+/*	$OpenBSD: ip_ah.c,v 1.178 2025/08/04 14:03:32 bluhm Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -363,7 +363,7 @@ ah_massage_headers(struct mbuf **mp, int af, int skip, int alg, int out)
 		}
 
 		/* Let's deal with the remaining headers (if any). */
-		if (skip - sizeof(struct ip6_hdr) > 0) {
+		if (skip > sizeof(struct ip6_hdr)) {
 			if (m->m_len <= skip) {
 				ptr = malloc(skip - sizeof(struct ip6_hdr),
 				    M_XDATA, M_NOWAIT);
@@ -393,9 +393,9 @@ ah_massage_headers(struct mbuf **mp, int af, int skip, int alg, int out)
 
 		nxt = ip6.ip6_nxt;  /* Next header type. */
 
-		for (off = 0; off < skip - sizeof(struct ip6_hdr);) {
-			if (off + sizeof(struct ip6_ext) >
-			    skip - sizeof(struct ip6_hdr))
+		for (off = 0; off + sizeof(struct ip6_hdr) < skip;) {
+			if (off + sizeof(struct ip6_hdr) +
+			    sizeof(struct ip6_ext) > skip)
 				goto error6;
 			ip6e = (struct ip6_ext *)(ptr + off);
 
@@ -405,7 +405,7 @@ ah_massage_headers(struct mbuf **mp, int af, int skip, int alg, int out)
 				noff = off + ((ip6e->ip6e_len + 1) << 3);
 
 				/* Sanity check. */
-				if (noff > skip - sizeof(struct ip6_hdr))
+				if (noff + sizeof(struct ip6_hdr) > skip)
 					goto error6;
 
 				/*
