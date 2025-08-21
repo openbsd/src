30aa4a8d90074327dcfcd4e15f6ab21537e1ba54
schwarze <schwarze@openbsd.org>
2025-08-21 15:36:20 +0000

    Avoid implicitly converting a potentially negative page offset to size_t and then back to int.  While this was not a bug and the end result was portably correct, Alexander Ziaee@ privately reported to me that the GCC 14 in the FreeBSD Jenkins CI felt uneasy about it.
    For clarity and readability, rewrite the truncation statement
    to not mix signed and unsigned types, to not use explicit casts,
    and make handling of the lower and upper cutoff more similar
    to each other.

--- usr.bin/mandoc/roff_term.c
+++ usr.bin/mandoc/roff_term.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: roff_term.c,v 1.24 2025/07/16 14:23:55 schwarze Exp $ */
+/* $OpenBSD: roff_term.c,v 1.25 2025/08/21 15:36:20 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2014, 2015, 2017-2021, 2025
  *               Ingo Schwarze <schwarze@openbsd.org>
@@ -163,6 +163,7 @@ roff_term_pre_po(ROFF_TERM_ARGS)
 	static int	 polast;  /* Previously requested. */
 	static int	 po;      /* Currently requested. */
 	static int	 pouse;   /* Currently used. */
+	int		 pomin;   /* Minimum to be used. */
 	int		 pomax;   /* Maximum to be used. */
 	int		 ponew;   /* Newly requested. */
 
@@ -184,9 +185,9 @@ roff_term_pre_po(ROFF_TERM_ARGS)
 	po = ponew;
 
 	/* Truncate to the range [-offset, 60], remember, and apply it. */
+	pomin = -p->tcol->offset;
 	pomax = term_len(p, 60);
-	pouse = po >= pomax ? pomax :
-	    po < -(int)p->tcol->offset ? -p->tcol->offset : po;
+	pouse = po > pomax ? pomax : po < pomin ? pomin : po;
 	p->tcol->offset += pouse;
 }
 
