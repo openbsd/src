30913f4bb99b6852aee1b9ab58f94c8a8bf31162
jsg <jsg@openbsd.org>
2025-08-22 04:43:15 +0000

    drm/amd/pm: fix null pointer access
    From Umio Yasuno
    a83ffafd02a7af59848755c109d544e3894af737 in linux-6.12.y/6.12.43
    d524d40e3a6152a3ea1125af729f8cd8ca65efde in mainline linux

--- sys/dev/pci/drm/amd/pm/amdgpu_pm.c
+++ sys/dev/pci/drm/amd/pm/amdgpu_pm.c
@@ -1492,6 +1492,8 @@ static ssize_t amdgpu_set_pp_power_profile_mode(struct device *dev,
 			if (ret)
 				return -EINVAL;
 			parameter_size++;
+			if (!tmp_str)
+				break;
 			while (isspace(*tmp_str))
 				tmp_str++;
 		}
@@ -3854,6 +3856,9 @@ static int parse_input_od_command_lines(const char *buf,
 		if (ret)
 			return -EINVAL;
 		parameter_size++;
+
+		if (!tmp_str)
+			break;
 
 		while (isspace(*tmp_str))
 			tmp_str++;
