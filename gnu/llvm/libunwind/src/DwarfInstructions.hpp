e6a6e6a160aa9e67b56a53c4b454b91793a54189
gkoehler <gkoehler@openbsd.org>
2025-08-25 04:24:05 +0000

    Stop the xonly crash in libunwind on powerpc64
    The powerpc64 unwinder was trying to read the instruction at the
    return address.  This crashed, because you can't read from
    execute-only memory.
    
    This has probably been broken since January 2023 (before OpenBSD 7.3),
    when powerpc64 became xonly.  This broke anything that got a
    backtrace(3) from libexecinfo or threw a C++ exception with libc++abi.
    editors/emacs,no_x11 crashed in libexecinfo.  devel/gdb crashed in
    libc++abi.  If you tried to examine the core dupm in gdb, then gdb
    crashed again.
    
    The unwinder is asking whether the return address has an instruction
    to restore the TOC register.  Instead of crashing, call dladdr(3)
    twice to find the shared objects.  Assume that each object has only
    one TOC.  Restore TOC if returning to a different object.
    
    ok kettenis@

--- gnu/llvm/libunwind/src/DwarfInstructions.hpp
+++ gnu/llvm/libunwind/src/DwarfInstructions.hpp
@@ -350,6 +350,23 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
       if (R::getArch() == REGISTERS_PPC64 && returnAddress != 0) {
         pint_t sp = newRegisters.getRegister(UNW_REG_SP);
         pint_t r2 = 0;
+#if defined(_LIBUNWIND_IS_NATIVE_ONLY) && defined(__OpenBSD__)
+        // Can't addressSpace.get32(returnAddress), because it might be
+        // in execute-only memory.  If returning to a different shared
+        // object, then restore r2.  This assumes one TOC per object.
+        // ELFv2 allows more than one (3.3 TOC, Modules Containing
+        // Multiple TOCs), but we have no way to detect that.
+        void *IPObject = NULL;
+        void *RAObject = NULL;
+        Dl_info info;
+        if (dladdr((void *)registers.getIP(), &info))
+          IPObject = info.dli_fbase;
+        if (dladdr((void *)returnAddress, &info))
+          RAObject = info.dli_fbase;
+        // Assume ELFv2.
+        if (IPObject != RAObject)
+          r2 = addressSpace.get64(sp + PPC64_ELFV2_R2_OFFSET);
+#else
         switch (addressSpace.get32(returnAddress)) {
         case PPC64_ELFV1_R2_LOAD_INST_ENCODING:
           r2 = addressSpace.get64(sp + PPC64_ELFV1_R2_OFFSET);
@@ -358,6 +375,7 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace, pint_t pc,
           r2 = addressSpace.get64(sp + PPC64_ELFV2_R2_OFFSET);
           break;
         }
+#endif
         if (r2)
           newRegisters.setRegister(UNW_PPC64_R2, r2);
       }
