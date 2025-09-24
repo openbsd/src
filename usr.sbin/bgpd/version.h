602ee8824480aca2191b6b95b205b3e38018438d
claudio <claudio@openbsd.org>
2025-09-24 15:51:15 +0000

    Bump version to 8.9

--- usr.sbin/bgpd/version.h
+++ usr.sbin/bgpd/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.19 2025/02/05 12:11:25 claudio Exp $ */
+/* $OpenBSD: version.h,v 1.20 2025/09/24 15:51:15 claudio Exp $ */
 
-#define BGPD_VERSION	"8.8"
+#define BGPD_VERSION	"8.9"
