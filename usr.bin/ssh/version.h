122bf0d56d5a321d00d192b3f731fdd0e12e0366
djm <djm@openbsd.org>
2025-10-08 00:32:52 +0000

    openssh-10.2
    The only change since 10.1 is the channels.c fix

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: version.h,v 1.106 2025/10/06 01:45:22 djm Exp $ */
+/* $OpenBSD: version.h,v 1.107 2025/10/08 00:32:52 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_10.1"
+#define SSH_VERSION	"OpenSSH_10.2"
 #define SSH_RELEASE    SSH_VERSION
