6b0a793cfe2a80cfe3963fab0e736c7f58c9fbb5
djm <djm@openbsd.org>
2025-11-06 17:24:28 +0000

    sk-dummy.so needs sshlog() stub after ed25519-openssl.c change

---
+++ regress/usr.bin/ssh/misc/sk-dummy/fatal.c
@@ -0,0 +1,43 @@
+/* public domain */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include "log.h"
+
+void
+sshlogv(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *suffix, const char *fmt, va_list args)
+{
+	if (showfunc)
+		fprintf(stderr, "%s: ", func);
+	vfprintf(stderr, fmt, args);
+	if (suffix != NULL)
+		fprintf(stderr, ": %s", suffix);
+	fputc('\n', stderr);
+}
+
+void
+sshlog(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *suffix, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	sshlogv(file, func, line, showfunc, level, suffix, fmt, args);
+	va_end(args);
+}
+
+void
+sshfatal(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *suffix, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	sshlogv(file, func, line, showfunc, level, suffix, fmt, args);
+	va_end(args);
+	_exit(1);
+}
