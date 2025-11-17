96ffaf2ce02844ae32670880de87ab0b557c6aa8
jsg <jsg@openbsd.org>
2025-11-17 23:20:42 +0000

    back to rev 1.24, enable GuC submission on Meteor/Arrow Lake again
    The problems I had with GuC submission were with an uncommitted inteldrm
    firmware update to 20250917.  Not yet seen with 20250708 currently
    on firmware mirrors.
    
    requested by kettenis@ for low power idle states in suspend

--- sys/dev/pci/drm/i915/i915_driver.c
+++ sys/dev/pci/drm/i915/i915_driver.c
@@ -2277,10 +2277,11 @@ inteldrm_attach(struct device *parent, struct device *self, void *aux)
 
 	intel_display_device_probe(dev_priv);
 
-	/* uc_expand_default_options() with no GuC submission */
-	if (GRAPHICS_VER(dev_priv) >= 12 &&
-	    (INTEL_INFO(dev_priv)->platform != INTEL_TIGERLAKE) &&
-	    (INTEL_INFO(dev_priv)->platform != INTEL_ROCKETLAKE))
+	/*
+	 * with GuC submission, init sometimes fails on Alder Lake-P
+	 * too early for IS_ALDERLAKE_P
+	 */
+	if (info->platform == INTEL_ALDERLAKE_P)
 		dev_priv->params.enable_guc = ENABLE_GUC_LOAD_HUC;
 
 	mmio_bar = (GRAPHICS_VER(dev_priv) == 2) ? 0x14 : 0x10;
