a48d060175e1ee0b4b3fe8f6ddd2a43055918429
sashan <sashan@openbsd.org>
2025-11-24 16:57:19 +0000

    ifa_load() in pfctl_parser.c may attempt to read beyond the buffer.
    The current ifa_load() is not paranoid enough when it deals with
    information which comes from kernel. The function just ignores
    sa_len member in socket address returned getifaddrs().
    
    The issue has been reported by anton@. The idea for fix here comes
    fromy claudio@.
    
    OK @claudio, @deraadt

--- sbin/pfctl/pfctl_parser.c
+++ sbin/pfctl/pfctl_parser.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfctl_parser.c,v 1.353 2025/11/11 04:06:20 dlg Exp $ */
+/*	$OpenBSD: pfctl_parser.c,v 1.354 2025/11/24 16:57:19 sashan Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1459,12 +1459,16 @@ ifa_load(void)
 			copy_satopfaddr(&n->addr.v.a.addr, ifa->ifa_addr);
 			ifa->ifa_netmask->sa_family = ifa->ifa_addr->sa_family;
 			copy_satopfaddr(&n->addr.v.a.mask, ifa->ifa_netmask);
-			if (ifa->ifa_broadaddr != NULL) {
-				ifa->ifa_broadaddr->sa_family = ifa->ifa_addr->sa_family;
+			if (ifa->ifa_broadaddr != NULL &&
+			    ifa->ifa_broadaddr->sa_len != 0) {
+				ifa->ifa_broadaddr->sa_family =
+				    ifa->ifa_addr->sa_family;
 				copy_satopfaddr(&n->bcast, ifa->ifa_broadaddr);
 			}
-			if (ifa->ifa_dstaddr != NULL) {
-				ifa->ifa_dstaddr->sa_family = ifa->ifa_addr->sa_family;
+			if (ifa->ifa_dstaddr != NULL &&
+			    ifa->ifa_dstaddr->sa_len != 0) {
+				ifa->ifa_dstaddr->sa_family =
+				    ifa->ifa_addr->sa_family;
 				copy_satopfaddr(&n->peer, ifa->ifa_dstaddr);
 			}
 			if (n->af == AF_INET6)
