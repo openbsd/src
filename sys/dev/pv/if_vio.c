636dc05f3c5f143cc6ed25716f673b58d64eca69
bluhm <bluhm@openbsd.org>
2025-11-24 14:16:17 +0000

    Limit number of queues to IF_MAX_VECTORS also in vio(4).
    OK sf@

--- sys/dev/pv/if_vio.c
+++ sys/dev/pv/if_vio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_vio.c,v 1.72 2025/09/16 12:18:10 hshoexer Exp $	*/
+/*	$OpenBSD: if_vio.c,v 1.73 2025/11/24 14:16:17 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2012 Stefan Fritsch, Alexander Fiveg.
@@ -663,7 +663,7 @@ vio_attach(struct device *parent, struct device *self, void *aux)
 		vsc->sc_nvqs = 2 * i + 1;
 		i = MIN(i, VIRTIO_NET_CTRL_MQ_VQ_PAIRS_MAX);
 		sc->sc_intrmap = intrmap_create(&sc->sc_dev, i,
-		    va->va_nintr - 2, 0);
+		    MIN(va->va_nintr - 2, IF_MAX_VECTORS), 0);
 		sc->sc_nqueues = intrmap_count(sc->sc_intrmap);
 		printf(": %u queue%s", sc->sc_nqueues,
 		    sc->sc_nqueues > 1 ? "s" : "");
