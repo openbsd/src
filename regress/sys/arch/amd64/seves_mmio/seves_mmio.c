fe7aaad813d1739c592c33c361a0d6f923e72fe7
hshoexer <hshoexer@openbsd.org>
2025-11-27 11:55:08 +0000

    Add license and rcs header.
    ok tb

--- regress/sys/arch/amd64/seves_mmio/seves_mmio.c
+++ regress/sys/arch/amd64/seves_mmio/seves_mmio.c
@@ -1,3 +1,21 @@
+/*	$OpenBSD: seves_mmio.c,v 1.2 2025/11/27 11:55:08 hshoexer Exp $ */
+
+/*
+ * Copyright (c) 2025 Hans-Joerg Hoexer <hshoexer@yerbouti.franken.de>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <sys/mman.h>
 #include <sys/param.h>
 
