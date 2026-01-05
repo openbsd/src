fc8b786ecf7e6dd24b8410d009b9e3f8c5564e07
mvs <mvs@openbsd.org>
2026-01-05 11:49:01 +0000

    Stop doing `if_timer' modifications. We don't set `if_watchdog' handler so the watchdog timeout is not used. However, `if_timer' is prtected with kernel lock and this makes upcoming genet_start() unlocking useless.
    If someone want to implement watchdog for bse(4), it should be done in
    mp-safe way, may be with preceding if_slowtimo reworking.
    
    No objections from kettenis.

--- sys/dev/ic/bcmgenet.c
+++ sys/dev/ic/bcmgenet.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bcmgenet.c,v 1.9 2026/01/05 11:47:04 mvs Exp $ */
+/* $OpenBSD: bcmgenet.c,v 1.10 2026/01/05 11:49:01 mvs Exp $ */
 /* $NetBSD: bcmgenet.c,v 1.3 2020/02/27 17:30:07 jmcneill Exp $ */
 
 /*-
@@ -656,7 +656,6 @@ genet_stop(struct genet_softc *sc)
 
 	ifp->if_flags &= ~IFF_RUNNING;
 	ifq_clr_oactive(&ifp->if_snd);
-	ifp->if_timer = 0;
 
 	intr_barrier(sc->sc_ih);
 
@@ -773,9 +772,6 @@ genet_txintr(struct genet_softc *sc, int qid)
 		--sc->sc_tx.queued;
 	}
 
-	if (sc->sc_tx.queued == 0)
-		ifp->if_timer = 0;
-
 	if (sc->sc_tx.cidx != cidx) {
 		sc->sc_tx.next = i;
 		sc->sc_tx.cidx = cidx;
@@ -823,10 +819,8 @@ genet_start(struct ifnet *ifp)
 		cnt++;
 	}
 
-	if (cnt != 0) {
+	if (cnt != 0)
 		WR4(sc, GENET_TX_DMA_PROD_INDEX(qid), sc->sc_tx.pidx);
-		ifp->if_timer = 5;
-	}
 }
 
 int
