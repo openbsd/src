4b3d1f29b6b53302f8e77945468211faf0992487
tb <tb@openbsd.org>
2026-01-20 16:49:44 +0000

    rpki-client regress: adjust for pkey -> spki rename

--- regress/usr.sbin/rpki-client/test-cert.c
+++ regress/usr.sbin/rpki-client/test-cert.c
@@ -1,4 +1,4 @@
-/*	$Id: test-cert.c,v 1.27 2025/10/23 05:26:25 tb Exp $ */
+/*	$Id: test-cert.c,v 1.28 2026/01/20 16:49:44 tb Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -85,7 +85,7 @@ main(int argc, char *argv[])
 			free(buf);
 			if (p == NULL)
 				break;
-			p = ta_parse(cert_path, p, tal->pkey, tal->pkeysz);
+			p = ta_parse(cert_path, p, tal->spki, tal->spkisz);
 			tal_free(tal);
 			if (p == NULL)
 				break;
