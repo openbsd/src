9625afb4b8f807c0bad97a0aa4ba3c516ee78e83
nicm <nicm@openbsd.org>
2026-03-02 08:48:57 +0000

    Revert r1.343 for the moment since it breaks behaviour (#() in status-left) that we need to keep.

--- usr.bin/tmux/format.c
+++ usr.bin/tmux/format.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: format.c,v 1.345 2026/03/02 08:41:43 nicm Exp $ */
+/* $OpenBSD: format.c,v 1.346 2026/03/02 08:48:57 nicm Exp $ */
 
 /*
  * Copyright (c) 2011 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -5224,13 +5224,11 @@ format_replace(struct format_expand_state *es, const char *key, size_t keylen,
 done:
 	/* Expand again if required. */
 	if (modifiers & FORMAT_EXPAND) {
-		format_copy_state(&next, es, FORMAT_EXPAND_NOJOBS);
-		new = format_expand1(&next, value);
+		new = format_expand1(es, value);
 		free(value);
 		value = new;
 	} else if (modifiers & FORMAT_EXPANDTIME) {
-		format_copy_state(&next, es, FORMAT_EXPAND_TIME|
-		    FORMAT_EXPAND_NOJOBS);
+		format_copy_state(&next, es, FORMAT_EXPAND_TIME);
 		new = format_expand1(&next, value);
 		free(value);
 		value = new;
