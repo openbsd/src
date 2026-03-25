bf229d989d7e73e71f35ad74fe707648ad44fe5f
jsg <jsg@openbsd.org>
2026-03-25 23:50:06 +0000

    drm/amd/display: Fix DisplayID not-found handling in parse_edid_displayid_vrr()
    From Srinivasan Shanmugam
    430334e9e4e815b0541f8337c2dbd49d7d3b55fa in linux-6.18.y/6.18.20
    2323b019651ad81c20a0f7f817c63392b3110652 in mainline linux

--- sys/dev/pci/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ sys/dev/pci/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -12901,7 +12901,7 @@ static void parse_edid_displayid_vrr(struct drm_connector *connector,
 	u16 min_vfreq;
 	u16 max_vfreq;
 
-	if (edid == NULL || edid->extensions == 0)
+	if (!edid || !edid->extensions)
 		return;
 
 	/* Find DisplayID extension */
@@ -12911,7 +12911,7 @@ static void parse_edid_displayid_vrr(struct drm_connector *connector,
 			break;
 	}
 
-	if (edid_ext == NULL)
+	if (i == edid->extensions)
 		return;
 
 	while (j < EDID_LENGTH) {
