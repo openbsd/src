ddc5737dc13b8d7b3ddb482920bfb00aa564afe9
tb <tb@openbsd.org>
2026-03-27 19:55:35 +0000

    rpki-client regress: initial plumbing to make this work with OpenSSL 4.0

--- regress/usr.sbin/rpki-client/openssl/unistd.h
+++ regress/usr.sbin/rpki-client/openssl/unistd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unistd.h,v 1.4 2025/08/19 11:31:48 job Exp $ */
+/*	$OpenBSD: unistd.h,v 1.5 2026/03/27 19:55:35 tb Exp $ */
 /*
  * Public domain
  * compatibility shim for OpenSSL 3
@@ -31,6 +31,7 @@ CMS_SignerInfo_get_version(CMS_SignerInfo *si, long *version)
 	return 1;
 }
 
+#if !HAVE_X509_CRL_GET0_SIGALG
 static inline const X509_ALGOR *
 X509_CRL_get0_tbs_sigalg(const X509_CRL *crl)
 {
@@ -39,5 +40,6 @@ X509_CRL_get0_tbs_sigalg(const X509_CRL *crl)
 	X509_CRL_get0_signature(crl, NULL, &alg);
 	return alg;
 }
+#endif
 
 #endif  /* ! RPKI_CLIENT_UNISTD_H */
