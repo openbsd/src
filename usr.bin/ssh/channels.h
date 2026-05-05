485ad110818c0be8dfb2ded1d3ef261af9eed9ad
djm <djm@openbsd.org>
2026-05-05 05:34:27 +0000

    classify dynamic-tcpip channels as bulk, not interactive; bz3958, ok markus@

--- usr.bin/ssh/channels.h
+++ usr.bin/ssh/channels.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: channels.h,v 1.164 2026/03/05 05:40:35 djm Exp $ */
+/* $OpenBSD: channels.h,v 1.165 2026/05/05 05:34:27 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -84,7 +84,8 @@
 
 /* default pattern-lists used to classify channel types as bulk */
 #define CHANNEL_BULK_TTY	""
-#define CHANNEL_BULK_NOTTY	"direct-*,forwarded-*,tun-*,x11-*,session*"
+#define CHANNEL_BULK_NOTTY	"direct-*,forwarded-*,dynamic-*,"
+				"tun-*,x11-*,session*"
 
 struct ssh;
 struct Channel;
