d871183a66a3dd43c9d34594527da28864a44535
jsg <jsg@openbsd.org>
2026-05-18 02:12:30 +0000

    drm/amdgpu/vcn3: Avoid overflow on msg bound check
    From Benjamin Cheng
    016b64a0313ea5346cf526e30c8d3e66aca10175 in linux-6.18.y/6.18.32
    e6e9faba8100628990cccd13f0f044a648c303cf in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/vcn_v3_0.c
+++ sys/dev/pci/drm/amd/amdgpu/vcn_v3_0.c
@@ -1971,6 +1971,7 @@ static int vcn_v3_0_dec_msg(struct amdgpu_cs_parser *p, struct amdgpu_job *job,
 
 	for (i = 0, msg = &msg[6]; i < num_buffers; ++i, msg += 4) {
 		uint32_t offset, size, *create;
+		uint64_t buf_end;
 
 		if (msg[0] != RDECODE_MESSAGE_CREATE)
 			continue;
@@ -1978,7 +1979,8 @@ static int vcn_v3_0_dec_msg(struct amdgpu_cs_parser *p, struct amdgpu_job *job,
 		offset = msg[1];
 		size = msg[2];
 
-		if (size < 4 || offset + size > end - addr) {
+		if (size < 4 || check_add_overflow(offset, size, &buf_end) ||
+		    buf_end > end - addr) {
 			DRM_ERROR("VCN message buffer exceeds BO bounds!\n");
 			r = -EINVAL;
 			goto out;
