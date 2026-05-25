322b5b202e9ac37cc6fb5bc1db4e62cd8996b6be
jsg <jsg@openbsd.org>
2026-05-25 09:18:44 +0000

    drm/amdgpu/vcn: set no_user_fence for VCN v5.0.0 enc ring
    From Yinjie Yao
    139a8a52ef4349c62129703b3a3e3a6ae4d634eb in linux-6.18.y/6.18.33
    8cae0ce77de492d7c31c1532a2e80c0c6e7e58cb in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/vcn_v5_0_0.c
+++ sys/dev/pci/drm/amd/amdgpu/vcn_v5_0_0.c
@@ -1207,6 +1207,7 @@ static const struct amdgpu_ring_funcs vcn_v5_0_0_unified_ring_vm_funcs = {
 	.type = AMDGPU_RING_TYPE_VCN_ENC,
 	.align_mask = 0x3f,
 	.nop = VCN_ENC_CMD_NO_OP,
+	.no_user_fence = true,
 	.get_rptr = vcn_v5_0_0_unified_ring_get_rptr,
 	.get_wptr = vcn_v5_0_0_unified_ring_get_wptr,
 	.set_wptr = vcn_v5_0_0_unified_ring_set_wptr,
