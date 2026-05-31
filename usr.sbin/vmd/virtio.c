54735d0f432ffccae5e55a230c6f25f614a5dcc2
bluhm <bluhm@openbsd.org>
2026-05-31 20:28:51 +0000

    Fix vmd(8) emulation for Linux guests.
    The memory safety checks introduced in previous commit were too
    strict and broke Linux guests running on top of vmd.  Adapt the
    checks to the variation of virtio 1.x that vmd implements.
    virtio 1.x doesn't need page alignment.  The used area offset needs
    to just be 4 byte aligned for some sanity, but not really critical.
    The descriptor table, available area, and used areas must be in
    guest memory ranges and not spill out allowing guest access to host
    memory by using hvaddr_mem() to check.
    
    from dv@; OK mlarkin@; tested by jmatthew@ and Remi Bougard

--- usr.sbin/vmd/virtio.c
+++ usr.sbin/vmd/virtio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: virtio.c,v 1.139 2026/05/28 17:14:38 deraadt Exp $	*/
+/*	$OpenBSD: virtio.c,v 1.140 2026/05/31 20:28:51 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2015 Mike Larkin <mlarkin@openbsd.org>
@@ -196,7 +196,7 @@ virtio_update_qa(struct virtio_dev *dev)
 {
 	struct virtio_vq_info *vq_info = NULL;
 	void *hva = NULL;
-	uint32_t expected_avail, expected_used, availoff, usedoff;
+	uint64_t availoff, usedoff, availsz, usedsz;
 
 	if (dev->driver_feature & VIRTIO_F_VERSION_1) {
 		if (dev->pci_cfg.queue_select >= dev->num_queues) {
@@ -213,20 +213,34 @@ virtio_update_qa(struct virtio_dev *dev)
 		vq_info->qs = dev->pci_cfg.queue_size;
 		vq_info->mask = vq_info->qs - 1;
 
-		/* Only enable the queue if the avail and used pointers are valid */
+		/*
+		 * Require the available (driver) and used (device) area to be
+		 * similar to Virtio 0.9 but support Virtio 1.x alignment.
+		 */
 		if (dev->pci_cfg.queue_avail < dev->pci_cfg.queue_desc ||
 		    dev->pci_cfg.queue_used < dev->pci_cfg.queue_desc) {
 			vq_info->vq_enabled = 0;
 			return;
 		}
 
-		/* Require the avail and used pointers to be set as per Virtio 0.9 */
-		expected_avail = sizeof(struct vring_desc) * vq_info->qs;
-		expected_used = VIRTQUEUE_ALIGN(expected_avail +
-		    sizeof(uint16_t) * (2 + vq_info->qs));
 		availoff = dev->pci_cfg.queue_avail - dev->pci_cfg.queue_desc;
 		usedoff = dev->pci_cfg.queue_used - dev->pci_cfg.queue_desc;
-		if (availoff != expected_avail || usedoff != expected_used) {
+		if (availoff > UINT32_MAX || usedoff > UINT32_MAX ||
+		    (usedoff & 3) != 0) {
+			vq_info->vq_enabled = 0;
+			return;
+		}
+
+		availsz = sizeof(uint16_t) * (2 + vq_info->qs);
+		usedsz = (sizeof(uint16_t) * 2) +
+		    (sizeof(struct vring_used_elem) * vq_info->qs);
+		hva = hvaddr_mem(dev->pci_cfg.queue_desc + availoff, availsz);
+		if (hva == NULL) {
+			vq_info->vq_enabled = 0;
+			return;
+		}
+		hva = hvaddr_mem(dev->pci_cfg.queue_desc + usedoff, usedsz);
+		if (hva == NULL) {
 			vq_info->vq_enabled = 0;
 			return;
 		}
