de15ecd0c7593a1f1a34ef1fdb568f2ccd3743bf
jsg <jsg@openbsd.org>
2026-06-02 03:50:47 +0000

    drm/i915/dp: Fix readback for target_rr in Adaptive Sync SDP
    From Ankit Nautiyal
    ad8e3d096fa1e2f8b1009731c6e0cdae7ebedf79 in linux-6.18.y/6.18.34
    f87abd0c6604fb6cc31cc86fc7ccc6a576924352 in mainline linux

--- sys/dev/pci/drm/i915/display/intel_dp.c
+++ sys/dev/pci/drm/i915/display/intel_dp.c
@@ -4899,7 +4899,7 @@ int intel_dp_as_sdp_unpack(struct drm_dp_as_sdp *as_sdp,
 	as_sdp->length = sdp->sdp_header.HB3 & DP_ADAPTIVE_SYNC_SDP_LENGTH;
 	as_sdp->mode = sdp->db[0] & DP_ADAPTIVE_SYNC_SDP_OPERATION_MODE;
 	as_sdp->vtotal = (sdp->db[2] << 8) | sdp->db[1];
-	as_sdp->target_rr = (u64)sdp->db[3] | ((u64)sdp->db[4] & 0x3);
+	as_sdp->target_rr = ((sdp->db[4] & 0x3) << 8) | sdp->db[3];
 	as_sdp->target_rr_divider = sdp->db[4] & 0x20 ? true : false;
 
 	return 0;
