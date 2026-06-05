13facf8de4382a91565046fef9d1b7c98a2c3788
nicm <nicm@openbsd.org>
2026-06-05 08:04:03 +0000

    Add a way to quote for command arguments and use for key in list-keys, GitHub issue 5153.

--- usr.bin/tmux/cmd-list-keys.c
+++ usr.bin/tmux/cmd-list-keys.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-list-keys.c,v 1.74 2026/04/02 09:58:14 nicm Exp $ */
+/* $OpenBSD: cmd-list-keys.c,v 1.75 2026/06/05 08:04:03 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -35,7 +35,7 @@
 	","							\
 	"bind-key #{?key_has_repeat,#{?key_repeat,-r,  },} "	\
 	"-T #{p|#{key_table_width}:key_table} "			\
-	"#{p|#{key_string_width}:key_string} "			\
+	"#{p|#{key_string_width}:#{q|a:key_string}} "		\
 	"#{key_command}}"
 
 static enum cmd_retval cmd_list_keys_exec(struct cmd *, struct cmdq_item *);
