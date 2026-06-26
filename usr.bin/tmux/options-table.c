d4158f12d96c795ced5353616b2ea0f71fdc3a2d
nicm <nicm@openbsd.org>
2026-06-26 12:41:08 +0000

    message-command-style should be inverse of message-style.

--- usr.bin/tmux/options-table.c
+++ usr.bin/tmux/options-table.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: options-table.c,v 1.225 2026/06/26 12:06:17 nicm Exp $ */
+/* $OpenBSD: options-table.c,v 1.226 2026/06/26 12:41:08 nicm Exp $ */
 
 /*
  * Copyright (c) 2011 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -911,7 +911,9 @@ const struct options_table_entry options_table[] = {
 	{ .name = "message-command-style",
 	  .type = OPTIONS_TABLE_STRING,
 	  .scope = OPTIONS_TABLE_SESSION,
-	  .default_str = "bg=themegreen,fg=themeblack,fill=themegreen",
+	  .default_str = "bg=themeblack,fg=themeyellow,"
+			 "#{?#{m/r:(^|#,)IS(PANE|MODE)($|#,),#{prompt_flags}},,"
+			 "fill=themeblack}",
 	  .flags = OPTIONS_TABLE_IS_STYLE,
 	  .separator = ",",
 	  .text = "Style of the command prompt when in command mode, if "
