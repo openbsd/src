0b481ee831598e57e02204b051f093bb0c4ebd72
nicm <nicm@openbsd.org>
2026-06-29 16:44:06 +0000

    Limit hyperlink URIs to 1024 bytes which seems enough and allows us not to have to worry about gigantic URIs in styles, part of a change from Moritz Angermann.

--- usr.bin/tmux/hyperlinks.c
+++ usr.bin/tmux/hyperlinks.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: hyperlinks.c,v 1.4 2024/08/27 07:49:07 nicm Exp $ */
+/* $OpenBSD: hyperlinks.c,v 1.5 2026/06/29 16:44:06 nicm Exp $ */
 
 /*
  * Copyright (c) 2021 Will <author@will.party>
@@ -42,6 +42,7 @@
  */
 
 #define MAX_HYPERLINKS 5000
+#define MAX_HYPERLINK_URI 1024
 
 static long long hyperlinks_next_external_id = 1;
 static u_int global_hyperlinks_count;
@@ -146,9 +147,13 @@ hyperlinks_put(struct hyperlinks *hl, const char *uri_in,
 		internal_id_in = "";
 
 	utf8_stravis(&uri, uri_in, VIS_OCTAL|VIS_CSTYLE);
-	utf8_stravis(&internal_id, internal_id_in, VIS_OCTAL|VIS_CSTYLE);
+	if (strlen(uri) > MAX_HYPERLINK_URI) {
+		free(uri);
+		return (0);
+	}
 
-	if (*internal_id_in != '\0') {
+	utf8_stravis(&internal_id, internal_id_in, VIS_OCTAL|VIS_CSTYLE);
+	if (*internal_id != '\0') {
 		find.uri = uri;
 		find.internal_id = internal_id;
 
