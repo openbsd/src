d2e81eb18e50973c87ae2c9c2ba348ff3ff89cf9
deraadt <deraadt@openbsd.org>
2026-07-16 06:21:08 +0000

    move to 8.0-beta (which means a few days of snapshot bumpiness)

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.216 2026/04/20 00:18:21 jsg Exp $
+#	$OpenBSD: newvers.sh,v 1.217 2026/07/16 06:21:10 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -68,13 +68,13 @@ id=`basename "${d}"`
 #	and disable POOL_DEBUG in sys/conf/GENERIC
 
 ost="OpenBSD"
-osr="7.9"
+osr="8.0"
 
 cat >vers.c <<eof
-#define STATUS "-current"		/* just after a release */
-#if 0
-#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
+#if 0
+#define STATUS "-current"		/* just after a release */
+#define STATUS ""			/* release */
 #define STATUS "-stable"		/* stable branch */
 #endif
 
