cdfad60cc5c19e23a25793161244c6ee4edb31d7
kettenis <kettenis@openbsd.org>
2026-07-22 20:07:22 +0000

    Add support for the Spacemit K3 SoC.  Fix a pasto in most of the Spacemit K1 UART reset control definitions spotted while working on this code.
    ok jsg@

--- sys/arch/riscv64/dev/smtclock.c
+++ sys/arch/riscv64/dev/smtclock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtclock.c,v 1.4 2026/04/13 12:02:19 kettenis Exp $	*/
+/*	$OpenBSD: smtclock.c,v 1.5 2026/07/22 20:07:22 kettenis Exp $	*/
 /*
  * Copyright (c) 2026 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -27,7 +27,7 @@
 #include <dev/ofw/ofw_misc.h>
 #include <dev/ofw/fdt.h>
 
-/* APBC clocks */
+/* K1 APBC clocks */
 #define K1_CLK_UART0		0
 #define K1_CLK_UART2		1
 #define K1_CLK_UART3		2
@@ -56,7 +56,7 @@
 #define K1_CLK_TWSI8_BUS	91
 #define K1_CLK_AIB_BUS		94
 
-/* APMU resets */
+/* K1 APBC resets */
 #define K1_RESET_UART0		0
 #define K1_RESET_UART2		1
 #define K1_RESET_UART3		2
@@ -75,7 +75,7 @@
 #define K1_RESET_TWSI7		38
 #define K1_RESET_TWSI8		39
 
-/* APMU clocks */
+/* K1 APMU clocks */
 #define K1_CLK_USB30		16
 #define K1_CLK_PCIE0_MASTER	28
 #define K1_CLK_PCIE0_SLAVE	29
@@ -89,7 +89,7 @@
 #define K1_CLK_EMAC0_BUS	37
 #define K1_CLK_EMAC1_BUS	39
 
-/* APMU resets */
+/* K1 APMU resets */
 #define K1_RESET_USB30_AHB	8
 #define K1_RESET_USB30_VCC	9
 #define K1_RESET_USB30_PHY	10
@@ -108,8 +108,73 @@
 #define K1_RESET_EMAC0		35
 #define K1_RESET_EMAC1		36
 
+/* K3 APBC clocks */
+#define K3_CLK_UART0		0
+#define K3_CLK_UART2		1
+#define K3_CLK_UART3		2
+#define K3_CLK_UART4		3
+#define K3_CLK_UART5		4
+#define K3_CLK_UART6		5
+#define K3_CLK_UART7		6
+#define K3_CLK_UART8		7
+#define K3_CLK_UART9		8
+#define K3_CLK_UART10		9
+#define K3_CLK_TWSI0		73
+#define K3_CLK_TWSI1		74
+#define K3_CLK_TWSI2		75
+#define K3_CLK_TWSI4		76
+#define K3_CLK_TWSI5		77
+#define K3_CLK_TWSI6		78
+#define K3_CLK_TWSI8		79
+#define K3_CLK_TWSI0_BUS	80
+#define K3_CLK_TWSI1_BUS	81
+#define K3_CLK_TWSI2_BUS	82
+#define K3_CLK_TWSI4_BUS	83
+#define K3_CLK_TWSI5_BUS	84
+#define K3_CLK_TWSI6_BUS	85
+#define K3_CLK_TWSI8_BUS	86
+#define K3_CLK_AIB		103
+#define K3_CLK_AIB_BUS		104
+
+/* K3 APBC resets */
+#define K3_RESET_UART0		0
+#define K3_RESET_UART2		1
+#define K3_RESET_UART3		2
+#define K3_RESET_UART4		3
+#define K3_RESET_UART5		4
+#define K3_RESET_UART6		5
+#define K3_RESET_UART7		6
+#define K3_RESET_UART8		7
+#define K3_RESET_UART9		8
+#define K3_RESET_UART10		9
+#define K3_RESET_TWSI0		35
+#define K3_RESET_TWSI1		36
+#define K3_RESET_TWSI2		37
+#define K3_RESET_TWSI4		38
+#define K3_RESET_TWSI5		39
+#define K3_RESET_TWSI6		40
+#define K3_RESET_TWSI8		41
+
+/* K3 APMU clocks */
+#define K3_CLK_USB2_BUS	30
+#define K3_CLK_EMAC0_BUS	66
+#define K3_CLK_EMAC0_RGMII_TX	69
+#define K3_CLK_EMAC1_BUS	70
+#define K3_CLK_EMAC1_RGMII_TX	73
+#define K3_CLK_EMAC2_BUS	74
+#define K3_CLK_EMAC2_RGMII_TX	77
+#define K3_CLK_AXI		-128
+
+/* K3 APMU resets */
+#define K3_RESET_USB2_AHB	16
+#define K3_RESET_USB2_VCC	17
+#define K3_RESET_USB2_PHY	18
+#define K3_RESET_EMAC0		67
+#define K3_RESET_EMAC1		68
+#define K3_RESET_EMAC2		69
+
 /* APBC registers */
-#define APBC_UART1_CLK_RST		0x0000
+#define APBC_UART0_CLK_RST		0x0000
 #define APBC_UART2_CLK_RST		0x0004
 #define APBC_TWSI8_CLK_RST		0x0020
 #define  APBC_TWSI8_CLK_RST_RST		(1U << 2)
@@ -130,10 +195,15 @@
 #define APBC_UART7_CLK_RST		0x0094
 #define APBC_UART8_CLK_RST		0x0098
 #define APBC_UART9_CLK_RST		0x009c
+#define APBC_UART10_CLK_RST		0x0154	/* only on K3 */
 #define  APBC_UARTX_CLK_RST_FNCLKSEL(x)	(((x) >> 4) & 0x7)
 
 /* APMU registers */
 #define APMU_USB_CLK_RES_CTRL		0x005c
+#define APMU_EMAC2_CLK_RST_CTRL		0x0248 /* only on K3 */
+#define APMU_ACLK_CTRL			0x0388
+#define  APMU_ACLK_CTRL_ACLK_DIV(x)	(((x) >> 1) & 0x3)
+#define  APMU_ACLK_CTRL_ACLK_SEL(x)	(((x) >> 0) & 0x1)
 #define APMU_PCIE_CLK_RES_CTRL_PORTA	0x03cc
 #define APMU_PCIE_CLK_RES_CTRL_PORTB	0x03d4
 #define APMU_PCIE_CLK_RES_CTRL_PORTC	0x03dc
@@ -163,7 +233,7 @@ struct smtreset {
 };
 
 static struct smtclock k1_apbc_clocks[] = {
-	{ K1_CLK_UART0, APBC_UART1_CLK_RST, 1 },
+	{ K1_CLK_UART0, APBC_UART0_CLK_RST, 1 },
 	{ K1_CLK_UART2, APBC_UART2_CLK_RST, 1 },
 	{ K1_CLK_UART3, APBC_UART3_CLK_RST, 1 },
 	{ K1_CLK_UART4, APBC_UART4_CLK_RST, 1 },
@@ -194,15 +264,15 @@ static struct smtclock k1_apbc_clocks[] = {
 };
 
 static struct smtreset k1_apbc_resets[] = {
-	{ K1_RESET_UART0, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART2, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART3, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART4, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART5, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART6, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART7, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART8, APBC_UART1_CLK_RST, 2, -1 },
-	{ K1_RESET_UART9, APBC_UART1_CLK_RST, 2, -1 },
+	{ K1_RESET_UART0, APBC_UART0_CLK_RST, 2, -1 },
+	{ K1_RESET_UART2, APBC_UART2_CLK_RST, 2, -1 },
+	{ K1_RESET_UART3, APBC_UART3_CLK_RST, 2, -1 },
+	{ K1_RESET_UART4, APBC_UART4_CLK_RST, 2, -1 },
+	{ K1_RESET_UART5, APBC_UART5_CLK_RST, 2, -1 },
+	{ K1_RESET_UART6, APBC_UART6_CLK_RST, 2, -1 },
+	{ K1_RESET_UART7, APBC_UART7_CLK_RST, 2, -1 },
+	{ K1_RESET_UART8, APBC_UART8_CLK_RST, 2, -1 },
+	{ K1_RESET_UART9, APBC_UART9_CLK_RST, 2, -1 },
 	{ K1_RESET_TWSI0, APBC_TWSI0_CLK_RST, 2, -1 },
 	{ K1_RESET_TWSI1, APBC_TWSI1_CLK_RST, 2, -1 },
 	{ K1_RESET_TWSI2, APBC_TWSI2_CLK_RST, 2, -1 },
@@ -251,6 +321,79 @@ static struct smtreset k1_apmu_resets[] = {
 	{ -1 },
 };
 
+static struct smtclock k3_apbc_clocks[] = {
+	{ K3_CLK_UART0, APBC_UART0_CLK_RST, 1 },
+	{ K3_CLK_UART2, APBC_UART2_CLK_RST, 1 },
+	{ K3_CLK_UART3, APBC_UART3_CLK_RST, 1 },
+	{ K3_CLK_UART4, APBC_UART4_CLK_RST, 1 },
+	{ K3_CLK_UART5, APBC_UART5_CLK_RST, 1 },
+	{ K3_CLK_UART6, APBC_UART6_CLK_RST, 1 },
+	{ K3_CLK_UART7, APBC_UART7_CLK_RST, 1 },
+	{ K3_CLK_UART8, APBC_UART8_CLK_RST, 1 },
+	{ K3_CLK_UART9, APBC_UART9_CLK_RST, 1 },
+	{ K3_CLK_UART10, APBC_UART10_CLK_RST, 1 },
+	{ K3_CLK_TWSI0, APBC_TWSI0_CLK_RST, 1 },
+	{ K3_CLK_TWSI1, APBC_TWSI1_CLK_RST, 1 },
+	{ K3_CLK_TWSI2, APBC_TWSI2_CLK_RST, 1 },
+	{ K3_CLK_TWSI4, APBC_TWSI4_CLK_RST, 1 },
+	{ K3_CLK_TWSI5, APBC_TWSI5_CLK_RST, 1 },
+	{ K3_CLK_TWSI6, APBC_TWSI6_CLK_RST, 1 },
+	{ K3_CLK_TWSI8, APBC_TWSI8_CLK_RST, 1 },
+	{ K3_CLK_TWSI0_BUS, APBC_TWSI0_CLK_RST, 0 },
+	{ K3_CLK_TWSI1_BUS, APBC_TWSI1_CLK_RST, 0 },
+	{ K3_CLK_TWSI2_BUS, APBC_TWSI2_CLK_RST, 0 },
+	{ K3_CLK_TWSI4_BUS, APBC_TWSI4_CLK_RST, 0 },
+	{ K3_CLK_TWSI5_BUS, APBC_TWSI5_CLK_RST, 0 },
+	{ K3_CLK_TWSI6_BUS, APBC_TWSI6_CLK_RST, 0 },
+	{ K3_CLK_TWSI8_BUS, APBC_TWSI8_CLK_RST, 0 },
+	{ K3_CLK_AIB, APBC_AIB_CLK_RST, 1 },
+	{ K3_CLK_AIB_BUS, APBC_AIB_CLK_RST, 0 },
+	{ -1 },
+};
+
+static struct smtreset k3_apbc_resets[] = {
+	{ K3_RESET_UART0, APBC_UART0_CLK_RST, 2, -1 },
+	{ K3_RESET_UART2, APBC_UART2_CLK_RST, 2, -1 },
+	{ K3_RESET_UART3, APBC_UART3_CLK_RST, 2, -1 },
+	{ K3_RESET_UART4, APBC_UART4_CLK_RST, 2, -1 },
+	{ K3_RESET_UART5, APBC_UART5_CLK_RST, 2, -1 },
+	{ K3_RESET_UART6, APBC_UART6_CLK_RST, 2, -1 },
+	{ K3_RESET_UART7, APBC_UART7_CLK_RST, 2, -1 },
+	{ K3_RESET_UART8, APBC_UART8_CLK_RST, 2, -1 },
+	{ K3_RESET_UART9, APBC_UART9_CLK_RST, 2, -1 },
+	{ K3_RESET_UART10, APBC_UART10_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI0, APBC_TWSI0_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI1, APBC_TWSI1_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI2, APBC_TWSI2_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI4, APBC_TWSI4_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI5, APBC_TWSI5_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI6, APBC_TWSI6_CLK_RST, 2, -1 },
+	{ K3_RESET_TWSI8, APBC_TWSI8_CLK_RST, 2, -1 },
+	{ -1 },
+};
+
+static struct smtclock k3_apmu_clocks[] = {
+	{ K3_CLK_USB2_BUS, APMU_USB_CLK_RES_CTRL, 1 },
+	{ K3_CLK_EMAC0_BUS, APMU_EMAC0_CLK_RST_CTRL, 0 },
+	{ K3_CLK_EMAC0_RGMII_TX, APMU_EMAC0_CLK_RST_CTRL, 8 },
+	{ K3_CLK_EMAC1_BUS, APMU_EMAC1_CLK_RST_CTRL, 0 },
+	{ K3_CLK_EMAC1_RGMII_TX, APMU_EMAC1_CLK_RST_CTRL, 8 },
+	{ K3_CLK_EMAC2_BUS, APMU_EMAC2_CLK_RST_CTRL, 0 },
+	{ K3_CLK_EMAC2_RGMII_TX, APMU_EMAC2_CLK_RST_CTRL, 8 },
+	{ K3_CLK_AXI, APMU_ACLK_CTRL, -1 },
+	{ -1 },
+};
+
+static struct smtreset k3_apmu_resets[] = {
+	{ K3_RESET_USB2_AHB, APMU_USB_CLK_RES_CTRL, -1, 0 },
+	{ K3_RESET_USB2_VCC, APMU_USB_CLK_RES_CTRL, -1, 2 },
+	{ K3_RESET_USB2_PHY, APMU_USB_CLK_RES_CTRL, -1, 3 },
+	{ K3_RESET_EMAC0, APMU_EMAC0_CLK_RST_CTRL, -1, 1 },
+	{ K3_RESET_EMAC1, APMU_EMAC1_CLK_RST_CTRL, -1, 1 },
+	{ K3_RESET_EMAC2, APMU_EMAC2_CLK_RST_CTRL, -1, 1 },
+	{ -1 },
+};
+
 struct smtclock_softc {
 	struct device		sc_dev;
 	bus_space_tag_t		sc_iot;
@@ -286,7 +429,9 @@ smtclock_match(struct device *parent, void *match, void *aux)
 	struct fdt_attach_args *faa = aux;
 
 	return OF_is_compatible(faa->fa_node, "spacemit,k1-syscon-apbc") ||
-	    OF_is_compatible(faa->fa_node, "spacemit,k1-syscon-apmu");
+	    OF_is_compatible(faa->fa_node, "spacemit,k1-syscon-apmu") ||
+	    OF_is_compatible(faa->fa_node, "spacemit,k3-syscon-apbc") ||
+	    OF_is_compatible(faa->fa_node, "spacemit,k3-syscon-apmu");
 }
 
 void
@@ -314,10 +459,18 @@ smtclock_attach(struct device *parent, struct device *self, void *aux)
 	if (OF_is_compatible(faa->fa_node, "spacemit,k1-syscon-apbc")) {
 		sc->sc_clocks = k1_apbc_clocks;
 		sc->sc_resets = k1_apbc_resets;
-	} else {
+	} else if (OF_is_compatible(faa->fa_node, "spacemit,k1-syscon-apmu")) {
 		sc->sc_clocks = k1_apmu_clocks;
 		sc->sc_resets = k1_apmu_resets;
+	} else if (OF_is_compatible(faa->fa_node, "spacemit,k3-syscon-apbc")) {
+		sc->sc_clocks = k3_apbc_clocks;
+		sc->sc_resets = k3_apbc_resets;
+	} else if (OF_is_compatible(faa->fa_node, "spacemit,k3-syscon-apmu")) {
+		sc->sc_clocks = k3_apmu_clocks;
+		sc->sc_resets = k3_apmu_resets;
 	}
+	KASSERT(sc->sc_clocks);
+	KASSERT(sc->sc_resets);
 
 	printf("\n");
 
@@ -340,7 +493,8 @@ smtclock_get_frequency(void *cookie, uint32_t *cells)
 	struct smtclock_softc *sc = cookie;
 	const struct smtclock *clock;
 	uint32_t idx = cells[0];
-	uint32_t reg;
+	uint32_t parent;
+	uint32_t div, reg;
 
 	for (clock = sc->sc_clocks; clock->idx != -1; clock++) {
 		if (clock->idx == idx)
@@ -374,6 +528,48 @@ smtclock_get_frequency(void *cookie, uint32_t *cells)
 		}
 	}
 
+	if (OF_is_compatible(sc->sc_node, "spacemit,k3-syscon-apbc")) {
+		switch (idx) {
+		case K3_CLK_UART0:
+		case K3_CLK_UART2:
+		case K3_CLK_UART3:
+		case K3_CLK_UART4:
+		case K3_CLK_UART5:
+		case K3_CLK_UART6:
+		case K3_CLK_UART7:
+		case K3_CLK_UART8:
+		case K3_CLK_UART9:
+		case K3_CLK_UART10:
+			reg = HREAD4(sc, clock->reg);
+			switch (APBC_UARTX_CLK_RST_FNCLKSEL(reg)) {
+			case 0:
+				return 57600000;
+			case 1:
+				return 14745600;
+			case 2:
+				return 48000000;
+			}
+			break;
+		}
+	}
+	if (OF_is_compatible(sc->sc_node, "spacemit,k3-syscon-apmu")) {
+		switch (idx) {
+		case K3_CLK_AXI:
+			reg = HREAD4(sc, clock->reg);
+			div = APMU_ACLK_CTRL_ACLK_DIV(reg);
+			switch (APMU_ACLK_CTRL_ACLK_SEL(reg)) {
+			case 0:
+				return 307200000 / (div + 1);
+			case 1:
+				return 409600000 / (div + 1);
+			}
+			break;
+		case K3_CLK_EMAC0_BUS:
+			parent = K3_CLK_AXI;
+			return smtclock_get_frequency(sc, &parent);
+		}
+	}
+
 fail:
 	printf("%s: 0x%08x\n", __func__, idx);
 	return 0;
@@ -400,7 +596,7 @@ smtclock_enable(void *cookie, uint32_t *cells, int on)
 			break;
 	}
 
-	if (clock->idx == -1) {
+	if (clock->idx == -1 || clock->bit == -1) {
 		printf("%s: 0x%08x\n", __func__, idx);
 		return;
 	}
