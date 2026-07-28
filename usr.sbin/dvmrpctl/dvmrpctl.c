d2c60852a609958a9bf1332a92bbb480c8478adf
claudio <claudio@openbsd.org>
2026-07-28 11:49:42 +0000

    Trivial switch from imsg_get() to imgbuf_get().
    The return value of imsg_get() was never used as a size.
    OK florian@

--- usr.sbin/dvmrpctl/dvmrpctl.c
+++ usr.sbin/dvmrpctl/dvmrpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dvmrpctl.c,v 1.21 2024/11/21 13:38:14 claudio Exp $ */
+/*	$OpenBSD: dvmrpctl.c,v 1.22 2026/07/28 11:49:42 claudio Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -184,8 +184,8 @@ main(int argc, char *argv[])
 			errx(1, "pipe closed");
 
 		while (!done) {
-			if ((n = imsg_get(ibuf, &imsg)) == -1)
-				errx(1, "imsg_get error");
+			if ((n = imsgbuf_get(ibuf, &imsg)) == -1)
+				errx(1, "imsgbuf_get error");
 			if (n == 0)
 				break;
 			switch (res->action) {
