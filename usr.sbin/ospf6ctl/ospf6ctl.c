2e3e6e176367205dab2f6e60b64febf076fcfb39
claudio <claudio@openbsd.org>
2026-07-28 11:49:04 +0000

    Trivial switch from imsg_get() to imgbuf_get().
    The return value of imsg_get() was never used as a size.
    OK florian@

--- usr.sbin/ospf6ctl/ospf6ctl.c
+++ usr.sbin/ospf6ctl/ospf6ctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospf6ctl.c,v 1.59 2024/11/21 13:38:14 claudio Exp $ */
+/*	$OpenBSD: ospf6ctl.c,v 1.60 2026/07/28 11:49:04 claudio Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -261,8 +261,8 @@ main(int argc, char *argv[])
 			errx(1, "pipe closed");
 
 		while (!done) {
-			if ((n = imsg_get(ibuf, &imsg)) == -1)
-				errx(1, "imsg_get error");
+			if ((n = imsgbuf_get(ibuf, &imsg)) == -1)
+				errx(1, "imsgbuf_get error");
 			if (n == 0)
 				break;
 			switch (res->action) {
