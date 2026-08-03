ab6a4f03c258b5465da69863f9226400aac9a053
claudio <claudio@openbsd.org>
2026-08-03 11:27:06 +0000

    imsgbuf_get() returns int, so adjust the type of 'n' to int.
    OK kirill@ rsadowski@

--- usr.sbin/relayd/proc.c
+++ usr.sbin/relayd/proc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.c,v 1.60 2026/07/14 08:30:09 martijn Exp $	*/
+/*	$OpenBSD: proc.c,v 1.61 2026/08/03 11:27:06 claudio Exp $	*/
 
 /*
  * Copyright (c) 2010 - 2016 Reyk Floeter <reyk@openbsd.org>
@@ -593,7 +593,7 @@ proc_dispatch(int fd, short event, void *arg)
 	struct privsep		*ps = p->p_ps;
 	struct imsgbuf		*ibuf;
 	struct imsg		 imsg;
-	ssize_t			 n;
+	int			 n;
 	int			 ver;
 	const char		*title;
 	struct privsep_fd	 pf;
