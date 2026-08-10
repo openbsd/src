6934dcd95324d4aa83db63660d7e253ce6be0429
jsg <jsg@openbsd.org>
2026-08-10 01:52:29 +0000

    add placeholders for unannounced devices with unknown pci ids

--- sys/dev/pci/drm/amd/amdgpu/amdgpu_devlist.h
+++ sys/dev/pci/drm/amd/amdgpu/amdgpu_devlist.h
@@ -280,10 +280,20 @@ static const struct pci_matchid amdgpu_devices[] = {
 	 */
 	{0x1002, 0x1902 },	/* Radeon 820M / 840M */
 
+	/* GC 11.5.4, DCN ?, APU, linux 7.0 */
+
+	/* GC 11.5.6, DCN ?, APU, linux 7.2 */
+
+	/* GC 11.7.0, DCN 4.2.0, APU, linux 7.2 */
+
+	/* GC 11.7.1, DCN 4.2.0, APU, linux 7.2 */
+
 	/* GC 12.0.0, DCN 4.0.1, dGPU, "Navi 44" */
 	{0x1002, 0x7590 },	/* Radeon RX 9050, 9060, 9060 XT */
 
 	/* GC 12.0.1, DCN 4.0.1, dGPU, "Navi 48" */
 	{0x1002, 0x7550 },	/* Radeon RX 9070, 9070 XT, 9070 GRE */
 	{0x1002, 0x7551 },	/* Radeon AI PRO R9700, R9700S, R9600D */
+
+	/* GC 12.1.0, DCN 4.0.1, dGPU, linux 7.0 */
 };
