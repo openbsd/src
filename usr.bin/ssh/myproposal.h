ab7ba9a6f458448c28d8ba2c4725ca85694fee22
djm <djm@openbsd.org>
2026-09-02 09:04:11 +0000

    reenable ssh-mldsa44-ed25519 at a low priority position

--- usr.bin/ssh/myproposal.h
+++ usr.bin/ssh/myproposal.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: myproposal.h,v 1.81 2026/09/02 00:37:59 djm Exp $ */
+/* $OpenBSD: myproposal.h,v 1.82 2026/09/02 09:04:11 djm Exp $ */
 
 /*
  * Copyright (c) 2000 Markus Friedl.  All rights reserved.
@@ -50,6 +50,7 @@
 	"webauthn-sk-ecdsa-sha2-nistp256-cert-v01@openssh.com," \
 	"rsa-sha2-512-cert-v01@openssh.com," \
 	"rsa-sha2-256-cert-v01@openssh.com," \
+	"ssh-mldsa44-ed25519-cert," \
 	"ssh-ed25519," \
 	"ecdsa-sha2-nistp256," \
 	"ecdsa-sha2-nistp384," \
@@ -58,7 +59,8 @@
 	"sk-ecdsa-sha2-nistp256@openssh.com," \
 	"webauthn-sk-ecdsa-sha2-nistp256@openssh.com," \
 	"rsa-sha2-512," \
-	"rsa-sha2-256"
+	"rsa-sha2-256," \
+	"ssh-mldsa44-ed25519"
 
 #define	KEX_SERVER_ENCRYPT \
 	"chacha20-poly1305@openssh.com," \
