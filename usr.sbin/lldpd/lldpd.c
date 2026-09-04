b6c2d001e78a864738d78ceba97eea8daa8610fc
dlg <dlg@openbsd.org>
2026-09-04 03:47:05 +0000

    add "mcast" to the pledges
    this is so it can take advantage of pledge_sockopt()s newfound
    understanding of the ethernet frame socket options.

--- usr.sbin/lldpd/lldpd.c
+++ usr.sbin/lldpd/lldpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lldpd.c,v 1.10 2026/08/23 16:07:19 deraadt Exp $ */
+/*	$OpenBSD: lldpd.c,v 1.11 2026/09/04 03:47:05 dlg Exp $ */
 
 /*
  * Copyright (c) 2024 David Gwynne <dlg@openbsd.org>
@@ -268,7 +268,7 @@ main(int argc, char *argv[])
 	if (!debug && rdaemon(devnull) == -1)
 		err(1, "unable to daemonize");
 
-	if (pledge("stdio unix route", NULL) == -1)
+	if (pledge("stdio unix route mcast", NULL) == -1)
 		err(1, "pledge");
 
 	event_init();
