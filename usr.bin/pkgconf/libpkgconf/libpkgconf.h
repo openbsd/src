64b90f8508af43da43de4f64670538f30c571afd
tb <tb@openbsd.org>
2026-09-09 07:05:49 +0000

    pkgconf: bump version to 3.0.7
    No other code change in pkgconf-lite

--- usr.bin/pkgconf/libpkgconf/libpkgconf.h
+++ usr.bin/pkgconf/libpkgconf/libpkgconf.h
@@ -71,8 +71,8 @@ typedef struct pkgconf_license_ pkgconf_license_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	30006
-#define LIBPKGCONF_VERSION_STR	"3.0.6"
+#define LIBPKGCONF_VERSION	30007
+#define LIBPKGCONF_VERSION_STR	"3.0.7"
 
 struct pkgconf_queue_ {
 	pkgconf_node_t iter;
