11cd0f263590316ca6de58487732b21f89b069c4
deraadt <deraadt@openbsd.org>
2026-09-10 03:51:11 +0000

    Export the new AUX_openbsd_execpath 4001 as well as the (soon to be removed) AUX_execpath. ok kettenis

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_elf.c,v 1.203 2026/09/08 15:33:26 deraadt Exp $	*/
+/*	$OpenBSD: exec_elf.c,v 1.204 2026/09/10 03:51:11 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996 Per Fogelstrom
@@ -1005,11 +1005,15 @@ exec_elf_fixup(struct proc *p, struct exec_package *epp)
 		a++;
 #endif /* __HAVE_CPU_HWCAP2 */
 
+		a->au_id = AUX_execpath;	/* XXX delete */
+		a->au_v = (vaddr_t)epp->ep_execpath;
+    		a++;
+
 		a->au_id = AUX_openbsd_timekeep;
 		a->au_v = p->p_p->ps_timekeep;
 		a++;
 
-		a->au_id = AUX_execpath;
+		a->au_id = AUX_openbsd_execpath;
 		a->au_v = (vaddr_t)epp->ep_execpath;
     		a++;
 
