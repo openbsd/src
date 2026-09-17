687c73e124b17526507b7209038fb50b9896162f
jan <jan@openbsd.org>
2026-09-17 15:00:12 +0000

    ice(4): reduced max. tso segs to 64
    With many ice(4) interfaces and many CPU cores we will run out of devbuf
    memory fast.  In worst cases tests, not more than 57 segs where used.
    Thus, we reduce the max. no. of segs per tso packet to support a larger
    number of interfaces interfaces.
    
    ok stsp@

--- sys/dev/pci/if_icevar.h
+++ sys/dev/pci/if_icevar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_icevar.h,v 1.12 2026/07/22 15:36:30 jan Exp $	*/
+/*	$OpenBSD: if_icevar.h,v 1.13 2026/09/17 15:00:12 jan Exp $	*/
 
 /*  Copyright (c) 2024, Intel Corporation
  *  All rights reserved.
@@ -764,7 +764,7 @@ enum ice_fw_modes {
 #define ICE_MIN_TSO_MSS		64
 
 #define ICE_MAX_TX_SEGS		8
-#define ICE_MAX_TSO_SEGS	128
+#define ICE_MAX_TSO_SEGS	64
 
 #define ICE_MAX_DMA_SEG_SIZE	((16*1024) - 1)
 
