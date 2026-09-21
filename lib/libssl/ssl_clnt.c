446f7cf698f7466d54f3a8a9765a9272b09a2319
jsing <jsing@openbsd.org>
2026-09-21 23:40:43 +0000

    Correct exit path from several calls to tls_key_share_peer_security()
    These should be goto err, rather than return 0, although only one of
    these actually matters. A client talking DHE to a server that presents
    an insufficiently strong key will result in a leak.
    
    ok beck@ joshua@ tb@

--- lib/libssl/ssl_clnt.c
+++ lib/libssl/ssl_clnt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_clnt.c,v 1.178 2026/09/21 23:25:03 jsing Exp $ */
+/* $OpenBSD: ssl_clnt.c,v 1.179 2026/09/21 23:40:43 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1219,7 +1219,7 @@ ssl3_get_server_kex_dhe(SSL *s, CBS *cbs)
 	if (!tls_key_share_peer_security(s, s->s3->hs.key_share)) {
 		SSLerror(s, SSL_R_DH_KEY_TOO_SMALL);
 		ssl3_send_alert(s, SSL3_AL_FATAL, SSL_AD_HANDSHAKE_FAILURE);
-		return 0;
+		goto err;
 	}
 
 	return 1;
@@ -1865,7 +1865,7 @@ ssl3_send_client_kex_dhe(SSL *s, CBB *cbb)
 	if (!tls_key_share_peer_security(s, s->s3->hs.key_share)) {
 		SSLerror(s, SSL_R_DH_KEY_TOO_SMALL);
 		ssl3_send_alert(s, SSL3_AL_FATAL, SSL_AD_HANDSHAKE_FAILURE);
-		return 0;
+		goto err;
 	}
 
 	if (!tls12_derive_master_secret(s, key, key_len))
--- lib/libssl/ssl_srvr.c
+++ lib/libssl/ssl_srvr.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_srvr.c,v 1.175 2026/09/21 23:37:20 jsing Exp $ */
+/* $OpenBSD: ssl_srvr.c,v 1.176 2026/09/21 23:40:43 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1356,7 +1356,7 @@ ssl3_send_server_kex_dhe(SSL *s, CBB *cbb)
 	if (!tls_key_share_peer_security(s, s->s3->hs.key_share)) {
 		SSLerror(s, SSL_R_DH_KEY_TOO_SMALL);
 		ssl3_send_alert(s, SSL3_AL_FATAL, SSL_AD_HANDSHAKE_FAILURE);
-		return 0;
+		goto err;
 	}
 
 	return 1;
