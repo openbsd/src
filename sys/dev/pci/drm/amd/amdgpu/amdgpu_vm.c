fbcb2de25887dafcc71628f27e9dca8503ccdf64
jsg <jsg@openbsd.org>
2026-09-22 04:18:39 +0000

    drm/amdgpu: Make amdgpu_vm_flush() non-failing in submission path
    From Srinivasan Shanmugam
    f90d671463defa0a1081dae4ec2be0f67f22b141 in linux-6.18.y/6.18.53
    37872d06f85f0349e7b1f0680aea77326f7c135e in mainline linux

--- sys/dev/pci/drm/amd/amdgpu/amdgpu_vm.c
+++ sys/dev/pci/drm/amd/amdgpu/amdgpu_vm.c
@@ -820,12 +820,9 @@ bool amdgpu_vm_need_pipeline_sync(struct amdgpu_ring *ring,
  * @need_pipe_sync: is pipe sync needed
  *
  * Emit a VM flush when it is necessary.
- *
- * Returns:
- * 0 on success, errno otherwise.
  */
-int amdgpu_vm_flush(struct amdgpu_ring *ring, struct amdgpu_job *job,
-		    bool need_pipe_sync)
+void amdgpu_vm_flush(struct amdgpu_ring *ring, struct amdgpu_job *job,
+		     bool need_pipe_sync)
 {
 	struct amdgpu_device *adev = ring->adev;
 	struct amdgpu_isolation *isolation =
@@ -871,7 +868,7 @@ int amdgpu_vm_flush(struct amdgpu_ring *ring, struct amdgpu_job *job,
 
 	if (!vm_flush_needed && !gds_switch_needed && !need_pipe_sync &&
 	    !cleaner_shader_needed)
-		return 0;
+		return;
 
 	amdgpu_ring_ib_begin(ring);
 
@@ -962,7 +959,6 @@ int amdgpu_vm_flush(struct amdgpu_ring *ring, struct amdgpu_job *job,
 	}
 
 	amdgpu_ring_ib_end(ring);
-	return 0;
 }
 
 /**
