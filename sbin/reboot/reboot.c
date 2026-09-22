21d1aaee0d3faf8c891ec6c79f083858881db5ae
deraadt <deraadt@openbsd.org>
2026-09-22 15:05:21 +0000

    oops not ready

--- sbin/reboot/reboot.c
+++ sbin/reboot/reboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reboot.c,v 1.40 2026/09/22 15:04:44 deraadt Exp $	*/
+/*	$OpenBSD: reboot.c,v 1.41 2026/09/22 15:05:21 deraadt Exp $	*/
 /*	$NetBSD: reboot.c,v 1.8 1995/10/05 05:36:22 mycroft Exp $	*/
 
 /*
@@ -135,9 +135,6 @@ main(int argc, char *argv[])
 	}
 #endif /* CPU_LIDACTION */
 
-	if (isatty(STDOUT_FILENO))
-		tcdrain(STDOUT_FILENO);
-
 	if (qflag) {
 		reboot(howto);
 		err(1, "reboot");
@@ -220,9 +217,6 @@ main(int argc, char *argv[])
 				howto |= RB_POWERDOWN;
 		}
 	}
-
-	if (isatty(STDOUT_FILENO))
-		tcdrain(STDOUT_FILENO);
 
 	/*
 	 * Point of no return, block all signals so we are sure to
